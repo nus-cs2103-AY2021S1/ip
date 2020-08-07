684d09dfccfe9ca1d52ea2973a3439d565e157a5
Junhua <jhsibeizai@gmail.com>
2020-08-07 18:42:21 +0800

    Added Task to allow tasks to be added

---
+++ src/main/java/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+
+    private final String message;
+
+    public Task(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+}
