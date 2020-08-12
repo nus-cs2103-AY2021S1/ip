5e5e8fd654f8d5edef2a7f504c1c157e7a499f98
Michaeliaaa <michaelia0816@me.com>
2020-08-13 02:45:06 +0800

    Level 3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,8 +15,8 @@ public class Duke {
                     + "\n       ░     ░  ░ ░        ░   ░  ░";
 
         String greet = "\n____________________________________________________________"
-                     + "\nHi, I'm Mich!"
-                     + "\nHow can I help you today?"
+                     + "\n Hi Michaelia, I am your productivity tracker bot."
+                     + "\n Start adding your tasks to the to-do list right now!"
                      + "\n____________________________________________________________";
 
         System.out.println(logo + greet);
@@ -24,14 +24,14 @@ public class Duke {
 
     public static void echo(String str) {
         String echo = "   ____________________________________________________________"
-                    + "\n   added: " + str
+                    + "\n    added: " + str
                     + "\n   ____________________________________________________________";
         System.out.println(echo);
     }
 
     public static void bye() {
         String bye = "   ____________________________________________________________"
-                   + "\n   Bye! Hope to see you again soon."
+                   + "\n    Bye! Hope to see you again soon."
                    + "\n   ____________________________________________________________";
         System.out.println(bye);
     }
@@ -40,27 +40,38 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         ArrayList<String> toDoList = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         greeting();
 
-        while (true) {
-            String input = sc.nextLine();
+        String input = sc.nextLine();
+        while (!input.isEmpty()) {
             if (input.equals("bye")) {
                 bye();
                 break;
             } else if (input.equals("list")) {
-                String lst = "   ____________________________________________________________";
+                String lst = "   ____________________________________________________________"
+                            + "\n    Here are the tasks in your list:";
                 for (int i = 0; i < toDoList.size(); i++) {
                     int index = i + 1;
-                    lst += "\n   " + index + ". " + toDoList.get(i);
+                    lst += "\n    " + index + ". " + tasks.get(i);
                 }
                 lst += "\n   ____________________________________________________________";
                 System.out.println(lst);
+            } else if (input.length() > 4 && input.substring(0, 4).equals("done")) {
+                int index = Integer.parseInt(input.substring(5));
+                tasks.get(index - 1).markAsDone();
+                String done = "   ____________________________________________________________"
+                            + "\n    Nice! I've marked this task as done:\n      "
+                            + tasks.get(index - 1)
+                            + "\n   ____________________________________________________________";
+                System.out.println(done);
             } else {
                 toDoList.add(input);
+                tasks.add(new Task(input));
                 echo(input);
             }
+            input = sc.nextLine();
         }
-
     }
 }
