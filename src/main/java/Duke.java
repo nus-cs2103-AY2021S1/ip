10c26110967747c31653474dec159812ccd2c19a
Michaeliaaa <michaelia0816@me.com>
2020-08-13 02:47:17 +0800

    Revert "Level 3: Mark as Done"
    This reverts commit 5e5e8fd6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,8 +15,8 @@ public class Duke {
                     + "\n       ░     ░  ░ ░        ░   ░  ░";
 
         String greet = "\n____________________________________________________________"
-                     + "\n Hi Michaelia, I am your productivity tracker bot."
-                     + "\n Start adding your tasks to the to-do list right now!"
+                     + "\nHi, I'm Mich!"
+                     + "\nHow can I help you today?"
                      + "\n____________________________________________________________";
 
         System.out.println(logo + greet);
@@ -24,14 +24,14 @@ public class Duke {
 
     public static void echo(String str) {
         String echo = "   ____________________________________________________________"
-                    + "\n    added: " + str
+                    + "\n   added: " + str
                     + "\n   ____________________________________________________________";
         System.out.println(echo);
     }
 
     public static void bye() {
         String bye = "   ____________________________________________________________"
-                   + "\n    Bye! Hope to see you again soon."
+                   + "\n   Bye! Hope to see you again soon."
                    + "\n   ____________________________________________________________";
         System.out.println(bye);
     }
@@ -40,38 +40,27 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         ArrayList<String> toDoList = new ArrayList<>();
-        ArrayList<Task> tasks = new ArrayList<>();
 
         greeting();
 
-        String input = sc.nextLine();
-        while (!input.isEmpty()) {
+        while (true) {
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 bye();
                 break;
             } else if (input.equals("list")) {
-                String lst = "   ____________________________________________________________"
-                            + "\n    Here are the tasks in your list:";
+                String lst = "   ____________________________________________________________";
                 for (int i = 0; i < toDoList.size(); i++) {
                     int index = i + 1;
-                    lst += "\n    " + index + ". " + tasks.get(i);
+                    lst += "\n   " + index + ". " + toDoList.get(i);
                 }
                 lst += "\n   ____________________________________________________________";
                 System.out.println(lst);
-            } else if (input.length() > 4 && input.substring(0, 4).equals("done")) {
-                int index = Integer.parseInt(input.substring(5));
-                tasks.get(index - 1).markAsDone();
-                String done = "   ____________________________________________________________"
-                            + "\n    Nice! I've marked this task as done:\n      "
-                            + tasks.get(index - 1)
-                            + "\n   ____________________________________________________________";
-                System.out.println(done);
             } else {
                 toDoList.add(input);
-                tasks.add(new Task(input));
                 echo(input);
             }
-            input = sc.nextLine();
         }
+
     }
 }
