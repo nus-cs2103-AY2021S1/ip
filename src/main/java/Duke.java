c1fd5216d3b510096366bc280ef2b2a5b3861519
erisjacey <eris_jacey@Hotmail.com>
2020-08-13 18:39:20 +0800

    Add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,21 +10,45 @@ public class Duke {
                 + "| |_| | |_| |   <  __/  __|  |\n"
                 + "|____/ \\__,_|_|\\_\\___|  \\____/\n";
         System.out.println(logo);
-        printChatWindow("Greetings! I'm Duke J.", "What can I do for you?");
+        printGeneralChatWindow("Greetings! I'm Duke J.", "What can I do for you?");
+
+        // Assuming no more than 100 tasks
+        final int taskCapacity = 100;
+
+        // Initialise a fixed array of tasks to store
+        List<String> tasks = new ArrayList<>(taskCapacity);
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         // Loop continues echoing input until input == "bye"
         while (!input.equals("bye")) {
-            printChatWindow(input);
+            if (input.equals("list")) {
+                printTasksChatWindow(tasks);
+            } else {
+                tasks.add(input);
+                printGeneralChatWindow("added: " + input);
+            }
             input = sc.nextLine();
         }
 
-        printChatWindow("Have a nice day. Goodbye!");
+        printGeneralChatWindow("Have a nice day. Goodbye!");
     }
 
-    public static void printChatWindow(String... messages) {
+    public static void printTasksChatWindow(List<String> tasks) {
+        String indent = "    ";
+        String border = "________________________________";
+        int index = 0;
+        System.out.printf("%s%s\n", indent, border);
+
+        for (String task : tasks) {
+            System.out.printf("%s%d. %s\n", indent, ++index, task);
+        }
+
+        System.out.printf("%s%s\n", indent, border);
+    }
+
+    public static void printGeneralChatWindow(String... messages) {
         String indent = "    ";
         String border = "________________________________";
         System.out.printf("%s%s\n", indent, border);
@@ -32,6 +58,5 @@ public class Duke {
         }
 
         System.out.printf("%s%s\n", indent, border);
-
     }
 }
