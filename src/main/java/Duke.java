e25be4523d82a090a9cbc53d6d58fb2e014cabb4
ruilingk <e0426268@u.nus.edu>
2020-08-13 23:03:47 +0800

    Add Level-2 and personalised the bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,48 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-//        String logo = "";
-//                " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-
-        String breaker = "\t_________________________________________";
-        String greetings = breaker + "\n\tHello! I'm Duke" +
-                "\n\tWhat can I do for you?\n" + breaker;
-        String exit = "Bye. Hope to see you again soon!";
-
-        System.out.println(greetings);
-
         Scanner sc = new Scanner(System.in);
+        String logo = "\n" +
+                "\t███████╗ ██████╗  ██████╗██╗   ██╗███████╗   \n" +
+                "\t██╔════╝██╔═══██╗██╔════╝██║   ██║██╔════╝   \n" +
+                "\t█████╗  ██║   ██║██║     ██║   ██║███████╗   \n" +
+                "\t██╔══╝  ██║   ██║██║     ██║   ██║╚════██║   \n" +
+                "\t██║     ╚██████╔╝╚██████╗╚██████╔╝███████║██╗\n" +
+                "\t╚═╝      ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝╚═╝\n";
+        System.out.println("Welcome to " + logo);
+
+        String divider = "\t_________________________________________\n";
+        String greetings = divider +
+                "\tI am Pocus, your personal assistant!" +
+                "\n\tMay I know your name?\n" + divider;
+        System.out.print(greetings);
+
+        String name = sc.nextLine();
+        String enquiry = divider +
+                "\tHi there, " + name + "!" +
+                "\n\tHow can I help you today?\n" + divider;
+        System.out.print(enquiry);
+
+        ArrayList<String> tasks = new ArrayList<>();
         String input = "";
         while (sc.hasNext()) {
             input = sc.nextLine();
-            if (!input.equals("bye")) {
-                System.out.println(breaker + "\n\t" + input + "\n" + breaker);
-            } else {
-                System.out.println(breaker + "\n\t" + exit + "\n" + breaker);
+            if (input.equals("bye")) {
+                String exit = "\tHopefully I helped you today. Bye! :)\n";
+                System.out.print(divider + exit + divider);
                 break;
+            } else if (input.equals("list")) {
+                System.out.print(divider);
+                for (int i = 0; i < tasks.size(); i++) {
+                    int number = i + 1;
+                    System.out.print("\t" + number + ": " + tasks.get(i) + "\n");
+                }
+                System.out.print(divider);
+            } else {
+                tasks.add(input);
+                System.out.print(divider + "\t added: " + input + "\n" + divider);
             }
         }
         sc.close();
