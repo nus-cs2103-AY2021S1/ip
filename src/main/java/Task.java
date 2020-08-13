184e709985770013818dd7acac81209fd0848fd2
erisjacey <eris_jacey@Hotmail.com>
2020-08-13 22:14:37 +0800

    Add Automated Text UI Testing

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,15 +1,10 @@
 public class Task {
     protected String description;
     protected boolean isDone;
-    protected TaskType taskType;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public String getStatusIcon() {
