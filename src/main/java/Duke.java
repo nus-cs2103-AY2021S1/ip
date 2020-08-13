8df824158ee229497d4d85b8d941ed8069d4a1d2
erisjacey <eris_jacey@Hotmail.com>
2020-08-13 14:48:54 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ____        _         -----------\n"
+                + "|  _ \\ _   _| | _____  \\__    __/\n"
+                + "| | | | | | | |/ / _ \\    |  |\n"
+                + "| |_| | |_| |   <  __/  __|  |\n"
+                + "|____/ \\__,_|_|\\_\\___|  \\____/\n";
+        System.out.println(logo);
+        printChatWindow("Greetings! I'm Duke J.", "What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        // Loop continues echoing input until input == "bye"
+        while (!input.equals("bye")) {
+            printChatWindow(input);
+            input = sc.next();
+        }
+
+        printChatWindow("Have a nice day. Goodbye!");
+    }
+
+    public static void printChatWindow(String... messages) {
+        String indent = "    ";
+        String border = "________________________________";
+        System.out.printf("%s%s\n", indent, border);
+
+        for (String message : messages) {
+            System.out.printf("%s%s\n", indent, message);
+        }
+
+        System.out.printf("%s%s\n", indent, border);
+
     }
 }
