9caafca83539bc740934e4744b0d99f9346b205c
ruilingk <e0426268@u.nus.edu>
2020-08-13 23:41:55 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String taskName;
+    protected boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return isDone
+                ? "\u2713"
+                : "\u2718"; // return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.taskName;
+    }
+}
