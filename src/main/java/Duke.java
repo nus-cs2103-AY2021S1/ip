772c624949696676f1721a872baa883044fdded3
IsaacTin <tin.isaac@yahoo.com.sg>
2020-08-15 00:54:28 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
         String logo = " ______  ___       __         __        _____\n"
                      + "   |    /         /  \\       /  \\     /\n"
                      + "   |    \\___     /____\\     /____\\   |\n"
@@ -11,13 +15,18 @@ public class Duke {
         System.out.println("Hello! I'm\n" + logo + "\nWhat can I do for you?");
         String next = "";
         while (true) {
-          next = sc.nextLine();
-          if (next.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
-            break;
-          } else {
-            System.out.println(next);
-          }
-        }
+            next = sc.nextLine();
+            if (next.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else if (next.equals("list")) {
+                for (String i :list) {
+                  System.out.println(list.indexOf(i)+1 + ". " + i);
+                }
+            } else {
+                System.out.println("added: " + next);
+                list.add(next);
+                }
+            }
     }
 }
