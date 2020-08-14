3ff66f257208d930f836208446c203158ae8a0c1
Bobby Law <zeranium97@gmail.com>
2020-08-14 15:32:33 +0800

    Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,38 @@
 import java.util.Scanner;
 
 public class Duke {
+    static int count = 0;
+    static String[] list = new String[100];
+
+    public static void addList(String str) {
+        list[count] = str;
+        count++;
+        System.out.println("added: " + str);
+    }
+    public static void printList() {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null) {
+                System.out.println((i + 1) + ". " + list[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         while(sc.hasNext()) {
-            String command = sc.next();
+            String command = sc.nextLine();
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equals("list")) {
+                printList();
+            } else {
+                addList(command);
             }
-            System.out.println(command);
+
         }
         sc.close();
     }
