f5f856e6e72cda867e82ada2409eab4d8c7d3f1c
ktaekwon000 <ktaekwon000@gmail.com>
2020-08-14 23:19:05 +0800

    Implement A-TextUiTesting

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -12,7 +12,7 @@ public abstract class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "Y" : "X"); //return tick or X symbols
     }
 
     @Override
