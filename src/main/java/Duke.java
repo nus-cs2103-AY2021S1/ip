5fcaecd9d77d1841e0563bc45df37c521d699d00
aqcd <aquajet173@gmail.com>
2020-08-14 16:42:38 +0800

    implement Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+        echo(sc);
+    }
+
+    private static void greet() {
+        System.out.println(">> Beep Boop. I am Aq-bot.\n>> How can I help?");
+    }
+
+    private static void echo(Scanner sc) {
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(">> Bye! Hope I helped!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
