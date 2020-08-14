9deeb57575dc7f966eaffad10aa8ca823b27b14e
gohsiauchiak <gsc928@gmail.com>
2020-08-14 17:54:43 +0800

    Level-2

--- src/main/java/Viscount.java
+++ src/main/java/Viscount.java
@@ -1,4 +1,6 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Viscount {
     private static final String VISCOUNT_LOGO = "        _  _____  _____                  _    \n" +
@@ -9,6 +11,8 @@ public class Viscount {
             "   \\_/ |_|_____/ \\_____\\___/ \\__,_|_| |_|\\__|";
     private static final String HORIZONTAL_LINE = "__________________________________________________";
 
+    private static List<String> list = new ArrayList<>();
+
     private static void printLogo() {
         System.out.println(Viscount.VISCOUNT_LOGO);
     }
@@ -17,14 +21,37 @@ public class Viscount {
         System.out.println(Viscount.HORIZONTAL_LINE);
         System.out.println(message);
         System.out.println(Viscount.HORIZONTAL_LINE);
+        System.out.println();
     }
 
-    private static void echo() {
+    private static String listToString() {
+        String result = "";
+
+        for (int i = 0; i < list.size(); i++) {
+            result += (i == list.size() - 1)
+                ? String.format("%d. %s", i + 1, list.get(i))
+                : String.format("%d. %s\n", i + 1, list.get(i));
+        }
+
+        return result;
+    }
+
+    private static void addToList(String text) {
+        list.add(text);
+    }
+
+    private static void chat() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            Viscount.speak(input);
+            if (input.equals("list")) {
+                Viscount.speak(Viscount.listToString());
+            } else {
+                Viscount.addToList(input);
+                Viscount.speak(String.format("added: %s", input));
+            }
+
             input = sc.nextLine();
         }
     }
@@ -34,7 +61,7 @@ public class Viscount {
 
         Viscount.speak("Good day to you sir/ma'am! I'm Viscount!\nWhat can I do for you on this blessed day?");
 
-        Viscount.echo();
+        Viscount.chat();
 
         Viscount.speak("Farewell, and hope to see you again!");
     }
