487f998d444b1a16224abdf9d8b0705534ebf3be
MorningLit <ambroseliew1998@gmail.com>
2020-08-14 20:47:21 +0800

    Level-3

---
+++ src/main/java/Chore.java
@@ -0,0 +1,30 @@
+public class Chore {
+    protected String description;
+    protected boolean isDone;
+
+    public Chore(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getIsDone() {
+        return isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s",getStatusIcon(), description);
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,21 +3,33 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static List<String> toDoList = new ArrayList<>();
+    public static List<Chore> toDoList = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         welcomeMessage();
-        String inputLine = sc.nextLine();
+        String inputLine = sc.nextLine().trim();
         while (!inputLine.equals("bye")) {
+            String[] arr = inputLine.split(" ");
             if (inputLine.equals("list")) {
                 displayList();
-            } else {
+            }
+            else if (arr.length == 2 && arr[0].equals("done") && isInteger(arr[1])) {
+                int num = Integer.parseInt(arr[1]);
+                if (num >= 1 && num <= toDoList.size()) {
+                    doneTask(num);
+                }
+                else {
+                    invalidRangeError();
+                }
+            }
+            else {
                 addToDo(inputLine);
             }
             inputLine = sc.nextLine();
         }
         goodbyeMessage();
+        sc.close();
     }
 
     private static void hrTag() {
@@ -34,14 +46,15 @@ public class Duke {
     private static void addToDo(String toDo) {
         hrTag();
         System.out.println("Chore added: " + toDo);
-        toDoList.add(toDo);
+        toDoList.add(new Chore(toDo));
         hrTag();
     }
 
     private static void displayList() {
         hrTag();
+        System.out.println("Here are the chores you have left:");
         for (int i = 0; i < toDoList.size(); i++) {
-            System.out.println((i+1) + ". " + toDoList.get(i));
+            System.out.println((i+1) + "." + toDoList.get(i));
         }
         hrTag();
     }
@@ -49,6 +62,38 @@ public class Duke {
     private static void goodbyeMessage() {
         hrTag();
         System.out.println("Goodbye my child!");
+        hrTag();
+    }
+
+    private static boolean isInteger(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    private static void doneTask(int num) {
+        hrTag();
+        if (toDoList.get(num - 1).getIsDone()) {
+            System.out.println(String.format("Sorry! You have already completed '%s'.",
+                    toDoList.get(num - 1).getDescription()));
+        } else {
+            System.out.println(String.format("Great job son! I'll mark '%s' as done for you. ^^",
+                    toDoList.get(num - 1).getDescription()));
+            toDoList.get(num - 1).markAsDone();
+        }
+        hrTag();
+    }
+
+    private static void invalidRangeError() {
+        hrTag();
+        System.out.println("Uh oh! That number looks like it is out of range. Check again!");
+        // can add feature for user to add todo
         hrTag();
     }
 }
