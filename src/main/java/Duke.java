fa082df9e57468b009083eda8c616c8268a16474
Ben-Hanan <benhanan98@gmail.com>
2020-08-14 19:38:52 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner sc = new Scanner(System.in);
+
+        String indent = "    ";
+        String textIndent = "     ";
+        String divide = indent + "____________________________________________________________\n";
+
+        String exitMessage = textIndent + "Bye. Hope to see you again soon!\n";
+        System.out.println(divide + textIndent + "Hello! I'm Duke\n" + textIndent + "What can I do for you?\n" + divide);
+
+        while(sc.hasNext()) {
+            String text = sc.nextLine();
+            if (text.equals("bye")) {
+                System.out.println(divide + exitMessage + divide);
+                break;
+            }
+            System.out.println(divide + textIndent + text + "\n" + divide);
+
+        }
     }
 }
