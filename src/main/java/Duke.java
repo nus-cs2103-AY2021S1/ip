1d96c6ec819d03fe0cb5fe58780c8d4d651c0441
khinkhinn <2xkhin@gmail.com>
2020-08-14 23:48:11 +0800

    Implemented Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> arr = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,14 +18,24 @@ public class Duke {
         System.out.println("______________________");
 
         String s = sc.nextLine();
+        int counter = 1;
 
-        while (!s.equals("bye")) {
-            System.out.println(s);
-            System.out.println("______________________");
-            s = sc.nextLine();
+        while (1 == 1) {
+            if (s.equals("bye")) {
+                System.out.println("ok u can leave lmao");
+                System.out.println("______________________");
+                break;
+            } else if (s.equals("list")) {
+                arr.forEach(System.out::println);
+                System.out.println("______________________");
+                s = sc.nextLine();
+            } else {
+                System.out.println("added: " + s + "\nyoure welcome ;)");
+                System.out.println("______________________");
+                arr.add(Integer.toString(counter) + ". " + s);
+                counter++;
+                s = sc.nextLine();
+            }
         }
-
-        System.out.println("ok u can leave lmao");
-        System.out.println("______________________");
     }
 }
