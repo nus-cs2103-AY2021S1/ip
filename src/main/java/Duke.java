41a213518bd3aaa3796cd486d0e834e0172eea2e
wilinetan <wilinetan26@gmail.com>
2020-08-14 22:26:48 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
 public class Duke {
+    static String[] inputs = new String[100];
+    static int index = 0;
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
+            }
+            if (input.equals("list")) {
+                for (int i = 0; i < 100; i++) {
+                    if (inputs[i] == null) {
+                        break;
+                    }
+                    System.out.println((i + 1) + ". " + inputs[i]);
+                }
             } else {
-                System.out.println(input);
+                inputs[index] = input;
+                index++;
+                System.out.println("added: " + input);
             }
         }
     }
