ed75e57ab0cdd83332645b29f9c9b90b6082f045
zhiayang <zhiayang@nascentnoodle.com>
2020-08-14 19:27:59 +0800

    First commit; Implement level 1

---
+++ src/main/java/Bot.java
@@ -0,0 +1,45 @@
+// Bot.java
+// Copyright (c) 2020, zhiayang, Apache License 2.0.
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Bot {
+    private String name;
+
+    public Bot(String name) {
+        this.name = name;
+    }
+
+    private void printMessage(List<String> msgs) {
+        var line = "―――――――――――――――――――――――――――――――――――――――――";
+        System.out.printf("\n  %s\n", line);
+        for (var m : msgs) {
+            System.out.printf("  %s\n", m);
+        }
+
+        System.out.printf("  %s\n\n", line);
+    }
+
+    public void greet() {
+        this.printMessage(Arrays.asList(
+            String.format("Hello, I'm %s", this.name),
+            String.format("What can I do for you?")
+        ));
+    }
+
+    public boolean processCommand(String cmd) {
+        if (cmd.equals("bye")) {
+            this.printMessage(Arrays.asList(
+                String.format("goodbye.")
+            ));
+            return false;
+        } else {
+            this.printMessage(Arrays.asList(
+                cmd
+            ));
+            return true;
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+// Main.java
+// Copyright (c) 2020, zhiayang, Apache License 2.0.
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static String LINE = "____________________________________________________________";
+
+
+
+    public static void main(String[] args) {
+        var bot = new Bot("ikurabowl");
+
+        var sc = new Scanner(System.in);
+
+        bot.greet();
+        while (bot.processCommand(sc.nextLine())) {
+        }
+    }
+}
