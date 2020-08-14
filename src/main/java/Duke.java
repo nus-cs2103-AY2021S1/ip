c7a58e0076156eaf1e12665c4d7e568bb0a1ba65
tohyuting <tohyuting@gmail.com>
2020-08-14 21:00:04 +0800

    Level 2 - Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,13 +11,24 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         String lines = "    ____________________________________________________________";
-        String default_Greeting = lines + "\n" + "     Hello! I'm Duke \n" + "     What can I do for you?\n" + lines + "\n";
-        System.out.println(default_Greeting);
+        String defaultGreeting = lines + "\n" + "     Hello! I'm Duke \n" + "     What can I do for you?\n" + lines + "\n";
+        System.out.println(defaultGreeting);
         Scanner sc = new Scanner(System.in);
+        List<String> currentList = new ArrayList<>();
         String input = sc.nextLine();
         while (!input.equals("bye")){
-            String response = lines + "\n      " + input + "\n" + lines + "\n";
-            System.out.println(response);
+            if (!input.equals("list")) {
+                String response = lines + "\n      added: " + input + "\n" + lines + "\n";
+                System.out.println(response);
+                currentList.add(input);
+            } else if (input.equals("list")){
+                System.out.println(lines);
+                for(int i = 0; i < currentList.size(); i++) {
+                    String currentLine = "      "+ (i + 1) + ". " + currentList.get(i);
+                    System.out.println(currentLine);
+                }
+                System.out.println(lines);
+            }
             input = sc.nextLine();
         }
         String end_Greeting = lines + "\n" + "      Bye. Hope to see you again soon!\n" + lines;
