bc73cfa67a26d028d96ad675189e3c6c33d24bb4
justacasul <zhengdl98@gmail.com>
2020-08-14 15:11:01 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,35 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    static ArrayList<String> storage = new ArrayList<>();
+
+    static class Task {
+        private boolean completion;
+        private String name;
+
+        Task(String name) {
+            this.name = name;
+            this.completion = false;
+        }
+
+        void complete() {
+            completion = true;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getCompletion() {
+            if (completion) {
+                return "[✓]";
+            } else {
+                return "[✗]";
+            }
+        }
+    }
+
+    // Task storage
+    static ArrayList<Task> storage = new ArrayList<>();
 
     // border line
     public static void line() {
@@ -18,16 +46,26 @@ public class Duke {
 
     // stores user inputs in storage
     public static void store(String item) {
-        storage.add(item);
+        storage.add(new Task(item));
     }
 
     // lists items in storage with numbers
     public static void listOut() {
         line();
+        System.out.println("Here's your tasks");
         for (int i = 0; i < storage.size(); i++) {
             // list starts from 1
-            System.out.println((i + 1) + ". " + storage.get(i));
+            Task currentTask = storage.get(i);
+            System.out.println((i + 1) + ". " + currentTask.getCompletion() + " " + currentTask.getName());
         }
+        line();
+    }
+
+    public static void markComplete(int taskNumber) {
+        storage.get(taskNumber).complete();
+        line();
+        System.out.println("Marked task " + (taskNumber + 1) + " as complete.");
+        System.out.println("[✓] " + storage.get(taskNumber).getName());
         line();
     }
 
@@ -43,8 +81,8 @@ public class Duke {
         echo(logo + "What's up?");
 
         Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()) {
-            String output = scanner.next();
+        while (scanner.hasNextLine()) {
+            String output = scanner.nextLine();
             // bye exits while
             if (output.equals("bye")) {
                 echo("Ciao!");
@@ -54,9 +92,14 @@ public class Duke {
             else if (output.equals("list")) {
                 listOut();
             }
+            else if (output.length() >= 6 && output.substring(0, 4).equals("done")) {
+                String index = output.substring(5);
+                int intIndex = Integer.parseInt(index);
+                markComplete(intIndex - 1);
+            }
             // no special order, adds user input to storage
             else {
-                storage.add(output);
+                store(output);
                 echo("added: " + output);
             }
         }
