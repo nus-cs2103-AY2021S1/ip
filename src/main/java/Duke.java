9c35cb1834c645e91b431445f7b2a0bbe9a6a2f0
dhafin <dhafinrazaq@gmail.com>
2020-08-14 18:21:56 +0700

    Add functionalities to store texts and display the stored texts

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,36 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     private final static String EXIT_COMMAND = "bye";
+    private List<String> list;
 
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.hello();
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        while (!input.equals(EXIT_COMMAND)){
-            duke.echo(input);
-            input = scanner.nextLine();
-        }
+        duke.handle();
         duke.bye();
     }
 
-    private void hello() {
+    private Duke() {
+        list = new ArrayList<>();
+    }
+
+    private void handle() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals(EXIT_COMMAND)){
+            switch (input) {
+                case "list":
+                    this.displayList();
+                    break;
+                default:
+                    this.addToList(input);
+            }
+            input = scanner.nextLine();
+        }
+        scanner.close();
+    }
+
+    private void say(String text) {
         System.out.println("------------------------------------------------------------");
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println(text);
         System.out.println("------------------------------------------------------------\n");
     }
 
-    private void echo(String word) {
-        System.out.println("------------------------------------------------------------");
-        System.out.println(word);
-        System.out.println("------------------------------------------------------------\n");
+    private void hello() {
+        say("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    private void addToList(String text) {
+        this.list.add(text);
+        say("added: " + text);
+    }
+
+    private void displayList() {
+        String text = "";
+        for (int i = 0; i < this.list.size(); i++) {
+            text += ((i + 1) + ". " + this.list.get(i) + (i == this.list.size() - 1 ? "" : "\n"));
+        }
+        say(text);
     }
 
     private void bye() {
-        System.out.println("------------------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("------------------------------------------------------------\n");
+        say("Bye. Hope to see you again soon!");
     }
+
 }
 
