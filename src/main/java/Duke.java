e5a78e36b98c392dc10c6e2b17f73677250984c0
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-08-14 17:06:02 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private final String lineSeparator = "***********************";
+
+    public void greet() {
+        printMessage("Hi! I'm Duke. What can I do for you?");
+    }
+
+    public void printMessage(String msg) {
+        System.out.println(lineSeparator);
+        System.out.println(msg);
+        System.out.println(lineSeparator);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.greet();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                duke.printMessage("Bye. Hope to see you soon!");
+                break;
+            } else {
+                duke.printMessage(userInput);
+            }
+        }
+        scanner.close();
     }
 }
