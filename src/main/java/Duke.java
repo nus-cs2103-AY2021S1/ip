d73f6a1edc6c33cd9ccaabe258f47943fdd85f2b
yeohhq <yeohhuiqing00@gmail.com>
2020-08-15 00:29:40 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.*;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        // displayed once main is run, without input from user
+        String greetings =
+            "    ____________________________________________________________\n" +
+            "     Hello! I'm Duke\n" +
+            "     What can I do for you?\n" +
+            "    ____________________________________________________________";
+
+        System.out.println(greetings);
+
+        Scanner sc = new Scanner(System.in);
+
+        // continuously scan for input from user until "bye" is invoked
+        while (sc.hasNextLine()) {
+            String input = sc.next();
+
+            if (!input.equals("bye")) {
+                String output =
+                    "    ____________________________________________________________\n" +
+                    "     " + input + "\n" +
+                    "    ____________________________________________________________";
+
+                System.out.println(output);
+            } else {
+                // 'bye' command invoked, print goodbye message and exit program
+                String goodbye =
+                    "    ____________________________________________________________\n" +
+                    "     Bye. Hope to see you again soon!\n" +
+                    "    ____________________________________________________________";
+
+                System.out.println(goodbye);
+                break;
+            }
+        }
     }
 }
