9b9b67335ca1dee282c4ed8bb0ede56a1d1e2a31
Joven-Heng <62068282+Joven-Heng@users.noreply.github.com>
2020-08-15 00:29:04 +0800

    Level 2. Add, list Ver 1.0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Duke {
     public final static String LINE = "*********************************************************";
+    public static ArrayList<String> shelf;
 
     public static void main(String[] args) {
         Welcome();
+        shelf = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        Repeater(sc);
+        Handler(sc);
         sc.close();
     }
 
@@ -20,11 +24,10 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you pal? :D");
         System.out.println(LINE);
-
     }
 
-
-    public static void Repeater(Scanner sc){
+    public static void Handler(Scanner sc){
+        int counter;
         while(true){
             String response = sc.nextLine();
             System.out.println(LINE);
@@ -32,8 +35,18 @@ public class Duke {
                 System.out.println("CYA PAL. Hope to see you again!");
                 System.out.println(LINE);
                 break;
-            } else {
-                System.out.println(response);
+            } else if(response.equals("list")){
+                Iterator<String> iter = shelf.iterator();
+                counter = 1;
+                while(iter.hasNext()) {
+                    System.out.println(counter + ". " + iter.next());
+                    counter ++;
+                }
+                System.out.println(LINE);
+            }
+            else {
+                shelf.add(response);
+                System.out.println("added: " + response);
                 System.out.println(LINE);
             }
         }
