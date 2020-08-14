218e8a1f5e404ae0d7454c55156722fd1c730b85
kerkpy <peiyongkerk@gmail.com>
2020-08-14 21:41:49 +0800

    added Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void doTask() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done:\n"
+            + this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + this.description;
+    }
+}
