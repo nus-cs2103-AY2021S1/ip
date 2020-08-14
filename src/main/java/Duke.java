667924d29e17fca09aea56325e8b1aaa4e11063a
Ben-Hanan <benhanan98@gmail.com>
2020-08-14 21:42:45 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,18 +3,20 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    String[] tasks;
+    Task[] tasks;
 
     String indent = "    ";
     String textIndent = "     ";
     String divide = indent + "____________________________________________________________\n";
     String welcomeMessage = divide + textIndent + "Hello! I'm Duke\n" + textIndent + "What can I do for you?\n" + divide;
     String exitMessage = textIndent + "Bye. Hope to see you again soon!\n";
+    String listMessage = "Here are the tasks in your list:\n";
+    String doneMessage = "Nice! I've marked this task as done:\n";
 
-    static String[] commands = {"bye", "list"};
+    static String[] commands = {"bye", "list", "done "};
 
     Duke() {
-        this.tasks = new String[100];
+        this.tasks = new Task[100];
     }
 
     public static void main(String[] args) {
@@ -24,23 +26,30 @@ public class Duke {
 
         int index = 0;
         while(sc.hasNext()) {
-            String text = sc.nextLine();
-            if (text.equals(commands[0])) {
+            String str = sc.nextLine();
+            String[] part = str.split("(?<=\\D)(?=\\d)");
+
+            if (part[0].equals(commands[0])) {
                 System.out.println(duke.divide + duke.exitMessage + duke.divide);
                 break;
-            } else if (text.equals(commands[1])) {
+            } else if (part[0].equals(commands[1])) {
                 for (int i = 0; i < index; i++) {
-                    String ls = (i + 1) +". " + duke.tasks[i];
+                    String ls = (i + 1) +"." + duke.tasks[i];
                     if (i == 0) {
-                        System.out.println(duke.divide + duke.textIndent + ls);
+                        System.out.println(duke.divide + duke.textIndent + duke.listMessage + duke.textIndent + ls);
                     } else {
                         System.out.println(duke.textIndent + ls);
                     }
                 }
                 System.out.println(duke.divide);
+            } else if (part[0].equals(commands[2])) {
+                int num = Integer.parseInt(part[1]);
+                Task curr = duke.tasks[num - 1];
+                curr.markAsDone();
+                System.out.println(duke.divide + duke.textIndent + duke.doneMessage + duke.textIndent + curr);
             } else {
-                String out = duke.divide + duke.textIndent + "added: " + text + "\n" + duke.divide;
-                duke.tasks[index] = text;
+                String out = duke.divide + duke.textIndent + "added: " + part[0] + "\n" + duke.divide;
+                duke.tasks[index] = new Task(part[0]);
                 System.out.println(out);
                 index++;
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
+}
