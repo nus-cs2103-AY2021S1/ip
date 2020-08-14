06891452a255ce3b304479bd293b25f76673bc6a
ktaekwon000 <ktaekwon000@gmail.com>
2020-08-14 17:57:54 +0800

    Implement Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String line = "    ____________________________________________________________\n";
+    private static String welcomeMessage = "Hello, I'm Duke!\n" +
+        "     What can I do for you?";
+    private static String goodbyeMessage = "Bye. Hope to see you again soon! :)";
+
+    private static String makeWrappedString(String txt) {
+        return line + "     " + txt + "\n" + line;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(makeWrappedString(welcomeMessage));
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+        while (!input.equals("bye")) {
+            System.out.println(makeWrappedString(input));
+            input = sc.next();
+        }
+
+        System.out.println(makeWrappedString(goodbyeMessage));
     }
 }
