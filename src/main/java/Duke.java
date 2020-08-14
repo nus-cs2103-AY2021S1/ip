d87f0e0fc9daf845a66c9d952244debde97b643c
“pangpuncake” <pangpuncake@gmail.comssh-keygen -t rsa -C pangpuncake@gmail.comSHA256:Kf5gd25novZ4tCYTSJtNXvQDJ3p2yAQnn7XU7Dioj1Aexitgit config --global user.email pangpuncake@gmail.com>
2020-08-14 18:16:54 +0800

    Level - 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,33 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(
+                "____________________________________________________________\nHello from\n" +
+                        logo + "\n What can I do for you?" +
+                        "\n____________________________________________________________\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean running = true;
+        while (running) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                running = false;
+                System.out.println(
+                        "____________________________________________________________\n" +
+                                "Bye. Hope to see you again soon!" +
+                                "\n____________________________________________________________\n");
+            } else {
+                System.out.println(
+                        "____________________________________________________________\n" +
+                                Duke.processCommand(command) +
+                                "\n____________________________________________________________\n");
+            }
+        }
+
+    }
+
+    private static String processCommand(String command) {
+        return command;
     }
 }
