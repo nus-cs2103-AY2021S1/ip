5e9f5423064dd4aa7ea6f35bea243dfea06fcff8
tankangliang <tankangliang98@gmail.com>
2020-08-14 14:54:20 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String LINE = "____________________________________________________________";
+    private static final String INDENT = "    ";
+
+    public static void printLine() {
+        System.out.println(INDENT + LINE);
+    }
+
+    public static void respond(List<String> responses) {
+        printLine();
+        for (String s : responses) {
+            System.out.print(INDENT);
+            System.out.println(s);
+        }
+        printLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        List<String> welcomeMessage = new ArrayList<>();
+        welcomeMessage.add("Hello! I'm Duke");
+        welcomeMessage.add("What can I do for you?");
+        respond(welcomeMessage);
+
+        boolean run = true;
+        Scanner sc = new Scanner(System.in);
+        while (run) {
+            String input = sc.nextLine();
+            List<String> responses = new ArrayList<>();
+            if (input.equals("bye")) {
+                responses.add("Bye. Hope to see you again soon!");
+                run = false;
+            } else {
+                responses.add(input);
+            }
+            respond(responses);
+        }
     }
 }
+
