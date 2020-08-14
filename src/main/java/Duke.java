430825e6d9da0d5300102a4223cd6447e1b30c64
aqcd <aquajet173@gmail.com>
2020-08-14 17:02:19 +0800

    implement Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static List<String> todos;
+
     public static void main(String[] args) {
+        todos = new ArrayList<>();
         greet();
         Scanner sc = new Scanner(System.in);
-        echo(sc);
+        converse(sc);
     }
 
     private static void greet() {
         System.out.println(">> Beep Boop. I am Aq-bot.\n>> How can I help?");
     }
 
-    private static void echo(Scanner sc) {
-        while(true) {
+    private static void converse(Scanner sc) {
+        boolean running = true;
+        while(running) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(">> Bye! Hope I helped!");
-                break;
-            } else {
-                System.out.println(input);
+            switch(input) {
+                case("bye"):
+                    System.out.println(">> Bye! Hope I helped!");
+                    running = false;
+                    break;
+                case("list"):
+                    int i = 1;
+                    for (String item : todos) {
+                        System.out.println(">> " + i++ + ". " + item);
+                    }
+                    break;
+                default:
+                    todos.add(input);
+                    System.out.println(">> added: " + input);
+                    break;
             }
         }
     }
