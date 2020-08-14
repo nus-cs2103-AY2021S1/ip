f612b5bccd40363f4a1b7df5e4bd4489d72897fb
TheSpaceCuber <royce.ho@u.nus.edu>
2020-08-14 22:59:39 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        String welcomeMessage = "____________________________________________________________\n"
+                + "Hello I'm Duke\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________";
+        System.out.println(welcomeMessage);
+
+        while (true) {
+            String s = sc.nextLine();
+            System.out.println("____________________________________________________________");
+            if (s.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(s);
+            }
+            System.out.println("\n____________________________________________________________");
+        }
     }
 }
