1a1169b296c00a3619660006fa5037d9e7825c51
aizatazhar <aizat.azharr@gmail.com>
2020-08-14 22:37:25 +0800

    Implement Level-1
    Duke starts by greeting the user, echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "Hello! I'm Duke \nWhat can I do for you?";
+        System.out.println(formatResponse(greeting));
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")){
+            System.out.println(formatResponse(userInput));
+            userInput = sc.nextLine();
+        }
+
+        System.out.println(formatResponse("Bye. Hope to see you again soon!"));
+
+    }
+
+    // Formats a given String by adding horizontal lines to the top and bottom
+    // as well as indenting 1 tab
+    private static String formatResponse(String string) {
+        String horizontalLine = "----------------------------------------------------------------";
+        return String.format("%s\n%s\n%s", horizontalLine, string, horizontalLine)
+                .replaceAll("(?m)^", "\t");
     }
 }
