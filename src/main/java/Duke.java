678ceac21c390b3fc130f98445b4efca45735517
yeohhq <yeohhuiqing00@gmail.com>
2020-08-15 00:39:10 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ public class Duke {
 
     public static void main(String[] args) {
         // arraylist to store all text entered by user
-        ArrayList<String> stored = new ArrayList<>();
+        ArrayList<Task> stored = new ArrayList<>();
 
         // displayed once main is run, without input from user
         String greetings =
@@ -22,31 +22,55 @@ public class Duke {
             String input = sc.nextLine();
 
             if (!input.equals("list") && !input.equals("bye")) {
-                stored.add(input);
+
+                if (input.length() > 4 && input.substring(0, 4).equals("done")) {
+                    // don't add new task
+                } else {
+                    // need to add new task
+                    Task newTask = new Task(input);
+                    stored.add(newTask);
+                }
             }
 
             System.out.println("    ____________________________________________________________");
 
+            // verify command to mark task as done
+            if (input.length() > 4 && input.substring(0, 4).equals("done")) {
+                // get task number to mark as done
+                int taskNo = Integer.parseInt(input.substring(5, input.length()));
+
+                // verify task number exists, then mark as done
+                if (taskNo - 1 < stored.size()) {
+                    stored.get(taskNo - 1).markAsDone();
+
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println("    [" + stored.get(taskNo - 1).getStatusIcon() + "] " + stored.get(taskNo - 1).description);
+                } else {
+                    // task number does not exist
+                    System.out.println("    Sorry, this task does not exist!");
+                }
+
             // display list of items to user when requested with "list" command
-            if (input.equals("list")) {
+            } else if (input.equals("list")) {
+                System.out.println("    Here are the tasks in your list:");
+
                 for (int i = 1; i < stored.size() + 1; i++) {
-                    String item = "     " + i + ". " + stored.get(i - 1);
+                    String item = "     " + i + ".[" + stored.get(i - 1).getStatusIcon() + "] " + stored.get(i - 1).description;
                     System.out.println(item);
                 }
 
-            } else if (!input.equals("bye")) {
-                String output = "     added: " + input;
-                System.out.println(output);
+            } else if (input.equals("bye")) {
+                // print goodbye message
+                String goodbye = "     Bye. Hope to see you again soon!";
+                System.out.println(goodbye);
+                System.out.println("    ____________________________________________________________\n");
+                // exits program
+                break;
 
             } else {
-                // 'bye' command invoked, print goodbye message and exit program
-                String goodbye =
-                    "    ____________________________________________________________\n" +
-                    "     Bye. Hope to see you again soon!\n" +
-                    "    ____________________________________________________________";
-
-                System.out.println(goodbye);
-                break;
+                // inform user item has been added
+                String output = "     added: " + input;
+                System.out.println(output);
             }
 
             System.out.println("    ____________________________________________________________\n");
