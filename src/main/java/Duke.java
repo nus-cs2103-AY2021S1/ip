90e8d9649308c4b2639599e1ff35bf6a6a0e693f
Criss-Wang <djj87018507@gmail.com>
2020-08-14 19:42:40 +0800

    Add checkstyle.xml

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String intro = "Hello! I'm Jarvis\n"
+                + "What can I do for you?";
+        System.out.println(intro);
     }
 }
