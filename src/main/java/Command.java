0ee6db9d95e5521a50eb0a560e57530138f79920
zhiayang <zhiayang@nascentnoodle.com>
2020-08-14 20:39:41 +0800

    "Fix" tabs to spaces

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -4,66 +4,66 @@
 import java.util.Optional;
 
 public class Command {
-	private final CommandType type;
-	private final Optional<String> itemName;
-	private final Optional<Integer> itemIndex;
+    private final CommandType type;
+    private final Optional<String> itemName;
+    private final Optional<Integer> itemIndex;
 
-	private Command(CommandType type) {
-		this.type = type;
-		this.itemName = Optional.empty();
-		this.itemIndex = Optional.empty();
-	}
+    private Command(CommandType type) {
+        this.type = type;
+        this.itemName = Optional.empty();
+        this.itemIndex = Optional.empty();
+    }
 
-	private Command(CommandType type, String name) {
-		this.type = type;
-		this.itemName = Optional.of(name);
-		this.itemIndex = Optional.empty();
-	}
+    private Command(CommandType type, String name) {
+        this.type = type;
+        this.itemName = Optional.of(name);
+        this.itemIndex = Optional.empty();
+    }
 
-	private Command(CommandType type, int idx) {
-		this.type = type;
-		this.itemIndex = Optional.of(idx);
-		this.itemName = Optional.empty();
-	}
+    private Command(CommandType type, int idx) {
+        this.type = type;
+        this.itemIndex = Optional.of(idx);
+        this.itemName = Optional.empty();
+    }
 
-	public CommandType getType() {
-		return this.type;
-	}
+    public CommandType getType() {
+        return this.type;
+    }
 
-	public Optional<String> getItemName() {
-		return this.itemName;
-	}
+    public Optional<String> getItemName() {
+        return this.itemName;
+    }
 
-	public Optional<Integer> getItemIndex() {
-		return this.itemIndex;
-	}
+    public Optional<Integer> getItemIndex() {
+        return this.itemIndex;
+    }
 
 
-	public static Command quit() {
-		return new Command(CommandType.Quit);
-	}
+    public static Command quit() {
+        return new Command(CommandType.Quit);
+    }
 
-	public static Command addItem(String item) {
-		return new Command(CommandType.AddItem, item);
-	}
+    public static Command addItem(String item) {
+        return new Command(CommandType.AddItem, item);
+    }
 
-	public static Command listItems() {
-		return new Command(CommandType.ListItems);
-	}
+    public static Command listItems() {
+        return new Command(CommandType.ListItems);
+    }
 
-	public static Command markItemAsDone(int idx) {
-		return new Command(CommandType.MarkItemAsDone, idx);
-	}
+    public static Command markItemAsDone(int idx) {
+        return new Command(CommandType.MarkItemAsDone, idx);
+    }
 
-	public static Command deleteItem(int idx) {
-		return new Command(CommandType.DeleteItem, idx);
-	}
+    public static Command deleteItem(int idx) {
+        return new Command(CommandType.DeleteItem, idx);
+    }
 
-	public enum CommandType {
-		Quit,
-		AddItem,
-		ListItems,
-		MarkItemAsDone,
-		DeleteItem
-	}
+    public enum CommandType {
+        Quit,
+        AddItem,
+        ListItems,
+        MarkItemAsDone,
+        DeleteItem
+    }
 }
