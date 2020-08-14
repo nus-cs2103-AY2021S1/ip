2d47973328a9373b4bbcc365ce9f882faa5e5a58
Bennett Clement <benclmnt.dev@gmail.com>
2020-08-14 17:04:14 +0800

    Implement Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void respondStart() {
+        System.out.println("     ----------------------------------------");
+        System.out.println("     Hellowww!! I'm Alexa, your personal todo manager!");
+        System.out.println("     How can I help you today?");
+        System.out.println("     ----------------------------------------");
+    }
+
+    public static void respondClose() {
+        System.out.println("     ----------------------------------------");
+        System.out.println("     Bye? I hope it's not forever! Come back soon!");
+        System.out.println("     ----------------------------------------");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String command;
+
+        // Flow starts here
+        respondStart();
+        while (sc.hasNextLine()) {
+            command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println("     ----------------------------------------");
+            System.out.printf("     %s%n", command);
+            System.out.println("     ----------------------------------------");
+        }
+        respondClose();
     }
 }
