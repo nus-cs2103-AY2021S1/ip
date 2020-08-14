6c4626b6ab913a98919814063b7769ce58729fd3
LAPTOP-S0GSC5AQ\kormi <kormingsoon@gmail.com>
2020-08-14 19:47:22 +0800

    Added Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        greet();
+        echo(sc, sc.nextLine());
+        exit();
+    }
+
+    private static void exit() {
+        String exitMessage = "Pikachu: Pika byebye!\n";
+        System.out.print(exitMessage);
+    }
+
+    public static void greet() {
+        System.out.println("Pikachu: Hello, I am Pikachu! Pika pika, what can pika DO FOR YOU!\n" +
+                "Type something and I will echo it for you! or you can say bye to end us </3");
+    }
+
+    public static void echo(Scanner sc, String input) {
+        while (!input.equals("bye")) {
+            System.out.print("Echoed Message: " + input + "\n");
+            input = sc.nextLine();
+        }
     }
 }
