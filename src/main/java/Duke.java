79598a52f977554b34bd6379a69ea48f976c2d29
rishi <rishi5154@gmail.com>
2020-08-14 18:19:50 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    public static List<String> toDoList = new ArrayList<>();
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,9 +16,25 @@ public class Duke {
         Scanner scan = new Scanner(System.in);
         String word = scan.nextLine();
         while (!word.equals("bye")) {
-            System.out.println(word);
+            if (word.equals("list")) {
+                for (String str : toDoList) {
+                    System.out.println(str);
+                }
+            } else {
+                System.out.println("added: " + word);
+                storeItems(word);
+            }
             word = scan.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void storeItems(String item){
+        int count = toDoList.size() + 1;
+        if (count > 100) {
+            System.out.println("You have 100 tasks yet to be cleared!");
+        } else {
+            String todo = count + ". " + item;
+            toDoList.add(todo);
+        }
     }
 }
