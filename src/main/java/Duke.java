aa9bc93c8ae5f544c7e064cbbfb58e6a228bbdd3
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2020-08-14 19:16:00 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
 public class Duke {
+    static void printDialog(String content) {
+        System.out.println("    ----------------------------------------");
+        System.out.println("    " + content + "\n");
+        System.out.println("    ----------------------------------------");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +13,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printDialog("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String content = sc.next();
+            if(content.equals("bye")) {
+                printDialog("Bye. Hope to see you again soon!");
+                break;
+            }
+            printDialog(content);
+        }
     }
 }
