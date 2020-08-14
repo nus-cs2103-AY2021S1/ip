c32721b21a3b884d982d0527213701b0342a2254
kswk <kswk@live.com>
2020-08-14 23:28:08 +0800

    Add ability to store and display user text input on request

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,18 +4,26 @@ public class Duke {
 
     private static void botStart() {
         Scanner sc = new Scanner(System.in);
+        String[] storage = new String[100];
+        int storageCount = 0;
         System.out.println("=========================================="
                 + "\nHi, my name is Duke."
                 + "\nWhat can I do for you today?"
                 + "\n==========================================");
         while (true) {
-            String command = sc.next();
+            String command = sc.nextLine();
             if (command.equals("bye")) {
                 System.out.println("Thanks for chatting with me, see you soon!"
                         + "\n==========================================");
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < storageCount; i++) {
+                    System.out.println(storage[i]);
+                }
             } else {
-                System.out.println(">" + command + "<");
+                storage[storageCount] = Integer.toString(storageCount + 1) + ". " + command;
+                System.out.println(">" + "added: " + command + "<");
+                storageCount++;
             }
         }
         sc.close();
