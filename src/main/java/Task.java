2c4a76246307d8c63291071aa5723d7d7a5f115c
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-08-14 18:49:29 +0800

    Add new Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+public class Task {
+
+    private boolean completed;
+    private String description;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public Task(String description, boolean completed) {
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Task markCompleted() {
+        return new Task(description, true);
+    }
+}
