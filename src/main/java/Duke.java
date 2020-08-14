9b0f56025e9c520d5e338ef8f575a0056c1ffce3
yeohhq <yeohhuiqing00@gmail.com>
2020-08-15 00:34:28 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,30 +2,42 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
+        // arraylist to store all text entered by user
+        ArrayList<String> stored = new ArrayList<>();
 
         // displayed once main is run, without input from user
         String greetings =
             "    ____________________________________________________________\n" +
             "     Hello! I'm Duke\n" +
             "     What can I do for you?\n" +
-            "    ____________________________________________________________";
+            "    ____________________________________________________________\n";
 
         System.out.println(greetings);
-
         Scanner sc = new Scanner(System.in);
 
         // continuously scan for input from user until "bye" is invoked
         while (sc.hasNextLine()) {
-            String input = sc.next();
+            String input = sc.nextLine();
 
-            if (!input.equals("bye")) {
-                String output =
-                    "    ____________________________________________________________\n" +
-                    "     " + input + "\n" +
-                    "    ____________________________________________________________";
+            if (!input.equals("list") && !input.equals("bye")) {
+                stored.add(input);
+            }
 
+            System.out.println("    ____________________________________________________________");
+
+            // display list of items to user when requested with "list" command
+            if (input.equals("list")) {
+                for (int i = 1; i < stored.size() + 1; i++) {
+                    String item = "     " + i + ". " + stored.get(i - 1);
+                    System.out.println(item);
+                }
+
+            } else if (!input.equals("bye")) {
+                String output = "     added: " + input;
                 System.out.println(output);
+
             } else {
                 // 'bye' command invoked, print goodbye message and exit program
                 String goodbye =
@@ -36,6 +48,9 @@ public class Duke {
                 System.out.println(goodbye);
                 break;
             }
+
+            System.out.println("    ____________________________________________________________\n");
+
         }
     }
 }
