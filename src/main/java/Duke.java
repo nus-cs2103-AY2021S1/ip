86842c8241f52246b8a0be6692f8eb12103b6f57
justacasul <zhengdl98@gmail.com>
2020-08-14 15:41:12 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
+// I'm taking a break
 public class Duke {
 
     static class Task {
         private boolean completion;
         private String name;
+        private Type type;
 
-        Task(String name) {
+        enum Type {
+            todo,
+            deadline,
+            event
+        }
+
+        Task(String type, String name) {
             this.name = name;
             this.completion = false;
+            this.type = Type.valueOf(type);
         }
 
         void complete() {
@@ -25,6 +33,20 @@ public class Duke {
                 return "[✓]";
             } else {
                 return "[✗]";
+            }
+        }
+
+        public String getType() {
+            switch (type) {
+                case todo:
+                    return "[T]";
+                case event:
+                    return "[E]";
+                case deadline:
+                    return "[D]";
+                default:
+                    // this should not happen?
+                    return "something went wrong";
             }
         }
     }
@@ -45,8 +67,17 @@ public class Duke {
     }
 
     // stores user inputs in storage
-    public static void store(String item) {
-        storage.add(new Task(item));
+    public static void store(String type, String name) {
+        Task taskToAdd = new Task(type, name);
+        storage.add(taskToAdd);
+        taskAdded(taskToAdd);
+    }
+
+    public static void taskAdded(Task task) {
+        String toEcho = "Task added: \n"
+                + task.getType() + task.getCompletion() + " " + task.getName() + "\n"
+                + "You now have " + storage.size() + " task(s).";
+        echo(toEcho);
     }
 
     // lists items in storage with numbers
@@ -56,16 +87,20 @@ public class Duke {
         for (int i = 0; i < storage.size(); i++) {
             // list starts from 1
             Task currentTask = storage.get(i);
-            System.out.println((i + 1) + ". " + currentTask.getCompletion() + " " + currentTask.getName());
+            System.out.println((i + 1) + ". "
+                    + currentTask.getType()
+                    + currentTask.getCompletion() + " "
+                    + currentTask.getName());
         }
         line();
     }
 
     public static void markComplete(int taskNumber) {
-        storage.get(taskNumber).complete();
+        Task currentTask = storage.get(taskNumber);
+        currentTask.complete();
         line();
         System.out.println("Marked task " + (taskNumber + 1) + " as complete.");
-        System.out.println("[✓] " + storage.get(taskNumber).getName());
+        System.out.println(currentTask.getType() + "[✓] " + storage.get(taskNumber).getName());
         line();
     }
 
@@ -83,6 +118,7 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String output = scanner.nextLine();
+            // special 1 word cases
             // bye exits while
             if (output.equals("bye")) {
                 echo("Ciao!");
@@ -92,17 +128,27 @@ public class Duke {
             else if (output.equals("list")) {
                 listOut();
             }
-            else if (output.length() >= 6 && output.substring(0, 4).equals("done")) {
-                String index = output.substring(5);
-                int intIndex = Integer.parseInt(index);
-                markComplete(intIndex - 1);
-            }
-            // no special order, adds user input to storage
+            // multiple words
             else {
-                store(output);
-                echo("added: " + output);
+                Scanner multiWord = new Scanner(output);
+                String firstWord = multiWord.next();
+                if (firstWord.equals("done")) {
+                    String index = multiWord.next();
+                    int intIndex = Integer.parseInt(index);
+                    markComplete(intIndex - 1);
+                }
+                // it's a task
+                else if (firstWord.equals("todo") || firstWord.equals("deadline") || firstWord.equals("event")) {
+                    // whitespace in front of nextLine
+                    String remainingWords = multiWord.nextLine().trim();
+                    System.out.println(remainingWords);
+                    store(firstWord, remainingWords);
+                }
+                // no special order, this shouldn't happen anymore
+                else {
+                    System.out.println("invalid order");
+                }
             }
         }
-
     }
 }
