60e877d6987eb33d6cf1eaa5f30ecef240db49fc
Criss-Wang <djj87018507@gmail.com>
2020-08-14 22:01:51 +0800

    Update Duke.java main() method
    Complete Level-1 Task.
    
    Let's simulate the chat bot called "Jarvis" by simple while loop
    which outputs the command from user and takes in a new one
    until "bye" command is received.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Driver class for Duke chat bot
+ */
 public class Duke {
     public static void main(String[] args) {
         String intro = "Hello! I'm Jarvis\n"
                 + "What can I do for you?";
+        String command;
+
         System.out.println(intro);
+
+        Scanner in = new Scanner(System.in);
+        command = in.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = in.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
