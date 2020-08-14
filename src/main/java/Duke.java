0f222eacffda741e529ff913f1393607c060ac0f
Joven-Heng <62068282+Joven-Heng@users.noreply.github.com>
2020-08-15 01:01:04 +0800

    Level-3: Mark as done Version 1.0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Duke {
     public final static String LINE = "*********************************************************";
-    public static ArrayList<String> shelf;
+    public static ArrayList<Task> shelf;
 
     public static void main(String[] args) {
         Welcome();
@@ -14,7 +14,7 @@ public class Duke {
         sc.close();
     }
 
-    public static void Welcome(){
+    public static void Welcome() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -26,26 +26,36 @@ public class Duke {
         System.out.println(LINE);
     }
 
-    public static void Handler(Scanner sc){
+    public static void Handler(Scanner sc) {
         int counter;
-        while(true){
+        int indexer;
+        Task book;
+        while (true) {
             String response = sc.nextLine();
             System.out.println(LINE);
-            if(response.equals("bye")){
+            if (response.equals("bye")) {
                 System.out.println("CYA PAL. Hope to see you again!");
                 System.out.println(LINE);
                 break;
-            } else if(response.equals("list")){
-                Iterator<String> iter = shelf.iterator();
+            } else if (response.equals("list")) {
+                Iterator<Task> iter = shelf.iterator();
                 counter = 1;
-                while(iter.hasNext()) {
+                System.out.println("Here are the tasks in your list: ");
+                while (iter.hasNext()) {
                     System.out.println(counter + ". " + iter.next());
-                    counter ++;
+                    counter++;
                 }
+                System.out.println(LINE);
+            } else if (response.indexOf("done ") == 0){
+                System.out.println("Nice! I've marked this task as done: ");
+                indexer = Integer.parseInt(response.replaceAll("\\D+","")) -1;
+                book = shelf.get(indexer);
+                book.complete();
+                System.out.println(book);
                 System.out.println(LINE);
             }
             else {
-                shelf.add(response);
+                shelf.add(new Task(response));
                 System.out.println("added: " + response);
                 System.out.println(LINE);
             }
