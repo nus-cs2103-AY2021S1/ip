6a268540e076855ff7b229f388535e63e9c169a8
dhafin <dhafinrazaq@gmail.com>
2020-08-14 17:54:58 +0700

    Add hello, echo, and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final static String EXIT_COMMAND = "bye";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.hello();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals(EXIT_COMMAND)){
+            duke.echo(input);
+            input = scanner.nextLine();
+        }
+        duke.bye();
+    }
+
+    private void hello() {
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("------------------------------------------------------------\n");
+    }
+
+    private void echo(String word) {
+        System.out.println("------------------------------------------------------------");
+        System.out.println(word);
+        System.out.println("------------------------------------------------------------\n");
+    }
+
+    private void bye() {
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------------------------------\n");
     }
 }
+
