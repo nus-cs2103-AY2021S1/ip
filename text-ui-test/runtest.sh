d11f72f07a228ba347315039ba846db1a9645193
aqcd <aquajet173@gmail.com>
2020-08-15 00:10:57 +0800

    implement A-TextUiTesting

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,18 +13,18 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Duke.java
+if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Task.java ../src/main/java/Todo.java \
+  ../src/main/java/Deadline.java ../src/main/java/Event.java ../src/main/java/Duke.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -Dfile.encoding=UTF-8 -classpath ../bin Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
