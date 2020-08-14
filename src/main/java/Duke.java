25245351cabb46630644aaee9cd54cc26b59c632
sebastiantoh <sebas.tsj.98@gmail.com>
2020-08-14 22:28:51 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    final static List<String> items = new ArrayList<>();
+    final static List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         speak("Hello! I'm Duke\nWhat can I do for you?");
@@ -13,23 +13,36 @@ public class Duke {
         String userInput;
         while (!(userInput = scanner.nextLine()).equals("bye")) {
             if (userInput.equals("list")) {
-                StringBuilder sb = new StringBuilder();
+                StringBuilder sb = new StringBuilder("Here are the tasks in your list:\n");
 
-                for (int i = 0; i < Duke.items.size(); i++) {
-                    sb.append(String.format("%d. %s\n", i + 1, items.get(i)));
+                for (int i = 0; i < Duke.tasks.size(); i++) {
+                    sb.append(String.format("%d. %s\n", i + 1, tasks.get(i)));
                 }
 
                 Duke.speak(sb.toString().trim());
+            } else if (userInput.startsWith("done")) {
+                String[] words = userInput.split(" ");
+
+                if (words.length < 2) {
+                    throw new IllegalArgumentException("taskId not found!");
+                }
+
+                int taskId = Integer.parseInt(userInput.split(" ")[1]);
+                Task task = tasks.get(taskId - 1);
+                task.markAsDone();
+
+                Duke.speak(String.format("Nice! I've marked this task as done:\n%s", task));
             } else {
-                items.add(userInput);
+                tasks.add(new Task(userInput));
+
                 Duke.speak(String.format("added: %s", userInput));
             }
-
         }
 
         Duke.speak("Bye. Hope to see you again soon!");
 
         scanner.close();
+
     }
 
     public static void speak(String message) {
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+class Task {
+    private String description;
+    private boolean isDone;
+
+    Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "\u2713" : "\u2718"); // return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+
+}
