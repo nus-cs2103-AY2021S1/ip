b1729483c0edcf33ba006d5abcf630f43be4f7da
hansebastian <sebastian_hans@hotmail.com>
2020-08-14 22:58:39 +0800

    Level-1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "Hans ㋡";
+                /*"_   _        _        \n"
+                   + "| |_| | ___  _| | _____ \n"
+                   + "|  ㋡ | | | | | |/ / _ \\\n"
+                   + "| |_| | |_| |   <  __/\n"
+                   + "|_| |_|/ \\_|,_|_|\\_\\___|\n"; */
+        System.out.println("Hello from " + logo + "\n" + "How may I be of service " +
+                "to you this fine day sire?");
+
+        awaitInstructions();
+    }
+
+    private static void awaitInstructions() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String userInput =  sc.nextLine();
+
+            switch (userInput) { // Determine output from user input
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                default:
+                    echo(userInput);
+                    break;
+            }
+        }
+    }
+
+    private static void echo(String userInput) {
+        System.out.println(userInput);
     }
 }
