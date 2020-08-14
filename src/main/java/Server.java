551110861d118c9ae4899156e7e65cdc87328024
Lingy12 <russellgy1229@gmail.com>
2020-08-14 16:35:44 +0800

    Add support for tracking three types of task
    Also, add the discription for list of item

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,14 +3,18 @@ import java.util.ArrayList;
 public class Server {
     ArrayList<Task> taskList = new ArrayList<>();
 
-    public void add(String description) {
-        Task newTask = new Task(description);
+    public void add(Task newTask) {
+        StringBuilder out = new StringBuilder();
         this.taskList.add(newTask);
-        System.out.print(Duke.formatOut("Added: " + description + "\n"));
+        out.append("Got it. I've added this task: ").append("\n\t\t")
+                .append(newTask.toString()).append("\n\t")
+                .append(String.format("Now you have %d tasks in the list.\n",taskList.size()));
+        System.out.print(Duke.formatOut(out.toString()));
     }
 
     public void list() {
         StringBuilder builder = new StringBuilder();
+        builder.append("Here are the tasks in your list: \n\t");
 
         for(Task task:taskList) {
             builder.append(taskList.indexOf(task) + 1).append(". ").append(task.toString()).append("\n").append("\t");
@@ -20,14 +24,31 @@ public class Server {
     }
 
     public void perform(String input) {
-        String[] s = input.split(" ");
+        String[] s = input.split(" ",2);
         if (s[0].equals("list")) {
             this.list();
         } else if (s[0].equals("done")) {
             int index = Integer.parseInt(s[1]) - 1;
             this.taskList.get(index).markAsDone();
         } else {
-            this.add(input);
+            switch (s[0]) {
+                case "todo":
+                    this.add(new Todo(s[1]));
+                    break;
+                case "deadline": {
+                    String[] set = s[1].split(" /by ");
+                    this.add(new Deadline(set[0], set[1]));
+                    break;
+                }
+                case "event": {
+                    String[] set = s[1].split(" /at ");
+                    this.add(new Event(set[0], set[1]));
+                    break;
+                }
+                default:
+                    this.add(new Task(input));
+                    break;
+            }
         }
     }
 
