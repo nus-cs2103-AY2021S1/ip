d5eafb4c9411431c353dd01d3e3bc36f656f3155
MorningLit <ambroseliew1998@gmail.com>
2020-08-14 15:01:15 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        welcomeMessage();
+        String inputLine = sc.nextLine();
+        while (!inputLine.equals("bye")) {
+            System.out.println(inputLine);
+            inputLine = sc.nextLine();
+        }
+        goodbyeMessage();
+    }
+
+    private static void welcomeMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm your mother!");
+        System.out.println("What can I do for you son!");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void goodbyeMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Goodbye my child!");
+        System.out.println("____________________________________________________________");
     }
 }
