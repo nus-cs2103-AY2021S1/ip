9cdee5aa4d15811cc4f9b0275704afa77c094b60
KendrewC <kendrewchanys@gmail.com>
2020-08-14 14:18:39 +0800

    Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,51 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        List<String> lst = new ArrayList<>();
+
         Duke.echo("Hello! I'm Duke\nWhat can I do for you?");
 
-        while (sc.hasNext()) {
-            String nxt = sc.next();
-            if (!nxt.equals("bye")) {
-                Duke.echo(nxt);
-            } else {
-                Duke.echo("Bye. Hope to see you again soon!");
-                break;
+        loop: while (sc.hasNextLine()) {
+            String nxt = sc.nextLine();
+            switch (nxt) {
+                case "list":
+                    Duke.printList(lst);
+                    break;
+                case "bye":
+                    Duke.echo("Bye. Hope to see you again soon!");
+                    break loop;
+                default:
+                    Duke.echo("added: " + nxt);
+                    lst.add(nxt);
+                    break;
             }
+            exit_loop: ;
+//            if (!nxt.equals("bye")) {
+//                Duke.echo(nxt);
+//                lst.add(nxt);
+//            } else {
+//                Duke.echo("Bye. Hope to see you again soon!");
+//                break;
+//            }
         }
     }
 
     private static void echo(String s) {
         String line = "____________________________________________________________";
         System.out.printf("%s\n%s\n%s\n", line, s, line);
+    }
+
+    private static void printList(List lst) {
+        String s = "";
+        for (int i = 1; i <= lst.size(); i++) {
+            s += String.format("%d. %s", i, lst.get(i-1));
+            s += i == lst.size() ? "" : "\n";
+        }
+        Duke.echo(s);
     }
 }
