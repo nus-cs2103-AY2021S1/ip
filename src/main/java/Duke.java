43c98d5126639363e10503ec06bcfd3a73307ea0
Choo Xing Yu <chooxingyu@hotmail.com>
2020-08-14 21:52:59 +0800

    Level-2
    implemented Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,29 @@
 import java.util.*;
 
 public class Duke {
+    private static ArrayList<String> userList = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can i do for you?");
+
         while (true) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                int listNumber = 0;
+                for (String str : userList) {
+                    listNumber++;
+                    System.out.println(listNumber + ". " + str);
+                }
             } else {
-                System.out.println(input);
+                    userList.add(input);
+                    System.out.println("added: " + input);
             }
         }
     }
+
 }
