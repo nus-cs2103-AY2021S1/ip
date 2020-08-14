3cbdf4200519e7f3d461bd1027c5adb7e2508955
kswk <kswk@live.com>
2020-08-14 23:48:34 +0800

    Override toString() method for Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,6 +8,11 @@ public class Task {
         isDone = false;
     }
 
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
@@ -15,4 +20,9 @@ public class Task {
     public void markAsDone() {
         isDone = true;
     }
+
+//    public static void main(String[] args) {
+//        Task task = new Task("return book");
+//        System.out.println(task);
+//    }
 }
