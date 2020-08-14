dc95fc0cee4d857dda9f02e40e78f9680243b94d
ktaekwon000 <ktaekwon000@gmail.com>
2020-08-14 19:14:57 +0800

    Implement Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,41 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static String line = "    ____________________________________________________________\n";
-    private static String welcomeMessage = "Hello, I'm Duke!\n" +
-        "     What can I do for you?";
-    private static String goodbyeMessage = "Bye. Hope to see you again soon! :)";
+    private static String LINE = "    ____________________________________________________________";
+    private static String WELCOME_MESSAGE = "Hello, I'm Duke!\n"
+        + "     What can I do for you?";
+    private static String GOODBYE_MESSAGE = "Bye. Hope to see you again soon! :)";
+
+    private static ArrayList<String> list = new ArrayList<>();
 
     private static String makeWrappedString(String txt) {
-        return line + "     " + txt + "\n" + line;
+        return LINE + "\n     " + txt + "\n" + LINE;
     }
 
     public static void main(String[] args) {
-        System.out.println(makeWrappedString(welcomeMessage));
+        System.out.println(makeWrappedString(WELCOME_MESSAGE));
 
         Scanner sc = new Scanner(System.in);
 
-        String input = sc.next();
+        String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(makeWrappedString(input));
-            input = sc.next();
+            switch (input) {
+            case "list":
+                System.out.println(LINE);
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("     " + (i + 1) + ": " + list.get(i));
+                }
+                System.out.println(LINE);
+                break;
+            default:
+                list.add(input);
+                System.out.println(makeWrappedString( "added: " + input));
+                break;
+            }
+            input = sc.nextLine();
         }
 
-        System.out.println(makeWrappedString(goodbyeMessage));
+        System.out.println(makeWrappedString(GOODBYE_MESSAGE));
     }
 }
