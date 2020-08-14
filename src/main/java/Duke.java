9c8e4bda86b1d86d6c961ad47bbe8db799432ecf
ypinhsuan <yenpinhsuan@gmail.com>
2020-08-14 20:31:11 +0800

    Added level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class Duke {
 
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
         String greet = "   ____________________________________________________________\n" +
                 "    Hello! I'm Duke\n" +
                 "    What can I do for you?\n" +
@@ -22,8 +24,18 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println(exit);
                 break;
+            } else if (input.equals("list")) {
+                Iterator<String> iterator = list.iterator();
+                int count = 0;
+                System.out.println(line);
+                while (iterator.hasNext()) {
+                    count++;
+                    System.out.println("    " + count + ". " + iterator.next());
+                }
+                System.out.println(line);
             } else {
-                System.out.println(line + "\n" + "     " + input + "\n" + line + "\n");
+                list.add(input);
+                System.out.println(line + "\n" + "     added: " + input + "\n" + line + "\n");
             }
         }
     }
