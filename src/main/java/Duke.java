eab7c332554b65303a750bc56dedcf535d82d0c6
jillzyt25 <jillzyt25@gmail.com>
2020-08-14 23:47:15 +0800

    Level-1
    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
 public class Duke {
+
+    private static String indent = "   ";
+    public static void greet() {
+        System.out.println(indent + "----------------------------");
+        System.out.println(indent + "Hello! I'm Best2103/TBot\n"+ indent + "What can I do for you?");
+        System.out.println(indent + "----------------------------");
+    }
+
+    public static void respond() {
+        Scanner userInput = new Scanner(System.in);
+        while(true) {
+            String input = userInput.nextLine();
+            System.out.println(indent + "----------------------------");
+            if (input.trim().equals("bye")) {
+                System.out.println(indent + "Bye. Hope to see you again soon!");
+                System.out.println(indent + "----------------------------");
+                break;
+            }
+            System.out.println(indent + input);
+            System.out.println(indent + "----------------------------");
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        respond();
     }
 }
