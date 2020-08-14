39039dc0a2ade95b5e5180189db512bb8651cdee
kswk <kswk@live.com>
2020-08-14 23:42:50 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+}
