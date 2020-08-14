989f6f47bb5ae246549907a7fd28c83a211de921
ruilingk <e0426268@u.nus.edu>
2020-08-14 18:53:25 +0800

    Update Duke.java
    Extracted out the codes for different commands (e.g. "list", "done", "bye")
    into separate methods within Duke class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,47 +2,79 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private final static String divider =
+            "\t_____________________________________________\n";
+
+    private static void greetings() {
+        String logo = "\n"
+                + "\t███████╗ ██████╗  ██████╗██╗   ██╗███████╗   \n"
+                + "\t██╔════╝██╔═══██╗██╔════╝██║   ██║██╔════╝   \n"
+                + "\t█████╗  ██║   ██║██║     ██║   ██║███████╗   \n"
+                + "\t██╔══╝  ██║   ██║██║     ██║   ██║╚════██║   \n"
+                + "\t██║     ╚██████╔╝╚██████╗╚██████╔╝███████║██╗\n"
+                + "\t╚═╝      ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝╚═╝\n";
+        System.out.println("Welcome to " + logo);
+        String greetings = divider
+                + "\t(｡･ω･)ﾉﾞ I am Pocus, your personal assistant!"
+                + "\n\tBefore we start, may I know your name?\n"
+                + divider;
+        System.out.print(greetings);
+    }
+
+    private static void addressUser(String name) {
+        String address = divider
+                + "\tHi there, " + name + "!"
+                + "\n\tHow can I help you today?\n"
+                + divider;
+        System.out.print(address);
+    }
+
+    private static void listTasks(ArrayList<Task> tasks) {
+        System.out.print(divider);
+        for (int i = 0; i < tasks.size(); i++) {
+            int number = i + 1;
+            System.out.print("\t" + number + ": " + tasks.get(i) + "\n");
+        }
+        System.out.print(divider);
+    }
+
+    private static void markTaskDone() {
+
+    }
+
+    private static void exitFocus() {
+        String exit = divider
+                + "\tヾ(｡･ω･｡) Hopefully I helped you today. Bye!\n"
+                + divider;
+        System.out.print(exit);
+    }
+
+    private static void printTask(String taskName) {
+        String task = divider + "\t added: " + taskName + "\n" + divider;
+        System.out.print(task);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String logo = "\n" +
-                "\t███████╗ ██████╗  ██████╗██╗   ██╗███████╗   \n" +
-                "\t██╔════╝██╔═══██╗██╔════╝██║   ██║██╔════╝   \n" +
-                "\t█████╗  ██║   ██║██║     ██║   ██║███████╗   \n" +
-                "\t██╔══╝  ██║   ██║██║     ██║   ██║╚════██║   \n" +
-                "\t██║     ╚██████╔╝╚██████╗╚██████╔╝███████║██╗\n" +
-                "\t╚═╝      ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝╚═╝\n";
-        System.out.println("Welcome to " + logo);
-
-        String divider = "\t_________________________________________\n";
-        String greetings = divider +
-                "\tI am Pocus, your personal assistant!" +
-                "\n\tMay I know your name?\n" + divider;
-        System.out.print(greetings);
-
+        greetings();
         String name = sc.nextLine();
-        String enquiry = divider +
-                "\tHi there, " + name + "!" +
-                "\n\tHow can I help you today?\n" + divider;
-        System.out.print(enquiry);
+        addressUser(name);
 
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         String input = "";
         while (sc.hasNext()) {
             input = sc.nextLine();
-            if (input.equals("bye")) {
-                String exit = "\tHopefully I helped you today. Bye! :)\n";
-                System.out.print(divider + exit + divider);
+            if (input.equals("list")) { // list tasks
+                listTasks(tasks);
+            } else if (input.contains("done")) { // done tasks
+                markTaskDone();
+            } else if (input.equals("bye")) { // say bye
+                exitFocus();
                 break;
-            } else if (input.equals("list")) {
-                System.out.print(divider);
-                for (int i = 0; i < tasks.size(); i++) {
-                    int number = i + 1;
-                    System.out.print("\t" + number + ": " + tasks.get(i) + "\n");
-                }
-                System.out.print(divider);
-            } else {
-                tasks.add(input);
-                System.out.print(divider + "\t added: " + input + "\n" + divider);
+            } else { // add tasks
+                Task task = new Task(input);
+                tasks.add(task);
+                printTask(input);
             }
         }
         sc.close();
