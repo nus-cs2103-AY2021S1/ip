eaefa724e85daaad3aab07566f15efab56a1600a
royleochan <royleochan@gmail.com>
2020-08-15 00:24:48 +0800

    Add DeadlineTask class

---
+++ src/main/java/DeadlineTask.java
@@ -0,0 +1,13 @@
+public class DeadlineTask extends Task {
+    private String deadline;
+
+    public DeadlineTask(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + "(by: " + this.deadline + ")";
+    }
+}
