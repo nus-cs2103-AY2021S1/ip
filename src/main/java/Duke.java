8140880722066fa58b037a89c5765ac539d7570d
kerkpy <peiyongkerk@gmail.com>
2020-08-14 20:09:55 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello from Bikini Bottom!");
+        System.out.println("____________________________________________________________\n"
+            + "Hello! I'm Spongebob\n"
+            + "What can I do for you?\n"
+            + "____________________________________________________________");
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        while (!str.equals("bye")) {
+            System.out.println("____________________________________________________________\n"
+                + str + "\n"
+                + "____________________________________________________________\n");
+
+            str = sc.nextLine();
+        }
+
+        System.out.println("____________________________________________________________\n"
+            + "Bye. Hope to see you again soon! Bahahahaha!\n"
+            + "____________________________________________________________\n");
+
+
+
     }
 }
