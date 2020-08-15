0fc4daca8659d56ca6a15e4fb0f79fe5c9091894
Chiam Yun Qing <chiamyunqing@gmail.com>
2020-08-15 15:03:54 +0800

    Able to record tasks (i.e. add and list tasks)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
 /*
  * Duke is a retired old uncle who likes to speak in Singlish.
  */
+import java.util.*;
 import java.util.Scanner;
 
 public class Duke {
+    private List<String> taskList = new ArrayList<>();
 
     public void greet() {
         String logo = " ____        _        \n"
@@ -32,10 +34,27 @@ public class Duke {
         }
     }
 
+    public void recordTasks() {
+        Scanner sc = new Scanner(System.in);
+        String task = sc.nextLine();
+        while (!task.toLowerCase().equals("bye")) {
+            if (task.toLowerCase().equals("list")) {
+                System.out.println("Lao Duke not so blur like you. I got remember your tasks one hor.");
+                for (int i = 0; i < this.taskList.size(); i++) {
+                    System.out.println("Task " + (i + 1) + ": " + taskList.get(i));
+                }
+            } else {
+                this.taskList.add(task);
+                System.out.println("Lao Duke has added this task for you: " + task);
+            }
+            task = sc.nextLine();
+        }
+    }
+
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.greet();
-        duke.echoDuke();
+        duke.recordTasks();
         duke.exit();
     }
 }
