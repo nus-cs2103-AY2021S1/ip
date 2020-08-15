551ce47be75536f9235d177701de5a904a50524b
nottiffchan <nottiffchan@gmail.com>
2020-08-16 00:09:29 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] list = new String[101];
+        String divider = "    _________________________________________\n";
+        int listIndex = 1;
         String MrCamel = "                  ,,__\n"
                 + "        ..  ..   / o._)\n"
                 + "       /--'/--\\  \\-'|| \n"
@@ -16,12 +19,25 @@ public class Duke {
         System.out.println("Mr Camel says hello. What can Mr Camel do for you today?\n" + MrCamel);
 
         while (sc.hasNext()) {
-            String toEcho = sc.next();
-            if (toEcho.equals("bye")) {
-                System.out.println("\n    Bye. Hope to see you again!");
+            String command = sc.nextLine();
+            if (command.equals("list")) {
+                System.out.println(divider);
+                for (int i = 1; i < listIndex; i++) {
+                    System.out.println("      " + i + ". " + list[i]);
+                }
+                System.out.println(divider);
+            } else if (command.equals("bye")) {
+                System.out.println(divider);
+                System.out.println("      Bye. Hope to see you again!");
+                System.out.println(divider);
                 break;
+            } else {
+                list[listIndex] = command;
+                listIndex++;
+                System.out.println(divider);
+                System.out.println("      added: " + command);
+                System.out.println(divider);
             }
-            System.out.println("\n    " + toEcho + "\n");
         }
     }
 }
