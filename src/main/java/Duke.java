2fae1910b0d14953ce191b354f06fd3831a41c0e
Robinho98 <ho.robin@yahoo.com.sg>
2020-08-15 14:57:13 +0800

    added level-2, by introducing a tasks arraylist to manage the list of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,15 +11,28 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n" +
                 "     What can I do for you?");
+        System.out.println("________________________________________");
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         while (true) {
             String s = sc.nextLine();
             if (s.equals("bye")) {
+                System.out.println("________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("________________________________________");
                 System.exit(0);
+            } else if (s.equals("list")) {
+                System.out.println("________________________________________");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i+1) + ". " + tasks.get(i));
+                }
+                    System.out.println("________________________________________");
             } else {
-                System.out.println(s);
+                System.out.println("________________________________________");
+                System.out.println("added: " + s);
+                tasks.add(s);
+                System.out.println("________________________________________");
             }
         }
 
