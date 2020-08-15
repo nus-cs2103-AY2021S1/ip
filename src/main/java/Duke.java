7e5d63e3f9c80d244fe54999cf85f76151bdb09b
Asthenosphere <e0271247@u.nus.edu>
2020-08-15 16:50:40 +0800

    chore: modified logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Calendar;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Calendar now = Calendar.getInstance();
+        int hour = now.get(Calendar.HOUR_OF_DAY);
+
+        String logo =
+                "         ,---._                                                            \n" +
+                "       .-- -.' \\    ,---,       ,-.----.                 ,---,  .--.--.    \n" +
+                "       |    |   :  '  .' \\      \\    /  \\        ,---.,`--.' | /  /    '.  \n" +
+                "       :    ;   | /  ;    '.    ;   :    \\      /__./||   :  :|  :  /`. /  \n" +
+                "       :        |:  :       \\   |   | .\\ : ,---.;  ; |:   |  ';  |  |--`   \n" +
+                "       |    :   ::  |   /\\   \\  .   : |: |/___/ \\  | ||   :  ||  :  ;_     \n" +
+                "       :         |  :  ' ;.   : |   |  \\ :\\   ;  \\ ' |'   '  ; \\  \\    `.  \n" +
+                "       |    ;   ||  |  ;/  \\   \\|   : .  / \\   \\  \\: ||   |  |  `----.   \\ \n" +
+                "   ___ l         '  :  | \\  \\ ,';   | |  \\  ;   \\  ' .'   :  ;  __ \\  \\  | \n" +
+                " /    /\\    J   :|  |  '  '--'  |   | ;\\  \\  \\   \\   '|   |  ' /  /`--'  / \n" +
+                "/  ../  `..-    ,|  :  :        :   ' | \\.'   \\   `  ;'   :  |'--'.     /  \n" +
+                "\\    \\         ; |  | ,'        :   : :-'      :   \\ |;   |.'   `--'---'   \n" +
+                " \\    \\      ,'  `--''          |   |.'         '---\" '---'                \n" +
+                "  \"---....--'                   `---'                                      \n";
+        if (hour < 12) {
+            System.out.println("Good morning boss! This is\n" + logo);
+        } else if (hour < 20) {
+            System.out.println("Good afternoon boss! This is\n" + logo);
+        } else {
+            System.out.println("Good evening boss! This is\n" + logo);
+        }
     }
 }
