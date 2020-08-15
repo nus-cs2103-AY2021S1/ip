ff4ece3ab2a7c815392057fc27ac24596692cce7
kerkpy <peiyongkerk@gmail.com>
2020-08-16 00:10:28 +0800

    added automated text ui testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Duke {
 
 
--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,8 @@ REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\Duke.java
+javac  -cp C:\repos\ip\src -Xlint:none -d C:\repos\ip\bin C:\repos\ip\src\main\java\Task.java C:\repos\ip\src\main\java\Deadline.java C:\repos\ip\src\main\java\Events.java C:\repos\ip\src\main\java\ToDo.java C:\repos\ip\src\main\java\Duke.java
+
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -15,7 +16,9 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+java -classpath C:\repos\ip\bin Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
+
+PAUSE
