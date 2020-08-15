64d3fb730b60b01831019c4ee0292dda5511138f
Thai Duong <htduong292001@gmail.com>
2020-08-15 20:22:46 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,31 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+        String greeting = "Hello human, I'm Duke the supporting chatbot \n"
+                + "What can I do for you?";
+        String exitCmd = "bye";
+        String exitMsg = "Bye human. See you again soon!";
+        String horizontalLine = "________________________________________";
+        String cmdReq = "Your command: ";
+
+        // logo and greeting
+        System.out.println(logo + "\n" + horizontalLine);
+        System.out.println(greeting + "\n" + horizontalLine);
+        System.out.print(cmdReq);
+
+        // getting command
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals(exitCmd)) {
+            System.out.println(horizontalLine);
+            System.out.println(input );
+            System.out.println(horizontalLine);
+            System.out.println(cmdReq);
+            input = sc.nextLine();
+        }
+
+        // exit
+        System.out.println(horizontalLine + "\n" +  exitMsg + "\n" + horizontalLine);
     }
 }
