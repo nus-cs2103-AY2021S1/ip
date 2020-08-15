c5e74f7c018e0e4ab2af1edaa7baa37f3bb1f311
wilinetan <wilinetan26@gmail.com>
2020-08-15 23:19:18 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,9 @@
-import java.rmi.server.ExportException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    static Task[] tasks = new Task[100];
-    static int index = 0;
+    static ArrayList<Task> tasks = new ArrayList<Task>();
+    static int total = 0;
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
@@ -16,11 +16,8 @@ public class Duke {
             }
             if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < 100; i++) {
-                    if (tasks[i] == null) {
-                        break;
-                    }
-                    Task task = tasks[i];
+                for (int i = 0; i < tasks.size(); i++) {
+                    Task task = tasks.get(i);
                     System.out.println((i + 1) + "." + task);
                 }
             } else {
@@ -31,7 +28,7 @@ public class Duke {
                             throw new DukeException("☹ OOPS!!! I don't know which task to mark as done.");
                         }
                         int taskIndex = Integer.parseInt(splitStr[1]) - 1;
-                        Task task = tasks[taskIndex];
+                        Task task = tasks.get(taskIndex);
                         task.markAsDone();
                         System.out.println("Nice! I've marked this task as done:\n" + task);
                     } else if (input.contains("todo")) {
@@ -39,10 +36,11 @@ public class Duke {
                             throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
                         }
                         Todo todo = new Todo(input.substring(5));
-                        tasks[index] = todo;
-                        index++;
+                        tasks.add(todo);
+//                        tasks[index] = todo;
+                        total++;
                         System.out.println("Got it. I've added this task:\n" + todo + "\nNow you have "
-                                + index + " tasks in the list.");
+                                + total + " tasks in the list.");
                     } else if (input.contains("deadline")) {
                         if (input.length() <= 9 || !input.contains("/by")) {
                             throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
@@ -50,10 +48,10 @@ public class Duke {
                         String[] splitStr = input.split("/by ");
                         String description = splitStr[0].substring(9).trim();
                         Deadline deadline = new Deadline(description, splitStr[1]);
-                        tasks[index] = deadline;
-                        index++;
+                        tasks.add(deadline);
+                        total++;
                         System.out.println("Got it. I've added this task:\n" + deadline + "\nNow you have "
-                                + index + " tasks in the list.");
+                                + total + " tasks in the list.");
                     } else if (input.contains("event")) {
                         if (input.length() <= 6 || !input.contains("/at")) {
                             throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
@@ -61,11 +59,24 @@ public class Duke {
                         String[] splitStr = input.split("/at ");
                         String description = splitStr[0].substring(6).trim();
                         Event event = new Event(description, splitStr[1]);
-                        tasks[index] = event;
-                        index++;
+                        tasks.add(event);
+                        total++;
                         System.out.println("Got it. I've added this task:\n" + event + "\nNow you have "
-                                + index + " tasks in the list.");
-                    } else {
+                                + total + " tasks in the list.");
+                    } else if (input.contains("delete")) {
+                        String[] splitStr = input.split(" ");
+                        if (splitStr.length == 1) {
+                            throw new DukeException("☹ OOPS!!! I don't know which task to delete.");
+                        }
+                        int taskIndex = Integer.parseInt(splitStr[1]) - 1;
+                        Task task = tasks.get(taskIndex);
+                        tasks.remove(taskIndex);
+                        total--;
+                        System.out.println("Noted. I've removed this task:\n" + task + "\nNow you have "
+                            + total + " tasks in the list.");
+
+                    }
+                    else {
                         throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     }
                 } catch (DukeException ex) {
