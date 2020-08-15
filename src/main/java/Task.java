6f89f4d417ac4f33b25dbdf600089600033025d6
Justin Tzuriel Krisnahadi <justin.tzuriel17@gmail.com>
2020-08-15 14:02:54 +0800

    Added Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    protected String desc;
+    protected boolean isDone;
+
+    public Task(String desc) {
+        this.desc = desc;
+        this.isDone = false;
+    }
+
+    public Task done() {
+        Task doneTask = new Task(this.desc);
+        doneTask.isDone = true;
+        return doneTask;
+    }
+
+    @Override
+    public String toString() {
+        return ("[" + (isDone ? "\u2713" : "\u2718") + "] " + desc);
+    }
+}
