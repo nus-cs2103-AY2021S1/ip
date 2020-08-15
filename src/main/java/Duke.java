1d5f09dc7d76db0363473d744d370a9d747aa2c1
junlong4321 <desmondwongjunlong@yahoo.com.sg>
2020-08-15 11:56:39 +0800

    Duke Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,34 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
+    static List<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________\n");
-
+        
         Scanner sc = new Scanner(System.in);
         String userCommand = sc.nextLine();
 
         while (!userCommand.equals("bye")) {
-            System.out.println("\t____________________________________________________________");
-            System.out.println("\t"+ userCommand);
-            System.out.println("\t____________________________________________________________\n");
-            userCommand = sc.nextLine();
+            if (userCommand.equals("list")) {
+                System.out.println("\t____________________________________________________________");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("\t" + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println("\t____________________________________________________________\n");
+                userCommand = sc.nextLine();
+            } else {
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\t" + "added: " + userCommand);
+                System.out.println("\t____________________________________________________________\n");
+                tasks.add(userCommand);
+                userCommand = sc.nextLine();
+            }
         }
 
         System.out.println("\t____________________________________________________________");
