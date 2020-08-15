e05745e9a10fcc2cbb59e09bbd4f67efe4195a37
lll-jy <E0376997@u.nus.edu>
2020-08-15 14:21:31 +0800

    stored inputs in an array

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,9 +12,11 @@ public class Duke {
 
         System.out.println(output("Hello! I'm Duke\n\t  What can I do for you?"));
         String[] list = new String[100];
+        int count = 0;
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             System.out.println(output(input));
+            list[count++] = input;
             input = sc.nextLine();
         }
         System.out.println(output("Bye. Hope to see you again soon!"));
