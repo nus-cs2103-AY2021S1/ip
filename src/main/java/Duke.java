6e3783a1f0aac8334d8772b09fb0c721750f04ad
peter-yeh <yehyuchun2@gmail.com>
2020-08-15 20:08:08 +0800

    Add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -18,9 +19,32 @@ public class Duke {
 
         String input = scanner.nextLine();
 
+        ArrayList<String> myList = new ArrayList<String>();
+
         while (!input.equals("bye")) {
 
-            displayThis(input);
+            if (input.equals("list")) {
+
+                if (myList.size() == 0) { // checks if the user types list to a empty list
+                    displayThis("List is empty");
+                }else {
+
+                    int temp = 0;
+                    System.out.println("\n    ---------------------------------");
+                    for (int i = 0; i < myList.size() && myList.get(i) != null; i++) {
+
+                        temp = i + 1;
+                        System.out.println("    " + temp + ". " + myList.get(i));
+
+                    }
+                    System.out.println("    ---------------------------------");
+                }
+
+            } else if(!input.trim().equals("")){
+                displayThis("added: " + input);
+                myList.add(input);
+            }
+
 
             input = scanner.nextLine();
         }
