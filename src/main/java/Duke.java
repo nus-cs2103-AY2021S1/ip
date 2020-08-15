9840f90a44b446c5530cba3ef93ce3238fbbc31a
Justin Tzuriel Krisnahadi <justin.tzuriel17@gmail.com>
2020-08-15 13:45:47 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String tab = "     ";
-        String line = "____________________________________________________________\n";
-        String intro = tab + line +
+        String line = tab + "____________________________________________________________";
+        String intro = line + "\n" +
                 tab + "Hello! I'm Duke\n" +
                 tab + "What can I do for you?\n" +
-                tab + line;
+                line + "\n";
         System.out.println(intro);
         Scanner sc = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
         while(true) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println(tab + line +
-                        tab + "Bye. Hope to see you again soon!\n" +
-                        tab + line);
+                System.out.println(line);
+                System.out.println(tab + "Bye. Hope to see you again soon!");
+                System.out.println(line + "\n");
                 break;
+            } else if (input.equals("list")){
+                int id = 1;
+                System.out.println(line);
+                for(String task : tasks) {
+                    System.out.println(tab + id + ". " + task);
+                    id++;
+                }
+                System.out.println(line + "\n");
+            } else {
+                tasks.add(input);
+                System.out.println(line);
+                System.out.println(tab + "added: " + input);
+                System.out.println(line + "\n");
             }
-            System.out.println(tab + line +
-                    tab + input +  "\n" +
-                    tab + line);
         }
     }
 }
