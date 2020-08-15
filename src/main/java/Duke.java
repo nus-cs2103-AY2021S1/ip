0e3b7fa30e9fbbfb42fbe410c1719f86e0cbf18e
Robinho98 <ho.robin@yahoo.com.sg>
2020-08-15 15:26:14 +0800

    added level-3 by adding new class Tasks and changing how the code works to allow users to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,7 +14,7 @@ public class Duke {
         System.out.println("________________________________________");
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         while (true) {
             String s = sc.nextLine();
             if (s.equals("bye")) {
@@ -25,13 +25,30 @@ public class Duke {
             } else if (s.equals("list")) {
                 System.out.println("________________________________________");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i+1) + ". " + tasks.get(i));
+                    System.out.println((i + 1) + ".[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).description);
                 }
+                System.out.println("________________________________________");
+            } else if (s.contains("done")) {
+                try {
+                    int index = Integer.parseInt(s.substring(5)) - 1;
+                    if (index < tasks.size()) {
+                        System.out.println("________________________________________");
+                        System.out.println("Nice! I've marked this task as done: ");
+                        tasks.get(index).markAsDone();
+                        System.out.println("[" + tasks.get(index).getStatusIcon() + "] " + tasks.get(index).description);
+                    } else {
+                        throw new Exception("Please key in this format: done <integer>");
+                    }
+                } catch (Exception e) {
                     System.out.println("________________________________________");
+                    System.out.println("Please key in this format: done <integer>");
+                } finally {
+                    System.out.println("________________________________________");
+                }
             } else {
                 System.out.println("________________________________________");
-                System.out.println("added: " + s);
-                tasks.add(s);
+                System.out.println("added: [\u2718] " + s);
+                tasks.add(new Task(s));
                 System.out.println("________________________________________");
             }
         }
