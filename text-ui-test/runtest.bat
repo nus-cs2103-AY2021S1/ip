47c9235bf00e780297161f7703ae2b947bd7c3f9
erisjacey <eris_jacey@Hotmail.com>
2020-08-15 22:40:02 +0800

    runtest.bat: Clean up script

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,15 +1,13 @@
 @ECHO OFF
 
-SET MY_PATH=C:\Users\Eris\OneDrive - National University of Singapore\NUS\Year 2\Sem 1\CS2103T\ip
-
 REM create bin directory if it doesn't exist
-if not exist "%MY_PATH%\bin" mkdir "%MY_PATH%\bin"
+if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp "%MY_PATH%\src\main\java" -Xlint:none -d "%MY_PATH%\bin" "%MY_PATH%\src\main\java\Duke.java"
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\Duke.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     PAUSE
@@ -18,7 +16,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath "%MY_PATH%\bin" Duke < input.txt > ACTUAL.TXT
+java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
