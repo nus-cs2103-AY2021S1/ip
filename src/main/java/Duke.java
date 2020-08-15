d79f531be813cfefc475e3bb2bf2395c9d596ae5
cwenling <e0425906@u.nus.edu>
2020-08-15 16:12:10 +0800

    completed Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,38 +2,51 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     private static void greet() {
         String greet = "\t____________________________________________________________\n"
-                + "\tHello! I'm Duke\n"
-                + "\tWhat can I do for you?\n"
+                + "\t Hello! I'm Duke\n"
+                + "\t What can I do for you?\n"
                 + "\t____________________________________________________________\n";
         System.out.println(greet);
     }
 
     private static void add(String task) {
         String echoizer = "\t____________________________________________________________\n"
-                + "\tadded: %s\n"
+                + "\t added: %s\n"
                 + "\t____________________________________________________________\n";
-        tasks.add(task);
+        tasks.add(new Task(task));
         System.out.printf((echoizer) + "%n", task);
     }
 
     private static void list() {
         System.out.print("\t____________________________________________________________\n");
+        System.out.print("\t Here are the tasks in your list:\n");
+        Task t;
         for (int i = 0; i < tasks.size(); i++) {
-            System.out.printf("\t%d. %s%n", i + 1, tasks.get(i));
+            t = tasks.get(i);
+            System.out.printf("\t %d.[%s] %s%n", i + 1, t.getStatusIcon(), t);
         }
         System.out.println("\t____________________________________________________________\n");
     }
 
     private static void exit() {
         String goodbye = "\t____________________________________________________________\n"
-                + "\tBye. Hope to see you again soon!\n"
+                + "\t Bye. Hope to see you again soon!\n"
                 + "\t____________________________________________________________\n";
         System.out.print(goodbye);
         System.exit(0);
+    }
+
+    private static void done(int i) {
+        Task t = tasks.get(i - 1);
+        t.markAsDone();
+        String done = "\t____________________________________________________________\n"
+                + "\t Nice! I've marked this task as done: \n"
+                + "\t   [%s] %s\n"
+                + "\t____________________________________________________________\n";
+        System.out.printf(done, t.getStatusIcon(), t);
     }
 
     public static void main(String[] args) {
@@ -53,6 +66,8 @@ public class Duke {
                 exit();
             } else if (input.equals("list")) {
                 list();
+            } else if (input.split(" ")[0].equals("done")) {
+                done(Integer.parseInt(input.split(" ")[1]));
             } else {
                 add(input);
             }
