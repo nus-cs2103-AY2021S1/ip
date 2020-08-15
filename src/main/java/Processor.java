e4b48f92a795cc9a53ef2db5214af016605e4e3d
IsaacTin <tin.isaac@yahoo.com.sg>
2020-08-15 22:41:08 +0800

    A-Enums

--- src/main/java/Processor.java
+++ src/main/java/Processor.java
@@ -3,47 +3,47 @@ import java.util.List;
 
 public class Processor {
     List<Task> list = new ArrayList<>();
-    String command;
     String description;
+    Commands command;
     boolean entered = false;
 
     public void process(String next) {
         try {
             findCommand(next);
 
-            switch (this.command) {
-                case "done":
+            switch (command) {
+                case DONE:
                     int pos = Integer.parseInt(this.description);
                     list.get(pos - 1).markAsDone();
                     System.out.println("Marked this task as done for you!\n" + list.get(pos - 1)
                             + "\nYou have " + list.size() + " tasks in the list.");
                     break;
-                case "list":
+                case LIST:
                     for (Task i : list) {
                         System.out.println(list.indexOf(i) + 1 + "." + i);
                     }
                     break;
-                case "todo":
+                case TODO:
                     list.add(new Todo(this.description));
                     System.out.println("Hi there, I've added this task:\n"
                             + list.get(list.size() - 1) + "\nYou have "
                             + list.size() + " tasks in the list.");
                     break;
-                case "deadline":
+                case DEADLINE:
                     String[] descriptions = this.description.split("/by ");
                     list.add(new Deadline(descriptions[0], descriptions[1]));
                     System.out.println("Hi there, I've added this task:\n"
                             + list.get(list.size() - 1) + "\nYou have "
                             + list.size() + " tasks in the list.");
                     break;
-                case "event":
+                case EVENT:
                     String[] descriptions2 = this.description.split("/at ");
                     list.add(new Event(descriptions2[0], descriptions2[1]));
                     System.out.println("Hi there, I've added this task:\n"
                             + list.get(list.size() - 1) + "\nYou have "
                             + list.size() + " tasks in the list.");
                     break;
-                case "delete":
+                case DELETE:
                     int pos2 = Integer.parseInt(this.description);
                     Task temp = list.get(pos2 - 1);
                     list.remove(pos2 - 1);
@@ -67,7 +67,7 @@ public class Processor {
             if (list.size() == 0) {
                 throw new DukeException("there's nothing on the list yet.");
             }
-            this.command = "list";
+            command = Commands.LIST;
         } else if (description.length() >= 4 && description.substring(0, 4).equals("done")) {
             if (description.substring(3).split(" ").length == 1) {
                 throw new DukeException("you need to give a number.");
@@ -83,13 +83,13 @@ public class Processor {
                     throw new DukeException("you need to put a number.");
                 }
             }
-            this.command = "done";
+            command = Commands.DONE;
             this.description = description.split(" ")[1];
         } else if (description.length() >= 4 && description.substring(0, 4).equals("todo")) {
             if (description.substring(3).split(" ").length == 1) {
                 throw new DukeException(("you don't even know what you want to do."));
             }
-            this.command = "todo";
+            command = Commands.TODO;
             this.description = description.substring(5);
         } else if (description.length() >= 8 && description.substring(0, 8).equals("deadline")) {
             if (description.substring(7).split(" ").length == 1) {
@@ -97,7 +97,7 @@ public class Processor {
             } else if (description.split("/by").length < 2) {
                 throw new DukeException("for deadlines, please include a '/by' just before the deadline!");
             }
-            this.command = "deadline";
+            command = Commands.DEADLINE;
             this.description = description.substring(9);
         } else if (description.length() >= 5 && description.substring(0, 5).equals("event")) {
             if (description.substring(4).split(" ").length == 1) {
@@ -105,7 +105,7 @@ public class Processor {
             } else if (description.split("/at").length < 2) {
                 throw new DukeException("for events, please include a '/at' just before the event!");
             }
-            this.command = "event";
+            command = Commands.EVENT;
             this.description = description.substring(6);
         } else if (description.length() >= 6 && description.substring(0,6).equals("delete")) {
             if (description.substring(5).split(" ").length == 1) {
@@ -122,7 +122,7 @@ public class Processor {
                     throw new DukeException("you need to put a number.");
                 }
             }
-            this.command = "delete";
+            command = Commands.DELETE;
             this.description = description.split(" ")[1];
         } else if (entered) {
             throw new DukeException("you gotta put in a correct command.");
