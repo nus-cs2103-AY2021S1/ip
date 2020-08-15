30a5a8e019747f8722a5a1251ec500eada806c73
csiongn <>
2020-08-16 01:36:02 +0800

    Level 1: Added Greet, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    // Attributes
+    private static final String NAME = "DUKE";
+    private static final String INTRODUCTION = String.format(
+            "Hello! I'm %s\n" +
+                    "I was created for the module CS2103T\n" +
+                    "What can I do for you?", NAME);
+    private static final String EXIT = "Bye. Hope to see you again soon!";
+    private static final String LINE = "____________________________________________________________";
+
+    // Scanner
+    private final static Scanner sc = new Scanner(System.in);
+
+    // Greeting
+    private static void greeting() {
+        System.out.println(wrapText(INTRODUCTION));
+    }
+
+    // Wrap text in lines
+    private static String wrapText(String text) {
+        return LINE + "\n" + text + "\n" + LINE + "\n";
+    }
+
+    // Exit
+    private static void exit() {
+        System.out.println(wrapText(EXIT));
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greeting();
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                exit();
+                break;
+            } else {
+                System.out.println(wrapText(input));
+            }
+        }
     }
 }
