789a7e0a98e045ac8de0df3de80d0bbb625e0223
Criss-Wang <djj87018507@gmail.com>
2020-08-15 14:53:36 +0800

    Change handleCommand() method
    There will be more types of request, in such cases, if-else method is
    less readable as compared to switch method.
    
    Change the conditioning method to switch method makes the code cleaner.
    
    As a result, let's condition on command's request and set actions and
    feedback for each case respectively.

--- src/main/java/CommandAgent.java
+++ src/main/java/CommandAgent.java
@@ -11,8 +11,7 @@ public class CommandAgent {
     }
 
     /**
-     * Takes in the command and handle it based on the request
-     * of the command.
+     * Takes in the command and handle it based on the request from the command.
      *
      * @param command the command parsed from user input.
      * @return a String feedback for the user.
@@ -24,12 +23,17 @@ public class CommandAgent {
             feedback = "Bye. Hope to see you again soon!";
             break;
         case "create":
-            String name = command.getContent().get(0);
+            String name = command.getContent();
             taskList = taskList.addTask(new Task(name));
             feedback = "added: " + name;
             break;
         case "retrieval":
             feedback = taskList.printTasks();
+            break;
+        case "update":
+            int task_id = Integer.parseInt(command.getContent());
+            taskList = taskList.markAsDone(task_id);
+            feedback = taskList.generateFeedbackById(task_id);
             break;
         default:
             feedback = "";
