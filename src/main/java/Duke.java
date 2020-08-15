f0ec1e8b87ee3f03f05df4bd1044c363572c8506
Kacalamaka <heinrich2001.ang@gmail.com>
2020-08-15 22:32:36 +0800

    Level 2 Add,List Impelement Add,List fucntionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,13 @@
 package main.java;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private List<String> list = new ArrayList<>();
+
     static String logo = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
@@ -10,6 +15,7 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|\n";
 
     static final String TERMINATE_COMMAND = "bye";
+    static final String LIST_COMMAND  = "list";
     static final String LINE = "    _____________________________________________________________________\n";
     static final String SINGLE_TAB = "  ";
     static final String DOUBLE_TAB = "      ";
@@ -22,18 +28,29 @@ public class Duke {
             if (input.equals(TERMINATE_COMMAND)) {
                 bye();
                 break;
-            } else {
-                echo(input);
+            } else if (input.equals(LIST_COMMAND)) {
+                System.out.println(LINE + showList() + LINE);
+            }else {
+                add(input);
             }
         }
+    }
+
+    private String showList() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < list.size() ; i++) {
+            sb.append(DOUBLE_TAB + (i+1) + ". " + list.get(i) +"\n");
+        }
+        return sb.toString();
     }
 
     private void greet() {
         System.out.println(LINE + DOUBLE_TAB + "Hello! I'm Rich.\n" + DOUBLE_TAB  + "What can I do for you?\n" + LINE);
     }
 
-    private void echo(String input) {
-        System.out.println(LINE + DOUBLE_TAB + input + "\n" + LINE);
+    private void add(String input) {
+        list.add(input);
+        System.out.println(LINE + DOUBLE_TAB + "added: " + input + "\n" + LINE);
     }
 
     public void bye() {
