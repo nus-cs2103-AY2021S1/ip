5b008f9de884569b50629464e1d2facde366e706
Criss-Wang <djj87018507@gmail.com>
2020-08-15 15:12:10 +0800

    Add attribute isDone in Task class
    A status for the task is needed for the bot's requirement. Operations
    on the status involves marking the task as done.
    
    In light of this, let's:
    * create the new attribute isDone
    * add isDoneTask() and complete() methods to update the task's status
    and display the status
    * modify the toString() method to include the status information into
    the string displayed

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,23 +4,54 @@
  *
  */
 public class Task {
-    protected String name;
+    protected final String name;
+    protected final boolean isDone;
 
     public Task(String name) {
         this.name = name;
+        this.isDone = false;
+    }
+
+    public Task(String name, boolean isDone) {
+        this.name = name;
+        this.isDone = isDone;
     }
 
     /**
      * Get the name of the task.
      *
-     * @return the name of the task.
+     * @return a String showing the name of the task.
      */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get the status of the task.
+     *
+     * @return a boolean indicating if the task is done.
+     */
+    public boolean isDoneTask() {
+        return isDone;
+    }
+
+    /**
+     * Mark the status of the task as done.
+     *
+     * @return a new Task object with the status updated to "done".
+     */
+    public Task complete() {
+        return new Task(this.name, true);
+    }
+
+    /**
+     * Display the task object as a string
+     *
+     * @return the status of the task followed by the name
+     */
     @Override
     public String toString() {
-        return getName();
+        String checkbox = "[" + (this.isDoneTask() ? "\u2713" : "\u2718") + "]";
+        return checkbox + " " + this.getName();
     }
 }
