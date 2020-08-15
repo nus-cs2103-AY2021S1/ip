38637f0751fadab759be7e1eee96c2f3200aeaad
Chiam Yun Qing <chiamyunqing@gmail.com>
2020-08-15 14:14:53 +0800

    Add the greeting and exit codes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+/*
+ * Duke is a retired old uncle who likes to speak in Singlish.
+ */
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String intro = "Eh yo, I am Duke! Some kids call me 'Lao Duke' (which means old Duke).\n" +
+                "I like to speak Singlish, come talk to me now lah!";
+        System.out.println(logo);
+        System.out.println(intro);
+    }
+
+    public static void exit() {
+        String bye = "Alamak why so fast end the convo with me? Ok lah I also need to sleep now Zzzz.\n" +
+                "Goodbye!";
+        System.out.println(bye);
+    }
+    
+    public static void main(String[] args) {
+       // System.out.println("Hello from\n" + logo);
     }
 }
