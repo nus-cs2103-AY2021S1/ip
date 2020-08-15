62fd5a295b9ec9e743d1553b9bfa74a9924a05df
TianYong-Goh <goh_ty@users.noreply.github.com>
2020-08-15 14:42:50 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void order() {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if(input.equals("bye")) {
+                System.out.println("** Bye. Hope to see you soon!! **");
+                break;
+            } else {
+                String output = "**\n" + input + "\n**";
+                System.out.println(output);
+            }
+        }
+
+
+    }
+
+    public static void welcome() {
+        String logo = " ___    ___        ______\n"
+                + "|   \\  /   |_    _|  ____|\n"
+                + "|    \\/    | |  | |  |  _ \n"
+                + "|          | |__| |  |_| |\n"
+                + "|__/\\__/\\__|\\___,_|______|\n";
+
+        String welcome = logo
+                + "\n"
+                + "** Hello! I'm MUG  **\n"
+                + "** What can I do for you ?_? **";
+
+        System.out.println(welcome);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.welcome();
+        Duke.order();
     }
 }
