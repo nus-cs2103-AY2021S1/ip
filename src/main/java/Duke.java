1f3dfe5b83a072f68abeaf64796f6ba1edcf225e
Justin Tzuriel Krisnahadi <justin.tzuriel17@gmail.com>
2020-08-15 14:58:11 +0800

    Refactored printing code, added task count

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,43 +5,50 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String tab = "     ";
-        String line = tab + "____________________________________________________________";
-        String intro = line + "\n" +
-                tab + "Hello! I'm Duke\n" +
-                tab + "What can I do for you?\n" +
-                line + "\n";
-        System.out.println(intro);
+        chatPrint("Hello! I'm Duke\n" +
+                tab + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
         List<Task> tasks = new ArrayList<>();
-        while(true) {
+        while (true) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(line);
-                System.out.println(tab + "Bye. Hope to see you again soon!");
-                System.out.println(line + "\n");
+            String command = input.split(" ")[0];
+            if (command.equals("bye")) {
+                chatPrint("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")){
+            } else if (command.equals("list")) {
                 int id = 1;
-                System.out.println(line);
-                System.out.println(tab + "Here are the tasks in your list:");
-                for(Task task : tasks) {
-                    System.out.println(tab + id + ". " + task);
+                String output = "Here are the tasks in your list:";
+                for (Task task : tasks) {
+                    output += "\n" + tab + id + ". " + task;
                     id++;
                 }
-                System.out.println(line + "\n");
-            } else if (input.split(" ")[0].equals("done")) {
-                System.out.println(tab + "Nice! I've marked this task as done:");
+                chatPrint(output);
+            } else if (command.equals("done")) {
                 int idx = Integer.parseInt(input.split(" ")[1]) - 1;
                 Task doneTask = tasks.get(idx).done();
                 tasks.set(idx, doneTask);
-                System.out.println(tab + "   " + doneTask);
-            }
-            else {
-                tasks.add(new Task(input));
-                System.out.println(line);
-                System.out.println(tab + "added: " + input);
-                System.out.println(line + "\n");
+                chatPrint("Nice! I've marked this task as done:\n" +
+                        tab + "   " + doneTask);
+            } else {
+                Task newTask = addTask(tasks, command, input.substring(input.indexOf(' ') + 1));
+                chatPrint("Got it. I've added this task:\n" +
+                        tab + "   " + newTask + "\n" +
+                        tab + "Now you have " + tasks.size() + " tasks in the list.");
             }
         }
+    }
+
+    public static void chatPrint(String toPrint) {
+        String tab = "     ";
+        String line = "____________________________________________________________";
+        System.out.println(tab + line);
+        System.out.println(tab + toPrint);
+        System.out.println(tab + line + "\n");
+    }
+
+    public static Task addTask(List<Task> tasks, String type, String info) {
+        Task newTask = new Task(info);
+        tasks.add(newTask);
+        return newTask;
     }
 }
