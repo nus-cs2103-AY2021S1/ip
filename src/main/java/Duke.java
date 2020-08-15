d08f1a361fc54050c569bb4e8dead48d71162eb6
lll-jy <E0376997@u.nus.edu>
2020-08-15 19:09:01 +0800

    Apply java Collections

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -88,7 +90,7 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         System.out.println(output("Hello! I'm Duke\n\t  What can I do for you?"));
-        Task[] list = new Task[100];
+        List<Task> list = new ArrayList<>();
         int count = 0;
         String input = sc.nextLine();
         while (!input.equals("bye")) {
@@ -96,19 +98,19 @@ public class Duke {
                 if (input.equals("list")) {
                     System.out.println(horizontalLine + "\n\t  " + "Here are the tasks in your list:");
                     for (int i = 0; i < count; i++) {
-                        System.out.println("\t  " + (i + 1) + "." + list[i]);
+                        System.out.println("\t  " + (i + 1) + "." + list.get(i));
                     }
                     System.out.println(horizontalLine + "\n");
                 } else if (isDoneCommand(input, count)) {
                     if (n > 0 && n <= count) {
-                        list[n - 1].markAsDone();
-                        System.out.println(output("Nice! I've marked this task as done:\n\t    " + list[n - 1]));
+                        list.get(n - 1).markAsDone();
+                        System.out.println(output("Nice! I've marked this task as done:\n\t    " + list.get(n - 1)));
                     } else {
                         System.out.println(output("The task does not exist!"));
                     }
                 } else {
                     Task task = generate(input);
-                    list[count++] = task;
+                    list.add(count++, task);
                     System.out.println(output("Got it. I've added this task:\n\t    " + task +
                             "\n\t  Now you have " + count + " tasks in the list."));
                 }
