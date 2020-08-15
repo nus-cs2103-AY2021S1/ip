80afdc3f817bdd98243bdddef816cf8bb4613984
khor-jingqian <khor.jingqian@gmail.com>
2020-08-16 02:30:56 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
+    public static List<String> toDoList = new ArrayList<String>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -21,10 +26,18 @@ public class Duke {
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println(seperator);
                 break;
+
+            } else if (newLine.equals("list")) {
+                System.out.println(seperator);
+                for ( int i = 1; i <= toDoList.size(); i++) {
+                    System.out.println("\t"+i + ". " + toDoList.get(i-1));
+                }
+                System.out.println(seperator);
             } else {
                 System.out.println(seperator);
-                System.out.println("\t"+newLine);
+                System.out.println("\tadded: "+newLine);
                 System.out.println(seperator);
+                toDoList.add(newLine);
             }
         }
     }
