ff002e4a018d132b9c25dd9766300c9f5c0b3025
Royxuzeng <e0376917@u.nus.edu>
2020-08-15 23:29:16 +0800

    Add Duke.java, Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Duke {
     public static void main(String[] args) {
         String message = "____________________________________________________________\n" +
@@ -14,17 +16,28 @@ public class Duke {
         String string1 = scanner.nextLine();
         String message1;
 
-        List lists = new ArrayList<String>();
+        List<Task> lists = new ArrayList<Task>();
 
         while (!string1.equals("bye")) {
             if (string1.equals("list")) {
-                System.out.println("____________________________________________________________\n");
+                System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < lists.size(); i++) {
-                    System.out.println((i + 1) + ". " + lists.get(i));
+                    Task task = lists.get(i);
+                    System.out.println((i + 1) + "." + "[" + task.getStatusIcon() + "] "
+                            + task.getDescription());
                 }
                 System.out.println("____________________________________________________________\n");
+            } else if (string1.contains("done ")) {
+                int order = parseInt(string1.substring(string1.length() - 1));
+                Task task = lists.get(order - 1);
+                task.markAsDone();
+                System.out.println("____________________________________________________________\n" +
+                        "  Nice! I've marked this task as done:");
+                System.out.println("    [" + task.getStatusIcon() + "] " + task.getDescription());
+                System.out.println("____________________________________________________________\n");
             } else {
-                lists.add(string1);
+                lists.add(new Task(string1));
                 message1 = "____________________________________________________________\n" +
                         "  added: " + string1 + "\n" +
                         "____________________________________________________________\n";
