96a5ec78a371c41394da1dc528cdfb3c9761fac4
Robinho98 <ho.robin@yahoo.com.sg>
2020-08-15 14:39:02 +0800

    Level-1 changed Duke.java to include scanner

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +
+                "     What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.exit(0);
+            } else {
+                System.out.println(s);
+            }
+        }
+
     }
 }
