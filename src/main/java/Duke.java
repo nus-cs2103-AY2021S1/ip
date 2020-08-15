45340de8f7a7f1bdc44cb41ab2543c6ff93b94ef
Wen Jin <Nijnxw@users.noreply.github.com>
2020-08-15 15:42:41 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final String line = "------------------------------------";
+    private final String stringFormat = line + "\n%s\n" + line + "\n\n";
+
+    private Scanner scanner;
+
+    private Duke() {
+        this.scanner = new Scanner(System.in);
+        greet();
+    }
+
+    private void greet() {
+        System.out.printf(stringFormat, "Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    private void echo(String str) {
+        System.out.printf(stringFormat, str);
+    }
+
+    private void processBye() {
+        System.out.printf(stringFormat, "Bye. Hope to see you again soon!");
+    }
+
+    private boolean processAction() {
+        String action = this.scanner.nextLine();
+        switch (action) {
+            case "bye":
+                this.processBye();
+                return false;
+            default:
+                this.echo(action);
+                break;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|";
         System.out.println("Hello from\n" + logo);
+
+        Duke duke = new Duke();
+        boolean status;
+        do {
+            status = duke.processAction();
+        } while (status);
+
     }
 }
