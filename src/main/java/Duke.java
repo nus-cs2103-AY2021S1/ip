b82c03ff773a3de2a3b5da3b7fede58092d951b9
hansebastian <sebastian_hans@hotmail.com>
2020-08-15 16:47:34 +0800

    Completed Level-5, Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,52 +6,64 @@ public class Duke {
 
     public static void main(String[] args) {
         String logo = "Duke";
-                /*"_   _        _        \n"
-                   + "| |_| | ___  _| | _____ \n"
-                   + "|  ㋡ | | | | | |/ / _ \\\n"
-                   + "| |_| | |_| |   <  __/\n"
-                   + "|_| |_|/ \\_|,_|_|\\_\\___|\n"; */
         System.out.println("Hello from " + logo + "\n" + "How may I be of service " +
                 "to you this fine day sire?");
 
-        awaitInstructions();
+        try {
+            awaitInstructions();
+        } catch (DukeException e) {
+            System.out.println(e);
+        }
     }
 
-    private static void awaitInstructions() {
+    private static void awaitInstructions() throws DukeException {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
-            String userInput =  sc.nextLine();
+            try {
+                String userInput =  sc.nextLine();
 
-            switch (userInput) { // Determine output from user input
-                case "bye":
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
-                case "list":
-                    listAllItems();
-                    break;
-                default:
-                    if (userMarkAsDone(userInput)) {
-                        markTaskAsDone(userInput);
+                switch (userInput) { // Determine output from user input
+                    case "bye":
+                        System.out.println("Bye. Hope to see you again soon!");
+                        break;
+                    case "list":
+                        listAllItems();
+                        break;
+                    default:
+                        if (userMarkAsDone(userInput)) {
+                            markTaskAsDone(userInput);
 
-                    } else if (userAddingToDo(userInput)) {
-                        addToDoTask(userInput);
+                        } else if (userAddingToDo(userInput)) {
+                            addToDoTask(userInput);
 
-                    } else if (userAddingEvent(userInput)) {
-                        addEventTask(userInput);
+                        } else if (userAddingEvent(userInput)) {
+                            addEventTask(userInput);
 
-                    } else if (userAddingDeadline(userInput)) {
-                        addDeadlineTask(userInput);
+                        } else if (userAddingDeadline(userInput)) {
+                            addDeadlineTask(userInput);
 
-                    } else {
-                        System.out.println("I don't know what you want me to do");
-                    }
-                    break;
+                        } else { // Any other command that Duke doesn't know
+                            throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                        }
+                        break;
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+                awaitInstructions();
             }
         }
     }
 
-    private static void addToDoTask(String userInput) {
+    private static boolean emptyToDoDescription(String userInput) {
+        return userInput.length() <= 4;
+    }
+
+    private static void addToDoTask(String userInput) throws DukeException {
+        if (emptyToDoDescription(userInput)) {
+            throw new DukeException("The description of a todo cannot be empty.");
+        }
+
         String taskDescription = userInput.substring(5);
         ToDo newToDoItem = new ToDo(taskDescription);
         addItem(newToDoItem); // Add to taskList
@@ -72,24 +84,25 @@ public class Duke {
     }
 
     private static boolean userAddingToDo(String userInput) {
-        return userInput.substring(0, 4).equals("todo");
+        return userInput.length() >= 4 && userInput.substring(0, 4).equals("todo");
     }
 
     private static boolean userAddingEvent(String userInput) {
-        return userInput.substring(0, 5).equals("event");
+        return userInput.length() >= 5 && userInput.substring(0, 5).equals("event");
     }
 
 
     private static boolean userAddingDeadline(String userInput) {
-        return userInput.substring(0, 8).equals("deadline");
+        return userInput.length() >= 8 && userInput.substring(0, 8).equals("deadline");
     }
 
     private static boolean userMarkAsDone(String userInput) {
-        return userInput.substring(0, 4).equals("done");
+        return userInput.length() >= 4 && userInput.substring(0, 4).equals("done");
     }
 
     // Updates taskList attribute to indicate task as done
-    public static void markTaskAsDone(String userInput) {
+    public static void markTaskAsDone(String userInput) throws DukeException {
+        try {
         String taskIndex = userInput.substring(5);
         int index = Integer.valueOf(taskIndex) - 1; // taskIndex started from 1
         Task completedTask = Duke.taskList.get(index);
@@ -98,6 +111,9 @@ public class Duke {
         System.out.println("Nice! I've marked this task as done:\n" +
                 completedTask.toString());
 
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            throw new DukeException("Please enter a valid task number for me to mark as done.");
+        }
     }
 
     // Adds a task item to Duke's taskList, which may be an Event, ToDoItem, Deadline
