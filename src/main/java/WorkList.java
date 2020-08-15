369c43a971e79e0599ae51cdb1cfc71058cf4808
TianYong-Goh <goh_ty@users.noreply.github.com>
2020-08-15 17:28:02 +0800

    Level-3

--- src/main/java/WorkList.java
+++ src/main/java/WorkList.java
@@ -1,25 +1,44 @@
 import java.util.ArrayList;
 
 public class WorkList {
-    private final ArrayList<String> workList;
+    private final ArrayList<Task> workList;
 
     WorkList() {
         this.workList = new ArrayList<>();
     }
 
-    public void addWork(String work) {
+    public void addWork(Task work) {
         this.workList.add(work);
     }
 
     public String readWork() {
-        StringBuilder results = new StringBuilder("1. " + this.workList.get(0));
+        StringBuilder results = new StringBuilder("1. "
+                +"["
+                + this.workList.get(0).getStatusIcon()
+                +"]"
+                + this.workList.get(0).getDescription());
         for(int i = 1; i < this.workList.size(); i ++) {
             results.append("\n");
             results.append(i + 1);
             results.append(". ");
-            results.append(this.workList.get(i));
+            results.append("[");
+            results.append(this.workList.get(i).getStatusIcon());
+            results.append("]");
+            results.append(this.workList.get(i).getDescription());
         }
         return results.toString();
 
+    }
+
+    public String updateTaskStatus(int taskId) {
+        Task tsk =  this.workList.get(taskId - 1);
+        this.workList.set(taskId - 1, tsk.markAsDone());
+
+        return  "Congratz! I've marked this task as done:\n"
+                +"["
+                + this.workList.get(taskId - 1).getStatusIcon()
+                +"]"
+                + " "
+                + this.workList.get(taskId - 1).getDescription();
     }
 }
