70b9b284dfb5386afc329a13fea0fdebb52f70ff
Jing Yen Loh <lohjingyen@gmail.com>
2020-08-15 23:02:44 +0800

    Implement bare minimum of Level-1
    This more or less fulfills the requirement of a __skeletal__ version
    that greets, echoes, and exits on the command "bye".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String BYE = "bye";
+    private static final String GOODBYE_MESSAGE = "Bye. Hope to see you again soon!";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +11,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Duke.displayMessage("What can I do for you?");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("> ");
+            while (scanner.hasNextLine()) {
+                String command = scanner.nextLine();
+
+                if (command.equalsIgnoreCase(BYE)) {
+                    break;
+                } else {
+                    Duke.displayMessage(command);
+                    System.out.print("> ");
+                }
+            }
+
+            Duke.displayMessage(GOODBYE_MESSAGE);
+        }
+    }
+
+    private static void displayMessage(String message) {
+        System.out.println("     ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+        System.out.println("     " + message);
+        System.out.println("     ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+        System.out.println();
     }
 }
