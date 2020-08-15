af6680364166e0890d449b6b8b43d055430f96c5
Junhua <jhsibeizai@gmail.com>
2020-08-15 23:30:46 +0800

    Updated documentation

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,30 +1,56 @@
 public class Task {
 
-    private final String message; //string var to store the message
-    private boolean completed; //boolean to keep track if the task is done
+    //Store the messages
+    private final String message;
 
+    //Boolean to check if task is completed
+    private boolean completed;
+
+    //Constructor for Task
     public Task(String description){
-        this.message = description; //store the description
-        this.markUnDone(); //mark the task as not done
+
+        //Store the message
+        this.message = description;
+
+        //Mark the current task as not done
+        this.markUnDone();
     }
 
     public String getMessage(){
+
+        //Return the message
         return this.message;
     }
 
     public boolean isDone(){
-        return this.completed; //returns if the task is done
+
+        //returns true if the task is done
+        return this.completed;
     }
 
     public void markDone(){
+
+        //Mark the task as complete
         this.completed = true; //mark the task as done
+
     }
 
     public void markUnDone(){
+
+        //Mark the test as not done
         this.completed = false; //mark the task as undone
     }
 
     public String getStatusIcon(){
+
+        //Get the statusIcon based on state
         return (this.isDone() ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    @Override
+    public String toString(){
+
+        //Convert the object to string
+        return String.format("[%s] %s", this.getStatusIcon(), this.getMessage());
     }
 }
