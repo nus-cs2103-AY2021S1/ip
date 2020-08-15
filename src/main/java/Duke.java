9e3e1603ac710609788e8decb170bc7c3e45d3bb
limkoonkiat <lim.koonkiat42@gmail.com>
2020-08-15 15:12:54 +0800

    add Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void printResponse(String response) {
+        System.out.println("____________________________________________________________");
+        System.out.println(response);
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void handleCommands() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userCommand = sc.nextLine();
+            if (userCommand.equals("bye")) {
+                printResponse("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                printResponse(userCommand);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +28,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printResponse("Hello! I'm Duke\nliWhat can I do for you?");
+        handleCommands();
+
     }
 }
