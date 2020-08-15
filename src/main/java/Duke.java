ccedd03bff1816b8740743f5e6a3eb433344a553
lamlaaaam <lamzhiyuan98@gmail.com>
2020-08-15 18:18:23 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,33 @@
 public class Duke {
+
     private static final String LOGO = " ____        _        \n"
                                     + "|  _ \\ _   _| | _____ \n"
                                     + "| | | | | | | |/ / _ \\\n"
                                     + "| |_| | |_| |   <  __/\n"
                                     + "|____/ \\__,_|_|\\_\\___|\n";
 
+    private static final String[] taskList = new String[100];
+    private static int taskIndex = 0;
+
     public static void main(String[] args) {
         greet();
-        String response = DukeIn.prompt();
-        String exitCondition = "bye";
-        while (!response.equals(exitCondition)) {
+        while (true) {
+            String response = DukeIn.prompt();
             handleResponse(response);
-            response = DukeIn.prompt();
         }
-        exit();
+    }
+
+    private static void handleResponse(String response) {
+        switch (response) {
+            case "bye":
+                exit();
+            case "list":
+                showList();
+                break;
+            default:
+                addToList(response);
+                DukeOut.print("added: " + response);
+        }
     }
 
     private static void greet() {
@@ -24,9 +38,25 @@ public class Duke {
     private static void exit() {
         String goodbye = "Bye! Hope to see you again!";
         DukeOut.print(goodbye);
+        System.exit(0);
     }
 
-    private static void handleResponse(String response) {
-        DukeOut.print(response);
+    private static void addToList(String task) {
+        taskList[taskIndex] = task;
+        ++taskIndex;
+    }
+
+    private static void showList() {
+        if (taskIndex == 0) {
+            DukeOut.print("You have no tasks!");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < taskIndex; ++i) {
+            sb.append((i + 1) + ". " + taskList[i] + "\n    ");
+        }
+        sb.setLength(sb.length() - 5);
+        DukeOut.print(sb.toString());
     }
 }
