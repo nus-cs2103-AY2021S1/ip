8a1d2d0403937cb6b556fce27fa0ae3b3f7f038a
jonahtanjz <jonahtanjz4884@gmail.com>
2020-08-16 00:10:38 +0800

    Added 'list tasks' feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,6 +7,14 @@ public class Duke {
         return line + "\n\t\t" + text.replaceAll("\\n", "\n\t\t") + "\n" + line;
     }
 
+    private static String listTasks(ArrayList<String> tasks) {
+        String listOfTasks = "";
+        for (int i = 0; i < tasks.size(); i++) {
+            listOfTasks = listOfTasks + (i + 1) + ". " + tasks.get(i)
+                    + (i == tasks.size() - 1 ? "" : "\n");
+        }
+        return listOfTasks;
+    }
     public static void main(String[] args) {
         System.out.println(formatReply("Hello! I'm Duke\nWhat can I do for you?"));
         ArrayList<String> taskList = new ArrayList<>();
@@ -15,6 +23,8 @@ public class Duke {
             String command = input.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                System.out.println(formatReply(listTasks(taskList)));
             } else {
                 taskList.add(command);
                 System.out.println(formatReply("added: " + command));
