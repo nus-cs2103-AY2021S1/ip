b88c00b6da4f094455854a1ba6c99d806678e3ec
ruixuantan <ruixuantan@yahoo.com.sg>
2020-08-15 13:22:37 +0800

    Add Duke logo and divider

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 import operation.Operation;
 
 public class Duke {
-    private static String startMessage() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String message = "Greetings, what may I do for you?";
-        return logo + message;
-    }
 
-    private static String getSeparatorLine() {
-        return "-------------------------------------------------";
+    private static String DIVIDER = "-------------------------------------------------";
+    private static String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static void printStartMessage() {
+        String message = "Greetings, what may I do for you?";
+        System.out.println(Duke.LOGO + message);
     }
 
     private static void runDuke() {
+        Duke.printStartMessage();
+
         boolean done = false;
         Scanner userInput = new Scanner(System.in);
         CommandParser commandParser = new CommandParser();
@@ -26,10 +27,10 @@ public class Duke {
             System.out.printf("\n");
             String command = userInput.nextLine();
 
-            System.out.println(Duke.getSeparatorLine());
+            System.out.println(Duke.DIVIDER);
             Operation operation = commandParser.parse(command);
             operation.execute();
-            System.out.println(Duke.getSeparatorLine());
+            System.out.println(Duke.DIVIDER);
 
             if (operation.isExit()) {
                 done = true;
@@ -38,7 +39,6 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        System.out.println(Duke.startMessage());
         Duke.runDuke();
     }
 }
