0c38d833c3d80dc4131a32ac80fec801fdbf772e
nottiffchan <nottiffchan@gmail.com>
2020-08-16 00:58:01 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] list = new String[101];
+        Task[] list = new Task[101];
         String divider = "    _________________________________________\n";
         int listIndex = 1;
         String MrCamel = "                  ,,__\n"
@@ -22,20 +22,29 @@ public class Duke {
             String command = sc.nextLine();
             if (command.equals("list")) {
                 System.out.println(divider);
+                System.out.println("      To do:");
                 for (int i = 1; i < listIndex; i++) {
                     System.out.println("      " + i + ". " + list[i]);
                 }
                 System.out.println(divider);
+            } else if (command.contains("done")) {
+                String[] temp = command.split(" ");
+                int indexDone = Integer.parseInt(temp[1]);
+                list[indexDone].markAsDone();
+                System.out.println(divider);
+                System.out.println("      Mr Camel will mark this task as done:\n" + "        " + list[indexDone]);
+                System.out.println(divider);
+
             } else if (command.equals("bye")) {
                 System.out.println(divider);
-                System.out.println("      Bye. Hope to see you again!");
+                System.out.println("      Bye. Mr Camel will miss you! :(");
                 System.out.println(divider);
                 break;
             } else {
-                list[listIndex] = command;
+                list[listIndex] = new Task(command);
                 listIndex++;
                 System.out.println(divider);
-                System.out.println("      added: " + command);
+                System.out.println("      Mr Camel has added: " + command);
                 System.out.println(divider);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected boolean isDone;
+    protected String description;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
