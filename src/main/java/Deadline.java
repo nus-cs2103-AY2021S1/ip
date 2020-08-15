9cd1594da6135c8b80ce7bdef1aaf7d9c8d2f789
kswk <kswk@live.com>
2020-08-15 15:35:59 +0800

    Add Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,19 @@
+public class Deadline extends Task {
+
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+
+//    public static void main(String[] args) {
+//        Task deadline = new Deadline("return book", "monday");
+//        System.out.println(deadline);
+//    }
+}
