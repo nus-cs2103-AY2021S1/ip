c535e0719731db845e1aad763a1791f3538ba7e6
ruixuantan <ruixuantan@yahoo.com.sg>
2020-08-15 21:46:06 +0800

    TaskStorage class: change array to arraylist

--- src/main/java/task/TaskStorage.java
+++ src/main/java/task/TaskStorage.java
@@ -1,35 +1,34 @@
 package task;
 
+import java.util.ArrayList;
+
 public class TaskStorage {
-    private Task[] storage;
-    private int currCapacity;
+    private ArrayList<Task> storage;
 
     public TaskStorage() {
-        this.storage = new Task[100];
-        this.currCapacity = 0;
+        this.storage = new ArrayList<>();
     }
 
     public int getCurrCapacity() {
-        return this.currCapacity;
+        return this.storage.size();
     }
 
     public void addTask(Task task) {
-        this.storage[currCapacity] = task;
-        currCapacity++;
+        this.storage.add(task);
     }
 
     public void completeTask(int index) {
-        this.storage[index - 1].completeTask();
+        this.storage.get(index - 1).completeTask();
     }
 
     public void printTask(int index) {
-        System.out.println(this.storage[index - 1]);
+        System.out.println(this.storage.get(index - 1));
     }
 
     public void printTaskStorage() {
         System.out.println("Here are your tasks:");
-        for (int i = 0; i < currCapacity; i++) {
-            String output = String.format("%d. %s", i + 1, this.storage[i]);
+        for (int i = 0; i < this.storage.size(); i++) {
+            String output = String.format("%d. %s", i + 1, this.storage.get(i));
             System.out.println(output);
         }
     }
