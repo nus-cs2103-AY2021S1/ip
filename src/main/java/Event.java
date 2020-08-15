bd3c6a9700ff7049bb7c9a05cea40f507a8bc1cb
lll-jy <E0376997@u.nus.edu>
2020-08-15 15:12:42 +0800

    Set up the Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+public class Event extends Task {
+    private final String atTime;
+
+    public Event(String description, String atTime) {
+        super(description);
+        this.atTime = atTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + atTime + ")";
+    }
+}
