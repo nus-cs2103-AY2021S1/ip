2be13bbfa32b81a69e1df03fe10f413f7396dcd1
lll-jy <E0376997@u.nus.edu>
2020-08-15 13:59:42 +0800

    done greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String horizontalLine = "--------------------------------------\n";
+        String horizontalLine = "==================================================\n";
 
+        System.out.println(horizontalLine + "Hello! I'm Duke\nWhat can I do for you?\n" + horizontalLine);
     }
 }
