a88d4c42668efdee21248ca40648c4ed07cdb79c
lll-jy <E0376997@u.nus.edu>
2020-08-15 14:13:20 +0800

    customized ouput format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static final String horizontalLine = "==================================================\n";
+    private static final String horizontalLine = "\t==================================================\n\t  ";
 
     private static String output(String message) {
         return horizontalLine + message + "\n" + horizontalLine;
@@ -10,7 +10,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(output("Hello! I'm Duke\nWhat can I do for you?"));
+        System.out.println(output("Hello! I'm Duke\n\t  What can I do for you?"));
 
         String input = sc.nextLine();
         while (!input.equals("bye")) {
