fe9f5006b03f38bd024ca5a9c0fb1690bd85d77a
Junhua <jhsibeizai@gmail.com>
2020-08-15 20:55:23 +0800

    Updated Task.java
    Change log:
    1. Added Completed boolean
    2. Added isDone, markDone, markUnDone and getStatusIcon methods

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,30 @@
 public class Task {
 
-    private final String message;
+    private final String message; //string var to store the message
+    private boolean completed; //boolean to keep track if the task is done
 
-    public Task(String message){
-        this.message = message;
+    public Task(String description){
+        this.message = description; //store the description
+        this.markUnDone(); //mark the task as not done
     }
 
     public String getMessage(){
         return this.message;
+    }
+
+    public boolean isDone(){
+        return this.completed; //returns if the task is done
+    }
+
+    public void markDone(){
+        this.completed = true; //mark the task as done
+    }
+
+    public void markUnDone(){
+        this.completed = false; //mark the task as undone
+    }
+
+    public String getStatusIcon(){
+        return (this.isDone() ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 }
