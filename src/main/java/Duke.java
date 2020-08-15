f6dd5edf9a7c81d655c4240d6ee8a78af2b67df7
boundtotheearth <farrellnah@gmail.com>
2020-08-15 13:38:40 +0800

    Add Automated Testing
    Let's
    * Add automated testing using I/O redirection

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,19 +8,20 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String logo =
-                "//\\\n" +
-                "V  \\\n" +
-                " \\  \\_\n" +
-                "  \\,'.`-.\n" +
-                "   |\\ `. `.       \n" +
-                "   ( \\  `. `-.                        _,.-:\\\n" +
-                "    \\ \\   `.  `-._             __..--' ,-';/\n" +
-                "     \\ `.   `-.   `-..___..---'   _.--' ,'/\n" +
-                "      `. `.    `-._        __..--'    ,' /\n" +
-                "        `. `-_     ``--..''       _.-' ,'\n" +
-                "          `-_ `-.___        __,--'   ,'\n" +
-                "             `-.__  `----\"\"\"    __.-'\n" +
-                "                   `--..____..--'";
+                " _\n" +
+                        "//\\\n" +
+                        "V  \\\n" +
+                        " \\  \\_\n" +
+                        "  \\,'.`-.\n" +
+                        "   |\\ `. `.\n" +
+                        "   ( \\  `. `-.                        _,.-:\\\n" +
+                        "    \\ \\   `.  `-._             __..--' ,-';/\n" +
+                        "     \\ `.   `-.   `-..___..---'   _.--' ,'/\n" +
+                        "      `. `.    `-._        __..--'    ,' /\n" +
+                        "        `. `-_     ``--..''       _.-' ,'\n" +
+                        "          `-_ `-.___        __,--'   ,'\n" +
+                        "             `-.__  `----\"\"\"    __.-'\n" +
+                        "                   `--..____..--'";
 
         System.out.println(logo);
         printInWindow("Hello, I'm a banana.\nWhat can I do for you?");
@@ -55,7 +56,7 @@ public class Duke {
                 Task task = tasks.get(taskNumber - 1);
                 task.markAsDone(true);
                 printInWindow(
-                        "Nice! I've marked the this as done." +
+                        "Nice! I've marked the this as done.\n" +
                                 task.toString()
                 );
                 break;
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -20,14 +20,14 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin main/java/Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
+diff ACTUAL.TXT EXPECTED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
