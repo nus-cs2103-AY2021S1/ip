e15d4687db77daf51f86470dc791f52b759061a4
yanchenglee98 <yanchenglee@gmail.com>
2020-08-15 21:36:16 +0800

    Implement Add and ListOut methods for level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    public static ArrayList<String> list = new ArrayList<>();
 
     public static void Greet() {
         System.out.println("    ____________________________________________________________");
@@ -16,20 +19,38 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
     }
 
-    public static void  Echo() {
+    public static void Echo() {
 
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                ListOut();
             } else {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     " + input);
-                System.out.println("    ____________________________________________________________");
+                Add(input);
                 System.out.println();
             }
         }
+    }
+
+    public static void Add(String string) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     added: " + string);
+        System.out.println("    ____________________________________________________________");
+
+        list.add(string);
+    }
+
+    public static void ListOut() {
+        System.out.println("    ____________________________________________________________");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("     %d. %s \n", i + 1, list.get(i));
+        }
+
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
