a70c5bcc4fef01f8268e9eb2bc5fe1d3b34f6b16
GilbertTan19 <Gilberttan95.gt@gmail.com>
2020-08-15 17:07:58 +0800

    Add Task to List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private List<String> taskList;
+
+    public Duke() {
+        taskList = new LinkedList<>();
+    }
+
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.hello();
         Scanner sc = new Scanner(System.in);
-        String next;
-        do {
+        String next = sc.nextLine();
+        while(!next.equals("bye")){
+            if(next.equals("list")){
+                duke.printList();
+            } else {
+                duke.Echo(next);
+            }
             next = sc.nextLine();
-            duke.command(next);
-        } while(!next.equals("bye"));
+        }
         duke.goodbyeMessage();
     }
 
-    public void goodbyeMessage(){
+    public void goodbyeMessage() {
         System.out.println("********************************************");
         System.out.println("GoodBye, Hope to see you back soon.");
         System.out.println("********************************************");
     }
 
-    public void hello(){
+    public void hello() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -28,10 +40,21 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
     }
-    public void command(String input){
+    public void Echo(String input) {
         System.out.println("********************************************");
-        System.out.println(input);
+        this.addTask(input);
+        System.out.println("added new Task: "+ input);
         System.out.println("********************************************");
         System.out.println();
+    }
+
+    public void addTask(String task) {
+        this.taskList.add(task);
+    }
+
+    private void printList() {
+        for(int i = 0; i < this.taskList.size(); i++) {
+            System.out.println( (i+1) + ". " + this.taskList.get(i));
+        }
     }
 }
