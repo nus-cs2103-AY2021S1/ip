ed40e40136a4eac5fb9a11dc3e3bc7cab70bc9b5
tanweijie123 <tanweijie123@gmail.com>
2020-08-15 17:33:54 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static ArrayList<String> previousInst;
+    private static int instCounter;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        previousInst = new ArrayList<>();
+        instCounter = 0;
+
+
         intro();
 
         String input = "";
         do {
             input = sc.nextLine();
-            processEcho(input);
+            process(input);
+            previousInst.add(++instCounter + ". " + input);
         } while (!input.equals("bye"));
 
     }
@@ -35,10 +45,16 @@ public class Duke {
         println(msg);
     }
 
-    private static void processEcho(String msg) {
+    private static void list() {
+        println(previousInst.toArray(new String[instCounter]));
+    }
+
+    private static void process(String msg) {
         if (msg.equals("bye"))
             exit();
+        else if (msg.equals("list"))
+            list();
         else
-            println(new String[] {msg});
+            println(new String[] {"added: " + msg});
     }
 }
