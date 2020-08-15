4aaff34f67c9c7bc8b72df6fcccb4fa9d283113d
ruilingk <e0426268@u.nus.edu>
2020-08-15 19:10:07 +0800

    Update input.txt and EXPECTED.TXT

--- src/main/java/DukeErrorsHandler.java
+++ src/main/java/DukeErrorsHandler.java
@@ -3,25 +3,25 @@ public class DukeErrorsHandler {
             "\t---------------------------------------------------\n";
 
     public static void toDoInvalidDescription() {
-        System.out.println(divider
+        System.out.print(divider
                 + "\tThe description of a todo cannot be empty!\n"
                 + divider);
     }
 
     public static void deadlineInvalidDescription() {
-        System.out.println(divider
+        System.out.print(divider
                 + "\tThe description of a deadline cannot be empty!\n"
                 + divider);
     }
 
     public static void eventInvalidDescription() {
-        System.out.println(divider
+        System.out.print(divider
                 + "\tThe description of an event cannot be empty!\n"
                 + divider);
     }
 
     public static void invalidInput() {
-        System.out.println(divider
+        System.out.print(divider
                 + "\tOops! I'm not sure what you meant! Please try again!\n"
                 + divider);
     }
