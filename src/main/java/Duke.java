e7692d0dacfa1cdfcad5feb701cc3df67dcdd94c
Kacalamaka <heinrich2001.ang@gmail.com>
2020-08-15 21:24:50 +0800

    Level 1 Greet,Echo, Exit. Added greet,echo,and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+package main.java;
+
+import java.util.Scanner;
+
 public class Duke {
+    static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    static String line = "_____________________________________________________________________\n";
+
+    public void mainProgram() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(line + "Hello! I'm Duke.\nWhat can I do for you?\n" + line);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if (input.equals("bye") || input.equals("Bye")) {
+                bye();
+                break;
+            } else {
+                echo(input);
+            }
+        }
+    }
+
+    private void echo(String input) {
+        System.out.println(line + input + "\n" + line);
+    }
+
+    public void bye() {
+        System.out.println(line + "Bye.Hope to see you again soon!\n" + line);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Duke duke = new Duke();
         System.out.println("Hello from\n" + logo);
+        duke.mainProgram();
     }
 }
