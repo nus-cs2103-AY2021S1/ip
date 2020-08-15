69b5e41842c08603fee48e3f835e205bfdbdd6d8
lll-jy <E0376997@u.nus.edu>
2020-08-15 16:57:08 +0800

    Set up InvalidCommandException

---
+++ src/main/java/InvalidCommandException.java
@@ -0,0 +1,5 @@
+public class InvalidCommandException extends Exception {
+    public InvalidCommandException(String msg) {
+        super("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
+    }
+}
