175d789e8bc98332be919b62abb6d27e29f5388b
kerkpy <peiyongkerk@gmail.com>
2020-08-15 23:34:25 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,6 +21,7 @@ public class Duke {
             System.out.println("____________________________________________________________\n");
 
             if (str.equals("list")) {
+                System.out.println("Here are the tasks in your list: \n");
                 for (int i = 0; i < 100; i++) {
                     if (lstOfTasks[i] != null) {
                         System.out.println(i + 1 + ". " + lstOfTasks[i]);
@@ -31,11 +32,38 @@ public class Duke {
             } else if (str.startsWith("done")){
                 int temp = Integer.parseInt(str.substring(5));
                 lstOfTasks[temp - 1].doTask();
-            } else {
-                lstOfTasks[counter] = new Task(str);
-                System.out.println("added: " + str);
+            } else if (str.startsWith("todo")) {
+                str = str.substring(5);
+                lstOfTasks[counter] = new ToDo(str);
                 counter++;
-            }
+                System.out.println("Got it. I've added this task: \n"
+                    + lstOfTasks[counter - 1]
+                    + "\nNow you have " + counter + " task(s) in the list.");
+
+            } else if (str.startsWith("deadline")) {
+                str = str.substring(9);
+                String[] temp = str.split(" /by ");
+                String desc = temp[0];
+                String by = temp[1];
+                lstOfTasks[counter] = new Deadline(desc, by);
+                counter++;
+                System.out.println("Got it. I've added this task: \n"
+                    + lstOfTasks[counter - 1]
+                    + "\nNow you have " + counter + " task(s) in the list.");
+
+            } else if (str.startsWith("event")) {
+                str = str.substring(6);
+                String[] temp = str.split(" /at ");
+                String desc = temp[0];
+                String at = temp[1];
+                lstOfTasks[counter] = new Events(desc, at);
+                counter++;
+                System.out.println("Got it. I've added this task: \n"
+                    + lstOfTasks[counter - 1]
+                    + "\nNow you have " + counter + " task(s) in the list.");
+
+            } else {}
+
             System.out.println("____________________________________________________________\n");
 
             str = sc.nextLine();
