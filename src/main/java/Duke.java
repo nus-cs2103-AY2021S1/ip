97ce51a3adaa610df4956972e5553a68848f8126
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2020-08-15 17:18:30 +0800

    Add support for greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,47 @@
+import java.util.Scanner;
+
 public class Duke {
+    public final Scanner scanner;
+
+    public Duke() {
+        this.scanner = new Scanner(System.in);
+    }
+
     public static void main(String[] args) {
+        Duke duke = new Duke();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        duke.sendMessage(duke.getGreetMessage());
+        while (true) {
+            String input = duke.getInput();
+            if (input.equalsIgnoreCase("bye")) {
+                duke.sendMessage(duke.getByeMessage());
+                break;
+            } else {
+                duke.sendMessage(input);
+            }
+        }
+    }
+
+    public String getGreetMessage() {
+        return "Hello. What can I do for you?";
+    }
+
+    public String getByeMessage() {
+        return "Bye. Hope to see you again soon!";
+    }
+
+    public void sendMessage(String message) {
+        System.out.println(String.format("----------\n%s\n----------\n", message));
+        return;
+    }
+
+    public String getInput() {
+        String input = this.scanner.nextLine();
+        return input;
     }
 }
