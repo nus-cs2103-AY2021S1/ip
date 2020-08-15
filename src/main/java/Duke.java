2f2024e926d7795581624d0d61a1153e38a54c0f
Jing Yen Loh <lohjingyen@gmail.com>
2020-08-16 03:05:32 +0800

    Implement requirements of Level-2
    In this level, we are required to:
    - Add the ability to store whatever text entered by the user and display
      them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -11,6 +13,9 @@ public class Duke {
     private static final String BYE = "bye";
     private static final String GOODBYE_MESSAGE = "Ok lor like that lor.";
 
+    // ASSUMPTION: There will be no more than 100 tasks.
+    private static final List<String> TASKS = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Hi, I'm\n" + LOGO);
         Duke.displayMessage("What do you need this time 😫");
@@ -18,18 +23,41 @@ public class Duke {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("> ");
             while (scanner.hasNextLine()) {
-                String command = scanner.nextLine();
+                String input = scanner.nextLine();
 
-                if (command.equalsIgnoreCase(BYE)) {
+                if (input.equalsIgnoreCase(BYE)) {
                     break;
                 } else {
-                    Duke.displayMessage(command);
-                    System.out.print("> ");
+                    Duke.handleCommand(input);
                 }
+                System.out.print("> ");
             }
 
             Duke.displayMessage(GOODBYE_MESSAGE);
         }
+    }
+
+    private static void handleCommand(String input) {
+        if ("list".equals(input.toLowerCase())) {
+            Duke.displayTasks();
+        } else {
+            Duke.addTask(input);
+        }
+    }
+
+    private static void addTask(String task) {
+        TASKS.add(task);
+        Duke.displayMessage(String.format("Ok, you want to: %s", task));
+    }
+
+    private static void displayTasks() {
+        System.out.println("     ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+        System.out.println("     Right, you said you wanted to:");
+        for (int i = 0; i < TASKS.size(); i++) {
+            System.out.printf("     %3d: %s\n", i + 1, TASKS.get(i));
+        }
+        System.out.println("     ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+        System.out.println();
     }
 
     private static void displayMessage(String message) {
