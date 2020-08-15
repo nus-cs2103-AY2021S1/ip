e7e33deb66538c91b171e4360ca0272752d9e207
kelvinvin <kelvinw0ng@hotmail.com>
2020-08-16 00:52:46 +0800

    duke renamed to batman, echos user input and exits when user says "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner myObj = new Scanner(System.in);
+        Boolean botRunning = true;
+        String logo =
+                "___.           __                         \n"
+                        + "\\_ |__ _____ _/  |_  _____ _____    ____  \n"
+                        + " | __ \\__  \\     __\\/     \\__  \\  /     \\\n"
+                        + " | \\_\\ \\/ __ \\|  | |  Y Y  \\/ __ \\|   |  \\ \n"
+                        + " |___  (____  /__| |__|_|  (____  /___|  /\n"
+                        + "     \\/     \\/           \\/     \\/     \\/\n";
         System.out.println("Hello from\n" + logo);
+
+        while (botRunning) {
+            String input = myObj.nextLine();
+            if (input.equals("bye")) {
+                botRunning = false;
+            } else {
+                System.out.println(input);
+            }
+        }
+        System.out.println("SAYONARA HUMAN ಥ﹏ಥ");
     }
 }
