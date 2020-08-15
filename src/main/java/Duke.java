5319d902a483f0e64b6812bad49d05e5adb83aa8
Anthony Lie <e0407672@u.nus.edu>
2020-08-15 21:59:03 +0800

    Add level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,54 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private List<String> tasks;
     private String BYE_COMMAND = "bye";
+    private String LIST_COMMAND = "list";
 
-    public void echo(String text) {
-        String message = text;
-        sendMessage(createLine(message));
+    Duke() {
+        tasks = new ArrayList<>();
+    }
+
+    private void addTask(String task) {
+        String message = "Added: " + task;
+        tasks.add(task);
+        System.out.println(message);
     }
 
     private void welcome() {
         String message = "Hi! My name is Duke.\nWhat do you want me to do?";
-        sendMessage(createLine(message));
+        sendMessage(message);
     }
 
     private void exit() {
         String message = "Bye. Thank you for using me!";
-        sendMessage(createLine(message));
+        sendMessage(message);
     }
 
-    public void takeUserInput() {
+    private void showAllTask() {
+        String message;
+
+        if (tasks.isEmpty()) {
+            message = "You haven't add any task!";
+        } else {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < tasks.size(); i++) {
+                sb.append((i + 1) + ". " + tasks.get(i) + "\n");
+            }
+            message = sb.toString().trim();
+        }
+
+        sendMessage(message);
+
+
+    }
+
+    private void takeUserInput() {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -28,17 +56,19 @@ public class Duke {
 
             if (userInput.equals(BYE_COMMAND)) {
                 break;
+            }else if (userInput.equals(LIST_COMMAND)) {
+                showAllTask();
             } else {
-                echo(userInput);
+                addTask(userInput);
             }
         }
     }
 
     private void sendMessage(String sendMessage) {
-        System.out.println(sendMessage);
+        System.out.println(createLine(sendMessage));
     }
 
-    public String createLine(String response) {
+    private String createLine(String response) {
         Scanner sc = new Scanner(response);
         String equalSign = "=";
         String indentation = "  ";
