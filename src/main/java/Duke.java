9ee94432ea987ce7dfd7306c99a4eee21fdc6220
FH-30 <finxictus@gmail.com>
2020-08-15 15:27:50 +0800

    Greet user; Echo commands from user; Exits on bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,33 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String greeting = "    ____________________________________________________________\n" +
+                "    Hello! I'm Duke\n" +
+                "    What can I do for you?\n" +
+                "    ____________________________________________________________";
+        System.out.println(greeting);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            if (!input.equals("")) {
+                String output = "    ____________________________________________________________\n" +
+                        "     " + input + "\n" +
+                        "    ____________________________________________________________";
+                System.out.println(output);
+            }
+        }
+
+        String byeMessage = "    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________";
+        System.out.println(byeMessage);
     }
 }
