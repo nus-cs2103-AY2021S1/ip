4dfffa5f0684c6701dd573727c7979cc16b5fb78
wilinetan <wilinetan26@gmail.com>
2020-08-15 22:51:41 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.rmi.server.ExportException;
 import java.util.Scanner;
 
 public class Duke {
@@ -23,41 +24,52 @@ public class Duke {
                     System.out.println((i + 1) + "." + task);
                 }
             } else {
-//                String[] splitStr = input.split(" ");
-                if (input.contains("done")) {
-                    String[] splitStr = input.split(" ");
-                    int taskIndex = Integer.parseInt(splitStr[1]) - 1;
-                    Task task = tasks[taskIndex];
-                    task.markAsDone();
-                    System.out.println("Nice! I've marked this task as done:\n" + task);
-
-                } else if (input.contains("todo")) {
-                    Todo todo = new Todo(input.substring(5));
-                    tasks[index] = todo;
-                    index++;
-                    System.out.println("Got it. I've added this task:\n" + todo + "\nNow you have "
-                            + index + " tasks in the list.");
-                } else if (input.contains("deadline")) {
-                    String[] splitStr = input.split("/by ");
-                    String description = splitStr[0].substring(9).trim();
-                    Deadline deadline = new Deadline(description, splitStr[1]);
-                    tasks[index] = deadline;
-                    index++;
-                    System.out.println("Got it. I've added this task:\n" + deadline + "\nNow you have "
-                            + index + " tasks in the list.");
-                } else if (input.contains("event")) {
-                    String[] splitStr = input.split("/at ");
-                    String description = splitStr[0].substring(6).trim();
-                    Event event = new Event(description, splitStr[1]);
-                    tasks[index] = event;
-                    index++;
-                    System.out.println("Got it. I've added this task:\n" + event + "\nNow you have "
-                            + index + " tasks in the list.");
-                } else {
-                    Task task = new Task(input);
-                    tasks[index] = task;
-                    index++;
-                    System.out.println("added: " + task);
+                try {
+                    if (input.contains("done")) {
+                        String[] splitStr = input.split(" ");
+                        if (splitStr.length == 1) {
+                            throw new DukeException("☹ OOPS!!! I don't know which task to mark as done.");
+                        }
+                        int taskIndex = Integer.parseInt(splitStr[1]) - 1;
+                        Task task = tasks[taskIndex];
+                        task.markAsDone();
+                        System.out.println("Nice! I've marked this task as done:\n" + task);
+                    } else if (input.contains("todo")) {
+                        if (input.length() <= 5) {
+                            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                        }
+                        Todo todo = new Todo(input.substring(5));
+                        tasks[index] = todo;
+                        index++;
+                        System.out.println("Got it. I've added this task:\n" + todo + "\nNow you have "
+                                + index + " tasks in the list.");
+                    } else if (input.contains("deadline")) {
+                        if (input.length() <= 9 || !input.contains("/by")) {
+                            throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                        }
+                        String[] splitStr = input.split("/by ");
+                        String description = splitStr[0].substring(9).trim();
+                        Deadline deadline = new Deadline(description, splitStr[1]);
+                        tasks[index] = deadline;
+                        index++;
+                        System.out.println("Got it. I've added this task:\n" + deadline + "\nNow you have "
+                                + index + " tasks in the list.");
+                    } else if (input.contains("event")) {
+                        if (input.length() <= 6 || !input.contains("/at")) {
+                            throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
+                        }
+                        String[] splitStr = input.split("/at ");
+                        String description = splitStr[0].substring(6).trim();
+                        Event event = new Event(description, splitStr[1]);
+                        tasks[index] = event;
+                        index++;
+                        System.out.println("Got it. I've added this task:\n" + event + "\nNow you have "
+                                + index + " tasks in the list.");
+                    } else {
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
+                } catch (DukeException ex) {
+                    System.out.println(ex);
                 }
             }
         }
