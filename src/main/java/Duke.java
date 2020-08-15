2027463f2593fb06aa39a1a9d6c97bf9d3516beb
boundtotheearth <farrellnah@gmail.com>
2020-08-15 10:53:04 +0800

    Add Level-2 Requirements
    The program echos user input.
    
    The program should remember user input and display them
    back to the user when requested.
    
    Let's
    * Add a 'list' command that lists previous commands
    * Remember all input that is not 'list' and 'bye'
    
    See below for more information
    https://nus-cs2103-ay2021s1.github.io/website/schedule/week2/project.html

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -21,17 +23,22 @@ public class Duke {
         System.out.println(logo);
         printInWindow("Hello, I'm a banana.\nWhat can I do for you?");
 
+        List<String> tasks = new ArrayList<>();
+
         while(sc.hasNextLine()) {
             String command = sc.nextLine();
 
             switch(command) {
+            case "list":
+                printInWindow(formatTaskListToString(tasks));
+                break;
             case "bye":
                 printInWindow("Bye. Hope to see you again soon!");
                 return;
+            default:
+                tasks.add(command);
+                printInWindow("Added: " + command);
             }
-
-            printInWindow(command);
-
         }
     }
 
@@ -40,5 +47,18 @@ public class Duke {
         System.out.println(divider);
         System.out.println(text);
         System.out.println(divider);
+    }
+
+    public static String formatTaskListToString(List<String> tasks) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < tasks.size(); i++) {
+            sb.append(i)
+                    .append(". ")
+                    .append(tasks.get(i));
+            if(i < tasks.size() - 1) {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
     }
 }
