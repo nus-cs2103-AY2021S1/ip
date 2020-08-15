e0fc3c5bae6ccf3ee1a06740062fa4fbc6b0741a
shawn-nyk <shawnngyongkwang@gmail.com>
2020-08-16 02:19:44 +0800

    Add greet, echo and exit functionality
    Duke now greets the user, will echo the user's input, and the program
    will exit when the user inputs "bye". Duke's name has also been
    changed to "Star Bot", as in that is the name that it goes by during
    user interaction, but the class and file name is left as "Duke" for the
    purposes of this assignment.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String logo = "     _______.___________.    ___      " +
+            ".______     " +
+            " \n" +
+            "    /       |           |   /   \\     |   _  \\     \n" +
+            "   |   (----`---|  |----`  /  ^  \\    |  |_)  |    \n" +
+            "    \\   \\       |  |      /  /_\\  \\   |      /     \n" +
+            ".----)   |      |  |     /  _____  \\  |  |\\  \\----.\n" +
+            "|_______/       |__|    /__/     \\__\\ | _| `._____|\n" +
+            "                                                   \n" +
+            "         .______     ______   .___________.        \n" +
+            "         |   _  \\   /  __  \\  |           |        \n" +
+            "         |  |_)  | |  |  |  | `---|  |----`        \n" +
+            "         |   _  <  |  |  |  |     |  |             \n" +
+            "         |  |_)  | |  `--'  |     |  |             \n" +
+            "         |______/   \\______/      |__|             \n" +
+            "                                                   ";
+
+    private static String divider =
+            "------------------------------------------------------\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println(logo + "\nHello, I'm Star Bot! What can I do for " +
+                "you?\nSay \"bye\" to exit.\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()) {
+            String line = sc.nextLine();
+            if (line.equals("bye")) {
+                System.out.println(botReply("Goodbye, see you again soon!"));
+                System.exit(0);
+            } else {
+                System.out.println(botReply(line));
+            }
+        }
+    }
+
+    private static String botReply(String reply) {
+        return divider + reply + "\n" + divider;
     }
 }
