503df439b2d6b034b9cd38a8ec06a7c8ac21c240
jonahtanjz <jonahtanjz4884@gmail.com>
2020-08-16 00:21:24 +0800

    Changed task from String to Class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,17 +7,17 @@ public class Duke {
         return line + "\n\t\t" + text.replaceAll("\\n", "\n\t\t") + "\n" + line;
     }
 
-    private static String listTasks(ArrayList<String> tasks) {
+    private static String listTasks(ArrayList<Task> tasks) {
         String listOfTasks = "";
         for (int i = 0; i < tasks.size(); i++) {
-            listOfTasks = listOfTasks + (i + 1) + ". " + tasks.get(i)
+            listOfTasks = listOfTasks + (i + 1) + ". " + tasks.get(i).getTaskDescription()
                     + (i == tasks.size() - 1 ? "" : "\n");
         }
         return listOfTasks;
     }
     public static void main(String[] args) {
         System.out.println(formatReply("Hello! I'm Duke\nWhat can I do for you?"));
-        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         while (true) {
             String command = input.nextLine();
@@ -26,10 +26,28 @@ public class Duke {
             } else if (command.equals("list")) {
                 System.out.println(formatReply(listTasks(taskList)));
             } else {
-                taskList.add(command);
+                taskList.add(new Task(command));
                 System.out.println(formatReply("added: " + command));
             }
         }
         System.out.println(formatReply("Bye. Hope to see you again soon!"));
+    }
+}
+
+class Task {
+    private String taskDescription;
+    private boolean done;
+
+    public Task(String taskDescription) {
+        this.taskDescription = taskDescription;
+        this.done = false;
+    }
+
+    public String getTaskDescription() {
+        return this.taskDescription;
+    }
+
+    public boolean getDone() {
+        return this.done;
     }
 }
