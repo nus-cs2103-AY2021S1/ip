c586f66131187a63bbab3d5e69c37122befedd9e
Criss-Wang <djj87018507@gmail.com>
2020-08-15 18:35:28 +0800

    Clear up DukeBuffer

--- src/main/java/DukeBuffer.java
+++ src/main/java/DukeBuffer.java
@@ -4,21 +4,4 @@
  * all the logic easily for now.
  */
 public class DukeBuffer {
-//    private TaskList taskList;
-//
-//    public DukeBuffer() {
-//        this.taskList = new TaskList();
-//    }
-//
-//    public DukeBuffer(TaskList taskList) {
-//        this.taskList = new TaskList();
-//    }
-//
-//    public DukeBuffer addTask(Task task) {
-//        return new DukeBuffer(taskList.addToList(task));
-//    }
-//
-//    public String showTasks() {
-//        return this.taskList.printList();
-//    }
 }
