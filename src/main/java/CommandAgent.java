12e288e89a06cd9bd150ae8254dc15b576a3abb2
Criss-Wang <djj87018507@gmail.com>
2020-08-15 18:54:23 +0800

    Add feedback generation methods and restructure handleCommand() code
    The feedback displayed can be decorated to look better. Feedback
    generation should be done in this class. The creation of a task can also
    be done in the agent as command should not have task creation ability.
    
    In light of this, let's create feedback generation methods for each of
    the creation/update/retrieval. Make sure the taskList is updated within
    the handleCommand() method and respective feedback are produced for each
    command request.

--- src/main/java/CommandAgent.java
+++ src/main/java/CommandAgent.java
@@ -1,14 +1,17 @@
+import java.util.List;
+
 import command.Command;
+import task.Deadline;
+import task.Event;
+import task.Task;
+import task.TaskList;
+import task.Todo;
 
 /**
  * An agent to process incoming command and return the feedback.
  */
 public class CommandAgent {
-    private static TaskList taskList;
-
-    public CommandAgent() {
-        this.taskList = new TaskList();
-    }
+    private static TaskList taskList = new TaskList();
 
     /**
      * Takes in the command and handle it based on the request from the command.
@@ -17,27 +20,82 @@ public class CommandAgent {
      * @return a String feedback for the user.
      */
     public String handleCommand(Command command) {
-        String feedback;
+        String feedback = "____________________________________________________________\n";
         switch (command.sendRequest()) {
         case "end":
-            feedback = "Bye. Hope to see you again soon!";
+            feedback += "Bye. Hope to see you again soon!";
             break;
         case "create":
-            String name = command.getContent();
-            taskList = taskList.addTask(new Task(name));
-            feedback = "added: " + name;
+            List<String> taskInfo = command.getContent();
+            Task newTask = this.createTask(taskInfo);
+            taskList = taskList.addTask(newTask);
+            feedback += this.generateCreateFeedback();
             break;
         case "retrieval":
-            feedback = taskList.printTasks();
+            feedback += this.generateRetrievalFeedback();
             break;
         case "update":
-            int task_id = Integer.parseInt(command.getContent());
-            taskList = taskList.markAsDone(task_id);
-            feedback = taskList.generateFeedbackById(task_id);
+            List<String> contentList = command.getContent();
+            int taskId = Integer.parseInt(contentList.get(0));
+            taskList = taskList.markAsDone(taskId);
+            feedback += this.generateUpdateFeedback(taskId);
             break;
         default:
-            feedback = "";
+            feedback += "";
         }
+        feedback += "\n____________________________________________________________\n";
         return feedback;
+    }
+
+    /**
+     * Create different types of tasks based on the identifier stored in the taskInfo.
+     *
+     * @param taskInfo a list of String containing all the relevant information for the task.
+     * @return a correct type of Task object.
+     */
+    public Task createTask(List<String> taskInfo) {
+        String identifier = taskInfo.get(0);
+        String name = taskInfo.get(1);
+        String schedule;
+        switch (identifier) {
+        case "E":
+            schedule = taskInfo.get(2);
+            return new Event(name, false, schedule);
+        case "T":
+            return new Todo(name, false);
+        case "D":
+            schedule = taskInfo.get(2);
+            return new Deadline(name, false, schedule);
+        default:
+            return new Task(name);
+        }
+    }
+
+    /**
+     * Generate the feedback for a task creation.
+     * @return a String suggesting the completion of task creation.
+     */
+    public String generateCreateFeedback() {
+        int taskId = taskList.getSize();
+        Task currentTask = taskList.getTaskById(taskId);
+        String result = "Got it. I've added this task:\n  ";
+        result += currentTask;
+        result += String.format("\nNow you have %d tasks in the list.", taskId);
+        return result;
+    }
+
+    /**
+     * Generate the feedback for an update of task status.
+     *
+     * @param taskId the displayed id in the taskList.
+     * @return a String suggesting the completion of task update.
+     */
+    public String generateUpdateFeedback(int taskId) {
+        Task currentTask = taskList.getTaskById(taskId);
+        return "Nice! I've marked this task as done:\n  " + currentTask;
+    }
+
+    public String generateRetrievalFeedback() {
+        return taskList.printTasks();
     }
 }
