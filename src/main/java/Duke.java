77e1d6b8b99d64a6fe44b56b99c09b9d32605957
Ngo Duc Binh <admin@admins-MacBook-3.local>
2020-08-16 02:39:56 +0800

    Increment level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+package main.java;
+import java.util.Scanner;
+
 public class Duke {
+    public static void echoInput() {
+        Scanner readInput = new Scanner(System.in);
+        String currentWord = readInput.next();
+        while (!currentWord.equals("bye")) {
+            System.out.println(currentWord);
+            currentWord = readInput.next();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        echoInput();
     }
 }
