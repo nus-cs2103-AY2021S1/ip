bb296612fa49be861ffdc093814ba9855da043fe
Asthenosphere <e0271247@u.nus.edu>
2020-08-15 17:02:00 +0800

    feat: built echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,51 @@
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        init();
+    }
+
+    static void init() {
         Calendar now = Calendar.getInstance();
         int hour = now.get(Calendar.HOUR_OF_DAY);
 
-        String logo =
-                "         ,---._                                                            \n" +
-                "       .-- -.' \\    ,---,       ,-.----.                 ,---,  .--.--.    \n" +
-                "       |    |   :  '  .' \\      \\    /  \\        ,---.,`--.' | /  /    '.  \n" +
-                "       :    ;   | /  ;    '.    ;   :    \\      /__./||   :  :|  :  /`. /  \n" +
-                "       :        |:  :       \\   |   | .\\ : ,---.;  ; |:   |  ';  |  |--`   \n" +
-                "       |    :   ::  |   /\\   \\  .   : |: |/___/ \\  | ||   :  ||  :  ;_     \n" +
-                "       :         |  :  ' ;.   : |   |  \\ :\\   ;  \\ ' |'   '  ; \\  \\    `.  \n" +
-                "       |    ;   ||  |  ;/  \\   \\|   : .  / \\   \\  \\: ||   |  |  `----.   \\ \n" +
-                "   ___ l         '  :  | \\  \\ ,';   | |  \\  ;   \\  ' .'   :  ;  __ \\  \\  | \n" +
-                " /    /\\    J   :|  |  '  '--'  |   | ;\\  \\  \\   \\   '|   |  ' /  /`--'  / \n" +
-                "/  ../  `..-    ,|  :  :        :   ' | \\.'   \\   `  ;'   :  |'--'.     /  \n" +
-                "\\    \\         ; |  | ,'        :   : :-'      :   \\ |;   |.'   `--'---'   \n" +
-                " \\    \\      ,'  `--''          |   |.'         '---\" '---'                \n" +
-                "  \"---....--'                   `---'                                      \n";
+        String logo = "         ,---._                                                            \n" +
+                        "       .-- -.' \\    ,---,       ,-.----.                 ,---,  .--.--.    \n" +
+                        "       |    |   :  '  .' \\      \\    /  \\        ,---.,`--.' | /  /    '.  \n" +
+                        "       :    ;   | /  ;    '.    ;   :    \\      /__./||   :  :|  :  /`. /  \n" +
+                        "       :        |:  :       \\   |   | .\\ : ,---.;  ; |:   |  ';  |  |--`   \n" +
+                        "       |    :   ::  |   /\\   \\  .   : |: |/___/ \\  | ||   :  ||  :  ;_     \n" +
+                        "       :         |  :  ' ;.   : |   |  \\ :\\   ;  \\ ' |'   '  ; \\  \\    `.  \n" +
+                        "       |    ;   ||  |  ;/  \\   \\|   : .  / \\   \\  \\: ||   |  |  `----.   \\ \n" +
+                        "   ___ l         '  :  | \\  \\ ,';   | |  \\  ;   \\  ' .'   :  ;  __ \\  \\  | \n" +
+                        " /    /\\    J   :|  |  '  '--'  |   | ;\\  \\  \\   \\   '|   |  ' /  /`--'  / \n" +
+                        "/  ../  `..-    ,|  :  :        :   ' | \\.'   \\   `  ;'   :  |'--'.     /  \n" +
+                        "\\    \\         ; |  | ,'        :   : :-'      :   \\ |;   |.'   `--'---'   \n" +
+                        " \\    \\      ,'  `--''          |   |.'         '---\" '---'                \n" +
+                        "  \"---....--'                   `---'                                      \n";
         if (hour < 12) {
-            System.out.println("Good morning boss! This is\n" + logo);
+            System.out.println("\tGood morning boss! This is\n" + logo);
         } else if (hour < 20) {
-            System.out.println("Good afternoon boss! This is\n" + logo);
+            System.out.println("\tGood afternoon boss! This is\n" + logo);
         } else {
-            System.out.println("Good evening boss! This is\n" + logo);
+            System.out.println("\tGood evening boss! This is\n" + logo);
         }
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t___________________________________________________________________________\n");
+
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        while (!input.equals("bye")) {
+            echo(input);
+            input = s.nextLine();
+        }
+        echo("\tBye. Hope to see you again soon");
+    }
+
+    static void echo(String s) {
+        System.out.println("\t___________________________________________________________________________");
+        System.out.println("\t" + s);
+        System.out.println("\t___________________________________________________________________________\n");
     }
 }
