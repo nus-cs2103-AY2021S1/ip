8b2f4b31df5eae5cf87b1866f9796bc6d4be528f
jonahtanjz <jonahtanjz4884@gmail.com>
2020-08-16 00:09:42 +0800

    Added 'add tasks' feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static String formatReply(String text) {
         String line = "\t_______________________________________________________________";
         return line + "\n\t\t" + text.replaceAll("\\n", "\n\t\t") + "\n" + line;
     }
+
     public static void main(String[] args) {
         System.out.println(formatReply("Hello! I'm Duke\nWhat can I do for you?"));
+        ArrayList<String> taskList = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         while (true) {
             String command = input.nextLine();
             if (command.equals("bye")) {
                 break;
             } else {
-                System.out.println(formatReply(command));
+                taskList.add(command);
+                System.out.println(formatReply("added: " + command));
             }
         }
         System.out.println(formatReply("Bye. Hope to see you again soon!"));
