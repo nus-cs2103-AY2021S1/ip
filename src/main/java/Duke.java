09545c7eedab02ad00ee6aa7f61397b4dec428a7
lerxcl <lerx98@gmail.com>
2020-08-16 00:52:03 +0800

    -Modify "Welcome" and "bye" message -Change name from "Duke" to "Bob" -Complete Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,19 +3,31 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] list = new String[100];
+        int index = 0;
         String divider = "____________________________________________________________";
-        String intro = "Hello! I'm Duke\n" +
+        String intro = "Hello! I'm Bob\n" +
                 "What can I do for you?\n";
 
         System.out.println(divider + "\n" + intro + "\n" + divider);
         String input = sc.next();
 
         while(!input.equals("bye")) {
-            System.out.println(divider + "\n" + input + "\n" + divider);
+            if (input.equals("list")) {
+                System.out.println(divider);
+                for (int i = 0; i < index; i++) {
+                    System.out.println(list[i]);
+                }
+                System.out.println(divider);
+            } else {
+                list[index] = (index + 1) + ". " + input;
+                index++;
+                System.out.println(divider + "\n" + "added: " + input + "\n" + divider);
+            }
             input = sc.next();
         }
 
-        String message = "Bye. Hope to see you again soon!";
+        String message = "Bye. Hope to see you again soon! :)";
         System.out.println(divider + "\n" + message + "\n" + divider);
     }
 }
