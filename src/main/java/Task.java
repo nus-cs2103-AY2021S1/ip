0afab54696a71d1f594f19449efc4ed04c316ef9
lll-jy <E0376997@u.nus.edu>
2020-08-15 15:54:02 +0800

    Generate method move to Duke.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -15,32 +15,6 @@ public class Task {
         isDone = true;
     }
 
-    public static Task generate(String cmd) {
-        if (cmd.startsWith("todo ")) {
-            return new ToDo(cmd.substring(5));
-        } else if (cmd.startsWith("deadline ")) {
-            String description = cmd.substring(9);
-            int s = description.indexOf("/by");
-            if (s == -1) {
-                return null;
-            }
-            String time = description.substring(s + 4);
-            description = description.substring(0, s - 1);
-            return new Deadline(description, time);
-        } else if (cmd.startsWith("event ")) {
-            String description = cmd.substring(6);
-            int s = description.indexOf("/at");
-            if (s == -1) {
-                return null;
-            }
-            String time = description.substring(s + 4);
-            description = description.substring(0, s - 1);
-            return new Event(description, time);
-        } else {
-            return null;
-        }
-    }
-
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
