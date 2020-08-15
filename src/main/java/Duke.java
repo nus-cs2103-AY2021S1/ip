168b69b08aace971ac7418c8a00e2cee8f5ba731
vanGoghhh <blackznigelng@gmail.com>
2020-08-15 15:50:04 +0800

    Add-List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,61 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
-    // Method to intiate the bot and allow it to echo the user's command or exit the bot
-    public static void startBot() {
-        String welcome = "Hello I am Duke!\nHow can I help you?";
+    private ArrayList<String> messages;
+
+    // Constructor for the bot
+    public Duke(ArrayList<String> messages) {
+        this.messages = messages;
+    }
+
+    // Method to intiate the bot
+    private static void startBot() {
+        String welcome = "Hello I am Duke!\nHow can I help you?\n";
         System.out.println(welcome);
+
+        Duke newBot = new Duke(new ArrayList<String>());
+
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
+        while (sc.hasNextLine()) {
             String message = sc.next();
+            message += sc.nextLine();
             if (message.equals("bye")) {
-                System.out.println("Bye! Have a nice day!");
+                System.out.println("\nBye! Have a nice day!\n");
                 break;
+            } else if (message.equals("list")){
+                if (newBot.messages.isEmpty()) {
+                    System.out.println("\nThere are currently no messages stored!\n");
+                } else {
+                    newBot.displayMessages();
+                }
             } else {
-                System.out.println(message);
+                newBot.addMessages(message);
             }
         }
     }
 
+    // Method to add new messages to the bot
+    private void addMessages(String message) {
+        this.messages.add(message);
+        System.out.println("\nadded: " + message + "\n");
+    }
+
+    // Method to display all messages
+    private void displayMessages() {
+        int index = 1;
+        System.out.println("\n");
+        for (String text: this.messages) {
+            if (text != null) {
+                System.out.println(index + ". " + text);
+                index++;
+            }
+        }
+        System.out.println("\n");
+    }
+
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
         startBot();
     }
 }
