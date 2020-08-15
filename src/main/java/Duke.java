7fdebeca96eec54ba1e9c95c04fa1f06eb1fe8c3
Perpetual09 <jackgao0309@gmail.com>
2020-08-15 12:59:47 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -45,25 +45,4 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
     }
 
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-        }
-
-        public String getDescription() {
-            return this.description;
-        }
-        public void done() {
-            this.isDone = true;
-        }
-    }
-
 }
