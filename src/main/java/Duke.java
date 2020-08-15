c6f294fa44dd0ad115a770d804dd04a98c8ed359
lll-jy <E0376997@u.nus.edu>
2020-08-15 14:54:08 +0800

    New method to detect mark-as-done command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 
 public class Duke {
     private static final String horizontalLine = "\t==================================================\n\t  ";
+    private static int n = -1;
 
     private static String output(String message) {
         return horizontalLine + message + "\n" + horizontalLine;
+    }
+
+    private static boolean isDoneCommand(String cmd) {
+        if (cmd.startsWith("done ")) {
+            try {
+                n = Integer.parseInt(cmd.substring(5));
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
@@ -21,6 +35,8 @@ public class Duke {
                     System.out.println("\t  " + (i + 1) + "." + list[i]);
                 }
                 System.out.println(horizontalLine);
+            } else if (isDoneCommand(input)) {
+                System.out.println("Mark as done: " + n);
             } else {
                 System.out.println(output("added: " + input));
                 list[count++] = new Task(input);
