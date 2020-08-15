99f23f72c2d31ca5e92d946fece5cfcca912c81d
wenhao <wenhaogoh1998@gmail.com>
2020-08-15 13:09:22 +0800

    Implement skeletal functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
+    private Scanner scanner;
+    private String logo, greetingMessage, exitMessage;
+
+    public void initialise() {
+        scanner = new Scanner(System.in);
+        logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greetingMessage = "Hello! My name is Duke.\n" + "What can I do for you?";
+        exitMessage = "Bye. Hope to see you again soon!";
+        greet();
+        listenForCommands();
+    }
+
+    private void greet() {
+        System.out.println(logo);
+        System.out.println(greetingMessage);
+    }
+
+    private void listenForCommands() {
+        String command = scanner.nextLine();
+        switch (command) {
+            case ("bye"):
+                System.out.println(exitMessage);
+                break;
+            default:
+                System.out.println(command);
+                listenForCommands();
+        }
     }
 }
