b4f77f0cfbba96415b0dd6c6150e6050aba33717
lll-jy <E0376997@u.nus.edu>
2020-08-15 15:21:36 +0800

    Generate task with given string

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -15,6 +15,26 @@ public class Task {
         isDone = true;
     }
 
+    public static Task generate(String cmd) {
+        if (cmd.startsWith("todo ")) {
+            return new ToDo(cmd.substring(5));
+        } else if (cmd.startsWith("deadline ")) {
+            String description = cmd.substring(9);
+            int s = description.indexOf("/by");
+            String time = description.substring(s + 4);
+            description = description.substring(0, s - 1);
+            return new Deadline(description, time);
+        } else if (cmd.startsWith("event ")) {
+            String description = cmd.substring(6);
+            int s = description.indexOf("/at");
+            String time = description.substring(s + 4);
+            description = description.substring(0, s - 1);
+            return new Event(description, time);
+        } else {
+            return null;
+        }
+    }
+
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
