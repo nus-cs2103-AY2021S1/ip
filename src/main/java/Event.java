22c19b2a036e5843f130b67b0606e930d71eaef8
Junhua <jhsibeizai@gmail.com>
2020-08-15 23:32:59 +0800

    Create Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,27 @@
+public class Event extends Task{
+
+    //Store the variables
+    protected String at;
+
+    //Constructor for the event class
+    public Event(String description, String at){
+
+        //Call the superclass constructor
+        super(description);
+
+        //Store the at variable
+        this.at = at;
+    }
+
+    //Getter for the date of the
+    public String getDate(){
+
+        //Return the date
+        return this.at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + String.format(" (at:%s)", this.getDate());
+    }
+}
