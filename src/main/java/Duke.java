e2fcccf0a4883121fef653be0779d6e8c905022f
Dexter Tan <dev@dextertanyj.com>
2020-08-15 15:45:19 +0800

    Add basic commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String DIVIDER = "    ________________________________________________________\n";
+    private static final String EXIT_COMMAND = "bye";
+
+    private static void greet() {
+        String welcomeMessage = "    Konnichiwa!\n"
+                + "    What can I do for you?\n";
+        System.out.println(DIVIDER + welcomeMessage + DIVIDER);
+    }
+
+    private static void exit() {
+        String exitMessage = "    Ja ne!\n";
+        System.out.println(DIVIDER + exitMessage + DIVIDER);
+    }
+
+    private static void echo(String message) {
+        System.out.println(DIVIDER + "    " + message + "\n" + DIVIDER);
+    }
+
+    private static void chat() {
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        input = sc.nextLine();
+        while (!input.equals(EXIT_COMMAND)) {
+            echo(input);
+            input = sc.nextLine();
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        chat();
+        exit();
     }
 }
