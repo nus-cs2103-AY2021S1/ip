1a48d1e129668a07fe429ef19a73d7974793e05e
samlsm <samlsm2803@gmail.com>
2020-08-16 06:08:53 +0800

    Edited Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Duke {
+    private static String lines = ".~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.\n";
+
+    public static void list(String message) {
+        System.out.println(lines + " " + message+ "\n" + lines);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner input = new Scanner(System.in);
+        String welcome = " Hello! I'm Yuki *Woof*\n What can I do for you? *Woof*\n";
+        String bye = " Bye. Hope to see you again soon! *Woof woof*";
+
+        System.out.println(lines + welcome + lines);
+
+        while (input.hasNext()) {
+            String query = input.next();
+            if (query.equalsIgnoreCase("bye")) {
+                System.out.println(lines + bye + "\n" + lines);
+                input.close();
+                break;
+            } else {
+                list(query);
+            }
+        }
+
     }
 }
