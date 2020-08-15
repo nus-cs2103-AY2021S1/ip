f991bbd5fb5fa95f3f8eb4b762e28a90270d703b
JinHao-L <limjinhao@gmail.com>
2020-08-15 18:34:29 +0800

    Implement add, list functionality

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Alice {
+    private final ArrayList<String> ls;
 
     public Alice() {
+        ls = new ArrayList<>();
         greet();
         prompt();
     }
@@ -11,17 +14,30 @@ public class Alice {
         if (s.equals("bye")) {
             display("Goodbye. Hope to see you again soon!");
             return false;
+        } else if (s.equals("list")) {
+            print_list();
+            prompt();
+            return true;
+        } else {
+            ls.add(s);
+            display("added: " + s);
+            prompt();
+            return true;
         }
-
-        display(s);
-        prompt();
-        return true;
     }
 
     private void display(String s) {
         System.out.println("____________________________________________________________\n"
                 + s
                 + "\n____________________________________________________________");
+    }
+
+    private void print_list() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < ls.size(); i++) {
+            System.out.println(i+1 + ". " + ls.get(i));
+        }
+        System.out.println("____________________________________________________________");
     }
 
     private void greet() {
