f84b98cc77318cf17cd7e58695a260cbb9001b15
FH-30 <finxictus@gmail.com>
2020-08-16 00:10:13 +0800

    Introduced Exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
+
+    private static void log(String message) {
+        String startLine = "    ____________________________________________________________\n";
+        String endLine = "    ____________________________________________________________";
+        String output = startLine + message + endLine;
+        System.out.println(output);
+    }
+
+    public static void main(String[] args) throws DukeException{
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -12,78 +21,101 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String startLine = "    ____________________________________________________________\n";
-        String endLine = "    ____________________________________________________________";
-
-        String greeting =  startLine +
-                "    Hello! I'm Duke\n" +
-                "    What can I do for you?\n" +
-                endLine;
-        System.out.println(greeting);
+        String greeting = "    Hello! I'm Duke\n" +
+                "    What can I do for you?\n";
+        log(greeting);
 
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> todos = new ArrayList<>();
 
         while (sc.hasNextLine()) {
-            String input = sc.nextLine();
+            try {
+                String input = sc.nextLine().trim();
 
-            if (input.equals("bye")) {
-                break;
-            }
-
-            if (input.startsWith("done")) {
-                int idx = Integer.parseInt(input.substring(5)) - 1;
-                Task toChange = todos.get(idx);
-                toChange.markAsDone();
-                String output = startLine +
-                        "     Nice! I've marked this task as done:\n" +
-                        "       " + toChange + "\n" +
-                        endLine;
-
-                System.out.println(output);
-            } else if (input.equals("list")) {
-                StringBuilder output = new StringBuilder(startLine).append("     Here are the tasks in your list:\n");
-
-                for (int i = 1; i <= todos.size(); i++) {
-                    Task theTask = todos.get(i - 1);
-                    output.append("\t ").append(i).append(".").append(theTask).append("\n");
+                if (input.equals("bye")) {
+                    break;
                 }
 
-                output.append(endLine);
+                if (input.startsWith("done")) {
+                    if (input.length() == 4 || input.substring(5).isBlank()) {
+                        throw new DukeException("\tNo task number specified.");
+                    }
+                    int idx;
+                    try {
+                        idx = Integer.parseInt(input.substring(5).trim()) - 1;
+                    } catch (NumberFormatException e) {
+                        throw new DukeException("\tTask number format invalid, " +
+                                "must be a number.");
+                    }
+                    if (idx < 0 || idx > todos.size() - 1) {
+                        throw new DukeException("\tThere is no such task.");
+                    }
+                    Task toChange = todos.get(idx);
+                    toChange.markAsDone();
+                    String output = "     Nice! I've marked this task as done:\n" +
+                            "       " + toChange + "\n";
+                    log(output);
+                } else if (input.equals("list")) {
+                    if (todos.size() == 0) {
+                        log("\tYay! You have nothing to do at the moment! :-)\n");
+                    } else {
+                        StringBuilder output = new StringBuilder("     Here are the tasks in your list:\n");
 
-                System.out.println(output);
-            } else if (!input.equals("")) {
-                Task newTask = null;
-                String desc;
+                        for (int i = 1; i <= todos.size(); i++) {
+                            Task theTask = todos.get(i - 1);
+                            output.append("\t ").append(i).append(".").append(theTask).append("\n");
+                        }
 
-                if (input.startsWith("todo")) {
-                    desc = input.substring(5);
-                    newTask = new Todo(desc);
-                } else if (input.startsWith("deadline")) {
-                    String[] components = input.split(" /by ");
-                    desc = components[0].substring(9);
-                    newTask = new Deadline(desc, components[1]);
-                } else if (input.startsWith("event")) {
-                    String[] components = input.split(" /at ");
-                    desc = components[0].substring(6);
-                    newTask = new Event(desc, components[1]);
+                        log(output.toString());
+                    }
+                } else if (!input.isBlank()){
+                    Task newTask = null;
+                    String desc;
+
+                    if (input.startsWith("todo")) {
+                        if (input.length() == 4 || input.substring(5).isBlank()) {
+                            throw new DukeException("\tThe description of a todo cannot be empty.");
+                        }
+                        desc = input.substring(5).trim();
+                        newTask = new Todo(desc);
+                    } else if (input.startsWith("deadline")) {
+                        String[] components = input.split(" /by ");
+                        if (components[0].length() == 8 || components[0].substring(9).isBlank()) {
+                            throw new DukeException("\tThe description of a deadline cannot be empty.");
+                        }
+                        if (components.length == 1 || components[1].isBlank()) {
+                            throw new DukeException("\tThe timing of a deadline cannot be empty.");
+                        }
+                        desc = components[0].substring(9).trim();
+                        newTask = new Deadline(desc, components[1].trim());
+                    } else if (input.startsWith("event")) {
+                        String[] components = input.split(" /at ");
+                        if (components[0].length() == 5 || components[0].substring(6).isBlank()) {
+                            throw new DukeException("\tThe description of an event cannot be empty.");
+                        }
+                        if (components.length == 1 || components[1].isBlank()) {
+                            throw new DukeException("\tThe timing of an event cannot be empty.");
+                        }
+                        desc = components[0].substring(6).trim();
+                        newTask = new Event(desc, components[1].trim());
+                    } else {
+                        throw new DukeException("\tI don't know what that means :-(");
+                    }
+
+                    todos.add(newTask);
+
+                    String output = "     Got it. I've added this task:\n" +
+                            "       " + newTask + "\n" +
+                            "     Now you have " + todos.size() + " tasks in the list.\n" ;
+
+                    log(output);
                 }
-
-                todos.add(newTask);
-
-                String output = startLine +
-                        "     Got it. I've added this task:\n" +
-                        "       " + newTask + "\n" +
-                        "     Now you have " + todos.size() + " tasks in the list.\n" +
-                        endLine;
-
-                System.out.println(output);
+            } catch (DukeException e) {
+                log(e.getMessage() + "\n");
             }
         }
 
-        String byeMessage = startLine +
-                "     Bye. Hope to see you again soon!\n" +
-                endLine;
-        System.out.println(byeMessage);
+        String byeMessage = "     Bye. Hope to see you again soon!\n";
+        log(byeMessage);
     }
 }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class DukeException extends Exception {
+
+    public DukeException(String message) {
+        super(message);
+    }
+}
