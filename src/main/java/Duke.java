33621819182194b8bc25fce959e11040276aea30
hansebastian <sebastian_hans@hotmail.com>
2020-08-15 12:46:08 +0800

    Added todo item functionality as a subclass of task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -31,34 +31,80 @@ public class Duke {
                     listAllItems();
                     break;
                 default:
-                    if (userInput.substring(0, 4).equals("done")) { // mark as done case, to be able to use regex
+                    if (userMarkAsDone(userInput)) {
                         String taskIndex = userInput.substring(5);
                         markTaskAsDone(taskIndex);
+
+                    } else if (userAddingToDo(userInput)) {
+                        addToDoTask(userInput);
+
+                    } else if (userAddingEvent(userInput)) {
+                        addEventTask(userInput);
+
+                    } else if (userAddingDeadline(userInput)) {
+                        addDeadlineTask(userInput);
+
                     } else {
-                        addItem(userInput);
+                        System.out.println("I don't know what you want me to do");
                     }
                     break;
             }
         }
     }
+    private static void addToDoTask(String userInput) {
+        String taskDescription = userInput.substring(5);
+        ToDo newToDoItem = new ToDo(taskDescription);
+        addItem(newToDoItem); // Add to taskList
+    }
 
-    public static void markTaskAsDone(String taskIndex) {
+    private static void addEventTask(String userInput) {
+
+    }
+
+    private static void addDeadlineTask(String userInput) {
+
+    }
+
+    private static boolean userAddingToDo(String userInput) {
+        return userInput.substring(0, 4).equals("todo");
+    }
+
+    private static boolean userAddingEvent(String userInput) {
+        return userInput.substring(0, 5).equals("event");
+    }
+
+
+    private static boolean userAddingDeadline(String userInput) {
+        return userInput.substring(0, 8).equals("deadline");
+    }
+
+    private static boolean userMarkAsDone(String userInput) {
+        return userInput.substring(0, 4).equals("done");
+    }
+
+    // Updates taskList attribute to indicate task as done
+    public static void markTaskAsDone(String userInput) {
+        String taskIndex = userInput.substring(5);
         int index = Integer.valueOf(taskIndex) - 1; // taskIndex started from 1
         Task completedTask = Duke.taskList.get(index);
         completedTask.markAsDone();
 
         System.out.println("Nice! I've marked this task as done: \n" +
-        completedTask.toString());
+                completedTask.toString());
 
     }
 
-    public static void addItem(String userInput) {
-        Task newTask = new Task(userInput);
+    // Adds a task item to Duke's taskList, which may be an Event, ToDoItem, Deadline
+    public static void addItem(Task newTask) {
         Duke.taskList.add(newTask);
-        System.out.println("added: " + userInput);
+        System.out.println("Got it. I've added this task:\n   " +
+                newTask.toString() + "\nNow you have " + (Duke.taskList.size())
+                + (Duke.taskList.size() > 1 ? " tasks" : " task")
+                + " in the list.");
     }
 
     private static void listAllItems() {
+        System.out.println("Here are the tasks in your list:\n");
         ArrayList<Task> currList = Duke.taskList;
         currList.forEach(item ->
                 System.out.println((currList.indexOf(item) + 1) + ". " + item));
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,6 +11,10 @@ public class Task {
         return (isDone ? "[\u2713]" : "[\u2718]"); //return tick or X symbols
     }
 
+    public String getDescription() {
+        return this.description;
+    }
+
     public void markAsDone() {
         this.isDone = true;
     }
