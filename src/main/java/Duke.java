b0a910c9ec62fa45df16a04c0eb6164b195eec7d
kelvinvin <kelvinw0ng@hotmail.com>
2020-08-16 01:49:16 +0800

    added the ability for batman to mask tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
-import java.lang.reflect.Array;
+package main.java;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -6,7 +7,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         Boolean botRunning = true;
-        ArrayList<String> taskList = new ArrayList<String>();
+        ArrayList<Task> taskList = new ArrayList<Task>();
         String logo =
                 "___.           __                         \n"
                         + "\\_ |__ _____ _/  |_  _____ _____    ____  \n"
@@ -21,12 +22,22 @@ public class Duke {
             if (input.equals("bye")) {
                 botRunning = false;
             } else if (input.equals("list")) {
-                for (String task : taskList) {
-                    System.out.println(task);
+                int n = 0;
+                System.out.println("Here are the tasks in your list: ");
+                for (Task task : taskList) {
+                    n++;
+                    System.out.println(n + "." + task);
                 }
+            } else if (input.contains("done")) {
+                int taskNumberToMark = Integer.parseInt(input.substring(input.length()-1));
+                Task taskToMark = taskList.get(taskNumberToMark - 1);
+                taskToMark.markAsDone();
+                System.out.println("Nicely done. I've marked this task as done: \n" +
+                        taskToMark);
             } else {
                 int listSize = taskList.size() + 1;
-                taskList.add(listSize + ". " + input);
+                Task newTask = new Task(input);
+                taskList.add(newTask);
                 System.out.println("Task added: " + input);
             }
         }
