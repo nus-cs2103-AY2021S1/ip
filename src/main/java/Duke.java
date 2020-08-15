58d289689085711c5d9645a98acb7cc939aa1f23
pockii <yeokexin2@gmail.com>
2020-08-16 03:54:33 +0800

    Added add feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
+    static List<String> taskList = new ArrayList<>();
+
     static String wrapMessage(String message) {
         String line = "----------------------------------------------";
-        return line + "\n" + message + "\n" + line;
+        return line + "\n " + message + "\n" + line;
     }
 
     static void greet() {
-        String greeting = " Hello! I'm Duke\n"
-                        + " What can I do for you?";
+        String greeting = "Hello! I'm Duke\n What can I do for you?";
         System.out.println(wrapMessage(greeting));
+    }
+
+    static void exit() {
+        String byeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(wrapMessage(byeMessage));
+    }
+
+    static void addTask(String task) {
+        taskList.add(task);
+        System.out.println(wrapMessage("added: " + task));
     }
 
     static void readInput() {
@@ -18,11 +31,10 @@ public class Duke {
             String input = sc.nextLine();
 
             if (input.equals("bye")) {
-                String byeMessage = " Bye. Hope to see you again soon!";
-                System.out.println(wrapMessage(byeMessage));
+                exit();
                 break;
             } else {
-                System.out.println(wrapMessage(" " + input));
+                addTask(input);
             }
         }
     }
