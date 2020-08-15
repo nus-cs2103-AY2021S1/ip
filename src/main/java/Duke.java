b732cf8959ade5fa1e76b651f3dbf875ade336ca
cwenling <e0425906@u.nus.edu>
2020-08-15 15:25:47 +0800

    Completed Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,41 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> tasks = new ArrayList<>();
+
+    private static void greet() {
+        String greet = "\t____________________________________________________________\n"
+                + "\tHello! I'm Duke\n"
+                + "\tWhat can I do for you?\n"
+                + "\t____________________________________________________________\n";
+        System.out.println(greet);
+    }
+
+    private static void add(String task) {
+        String echoizer = "\t____________________________________________________________\n"
+                + "\tadded: %s\n"
+                + "\t____________________________________________________________\n";
+        tasks.add(task);
+        System.out.printf((echoizer) + "%n", task);
+    }
+
+    private static void list() {
+        System.out.print("\t____________________________________________________________\n");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("\t%d. %s%n", i + 1, tasks.get(i));
+        }
+        System.out.println("\t____________________________________________________________\n");
+    }
+
+    private static void exit() {
+        String goodbye = "\t____________________________________________________________\n"
+                + "\tBye. Hope to see you again soon!\n"
+                + "\t____________________________________________________________\n";
+        System.out.print(goodbye);
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -8,24 +43,18 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from the other side of\n" + logo);
-        String greet = "\t____________________________________________________________\n"
-                + "\tHello! I'm Duke\n"
-                + "\tWhat can I do for you?\n"
-                + "\t____________________________________________________________\n";
-        System.out.println(greet);
+
+        greet();
         Scanner sc = new Scanner(System.in);
-        String input = "";
-        String echoizer = "\t____________________________________________________________\n"
-                + "\t%s\n"
-                + "\t____________________________________________________________\n";
-        String exit = "Bye. Hope to see you again soon!";
+        String input;
         while (sc.hasNext()) {
             input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println(String.format(echoizer, exit));
-                break;
+                exit();
+            } else if (input.equals("list")) {
+                list();
             } else {
-                System.out.println(String.format(echoizer, input));
+                add(input);
             }
         }
     }
