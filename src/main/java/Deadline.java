89a675f646ca45b3e06a5ba200f1764f5ae2dbac
Justin Tzuriel Krisnahadi <justin.tzuriel17@gmail.com>
2020-08-15 15:29:55 +0800

    "At" to "By"

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -9,6 +9,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (at: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + this.by + ")";
     }
 }
