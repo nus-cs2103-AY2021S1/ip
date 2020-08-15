815e595c52ed12e2442cc45f3c41521ea45cb63f
Wincenttjoi <wincenttsjoi@gmail.com>
2020-08-15 13:25:15 +0800

    Add functionality of add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        List<String> storage = new ArrayList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -12,12 +15,20 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String toEcho = sc.nextLine();
         while (!toEcho.equals("bye")) {
-            System.out.println("--------------------------------------\n" +
-                toEcho + "\n" + "--------------------------------------");
+            System.out.println("--------------------------------------");
+            if (toEcho.equals("list")) {
+                for (int i = 0; i < storage.size(); i++) {
+                    System.out.println(i + ". " + storage.get(i));
+                }
+            } else {
+                System.out.println(toEcho);
+                storage.add(toEcho);
+            }
+            System.out.println("--------------------------------------");
             toEcho = sc.nextLine();
         }
         System.out.println("--------------------------------------\n" +
-            "Bye Boss! Hope to see you again!" + "\n" + "--------------------------------------");
-
+            "Bye Boss! Hope to see you again!" + "\n"
+            + "--------------------------------------");
     }
 }
