38b7d924ccb62c68baa7f362eb0917b64779bb13
Wincenttjoi <wincenttsjoi@gmail.com>
2020-08-15 12:53:08 +0800

    Add bot functionality to greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,17 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm\n" + logo + "How can I help you, Boss?");
+
+        Scanner sc = new Scanner(System.in);
+        String toEcho = sc.nextLine();
+        while (!toEcho.equals("bye")) {
+            System.out.println("--------------------------------------\n" +
+                toEcho + "\n" + "--------------------------------------");
+            toEcho = sc.nextLine();
+        }
+        System.out.println("--------------------------------------\n" +
+            "Bye Boss! Hope to see you again!" + "\n" + "--------------------------------------");
+
     }
 }
