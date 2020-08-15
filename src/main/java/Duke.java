f8e0d9e284b2f8d631471b61756ec03f15045a28
Justin Tzuriel Krisnahadi <justin.tzuriel17@gmail.com>
2020-08-15 14:13:27 +0800

    Mark as Done, changed String to Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,7 +12,7 @@ public class Duke {
                 line + "\n";
         System.out.println(intro);
         Scanner sc = new Scanner(System.in);
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
         while(true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
@@ -23,13 +23,21 @@ public class Duke {
             } else if (input.equals("list")){
                 int id = 1;
                 System.out.println(line);
-                for(String task : tasks) {
+                System.out.println(tab + "Here are the tasks in your list:");
+                for(Task task : tasks) {
                     System.out.println(tab + id + ". " + task);
                     id++;
                 }
                 System.out.println(line + "\n");
-            } else {
-                tasks.add(input);
+            } else if (input.split(" ")[0].equals("done")) {
+                System.out.println(tab + "Nice! I've marked this task as done:");
+                int idx = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task doneTask = tasks.get(idx).done();
+                tasks.set(idx, doneTask);
+                System.out.println(tab + "   " + doneTask);
+            }
+            else {
+                tasks.add(new Task(input));
                 System.out.println(line);
                 System.out.println(tab + "added: " + input);
                 System.out.println(line + "\n");
