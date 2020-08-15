22b31b01e0a426c1292178f07a16e4e40a4bf9a2
qlchan24 <qlchan24@gmail.com>
2020-08-15 21:58:48 +0800

    Implement skeletal version of Duke
    The chatbot has a proper layout and can greet the user, echo a command,
    and exit upon command.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    // General style properties of the chatbot.
+    private static final String LEFT_MARGIN = "    ";
+    private static final String LOGO =
+            "\n" + LEFT_MARGIN + "███╗   ███╗     █████╗     ██████╗     ██╗     ██████╗ \n"
+                    + LEFT_MARGIN + "████╗ ████║    ██╔══██╗    ██╔══██╗    ██║    ██╔═══██╗\n"
+                    + LEFT_MARGIN + "██╔████╔██║    ███████║    ██████╔╝    ██║    ██║   ██║\n"
+                    + LEFT_MARGIN + "██║╚██╔╝██║    ██╔══██║    ██╔══██╗    ██║    ██║   ██║\n"
+                    + LEFT_MARGIN + "██║ ╚═╝ ██║    ██║  ██║    ██║  ██║    ██║    ╚██████╔╝\n"
+                    + LEFT_MARGIN + "╚═╝     ╚═╝    ╚═╝  ╚═╝    ╚═╝  ╚═╝    ╚═╝     ╚═════╝ \n";
+    private static final String WELCOME_MESSAGE = "It's a-me, Mario! how can I help you today?\n";
+    private static final String EXIT_MESSAGE = "Hey! Come back here! You big-a monkey!\n";
+    private static final String DIVIDER =
+            LEFT_MARGIN + "_______________________________________________________\n";
+
+
+    // processes the input and generates the output. In this implementation, it just repeats the input in another
+    // format.
+    public static String commandOutput(String input) {
+        return DIVIDER + LEFT_MARGIN + input + "\n" + DIVIDER;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(DIVIDER + LOGO + commandOutput(WELCOME_MESSAGE));
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.print(commandOutput(EXIT_MESSAGE));
+                break;
+            } else {
+                System.out.print(commandOutput(input));
+            }
+        }
     }
 }
