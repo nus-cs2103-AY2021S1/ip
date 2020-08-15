77ffdacf4d0d7c515d72036f15c6e52b24105530
MatthiasLHK <matthiasli1998@hotmail.com>
2020-08-15 15:08:25 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
+//                + "|____/ \\__,_|_|\\_\\___|\n"; 
 //        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
