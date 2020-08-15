b4f95d9bceaad63923abe42f53b91f9a14d42c49
LeeMingDe <leemingde@gmail.com>
2020-08-15 22:15:52 +0800

    Added start() method

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,9 +1,34 @@
 package main.java;
 
+
+import java.util.Scanner;
+
 public class Commands {
+    private boolean shouldBreak = true;
+
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        String inputs = scanner.nextLine().toLowerCase();
+
+        while (shouldBreak) {
+            switch (inputs) {
+            case "list":
+                this.lst();
+            case "bye":
+                shouldBreak = !shouldBreak;
+            }
+            inputs = scanner.nextLine().toLowerCase();
+        }
+
+    }
 
     public static void greet() {
-        System.out.println("~ \n Hello I'm Trump \n What can I do for you?");
+        System.out.println("~ \n Hello I'm the Terminator \n What can I do for you? \n~");
     }
-    
+
+    public void lst() {
+        System.out.println("~ \n list \n~");
+    }
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+package main.java;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Commands.greet();
+        Commands commands = new Commands();
+        commands.start();
+
     }
 }
