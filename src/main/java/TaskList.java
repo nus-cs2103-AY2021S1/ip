fb5bd05f2b58cca67c466dacdee44b40a3fccb8b
Criss-Wang <djj87018507@gmail.com>
2020-08-15 15:15:50 +0800

    Enable update of task status and extraction of task by id in TaskList
    We need to the get task from the TaskList object and update it in the
    list.
    
    In light of this, let's:
    * Add a getTaskById() method to extract the task via its id
    * Add a markAsDone() method to update the task status in the list
    * Add a generateFeedbackById() method to send a feedback to inform the
    user that task has been done

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -10,11 +10,22 @@ public class TaskList {
     private final List<Task> tasks;
 
     public TaskList() {
-        tasks = new ArrayList<>();
+        this.tasks = new ArrayList<>();
     }
 
     public TaskList(List<Task> tasks) {
         this.tasks = tasks;
+    }
+
+    /**
+     * Get the task in the list by its id.
+     *
+     * @param task_id the displayed id in the list.
+     * @return the task associated with id.
+     */
+    public Task getTaskById(int task_id) {
+        int index = task_id - 1;
+        return this.tasks.get(index);
     }
 
     /**
@@ -25,21 +36,48 @@ public class TaskList {
      * the new task.
      */
     public TaskList addTask(Task newTask) {
-        List<Task> newTaskList = tasks;
+        List<Task> newTaskList = this.tasks;
         newTaskList.add(newTask);
         return new TaskList(newTaskList);
     }
 
     /**
+     * Set a task with input id in the list as done.
+     *
+     * @param task_id the displayed id in the list.
+     * @return a new TaskList containing all the update tasks.
+     */
+    public TaskList markAsDone(int task_id) {
+        Task currentTask = this.getTaskById(task_id);
+        Task doneTask = currentTask.complete();
+
+        int index = task_id - 1;
+        List<Task> newTaskList = this.tasks;
+        newTaskList.set(index, doneTask);
+        return new TaskList(newTaskList);
+    }
+
+    /**
+     * Generate the feedback for an update of task status.
+     *
+     * @param task_id the displayed id in the list.
+     * @return a String suggesting the completion of task update.
+     */
+    public String generateFeedbackById(int task_id) {
+        Task currentTask = this.getTaskById(task_id);
+        return "Nice! I've marked this task as done:\n" + currentTask;
+    }
+
+    /**
      * Print all the tasks.
      *
-     * @return a String shows all takes in a formatted way.
+     * @return a String shows all tasks in a formatted way.
      */
     public String printTasks() {
-        String result = "";
+        String result = "Here are the tasks in your list:";
 
         result += IntStream.range(0, tasks.size())
-                .mapToObj((id) -> String.format("%d.%s\n", id + 1, tasks.get(id)))
+                .mapToObj((id) -> String.format("\n%d.%s", id + 1, tasks.get(id)))
                 .reduce((a, b) -> a + b)
                 .get();
 
