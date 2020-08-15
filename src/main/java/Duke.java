350b0cdaa9f848da68796960122acd2604de06de
Aloysius Lim <aloysiuslim.dw@gmail.com>
2020-08-15 19:11:49 +0800

    add level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String str;
+        str = sc.nextLine();
+        while (!str.equals("bye")){
+            System.out.println("-----------------");
+            System.out.println(str);
+            System.out.println("-----------------");
+            str = sc.nextLine();
+        }
+        System.out.println("-----------------");
+        System.out.println("Bye. Hope to see you again!");
+        System.out.println("-----------------");
     }
 }
