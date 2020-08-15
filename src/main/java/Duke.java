ec80aaa434adc6a0eefb0f545a8194d23e742749
junlong4321 <desmondwongjunlong@yahoo.com.sg>
2020-08-15 16:13:52 +0800

    Duke Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Duke {
-    static List<String> tasks = new ArrayList<>();
+    static List<Task> tasks = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void greet() {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________\n");
-        
+    }
+
+    public static void farewell() {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        greet();
+
         Scanner sc = new Scanner(System.in);
         String userCommand = sc.nextLine();
 
@@ -18,21 +29,48 @@ public class Duke {
             if (userCommand.equals("list")) {
                 System.out.println("\t____________________________________________________________");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("\t" + (i + 1) + ". " + tasks.get(i));
+                    int serialNumber = i + 1;
+                    Task task = tasks.get(i);
+                    System.out.println("\t" + serialNumber + "." + task);
                 }
                 System.out.println("\t____________________________________________________________\n");
                 userCommand = sc.nextLine();
+            }  else if (userCommand.contains("done")) {
+                try {
+                    // E.g given "done 1", we split to ["done", "1"]
+                    String[] userCommandSplit = userCommand.split(" ");
+                    // To prevent cases such as "done 1 7"
+                    if (userCommandSplit.length != 2) {
+                        System.out.println("\t____________________________________________________________");
+                        System.out.println("Oops! Something went wrong!");
+                        System.out.println("\t____________________________________________________________\n");
+                        userCommand = sc.nextLine();
+                    } else {
+                        // Take serial number e.g 1 "done 1"
+                        int serialNumber = Integer.parseInt(userCommandSplit[1]);
+                        Task doneTask = tasks.get(serialNumber - 1);
+                        doneTask.markAsDone();
+                        System.out.println("\t____________________________________________________________");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("\t" + doneTask);
+                        System.out.println("\t____________________________________________________________\n");
+                        userCommand = sc.nextLine();
+                    }
+                } catch (Exception e) {
+                    System.out.println("\t____________________________________________________________");
+                    System.out.println("Oops! Something went wrong!");
+                    System.out.println("\t____________________________________________________________\n");
+                    userCommand = sc.nextLine();
+                }
             } else {
+                tasks.add(new Task(userCommand));
                 System.out.println("\t____________________________________________________________");
                 System.out.println("\t" + "added: " + userCommand);
                 System.out.println("\t____________________________________________________________\n");
-                tasks.add(userCommand);
                 userCommand = sc.nextLine();
             }
         }
 
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
+        farewell();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "Y" : "N"); // Return Y or N
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
