3e815db65dc8621d81226c67d30e8bcf55388e9f
Marcon2509 <marcus.duigan@gmail.com>
2020-08-15 20:35:39 +0800

    Level2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
 
@@ -8,6 +10,7 @@ public class Duke {
         System.out.println(introduction);
         System.out.println(line);
 
+        ArrayList<String> ListOfItems = new ArrayList<String>();
         boolean flag = false;
         String echo;
         Scanner sc = new Scanner(System.in);
@@ -20,11 +23,21 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 flag = true;
+            } else if (echo.equals("list")) {
+                System.out.println(line);
+            for (int i = 0; i< ListOfItems.size(); i++) {
+                int number = i+1;
+                System.out.println(Integer.toString(number) + ". " + ListOfItems.get(i));
+
+            }
+                System.out.println(line);
+                echo = sc.nextLine();
             } else {
 
+                ListOfItems.add(echo);
 
                 System.out.println(line);
-                System.out.println(echo);
+                System.out.println("added: " + echo);
                 System.out.println(line);
 
                 echo = sc.nextLine();
