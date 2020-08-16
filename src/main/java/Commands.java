87250908daaabde5f0e7372aff6844c2e9cdf56d
LeeMingDe <leemingde@gmail.com>
2020-08-16 23:53:12 +0800

    Level-5
    Added Duke class and Exceptions

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -9,7 +9,6 @@ public class Commands {
     private ArrayList<Task> taskList = new ArrayList<>();
 
     private final String INPUT_LIST = "list";
-    private final String INPUT_BLAH = "blah";
     private final String INPUT_BYE = "bye";
     private final String INPUT_DONE = "done";
     private final String INPUT_TODO = "todo";
@@ -25,55 +24,55 @@ public class Commands {
 
         while (shouldBreak) {
             String[] inputArray = inputs.split(" ", 2);
-            switch (inputArray[0].toLowerCase()) {
-            case INPUT_LIST:
-                this.lst();
-                inputs = scanner.nextLine().trim();
-                break;
-            case INPUT_BLAH:
-                this.blah();
-                inputs = scanner.nextLine().trim();
-                break;
-            case INPUT_BYE:
-                System.out.println("~ \n I will be back \n~ ");
-                shouldBreak = !shouldBreak;
-                break;
-            case INPUT_DONE:
-                markDone(inputs.charAt(5));
-                inputs = scanner.nextLine().trim();
-                break;
-            case INPUT_TODO:
-                addTodo(inputArray[1]);
-                inputs = scanner.nextLine().trim();
-                break;
-            case INPUT_DEADLINES:
-                addDeadline(inputArray[1]);
-                inputs = scanner.nextLine().trim();
-                break;
-            case INPUT_EVENTS:
-                addEvent(inputArray[1]);
-                inputs = scanner.nextLine().trim();
-                break;
-            default:
-                taskList.add(new Task(inputs));
-                System.out.println("~ \n added: " + inputs + "\n~ ");
+            try {
+                switch (inputArray[0].toLowerCase()) {
+                case INPUT_LIST:
+                    this.lst();
+                    inputs = scanner.nextLine().trim();
+                    break;
+                case INPUT_BYE:
+                    System.out.println("~ \n I will be back \n~ ");
+                    shouldBreak = !shouldBreak;
+                    break;
+                case INPUT_DONE:
+                    markDone(inputArray);
+                    inputs = scanner.nextLine().trim();
+                    break;
+                case INPUT_TODO:
+                    addTodo(inputArray);
+                    inputs = scanner.nextLine().trim();
+                    break;
+                case INPUT_DEADLINES:
+                    addDeadline(inputArray);
+                    inputs = scanner.nextLine().trim();
+                    break;
+                case INPUT_EVENTS:
+                    addEvent(inputArray);
+                    inputs = scanner.nextLine().trim();
+                    break;
+                default:
+                    throw new DukeException("~\n ERROR... INPUT NOT RECOGNIZED. \n PLEASE TRY AGAIN \n~");
+                }
+            } catch (DukeException dukeException) {
+                System.out.println(dukeException.getMessage());
                 inputs = scanner.nextLine().trim();
             }
         }
     }
 
-    private void markDone(char input) {
-        if (Character.isDigit(input)) {
-            int taskNumber = Character.getNumericValue(input) - 1;
+    private void markDone(String[] inputs) throws DukeException {
+        if (inputs.length == 2 && Character.isDigit(inputs[1].charAt(0))) {
+            int taskNumber = Character.getNumericValue(inputs[1].charAt(0)) - 1;
             if (!taskList.isEmpty() && taskNumber < taskList.size()) {
                 taskList.get(taskNumber).doneTask();
                 System.out.println("~ \n Nice! Target Eliminated: \n   "
                         + taskList.get(taskNumber).toString() + "\n~");
             } else {
-                System.out.println("Please choose a task to mark as done, with \"done <task number>\"");
+                throw new DukeException("~\n ERROR... TASK NOT FOUND. \n PLEASE TRY AGAIN \n~");
             }
         } else {
-            System.out.println("Please input a valid integer");
+            throw new DukeException("~\n ERROR... NON-INTEGER RECOGNIZED OR TASK NUMBER NOT INPUTTED. \n " +
+                    "PLEASE TRY AGAIN \n~");
         }
     }
 
@@ -92,35 +91,43 @@ public class Commands {
         System.out.println("\n~ ");
     }
 
-    public void blah() {
-        System.out.println("~ \n blah \n~");
+    public void addTodo(String[] inputs) throws DukeException {
+        if (inputs.length > 1) {
+            System.out.println("~ \n Got it. I've added this task: ");
+            ToDos toDo = new ToDos(inputs[1]);
+            taskList.add(toDo);
+            System.out.println(String.format("   %s \n Now you have %d tasks in the list. \n~",
+                    toDo, taskList.size()));
+        } else {
+            throw new DukeException("~\n ERROR... TODO DESCRIPTION EMPTY. \n PLEASE TRY AGAIN \n~");
+        }
     }
 
-    public void addTodo(String input) {
-        System.out.println("~ \n Got it. I've added this task: ");
-        ToDos toDo = new ToDos(input);
-        taskList.add(toDo);
-        System.out.println(String.format("   %s \n Now you have %d tasks in the list. \n~",
-                toDo, taskList.size()));
+    public void addDeadline(String[] inputs) throws DukeException {
+        if (inputs.length > 1) {
+            String[] stringArray = inputs[1].split("/", 2);
+            String day = stringArray[1].split(" ", 2)[1];
+            System.out.println("~ \n Got it. I've added this task: ");
+            Deadlines deadline = new Deadlines(stringArray[0], day);
+            taskList.add(deadline);
+            System.out.println(String.format("   %s \n Now you have %d tasks in the list. \n~",
+                    deadline, taskList.size()));
+        } else {
+            throw new DukeException("~\n ERROR... DEADLINE DESCRIPTION OR DATE EMPTY. \n PLEASE TRY AGAIN \n~");
+        }
     }
 
-    public void addDeadline(String input) {
-        System.out.println("~ \n Got it. I've added this task: ");
-        String[] stringArray = input.split("/", 2);
-        String day = stringArray[1].split(" ", 2)[1];
-        Deadlines deadline = new Deadlines(stringArray[0], day);
-        taskList.add(deadline);
-        System.out.println(String.format("   %s \n Now you have %d tasks in the list. \n~",
-                deadline, taskList.size()));
-    }
-
-    public void addEvent(String input) {
-        System.out.println("~ \n Got it. I've added this task: ");
-        String[] stringArray = input.split("/", 2);
-        String at = stringArray[1].split(" ", 2)[1];
-        Events event = new Events(stringArray[0], at);
-        taskList.add(event);
-        System.out.println(String.format("   %s \n Now you have %d tasks in the list. \n~",
-                event, taskList.size()));
+    public void addEvent(String[] inputs) throws DukeException {
+        if (inputs.length > 1) {
+            String[] stringArray = inputs[1].split("/", 2);
+            String at = stringArray[1].split(" ", 2)[1];
+            System.out.println("~ \n Got it. I've added this task: ");
+            Events event = new Events(stringArray[0], at);
+            taskList.add(event);
+            System.out.println(String.format("   %s \n Now you have %d tasks in the list. \n~",
+                    event, taskList.size()));
+        } else {
+            throw new DukeException("~\n ERROR... EVENT DESCRIPTION OR DATE EMPTY. \n PLEASE TRY AGAIN \n~");
+        }
     }
 }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class DukeException extends Exception {
+
+    public DukeException(String message) {
+        super(message);
+    }
+
+}
