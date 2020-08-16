433c39120239f597275d63f7e45c9c26263e2eae
Wincenttjoi <wincenttsjoi@gmail.com>
2020-08-16 14:46:23 +0800

    Change test into relative path

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,15 +1,13 @@
 @ECHO OFF
 
-SET MY_PATH=C:\Users\Wincent\Desktop\cs2103t\ip
-
 REM create bin directory if it doesn't exist
-if not exist "%MY_PATH%\bin" mkdir "%MY_PATH%\bin"
+if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp "%MY_PATH%\src\main\java" -Xlint:none -d "%MY_PATH%\bin" "%MY_PATH%\src\main\java\Duke.java"
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\Duke.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -17,7 +15,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath "%MY_PATH%\bin" Duke < input.txt > ACTUAL.TXT
+java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
