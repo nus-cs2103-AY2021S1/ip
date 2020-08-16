871d2c533b19a1e65e301e156069a26485582754
constancensq <constancensq@gmail.com>
2020-08-16 20:15:50 +0800

    Store text entered by user and display in chronological order when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,19 +2,44 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static void echoInput() {
+    static String[] tasks = new String[100];
+    static int numTasks = 0;
+
+    // loop through tasks array and return all tasks as a string
+    public static String printTasks() {
+        String tasksList = "";
+        for (int i = 0; i < numTasks; i++) {
+            if (i == 0) {
+                tasksList = "1. " + tasks[i];
+            } else {
+                tasksList = tasksList + "\n " + (i + 1) + ". " + tasks[i];
+            }
+        }
+
+        return tasksList;
+    }
+
+    public static void getInput() {
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNext()) {
-            String inputData = sc.next();
-            if (!inputData.equals("bye")) {
+        while (sc.hasNextLine()) {
+            String inputData = sc.nextLine();
+
+            if (inputData.equals("list")) {
                 System.out.println(" ____________________________________________________________\n " +
-                        inputData +
+                        printTasks() +
+                        "\n ____________________________________________________________");
+            } else if (!inputData.equals("bye")) {
+                // add inputData to next index in tasks array and print inputData
+                tasks[numTasks] = inputData;
+                numTasks++;
+                System.out.println(" ____________________________________________________________\n " +
+                        "added: " + inputData +
                         "\n ____________________________________________________________");
             } else {
                 String ending = " ____________________________________________________________\n " +
                         " Goodbye! See you again!\n" +
-                        " ____________________________________________________________\n";
+                        " ____________________________________________________________";
 
                 System.out.println(ending);
 
@@ -37,7 +62,7 @@ public class Duke {
                 " What can I do for you today?\n" +
                 " ____________________________________________________________");
 
-        echoInput();
+        getInput();
 
     }
 }
