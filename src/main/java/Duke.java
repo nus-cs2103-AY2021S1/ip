2671f47244d5dfe542f16b7f3466758f3184f84a
Jay <Nopenotj@gmail.com>
2020-08-16 15:21:57 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+import java.util.concurrent.LinkedTransferQueue;
+
 public class Duke {
+    static String divider = "    ____________________________________________________________\n";
+    static void start() {
+        String logo =
+                                " .----------------.  .----------------.  .----------------.  .----------------. \n" +
+                                        "| .--------------. || .--------------. || .--------------. || .--------------. |\n" +
+                                        "| |  ________    | || |     ____     | || |     ____     | || |  ___  ____   | |\n" +
+                                        "| | |_   ___ `.  | || |   .'    `.   | || |   .'    `.   | || | |_  ||_  _|  | |\n" +
+                                        "| |   | |   `. \\ | || |  /  .--.  \\  | || |  /  .--.  \\  | || |   | |_/ /    | |\n" +
+                                        "| |   | |    | | | || |  | |    | |  | || |  | |    | |  | || |   |  __'.    | |\n" +
+                                        "| |  _| |___.' / | || |  \\  `--'  /  | || |  \\  `--'  /  | || |  _| |  \\ \\_  | |\n" +
+                                        "| | |________.'  | || |   `.____.'   | || |   `.____.'   | || | |____||____| | |\n" +
+                                        "| |              | || |              | || |              | || |              | |\n" +
+                                        "| '--------------' || '--------------' || '--------------' || '--------------' |\n" +
+                                        " '----------------'  '----------------'  '----------------'  '----------------' \n";
+        String intro =
+                "\n" +
+                "\n" +
+                logo +
+                "\n" +
+                divider +
+                "     Hola! I am dook\n" +
+                "     how i can help u?\n" + divider;
+        System.out.println(intro);
+    }
+    static void print(String str){
+        String intro = divider + "     " + str + "\n" + divider;
+        System.out.println(intro);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        start();
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            print(input);
+            input = sc.nextLine();
+        }
+        print("see u later alligator");
     }
 }
