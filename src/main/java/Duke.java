b0976844284665adcd4075d67afbadd6a4be5e62
CodingCookieRookie <e0415767@u.nus.edu>
2020-08-16 21:25:59 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import   java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hi handsome! My name is Duck. What can I do for you?");
+        while (sc.hasNext()) {
+            String echo = sc.next();
+            if (echo.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(echo);
+            }
+        }
+        sc.close();
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
     }
 }
