1257519f9f40ed1b1e1211f891c4d6b7d13ac615
florenciamartina <e0407741@u.nus.edu>
2020-08-16 17:57:36 +0800

    Level-3 added Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public Task(String description, Boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+}
