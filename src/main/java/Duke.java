fb5529873cadcc489df706b03508f414c7fb7fef
luo-git <lluoyi.lu@gmail.com>
2020-08-16 16:51:31 +0800

    Implement Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,48 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    /**
+     * Print greeting message.
+     */
+    public void greet() {
+        System.out.println(TextFormatter.LOGO);
+        System.out.println(TextFormatter.getFormattedText("Hi I'm Cat Bot.\nWhat can I do for you?"));
+    }
+
+    /**
+     * Print goodbye message.
+     */
+    public void bye() {
+        System.out.println(TextFormatter.getFormattedText("Bye! Hope to see you again!"));
+    }
+
+    /**
+     * Print echoed command.
+     *
+     * @param command User command.
+     */
+    public void echo(String command) {
+        System.out.println(TextFormatter.getFormattedText(command));
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke bot = new Duke();
+        Scanner sc = new Scanner(System.in);
+
+        // Greet user
+        bot.greet();
+
+        // Echo command until user types bye
+        while (sc.hasNext()) {
+            String s = sc.next();
+            if (s.equals("bye")) {
+                break;
+            }
+            bot.echo(s);
+        }
+
+        // Exit bot
+        bot.bye();
     }
 }
