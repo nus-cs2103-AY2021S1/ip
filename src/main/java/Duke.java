3285e562f8131710b54589df9b24aa3e14b004ce
Galvin <galvinleow@gmail.com>
2020-08-16 11:03:11 +0800

    Level 2 - Add, List
    Edited code to expand scalability:
    1. Switch case is used instead of if-else

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,15 +9,28 @@ public class Duke {
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
-    System.out.println("Hello ! I'm Duke\nWhat can I do for you?\n" + logo);
+    System.out.println(logo + "\nHello ! I'm Duke\nWhat can I do for you?\n");
 
-    while (true) {
-      Scanner scanner = new Scanner(System.in);
-      String input = scanner.nextLine();
-      if (input.equals("bye")) {
-        break;
-      } else {
-        System.out.println(input);
+    // Arraylist used to ensure scalability
+    ArrayList<String> dataArrayList = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    String input = "";
+    while (!input.equals("bye")) {
+      input = scanner.nextLine();
+      // Switch case use to prepare for future requirements
+      switch (input) {
+        case "list":
+          for (int i = 0; i < dataArrayList.size(); i++) {
+            String listing = String.format("%d. %s", i + 1, dataArrayList.get(i));
+            System.out.println(listing);
+          }
+          break;
+        default:
+          if (!input.equals("bye")) {
+            dataArrayList.add(input);
+            System.out.println("added: " + input);
+          }
+          break;
       }
     }
     System.out.println("Bye. Hope to see you again soon!");
