298ff80565cd86483cacb7e6f9a7062a62cd0035
maguireong <maguire2622@yahoo.com.sg>
2020-08-16 17:49:12 +0800

    "Add Greet, Echo, Exit functionality"
    This reverts commit 462f975a

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+
+            String echo = sc.nextLine();
+            String[] s = echo.split("\\s");
+            String first = s[0];
+
+            System.out.println(first);
+            if (first.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
     }
 }
