7e5ab43c88593662cd0bcbbad4ed31d08f5793e7
richardcom <leonardlym61@gmail.com>
2020-08-16 12:07:42 +0800

    Add mark as done for level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,45 +5,60 @@ import java.util.Scanner;
 public class Duke {
     public static String greetings = "Hello! I'm Duke\n     What can I do for you?";
     public static String farewell = "Bye. Hope to see you again soon!";
+    public static String doneAlert = "Nice! I've marked this task as done: ";
 
 
     public static void main(String[] args) {
-        List<String> result = new ArrayList<>();
+        List<Task> result = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        printAnswer(greetings);
+        printAnswer("", greetings);
         while (sc.hasNextLine()) {
             String instruction = sc.nextLine();
 
             if (instruction.equals("bye")) {
-                printAnswer(farewell);
+                printAnswer("", farewell);
                 break;
+            } else if (instruction.length() > 4 && instruction.substring(0,4).equals("done")) {
+                Integer index = Integer.valueOf(instruction.substring(5)) - 1;
+                Task temp = result.get(index);
+                temp.markAsDone();
+                result.set(index, temp);
+                printAnswer(doneAlert, "   " + temp.toString());
             } else if (instruction.equals("list")) {
-                String resultString = "";
-                for (int i = 0; i < result.size(); i++) {
-                    if (i < result.size() -1) {
-                        resultString = resultString + (i + 1) + ". " + result.get(i) + "\n     ";
-                    } else {
-                        resultString = resultString + (i + 1) + ". " + result.get(i);
-                    }
-                }
-                printAnswer(resultString);
-
+                printList(result);
             } else {
-                result.add(instruction);
-                printAnswer("added: " + instruction);
+                result.add(new Task(instruction));
+                printAnswer("", "added: " + instruction);
             }
         }
 
 
     }
 
-    public static void printAnswer(String answer) {
+    public static void printAnswer(String guidance, String answer) {
         String Line = "____________________________________________________________";
         String smallSpace = "    ";
         String bigSpace = "     ";
 
         System.out.println(smallSpace + Line);
+        if (guidance.length() != 0) {
+            System.out.println(bigSpace + guidance);
+        }
         System.out.println(bigSpace + answer);
+        System.out.println(smallSpace + Line + "\n");
+    }
+
+    public static void printList(List<Task> result) {
+        String Line = "____________________________________________________________";
+        String smallSpace = "    ";
+        String bigSpace = "     ";
+        String reminder = "Here are the tasks in your list:";
+
+        System.out.println(smallSpace + Line);
+        System.out.println(bigSpace + reminder);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(bigSpace + (i + 1) + ". " + result.get(i));
+        }
         System.out.println(smallSpace + Line + "\n");
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
+}
