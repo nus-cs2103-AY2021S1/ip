fdaed0cd4cc441dac292a827fb4597cc51fd0d83
Timothy Ong <timothyjkong@gmail.com>
2020-08-16 23:54:07 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        response();
+    }
+
+    public static void response() {
+        Scanner sc = new Scanner(System.in);
+        String next = sc.nextLine();
+        if (next.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(next);
+            response();
+        }
     }
 }
