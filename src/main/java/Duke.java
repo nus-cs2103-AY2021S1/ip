843a34d19c3b94517bcef8984909325a48a526ba
JunCheng98 <junchengillusian@gmail.com>
2020-08-16 17:23:47 +0800

    Level-2 Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,11 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // attributes for word storage
+        String[] lib = new String[100];
+        int curr = 0;
+
         String logo = "   ____        _        \n"
                 + "  |  _ \\ _   _| | _____ \n"
                 + "  | | | | | | | |/ / _ \\\n"
@@ -14,12 +19,26 @@ public class Duke {
 
         while (sc.hasNext()) {
             String echo = sc.next();
+
             if (echo.equals("bye")) {
                 System.out.println("---------------\n" + "Hope to see you again!\n"
                         + "---------------\n");
                 break;
             }
-            System.out.println("---------------\n" + echo + "\n" + "---------------\n");
+
+            if (echo.equals("list")) {
+                System.out.println("---------------\n");
+                for (int i = 1; i <= curr; i++) {
+                    System.out.println(i + ". " + lib[i - 1] +"\n");
+                }
+                System.out.println("---------------\n");
+                continue;
+            }
+
+            System.out.println("---------------\n" + "Storing item " + "(" + curr + "/100)"+
+                    " : " + echo + "\n" + "---------------\n");
+            lib[curr] = echo;
+            curr++;
         }
 
         sc.close();
