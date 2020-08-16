87279939b6a4723bc622c2864906778accc2c47c
lamlaaaam <lamzhiyuan98@gmail.com>
2020-08-16 11:15:18 +0800

    Completed Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,11 +13,15 @@ public class Duke {
         greet();
         while (true) {
             String response = DukeIn.prompt();
-            handleResponse(response);
+            try {
+                handleResponse(response);
+            } catch (DukeException ex) {
+                DukeOut.print(ex.toString());
+            }
         }
     }
 
-    private static void handleResponse(String response) {
+    private static void handleResponse(String response) throws DukeException {
         String[] parsedResponse = response.split(" ", 2);
         String command = parsedResponse[0];
         String rest = parsedResponse.length == 1 ? null : parsedResponse[1];
@@ -25,35 +29,87 @@ public class Duke {
             case "bye":
                 exit();
             case "list":
-                showList();
+                handleList();
                 break;
             case "done":
-                int taskDone = Integer.parseInt(rest) - 1;
-                if (taskDone < 0 || taskDone >= taskIndex) {
-                    DukeOut.print("No such task!");
-                    break;
-                }
-                markTaskDone(taskDone);
-                DukeOut.print("Nice! I've marked this task as done:\n      " + taskList[taskDone]);
+                handleDone(rest);
                 break;
             case "todo":
-                addToList(new Todo(rest));
+                handleTodo(rest);
                 break;
             case "deadline":
-                String[] deadlineParsed = rest.split("/");
-                String deadlineName = deadlineParsed[0].trim();
-                String by = deadlineParsed[1].split(" ", 2)[1];
-                addToList(new Deadline(deadlineName, by));
+                handleDeadline(rest);
                 break;
             case "event":
-                String[] eventParsed = rest.split("/");
-                String eventName = eventParsed[0].trim();
-                String at = eventParsed[1].split(" ", 2)[1];
-                addToList(new Event(eventName, at));
+                handleEvent(rest);
                 break;
             default:
-                DukeOut.print("Unrecognized command!");
+                throw new DukeException("Unrecognized command!");
         }
+    }
+
+    private static void handleList() {
+        if (taskIndex == 0) {
+            DukeOut.print("You have no tasks!");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder("Here are the tasks in your list:\n    ");
+        for (int i = 0; i < taskIndex; ++i) {
+            sb.append((i + 1) + "." + taskList[i] + "\n    ");
+        }
+        sb.setLength(sb.length() - 5);
+        DukeOut.print(sb.toString());
+    }
+
+    private static void handleDone(String rest) throws DukeException {
+        int taskDone = Integer.parseInt(rest) - 1;
+        if (taskDone < -1 || taskDone >= taskIndex) {
+            throw new DukeException("No such task!");
+        }
+        markTaskDone(taskDone);
+        DukeOut.print("Nice! I've marked this task as done:\n      " + taskList[taskDone]);
+    }
+
+    private static void handleTodo(String rest) throws DukeException {
+        if (rest == null) {
+            throw new DukeException("Description of a todo cannot be empty!");
+        }
+        addToList(new Todo(rest));
+    }
+
+    private static void handleDeadline(String rest) throws DukeException {
+        if (rest == null) {
+            throw new DukeException("Description of a deadline cannot be empty!");
+        }
+        String[] deadlineParsed = rest.split("/");
+        if (deadlineParsed.length == 1) {
+            throw new DukeException("Prefix the keyword 'by' with a forward slash!");
+        }
+        String deadlineName = deadlineParsed[0].trim();
+        String[] byParsed = deadlineParsed[1].split(" ", 2);
+        if (byParsed.length == 1) {
+            throw new DukeException("Deadline due time cannot be empty!");
+        }
+        String by = byParsed[1];
+        addToList(new Deadline(deadlineName, by));
+    }
+
+    private static void handleEvent(String rest) throws DukeException {
+        if (rest == null) {
+            throw new DukeException("Description of an event cannot be empty!");
+        }
+        String[] eventParsed = rest.split("/");
+        if (eventParsed.length == 1) {
+            throw new DukeException("Prefix the keyword 'at' with a forward slash!");
+        }
+        String eventName = eventParsed[0].trim();
+        String[] atParsed = eventParsed[1].split(" ", 2);
+        if (atParsed.length == 1) {
+            throw new DukeException("Event time cannot be empty!");
+        }
+        String at = atParsed[1];
+        addToList(new Event(eventName, at));
     }
 
     private static void greet() {
@@ -71,20 +127,6 @@ public class Duke {
         taskList[taskIndex] = task;
         ++taskIndex;
         DukeOut.print("Got it. I've added this task:\n      " + task + "\n    " + "Now you have " + taskIndex + " task(s) in the list.");
-    }
-
-    private static void showList() {
-        if (taskIndex == 0) {
-            DukeOut.print("You have no tasks!");
-            return;
-        }
-
-        StringBuilder sb = new StringBuilder("Here are the tasks in your list:\n    ");
-        for (int i = 0; i < taskIndex; ++i) {
-            sb.append((i + 1) + "." + taskList[i] + "\n    ");
-        }
-        sb.setLength(sb.length() - 5);
-        DukeOut.print(sb.toString());
     }
 
     private static void markTaskDone(int taskNum) {
