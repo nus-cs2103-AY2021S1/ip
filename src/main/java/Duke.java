f23f65c708971e8b464ff99406faf6205f928cc4
Jaylenlee <e0424677@u.nus.edu>
2020-08-16 14:32:03 +0800

    add echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n";
+        String greeting = "    ____________________________________________________________\n"
+                + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n"
+                + "    ____________________________________________________________\n";
         System.out.println(greeting);
+
+        Scanner sc  = new Scanner(System.in);
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println("    ____________________________________________________________\n"
+                    + "     "
+                    + command
+                    + "\n"
+                    + "    ____________________________________________________________\n"
+            );
+
+            command = sc.nextLine();
+        }
+
+        System.out.println("    ____________________________________________________________\n"
+                + "     Bye. Hope to see you again soon!\n"
+                + "    ____________________________________________________________\n"
+        );
+
+        sc.close();
     }
 }
