74234f12326b3be56251f17595591ff7556f896a
StopTakingAllTheNames <ong.yinggao@gmail.com>
2020-08-16 15:54:26 +0800

    Changed greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,5 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(lines + "\tHello, I'm your chatbot!\n\tWhat can I do for you?\n" + lines);
     }
 }
