ece5fcc4d52c2fabc704a4c8a5d3364670fd54b6
BreadPeanutButter <joonjie98@gmail.com>
2020-08-16 20:02:20 +0800

    Implement Level-1: 1. Greet user 2. Echo user commands 3. Exit upon bye command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        String response;
+        System.out.println("Hi there! I'm Peanut.\nHow can I be of assistance?\n");
+        response = sc.nextLine();
+        while (!response.equals("bye")) {
+            System.out.println(response + "\n");
+            response = sc.nextLine();
+        }
+        System.out.println("Bye! Sad to see you go :(");
+
     }
+
 }
+
+
+
