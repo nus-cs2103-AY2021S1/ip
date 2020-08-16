1b045bc713434cd1da979b8ac84a1bda71caa93d
khenghun <e0406137@u.nus.edu>
2020-08-16 13:19:34 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void line(){
+        for (int i = 0; i < 50; i++) {
+            System.out.print("\u2500");
+        }
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm duke. What can I do for you? \n" + logo);
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    while(!str.equals("bye")){
+        line();
+        System.out.println(str);
+        line();
+        str = sc.nextLine();
+    }
+    System.out.println("Bye. Hope to see you again soon!");
+    sc.close();
     }
 }
