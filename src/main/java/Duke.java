8584667a9687be4dc33379d62c45d8c42136423f
Bangyi Wu <bangyi98@gmail.com>
2020-08-16 23:34:05 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,32 +2,54 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    public static void printLine() {
+        System.out.println("-------------------------------");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> todo = new ArrayList<String>();
+        ArrayList<Task> todo = new ArrayList<Task>();
+        printLine();
         System.out.println("What's new scooby doo?\n" + "How can I help you today?");
-        while (sc.hasNextLine()) {
-            String command = sc.nextLine();
-            switch (command) {
-                case "bye":
-                    System.out.println("See you later alligator ");
-                    System.exit(0);
-                    break;
-                case "list":
-                    int index = 1;
-                    for (String str :
-                            todo) {
-                        System.out.println(index + ". " + str);
-                        index++;
-                    }
-                    if (todo.size() == 0) {
-                        System.out.println("Your life is empty now bruh");
-                    }
-                    break;
-                default:
-                    todo.add(command);
-                    System.out.println("okay you need to: " + command);
+        printLine();
+            while (sc.hasNext()) {
+                String command = sc.next();
+                switch(command) {
+                    case "bye":
+                        printLine();
+                        System.out.println("See you later alligator ");
+                        printLine();
+                        System. exit(0);
+                        break;
+                    case "list":
+                        printLine();
+                        System.out.println("These are what you got: ");
+                        for ( Task task:
+                                todo) {
+                            System.out.println( todo.indexOf(task)+1 + ". " + task.getStatusIcon() + " " + task.description);
+                        }
+                        if (todo.size() == 0) {
+                            System.out.println("Your life is empty now bruh");
+                        }
+                        printLine();
+                        break;
+                    case "done":
+                        int taskID = sc.nextInt() - 1;
+                        Task task = todo.get(taskID);
+                        task.markAsDone();
+                        printLine();
+                        System.out.println("Gratz, you finished this dawg :");
+                        System.out.println(task.getStatusIcon()+ task.description);
+                        printLine();
+                        break;
+                    default:
+                        String fullTask = command + sc.nextLine();
+                        todo.add(new Task(fullTask));
+                        printLine();
+                        System.out.println("okay you need to: " + fullTask);
+                        printLine();
+                }
             }
         }
     }
-}
---
+++ src/main/java/Task.java
@@ -0,0 +1,15 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "[✓]" : "[x]"); //return tick or X symbols
+    }
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+}
