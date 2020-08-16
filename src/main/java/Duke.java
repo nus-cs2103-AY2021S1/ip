305a2472cd26991eaae635537623de7b86fba6e3
Lim Chun Yong <lim.chunyong@U.nus.edu>
2020-08-16 17:46:13 +0800

    lvl3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,10 +19,25 @@ public class Duke {
         }
         else if (word.equals("list")) {
             printTopLine();
+            System.out.println("Here are the tasks in your list:");
             for (int i = 0; i < tasks.size(); i++) {
                 System.out.println((i + 1) + ". " + tasks.get(i));
             }
             printBottomLine();
+        }
+        else if (word.startsWith("done")) {
+            try {
+                int index = Character.getNumericValue(word.charAt(5));
+                Task originalTask = tasks.get(index);
+                Task nextTask = originalTask.setTaskAsCompleted();
+                tasks.set(index, nextTask);
+                printTopLine();
+                System.out.println("Nice! I've marked this task as done: \n" + nextTask);
+                printBottomLine();
+            }
+            catch (Exception e) {
+                return;
+            }
         }
         else {
             Task newTask = new Task(word);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,23 @@
 public class Task {
     String taskName;
+    boolean isCompleted;
     Task(String task) {
         this.taskName = task;
+        this.isCompleted = false;
+    }
+
+    private Task(String task, boolean isCompleted) {
+        this.taskName = task;
+        this.isCompleted = isCompleted;
+    }
+
+    public Task setTaskAsCompleted() {
+        return new Task(taskName, true);
     }
 
     @Override
     public String toString() {
-        return taskName;
+        String tickOrCross  = isCompleted? "\u2713" : "\u2717";
+        return String.format("%s %s %s %s", "[", tickOrCross, "] ", taskName);
     }
 }
