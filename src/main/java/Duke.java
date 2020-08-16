e7408256164ba0d8011cd8eb187773ce86332c3e
minzzelo <siminnnx@gmail.com>
2020-08-16 16:09:07 +0800

    Complete level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private List<String> tasks = new ArrayList<>();
+
     void greet() {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
     }
@@ -9,19 +13,36 @@ public class Duke {
         System.out.println("Bye. Hope to see you soon!");
     }
 
-    public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.greet();
+    void addTask(String task) {
+        tasks.add(task);
+        System.out.println("added: " + task);
+    }
+
+    void list() {
+        for(int i = 1; i <= tasks.size(); i++) {
+            System.out.println(i + ". " + tasks.get(i-1));
+        }
+    }
+
+    void start() {
+        greet();
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()) {
-            String command = sc.next();
+            String command = sc.nextLine();
             if(command.equals("bye")) {
-                duke.exit();
+                exit();
                 break;
+            } else if(command.equals("list")){
+                list();
             } else {
-                System.out.println(command);
+                addTask(command);
             }
         }
         sc.close();
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.start();
     }
 }
