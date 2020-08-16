86e68f4b977bf6f39ddb110abcab8fe28f143855
Rayson Koh <rayson.rk.koh@gmail.com>
2020-08-17 04:43:51 +0800

    Add DeadlineTask extending Task

---
+++ src/main/java/DeadlineTask.java
@@ -0,0 +1,23 @@
+public class DeadlineTask extends Task {
+    private final String deadline;
+
+    public DeadlineTask(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    private DeadlineTask(String description, boolean isDone, String deadline) {
+        super(description, isDone);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public DeadlineTask markAsDone() {
+        return new DeadlineTask(description, true, deadline);
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + deadline + ")";
+    }
+}
