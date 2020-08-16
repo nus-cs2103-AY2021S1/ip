33d28cfc0bf85c0e339204f4bcdff6434e0b9c3e
Andrea <andreaxtan611@gmail.com>
2020-08-16 19:47:25 +0800

    Level 1 with customization and amendments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     public static void main(String[] args) {
         String bot = "Dave: \n";
         String user = "Me: \n";
+
         //Initial greetings
         System.out.println("_______________________________________________________________");
         System.out.println(bot + "Greetings from me, Dave! \n"  + "How can I help you? ^_^");
@@ -14,23 +15,19 @@ public class Duke {
         //Looping and echoing user inputs
         while(scanner.hasNext()) {
             String userInput = scanner.next(); //may be string, int, etc
-            if(userInput.equals("Bye")) {
+            if(userInput.equals("bye")) {
                 System.out.println("_______________________________________________________________");
                 System.out.println(bot + "Goodbye! Hope to see you again soon! ^_^");
                 System.out.println("_______________________________________________________________");
                 break;
             } else {
+                //added.add(userInput);
+                System.out.println("_______________________________________________________________");
                 System.out.println(bot + userInput);
+                System.out.println("_______________________________________________________________");
             }
 
         }
-
-
-
-
-
-
-
 
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
