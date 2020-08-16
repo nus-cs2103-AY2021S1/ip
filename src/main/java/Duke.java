343317a8d5549e8583628486104e96434c5bff2e
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2020-08-16 18:52:41 +0800

    add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+import main.java.Task;
+
+import java.util.*;
 
 public class Duke {
 
-    String introduction = "Hello! I'm Duke";
-    String greeting = "What can I do for you?";
-    String goodbye = "Bye. Hope to see you again soon!";
+    private String introduction = "Hello! I'm Duke";
+    private String greeting = "What can I do for you?";
+    private String goodbye = "Bye. Hope to see you again soon!";
+
+    private List<Task> taskList = new ArrayList<>();
 
     private void printGreeting() {
         System.out.println(this.introduction);
@@ -15,11 +19,27 @@ public class Duke {
         System.out.println(this.goodbye);
     }
 
+    private void listTasks() {
+        for (int i = 0; i < this.taskList.size(); i++) {
+            System.out.println((i + 1) + ". " + this.taskList.get(i).getDescription());
+        }
+    }
+
+    private void addTask(String input) {
+        Task newTask = new Task(input);
+        this.taskList.add(newTask);
+        System.out.println("added: " + input);
+    }
+
     private void handleUserInput() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                listTasks();
+            } else {
+                addTask(input);
+            }
             input = sc.nextLine();
         }
         sc.close();
---
+++ src/main/java/Task.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Task {
+
+    private String description;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
