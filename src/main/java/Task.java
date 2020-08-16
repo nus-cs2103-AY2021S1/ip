ae4d2fd7c6d02da8bff304412c9d74a0ca9da0a0
pr4aveen <ajjuu98@gmail.com>
2020-08-16 12:56:20 +0800

    Create task object
    Program stores all tasks as Strings.
    
    Tasks should be represented as a separate object so that more
    information can be stored within each task.
    
    Let's
     * Create a Task object to represent individual tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+
+    private String taskName;
+    private boolean isDone = false;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void competeTask() {
+        isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        if (isDone) {
+            return "[✓] " + taskName;
+        } else {
+            return "[✗] " + taskName;
+        }
+    }
+}
