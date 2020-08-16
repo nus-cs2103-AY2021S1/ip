369a9fbb6d112bb12f2f53bb443d7ebe1c362b35
Preston Toh <prestontoh@gmail.com>
2020-08-16 21:53:05 +0800

    add skeletal Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner inputs = new Scanner(System.in);
+        boolean run = true;
+
+        System.out.println("Hello~, I'm Duke!");
+
+        while (run == true) {
+            String next = inputs.next();
+
+            if (next.equals("bye")) {
+                System.out.println("Goodbye~");
+                run = false;
+            } else {
+                System.out.println(next);
+            }
+        }
     }
+
+    
+//    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+//    }
 }
