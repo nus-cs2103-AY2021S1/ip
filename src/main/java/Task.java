a8f64b902adaa4aed12a9c3baa64c40f701ff2e7
tankangliang <tankangliang98@gmail.com>
2020-08-16 19:10:16 +0800

    Add done attribute to Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,26 @@
 public class Task {
     private String description;
+    private boolean done;
 
     public Task(String d) {
         this.description = d;
+        this.done = false;
     }
 
     public String getDescription() {
         return this.description;
+    }
+
+    public boolean getDone() {
+        return this.done;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public String getStatus() {
+        String check = this.done ? "X" : " ";
+        return "[" + check + "] " + this.getDescription();
     }
 }
