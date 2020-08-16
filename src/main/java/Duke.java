938f3420a4af389d0a3dd50d9d0713839c2ad9ff
sudogene <andywubby@gmail.com>
2020-08-16 12:00:16 +0800

    level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Duke {
 
-    private String name;
+    private final String name;
     private boolean running;
-    private List<Task> list;
+    private final List<Task> list;
 
     public Duke(String name) {
         this.name = name;
@@ -45,13 +46,34 @@ public class Duke {
         System.out.println(line + "\n    " + sb + "\n" + line);
     }
 
-    public void addToList(String description) {
-        Task task = new Task(description);
-        if (list.add(task)) {
-            sendMessage("Added: " + description);
-        } else {
-            genericError();
+    public void addToList(String type, String description) {
+        Task task;
+        String[] split;
+        String desc;
+        switch (type) {
+            case "todo":
+                task = new Todo(description);
+                break;
+            case "deadline":
+                split = description.split(" /by ");
+                desc = split[0];
+                String by = split[1];
+                task = new Deadline(desc, by);
+                break;
+            case "event":
+                split = description.split(" /at ");
+                desc = split[0];
+                String at = split[1];
+                task = new Event(desc, at);
+                break;
+            default:
+                task = new Task(description);
+                break;
         }
+        list.add(task);
+        String msg = "Okay I've added:\n    " + task + "\n";
+        msg += String.format("Now you have %d tasks in the list.", list.size());
+        sendMessage(msg);
     }
 
     public void markTaskDone(int i) {
@@ -74,22 +96,22 @@ public class Duke {
             sendMessage("Your list is empty!");
         } else {
             int num = 1;
-            String msg = (list.size() > 1)
+            StringBuilder msg = new StringBuilder((list.size() > 1)
                     ? "Here are the tasks in your list:\n"
-                    : "Here's your one and only task:\n";
+                    : "Here's your one and only task:\n");
             for (Task task: list) {
-                msg += String.format("%d. %s", num, task);
+                msg.append(String.format("%d. %s", num, task));
                 if (num < list.size()) {
-                    msg += "\n";
+                    msg.append("\n");
                 }
                 num++;
             }
-            sendMessage(msg);
+            sendMessage(msg.toString());
         }
     }
 
     public static void main(String[] args) {
-        String lvl = "3";
+        String lvl = "4";
         String logo = "                                                 _     _\n"
                 + " _______  _______  _______  _______  __   __    (c).-.(c)\n"
                 + "|       ||       ||  _    ||  _    ||  | |  |    / ._. \\ \n"
@@ -108,9 +130,10 @@ public class Duke {
         while (duke.isRunning()) {
             String input = sc.nextLine();
             String[] inputSplit = input.split(" ");
+            String command = inputSplit[0];
             boolean doDefault = false;
 
-            switch(inputSplit[0].toLowerCase()) {
+            switch(command.toLowerCase()) {
                 case "bye":
                     duke.exit();
                     break;
@@ -119,20 +142,28 @@ public class Duke {
                     break;
                 case "done":
                     try {
-                        int i = Integer.valueOf(inputSplit[1]);
+                        int i = Integer.parseInt(inputSplit[1]);
                         duke.markTaskDone(i);
                         break;
                     } catch (ArrayIndexOutOfBoundsException
                             | NumberFormatException e) {
-                        doDefault = true;
+                        break;
+                    }
+                case "todo":
+                case "deadline":
+                case "event":
+                    try {
+                        String[] descArr = Arrays.copyOfRange(inputSplit,
+                                1, inputSplit.length);
+                        String desc = String.join(" ", descArr);
+                        duke.addToList(command, desc);
+                        break;
+                    } catch (ArrayIndexOutOfBoundsException e) {
                         break;
                     }
                 default:
-                    duke.addToList(input);
+                    duke.addToList("", input);
                     break;
-            }
-            if (doDefault) {
-                duke.addToList(input);
             }
         }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,5 @@
 public class Task {
+
     protected String description;
     protected boolean isDone;
 
