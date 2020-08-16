689b30c8c6c09180db3ef5a9bf09adf4bf96b908
Licheng-Wu <wlc.licheng@gmail.com>
2020-08-16 22:10:20 +0800

    Added Echo.java and feature to echo 1 command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter command:");
+
+        String command = sc.nextLine();  // Read user input
+        Echo.echoMessage(command); // Output user input
     }
 }
---
+++ src/main/java/Echo.java
@@ -0,0 +1,9 @@
+public class Echo {
+    public static void echoMessage(String message) {
+        if (message != "bye") {
+            System.out.println(message);
+        } else {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+    }
+}
