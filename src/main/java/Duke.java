7ec4d59fe8c5c9c6f2c462e866769724e49b01d0
SherryWu178 <sherry.wu178@gmail.com>
2020-08-16 11:50:27 +0800

    Add list function (lvl2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Duke dk = new Duke();
-        dk.greet();
+        ArrayList<String> tasks = new ArrayList<String>();
 
+        dk.greet();
         Scanner sc = new Scanner( System.in );
         while (sc.hasNext()) {
             String input = sc.next();
             if (input.equals("bye")) {
-                dk.end_conversation();
+                dk.endConversation();
+            } else if (input.equals("list")) {
+                dk.showTask(tasks);
             } else {
                 dk.echo(input);
+                tasks.add(input);
             }
         }
     }
@@ -21,11 +26,19 @@ public class Duke {
     }
 
     public void echo(String input){
-        System.out.println(input);
+        System.out.println("added: " + input);
     }
 
-    public void end_conversation(){
-        System.out.println("Bye. Hope to see you again soon! ∠( ᐛ 」∠)＿");
+    public void showTask(ArrayList<String> tasks){
+        int no = 1;
+        for (String task : tasks) {
+            System.out.println(no + ". " + task);
+            no++;
+        }
+    }
+
+    public void endConversation(){
+        System.out.println("Bye~ Hope to see you again soon! ∠( ᐛ 」∠)＿");
         System.exit(0);
     }
 
