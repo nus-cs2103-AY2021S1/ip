aa76475772e519c76883acb00e89770845d5ad3a
Dora Heng <e0425696@u.nus.edu>
2020-08-16 13:31:56 +0800

    Modify level-1

--- src/main/java/duke/Message.java
+++ src/main/java/duke/Message.java
@@ -1,4 +1,11 @@
 package duke;
 
+
 public class Message {
+    public static final String MESSAGE_WELCOME = "Hello! I'm Duke\n    What can I do for you?";
+    public static final String MESSAGE_LIST = "list";
+    public static final String MESSAGE_RANDOM = "blah";
+    public static final String MESSAGE_EXIT = "Bye. Hope to see you again soon!";
+    public static final String INDENT = "    ";
+    public static final String BORDERS = "_________________________________________________";
 }
