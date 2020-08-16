c68249a3ee29fa5de296c1ff52d026160541c478
Rayson Koh <rayson.rk.koh@gmail.com>
2020-08-16 10:44:41 +0800

    Added Task class with isDone flag

---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    private String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
+}
