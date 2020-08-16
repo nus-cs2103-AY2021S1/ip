41d47420d250ec69392317e2d3635008f409c360
Theodore Leebrant <theodoreleebrant@gmail.com>
2020-08-16 20:59:34 +0800

    Finish Level 2 Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void initialMessage() {
         System.out.println("    ____________________________________________________________");
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can I do for you?");
         System.out.println("    ____________________________________________________________");
+    }
 
+    public static void exitMessage() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void printMessage(String msg) {
+        System.out.println("    ____________________________________________________________");
+        System.out.print(msg);
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> todoList = new ArrayList<>();
 
+        initialMessage();
         while(true) {
             String echo = sc.nextLine();
-
             if (echo.equals("bye")) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Bye. Hope to see you again soon!");
-                System.out.println("    ____________________________________________________________");
+                exitMessage();
                 break;
             } else {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     " + echo);
-                System.out.println("    ____________________________________________________________");
+                todoList.add(echo);
+                String message = "";
+                for (int i = 0; i < todoList.size(); i++) {
+                    message = message + "     " + (i + 1) + ". " + todoList.get(i) + "\n";
+                }
+                printMessage(message);
             }
         }
 
