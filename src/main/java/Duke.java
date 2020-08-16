786feccc8bca1e0ec5793784c25aa3ba164584fc
AlexCQY <alex_chua@u.nus.edu>
2020-08-16 15:23:17 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void chatbot() {
+        Scanner sc = new Scanner(System.in);
+
+        String indent = "    ";
+        String upperLine = indent + "____________________________________" + "\n";
+        String lowerLine =  indent + "____________________________________" +"\n";
+        String greetings = indent + "Hello! I'm Duke" + "\n"
+                + indent + "What can I do for you?" +"\n";
+
+        System.out.println(upperLine + greetings + lowerLine);
+
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            String outputIndent = indent + " ";
+            String output = outputIndent + input + "\n";
+
+            if (input.equals("bye")) {
+                String exit = outputIndent + "Bye. Hope to see you again soon!" + "\n";
+                System.out.println(upperLine + exit + lowerLine);
+                break;
+            }
+            System.out.println(upperLine + output + lowerLine);
+
+        }
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        chatbot();
     }
 }
