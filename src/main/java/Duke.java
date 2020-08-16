0c96d1e16f459fa04d950219d6a73ebd49dcaf4c
PhongTran98 <giaphong8127@gmail.com>
2020-08-16 16:08:02 +0800

    add Level-1
    Implement a skeletal version of Duke that starts by greeting the user,
    simply echos commands entered by the user, and exits when the user types
    bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+package main.java;
+import java.util.*;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+    private static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static void startMessage() {
+        System.out.println("┌────────────────────────────────────────────────┐");
+    }
+
+    private static void endMessage() {
+        System.out.println("└────────────────────────────────────────────────┘");
+    }
+
+    private static void greet() {
+        startMessage();
         System.out.println("Hello from\n" + logo);
+        endMessage();
+    }
+
+    private static void handleCommand(String cmd) {
+        startMessage();
+        if (!cmd.equals("bye")) {
+            System.out.println(cmd);
+        } else {
+            System.out.println("See you again!");
+        }
+        endMessage();
+    }
+
+    public static void main(String[] args) {
+        greet();
+        Scanner sc = new Scanner(System.in);
+        String input;
+        do {
+            input = sc.next();
+            handleCommand((input));
+        }
+        while (!input.equals("bye"));
     }
 }
