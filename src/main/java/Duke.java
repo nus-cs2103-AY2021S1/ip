4689ff9c5ad80272e048034a30f85764c9abb008
Jeanne Toh <jeannetoh99@gmail.com>
2020-08-16 20:07:20 +0800

    added level 2 implementations -- Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private List<String> list;
+    private static final String starline = "**************************************************************************";
     private static final String logo =
               " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
+            + "|____/ \\__,_|_|\\_\\___|";
 
-    public static String getLogo() {
-        return logo;
+    public Duke() {
+        this.list = new ArrayList<>();
     }
 
     public static void greet() {
-        System.out.println("Welcome! I am copycat\n" + getLogo());
-        System.out.println("If you are sick of me, feel free to say 'bye'!");
-        System.out.println("In the meantime, what can I repeat for you?");
+        System.out.println(starline +
+                "\nWelcome! I am \n" + logo +
+                "\nHere are some magic words to get you going: " +
+                "\nIf you are done, feel free to say 'bye'!" +
+                "\nIf you'd like to view your tasks, say 'list'." +
+                "\nOtherwise, send me new tasks to add them to your todo list!\n" +
+                starline);
     }
 
-    public static void awaitInputCommand(Scanner sc) {
+    public void awaitInputCommand() {
+        Scanner sc = new Scanner(System.in);
         String next = sc.nextLine();
         while (!next.equals("bye")){
-            echo(next);
+            if (next.equals("list")) {
+                list();
+            } else {
+                add(next);
+            }
             next = sc.nextLine();
         }
-        exit();
+        sc.close();
     }
 
-    public static void echo(String input) {
-        System.out.println(input);
+    public void echo(String input) {
+        System.out.println("added: " + input);
+    }
+
+    public void add(String input) {
+        this.list.add(input);
+        echo(input);
+    }
+
+    public void list() {
+        System.out.println(starline);
+        for (int i=0; i < this.list.size(); i++) {
+            System.out.println(String.format("%d. %s", i+1, this.list.get(i)));
+        }
+        System.out.println(starline);
     }
 
     public static void exit() {
@@ -40,13 +66,13 @@ public class Duke {
                         "| |_/ /   |  |   |  |__  \n" +
                         "|____/    |__|   |_____| \n"
         );
-        System.out.println("just kidding! it's sad to see you go!! hope to talk to you again soon!");
+        System.out.println("Hope you have a productive day ahead! :))");
     }
 
     public static void main(String[] args) {
+        Duke duke = new Duke();
         greet();
-        Scanner sc = new Scanner(System.in);
-        awaitInputCommand(sc);
-        sc.close();
+        duke.awaitInputCommand();
+        exit();
     }
 }
