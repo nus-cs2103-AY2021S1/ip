4d504dab96b91eb46f0800449ce0fff7f6050896
Persdre <persdre@gmail.com>
2020-08-17 00:11:07 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(
+                "I'm DukeForQ, your chatbot! You can enter everything you want to enter. If you want to exit, enter 'bye'!");
+        String s;
+        while (true) {
+            s = sc.nextLine();
+            if (s.equals("bye")) {
+                System.out.println("Bye, hope to see you again!");
+                sc.close();
+                System.exit(0);
+            } else {
+                System.out.println(s);
+            }
+        }
     }
 }
