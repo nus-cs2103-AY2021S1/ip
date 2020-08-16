8089e76a68b4c506f52a5b5707ae022440ef2cb8
chanwei53 <hanwei_cheak@hotmail.sg>
2020-08-16 22:34:03 +0800

    Completed Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static ArrayList<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         greet();
-        echo();
+        readTasks();
     }
 
     private static void greet() {
@@ -14,22 +17,33 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.print("Hello, I am\n" + logo);
-        System.out.println("___________________________________________________");
-
-        System.out.println("Duke: What can i do for you?");
+        System.out.println("___________________________________________________" +
+            "\nDuke: What can i do for you?");
     }
 
-    private static void echo() {
+    private static void printTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            String task = tasks.get(i);
+            System.out.println(i + 1 + ". " + task);
+        }
+        System.out.println("--------------------------" +
+                "-------------------------");
+    }
+
+    private static void readTasks() {
         Scanner sc = new Scanner(System.in);
-        String reply = "Duke: " + "%s" + "\n--------------------------" +
+        String reply = "Duke: Added - " + "%s" + "\n--------------------------" +
                 "-------------------------";
         while (sc.hasNext()) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println(String.format(reply, "Bye. Hope to see you again soon!"));
+                System.out.println("Bye. Hope to see you again soon!");
                 sc.close();
                 break;
+            } else if (input.equals("list")) {
+                printTasks();
             } else {
+                tasks.add(input);
                 System.out.println(String.format(reply, input));
             }
         }
