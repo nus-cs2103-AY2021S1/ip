e8ffe9ad64e9d32eaf8c160ec4c2b00af266970a
Rahul0506 <rahul.gidijala@gmail.com>
2020-08-16 20:13:30 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void writeOutput(String... messages) {
+        System.out.println("\t-----------------------------------------");
+        for (String message : messages) {
+            System.out.println("\t" + message);
+        }
+        System.out.println("\t-----------------------------------------");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        writeOutput("Hello! I'm Duke", "What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        boolean keepGoing = true;
+        while (keepGoing) {
+            input = scanner.nextLine();
+            if (!input.equals("bye")) {
+                writeOutput(input);
+            } else {
+                writeOutput("Bye. Hope to see you again soon!");
+                keepGoing = false;
+            }
+        }
+        scanner.close();
+
     }
 }
