f72c21f0d67c93a9a643335937ec8c2526932202
JoeTan <tjwjoe1998@gmail.com>
2020-08-16 15:52:42 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
+    Scanner sc = new Scanner(System.in);
+
+    void takeInputs() {
+        boolean quit = false;
+        while (!quit) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                quit = true;
+                System.out.println("cya");
+            } else {
+                System.out.println(input);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +23,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        
+        Duke duke = new Duke();
+        duke.takeInputs();
     }
 }
