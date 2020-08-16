b50fc928ac4ad0b7d9c4811f83f46ffca446a16d
pr4aveen <ajjuu98@gmail.com>
2020-08-16 12:02:24 +0800

    Add Level-1 Requirements
    Add features for Level-1 of Duke as specified in the project
    requirements.
    
    Let's
     * Make the program start by greeting the user
     * Make the program echo commands entered by the user
     * Make the program exit when the user types 'bye'
    
    See below for more details:
    https://nus-cs2103-ay2021s1.github.io/website/admin/ip-w2.html

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String DIVIDER = "_________________________________________________________________________________";
+
+    private static final String LOGO = "             _        ______   _____    ______   _____  \n" +
+            "     /\\     | |      |  ____| |  __ \\  |  ____| |  __ \\ \n" +
+            "    /  \\    | |      | |__    | |__) | | |__    | |  | |\n" +
+            "   / /\\ \\   | |      |  __|   |  _  /  |  __|   | |  | |\n" +
+            "  / ____ \\  | |____  | |      | | \\ \\  | |____  | |__| |\n" +
+            " /_/    \\_\\ |______| |_|      |_|  \\_\\ |______| |_____/ \n";
+
+    private static void printToConsole(String message) {
+        System.out.println(DIVIDER);
+        System.out.println(message);
+        System.out.println(DIVIDER);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(LOGO);
+        printToConsole("Hi I'm Alfred! How can I help you today?");
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                printToConsole("Goodbye!");
+                break;
+            }
+
+            printToConsole(input);
+        }
+
+        sc.close();
     }
 }
