dc6b779ce7c7a92e8be9ae7545192d028baaf7ae
Rayson Koh <rayson.rk.koh@gmail.com>
2020-08-17 07:01:02 +0800

    Refactored parseInputSections to throw DukeException for: - empty description - no secondary keyword such as /at, /by - no content after secondary keyword

--- src/main/java/StringUtils.java
+++ src/main/java/StringUtils.java
@@ -1,4 +1,40 @@
 public class StringUtils {
+    public static String[] parseInputSections(String input, String breakPt1, String breakPt2) throws DukeException {
+        int size = 2;
+        if (breakPt2.equals("")) {
+            size = 1;
+        }
+        String[] res = new String[size];
+
+        String[] splitByBP1 = input.split(breakPt1);
+        if (splitByBP1.length <= 1) {
+            throw new DukeException(breakPt1 + " description cannot be empty!");
+        }
+
+        String description = splitByBP1[1].trim();
+        if (description.equals("")) {
+            throw new DukeException(breakPt1 + " description cannot be empty!");
+        }
+
+        if (size == 1) {
+            res[0] = description;
+            return res;
+        }
+
+        String[] splitByBP2 = description.split(breakPt2);
+        if (!description.contains(breakPt2)) {
+            throw new DukeException(breakPt2 + " keyword must be in input for all " + breakPt1 + " commands!");
+        }
+
+        if (splitByBP2.length <= 1 || splitByBP2[1].trim().equals("")) {
+            throw new DukeException("Content after " + breakPt2 + " should not be empty!");
+        }
+
+        res[0] = splitByBP2[0].trim();
+        res[1] = splitByBP2[1].trim();
+
+        return res;
+    }
 
     public static <T> void printWithWrapper(T[] arr, boolean withNumbering) {
         String BORDER = "=======================================";
