41f957b35f02c8fd0b05bf7462b7d9897a992510
Foo Kai En <kaien.foo@gmail.com>
2020-08-16 14:24:11 +0800

    Add greet, echo and exit commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        boolean isRunning = true;
+        greet();
+        while (isRunning) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("bye! see you soon!\n");
+                isRunning = false;
+            } else {
+                echo(input);
+            }
+        }
     }
+
+    static void greet() {
+        String logo = "\n.-. .-')                .-') _  .-. .-')                .-') _    \n"
+                + "\\  ( OO )              (  OO) ) \\  ( OO )              (  OO) )   \n"
+                + " ;-----.\\  .-'),-----. /     '._ ;-----.\\  .-'),-----. /     '._  \n"
+                + " | .-.  | ( OO'  .-.  '|'--...__)| .-.  | ( OO'  .-.  '|'--...__) \n"
+                + " | '-' /_)/   |  | |  |'--.  .--'| '-' /_)/   |  | |  |'--.  .--' \n"
+                + " | .-. `. \\_) |  | |  |   |  |   | .-. `. \\_) |  | |  |   |  |    \n"
+                + " | |  \\  |  \\ |  | |  |   |  |   | |  \\  |  \\ |  | |  |   |  |    \n"
+                + " | '--'  /   `'  '-'  '   |  |   | '--'  /   `'  '-'  '   |  |    \n"
+                + " `------'      `-----'    `--'   `------'      `-----'    `--'    \n";
+        System.out.println("helluu! I'm\n" + logo + "\nwhat would you like me to do?\n");
+    }
+
+    static void echo(String str) {
+        System.out.println(str + "\n");
+    }
+
 }
