4d6802f596c45fc575d2b386dc5ba9f7b012ebc1
Bangyi Wu <bangyi98@gmail.com>
2020-08-16 23:18:47 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello I'm Vector\n" + "How can I help you today?");
+        while (sc.hasNext()) {
+            String command = sc.next();
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }
