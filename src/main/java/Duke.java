8d365a02687fa7d029b0e27194143a667f9f36a2
mgiang2015 <mgiang2015@gmail.com>
2020-08-16 15:52:34 +0800

    Implement functions to help with Hal9000 operations. Specificly get input, generate output and then format output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,63 @@
+import java.util.Scanner;
+
 public class Duke {
+    /**
+     * get_input() is in charge of asking for input from user
+     * and return the input as a string to be used for something else.
+     * @param sc Scanner object
+     * @return a string representing user input
+     */
+    public static String get_input(Scanner sc) {
+        String user_input = sc.nextLine().trim(); // Read user input
+        return user_input;
+    }
+
+    /**
+     * get_output(String) decides what Hal's output should be
+     * based on user's input.
+     * @param user_input input from user as String
+     * @return output by Hal9000
+     */
+    public static String get_output(String user_input) {
+        return user_input; // level 1
+    }
+
+    public static String format_response(String output_msg) {
+        return 
+        "____________________________________________________________\n"+
+        output_msg + "\n" +
+        "____________________________________________________________\n";
+    }
+
+    public static void hal9000_op() {
+        boolean end = false;
+        Scanner sc = new Scanner(System.in);
+        while (!end) {
+            String input = get_input(sc);
+            if (input.compareTo("bye") == 0) {
+                end = true;
+            } else {
+                String output_string = format_response(get_output(input));
+                System.out.println(output_string);
+            }
+        }
+        System.out.println(format_response("Bye. Hope to see you again soon!"));
+    }
+    
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        */
         String logo =
 "   __ _____   __  ___  ___  ___  ___\n" +
 "  / // / _ | / / / _ \\/ _ \\/ _ \\/ _ \\\n" +
 " / _  / __ |/ /__\\_, / // / // / // /\n" +
 "/_//_/_/ |_/____/___/\\___/\\___/\\___/\n";
                                      
-        
-        System.out.println("Hello from\n" + logo);
+        // Intro message
+        System.out.println(logo);
+        System.out.println(format_response(
+            "Hello! I'm Hal9000\nWhat can I do for you?"
+        ));
+
+        hal9000_op();
+
     }
 }
