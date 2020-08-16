357e8d7d2b0da3c53ad345be98b28a5920e1a9fb
CodingCookieRookie <e0415767@u.nus.edu>
2020-08-16 21:56:33 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,35 @@
-import   java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
 
 public class Duke {
+    private static List<String> taskList = new ArrayList<>();
+    public static void intro() {
+        System.out.println("Hi handsome! My name is Duck. What can I do for you?");
+    }
+    public static void addTask(String task) {
+        taskList.add(task);
+        System.out.println("added: " + task);
+    }
+    public static void printList() {
+        for(int i = 0; i < taskList.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, taskList.get(i)));
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hi handsome! My name is Duck. What can I do for you?");
-        while (sc.hasNext()) {
-            String echo = sc.next();
-            if (echo.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+        Duke.intro();
+        while (sc.hasNextLine()) {
+            String task = sc.nextLine();
+            if (task.equals("bye")) {
+                System.out.print("Bye. Hope to see you again soon!");
                 break;
             } else {
-                System.out.println(echo);
+                if (task.equals("list")) {
+                    Duke.printList();
+                } else {
+                    Duke.addTask(task);
+                }
             }
         }
         sc.close();
