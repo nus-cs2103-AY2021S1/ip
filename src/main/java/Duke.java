ee8bd8fb91444b8fb4015e451e667870caa79d8b
rizhaow <rizhaow@gmail.com>
2020-08-16 11:10:58 +0800

    feat(level2): Add list and add commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private List<String> tasks;
+
+    public Duke() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void greet() {
+        System.out.println(String.format("Hello! I'm Duke\nWhat can I do for you?"));
+    }
+
+    public void add(String task) {
+        this.tasks.add(task);
+        System.out.println(String.format("added: %s", task));
+    }
+
+    public void list() {
+        for (int i = 0; i < this.tasks.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, this.tasks.get(i)));
+        }
+    }
+
+    public void bye() {
+        System.out.println("Bye! Hope to never see you again!");
+    }
+    
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.greet();
+
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
 
         while (!cmd.equals("bye")) {
-            System.out.println(cmd);
+            switch (cmd) {
+                case "list":
+                    duke.list();
+                    break;
+                default:
+                    duke.add(cmd);
+                    break;
+            }
+
             cmd = sc.nextLine();
         }
 
-        System.out.println("Bye!");
-
+        duke.bye();
         sc.close();
     }
 }
