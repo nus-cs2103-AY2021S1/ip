507004eb045d7ae510488fa3e08b7c7f11231d89
hcy <2320032466@qq.com>
2020-08-16 13:09:05 +0800

    This is the commit for level 1

--- src/main/java/LevelOne.java
+++ src/main/java/LevelOne.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class LevelOne {
     public static void interact() {
         Scanner scanner = new Scanner(System.in);
-        String next = scanner.next();
+        String next = scanner.nextLine();
         while(!next.equals("bye")) {
             System.out.println(Duke.makeBlock(next));
-            next = scanner.next();
+            next = scanner.nextLine();
         }
         System.out.println(Duke.makeBlock(Duke.EXITMESSAGE));
     }
