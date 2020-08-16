8be51a037a674f303b06d85e51b9d735f8d63406
maguireong <maguire2622@yahoo.com.sg>
2020-08-16 18:34:48 +0800

    Add Todos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,18 +14,46 @@ public class Duke {
             String[] s = echo.split("\\s");
             String first = s[0];
 
-            if (first.equals("list")) {
-                for (int i = 0; i < books.size(); i++) {
-                    System.out.println(i+1 + ". " + books.get(i));
-                }
-            } else if (first.equals("bye")) {
+            if (first.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else {
-                books.add(echo);
-                System.out.println("added: " + echo);
             }
-
+            if (first.equals("list")) {
+                for (int i = 0; i < books.size(); i++) {
+                    int l = i + 1;
+                    System.out.println(books.get(i).getStatusIcon() + " " + l + ". " + books.get(i).description);
+                }
+            } else if (first.equals("done")) {
+                int index = Integer.parseInt(s[1]);
+                books.get(index - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + books.get(index - 1).getStatusIcon() + " return book");
+            } else if (first.equals("delete")) {
+                int index = Integer.parseInt(s[1]);
+                books.remove(index - 1);
+                System.out.println("Noted. I've removed this task:");
+                System.out.println(books.get(books.size() - 1));
+                System.out.println("Now you have " + books.size() + " tasks in the list.");
+            } else {
+                if (first.equals("todo")) {
+                    Todo t = new Todo(echo.substring(5));
+                    books.add(t);
+                } else if (first.equals("event")) {
+                    int start = echo.indexOf("/at");
+                    String date = echo.substring(start + 4);
+                    Event t = new Event(echo.substring(6, start - 1), date);
+                    books.add(t);
+                } else if (first.equals("deadline")) {
+                    int start = echo.indexOf("/by");
+                    String date = echo.substring(start + 4);
+                    Deadline t = new Deadline(echo.substring(9, start - 1), date);
+                    books.add(t);
+                }
+                System.out.println("Got it. I've added this task:");
+                System.out.println(books.get(books.size() - 1));
+                System.out.println("Now you have " + books.size() + " tasks in the list.");
+            }
         }
     }
 }
+
+
