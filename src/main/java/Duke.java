12bc7a7c921d8355704db0ccbe50e7a9aec7458f
shawn-nyk <shawnngyongkwang@gmail.com>
2020-08-16 14:11:25 +0800

    Add task list functionality
    Duke can now keep a record of the user's inputs as a list of tasks and
    can show the list to the user upon request.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
+
+    /** Star Bot's logo shown upon start up */
     private static String logo = "     _______.___________.    ___      " +
             ".______     " +
             " \n" +
@@ -18,8 +22,12 @@ public class Duke {
             "         |______/   \\______/      |__|             \n" +
             "                                                   ";
 
+    /** Divider that delineates Star Bot's replies */
     private static String divider =
             "------------------------------------------------------\n";
+
+    /** Stores user's tasks */
+    private static List<String> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -30,16 +38,35 @@ public class Duke {
 
         while(sc.hasNextLine()) {
             String line = sc.nextLine();
-            if (line.equals("bye")) {
+            if (line.equals("bye")) { // Exits the program
                 System.out.println(botReply("Goodbye, see you again soon!"));
                 System.exit(0);
-            } else {
-                System.out.println(botReply(line));
+            } else if (line.equals("list")) { // Lists out task list
+                System.out.println(botReply(printList()));
+            }
+            else { // Add task to task list
+                taskList.add(line);
+                System.out.println(botReply("added: " + line));
             }
         }
     }
 
+    /** Standardises the look of Star Bot's replies by wrapping it in
+     * dividers */
     private static String botReply(String reply) {
         return divider + reply + "\n" + divider;
+    }
+
+    /** Formats the task list to be shown to the user */
+    private static String printList() {
+        String result = "";
+        int index = 1;
+        for (String task : taskList) {
+            if (index != 1) {
+                result += "\n";
+            }
+            result += index++ + ". " + task;
+        }
+        return result;
     }
 }
