7261e28b866ae70c69f068a9da1c08525306a5c6
sagarsureka <sagar150102@gmail.com>
2020-08-16 13:20:57 +0530

    Add, List

--- src/main/java/Dobby.java
+++ src/main/java/Dobby.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Dobby {
     public static void main(String[] args) {
         /* String logo = " ____        _        \n"
@@ -8,21 +9,34 @@ public class Dobby {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-        String ln = "______________________________________";
-        System.out.println(ln);
-        System.out.println("Hello! I'm Dobby\nHow can I help you?");
-        System.out.println(ln);
+        String underscore_line = "    __________________________________________";
+        System.out.println(underscore_line);
+        System.out.println("    Hello! I'm Dobby\n    How can I help you?");
+        System.out.println(underscore_line);
+        ArrayList<String> list = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         while(scanner.hasNext()) {
             String text = scanner.nextLine();
-
             if (text.equalsIgnoreCase("bye")) {
                 scanner.close();
-                System.out.println(ln + "\nGoodbye. Hope to see you again soon!\n" + ln);
+                System.out.println(underscore_line);
+                System.out.println("    Goodbye. Hope to see you again soon!");
+                System.out.println(underscore_line);
                 System.exit(0);
+            } else if (text.equalsIgnoreCase("list")) {
+                int i = 0;
+                System.out.println(underscore_line);
+                for (String item: list) {
+                    i++;
+                    System.out.println("    " + i + ". " + item);
+                }
+                System.out.println(underscore_line);
             } else {
-                System.out.println(ln + "\n" + text + "\n" + ln);
+                list.add(text);
+                System.out.println(underscore_line);
+                System.out.println("    Added: " + text );
+                System.out.println(underscore_line);
             }
         }
     }
