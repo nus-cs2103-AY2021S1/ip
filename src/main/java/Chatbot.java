28c6fa9454bbb2bb335a6b9ee6bd192577bade01
the-reefshark <rishabh92k@gmail.com>
2020-08-17 05:46:30 +0800

    Change switch case to if-else

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -14,22 +14,21 @@ public class Chatbot {
             String input = scanner.nextLine();
             System.out.println(divider);
 
-            switch(input) {
-                case "list":
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(indent + (i + 1) + ". " + tasks.get(i));
-                    }
-                    System.out.println(divider);
-                    break;
-                case "bye": // Termination statement
-                    System.out.println(indent + "Guess its time for us to part ways\n"
-                            + indent + "Thanks for the memories\n" + indent + ":`(");
-                    System.out.println(divider);
-                    break loop;
-                default: // Echo back user input
-                    tasks.add(input);
-                    System.out.println(indent + "Added: " + input);
-                    System.out.println(divider);
+            if (input.equals("list")) {
+                System.out.println(indent + "Here are your tasks for today:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(indent + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(divider);
+            } else if (input.equals("bye")) { // Termination statement
+                System.out.println(indent + "Guess its time for us to part ways\n"
+                        + indent + "Thanks for the memories\n" + indent + ":`(");
+                System.out.println(divider);
+                break;
+            } else { // Echo back user input
+                tasks.add(input);
+                System.out.println(indent + "Added: " + input);
+                System.out.println(divider);
             }
         }
     }
