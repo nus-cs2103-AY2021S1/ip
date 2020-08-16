bab98963e9b9bf9c5970b98fe4c3b531f19d3a47
Andrea <andreaxtan611@gmail.com>
2020-08-16 20:24:19 +0800

    Level 2 with String line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,12 +6,13 @@ public class Duke {
         String bot = "Dave: \n";
         String user = "Me: \n";
         String addedText = "Added: ";
+        String line = "_______________________________________________________________";
         ArrayList<String> added = new ArrayList<>();
 
         //Initial greetings
-        System.out.println("_______________________________________________________________");
+        System.out.println(line);
         System.out.println(bot + "Greetings from me, Dave! \n" + "How can I help you? ^_^");
-        System.out.println("_______________________________________________________________");
+        System.out.println(line);
 
         Scanner scanner = new Scanner(System.in);
 
@@ -19,27 +20,27 @@ public class Duke {
         while (scanner.hasNext()) {
             String userInput = scanner.next(); //may be string, int, etc
             if (userInput.equals("bye")) {
-                System.out.println("_______________________________________________________________");
+                System.out.println(line);
                 System.out.println(bot + "Goodbye! Hope to see you again soon! ^_^");
-                System.out.println("_______________________________________________________________");
+                System.out.println(line);
                 break;
             } else if (userInput.equals("list")) {
                 if(added.isEmpty()) {
                     System.out.println("");
                 } else {
-                    System.out.println("_______________________________________________________________");
+                    System.out.println(line);
                     System.out.print(bot);
                     for (int i = 0; i < added.size(); i++) {
                         System.out.println(i + 1 + "." + " " + added.get(i));
                     }
-                    System.out.println("_______________________________________________________________");
+                    System.out.println(line);
                 }
             } else {
                 added.add(userInput);
-                System.out.println("_______________________________________________________________");
+                System.out.println(line);
                 System.out.print(bot);
                 System.out.println(addedText + userInput);
-                System.out.println("_______________________________________________________________");
+                System.out.println(line);
             }
 
         }
