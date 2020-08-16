1c4039c74d8e7abee0d8ce5933bbce3c4d803348
the-reefshark <rishabh92k@gmail.com>
2020-08-17 05:32:12 +0800

    Add Level-2

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Chatbot {
@@ -6,20 +7,29 @@ public class Chatbot {
 
     public void start() {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         System.out.println(indent + "What can I do for you delightful human?\n" + divider);
 
-        while(scanner.hasNext()) {
-            String input = scanner.next();
-
+        loop: while(scanner.hasNext()) {
+            String input = scanner.nextLine();
             System.out.println(divider);
-            if(input.equals("bye")) { // Termination statement
-                System.out.println(indent + "Guess its time for us to part ways\n"
-                        + indent + "Thanks for the memories\n" + indent + ":`(");
-                System.out.println(divider);
-                break;
-            } else { // Echo back user input
-                System.out.println(indent + input);
-                System.out.println(divider);
+
+            switch(input) {
+                case "list":
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(indent + (i + 1) + ". " + tasks.get(i));
+                    }
+                    System.out.println(divider);
+                    break;
+                case "bye": // Termination statement
+                    System.out.println(indent + "Guess its time for us to part ways\n"
+                            + indent + "Thanks for the memories\n" + indent + ":`(");
+                    System.out.println(divider);
+                    break loop;
+                default: // Echo back user input
+                    tasks.add(input);
+                    System.out.println(indent + "Added: " + input);
+                    System.out.println(divider);
             }
         }
     }
