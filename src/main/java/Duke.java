698dbfefd0222b89279e85e5b71a62fa27657ed6
Rayson Koh <rayson.rk.koh@gmail.com>
2020-08-17 04:49:47 +0800

    Added parseInputSections method in Duke to parse inputs for different Task types

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -24,10 +23,25 @@ public class Duke {
 
     private static void processInput(String input, Store store) {
         String[] splitInput = input.split(" ");
+        String firstInput = splitInput[0];
+        String[] sections;
 
-        if (splitInput[0].equals("done")) {
-            store.markTaskAsDone(Integer.parseInt(splitInput[1]));
-            return;
+        switch (firstInput) {
+            case "done":
+                store.markTaskAsDone(Integer.parseInt(splitInput[1]));
+                return;
+            case "todo":
+                sections = parseInputSections(input, firstInput, null);
+                store.add(sections, firstInput);
+                return;
+            case "deadline":
+                sections = parseInputSections(input, firstInput, "/by");
+                store.add(sections, firstInput);
+                return;
+            case "event":
+                sections = parseInputSections(input, firstInput, "/at");
+                store.add(sections, firstInput);
+                return;
         }
 
         switch (input) {
@@ -35,7 +49,25 @@ public class Duke {
                 store.list();
                 break;
             default:
-                store.add(input);
+                store.add(new String[]{input}, "");
         }
+    }
+
+    private static String[] parseInputSections(String input, String breakPt1, String breakPt2) {
+        int size = 2;
+        if (breakPt2 == null) {
+            size = 1;
+        }
+        String[] res = new String[size];
+
+        String[] splitByBP1 = input.split(breakPt1 + " ");
+        if (size == 1) {
+            res[0] = splitByBP1[1];
+        } else {
+            String[] splitByBP2 = splitByBP1[1].split(" " + breakPt2 + " ");
+            res[0] = splitByBP2[0];
+            res[1] = splitByBP2[1];
+        }
+        return res;
     }
 }
