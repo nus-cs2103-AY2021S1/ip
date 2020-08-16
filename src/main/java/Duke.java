ec6777f77e2145e99f356daea844cd376ec7f2e5
Royxuzeng <e0376917@u.nus.edu>
2020-08-16 15:06:19 +0800

    Add logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,12 @@ import static java.lang.Integer.parseInt;
 
 public class Duke {
     public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
         String message = "____________________________________________________________\n" +
                 "  Hello! I'm Duke\n" +
                 "  What can I do for you?\n" +
