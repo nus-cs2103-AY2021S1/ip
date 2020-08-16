7d54d082c5a08cb7882cdbbebb5b3e333798d0c4
Rayson Koh <rayson.rk.koh@gmail.com>
2020-08-17 04:44:09 +0800

    Add TodoTask extending Task

---
+++ src/main/java/TodoTask.java
@@ -0,0 +1,20 @@
+public class TodoTask extends Task {
+
+    public TodoTask(String description) {
+        super(description);
+    }
+
+    private TodoTask(String description, boolean isDone) {
+        super(description, isDone);
+    }
+
+    @Override
+    public TodoTask markAsDone() {
+        return new TodoTask(description, true);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
