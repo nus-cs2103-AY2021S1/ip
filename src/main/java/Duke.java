b7cfb96038078733813b8e2b4986da115f76eda4
yongmingyang <yongmingyang1998@gmail.com>
2020-08-16 18:52:35 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -15,12 +16,21 @@ public class Duke {
         Scanner input =  new Scanner(System.in);
         String underscore = "____________________________________________________________ \n";
         String line = "";
+        ArrayList<String> todo = new ArrayList<>();
         while(!line.equals("bye")) {
             line = input.nextLine();
             if(line.equals("bye")) {
                 System.out.println(underscore + " Bye. Hope to see you again soon!" + "\n" + underscore);
+            } else if (line.equals("list")) {
+                System.out.println(underscore);
+                for (int i = 0; i < todo.size(); i++){
+                    int number = i + 1;
+                    System.out.println(" " + number + ". " + todo.get(i));
+                }
+                System.out.println(underscore);
             } else {
-                System.out.println(underscore + " " + line + "\n" + underscore);
+                todo.add(line);
+                System.out.println(underscore + " added: " + line + "\n" + underscore);
             }
         }
         input.close();
