3234df8b166027557e72a82f3eb209e2d9be1394
Cedo8 <seidobeh1998@gmail.com>
2020-08-16 22:57:24 +0800

    Added greeting, echoing and exit functionality to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String divider = "______________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(divider);
+        System.out.println("Beep Boop! Hello there!\n" + "What can I do for you?");
+        System.out.println(divider);
+
+        // Takes in user inputs. Program terminates when the String "bye" is entered.
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(divider);
+            System.out.println(input);
+            System.out.println(divider);
+            input = sc.nextLine();
+        }
+
+        System.out.println(divider);
+        System.out.println("Goodbye, have a nice day :D");
+        System.out.println(divider);
     }
 }
