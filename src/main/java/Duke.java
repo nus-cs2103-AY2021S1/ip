fb51a15fc9b1f5a9df14f66f98c665107b83f3e2
joeychensmart <chen.kangaroo@gmail.com>
2020-08-16 22:51:14 +0800

    Implement skeletal version that only echoes output
    Changed the logo, and added a few functions to format system output
    to stdout. Main function now also reads in and echoes input, and
    exits when "bye" is entered. It will also print a goodbye message.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        systemMessage(logo);
+        String input = scanner.nextLine();
+        while(!input.equals("bye")) {
+            systemMessage(input);
+            input = scanner.nextLine();
+        }
+        systemMessage("Bye sir thanks for using me sir hope to see you again sir");
     }
+    private static final String logo =
+                "                 /,   ,|   ,|     \n" +
+                "             /| /(  ,' / ,//      \n" +
+                "          \\`( |/ /,'  (,/ |      \n" +
+                "           \\ \\ ` `   `  /--,    \n" +
+                "         _,_\\ `  ` `  ``  /__    \n" +
+                "          '-.____________`  /     \n" +
+                "            [  \\@,    :] `--,-..-\n" +
+                "            [__________]__,'-._/  \n" +
+                "             )'o\\ ' o) \\/ )       Hello sir\n" +
+                "             \\  /   __  ./        Its me your assistant sir\n" +
+                "              \\=`   ==,\\..        How may I be of service sir\n" +
+                "               \\ -. `,' (        \n" +
+                "               \\`--''    \\.     \n";
+    private static void systemMessage(String input) {
+        System.out.println(horizontalRule+indent(input)+"\n"+horizontalRule);
+    }
+    private static String indent(String original) {
+        return "     "+ original.replaceAll("\n", "    \n");
+    }
+    private static final String horizontalRule = "    ____________________________________________________________\n";
 }
