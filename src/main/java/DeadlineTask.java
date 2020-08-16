88f78a7893e634be927e18f0b6320ccda39d5852
Jaylenlee <e0424677@u.nus.edu>
2020-08-16 17:39:13 +0800

    add DeadlineTask

---
+++ src/main/java/DeadlineTask.java
@@ -0,0 +1,13 @@
+public class DeadlineTask extends Task {
+    protected String time;
+
+    public DeadlineTask(String description, String time) {
+        super(description);
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + this.time + ")";
+    }
+}
