cccc0d58b288c623c9c48d20bfeff202979627d0
Rayson Koh <rayson.rk.koh@gmail.com>
2020-08-16 10:01:16 +0800

    Added greet, echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printWithWrapper(new String[]{"Hello, my name is \n" + logo, "How may I help you?"});
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            printWithWrapper(new String[]{input});
+            input = sc.nextLine();
+        }
+        printWithWrapper(new String[]{"Bye bye! Hope to see you again soon!"});
+    }
+
+    /**
+     * Wraps and prints the strArr with indentation and top and bottom borders
+     */
+    private static void printWithWrapper(String[] strArr) {
+        String BORDER = "=======================================";
+        String INDENT = "    ";
+        System.out.println(BORDER);
+        for (String str : strArr) {
+            System.out.println(INDENT + str);
+        }
+        System.out.println(BORDER);
     }
 }
