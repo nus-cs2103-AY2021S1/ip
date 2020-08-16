d5558aef2715ab23a6c77f6d729023c941833a90
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2020-08-16 20:36:20 +0800

    mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,24 +4,21 @@ import java.util.*;
 
 public class Duke {
 
-    private String introduction = "Hello! I'm Duke";
-    private String greeting = "What can I do for you?";
-    private String goodbye = "Bye. Hope to see you again soon!";
-
     private List<Task> taskList = new ArrayList<>();
 
     private void printGreeting() {
-        System.out.println(this.introduction);
-        System.out.println(this.greeting);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
     }
 
     private void printGoodbye() {
-        System.out.println(this.goodbye);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
     private void listTasks() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < this.taskList.size(); i++) {
-            System.out.println((i + 1) + ". " + this.taskList.get(i).getDescription());
+            System.out.println((i + 1) + "." + this.taskList.get(i).toString());
         }
     }
 
@@ -31,12 +28,34 @@ public class Duke {
         System.out.println("added: " + input);
     }
 
+    private void markTaskAsDone(int index) {
+        this.taskList.get(index).markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(this.taskList.get(index).toString());
+    }
+
+    private boolean isDoneCommand(String input) {
+        String[] words = input.split(" ");
+        boolean checkBackNumber = words.length == 2 &&
+                words[1].chars().allMatch(Character::isDigit);
+        if (checkBackNumber) {
+            int index = Integer.parseInt(words[1]) - 1;
+            boolean isValidNumber = index < this.taskList.size();
+            return words[0].equals("done") && isValidNumber;
+        } else {
+            return false;
+        }
+    }
+
     private void handleUserInput() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 listTasks();
+            } else if (isDoneCommand(input)) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                markTaskAsDone(index);
             } else {
                 addTask(input);
             }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,12 +3,26 @@ package main.java;
 public class Task {
 
     private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
     }
 
     public String getDescription() {
         return this.description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.description;
     }
 }
