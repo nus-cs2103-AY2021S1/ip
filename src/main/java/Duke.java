e7a6503ab3682cb95ab390580392b9694278139a
shermzlim <shermz.lim.jh@gmail.com>
2020-08-16 21:37:39 +0800

    Add basic greet, echo, exit functionality for chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Omega";
+        Duke.printHorizontalLine();
+        System.out.println("Hi! I am " + name + ", your personal assistant.");
+        System.out.println("How may I help you today?");
+        Duke.printHorizontalLine();
+        Duke.interactWithUser();
+    }
+
+    private static void interactWithUser() {
+        boolean exitProgram = false;
+        Scanner scn = new Scanner(System.in);
+        while (!exitProgram) {
+            System.out.println();
+            String userInput = scn.nextLine();
+            Duke.printHorizontalLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Goodbye! Shutting down now...");
+                exitProgram = true;
+            } else {
+                System.out.println(userInput);
+            }
+            Duke.printHorizontalLine();
+        }
+    }
+
+    private static void printHorizontalLine() {
+        String horizontalLine = "---------------------------------------------";
+        System.out.println(horizontalLine);
     }
 }
