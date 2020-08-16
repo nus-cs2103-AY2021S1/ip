0f93b19ebe3ef9ee3ecf138f904ba283d1033cec
Lye Yi Xian <lyeyx999@hotmail.com>
2020-08-16 19:46:07 +0800

    Created class Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+  private String description;
+  private boolean isDone;
+
+  public Task(String description) {
+    this.description = description;
+    this.isDone = false;
+  }
+
+  public String getStatusIcon() {
+    return isDone
+        ? "\u2713"
+        : "\u2718";
+  }
+
+  @Override
+  public String toString() {
+    return "[" + getStatusIcon() + "] " + this.description;
+  }
+}
