ff5ded327d3a96b99e7865528c0d112c18f2300d
chewypiano <gimikamil@gmail.com>
2020-08-16 17:55:31 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String div = "\n____________________________________________________________\n\n";
+        String logo = "                      █████████\n"
+                    +  "  ███████          ███        ███\n"
+                    +  "  █      █       ███             ███\n"
+                    +  "   █      █    ██                   ██\n"
+                    +  "    █     █   ██     ██      ██     ███          Hey! I'm Jimmy,\n"
+                    +  "     █   █   █      ████    ████      ██     your personal assistant!\n"
+                    +  "   █████████████                      ██\n"
+                    +  "   █            █         █           ██   What can I do for you today?\n"
+                    +  " ██             █   ██          ██    ██\n"
+                    +  "██   ███████████     ██        ██     ██\n"
+                    +  "█               █      ████████       ██\n"
+                    +  "██              █                    ██\n"
+                    +  " █   ███████████                   ██\n"
+                    +  " ██          ████                 █\n"
+                    +  "  ████████████   █████████████████";
+        
+        System.out.println(div + logo + div);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String msg = sc.nextLine();
+            if (msg.equals("bye")) {
+                System.out.println(div + "    Bye. Hope to see you again soon!" + div);
+                break;
+            }
+            System.out.println(div + "\t" + msg + div);
+        }
+        sc.close();
     }
 }
