78b15a7526baca68a8d7fa8cf1eaf8f1b665976f
samlsm <samlsm2803@gmail.com>
2020-08-16 18:35:18 +0800

    Edited test-ui files for testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -14,7 +14,7 @@ fi
 
 # compile the code into the bin folder, terminates if error occurred
 if ! (
-    find ../src/main/java -name "*.java" >sources.txt
+    find ../src/main/java -name "*.java" > sources.txt
     javac -cp ../src -Xlint:none -d ../bin @sources.txt
   )
 then
@@ -23,15 +23,16 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+#java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -Dfile.encoding=UTF-8 -classpath ../bin Duke < input.txt > ACTUAL.TXT
+
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
-#diff ACTUAL.TXT EXPECTED-UNIX.TXT
-diff ACTUAL.TXT EXPECTED.TXT
+diff ACTUAL.TXT EXPECTED-UNIX.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
