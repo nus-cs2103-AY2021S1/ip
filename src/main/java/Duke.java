5f5f3c8f86cd9e5230e62ee83c26c38780550fbc
murtubak <murtubakchickenss@gmail.com>
2020-08-16 22:25:54 +0800

    Add storage and list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,38 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    final static String GREETING_MESSAGE = "Buenos Dias! Soy Duke, como estas mi amigo?";
-    final static String TERMINATION = "bye";
-    final static String TERMINATION_MESSAGE = "Adios, amigos!";
+    private final static String GREETING_MESSAGE = "Buenos Dias! Soy Duke, como estas mi amigo?";
+    private final static String ACTION_LIST = "list";
+    private final static String TERMINATION = "bye";
+    private final static String TERMINATION_MESSAGE = "Adios, amigos!";
+    private static ArrayList<String> userInputsList = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println(GREETING_MESSAGE);
 
         Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-        if (userInput.equals(TERMINATION)) {
-            System.out.println(TERMINATION_MESSAGE);
-            sc.close();
-        } else {
-            System.out.println(userInput);
+        while (sc.hasNext()) {
+            String userInput = sc.nextLine();
+            switch (userInput) {
+                case ACTION_LIST:
+                    for (String s : userInputsList) {
+                        System.out.println(s);
+                    }
+                    break;
+                case TERMINATION:
+                    System.out.println(TERMINATION_MESSAGE);
+                    userInputsList.clear();
+                    break;
+                default:
+                    System.out.println("added: " + userInput);
+                    int position = userInputsList.size() + 1;
+                    userInputsList.add(position + ". " + userInput);
+            }
+            if (userInput.equals(TERMINATION)) {
+                sc.close();
+                break;
+            }
         }
     }
 }
