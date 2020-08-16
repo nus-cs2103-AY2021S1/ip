b6b0643e5ea214847050cd0abb6b44ba65f07c61
Jay <Nopenotj@gmail.com>
2020-08-16 15:48:35 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.LinkedTransferQueue;
 
 public class Duke {
-    static String divider = "    ____________________________________________________________\n";
+    static String divider = "____________________________________________________________\n";
+
     static void start() {
         String logo =
                                 " .----------------.  .----------------.  .----------------.  .----------------. \n" +
@@ -22,20 +25,44 @@ public class Duke {
                 logo +
                 "\n" +
                 divider +
-                "     Hola! I am dook\n" +
-                "     how i can help u?\n" + divider;
+                "Hola! I am dook\n" +
+                "how i can help u?\n" + divider;
         System.out.println(intro);
     }
     static void print(String str){
-        String intro = divider + "     " + str + "\n" + divider;
+        String intro = divider + str + "\n" + divider;
         System.out.println(intro);
+    }
+    static void addTask(String str, List<String> taskList) {
+        taskList.add(str);
+        print("added: " + str);
+    }
+    static void displayList(List<String> list) {
+        if(list.size() == 0) {
+            print("No task added yet!");
+        } else {
+            String result = "";
+            for(int i = 0; i < list.size(); i++) {
+                result += "" + (i + 1)+ ". " + list.get(i) + ( i + 1 == list.size() ? "" : "\n");
+            }
+            print(result);
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Initialise
         start();
+        List<String> tasks = new ArrayList<>();
+
         String input = sc.nextLine();
         while(!input.equals("bye")) {
-            print(input);
+            if (input.equals("list")) {
+                displayList(tasks);
+            } else {
+                addTask(input, tasks);
+            }
+
             input = sc.nextLine();
         }
         print("see u later alligator");
