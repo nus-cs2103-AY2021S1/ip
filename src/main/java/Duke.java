5d6c6a374068c02c8b5b1060e65af0db81dc4c04
chrystalquek <chrystalquek@yahoo.com.sg>
2020-08-16 17:24:54 +0800

    Add ability to display list of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static final String INDENT = "    ";
     private static final String LINE = "____________________________________________________________";
+    private static final ArrayList<String> tasks = new ArrayList<>();
 
-    private static void formatResponse(String[] response) {
+    private static void formatResponse(ArrayList<String> response) {
         System.out.println(INDENT + LINE);
         for (String resp: response) {
             System.out.println(INDENT + " " + resp);
@@ -13,21 +15,33 @@ public class Duke {
         System.out.println();
     }
 
-    private static void formatResponse(String response) {
-        System.out.println(INDENT + LINE);
-        System.out.println(INDENT + " " + response);
-        System.out.println(INDENT + LINE);
-        System.out.println();
+    private static void formatResponse(String ...response) {
+        ArrayList<String> lst = new ArrayList<>();
+        for (String resp: response) {
+            lst.add(resp);
+        }
+        formatResponse(lst);
+    }
+
+    private static void formatList(ArrayList<String> response) {
+        ArrayList<String> lst = new ArrayList<>();
+        for (int i = 0; i < response.size(); i++) {
+            lst.add((i + 1) + ". " + response.get(i));
+        }
+        formatResponse(lst);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        formatResponse(new String[]{"Hello! I'm Duke", "What can I do for you?"});
+        formatResponse("Hello! I'm Duke", "What can I do for you?");
         String display = "";
         while (!display.equals("bye")) {
             display = sc.nextLine();
-            if (!display.equals("bye")) {
-                formatResponse(display);
+            if (display.equals("list")) {
+                formatList(tasks);
+            } else if (!display.equals("bye")) {
+                formatResponse("added: " + display);
+                tasks.add(display);
             }
         }
        formatResponse("Bye. Hope to see you again soon!");
