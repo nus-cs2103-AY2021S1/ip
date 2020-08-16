8fcf9d8541a73dc500ee7f091f8022197b3d9623
khoodehui <dehui@Khoos-MacBook-Pro.local>
2020-08-16 16:53:59 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,14 +11,22 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         System.out.println("Hello from\n" + logo);
+
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> todoList = new ArrayList<>();
+
         while(sc.hasNextLine()) {
             String userInput = sc.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < todoList.size(); i++) {
+                    System.out.println((i + 1) + ". " + todoList.get(i));
+                }
             } else {
-                System.out.println(userInput);
+                todoList.add(userInput);
+                System.out.println("added: " + userInput);
             }
         }
     }
