1e1c4d629ad95e9bc914c1c2f33a42b275e97583
shaokiat <shaokiat@gmail.com>
2020-08-16 23:33:21 +0800

    Level-5 Exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,4 @@
-import main.java.Deadline;
-import main.java.Event;
-import main.java.Task;
-import main.java.Todo;
+import main.java.*;
 
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -23,46 +20,58 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
+
         Scanner scan = new Scanner(System.in);
 
         while (scan.hasNext()) {
             String input = scan.nextLine();
-            if (input.length() >= 4 && input.substring(0,4).equals("done")) {
+            try {
+                if (input.length() >= 4 && input.substring(0, 4).equals("done")) {
+                    if (input.length() == 4) {
+                        throw new DukeException("☹ OOPS!!! Please select a task to be done.");
+                    }
                     int index = Integer.parseInt(input.substring(5));
-                    Task done = taskList.get(index-1);
+                    Task done = taskList.get(index - 1);
                     done.markAsDone();
                     System.out.println("Nice! I've marked this task as done:");
                     System.out.println(done);
-            } else if (input.length() >= 4 && input.substring(0,4).equals("todo")) {
-                String tasking = input.substring(5);
-                Task todo = new Todo(tasking);
-                taskList.add(todo);
-                addedReply(todo);
-            } else if (input.length() >= 8 && input.substring(0,8).equals("deadline")) {
-                String[] split = input.substring(9).split("/by", 2);
-                Task deadline = new Deadline(split[0], split[1]);
-                taskList.add(deadline);
-                addedReply(deadline);
-            } else if (input.length() >= 5 && input.substring(0,5).equals("event")) {
-                String[] split = input.substring(6).split("/at", 2);
-                Task event = new Event(split[0], split[1]);
-                taskList.add(event);
-                addedReply(event);
-            }
-
-
-            else if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                return;
-            } else if (input.equals("list")) {
-                for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println((i + 1) + "." + taskList.get(i));
+                } else if (input.length() >= 4 && input.substring(0, 4).equals("todo")) {
+                    if (input.length() == 4) {
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    String tasking = input.substring(5);
+                    Task todo = new Todo(tasking);
+                    taskList.add(todo);
+                    addedReply(todo);
+                } else if (input.length() >= 8 && input.substring(0, 8).equals("deadline")) {
+                    if (input.length() == 8) {
+                        throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    String[] split = input.substring(9).split("/by", 2);
+                    Task deadline = new Deadline(split[0], split[1]);
+                    taskList.add(deadline);
+                    addedReply(deadline);
+                } else if (input.length() >= 5 && input.substring(0, 5).equals("event")) {
+                    if (input.length() == 5) {
+                        throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+                    }
+                    String[] split = input.substring(6).split("/at", 2);
+                    Task event = new Event(split[0], split[1]);
+                    taskList.add(event);
+                    addedReply(event);
+                } else if (input.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    return;
+                } else if (input.equals("list")) {
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println((i + 1) + "." + taskList.get(i));
+                    }
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-            } else {
-                taskList.add(new Task(input));
-                System.out.println("Added: " + input);
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
             }
         }
     }
