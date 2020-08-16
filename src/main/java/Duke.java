a44740bed2af481ecaa1082b26f35177a5ba2b9e
Noorul Azlina <>
2020-08-16 13:04:29 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,55 @@
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-
-    public static void greet(){
-
+    private static List<String> todos = new ArrayList<>();
+    public static void scan(FileInputStream inputStream){
+        try {
+            int size = inputStream.available();
+            StringBuffer text = new StringBuffer();
+            for(int i = 0; i < size; i++){
+                char c = (char)inputStream.read();
+                text.append(c);
+            }
+            String s = " ";
+            for(int i = 0 ; i < size; i++){
+                if(text.toString().charAt(i) == 10){
+                    todos.add(s.substring(1));
+                    s = " ";
+                }else {
+                    s = s + text.toString().charAt(i);
+                }
+            }
+            todos.add(s.substring(1));
+        }catch (Exception e){
+            System.out.println();
+        }
+    }
+    public static void output(){
+        System.out.println("  ____________________________________________________________\n" + "  Hello! I'm Duke\n" + "  What can I do for you?\n" +
+                "  ____________________________________________________________");
+        for(String string : todos){
+            System.out.println("\n" + string + "\n  ____________________________________________________________");
+            if(string.equals("bye")){
+                System.out.println("  Bye. Hope to see you again soon!\n" + "  ____________________________________________________________");
+                break;
+            }
+            if(string.equals("list")){
+                Task.listing();
+            }else if(string.substring(0,4).equals("done")){
+                int ID = Integer.parseInt(string.substring(5));
+                Task.tasks.get(ID - 1).setDone();
+                Task.tasks.get(ID - 1).donePrint();
+            }else {
+                Task t = new Task(string);
+            }
+        }
     }
     public static void main(String[] args) {
-        System.out.println("  ____________________________________________________________\n" + "  Hello! I'm Duke\n" + "  What can I do for you?\n"
+        /*System.out.println("  ____________________________________________________________\n" + "  Hello! I'm Duke\n" + "  What can I do for you?\n"
                          + "  ____________________________________________________________");
         try {
             String file = args[0];
@@ -48,13 +89,21 @@ public class Duke {
             }
         }catch (Exception e){
             System.out.println(e);
-        }
+        }*/
        /* String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
+        try {
+            String file = args[0];
+            FileInputStream inputStream = new FileInputStream(file);
+            scan(inputStream);
+            output();
+        }catch (Exception e){
+
+        }
 
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task {
+    static List<Task> tasks = new ArrayList<>();
+    private boolean done;
+    private String name;
+    private int ID;
+    Task(String name){
+        this.done = false;
+        this.name = name;
+        this.ID = tasks.size() + 1;
+        tasks.add(this);
+        System.out.println("  " + "added: " + this.name + "\n" +
+                "  ____________________________________________________________");
+    }
+    public void setDone(){
+        this.done = true;
+        tasks.get(this.ID - 1).done = true;
+    }
+    public void donePrint(){
+        System.out.println("   Nice! I've marked this task as done: ");
+        System.out.println("   " + this.toString());
+        System.out.println("  ____________________________________________________________");
+    }
+    public static void listing(){
+        System.out.println("   Here are the tasks in your list: ");
+        for(Task task : tasks){
+            System.out.println("  " + task.ID + "." + task);
+        }
+        System.out.println("\n" + "  ____________________________________________________________");
+    }
+    public String toString(){
+        if(this.done) {
+            return "[✓] " + this.name;
+        }
+        return "[✗] " + this.name;
+    }
+}
