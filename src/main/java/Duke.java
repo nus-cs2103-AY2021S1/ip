efc9f87aa8ec78e84b5e5ed2229693667ba29950
esmanda3w <esmanda3w@gmail.com>
2020-08-16 22:58:16 +0800

    added storing user text and displaying it in a list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class Duke {
     public static void replyUser() {
         String exitMessage = "BYEEE!! SEE YOU AGAIN!!! >O<";
+        String[] list = new String[100];
+        int i = 0;
 
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNext()) {
-            String input = sc.next();
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
 
             if (input.toLowerCase().equals("bye")) {
                 printReply(exitMessage);
                 sc.close();
                 break;
+            } else if (input.toLowerCase().equals("list")) {
+                printReply(makeList(list, i));
             } else {
-                printReply(input + " =D");
+                list[i] = input;
+                i++;
+                printReply("added: \"" + input + "\" to list! =D");
             }
         }
     }
@@ -23,6 +29,20 @@ public class Duke {
         System.out.println("--------------------------------------");
         System.out.println(reply);
         System.out.println("--------------------------------------");
+    }
+
+    public static String makeList(String[] list, int end) {
+        String numberedList = "";
+        for (int i = 0; i < end; i++) {
+            if (i == 0) {
+                numberedList = "1. " + list[i];
+            } else {
+                int num = i + 1;
+                numberedList += "\n" + num + ". " + list[i];
+            }
+        }
+
+        return numberedList;
     }
 
     public static void main(String[] args) {
