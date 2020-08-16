4bcb8d7ddd8d9d9b5c1e9a355c98f742e1440f6a
maguireong <maguire2622@yahoo.com.sg>
2020-08-16 18:20:49 +0800

    Add list functionality and "added" wording

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -6,18 +7,25 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
         Scanner sc = new Scanner(System.in);
-
+        ArrayList<String> books = new ArrayList<>();
         while (sc.hasNext()) {
 
             String echo = sc.nextLine();
             String[] s = echo.split("\\s");
             String first = s[0];
 
-            System.out.println(first);
-            if (first.equals("bye")) {
+            if (first.equals("list")) {
+                for (int i = 0; i < books.size(); i++) {
+                    System.out.println(i+1 + ". " + books.get(i));
+                }
+            } else if (first.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else {
+                books.add(echo);
+                System.out.println("added: " + echo);
             }
+
         }
     }
 }
