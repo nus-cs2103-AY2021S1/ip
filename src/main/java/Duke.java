8dad41920043ea57d938de35d85b19966e0f3b20
Andrea <andreaxtan611@gmail.com>
2020-08-16 18:49:23 +0800

    Initial greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //Initial greetings
+        System.out.println("_______________________________________________________________");
+        System.out.println("Greetings from me, Dave! \n"  + "How can I help you? ^_^");
+        System.out.println("_______________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String userinput = scanner.next(); //may be string, int, etc
+
+
+
+
+
+
+
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
     }
 }
