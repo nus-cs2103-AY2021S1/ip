f13f39d495275a08972fabee06afbab632d6c285
ryanlimjr <ryanlimjr@gmail.com>
2020-08-16 09:48:19 +0800

    -

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,5 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
     }
 }
