ff5075f10d0d36e2c2740ceca59a1b686ec842f8
Galvin <galvinleow@gmail.com>
2020-08-16 11:34:21 +0800

    Level 3 - Mark as Done
    Created class for task: 
    1. Added feature for done jobs
    
    Refactor print statement for easy of use

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,22 +12,31 @@ public class Duke {
     System.out.println(logo + "\nHello ! I'm Duke\nWhat can I do for you?\n");
 
     // Arraylist used to ensure scalability
-    ArrayList<String> dataArrayList = new ArrayList<>();
+    ArrayList<Task> dataArrayList = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
     String input = "";
     while (!input.equals("bye")) {
-      input = scanner.nextLine();
+      input = scanner.next();
       // Switch case use to prepare for future requirements
       switch (input) {
         case "list":
-          for (int i = 0; i < dataArrayList.size(); i++) {
-            String listing = String.format("%d. %s", i + 1, dataArrayList.get(i));
-            System.out.println(listing);
+          int count = 1;
+          for (Task task : dataArrayList) {
+            System.out.printf("%d.[%s] %s\n", count, task.getStatusIcon(), task.description);
+            count++;
           }
+          break;
+        case "done":
+          int change = scanner.nextInt();
+          Task task = dataArrayList.get(change);
+          task.markAsDone();
+          System.out.println("Nice! I've marked this task as done:");
+          System.out.printf("[%s] %s%n", task.getStatusIcon(), task.description);
           break;
         default:
           if (!input.equals("bye")) {
-            dataArrayList.add(input);
+            Task addTask = new Task(input);
+            dataArrayList.add(addTask);
             System.out.println("added: " + input);
           }
           break;
