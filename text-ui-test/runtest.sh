081049d453d35dbc1a4689fa2a3f31554f3a857b
dianneloh9 <dianneloh9@gmail.com>
2020-08-16 12:17:50 +0800

    Add input and expected files for testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -14,7 +14,7 @@ fi
 
 # compile the code into the bin folder, terminates if error occurred
 
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Duke.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Duke.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
