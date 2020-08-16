ba42e70119247552763a50878184f08b1ba98389
jianhandev <jianhanli.dev@gmail.com>
2020-08-16 23:23:03 +0800

    Add Task class
    Objects of the Task class stores a description of the task and whether it is completed.

---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    String description;
+    boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public Task markDone() {
+        return new Task(this.description, true);
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + this.description;
+    }
+}
