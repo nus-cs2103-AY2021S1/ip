e9a823d1c5f245d00615d898aa53fc4290cf974a
Sean Tay <seantaysl@gmail.com>
2020-08-17 06:26:42 +0800

    Add Level 1 (Greet, Echo, Exit)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String border = "____________________________________________________________\n";
+        System.out.println(border + "Hello! I'm Duke\n" + "What can I do for you?\n" + border);
+
+        Scanner scan = new Scanner(System.in);
+
+        while(scan.hasNext()) {
+            String input = scan.next();
+            if(input.equals("bye")) {
+                System.out.println(border + "Bye. Hope to see you again soon!\n" + border);
+                return;
+            } else {
+                System.out.println(border + input + "\n" + border);
+            }
+        }
     }
 }
