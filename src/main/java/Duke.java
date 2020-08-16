791051cb04d553b2b5a609e2dd205bec3da9a275
keanecjy <e0425601@u.nus.edu>
2020-08-16 23:38:57 +0800

    Duke skeleton implemented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String separator = "   -------------------------------------------------";
+        while (sc.hasNextLine()) {
+            String next = sc.nextLine();
+            if (next.equals("bye")) {
+                break;
+            }
+            System.out.println(separator);
+            System.out.println("   " + next);
+            System.out.println(separator);
+            System.out.println();
+        }
+        sc.close();
+        System.out.println(separator);
+        System.out.println("   Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
