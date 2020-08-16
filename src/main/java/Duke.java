ccabac0647f4ea5fde242fb66c0bcfa2cf2fabba
yongmingyang <yongmingyang1998@gmail.com>
2020-08-16 18:35:24 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        String intro = "____________________________________________________________ \n" +
+                        "Hello! I'm Duke \n" + "What can I do for you? \n" +
+                        "____________________________________________________________";
+        System.out.println(intro);
+        Scanner input =  new Scanner(System.in);
+        String underscore = "____________________________________________________________ \n";
+        String line = "";
+        while(!line.equals("bye")) {
+            line = input.nextLine();
+            if(line.equals("bye")) {
+                System.out.println(underscore + " Bye. Hope to see you again soon!" + "\n" + underscore);
+            } else {
+                System.out.println(underscore + " " + line + "\n" + underscore);
+            }
+        }
+        input.close();
     }
 }
