9efcbdb6db5aec05aff92cfeaf7b075b0d645855
erisjacey <eris_jacey@Hotmail.com>
2020-08-16 10:42:09 +0800

    ToDo.java: Clean up toString method

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -2,9 +2,4 @@ public class ToDo extends Task {
     public ToDo(String description) {
         super(description, TaskType.TODO);
     }
-
-    @Override
-    public String toString() {
-        return String.format("%s", super.toString());
-    }
 }
