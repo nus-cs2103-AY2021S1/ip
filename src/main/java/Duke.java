80da2131690ec41a752f9b38a03bd27941244ddb
lyeyixian <lyeyx999@hotmail.com>
2020-08-16 18:57:29 +0800

    Initialized the project, did some simple output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+  public static void main(String[] args) {
+    String divider = "---------------------------------------------------------------------------------------------";
+    String logo = " ____        _        \n"
+        + "|  _ \\ _   _| | _____ \n"
+        + "| | | | | | | |/ / _ \\\n"
+        + "| |_| | |_| |   <  __/\n"
+        + "|____/ \\__,_|_|\\_\\___|\n";
+    Scanner sc = new Scanner(System.in);
+    String input = "";
+
+    System.out.println("\n" + logo);
+    System.out.println("\t" + divider);
+    System.out.println("\t" + "Hello! I'm Duke\n\tWhat can I do for you?");
+    System.out.println("\t" + divider);
+
+    input = sc.next();
+
+    System.out.println("\t" + divider);
+    System.out.println("\t" + input);
+    System.out.println("\t" + divider);
+  }
 }
