422bc781cbd7f7ad089b6e41aba5ccb38c4fbe57
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-08-16 20:51:34 +0800

    Add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                scanner.close();
+                printMessage("Bye! See you next time :)");
+                System.exit(0);
+            }
+            printMessage(input);
+
+        }
+
+    }
+
+    private static void printMessage(String message) {
+        System.out.println("___________________________");
+        System.out.println(message);
+        System.out.println("___________________________");
     }
 }
