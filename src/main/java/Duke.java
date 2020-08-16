64fd472bbea0a0278af6bdb57b9396ef50cb2008
Preston Toh <prestontoh@gmail.com>
2020-08-16 22:12:49 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,18 +4,27 @@ public class Duke {
     public static void main(String[] args) {
         Scanner inputs = new Scanner(System.in);
         boolean run = true;
+        int noOfTasks = 0;
+        String[] tasks = new String[100];
 
         System.out.println("Hello~, I'm Duke!\n"
         + "What can I do for you?");
 
         while (run == true) {
-            String next = inputs.next();
+            String next = inputs.nextLine();
             System.out.println("____________________________________________________________");
             if (next.equals("bye")) {
                 System.out.println("Goodbye~ Hope to see you again soon!");
                 run = false;
+            } else if (next.equals("list")) {
+                for (int i = 0; i < noOfTasks; i++) {
+                    int taskNo = i + 1;
+                    System.out.println(taskNo + ". " + tasks[i]);
+                }
             } else {
-                System.out.println(next);
+                tasks[noOfTasks] = next;
+                noOfTasks += 1;
+                System.out.println("Added: " + next);
             }
             System.out.println("____________________________________________________________");
         }
