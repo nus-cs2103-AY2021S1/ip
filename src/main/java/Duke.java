da96527b53f456eeccc7a1aea68950df8fa287e9
Licheng-Wu <wlc.licheng@gmail.com>
2020-08-16 22:36:55 +0800

    Added feature where Duke does not terminate until "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,11 +8,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        Scanner sc = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter command:");
-
-        String command = sc.nextLine();  // Read user input
-        Echo.echoMessage(command); // Output user input
+        Echo.initialise();
     }
 }
--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Echo {
+    private static boolean terminate = true;
+    public static void initialise() {
+        while (Echo.terminate) {
+            Scanner sc = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter command:");
+
+            String command = sc.nextLine();  // Read user input
+            Echo.echoMessage(command); // Output user input
+        }
+    }
+
     public static void echoMessage(String message) {
-        if (message != "bye") {
+        if (!message.equals("bye")) {
             System.out.println(message);
         } else {
+            Echo.terminate = false;
             System.out.println("Bye. Hope to see you again soon!");
         }
     }
