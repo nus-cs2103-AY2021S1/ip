5aeace8d0507d0ed9ae7f2a6622b206e2209a260
PhongTran98 <giaphong8127@gmail.com>
2020-08-16 16:58:58 +0800

    Add Level-3
    Add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,28 +21,29 @@ public class Duke {
     }
     private static void handleCommand(String cmd) {
         startMessage();
-        switch (cmd) {
+        switch (cmd.split(" ", 2)[0]) {
             case "bye":
                 System.out.println("See you again!");
                 break;
             case "list":
-                handleList();
+                Task.printTasks();
+                break;
+            case "done":
+                try {
+                    int index = Integer.parseInt(cmd.split(" ", 2)[1]);
+                    Task.iDone(index);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(Task.getTask(index));
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getLocalizedMessage());
+                }
                 break;
             default:
-                handleDefault(cmd);
+                Task.addTask(cmd);
+                System.out.println("added: " + cmd);
         }
         endMessage();
     }
-
-    private static void handleList() {
-        Task.printTasks();
-    }
-
-    private static void handleDefault(String cmd) {
-        Task.addTask(cmd);
-        System.out.println("added: " + cmd);
-    }
-
     public static void main(String[] args) {
         greet();
         Scanner sc = new Scanner(System.in);
@@ -56,15 +57,42 @@ public class Duke {
 }
 
 class Task {
-    private static List<String> database = new ArrayList<>();
+    private static List<Task> database = new ArrayList<>();
+    private String description;
+    private boolean isDone = false;
+
+    private Task(String cmd) {
+        this.description = cmd;
+    }
 
     public static void addTask(String cmd) {
-        database.add(cmd);
+        database.add(new Task(cmd));
+    }
+
+    public static String getTask(int index) {
+        if (index <= 0 || index > database.size()) {
+            throw new IllegalArgumentException("Invalid argument for the LIST command.");
+        }
+        return database.get(index - 1).toString();
+    }
+
+    private void markAsDone() {isDone = true;}
+
+    public static void iDone(int index) {
+        if (index <= 0 || index > database.size()) {
+            throw new IllegalArgumentException("Invalid argument for the LIST command.");
+        }
+        database.get(index - 1).markAsDone();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", (isDone ? "\u2713" : "\u2718"), description);
     }
 
     public static void printTasks() {
         for (int i = 0; i < database.size(); i++) {
-            System.out.println((i + 1) + ". " + database.get(i));
+            System.out.println((i + 1) + "." + database.get(i));
         }
     }
 }
