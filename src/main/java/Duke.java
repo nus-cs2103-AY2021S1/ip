ab3a1b87168435e1bea463c47121d6c5288596b2
chloelee767 <echloelee@gmail.com>
2020-08-17 00:17:44 +0800

    Duke class: refactor route function for less repetition
    There is a lot of repeated logic for parsing and validating input in
    the route function.
    
    Let's:
    - extract the common logic into helper functions
    - create a new type of exception (DukeRoutingException)
    
    Exceptions are thrown by the helper functions to make it easy to break
    out of the control flow at many different points within the route function.
    
    The exceptions are caught within the route function, rather than
    becoming a checked exception for the route function, so as to keep the
    run function clean.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String INDENT = "  ";
-    private final String SEPARATOR = INDENT + "_".repeat(68);
-
     public static void main(String[] args) {
         new Duke().run();
     }
@@ -36,11 +33,14 @@ public class Duke {
     }
 
     private void say(List<String> strings) {
-        System.out.println(SEPARATOR);
+        final String indent = "  ";
+        final String separator = indent + "_".repeat(68);
+
+        System.out.println(separator);
         for (String s : strings) {
-            System.out.println(INDENT + s);
+            System.out.println(indent + s);
         }
-        System.out.println(SEPARATOR);
+        System.out.println(separator);
         System.out.println();
     }
 
@@ -48,66 +48,74 @@ public class Duke {
         say(Arrays.asList(string));
     }
 
-    // TODO: reduce repetition here
     private Function route(String input) {
         input = input.strip();
         String[] split = input.split("\\s+", 2); // guranteed to contain at least ""
         String command = split[0];
         String args = split.length == 2 ? split[1] : "";
 
-        if (command.equals("bye")) {
-            if (args.isEmpty()) {
+        try {
+            if (command.equals("bye")) {
+                ensureNoArgs(args, command);
                 return new ByeFunction();
-            } else {
-                return new ErrorFunction(Arrays.asList("Didn't understand " + input + ".", "Did you mean bye?"));
-            }
-        } else if (command.equals("list")) {
-            if (args.isEmpty()) {
+            } else if (command.equals("list")) {
+                ensureNoArgs(args, command);
                 return new ListFunction();
+            } else if (command.equals("done")) {
+                int index = parseTaskNumber(args, "you have completed", "done 1") - 1;
+                return new DoneFunction(index);
+            } else if (command.equals("delete")) {
+                int index = parseTaskNumber(args, "you want to remove", "delete 1") - 1;
+                return new DeleteFunction(index);
+            } else if (command.equals("todo")) {
+                if (args.isEmpty()) {
+                    return new ErrorFunction("Couldn't add todo! The description of a todo cannot be empty.");
+                }
+                return new AddFunction(new Task(args));
+            } else if (command.equals("deadline")) {
+                String[] argsSplit = parseDeadlineEventArgs(args, "/by", command, "deadline <description> /by <date>");
+                return new AddFunction(new Deadline(argsSplit[0], argsSplit[1]));
+            } else if (command.equals("event")) {
+                String[] argsSplit = parseDeadlineEventArgs(args, "/at", command, "event <description> /at <date>");
+                return new AddFunction(new Event(argsSplit[0], argsSplit[1]));
+            } else if (input.isBlank()) {
+                return new ErrorFunction("You need to tell me what you want me to do!");
             } else {
-                return new ErrorFunction(Arrays.asList("Didn't understand " + input + ".", "Did you mean list?"));
+                return new ErrorFunction("Sorry, I don't understand that!");
             }
-        } else if (command.equals("done")) {
-            int index;
-            try {
-                index = Integer.valueOf(args) - 1;
-            } catch (NumberFormatException e) {
-                return new ErrorFunction(Arrays.asList("You need to tell me the number of the task you have completed.",
-                                                       "Eg. done 1"));
-            }
-            return new DoneFunction(index);
-        } else if (command.equals("delete")) {
-            int index;
-            try {
-                index = Integer.valueOf(args) - 1;
-            } catch (NumberFormatException e) {
-                return new ErrorFunction(Arrays.asList("You need to tell me the number of the task you want to remove.",
-                                                       "Eg. delete 1"));
-            }
-            return new DeleteFunction(index);
-        } else if (command.equals("todo")) {
-            if (args.isEmpty()) {
-                return new ErrorFunction("Couldn't add todo! The description of a todo cannot be empty.");
-            }
-            return new AddFunction(new Task(args));
-        } else if (command.equals("deadline")) {
-            String[] argsSplit = args.split("\\s+/by\\s+", 2);
-            if (argsSplit.length != 2 || argsSplit[0].isBlank() || argsSplit[1].isBlank()) {
-                return new ErrorFunction(Arrays.asList("Couldn't add deadline! To add a deadline, talk to me using the",
-                                                       "format: deadline <description> /by <date>"));
-            }
-            return new AddFunction(new Deadline(argsSplit[0], argsSplit[1]));
-        } else if (command.equals("event")) {
-            String[] argsSplit = args.split("\\s+/at\\s+", 2);
-            if (argsSplit.length != 2 || argsSplit[0].isBlank() || argsSplit[1].isBlank()) {
-                return new ErrorFunction(Arrays.asList("Couldn't add event! To add an event, talk to me using the format:",
-                                                       "event <description> /at <date>"));
-            }
-            return new AddFunction(new Event(argsSplit[0], argsSplit[1]));
-        } else if (input.isBlank()) {
-            return new ErrorFunction("Please tell me what you want me to do!");
-        } else {
-            return new ErrorFunction("Sorry, I don't understand that!");
+        } catch (DukeRoutingException e) {
+            return new ErrorFunction(e.getDukeMessage());
         }
     }
+
+    // helpers for route function
+
+    private void ensureNoArgs(String args, String commandName) throws DukeRoutingException {
+        if (!args.isEmpty()) {
+            throw new DukeRoutingException(String.format("I don't understand that. Did you mean %s?", commandName));
+        }
+    }
+
+    private int parseTaskNumber(String args, String taskDescription, String example) throws DukeRoutingException {
+        try {
+            return Integer.valueOf(args);
+        } catch (NumberFormatException e) {
+            throw new DukeRoutingException(Arrays.asList(
+                        String.format("You need to tell me the number of the task %s.", taskDescription),
+                        "Eg. " + example));
+        }
+    }
+
+    private String[] parseDeadlineEventArgs(String args, String splitAround, String taskType, String example)
+        throws DukeRoutingException {
+        String[] argsSplit = args.split("\\s+" + splitAround + "\\s+", 2);
+        if (argsSplit.length != 2 || argsSplit[0].isBlank() || argsSplit[1].isBlank()) {
+            String n = taskType.matches("^a|e|i|o|u") ? "n" : ""; // starts with vowel
+            throw new DukeRoutingException(Arrays.asList(
+                        String.format("Couldn't add %s! To add a%s %s, talk to me using the", taskType, n, taskType),
+                        "format: " + example));
+        }
+        return argsSplit;
+    }
+
 }
---
+++ src/main/java/DukeRoutingException.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This exception is handled within the route function in the Duke class.
+ */
+public class DukeRoutingException extends Exception {
+    private List<String> dukeMessage;
+
+    public DukeRoutingException(List<String> message) {
+        super(String.join("\n", message));
+        this.dukeMessage = message;
+    }
+
+    public DukeRoutingException(String message) {
+        this(Arrays.asList(message));
+    }
+
+    public List<String> getDukeMessage() {
+        return dukeMessage;
+    }
+}
