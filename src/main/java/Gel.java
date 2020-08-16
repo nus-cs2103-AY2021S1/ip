e6714493fd2ae8a37a3c142f2626f6bcc8382b53
Elgoh <elgoh3@gmail.com>
2020-08-17 03:53:17 +0800

    Added the ability to mark tasks as done.

--- src/main/java/Gel.java
+++ src/main/java/Gel.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Gel {
     public static void keepingList() {
         // initialise list and scanner
-        List<String> listOfText = new ArrayList<>();
+        List<Task> listOfText = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("    Hello! I'm Gel\n    What can I do for you?\n");
@@ -16,13 +16,24 @@ public class Gel {
                 System.out.println("\n    Bye. Hope to see you again soon!\n");
                 break;
             } else if (line.equals("list")){
-                System.out.println();
+                System.out.println("\n    Here are the task(s) in your list:");
                 for (int i = 1; i <= listOfText.size(); i++) {
-                    System.out.println("    "+ i + ". " + listOfText.get(i - 1));
+                    Task task = listOfText.get(i - 1);
+                    System.out.println("    "+ i + "." + task.getStatusIcon()
+                            + " " + task.getDescription());
                 }
                 System.out.println();
+            } else if (line.length() > 4 && line.substring(0, 4).equals("done")) {
+                int index = Integer.parseInt(line.substring(5)) - 1;
+                Task taskToBeDone = listOfText.remove(index);
+                taskToBeDone.markAsDone();
+                listOfText.add(index, taskToBeDone);
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("      " + taskToBeDone.getStatusIcon() + " "
+                        + taskToBeDone.getDescription());
+                System.out.println();
             } else {
-                listOfText.add(line);
+                listOfText.add(new Task(line));
                 System.out.println("\n    added: " + line + "\n");
             }
         }
