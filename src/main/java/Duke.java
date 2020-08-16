9e71ff9792397ee93655a397f3e8f445410d0d3d
ChooJiaXin <e0426255@u.nus.edu>
2020-08-16 16:11:30 +0800

    Implement a skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String chatbot = "Bob: ";
+        String skipLine = "\n";
+        String user = skipLine + "You: ";
+
+
+        System.out.println(chatbot + "Hey there! I'm Bob" + skipLine + "What can I do for you today?");
+        System.out.println(user);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String echoUser = sc.nextLine();
+
+            boolean exit = echoUser.equals("bye");
+
+            if (exit) {
+                break;
+            }
+
+            System.out.println(skipLine + chatbot + echoUser);
+            System.out.println(user);
+
+        }
+
+        sc.close();
+        System.out.println(skipLine + chatbot + "Goodbye! Have a nice day :D");
+
     }
 }
