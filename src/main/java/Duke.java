ef0172cf67cba250c937dd318769ec4497838d54
duynguyen24501 <duynguyen24501@gmail.com>
2020-08-16 13:44:06 +0800

    Update Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,7 +17,7 @@ public class Duke {
         System.out.println("    ______________________________________");
 
         Scanner sc = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         while (true) {
             String userInput = sc.nextLine();
@@ -26,17 +26,29 @@ public class Duke {
                 System.out.println("        Bye. Hope to see you again soon!");
                 System.out.println("    ______________________________________");
                 break;
+
             } else if (userInput.equals("list")) {
                 System.out.println("    ______________________________________");
+                System.out.println("        Here are the tasks in your list:");
                 for(int i=0; i<list.size(); i++) {
-                    System.out.println("        " + (i+1) + ". " + list.get(i));
+                    System.out.println("        " + (i+1) + "." + list.get(i));
                 }
                 System.out.println("    ______________________________________");
+
+            }  else if (userInput.startsWith("done")) {
+                int spacePosition = userInput.indexOf(" ");
+                int taskPosition = Integer.parseInt(userInput.substring(spacePosition+1));
+                list.get(taskPosition-1).markAsDone();
+                System.out.println("    ______________________________________");
+                System.out.println("        Nice! I've marked this task as done:");
+                System.out.println("        " + list.get(taskPosition-1));
+                System.out.println("    ______________________________________");
+
             } else {
                 System.out.println("    ______________________________________");
                 System.out.println("        added: " + userInput);
                 System.out.println("    ______________________________________");
-                list.add(userInput);
+                list.add(new Task(userInput));
             }
         }
     }
