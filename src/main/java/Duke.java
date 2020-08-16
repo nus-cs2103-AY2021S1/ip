a2853a0be42bff892e8534e17c45ab1acadad490
aizatazhar <aizat.azharr@gmail.com>
2020-08-16 13:46:54 +0800

    Refactor code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
@@ -19,56 +20,92 @@ public class Duke {
     private static void acceptCommand(Scanner sc) {
         String command = sc.nextLine();
 
-        while (!command.equals("bye")){
-            if (command.equals("list")) {
-                printResponse(listTasks());
+        while (true) {
+            if (command.equals("bye")) {
+                handleBye();
+                break;
+            } else if (command.equals("list")) {
+                handleList();
             } else if (command.startsWith("todo")) {
-                String description = command.substring(command.indexOf(' ') + 1);
-                Task task = new ToDo(description);
-                tasks.add(task);
-                String response = String.format(
-                        "I've added this task:\n  %s \nNow you have %s tasks in the list.",
-                        task, tasks.size()
-                );
-                printResponse(response);
+                handleToDo(command);
             } else if (command.startsWith("deadline")) {
-                String[] input = command.substring(command.indexOf(' ') + 1).split(" /by ");
-                String description = input[0];
-                String by = input[1];
-                Task task = new Deadline(description, by);
-                tasks.add(task);
-                String response = String.format(
-                        "I've added this task:\n  %s \nNow you have %s tasks in the list.",
-                        task, tasks.size()
-                );
-                printResponse(response);
+                handleDeadline(command);
             } else if (command.startsWith("event")) {
-                String[] input = command.substring(command.indexOf(' ') + 1).split(" /at ");
-                String description = input[0];
-                String at = input[1];
-                Task task = new Event(description, at);
-                tasks.add(task);
-                String response = String.format(
-                        "I've added this task:\n  %s \nNow you have %s tasks in the list.",
-                        task, tasks.size()
-                );
-                printResponse(response);
+                handleEvent(command);
             } else if (command.matches("done \\d+")) {
-                int index = Integer.parseInt(command.split(" ")[1]);
-                if (index > tasks.size()) {
-                    printResponse("No such task!");
-                } else {
-                    Task task = tasks.get(index-1);
-                    task.completeTask();
-                    String response = "Nice! I've marked this task as done:\n  " + task.toString();
-                    printResponse(response);
-                }
+                handleDone(command);
+            } else {
+                handleUnknown();
             }
-
             command = sc.nextLine();
         }
+    }
 
+    private static void handleBye() {
         printResponse("Bye. Hope to see you again soon!");
+    }
+
+    private static void handleList() {
+        printResponse(listTasks());
+    }
+
+    private static void handleToDo(String command) {
+        String[] split = command.split(" ");
+        if (split.length == 1) {
+            printResponse("OOPS!!! The description of a todo cannot be empty.");
+        } else {
+            String[] descriptionArray = Arrays.copyOfRange(split, 1, split.length);
+            String description = String.join(" ", descriptionArray);
+            Task task = new ToDo(description);
+            tasks.add(task);
+            String response = String.format(
+                    "I've added this task:\n  %s \nNow you have %s tasks in the list.",
+                    task, tasks.size()
+            );
+            printResponse(response);
+        }
+    }
+
+    private static void handleDeadline(String command) {
+        String[] input = command.substring(command.indexOf(' ') + 1).split(" /by ");
+        String description = input[0];
+        String by = input[1];
+        Task task = new Deadline(description, by);
+        tasks.add(task);
+        String response = String.format(
+                "I've added this task:\n  %s \nNow you have %s tasks in the list.",
+                task, tasks.size()
+        );
+        printResponse(response);
+    }
+
+    private static void handleEvent(String command) {
+        String[] input = command.substring(command.indexOf(' ') + 1).split(" /at ");
+        String description = input[0];
+        String at = input[1];
+        Task task = new Event(description, at);
+        tasks.add(task);
+        String response = String.format(
+                "I've added this task:\n  %s \nNow you have %s tasks in the list.",
+                task, tasks.size()
+        );
+        printResponse(response);
+    }
+
+    private static void handleDone(String command) {
+        int index = Integer.parseInt(command.split(" ")[1]);
+        if (index > tasks.size()) {
+            printResponse("No such task!");
+        } else {
+            Task task = tasks.get(index-1);
+            task.completeTask();
+            String response = "Nice! I've marked this task as done:\n  " + task.toString();
+            printResponse(response);
+        }
+    }
+
+    private static void handleUnknown() {
+        printResponse("OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 
     // Wrapper method for printing with horizontal line borders and 1 tab indent
