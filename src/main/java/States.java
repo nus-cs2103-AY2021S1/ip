40f6c493b61c6fe6761e139ca738e0366a1be397
Junhua <jhsibeizai@gmail.com>
2020-08-16 17:53:20 +0800

    Refactored States

--- src/main/java/States.java
+++ src/main/java/States.java
@@ -1,26 +1,18 @@
-import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public enum States{
 
     //The main State of tasks
-    TODO(Todo::new),
-    EVENT(Event::new),
-    DEADLINE(Deadline::new);
+    TODO(Todo::parseCommand),
+    EVENT(Event::parseCommand),
+    DEADLINE(Deadline::parseCommand);
 
     //Create a variable to store the Function / Bi Function
     protected Function<String, Task> taskSupplier = null;
-    protected BiFunction<String, String, Task> taskSupplier1 = null;
 
     //Constructor for the function
     States(Function<String, Task> taskSupplier){
         this.taskSupplier = taskSupplier;
-    }
-
-
-    //Constructor if it is a Bi Function
-    States(BiFunction<String, String, Task> taskSupplier){
-        this.taskSupplier1 = taskSupplier;
     }
 
     //Get the task given description
@@ -28,9 +20,5 @@ public enum States{
         return taskSupplier.apply(description);
     }
 
-    //Create the task given desciption and date
-    public Task createTask(String description, String date){
-        return taskSupplier1.apply(description, date);
-    }
 
 }
