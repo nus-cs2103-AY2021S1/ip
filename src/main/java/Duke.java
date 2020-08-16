5bd8fbf60c3495c9d8ec9bb62cb2a69d967616ab
Galvin <galvinleow@gmail.com>
2020-08-16 13:54:16 +0800

    Level 4 - ToDo, Event, Deadline
    Added 3 new subclass
    Each have it own overridden method
    Add if-else case to handle scanner for each case

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,7 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|\n";
     System.out.println(logo + "\nHello ! I'm Duke\nWhat can I do for you?\n");
 
-    // Arraylist used to ensure scalability (Edited out to fix level-4 requirement)
+    // Arraylist used to ensure scalability (Edited out to fit level-4 requirement)
     //    ArrayList<Task> dataArrayList = new ArrayList<>();
     Task[] tasksArr = new Task[100];
     int taskTracker = 0;
@@ -22,7 +22,7 @@ public class Duke {
       switch (input) {
         case "list":
           /*
-          // Implementation code using Arraylist (Edited out to fix level-4 requirement)
+          // Implementation code using Arraylist (Edited out to fit level-4 requirement)
                    int count = 1;
                    for (Task task : dataArrayList) {
                      System.out.printf("%d.[%s] %s\n", count, task.getStatusIcon(), task);
@@ -40,7 +40,7 @@ public class Duke {
         case "done":
           int change = scanner.nextInt() - 1;
           /*
-          // Implementation code using Arraylist (Edited out to fix level-4 requirement)
+          // Implementation code using Arraylist (Edited out to fit level-4 requirement)
                     Task task = dataArrayList.get(change);
                     task.markAsDone();
                     System.out.println("Nice! I've marked this task as done:");
@@ -52,11 +52,36 @@ public class Duke {
           break;
         default:
           if (!input.equals("bye")) {
-            // Implementation code using Arraylist (Edited out to fix level-4 requirement)
-            // dataArrayList.add(new Task(input));
-            tasksArr[taskTracker] = new Task(input);
+            String addMessage = "Got it. I've added this task:";
+            if (input.equals("todo")) {
+              System.out.println(addMessage);
+              // Change line below to for Arraylist implementation
+              tasksArr[taskTracker] = new Todo(scanner.nextLine());
+            } else if (input.equals("deadline")) {
+              System.out.println(addMessage);
+              scanner.useDelimiter("/by");
+              String desc = scanner.next().trim();
+              scanner.skip("/by");
+              String by = scanner.nextLine().trim();
+              // Change line below to for Arraylist implementation
+              tasksArr[taskTracker] = new Deadline(desc, by);
+            } else if (input.equals("event")) {
+              System.out.println(addMessage);
+              scanner.useDelimiter("/at");
+              String desc = scanner.next().trim();
+              scanner.skip("/at");
+              String at = scanner.nextLine().trim();
+              // Change line below to for Arraylist implementation
+              tasksArr[taskTracker] = new Event(desc, at);
+            } else {
+              // Change line below to for proper throw exception
+              System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+              break;
+            }
+            System.out.println("\t" + tasksArr[taskTracker]);
             taskTracker++;
-            System.out.println("added: " + input);
+            scanner.reset();
+            System.out.printf("Now you have %o tasks in list.\n", taskTracker);
           }
           break;
       }
