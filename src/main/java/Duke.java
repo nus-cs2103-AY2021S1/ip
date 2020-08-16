1f4f6624fc1a430ff7e5932588a1662a7d43e815
Nikhilalalalala <uvnikhi@gmail.com>
2020-08-16 16:07:11 +0800

    add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Duke.run();
+    }
+    public static void run() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String INITIAL_PRINTING = "Hello! I'm Duke\n" + "What can I do for you?\n" + "____________________________________________________________\n";
+        System.out.println(INITIAL_PRINTING);
+        Duke.repeat();
+    }
+    public static void repeat() {
+        String answer = "";
+        Scanner sc = new Scanner(System.in);
+        do {
+            answer = sc.nextLine();
+            System.out.println(answer);
+            answer = answer.toLowerCase();
+        } while (!answer.equals("bye"));
     }
 
 }
