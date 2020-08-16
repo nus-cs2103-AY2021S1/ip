5b1aff991def39481edae50bd86e4e5e1dcfae6a
siangernlow <65005806+siangernlow@users.noreply.github.com>
2020-08-16 15:18:29 +0800

    Modified the user interface to use enums
    Commands sent to TaskBot are now controlled by enums instead of strings.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
  * This is the main class which handles user input.
  */
 public class Main {
+    //Commands for the bot to execute
+    public enum Command {
+        LIST, DONE, NONE
+    }
     public static void main(String[] args) {
         //The logo to be used for TaskBot
         String logo =
@@ -24,15 +28,34 @@ public class Main {
         //Initialises a Scanner to take in user input
         Scanner sc = new Scanner(System.in);
 
+        Command cmd = Command.NONE;
         //Loops while the user does not input "bye"
         while (sc.hasNextLine()) {
+            int taskIndex = -1;
             String input = sc.nextLine();
+
+            //Splits the string to find keywords for commands
             if (input.equals("bye")) {
                 break;
-            } else if (input.equals("list")){
-                tb.listTasks();
-            } else {
-                tb.addTask(input);
+            }
+            String parse = input.length() > 3 ? input.substring(0, 4) : input;
+            if (parse.equals("list")){
+                cmd = Command.LIST;
+            } else if (parse.equals("done")){
+                cmd = Command.DONE;
+                taskIndex = Integer.parseInt(input.substring(5, 6)) - 1;
+            }
+
+            //instructs the bot given the command
+            switch (cmd) {
+                case LIST:
+                   tb.listTasks();
+                   break;
+                case DONE:
+                    tb.completeTask(taskIndex);
+                    break;
+                case NONE:
+                    tb.addTask(input);
             }
         }
 
