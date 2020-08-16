3c8ca5b6c2e09851d76751f83c7f70f994bcdb08
khoodehui <dehui@Khoos-MacBook-Pro.local>
2020-08-16 16:41:39 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye! Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
