86fbf9a519cf63a27d77d47f8eb6a286fddb7b95
Wei Kit <weikit89@gmail.com>
2020-08-16 15:44:03 +0800

    Completed Increment Level-1 for greeting and echoing of commands. Option to exit using the command 'bye'.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String introduction =
+                "Hi, I'm your Professor, Martin." +
+                "\nWhat can I do for you?";
+        System.out.println(introduction);
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+
+        // Loop program until command 'bye' is entered as input.
+        while (true) {
+            try {
+                System.out.print("Enter command: ");
+                input = sc.nextLine();
+                processCommand(input);
+                if (!input.equals("bye")) {
+                    System.out.println("Restarting...");
+                } else {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Exception: " + e.toString());
+            }
+        }
+        sc.close();
+    }
+
+    public static void processCommand(String command) {
+        if (command.equals("bye") ) {
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println("Echoing command: " + command);
+        }
     }
 }
