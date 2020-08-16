1dc27fe5bd23380729646a2ae04a5520421c4967
murtubak <murtubakchickenss@gmail.com>
2020-08-16 16:04:09 +0800

    Add greet, echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
+    final static String GREETING_MESSAGE = "Buenos Dias! Soy Duke, como estas mi amigo?";
+    final static String TERMINATION = "bye";
+    final static String TERMINATION_MESSAGE = "Adios, amigos!";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(GREETING_MESSAGE);
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        if (userInput.equals(TERMINATION)) {
+            System.out.println(TERMINATION_MESSAGE);
+            sc.close();
+        } else {
+            System.out.println(userInput);
+        }
     }
 }
