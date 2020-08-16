cde0713276feb0b037ec1b7cebc0f4ae6b60630e
Galvin <galvinleow@gmail.com>
2020-08-16 12:44:16 +0800

    Change code to fit Level-4 requirement
    In level 3, the code used arraylist to get the job done. However, in level 4 using array was the requirement. Hence this commit added code for all functionality till level 3 using array. The arraylist code is commented out. 
    
    Task class changes toString method to better fit future requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -11,8 +10,10 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|\n";
     System.out.println(logo + "\nHello ! I'm Duke\nWhat can I do for you?\n");
 
-    // Arraylist used to ensure scalability
-    ArrayList<Task> dataArrayList = new ArrayList<>();
+    // Arraylist used to ensure scalability (Edited out to fix level-4 requirement)
+    //    ArrayList<Task> dataArrayList = new ArrayList<>();
+    Task[] tasksArr = new Task[100];
+    int taskTracker = 0;
     Scanner scanner = new Scanner(System.in);
     String input = "";
     while (!input.equals("bye")) {
@@ -20,23 +21,41 @@ public class Duke {
       // Switch case use to prepare for future requirements
       switch (input) {
         case "list":
-          int count = 1;
-          for (Task task : dataArrayList) {
-            System.out.printf("%d.[%s] %s\n", count, task.getStatusIcon(), task);
-            count++;
+          /*
+          // Implementation code using Arraylist (Edited out to fix level-4 requirement)
+                   int count = 1;
+                   for (Task task : dataArrayList) {
+                     System.out.printf("%d.[%s] %s\n", count, task.getStatusIcon(), task);
+                     count++;
+                   }
+          */
+
+          for (int i = 0; i < tasksArr.length; i++) {
+            if (tasksArr[i] == null) {
+              break;
+            }
+            System.out.printf("%d.%s\n", i + 1, tasksArr[i]);
           }
           break;
         case "done":
           int change = scanner.nextInt() - 1;
-          Task task = dataArrayList.get(change);
-          task.markAsDone();
+          /*
+          // Implementation code using Arraylist (Edited out to fix level-4 requirement)
+                    Task task = dataArrayList.get(change);
+                    task.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("%s\n", task);
+          */
+          tasksArr[change].markAsDone();
           System.out.println("Nice! I've marked this task as done:");
-          System.out.printf("[%s] %s%n", task.getStatusIcon(), task);
+          System.out.printf("%s\n", tasksArr[change]);
           break;
         default:
           if (!input.equals("bye")) {
-            Task addTask = new Task(input);
-            dataArrayList.add(addTask);
+            // Implementation code using Arraylist (Edited out to fix level-4 requirement)
+            // dataArrayList.add(new Task(input));
+            tasksArr[taskTracker] = new Task(input);
+            taskTracker++;
             System.out.println("added: " + input);
           }
           break;
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -16,6 +16,6 @@ public class Task {
   }
 
   public String toString() {
-    return this.description;
+    return String.format("[%s] %s", this.getStatusIcon(), this.description);
   }
 }
