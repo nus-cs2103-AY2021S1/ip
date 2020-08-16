193d7d537041213ff4af33ce8c7a136de977da02
sogggy <songyu98@gmail.com>
2020-08-16 15:24:20 +0800

    A-TextUiTesting

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,34 +1,37 @@
 public class Bot {
     public void repeat(Store store) {
-        System.out.println("\t___________________");
+        Bot.sectionize();
         System.out.println("\tGot it. I've added this task: ");
         System.out.println("\t\t" + store.getList().get(store.size() - 1).toString());
         System.out.println("\tNow you have " + store.size() + " tasks in the list.");
-        System.out.println("\t___________________");
+        Bot.sectionize();
     }
 
     public void listItems(Store store) {
-        System.out.println("\t___________________");
-        System.out.println("Here are the tasks in your list:");
+        Bot.sectionize();
+        System.out.println("\tHere are the tasks in your list:");
         int counter = 1;
         for (int i = 0; i < store.size(); i++) {
             System.out.println("\t" + counter + "." + store.getItem(i).toString());
             counter++;
         }
-        System.out.println("\t___________________");
+        Bot.sectionize();
     }
 
     public void sayBye() {
-        System.out.println("\t___________________");
+        Bot.sectionize();
         System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("\t___________________");
+        Bot.sectionize();
     }
 
     public void markDone(int index, Store store) {
         store.setDone(index);
-        System.out.println("\t_________________");
+        Bot.sectionize();
         System.out.println("\tNice! I've marked this task as done: ");
-        System.out.println("\t[✓] " + store.getItem(index).getTask());
-        System.out.println("\t_________________");
+        System.out.println("\t" + store.getItem(index).toString());
+        Bot.sectionize();
     }
+
+    public static void sectionize() { System.out.println("\t____________________________________________________________"); }
+
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,17 +20,24 @@ public class Duke {
             } else if (input.equals("list")) {
                 bot.listItems(store);
             } else {
-                String taskType = input.substring(0, input.indexOf(" "));
-                String task = input.substring(input.indexOf(" ") + 1);
-                if (taskType.equals("todo")) {
-                    store.addItem(new ToDos(task));
-                } else if (taskType.equals("deadline")) {
-                    // date = 'by Sunday'
-                    store.addItem(new Deadlines(task));
-                } else if (taskType.equals("event")) {
-                    store.addItem(new Events(task));
+                try {
+                    String taskType = input.substring(0, input.indexOf(" "));
+                    String task = input.substring(input.indexOf(" ") + 1);
+                    String date = input.substring(input.indexOf("/") + 1, input.length());
+                    if (taskType.equals("todo")) {
+                        store.addItem(new ToDos(task));
+                    } else if (taskType.equals("deadline")) {
+                        // date = 'by Sunday'
+                        store.addItem(new Deadlines(task));
+                    } else if (taskType.equals("event")) {
+                        store.addItem(new Events(task));
+                    }
+                    bot.repeat(store);
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("    ____________________________________________________________\n" +
+                            "     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
+                            "    ____________________________________________________________");
                 }
-                bot.repeat(store);
             }
         }
     }
