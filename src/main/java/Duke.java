657287b6b3d65a3740bdfa67e3ab2c7e4b2e184b
KishenKumarrrrr <kishen1198@gmail.com>
2020-08-16 11:42:58 +0800

    Add Greet, Echo, Exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "        ___\n"
+                    + "    . -^   `--,\n"
+                    + "   /# =========`-_\n"
+                    + "  /# (--====___====\\\n"
+                    + " /#   .- --.  . --.|\n"
+                    + "/##   |  * ) (   * ),\n"
+                    + "|##   \\    /\\ \\   / |\n"
+                    + "|###   ---   \\ ---  |\n"
+                    + "|####      ___)    #|\n"
+                    + "|######           ##|\n"
+                    + " \\##### ---------- /\n"
+                    + "  \\####           (\n"
+                    + "   `\\###          |\n"
+                    + "     \\###         |\n"
+                    + "      \\##        |\n"
+                    + "       \\###.    .)\n"
+                    + "         `======/";
+
+        System.out.println("A massive alien head has appeared\n" + logo);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    SHOW ME WHAT YOU'VE GOT");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (!sc.hasNext("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + sc.nextLine());
+            System.out.println("    ____________________________________________________________");
+            System.out.println("");
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
