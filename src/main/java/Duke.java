90a6b8ba75d06d46dfc94a51fa7b325a850d664d
Licheng-Wu <wlc.licheng@gmail.com>
2020-08-16 22:59:48 +0800

    Revert "Updated chatbot's name to Mrs Dino"
    This reverts commit f7774b9f3d59b629f6cc6434fb4a94e9111e2f5e.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = "    _____                 ________  .__\n"
-                + "   /     \\_______  ______ \\______ \\ |__| ____   ____\n"
-                + "  /  \\ /  \\_  __ \\/  ___/  |    |  \\|  |/    \\ /  _ \\ \n"
-                + " /    Y    \\  | \\/\\___ \\   |    |   \\  |   |  (  <_> ) \n"
-                + " \\____|__  /__|  /____  > /_______  /__|___|  /\\____/ \n"
-                + "        \\/           \\/          \\/        \\/ \n";
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Welcome! I'm Mrs Dino\nWhat can I do for you?");
-//        Echo.initialise();
+        Echo.initialise();
     }
 }
