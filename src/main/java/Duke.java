bb7f40b1dc17bb622caced3aa2df4bf10e1f6691
Ye Jiadong <ye_jia_dong@hotmail.com>
2020-08-16 17:31:54 +0800

    Removed time-specific greetings to allow for text-ui-testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,10 @@
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Duke {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args)  {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ \uD83D\uDD34 \uD83D\uDD34 \\\n"
@@ -13,37 +12,13 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm\n" + logo);
 
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Date today = new Date();
-        DateFormat timeFormat = new SimpleDateFormat("HH:mm");
-        String time = timeFormat.format(today);
-        boolean checkLate;
-
-        if (timeFormat.parse(time).after(timeFormat.parse("12:00")))
-        {
-            checkLate = true;
-
-        } else {
-            checkLate = false;
-        }
-
-        System.out.println("Its " + dateFormat.format(today)  + " and " + "the time now is " + time);
-
-        if (checkLate == true) {
-            System.out.println("It's Late! YAWNS... Almost my bed time, but always ready to serve you, my trusted friend!");
-        } else {
-            System.out.println("It's still early! Let me go get my cup of coffee first... then I will be ready to answer your questions!");
-            System.out.println("...");
-            System.out.println("Okay, I'm back and ready!");
-        }
-
         System.out.println("\nWhat can I do for you?");
 
         ArrayList<Task> inputStore = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
+        while (sc.hasNextLine()) {
             String userInput = sc.nextLine();
             String[] splitString = userInput.split(" ");
             String returnString = "";
