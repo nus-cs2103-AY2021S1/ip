ec43131d83afe52fd65d47f1c71a8f251b8ff061
pengxiangg <wong.pengxiang@gmail.com>
2020-08-16 21:00:36 +0800

    Add greeting, echo and goodbye messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
+
+
 public class Duke {
+
+    public static void readAndEcho() {
+        String divider = "___________________________";
+        String dividerFormatted = String.format("%" + (5 + divider.length()) + "s", divider);
+        String intro1 = "Hello! I'm Duke";
+        String intro2 = "What can I do for you?";
+
+        System.out.println(dividerFormatted);
+        System.out.println(String.format("%" + (6 + intro1.length()) + "s", intro1));
+        System.out.println(String.format("%" + (6 + intro2.length()) + "s", intro2));
+        System.out.println(dividerFormatted);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(dividerFormatted);
+            System.out.println(String.format("%" + (6 + input.length()) + "s", input));
+            System.out.println(dividerFormatted);
+            input = sc.nextLine();
+        }
+        String goodbye = "Bye. Hope to see you again soon!";
+        System.out.println(dividerFormatted);
+        System.out.println(String.format("%" + (6 + goodbye.length()) + "s", goodbye));
+        System.out.println(dividerFormatted);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +35,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        readAndEcho();
     }
 }
