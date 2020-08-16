4b17c3d98b7db339c362d1845d84c0fba06b5d0d
Timothy Ong <timothyjkong@gmail.com>
2020-08-17 01:38:32 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Duke {
-    private static List<String> list = new ArrayList<>();
+    private static List<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -21,12 +21,24 @@ public class Duke {
             System.out.println("Bye. Hope to see you again soon!");
         } else if (next.equals("list")) {
             for (int i = 0; i < list.size(); i++) {
-                System.out.println(i + 1 + ". " + list.get(i));
+                System.out.println(i + 1 + "." + list.get(i));
             }
             response();
+        } else if (next.startsWith("done")){
+            try {
+                int done = Integer.valueOf(next.substring(5, next.length()));
+                list.get(done - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + list.get(done - 1));
+            } catch(NumberFormatException e) {
+                System.out.println("Please input a number");
+            } catch(IndexOutOfBoundsException e) {
+                System.out.println("Please input a valid number");
+            } finally {
+                response();
+            }
         } else {
             System.out.println("added " + next);
-            list.add(next);
+            list.add(new Task(next));
             response();
         }
     }
