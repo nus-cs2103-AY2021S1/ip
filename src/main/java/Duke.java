cc0c41db90d523da96d286454deb15547ab3b0c5
StopTakingAllTheNames <ong.yinggao@gmail.com>
2020-08-16 15:55:42 +0800

    Added bye command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String lines = "\t____________________\n";
+        String farewell = "\tSad to see you go!\n";
         System.out.println(lines + "\tHello, I'm your chatbot!\n\tWhat can I do for you?\n" + lines);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(lines + farewell + lines);
+                break;
+            } else {
+                System.out.println(lines + "\t" +input + "\n" + lines);
+            }
+        }
     }
 }
