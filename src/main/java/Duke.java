2353fe0f73b3e9a54b541d753c44389ab5c7ad0b
SherryWu178 <sherry.wu178@gmail.com>
2020-08-16 11:35:42 +0800

    Complete level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke dk = new Duke();
+        dk.greet();
+
+        Scanner sc = new Scanner( System.in );
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                dk.end_conversation();
+            } else {
+                dk.echo(input);
+            }
+        }
     }
+
+    public void greet(){
+        System.out.println("Hello! I am Duke Chan.\nWhat can I do for you? ᕕ( ᐛ )ᕗ");
+    }
+
+    public void echo(String input){
+        System.out.println(input);
+    }
+
+    public void end_conversation(){
+        System.out.println("Bye. Hope to see you again soon! ∠( ᐛ 」∠)＿");
+        System.exit(0);
+    }
+
+
 }
