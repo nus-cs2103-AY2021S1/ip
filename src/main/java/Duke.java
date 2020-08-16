5edd7c80194229cb8bffedcaf7901e87dca3fdbf
Preston Toh <prestontoh@gmail.com>
2020-08-16 21:58:40 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,21 +5,23 @@ public class Duke {
         Scanner inputs = new Scanner(System.in);
         boolean run = true;
 
-        System.out.println("Hello~, I'm Duke!");
+        System.out.println("Hello~, I'm Duke!\n"
+        + "What can I do for you?");
 
         while (run == true) {
             String next = inputs.next();
-
+            System.out.println("____________________________________________________________");
             if (next.equals("bye")) {
-                System.out.println("Goodbye~");
+                System.out.println("Goodbye~ Hope to see you again soon!");
                 run = false;
             } else {
                 System.out.println(next);
             }
+            System.out.println("____________________________________________________________");
         }
     }
 
-    
+
 //    public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
