21fea09d32239dba6c95f8f21966212e8b509f11
keanecjy <e0425601@u.nus.edu>
2020-08-17 00:45:25 +0800

    Added ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,22 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Duke {
+
+    public static boolean isValid(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
         String separator = "   -------------------------------------------------";
 
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         while (sc.hasNextLine()) {
             String next = sc.nextLine();
@@ -17,11 +27,32 @@ public class Duke {
             }
             System.out.println(separator);
             if (next.equals("list")) {
+                System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("    " + (i + 1) + ". " + tasks.get(i));
+                    System.out.println("     " + (i + 1) + "." +  tasks.get(i));
+                }
+            } else if (next.startsWith("done")) {
+                int len = next.length();
+                if (len >= 6 && isValid(next.substring(5, len))) {
+                    int digit = Integer.parseInt(next.substring(5, len));
+
+                    if (digit <= tasks.size() && digit > 0) {
+                        Task current = tasks.get(digit - 1);
+                        if (current.checkIfDone()) {
+                            System.out.println("    Task has already been marked done!");
+                        } else {
+                            current.markAsDone();
+                            System.out.println("    Nice! I've marked this task as done:");
+                            System.out.println("      " + current);
+                        }
+                    } else {
+                        System.out.println("    Task number does not exist in the list");
+                    }
+                } else {
+                    System.out.println("    Input format is invalid");
                 }
             } else {
-                tasks.add(next);
+                tasks.add(new Task(next));
                 System.out.println("    added: " + next);
             }
             System.out.println(separator);
