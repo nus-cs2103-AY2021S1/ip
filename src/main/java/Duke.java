d4d4cbcffa2f308369c6c1e88350b93c6e5e7200
mgiang2015 <mgiang2015@gmail.com>
2020-08-16 17:04:21 +0800

    Implement level 3. Create function mark done, and fix get output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -30,29 +30,49 @@ public class Duke {
         "____________________________________________________________\n";
     }
 
-    public static String summarize(ArrayList<String> tasks) {
-        String all_tasks = "";
+    public static String summarize(ArrayList<String> tasks, ArrayList<Integer> is_done) {
+        String all_tasks = "Here are the tasks in your list:\n";
         for (int i = 0; i < tasks.size(); i++) {
-            all_tasks += String.format("%d. %s\n", i+1, tasks.get(i));
+            String tick = "";
+            if (is_done.get(i) > 0) {
+                tick = "✓";
+            } else {
+                tick = "✗";
+            }
+
+            all_tasks += String.format("%d.[%s] %s\n", i+1, tick, tasks.get(i));
         }
 
         return all_tasks;
+    }
+
+    public static String mark_done(ArrayList<String> tasks, ArrayList<Integer> is_done, int index) {
+        if (is_done.get(index) == 0) {
+            is_done.set(index, 1);
+            return String.format("Nice! I've marked this task as done:\n[✓] %s\n", tasks.get(index));
+        } else {
+            return String.format("Task %d already done!\n", index);
+        }
     }
 
     public static void hal9000_op() {
         boolean end = false;
         Scanner sc = new Scanner(System.in);
         ArrayList<String> tasks = new ArrayList<>();
-        
+        ArrayList<Integer> is_done = new ArrayList<>();
         while (!end) {
             String input = get_input(sc);
+            String[] parts = input.split(" ");
             if (input.compareTo("bye") == 0) {
                 end = true;
             } else if (input.compareTo("list") == 0) {
-                System.out.println(format_response(summarize(tasks)));
+                System.out.println(format_response(summarize(tasks, is_done)));
+            } else if (parts[0].compareTo("done") == 0) {
+                System.out.println(format_response(mark_done(tasks, is_done, Integer.parseInt(parts[1]) - 1))); 
             } else {
                 // add task to list of tasks
                 tasks.add(input);
+                is_done.add(0); // add 0 for false
 
                 String output_string = format_response(get_output(input));
                 System.out.println(output_string);
