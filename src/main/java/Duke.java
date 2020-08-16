e35f84b17a31b67115d87173596ea038aced0eb5
orzymandias <ganwancheng@gmail.com>
2020-08-16 16:19:54 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
+
+    // Collection of user's tasks
+    static List<String> taskItems = new ArrayList<>();
 
     static String replyFormatter(String reply) {
         String partition = "__________________________";
         return String.format(partition + "\n%s\n" + partition, reply);
+    }
+
+    static String listFormatter(List<String> ls) {
+        String formattedListString = "";
+        for (int i = 0; i < ls.size(); i ++) {
+            formattedListString+= String.format("%d. %s\n", i + 1, ls.get(i));
+        }
+        return formattedListString;
     }
 
     static void printReply(String reply) {
@@ -21,15 +34,19 @@ public class Duke {
                         "  888   888  Y88888     888     888 T88b   888     888 888    888 888     888    888     \n" +
                         "  888   888   Y8888     888     888  T88b  Y88b. .d88P 888   d88P Y88b. .d88P    888     \n" +
                         "8888888 888    Y888     888     888   T88b  \"Y88888P\"  8888888P\"   \"Y88888P\"     888";
-        System.out.println("ITS ME: \n" + logo);
+       printReply(replyFormatter("ITS ME: \n" + logo));
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
             String reply = sc.nextLine();
             if (reply.equals("bye")) {
                 printReply(replyFormatter(reply));
                 break;
+            } else if (reply.equals("list")) {
+                printReply(replyFormatter(listFormatter(taskItems)));
+            } else {
+                taskItems.add(reply);
+                printReply(replyFormatter(String.format("added: %s", reply)));
             }
-            printReply(replyFormatter(reply));
         }
 
     }
