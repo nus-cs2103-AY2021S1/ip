d2b72c1903164485fe26b4de1ebed5e22a054ca8
Rayson Koh <rayson.rk.koh@gmail.com>
2020-08-17 04:44:00 +0800

    Add EventTask extending Task

---
+++ src/main/java/EventTask.java
@@ -0,0 +1,23 @@
+public class EventTask extends Task {
+    private final String timePeriod;
+
+    public EventTask(String description, String timePeriod) {
+        super(description);
+        this.timePeriod = timePeriod;
+    }
+
+    private EventTask(String description, boolean isDone, String timePeriod) {
+        super(description, isDone);
+        this.timePeriod = timePeriod;
+    }
+
+    @Override
+    public EventTask markAsDone() {
+        return new EventTask(description, true, timePeriod);
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + timePeriod + ")";
+    }
+}
