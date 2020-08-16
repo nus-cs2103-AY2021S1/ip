23f552c9d6066ada4aebddb09cf065a9f71c52bc
Cy <chunyonggg@gmail.com>
2020-08-16 17:30:06 +0800

    lvl1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class Duke {
+
+    static char line  = (char) 	0x2501 ;
+    static void loopMethod() {
+        //gets input and displays it
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        if (word.equals("bye")) {
+        encloseWithinHorizontalLines("Bye. Hope to see you again soon!");
+        return;
+        }
+        else {
+        encloseWithinHorizontalLines(word);
+        }
+        loopMethod();
+    }
+
+    static void encloseWithinHorizontalLines(String word) {
+        Stream.generate(() -> line).limit(50).forEach(System.out::print); // _ _ _ _ _
+        System.out.println();
+        System.out.println(word);
+        System.out.println();
+        Stream.generate(() -> line).limit(50).forEach(System.out::print); // _ _ _ _ _
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        encloseWithinHorizontalLines("\"Hello! I'm Duke \\n What can I do for you?\"");
+        loopMethod();
     }
 }
