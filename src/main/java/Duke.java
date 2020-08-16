3ff4cab572dcd430ae2ac0196c594ec440f2f8f8
StopTakingAllTheNames <ong.yinggao@gmail.com>
2020-08-16 18:08:59 +0800

    Reorganised commands into their own methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,59 +3,71 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static final String LINES = "____________________\n";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Task> tasks = new ArrayList<>(100);
-
-        String lines = "____________________\n";
         String farewell = "Sad to see you go!\n";
 
-        System.out.println(lines
+        System.out.println(LINES
                 + "Hello, I'm your chatbot!\n"
                 + "What can I do for you?\n"
-                + lines);
+                + LINES);
 
         while (sc.hasNext()) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println(lines + farewell + lines);
+                System.out.println(LINES + farewell + LINES);
                 break;
             } else if (input.equals("list")) {
-                System.out.println(lines + "Here are the tasks in your list:");
-                int number = 1;
-                for (Task task : tasks) {
-                    System.out.println(String.format("%d.", number++) + task);
-                }
-                System.out.println(lines);
+                list(tasks);
             } else if (input.startsWith("done")) {
-                int toDo = Integer.parseInt(input.substring(5)) - 1;
-                Task task = tasks.get(toDo);
-                task.setDone();
-                System.out.println(lines
-                        + "Well done! The following task is complete:\n" + task
-                        + "\n" + lines);
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                completeTask(tasks, index);
             } else {
-                Task newTask = null;
-
-                if (input.startsWith("todo")) {
-                    newTask = new ToDo(input.substring(5));
-                } else if (input.startsWith("deadline")) {
-                    String[] taskInfo = input.substring(9).split("/by ", 2);
-                    newTask = new Deadline(taskInfo[0], taskInfo[1]);
-                } else if (input.startsWith("event")) {
-                    String[] taskInfo = input.substring(6).split("/at ", 2);
-                    newTask = new Event(taskInfo[0], taskInfo[1]);
-                } else {
-                    continue;
-                }
-
-                tasks.add(newTask);
-                System.out.println(lines +
-                        "Got it, I have added this task:\n"
-                        + newTask
-                        + "\nNow you have " + tasks.size() + " tasks in this list\n"
-                        + lines);
+                addTask(tasks, input);
             }
+        }
+    }
+
+    private static void list(List<Task> tasks) {
+        System.out.println(LINES + "Here are the tasks in your list:");
+        int number = 1;
+        for (Task task : tasks) {
+            System.out.println(String.format("%d.", number++) + task);
+        }
+        System.out.println(LINES);
+    }
+
+    private static void completeTask(List<Task> tasks, int index) {
+        Task task = tasks.get(index);
+        task.setDone();
+        System.out.println(LINES
+                + "Well done! The following task is complete:\n" + task
+                + "\n" + LINES);
+    }
+
+    private static void addTask(List<Task> tasks, String task) {
+        Task newTask = null;
+
+        if (task.startsWith("todo")) {
+            newTask = new ToDo(task.substring(5));
+        } else if (task.startsWith("deadline")) {
+            String[] taskInfo = task.substring(9).split("/by ", 2);
+            newTask = new Deadline(taskInfo[0], taskInfo[1]);
+        } else if (task.startsWith("event")) {
+            String[] taskInfo = task.substring(6).split("/at ", 2);
+            newTask = new Event(taskInfo[0], taskInfo[1]);
+        }
+
+        if (newTask != null) {
+            tasks.add(newTask);
+            System.out.println(LINES +
+                    "Got it, I have added this task:\n"
+                    + newTask
+                    + "\nNow you have " + tasks.size() + " tasks in this list\n"
+                    + LINES);
         }
     }
 }
