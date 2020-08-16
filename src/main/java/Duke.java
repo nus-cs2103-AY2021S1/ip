280f7938c013a9246de569136d0dd6845f50fd00
ChooJiaXin <e0426255@u.nus.edu>
2020-08-16 16:57:53 +0800

    Add ability to store items in a list and display the list when prompted by the user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,8 +7,9 @@ public class Duke {
         String chatbot = "Bob: ";
         String skipLine = "\n";
         String user = skipLine + "You: ";
+        List<String> listOfCommands = new ArrayList<>();
 
-
+        // Greetings
         System.out.println(chatbot + "Hey there! I'm Bob" + skipLine + "What can I do for you today?");
         System.out.println(user);
 
@@ -17,11 +20,35 @@ public class Duke {
 
             boolean exit = echoUser.equals("bye");
 
+            // Exit chatbot
             if (exit) {
                 break;
             }
 
-            System.out.println(skipLine + chatbot + echoUser);
+            boolean showListOfCommands = echoUser.equals("list");
+
+            // Display List
+            if (showListOfCommands) {
+                System.out.println(skipLine + chatbot);
+
+                if (listOfCommands.size() == 0) {
+                    System.out.println("List is empty :(");
+                }
+
+                for (String command: listOfCommands) {
+                    int listIndex = listOfCommands.indexOf(command) + 1;
+                    System.out.println(listIndex + ". " + command);
+                }
+
+            // Add text to the list
+            } else {
+                listOfCommands.add(echoUser);
+
+                System.out.println();
+                System.out.println(chatbot + "added '" + echoUser + "'");
+            }
+
+            // Prompt user commands
             System.out.println(user);
 
         }
