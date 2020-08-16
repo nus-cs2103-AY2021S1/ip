b8627e055a011f46402f4f2d34f6f0250d55f811
Marcon2509 <marcus.duigan@gmail.com>
2020-08-16 19:31:45 +0800

    Added runtest.bat to work for local files and updated input files with expected files for A-TextUiTesting: Automated Text UI Testing

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,28 +1,28 @@
 public class Task {
 
 
-        protected String description;
-        protected boolean isDone;
+    protected String description;
+    protected boolean isDone;
 
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
 
-        public String getStatusIcon() {
-            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-        }
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
 
-        //...
-        @Override
-        public String toString() {
-            String s = "[" + this.getStatusIcon() +"] " + this.description;
-            return s;
-        }
+    //...
+    @Override
+    public String toString() {
+        String s = "[" + this.getStatusIcon() + "] " + this.description;
+        return s;
+    }
 
-        public Task markAsDone() {
-            this.isDone = true;
-            return this;
-        }
+    public Task markAsDone() {
+        this.isDone = true;
+        return this;
+    }
 
 }
