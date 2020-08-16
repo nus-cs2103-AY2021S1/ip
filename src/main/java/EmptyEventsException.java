a3dbf0b746157511c05566f0789bae5c790e26c6
maguireong <maguire2622@yahoo.com.sg>
2020-08-16 18:42:42 +0800

    Add exceptions and tested them

---
+++ src/main/java/EmptyEventsException.java
@@ -0,0 +1,7 @@
+
+public class EmptyEventsException extends DukeException {
+
+    public EmptyEventsException() {
+        super("\u2639 OOPS!!! The description of a event cannot be empty.");
+    }
+}
---
+++ src/main/java/EmptyTodoException.java
@@ -0,0 +1,7 @@
+
+public class EmptyTodoException extends DukeException {
+
+    public EmptyTodoException() {
+        super("\u2639 OOPS!!! The description of a todo cannot be empty.");
+    }
+}
