cea12adfd874e17365760163762aaf7512163ed2
StopTakingAllTheNames <ong.yinggao@gmail.com>
2020-08-16 16:29:06 +0800

    Added ability to record input strings from user as a task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,29 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String lines = "\t____________________\n";
-        String farewell = "\tSad to see you go!\n";
-        System.out.println(lines + "\tHello, I'm your chatbot!\n\tWhat can I do for you?\n" + lines);
+        List<String> tasks = new LinkedList<>();
+
+        int number = 1;
+        String lines = "____________________\n";
+        String farewell = "Sad to see you go!\n";
+
+        System.out.println(lines
+                + "Hello, I'm your chatbot!\n"
+                + "What can I do for you?\n"
+                + lines);
+
         while (sc.hasNext()) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println(lines + farewell + lines);
                 break;
             } else {
-                System.out.println(lines + "\t" +input + "\n" + lines);
+                tasks.add(String.format("%d. ", number++) + input);
+                System.out.println(lines + "added: " + input + "\n" + lines);
             }
         }
     }
