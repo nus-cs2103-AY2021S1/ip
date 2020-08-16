1e0a96e037c9bb70dc0440c1316dd8ac2159aa43
AlexCQY <alex_chua@u.nus.edu>
2020-08-16 17:51:45 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
     public static void chatbot() {
         Scanner sc = new Scanner(System.in);
+        List<String> lst = new ArrayList<>();
+        String listCommand = "list";
 
         String indent = "    ";
         String upperLine = indent + "____________________________________" + "\n";
@@ -12,18 +16,34 @@ public class Duke {
                 + indent + "What can I do for you?" +"\n";
 
         System.out.println(upperLine + greetings + lowerLine);
-
+        int numberOfItems = 0;
         while (sc.hasNext()) {
+
             String input = sc.nextLine();
             String outputIndent = indent + " ";
-            String output = outputIndent + input + "\n";
+            String output = input + "\n";
+            String addedMessage = outputIndent + "added: ";
 
             if (input.equals("bye")) {
                 String exit = outputIndent + "Bye. Hope to see you again soon!" + "\n";
                 System.out.println(upperLine + exit + lowerLine);
                 break;
             }
-            System.out.println(upperLine + output + lowerLine);
+
+            if (input.equals(listCommand)) {
+                StringBuilder concat = new StringBuilder();
+                for (String s : lst) {
+                    concat.append(s);
+                }
+                System.out.println(upperLine + concat.toString() + lowerLine);
+            } else {
+                numberOfItems++;
+                lst.add(outputIndent + numberOfItems + "." + " " + output);
+                System.out.println(upperLine + addedMessage
+                        + input +"\n" + lowerLine);
+
+            }
+
 
         }
     }
