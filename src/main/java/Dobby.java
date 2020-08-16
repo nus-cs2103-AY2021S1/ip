ec83e48eaa829ae0984f1b4bcd2c7dc61323b45f
sagarsureka <sagar150102@gmail.com>
2020-08-17 01:06:03 +0530

    Fix indentation

--- src/main/java/Dobby.java
+++ src/main/java/Dobby.java
@@ -28,7 +28,7 @@ public class Dobby {
         String message = "";
 
         if (text.equalsIgnoreCase("bye")) {
-            message = "\n   Goodbye. Hope to see you again soon!\n    ";
+            message = "\n    Goodbye. Hope to see you again soon!\n    ";
         } else if (text.equalsIgnoreCase("list")) {
             int i = 0;
             String all_tasks = "\n    ";
@@ -41,29 +41,29 @@ public class Dobby {
             int index = Integer.parseInt(text.substring(5));
             Task task = tasks.get(index - 1);
             task.setDone();
-            message = "\n    Great! I've marked this task as done:\n    " + task.getDescription() + "\n    ";
+            message = "\n    Great! I've marked this task as done:\n      " + task.getDescription() + "\n    ";
         } else if (text.startsWith("deadline")) {
             text = text.substring(9);
             String by = text.substring(text.indexOf("/by") + 4);
             text = text.substring(0, text.indexOf("/by") - 1);
             Deadline deadline = new Deadline(text, by);
             tasks.add(deadline);
-            message = "\n    Great! I've added the following task:\n    " + deadline.getDescription() +
-                    String.format("    Now you have %d tasks in the list.\n    ", tasks.size());
+            message = "\n    Great! I've added the following task:\n      " + deadline.getDescription() +
+                    String.format("\n    Now you have %d tasks in the list.\n    ", tasks.size());
         } else if (text.startsWith("todo")) {
             text = text.substring(5);
             Todo todo = new Todo(text);
             tasks.add(todo);
-            message = "\n    Great! I've added the following task:\n    " + todo.getDescription() +
-                    String.format("    Now you have %d tasks in the list.\n    ", tasks.size());
+            message = "\n    Great! I've added the following task:\n      " + todo.getDescription() +
+                    String.format("\n    Now you have %d tasks in the list.\n    ", tasks.size());
         } else if (text.startsWith("event")) {
             text = text.substring(6);
             String at = text.substring(text.indexOf("/at") + 4);
             text = text.substring(0, text.indexOf("/at") - 1);
             Event event = new Event(text, at);
             tasks.add(event);
-            message = "\n    Great! I've added the following task:\n    " + event.getDescription() +
-                    String.format("    Now you have %d tasks in the list.\n    ", tasks.size());
+            message = "\n    Great! I've added the following task:\n      " + event.getDescription() +
+                    String.format("\n    Now you have %d tasks in the list.\n    ", tasks.size());
         } else {
             message =  "\n    Sorry that command is not supported. Please try again.\n   ";
         }
