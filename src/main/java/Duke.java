7b4ed7328b6ef5c52e05e14819ce79dbdc9b2b6e
Theodore Leebrant <theodoreleebrant@gmail.com>
2020-08-16 20:28:29 +0800

    Level 1 commit Add greet, echo, and exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String echo = sc.next();
+            if (echo.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            } else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     " + echo);
+                System.out.println("    ____________________________________________________________");
+            }
+        }
+
     }
 }
