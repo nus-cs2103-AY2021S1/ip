08ae3d8709011b50356864b5821464977f563a21
Hazel1603 <tan.hazel.2ooo@gmail.com>
2020-08-17 12:28:59 +0800

    Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,8 +8,32 @@ public class Duke {
     public static String addedMsg = "Alright, I've added a new order: ";
     public static ArrayList<Task> list = new ArrayList<>();
 
-    public static void addItem(Task input) {
-        list.add(input);
+    public static Task addItem(String input) throws DukeException {
+        String arr[] = input.split(" ", 2);
+        Task curr = new Task("");
+        if (arr.length == 1) {
+            throw new DukeException("The description of a " + arr[0] + " cannot be empty!");
+        } else if (arr[0].equals("todo")) {
+            curr = new ToDo(arr[1]);
+            list.add(curr);
+        } else if (arr[0].equals("deadline")) {
+            String info[] = arr[1].split("/by", 2);
+            if (info.length == 1) {
+                throw new DukeException("Deadline not provided!");
+            } else {
+                curr = new Deadline(info[0], info[1]);
+                list.add(curr);
+            }
+        } else if (arr[0].equals("event")) {
+            String info[] = arr[1].split("/at", 2);
+            if (info.length == 1) {
+                throw new DukeException("Time not provided!");
+            } else {
+                curr = new Event(info[0], info[1]);
+                list.add(curr);
+            }
+        }
+        return curr;
     }
 
 
@@ -26,34 +50,19 @@ public class Duke {
 
         while (!nextLine.equals("bye")) {
             if (nextLine.contains("todo") || nextLine.contains("deadline") || nextLine.contains("event")) {
-                Task curr = new Task("");
-                if(nextLine.contains("todo")){ // Case 1: checking to-do item
-                    String arr[] = nextLine.split(" ", 2);
-                    curr = new ToDo(arr[1]);
-                    addItem(curr);
+                try {
+                    Task curr = addItem(nextLine);
 
-//                    nextLine = scanner.nextLine();
-                } else if (nextLine.contains("deadline")) { // Case 2: Checking deadline
-                    String arr[] = nextLine.split(" ", 2);
-                    String info[] = arr[1].split("/by", 2);
-                    curr = new Deadline(info[0], info[1]);
-                    addItem(curr);
-
-//                    nextLine = scanner.nextLine();
-                } else if (nextLine.contains("event")) { // Case 3: checking event
-                    String arr[] = nextLine.split(" ", 2);
-                    String info[] = arr[1].split("/at", 2);
-                    curr = new Event(info[0], info[1]);
-                    addItem(curr);
-
-//                    nextLine = scanner.nextLine();
+                    System.out.println(line);
+                    System.out.println(addedMsg);
+                    System.out.println(curr);
+                    System.out.println("You have ordered " + list.size() + " items.");
+                    System.out.println(line);
+                } catch (DukeException ex1) {
+                    System.out.println(line);
+                    System.out.println(ex1);
+                    System.out.println(line);
                 }
-
-                System.out.println(line);
-                System.out.println(addedMsg);
-                System.out.println(curr);
-                System.out.println("You have ordered " + list.size() + " items.");
-                System.out.println(line);
                 nextLine = scanner.nextLine();
             } else if (nextLine.contains("done")) { // Case 4: checking off an item
                 String arr[] = nextLine.split(" ", 2);
@@ -62,30 +71,30 @@ public class Duke {
                 if (index > list.size()) {
                     System.out.println(defaultError);
                 } else {
-                    list.get(index-1).markAsDone();
+                    list.get(index - 1).markAsDone();
 
                     System.out.println(line);
                     System.out.println("Great choice! I have taken your order: ");
-                    System.out.println(list.get(index-1));
+                    System.out.println(list.get(index - 1));
                     System.out.println(line);
                 }
                 nextLine = scanner.nextLine();
             } else if (nextLine.equals("list")) { // Case 5: Displaying List
                 System.out.println(line);
                 System.out.println("Here's what you have ordered so far...");
-                for(int k = 0; k < list.size(); k++) {
-                    System.out.println((k+1) + ": " + list.get(k));
+                for (int k = 0; k < list.size(); k++) {
+                    System.out.println((k + 1) + ": " + list.get(k));
                 }
                 System.out.println((line));
                 nextLine = scanner.nextLine();
-            }
-            else { // Case 6: Default errors
+            } else { // Case 6: Default errors
                 System.out.println(line);
                 System.out.println(defaultError);
                 System.out.println(line);
                 nextLine = scanner.nextLine();
             }
         }
+
 
         // Ending the bot
         System.out.println(line);
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,12 @@
+public class DukeException extends Exception {
+    String description;
+
+    public DukeException(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Sorry (not sorry)!!" + description;
+    }
+}
