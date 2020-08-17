a26e92591d4892a82fb06b9ebe4cc084a946c31b
CalistaIo <e0426281@u.nus.edu>
2020-08-17 20:28:36 +0800

    Added Level-2 of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static String[] lst = new String[100];
+    private static int counter = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,12 +21,22 @@ public class Duke {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         while (!s.equals("bye")) {
-            System.out.println("-------------------------------------------------------------------------------------");
-            System.out.println(s);
-            System.out.println("-------------------------------------------------------------------------------------");
+            if (s.equals("list")) {
+                System.out.println("-------------------------------------------------------------------------------------");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ". " + lst[i]);
+                }
+                System.out.println("-------------------------------------------------------------------------------------");
+            } else {
+                System.out.println("-------------------------------------------------------------------------------------");
+                System.out.println("added: " + s);
+                System.out.println("-------------------------------------------------------------------------------------");
+                lst[counter] = s;
+                counter += 1;
+            }
             s = scan.nextLine();
         }
-        
+
         System.out.println("-------------------------------------------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("-------------------------------------------------------------------------------------");
