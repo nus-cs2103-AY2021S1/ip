3259d6df7e8deea073562f16ef0cdf72231f6464
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-08-17 16:36:17 +0800

    Add Level 3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,7 +15,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello, my name is\n" + logo);
-        System.out.println("What can I do for you");
+        System.out.println("What can I do for you?");
 
         while (!quitProgram) {
             // blocks program until input is received
@@ -25,6 +25,13 @@ public class Duke {
                 quitProgram = true;
             } else if (newInput.equals("list")) {
                 taskList.displayTasks();
+            } else if (newInput.length() > 5 && newInput.substring(0, 5).equals("done ")) {
+                int taskIndex = Integer.parseInt(newInput.substring(5));    // this is not corrected for 0 index
+                Task completedTask = taskList.getTask(taskIndex);
+                completedTask.markAsDone();
+
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(completedTask);
             } else {
                 taskList.addTask(newInput);
                 System.out.println("added: " + newInput);
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
 
 public class TaskList {
-    private final ArrayList<String> taskList;
+    private final ArrayList<Task> taskList;
 
     TaskList() {
         taskList = new ArrayList<>();
     }
 
-    public void addTask(String task){
-        taskList.add(task);
+    public void addTask(String taskDesc){
+        taskList.add(new Task(taskDesc));
+    }
+
+    public Task getTask(int index) {
+        // index is bounded from 0 to len - 1
+        return taskList.get(index - 1);
     }
 
     public void displayTasks() {
+        System.out.println("Here are the tasks in your list: ");
         for (int i = 0; i < taskList.size(); i++) {
             System.out.println((i + 1) + ". " + taskList.get(i));
         }
