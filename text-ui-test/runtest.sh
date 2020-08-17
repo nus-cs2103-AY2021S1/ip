80fe1c8c64a85c102f8eadfaff29b201f623070e
lll-jy <E0376997@u.nus.edu>
2020-08-17 13:47:18 +0800

    Shorten compiling command

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Task.java ../src/main/java/ToDo.java ../src/main/java/Deadline.java ../src/main/java/Event.java ../src/main/java/InvalidCommandException.java ../src/main/java/Duke.java
+if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
