0741667a5beb4817727c69170365b085a3b10d8c
yanbingtao <yanbingtao963@Gmail.com>
2020-08-17 21:35:11 +0800

    Implemented doneTask, InvalidDoneTaskIndex methods, and change 'tasks' ArrayList type from ArrayList<String> to ArrayList<Task>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,25 +5,26 @@ public class Duke {
 
     public static void greeting() {
         String greetingMsg = "\n___________________________________________________________"
-                + "\n Hello! I'm DukeBT. |°‿°|"
+                + "\n |°‿°|"
+                + "\n Hello! I'm DukeBT."
                 + "\n What can I do for you?"
-                + "\n\n **Type 'help' to see what I can do |^_^|"
+                + "\n\n **Type 'help' to see what I can do. |^_^|"
                 + "\n___________________________________________________________\n";
         System.out.println(greetingMsg);
     }
 
-    public static ArrayList<String> addTask(String inputMsg, ArrayList<String> tasks) {
+    public static ArrayList<Task> addTask(String inputMsg, ArrayList<Task> tasks) {
         String outputMsg = "\n___________________________________________________________"
-                + "\n added: " + inputMsg
+                + "\n (^.^)"
+                + "\n Task added: " + inputMsg
                 + "\n___________________________________________________________\n";
-
         System.out.println(outputMsg);
 
-        tasks.add(inputMsg);
+        Task.createTask(inputMsg, tasks);
         return tasks;
     }
 
-    public static void getAllTasks(ArrayList<String> tasks) {
+    public static void getAllTasks(ArrayList<Task> tasks) {
         String outputMsg = "";
 
         if (tasks.isEmpty()) {
@@ -45,6 +46,17 @@ public class Duke {
         System.out.println(outputMsg);
     }
 
+    public static void doneTask(int index, ArrayList<Task> tasks) {
+        tasks.get(index - 1).markAsDone();
+        String outputMsg = "___________________________________________________________"
+                + "\n (ﾉﾟ0ﾟ)ﾉ~"
+                + "\n Congratulations from DukeBT! You have done 1 task!"
+                + "\n The task below has been marked as done:"
+                + "\n      Task #" + index + ". " + tasks.get(index - 1)
+                + "\n___________________________________________________________\n";
+        System.out.println(outputMsg);
+    }
+
     public static void help() {
         String commandList = "**************************************************************"
                 + "\n ┗( ⊙.⊙ )┛ "
@@ -52,8 +64,21 @@ public class Duke {
                 + "\n     1.list -- show all tasks"
                 + "\n     2.help -- show all commands"
                 + "\n     3.bye -- exit the chatbot"
+                + "\n     4.done <task index> -- mark this task as done"
                 + "\n**************************************************************\n";
         System.out.println(commandList);
+    }
+
+
+    public static void invalidDoneTaskIndex(int tasksSize) {
+        String warningMsg = "**************************************************************"
+                + "\n 【• ▂ •】What? "
+                + "\n I couldn't find this task, please re-enter a valid task index!"
+                + "\n And make sure you follow this format - 'done <Task_Index>'."
+                + "\n You have " + tasksSize + " tasks in total."
+                + "\n\n **Type 'help' to see what I can do. |^_^|"
+                + "\n**************************************************************";
+        System.out.println(warningMsg);
     }
 
     public static void bye() {
@@ -67,15 +92,23 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         greeting();
         String inputMsg = sc.nextLine();
+
         while (!inputMsg.equals("bye")) {
             if (inputMsg.equals("list")) {
                 getAllTasks(tasks);
             } else if (inputMsg.equals("help")) {
                 help();
+            } else if (inputMsg.startsWith("done")) {
+                try {
+                    int index = Integer.parseInt(inputMsg.split(" ")[1]);
+                    doneTask(index, tasks);
+                } catch (IndexOutOfBoundsException e) {
+                    invalidDoneTaskIndex(tasks.size());
+                }
             } else {
                 tasks = addTask(inputMsg, tasks);
             }
