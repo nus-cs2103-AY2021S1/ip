7541095e62484c57a5d6a2842d0be4a667a90be2
Preston Toh <prestontoh@gmail.com>
2020-08-17 22:01:41 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,19 +19,41 @@ public class Duke {
 
             } else if (next.equals("list")) {
                 for (int i = 0; i < noOfTasks; i++) {
-                    System.out.println(tasks[i]);
+                    System.out.println(i + 1 + ". " + tasks[i]);
                 }
             } else if (next.equals("done")) {
                 int doneTask = Integer.parseInt(inputs.next()) - 1;
                 tasks[doneTask].setDone();
                 System.out.println("Nice! I've set this task as done~\n" +
                         tasks[doneTask]);
-            } else {
-                Task newTask = new Task(next, noOfTasks + 1);
+            } else if (next.equals("todo")) {
+                Todo newTask = new Todo(inputs.nextLine());
                 tasks[noOfTasks] = newTask;
                 noOfTasks += 1;
-                System.out.println("Added: " + next);
+                System.out.println("Got it~ I've added this task:\n"
+                        + newTask + "\n" + "You now have " + noOfTasks + " tasks in the list~");
+            } else if (next.equals("deadline")) {
+                String[] deadlineSplit = inputs.nextLine().split("/by");
+                Deadline newTask = new Deadline(deadlineSplit[0], deadlineSplit[1]);
+                tasks[noOfTasks] = newTask;
+                noOfTasks += 1;
+                System.out.println("Got it~ I've added this task:\n"
+                        + newTask + "\n" + "You now have " + noOfTasks + " tasks in the list~");
+            } else if (next.equals("event")) {
+                String[] eventSplit = inputs.nextLine().split("/at");
+                Event newTask = new Event(eventSplit[0], eventSplit[1]);
+                tasks[noOfTasks] = newTask;
+                noOfTasks += 1;
+                System.out.println("Got it~ I've added this task:\n"
+                        + newTask + "\n" + "You now have " + noOfTasks + " tasks in the list~");
             }
+
+//            } else {
+//                Task newTask = new Task(next, noOfTasks + 1);
+//                tasks[noOfTasks] = newTask;
+//                noOfTasks += 1;
+//                System.out.println("Added: " + next);
+//            }
             System.out.println("____________________________________________________________");
         }
     }
