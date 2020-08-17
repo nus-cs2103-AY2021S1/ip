00a2f522c157612a48c1bcf3df0b295cb1e5b192
Mehak Virdi <Mehak@Mehaks-MacBook-Pro.local>
2020-08-17 21:06:50 +0800

    completed level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void start(Scanner scanner) {
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        String line = scanner.nextLine();
+        while(!line.equals("bye")) {
+            System.out.println(line);
+            line = scanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +20,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        Duke.start(scanner);
     }
 }
