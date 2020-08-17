a337f8152630f306e12e0dc77439b57fc76a7327
hogantan <hogan_tan@hotmail.sg>
2020-08-17 18:08:18 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 public class Duke {
     //Characteristics of Duke
     private boolean isChatting;
-    private ArrayList<String> todoList;
+    private ArrayList<Task> todoList;
 
     //Editable Strings for system messages
     private static String greeting = "Hello! I'm Duke\n What can I do for you?";
     private static String parting = "Bye. Hope to see you again soon!";
-    private static String error_msg = "Please enter an appropriate command!";
+    private static String inappropriateCommandMsg = "Please enter an appropriate command!";
     private static String add_to_list = "added: ";
+    private static String getListMsg = "Here are the tasks in your list:";
+    private static String markDoneMsg = "Nice! I've marked this task as done: ";
 
     //Constructor
-    Duke() {
+    public Duke() {
         //Characteristic of Duke
         this.isChatting = true;
         this.todoList = new ArrayList<>();
@@ -33,7 +35,7 @@ public class Duke {
             String user_input = sc.nextLine();
 
             //Conditionals for user inputs
-            //Return toDoList
+            //list command
             if (user_input.equals("list")) {
                 System.out.println(getToDoList());
             }
@@ -41,15 +43,20 @@ public class Duke {
             else if (user_input.equals("blah")) {
                 System.out.println(user_input);
             }
-            //Exit Duke
+            //bye command
             else if (user_input.equals("bye")) {
                 System.out.println(parting);
                 this.isChatting = false;
             }
-            //Add to todoList
-            else {
-                this.todoList.add(user_input);
+            //add command
+            else  if (!user_input.contains("done")){
+                Task toAdd = new Task(user_input);
+                this.todoList.add(toAdd);
                 System.out.println(add_to_list + user_input);
+            }
+            //done command
+            else {
+                markTaskDone(user_input);
             }
         }
     }
@@ -58,18 +65,48 @@ public class Duke {
     private String getToDoList() {
         String output ="";
         for (int i = 0 ; i < todoList.size() ; i++) {
+            Task currentTask = todoList.get(i);
             if (i == todoList.size() - 1) {
-                output = output + (i + 1) + ". " + todoList.get(i);
+                output = output + (i + 1) + ".[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription();
             } else {
-                output = output + (i + 1) + ". " + todoList.get(i) + "\n";
+                output = output + (i + 1) + ".[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription() + "\n";
             }
         }
+        System.out.println(getListMsg);
         return output;
+    }
+
+    //Done command
+    //Check if user input done keyword is located in the correct place
+    //Then, check if user input contains apt integer
+    private void markTaskDone(String user_input) {
+        //Error Messages to guide user how to correct incorrect input
+        String outOfBoundsMsg = "That item does not exist in your list! The input number has exceeded your list size!";
+        String numberFormatMsg = "Please key in an appropriate number!";
+
+        //Checking if done keyword is located in the correct place
+        if (user_input.substring(0, 4).contains("done")) {
+            String int_substring = user_input.substring(5);
+            try {
+                int int_substring_converted = Integer.parseInt(int_substring);
+                Task currentTask = this.todoList.get(int_substring_converted - 1);
+                currentTask.markAsDone();
+                System.out.println(markDoneMsg);
+                System.out.println("  [" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
+            } catch (NumberFormatException ex){
+                System.out.println(numberFormatMsg);
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println(outOfBoundsMsg);
+            }
+        } else {
+            System.out.println(inappropriateCommandMsg);
+        }
     }
 
     public static void main(String[] args) {
         //Initialise Duke
         Duke chatBot = new Duke();
+        //Start chatting with Bot
         chatBot.startChat();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+//A task represents an item in Duke's todoList
+//A task consists of:
+// 1) Content Description
+// 2) Status (isDone or not)
+
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    //Constructor
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    //return tick or cross symbol accordingly
+    public String getStatusIcon() {
+        if (isDone) {
+            return "\u2713";
+        } else {
+            return "\u2718";
+        }
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+}
