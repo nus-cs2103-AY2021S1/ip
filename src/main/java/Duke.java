1a3385890439cee63e91f96862ff23adf970045f
Clara Adora <claraadora@gmail.com>
2020-08-18 00:59:53 +0800

    feat: enable storing and displaying of commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -10,15 +13,27 @@ public class Duke {
         System.out.println("Hello! I'm Clara! :D How may I help you? :)");
 
         Scanner sc = new Scanner(System.in);
+        List<String> commands = new ArrayList<>();
 
         while (sc.hasNext()) {
             String command = sc.nextLine();
 
+            switch(command) {
+                case "bye":
+                    System.out.println("\tBye! Have a great day and hope to see you soon! :D");
+                    break;
+                case "list":
+                    for (int i = 0; i < commands.size(); i++) {
+                        System.out.println(String.format("\t%d. %s", i+1, commands.get(i)));
+                    }
+                    break;
+                default:
+                    commands.add(command);
+                    System.out.println("\tadded: " + command);
+            }
+
             if (command.equals("bye")) {
-                System.out.println("Bye! Have a great day and hope to see you soon! :D");
                 break;
-            } else {
-               System.out.println(">>> " + command);
             }
         }
 
