9b129a8226d993ec7d71532496d30d8ece10226a
Lye Yi Xian <lyeyx999@hotmail.com>
2020-08-17 18:52:56 +0800

    Created class Event

---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+public class Event extends Task {
+  String at;
+
+  public Event(String description, String at) {
+    super(description);
+    this.at = at;
+  }
+
+  @Override
+  public String toString() {
+    return "[E]" + super.toString() + " (at: " + at + ")";
+  }
+}
