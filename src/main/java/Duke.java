c388273996b3ed2f2bd2ad5d9685d923698496ef
foongsiqi <edu.foongs@gmail.com>
2020-08-17 11:29:46 +0800

    implement feature to save input to an arraylist

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Yooo, I'm Deke.\nWhat can I do for you today?");
+        System.out.println("Yooo, I'm Deke.\nWhat can I do for you today?"); //Greeting
+
+        ArrayList<String> list = new ArrayList<>();
         String input = sc.nextLine();
+
         while (!input.isEmpty()) {
             if (input.equals("bye")) { //if user types "bye"
                 System.out.println("Bye bye!!! See you again next time :)");
                 input = "";
                 sc.close();
-            } else { //if user types anything other than "bye"
-                System.out.println("you said: " + input);
+            } else if (input.equals("list")) { //if user types "list"
+                //do a for loop to print out the list
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i + 1 + ". " + list.get(i));
+                }
+                input = sc.nextLine();
+            } else { //if user types anything other than "bye" or "list"
+                list.add(input);
+                System.out.println("added: " + input);
                 input = sc.nextLine();
             }
         }
