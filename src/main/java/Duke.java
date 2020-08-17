b9df6691d988f92206e66d2f811cfa42c25de162
WM71811 <55304760+WM71811@users.noreply.github.com>
2020-08-17 23:22:44 +0800

    implement level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
 public class Duke {
+    private static final String logo = "      ____        _        \n"
+            + "     |  _ \\ _   _| | _____ \n"
+            + "     | | | | | | | |/ / _ \\\n"
+            + "     | |_| | |_| |   <  __/\n"
+            + "     |____/ \\__,_|_|\\_\\___|\n";
+
+    private static final Scanner s = new Scanner(System.in);
+    private static final String line = "     _____________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        while (s.hasNextLine()) {
+            String userCommand = s.nextLine();
+            if (userCommand.equals("bye")) {
+                exit();
+                break;
+            } else {
+                echo(userCommand);
+            }
+        }
     }
+        private static void greet() {
+            System.out.println(line);
+            System.out.println("     Hi, I am\n" + logo);
+            System.out.println("     Is there anything I could help with?");
+            System.out.println(line);
+        }
+
+        private static void echo(String command) {
+            System.out.println(line);
+            System.out.println("     " + command);
+            System.out.println(line);
+        }
+
+        private static void exit() {
+            System.out.println(line);
+            System.out.println("     Bye! I look forward to meeting you next time!");
+            System.out.println(line);
+        }
 }
