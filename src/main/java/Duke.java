51a3fbe089063255de25e43abd7ef95d00efdf33
Tan Yan Lyn <e0424594@u.nus.edu>
2020-08-17 21:40:42 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,24 +12,57 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello! I'm Duke \n"
-                            + "What can I do for you?");
+                + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
-        List<String> inputs = new ArrayList<>();
-        String input = "";
+        List<Input> inputs = new ArrayList<>();
         while (true) {
-            input = sc.nextLine();
-            if (!input.equals("bye") && !input.equals("list")) {
-                inputs.add(input);
-                System.out.println("added: " + input);
-            } else if (input.equals("list")) {
-                int len = inputs.size();
-                for (int i = 1; i <= len; i++) {
-                    System.out.println(i + ". " + inputs.get(i-1));
-                }
+            String nextLine = sc.nextLine();
+            if (nextLine.startsWith("done")) {
+                int numTaskDone = Integer.valueOf(nextLine.substring(5));
+                System.out.println("Nice! I've marked this task as done:");
+                if (numTaskDone < inputs.size()) {
+                    inputs.get(numTaskDone - 1).taskDone();
+                    System.out.println("[/] " + inputs.get(numTaskDone -1).content);
+                } else {}
             } else {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
+                Input input = new Input(nextLine);
+                if (!input.content.equals("bye") && !input.content.equals("list")) {
+                    inputs.add(input);
+                    System.out.println("added: " + input.content);
+                } else if (input.content.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    int len = inputs.size();
+                    for (int i = 1; i <= len; i++) {
+                        if (inputs.get(i-1).done) {
+                            System.out.println(i + ". [/] " + inputs.get(i - 1).content);
+                        } else {
+                            System.out.println(i + ". [x] " + inputs.get(i - 1).content);
+                        }
+                    }
+                } else {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                }
             }
+        }
+    }
+
+
+    public static class Input {
+        boolean done;
+        String content;
+
+        Input(String content) {
+            this.content = content;
+            boolean done = false;
+        }
+        Input(boolean done, String content) {
+            this.done = done;
+            this.content = content;
+        }
+
+        void taskDone() {
+            this.done = true;
         }
     }
 }
