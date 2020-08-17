693d753d6e1517237f8077b97a0da09eecde00be
Tan Su Yin <e0425292@u.nus.edu>
2020-08-17 15:24:52 +0800

    Implement skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    public static String dashedLineBreak() {
+        String dashedLine = "- ";
+        return dashedLine.repeat(24);
+    }
+
+    public static void init() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm your loyal Duke.");
+        System.out.println("How may I serve you, my Queen?");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        init();
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String userInput = sc.nextLine();
+            if (!userInput.equalsIgnoreCase("bye")) {
+                System.out.println(userInput);
+                System.out.println(dashedLineBreak());
+            } else {
+                System.out.println("Your wish is my command, milady. Till I see you again.");
+                sc.close();
+                System.exit(0);
+            }
+        }
     }
 }
