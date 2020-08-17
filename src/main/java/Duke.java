0f39acb657f0f3d4b7cd52b6469f3ee86d679749
michael-setia <michaelsetia55@gmail.com>
2020-08-17 17:50:48 +0700

    Add add and list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,19 +9,34 @@ public class Duke {
         line.append("_".repeat(50));
 
         // Print greeting message.
-        System.out.println(line + "\n" + "Hey there! I am Popi" + "\n"
-            + "How can I help you?" + "\n" + line + "\n");
+        System.out.println(line + "\n" + " Hey there! I am Popi" + "\n"
+            + " How can I help you?" + "\n" + line + "\n");
+
+        // Array to store text(s)
+        String[] storage = new String[100];
+        int counter = 0;
 
         // Process user input(s).
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println(line + "\n" + "Bye! Hope to see you again in the future!"
+                System.out.println(line + "\n" + " Bye! Hope to see you again in the future!"
                     + "\n" + line + "\n");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < storage.length; i++) {
+                    if (storage[i] == null) {
+                        break;
+                    }
+                    System.out.println(" " + (i + 1) + ". " + storage[i]);
+                }
+                System.out.println(line);
             } else {
-                System.out.println(line + "\n" + input + "\n" + line + "\n");
+                storage[counter] = input;
+                counter++;
+                System.out.println(line + "\n" + " added: " + input + "\n" + line);
             }
         }
     }
