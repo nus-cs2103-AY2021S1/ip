36d478250ebd17e7cf23fe9b27f81e1e74a5a721
Nahoyhp <phyohan@hotmail.com>
2020-08-17 21:55:29 +0800

    modify TaskManager to accomodate the Status of Task

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -14,11 +14,19 @@ public class TaskManager {
         this.tasks.add(new Task(taskName));
     }
 
+    public void doTask(int index) {
+        this.tasks.get(index-1).setDone();
+    }
+
+    public String getTaskStatus(int index) {
+        return tasks.get(index-1).toString();
+    }
+
     @Override
     public String toString() {
         String output = "";
         for (int i = 1; i <= tasks.size(); i++) {
-            output += i + ". " + tasks.get(i-1).toString();
+            output += i + "." + tasks.get(i-1).toString();
             if (i != tasks.size()) {
                 output += "\n";
             }
