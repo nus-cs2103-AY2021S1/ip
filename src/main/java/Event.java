999857ba9d80ee0b44099e3b5f1f42d2db53d8c7
Sean Tay <seantaysl@gmail.com>
2020-08-17 17:46:14 +0800

    Add Deadline.java, Event.java, and ToDo.java

---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+public class Event extends Task {
+    protected String at;
+
+    public Event(String name, String at) {
+        super(name);
+        this.at = at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(at: " + at + ")";
+    }
+}
