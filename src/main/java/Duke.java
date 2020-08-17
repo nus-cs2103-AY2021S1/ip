7a7af9e63a975de1971d672e7373165b44792898
Soon Xiang, Yan <65216986+y4ns00n@users.noreply.github.com>
2020-08-17 18:23:28 +0800

    added level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,11 +11,38 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         Scanner scan1 = new Scanner(System.in);
+        ArrayList<String> storage = new ArrayList<>();
+        int count = 1;
 
+        /* level 1
         while (scan1.hasNext()) {
             String input = scan1.next();
             if (!input.equals("bye")) {
                 System.out.println(input);
+            } else {
+                String bye = "Bye. Hope to see you again soon!";
+                System.out.println(bye);
+                scan1.close();
+                break;
+            }
+        }
+        */
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        while (scan1.hasNext()) {
+            String input = scan1.next();
+            if (!input.equals("bye") && !input.equals("list")) {
+                storage.add(input);
+                String added = "added: " + input;
+                System.out.println(added);
+            } else if (input.equals("list")) {
+                for (String item: storage) {
+                    System.out.println(count + ". " + item);
+                    count++;
+                }
+
             } else {
                 String bye = "Bye. Hope to see you again soon!";
                 System.out.println(bye);
