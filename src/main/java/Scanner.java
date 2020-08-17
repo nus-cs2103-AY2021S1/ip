18ed2cfc543783f92950fb7a170ebc07416c09f6
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-08-17 17:45:45 +0800

    Refactor scanner loop

--- src/main/java/Scanner.java
+++ src/main/java/Scanner.java
@@ -6,27 +6,23 @@ public class Scanner {
     static void scan() throws IOException {
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader((System.in)));
-
         ItemList userItemList = new ItemList();
 
+        scanLoop:
         while(true) {
             String line = reader.readLine();
-            // when ctrl-D is pressed, reader returns null
-            if (line == null) {
-                break;
+            switch(line) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again");
+                    break scanLoop;
+                case "list":
+                    userItemList.printList();
+                    break;
+                default:
+                    userItemList.addItem(line);
+                    System.out.println("added: " + line);
+                    break;
             }
-            if (line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again");
-                break;
-            }
-
-            if (line.equals("list")) {
-                userItemList.printList();
-                // don't break even after printing
-            }
-
-            userItemList.addItem(line);
-            System.out.println("added: " + line);
         }
     }
 }
