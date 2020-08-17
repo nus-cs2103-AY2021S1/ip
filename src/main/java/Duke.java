8dd81db2a5dee9c1246938cf4ce5ad9713c0479f
Maxx <maxxyh@gmail.com>
2020-08-18 03:22:18 +0800

    Level-1. Greet, Echo, Exit
    - A skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.
    - Changed name to Alpha

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "   ___      _      _ __   _              \n" +
+                "  /   \\    | |    | '_ \\ | |_     __ _   \n" +
+                "  | - |    | |    | .__/ | ' \\   / _` |  \n" +
+                "  |_|_|   _|_|_   |_|__  |_||_|  \\__,_|  \n" +
+                "_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n" +
+                "\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' ";
         System.out.println("Hello from\n" + logo);
+
+        String greeting = "    ____________________________________________________________\n" +
+                "     Hello, Alpha here... welcome to my help centre... again :/\n" +
+                "     Would you like to explain what you want?\n" +
+                "    ____________________________________________________________";
+        System.out.println(greeting);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        while (!input.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.print("    ");
+            respondToInput(input);
+            System.out.println("    ____________________________________________________________");
+            input = sc.next();
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Try not to come again please... let me live.");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    private static void respondToInput(String input) {
+        System.out.println(input);
     }
 }
