be15b49038ee48c8137d416bd889436484e9fc05
Zhu Yuxuan <e0424709@u.nus.edu>
2020-08-17 21:08:59 +0800

    Add Task.java, Duke.java: add markAsDone method, import java.util.ArrayList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private final static ArrayList<String> list = new ArrayList<>();
+    private final static ArrayList<Task> list = new ArrayList<>();
 
     public static void start() {
         System.out.println("Hello! I'm Duke");
@@ -16,14 +16,32 @@ public class Duke {
             for (int i = 1; i < list.size() + 1; i++) {
                 System.out.println(i + ". " + list.get(i - 1));
             }
+        } else if (input.startsWith("done ") || input.equals("done")) {
+            if (input.length() > 5) {
+                String task = input.substring(5);
+                markAsDone(task);
+            } else {
+                System.out.println("Which task do you want to mark as done?");
+            }
         } else {
             addTask(input);
         }
     }
 
     public static void addTask(String input) {
-        list.add(input);
+        list.add(new Task(input));
         System.out.println("added: " + input);
+    }
+
+    public static void markAsDone(String task) {
+        int index = Integer.parseInt(task) - 1;
+        if (index > -1 && index < list.size()) {
+            list.get(index).done();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(list.get(index));
+        } else {
+            System.out.println("This task is not in your list");
+        }
     }
 
     public static void exit() {
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected final String name;
+    protected boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void done() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        if (this.isDone) {
+            return "[\u2713] " + this.name;
+        } else {
+            return "[\u2718] " + this.name;
+        }
+    }
+}
