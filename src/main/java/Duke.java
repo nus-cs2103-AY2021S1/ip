ad7758a2624a105190b30d5088ddbe5f0e06b4c3
angrybunny123 <kennethteo98@gmail.com>
2020-08-17 18:19:42 +0800

    used the ArrayList collection to implement level 6.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    private static Task[] tasks = new Task[100];
-    private static int count = 0;
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void start() {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
@@ -16,21 +16,26 @@ public class Duke {
                     break;
                 } else if (input.equals("list")) {
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < tasks.length; i++) {
-                        if (tasks[i] != null) {
-                            System.out.println((i + 1) + ". " + tasks[i].toString());
-                        } else {
-                            break;
-                        }
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i).toString());
                     }
                 } else if (splitArr.length == 2 && splitArr[0].equals("done") && Integer.parseInt(splitArr[1]) > 0) {
                     int index = Integer.parseInt(splitArr[1]);
-                    if (index > count || index < 0) {
+                    if (index > tasks.size() || index < 0) {
                         throw new DukeException("OOPS!!! That task number does not exist.");
                     }
-                    tasks[index - 1].setDone();
+                    tasks.get(index - 1).setDone();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks[index - 1].toString());
+                    System.out.println(tasks.get(index - 1).toString());
+                } else if (splitArr.length == 2 && splitArr[0].equals("delete") && Integer.parseInt(splitArr[1]) > 0) {
+                    int index = Integer.parseInt(splitArr[1]);
+                    if (index > tasks.size() || index < 0) {
+                        throw new DukeException("OOPS!!! That task number does not exist.");
+                    }
+                    Task deletedTask = tasks.remove(index - 1);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(deletedTask.toString());
+                    System.out.println("Now, you have " + tasks.size() + " tasks in the list");
                 } else {
                     switch (splitArr[0]) {
                         case "todo":
@@ -38,9 +43,9 @@ public class Duke {
                                 throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
                             }
                             System.out.println("Got it. I've added this task:");
-                            tasks[count] = new ToDo(input.substring(5));
-                            count++;
-                            System.out.println(tasks[count - 1].toString());
+                            Task newTask = new ToDo(input.substring(5));
+                            tasks.add(newTask);
+                            System.out.println(newTask.toString());
                             break;
                         case "deadline":
                             if (splitArr.length <= 1) {
@@ -53,9 +58,9 @@ public class Duke {
                             System.out.println("Got it. I've added this task:");
                             String desc = input.substring(9, index - 1);
                             String date = input.substring(index + 4);
-                            tasks[count] = new Deadline(desc, date);
-                            count++;
-                            System.out.println(tasks[count - 1].toString());
+                            Task newDeadline = new Deadline(desc, date);
+                            tasks.add(newDeadline);
+                            System.out.println(newDeadline.toString());
                             break;
                         case "event":
                             if (splitArr.length <= 1) {
@@ -68,16 +73,16 @@ public class Duke {
                             System.out.println("Got it. I've added this task:");
                             String des = input.substring(6, ind - 1);
                             String dat = input.substring(ind + 4);
-                            tasks[count] = new Event(des, dat);
-                            count++;
-                            System.out.println(tasks[count - 1].toString());
+                            Task newEvent = new Event(des, dat);
+                            tasks.add(newEvent);
+                            System.out.println(newEvent.toString());
                             break;
                         default:
                             System.out.println("dada");
                             throw new DukeException("OOPS!! I'm sorry, but I don't know what that means :-(");
 
                     }
-                    System.out.println("Now, you have " + count + " tasks in the list");
+                    System.out.println("Now, you have " + tasks.size() + " tasks in the list");
                 }
             }
         } catch (DukeException e) {
