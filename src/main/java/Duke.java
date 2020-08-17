3fa71a81f5805582628d1e5b7e639abf8767607e
MarcusTw <60913700+MarcusTw@users.noreply.github.com>
2020-08-17 14:46:02 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    static final String HORIZONTAL_LINE =
+            "____________________________________________________________";
+    static final String INDENT = "    ";
+
+    public static void printWindow(String s) {
+        System.out.println(INDENT + HORIZONTAL_LINE);
+        System.out.println(INDENT + s);
+        System.out.println(INDENT + HORIZONTAL_LINE);
     }
+
+    public static void printBye() {
+        System.out.println(INDENT + HORIZONTAL_LINE);
+        System.out.println(INDENT + "Bye. Hope to see you again soon!");
+        System.out.print(INDENT + HORIZONTAL_LINE);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(INDENT + HORIZONTAL_LINE);
+        System.out.println("    Hello! I'm Duke\n" +
+                "    What can I do for you?");
+        System.out.println(INDENT + HORIZONTAL_LINE);
+
+        while (sc.hasNext()) {
+            String nextLine = sc.nextLine();
+            if (nextLine.equals("bye")) {
+                printBye();
+                break;
+            } else {
+                printWindow(nextLine);
+            }
+        }
+    }
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
 }
