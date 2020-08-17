f1387c3a34613ae8b7ae410a52adcfed4cb96dca
erinmayg <erinmaygunawan@gmail.com>
2020-08-18 01:36:54 +0800

    Abstraction.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static String line = "____________________________________________________________";
+    private static final String line = "____________________________________________________________";
 
-    private String name = "Bolot";
-    private String end = "bye";
+    private final String name = "Bolot";
+    private final String end = "bye";
     private ArrayList<Task> list = new ArrayList<>();
+
+    public void printLogo() {
+        System.out.println("Greetings, human. I am");
+        System.out.println(" ______      ___   _____       ___    _________");
+        System.out.println("|_   _ \\   .'   `.|_   _|    .'   `. |  _   _  |");
+        System.out.println("  | |_) | /  .-.  \\ | |     /  .-.  \\|_/ | | \\_|");
+        System.out.println(" _| |__) |\\  `-'  /_| |__/ |\\  `-'  /   _| |_");
+        System.out.println("|_______/  `.___.'|________| `.___.'   |_____|");
+    }
 
     public void greet() {
         System.out.println(line);
@@ -39,93 +48,99 @@ public class Duke {
         bye();
     }
 
-    public void addList() {
+    private void printList() {
+
+        if (list.size() == 0) {
+            System.out.println("You have nothing on your list!");
+        }
+
+        int i = 1;
+        for (Task todo: list) {
+            System.out.println(String.format("%d. %s", i, todo.toString()));
+            i++;
+        }
+    }
+
+    private void addTask(String firstWord, String input) {
+
+        String task = firstWord.equals("todo")
+                ? input.substring(firstWord.length() + 1)
+                : input.substring(firstWord.length() + 1, input.indexOf('/'));
+
+        String date = firstWord.equals("todo")
+                ? null
+                : input.substring(input.indexOf('/') + 4);
+
+        System.out.println("Got it. I've added this task:");
+
+        switch (firstWord) {
+            case "todo":
+                list.add(new ToDo(task));
+                break;
+            case "deadline":
+                list.add(new Deadline(task, date));
+                break;
+            case "event":
+                list.add(new Event(task, date));
+                break;
+        }
+
+        System.out.println(list.get(list.size() - 1).toString());
+
+        String taskText = list.size() <= 1 ? " task " : " task ";
+        System.out.println("Now you have " + list.size() + taskText + "in the list.");
+    }
+
+    private void markDone(int taskNo) {
+        list.set(taskNo, list.get(taskNo).markDone());
+
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(list.get(taskNo));
+    }
+
+    public void addToList() {
         Scanner sc = new Scanner(System.in);
 
-        String type = sc.nextLine();
+        String input = sc.nextLine();
 
-        while (!type.equalsIgnoreCase(end)) {
+        while (!input.equalsIgnoreCase(end)) {
 
             System.out.println(line);
 
-            if (type.equalsIgnoreCase("list")) {
-                int i = 1;
-                for (Task todo: list) {
-                    System.out.println(String.format("%d. %s", i, todo.toString()));
-                    i++;
-                }
+            if (input.trim().equalsIgnoreCase("list")) {
+
+                printList();
+
             } else {
-                String firstWord = type.contains(" ")
-                        ? type.substring(0, type.indexOf(" ")).toLowerCase()
-                        : type;
+                String firstWord = input.contains(" ")
+                        ? input.substring(0, input.indexOf(" ")).toLowerCase()
+                        : input;
 
                 if (firstWord.equals("done")) {
-                    int taskNo = Integer.parseInt(type.substring(5)) - 1;
-                    list.set(taskNo, list.get(taskNo).markDone());
 
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(list.get(taskNo));
+                    int taskNo = Integer.parseInt(input.substring(5)) - 1;
+                    markDone(taskNo);
 
                 } else {
 
-                    String task = firstWord.equals("todo")
-                                ? type.substring(firstWord.length() + 1)
-                                : type.substring(firstWord.length() + 1, type.indexOf('/'));
+                    addTask(firstWord, input);
 
-                    String date = firstWord.equals("todo")
-                                    ? null
-                                    : type.substring(type.indexOf('/') + 4);
-
-                    System.out.println("Got it. I've added this task:");
-
-                    switch (firstWord) {
-                        case "todo":
-                            list.add(new ToDo(task));
-                            break;
-                        case "deadline":
-                            list.add(new Deadline(task, date));
-                            break;
-                        case "event":
-                            list.add(new Event(task, date));
-                            break;
-                    }
-
-                    System.out.println(list.get(list.size() - 1).toString());
-
-                    String taskText = list.size() <= 1 ? " task " : " task ";
-                    System.out.println("Now you have " + list.size() + taskText + "in the list.");
                 }
             }
 
             System.out.println(line);
 
-            type = sc.nextLine();
+            input = sc.nextLine();
         }
 
         bye();
     }
 
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//
-//        System.out.println("Hello from\n" + logo);
-
-        String logo =
-                " ______      ___   _____       ___    _________\n"
-                + "|_   _ \\   .'   `.|_   _|    .'   `. |  _   _  |\n"
-                + "  | |_) | /  .-.  \\ | |     /  .-.  \\|_/ | | \\_| \n"
-                + " _| |__) |\\  `-'  /_| |__/ |\\  `-'  /   _| |_   \n"
-                + "|_______/  `.___.'|________| `.___.'   |_____|";
-
-        System.out.println("Greetings, human. I am\n" + logo);
-
         Duke bot = new Duke();
+        bot.printLogo();
         bot.greet();
 //        bot.echo();
-        bot.addList();
+        bot.addToList();
     }
 }
