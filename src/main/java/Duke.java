44ffb59c5a7ff4eb617527ff834adf2c67366536
yanbingtao <yanbingtao963@Gmail.com>
2020-08-17 18:46:31 +0800

    add greeting and bye message for level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,14 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String greetingMsg = "\n___________________________________________________________"
+                + "\n Hello! I'm Duke"
+                + "\n What can I do for you?";
+
+        String byeMsg = "\n___________________________________________________________"
+                + "\n Bye. Hope to see you again soon!"
+                + "\n___________________________________________________________\n";
+        System.out.println(logo + greetingMsg + byeMsg);
     }
 }
