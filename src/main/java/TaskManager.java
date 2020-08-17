87b45e930b28124aa3b7a3298c41f19d7ef0c7d7
davidcaiqifan <e0406982@u.nus.edu>
2020-08-17 20:40:31 +0800

    Added "Added :" message

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -6,14 +6,20 @@ public class TaskManager {
     public TaskManager() {
         this.toDoList = new ArrayList<>();
     }
+
     public void addToList(String task) {
         this.toDoList.add(task);
     }
+
     public void displayList() {
         int i = 1;
         for (String s : this.toDoList) {
             System.out.println(i + "." + s);
             i += 1;
         }
+    }
+
+    public void taskPrint(String msg) {
+        System.out.println("Added : re" + msg);
     }
 }
