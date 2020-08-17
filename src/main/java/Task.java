1312be50267dd0534936390fa1fca5c4ed66f044
ruilingk <e0426268@u.nus.edu>
2020-08-17 18:15:26 +0800

    Replace tick and cross symbols with Y and N respectively

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -9,8 +9,8 @@ public class Task {
 
     public String getStatusIcon() {
         return isDone
-                ? "\u2713"
-                : "\u2718"; // return tick or X symbols
+                ? "Y"
+                : "N";
     }
 
     public void markAsDone() {
