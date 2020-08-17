15c36e4d43032b44add375138d00526226808ca2
Gordon <gordonfgz@gmail.com>
2020-08-17 08:49:49 +0800

    Duke.java: replaced default code with a basic skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        String lines = "____________________________________________________________";
+
+        String greeting = "____________________________________________________________\n" +
+                "Hello! I'm Duke\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________";
+
+        System.out.println(greeting);
+        input = sc.next();
+
+        while (!input.equals("bye")) {
+            System.out.println(lines);
+            System.out.println(input);
+            System.out.println(lines);
+            input = sc.next();
+        }
+
+        System.out.println(lines);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(lines);
+
+
     }
+
 }
