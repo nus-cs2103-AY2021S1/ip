4efec3b72bf7d8ede082978bcc4a153683b68df1
davidcaiqifan <e0406982@u.nus.edu>
2020-08-17 21:04:37 +0800

    modified TaskManager to work with task

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -2,22 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
-    private List<String> toDoList;
+    private List<Task> toDoList;
     public TaskManager() {
         this.toDoList = new ArrayList<>();
     }
 
     public void addToList(String task) {
-        this.toDoList.add(task);
+        this.toDoList.add(new Task(task));
     }
 
     public void displayList() {
         int i = 1;
-        for (String s : this.toDoList) {
-            System.out.println(i + "." + s);
+        for (Task s : this.toDoList) {
+            System.out.println(i + "." + s.toString());
             i += 1;
         }
     }
+
+
 
     public void taskPrint(String msg) {
         System.out.println("Added : " + msg);
