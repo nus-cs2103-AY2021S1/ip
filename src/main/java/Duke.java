06d3e72dd19ccbd9916c23ad1fb8fd1300b7df7d
Wong-ZZ <wongzhengzhi@gmail.com>
2020-08-17 20:06:48 +0800

    Added ability to store text entered by users and display them when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String DIVIDER = "____________________________________________________________\n";
+    private static final String DIVIDER = "____________________________________________________________";
+    private static final ArrayList<String> input_list = new ArrayList<>();
 
     public static void main(String[] args) {
         String start_msg =
-            DIVIDER +
+            DIVIDER + "\n" +
             "Hello! I'm Duke\n" +
             "What can I do for you?\n" +
             DIVIDER;
@@ -14,18 +16,33 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
         while(!input.equals("bye")) {
-            System.out.println(get_echo(input));
+            handle_input(input);
             input = sc.next();
         }
 
         String exit_msg =
-            DIVIDER +
+            DIVIDER + "\n" +
             "Bye. Hope to see you again soon!\n" +
             DIVIDER;
         System.out.println(exit_msg);
     }
 
     private static String get_echo(String str) {
-        return DIVIDER + str + "\n" + DIVIDER;
+        String text = "added: " + str + "\n";
+        return DIVIDER + "\n" + text + DIVIDER;
+    }
+
+    private static void handle_input(String in) {
+        if (in.equals("list")) {
+            int len = input_list.size();
+            System.out.println(DIVIDER);
+            for (int i = 0; i < len; i++) {
+                System.out.println(i + ". " + input_list.get(i));
+            }
+            System.out.println(DIVIDER);
+        } else {
+            input_list.add(in);
+            System.out.println(get_echo(in));
+        }
     }
 }
