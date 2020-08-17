1ffd97690854932bd1fc400fb25ce12e4ab30129
Hazel1603 <tan.hazel.2ooo@gmail.com>
2020-08-17 11:24:11 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,11 +5,13 @@ public class Duke {
 
     public static String line = "-------------------------------------";
     public static String defaultError = "Wat talking you?";
+    public static String addedMsg = "Alright, I've added a new order: ";
     public static ArrayList<Task> list = new ArrayList<>();
 
-    public static void addItem(String input) {
-        list.add(new Task(input));
+    public static void addItem(Task input) {
+        list.add(input);
     }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -23,7 +25,37 @@ public class Duke {
         nextLine = scanner.nextLine();
 
         while (!nextLine.equals("bye")) {
-            if (nextLine.contains("done")) {
+            if (nextLine.contains("todo") || nextLine.contains("deadline") || nextLine.contains("event")) {
+                Task curr = new Task("");
+                if(nextLine.contains("todo")){ // Case 1: checking to-do item
+                    String arr[] = nextLine.split(" ", 2);
+                    curr = new ToDo(arr[1]);
+                    addItem(curr);
+
+//                    nextLine = scanner.nextLine();
+                } else if (nextLine.contains("deadline")) { // Case 2: Checking deadline
+                    String arr[] = nextLine.split(" ", 2);
+                    String info[] = arr[1].split("/by", 2);
+                    curr = new Deadline(info[0], info[1]);
+                    addItem(curr);
+
+//                    nextLine = scanner.nextLine();
+                } else if (nextLine.contains("event")) { // Case 3: checking event
+                    String arr[] = nextLine.split(" ", 2);
+                    String info[] = arr[1].split("/at", 2);
+                    curr = new Event(info[0], info[1]);
+                    addItem(curr);
+
+//                    nextLine = scanner.nextLine();
+                }
+
+                System.out.println(line);
+                System.out.println(addedMsg);
+                System.out.println(curr);
+                System.out.println("You have ordered " + list.size() + " items.");
+                System.out.println(line);
+                nextLine = scanner.nextLine();
+            } else if (nextLine.contains("done")) { // Case 4: checking off an item
                 String arr[] = nextLine.split(" ", 2);
                 int index = Integer.parseInt(arr[1]);
 
@@ -38,21 +70,24 @@ public class Duke {
                     System.out.println(line);
                 }
                 nextLine = scanner.nextLine();
-            } else if (nextLine.equals("list")) {
+            } else if (nextLine.equals("list")) { // Case 5: Displaying List
                 System.out.println(line);
+                System.out.println("Here's what you have ordered so far...");
                 for(int k = 0; k < list.size(); k++) {
                     System.out.println((k+1) + ": " + list.get(k));
                 }
                 System.out.println((line));
                 nextLine = scanner.nextLine();
-            } else {
-                addItem(nextLine);
+            }
+            else { // Case 6: Default errors
                 System.out.println(line);
-                System.out.println("added: " + nextLine);
+                System.out.println(defaultError);
                 System.out.println(line);
                 nextLine = scanner.nextLine();
             }
         }
+
+        // Ending the bot
         System.out.println(line);
         System.out.println("Bye! Please come again!");
         System.out.println(line);
---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+public class Event extends Task {
+
+    protected String time;
+
+    public Event(String description, String time) {
+        super(description);
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + time + ")";
+    }
+}
