a5d34e6a5e2574238829acdc262c7c8cade67218
shadowezz <wenjie.cao@hotmail.com>
2020-08-18 00:46:50 +0800

    Implemented greet,echo,exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                printMessage("Bye. Hope to see you again soon!");
+                return ;
+            } else {
+                printMessage(command);
+            }
+
+        }
+    }
+
+    public static void printMessage(String message) {
+        System.out.println("    ___________________________\n" +
+                "     " + message + "\n    ___________________________");
     }
 }
