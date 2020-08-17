69dfe3498cc0d14646d000f502a05b83a9dd39a6
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-08-17 18:06:19 +0800

    Create Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    private final String taskDescription;
+    private boolean isDone = false;
+
+    Task(String description) {
+        taskDescription = description;
+    }
+
+    void markAsDone() {
+       this.isDone = true;
+       System.out.println("I've marked this task as done:\n" + toString());
+    }
+
+    public String toString() {
+        String status = isDone ? "✓" : "✗";
+        return "[" + status + "]" + taskDescription;
+    }
+}
