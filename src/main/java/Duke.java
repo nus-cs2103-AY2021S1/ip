afcffa78534f5c2f58df5cdc48da3468cc0c5237
Licheng-Wu <wlc.licheng@gmail.com>
2020-08-17 11:57:13 +0800

    Updated Duke.java to initialise Level-2 on startup

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,6 +11,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Welcome!");
         System.out.println("What can Mrs Dino do for you?");
-        Echo.initialise();
+//        Echo.initialise();
+        TodoList.initialise();
     }
 }
