5b3a12941c61e3175287b4315da6b214421b1cc4
pengxiangg <wong.pengxiang@gmail.com>
2020-08-17 21:19:38 +0800

    Add support for deleting tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -29,10 +29,18 @@ public class Duke {
                 // Mark task as done when user inputs "done"
                 else if (inputArr[0].equals("done")) {
                     if (inputArr.length < 2) {
-                        throw new DukeException("Task is empty :(\n");
+                        throw new DukeException("Task to be done not specified :(\n");
                     }
                     String taskNumber = inputArr[1];
                     markTaskDoneInList(list, Integer.parseInt(taskNumber) - 1);
+                }
+
+                else if (inputArr[0].equals("delete")) {
+                    if (inputArr.length < 2) {
+                        throw new DukeException("Task to delete not specified :(\n");
+                    }
+                    String taskNumber = inputArr[1];
+                    deleteTaskFromList(list, Integer.parseInt(taskNumber) - 1);
                 }
                 //Add a new task to the list
                 else {
@@ -102,6 +110,19 @@ public class Duke {
         //Formatting and printing of goodbye message
         String goodbye = "Bye. Hope to see you again soon! \n";
         System.out.println(addDividers(formatString(goodbye)));
+    }
+
+    private static void deleteTaskFromList(List<Task> list, int taskNumber) throws DukeException {
+        if (taskNumber < 0 || taskNumber > list.size() - 1) {
+            throw new DukeException("Please enter a valid task number\n");
+        } else {
+            Task task = list.get(taskNumber);
+            list.remove(task);
+            String success = formatString("Noted. I've removed this task: \n") +
+                    formatString(task.toString() + "\n") +
+                    formatString("Now you have " + list.size() + " tasks in the list. \n");
+            System.out.println(addDividers(success));
+        }
     }
 
 
