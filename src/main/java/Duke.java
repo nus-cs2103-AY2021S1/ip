ed8088b45231119255803a322b11a62e47c86570
cx0810 <chengxin0810@gmail.com>
2020-08-17 13:21:40 +0800

    add the ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static List<String> items;
+    private static List<Task> items;
+    private static final String DONE = "done";
+    private static final String BYE = "bye";
+    private static final String LIST = "list";
+
 
     public static void main(String[] args) {
         String userInput;
@@ -14,15 +18,22 @@ public class Duke {
         do {
             userInput = getUserInput();
 
-            if (userInput.equals("bye")) {
+            if (userInput.equals(BYE)) {
                 exit();
-            } else if (userInput.equals("list")) {
+            } else if (userInput.equals(LIST)) {
                 displayList();
             } else {
-                addItem(userInput);
+                String[] inputArr = userInput.split(" ");
+
+                if (inputArr[0].equals(DONE)) {
+                    int itemsIdx = Integer.parseInt(inputArr[1]) - 1;
+                    markAsDone(itemsIdx);
+                } else {
+                    addItem(userInput);
+                }
             }
 
-        } while (!userInput.equals("bye"));
+        } while (!userInput.equals(BYE));
     }
 
     public static void greeting() {
@@ -41,16 +52,27 @@ public class Duke {
     private static void displayList() {
         int size = items.size();
         int index = 1;
-        for (String s : items) {
-            System.out.println(index + ". " + s);
+        System.out.println("Here are the tasks in your list: ");
+        for (Task t : items) {
+            System.out.println(index + ". " + t);
             index++;
         }
     }
 
     private static void addItem(String input) {
-        items.add(input);
+        items.add(new Task(input));
         String text = "added: " + input;
         System.out.println(text);
+    }
+
+    private static void markAsDone(int itemsIdx) {
+        if (itemsIdx < 0 || itemsIdx > items.size() - 1) {
+            System.out.println("Sorry, task does not exist");
+        } else {
+            items.get(itemsIdx).setDone();
+            System.out.println("Nice! I've marked this task as done: ");
+            System.out.println(items.get(itemsIdx));
+        }
     }
 
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public void setDone() {
+        isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
+}
