bf9e08110ad026563e723bd79102c9fd91c7ad46
successs404 <neoruien@u.nus.edu>
2020-08-18 03:43:42 +0800

    This is my Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+
+    static String line = "____________________________________________________________";
+    static String userInput = "";
+    static ArrayList<String> lst = new ArrayList<>();
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        DukeBot();
+    }
+
+    public static void DukeBot() {
+        Scanner input = new Scanner(System.in);
+
+        format("Hello! I'm Duke\nWhat can I do for you?");
+
+        userInput = input.nextLine();
+        while (!userInput.equals("bye")) {
+            format(userInput);
+            userInput = input.nextLine();
+        }
+
+        format("Bye. Hope to see you again soon!");
+    }
+
+    public static void format(String message) {
+        System.out.println(line);
+        System.out.println(message);
+        System.out.println(line);
     }
 }
