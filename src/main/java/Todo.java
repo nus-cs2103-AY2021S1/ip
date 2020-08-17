d88dbf2a693a036c2afbfcc0e55b64c021b1e4ee
sgn00 <37658877+sgn00@users.noreply.github.com>
2020-08-17 19:17:51 +0800

    Added toString for Todo class

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -3,4 +3,9 @@ public class Todo extends Task {
     public Todo(String name) {
         super(name);
     }
+
+    @Override
+    public String toString() {
+        return String.format("[T]%s", super.toString());
+    }
 }
