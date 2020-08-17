4aec014b6af849240bc5d2844cd47eb1192ee29c
yanbingtao <yanbingtao963@Gmail.com>
2020-08-17 20:15:13 +0800

    Added the ability to store whatever text entered by the user and display them back to the user when requested for level 2.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,53 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
     public static void greeting() {
         String greetingMsg = "\n___________________________________________________________"
-                + "\n Hello! I'm DukeBT. (:"
+                + "\n Hello! I'm DukeBT. |°‿°|"
                 + "\n What can I do for you?"
-                + "\n\n **Type 'bye' to exit (: "
+                + "\n\n **Type 'help' to see what I can do |^_^|"
                 + "\n___________________________________________________________\n";
         System.out.println(greetingMsg);
     }
 
-    public static void echo(String inputMsg) {
+    public static ArrayList<String> addTask(String inputMsg, ArrayList<String> tasks) {
         String outputMsg = "\n___________________________________________________________"
-                + "\n "+ inputMsg
+                + "\n added: " + inputMsg
                 + "\n___________________________________________________________\n";
+
+        System.out.println(outputMsg);
+
+        tasks.add(inputMsg);
+        return tasks;
+    }
+
+    public static void getAllTasks(ArrayList<String> tasks) {
+        String outputMsg = "";
+
+        if (tasks.isEmpty()) {
+            outputMsg = "___________________________________________________________"
+                    + "\n (⊙ ‿ ⊙)"
+                    + "\n Task list is empty, please try to add some tasks first."
+                    + "\n___________________________________________________________\n";
+        } else {
+            outputMsg = "___________________________________________________________"
+                    + "\n (⊙ ‿ ⊙)"
+                    + "\n You have " + tasks.size() + " tasks in total."
+                    + "\n Here they are:";
+            for (int i = 0; i < tasks.size(); i++) {
+                outputMsg += "\n      " + (i + 1) + ". " + tasks.get(i);
+            }
+            outputMsg += "\n___________________________________________________________\n";
+        }
 
         System.out.println(outputMsg);
     }
 
     public static void bye() {
         String byeMsg = "\n___________________________________________________________"
-                +"\n Bye. Hope to see you again soon!"
+                + "\n Bye. Hope to see you again soon! |^_^|┛"
                 + "\n___________________________________________________________\n";
 
         System.out.println(byeMsg);
@@ -30,11 +56,16 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
         greeting();
         String inputMsg = sc.nextLine();
-        while (!inputMsg.equals("bye")){
-            echo(inputMsg);
+        while (!inputMsg.equals("bye")) {
+            if (inputMsg.equals("list")) {
+                getAllTasks(tasks);
+            } else {
+                tasks = addTask(inputMsg, tasks);
+            }
             inputMsg = sc.nextLine();
         }
         bye();
