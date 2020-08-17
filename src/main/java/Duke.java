095aa19a1aa3050ca2e62a0aec6a34060f075d48
Wong-ZZ <wongzhengzhi@gmail.com>
2020-08-17 20:38:25 +0800

    Added ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 
 public class Duke {
     private static final String DIVIDER = "____________________________________________________________";
-    private static final ArrayList<String> inputList = new ArrayList<>();
+    private static final String EXIT_CMD = "bye";
+    private static final String LIST_CMD = "list";
+    private static final String DONE_CMD = "done";
+    private static final ArrayList<Task> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
         String startMsg =
@@ -14,10 +17,10 @@ public class Duke {
         System.out.println(startMsg);
 
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        while(!input.equals("bye")) {
+        String input = sc.nextLine();
+        while(!input.equals(EXIT_CMD)) {
             handleInput(input);
-            input = sc.next();
+            input = sc.nextLine();
         }
 
         String exitMsg =
@@ -27,22 +30,72 @@ public class Duke {
         System.out.println(exitMsg);
     }
 
-    private static String getEcho(String str) {
+    private static String createEchoMsg(String str) {
         String text = "added: " + str + "\n";
         return DIVIDER + "\n" + text + DIVIDER;
     }
 
     private static void handleInput(String in) {
-        if (in.equals("list")) {
-            int len = inputList.size();
-            System.out.println(DIVIDER);
-            for (int i = 0; i < len; i++) {
-                System.out.println(i + ". " + inputList.get(i));
-            }
-            System.out.println(DIVIDER);
-        } else {
-            inputList.add(in);
-            System.out.println(getEcho(in));
+        String[] input = in.split(" ");
+        String cmdWord = input[0];
+        switch (cmdWord) {
+            case (LIST_CMD):
+                handleList();
+                break;
+            case (DONE_CMD):
+                handleDone(Integer.parseInt(input[1]));
+                break;
+            default:
+                handleEcho(in);
+        }
+    }
+
+    private static void handleList() {
+        int len = taskList.size();
+        System.out.println(DIVIDER);
+        for (int i = 1; i <= len; i++) {
+            Task task = taskList.get(i - 1);
+            String output = i + "." + task.toString();
+            System.out.println(output);
+        }
+        System.out.println(DIVIDER);
+    }
+
+    private static void handleDone(int index) {
+        Task task = taskList.get(index - 1);
+        task.markAsDone();
+        System.out.println(
+            DIVIDER + "\n" +
+            "Nice! I've marked this task as done\n" +
+            "  " + task.toString() + "\n" +
+            DIVIDER
+        );
+    }
+
+    private static void handleEcho(String str) {
+        taskList.add(new Task(str));
+        System.out.println(createEchoMsg(str));
+    }
+
+    private static class Task {
+        protected String description;
+        protected boolean isDone = false;
+
+        public Task(String description) {
+            this.description = description;
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public String getStatusIcon() {
+            return isDone ? "\u2713" : "\u2718";
+        }
+
+        @Override
+        public String toString() {
+            return "[" + getStatusIcon() + "]" + " " + description;
         }
     }
 }
