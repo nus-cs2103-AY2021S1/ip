19ada9d20bc2bdeb280fb8ce0be5ef6749b01f45
yeohhq <yeohhuiqing00@gmail.com>
2020-08-17 15:28:15 +0800

    Level-6: Delete, trimming of user input to handle whitespaces

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,9 +19,11 @@ public class Duke {
         // continuously scan for input from user until "bye" is invoked
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
+            String trimmedInput = input.trim(); // get rid of trailing spaces
+
             System.out.println("____________________________________________________________");
             try {
-                processInput(input);
+                processInput(trimmedInput);
             } catch (DukeException ex) {
                 System.err.print(ex);
             }
@@ -45,9 +47,30 @@ public class Duke {
         int taskNo = Integer.parseInt(input.substring(5));
         // verify task number exists, then mark as done
         if (taskNo - 1 < stored.size()) {
-            stored.get(taskNo - 1).markAsDone();
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.println(stored.get(taskNo - 1).toString());
+            if (stored.get(taskNo - 1).isDone) {
+                // task marked as done already
+                throw new DukeException("Task is already done! :)\n");
+            } else {
+                stored.get(taskNo - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(stored.get(taskNo - 1).toString());
+            }
+        } else {
+            // task number does not exist
+            throw new DukeException("Sorry, this task does not exist!\n");
+        }
+    }
+
+    // method to delete task
+    public static void deleteTask(String input) throws DukeException {
+        int taskNo = Integer.parseInt(input.substring(7));
+        // verify task number exists, then delete
+        if (taskNo - 1 < stored.size()) {
+            Task toDelete = stored.get(taskNo - 1);
+            stored.remove(taskNo - 1);
+            System.out.println("Noted. I've removed this task:");
+            System.out.println(toDelete.toString());
+            System.out.println("Now you have " + stored.size() + " tasks in the list.");
         } else {
             // task number does not exist
             throw new DukeException("Sorry, this task does not exist!\n");
@@ -65,22 +88,29 @@ public class Duke {
             System.exit(0);
 
         // display list of items to user when requested with "list" command
-        } else if (input.equals("list")) {
+        } else if (input.startsWith("list") && input.length() == 4) {
             System.out.println("Here are the tasks in your list:");
             for (int i = 1; i < stored.size() + 1; i++) {
                 String item = i + "." + stored.get(i - 1).toString();
                 System.out.println(item);
             }
-        } else if (input.length() > 4 && input.substring(0, 4).equals("done")) {
+        } else if (input.startsWith("done")) {
             // don't add new task, get task number to mark as done
             try {
                 markTaskDone(input);
             } catch (DukeException ex) {
                 System.err.print(ex); // catch if task does not exist
             }
+        } else if (input.startsWith("delete")) { // delete task
+            // don't add new task, get task number to delete task
+            try {
+                deleteTask(input);
+            } catch (DukeException ex) {
+                System.err.print(ex); // catch if task does not exist
+            }
         } else {
             // add item to tasks stored (To-Do/Deadline/Event) depending on command keyword
-            if (input.length() >= 4 && input.substring(0, 4).equals("todo")) { // is To-Do task
+            if (input.startsWith("todo")) { // is To-Do task
                 if (input.length() > 4 && !Character.isWhitespace(input.charAt(4))) {
                     // is not "todo" command (e.g. "todoo")
                     throw new DukeException("I'm sorry, but I don't know what that means :-(\n");
@@ -91,10 +121,10 @@ public class Duke {
                         Todo newTodo = new Todo(input.substring(5));
                         stored.add(newTodo);
                     } catch (StringIndexOutOfBoundsException ex) {
-                        throw new DukeException("Invalid input - please check if there are too many redundant spaces.\n");
+                        throw new DukeException("Invalid input - please check if there are missing info.\n");
                     }
                 }
-            } else if (input.length() >= 8 && input.substring(0, 8).equals("deadline")) { // is Deadline task
+            } else if (input.startsWith("deadline")) { // is Deadline task
                 if (input.length() > 8 && !Character.isWhitespace(input.charAt(8))) {
                     // is not "deadline" command (e.g. "deadlines")
                     throw new DukeException("I'm sorry, but I don't know what that means :-(\n");
@@ -109,7 +139,7 @@ public class Duke {
                         throw new DukeException("Invalid input - please check if there are too many redundant spaces.\n");
                     }
                 }
-            } else if (input.length() >= 5 && input.substring(0, 5).equals("event")) { // is Event task
+            } else if (input.startsWith("event")) { // is Event task
                 if (input.length() > 5 && !Character.isWhitespace(input.charAt(5))) {
                     // is not "event" command (e.g. "events")
                     throw new DukeException("I'm sorry, but I don't know what that means :-(\n");
