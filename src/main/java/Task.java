8370e988d650946c0f40d4f203f15faf28b471c2
Diwu-Yi <59159857+Diwu-Yi@users.noreply.github.com>
2020-08-17 23:36:10 +0800

    partial solution for level 3

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,32 @@
 public class Task {
 
-    public static String[] taskStorage = new String[100];
+    protected String description;
+    protected boolean isDone;
+
+    public static Task[] taskStorage = new Task[100];
+
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public Task(String description, Boolean isDone){
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon(){
+        return (this.isDone ? "\u2713" : "\u2718");
+    }
+
+    public Task markAsDone(){
+        return new Task(this.description, true);
+    }
 
     public static void write(String input){
         for(int i = 0; i < 100; i++){
             if(taskStorage[i] == null){
-                taskStorage[i] = input;
+                taskStorage[i] = new Task(input);
                 System.out.println("added: " + input);
                 break;
             }
@@ -20,6 +41,11 @@ public class Task {
                 break;
             }
         }
+    }
+
+    @Override
+    public String toString(){
+        return "[" + this.getStatusIcon() + "]" + this.description;
     }
 
 }
