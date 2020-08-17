35651850ca0a6b2fb44f6a23f948262b552d0171
Zhu Yuxuan <e0424709@u.nus.edu>
2020-08-17 20:46:30 +0800

    Duke.java: add start and exit methods, modify main method, import java.util.Scanner

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void start() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        start();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+        exit();
     }
 }
