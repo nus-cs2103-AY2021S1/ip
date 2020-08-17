0e14383b404e3584aa4ee42747501b8d9f900d63
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-08-17 13:29:29 +0800

    Complete code for Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,25 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, my name is\n" + logo);
+        System.out.println("What can I do for you");
+
+        // Initialise booleans and scanners
+        boolean quitProgram = false;
+        Scanner inputScanner = new Scanner(System.in);
+
+        while (!quitProgram) {
+            // blocks program until input is received
+            String newInput = inputScanner.nextLine();
+            // then prints the input
+            if (newInput.equals("bye")) {
+                quitProgram = true;
+            } else {
+                System.out.println(newInput);
+            }
+        }
+
+        // quit
+        System.out.println("See you space cowboy!");
     }
 }
