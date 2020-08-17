3be45bade032eb5b25bf508d72ef6fb4d0937641
CalistaIo <e0426281@u.nus.edu>
2020-08-17 20:33:41 +0800

    Added Task class to represent task

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
