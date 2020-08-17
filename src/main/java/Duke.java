c93dc606e27214f9899347a84850c17a18d2a536
Nauman-S <58757898+Nauman-S@users.noreply.github.com>
2020-08-17 11:55:45 +0800

    Completed level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public interface Printable{
+        public String print();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +12,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Printable greeting = () -> "Hello! I'm Duke \nWhat can I do for you?";
+        Printable goodbye = () -> "Bye. Hope to see you again soon!";
+        speak(greeting);
+        Printable input;
+        Scanner sc = new Scanner(System.in);
+        do {
+            input = getUserInput(sc);
+            if (input.print().equals("bye")) {
+                speak(goodbye);
+                break;
+            } else {
+                speak(input);
+            }
+        } while (true);
     }
+
+    public static void speak(Printable printable) {
+        System.out.println("------------------------------------------------------");
+        System.out.println(printable.print() + "\n");
+        System.out.println("------------------------------------------------------");
+    }
+
+    public static Printable getUserInput(Scanner sc) {
+        Printable input = () -> sc.nextLine().toLowerCase();
+        return input;
+    }
+
 }
