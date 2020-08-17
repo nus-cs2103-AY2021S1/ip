713f0b52fbf40d970503ff2deba549854615bc7d
maguireong <maguire2622@yahoo.com.sg>
2020-08-17 11:33:47 +0800

    Add exceptions handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,54 +9,66 @@ public class Duke {
         ArrayList<Task> books = new ArrayList<>();
 
         while (sc.hasNext()) {
+            try {
+                String echo = sc.nextLine();
+                String[] s = echo.split("\\s");
+                String first = s[0];
 
-            String echo = sc.nextLine();
-            String[] s = echo.split("\\s");
-            String first = s[0];
-
-            if (first.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-            if (first.equals("list")) {
-                for (int i = 0; i < books.size(); i++) {
-                    int l = i + 1;
-                    System.out.println(l + "." + books.get(i));
+                if (first.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
                 }
-            } else if (first.equals("done")) {
-                int index = Integer.parseInt(s[1]);
-                books.get(index - 1).markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n  " + books.get(index - 1).getStatusIcon() + " return book");
-            } else {
-                if (first.equals("todo")) {
+                if (first.equals("list")) {
+                    for (int i = 0; i < books.size(); i++) {
+                        int l = i + 1;
+                        System.out.println(l + "." + books.get(i));
+                    }
+                } else if (first.equals("done")) {
+                    int index = Integer.parseInt(s[1]);
+                    books.get(index - 1).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:\n  " + books.get(index - 1).getStatusIcon() + " return book");
+                } else {
+                    if (first.equals("todo")) {
+                        if (s.length == 1) {
+                            throw new EmptyTodoException();
+                        }
+                        Todo t = new Todo(echo.substring(5));
+                        books.add(t);
 
-                    Todo t = new Todo(echo.substring(5));
-                    books.add(t);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + books.get(books.size() - 1));
+                        System.out.println("Now you have " + books.size() + " tasks in the list.");
+                    } else if (first.equals("event")) {
+                        if (s.length == 1) {
+                            throw new EmptyEventsException();
+                        }
+                        int start = echo.indexOf("/at");
+                        String date = echo.substring(start + 4);
+                        Event t = new Event(echo.substring(6, start - 1), date);
+                        books.add(t);
 
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + books.get(books.size() - 1));
-                    System.out.println("Now you have " + books.size() + " tasks in the list.");
-                } else if (first.equals("event")) {
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + books.get(books.size() - 1));
+                        System.out.println("Now you have " + books.size() + " tasks in the list.");
+                    } else if (first.equals("deadline")) {
+                        if (s.length == 1) {
+                            throw new EmptyDeadlineException();
+                        }
 
-                    int start = echo.indexOf("/at");
-                    String date = echo.substring(start + 4);
-                    Event t = new Event(echo.substring(6, start - 1), date);
-                    books.add(t);
+                        int start = echo.indexOf("/by");
+                        String date = echo.substring(start + 4);
+                        Deadline t = new Deadline(echo.substring(9, start - 1), date);
+                        books.add(t);
 
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + books.get(books.size() - 1));
-                    System.out.println("Now you have " + books.size() + " tasks in the list.");
-                } else if (first.equals("deadline")) {
-
-                    int start = echo.indexOf("/by");
-                    String date = echo.substring(start + 4);
-                    Deadline t = new Deadline(echo.substring(9, start - 1), date);
-                    books.add(t);
-
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + books.get(books.size() - 1));
-                    System.out.println("Now you have " + books.size() + " tasks in the list.");
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + books.get(books.size() - 1));
+                        System.out.println("Now you have " + books.size() + " tasks in the list.");
+                    } else {
+                        throw new UnknownCommandException();
+                    }
                 }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
