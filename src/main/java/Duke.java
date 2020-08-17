993e55a5c0d1c5613b38543a2d04a2fc03bc0b21
Tan Yan Lyn <e0424594@u.nus.edu>
2020-08-17 20:58:48 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke \n"
+                            + "What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            input = sc.next();
+            if (!input.equals("bye")) {
+                System.out.println(input);
+            } else {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
     }
 }
