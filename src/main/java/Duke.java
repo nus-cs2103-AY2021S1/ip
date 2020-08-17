336963206c2c50cbd2855d1cf133f3acb69e7fa2
chanwei53 <hanwei_cheak@hotmail.sg>
2020-08-17 13:27:45 +0800

    Completed Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         greet();
@@ -23,11 +23,19 @@ public class Duke {
 
     private static void printTasks() {
         for (int i = 0; i < tasks.size(); i++) {
-            String task = tasks.get(i);
-            System.out.println(i + 1 + ". " + task);
+            Task task = tasks.get(i);
+            System.out.println(i + 1 + ". " + task.getStatusIcon() + " " + task);
         }
         System.out.println("--------------------------" +
                 "-------------------------");
+    }
+
+    private static void setDoneTask(int index) {
+        Task task = tasks.get(index - 1);
+        task.markDone();
+        System.out.println("Nice! I've marked this task as done: " +
+                task.getStatusIcon() + " " + task +
+                "\n---------------------------------------------------");
     }
 
     private static void readTasks() {
@@ -36,15 +44,18 @@ public class Duke {
                 "-------------------------";
         while (sc.hasNext()) {
             String input = sc.nextLine();
+            Task newTask = new Task(input);
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 sc.close();
                 break;
             } else if (input.equals("list")) {
                 printTasks();
+            } else if (input.split(" ")[0].equals("done")) {
+                setDoneTask(Integer.parseInt(input.split(" ")[1]));
             } else {
-                tasks.add(input);
-                System.out.println(String.format(reply, input));
+                tasks.add(newTask);
+                System.out.println(String.format(reply, newTask));
             }
         }
     }
