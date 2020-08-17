d73804c9de404a1e36ced16f1340bd630a2d8f3e
yanbingtao <yanbingtao963@Gmail.com>
2020-08-17 22:53:47 +0800

    Implemented Deadlines Class with constructor, createDeadline method and toString method (@override)

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,10 +1,20 @@
-public class Deadlines extends Task{
-    public Deadlines(String taskTitle) {
+import java.util.ArrayList;
+
+public class Deadlines extends Task {
+    private String deadlineTime;
+
+    public Deadlines(String taskTitle, String deadlineTime) {
         super(taskTitle);
+        this.deadlineTime = deadlineTime;
+    }
+
+    public static void createDeadline(String taskTitle, String deadlineTime, ArrayList<Task> tasks) {
+        Deadlines newDeadline = new Deadlines(taskTitle, deadlineTime);
+        tasks.add(newDeadline);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString();
+        return "[D]" + super.toString() + " (by:" + deadlineTime + ")";
     }
 }
