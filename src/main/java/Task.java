8dbba7d1758f76911773d1b35124b427290d5eed
yanbingtao <yanbingtao963@Gmail.com>
2020-08-17 21:32:11 +0800

    Implemented class Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Task {
+    private String taskTitle;
+    private boolean isDone;
+
+    public Task(String taskTitle) {
+        this.taskTitle = taskTitle;
+        this.isDone = false;
+    }
+
+    public static void createTask(String taskTitle, ArrayList<Task> tasks) {
+        Task newTask = new Task(taskTitle);
+        tasks.add(newTask);
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatus() {
+        return (isDone ? "✓" : "✗");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatus() + "] " + taskTitle;
+    }
 }
