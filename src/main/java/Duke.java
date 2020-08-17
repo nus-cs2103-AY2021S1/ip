5543ea1300ba9aab24c3f5685a609b8aaaeb750d
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-08-17 23:59:31 +0800

    Add basic exception handling for Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,46 +21,57 @@ public class Duke {
             // blocks program until input is received
             String newInput = inputScanner.nextLine();
             // then prints the input
-            if (newInput.equals("bye")) {
-                quitProgram = true;
-            } else if (newInput.equals("list")) {
-                taskList.displayTasks();
-            } else if (newInput.length() > 5 && newInput.substring(0, 5).equals("done ")) {
-                int taskIndex = Integer.parseInt(newInput.substring(5));    // this is not corrected for 0 index
-                Task completedTask = taskList.getTask(taskIndex);
-                completedTask.markAsDone();
 
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(completedTask);
-            } else {
-                // use indexOf() method to find substring
-                Task newTask = new Task("");    // this is to make the compiler happy
+            try {
+                if (newInput.equals("bye")) {
+                    quitProgram = true;
+                } else if (newInput.equals("list")) {
+                    taskList.displayTasks();
+                } else if (newInput.length() > 5 && newInput.substring(0, 5).equals("done ")) {
+                    int taskIndex = Integer.parseInt(newInput.substring(5));    // this is not corrected for 0 index
+                    Task completedTask = taskList.getTask(taskIndex);
+                    completedTask.markAsDone();
 
-                if (newInput.indexOf("event ") == 0) {
-                    int indexOfAtKeyword = newInput.indexOf(" /at ");
-                    String eventDesc = newInput.substring(6, indexOfAtKeyword);
-                    String eventTime = newInput.substring(indexOfAtKeyword + 5);
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(completedTask);
+                } else {
+                    // use indexOf() method to find substring
+                    Task newTask = new Task("");    // this is to make the compiler happy
 
-                    newTask = new Event(eventDesc, eventTime);
-                    taskList.addTask(newTask);
+                    if (newInput.indexOf("event ") == 0) {
+                        int indexOfAtKeyword = newInput.indexOf(" /at ");
+                        String eventDesc = newInput.substring(6, indexOfAtKeyword);
+                        String eventTime = newInput.substring(indexOfAtKeyword + 5);
 
-                } else if (newInput.indexOf("deadline ") == 0) {
-                    int indexOfByKeyword = newInput.indexOf(" /by ");
-                    String deadlineDesc = newInput.substring(9, indexOfByKeyword);
-                    String deadlineTime = newInput.substring(indexOfByKeyword + 5);
+                        newTask = new Event(eventDesc, eventTime);
+                        taskList.addTask(newTask);
 
-                    newTask = new Deadline(deadlineDesc, deadlineTime);
-                    taskList.addTask(newTask);
-                } else if (newInput.indexOf("todo ") == 0){
+                    } else if (newInput.indexOf("deadline ") == 0) {
+                        int indexOfByKeyword = newInput.indexOf(" /by ");
+                        String deadlineDesc = newInput.substring(9, indexOfByKeyword);
+                        String deadlineTime = newInput.substring(indexOfByKeyword + 5);
 
-                    newTask = new Todo(newInput.substring(5));
-                    taskList.addTask(newTask);
+                        newTask = new Deadline(deadlineDesc, deadlineTime);
+                        taskList.addTask(newTask);
+                    } else if (newInput.indexOf("todo") == 0){
+                        if (newInput.length() <= 4 || newInput.substring(5).trim().length() == 0) {
+                            throw new DukeException(" ☹ OOPS!!! The description of a todo cannot be empty.");
+                        } else {
+                            newTask = new Todo(newInput.substring(5));
+                            taskList.addTask(newTask);
+                        }
+                    } else {
+                        throw new DukeException(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
+
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println(newTask);
+                    System.out.println("Now you have " + taskList.numTasks() + " tasks in the list.");
                 }
-
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(newTask);
-                System.out.println("Now you have " + taskList.numTasks() + " tasks in the list.");
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
+
         }
 
         // quit
