134a3b9c6a6001442cce62483b313a4e9f69b014
michael-setia <michaelsetia55@gmail.com>
2020-08-17 17:19:18 +0700

    Add code to Duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+package main.java;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Create horizontal line.
+        StringBuilder line = new StringBuilder();
+        line.append("_".repeat(50));
+
+        // Print greeting message.
+        System.out.println(line + "\n" + "Hey there! I am Popi" + "\n"
+            + "How can I help you?" + "\n" + line + "\n");
+
+        // Process user input(s).
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line + "\n" + "Bye! Hope to see you again in the future!"
+                    + "\n" + line + "\n");
+                break;
+            } else {
+                System.out.println(line + "\n" + input + "\n" + line + "\n");
+            }
+        }
     }
 }
