3314a7f639d0b9b77bbaa23316df57ca4ddb21d2
davidcaiqifan <e0406982@u.nus.edu>
2020-08-17 21:34:54 +0800

    added checkList function

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -12,14 +12,22 @@ public class TaskManager {
     }
 
     public void displayList() {
+        System.out.println("Check out your missions!");
         int i = 1;
         for (Task s : this.toDoList) {
-            System.out.println(i + "." + s.toString());
+            System.out.println(i + ". " + s.toString() + " [" + s.getTaskStatusIcon() + "]");
             i += 1;
         }
     }
 
-
+    public void checkList(int checkNumber) {
+        Task task = toDoList.get(checkNumber - 1);
+        task.markAsDone();
+        System.out.println("--------------------------------------");
+        System.out.println("Such wow! I have completed the following task!");
+        System.out.println(task.toString() + " [" + task.getTaskStatusIcon() + "]");
+        System.out.println("--------------------------------------");
+    }
 
     public void taskPrint(String msg) {
         System.out.println("Added : " + msg);
