f62eb57d692fc38aa6d44329b0cd4c0fd0206a16
angrybunny123 <kennethteo98@gmail.com>
2020-08-17 15:44:16 +0800

    completed level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] tasks;
+    private static int count;
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -10,16 +12,27 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         Scanner userInput = new Scanner(System.in);
+        tasks = new String[100];
+        count = 0;
 
         while (userInput.hasNext()) {
             String input = userInput.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < tasks.length; i++) {
+                    if (tasks[i] != null) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    } else {
+                        break;
+                    }
+                }
             } else {
-                System.out.println(input);
+                tasks[count] = input;
+                count ++;
+                System.out.println("added: " + input);
             }
         }
-
     }
 }
