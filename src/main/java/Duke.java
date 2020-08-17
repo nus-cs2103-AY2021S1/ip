a9224029a039c5fac0e085fa8b9164a6f2142ccf
Roger <E0406757@u.nus.edu>
2020-08-17 14:19:54 +0800

    level 3 + spilt up main into individual methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,41 @@ import java.util.Scanner;
 
 
 public class Duke {
+    static String line = "    ____________________________________________________________";
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<Task> arr =  new ArrayList<Task>();
+
+    private static void markAsDone(String input) {
+        String index = input.substring(5, input.length());
+        int number = Integer.parseInt(index) - 1;
+        arr.get(number).setDone();
+        System.out.println(line);
+        System.out.println("     Nice! I've marked this task as done: ");
+        System.out.println(String.format("       %s", arr.get(number).getOutput()));
+        System.out.println(line);
+    }
+
+    private static void printList() {
+        System.out.println("     Here are the tasks in your list:");
+        System.out.println(line);
+        for(int i = 0; i < arr.size(); i++) {
+            System.out.println(String.format("     %d. %s", i + 1, arr.get(i).getOutput()));
+        }
+        System.out.println(line);
+    }
+
+    private static void addNewTask(String input) {
+        arr.add(new Task(input));
+        System.out.println(line);
+        System.out.println(String.format("     added: %s", input));
+        System.out.println(line);
+    }
+
     public static void main(String[] args) {
         boolean on = true;
-        String line = "    ____________________________________________________________";
-        Scanner sc = new Scanner(System.in);
         System.out.println(line);
         System.out.println("     Hello! I'm Duke\n     What can I do for you?");
         System.out.println(line);
-        ArrayList arr =  new ArrayList<String>();
 
         while(on) {
             String input = sc.nextLine();
@@ -19,17 +46,12 @@ public class Duke {
                 System.out.println(line);
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println(line);
+            } else if (input.indexOf("done") == 0) {
+                markAsDone(input);
             } else if (input.compareTo("list") == 0){
-                System.out.println(line);
-                for(int i = 0; i < arr.size(); i++) {
-                    System.out.println(String.format("     %d. %s", i + 1, arr.get(i)));
-                }
-                System.out.println(line);
+                printList();
             } else {
-                arr.add(input);
-                System.out.println(line);
-                System.out.println(String.format("     added: %s", input));
-                System.out.println(line);
+                addNewTask(input);
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        String sign = isDone ? "\u2713" : "\u2718"; //return tick or X symbols
+        return (String.format("[%s]", sign));
+    }
+
+    public String getOutput() {
+        return String.format("%s %s", getStatusIcon(), this.description);
+    }
+
+    public void setDone() {
+        isDone = true;
+    }
+}
