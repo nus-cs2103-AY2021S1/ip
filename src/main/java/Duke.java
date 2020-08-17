4f5f2c14cee855b2333e2d5609284f67c216fbbe
joeychensmart <chen.kangaroo@gmail.com>
2020-08-17 13:02:15 +0800

    Feat: Implement add, list features (Level-2)
    firstly, created a constructor for Duke to make it easier to hold
    state/variables. Also made some of the utility functions non-static.
    
    added the `add` and `list` features. Uses an arraylist to store and
    display strings input by the user. Created the functions `addItem`
    and `printList` to carry this out.
    
    minor fix: the indent function was not working properly for strings
    with multiple lines.
    
    note: will start using conventional commits style messages for
    future commits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,61 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
+    private final List<String> items;
+    public Duke() {
+        this.items = new ArrayList<>();
+    }
+    private void addItem(String input) {
+        this.items.add(input);
+        systemMessage("morning sir i have added \""+input+"\" to the list sir");
+    }
+    private void printList() {
+        String numberedItems = "";
+        for (int i=0;i<this.items.size();i++) {
+            numberedItems += Integer.toString(i+1) + ". " + this.items.get(i) + "\n";
+        }
+        systemMessage(numberedItems);
+    }
+    public void run() {
         Scanner scanner = new Scanner(System.in);
         systemMessage(logo);
         String input = scanner.nextLine();
-        while(!input.equals("bye")) {
-            systemMessage(input);
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                this.printList();
+            } else {
+                this.addItem(input);
+            }
             input = scanner.nextLine();
         }
-        systemMessage("Bye sir thanks for using me sir hope to see you again sir");
+        systemMessage("bye sir thanks for using me sir hope to see you again sir");
+    }
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.run();
     }
     private static final String logo =
-                "                 /,   ,|   ,|     \n" +
-                "             /| /(  ,' / ,//      \n" +
-                "          \\`( |/ /,'  (,/ |      \n" +
-                "           \\ \\ ` `   `  /--,    \n" +
-                "         _,_\\ `  ` `  ``  /__    \n" +
-                "          '-.____________`  /     \n" +
-                "            [  \\@,    :] `--,-..-\n" +
-                "            [__________]__,'-._/  \n" +
-                "             )'o\\ ' o) \\/ )       Hello sir\n" +
-                "             \\  /   __  ./        Its me your assistant sir\n" +
-                "              \\=`   ==,\\..        How may I be of service sir\n" +
-                "               \\ -. `,' (        \n" +
-                "               \\`--''    \\.     \n";
-    private static void systemMessage(String input) {
+                "               /,   ,|   ,|     \n" +
+                "           /| /(  ,' / ,//      \n" +
+                "        \\`( |/ /,'  (,/ |      \n" +
+                "         \\ \\ ` `   `  /--,    \n" +
+                "       _,_\\ `  ` `  ``  /__    \n" +
+                "        '-.____________`  /     \n" +
+                "          [  \\@,    :] `--,-..-\n" +
+                "          [__________]__,'-._/  \n" +
+                "           )'o\\ ' o) \\/ )       hello sir\n" +
+                "           \\  /   __  ./        its me your assistant sir\n" +
+                "            \\=`   ==,\\..        how may i be of service sir\n" +
+                "             \\ -. `,' (        \n" +
+                "             \\`--''    \\.     \n";
+    private static final String horizontalRule =
+                "    ____________________________________________________________\n";
+    private void systemMessage(String input) {
         System.out.println(horizontalRule+indent(input)+"\n"+horizontalRule);
     }
-    private static String indent(String original) {
-        return "     "+ original.replaceAll("\n", "    \n");
+    private String indent(String original) {
+        return "     "+ original.replace("\n", "\n     ");
     }
-    private static final String horizontalRule = "    ____________________________________________________________\n";
 }
