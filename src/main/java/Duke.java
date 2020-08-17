7021f4e4d4ff4d755d2916e4605a0154d1329234
xinyue <xinyue98.zhang@gmail.com>
2020-08-17 18:37:58 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+    static String line = "    ____________________________________________________________\n";
+
+    private static String format(String string) {
+        return line + string + "\n" + line;
+    }
+
+    private static void echo() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        if (input.equals("bye")) {
+            System.out.println(format("     Bye. Hope to see you again soon!"));
+        } else {
+            System.out.println(format(input));
+            echo();
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +25,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(format("     Hello! I'm Duke\n" +
+                "     What can I do for you?"));
+        echo();
     }
 }
