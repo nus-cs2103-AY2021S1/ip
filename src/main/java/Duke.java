c82a39543ab199986523d68e5f7efd378af7654b
jiaax <ngjiaxin11@gmail.com>
2020-08-17 21:01:59 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,25 +11,47 @@ public class Duke {
         String command = sc.nextLine();
 
         while(!command.equals("bye")) {
-            if (command.equals("list")) {
+            String key = command.split(" ",2)[0];
+            if (key.equals("list")) {
                 System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < store.size(); i++) {
                     System.out.println((i+1) + "." + store.get(i).toString());
                 }
                 command = sc.nextLine();
             }
-            if (command.contains("done")) {
+            else if (key.equals("done")) {
+                //System.out.println("what");
                 int k = Integer.parseInt(command.split(" ")[1]);
                 store.get(k-1).markAsDone();
                 System.out.println("Nice! This task is marked as done!");
                 System.out.println(store.get(k-1));
                 command = sc.nextLine();
             }
+            //adding of task
             else {
-                store.add(new Task(command));
-                System.out.println("added: " + command);
+                String remain = command.split(" ", 2)[1];
+                if (key.equals("todo")) {
+                    store.add(new Todo(remain));
+                    System.out.println("Got it. I've added this task:\n" + store.get(store.size() -1 ));
+                } else {
+                    String description = remain.split("/", 2)[0];
+
+                    if (key.equals("deadline")) {
+                        String by = remain.split("/by", 2)[1];
+                        store.add(new Deadline(description, by));
+                    }
+                    if (key.equals("event")) {
+                        String at = remain.split("/at", 2)[1];
+                        store.add(new Event(description, at));
+                    }
+                    System.out.println("Got it. I've added this task:\n" + store.get(store.size() -1 ));
+                }
+                System.out.println("Now you have " + store.size() + " tasks in the list.");
                 command = sc.nextLine();
             }
+            //System.out.println("Got it. I've added this task:\n" + store.get(store.size() -1 ));
+
+
         }
         System.out.println("Bye. Meimei will miss you!");
     }
