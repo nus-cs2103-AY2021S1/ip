ea3974564993c3acb75657ff982d0b54f6b019b8
m0nggh <malcolmong27@gmail.com>
2020-08-17 17:09:08 +0800

    Level-2 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Duke {
     private static final String line = "----------------------";
 
+    // for the list
+    private static final String[] tasks = new String[100];
+    private static int index = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,12 +18,14 @@ public class Duke {
         while (sc.hasNext()) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
+                exit();
                 break;
+            } else if (input.equals("list")) {
+                displayList();
             } else {
-                echo(input);
+                addOnToList(input);
             }
         }
-        exit();
     }
 
     private static void greet() {
@@ -33,6 +39,22 @@ public class Duke {
 
     private static void exit() {
         echo("Bye. Hope to see you again soon!");
+    }
+
+    private static void addOnToList(String task) {
+        tasks[index++] = task;
+        System.out.println("added: " + task);
+    }
+
+    private static void displayList() {
+        if (index == 0) {
+            System.out.println("No tasks!");
+            return;
+        }
+
+        for (int i = 0; i < index; i++) {
+            System.out.println(i + 1 + ". " + tasks[i]);
+        }
     }
 
 }
