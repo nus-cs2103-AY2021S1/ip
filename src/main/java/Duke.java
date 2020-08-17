aa2a5d515e3dc408fd23dc1e9a91ad8affb23ae6
jiaax <ngjiaxin11@gmail.com>
2020-08-17 19:32:32 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        ArrayList<String> store = new ArrayList<>();
 
         System.out.println("Hello! I'm Duke ^_^\nWhat can I do for you?");
         String command = sc.nextLine();
 
         while(!command.equals("bye")) {
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < store.size(); i++) {
+                    System.out.println((i+1) + ". " + store.get(i));
+                }
+            } else {
+                store.add(command);
+                System.out.println("added: " + command);
+            }
             command = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
