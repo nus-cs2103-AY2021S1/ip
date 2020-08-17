f665d1e940dbd1ba7d5e2254e6ad3ed84d761c3c
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-08-18 02:13:19 +0800

    Level-6
    Delete
    Added "delete" case
    
    Handled a few more exception scenarios

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,6 +8,7 @@ public class Duke {
         ArrayList<Task> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String operation = "start";
+        Task task;
 
         while (!operation.equals("bye")) {
             String[] line = scanner.nextLine().split(" ", 2);
@@ -25,24 +26,31 @@ public class Duke {
                     break;
 
                 case "done":
-                    int task_index = Integer.parseInt(line[1]);
                     // Validate task index
-                    if (task_index <= tasks.size()) {
-                        Task task = tasks.get(task_index - 1);
-                        task.markAsDone();
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println("[" + task.getStatusIcon() + "]" + " " + task.description);
-                    } else {
-                        throw new DukeException("Only existing tasks with index <= " + tasks.size() + " can be marked as done!");
+                    if (line.length == 1) {
+                        throw new DukeException("Tell me which task you have done!");
                     }
+                    if (line[1].isEmpty()) {
+                        throw new DukeException("Tell me which task you have done!");
+                    }
+                    int task_index = Integer.parseInt(line[1]);
+                    if (task_index > tasks.size()) {
+                        throw new DukeException("Only existing tasks of index <= " + tasks.size() + " can be marked as done!");
+                    }
+                    task = tasks.get(task_index - 1);
+                    task.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(task);
                     break;
 
                 case "todo":
                 case "deadline":
                 case "event":
-                    Task task;
                     // Validate descriptions
                     if (line.length == 1) {
+                        throw new DukeException("☹ OOPS!!! The description of " + operation +" cannot be empty.");
+                    }
+                    if (line[1].isEmpty()) {
                         throw new DukeException("☹ OOPS!!! The description of " + operation +" cannot be empty.");
                     } else if (operation.equals("todo")) {
                         String description = line[1];
@@ -69,6 +77,27 @@ public class Duke {
                     System.out.println(task);
                     System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
                     break;
+
+                case "delete":
+                    // Validate task index
+                    if (line.length == 1) {
+                        throw new DukeException("Tell me which task you want to delete!");
+                    }
+                    if (line[1].isEmpty()) {
+                        throw new DukeException("Tell me which task you want to delete!");
+                    }
+                    task_index = Integer.parseInt(line[1]);
+                    // Validate task index
+                    if (task_index > tasks.size()) {
+                        throw new DukeException("Only existing tasks of index <= " + tasks.size() + " can be deleted!");
+                    }
+                    task = tasks.get(task_index - 1);
+                    tasks.remove(task);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(task);
+                    System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+                    break;
+
                 default:
                     // Handles all other inputs
                     if (!operation.equals("bye")) {
