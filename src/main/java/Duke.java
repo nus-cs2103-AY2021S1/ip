9182c74b87d608aa0872d3d27ef75a35001a5fb7
richardcom <leonardlym61@gmail.com>
2020-08-17 14:07:20 +0800

    Add error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,35 +20,81 @@ public class Duke {
         while (sc.hasNextLine()) {
             String instruction = sc.nextLine();
 
-            if (instruction.equals("bye")) {
-                printAnswer("", farewell, "");
-                break;
-            } else if (instruction.length() > 4 && instruction.substring(0,4).equals("done")) {
-                Integer index = Integer.valueOf(instruction.substring(5)) - 1;
-                Task temp = result[index];
-                temp.markAsDone();
-                result[index] = temp;
-                printAnswer(doneAlert, "   " + temp.toString(), "");
-            } else if (instruction.equals("list")) {
-                printList("", result, "");
-            } else if (instruction.length() > 4 && instruction.substring(0,4).equals("todo")) {
-                result[count++] = new ToDo(instruction.substring(4));
-                printAnswer(addTaskFrontAlert, "   " + result[count - 1].toString(), String.format(addTaskTailAlert, count));
+            try {
+                if (instruction.equals("bye")) {
+                    printAnswer("", farewell, "");
+                    break;
+                } else if (instruction.length() >= 4 && instruction.substring(0, 4).equals("done")) {
 
-            } else if (instruction.length() > 8 && instruction.substring(0,8).equals("deadline")) {
-                String[] temp = instruction.substring(8).split("/by ");
-                String desc = temp[0];
-                String date = temp[1];
-                result[count++] = new Deadlines(desc, date);
-                printAnswer(addTaskFrontAlert, "   " + result[count - 1].toString(), String.format(addTaskTailAlert, count));
+                    if (instruction.length() <= 5) {
+                        throw new DukeException("The description of a done message cannot be empty.");
+                    }
 
-            } else if (instruction.length() > 5 && instruction.substring(0,5).equals("event")) {
-                String[] temp = instruction.substring(5).split("/at ");
-                String desc = temp[0];
-                String date = temp[1];
-                result[count++] = new Events(desc, date);
-                printAnswer(addTaskFrontAlert, "   " + result[count - 1].toString(), String.format(addTaskTailAlert, count));
+                    Integer index = Integer.valueOf(instruction.substring(5)) - 1;
 
+                    if (index + 1 > count) {
+                        throw new DukeException("The index of the task to be done is out of range.");
+                    }
+
+                    Task temp = result[index];
+                    temp.markAsDone();
+                    result[index] = temp;
+                    printAnswer(doneAlert, "   " + temp.toString(), "");
+                } else if (instruction.equals("list")) {
+                    printList("", result, "");
+                } else if (instruction.length() >= 4 && instruction.substring(0, 4).equals("todo")) {
+
+                    if (instruction.length() <= 5) {
+                        throw new DukeException("The description of a todo cannot be empty.");
+                    }
+
+                    result[count++] = new ToDo(instruction.substring(4));
+                    printAnswer(addTaskFrontAlert, "   " + result[count - 1].toString(), String.format(addTaskTailAlert, count));
+
+                } else if (instruction.length() >= 8 && instruction.substring(0, 8).equals("deadline")) {
+
+                    if (instruction.length() <= 9) {
+                        throw new DukeException("The description of a deadline cannot be empty.");
+                    }
+
+                    String[] temp = instruction.substring(8).split("/by ");
+
+                    if (temp.length < 2) {
+                        throw new DukeException("The date and time of the event cannot be empty.");
+                    }
+
+                    String desc = temp[0];
+                    String date = temp[1];
+
+
+                    result[count++] = new Deadlines(desc, date);
+                    printAnswer(addTaskFrontAlert, "   " + result[count - 1].toString(), String.format(addTaskTailAlert, count));
+
+                } else if (instruction.length() >= 5 && instruction.substring(0, 5).equals("event")) {
+
+                    if (instruction.length() <= 6) {
+                        throw new DukeException("The description of an event cannot be empty.");
+                    }
+
+                    String[] temp = instruction.substring(5).split("/at ");
+
+                    if (temp.length < 2) {
+                        throw new DukeException("The date and time of the event cannot be empty.");
+                    }
+
+                    String desc = temp[0];
+                    String date = temp[1];
+
+
+
+                    result[count++] = new Events(desc, date);
+                    printAnswer(addTaskFrontAlert, "   " + result[count - 1].toString(), String.format(addTaskTailAlert, count));
+
+                } else {
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                System.out.println(e.toString());
             }
         }
 
@@ -56,11 +102,11 @@ public class Duke {
     }
 
     public static void printAnswer(String FrontGuidance, String answer, String TailGuidance) {
-        String Line = "____________________________________________________________";
+        String line = "____________________________________________________________";
         String smallSpace = "    ";
         String bigSpace = "     ";
 
-        System.out.println(smallSpace + Line);
+        System.out.println(smallSpace + line);
         if (FrontGuidance.length() != 0) {
             System.out.println(bigSpace + FrontGuidance);
         }
@@ -68,7 +114,7 @@ public class Duke {
         if (TailGuidance.length() != 0) {
             System.out.println(bigSpace + TailGuidance);
         }
-        System.out.println(smallSpace + Line + "\n");
+        System.out.println(smallSpace + line + "\n");
     }
 
     public static void printList(String FrontGuidance, Task[] result, String TailGuidance) {
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,14 @@
+public class DukeException extends Exception {
+    String line = "____________________________________________________________";
+    String smallSpace = "    ";
+    String bigSpace = "     ";
+
+    public DukeException(String message) {
+        super(message);
+    }
+
+    public String toString() {
+        return smallSpace + line + "\n" + bigSpace + "☹ OOPS!!! " +
+                    this.getMessage() + "\n" + smallSpace + line + "\n";
+    }
+}
