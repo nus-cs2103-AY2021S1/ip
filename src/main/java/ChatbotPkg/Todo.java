179c537841cd4d80f3bfd20ef36db2bf270151cc
jianhandev <jianhanli.dev@gmail.com>
2020-08-17 19:19:55 +0800

    Handle Errors
    Add error handling for invalid inputs and update test files.

--- src/main/java/ChatbotPkg/Todo.java
+++ src/main/java/ChatbotPkg/Todo.java
@@ -6,6 +6,13 @@ public class Todo extends Task {
         super(description, "T");
     }
 
+    public static Todo newTodo(String text) throws ChatbotException {
+        if (text.length() == 0) {
+            throw new ChatbotException("Ooopsss (>.>) Todo task cannot be empty!!");
+        }
+        return new Todo(text);
+    }
+
     private Todo(String description, boolean isDone) {
         super(description, "T", isDone);
     }
