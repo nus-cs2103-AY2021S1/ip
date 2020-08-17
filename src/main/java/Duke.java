c73a6d6552536b010e03efeac210b72e6ebabc4a
Tan Yan Lyn <e0424594@u.nus.edu>
2020-08-17 21:06:38 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,11 +14,18 @@ public class Duke {
         System.out.println("Hello! I'm Duke \n"
                             + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
         String input = "";
         while (true) {
-            input = sc.next();
-            if (!input.equals("bye")) {
-                System.out.println(input);
+            input = sc.nextLine();
+            if (!input.equals("bye") && !input.equals("list")) {
+                inputs.add(input);
+                System.out.println("added: " + input);
+            } else if (input.equals("list")) {
+                int len = inputs.size();
+                for (int i = 1; i <= len; i++) {
+                    System.out.println(i + ". " + inputs.get(i-1));
+                }
             } else {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
