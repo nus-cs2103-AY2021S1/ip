907f7047a22a1c0e4a92306c6d1b8bc89db8175a
Hou-Rui <houruinus@gmail.com>
2020-08-17 21:44:51 +0800

    Complete Level 2

--- src/main/java/Tickbot.java
+++ src/main/java/Tickbot.java
@@ -1,19 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Tickbot {
     private static Scanner inputScanner = new Scanner(System.in);
+    private static List<String> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         printMessage("hello, This is tickbot! How can I help you?");
-        while (true) {
+        boolean running = true;
+        while (running) {
             System.out.print("==> ");
-            String command = inputScanner.nextLine();
-            if (Objects.equals(command, "bye")) {
-                printMessage("See you next time!");
+            if (!inputScanner.hasNextLine()) {
                 break;
-            } else {
-                printMessage(command);
+            }
+            String command = inputScanner.nextLine();
+            switch (command) {
+                case "bye": {
+                    printMessage("See you next time!");
+                    running = false;
+                    break;
+                }
+                case "list": {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        int index = i + 1;
+                        printMessage(index + ". " + tasks.get(i));
+                    }
+                    break;
+                }
+                default: {
+                    tasks.add(command);
+                    printMessage("Task added: " + command);
+                }
             }
         }
     }
