6633842beaa72a363b8b53f9a608d8d389f97d7c
Dexter Tan <dev@dextertanyj.com>
2020-08-17 22:10:56 +0800

    Refactor command logic for enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,33 @@ import java.util.Scanner;
 public class Duke {
 
     private static final String DIVIDER = "________________________________________________________\n";
-    private static final String LIST_COMMAND = "list";
-    private static final String DONE_COMMAND = "done";
-    private static final String EXIT_COMMAND = "bye";
-    private static final String DEADLINE_COMMAND = "deadline";
-    private static final String TODO_COMMAND = "todo";
-    private static final String EVENT_COMMAND = "event";
-    private static final String DELETE_COMMAND = "delete";
 
+    enum Command {
+        EXIT("bye"),
+        LIST("list"),
+        DONE("done"),
+        TODO("todo"),
+        DEADLINE("deadline"),
+        EVENT("event"),
+        DELETE("delete");
+
+        private final String keyword;
+
+        Command(String keyword) {
+            this.keyword = keyword;
+        }
+
+        public static Command findCommand(String keyword) throws DukeException {
+            for (Command c : values()) {
+                if (keyword.equals(c.keyword)) {
+                    return c;
+                }
+            }
+            throw new DukeException("Wakarimasen~");
+        }
+    }
+
+    // TODO: Consider moving prependIndent to utils class/package for usage in other classes.
     private static String prependIndent(String content, int indent) {
         String spaces = "";
         for (int i = 0; i < indent; i++) {
@@ -22,6 +41,12 @@ public class Duke {
             result += spaces + contentArray[i] + "\n";
         }
         return result;
+    }
+
+    private static void formattedPrint(String content) {
+        System.out.print(prependIndent(DIVIDER, 4));
+        System.out.print(prependIndent(content, 4));
+        System.out.println(prependIndent(DIVIDER, 4));
     }
 
     private static void greet() {
@@ -42,40 +67,37 @@ public class Duke {
     private static void chat() {
         Store storage = new Store();
         Scanner sc = new Scanner(System.in);
-        String input = "";
-        input = sc.nextLine();
-        while (!input.equals(EXIT_COMMAND)) {
-            System.out.print(prependIndent(DIVIDER, 4));
+        String rawInput = "";
+        Boolean exit = false;
+        while (!exit) {
+            rawInput = sc.nextLine();
             try {
-                String[] rawInput = input.split(" ", 2);
-                String command = rawInput[0];
+                String[] inputs = rawInput.split(" ", 2);
+                // By spec, inputs is guaranteed to have at least one element.
+                String commandString = inputs[0];
+                Command command = Command.findCommand(commandString);
                 switch (command) {
-                case LIST_COMMAND:
-                    System.out.printf(prependIndent(storage.listItems(), 5));
+                case EXIT:
+                    exit = true;
                     break;
-                case DONE_COMMAND:
+                case LIST:
+                    formattedPrint(prependIndent(storage.listItems(), 1));
+                    break;
+                case DONE:
                     try {
-                        int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                        System.out.printf(prependIndent(storage.markAsDone(index), 5));
+                        int index = Integer.parseInt(rawInput.split(" ")[1]) - 1;
+                        formattedPrint(prependIndent(storage.markAsDone(index), 1));
                         break;
                     } catch (NumberFormatException e) {
                         throw new DukeException("This isn't harry potter, please use only integers.");
                     }
-                case TODO_COMMAND:
+                case DEADLINE:
                     try {
-                        ToDo newToDo = new ToDo(rawInput[1]);
-                        System.out.printf(prependIndent(storage.add(newToDo), 5));
-                        break;
-                    } catch (ArrayIndexOutOfBoundsException e) {
-                        throw new DukeException("I know your life is empty but your todo can't be empty.");
-                    }
-                case DEADLINE_COMMAND:
-                    try {
-                        String deadlineDescription = rawInput[1].split("/")[0].trim();
+                        String deadlineDescription = inputs[1].split("/")[0].trim();
                         try {
-                            String due = rawInput[1].split("/by", 2)[1].trim();
+                            String due = inputs[1].split("/by", 2)[1].trim();
                             Deadline newDeadline = new Deadline(deadlineDescription, due);
-                            System.out.printf(prependIndent(storage.add(newDeadline), 5));
+                            formattedPrint(prependIndent(storage.add(newDeadline), 1));
                             break;
                         } catch (ArrayIndexOutOfBoundsException e) {
                             throw new DukeException("So you never did plan on doing it huh...");
@@ -83,13 +105,21 @@ public class Duke {
                     } catch (ArrayIndexOutOfBoundsException e) {
                         throw new DukeException("What are you rushing for? To wait?");
                     }
-                case EVENT_COMMAND:
+                case TODO:
                     try {
-                        String eventDescription = rawInput[1].split("/")[0].trim();
+                        ToDo newToDo = new ToDo(inputs[1]);
+                        formattedPrint(prependIndent(storage.add(newToDo), 1));
+                        break;
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        throw new DukeException("I know your life is empty but your todo can't be empty.");
+                    }
+                case EVENT:
+                    try {
+                        String eventDescription = inputs[1].split("/")[0].trim();
                         try {
-                            String time = rawInput[1].split("/at", 2)[1].trim();
+                            String time = inputs[1].split("/at", 2)[1].trim();
                             Event newEvent = new Event(eventDescription, time);
-                            System.out.printf(prependIndent(storage.add(newEvent), 5));
+                            formattedPrint(prependIndent(storage.add(newEvent), 1));
                             break;
                         } catch (ArrayIndexOutOfBoundsException e) {
                             throw new DukeException("Wow that sure is one long event...");
@@ -97,22 +127,18 @@ public class Duke {
                     } catch (ArrayIndexOutOfBoundsException e) {
                         throw new DukeException("Are you going to attend a nameless event?");
                     }
-                case DELETE_COMMAND:
+                case DELETE:
                     try {
-                        int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                        System.out.printf(prependIndent(storage.deleteTask(index), 5));
+                        int index = Integer.parseInt(rawInput.split(" ")[1]) - 1;
+                        formattedPrint(prependIndent(storage.deleteTask(index), 1));
                         break;
                     } catch (NumberFormatException e) {
                         throw new DukeException("This isn't harry potter, please use only integers.");
                     }
-                default:
-                    throw new DukeException("Wakarimasen~");
                 }
-            } catch (DukeException dukeEx) {
-                System.out.printf(prependIndent(dukeEx.getMessage(), 5));
+            } catch (DukeException e) {
+                formattedPrint(prependIndent(e.getMessage(), 1));
             }
-            System.out.println(prependIndent(DIVIDER, 4));
-            input = sc.nextLine();
         }
     }
 
