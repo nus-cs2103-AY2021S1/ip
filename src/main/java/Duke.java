c3a2806979e715b90f970ede1107fdadab481850
JoshuaChewJianXiang <joshuachewabc@gmail.com>
2020-08-17 17:14:54 +0800

    Completed Level-5 for Week 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Duke {
 
     public static void printTaskList(List<Task> taskList) {
@@ -17,11 +18,62 @@ public class Duke {
         }
     }
 
-    public static void markTaskAsDone(List<Task> taskList, int taskNumber) {
-        Task task = taskList.get(taskNumber - 1);
-        task.markAsDone();
-        taskList.set(taskNumber - 1, task);
-        System.out.println("Nice! I've marked this task as done:\n" + "  " + task.toString());
+    public static void markTaskAsDone(List<Task> taskList, String command) throws DukeException {
+        // If task number is not given
+        if (command.equals("done") || command.equals("done ")) {
+            throw new DukeException("OOPS!!! Please indicate the number of the task you have completed.");
+        } else {
+            try {
+                int taskNumber = Integer.parseInt(command.split(" ", 2)[1]);
+                Task task = taskList.get(taskNumber - 1);
+                task.markAsDone();
+                taskList.set(taskNumber - 1, task);
+                System.out.println("Nice! I've marked this task as done:\n" + "  " + task.toString());
+            } catch (IndexOutOfBoundsException indexException) {
+                throw new DukeException("OOPS!!! Please enter a valid task number.");
+            }
+        }
+    }
+
+    public static void addTaskToList(List<Task> taskList, String command) throws DukeException {
+        Task task;
+        String taskType = command.split(" ", 2)[0];
+        String taskBody;
+
+        // If there is no taskBody available
+        if (command.split(" ", 2).length == 1
+                || command.split(" ", 2)[1].equals("")) {
+            throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+        } else {
+            taskBody = command.split(" ", 2)[1];
+        }
+
+        // Generate task with appropriate type and add to taskList
+        if (taskType.equals("todo")) {
+            task = new Todo(taskBody);
+        } else {
+            if (taskType.equals("deadline")) {
+                try {
+                    String description = taskBody.split(" /by ")[0];
+                    String by = taskBody.split(" /by ")[1];
+                    task = new Deadline(description, by);
+                } catch (ArrayIndexOutOfBoundsException outOfBoundsException) {
+                    throw new DukeException("OOPS!!! Please add a deadline for your task.");
+                }
+            } else { // Events
+                try {
+                    String description = taskBody.split(" /at ")[0];
+                    String at = taskBody.split(" /at ")[1];
+                    task = new Event(description, at);
+                } catch (ArrayIndexOutOfBoundsException outOfBoundsException) {
+                    throw new DukeException("OOPS!!! Please add a date for your event.");
+                }
+            }
+        }
+        taskList.add(task);
+        System.out.println("Got it. I've added this task:\n"
+                + "  " + task.toString() + "\n"
+                + "Now you have " + taskList.size() + " tasks in the list");
     }
 
     public static void main(String[] args) {
@@ -39,38 +91,33 @@ public class Duke {
             command = scanner.nextLine();
             String firstWordOfCommand = command.split(" ", 2)[0];
 
-            if (command.equals("list")) {
-                printTaskList(taskList);
+            try {
+                if (firstWordOfCommand.equals("list")) {
+                    printTaskList(taskList);
 
-            } else if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
+                } else if (firstWordOfCommand.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
 
-            // Mark a task as done
-            } else if (firstWordOfCommand.equals("done")) {
-                int taskNumber = Integer.parseInt(command.split(" ", 2)[1]);
-                markTaskAsDone(taskList, taskNumber);
+                // Mark a task as done
+                } else if (firstWordOfCommand.equals("done")) {
+                    markTaskAsDone(taskList, command);
 
-            // Add task to task list
-            } else {
-                Task task;
-                String remainingWordsOfCommand = command.split(" ", 2)[1];
-                if (firstWordOfCommand.equals("todo")) {
-                    task = new Todo(remainingWordsOfCommand);
-                } else if (firstWordOfCommand.equals("deadline")) {
-                    String description = remainingWordsOfCommand.split(" /by ")[0];
-                    String by = remainingWordsOfCommand.split(" /by ")[1];
-                    task = new Deadline(description, by);
-                } else { // Event
-                    String description = remainingWordsOfCommand.split(" /at ")[0];
-                    String at = remainingWordsOfCommand.split(" /at ")[1];
-                    task = new Event(description, at);
+                // Add task to task list
+                } else if (firstWordOfCommand.equals("todo")
+                        || firstWordOfCommand.equals("deadline")
+                        || firstWordOfCommand.equals("event")) {
+                    addTaskToList(taskList, command);
+
+                // Unrecognizable commands
+                } else {
+                    throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-                taskList.add(task);
-                System.out.println("Got it. I've added this task:\n"
-                        + "  " + task.toString() + "\n"
-                        + "Now you have " + taskList.size() + " tasks in the list");
+
+            } catch (DukeException dukeException) {
+                System.out.println(dukeException.getMessage());
             }
         }
     }
+
 }
