462b69b5a29bdaddd952c2e4eac435d4b86b257b
qlchan24 <qlchan24@gmail.com>
2020-08-17 10:41:37 +0800

    Allow users to create tasks from any of the task subtypes.
    Modify the input processing to look for specific keywords representing
     the subtype of Task to create, and add new messages to complement the
      creation of these tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,14 +6,13 @@ public class Duke {
     private static final String LEFT_MARGIN = "    ";
     private static final String LEFT_MARGIN_DOUBLE = LEFT_MARGIN + LEFT_MARGIN;
     private static final String LOGO =
-            "\n" + LEFT_MARGIN + "███╗   ███╗     █████╗     ██████╗     ██╗     ██████╗ \n"
-                    + LEFT_MARGIN + "████╗ ████║    ██╔══██╗    ██╔══██╗    ██║    ██╔═══██╗\n"
-                    + LEFT_MARGIN + "██╔████╔██║    ███████║    ██████╔╝    ██║    ██║   ██║\n"
-                    + LEFT_MARGIN + "██║╚██╔╝██║    ██╔══██║    ██╔══██╗    ██║    ██║   ██║\n"
-                    + LEFT_MARGIN + "██║ ╚═╝ ██║    ██║  ██║    ██║  ██║    ██║    ╚██████╔╝\n"
-                    + LEFT_MARGIN + "╚═╝     ╚═╝    ╚═╝  ╚═╝    ╚═╝  ╚═╝    ╚═╝     ╚═════╝ \n";
-    private static final String BORDER =
-            LEFT_MARGIN + "_______________________________________________________\n";
+            "\n" + LEFT_MARGIN + "███╗   ███╗     █████╗     ██████╗     ██╗     ██████╗ \n" + LEFT_MARGIN
+                    + "████╗ ████║    ██╔══██╗    ██╔══██╗    ██║    ██╔═══██╗\n" + LEFT_MARGIN
+                    + "██╔████╔██║    ███████║    ██████╔╝    ██║    ██║   ██║\n" + LEFT_MARGIN
+                    + "██║╚██╔╝██║    ██╔══██║    ██╔══██╗    ██║    ██║   ██║\n" + LEFT_MARGIN
+                    + "██║ ╚═╝ ██║    ██║  ██║    ██║  ██║    ██║    ╚██████╔╝\n" + LEFT_MARGIN
+                    + "╚═╝     ╚═╝    ╚═╝  ╚═╝    ╚═╝  ╚═╝    ╚═╝     ╚═════╝ \n";
+    private static final String BORDER = LEFT_MARGIN + "_______________________________________________________\n";
 
     // Messages
     private static final String MESSAGE_WELCOME = "It's a-me, Mario! how can I help you today?";
@@ -21,6 +20,9 @@ public class Duke {
     private static final String MESSAGE_EMPTY = "What do you expect me to say when you didn't save any tasks?";
     private static final String MESSAGE_TASKS = "As you wish, here's what you gotta do:";
     public static final String MESSAGE_DONE = "You did it! Good job little guy!";
+    public static final String MESSAGE_INVALID = "I don't understand anything you just said";
+    public static final String MESSAGE_ADD = "Got it! I've added this task:";
+    public static final String MESSAGE_COUNT = "Now you have %s tasks in the list.";
 
     // processes the input and generates the output in the correct format.
     public static String displayOutput(String input) {
@@ -37,15 +39,28 @@ public class Duke {
                 out.append(LEFT_MARGIN_DOUBLE).append(inputArr[i].getId())
                         .append(".").append(inputArr[i]).append("\n");
             }
-            return displayOutput(out.substring(0, out.length() - 2));
+            return displayOutput(out.substring(0, out.length() - 1));
         }
     }
 
     // adds task to list
-    public static String addToList(String input, Task[] taskList) {
-        Task inputTask = new Task(input);
+    public static String addToList(Task[] taskList, char taskType, String taskStr) {
+        Task inputTask;
+        int delimiter = taskStr.indexOf("/");
+        switch (taskType) {
+        case 'D':
+            inputTask = new Deadline(taskStr.substring(0, delimiter), taskStr.substring(delimiter + 4));
+            break;
+        case 'E':
+            inputTask = new Event(taskStr.substring(0, delimiter), taskStr.substring(delimiter + 4));
+            break;
+        default:
+            inputTask = new ToDo(taskStr);
+            break;
+        }
         taskList[inputTask.getId()] = inputTask;
-        return displayOutput("added: " + inputTask.getName());
+        return displayOutput(MESSAGE_ADD + "\n" + LEFT_MARGIN_DOUBLE + inputTask + "\n"
+                + LEFT_MARGIN + String.format(MESSAGE_COUNT, Task.getCount()));
     }
 
     // mark task as done
@@ -56,19 +71,34 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean terminate = false;
         Task[] taskList = new Task[100];
         System.out.print(BORDER + LOGO + displayOutput(MESSAGE_WELCOME));
-        while (sc.hasNext()) {
-            String input = sc.nextLine();
-            if (input.equals("bye")) {
+        while (!terminate) {
+            switch (sc.next()) {
+            case "bye":
                 System.out.print(displayOutput(MESSAGE_EXIT));
+                terminate = true;
                 break;
-            } else if (input.equals("list")) {
+            case "list":
                 System.out.print(displayList(taskList));
-            } else if (input.startsWith("done")) {
-                System.out.print(markAsDone(taskList, Short.parseShort(input.substring(5))));
-            } else {
-                System.out.print(addToList(input, taskList));
+                break;
+            case "done":
+                System.out.print(markAsDone(taskList, sc.nextShort()));
+                break;
+            case "todo":
+                System.out.print(addToList(taskList, 'T', sc.nextLine()));
+                break;
+            case "event":
+                System.out.print(addToList(taskList, 'E', sc.nextLine()));
+                break;
+            case "deadline":
+                System.out.print(addToList(taskList, 'D', sc.nextLine()));
+                break;
+            default:
+                sc.nextLine();
+                System.out.print(displayOutput(MESSAGE_INVALID));
+                break;
             }
         }
     }
