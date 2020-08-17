36a83cfb4d75ae75966473709750bdd31af06624
Sushi <54584459+yuki-cell@users.noreply.github.com>
2020-08-17 12:53:34 +0800

    commit Level 3

--- iP/src/Duke.java
+++ iP/src/Duke.java
@@ -16,7 +16,7 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         String user_input = "";
-        ArrayList<String> textList = new ArrayList<String>();
+        ArrayList<Task> taskList = new ArrayList<Task>();
         while (true) {
             user_input = scanner.nextLine();  // Read user input
             if (user_input.equals("bye")) {
@@ -24,15 +24,24 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (user_input.equals("list")) {
-                // list text
+                // list task
+                System.out.println("Here are the tasks in your list:");
                 int index = 1;
-                for (String text : textList) {
-                    System.out.println(String.format("%s. %s", index, text));
+                for (Task task : taskList) {
+                    System.out.println(String.format("%s.%s %s", index, task.getStatusIcon(), task.description));
                     index += 1;
                 }
+            } else if (user_input.split(" ")[0].equals("done")) {
+                int index = Integer.parseInt(user_input.split(" ")[1]) - 1;
+                Task chosenTask = taskList.get(index);
+                chosenTask.markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(String.format("%s %s", chosenTask.getStatusIcon(), chosenTask.description));
+
             } else {
-                // add text
-                textList.add(user_input);
+                // add task
+                Task task = new Task(user_input);
+                taskList.add(task);
                 System.out.println(String.format("added: %s", user_input));  // Output user input
             }
         }
