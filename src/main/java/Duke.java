4a3836957c6c334567a06b7fe9a8e3b7d86b5f6e
Gordon <gordonfgz@gmail.com>
2020-08-17 15:24:26 +0800

    Added Task.java Duke.java: Add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,25 +16,34 @@ public class Duke {
 
         System.out.println(greeting);
         input = sc.nextLine();
-        String[] stringArray = new String[100];
+        ArrayList<Task> list = new ArrayList<>();
         int counter = 0;
 
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 System.out.println(lines);
-                for (int i = 0; i < counter; i++) {
-                    int index = i+1;
-                    String stringedIndex = Integer.toString(index);
-                    String item = stringArray[i];
-                    String outputLine = stringedIndex + ". " + item;
+                System.out.println("Here are the tasks in your list:");
+
+                for (Task task : list) {
+                    String stringedIndex = Integer.toString(list.indexOf(task) + 1);
+                    String outputLine = stringedIndex + ". " + task;
                     System.out.println(outputLine);
                 }
                 System.out.println(lines);
                 input = sc.nextLine();
+            } else if (input.contains("done")) {
+                String stringIndex = input.substring(5, input.length());
+                int index = Integer.parseInt(stringIndex);
+                Task chosen = list.get(index -1);
+                chosen.finish();
+                System.out.println(lines);
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(chosen);
+                System.out.println(lines);
+                input = sc.nextLine();
             } else {
                 System.out.println(lines);
-                stringArray[counter] = input;
-                counter = counter + 1;
+                list.add(new Task(input));
                 String outputLine = "added: " + input;
                 System.out.println(outputLine);
                 input = sc.nextLine();
