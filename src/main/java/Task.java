a239fc9daa60ad92f50ec9bdf6ae716d922a82da
Sean Tay <seantaysl@gmail.com>
2020-08-17 14:44:35 +0800

    Fill in Task class attributes and methods

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private final String name;
+    private final boolean done;
+
+    public Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+
+    public Task(String name, boolean done) {
+        this.name = name;
+        this.done = done;
+    }
+
+    public Task setDone(boolean b) {
+        return new Task(this.name, true);
+    }
+
+    public boolean isDone() {
+        return done;
+    }
 }
