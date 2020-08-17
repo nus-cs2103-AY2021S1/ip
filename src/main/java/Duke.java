a2cb107221ff3f73f379e14a400b7dc47b8502a4
Soon Xiang, Yan <65216986+y4ns00n@users.noreply.github.com>
2020-08-17 18:39:37 +0800

    added level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,8 +11,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         Scanner scan1 = new Scanner(System.in);
-        ArrayList<String> storage = new ArrayList<>();
+        ArrayList<Task> storage = new ArrayList<>();
         int count = 1;
+
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
 
         /* level 1
         while (scan1.hasNext()) {
@@ -28,9 +33,7 @@ public class Duke {
         }
         */
 
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-
+        /* level 2
         while (scan1.hasNext()) {
             String input = scan1.next();
             if (!input.equals("bye") && !input.equals("list")) {
@@ -50,5 +53,35 @@ public class Duke {
                 break;
             }
         }
+        */
+
+        while (scan1.hasNext()) {
+            String input = scan1.next();
+            if (!input.equals("bye") && !input.equals("list") && !input.equals("done")) {
+                Task task = new Task(input);
+                storage.add(task);
+                String added = "added: " + task.getDescription();
+                System.out.println(added);
+            } else if (input.equals("list")) {
+                for (Task item: storage) {
+                    System.out.println(count + ".[" + item.getStatusIcon() + "] " + item.getDescription());
+                    count++;
+                }
+            } else if (input.equals("done")) {
+                int num = scan1.nextInt();
+                Task task = storage.get(num - 1);
+                task.setDone();
+                System.out.println("Nice! I've marked this task as done;");
+                System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+            }
+            else {
+                String bye = "Bye. Hope to see you again soon!";
+                System.out.println(bye);
+                scan1.close();
+                break;
+            }
+        }
+
+
     }
 }
