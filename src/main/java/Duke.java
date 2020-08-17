66642c4e15541540157b3566e9c98d442ad0e82a
rolandyuwy <rolandyuwenyang@hotmail.com>
2020-08-17 20:32:00 +0800

    Level-3: Added Task.java. Added ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,45 +1,67 @@
+import javax.annotation.processing.SupportedSourceVersion;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
 /**
  * Duke, more commonly known as Duck, is a Personal Assistant Chat Bot that
- * helps a person to keep track of various things.
- * Contains static attribute stored_text which stores text input from user.
+ * helps a person to keep track of various tasks.
+ * Contains static attribute stored_task which stores task input from user.
  **/
 public class Duke {
 
-    public static List<String> stored_text = new ArrayList<>();
+    public static List<Task> stored_task = new ArrayList<>();
 
     /**
-     * Lists stored text by looping through stored_text.
+     * Lists stored task by looping through stored_task, along with their status.
      **/
-    public static void listStoredText() {
-        if (stored_text.isEmpty()) {
+    public static void listStoredTasks() {
+        if (stored_task.isEmpty()) {
             System.out.println("No text stored...");
         } else {
             int count = 1;
-            for (String text : stored_text) {
-                System.out.println(count + ". " + text);
+            for (Task task : stored_task) {
+                System.out.println(count + ". " + task.getStatusIcon() + " " + task.getDescription());
                 count++;
             }
         }
     }
 
     /**
-     * Adds input text into stored_text.
+     * Adds input task into stored_task.
      *
-     * @param input Input text from user to be stored.
+     * @param input Input task from user to be stored.
      **/
-    public static void addText(String input) {
-        stored_text.add(input);
+    public static void addTask(String input) {
+        stored_task.add(new Task(input));
         System.out.println("Quack! I have added: " + input + " \uD83C\uDF0A");
     }
 
     /**
+     * Marks task as done.
+     *
+     * @param taskNumber Task number of task to be marked as done.
+     **/
+    public static void markTaskAsDone(int taskNumber) {
+        if (taskNumber <= 0 || taskNumber > stored_task.size()) {
+            System.out.println("Wrong task number!");
+        } else {
+            Task t = stored_task.get(taskNumber - 1);
+            if (t.checkIfDone()) {
+                System.out.println("This task is already done: " + t.getDescription());
+            } else {
+                t.markAsDone();
+                System.out.println("Quack! I have marked this task as done: \n" +
+                        t.getStatusIcon() + " " + t.getDescription() + " \uD83C\uDF0A");
+            }
+        }
+    }
+
+    /**
      * Prints greeting message.
-     * Scans for commands entered by the user, then stores input text into stored_text.
-     * Upon user command input "list", stored text will be listed.
+     * Scans for commands entered by the user, then stores input task into stored_task.
+     * Upon user command input "done" followed by the task number, task will be marked as done.
+     * Upon user command input "list", stored task will be listed.
      * Upon user command input "bye", system is exited.
      **/
     public static void main(String[] args) {
@@ -59,11 +81,16 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 System.out.println(line);
-                listStoredText();
+                listStoredTasks();
+                System.out.println(line);
+            } else if (input.startsWith("done ") && input.length() > 5) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                System.out.println(line);
+                markTaskAsDone(taskNumber);
                 System.out.println(line);
             } else {
                 System.out.println(line);
-                addText(input);
+                addTask(input);
                 System.out.println(line);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,51 @@
+/**
+ * Contains task description and completion status of task.
+ **/
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Constructor for Task.
+     *
+     * @param description Task description
+     **/
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Retrieves status and returns icon corresponding to status.
+     *
+     * @return Icon corresponding to status.
+     **/
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    /**
+     * Gets task description.
+     *
+     * @return Task description.
+     **/
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Marks task as done.
+     **/
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /**
+     * Checks if task is done.
+     *
+     * @return If Task is Done.
+     **/
+    public boolean checkIfDone() {
+        return isDone;
+    }
+}
