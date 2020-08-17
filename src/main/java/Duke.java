e04713af81d5284bd5f00da4f2857112e7781e0d
foongsiqi <edu.foongs@gmail.com>
2020-08-17 12:27:50 +0800

    implement mark as done feature, create Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -7,7 +6,7 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         System.out.println("Yooo, I'm Deke.\nWhat can I do for you today?"); //Greeting
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         String input = sc.nextLine();
 
         while (!input.isEmpty()) {
@@ -16,13 +15,23 @@ public class Duke {
                 input = "";
                 sc.close();
             } else if (input.equals("list")) { //if user types "list"
-                //do a for loop to print out the list
+                System.out.println("here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
                     System.out.println(i + 1 + ". " + list.get(i));
                 }
                 input = sc.nextLine();
+            } else if (input.contains("done")) { //if user input contains "done"
+                int index = Integer.parseInt(input.split(" ")[1]);
+                if (index >= 1) { //if input index is valid
+                    Task newTask = list.get(index - 1).markAsDone();
+                    list.set(index - 1, newTask);
+                    System.out.println("Nice! I've marked this task as doneee! yayy:\n" + newTask);
+                } else {
+                    System.out.println("Please enter a valid input number");
+                }
+                input = sc.nextLine();
             } else { //if user types anything other than "bye" or "list"
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("added: " + input);
                 input = sc.nextLine();
             }
