13b5afd5007c5c694556ab05a2aea5ff18939051
lowjiefeng <lowjiefeng1998@gmail.com>
2020-08-18 02:50:01 +0800

    Refactor exit to constant

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static final String DIVIDER = "----------------------------";
+    private static final String EXIT_COMMAND = "bye";
 
     private static String sandwichWithDivider(String text) {
         return DIVIDER + "\n" + text + "\n" + DIVIDER;
@@ -16,13 +17,12 @@ public class Duke {
     private static final String EXIT_MESSAGE = sandwichWithDivider("Bye! See you around!");
 
     public static void main(String[] args) {
-        String exitCommand = "bye";
         Scanner sc = new Scanner(System.in);
 
         System.out.println(HELLO_MESSAGE);
         while (true) {
             String command = sc.next();
-            if (command.equals(exitCommand)) {
+            if (command.equals(EXIT_COMMAND)) {
                 System.out.println(EXIT_MESSAGE);
                 break;
             } else {
