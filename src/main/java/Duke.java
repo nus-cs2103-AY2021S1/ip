ac9730d4a80afc2a1c3e81c0d862f948519c0ab9
chanwei53 <hanwei_cheak@hotmail.sg>
2020-08-17 17:09:18 +0800

    Completed Level-4: ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
 
     public static void main(String[] args) {
         greet();
-        readTasks();
+        readInputs();
     }
 
     private static void greet() {
@@ -22,41 +22,83 @@ public class Duke {
     }
 
     private static void printTasks() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             Task task = tasks.get(i);
-            System.out.println(i + 1 + ". " + task.getStatusIcon() + " " + task);
+            System.out.println(i + 1 + ". " + task);
         }
         System.out.println("--------------------------" +
                 "-------------------------");
     }
 
+    private static void printTotalNumberOfTasks() {
+        int numTasks = tasks.size();
+        if (numTasks < 2) {
+            System.out.println("Now you have " + numTasks + " task in the list.");
+        } else {
+            System.out.println("Now you have " + numTasks + " tasks in the list.");
+        }
+    }
+
     private static void setDoneTask(int index) {
-        Task task = tasks.get(index - 1);
+        Task task = tasks.get(index - 1); // index - 1 to match the index in ArrayList
         task.markDone();
         System.out.println("Nice! I've marked this task as done: " +
-                task.getStatusIcon() + " " + task +
+                task +
                 "\n---------------------------------------------------");
     }
 
-    private static void readTasks() {
+    private static void handleTask(String input) {
+        String taskType = input.split(" ")[0];
+        if (taskType.equals("done")) {
+            String taskIndex = input.split(" ")[1];
+            setDoneTask(Integer.parseInt(taskIndex));
+        } else {
+            String taskName = "";
+            if (taskType.equals("todo")){
+                taskName = input.substring(5);
+                Task taskCreated = new Todo(taskName);
+                tasks.add(taskCreated);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("\t" + taskCreated);
+                printTotalNumberOfTasks();
+            } else if (taskType.equals("deadline")) {
+                String[] task = input.substring(9).split(" /by ");
+                taskName = task[0];
+                String timeBy = task[1];
+                Task taskCreated = new Deadline(taskName, timeBy);
+                tasks.add(taskCreated);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("\t" + taskCreated);
+                printTotalNumberOfTasks();
+            } else if (taskType.equals("event")) {
+                String[] task = input.substring(6).split(" /at ");
+                taskName = task[0];
+                String timeAt = task[1];
+                Task taskCreated = new Event(taskName, timeAt);
+                tasks.add(taskCreated);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("\t" + taskCreated);
+                printTotalNumberOfTasks();
+            } else { }
+            System.out.println("___________________________________________________");
+        }
+    }
+
+    private static void readInputs() {
         Scanner sc = new Scanner(System.in);
-        String reply = "Duke: Added - " + "%s" + "\n--------------------------" +
-                "-------------------------";
         while (sc.hasNext()) {
             String input = sc.nextLine();
-            Task newTask = new Task(input);
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 sc.close();
                 break;
             } else if (input.equals("list")) {
                 printTasks();
-            } else if (input.split(" ")[0].equals("done")) {
-                setDoneTask(Integer.parseInt(input.split(" ")[1]));
             } else {
-                tasks.add(newTask);
-                System.out.println(String.format(reply, newTask));
+               handleTask(input);
             }
+
         }
     }
 }
