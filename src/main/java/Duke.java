d0c64abe735b00afa068f14229459489f038dc91
yongping827 <yongping827@users.noreply.github.com>
2020-08-17 22:54:27 +0800

    Add Level 1. Greet. Echo. Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String lineIndentation = "    ";
+        String horizontalLine = lineIndentation + "____________________________________________________________\n";
+
+        String textIndentation = "     ";
+        String greetMessage = textIndentation + " Hello! I'm Duke\n" +
+                textIndentation + " What can I do for you?\n";
+        String exitMessage = textIndentation + "Bye. Hope to see you again soon!";
+
+        System.out.println(horizontalLine + greetMessage + horizontalLine);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String inputMessage = sc.next();
+            String echoMessage = textIndentation + inputMessage;
+
+            if (inputMessage.toLowerCase().equals("bye")) {
+                System.out.println(horizontalLine + exitMessage + "\n" + horizontalLine);
+                break;
+            } else {
+                System.out.println(horizontalLine + echoMessage + "\n" + horizontalLine);
+            }
+        }
     }
 }
