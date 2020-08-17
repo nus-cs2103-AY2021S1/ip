e3331cae8f0449d50035ef9238b6a0ab50e56048
jmleong666 <ljming4848@hotmail.com>
2020-08-17 17:34:42 +0800

    Implement Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
+    boolean echoInput(String input) {
+        if (input.equals("bye")) {
+            exit();
+            return true;
+        }
+        System.out.println(
+            "    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n    "
+            + input
+            + "\n    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+        );
+        return false;
+    }
+
+    void exit() {
+        System.out.println(
+            "    ========================================================\n"
+            + "    Goodbye\n"
+            + "    ========================================================"
+        );
+    }
+
     public static void main(String[] args) {
+        Duke duke = new Duke();
+        Scanner scanner = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello my name\n" + logo + "\nHow may I help?");
+
+        while (scanner.hasNext()) {
+            String input = scanner.next();
+            boolean exit = duke.echoInput(input);
+            if (exit) {
+                scanner.close();
+                System.exit(0);
+            }
+        }
     }
 }
