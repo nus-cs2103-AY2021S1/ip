a769cc50184c59ca73426059c800d5149e297a91
angrybunny123 <kennethteo98@gmail.com>
2020-08-17 16:10:01 +0800

    completed level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,33 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] tasks;
-    private static int count;
+    private static Task[] tasks = new Task[100];
+    private static int count = 0;
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         Scanner userInput = new Scanner(System.in);
-        tasks = new String[100];
-        count = 0;
-
         while (userInput.hasNext()) {
             String input = userInput.nextLine();
+            String[] splitArr = input.split(" ");
             if (input.equals("bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.length; i++) {
                     if (tasks[i] != null) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                        System.out.println((i + 1) + ". " + tasks[i].getStatusIcon() + " " + tasks[i].getDescription());
                     } else {
                         break;
                     }
                 }
+            } else if (splitArr.length == 2 && splitArr[0].equals("done") && Integer.parseInt(splitArr[1]) > 0) {
+                int index = Integer.parseInt(splitArr[1]);
+                tasks[index - 1].setDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[index -1].getStatusIcon() + " " + tasks[index - 1].getDescription());
             } else {
-                tasks[count] = input;
+                tasks[count] = new Task(input);
                 count ++;
                 System.out.println("added: " + input);
             }
