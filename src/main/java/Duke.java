0dfe622da33da2899b452758c2c8c439cccb49eb
CalistaIo <e0426281@u.nus.edu>
2020-08-17 20:21:24 +0800

    Added Level-1 of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        while (!s.equals("bye")) {
+            System.out.println("-------------------------------------------------------------------------------------");
+            System.out.println(s);
+            System.out.println("-------------------------------------------------------------------------------------");
+            s = scan.nextLine();
+        }
+        
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-------------------------------------------------------------------------------------");
     }
 }
