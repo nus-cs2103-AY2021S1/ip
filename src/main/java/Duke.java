ac825489084e1527f552bd68b4f5b16a7768e69b
Wong-ZZ <wongzhengzhi@gmail.com>
2020-08-17 19:44:59 +0800

    Added ability to greet users, echo commands and exit when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String DIVIDER = "____________________________________________________________\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String start_msg =
+            DIVIDER +
+            "Hello! I'm Duke\n" +
+            "What can I do for you?\n" +
+            DIVIDER;
+        System.out.println(start_msg);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        while(!input.equals("bye")) {
+            System.out.println(get_echo(input));
+            input = sc.next();
+        }
+
+        String exit_msg =
+            DIVIDER +
+            "Bye. Hope to see you again soon!\n" +
+            DIVIDER;
+        System.out.println(exit_msg);
+    }
+
+    private static String get_echo(String str) {
+        return DIVIDER + str + "\n" + DIVIDER;
     }
 }
