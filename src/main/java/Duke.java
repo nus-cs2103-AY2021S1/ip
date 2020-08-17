254597439f71b62af506337ca85fef742def99c1
sgn00 <37658877+sgn00@users.noreply.github.com>
2020-08-17 18:55:29 +0800

    Abstract list and done into methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
+    List<Task> store;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,9 +17,24 @@ public class Duke {
         duke.run();
     }
 
+    public void list() {
+        System.out.println("Here are the tasks in your list:");
+        int count = 0;
+        for (Task task : store) {
+            count++;
+            System.out.println(String.format("%d. %s", count, task));
+        }
+    }
+
+    public void doneTask(int num) {
+        store.get(num - 1).setDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(store.get(num - 1));
+    }
+
     public void run() {
         Scanner scanner = new Scanner(System.in);
-        List<Task> store = new ArrayList<>();
+        store = new ArrayList<>();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         boolean running = true;
         while (running) {
@@ -28,18 +46,11 @@ public class Duke {
                     running = false;
                     break;
                 case "list":
-                    System.out.println("Here are the tasks in your list:");
-                    int count = 0;
-                    for (Task task : store) {
-                        count++;
-                        System.out.println(String.format("%d. %s", count, task));
-                    }
+                    list();
                     break;
                 case "done":
                     int num = Integer.parseInt(splitted[1]);
-                    store.get(num - 1).setDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(store.get(num - 1));
+                    doneTask(num);
                     break;
                 default:
                     store.add(new Task(input));
