d6ff8fc45fadd6006cf7c74259eb6fb5a20050d7
Preston Toh <prestontoh@gmail.com>
2020-08-17 17:24:05 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,24 +5,30 @@ public class Duke {
         Scanner inputs = new Scanner(System.in);
         boolean run = true;
         int noOfTasks = 0;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
 
         System.out.println("Hello~, I'm Duke!\n"
-        + "What can I do for you?");
+                + "What can I do for you?");
 
         while (run == true) {
-            String next = inputs.nextLine();
+            String next = inputs.next();
             System.out.println("____________________________________________________________");
             if (next.equals("bye")) {
                 System.out.println("Goodbye~ Hope to see you again soon!");
                 run = false;
+
             } else if (next.equals("list")) {
                 for (int i = 0; i < noOfTasks; i++) {
-                    int taskNo = i + 1;
-                    System.out.println(taskNo + ". " + tasks[i]);
+                    System.out.println(tasks[i]);
                 }
+            } else if (next.equals("done")) {
+                int doneTask = Integer.parseInt(inputs.next()) - 1;
+                tasks[doneTask].setDone();
+                System.out.println("Nice! I've set this task as done~\n" +
+                        tasks[doneTask]);
             } else {
-                tasks[noOfTasks] = next;
+                Task newTask = new Task(next, noOfTasks + 1);
+                tasks[noOfTasks] = newTask;
                 noOfTasks += 1;
                 System.out.println("Added: " + next);
             }
