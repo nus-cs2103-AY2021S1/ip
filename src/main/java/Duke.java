ed47932e8acec09a04c118e2c3b82375ad763fa5
siqiang-ng <nsq.siqiang@gmail.com>
2020-08-17 21:38:57 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.net.IDN;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,6 +11,7 @@ public class Duke {
             + INDENT + "| | | | | | | |/ / _ \\\n"
             + INDENT + "| |_| | |_| |   <  __/\n"
             + INDENT + "|____/ \\__,_|_|\\_\\___|\n";
+    private static ArrayList<String> lst = new ArrayList<>();
 
     private static void intro() {
         System.out.println(horizL + "\n" + logo + "\n" +
@@ -16,9 +19,20 @@ public class Duke {
                 INDENT + "How can I help you?\n" + horizL);
     }
 
-    private static void bye() {
+    private static void ending() {
         System.out.println("\n" + INDENT +
                 "Bye! Hope to see you again soon!\n" + horizL);
+    }
+
+    private static void displayList() {
+        int len = lst.size();
+        int index = 1;
+        System.out.println(horizL);
+        for (int i = 0; i < len; i++) {
+            System.out.println(INDENT + index + ". " + lst.get(i));
+            index++;
+        }
+        System.out.println(horizL);
     }
 
     public static void main(String[] args) {
@@ -29,12 +43,15 @@ public class Duke {
             if (input.toLowerCase().equals("bye")) {
                 System.out.print(horizL);
                 break;
+            } else if (input.toLowerCase().equals("list")) {
+                Duke.displayList();
             } else {
+                lst.add(input);
                 System.out.println(horizL);
-                System.out.println(INDENT + input);
+                System.out.println(INDENT + "Added: " + input);
                 System.out.println(horizL);
             }
         }
-        Duke.bye();
+        Duke.ending();
     }
 }
