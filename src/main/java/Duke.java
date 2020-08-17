414edbe9a43ef38907010e17c855b76396bc073e
zhaolingshan <e0425857@u.nus.edu>
2020-08-17 18:37:51 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void readUserInput() {
+        Scanner sc = new Scanner(System.in);
+        String display = sc.next();
+        if (display.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(display);
+            readUserInput();
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "Hello! I'm Duke \n" + "What can I do for you?";
+        System.out.println("Hello from\n" + logo + "\n" + greeting);
+        readUserInput();
     }
 }
