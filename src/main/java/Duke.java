1e4c400c92071693af1837c61ef08be58c7c2570
Nahoyhp <phyohan@hotmail.com>
2020-08-17 20:54:23 +0800

    add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+    /**
+     * Print on screen the message wrapped with dotted lines.
+     * @param message Message to be printed out
+     */
+    public static void echo(String message) {
+        String line = "____________________________________________________________\n";
+        System.out.println(line + message + "\n" + line);
+
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        echo("    Hello! I'm Duke\n    How can I help you?");
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")){
+                echo("Bye. Hope to see you again, bro!");
+                break;
+            }
+            echo(input);
+        }
     }
 }
