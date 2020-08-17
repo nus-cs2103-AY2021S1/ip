f9ae0c5969e2fbe362e5017cf43758548aac408f
Zhu Xiaochen <zhuxiaochen1005@gmail.com>
2020-08-17 14:43:31 +0800

    Implement "mark as done" feature

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -7,30 +7,39 @@ import java.util.Scanner;
 
 // The chat bot class to handle the internal logic
 public class ChatBot {
-    List<String> list = new LinkedList<>();
+    List<Task> list = new LinkedList<>();
 
     // The entry point to run the chat bot
     public void start() {
-        String welcome = "Hello! I'm Duke \n    What can I do for you?";
+        String welcome = " Hello! I'm Duke \n     What can I do for you?";
         sendChat(welcome);
         String response = receiveChat();
         while (!response.equals("bye")) {
             if (response.equals("list")) {
                 printList();
+            } else if (response.startsWith("done")) {
+                int index = Integer.parseInt(response.split(" ")[1]) - 1;
+                list.get(index).markAsDone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Nice! I've marked this task as done: ");
+                System.out.println("       "+list.get(index));
+                System.out.println("    ____________________________________________________________");
             } else {
-                sendChat("Added: " + response);
-                list.add(response);
+                Task newTask = new Task(response);
+                sendChat(" added: " + response);
+                list.add(newTask);
             }
             response = receiveChat();
         }
-        sendChat("Bye. Hope to see you again soon!");
+        sendChat(" Bye. Hope to see you again soon!");
     }
 
     // Return the list of tasks
     private void printList() {
         System.out.println("    ____________________________________________________________");
+        System.out.println("     Here are the tasks in your list:");
         for (int i = 1; i <= list.size(); i++) {
-            System.out.println("    " + i + ". " + list.get(i - 1));
+            System.out.println("     " + i + "." + list.get(i - 1));
         }
         System.out.println("    ____________________________________________________________");
     }
