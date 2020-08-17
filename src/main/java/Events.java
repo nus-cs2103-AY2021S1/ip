4ba4c7630e39cdb7177b12b4dee822dae9120fa5
yanbingtao <yanbingtao963@Gmail.com>
2020-08-17 22:54:28 +0800

    Implemented Events Class with constructor, createEvent method and toString method (@override)

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,10 +1,20 @@
-public class Events extends Task{
-    public Events(String taskTitle) {
+import java.util.ArrayList;
+
+public class Events extends Task {
+    private String eventTime;
+
+    public Events(String taskTitle, String eventTime) {
         super(taskTitle);
+        this.eventTime = eventTime;
+    }
+
+    public static void createEvent(String taskTitle, String eventTime, ArrayList<Task> tasks) {
+        Events newEvent = new Events(taskTitle, eventTime);
+        tasks.add(newEvent);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString();
+        return "[E]" + super.toString()+ " (at:" + eventTime + ")";
     }
 }
