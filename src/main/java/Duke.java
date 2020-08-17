a71b0c48d7dd43ce83189bfd0f211db89949a21f
Gordon <gordonfgz@gmail.com>
2020-08-17 17:48:01 +0800

    Automated Text UI Testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,67 +15,66 @@ public class Duke {
                 "____________________________________________________________";
 
         System.out.println(greeting);
-        input = sc.nextLine();
-        ArrayList<Task> list = new ArrayList<>();
-        int counter = 0;
+        if (sc.hasNextLine()) {
+            input = sc.nextLine();
+            ArrayList<Task> list = new ArrayList<>();
 
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                System.out.println(lines);
-                System.out.println("Here are the tasks in your list:");
+            while (!input.equals("bye")) {
+                if (input.equals("list")) {
+                    System.out.println(lines);
+                    System.out.println("Here are the tasks in your list:");
 
-                for (Task task : list) {
-                    String stringedIndex = Integer.toString(list.indexOf(task) + 1);
-                    String outputLine = stringedIndex + ". " + task;
-                    System.out.println(outputLine);
+                    for (Task task : list) {
+                        String stringedIndex = Integer.toString(list.indexOf(task) + 1);
+                        String outputLine = stringedIndex + ". " + task;
+                        System.out.println(outputLine);
+                    }
+                    System.out.println(lines);
+                    input = sc.nextLine();
+                } else if (input.contains("done")) {
+                    String stringIndex = input.substring(5, input.length());
+                    int index = Integer.parseInt(stringIndex);
+                    Task chosen = list.get(index -1);
+                    chosen.finish();
+                    System.out.println(lines);
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(chosen);
+                    System.out.println(lines);
+                    input = sc.nextLine();
+                } else if (input.contains("todo") || input.contains("deadline") || input.contains("event")) {
+                    Task task = null;
+                    if (input.contains("todo")) {
+                        task = new Todo(input.substring(5, input.length()));
+                    } else if (input.contains("deadline")) {
+                        String desc = input.substring(9, input.indexOf("/") - 1);
+                        String by = input.substring(input.indexOf("/") + 4, input.length());
+                        task = new Deadline(desc, by);
+                    } else if (input.contains("event")) {
+                        String desc = input.substring(6, input.indexOf("/") - 1);
+                        String at = input.substring(input.indexOf("/") + 4, input.length());
+                        task = new Event(desc, at);
+                    }
+
+                    System.out.println(lines);
+                    System.out.println(" Got it. I've added this task: ");
+
+                    list.add(task);
+                    System.out.println("  " + task);
+                    int listCount = list.size();
+                    System.out.println(" Now you have " + Integer.toString(listCount) + " tasks in the list.");
+                    System.out.println(lines);
+                    input = sc.nextLine();
+
+
+
+                } else {
+
                 }
-                System.out.println(lines);
-                input = sc.nextLine();
-            } else if (input.contains("done")) {
-                String stringIndex = input.substring(5, input.length());
-                int index = Integer.parseInt(stringIndex);
-                Task chosen = list.get(index -1);
-                chosen.finish();
-                System.out.println(lines);
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(chosen);
-                System.out.println(lines);
-                input = sc.nextLine();
-            } else if (input.contains("todo") || input.contains("deadline") || input.contains("event")) {
-                Task task = null;
-                if (input.contains("todo")) {
-                    task = new Todo(input.substring(5, input.length()));
-                } else if (input.contains("deadline")) {
-                    String desc = input.substring(9, input.indexOf("/") - 1);
-                    String by = input.substring(input.indexOf("/") + 4, input.length());
-                    task = new Deadline(desc, by);
-                } else if (input.contains("event")) {
-                    String desc = input.substring(6, input.indexOf("/") - 1);
-                    String at = input.substring(input.indexOf("/") + 4, input.length());
-                    task = new Event(desc, at);
-                }
-
-                System.out.println(lines);
-                System.out.println(" Got it. I've added this task: ");
-
-                list.add(task);
-                System.out.println("  " + task);
-                int listCount = list.size();
-                System.out.println(" Now you have " + Integer.toString(listCount) + " tasks in the list.");
-                System.out.println(lines);
-                input = sc.nextLine();
-
-            } else {
-
             }
+            System.out.println(lines);
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(lines);
         }
 
-
-        System.out.println(lines);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(lines);
-
-
     }
-
 }
