8aa1e9df3f8480daecd72a3ec49257afc7236a73
Tan Su Yin <e0425292@u.nus.edu>
2020-08-17 16:13:47 +0800

    Add ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] tasks = new String[100];
+    private static Task[] tasks = new Task[100];
     private static int index = 0;
 
     public static void init() {
@@ -21,13 +21,14 @@ public class Duke {
         return dashedLine.repeat(24);
     }
 
-    public static void printTasks() {
+    public static void printAllTasks() {
         if (index == 0) {
             System.out.println("You have no outstanding tasks, milady.");
         } else {
             System.out.println(dashedLineBreak());
+            System.out.println("Your current list, milady:");
             for (int i = 0; i < index; i++) {
-                System.out.printf("%s. %s", i + 1, tasks[i]);
+                System.out.printf("%s.%s", i + 1, tasks[i]);
                 System.out.println();
             }
             System.out.println(dashedLineBreak());
@@ -36,8 +37,26 @@ public class Duke {
     }
 
     public static void addTask(String task) {
-        tasks[index] = task;
+        Task newTask = new Task(task);
+        tasks[index] = newTask;
         index++;
+        System.out.println(dashedLineBreak());
+        System.out.println("added: " + task);
+        System.out.println(dashedLineBreak());
+        System.out.println();
+    }
+
+    public static void doneTask(int taskNumber) {
+        if (taskNumber - 1 < 0 || taskNumber - 1 > 99 || tasks[taskNumber - 1] == null) {
+            System.out.println("Milady, there's no such task in my detailed records.");
+        } else {
+            tasks[taskNumber - 1].markAsDone();
+            System.out.println(dashedLineBreak());
+            System.out.println("As you wish, milady. I have marked this as done.");
+            System.out.println(tasks[taskNumber - 1]);
+            System.out.println(dashedLineBreak());
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -45,18 +64,22 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
             String userInput = sc.nextLine();
-            if (userInput.equalsIgnoreCase("list")) {
-                printTasks();
-            } else if (userInput.equalsIgnoreCase("bye")){
-                System.out.println("Your wish is my command, milady. Till I see you again.");
-                sc.close();
-                System.exit(0);
-            } else {
-                addTask(userInput);
-                System.out.println(dashedLineBreak());
-                System.out.println("added: " + userInput);
-                System.out.println(dashedLineBreak());
-                System.out.println();
+            String[] splitUserInput = userInput.split(" ");
+            switch(splitUserInput[0].toLowerCase()) {
+                case "bye":
+                    System.out.println("Your wish is my command, milady. Till I see you again.");
+                    sc.close();
+                    System.exit(0);
+                    break;
+                case "list":
+                    printAllTasks();
+                    break;
+                case "done":
+                    int taskNumber = Integer.parseInt(splitUserInput[1]);
+                    doneTask(taskNumber);
+                    break;
+                default:
+                    addTask(userInput);
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected boolean isDone;
+    protected String description;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return this.isDone ? "\u2713" : "\u2718";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
