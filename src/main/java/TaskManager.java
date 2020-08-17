52880beef17a53bbb86cd7538fe8a8bd0845f2e2
davidcaiqifan <e0406982@u.nus.edu>
2020-08-17 20:30:01 +0800

    implemented TaskManager class for level 2

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskManager {
+    private List<String> toDoList;
+    public TaskManager() {
+        this.toDoList = new ArrayList<>();
+    }
+    public void addToList(String task) {
+        this.toDoList.add(task);
+    }
+    public void displayList() {
+        for (String s : this.toDoList) {
+            System.out.println(s);
+        }
+    }
 }
