f9f888d7042f7d7f692589e9f13e156cd5838621
lowjiefeng <lowjiefeng1998@gmail.com>
2020-08-18 02:00:50 +0800

    Implement skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String DIVIDER = "----------------------------";
+
+    private static String sandwichWithDivider(String text) {
+        return DIVIDER + "\n" + text + "\n" + DIVIDER;
+    }
+
+    private static String processCommand(String command) {
+        return sandwichWithDivider(command);
+    }
+
+    private static final String HELLO_MESSAGE = sandwichWithDivider("Greetings! I am Duke."
+            + "\nWhat can I do for you?");
+    private static final String EXIT_MESSAGE = sandwichWithDivider("Bye! See you around!");
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String exitCommand = "bye";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(HELLO_MESSAGE);
+        while (true) {
+            String command = sc.next();
+            if (command.equals(exitCommand)) {
+                System.out.println(EXIT_MESSAGE);
+                break;
+            } else {
+                System.out.println(processCommand(command));
+            }
+        }
+
+        sc.close();
     }
 }
