d48fa3efba0bda135be3502d7075b315d164e165
CalistaIo <e0426281@u.nus.edu>
2020-08-17 22:53:26 +0800

    Added Level-5 of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,15 +5,89 @@ public class Duke {
     private static Task[] lst = new Task[100];
     private static int counter = 0;
 
-    public static Task getTask(String st) {
-        if ((st.length() >= 4) && (st.substring(0, 4).equals("todo"))) {
-            return new ToDo(st.substring(5));
+    public static void showList() {
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Here are the tasks in your list: ");
+        for (int i = 0; i < counter; i++) {
+            System.out.println((i + 1) + "." + lst[i]);
+        }
+        System.out.println("-------------------------------------------------------------------------------------");
+    }
+
+    public static void markDone(int i) {
+        lst[i].markAsDone();
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("    " + lst[i]);
+        System.out.println("-------------------------------------------------------------------------------------");
+    }
+
+    public static void addTask(Task t) {
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Got it. I've added this task:");
+        lst[counter] = t;
+        System.out.println("    " + lst[counter]);
+        counter += 1;
+        System.out.println("Now you have " + counter + " tasks in the list.");
+        System.out.println("-------------------------------------------------------------------------------------");
+    }
+
+    public static void processString(String st) throws InvalidDoneException, InvalidTaskArgumentException,
+            InvalidCommandException {
+        if (st.equals("list")) {
+            showList();
+        } else if ((st.length() >= 4) && (st.substring(0, 4).equals("done"))) {
+            if (st.length() <= 5) {
+                throw new InvalidDoneException("\u2639" + " OOPS!!! The task to be marked as done is not " +
+                        "specified.");
+            } else {
+                try {
+                    int i = Integer.parseInt(st.substring(5)) - 1;
+                    if (i >= counter) {
+                        throw new InvalidDoneException("\u2639" + " OOPS!!! The number specified does not represent " +
+                                "a valid task.");
+                    }
+                    markDone(i);
+                } catch (NumberFormatException e) {
+                    throw new InvalidDoneException("\u2639" + " OOPS!!! The task to be marked as done is not " +
+                            "specified by a valid number.");
+                }
+            }
+        } else if ((st.length() >= 4) && (st.substring(0, 4).equals("todo"))) {
+            if (st.length() <= 5) {
+                throw new InvalidTaskArgumentException("\u2639" + " OOPS!!! The description of a todo cannot " +
+                        "be empty.");
+            } else {
+                addTask(new ToDo(st.substring(5)));
+            }
         } else if ((st.length() >= 8) && (st.substring(0, 8).equals("deadline"))) {
-            String[] arr = st.substring(9).split(" /by ");
-            return new Deadline(arr[0], arr[1]);
+            if (st.length() <= 9) {
+                throw new InvalidTaskArgumentException("\u2639" + " OOPS!!! The deadline is lacking a " +
+                        "description/date.");
+            } else {
+                String[] arr = st.substring(9).split(" /by ");
+                if (arr.length < 2) {
+                    throw new InvalidTaskArgumentException("\u2639" + " OOPS!!! The deadline is lacking a " +
+                            "description/date.");
+                } else {
+                    addTask(new Deadline(arr[0], arr[1]));
+                }
+            }
+        } else if ((st.length() >= 5) && (st.substring(0, 5).equals("event"))) {
+            if (st.length() <= 6) {
+                throw new InvalidTaskArgumentException("\u2639" + " OOPS!!! The event is lacking a " +
+                        "description/date.");
+            } else {
+                String[] arr = st.substring(6).split(" /at ");
+                if (arr.length < 2) {
+                    throw new InvalidTaskArgumentException("\u2639" + " OOPS!!! The event is lacking a " +
+                            "description/date.");
+                } else {
+                    addTask(new Event(arr[0], arr[1]));
+                }
+            }
         } else {
-            String[] arr = st.substring(6).split(" /at ");
-            return new Event(arr[0], arr[1]);
+            throw new InvalidCommandException("\u2639" + " OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
     }
 
@@ -33,30 +107,15 @@ public class Duke {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         while (!s.equals("bye")) {
-            if (s.equals("list")) {
+            try {
+                processString(s);
+            } catch (InvalidTaskArgumentException | InvalidDoneException | InvalidCommandException e) {
                 System.out.println("-------------------------------------------------------------------------------------");
-                System.out.println("Here are the tasks in your list: ");
-                for (int i = 0; i < counter; i++) {
-                    System.out.println((i + 1) + "." + lst[i]);
-                }
+                System.out.println(e.getMessage());
                 System.out.println("-------------------------------------------------------------------------------------");
-            } else if ((s.length() >= 4) && (s.substring(0, 4).equals("done"))) {
-                int i = Integer.parseInt(s.substring(5)) - 1;
-                lst[i].markAsDone();
-                System.out.println("-------------------------------------------------------------------------------------");
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println("    " + lst[i]);
-                System.out.println("-------------------------------------------------------------------------------------");
-            } else {
-                System.out.println("-------------------------------------------------------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                lst[counter] = getTask(s);
-                System.out.println("    " + lst[counter]);
-                counter += 1;
-                System.out.println("Now you have " + counter + " tasks in the list.");
-                System.out.println("-------------------------------------------------------------------------------------");
+            } finally {
+                s = scan.nextLine();
             }
-            s = scan.nextLine();
         }
 
         System.out.println("-------------------------------------------------------------------------------------");
