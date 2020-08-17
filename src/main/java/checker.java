1fa30d89c1b06928c6df041800d4f296c3065a63
MatthiasLHK <matthiasli1998@hotmail.com>
2020-08-17 22:16:59 +0800

    added delete

--- src/main/java/checker.java
+++ src/main/java/checker.java
@@ -14,11 +14,26 @@ public class checker {
                     isDone = I;
                 }
                 catch(NumberFormatException e){ // next word is not an integer
-                    System.out.println("Error 2: Not a complete 'Done' command, added as a task!");
+//                    System.out.println("Error 2: Not a complete 'Done' command, added as a task!");
                 }
             }
             catch(NoSuchElementException e){
-                System.out.println("Error 1: Not a complete 'Done' command, added as a task!");
+//                System.out.println("Error 1: Not a complete 'Done' command, added as a task!");
+            }
+        }
+        else if(first.equals("delete")){
+            try{
+                String index = sc.next();
+                try{
+                    int I = Integer.parseInt(index);
+                    isDone = -1 * I;
+                }
+                catch(NumberFormatException e){ // next word is not an integer
+//                    System.out.println("Error 2: Not a complete 'Delete' command, added as a task!");
+                }
+            }
+            catch(NoSuchElementException e){
+//                System.out.println("Error 1: Not a complete 'Delete' command, added as a task!");
             }
         }
         return isDone;
