5d8e62a45efd3dd84bff81f604422fd5194bcebb
maguireong <maguire2622@yahoo.com.sg>
2020-08-17 10:54:27 +0800

    Add Done functionality and markings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,7 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> books = new ArrayList<>();
+        ArrayList<Task> books = new ArrayList<>();
         while (sc.hasNext()) {
 
             String echo = sc.nextLine();
@@ -15,17 +15,22 @@ public class Duke {
             String first = s[0];
 
             if (first.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < books.size(); i++) {
                     System.out.println(i+1 + ". " + books.get(i));
                 }
             } else if (first.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (first.equals("done")) {
+                int index = Integer.parseInt(s[1]);
+                books.get(index - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + books.get(index - 1).getStatusIcon() + " return book");
             } else {
-                books.add(echo);
+                Task t = new Task(echo);
+                books.add(t);
                 System.out.println("added: " + echo);
             }
-
         }
     }
 }
