e1343e313d6233c89f6242f80ac2eb89fb5d76bd
Clara Adora <claraadora@gmail.com>
2020-08-18 00:38:05 +0800

    feat: implement a skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Clara! :D How may I help you? :)");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String command = sc.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("Bye! Have a great day and hope to see you soon! :D");
+                break;
+            } else {
+               System.out.println(">>> " + command);
+            }
+        }
+
+
     }
 }
