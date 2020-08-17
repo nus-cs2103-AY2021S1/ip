65447f76ced51372798140d150990a3851fa9761
KOH-ZHE-HAO <lincolnkoh99@gmail.com>
2020-08-17 15:28:05 +0800

    Complete implementation of level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE_BREAK_BOT = "____________________________________________________________";
+    private static final String LINE_BREAK_USER = "************************************************************";
+    private static final String SPACE_BEFORE_LINE = "   ";
+    private static final String SPACE_BEFORE_TEXT = "    ";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+        System.out.println(SPACE_BEFORE_TEXT + "Hey, I'm Emilia!" + "\n" +
+                SPACE_BEFORE_TEXT + "What can I do for you ♥ ?");
+        System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if (!input.trim().toLowerCase().equals("bye")) {
+                System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_USER + "\n" + SPACE_BEFORE_TEXT +
+                        input + "\n" + SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+            } else {
+                System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT + "\n" + SPACE_BEFORE_TEXT +
+                        "Welcome back, master ♥ !" + "\n" + SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+                break;
+            }
+        }
     }
 }
