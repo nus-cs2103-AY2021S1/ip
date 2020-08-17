61c8b22cbf20b080c99a2944ce45595e76e516d1
Sean <e0406739@u.nus.edu>
2020-08-18 03:16:13 +0800

    Level-6: Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,13 +4,13 @@ import static java.lang.Integer.parseInt;
 
 public class Duke {
     private static final String LINE = "_______________________________________________";
-    private static final String ADDED = "added: "; // might remove (have to see later level)
     private static final String KEYWORD_BYE = "bye";
     private static final String KEYWORD_LIST = "list";
     private static final String KEYWORD_DONE = "done";
     private static final String KEYWORD_TODO = "todo";
     private static final String KEYWORD_EVENT = "event";
     private static final String KEYWORD_DEADLINE = "deadline";
+    private static final String KEYWORD_DELETE = "delete";
 
     private final Scanner sc;
     private ArrayList<Task> listTasks;
@@ -33,6 +33,10 @@ public class Duke {
                 } else if (isList(inputArr[0])) {
                     showListTasks(listTasks);
                 } else if (isValidDone(inputArr[0]) && isNumber(inputArr[1])) {
+                    // checking if the input is valid
+                    if (inputArr.length == 1) {
+                        throw new InvalidFormatDoneException();
+                    }
                     marking(parseInt(inputArr[1]), listTasks.size());
                 } else if (isTask(inputArr[0])) {
                     // checking if the input is valid
@@ -40,6 +44,12 @@ public class Duke {
                         throw new EmptyTextException(inputArr[0]);
                     }
                     addTask(inputArr[0], inputArr[1]);
+                } else if (isDelete(inputArr[0])) {
+                    // checking if the input is valid
+                    if (inputArr.length == 1) {
+                        throw new InvalidFormatDeleteException();
+                    }
+                    deleteTask(parseInt(inputArr[1]));
                 } else {
                     throw new DukeException(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
@@ -109,6 +119,10 @@ public class Duke {
         return isDeadline(type) || isTODO(type) || isEvent(type);
     }
 
+    public boolean isDelete(String type) {
+        return type.equals(KEYWORD_DELETE);
+    }
+
     // Printing out the items in the list
     private void showListTasks(ArrayList<Task> listTasks) {
         if (listTasks.size() == 0) {
@@ -126,7 +140,7 @@ public class Duke {
     // marking the task
     private void marking(int num, int size) throws InvalidFormatDoneException {
         if (num <= 0 || size < num) {
-            throw new InvalidFormatDoneException();
+            messageFormatter(() -> System.out.println("Invalid input for done"));
         } else {
             Task task = listTasks.get(num - 1);
             messageFormatter(() -> task.markAsDone());
@@ -163,6 +177,22 @@ public class Duke {
             System.out.println(task);
             System.out.printf("Now you have %d tasks in the list.\n", listTasks.size());
         });
+    }
+
+    private void deleteTask(int pos) {
+        if (listTasks.size() == 0) {
+            messageFormatter(() -> System.out.println("Your list is empty!!!"));
+        } else if (pos <= 0 || pos > listTasks.size()) {
+            messageFormatter(() -> System.out.println("Invalid input for delete"));
+        } else {
+            Task task = listTasks.get(pos - 1);
+            listTasks.remove(pos - 1);
+            messageFormatter(() -> {
+                System.out.println("Noted . I've removed this task:");
+                System.out.println(task);
+                System.out.printf("Now you have %d tasks in the list.\n", listTasks.size());
+            });
+        }
     }
 
 }
