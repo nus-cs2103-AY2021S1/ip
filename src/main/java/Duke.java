04795bdf077f9f1ca07d2e8d1c8036f5054c6329
blackonyyx <35429757+blackonyyx@users.noreply.github.com>
2020-08-17 10:19:35 +0800

    Level 1 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(lineWrapper("Hello! I'm Duke\n\tWhat can I do for you\n"));
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (true){
+            input = sc.next();
+            if (input.equals("bye")) break;
+            System.out.println(lineWrapper(input));
+        }
+        System.out.println(lineWrapper("Bye. Hope to see you again soon!"));
     }
+
+    /**
+     * Takes text input from Duke and Wraps it in a Line Seperator
+     * @param text
+     * @return
+     */
+    private static String lineWrapper(String text){
+        //placeholder until lines can be added
+        String newLine = "\t"+System.getProperty("line.separator")+"_________________________________________________";
+        return newLine+"\n\t"+text+newLine;
+    }
+
 }
