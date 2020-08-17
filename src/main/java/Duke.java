9550e36df8a7f172186da57b89707bf61b8d44b8
Persdre <persdre@gmail.com>
2020-08-17 17:09:18 +0800

    add level 2 implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,9 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         System.out.println(
                 "I'm DukeForQ, your chatbot! You can enter everything you want to enter. If you want to exit, enter 'bye'!");
+
+        String[] store = new String[100];
+        int counter = 0;
         String s;
         while (true) {
             s = sc.nextLine();
@@ -12,8 +15,22 @@ public class Duke {
                 System.out.println("Bye, hope to see you again!");
                 sc.close();
                 System.exit(0);
-            } else {
-                System.out.println(s);
+            } 
+            
+            if(s.equals("list")) {
+                if(counter == 0) {
+                    System.out.println("There is no text now.");
+                }
+
+                for(int i = 1; i <= counter; i++) {
+                    System.out.println(i + ". " + store[i-1]);
+                }
+            }
+
+            else {
+                store[counter] = s;
+                counter++;
+                System.out.println("added: " + s);
             }
         }
     }
