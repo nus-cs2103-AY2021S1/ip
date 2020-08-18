d4ef6195a343b83bed3b43ec615b9558b6ca3806
Ma-Yueran <mayuerantom@outlook.com>
2020-08-18 20:18:24 +0800

    Add icons for todo, deadline, event

--- src/main/java/UIPrint.java
+++ src/main/java/UIPrint.java
@@ -3,8 +3,11 @@ package main.java;
 public class UIPrint {
 
     public static char star = '*';
-    public static char tick = '\u2713';
-    public static char cross = '\u2718';
+    public static String tick = "[\u2713]";
+    public static String cross = "[\u2718]";
+    public static String todoIcon = "[T]";
+    public static String deadlineIcon = "[D]";
+    public static String eventIcon = "[E]";
 
     public static void drawLine(char ch, int length) {
         System.out.println();
