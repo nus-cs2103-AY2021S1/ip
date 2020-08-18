4f0f54a15d3cf1687c5e557fceba497bc23db730
underthehai <seamountain00@gmail.com>
2020-08-18 22:02:51 +0800

    level 2: Add, List items

--- src/main/java/Emily.java
+++ src/main/java/Emily.java
@@ -1,24 +1,49 @@
 package main.java;
 import java.util.Scanner;
-
 public class Emily {
-    //level 1: response and echo
+
+
+
     public static void main(String[] args) {
         String divider = "---------------";
-        System.out.println("Hello, I am Emily\nWhat can i do for you?\n"+ divider);
+        String[] store = new String[100];
+        int counter = 1;
+
+
+        System.out.println("Hello, I am Emily\n" +
+                "What can i do for you?\n"+
+                divider);
         Scanner sc = new Scanner(System.in);
 
         String input = sc.nextLine();
 
         while(!input.equals("bye")){
 
-            System.out.println("    " +  divider +
-                    "\n    " + input +
-                    "\n    " + divider);
+            if(input.equals("list")){
+                System.out.println("    " + divider);
 
-            input = sc.nextLine();
+                for(int i = 1; i<counter; i++){
+                    String description = store[i];
+                    String item = "    " + i + ". " + description;
+
+                    System.out.println(item);
+                }
+                System.out.println("    " + divider);
+                input = sc.nextLine();
+
+            }else {
+                store[counter] = input;
+                counter++;
+
+                System.out.println("    " + divider +
+                        "\n    added " + input +
+                        "\n    " + divider);
+
+                input = sc.nextLine();
+            }
         }
 
-        System.out.println(divider + "\nBye~, hope to see you again!");
+        System.out.println(divider + "\n" +
+                "Bye~, hope to see you again!");
     }
 }
