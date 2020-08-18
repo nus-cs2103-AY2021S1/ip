0e3a182bd9e5b149256ddd9e829d08c4a0d4eccd
royleochan <royleochan@gmail.com>
2020-08-18 13:03:10 +0800

    Migrate from using array to using ArrayList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
-import jdk.jfr.Event;
-
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -24,8 +24,7 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String welcomeMessage = "----- Serina here, what would you like to do?";
         System.out.println(welcomeMessage); // print welcome message
-        int index = 0;
-        Task[] tasks = new Task[100];
+        List<Task> tasks = new ArrayList<>(); // create list of tasks
 
         while (sc.hasNextLine()) {
             String userInput = sc.nextLine();
@@ -36,53 +35,57 @@ public class Duke {
                 String value;
                 value = splitInput.length > 1 ? splitInput[1] : "";
 
-                if (command == Command.BYE) { // exit the application
+                // exit the application
+                if (command == Command.BYE) {
                     System.out.print("----- Goodbye, call me when you need me.");
                     break;
                 }
 
                 switch (command) {
-                    case LIST:  // list task
+                    // list task
+                    case LIST:
                         System.out.println("----- Your Tasks: ");
-                        for (int i = 0; i < tasks.length; i++) {
-                            if (tasks[i] == null) {
-                                break;
-                            } else {
-                                int num = i + 1;
-                                System.out.println(num + ". " + tasks[i]);
-                            }
+                        for (int i = 0; i < tasks.size(); i++) {
+                            int num = i + 1;
+                            System.out.println(num + ". " + tasks.get(i));
                         }
                         break;
-                    case DONE:  // mark task as done and print it
+                    // mark task as done and print it
+                    case DONE:
                         int taskNum = Integer.parseInt(value) - 1;
-                        System.out.println(tasks[taskNum].markAsDone());
+                        System.out.println(tasks.get(taskNum).markAsDone());
                         break;
-                    case TODO: // add toDoTask
+                    // add toDoTask
+                    case TODO:
                         Duke.validateCommandDesc(value);
-                        tasks[index] = new ToDoTask(value);
-                        System.out.println("----- Received, added the following task:\n" + tasks[index].toString());
-                        index += 1;
-                        System.out.println("You now have " + index + " pending tasks.");
+                        tasks.add(new ToDoTask(value));
+                        System.out.println("----- Received, added the following task:\n" + tasks.get(tasks.size() - 1));
+                        System.out.println("You now have " + tasks.size() + " pending tasks.");
                         break;
-                    case DEADLINE: // add deadlineTask
-                    case EVENT: // add eventTask
+                    // add deadlineTask
+                    // add eventTask
+                    case DEADLINE:
+                    case EVENT:
                         String[] splitValue;
                         if (command == Command.DEADLINE) {
                             splitValue = value.split("/by ");
-                            tasks[index] = new DeadlineTask(splitValue[0], splitValue[1]);
+                            tasks.add(new DeadlineTask(splitValue[0], splitValue[1]));
                         } else {
                             splitValue = value.split("/at ");
-                            tasks[index] = new EventTask(splitValue[0], splitValue[1]);
+                            tasks.add(new EventTask(splitValue[0], splitValue[1]));
                         }
-                        System.out.println("----- Received, added the following task:\n" + tasks[index].toString());
-                        index += 1; // increment index
-                        System.out.println("You now have " + index + " pending tasks.");
+                        System.out.println("----- Received, added the following task:\n" + tasks.get(tasks.size() - 1));
+                        System.out.println("You now have " + tasks.size() + " pending tasks.");
                         break;
                 }
-            } catch (IllegalArgumentException ex) {
-                System.err.println("----- I can't help you with that request, try something else."); // catch error when command is invalid
-            } catch (DukeException ex) {
-                String result = "----- You entered a valid command but I can't carry it out. " + ex.getMessage(); // catch invalid command description errors
+            }
+            // catch error when command is invalid
+            catch (IllegalArgumentException ex) {
+                System.err.println("----- I can't help you with that request, try something else.");
+            }
+            // catch invalid command description errors
+            catch (DukeException ex) {
+                String result = "----- You entered a valid command but I can't carry it out. " + ex.getMessage();
                 System.err.println(result);
             }
         }
