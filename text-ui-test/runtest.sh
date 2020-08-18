43a72cf2369112524c1dc797342beaba30d18b49
chrystalquek <chrystalquek@yahoo.com.sg>
2020-08-18 22:43:44 +0800

    Add automated text UI testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,10 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Duke.java
+if ! (
+    find ../src/main/java -name "*.java" >sources.txt
+    javac -cp ../src -Xlint:none -d ../bin @sources.txt
+  )
 then
     echo "********** BUILD FAILURE **********"
     exit 1
