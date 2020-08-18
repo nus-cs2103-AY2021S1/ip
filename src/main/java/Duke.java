3ce4a35fc1458be4ad20afdc44ebba75e66523d9
davidsqf <e0376993@u.nus.edu>
2020-08-18 22:01:22 +0800

    add "add" "list" features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static String greeting = "Hello, I'm Duke, your personal assistant. \n What can I do for you?";
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -13,16 +16,32 @@ public class Duke {
         System.out.println(greeting);
         while(sc.hasNextLine()) {
             String order = sc.nextLine();
-            String reply = process(order);
+            String reply = process(order, list);
             System.out.println(reply);
         }
     }
 
-    private static String process(String order) {
+    public static String process(String order, List<String> list) {
         if (order.equals("bye")) {
             return "    Bye-bye, see you next time!";
+        } else if (order.equals("list")) {
+            if (list.size() == 0) {
+                return "    Well done! You've completed all your tasks.";
+            } else {
+                return printList(list);
+            }
         } else {
-            return "    "+order;
+            list.add(order);
+            return "    added:"+order;
         }
+    }
+
+    private static String printList(List<String> list) {
+        int size = list.size();
+        String str = "";
+        for (int i = 1; i <= size; i++) {
+            str += "\n"+i+". "+list.get(i-1);
+        }
+        return str;
     }
 }
