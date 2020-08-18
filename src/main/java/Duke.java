e54ff8038224dbca7bcc1bb8831ec8961fbc1d63
amylzting <amylimzting@gmail.com>
2020-08-18 19:03:11 +0800

    Add skeletal version of Dino
    Let's add a greeting that Dino says each time he starts,
    and configure Dino such that he echos the user command
    and outputs a horizontal line separating each output,
    and exits when the user types bye.
    
    Using a horizontal line to separate each output from Dino
    improves the visibility.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Rawr! I'm Dino ><\n" + "What can I do for you?"
+                + "\n____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        boolean isBye = false;
+
+        while (!isBye) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Rawr. Hope to see you again soon! ><"
+                        + "\n____________________________________________________________");
+                isBye = true;
+            } else {
+                System.out.println(input
+                        + "\n____________________________________________________________");
+            }
+        }
+        scanner.close();
     }
 }
