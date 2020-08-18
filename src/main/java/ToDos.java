761b9381f630aaed841b7df0f07bc9c756b618e8
limhaishan <haishanlim@gmail.com>
2020-08-18 23:30:31 +0800

    introduce different types of tasks

---
+++ src/main/java/ToDos.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class ToDos extends Task{
+
+    public ToDos(String description) {
+        super(description);
+    }
+    @Override
+    public String toString(){
+        return "[T]" + super.toString();
+    }
+}
