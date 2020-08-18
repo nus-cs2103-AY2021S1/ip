7b89319d7d48121109d04301bb97704ba3567fe5
wltan <53135010+wltan@users.noreply.github.com>
2020-08-18 23:16:05 +0800

    Add task completion status

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,17 +3,30 @@ public class Task {
     // task description
     private final String desc;
 
+    // indicates whether the task is done or not
+    private boolean done;
+
     public Task(String desc) {
         this.desc = desc;
+        this.done = false;
     }
 
     public String getDescription() {
         return desc;
     }
 
+    public void markAsDone() {
+        this.done = true;
+    }
+
+    private char getStatusIcon() {
+        // returns tick or X symbols
+        return done ? '\u2713' : '\u2718';
+    }
+
     @Override
     public String toString() {
-        return getDescription();
+        return String.format("[%c] %s", getStatusIcon(), getDescription());
     }
 
 }
