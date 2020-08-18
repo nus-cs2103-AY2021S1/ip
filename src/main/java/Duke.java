12b007bdde7e8c22b5cfa904a63b7643b312fcd9
JinYuze <1072217974@qq,com>
2020-08-18 20:36:31 +0800

    Level-2: Duke knows how to record users' words~

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,6 +9,7 @@ public class Duke {
 
         Scanner inputScanner = new Scanner(System.in);
         String userInput = "";
+        List<String> orderList = new ArrayList<>();
 
         System.out.println("//////////\n" + "->Hello! I'm Duke\n" +
                 "->What can I do for you?" + "\n\n");
@@ -15,8 +18,19 @@ public class Duke {
             userInput = inputScanner.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("\nBye. Hope to see you again soon!");
+            } else if (userInput.equals("list")) {
+                if (orderList.size() > 0) {
+                    System.out.println("\n-> Current List:\n");
+                    for (int i = 1; i <= orderList.size(); i++) {
+                        System.out.println(i + ". " + orderList.get(i - 1) + "  <-");
+                    }
+                    System.out.println("\n");
+                } else {
+                    System.out.println("\n//////There is NO order in my list now//////\n");
+                }
             } else {
-                System.out.println("\n->" + userInput + "\n");
+                orderList.add(userInput);
+                System.out.println("\n-> Added: " + userInput + "\n");
             }
         }
     }
