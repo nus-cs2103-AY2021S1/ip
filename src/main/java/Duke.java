e12e773a9a52e4cdeb67f219389f1e1160bc9e07
rgabelarde <rgabelarde@gmail.com>
2020-08-19 02:26:48 +0800

    Store text entered by user and display back when requested (using fixed array of size 100)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,12 @@
 package main.java;
 
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
-
+        String[] tasks = new String[100];
+        int index = 0;
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
@@ -22,14 +21,15 @@ public class Duke {
         while(!input.equals("bye")){
             if(input.equals("list")){
                 System.out.println("____________________________________________________________\n");
-                for(int i = 0; i < tasks.size(); i++){
-                    System.out.println((i+1) + ". " + tasks.get(i) + "\n");
+                for(int i = 0; i < index; i++){
+                    System.out.println((i+1) + ". " + tasks[i]);
                 }
                 System.out.println("____________________________________________________________\n");
             }
             else {
                 System.out.println("____________________________________________________________\n");
-                tasks.add(input);
+                tasks[index] = input;
+                index++;
                 System.out.println("added: " + input + "\n");
                 System.out.println("____________________________________________________________\n");
             }
