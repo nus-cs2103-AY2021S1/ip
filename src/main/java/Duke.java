98c926b822380f5e4372affc06eb55de5968fa8b
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-08-18 12:40:01 +0800

    Update chatbot to greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        // Initialise strings to separate messages from Duke
+        // and commands from CLI
+        String servantSpeak = "Duke:\n";
+        String masterSpeak = "Your Command Sire:";
+
+        // Introduction at the beginning of the chat
+        System.out.println(servantSpeak
+                + "    Greetings my Liege.\n"
+                + "    Why have you summoned me?\n");
+
+        // Initialise the Scanner object to get input from user
+        Scanner myObj = new Scanner(System.in);
+        String input;
+
+        // Start chat
+        do {
+            System.out.println(masterSpeak);
+            input = myObj.nextLine();
+            System.out.println();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(servantSpeak
+                        + "    It was a pleasure to serve you my Liege.\n"
+                        + "    Till next time.");
+                break;
+            }
+            System.out.println(servantSpeak
+                    + "    " + input + "\n");
+        } while (!input.equalsIgnoreCase("bye"));
+
     }
 }
