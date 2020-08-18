e30b73ab3019cfbb1906716562fe2382336a4f2c
Uriel Tan <urieltan3@gmail.com>
2020-08-18 15:54:16 +0800

    Added greet, echo and exit functionality to duke.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you today?");
+
+        //Level 1, echo bot that echos everything except bye
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+
+        while(true){
+            String input = scanner.nextLine();
+            if(input.contentEquals("bye")){
+                System.out.println("\t" + "Bye. Hope to see you again soon!");
+                break;
+            }
+            else{
+                System.out.println("\t" + input);
+            }
+        }
     }
 }
