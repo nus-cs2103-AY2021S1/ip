21bb123b60f7b57c777b5da3df5c3beea6dbadc0
augustinekau <augustinekau@gmail.com>
2020-08-18 19:32:20 +0800

    Level 1: Implement a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalDiv = "____________________________________________________________";
+
+        // Using Scanner for Getting Input from User
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(horizontalDiv);
+        System.out.println("Hello! I'm Dude\n" + "What can I do for you today?");
+        System.out.println(horizontalDiv);
+        while(in.hasNext()) {
+            String str = in.next();
+
+            if(str.equals("bye")) {
+                System.out.println(horizontalDiv);
+                System.out.println("Bye! Hope to see you again soon!");
+                System.out.println(horizontalDiv);
+                break;
+            } else {
+                System.out.println(horizontalDiv);
+                System.out.println(str);
+                System.out.println(horizontalDiv);
+            }
+        }
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
     }
 }
