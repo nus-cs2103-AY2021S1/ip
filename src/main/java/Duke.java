9ab12cb5bd046a93fd9e2e9298c9a94b2f0a527d
solkim-83 <65942359+solkim-83@users.noreply.github.com>
2020-08-18 20:55:17 +0800

    Level  - 3. Mark as Done
    Created a new Task Class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("-------------------------------------------\n" +
@@ -9,27 +10,44 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String input = "";
+        ArrayList<Task> taskList = new ArrayList<>();
         int i = 1;
-        String[] list = new String[100];
+        String done = "done x";
+        char[] doneArray = done.toCharArray();
+        int number = 0;
 
         while (input != null) {
             input = sc.nextLine();
-            if (input.equals("bye")) {
+            Task t = new Task(input);
+            char[] chars = t.getDescription().toCharArray();
+            if (t.getDescription().equals("bye")) {
                 System.out.println("-------------------------------------------\n" +
                                         "Bye. Hope to see you again soon!" +
                                                 "\n-------------------------------------------\n");
                 System.exit(0);
 
-            } else if (input.equals("list")) {
-                System.out.println("-------------------------------------------\n");
+            } else if (t.getDescription().equals("list")) {
+                System.out.println("-------------------------------------------\n" +
+                                        "Here are the tasks in your list:\n");
+
                 for(int j = 1; j < i; j++) {
-                    System.out.println(j + ". " + list[j-1]);
+                    System.out.println(j + ". " + "[" + t.getStatusIcon() + "] " + taskList.get(j-1).getDescription());
                 }
                 System.out.println("-------------------------------------------\n");
             }
+            else if (chars.length == doneArray.length) {
+                if (chars[0] == doneArray[0] && chars[1] == doneArray[1] && chars[2] == doneArray[2] &&
+                        chars[3] == doneArray[3] && chars[4] == doneArray[4] &&
+                        Character.isDigit(chars[5])) {
 
-            else {
-                list[i-1] = input;
+                    number = Character.getNumericValue(chars[chars.length - 1]);
+                    taskList.get(number -1).markAsDone();
+                    System.out.println("-------------------------------------------\n" +
+                            " Nice! I've marked this task as done:\n"
+                            + "[" + taskList.get(number -1).getStatusIcon() + "] " + taskList.get(number -1).getDescription());
+                }
+            } else {
+                taskList.add(i-1, t);
                 System.out.println("-------------------------------------------\n" +
                                         "added:" + input +
                                                 "\n-------------------------------------------\n");
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    protected int i = 1;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getDescription() {
+        return this.description.toString();
+    }
+
+    public int getNumber() {
+        return i;
+    }
+}
