da2bd83a626a9596d0a21eace58ae6b03bf8a260
ureshiiYing <tan.ying.hui@u.nus.edu>
2020-08-19 01:07:20 +0800

    Implemented Level 2 and did automated testing

--- src/main/java/PandaBot.java
+++ src/main/java/PandaBot.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
 public class PandaBot {
-    
+    private static String[] tasks = new String[100];
+    private static int counter = 0;
+
+
     public static void main(String[] args) {
         String logo =
                  " ____                    _   \n"
@@ -9,9 +12,9 @@ public class PandaBot {
                + "| |_| |___  _ _  __  ___| | ___  _\n"
                + "| ___/  _ \\| | |/  |/ _   |/ _ \\| | \n"
                + "| |  | |_|   |  _  | |_|  | |_|   | \n"
-               + "|_|  \\____,__|_| |_|\\___,_|\\___,__|" + " bot \n\n";
+               + "|_|  \\____,__|_| |_|\\___,_|\\___,__|" + " bot\n\n";
 
-        System.out.println(logo + "Hello! I'm PandaBot.\n" + "What can I do for you? \n");
+        System.out.println(logo + "Hello! I'm PandaBot.\n" + "What can I do for you?\n");
 
         // echoes command by user
         Scanner sc = new Scanner(System.in);
@@ -19,11 +22,20 @@ public class PandaBot {
         while (sc.hasNextLine()) {
             String command = sc.nextLine();
             if (command.equals("bye")) {
-                // exits when user types bye
                 System.out.println("Bye! Remember to do your work! See you soon~");
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < tasks.length; i++) {
+                    if (tasks[i] != null) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    } else {
+                        break;
+                    }
+                }
             } else {
-                System.out.println(command + "\n");
+                System.out.println("Added: " + command);
+                tasks[counter] = command;
+                counter++;
             }
         }
         sc.close();
