b5bf6de12b8f2f8f8fcf6e780ea8df06ecf4230c
onglijin <onglijinlj@gmail.com>
2020-08-18 21:05:37 +0800

    Add the ability to mark tasks as done

--- src/main/java/Mocha.java
+++ src/main/java/Mocha.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Mocha {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MissingTaskNumberException {
         // Contains all tasks
-        ArrayList<String> listOfTasks = new ArrayList<>();
+//        ArrayList<String> listOfTasks = new ArrayList<>();
+        ArrayList<Task> listOfTasks = new ArrayList<>();
 
         // Introduction of Mocha
         String horizontalLine = "_______________________________________________________";
@@ -16,19 +17,48 @@ public class Mocha {
         Scanner userCommand = new Scanner(System.in);
 
         while (userCommand.hasNext()) {
-            // Any command other than "list" and "bye" - add into listOfTasks
-            if (!userCommand.hasNext("list") && !userCommand.hasNext("bye")) {
+            // Any command other than "list", "bye" and "done" - add into listOfTasks
+            if (!userCommand.hasNext("list")
+                    && !userCommand.hasNext("bye")
+                    && !userCommand.hasNext("done")) {
                 String itemToBeAdded = userCommand.nextLine();
-                System.out.println(horizontalLine + "\r\n" + "added: "
-                        + itemToBeAdded + "\r\n" + horizontalLine + "\r\n");
-                listOfTasks.add(itemToBeAdded);
+                Task newTask = new Task(itemToBeAdded);
+                System.out.println(horizontalLine
+                        + "\r\n"
+                        + "added: "
+                        + itemToBeAdded
+                        + "\r\n"
+                        + horizontalLine
+                        + "\r\n");
+                listOfTasks.add(newTask);
+            }
+
+            // "done" command - mark task as done
+            if (userCommand.hasNext("done")) {
+                String doneCommandString = userCommand.nextLine();
+                String[] split = doneCommandString.split("\\s+");
+                if (split.length < 2) {
+                    throw new MissingTaskNumberException("Task Number needs to be stated");
+                }
+                int taskNumber = Integer.parseInt(split[1]) - 1;
+                Task task = listOfTasks.get(taskNumber);
+                task.markAsDone();
+                System.out.println(horizontalLine
+                        + "\r\n Nice! One task done: \r\n ["
+                        + task.getStatusIcon()
+                        + "] " + listOfTasks.get(taskNumber).getDescription()
+                        + "\r\n" + horizontalLine);
             }
 
             // "list" command - displays all tasks
             if (userCommand.hasNext("list")) { // Process closing here
-                System.out.println(horizontalLine + " \r\n");
+                System.out.println(horizontalLine + " \r\n" + "Here's all the tasks in your list: ");
                 for (int i = 0; i < listOfTasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + listOfTasks.get(i));
+                    System.out.println((i + 1)
+                            + ".["
+                            + listOfTasks.get(i).getStatusIcon()
+                            + "] "
+                            + listOfTasks.get(i).getDescription());
                 }
                 System.out.println(horizontalLine + " \r\n");
                 userCommand.nextLine();
@@ -36,7 +66,11 @@ public class Mocha {
 
             // Mocha's farewell
             if (userCommand.hasNext("bye")) {
-                System.out.println(horizontalLine + "\r\n" + "Bye! See ya soon!" + "\r\n" + horizontalLine);
+                System.out.println(horizontalLine
+                        + "\r\n"
+                        + "Bye! See ya soon!"
+                        + "\r\n"
+                        + horizontalLine);
                 userCommand.nextLine();
             }
         }
