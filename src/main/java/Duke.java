821b83521b5dc54112619a18fcda9f02758e80c6
tengjianling <tengjianling98@gmail.com>
2020-08-18 21:01:03 +0800

    Complete Level 1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        System.out.println("    _________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    _________________________________");
+
+        while (!str.equals("bye")) {
+            str = sc.next();
+            if (!str.equals("bye")) {
+                System.out.println("    _________________________________");
+                System.out.println("    " + str);
+                System.out.println("    _________________________________");
+            }
+        }
+
+        System.out.println("    _________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    _________________________________");
+
     }
 }
