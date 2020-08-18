e479526cc6e60c61f714ad34320aa10cabb81514
Lye Yi Xian <lyeyx999@hotmail.com>
2020-08-18 23:35:41 +0800

    Abstract out Duke's logic

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,63 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+  public static String process(String input, List<Task> list) {
+    String output;
+    String[] command = input.split(" ", 2);
+
+    if (input.equals("bye")) {
+      output = "\tBye. Hope to see you again soon!";
+    } else if (input.equals("list")) {
+      StringBuilder concat = new StringBuilder();
+
+      for (int i = 0; i < list.size(); i++) {
+        concat.append(String.format("\n\t%d. %s", i + 1, list.get(i)));
+      }
+
+      output = "\tHere are the tasks in your list: " + concat;
+    } else if (command[0].equals("done")){
+      int index = Integer.parseInt(command[1]) - 1;
+      Task targetTask = list.get(index);
+
+      targetTask.setDone();
+      output = "\tNice! I've marked this task as done: \n\t\t" + targetTask;
+    } else if (command[0].equals("todo")) {
+      if (command.length < 2) {
+        output = "\t☹ OOPS!!! The description of a todo cannot be empty.";
+      } else {
+        ToDo newToDo = new ToDo(command[1]);
+
+        list.add(newToDo);
+        output = "\tGot it. I've added this task: \n\t\t" + newToDo + "\n\tNow you have " + list.size() + " tasks in the list.";
+      }
+    } else if (command[0].equals("deadline")) {
+      String[] commandParam = command[1].split("/by");
+      String description = commandParam[0].trim();
+      String by = commandParam[1].trim();
+      Deadline newDeadline = new Deadline(description, by);
+
+      list.add(newDeadline);
+      output = "\tGot it. I've added this task: \n\t\t" + newDeadline + "\n\tNow you have " + list.size() + " tasks in the list.";
+    } else if (command[0].equals("event")) {
+      String[] commandParam = command[1].split("/at");
+      String description = commandParam[0].trim();
+      String at = commandParam[1].trim();
+      Event newEvent = new Event(description, at);
+
+      list.add(newEvent);
+      output = "\tGot it. I've added this task: \n\t\t" + newEvent + "\n\tNow you have " + list.size() + " tasks in the list.";
+    } else if (command[0].equals("delete")) {
+      int index = Integer.parseInt(command[1]) - 1;
+      Task targetTask = list.remove(index);
+
+      output = "\tNoted. I've removed this task: \n\t\t" + targetTask + "\n\tNow you have " + list.size() + " tasks in the list.";
+    } else {
+      output = "\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+    }
+
+    return output;
+  }
+
   public static void main(String[] args) {
     String divider = "---------------------------------------------------------------------------------------------";
     String logo = " ____        _        \n"
@@ -21,64 +78,12 @@ public class Duke {
     System.out.println("\t" + divider);
 
     while (!input.equals("bye")) {
-      output = "";
       input = sc.nextLine();
-      String[] command = input.split(" ", 2);
-
-      if (input.equals("bye")) {
-        output = "\tBye. Hope to see you again soon!";
-      } else if (input.equals("list")) {
-        StringBuilder concat = new StringBuilder();
-
-        for (int i = 0; i < list.size(); i++) {
-          concat.append(String.format("\n\t%d. %s", i + 1, list.get(i)));
-        }
-
-        output = "\tHere are the tasks in your list: " + concat;
-      } else if (command[0].equals("done")){
-        int index = Integer.parseInt(command[1]) - 1;
-        Task targetTask = list.get(index);
-
-        targetTask.setDone();
-        output = "\tNice! I've marked this task as done: \n\t\t" + targetTask;
-      } else if (command[0].equals("todo")) {
-        if (command.length < 2) {
-          output = "\t☹ OOPS!!! The description of a todo cannot be empty.";
-        } else {
-          ToDo newToDo = new ToDo(command[1]);
-
-          list.add(newToDo);
-          output = "\tGot it. I've added this task: \n\t\t" + newToDo + "\n\tNow you have " + list.size() + " tasks in the list.";
-        }
-      } else if (command[0].equals("deadline")) {
-        String[] commandParam = command[1].split("/by");
-        String description = commandParam[0].trim();
-        String by = commandParam[1].trim();
-        Deadline newDeadline = new Deadline(description, by);
-
-        list.add(newDeadline);
-        output = "\tGot it. I've added this task: \n\t\t" + newDeadline + "\n\tNow you have " + list.size() + " tasks in the list.";
-      } else if (command[0].equals("event")) {
-        String[] commandParam = command[1].split("/at");
-        String description = commandParam[0].trim();
-        String at = commandParam[1].trim();
-        Event newEvent = new Event(description, at);
-
-        list.add(newEvent);
-        output = "\tGot it. I've added this task: \n\t\t" + newEvent + "\n\tNow you have " + list.size() + " tasks in the list.";
-      } else if (command[0].equals("delete")) {
-        int index = Integer.parseInt(command[1]) - 1;
-        Task targetTask = list.remove(index);
-
-        output = "\tNoted. I've removed this task: \n\t\t" + targetTask + "\n\tNow you have " + list.size() + " tasks in the list.";
-      } else {
-        output = "\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
-      }
+      output = process(input, list);
+      
       System.out.println("\t" + divider);
       System.out.println(output);
       System.out.println("\t" + divider + "\n");
     }
-
-
   }
 }
