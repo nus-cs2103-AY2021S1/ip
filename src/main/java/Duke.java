87af9730f538df643a5b4f6760f1a16f69a60c3a
Xingjian <xingjian.chen@yahoo.com.sg>
2020-08-18 15:06:20 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String lineDivider = "------------------------------------------";
+
+    public static void echo(String message) {
+        System.out.println(lineDivider);
+        System.out.println(message);
+        System.out.println(lineDivider + "\n");
+    }
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        echo("Hello! I'm Duke\nWhat can I do for you?");
+        while (sc.hasNext()) {
+            String msg = sc.nextLine();
+            if (msg.equals("bye")) {
+                echo("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                echo(msg);
+            }
+        }
     }
 }
