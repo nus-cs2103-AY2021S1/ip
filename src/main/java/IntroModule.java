f83430ac6987d0b2ff1befddad879abe32a56d6f
Lim Jun Jie <ljj98855221@hotmail.com>
2020-08-18 10:19:59 +0800

    IntroModule class abstraction added

---
+++ src/main/java/IntroModule.java
@@ -0,0 +1,15 @@
+public class IntroModule {
+    private static String logo = " ____        _        \n"
+                                + "|  _ \\ _   _| | _____ \n"
+                                + "| | | | | | | |/ / _ \\\n"
+                                + "| |_| | |_| |   <  __/\n"
+                                + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public static void display() {
+        System.out.println("____________________________________________________________\nWelcome to\n"
+                            + logo
+                            + "\nHello! I'm Duke"
+                            + "\nWhat can I do for you?"
+                            + "\n____________________________________________________________");
+    }
+}
