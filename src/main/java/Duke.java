21b339b92b6bbb5e01db0c6fccec93ce0591acd8
Yuxi <liuyuxi.yx@gmail.com>
2020-08-18 12:35:07 +0800

    edit level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String linePrinter() {
+        return "\n----------------------------\n";
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String endCommand = "bye";
+        System.out.println((linePrinter() +
+                "Hello! I'm Duke\n" +
+                "What can I do for you?" +
+                linePrinter()).replaceAll("(?m)^", "\t"));
+
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        while(!s.equals(endCommand)) {
+            System.out.println((linePrinter() +
+                    s +
+                    linePrinter()).replaceAll("(?m)^", "\t"));
+            s = in.nextLine();
+        }
+        if (s.equals(endCommand)) {
+            System.out.println((linePrinter() +
+                    "Bye. Hope to see you again soon!" +
+                    linePrinter()).replaceAll("(?m)^", "\t"));
+        }
     }
 }
