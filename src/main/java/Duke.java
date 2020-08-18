019ac0a818de0fe5954c3f13a941d352dbd6631c
augustinekau <augustinekau@gmail.com>
2020-08-18 19:52:24 +0800

    Level 2: Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+
+        ArrayList<String> ls = new ArrayList<>();
+        int counter = 1;
         String horizontalDiv = "____________________________________________________________";
 
         // Using Scanner for Getting Input from User
@@ -10,25 +14,26 @@ public class Duke {
         System.out.println(horizontalDiv);
         System.out.println("Hello! I'm Dude\n" + "What can I do for you today?");
         System.out.println(horizontalDiv);
-        while(in.hasNext()) {
-            String str = in.next();
+        while(in.hasNextLine()) {
+            String str = in.nextLine();
 
             if(str.equals("bye")) {
                 System.out.println(horizontalDiv);
                 System.out.println("Bye! Hope to see you again soon!");
                 System.out.println(horizontalDiv);
                 break;
+            } else if (str.equals("list")) {
+                for (String l : ls) {
+                    System.out.println(l);
+                }
             } else {
+                ls.add(counter + ". " + str);
                 System.out.println(horizontalDiv);
-                System.out.println(str);
+                System.out.println("added: " + str);
                 System.out.println(horizontalDiv);
+
+                counter++;
             }
         }
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
     }
 }
