05bb657951f6ca430f6ac5900d81a25e2ed3befe
kaichaoang <kaichaoa@gmail.com>
2020-08-18 23:03:50 +0800

    Add support for tracking 3 types of tasks
    Let's add 3 new conditions to handle 3 special input types of tasks.
    This will allow the user to see which types of specific tasks there is
    remaining in the list and eases task handling for them.
    
    We should also let user know the number of pending tasks there is
    remaining.
    
    Let's,
    - add a line of output that allow users to know the number of tasks
      they have remaining after they added a new task.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -29,6 +29,10 @@ public class Duke {
                 scanner.close();
             } else if (splitInput[0].equals("help")) {
                 System.out.println("list: displays a sequential view of past inputs\n" +
+                        "done <task number>: denotes a task as done by checking it\n" +
+                        "deadline <description> /by <date/time>: adds a deadline with desired date/time\n" +
+                        "event <description> /at <date/time>: adds an event with desired date/time\n" +
+                        "todo <description>: adds a todo task\n" +
                         "bye: terminates program");
             } else if (splitInput[0].equals("list")){
                 if (pastInputs.size() == 0) {
@@ -58,11 +62,68 @@ public class Duke {
                     }
                 } else {
                     pastInputs.add(new Task(userInput));
-                    System.out.println("Duke added into your task list: " + userInput);
+                    System.out.println("Duke added into your task list: \n" + userInput);
+                    System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
                 }
+            } else if (splitInput[0].equals("todo")) {
+                String des = "";
+                for (int i = 1; i < splitInput.length; i++) {
+                    des += splitInput[i];
+                    if (i != splitInput.length - 1) {
+                        des += " ";
+                    }
+                }
+                ToDo toDo = new ToDo(des);
+                pastInputs.add(toDo);
+                System.out.println("Duke added into your task list: \n" + toDo);
+                System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
+
+            } else if (splitInput[0].equals("deadline")) {
+                String des = "";
+                String dateAndOrTime = "";
+                boolean toBreak = false;
+                for (int i = 1; i < splitInput.length; i++) {
+                    if (splitInput[i].equals("/by")) {
+                        //sets the breaking point of input
+                        toBreak = true;
+                    } else if (!toBreak) {
+                        des += splitInput[i] + " ";
+                    } else {
+                        dateAndOrTime += splitInput[i];
+                        if (i != splitInput.length - 1) {
+                            dateAndOrTime += " ";
+                        }
+                    }
+                }
+                Deadline deadline = new Deadline(des, dateAndOrTime);
+                pastInputs.add(deadline);
+                System.out.println("Duke added into your task list: \n" + deadline);
+                System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
+            } else if (splitInput[0].equals("event")) {
+                String des = "";
+                String dateAndOrTime = "";
+                boolean toBreak = false;
+                for (int i = 1; i < splitInput.length; i++) {
+                    if (splitInput[i].equals("/at")) {
+                        //sets the breaking point of input
+                        toBreak = true;
+                    } else if (!toBreak) {
+                        des += splitInput[i] + " ";
+                    } else {
+                        dateAndOrTime += splitInput[i];
+                        if (i != splitInput.length - 1) {
+                            dateAndOrTime += " ";
+                        }
+                    }
+                }
+                Event event = new Event(des, dateAndOrTime);
+                pastInputs.add(event);
+                System.out.println("Duke added into your task list: \n" + event);
+                System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
             } else {
                 pastInputs.add(new Task(userInput));
-                System.out.println("Duke added into your task list: " + userInput);
+                System.out.println("Duke added into your task list: \n" + userInput);
+                System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
             }
             System.out.println("=========================================================================");
         }
