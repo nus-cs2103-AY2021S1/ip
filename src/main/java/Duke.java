69537592cde3f8bb03fc1661c4c4eedbc8403c2e
JoeyToh <joeytoh29@gmail.com>
2020-08-18 11:39:56 +0800

    Implemented Level 0.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
+    public static String line = "____________________________________________________________";
+    public static String start = "Hello! I'm Duke, your personal assistant. \nWhat can I do for you?";
+    public static String end = "Goodbye! Hope to see you again soon. :)";
+    public static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println(logo + start);
+        System.out.println(line);
+        System.out.println(end);
+        System.out.println(line);
     }
 }
