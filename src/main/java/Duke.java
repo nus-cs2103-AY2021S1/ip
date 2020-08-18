2c44b776083d7538017301a203824d4bb69ee57d
duckmoon99 <e0407816@u.nus.edu>
2020-08-18 23:15:04 +0800

    Add "mark done" functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,32 +19,52 @@ public class Duke {
         printNice(exit);
     }
 
-    private static void listOut(ArrayList<String> tasks){
+    private static void listOut(ArrayList<Task> tasks){
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < tasks.size(); i++) {
-            s.append((i > 0 ? "\n    " : "") + String.format("%d. %s", i+1, tasks.get(i)));
+            s.append((i > 0 ? "\n    " : "") + String.format("%d.%s", i+1, tasks.get(i)));
         }
         printNice(s.toString());
+    }
+
+    private static boolean isBye(String s){
+        return s.toLowerCase().equals("bye");
+    }
+
+    private static boolean isList(String s){
+        return s.toLowerCase().equals("list");
+    }
+
+    private static boolean isDone(String s){
+        return s.length() >= 4 && s.substring(0, 4).toLowerCase().equals("done");
+    }
+
+    private static void done(ArrayList<Task> tasks, int i){
+        tasks.get(i).done();
+        printNice("Nice! I've marked this task as done:\n      " + tasks.get(i).toString());
+    }
+
+    private static void add(ArrayList<Task> tasks, String s){
+        tasks.add(new Task(s));
+        printNice("added: " + s);
     }
 
     public static void main(String[] args) {
         greet();
         Scanner scanner = new Scanner(System.in);
         String input;
-        ArrayList<String> tasks = new ArrayList<>();
-        while(true){
+        ArrayList<Task> tasks = new ArrayList<>();
+        while(true) {
             input = scanner.nextLine();
-            switch (input) {
-                case "bye":
-                    bye();
-                    return;
-                case "list":
-                    listOut(tasks);
-                    break;
-                default:
-                    printNice("added: " + input);
-                    tasks.add(input);
-                    break;
+            if (isBye(input)) {
+                bye();
+                return;
+            } else if (isList(input)) {
+                listOut(tasks);
+            } else if (isDone(input)) {
+                done(tasks, Integer.parseInt(input.split(" ")[1]) - 1);
+            } else {
+                add(tasks, input);
             }
         }
     }
