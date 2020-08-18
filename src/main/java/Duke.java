bfa716e996ad7532e9545ec6fc222bd9381bdc99
Ma-Yueran <mayuerantom@outlook.com>
2020-08-18 15:58:12 +0800

    Add greet() & drawLine() functions into Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,25 @@
+package main.java;
+
 public class Duke {
+
+    private static void drawLine(char ch, int length) {
+        System.out.println();
+        for (int i = 0; i < length; i++) {
+            System.out.print(ch);
+        }
+        System.out.println();
+    }
+
+    private static void greet() {
+        char star = '*';
+        int length = 8;
+
+        drawLine(star, length);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        drawLine(star, length);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +27,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greet();
     }
 }
