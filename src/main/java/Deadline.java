caf233d0fd74f38eef1c953a8bce8ddc48d544e9
Low Ming Lim <48002577+Avalionnet@users.noreply.github.com>
2020-08-18 23:18:15 +0800

    Completed Level 4 of Duke Project.
    Implemented todo, event and deadline functionalities for users.

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task {
+
+    protected String by;
+
+    public Deadline(String name, String by) {
+        super(name);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,7 +27,7 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
             String userCommand = sc.nextLine();
-            String[] userWord = userCommand.split(" ");
+            String[] userWord = userCommand.split(" ", 2);
 
             if (userCommand.equals("bye")) {
                 System.out.println(breakline);
@@ -39,6 +39,35 @@ public class Duke {
                 case "list":
                     System.out.println(breakline);
                     listItems(listOfTask);
+                    System.out.println(breakline);
+                    break;
+
+                case "todo":
+                    ToDo newT = new ToDo(userWord[1]);
+                    listOfTask.add(newT);
+                    System.out.println(breakline);
+                    System.out.println("    Got it. I've added this task: \n     " + newT.toString());
+                    System.out.println("    Now you have " + listOfTask.size() + " tasks in the list.");
+                    System.out.println(breakline);
+                    break;
+
+                case "deadline":
+                    String[] content = userWord[1].split("/by", 2);
+                    Deadline newD = new Deadline(content[0], content[1]);
+                    listOfTask.add(newD);
+                    System.out.println(breakline);
+                    System.out.println("    Got it. I've added this task: \n     " + newD.toString());
+                    System.out.println("    Now you have " + listOfTask.size() + " tasks in the list.");
+                    System.out.println(breakline);
+                    break;
+
+                case "event":
+                    String[] content2 = userWord[1].split("/at", 2);
+                    Event newE = new Event(content2[0], content2[1]);
+                    listOfTask.add(newE);
+                    System.out.println(breakline);
+                    System.out.println("    Got it. I've added this task: \n     " + newE.toString());
+                    System.out.println("    Now you have " + listOfTask.size() + " tasks in the list.");
                     System.out.println(breakline);
                     break;
 
@@ -59,10 +88,8 @@ public class Duke {
                     break;
 
                 default:
-                    Task newT = new Task(userCommand);
-                    listOfTask.add(newT);
                     System.out.println(breakline);
-                    System.out.println("    added: " + userCommand);
+                    System.out.println("    Sorry! I'm not really sure what to do with this command yet ☹");
                     System.out.println(breakline);
                     break;
             }
