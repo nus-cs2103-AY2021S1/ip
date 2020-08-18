50fa8e2dd8349c30e3133d403be7abffa1c6a1bc
unknown <schoolex@hotmail.com>
2020-08-18 12:58:28 +0800

    Added lvl 1 functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("______________________");
+
+        String input;
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextLine();
+
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
     }
 }
