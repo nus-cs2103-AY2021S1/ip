7ca92c4270055d91de060d710efead51298d1133
xnmng <puaxuanmingpxm18@gmail.com>
2020-08-19 01:01:02 +0800

    added the Greet feature in Level 1 of iP

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,16 @@
 public class Duke {
+
+    // this field is used when Duke requires a horizontal line
+    public static final String LINE = "    ____________________________________________________________";
+
+    // this function greets the user when Duke is started
+    public static void greeting(){
+        System.out.println(LINE);
+        System.out.println("     Hello! I'm Duke and I was designed by Xuan Ming!\n");
+        System.out.println("     What can I do for you?");
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +18,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greeting();
     }
 }
