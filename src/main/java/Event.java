1a5c195df0ac4500f751801699077dd0224f59fa
Nahoyhp <phyohan@hotmail.com>
2020-08-18 14:36:16 +0800

    create Event.java

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class Event extends Task {
+    private String time;
+
+    public Event(String name, String time) {
+        super(name);
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[E]%s (at: %s)", super.toString(), this.time);
+    }
+}
