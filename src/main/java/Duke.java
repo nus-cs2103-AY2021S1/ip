5cea4312dde152bb02b0ab1a1057c3b09b02585c
lowjiefeng <lowjiefeng1998@gmail.com>
2020-08-18 21:51:46 +0800

    Refactor commands into own method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,45 +3,57 @@ import java.util.Scanner;
 
 public class Duke {
     private static final String DIVIDER = "-------------------------------------";
+
+    private static final String HELLO_MESSAGE = "Greetings! I am Duke.\nWhat can I do for you?";
+    private static final String EXIT_MESSAGE = "Bye! See you around!";
+
+    private static void printWithDivider(String text) {
+        System.out.println(DIVIDER + "\n" + text + "\n" + DIVIDER);
+    }
+
     private static final String EXIT_COMMAND = "bye";
     private static final String LIST_COMMAND = "list";
 
-    private static ArrayList<Task> taskList = new ArrayList<>();
-
-    private static String sandwichWithDivider(String text) {
-        return DIVIDER + "\n" + text + "\n" + DIVIDER;
+    private static void processCommand(String input) {
+        String[] inputList = input.split(" ");
+        if (inputList.length > 0) {
+            switch (inputList[0]) {
+                case LIST_COMMAND:
+                    listTasks();
+                    break;
+                default:
+                    addTask(input);
+                    break;
+            }
+        }
     }
 
-    private static String prettyPrintTaskList() {
+    private static void listTasks() {
         String rv = "You have " + taskList.size() + " items.\n";
         for (int i = 0; i < taskList.size(); i++) {
             rv += "\n" + (i + 1) + ": " + taskList.get(i).toString();
         }
-        return sandwichWithDivider(rv);
+        printWithDivider(rv);
     }
 
-    private static String processTask(String task) {
-        taskList.add(new Task(task));
-        return sandwichWithDivider("Successfully added: " + task);
+    private static void addTask(String input) {
+        taskList.add(new Task(input));
+        printWithDivider("Successfully added: " + input);
     }
 
-    private static final String HELLO_MESSAGE = sandwichWithDivider("Greetings! I am Duke."
-            + "\nWhat can I do for you?");
-    private static final String EXIT_MESSAGE = sandwichWithDivider("Bye! See you around!");
+    private static ArrayList<Task> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(HELLO_MESSAGE);
+        printWithDivider(HELLO_MESSAGE);
         while (true) {
             String input = sc.nextLine();
             if (input.equals(EXIT_COMMAND)) {
-                System.out.println(EXIT_MESSAGE);
+                printWithDivider(EXIT_MESSAGE);
                 break;
-            } else if (input.equals(LIST_COMMAND)) {
-                System.out.println(prettyPrintTaskList());
             } else {
-                System.out.println(processTask(input));
+                processCommand(input);
             }
         }
 
