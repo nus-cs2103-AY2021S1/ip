1c19ecd6cd26b2c8f68726f25bb515f049a813b2
solkim-83 <65942359+solkim-83@users.noreply.github.com>
2020-08-18 14:23:24 +0800

    Duke Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("-------------------------------------------\n" +
+                                "Hello! I'm Duke\n" + "What can I do for you?" +
+                                        "\n-------------------------------------------\n");
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        do {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("-------------------------------------------\n" +
+                                        "Bye. Hope to see you again soon!" +
+                                                "\n-------------------------------------------\n");
+                System.exit(0);
+
+            } else {
+                System.out.println("-------------------------------------------\n" +
+                                        input +
+                                                "\n-------------------------------------------\n");
+            }
+
+        } while (input != null);
+        System.out.println("-------------------------------------------\n" +
+                                        input +
+                                                "\n-------------------------------------------\n");
+
+
     }
 }
+
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
