3cd6d3052d21a11639464db1659429100268be0f
Tan Chia Qian <tanchiaqian@Tans-MacBook-Pro.local>
2020-08-19 02:12:50 +0800

    Revert "Level-2"
    This reverts commit fe7ef9d69bc731c14fdb30ca4f2fafea66780a11.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,36 +1,27 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String tab = "  ";
-        String lineBreaker = tab + "___________________________________________________________";
-        String greet = tab + "Hello! I'm Duke" + "\n" + tab + "What can I do for you?";
-        String end = tab + "Bye. Hope to see you again soon!";
+        String lineBreaker = "__________________________________";
+        String greet = "Hello! I'm Duke" + "\n" + tab + "What can I do for you?";
+        String end = "Bye. Hope to see you again soon!";
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> listOfStrings = new ArrayList<>();
 
-        System.out.println(lineBreaker);
-        System.out.println(greet);
-        System.out.println(lineBreaker);
+        System.out.println(tab + lineBreaker);
+        System.out.println(tab + greet);
+        System.out.println(tab + lineBreaker);
 
         while(sc.hasNext()) {
             String input = sc.next();
-            System.out.println(lineBreaker);
+            System.out.println(tab + lineBreaker);
             if(input.equals("bye")) {
-                System.out.println(end);
-                System.out.println(lineBreaker);
+                System.out.println(tab + end);
+                System.out.println(tab + lineBreaker);
                 break;
-            } else if(input.equals("list")) {
-                int i = 1;
-                for(String item : listOfStrings) {
-                    System.out.println(tab + i++ + ". " + item);
-                }
-                System.out.println(lineBreaker);
             } else {
-                listOfStrings.add(input);
-                System.out.println(tab + "added: " + input);
-                System.out.println(lineBreaker);
+                System.out.println(tab + input);
+                System.out.println(tab + lineBreaker);
             }
         }
     }
