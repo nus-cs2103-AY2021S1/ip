8ed32568a626c3307019e28187163574b90ac9a7
jeminsieow <jemin.sieow@prepr.org>
2020-08-18 22:09:53 +0800

    Add Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,15 +10,29 @@ public class Duke {
                 + "      What do you want?\n"
                 + line;
         System.out.println(greeting);
-
+        ArrayList<String> list = new ArrayList<>();
         String command = scanner.nextLine();
         String message;
         while (!command.equals("bye")) {
-            message = line
-                    + "      "
-                    + command
-                    + "\n"
-                    + line;
+            if (command.equals("list")) {
+                message = line;
+                for (int i = 0; i < list.size(); i++) {
+                    message = message
+                            + "      "
+                            + (i + 1)
+                            + ". "
+                            + list.get(i)
+                            + "\n";
+                }
+                message = message + line;
+            } else {
+                message = line
+                        + "      added: "
+                        + command
+                        + "\n"
+                        + line;
+                list.add(command);
+            }
             System.out.println(message);
             command = scanner.nextLine();
         }
