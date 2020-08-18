8aa8721ce6d800fe2e3a55a30cff87b43abaf905
PhongTran98 <giaphong8127@gmail.com>
2020-08-19 00:32:15 +0800

    Add level 5
    Teach Duke to deal with errors such as incorrect inputs entered by the user.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,10 +9,12 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|\n";
 
     private static void startMessage() {
+        System.out.println();
         System.out.println("++++++++++++++++++++++++++++++++++++++++++");
     }
     private static void endMessage() {
         System.out.println("------------------------------------------");
+        System.out.println();
     }
     private static void greet() {
         startMessage();
@@ -31,6 +33,7 @@ public class Duke {
     private static void handleCommand(String cmd) {
         startMessage();
         List<String> tokens = tokenize(cmd);
+        Task task;
         switch (tokens.get(0)) {
             case "bye":
                 System.out.println("See you again!");
@@ -40,26 +43,36 @@ public class Duke {
                 Task.printTasks();
                 break;
             case "done":
-                try {
-                    int index = Integer.parseInt(tokens.get(1));
-                    Task.iDone(index);
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("    " + Task.getTask(index));
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e.getLocalizedMessage());
-                }
+                handleDone(Integer.parseInt(tokens.get(1)));
                 break;
-            case "remove":
-                try {
-                    int index = Integer.parseInt(tokens.get(1));
-                    Task task = Task.iRemove(index);
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("    " + Task.getTask(index));
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e.getLocalizedMessage());
-                }
+//            case "remove":
+//                handleRemove(Integer.parseInt(tokens.get(1)));
+//                break;
+            case "todo":
+            case "deadline":
+            case "event":
+                handleAdd(tokens);
+                break;
             default:
-                Task task;
+                System.out.println("Sorry, I don't know what that means.");
+        }
+        endMessage();
+    }
+
+    private static void handleDone(int index) {
+        try {
+            Task.iDone(index);
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("    " + Task.getTask(index));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void handleAdd(List<String> tokens) {
+        try {
+            Task task;
+            try {
                 switch (tokens.get(0)) {
                     case "todo":
                         task = new Todo(tokens.get(1));
@@ -71,16 +84,33 @@ public class Duke {
                         task = new Event(tokens.get(1), tokens.get(3));
                         break;
                     default:
-                        System.out.println("Something when wrong");
+                        System.out.println("An unexpected error has occurred");
                         return;
                 }
-                Task.addTask(task);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println("    " + task);
-                System.out.printf("Now you have %d tasks in the list. \n", Task.count());
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Missing argument(s)");
+                return;
+            }
+
+            Task.addTask(task);
+            System.out.println("Got it. I've added this task: ");
+            System.out.println("    " + task);
+            System.out.printf("Now you have %d tasks in the list. \n", Task.count());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-        endMessage();
     }
+//
+//    private static void handleRemove(int index) {
+//        try {
+//            Task task = Task.iRemove(index);
+//            System.out.println("Nice! I've marked this task as done:");
+//            System.out.println("    " + Task.getTask(index));
+//        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getLocalizedMessage());
+//        }
+//    }
+
     public static void main(String[] args) {
         greet();
         Scanner sc = new Scanner(System.in);
