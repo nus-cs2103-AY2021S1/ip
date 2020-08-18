16ca0237f580f5357e01aded65689137d0708f94
Preston Toh <prestontoh@gmail.com>
2020-08-18 16:50:53 +0800

    clean up the Duke class to be more easily readable

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,61 +1,87 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        Scanner inputs = new Scanner(System.in);
-        boolean run = true;
-        int noOfTasks = 0;
-        Task[] tasks = new Task[100];
 
-        System.out.println("Hello~, I'm Duke!\n"
-                + "What can I do for you?");
+    private Scanner sc;
+    int noOfTasks;
+    Task[] tasks;
 
-        while (run == true) {
-            String next = inputs.next();
+    public static void main (String[] args) {
+        Duke duke = new Duke();
+        duke.initializeDuke();
+
+    }
+
+    public void initializeDuke() {
+        sc = new Scanner(System.in);
+        tasks = new Task[100];
+        System.out.println("Hello~ I'm Duke!\n" + "What can I do for you?");
+
+        while (sc.hasNextLine()) {
+            String nextInput = sc.next();
             System.out.println("____________________________________________________________");
-            if (next.equals("bye")) {
-                System.out.println("Goodbye~ Hope to see you again soon!");
-                run = false;
 
-            } else if (next.equals("list")) {
-                for (int i = 0; i < noOfTasks; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i]);
-                }
-            } else if (next.equals("done")) {
-                int doneTask = Integer.parseInt(inputs.next()) - 1;
-                tasks[doneTask].setDone();
-                System.out.println("Nice! I've set this task as done~\n" +
-                        tasks[doneTask]);
-            } else if (next.equals("todo")) {
-                Todo newTask = new Todo(inputs.nextLine());
-                tasks[noOfTasks] = newTask;
-                noOfTasks += 1;
-                System.out.println("Got it~ I've added this task:\n"
-                        + newTask + "\n" + "You now have " + noOfTasks + " tasks in the list~");
-            } else if (next.equals("deadline")) {
-                String[] deadlineSplit = inputs.nextLine().split("/by");
-                Deadline newTask = new Deadline(deadlineSplit[0], deadlineSplit[1]);
-                tasks[noOfTasks] = newTask;
-                noOfTasks += 1;
-                System.out.println("Got it~ I've added this task:\n"
-                        + newTask + "\n" + "You now have " + noOfTasks + " tasks in the list~");
-            } else if (next.equals("event")) {
-                String[] eventSplit = inputs.nextLine().split("/at");
-                Event newTask = new Event(eventSplit[0], eventSplit[1]);
-                tasks[noOfTasks] = newTask;
-                noOfTasks += 1;
-                System.out.println("Got it~ I've added this task:\n"
-                        + newTask + "\n" + "You now have " + noOfTasks + " tasks in the list~");
+            if (nextInput.equals("bye")) {
+                sc.close();
+                System.out.println("Goodbye~\n"
+                + "____________________________________________________________");
+                System.exit(0);
+                return;
+
+            } else if (nextInput.equals("list")) {
+                listTasks();
+            } else if (nextInput.equals("done")) {
+                processDone();
+            } else if (nextInput.equals("todo")) {
+                processToDo();
+            } else if (nextInput.equals("deadline")) {
+                processDeadline();
+            } else if (nextInput.equals("event")) {
+                processEvent();
             }
 
-//            } else {
-//                Task newTask = new Task(next, noOfTasks + 1);
-//                tasks[noOfTasks] = newTask;
-//                noOfTasks += 1;
-//                System.out.println("Added: " + next);
-//            }
             System.out.println("____________________________________________________________");
         }
+
+    }
+
+    public void listTasks() {
+        for (int i = 0; i < noOfTasks; i++) {
+            System.out.println(i + 1 + ". " + tasks[i]);
+        }
+    }
+
+    public void processDone() {
+        int doneTask = Integer.parseInt(sc.next()) - 1;
+        tasks[doneTask].setDone();
+        System.out.println("Nice! I've set this task as done~\n" +
+                tasks[doneTask]);
+    }
+
+    public void processToDo() {
+        Todo newTask = new Todo(sc.nextLine());
+        tasks[noOfTasks] = newTask;
+        noOfTasks += 1;
+        System.out.println("Got it~ I've added this task:\n"
+                + newTask + "\n" + "You now have " + noOfTasks + " tasks in the list~");
+    }
+
+    public void processDeadline() {
+        String[] deadlineSplit = sc.nextLine().split("/by");
+        Deadline newTask = new Deadline(deadlineSplit[0], deadlineSplit[1]);
+        tasks[noOfTasks] = newTask;
+        noOfTasks += 1;
+        System.out.println("Got it~ I've added this task:\n"
+                + newTask + "\n" + "You now have " + noOfTasks + " tasks in the list~");
+    }
+
+    public void processEvent() {
+        String[] eventSplit = sc.nextLine().split("/at");
+        Event newTask = new Event(eventSplit[0], eventSplit[1]);
+        tasks[noOfTasks] = newTask;
+        noOfTasks += 1;
+        System.out.println("Got it~ I've added this task:\n"
+                + newTask + "\n" + "You now have " + noOfTasks + " tasks in the list~");
     }
 
 
