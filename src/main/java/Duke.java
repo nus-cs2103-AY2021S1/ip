469e0bdab069e14e092ff6f4e3f3ba34f320f03e
Su <kkangs0226@gmail.com>
2020-08-18 23:27:28 +0800

    Add Level 1 features greet, echo, exit
    There is a basic greeting feature but no echo and exit features.
    
    A more personlized chat bot with a unique logo and tone will be more fun.
    
    Let's
    * add echo and exit features
    * change the logo and tone of the chat bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,51 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String divider = "_________________________________________________________________";
+    public static String logo = "             *\n"
+            + "      o  o  / \\  o  o\n"
+            + "      |\\/ \\/   \\/ \\/|\n"
+            + "      |             |\n"
+            + "      |ooooooooooooo|\n"
+            + " __  _  ____  ____    ____      ____    ___   ____  \n"
+            + "|  |/ ||    ||    \\  /    |    |    \\  /   \\ |    \\ \n"
+            + "|  ' /  |  | |  _  ||   __|    |  o  )|     ||  o  )\n"
+            + "|    \\  |  | |  |  ||  |  |    |     ||  O  ||     |\n"
+            + "|     | |  | |  |  ||  |_ |    |  O  ||     ||  O  |\n"
+            + "|  .  | |  | |  |  ||     |    |     ||     ||     |\n"
+            + "|__|\\_||____||__|__||___,_|    |_____| \\___/ |_____|\n";
+
+    public static void byeMessage() {
+        System.out.println(divider);
+        System.out.println("   Banana! King Bob is sad to see you go. Farewell my friend!");
+        System.out.println(divider);
+    }
+
+    public static void repeatInput(String nextLine) {
+        System.out.println(divider);
+        System.out.println("   " + nextLine);
+        System.out.println(divider + "\n");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Bello from the Majestic\n" + logo);
+        System.out.println("Banana! What can King Bob do for you?\n" + divider + "\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String nextLine = "";
+
+        while (scanner.hasNext()) {
+            nextLine = scanner.nextLine();
+            if (nextLine.equals("bye")) {
+                byeMessage();
+                break;
+            } else {
+                repeatInput(nextLine);
+            }
+        }
+        scanner.close();
+
     }
 }
