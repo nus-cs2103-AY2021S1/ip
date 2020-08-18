b20e536f4b5941b009437f364f3c97ab6374b576
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-08-19 01:07:58 +0800

    Deadline, Todo, Event

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task {
+    private String date;
+
+    public Deadline(String description, String date) {
+        super(description);
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + date + ")";
+    }
+}
