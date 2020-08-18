76c742078329f9c72b2fc3f203958bc46bc345b6
Xingjian <xingjian.chen@yahoo.com.sg>
2020-08-19 02:03:21 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,23 +11,35 @@ public class Duke {
         System.out.println(lineDivider + "\n");
     }
 
-    public static String showList(ArrayList<String> list) {
+    public static String showList(ArrayList<Task> list) {
         StringBuffer lst = new StringBuffer();
+        lst.append("Here are the tasks in your list:\n");
         int listSize = list.size();
         for (int i = 0; i < listSize; i++) {
             if (i + 1 != listSize) {
-                lst.append((i + 1) + ". " + list.get(i) + "\n");
+                lst.append((i + 1) + ". " + list.get(i).toString() + "\n");
             } else {
-                lst.append((i + 1) + ". " + list.get(i));
+                lst.append((i + 1) + ". " + list.get(i).toString());
             }
         }
         return lst.toString();
     }
 
+    public static boolean isInt(String s) {
+        try{
+            int num = Integer.parseInt(s);
+            // is an integer!
+            return true;
+        } catch (NumberFormatException e) {
+            // not an integer!
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -37,17 +49,35 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         echo("Hello! I'm Duke\nWhat can I do for you?");
+
         while (sc.hasNext()) {
             String msg = sc.nextLine();
+
             if (msg.equals("bye")) {
                 echo("Bye. Hope to see you again soon!");
                 break;
-            } else if (msg.equals("list")) {
-                echo(showList(list));
-            } else {
-                list.add(msg);
-                echo("added: " + msg);
             }
+
+            if (msg.equals("list")) {
+                echo(showList(list));
+                continue;
+            }
+
+            String[] msgArray = msg.split(" ");
+            if (msgArray.length == 2 && msgArray[0].equals("done") && isInt(msgArray[1])) {
+                 int index = Integer.parseInt(msgArray[1]) - 1;
+
+                 if (index < 0 || index >= list.size()) {
+                     echo("Index out of range!");
+                 } else {
+                     list.get(index).markAsDone();
+                     echo("Nice! I've marked this task as done:\n" + list.get(index).toString());
+                 }
+                 continue;
+            }
+
+            list.add(new Task(msg));
+            echo("added: " + msg);
         }
     }
 }
