0b8a706e2b0ddebe1bef24d38232516838b312aa
kaichaoang <kaichaoa@gmail.com>
2020-08-18 18:16:23 +0800

    Add the skeletal version of Duke
    Let's start by greeting the user. Duke will echo commands entered by the user, and exits
    when the user types the keyword "bye".
    
    To enhance user experience, reserved keywords are added which the user can type
    "help" to view all of those, and there are also visual lining breaks to provide a
    clearer view of events.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you today? (type: \"help\" to view list of commands)\n" +
+                "=========================================================================");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean terminated = false;
+
+        while (!terminated) {
+            String userInput = scanner.next();
+            if (userInput.equals("bye")) {
+                terminated = true;
+                System.out.println("Duke says: Goodbye and have a nice day! :D");
+                scanner.close();
+            } else if (userInput.equals("help")) {
+                System.out.println("bye: terminates program");
+            } else {
+                System.out.println("Duke says: " + userInput);
+            }
+            System.out.println("=========================================================================");
+        }
     }
 }
+
