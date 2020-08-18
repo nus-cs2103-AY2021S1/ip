cb305dcee48d19467059ec27d1b6db7a8e4586fb
jeminsieow <jemin.sieow@prepr.org>
2020-08-18 22:43:49 +0800

    Refactor Duke main function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,43 +4,59 @@ import java.util.Scanner;
 public class Duke {
     static Scanner scanner = new Scanner(System.in);
     static ArrayList<Task> taskList = new ArrayList<>();
+    final static String line = "    ____________________________________________________________\n";
 
     public static void main(String[] args) {
-        String line = "    ____________________________________________________________\n";
-        String greeting = line
+        greeting();
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            if (command.equals("list")) {
+                listCommand();
+            } else {
+                addCommand(command);
+            }
+            command = scanner.nextLine();
+        }
+        goodbye();
+    }
+
+    public static void greeting() {
+        String message = line
                 + "      Eh what's up\n"
                 + "      What do you want?\n"
                 + line;
-        System.out.println(greeting);
+        System.out.println(message);
+    }
 
-        String command = scanner.nextLine();
-        String message;
-        while (!command.equals("bye")) {
-            if (command.equals("list")) {
-                message = line;
-                for (int i = 0; i < taskList.size(); i++) {
-                    message = message
-                            + "      "
-                            + (i + 1)
-                            + ". "
-                            + taskList.get(i)
-                            + "\n";
-                }
-                message = message + line;
-            } else {
-                Task task = new Task(command);
-                taskList.add(task);
-                message = line
-                        + "      added: "
-                        + command
-                        + "\n"
-                        + line;
-            }
-            System.out.println(message);
-            command = scanner.nextLine();
-        }
-        message = line
+    public static void goodbye() {
+        String message = line
                 + "     Alright I'll see you around!\n"
+                + line;
+        System.out.println(message);
+    }
+
+    public static void listCommand() {
+        String message;
+        message = line;
+        for (int i = 0; i < taskList.size(); i++) {
+            message = message
+                    + "      "
+                    + (i + 1)
+                    + ". "
+                    + taskList.get(i)
+                    + "\n";
+        }
+        message = message + line;
+        System.out.println(message);
+    }
+
+    public static void addCommand(String command) {
+        Task task = new Task(command);
+        taskList.add(task);
+        String message = line
+                + "      added: "
+                + command
+                + "\n"
                 + line;
         System.out.println(message);
     }
