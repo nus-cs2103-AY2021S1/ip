553c9211f479a6616291a46baa34159d4ebefac0
zhaohuanqdcn <61936274+zhaohuanqdcn@users.noreply.github.com>
2020-08-18 22:16:02 +0800

    Level-1
    Greet, Echo, Exit are implemented
    
    Greet: auto-triggered upon running
    Echo: echo any input from the user
    Exit: exit on commands containing "bye" (not case-sensitive)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String logo =
+                      " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|";
+
+    private static void greet() {
+        System.out.println(logo);
+        System.out.println("Hello! This is Duke.\nWhat can I do for you?");
+    }
+
+    private static boolean isExit(String line) {
+        return line.toLowerCase().contains("bye");
+    }
+
+    private static void exit() {
+        System.out.println("Bye. Duke is always there for you!");
+        System.out.println(logo);
+    }
+
+    private static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        while (true) {
+            if (isExit(line)) {
+                exit();
+                break;
+            }
+            else {
+                System.out.println(line);
+                line = scanner.nextLine();
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        echo();
     }
 }
