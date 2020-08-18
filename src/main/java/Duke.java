72c2c11ca87304d77f760b0814bdf992785a33f1
wuqirui <53338059+hhdqirui@users.noreply.github.com>
2020-08-18 23:10:36 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    // print all the content in the list
+    public static void printList(ArrayList<String> list){
+        for(int i = 0; i < list.size(); i++){
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,16 +24,28 @@ public class Duke {
                            "What can I do for you?" +
                            "\n____________________________________________________________");
 
-        // echo command entered by the user
+        // add command entered by the user to the list
+        ArrayList<String> list = new ArrayList<>();
         String command;
         Scanner sc = new Scanner(System.in);
         while(true){
             command = sc.nextLine();
-            if(command.equals("bye")) break;
-            else System.out.println("____________________________________________________________\n" +
-                               command +
+            if(command.equals("bye")){
+                break;
+            }
+            else if(command.equals("list")){
+                System.out.println("____________________________________________________________");
+                printList(list);
+                System.out.println("____________________________________________________________");
+            }
+            else{
+                list.add(command);
+                System.out.println("____________________________________________________________\n" +
+                               "added: " + command +
                                "\n____________________________________________________________");
+            }
         }
+
         System.out.println("____________________________________________________________\n" +
                            "Bye. Hope to see you again soon!" +
                            "\n____________________________________________________________");
