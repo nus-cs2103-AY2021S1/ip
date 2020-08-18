f87a86a9a51ec2c4c690eece90514b44f9f80345
iamjamestan <jamestanyuli@gmail.com>
2020-08-18 19:27:34 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +6,14 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Duke\nSay something and I'll echo it.\nSay \"bye\" to end our conversation.");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.next();
+        }
+        System.out.println("Bye, hope to chat again soon!");
     }
 }
