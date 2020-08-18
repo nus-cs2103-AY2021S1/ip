cd1d1f3e7e113e30b4ce4bc2259d18136e584691
gohyeeloon <goh.yeeloon@gmail.com>
2020-08-18 17:17:20 +0800

    changed all instances of "introduction" to "greet"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
 public class Duke {
     public static void main(String[] args) {
-        System.out.println(Introduction.introduction());
+        System.out.println(Greet.greet());
     }
 }
--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,13 +1,13 @@
-public class Introduction {
-    public static String introduction() {
+public class Greet {
+    public static String greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String introduction = logo + "\n" +
+        String greet = logo + "\n" +
                 "hello! i'm duke :-)" + "\n" +
                 "how may i help you?" + "\n";
-        return introduction;
+        return greet;
     }
 }
