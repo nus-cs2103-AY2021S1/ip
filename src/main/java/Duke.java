849de7028c512d924c6168e96fa78dad48beb10f
GabriellaTeh <e0425488@u.nus.edu>
2020-08-19 00:36:57 +0800

    Add level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
 public class Duke {
+    public static String horizontal = "________________________" + "\n";
+
+    public static void action() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if (input.equals("list")) {
+                System.out.println(horizontal + input + "\n" + horizontal);
+            } else if (input.equals("blah")) {
+                System.out.println(horizontal + input + "\n" + horizontal);
+            } else if (input.equals("bye")) {
+                String bye = "Bye. Hope to see you again soon!" + "\n";
+                System.out.println(horizontal + bye + horizontal);
+                break;
+            }
+        }
+    }
+
+    public static void greet() {
+        String hello = "Hello! I'm Duke" + "\n" + "What can I do for you?" + "\n";
+        System.out.println(horizontal + hello + horizontal);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        action();
+
+        //String logo = " ____        _        \n"
+        //      + "|  _ \\ _   _| | _____ \n"
+        //        + "| | | | | | | |/ / _ \\\n"
+        //        + "| |_| | |_| |   <  __/\n"
+        //        + "|____/ \\__,_|_|\\_\\___|\n";
+        //System.out.println("Hello from\n" + logo);
     }
 }
