f730663111185f603fc73c9a9233443f28a9b941
Marcus Tan <marcustan26@hotmail.com>
2020-08-18 21:46:11 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String horiLine = "____________________________________________________________" + "\n";
+
+    private static void greet() {
+        String greeting = "Hello! I'm Duke" + "\n" + "What can I do for you?" + "\n";
+        System.out.println(horiLine + greeting + horiLine);
+    }
+
+    private static void echo() {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String input = sc.nextLine();
+            if(input.equals("bye")) { //exit condition
+                String goodbye = "Bye. Hope to see you again soon!";
+                System.out.println(horiLine + goodbye + "\n" + horiLine);
+                break;
+            } else {
+                System.out.println(horiLine + input + "\n" + horiLine);
+            }
+        }
+        sc.close();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +30,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        echo();
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
     }
 }
