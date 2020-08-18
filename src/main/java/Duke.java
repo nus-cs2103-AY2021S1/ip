2580f82529bcab1b9aa6ddd94097d8cb0692db6b
wuqirui <53338059+hhdqirui@users.noreply.github.com>
2020-08-18 22:50:19 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // print greetings of chatbot
+        System.out.println("____________________________________________________________\n" +
+                           "Hello! I'm Duke\n" +
+                           "What can I do for you?" +
+                           "\n____________________________________________________________");
+
+        // echo command entered by the user
+        String command;
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            command = sc.nextLine();
+            if(command.equals("bye")) break;
+            else System.out.println("____________________________________________________________\n" +
+                               command +
+                               "\n____________________________________________________________");
+        }
+        System.out.println("____________________________________________________________\n" +
+                           "Bye. Hope to see you again soon!" +
+                           "\n____________________________________________________________");
     }
 }
