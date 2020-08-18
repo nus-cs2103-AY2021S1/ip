1ea1333c7f54aabbdeb27546e7f3afa60c3d5557
onglijin <onglijinlj@gmail.com>
2020-08-18 19:30:48 +0800

    Add the ability to store text entered by user and display when requested

--- src/main/java/Mocha.java
+++ src/main/java/Mocha.java
@@ -1,20 +1,39 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Mocha {
     public static void main(String[] args) {
+        ArrayList<String> listOfTasks = new ArrayList<>();
 
-        String horizontalLine = "_______________________________________________________" + "\r\n";
+        String horizontalLine = "_______________________________________________________";
         String nameIntro = "Hello, I'm Mocha!";
         String greeting = "What's up today!";
-        System.out.println(horizontalLine + "\r\n" + nameIntro + "\r\n" + greeting + "\r\n" + horizontalLine + "\r\n");
+        System.out.println(horizontalLine + "\r\n" + nameIntro + "\r\n"
+                + greeting + "\r\n" + horizontalLine + "\r\n");
 
         Scanner userCommand = new Scanner(System.in);
-        while (userCommand.hasNext() && !userCommand.hasNext("bye")) {
-            System.out.println(horizontalLine + "\r\n" + userCommand.next() + "\r\n" + horizontalLine);
-        }
-        if (userCommand.hasNext("bye")) {
-            System.out.println(horizontalLine + "\r\n" + "Bye! See ya soon!" + "\r\n" + horizontalLine);
-            userCommand.close();
+
+        while (userCommand.hasNext()) {
+            if (!userCommand.hasNext("list") && !userCommand.hasNext("bye")) {
+                String itemToBeAdded = userCommand.nextLine();
+                System.out.println(horizontalLine + "\r\n" + "added: "
+                        + itemToBeAdded + "\r\n" + horizontalLine + "\r\n");
+                listOfTasks.add(itemToBeAdded);
+            }
+
+            if (userCommand.hasNext("list")) { // Process closing here
+                System.out.println(horizontalLine + " \r\n");
+                for (int i = 0; i < listOfTasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + listOfTasks.get(i));
+                }
+                System.out.println(horizontalLine + " \r\n");
+                userCommand.nextLine();
+            }
+
+            if (userCommand.hasNext("bye")) {
+                System.out.println(horizontalLine + "\r\n" + "Bye! See ya soon!" + "\r\n" + horizontalLine);
+                userCommand.nextLine();
+            }
         }
     }
 }
