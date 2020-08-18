28a57dacd441942aafcc45b144327e1653c00e80
unknown <schoolex@hotmail.com>
2020-08-18 22:06:46 +0800

    Added basic exception handling for invalid commands and invalid tasks(done)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,14 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static String command;
+    public static String[] inputs,tokens;
+    public static ArrayList<Task> tasks = new ArrayList<Task>();
+
+    public static void main(String[] args) throws DukeException{
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("______________________");
 
-        String command;
-        String[] inputs,tokens;
-        ArrayList<Task> tasks = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
         inputs = sc.nextLine().split(" ",2);
         command = inputs[0];
@@ -24,9 +25,11 @@ public class Duke {
                     }
                     break;
                 case "done":
-                    tasks.get(Integer.parseInt(inputs[1]) - 1).markDone();
-                    System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println(tasks.get(Integer.parseInt(inputs[1]) - 1));
+                    try {
+                        handleDone();
+                    } catch (DukeException e) {
+                        System.out.println(e);
+                    }
                     break;
                 case "deadline":
                     tokens = inputs[1].split(" /by ");
@@ -43,14 +46,18 @@ public class Duke {
                     System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     break;
                 case "todo":
-                    tasks.add(new ToDo(inputs[1]));
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks.get(tasks.size() - 1));
-                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    try {
+                        handleToDo();
+                    } catch (DukeException e) {
+                        System.out.println(e);
+                    }
                     break;
                 default:
-                    tasks.add(new Task(String.join(" ",inputs)));
-                    System.out.println("added: " + String.join(" ",inputs));
+                    try {
+                        throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                    } catch (DukeException e) {
+                        System.out.println(e);
+                    }
             }
             inputs = sc.nextLine().split(" ",2);
             command = inputs[0];
@@ -58,5 +65,24 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("--------------------------------");
     }
-}
 
+    private static void handleToDo() throws DukeException {
+        if (inputs.length < 2) {
+            throw new DukeException("The description of a todo cannot be empty.");
+        }
+        tasks.add(new ToDo(inputs[1]));
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+    }
+
+    private static void handleDone() throws DukeException {
+        int index = Integer.parseInt(inputs[1]) - 1;
+        if (index >= tasks.size() || index < 0) {
+            throw new DukeException("This task does not exist");
+        }
+        tasks.get(index).markDone();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println(tasks.get(index));
+    }
+}
