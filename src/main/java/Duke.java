456de8dcde365abc5cb462232ac637520bd36bf1
kaitlynng <Kaitlyn.nky@gmail.com>
2020-08-18 18:19:12 +0800

    Level 1 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "----------------------------------------------------";
+        String botName = "Duke";
+        String userName = "You";
+        String exitStr = "bye";
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        System.out.println("Hello, I'm\n" + logo + "\nWhat can I do for you?\n" + line);
+
+        System.out.print(userName + " said: ");
+
+        userInput = scanner.nextLine();
+
+        while (!userInput.equals(exitStr)) {
+            System.out.println(line);
+            System.out.println(botName + " said: " + userInput);
+            System.out.println(line);
+            System.out.print(userName + " said: ");
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println(line);
+        System.out.println(botName + " said: Bye! Hope to see you again!");
+        System.out.println(line);
     }
 }
