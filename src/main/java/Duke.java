7e4841dc9883b033939a3309babf35d92e00d559
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-08-18 21:28:52 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Duke: Hello! I'm Duke.");
+        System.out.println("Duke: What can I do for you?");
+
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("Duke: " + input);
+            input = sc.nextLine();
+        }
+        System.out.println("Duke: Bye. Hope to see you again!");
     }
 }
