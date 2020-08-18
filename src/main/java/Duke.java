e6296b6d4bde4c53ee05f0e346695f1d91701b2e
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-08-18 09:49:07 +0800

    Replace greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,9 @@ import java.io.IOException;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hi I'm Duke, your personal task-tracker bot!");
+        System.out.println("You can add todos, deadlines, or events to my " +
+                                   "list.");
 
         try {
             Scanner.scan();
