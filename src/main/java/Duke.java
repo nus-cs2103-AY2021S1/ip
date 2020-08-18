1c0d3e017051fa7a8722572e2dbaf759d4045301
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-08-18 18:12:00 +0800

    Update chatbot to add and list input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -17,19 +18,42 @@ public class Duke {
         Scanner myObj = new Scanner(System.in);
         String input;
 
+        // Initialise ArrayList to store input from user
+        ArrayList<String> userTexts = new ArrayList<String>();
+
         // Start chat
         do {
+            // Get input from user
             System.out.println(masterSpeak);
             input = myObj.nextLine();
             System.out.println();
-            if (input.equalsIgnoreCase("bye")) {
+
+            // If user inputs "bye" in any case, end the chat
+            if (input.equals("bye")) {
                 System.out.println(servantSpeak
                         + "    It was a pleasure to serve you my Liege.\n"
                         + "    Till next time.");
                 break;
             }
+
+            // If user requests for list, display list
+            if (input.equals("list")) {
+                int count = 1;
+                System.out.print(servantSpeak);
+                for (String i : userTexts) {
+                    System.out.println("    "
+                            + count + ". "
+                            + i);
+                    count++;
+                }
+                System.out.println();
+                continue;
+            }
+
+            // Default: Add input from user into the ArrayList
+            userTexts.add(input);
             System.out.println(servantSpeak
-                    + "    " + input + "\n");
+                    + "    added: " + input + "\n");
         } while (!input.equalsIgnoreCase("bye"));
 
     }
