67bf776eb6292272d7b80ee22b6a8801b966151d
Tan Chia Qian <tanchiaqian@Tans-MacBook-Pro.local>
2020-08-18 19:50:09 +0800

    Revert "Level 1"
    This reverts commit ed6394fd9de2752ddf56de55733db33f2d696e79.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,10 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
-        String lineBreaker = "__________________________________";
-        String greet = "Hello! I'm Duke" + "\n" +"What can I do for you?";
-        String end = "Bye. Hope to see you again soon!";
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println(lineBreaker);
-        System.out.println(greet);
-        System.out.println(lineBreaker);
-
-        while(sc.hasNext()) {
-            String input = sc.next();
-            System.out.println(lineBreaker);
-            if(input.equals("bye")) {
-                System.out.println(end);
-                System.out.println(lineBreaker);
-                break;
-            } else {
-                System.out.println(input);
-                System.out.println(lineBreaker);
-            }
-        }
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
 }
