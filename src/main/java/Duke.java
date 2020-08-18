6aec828fdbe9913f202e5fa1df2b6e5e2ce9ccbd
rachel170 <e0418127@u.nus.edu>
2020-08-18 19:03:59 +0800

    Added ability to store text input from user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        /** Initialize objects required **/
         // Using Scanner for reading inputs
         Scanner scanner = new Scanner(System.in);
+        // Initialize List for Duke to store stuff in
+        ArrayList<String> list = new ArrayList<>();
 
         // Text Images
         String logo = " ____        _        \n"
@@ -30,11 +34,11 @@ public class Duke {
         while (!instructions.equals("bye")) {
             switch(instructions) {
                 default:
-                    // Echo messages by default
+                    // Store messages by default
                     System.out.print(outputBreaker);
-                    System.out.println(instructions);
-                    System.out.println("\n" + lineBreaker);
-                    System.out.println();
+                    list.add(instructions);
+                    System.out.println("added: " + instructions);
+                    System.out.println("\n" + lineBreaker + "\n");
                     break;
             }
 
