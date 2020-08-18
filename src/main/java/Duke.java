ed71b69485fa5f5059292e56350e2d39374f5ca3
JinYuze <1072217974@qq,com>
2020-08-18 20:18:22 +0800

    level-1 : Duke is a repeater unless you let him go...

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+package main.java;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner inputScanner = new Scanner(System.in);
+        String userInput = "";
+
+        System.out.println("//////////\n" + "->Hello! I'm Duke\n" +
+                "->What can I do for you?" + "\n\n");
+
+        while (!userInput.equals("bye")) {
+            userInput = inputScanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("\nBye. Hope to see you again soon!");
+            } else {
+                System.out.println("\n->" + userInput + "\n");
+            }
+        }
     }
 }
