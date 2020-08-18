efbcae6fe16364ac331a3e78a2e7bd443085e012
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-08-18 23:39:43 +0800

    Marked as Done, add Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,8 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+
+        ArrayList<Task> list = new ArrayList<>();
 
         System.out.println("Hello! I'm Duke.");
         System.out.println("What can I do for you?");
@@ -23,15 +24,25 @@ public class Duke {
                 if (listSize == 0) {
                     System.out.println("Your list is empty.");
                 } else {
-                    System.out.println("Your list is as follows:");
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < listSize; i++) {
-                        int index = i + 1;
-                        System.out.print(index + ". ");
+                        int taskNum = i + 1;
+                        System.out.print(taskNum + ".");
                         System.out.println(list.get(i));
                     }
                 }
+            } else if (input.length() > 5 && input.substring(0,4).equals("done")) {
+                int index = Integer.parseInt(input.substring(input.length()-1)) - 1;
+                if (index > list.size() - 1) {
+                    System.out.println("Task number out of range.");
+                    input = sc.nextLine();
+                    continue;
+                }
+                Task task = list.get(index);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task.done());
             } else {
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("I have added: " + input);
             }
             input = sc.nextLine();
