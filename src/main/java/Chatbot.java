8d26e7fdc5a9751de62c30fe3f4c771a4658167c
the-reefshark <rishabh92k@gmail.com>
2020-08-18 14:27:33 +0800

    Refactor code

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -2,93 +2,135 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Chatbot {
-    String divider = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-    String indent = "        ";
+
+    private final String DIVIDER = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    private final String INDENT = "        ";
+    private final ArrayList<Task> tasks;
+
+    public Chatbot() {
+        this.tasks = new ArrayList<>();
+    }
 
     public void start() {
+
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<>();
-        System.out.println(indent + "What can I do for you delightful human?\n" + divider);
+        System.out.println(INDENT + "What can I do for you delightful human?\n" + DIVIDER);
 
         while(scanner.hasNext()) {
+
             String input = scanner.nextLine();
-            System.out.println(divider);
+            System.out.println(DIVIDER);
 
             try {
-                if (input.startsWith("done")) {
-                    if (input.equals("done")) { // Input does not contain which task to mark as done
-                        throw new DukeException(indent + "Sorry sir you will have to try again and this time " +
-                                "tell me which task you want me to mark as done");
-                    }
-                    int index = Integer.parseInt(input.split(" ")[1]);
-                    Task curr = tasks.get(index - 1);
-                    curr.markAsDone();
-                    System.out.println(indent + "Good job! I've marked this task as done:");
-                    System.out.println(indent + "  " + curr);
-                    System.out.println(divider);
-                } else if (input.startsWith("todo") || input.startsWith("deadline") || input.startsWith("event")) {
-                    Task curr;
-                    if (input.startsWith("todo")) { // Create Todo task
-                        // Input does not contain the required keyword
-                        if (input.equals("todo") || input.substring(5).isEmpty()) {
-                            throw new DukeException(indent + "My apologies sir but the description of todo " +
-                                    "cannot be empty :(");
-                        }
-                        curr = new Todo(input.substring(5));
-                        tasks.add(curr);
-                    } else if (input.startsWith("deadline")) { // Create Deadline task
-                        // Input does not contain the required keyword
-                        if (input.equals("deadline") || !input.substring(9).contains("/by")) {
-                            throw new DukeException(indent + "My apologies sir but you will have to use the correct " +
-                                    "format to create a deadline :(");
-                        }
-                        String[] splicedInput = input.substring(9).split(" /by ");
-                        curr = new Deadline(splicedInput[0], splicedInput[1]);
-                        tasks.add(curr);
-                    } else { // Create Event task
-                        // Input does not contain the required keyword
-                        if (input.equals("event") || !input.substring(6).contains("/at")) {
-                            throw new DukeException(indent + "My apologies sir but you will have to use the correct " +
-                                    "format to create a event :(");
-                        }
-                        String[] splicedInput = input.substring(6).split(" /at ");
-                        curr = new Event(splicedInput[0], splicedInput[1]);
-                        tasks.add(curr);
-                    }
-                    System.out.println(indent + "Affirmative. I've added this task:\n  " +
-                            indent + curr);
-                    System.out.println(indent + "Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println(divider);
-                } else if (input.equals("list")) {
-                    System.out.println(indent + "Here are your tasks for today:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        Task curr = tasks.get(i);
-                        System.out.println(indent + (i + 1) + "." + curr);
-                    }
-                    System.out.println(divider);
-                } else if (input.startsWith("delete")) {
-                    if (input.equals("delete")) { // Input does not contain which task to mark as done
-                        throw new DukeException(indent + "Sorry sir you will have to try again and this time " +
-                                "tell me which task you want me to delete");
-                    }
-                    int index = Integer.parseInt(input.split(" ")[1]);
-                    Task deletedTask = tasks.remove(index - 1); // Subtract 1 due to offset
-                    System.out.println(indent + "Of course sir. I have removed this task:\n" +
-                            indent + "  " + deletedTask);
-                    System.out.println(indent + "Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println(divider);
-                } else if (input.equals("bye")) { // Termination statement
-                    System.out.println(indent + "Guess its time for us to part ways\n"
-                            + indent + "Thanks for the memories\n" + indent + ":`(");
-                    System.out.println(divider);
+                if (input.equals("bye")) {
+                    System.out.println(INDENT + "Guess its time for us to part ways\n"
+                            + INDENT + "Thanks for the memories\n" + INDENT + ":`(");
+                    System.out.println(DIVIDER);
                     break;
-                } else { // Invalid command
-                    throw new DukeException(indent + "Sorry sir but I don't know what you mean :(");
+                } else if (input.equals("list")) {
+                    printTasks();
+                } else if (input.startsWith("done")) {
+                    markAsDone(input);
+                } else if (input.startsWith("delete")) {
+                    deleteTask(input);
+                } else {
+
+                    Task currTask = null;
+
+                    if (input.startsWith("todo")) {
+                        currTask = createTodo(input);
+                    } else if (input.startsWith("deadline")) {
+                        currTask = createDeadline(input);
+                    } else if (input.startsWith("event")) {
+                        currTask = createEvent(input);
+                    } else {
+                        invalidCommand();
+                    }
+
+                    if (currTask != null) {
+                        addTask(currTask);
+                    }
                 }
             } catch (DukeException e) {
                 System.out.println(e.getMessage());
-                System.out.println(divider);
+                System.out.println(DIVIDER);
             }
         }
+    }
+
+    private void printTasks() {
+        System.out.println(INDENT + "Here are your tasks for today:");
+        for (int i = 0; i < tasks.size(); i++) {
+            Task curr = tasks.get(i);
+            System.out.println(INDENT + (i + 1) + "." + curr);
+        }
+        System.out.println(DIVIDER);
+    }
+
+    private void markAsDone(String input) throws DukeException {
+        if (input.equals("done")) { // Input does not contain which task to mark as done
+            throw new DukeException(INDENT + "Sorry sir you will have to try again and this time " +
+                    "tell me which task you want me to mark as done");
+        }
+        int index = Integer.parseInt(input.split(" ")[1]);
+        Task curr = tasks.get(index - 1);
+        curr.markAsDone();
+        System.out.println(INDENT + "Good job! I've marked this task as done:");
+        System.out.println(INDENT + "  " + curr);
+        System.out.println(DIVIDER);
+    }
+
+    private void deleteTask(String input) throws DukeException {
+        if (input.equals("delete")) { // Input does not contain which task to mark as done
+            throw new DukeException(INDENT + "Sorry sir you will have to try again and this time " +
+                    "tell me which task you want me to delete");
+        }
+        int index = Integer.parseInt(input.split(" ")[1]);
+        Task deletedTask = tasks.remove(index - 1); // Subtract 1 due to offset
+        System.out.println(INDENT + "Of course sir. I have removed this task:\n" +
+                INDENT + "  " + deletedTask);
+        System.out.println(INDENT + "Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println(DIVIDER);
+    }
+
+    private Task createTodo(String input) throws DukeException {
+        // Input does not contain the required keyword
+        if (input.equals("todo") || input.substring(5).isEmpty()) {
+            throw new DukeException(INDENT + "My apologies sir but the description of todo " +
+                    "cannot be empty :(");
+        }
+        return new Todo(input.substring(5));
+    }
+
+    private Task createDeadline(String input) throws DukeException {
+        // Input does not contain the required keyword
+        if (input.equals("deadline") || !input.substring(9).contains("/by")) {
+            throw new DukeException(INDENT + "My apologies sir but you will have to use the correct " +
+                    "format to create a deadline :(");
+        }
+        String[] splicedInput = input.substring(9).split(" /by ");
+        return new Deadline(splicedInput[0], splicedInput[1]);
+    }
+
+    private Task createEvent(String input) throws DukeException {
+        // Input does not contain the required keyword
+        if (input.equals("event") || !input.substring(6).contains("/at")) {
+            throw new DukeException(INDENT + "My apologies sir but you will have to use the correct " +
+                    "format to create a event :(");
+        }
+        String[] splicedInput = input.substring(6).split(" /at ");
+        return new Event(splicedInput[0], splicedInput[1]);
+    }
+
+    private void invalidCommand() throws DukeException {
+        throw new DukeException(INDENT + "Sorry sir but I don't know what you mean :(");
+    }
+
+    private void addTask(Task curr) {
+        tasks.add(curr);
+        System.out.println(INDENT + "Affirmative. I've added this task:\n  " +
+                INDENT + curr);
+        System.out.println(INDENT + "Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println(DIVIDER);
     }
 }
