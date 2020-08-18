8dbcbe175c024e70c3e4caec274eeff2879a9f86
Tuan Le <lequangtuan6b@gmail.com>
2020-08-18 16:19:31 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final static String SEPARATOR = "___________________________________________________________";
+
+    private static void printMessage(String message)  {
+        System.out.println("     " + SEPARATOR);
+        System.out.println("     " + message);
+        System.out.println("     " + SEPARATOR);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +16,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+
+            if (command.equals("bye")) {
+                String BYE_STATEMENT = "Bye. Hope to see you again soon!";
+                printMessage(BYE_STATEMENT);
+                break;
+            } else {
+                printMessage(command);
+            }
+        }
     }
 }
