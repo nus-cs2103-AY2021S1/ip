eda0708f9948b4baec306f1fb09184e8ba58b0eb
Low Ming Lim <48002577+Avalionnet@users.noreply.github.com>
2020-08-18 18:33:17 +0800

    Initialised and completed Level 1 of Duke Project. Added scanner to take in user input and detection of "bye" command for user to quit program.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String breakline = "    ______________________________________________________";
+        String logo = "     ____        _        \n"
+                + "    |  _ \\ _   _| | _____ \n"
+                + "    | | | | | | | |/ / _ \\\n"
+                + "    | |_| | |_| |   <  __/\n"
+                + "    |____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("    Welcome to \n" + logo + "\n    Your personal assistant :)");
+        System.out.println(breakline);
+        System.out.println(breakline);
+
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String userCommand = sc.nextLine();
+            if (userCommand.equals("bye")) {
+                System.out.println(breakline);
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println(breakline);
+                break;
+            }
+            switch (userCommand) {
+                default:
+                    System.out.println(breakline);
+                    System.out.println("    " + userCommand);
+                    System.out.println(breakline);
+                    break;
+            }
+        }
+        sc.close();
     }
 }
