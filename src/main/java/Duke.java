0cf101d242ccfdbdd5eec579412c112e8096c595
Zhengyang Lin <nauw1010@gmail.com>
2020-08-19 03:13:13 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "____________________________________________________________\n";
-        System.out.println(line + logo + "\nHello, I'm Duke \nWhat can I do for you?\n" + line);
+        System.out.println(line
+                + logo
+                + "\n Hello, I'm Duke \n What can I do for you?\n"
+                + line);
 
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
@@ -18,25 +21,46 @@ public class Duke {
         List<Task> taskList = new ArrayList<>();
 
         while(!command.equals("bye")) {
+            //command = list
             if(command.equals("list")) {
                 System.out.print(line);
-                System.out.println("Here are the tasks in your list:");
+                System.out.println(" Here are the tasks in your list:");
                 for(int i = 0; i < taskList.size(); i++) {
-                    System.out.println((i + 1) + ". " + taskList.get(i));
+                    System.out.println(" " + (i + 1) + ". " + taskList.get(i));
                 }
                 System.out.println(line);
             } else {
-                String[] commands = command.split(" ");
+                String[] commands = command.split(" ", 2);
+                //command = done
                 if(commands[0].equals("done")) {
                     int index = Integer.parseInt(commands[1]) - 1;
                     taskList.get(index).markAsDone();
                     System.out.println(line
-                            + "Nice! I've marked this task as done: "
-                            + "\n  " + taskList.get(index) + "\n"
+                            + " Nice! I've marked this task as done: "
+                            + "\n   " + taskList.get(index) + "\n"
                             + line);
-                } else {
-                    System.out.println(line + "added: " + command + "\n" + line);
-                    taskList.add(new Task(command));
+                } else { //add tasks
+                    System.out.println(line
+                            + " Got it. I've added this task: ");
+                    Task task = null;
+                    //toDos
+                    if(commands[0].equals("todo")) {
+                        task = new Todo(commands[1]);
+                    }
+                    //deadline
+                    if(commands[0].equals("deadline")) {
+                        String[] arguments = commands[1].split(" /by ");
+                        task = new Deadline(arguments[0], arguments[1]);
+                    }
+                    //event
+                    if(commands[0].equals("event")) {
+                        String[] arguments = commands[1].split(" /at ");
+                        task = new Event(arguments[0], arguments[1]);
+                    }
+                    taskList.add(task);
+                    System.out.println("   " + task
+                            + "\n Now you have " + taskList.size() + " tasks in the list.\n"
+                            + line);
                 }
             }
             command = sc.nextLine();
