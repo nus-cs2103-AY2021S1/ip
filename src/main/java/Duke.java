97a53b753b051db1e1fcd38f6611c5816e429907
WayneTanKinLun <waynetan2000@gmail.com>
2020-08-18 20:37:15 +0800

    add level3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,11 +9,12 @@ public class Duke {
 
     public static void printAllTask(Task[] tasks) {
         int numTask = 0;
+        System.out.println("Here are the tasks in your list:");
         while (tasks[numTask] != null) {
-            System.out.println( Integer.valueOf(numTask + 1) + ". " + tasks[numTask]);
+            System.out.println( Integer.valueOf(numTask + 1) + ".[" + tasks[numTask].getStatusIcon() + "] "
+                    + tasks[numTask]);
             numTask++;
         }
-
     }
 
     public static void main(String[] args) {
@@ -23,14 +24,20 @@ public class Duke {
 
         greeting();
         String input;
+        String[] inputArr;
         while(true) {
             input = sc.nextLine();
-            if (input.equals("bye")) {
+            inputArr = input.split(" ");
+            if (inputArr[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 sc.close();
                 break;
-            } else if (input.equals("list")) {
+            } else if (inputArr[0].equals("list")) {
                 printAllTask(tasks);
+            } else if (inputArr[0].equals("done")) {
+                String counter = inputArr[1];
+                int pos = Integer.parseInt(counter);
+                tasks[pos - 1].markAsDone();
             } else {
                 tasks[numTask] = new Task(input);
                 System.out.println("added: " + tasks[numTask]);
