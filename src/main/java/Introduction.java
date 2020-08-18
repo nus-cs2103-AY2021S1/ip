b95c65151210c00d1ad77bbc61b64636f3093482
gohyeeloon <goh.yeeloon@gmail.com>
2020-08-18 17:12:47 +0800

    edited Duke's introduction and added the introduction to the separate class file Introduction.java

---
+++ src/main/java/Introduction.java
@@ -0,0 +1,13 @@
+public class Introduction {
+    public static String introduction() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String introduction = logo + "\n" +
+                "hello! i'm duke :-)" + "\n" +
+                "how may i help you?" + "\n";
+        return introduction;
+    }
+}
