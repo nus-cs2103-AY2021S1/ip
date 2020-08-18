434e576377de4b647646eaa6b9583277f66bb3a3
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-08-18 22:03:15 +0800

    Update getStatusIcon to return tick or X symbol

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -16,6 +16,6 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "Done" : "Not Done"); //return tick or X symbols
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 }
