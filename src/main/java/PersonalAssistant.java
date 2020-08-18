277bca951e9607667ea7b97b75c2f3161e8945e4
Kwan Noel <noelkwan1998@gmail.com>
2020-08-18 15:51:42 +0800

    Finish level 2

--- src/main/java/PersonalAssistant.java
+++ src/main/java/PersonalAssistant.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class PersonalAssistant {
-    public static void run() {
+    private ArrayList<String> store;
+
+    /**
+     * Initialize personal assistant with a store for user input
+     */
+    public PersonalAssistant() {
+        store = new ArrayList<String>();
+    }
+
+    public void run() {
         System.out.println("> What can I do for you?");
         getUserCommands();
     }
@@ -11,16 +21,30 @@ public class PersonalAssistant {
      * Exit and echo a farewell message
      * Otherwise echoes the input
      */
-    public static void getUserCommands() {
+    public void getUserCommands() {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter your command or \"bye\" to exit: ");
         String cmd = reader.nextLine();
-        if (cmd.equals("bye")) {
-            System.out.println("Goodbye!");
-        } else {
-            System.out.println(cmd);
-            System.out.println("\n");
-            getUserCommands();
+        switch (cmd) {
+            case "bye":
+                System.out.println("Goodbye!");
+                break;
+            case "list":
+                this.list();
+                getUserCommands();
+                break;
+            default:
+                store.add(cmd);
+                System.out.println(cmd);
+                System.out.println("\n");
+                getUserCommands();
+        }
+    }
+
+    public void list() {
+        for (int i = 0; i < store.size(); i++) {
+            String listText = String.format("%d. %s", i + 1, store.get(i));
+            System.out.println(listText);
         }
     }
 }
