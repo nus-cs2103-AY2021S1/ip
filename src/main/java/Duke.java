8d28a10dcf29f79de26918f00edbfbc93258049c
YangYue128-helen <e0376999@u.nus.edu>
2020-08-19 00:50:41 +0800

    level 2.Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,30 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] orderList = new String[100];
         Scanner sc =  new Scanner(System.in);
         String seperateLine = "    _______________________________________";
         String spaceBeforeOder = "      ";
         System.out.println(seperateLine);
         System.out.println(spaceBeforeOder + "Hello! I'm Duke yy\n      What can I do for you?");
         System.out.println(seperateLine);
-        String order = sc.next();
+        String order = sc.nextLine();
+        int numOfOders = 0;
         while (!order.equals("bye")) {
-            System.out.println(seperateLine);
-            System.out.println(spaceBeforeOder + order);
-            System.out.println(seperateLine);
-            order = sc.next();
+            if (order.equals("list")) {
+                System.out.println(seperateLine);
+                for (int i = 0; i < numOfOders; i++){
+                    System.out.println(spaceBeforeOder + (i + 1) + ". " + orderList[i]);
+                }
+                System.out.println(seperateLine);
+            } else {
+                System.out.println(seperateLine);
+                System.out.println(spaceBeforeOder + "added: " + order);
+                System.out.println(seperateLine);
+                orderList[numOfOders] = order;
+                numOfOders++;
+            }
+            order = sc.nextLine();
         }
         System.out.println(seperateLine);
         System.out.println(spaceBeforeOder + "Bye. Very nice to meet you!");
