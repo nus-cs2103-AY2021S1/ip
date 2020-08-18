397e82cb6f9a657a02705a557c8d263e5f9a3633
YangYue128-helen <e0376999@u.nus.edu>
2020-08-19 00:36:57 +0800

    Level 1.Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc =  new Scanner(System.in);
+        String seperateLine = "    _______________________________________";
+        String spaceBeforeOder = "      ";
+        System.out.println(seperateLine);
+        System.out.println(spaceBeforeOder + "Hello! I'm Duke yy\n      What can I do for you?");
+        System.out.println(seperateLine);
+        String order = sc.next();
+        while (!order.equals("bye")) {
+            System.out.println(seperateLine);
+            System.out.println(spaceBeforeOder + order);
+            System.out.println(seperateLine);
+            order = sc.next();
+        }
+        System.out.println(seperateLine);
+        System.out.println(spaceBeforeOder + "Bye. Very nice to meet you!");
+        System.out.println(spaceBeforeOder + "Hope to see you again soon! ");
+        System.out.println(seperateLine);
+        sc.close();
     }
 }
