d3b4eb9fc8573fb01346c56677da44ddb0594321
unknown <schoolex@hotmail.com>
2020-08-18 20:01:49 +0800

    Added mark as done functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,26 +7,59 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("______________________");
 
-        String input;
-        ArrayList<String> tasks = new ArrayList<String>();
+        String command;
+        String[] inputs;
+        ArrayList<Task> tasks = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
-        input = sc.nextLine();
+        inputs = sc.nextLine().split(" ",2);
+        command = inputs[0];
 
-        while(!input.equals("bye")) {
-            if (input.equals("list")) {
-                System.out.println("Task list");
-                int i = 1;
-                for (String task : tasks) {
-                    System.out.println(i++ + ". " + task);
-                }
-            } else {
-                tasks.add(input);
-                System.out.println("added: " + input);
+        while(!command.equals("bye")) {
+            switch(command) {
+                case "list":
+                    System.out.println("Task list");
+                    int i = 1;
+                    for (Task task : tasks) {
+                        System.out.println(i++ + ". " + task);
+                    }
+                    break;
+                case "done":
+                    tasks.get(Integer.parseInt(inputs[1]) - 1).markDone();
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(tasks.get(Integer.parseInt(inputs[1]) - 1));
+                    break;
+                default:
+                    tasks.add(new Task(String.join(" ",inputs)));
+                    System.out.println("added: " + String.join(" ",inputs));
             }
-            input = sc.nextLine();
+            inputs = sc.nextLine().split(" ",2);
+            command = inputs[0];
         }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("--------------------------------");
     }
 }
 
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone(){
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[\u2713]" : "[\u2718]"); //return tick or X symbols
+    }
+
+    @Override
+    public String toString(){
+        return getStatusIcon() + " " + description;
+    }
+
+}
