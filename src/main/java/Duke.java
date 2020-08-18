76897b03ecfcecc598b4118b6db72d2b0b9f98e2
ngzhenteng <ngzhenteng@gmail.com>
2020-08-18 14:26:07 +0800

    Level 2. Add, List
    Complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,28 +11,33 @@ public class Duke {
         }
     }
 
+
+    // Note that all the outputs are formatted with two spaces before.
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
         Scanner sc = new Scanner(System.in);
+        ArrayList tempStorage = new ArrayList();
         // welcome message
         horiLine(60);
         System.out.println("  Hello! I'm IntelliGent!\n  What can I do for you?");
         horiLine(60);
-        while (sc.hasNext()) {
-            String nextInput = sc.next();
+        while (sc.hasNextLine()) {
+            String nextInput = sc.nextLine();
             if (nextInput.equals("bye")) {
                 horiLine(60);
                 System.out.println("  Bye. Hope to see you again soon!");
                 horiLine(60);
                 sc.close();
                 break;
+            } else if (nextInput.equals("list")) {
+                horiLine(60);
+                for (int i = 0; i < tempStorage.size(); i++) {
+                    System.out.println("  " + (i+1) + ". " + tempStorage.get(i));
+                }
+                horiLine(60);
             } else {
                 horiLine(60);
-                System.out.println("  " + nextInput);
+                tempStorage.add(nextInput);
+                System.out.println("  added: " + nextInput);
                 horiLine(60);
             }
         }
