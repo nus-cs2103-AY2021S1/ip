175c37b5b37f50822019255f78d558893ae979b8
Tian Fang <tianfangsg@gmail.com>
2020-08-18 11:04:55 +0800

    Modify Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,21 @@
 public class Task {
-    private String description;
+    protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        isDone = true;
     }
 
     public String toString() {
-        return description;
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
