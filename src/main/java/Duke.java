88d9af741084c5908f7631d08d7d1e416836039d
WhiteLio <lio.lim@yahoo.com>
2020-08-18 21:46:39 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,25 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+class Task {
+    String name;
+    int id;
+    boolean done = false;
+
+    static int count = 0;
+
+    public Task(String name) {
+        this.name = name;
+        count++;
+        this.id = count;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". [" + (done ? "✓" : "✗") + "] " + name;
+    }
+}
+
 public class Duke {
     public static void main(String[] args) {
         // startup
         System.out.println("hi");
         Scanner s = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         // main loop
+        mainLoop:
         while (true) {
             String input = s.next();
-            if (input.equals("bye")){
-                break;
-            } else if (input.equals("list")) {
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i+1) + ". " + list.get(i));
-                }
-            } else {
-                list.add(input);
-                System.out.println("added: " + input);
+            switch (input) {
+                case "bye":
+                    break mainLoop;
+                case "list":
+                    for (Task task : list) {
+                        System.out.println(task);
+                    }
+                    break;
+                case "done":
+                    Task task = list.get(s.nextInt() - 1);
+                    task.done = true;
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(task);
+                    break;
+                default:
+                    list.add(new Task(input));
+                    System.out.println("added: " + input);
+                    break;
             }
         }
 
