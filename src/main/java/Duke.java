489bbc5cdffe4c93c5496df1aa814c49254f625b
shadowezz <wenjie.cao@hotmail.com>
2020-08-18 11:44:06 +0800

    Implemented add and list tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    public static ArrayList<Task> taskList = new ArrayList<>();
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,15 +17,36 @@ public class Duke {
             if (command.equals("bye")) {
                 printMessage("Bye. Hope to see you again soon!");
                 return ;
-            } else {
-                printMessage(command);
+            } else if (command.equals("list")) {
+                listTasks();
+            }
+            else {
+                addTask(command);
             }
 
         }
     }
 
     public static void printMessage(String message) {
-        System.out.println("    ___________________________\n" +
-                "     " + message + "\n    ___________________________");
+        System.out.println("    ______________________________________________________\n" +
+                "     " + message + "\n    ______________________________________________________");
+    }
+
+    public static void addTask(String task) {
+        Task newTask = new Task(task);
+        taskList.add(newTask);
+        printMessage("added: " + newTask);
+    }
+
+    public static void listTasks() {
+        String output = "";
+        int taskLen = taskList.size();
+        for (int i = 0; i < taskLen; i++) {
+            output += String.format("%d. %s", i + 1, taskList.get(i));
+            if (i != taskLen - 1) {
+                output += "\n     ";
+            }
+        }
+        printMessage(output);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+    String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
