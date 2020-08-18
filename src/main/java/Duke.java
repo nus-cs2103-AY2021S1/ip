3efa85d0b5c9bc44956e11a5a12788d4b719fcf2
Tan Chia Qian <tanchiaqian@Tans-MacBook-Pro.local>
2020-08-18 21:20:36 +0800

    Level-1 with layout adjusted

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String tab = "  ";
         String lineBreaker = "__________________________________";
-        String greet = "Hello! I'm Duke" + "\n" +"What can I do for you?";
+        String greet = "Hello! I'm Duke" + "\n" + tab + "What can I do for you?";
         String end = "Bye. Hope to see you again soon!";
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(lineBreaker);
-        System.out.println(greet);
-        System.out.println(lineBreaker);
+        System.out.println(tab + lineBreaker);
+        System.out.println(tab + greet);
+        System.out.println(tab + lineBreaker);
 
         while(sc.hasNext()) {
             String input = sc.next();
-            System.out.println(lineBreaker);
+            System.out.println(tab + lineBreaker);
             if(input.equals("bye")) {
-                System.out.println(end);
-                System.out.println(lineBreaker);
+                System.out.println(tab + end);
+                System.out.println(tab + lineBreaker);
                 break;
             } else {
-                System.out.println(input);
-                System.out.println(lineBreaker);
+                System.out.println(tab + input);
+                System.out.println(tab + lineBreaker);
             }
         }
     }
