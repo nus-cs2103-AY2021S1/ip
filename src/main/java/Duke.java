c9d94d9372a1ed6618ea0c2343806f1af5565565
Lim Jun Jie <ljj98855221@hotmail.com>
2020-08-18 11:58:50 +0800

    Added the ability to mark tasks as done, Added new Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         IntroModule.display();
+
         StringIdentifier strIden = new StringIdentifier();
         Scanner sc = new Scanner(System.in);
 
--- src/main/java/StringIdentifier.java
+++ src/main/java/StringIdentifier.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class StringIdentifier {
     private static boolean isProgramRunning = true;
-    private static List<String> lst = new ArrayList<>();
+    private static List<Task> lst = new ArrayList<>();
 
     public boolean isRunning() {
         return this.isProgramRunning;
@@ -13,25 +13,45 @@ public class StringIdentifier {
         System.out.println("    ____________________________________________________________");
         if (str.equals("bye")) {
             close();
+
         } else if (str.equals("list")) {
             displayList();
+
+        } else if (str.substring(0, 5).equals("done ")) {
+            int length = str.length();
+            String index = str.substring(5, length);
+            markDone(Integer.parseInt(index));
+
         } else {
-            store(str);
+            Task newTask = new Task(str);
+            store(newTask);
         }
+
         System.out.println("    ____________________________________________________________\n");
     }
 
-    public void store(String str) {
-        this.lst.add(str);
-        System.out.println("     added: " + str);
+    public void store(Task task) {
+        this.lst.add(task);
+        System.out.println("     added: " + task.getDescription());
     }
 
     public void displayList() {
         int size = this.lst.size();
+        System.out.println("     Here are the tasks in your list:");
+
         int index = 1;
         for (int i = 0; i < size; i++) {
-            System.out.println("     " + index + ". " + this.lst.get(i));
+            System.out.println("     " + index + "." + this.lst.get(i));
+            index ++;
         }
+    }
+
+    public void markDone(int index) {
+        int realIndex = index - 1;
+        Task taskSubject = this.lst.get(realIndex);
+        taskSubject.markAsDone();
+        System.out.println("     Nice! I've marked this task as done:\n"
+                         + "       " + taskSubject);
     }
 
     public void close() {
