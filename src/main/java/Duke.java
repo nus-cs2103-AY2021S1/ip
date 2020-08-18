1d1e6a37ef241655b052a4cbee3efc5a6773f5c0
jzwoo <jianzhe@Woos-MacBook-Pro.local>
2020-08-18 21:17:44 +0800

    Implemented Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,18 +9,53 @@ public class Duke {
         String current = scanner.nextLine();
         List<String> storedText = new ArrayList<>();
 
-        while (!current.equals("bye")) {
-            if (!current.equals("list")) {
-                storedText.add(current);
+        while (isNotTerminateCommand(current)) {
+            if (!isListCommand(current) && !isDoneCommand(current)) {
+                storedText.add("[✗] " +  current);
                 System.out.println("added: " + current);
+            } else if (isDoneCommand(current)) {
+            //Mark item
+
+                //Getting the index
+                String[] parts = current.split(" ");
+                int index = Integer.parseInt(parts[1]) - 1;
+
+                if (index >= storedText.size() || index < 0) {
+                    System.out.println("Sorry, invalid command!");
+                } else {
+                    storedText = markDone(storedText, index);
+                    System.out.println("Nice! I've marked this task as done:" + "\n" + storedText.get(index));
+                }
             } else {
                 System.out.println(readList(storedText));
             }
-            current = scanner.nextLine();
+                current = scanner.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
         scanner.close();
+    }
+
+    public static boolean isDoneCommand(String input) {
+        String[] parts = input.split(" ");
+        return parts[0].equals("done");
+    }
+
+    public static boolean isListCommand(String input) {
+        return input.equals("list");
+    }
+
+    public static boolean isNotTerminateCommand(String input) {
+        return !input.equals("bye");
+    }
+
+    public static List<String> markDone(List<String> list, int position) {
+        String doneTask = list.get(position);
+        String[] parts = doneTask.split(" ", 2);
+        String markDone = "[✓] " + parts[1];
+
+        list.set(position, markDone);
+        return list;
     }
 
     public static String readList(List<String> list) {
