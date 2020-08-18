a1f5101025d689527aae2aedfd5c77a893b9c28e
jzwoo <jianzhe@Woos-MacBook-Pro.local>
2020-08-18 17:55:36 +0800

    Implemented Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Hi! I'm Duke" + "\n" + "What can I do for you?");
+      String current = scanner.nextLine();
+
+      while (!current.equals("bye")) {
+          System.out.println(current);
+          current = scanner.nextLine();
+      }
+
+      System.out.println("Bye. Hope to see you again!");
+      scanner.close();
     }
 }
