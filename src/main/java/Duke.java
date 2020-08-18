d65af9fb74c3e3a6ee2820119deb64a3a96947fb
zhaolingshan <e0425857@u.nus.edu>
2020-08-18 12:35:21 +0800

    level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,6 +28,47 @@ public class Duke {
         }
     }
 
+    public static class Deadline extends Task {
+
+        protected String by;
+
+        public Deadline(String description, String by) {
+            super(description);
+            this.by = by;
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + "(by:" + by + ")";
+        }
+    }
+
+    public static class Event extends Task {
+        protected String at;
+
+        public Event(String description, String at) {
+            super(description);
+            this.at = at;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + "(at:" + at + ")";
+        }
+    }
+
+    public static class Todo extends Task {
+
+        public Todo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+
     public static void readUserInput() {
         Scanner sc = new Scanner(System.in);
         String display = sc.nextLine();
@@ -45,11 +86,39 @@ public class Duke {
             }
             readUserInput();
 
-        } else if (display.substring(0, 4).equals("done")) {
-            int index = Integer.parseInt(display.substring(5, display.length()));
+        } else if (display.startsWith("done")) {
+            int index = Integer.parseInt(display.substring(5));
             Task task = taskList.get(index - 1);
             task.markAsDone();
             System.out.println("Nice! I've marked this task as done:\n" + task);
+            readUserInput();
+
+        } else if (display.startsWith("todo")) {
+            String description = display.substring(5);
+            Todo todo = new Todo(description);
+            taskList.add(todo);
+            System.out.println("Got it. I've added this task:\n" + todo + "\n"
+                    + "Now you have " + taskList.size() + " tasks in the list.");
+            readUserInput();
+
+        } else if (display.startsWith("deadline")) {
+            int index = display.indexOf("/");
+            String description = display.substring(9, index);
+            String deadlineDate = display.substring(index + 3);
+            Deadline deadline = new Deadline(description, deadlineDate);
+            taskList.add(deadline);
+            System.out.println("Got it. I've added this task:\n" + deadline + "\n"
+                    + "Now you have " + taskList.size() + " tasks in the list.");
+            readUserInput();
+
+        } else if (display.startsWith("event")) {
+            int index = display.indexOf("/");
+            String description = display.substring(6, index);
+            String eventDate = display.substring(index + 3);
+            Event event = new Event(description, eventDate);
+            taskList.add(event);
+            System.out.println("Got it. I've added this task:\n" + event + "\n"
+                    + "Now you have " + taskList.size() + " tasks in the list.");
             readUserInput();
 
         } else {
