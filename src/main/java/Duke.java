790df555cd37b20ea622ee9cd69b3794a944b5a0
jayarengam <jaya.yman@gmail.com>
2020-08-18 23:22:32 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    static Scanner sc = new Scanner(System.in);
+    private static Scanner sc = new Scanner(System.in);
+    private static ArrayList<String> entryList = new ArrayList<String>();
+
+    static String line = "    ____________________________________________________________\n";
+
+    public static void printList() {
+        int counter = 0;
+
+        System.out.printf(line);
+
+        for (String s: entryList) {
+            counter++;
+            System.out.printf("     %d. %s%n", counter, s);
+        }
+
+        System.out.println(line);
+    }
 
     public static void main(String[] args) {
         /**
@@ -12,7 +29,6 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
          System.out.println("Hello from\n" + logo);
          **/
-        String line = "    ____________________________________________________________\n";
         String welcome = line + "     Hello! I'm Cartona\n" +
                          "     What can I do for you?\n" + line;
 
@@ -21,11 +37,17 @@ public class Duke {
         String nextInput = "";
         while (true) {
             nextInput = sc.nextLine();
+
             if (nextInput.equals("bye")) {
                 break;
+            } else if (nextInput.equals("list")) {
+                printList();
+                continue;
             }
-            String echo = line + "     " + nextInput + "\n" + line;
+
+            String echo = line + "     added: " + nextInput + "\n" + line;
             System.out.println(echo);
+            entryList.add(nextInput);
         }
 
         String goodbye = line + "     Bye. Hope to see you again soon!\n" + line;
