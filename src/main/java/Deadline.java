727962ed5775d7e630ca60622e8989d421179fa8
Nahoyhp <phyohan@hotmail.com>
2020-08-18 14:35:55 +0800

    create Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class Deadline extends Task {
+    private String date;
+
+    public Deadline(String name, String date) {
+        super(name);
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D]%s (by: %s)", super.toString(), this.date);
+    }
+}
