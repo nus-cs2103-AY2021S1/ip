385cf14abbd9e7b61082db04922b88ff57c66a44
Yuxi <liuyuxi.yx@gmail.com>
2020-08-18 12:58:09 +0800

    edit level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,53 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static String linePrinter() {
-        return "\n----------------------------\n";
+        return ("\n--------------------------------------------------------\n").replaceAll("(?m)^", "\t");
+    }
+
+    public static String start() {
+        String s = ("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        return linePrinter() +
+                s.replaceAll("(?m)^", "\t") +
+                linePrinter();
     }
 
     public static void main(String[] args) {
         String endCommand = "bye";
-        System.out.println((linePrinter() +
-                "Hello! I'm Duke\n" +
-                "What can I do for you?" +
-                linePrinter()).replaceAll("(?m)^", "\t"));
+        String listCommand = "list";
+        ArrayList<String> ls = new ArrayList<String>();
+
+        System.out.println(start());
 
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
         while(!s.equals(endCommand)) {
-            System.out.println((linePrinter() +
-                    s +
-                    linePrinter()).replaceAll("(?m)^", "\t"));
-            s = in.nextLine();
+            if (s.equals(listCommand)) {
+                System.out.println(linePrinter());
+                for (String obj : ls) {
+                    System.out.println(
+                            ((ls.indexOf(obj) + 1) + ". " +
+                            obj).replaceAll("(?m)^", "\t"));
+                }
+                System.out.println(linePrinter());
+                s = in.nextLine();
+            } else {
+                ls.add(s);
+                String thing = "added: " + s;
+                System.out.println(linePrinter() +
+                        thing.replaceAll("(?m)^", "\t") +
+                        linePrinter());
+                s = in.nextLine();
+            }
         }
+
         if (s.equals(endCommand)) {
-            System.out.println((linePrinter() +
-                    "Bye. Hope to see you again soon!" +
-                    linePrinter()).replaceAll("(?m)^", "\t"));
+            String byetext = "Bye. Hope to see you again soon!";
+            System.out.println(linePrinter() +
+                    byetext.replaceAll("(?m)^", "\t") +
+                    linePrinter());
         }
     }
 }
