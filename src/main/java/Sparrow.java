15756cb2c7de01771213e8b064ac1c49f414a1c9
Jonathan Foo <jonathanfoocy@gmail.com>
2020-08-19 15:06:02 +0800

    Change welcome message, add reply method

---
+++ src/main/java/Sparrow.java
@@ -0,0 +1,21 @@
+public class Sparrow {
+    public static void main(String[] args) {
+        String welcome = "  _  _ _   ___ _                    \n" +
+                " | || (_) |_ _( )_ __               \n" +
+                " | __ | |  | ||/| '  \\              \n" +
+                " |_||_|_| |___| |_|_|_|             \n" +
+                " / __|_ __  __ _ _ _ _ _ _____ __ __\n" +
+                " \\__ \\ '_ \\/ _` | '_| '_/ _ \\ V  V /\n" +
+                " |___/ .__/\\__,_|_| |_| \\___/\\_/\\_/ \n" +
+                "     |_|                            ";
+        System.out.println(welcome);
+        reply("How can I help ye?");
+    }
+
+    public static void reply(String message) {
+        System.out.println("    ________________________________________");
+        System.out.println("      " + message);
+        System.out.println("    ________________________________________");
+    }
+
+}
