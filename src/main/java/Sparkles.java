172b40de477283963483df4419766b0e571f1ca5
kunnan <hongkunnan1997@gmail.com>
2020-08-20 01:02:37 +0800

    Handles exceptions for empty descriptions in input

--- src/main/java/Sparkles.java
+++ src/main/java/Sparkles.java
@@ -17,7 +17,7 @@ public class Sparkles {
         } else if (lowerCase.startsWith("done")) {
             markAsDone(str);
         } else if (lowerCase.equals("bye")) {
-            exit(str);
+            exit();
         } else {
             addToList(str);
         }
@@ -26,52 +26,86 @@ public class Sparkles {
     }
 
     private void showList() {
-        for(int i = 1; i < storage.size() + 1; i++) {
-            Task task = storage.get(i - 1);
-            task.printTask(i);
+        if (storage.size() == 0) {
+            print("     List is Empty.");
+        } else {
+            for (int i = 1; i < storage.size() + 1; i++) {
+                Task task = storage.get(i - 1);
+                task.printTask(i);
+            }
         }
     }
 
     private void markAsDone(String str) {
-        int index = Integer.parseInt(String.valueOf(str.charAt(5)));
-        Task task = storage.get(index - 1);
-        task.markAsDone();
-        print("     Nice! I have marked this task as done :-)");
+        int index = -1;
+        try {
+            index = Integer.parseInt(String.valueOf(str.charAt(5)));
+            Task task = storage.get(index - 1);
+            task.markAsDone();
+            print("     Nice! I have marked this task as done :-)");
+        } catch (Exception ex) {
+            print("     OOPS!! Task in the list to be marked as done is not specified!");
+        }
     }
 
-    private void exit(String str) {
+    private void exit() {
         print("     Bye. Hope to see you again!");
         printDash();
         System.exit(0);
     }
 
     private void addToList(String str) {
-        print("     Got it. I've added this task");
-
         String lowerCase = str.toLowerCase();
 
-        String[] arr = null;
+        String[] arr;
         String desc = null;
-        Task task = null;
+        Task task;
         if (lowerCase.startsWith("deadline")) {
             arr = str.split(" /by ");
-            desc = arr[0];
-            String by = arr[1];
-            task = new Deadline(desc, by);
+            String by = null;
+
+            try {
+                desc = arr[0];
+                by = arr[1];
+                task = new Deadline(desc, by);
+                print("     Got it. I've added this task");
+                task.printTask();
+                storage.add(task);
+                printListSize();
+            } catch (Exception ex) {
+                print("     OOPS!! The description and deadline of a Deadline cannot be empty!");
+            }
+
         } else if (lowerCase.startsWith("event")) {
             arr = str.split(" /at ");
-            desc = arr[0];
-            String at = arr[1];
-            task = new Event(desc, at);
+            String at = null;
+
+            try {
+                desc = arr[0];
+                at = arr[1];
+                task = new Event(desc, at);
+                print("     Got it. I've added this task");
+                task.printTask();
+                storage.add(task);
+                printListSize();
+            } catch (Exception ex) {
+                print("     OOPS!! The description and time of an Event cannot be empty!");
+            }
+
         } else if ((lowerCase.startsWith("todo"))) {
-            desc = str.substring(5).trim();
-            task = new Todo(desc);
+            try {
+                desc = str.substring(5).trim();
+                task = new Todo(desc);
+                print("     Got it. I've added this task");
+                task.printTask();
+                storage.add(task);
+                printListSize();
+            } catch (Exception ex) {
+                print("     OOPS!! The description of a todo cannot be empty!");
+            }
         } else {
-            task = new Task(str);
+            print("     Task need to be more specific!");
         }
-        task.printTask();
-        storage.add(task);
-        printListSize();
     }
 
     private void printListSize() {
