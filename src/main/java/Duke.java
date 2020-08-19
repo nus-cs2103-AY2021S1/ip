a9a105e5d7db079ac5098f4eabb26883d21d9cc5
Joe <jteddysanjaya@gmail.com>
2020-08-19 16:24:40 +0800

    Add, List
    User can add task. List will return all the tasks that have been added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
-    public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.activate();
-    }
+
+    List<String> tasks = new ArrayList<>();
 
     private void activate() {
         Scanner sc = new Scanner(System.in);
 
-        String greeting = "Hello! I'm Duke\nWhat can I do for you?\n";
-        System.out.print(greeting + "\nMe: ");
+        String greeting = "Hello! I'm Duke\nWhat can I do for you?\n\nMe: ";
+        System.out.print(greeting);
+
         String input = "";
 
         while (!input.equals("bye") && sc.hasNextLine()) {
             input = sc.nextLine();
-            if (input.equals("bye")) {
-                quit();
-            } else {
-                echo(input);
-            }
+            reply(input);
+        }
+    }
+
+    private void reply(String input) {
+        if (input.equals("bye")) {
+            quit();
+        } else if (input.equals("list")) {
+            showTasks();
+        } else {
+            add(input);
         }
     }
 
@@ -27,9 +34,22 @@ public class Duke {
         System.out.println("Duke: Adios!\n");
     }
 
-    private void echo(String input) {
-        System.out.println("Duke: " + input + "\n");
-        System.out.print("Me: ");
+    private void showTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            String message = String.valueOf(i + 1) + ". " + tasks.get(i);
+            System.out.println(message);
+        }
+        System.out.print("\nMe: ");
     }
 
+    private void add(String input) {
+        tasks.add(input);
+        System.out.println("Duke: Added '" + input + "' to list of tasks");
+        System.out.print("\nMe: ");
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.activate();
+    }
 }
