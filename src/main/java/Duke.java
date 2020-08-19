d55252b522d5cd40591fba8b9df8206402207404
yu-ming-chen <chen.yuming.mao@gmail.com>
2020-08-19 18:33:26 +0800

    Level-5 completed, added DukeException class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,22 +18,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("What can I do for you mateeee?");
+        System.out.println("What can I do for you mate?");
     }
-
+    public void saySomthing(String str) {
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println(str);
+        System.out.println("------------------------------------------------------------------------");
+    }
     public void echo(String input) {
-        System.out.println("------------------------------------");
-        System.out.println(input);
-        System.out.println("------------------------------------");
+        this.saySomthing(input);
     }
 
     public void byeMessage() {
-        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-");
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-");
         System.out.println("Bye. Hope to see you again soon!!!");
-        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-");
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-");
     }
 
     public void run() {
+        sayHi();
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
             String str = sc.nextLine();
@@ -44,55 +47,107 @@ public class Duke {
             } else if (keyWord.equals("list")) {
                 showList();
             } else if(keyWord.equals("todo")) {
-                Todo task = new Todo(str.substring(5));
-                addToList(task);
-            } else if(keyWord.equals("deadline") && str.contains("/by")){
-                String holder[] = str.split("deadline")[1].split("/by ");
-                String description = holder[0].trim();
-                String by = holder[1].trim();
-                Deadline task = new Deadline(description, by);
-                addToList(task);
-            } else if (keyWord.equals("event") && str.contains("/at")){
-                String holder[] = str.split("event")[1].split("/at ");
-                String description = holder[0].trim();
-                String at = holder[1].trim();
-                Event task = new Event(description, at);
-                addToList(task);
+                addToDo(str);
+            } else if(keyWord.equals("deadline")){
+                addDeadline(str);
+            } else if (keyWord.equals("event")){
+                addEvent(str);
             } else if(keyWord.equals("done")) {
-                int index = Character.getNumericValue(str.charAt(5));
-                completeTask(index);
+                completeTask(str);
+            } else {
+                try {
+                    throw new DukeException("☹ OOPS!!! wait..... I don't understand you order my sir.");
+                } catch (DukeException e) {
+                    saySomthing(e.getMessage());
+                }
             }
         }
         byeMessage();
     }
 
+    public void addDeadline(String str) {
+        try {
+            if (str.trim().length() == 8) {
+                throw new DukeException("☹ OOPS!!! Check deadline formatting, include description and /by.");
+            } else if (!str.contains("/by")) {
+                throw new DukeException("☹ OOPS!!! Check deadline formatting, include /by.");
+            }
+            String holder[] = str.split("deadline")[1].split("/by ");
+            String description = holder[0].trim();
+            String by = holder[1].trim();
+            Deadline task = new Deadline(description, by);
+            addToList(task);
+        } catch (DukeException e) {
+            this.saySomthing(e.getMessage());
+        }
+    }
+
+    public void addToDo(String str) {
+        try {
+            if (str.trim().length() == 4) {
+                throw new DukeException("☹ OOPS!!! Check todo formatting, include description");
+            }
+            Todo task = new Todo(str.substring(5));
+            addToList(task);
+        } catch (DukeException e) {
+            this.saySomthing(e.getMessage());
+        }
+    }
+
+    public void addEvent(String str) {
+        try {
+            if (str.trim().length() == 5) {
+                throw new DukeException("☹ OOPS!!! Check event formatting, include description and /at.");
+            } else if (!str.contains("/at")) {
+                throw new DukeException("☹ OOPS!!! Check event formatting, include /at.");
+            }
+            String holder[] = str.split("event")[1].split("/at ");
+            String description = holder[0].trim();
+            String at = holder[1].trim();
+            Event task = new Event(description, at);
+            addToList(task);
+        } catch (DukeException e) {
+            this.saySomthing(e.getMessage());
+        }
+    }
+
+    public void completeTask(String str) {
+        try {
+            if (str.trim().length() == 4) {
+                throw new DukeException("☹ OOPS!!! Check done formatting, include which task to complete.");
+            } else if (Character.getNumericValue(str.charAt(5)) > list.size()) {
+                throw new DukeException("☹ OOPS!!! Task not in the list");
+            }
+            int index = Character.getNumericValue(str.charAt(5));
+            completeTask(index);
+        } catch (DukeException e) {
+            this.saySomthing(e.getMessage());
+        }
+    }
+
+
     public void completeTask(int index) {
         list.get(index-1).markAsDone();
-        System.out.println("------------------------------------");
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println(list.get(index-1).toString());
-        System.out.println("------------------------------------");
+        saySomthing("Nice! I've marked this task as done: \n" + list.get(index-1).toString());
     }
 
     public void addToList(Task task) {
         list.add(task);
-        System.out.println("------------------------------------");
-        System.out.println("Got it. I've added this task:");
-        System.out.println(task.toString());
-        System.out.println(String.format("Now you have %d tasks in the list.", list.size()));
-        System.out.println("------------------------------------");
-
+        saySomthing("Got it. I've added this task: \n" + task.toString() + "\n" + String.format("Now you have %d tasks in the list.", list.size()));
     }
     public void showList() {
-        System.out.println("------------------------------------");
+        String print = "";
         for (int i = 0; i < list.size(); i++) {
-            System.out.println(String.format("%d. ", i+1) + list.get(i));
+            if (i == list.size()-1) {
+                print += String.format("%d. ", i+1) + list.get(i);
+            } else {
+                print += String.format("%d. ", i + 1) + list.get(i) + "\n";
+            }
         }
-        System.out.println("------------------------------------");
+        saySomthing(print);
     }
     public static void main(String[] args) {
         Duke bot = new Duke();
-        bot.sayHi();
         bot.run();
     }
 }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class DukeException extends Exception {
+    public DukeException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
