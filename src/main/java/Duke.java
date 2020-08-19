ec57d7361885fcbc4d54f7d1781217abc673da89
jeminsieow <jemin.sieow@prepr.org>
2020-08-19 23:03:54 +0800

    Add Event task type

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,29 +4,30 @@ import java.util.Scanner;
 public class Duke {
     static Scanner scanner = new Scanner(System.in);
     static ArrayList<Task> taskList = new ArrayList<>();
-    final static String line = "    ____________________________________________________________\n";
 
     public static void main(String[] args) {
-        greeting();
+        showGreeting();
         String[] inputArr = getInputArr();
         String command = inputArr[0];
-
         while (!command.equals("bye")) {
             switch (command) {
             case "list":
-                list();
+                showList();
                 break;
             case "done":
                 done(inputArr);
                 break;
             case "deadline":
-                addDeadline(inputArr);
+                addTask("deadline", inputArr);
+                break;
+            case "event":
+                addTask("event", inputArr);
                 break;
             }
             inputArr = getInputArr();
             command = inputArr[0];
         }
-        goodbye();
+        showGoodbye();
     }
 
     public static String[] getInputArr() {
@@ -34,68 +35,69 @@ public class Duke {
         return input.split("\\s+");
     }
 
-    public static void greeting() {
-        String message = line
-                + "      Eh what's up\n"
-                + "      What do you want?\n"
-                + line;
-        System.out.println(message);
+    public static void showGreeting() {
+        String message = "      Eh what's up\n"
+                + "      What do you want?";
+        System.out.println(wrapMessage(message));
     }
 
-    public static void goodbye() {
-        String message = line
-                + "     Alright I'll see you around!\n"
-                + line;
-        System.out.println(message);
+    public static void showGoodbye() {
+        String message = "     Alright I'll see you around!";
+        System.out.println(wrapMessage(message));
     }
 
-    public static void list() {
-        String message;
-        message = line;
+    public static void showList() {
+        String message = "    Here are the tasks in your list:\n";
         for (int i = 0; i < taskList.size(); i++) {
             Task task = taskList.get(i);
-            message = message
-                    + "    Here are the tasks in your list:\n"
-                    + "    "
+            message += "    "
                     + (i + 1)
                     + ". "
                     + task
-                    + "\n";
+                    +"\n";
         }
-        message = message + line;
-        System.out.println(message);
+        System.out.println(wrapMessage(message));
     }
 
-    public static void addDeadline(String[] inputArr) {
+    public static void addTask(String type, String[] inputArr) {
         String desc = "";
-        String by = "";
+        String dateTime = "";
+        Task task;
 
         int i = 1;
-        while (!inputArr[i].contains("/")) {
+        System.out.println("inputArr.length = " + inputArr.length);
+        while (!inputArr[i].contains("/") || i < inputArr.length - 1) {
             desc = desc + inputArr[i] + " ";
             i++;
+            System.out.println("i = " + i);
         }
         desc = desc.substring(0, desc.length() - 1);
-        i++;
-        while (i < inputArr.length) {
-            by = by + inputArr[i] + " ";
+
+        if (type.equals("deadline") || type.equals("event")) {
             i++;
+            while (i < inputArr.length) {
+                dateTime = dateTime + inputArr[i] + " ";
+                i++;
+            }
+            dateTime = dateTime.substring(0, dateTime.length() - 1);
         }
-        by = by.substring(0, by.length() - 1);
 
-        Deadline deadline = new Deadline(desc, by);
-        taskList.add(new Deadline(desc, by));
+        if (type.equals("deadline")) {
+            task = new Deadline(desc, dateTime);
+        } else if (type.equals("event")) {
+            task = new Event(desc, dateTime);
+        }
 
-        String message = line
-                + "    Got it. I've added this task:\n"
+        taskList.add(task);
+
+        String message = "    Got it. I've added this task:\n"
                 + "      "
-                + deadline
+                + task
                 + "\n"
                 + "    Now you have "
                 + taskList.size()
-                + " tasks in the list.\n"
-                + line;
-        System.out.println(message);
+                + " tasks in the list.";
+        System.out.println(wrapMessage(message));
     }
 
     public static void done(String[] inputArr) {
@@ -104,12 +106,21 @@ public class Duke {
         i -= 1;
         Task task = taskList.get(i);
         task.markAsDone();
-        String message = line
-                + "      Nice! I've marked this task as done:\n"
+        String message = "      Nice! I've marked this task as done:\n"
                 + "      "
-                + task
+                + task;
+        System.out.println(wrapMessage(message));
+    }
+
+    public static String wrapMessage(String message) {
+        if (message.endsWith("\n")) {
+            message = message.substring(0, message.length() - 1);
+        } // If the message ends with a newline, remove the newline
+
+        String line = "    ____________________________________________________________\n";
+        return line
+                + message
                 + "\n"
                 + line;
-        System.out.println(message);
     }
 }
