5a8470adde4f101e1020d817cadc260e400657a7
lucastai98 <lucastai.98@gmail.com>
2020-08-19 18:24:26 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> myList = new ArrayList<>();
 
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -11,16 +13,23 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(
                 "_______________________________"+
-                "Hello! I'm Duke\n"+
-                "What can I do for you?\n"+
-                "_______________________________"
+                        "Hello! I'm Duke\n"+
+                        "What can I do for you?\n"+
+                        "_______________________________"
         );
         String input = scan.nextLine();
         while(!input.equals("bye")) {
-            System.out.println(
-                    "_______________________________\n" +
-                            input + "\n" +
-                            "_______________________________");
+            if(input.equals("list")){
+                for(int i = 0 ; i <myList.size();i++) {
+                    System.out.println((i + 1) +". "+myList.get(i));
+                }
+            }else {
+                System.out.println(
+                        "_______________________________\n" +
+                                "added: " + input + "\n" +
+                                "_______________________________");
+                myList.add(input);
+            }
             input = scan.nextLine();
 
         }
