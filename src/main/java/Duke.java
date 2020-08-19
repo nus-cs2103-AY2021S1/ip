e833d847be696020a9ecf4a1089c4a17e82ca4a5
chuyue <chuyue.liu@gmail.com>
2020-08-19 20:45:41 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("hi! im conundrum boy :)");
+        List<String> things = new ArrayList<String>();
         Scanner input = new Scanner(System.in);
 
-        String inn = "";
-        while (!inn.equals("bye")) {
-            System.out.println(inn);
+        String inn;
+        label:
+        while (true) {
             inn = input.nextLine();
+            switch (inn) {
+                case "bye":
+                    break label;
+                case "":
+                    continue;
+                case "list":
+                    for (int i = 1; i <= things.size(); i++) {
+                        System.out.println(i + ". " + things.get(i - 1));
+                    }
+                    break;
+                default:
+                    System.out.println("added: " + inn);
+                    things.add(inn);
+                    break;
+            }
+
         }
         System.out.println("bye bye!");
 
