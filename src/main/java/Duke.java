4d4987a6371062419879913d5528a02e174cd036
kaichaoang <kaichaoa@gmail.com>
2020-08-19 20:05:34 +0800

    Update Duke input types and exceptions
    Duke accepts any form of input
    
    Duke might be buggy when faced with unsupported input formats.
    
    Let's narrow Duke's input validity based on the given keywords to the
    user via the help keyword and inform users should there be any invalid
    inputs via exceptions
    
    This can help the user plan their list in a more systematic structure
    and minimise bug experienced.
    
    Let's,
    - restrict our keywords to add tasks only if supported keywords are used
     (as shown by inputting "help") .
    - utilise Duke's customised exception classes wherever applicable to
      enhance UI

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
@@ -16,116 +14,178 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         List<Task> pastInputs = new ArrayList<>();
+
         boolean terminated = false;
 
         while (!terminated && scanner.hasNext()) {
-            String userInput = scanner.nextLine();
-            //determining user input type via the first word
-            String[] splitInput = userInput.split(" ");
+            try {
+                String userInput = scanner.nextLine();
+                //determining user input type via the first word
+                String[] splitInput = userInput.split(" ");
+                String keyWord = getKeyWord(splitInput[0]);
 
-            if (splitInput[0].equals("bye")) {
-                terminated = true;
-                System.out.println("Duke says: Goodbye and have a nice day! :D");
-                scanner.close();
-            } else if (splitInput[0].equals("help")) {
-                System.out.println("list: displays a sequential view of past inputs\n" +
-                        "done <task number>: denotes a task as done by checking it\n" +
-                        "deadline <description> /by <date/time>: adds a deadline with desired date/time\n" +
-                        "event <description> /at <date/time>: adds an event with desired date/time\n" +
-                        "todo <description>: adds a todo task\n" +
-                        "bye: terminates program");
-            } else if (splitInput[0].equals("list")){
-                if (pastInputs.size() == 0) {
-                    System.out.println("Duke says: No past inputs found");
-                } else {
-                    System.out.println("Here are your tasks:");
-                    for (int i = 1; i <= pastInputs.size(); i++) {
-                        System.out.println(i + ". " + pastInputs.get(i - 1));
+                if (keyWord.equals("bye")) {
+                    terminated = true;
+                    System.out.println("Duke says: Goodbye and have a nice day! :D");
+                    scanner.close();
+                } else if (keyWord.equals("help")) {
+                    System.out.println("list: displays a sequential view of past inputs\n" +
+                            "done <task number>: denotes a task as done by checking it\n" +
+                            "deadline <description> /by <date/time>: adds a deadline with desired date/time\n" +
+                            "event <description> /at <date/time>: adds an event with desired date/time\n" +
+                            "todo <description>: adds a todo task\n" +
+                            "bye: terminates program");
+                } else if (keyWord.equals("list")) {
+                    if (pastInputs.size() == 0) {
+                        System.out.println("Duke says: No past inputs found");
+                    } else {
+                        System.out.println("Here are your tasks:");
+                        for (int i = 1; i <= pastInputs.size(); i++) {
+                            System.out.println(i + ". " + pastInputs.get(i - 1));
+                        }
+                        System.out.println("If you wish to mark a task as completed, input \"done <task number>\"");
                     }
-                    System.out.println("If you wish to mark a task as completed, input \"done <task number>\"");
-                }
-            } else if (splitInput[0].equals("done")){
-                //checks the formatting of user input
-                if (splitInput.length <= 2) {
-                    try {
-                        int taskNumber = Integer.parseInt(splitInput[1]);
-                        Task doneTask = pastInputs.get(taskNumber - 1);
-                        doneTask.markDone();
-                        pastInputs.set(taskNumber - 1, doneTask);
-                        System.out.println("Duke says: Good Job! I've marked this task as done:");
-                        System.out.println(doneTask);
-                    } catch(NumberFormatException ex) {
+                } else if (keyWord.equals("done")) {
+                    //checks the formatting of user input
+                    if (splitInput.length <= 2) {
+                        try {
+                            int taskNumber = Integer.parseInt(splitInput[1]);
+                            Task doneTask = pastInputs.get(taskNumber - 1);
+                            doneTask.markDone();
+                            pastInputs.set(taskNumber - 1, doneTask);
+                            System.out.println("Duke says: Good Job! I've marked this task as done:");
+                            System.out.println(doneTask);
+                        } catch (NumberFormatException ex) {
+                            pastInputs.add(new Task(userInput));
+                            System.out.println("Duke added into your task list: " + userInput);
+                        } catch (Exception ex) {
+                            System.out.println("Duke says: Please try again with a valid task number");
+                        }
+                    } else {
                         pastInputs.add(new Task(userInput));
-                        System.out.println("Duke added into your task list: " + userInput);
-                    } catch (Exception ex) {
-                        System.out.println("Duke says: Please try again with a valid task number");
+                        System.out.println("Duke added into your task list:\n" + userInput);
+                        System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
                     }
                 } else {
-                    pastInputs.add(new Task(userInput));
-                    System.out.println("Duke added into your task list:\n" + userInput);
+                    String[] data = processInput(splitInput);
+                    if (keyWord.equals("todo")) {
+                        ToDo toDo = new ToDo(data[0]);
+                        pastInputs.add(toDo);
+                        System.out.println("Duke added into your task list:\n" + toDo);
+                    } else if (keyWord.equals("deadline")) {
+                        Deadline deadline = new Deadline(data[0], data[1]);
+                        pastInputs.add(deadline);
+                        System.out.println("Duke added into your task list:\n" + deadline);
+                    } else if (keyWord.equals("event")){
+                        Event event = new Event(data[0], data[1]);
+                        pastInputs.add(event);
+                        System.out.println("Duke added into your task list:\n" + event);
+                    }
                     System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
                 }
-            } else if (splitInput[0].equals("todo")) {
-                String des = "";
-                for (int i = 1; i < splitInput.length; i++) {
-                    des += splitInput[i];
-                    if (i != splitInput.length - 1) {
-                        des += " ";
-                    }
-                }
-                ToDo toDo = new ToDo(des);
-                pastInputs.add(toDo);
-                System.out.println("Duke added into your task list:\n" + toDo);
-                System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
-
-            } else if (splitInput[0].equals("deadline")) {
-                String des = "";
-                String dateAndOrTime = "";
-                boolean toBreak = false;
-                for (int i = 1; i < splitInput.length; i++) {
-                    if (splitInput[i].equals("/by")) {
-                        //sets the breaking point of input
-                        toBreak = true;
-                    } else if (!toBreak) {
-                        des += splitInput[i] + " ";
-                    } else {
-                        dateAndOrTime += splitInput[i];
-                        if (i != splitInput.length - 1) {
-                            dateAndOrTime += " ";
-                        }
-                    }
-                }
-                Deadline deadline = new Deadline(des, dateAndOrTime);
-                pastInputs.add(deadline);
-                System.out.println("Duke added into your task list:\n" + deadline);
-                System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
-            } else if (splitInput[0].equals("event")) {
-                String des = "";
-                String dateAndOrTime = "";
-                boolean toBreak = false;
-                for (int i = 1; i < splitInput.length; i++) {
-                    if (splitInput[i].equals("/at")) {
-                        //sets the breaking point of input
-                        toBreak = true;
-                    } else if (!toBreak) {
-                        des += splitInput[i] + " ";
-                    } else {
-                        dateAndOrTime += splitInput[i];
-                        if (i != splitInput.length - 1) {
-                            dateAndOrTime += " ";
-                        }
-                    }
-                }
-                Event event = new Event(des, dateAndOrTime);
-                pastInputs.add(event);
-                System.out.println("Duke added into your task list:\n" + event);
-                System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
-            } else {
-                pastInputs.add(new Task(userInput));
-                System.out.println("Duke added into your task list:\n" + userInput);
-                System.out.println("You now have " + pastInputs.size() + " task(s) in your list");
+                System.out.println("=========================================================================");
+            } catch (InvalidKeyWordException ex) {
+                System.out.println(ex.getMessage());
+                System.out.println("=========================================================================");
+            } catch (EmptyTaskException ex) {
+                System.out.println(ex.getMessage());
+                System.out.println("=========================================================================");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                System.out.println("=========================================================================");
             }
-            System.out.println("=========================================================================");
         }
     }
+
+    //returns a reserved keyword
+    public static String getKeyWord(String word) throws InvalidKeyWordException {
+        Set<String> reservedKeyWords = new HashSet<>();
+        reservedKeyWords.add("help");
+        reservedKeyWords.add("list");
+        reservedKeyWords.add("done");
+        reservedKeyWords.add("deadline");
+        reservedKeyWords.add("event");
+        reservedKeyWords.add("todo");
+        reservedKeyWords.add("bye");
+
+        if (!reservedKeyWords.contains(word)) {
+            throw new InvalidKeyWordException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+        return word;
+    }
+
+    //returns an array of description and date/time(if applicable)
+    public static String[] processInput(String[] array) throws EmptyTaskException {
+        Set<String> reservedKeyWords = new HashSet<>();
+        reservedKeyWords.add("help");
+        reservedKeyWords.add("list");
+        reservedKeyWords.add("done");
+        reservedKeyWords.add("deadline");
+        reservedKeyWords.add("event");
+        reservedKeyWords.add("todo");
+        reservedKeyWords.add("bye");
+
+        if (array.length <= 1) {
+            if (array[0].equals("event")) {
+                throw new EmptyTaskException("☹ OOPS!!! The description of a event cannot be empty.");
+            } else if (array[0].equals("deadline")) {
+                throw new EmptyTaskException("☹ OOPS!!! The description of a deadline cannot be empty.");
+            } else if (array[0].equals("todo")) {
+                throw new EmptyTaskException("☹ OOPS!!! The description of a todo cannot be empty.");
+            } else {
+                throw new EmptyTaskException("Unknown error!");
+            }
+        } else if (array[0].equals("event")) {
+            String des = "";
+            String dateAndOrTime = "";
+            boolean toBreak = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i].equals("/at")) {
+                    //sets the breaking point of input
+                    toBreak = true;
+                } else if (!toBreak) {
+                    des += array[i] + " ";
+                } else {
+                    dateAndOrTime += array[i];
+                    if (i != array.length - 1) {
+                        dateAndOrTime += " ";
+                    }
+                }
+            }
+            //index 0 is description, index 1 is date/time
+            return new String[] {des, dateAndOrTime};
+
+        } else if (array[0].equals("deadline")) {
+            String des = "";
+            String dateAndOrTime = "";
+            boolean toBreak = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i].equals("/by")) {
+                    //sets the breaking point of input
+                    toBreak = true;
+                } else if (!toBreak) {
+                    des += array[i] + " ";
+                } else {
+                    dateAndOrTime += array[i];
+                    if (i != array.length - 1) {
+                        dateAndOrTime += " ";
+                    }
+                }
+            }
+            //index 0 is description, index 1 is date/time
+            return new String[] {des, dateAndOrTime};
+        } else if (array[0].equals("todo")) {
+            String des = "";
+            for (int i = 1; i < array.length; i++) {
+                des += array[i];
+                if (i != array.length - 1) {
+                    des += " ";
+                }
+            }
+            //index 0 is description
+            return new String[] {des};
+        }
+        return new String[] {};
+    }
+
 }
