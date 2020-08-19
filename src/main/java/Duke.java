796c4155c9b8c3bf6797f11bc324bb7ab1e43df6
Sigmund <sigmundchianasta@gmail.com>
2020-08-19 22:03:38 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,12 +12,23 @@ public class Duke {
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
+                System.out.println("Here's the extent of our list so far:");
                 for (int i = 0; i < taskList.size(); i++) {
                     System.out.println((i + 1) + ". " + taskList.get(i));
                 }
+            } else if (input.startsWith("done")) {
+                try {
+                    int taskIndex = Integer.parseInt(input.split(" ")[1]);
+                    Task markedTask = taskList.get(taskIndex - 1); // shown list is base 1, implemented list is base 0
+                    markedTask.markDone();
+                    System.out.println("Right. This task is now marked as done:" + markedTask);
+                } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                    System.out.println("Hmm? Please mention \"done\" followed by the number of the task we're marking as done.");
+                }
             } else {
-                taskList.add(new Task(input));
-                System.out.println("Fine. I added the following to the list: " + input);
+                Task newTask = new Task(input);
+                taskList.add(newTask);
+                System.out.println("Fine. I added the following to the list: " + newTask);
             }
             input = sc.nextLine();
         }
