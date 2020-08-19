faa451c825e48d36f68b951a26e5adde32f4c9fe
Clara Adora <claraadora@gmail.com>
2020-08-19 13:52:40 +0800

    refactor: add methods to refactor Duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,18 +3,50 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    protected List<Task> tasks;
+    protected Scanner sc;
+
+    public Duke() {
+        this.tasks = new ArrayList<>();
+        this.sc = new Scanner(System.in);
+    }
+
+    public void sayHello() {
         System.out.println("Hello! I'm Clara! :D How may I help you? :)");
+    }
 
-        Scanner sc = new Scanner(System.in);
-        List<Task> tasks = new ArrayList<>();
+    public void sayBye() {
+        System.out.println("\tBye! Have a great day and hope to see you soon! :D");
+    }
 
+    public void addTask(String description) {
+        this.tasks.add(new Task(description, false));
+        System.out.println("\tadded: " + description);
+    }
+
+    public void markTaskAsDone(int index) {
+        String description = this.tasks.get(index).description;
+        Task updatedTask = new Task(description, true);
+
+        this.tasks.set(index, updatedTask);
+
+        System.out.println("Nice! I've marked this task as done: \n\t" + updatedTask);
+    }
+
+    public void displayTasks() {
+        if (tasks.size() > 0) {
+            System.out.println("These are the tasks in your list. Jiayous! :)");
+        } else {
+            System.out.println("You have no task in your list. :D");
+        }
+
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(String.format("\t%d. %s", i+1, this.tasks.get(i).toString()));
+        }
+    }
+
+    public void serveClient() {
+        sayHello();
         while (sc.hasNext()) {
             String command = sc.nextLine();
             String[] splitCommand = command.split(" ");
@@ -22,28 +54,17 @@ public class Duke {
 
             switch(keyword) {
                 case "bye":
-                    System.out.println("\tBye! Have a great day and hope to see you soon! :D");
+                    sayBye();
                     break;
                 case "list":
-                    if (tasks.size() > 0) {
-                        System.out.println("These are the tasks in your list. Jiayous! :)");
-                    } else {
-                        System.out.println("You have no task in your list. :D");
-                    }
-
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(String.format("\t%d. %s", i+1, tasks.get(i).toString()));
-                    }
+                    displayTasks();
                     break;
                 case "done":
                     int index = Integer.parseInt(splitCommand[1])-1;
-                    Task updatedTask = new Task(tasks.get(index).description, true);
-                    tasks.set(index, updatedTask);
-                    System.out.println("Nice! I've marked this task as done: \n\t" + updatedTask);
+                    markTaskAsDone(index);
                     break;
                 default:
-                    tasks.add(new Task(command, false));
-                    System.out.println("\tadded: " + command);
+                    addTask(command);
             }
 
             if (command.equals("bye")) {
