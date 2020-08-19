bf81fb499f67275f930b351498643f7e7701df2d
SONGYI98 <e0392524@u.nus.edu>
2020-08-19 22:57:55 +0800

    Edit Duke's greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________\n";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n\n";
+        String introduction = "Hello! I'm Duke\nWhat can I do for you?\n";
+        System.out.println(divider + logo + introduction + divider);
     }
 }
