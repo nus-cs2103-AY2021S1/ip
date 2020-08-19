c755b5df51e4414f8da02f1deeee11a1a95ae54b
WayneTanKinLun <waynetan2000@gmail.com>
2020-08-19 11:58:34 +0800

    add level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,46 +1,129 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+    public static Task[] tasks = new Task[100];
 
     public static void greeting() {
         String greeting = "Hello! I'm Duke\nWhat can I do for you?";
         System.out.println(greeting);
     }
 
-    public static void printAllTask(Task[] tasks) {
+    public static void printAllTask() {
         int numTask = 0;
         System.out.println("Here are the tasks in your list:");
         while (tasks[numTask] != null) {
-            System.out.println( Integer.valueOf(numTask + 1) + ".[" + tasks[numTask].getStatusIcon() + "] "
-                    + tasks[numTask]);
+            System.out.println(Integer.valueOf(numTask + 1) + "." + tasks[numTask]);
             numTask++;
+        }
+    }
+
+    public static void printAddedTask(Task task) {
+        int len;
+        for(len = 0; len < tasks.length; len++) {
+            if (tasks[len] == null) {
+                break;
+            }
+        }
+        System.out.println("Got it. I've added this task: \n" + task +
+                "\nNow you have " + String.valueOf(len)  + " tasks in the list.");
+    }
+
+    public static String[] removeFirst(String[] arr) {
+        String[] tempArr = new String[arr.length];
+        for (int i = 0; i < arr.length - 1; i++) {
+            tempArr[i] = arr[i + 1];
+        }
+        return tempArr;
+    }
+
+    public static String[] removeAfterWord(String[] arr, String word) {
+        String[] temp = new String[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(word)) {
+                break;
+            } else {
+                temp[i] = arr[i];
+            }
+        }
+        return temp;
+    }
+
+    public static String[] keepAfterWord(String[] arr, String word) {
+        String[] temp = new String[arr.length];
+        int counter = 0;
+        // find position of the word
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(word)) {
+                counter = i;
+                break;
+            }
+        }
+        counter++;
+        for(int i = 0; (counter + i) < arr.length; i++) {
+            temp[i] = arr[counter + i];
+        }
+        return temp;
+    }
+
+    public static String joinString(String[] arr) {
+        String text = arr[0];
+        if (arr.length == 1) {
+            return text;
+        } else {
+            for (int i = 1; i < arr.length && arr[i] != null; i++) {
+                text = text + " " + arr[i];
+            }
+        }
+        return text;
+    }
+
+    public static void addNewTask(String[] task, int pos) {
+        if (task[0].equals("todo")) {
+            String[] modifiedTask = removeFirst(task);
+            tasks[pos] = new Todo(joinString(modifiedTask));
+            printAddedTask(tasks[pos]);
+        } else if (task[0].equals("deadline")) {
+            String[] modifiedTask = removeFirst(task);
+            String[] upper = removeAfterWord(modifiedTask, "/by");
+            String[] lower = keepAfterWord(modifiedTask, "/by");
+            tasks[pos] = new Deadline(joinString(upper), joinString(lower));
+            printAddedTask(tasks[pos]);
+        } else if (task[0].equals("event")) {
+            String[] modifiedTask = removeFirst(task);
+            String[] upper = removeAfterWord(modifiedTask, "/at");
+            String[] lower = keepAfterWord(modifiedTask, "/at");
+            tasks[pos] = new Event(joinString(upper), joinString(lower));
+            printAddedTask(tasks[pos]);
+        } else {
+            System.out.println("error");
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Task[] tasks = new Task[100];
         int numTask = 0;
 
         greeting();
         String input;
         String[] inputArr;
+
         while(true) {
-            input = sc.nextLine();
-            inputArr = input.split(" ");
+            input = sc.nextLine(); // original input line
+            inputArr = input.split(" "); // split input into string array
+
             if (inputArr[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 sc.close();
                 break;
             } else if (inputArr[0].equals("list")) {
-                printAllTask(tasks);
+                printAllTask();
             } else if (inputArr[0].equals("done")) {
-                String counter = inputArr[1];
-                int pos = Integer.parseInt(counter);
-                tasks[pos - 1].markAsDone();
+                int counter =  Integer.parseInt(inputArr[1]);
+                tasks[counter - 1].markAsDone();
             } else {
-                tasks[numTask] = new Task(input);
-                System.out.println("added: " + tasks[numTask]);
+                addNewTask(input.split(" "), numTask);
                 numTask++;
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,6 +18,6 @@ public class Task {
 
     @Override
     public String toString() {
-        return description;
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
