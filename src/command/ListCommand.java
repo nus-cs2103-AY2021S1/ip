a188cccb4fdb7194caf89b00b01de5c3791f7820
Ma-Yueran <mayuerantom@outlook.com>
2020-08-19 21:45:14 +0800

    Add reportCurrentTasks function to DukeFunction.java, Call the function in ListCommand.java

--- src/command/ListCommand.java
+++ src/command/ListCommand.java
@@ -1,7 +1,6 @@
 package command;
 
-import ui.UIPrint;
-import data.DukeData;
+import function.DukeFunction;
 
 public class ListCommand extends Command {
 
@@ -11,17 +10,6 @@ public class ListCommand extends Command {
 
     @Override
     public void execute(String str) {
-        UIPrint.drawLine(UIPrint.star, 50);
-        System.out.println("Current tasks:\n");
-
-        for (int i = 0; i < DukeData.tasks.size(); i++) {
-            System.out.println(i + 1 + ". " + DukeData.tasks.get(i));
-        }
-
-        if (DukeData.tasks.size() == 0) {
-            System.out.println("None");
-        }
-
-        UIPrint.drawLine(UIPrint.star, 50);
+        DukeFunction.reportCurrentTasks();
     }
 }
--- src/function/DukeFunction.java
+++ src/function/DukeFunction.java
@@ -29,6 +29,22 @@ public class DukeFunction {
         UIPrint.drawLine(UIPrint.star, 50);
     }
 
+    public static void reportCurrentTasks() {
+        UIPrint.drawLine(UIPrint.star, 50);
+
+        System.out.println("Current tasks:\n");
+
+        for (int i = 0; i < DukeData.tasks.size(); i++) {
+            System.out.println(i + 1 + ". " + DukeData.tasks.get(i));
+        }
+
+        if (DukeData.tasks.size() == 0) {
+            System.out.println("None");
+        }
+
+        UIPrint.drawLine(UIPrint.star, 50);
+    }
+
     public static void reportNewTask(Task task) {
         UIPrint.drawLine(UIPrint.star, 50);
 
