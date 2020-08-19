2254a7de297569513616ca09d57d1ec506105a24
Christopher Goh <chrisgzf@gmail.com>
2020-08-20 07:36:48 +0800

    feat: Add support for todo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 public class Duke {
     private static final List<Task> savedItems = new ArrayList<>();
 
-    private static void lsHandler() {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < savedItems.size(); ++i) {
-            Task task = savedItems.get(i);
-            System.out.println(i + 1 + ". " + task.getStatus());
-        }
-    }
-
     private static void doneHandler(String userInput) {
         // TODO: input sanitization
         int taskNo = Integer.parseInt(userInput.split(" ")[1]);
@@ -21,6 +13,25 @@ public class Duke {
 
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(taskNo + ". " + task.getStatus());
+    }
+
+    private static void todoHandler(String userInput) {
+        String description = userInput.replaceFirst("todo ", "");
+        Todo todo = new Todo(description);
+        savedItems.add(todo);
+        System.out.println("Got it. I've added this task:");
+        System.out.printf(todo.getStatus());
+    }
+    private static void lsHandler() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < savedItems.size(); ++i) {
+            Task task = savedItems.get(i);
+            System.out.println(i + 1 + ". " + task.getStatus());
+        }
+    }
+
+    private static void printRemainingCount() {
+        System.out.println("You now have " + savedItems.size() + " tasks in the list.");
     }
 
     public static void main(String[] args) {
@@ -41,15 +52,22 @@ public class Duke {
                 case "exit":
                     System.out.println("Bye. Hope to see you again soon!");
                     return;
+                case "todo":
+                    todoHandler(userInput);
+                    printRemainingCount();
+                    break;
                 case "done":
                     doneHandler(userInput);
                     break;
                 case "ls":
                     lsHandler();
+                    printRemainingCount();
                     break;
                 default:
                     savedItems.add(new Task(userInput));
                     System.out.println("added: " + userInput);
+                    printRemainingCount();
+                    break;
             }
         }
     }
