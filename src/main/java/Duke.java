d04f9933e41a2d8958de8970c8f337dbc7fe2559
chan-j-d <chan.j.d.98@gmail.com>
2020-08-20 00:57:30 +0800

    Added static String messages for greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
+
+    private static final String lineBreak = "    ____________________________________________________________";
+    private static final String openingMessage = "Hello! I'm Duke\nWhat can I do for you?";
+    private static final String closingMessage = "Bye. Hope to see you again soon!";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
     }
 }
