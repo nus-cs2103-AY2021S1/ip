d16976d39595a9e3a17adfa69284bdd4b770044c
gsmoon97 <gun6ixxmoon@gmail.com>
2020-08-19 16:40:45 +0900

    Level-2

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,16 +1,30 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Luke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hey there! I'm Luke. How may I help you?");
+        ArrayList<String> todo = new ArrayList<>();
+        System.out.printf("Luke : Hey there! I'm Luke.\nPlease tell me what to add to your to-do list.\nYou : ");
         while (true) {
-            String echo = sc.nextLine();
-            if (echo.equals("bye")) {
-                System.out.println("Oh, are you leaving? Hope to see you soon!");
+            String input = sc.nextLine().toLowerCase();
+            if (input.equals("list")) {
+                String list = "";
+                for (int i = 0; i < todo.size(); i++) {
+                    if (i == 0) {
+                        list += String.format("%d. %s", i + 1, todo.get(i));
+                    } else {
+                        list += String.format("\n%d. %s", i + 1, todo.get(i));
+                    }
+                }
+                System.out.printf("%s\nYou : ", list);
+            } else if (input.equals("bye")) {
+                System.out.println("Luke : Oh, are you leaving? Hope to see you soon!");
                 break;
             } else {
-                System.out.println(echo);
+                todo.add(input);
+                System.out.printf("Luke : Added '%s' to your to-do list.\nYou : ", input);
             }
         }
     }
