ac1345d8b980f630586ae70bca33b394f14e516a
theyifan <zhang.yifan@u.nus.edu>
2020-08-19 23:37:50 +0800

    added level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        String greeting = "Hello I'm Duke, your favourite chatbot! \n"
+                + "   What can I do for you? ";
+        System.out.println(formatPrint(greeting));
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.strip().toLowerCase().equals("bye")) {
+                System.out.println(formatPrint("Bye! Hope to see you again soon! "));
+                break;
+            } else {
+                System.out.println(formatPrint(input));
+            }
+        }
+
+        sc.close();
+    }
+
+    private static String formatPrint(String str) {
+        String divider = "  ---------------------------- \n   ";
+        return divider + str + "\n" + divider;
     }
 }
