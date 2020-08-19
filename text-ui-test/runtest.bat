0d187f1a0a3924f16c9363939320985aa7bf2afd
Lim Jin Feng <ljfof98@gmail.com>
2020-08-20 01:40:36 +0800

    textuitest

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,12 @@ REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
+javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\Task.java
+javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\Deadline.java
+javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\Event.java
+javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\ToDo.java
 javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\Duke.java
+
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
