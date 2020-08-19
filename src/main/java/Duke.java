a071d6de679238fe2526bc07588cbe70358412e2
chan-j-d <chan.j.d.98@gmail.com>
2020-08-20 01:02:13 +0800

    Added methods to process Duke's messages to the desired way

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
 
-    private static final String lineBreak = "    ____________________________________________________________";
-    private static final String openingMessage = "Hello! I'm Duke\nWhat can I do for you?";
+    private static final String lineBreak = "    ____________________________________________________________\n";
+    private static final String preSpacing = "     ";
+    private static final String openingMessage = "Hello! I'm Duke\n" + preSpacing + "What can I do for you?";
     private static final String closingMessage = "Bye. Hope to see you again soon!";
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
     }
+
+    private String processString(String string) {
+        return lineBreak  + preSpacing + string + '\n' + lineBreak;
+    }
+
+    private String getOpeningText() {
+        return processString(openingMessage);
+    }
+
+    private String getClosingText() {
+        return processString(closingMessage);
+    }
+
+
+
 }
