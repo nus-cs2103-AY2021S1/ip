9cbc405336a948299452c05ef7ce6fd25a99268c
yaoyuan <yaoyuan@u.nus.edu>
2020-08-19 19:31:01 +0800

    Add, List for Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
 
     static String line = "—————————————————————————————————————————————————————————————————";
+    static String[] list = new String[100];
+    static int index = 0;
 
     public static void greeting() {
         String str = ("\t" + line + "\n"
@@ -12,11 +14,15 @@ public class Duke {
         System.out.println(str);
     }
 
+    public static void bye(){
+        System.out.println("\t" + line + "\n"
+                + "\tBye. Hope to see you again soon!\n"
+                + "\t" + line);
+    }
+
     public static void repeat(String input){
         if (input.equals("bye")) {
-            System.out.println("\t" + line + "\n"
-                    + "\tBye. Hope to see you again soon!\n"
-                    + "\t" + line);
+            bye();
         } else {
             System.out.println( "\t" + line + "\n"
                     + "\t" + input + "\n"
@@ -24,17 +30,37 @@ public class Duke {
         }
     }
 
+    public static void addTask(String task) {
+        list[index] = task;
+        index++;
+        System.out.println("\t" + line + "\n"
+                + "\t" + "added: " + task + "\n"
+                + "\t" + line);
+    }
+
+    public static void showList(){
+        System.out.println("\t" + line);
+        for (int i = 0; i < index; i++){
+            System.out.println("\t" + (i+1) + ". " + list[i]);
+        }
+        System.out.println("\t" + line);
+    }
+
+
+
     public static void main(String[] args) {
         greeting();
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                repeat(input);
+                bye();
                 sc.close();
                 break;
+            } else if (input.equals("list")){
+                showList();
             } else {
-                repeat(input);
+                addTask(input);
             }
         }
     }
