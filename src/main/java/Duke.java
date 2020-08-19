098dde824caa1da18adc425056017cf3b4d9bc51
xnmng <puaxuanmingpxm18@gmail.com>
2020-08-19 12:46:02 +0800

    added both the Echo and Exit feature of Level 1 of iP (decided to implement both small parts together as the input to Duke at this point is binary: Duke either recieves a "bye", which it will then respond by exiting, or Duke can recieve any other input which it will then respond by echoing that input and prompt the user for another input)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
 
     // this field is used when Duke requires a horizontal line
@@ -11,6 +13,20 @@ public class Duke {
         System.out.println(LINE);
     }
 
+    // this function says bye to the user when Duke recieves the input "bye"
+    public static void goodbye() {
+        System.out.println(LINE);
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
+    // calling this function will cause Duke to echo what the user inputs to Duke
+    public static void echo(String s) {
+        System.out.println(LINE);
+        System.out.println("     " + s);
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -19,5 +35,19 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greeting();
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            // if the user inputs a "bye", we simply break out of the Duke program
+            if (sc.hasNext("bye")) {
+                goodbye();
+                break;
+            }
+
+            else {
+                String input = sc.nextLine();
+                echo(input);
+            }
+        }
     }
 }
