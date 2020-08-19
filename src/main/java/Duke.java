ea2d4bedcd0d4fb8c099b89a05828e39f96a45a6
chan-j-d <chan.j.d.98@gmail.com>
2020-08-20 01:05:00 +0800

    Completed greet, echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,21 +7,36 @@ public class Duke {
     private static final String openingMessage = "Hello! I'm Duke\n" + preSpacing + "What can I do for you?";
     private static final String closingMessage = "Bye. Hope to see you again soon!";
 
+    private static final String CLOSING_STRING = "bye";
+
     public static void main(String[] args) {
+        System.out.println(getOpeningText());
 
         Scanner scanner = new Scanner(System.in);
 
+        String userInput = null;
+
+        while (!userInput.equals(CLOSING_STRING)) {
+            userInput = scanner.next();
+            if (userInput.equals(CLOSING_STRING)) {
+                System.out.println(processString(userInput));
+            } else {
+                System.out.println(getClosingText());
+            }
+        }
+
+
     }
 
-    private String processString(String string) {
+    private static String processString(String string) {
         return lineBreak  + preSpacing + string + '\n' + lineBreak;
     }
 
-    private String getOpeningText() {
+    private static String getOpeningText() {
         return processString(openingMessage);
     }
 
-    private String getClosingText() {
+    private static String getClosingText() {
         return processString(closingMessage);
     }
 
