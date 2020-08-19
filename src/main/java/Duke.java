49fdaf1e582a8ffbfef057aa80938c952cf9cf2a
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-08-19 17:42:10 +0800

    add greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Duke {
     final static String LINE = "____________________________________________________________\n";
     final static String BYE = "aight imma head out\n";
+    final static String WELCOME = "Hello! I'm Duke\nWhat can I do for you?\n";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,6 +10,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(LINE + WELCOME + "\n" + LINE);
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while(!input.equals("bye")) {
