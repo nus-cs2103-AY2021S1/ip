91f823ba8bee37e2fcd9f14dda7f6d8c8d8695f9
BILL <xie.yaoren@u.nus.edu>
2020-08-19 23:15:45 +0800

    Done with A-TextUiTesting

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,15 +12,20 @@ then
     rm ACTUAL.TXT
 fi
 
+export LC_ALL=en_GB.UTF-8
+
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Duke.java
+if !(
+find ../src/main/java -name "*.java" > sources.txt
+javac -cp ../src -Xlint:none -d ../bin @sources.txt
+)
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin level4 < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
