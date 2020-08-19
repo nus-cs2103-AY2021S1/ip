e131e6c732504c6106de9f0310efce7555c3a8ed
nottiffchan <nottiffchan@gmail.com>
2020-08-19 20:55:06 +0800

    Change to follow OOP better

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,122 +1,175 @@
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Duke {
+    ArrayList<Task> tasklist;
+    String divider = "\t_________________________________________\n";
 
-    public static void main(String[] args) throws DukeException {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Task> list = new ArrayList<>();
-        String divider = "    _________________________________________\n";
-        int listIndex = 1;
-        String MrCamel = "                  ,,__\n"
-                + "        ..  ..   / o._)\n"
-                + "       /--'/--\\  \\-'|| \n"
-                + "      /        \\_/ / |\n"
-                + "    .'\\  \\__\\  __.'.'\n"
-                + "      )\\ |  )\\ |\n"
-                + "     // \\\\ // \\\\\n"
-                + "    ||_  \\\\|_  \\\\_\n"
-                + "    '--' '--'' '--'\n";
+    public Duke() {
+        this.tasklist = new ArrayList<>();
+    }
 
+    public void addToDo(String description) throws DukeException {
+        if (description.length() <= 0) {
+            throw new DukeException("Please provide a description!");
+        }
+        Task t = new Todo(description);
+        this.tasklist.add(t);
+        printOutputAddTask(t);
+    }
+
+    public void addDeadline(String details) throws DukeException {
+        try {
+            String[] temp = details.split("/by");
+            String description = temp[0].trim();
+            String by = temp[1].trim();
+
+            if (description.length() <= 0 || by.length() <= 0) {
+                throw new DukeException("Please provide both a description and deadline!");
+            }
+            Task t = new Deadline(description, by);
+            this.tasklist.add(t);
+            printOutputAddTask(t);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("Please provide both a description and deadline!");
+        }
+    }
+
+    public void addEvent(String details) throws DukeException {
+        try {
+            String[] temp2 = details.split("/at");
+            String description = temp2[0].trim();
+            String at = temp2[1].trim();
+
+            if (description.length() <= 0 || at.length() <= 0) {
+                throw new DukeException("Please provide both a description and timing!");
+            }
+            Task t = new Event(description, at);
+            this.tasklist.add(t);
+            printOutputAddTask(t);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("Please provide both a description and timing!");
+        }
+    }
+
+    public void deleteTask(int taskIndex) throws DukeException {
+        try {
+            Task t = tasklist.get(taskIndex - 1);
+            tasklist.remove(taskIndex - 1);
+
+            System.out.println(divider);
+            System.out.println("\tMr Camel will delete this task:\n" + "\t" + t);
+            System.out.println("\tNumber of tasks: " + tasklist.size());
+            System.out.println(divider);
+
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("Invalid task number!");
+        }
+    }
+
+    public void doneTask(int taskIndex) throws DukeException {
+        try {
+            Task t = tasklist.get(taskIndex - 1);
+            t.markAsDone();
+
+            System.out.println(divider);
+            System.out.println("\tMr Camel will mark this task as done:\n" + "\t" + t);
+            System.out.println(divider);
+
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("Invalid task number!");
+        }
+    }
+
+    public void printOutputAddTask(Task t) {
+        System.out.println(divider);
+        System.out.println("\tMr Camel has added: " + t);
+        System.out.println("\tNumber of tasks: " + tasklist.size());
+        System.out.println(divider);
+    }
+
+
+    public void printList() {
+        System.out.println(divider);
+        System.out.println("\tTasks:");
+
+        for (int i = 1; i < tasklist.size() + 1; i++) {
+            System.out.println("\t" + i + ". " + tasklist.get(i-1));
+        }
+        System.out.println(divider);
+    }
+
+    public void initDuke() {
+        String MrCamel =
+                "                  ,,__\n"
+                        + "        ..  ..   / o._)\n"
+                        + "       /--'/--\\  \\-'|| \n"
+                        + "      /        \\_/ / |\n"
+                        + "    .'\\  \\__\\  __.'.'\n"
+                        + "      )\\ |  )\\ |\n"
+                        + "     // \\\\ // \\\\\n"
+                        + "    ||_  \\\\|_  \\\\_\n"
+                        + "    '--' '--'' '--'\n";
         System.out.println("Mr Camel says hello. What can Mr Camel do for you today?\n" + MrCamel);
+    }
 
-        while (sc.hasNext()) {
-            String command = sc.nextLine();
-            if (command.equals("list")) {
+    public void runDuke() {
+        Scanner sc = new Scanner(System.in);
+        boolean hasNext = true;
+
+        while (hasNext) {
+            String command = sc.next();
+            try {
+                switch (command) {
+                    case "bye":
+                        System.out.println(divider);
+                        System.out.println("\tBye. Mr Camel will miss you! :(");
+                        System.out.println(divider);
+                        hasNext = false;
+                        break;
+                    case "list":
+                        printList();
+                        break;
+                    case "todo":
+                        String todoDescription = sc.nextLine().trim();
+                        addToDo(todoDescription);
+                        break;
+                    case "deadline":
+                        String deadlineDetails = sc.nextLine();
+                        addDeadline(deadlineDetails);
+                        break;
+                    case "event":
+                        String eventDetails = sc.nextLine();
+                        addEvent(eventDetails);
+                        break;
+                    case "done":
+                        int doneTaskIndex = sc.nextInt();
+                        doneTask(doneTaskIndex);
+                        break;
+                    case "delete":
+                        int deleteTaskIndex = sc.nextInt();
+                        deleteTask(deleteTaskIndex);
+                        break;
+
+                    default:
+                        throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
                 System.out.println(divider);
-                System.out.println("      Tasks:");
-
-                for (int i = 1; i < list.size() + 1; i++) {
-                    System.out.println("      " + i + ". " + list.get(i-1));
-                }
+                System.out.println("\t" + e.getMessage());
                 System.out.println(divider);
-
-            } else if (command.contains("deadline")) {
-                try {
-                    int indexOfDate = command.indexOf("/");
-                    String taskDescription = command.substring(9, indexOfDate - 1);
-                    String by = command.substring(indexOfDate + 3);
-                    Deadline deadline = new Deadline(taskDescription, by);
-                    list.add(deadline);
-
-                    System.out.println(divider);
-                    System.out.println("      Mr Camel has added: " + deadline);
-                    System.out.println("      Number of tasks: " + list.size());
-                    System.out.println(divider);
-                } catch (IndexOutOfBoundsException e) {
-                    throw new DukeException("Please provide both a description and a deadline!");
-                }
-
-            } else if (command.contains("todo")) {
-                try {
-                    String taskDescription = command.substring(5);
-                    Todo todo = new Todo(taskDescription);
-                    list.add(todo);
-
-                    System.out.println(divider);
-                    System.out.println("      Mr Camel has added: " + todo);
-                    System.out.println("      Number of tasks: " + list.size());
-                    System.out.println(divider);
-                } catch (StringIndexOutOfBoundsException e) {
-                    throw new DukeException("Please provide a description!");
-                }
-
-            } else if (command.contains("event")) {
-                try {
-                    int indexOfDate = command.indexOf("/");
-                    String taskDescription = command.substring(6, indexOfDate - 1);
-                    String at = command.substring(indexOfDate + 3);
-                    Event event = new Event(taskDescription, at);
-                    list.add(event);
-
-                    System.out.println(divider);
-                    System.out.println("      Mr Camel has added: " + event);
-                    System.out.println("      Number of tasks: " + list.size());
-                    System.out.println(divider);
-                } catch (IndexOutOfBoundsException e) {
-                    throw new DukeException("Please provide both a description and date!");
-                }
-
-            } else if (command.contains("done")) {
-                try {
-                    int indexDone = Integer.parseInt(command.substring(5));
-                    list.set(indexDone - 1, list.get(indexDone - 1).markAsDone());
-
-                    System.out.println(divider);
-                    System.out.println("      Mr Camel will mark this task as done:\n" + "        " + list.get(indexDone - 1));
-                    System.out.println(divider);
-                } catch (IndexOutOfBoundsException e) {
-                    throw new DukeException("Invalid task number!");
-                } catch (NullPointerException e) {
-                    throw new DukeException("Invalid task number!");
-                }
-
-            } else if (command.contains("delete")) {
-                try {
-                    int indexDone = Integer.parseInt(command.substring(7));
-
-                    System.out.println(divider);
-                    System.out.println("      Mr Camel will delete this task:\n" + "        " + list.get(indexDone - 1));
-                    list.remove(indexDone - 1);
-                    System.out.println("      Number of tasks: " + list.size());
-                    System.out.println(divider);
-
-                } catch (IndexOutOfBoundsException e) {
-                    throw new DukeException("Invalid task number!");
-                } catch (NullPointerException e) {
-                    throw new DukeException("Invalid task number!");
-                }
-
-            } else if (command.equals("bye")) {
-                System.out.println(divider);
-                System.out.println("      Bye. Mr Camel will miss you! :(");
-                System.out.println(divider);
-                break;
-            } else { //invalid input
-
-                throw new DukeException("I'm sorry, but I don't know what that means :-(");
             }
         }
+    }
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+
+        duke.initDuke();
+        duke.runDuke();
     }
 }
