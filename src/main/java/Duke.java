e13895170195c63deee3c79000ca128b46110c7d
KOH-ZHE-HAO <lincolnkoh99@gmail.com>
2020-08-19 13:39:41 +0800

    Complete implementation of level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,37 +8,65 @@ public class Duke {
     private static final String SPACE_BEFORE_LINE = "   ";
     private static final String SPACE_BEFORE_TEXT = "    ";
 
+    private static boolean isInteger (String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-        List<String> userInput = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
         System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT);
         System.out.println(SPACE_BEFORE_TEXT + "Hey, I'm Emilia!" + "\n" +
                 SPACE_BEFORE_TEXT + "What can I do for you ♥ ?");
         System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT);
 
-        Scanner sc = new Scanner(System.in);
-
         while (sc.hasNext()) {
-            String input = sc.nextLine();
-            if (input.trim().toLowerCase().equals("bye")) {
+            String input = sc.nextLine().trim().toLowerCase();
+
+            if (input.equals("bye")) {
                 System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT + "\n" + SPACE_BEFORE_TEXT +
                         "Welcome back, master ♥ !" + "\n" + SPACE_BEFORE_LINE + LINE_BREAK_BOT);
                 break;
 
-            } else if (input.trim().toLowerCase().equals("list")) {
+            }
+
+            if (input.equals("list")) {
                 System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_USER);
+                System.out.println(SPACE_BEFORE_LINE + "Please take a look at the tasks:");
                 int count = 1;
-                for (String text : userInput) {
-                    System.out.println(SPACE_BEFORE_TEXT + count + ". " + text);
+                for (Task task : tasks) {
+                    System.out.println(SPACE_BEFORE_TEXT + count + ". " + task);
                     count++;
                 }
                 System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT);
-
-            } else {
-                userInput.add(input);
-                System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_USER + "\n" + SPACE_BEFORE_TEXT +
-                        "added: " + input + "\n" + SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+                continue;
             }
+
+            String[] process = input.split(" ");
+            if (process[0].equals("done") && isInteger(process[1])) {
+                int index = Integer.parseInt(process[1]);
+                try {
+                    Task task = tasks.get(index);
+                    task.markAsDone();
+                    System.out.println(SPACE_BEFORE_LINE + "Understood, I've marked this task as done:" +
+                            "\n" + task);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Sorry master, I can't seem to find the task...");
+                }
+                continue;
+            }
+
+            Task current = new Task(input);
+            tasks.add(current);
+            System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_USER + "\n" + SPACE_BEFORE_TEXT +
+                    "added: " + input + "\n" + SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String name;
+    protected boolean status;
+
+    public Task(String name) {
+        this.name = name;
+        this.status = false;
+    }
+
+    public boolean isDone() {
+        return this.status;
+    }
+
+    public void markAsDone() {
+        this.status = true;
+    }
+
+    public String toString() {
+        return "[" + (status ? "\u2713" : "\u2718") + "] " + this.name;
+    }
+
+}
