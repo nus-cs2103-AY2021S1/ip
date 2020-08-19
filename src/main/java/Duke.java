13a769b3924f5810638446809a70e4d2a75c14c3
Lim Jin Feng <ljfof98@gmail.com>
2020-08-19 16:45:49 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,9 @@ public class Duke {
     private final static String greeting = "Hello! I'm Duke\n"
             + "What can I do for you?";
     private final static String goodbye = "Bye. Hope to see you again soon!";
+    //----- level 2 here -----
+    private static String[] list = new String[100];
+    private static int count = 0;
 
     public static void main(String[] args) {
         System.out.println("Hello from\n" + Duke.logo);
@@ -21,10 +24,18 @@ public class Duke {
         String userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
             System.out.println(Duke.bar);
-            System.out.println(userInput);
+            if (userInput.equals("list")) {
+                for (int i = 0; i < Duke.count; i++) {
+                    System.out.println(i+1 + ". " + list[i]);
+                }
+            } else {
+                Duke.list[Duke.count++] = userInput;
+                System.out.println("added: " + userInput);
+            }
             System.out.println(Duke.bar);
             userInput = sc.nextLine();
         }
+        sc.close();
         System.out.println(Duke.bar);
         System.out.println(Duke.goodbye);
         System.out.println(Duke.bar);
