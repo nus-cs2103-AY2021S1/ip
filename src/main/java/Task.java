05f2646b0e0124fd1ccf62ff96aeba37aeb1fc22
Ting Xiao <tigerting98@gmail.com>
2020-08-19 23:20:17 +0800

    Add new Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,17 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+}
