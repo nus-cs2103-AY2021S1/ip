e9184d12b2fc71cbc46e278e5cca2f3ff010fb9e
rnmy <>
2020-08-19 15:43:38 +0800

    level 4

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,9 @@ public class Task {
     public void makeDone() {
         isDone = true;
     }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 public class TaskList {
     protected ArrayList<Task> taskList;
     protected boolean active = false;
-    protected String welcomeMessage = "____________________________________________________\n" +
-            "Hello! I'm Duke\n" +
-            "What can I do for you?\n" +
-            "____________________________________________________\n";
-    protected String goodbyeMessage = "____________________________________________________\n" +
-            "Bye. Hope to see you again soon!\n" +
-            "____________________________________________________\n";
 
     public TaskList() {
         taskList = new ArrayList<>();
@@ -18,6 +11,10 @@ public class TaskList {
 
     public void start() {
         active = true;
+        String welcomeMessage = "____________________________________________________\n" +
+                "Hello! I'm Duke\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________\n";
         System.out.println(welcomeMessage);
         Scanner sc = new Scanner(System.in);
         while (active) {
@@ -26,11 +23,16 @@ public class TaskList {
                 this.view();
             } else if (input.equals("bye")) {
                 this.exit();
-            } else if (input.split(" ")[0].equals("done")) {
-                int doneIndex = Integer.valueOf(input.split(" ")[1]) - 1;
-                this.markDone(doneIndex);
             } else {
-                this.addTask(input);
+                String[] inputInfo = input.split(" ", 2);
+                String command = inputInfo[0];
+                String info = inputInfo[1];
+                if (command.equals("done")) {
+                    int doneIndex = Integer.valueOf(info) - 1;
+                    this.markDone(doneIndex);
+                } else if (command.equals("todo") || command.equals("deadline") || command.equals("event")) {
+                    this.addTask(command, info);
+                }
             }
         }
         sc.close();
@@ -38,16 +40,30 @@ public class TaskList {
 
     public void exit() {
         active = false;
+        String goodbyeMessage = "____________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________\n";
         System.out.println(goodbyeMessage);
     }
 
-    public void addTask(String description) {
-        Task newTask = new Task(description);
+    public void addTask(String type, String info) {
+        Task newTask = null;
+        if (type.equals("todo")) {
+            newTask = new ToDo(info);
+        } else if (type.equals("deadline")) {
+            String[] descriptionAndBy = info.split(" /by ", 2);
+            newTask = new Deadline(descriptionAndBy[0], descriptionAndBy[1]);
+        } else if (type.equals("event")) {
+            String[] descriptionAndAt = info.split(" /at ", 2);
+            newTask = new Event(descriptionAndAt[0], descriptionAndAt[1]);
+        }
         taskList.add(newTask);
-        String addTaskText = "____________________________________________________\n" +
-                         "added: " + description + "\n" +
+        String addTaskMessage = "____________________________________________________\n" +
+                         "Got it. I've added this task:\n" +
+                         newTask.toString() + "\n" +
+                         "Now you have " + taskList.size() + (taskList.size() > 1 ? " tasks " : " task ") + "in the list.\n" +
                          "____________________________________________________\n";
-        System.out.println(addTaskText);
+        System.out.println(addTaskMessage);
     }
 
     public void view() {
@@ -55,8 +71,8 @@ public class TaskList {
                       "Here are the tasks in your list:\n";
         for (int i = 0; i < taskList.size(); i++) {
             Task task = taskList.get(i);
-            String taskText = String.valueOf(i + 1) + ".[" +  task.getStatusIcon() + "] " + task.getDescription() + "\n";
-            list += taskText;
+            String taskString = String.valueOf(i + 1) + "." + task.toString() + "\n";
+            list += taskString;
         }
         list += "____________________________________________________\n";
         System.out.println(list);
