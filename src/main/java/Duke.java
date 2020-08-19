eb8e2de8444ce850e4827fce4ce4b75fa076842b
g-erm <tanyuting.gt@gmail.com>
2020-08-19 15:07:02 +0800

    second level

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,12 +12,32 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+
         String bye = "bye";
+        String list = "list";
+
+        String[] taskList = new String[100];
+        int taskSize = 0;
 
         do {
-            System.out.println(input + "\n_________________________________________");
-            input = sc.nextLine();
-        } while (!input.equals(bye));
+            if (input.equals(list)) {
+                int curr = 0;
+                while (curr < taskSize) {
+                    System.out.println((curr + 1) + ". " + taskList[curr]);
+                    curr += 1;
+                }
+                System.out.println("_________________________________________");
+                input = sc.nextLine();
+            }
+            else {
+                taskList[taskSize] = input;
+                System.out.println("I have added: " + input
+                        + "\n_________________________________________");
+                input = sc.nextLine();
+                taskSize += 1;
+            }
+        }
+        while (!input.equals(bye));
 
         System.out.println("You're leaving? Bye :( Come back soon!"
                 + "\n_________________________________________");
