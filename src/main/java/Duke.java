1712a8df855cdc182477121cef9d44fcbe4f2bd4
rolandyuwy <rolandyuwenyang@hotmail.com>
2020-08-20 01:42:31 +0800

    A-Enums: Incorporated command inputs as enum constants. Changed NumberFormatException to DukeException. Abstracted lines to be printed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,8 +10,24 @@ import java.util.ArrayList;
  **/
 public class Duke {
 
-    public static List<Task> stored_task = new ArrayList<>();
-    public static String line = "____________________________________________________________";
+    public static final List<Task> stored_task = new ArrayList<>();
+    public static final String line = "____________________________________________________________";
+
+    //Represents command inputs as constants
+    public enum Command {
+        LIST("list"), BYE("bye"), DELETE("delete"), DONE("done"),
+        TODO("todo"), DEADLINE("deadline"), EVENT("event");
+
+        private final String input;
+
+        private Command(String input) {
+            this.input = input;
+        }
+
+        public String getInput() {
+            return input;
+        }
+    }
 
     /**
      * Lists stored task by looping through stored_task, along with their status.
@@ -20,16 +36,18 @@ public class Duke {
         if (stored_task.isEmpty()) {
             System.out.println("No tasks stored...");
         } else {
+            System.out.println(line);
             System.out.println("Quack! Here are the tasks in your list:");
             int count = 1;
             for (Task task : stored_task) {
                 System.out.println(count + ". " + task);
                 count++;
             }
+            System.out.println(line);
         }
     }
 
-    public static void displayTaskCount(){
+    public static void displayTaskCount() {
         int numOfTasks = stored_task.size();
         if (numOfTasks == 1) {
             System.out.println("My duck senses tell me you have 1 task in the list.");
@@ -45,8 +63,20 @@ public class Duke {
      **/
     public static void addTask(Task newTask) {
         stored_task.add(newTask);
+        System.out.println(line);
         System.out.println("Quack! I have added: " + newTask);
         displayTaskCount();
+        System.out.println(line);
+    }
+
+    /**
+     * Checks if string contains only numbers.
+     *
+     * @param input input string to check
+     * @return if string contains only numbers
+     **/
+    public static boolean isNumber(String input){
+        return input.matches("[0-9]+");
     }
 
     /**
@@ -64,11 +94,15 @@ public class Duke {
                     throw new DukeException("This task is already done: " + t.getDescription());
                 } else {
                     t.markAsDone();
+                    System.out.println(line);
                     System.out.println("Quack! I have marked this task as done: \n" + t);
+                    System.out.println(line);
                 }
             }
-        } catch (DukeException e){
+        } catch (DukeException e) {
+            System.out.println(line);
             System.out.println(e.getMessage());
+            System.out.println(line);
         }
     }
 
@@ -84,11 +118,15 @@ public class Duke {
             } else {
                 Task taskToDelete = stored_task.get(taskNumber - 1);
                 stored_task.remove(taskToDelete);
+                System.out.println(line);
                 System.out.println("Quack! I have deleted this task: \n" + taskToDelete);
                 displayTaskCount();
+                System.out.println(line);
             }
-        } catch (DukeException e){
+        } catch (DukeException e) {
+            System.out.println(line);
             System.out.println(e.getMessage());
+            System.out.println(line);
         }
     }
 
@@ -113,53 +151,55 @@ public class Duke {
         while (true) {
             try {
                 String input = sc.nextLine();
-                if (input.equals("bye")) {
+                if (input.equals(Command.BYE.getInput())) {
                     System.out.println(exit_message);
                     break;
-                } else if (input.equals("list")) {
-                    System.out.println(line);
+                } else if (input.equals(Command.LIST.getInput())) {
                     listStoredTasks();
-                    System.out.println(line);
-                } else if (input.startsWith("done")) {
+                } else if (input.startsWith(Command.DONE.getInput())) {
                     if (input.length() > 4) {
-                        if (!input.substring(4, 5).equals(" ")){
+                        if (!input.substring(4, 5).equals(" ")) {
                             throw new DukeException("My duck instincts tell me your input makes no sense...");
                         } else {
-                            int taskNumber = Integer.parseInt(input.substring(5));
-                            System.out.println(line);
-                            markTaskAsDone(taskNumber);
-                            System.out.println(line);
+                            String taskNumberString = input.substring(5);
+                            if (isNumber(taskNumberString)) {
+                                int taskNumber = Integer.parseInt(input.substring(5));
+                                markTaskAsDone(taskNumber);
+                            } else {
+                                throw new DukeException("You need to input the task number!");
+                            }
                         }
                     } else {
                         throw new DukeException("You need to include your task number to mark done...");
                     }
-                } else if (input.startsWith("delete")) {
+                } else if (input.startsWith(Command.DELETE.getInput())) {
                     if (input.length() > 6) {
-                        if (!input.substring(6, 7).equals(" ")){
+                        if (!input.substring(6, 7).equals(" ")) {
                             throw new DukeException("My duck instincts tell me your input makes no sense...");
                         } else {
-                            int taskNumber = Integer.parseInt(input.substring(7));
-                            System.out.println(line);
-                            deleteTask(taskNumber);
-                            System.out.println(line);
+                            String taskNumberString = input.substring(7);
+                            if (isNumber(taskNumberString)) {
+                                int taskNumber = Integer.parseInt(input.substring(7));
+                                deleteTask(taskNumber);
+                            } else {
+                                throw new DukeException("You need to input the task number!");
+                            }
                         }
                     } else {
                         throw new DukeException("You need to include your task number to delete...");
                     }
-                } else if (input.startsWith("todo")) {
+                } else if (input.startsWith(Command.TODO.getInput())) {
                     if (input.length() > 4) {
                         if (!input.substring(4, 5).equals(" ")) {
                             throw new DukeException("My duck instincts tell me your input makes no sense...");
                         } else {
                             ToDo newTask = new ToDo(input.substring(5));
-                            System.out.println(line);
                             addTask(newTask);
-                            System.out.println(line);
                         }
                     } else {
                         throw new DukeException("Your todo description can't be empty...");
                     }
-                } else if (input.startsWith("deadline")) {
+                } else if (input.startsWith(Command.DEADLINE.getInput())) {
                     if (input.length() > 8) {
                         if (!input.substring(8, 9).equals(" ")) {
                             throw new DukeException("My duck instincts tell me your input makes no sense...");
@@ -171,15 +211,13 @@ public class Duke {
                                 String description = input.substring(9, indexOfBy);
                                 String by = input.substring(indexOfBy + 3);
                                 Deadline newTask = new Deadline(description, by);
-                                System.out.println(line);
                                 addTask(newTask);
-                                System.out.println(line);
                             }
                         }
                     } else {
                         throw new DukeException("Your deadline description can't be empty...");
                     }
-                } else if (input.startsWith("event")) {
+                } else if (input.startsWith(Command.EVENT.getInput())) {
                     if (input.length() > 5) {
                         if (!input.substring(5, 6).equals(" ")) {
                             throw new DukeException("My duck instincts tell me your input makes no sense...");
@@ -191,9 +229,7 @@ public class Duke {
                                 String description = input.substring(6, indexOfAt);
                                 String at = input.substring(indexOfAt + 3);
                                 Event newTask = new Event(description, at);
-                                System.out.println(line);
                                 addTask(newTask);
-                                System.out.println(line);
                             }
                         }
                     } else {
@@ -203,13 +239,9 @@ public class Duke {
                 } else {
                     throw new DukeException("My duck instincts tell me your input makes no sense...");
                 }
-            } catch (DukeException e){
+            } catch (DukeException e) {
                 System.out.println(line);
                 System.out.println(e.getMessage());
-                System.out.println(line);
-            } catch (NumberFormatException e){ //when user does not input a number after "done" or "delete" command
-                System.out.println(line);
-                System.out.println("You need to input the task number!");
                 System.out.println(line);
             }
         }
