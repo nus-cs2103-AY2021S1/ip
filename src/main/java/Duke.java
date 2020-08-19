bf848a15ad0a4e8eacb73448f78e2a7db0c2e4a4
DESKTOP-8F04SB6\Jia Le <chongjiale0@gmail.com>
2020-08-19 23:05:09 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void greet() {
+        System.out.println("Hello! I'm Jia Le :)");
+        System.out.println("How may I be of assistance to you?");
+    }
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line =scanner.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye! Hope to see you again ;)");
+                break;
+            }
+            System.out.println(line);
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +24,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        echo();
     }
 }
