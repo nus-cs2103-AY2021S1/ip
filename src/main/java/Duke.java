37413fe8c11c76b637e55e9cfed06281269282d7
hejl <hejl20150101@gmail.com>
2020-08-19 17:13:01 +0800

    Added Mark as Done feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,7 +15,7 @@ public class Duke {
         String starting_line = separation_line + "\n" + indent;
         String ending_line = "\n" + separation_line + "\n";
 
-        String[] task_arr = new String[100];
+        Task[] task_arr = new Task[100];
         int count = 0;
 
         String greeting = starting_line + "Hello! This is J.A.R.V.I.S.\n" + indent + "How may I help you?" + ending_line;
@@ -29,19 +29,25 @@ public class Duke {
                 System.out.println(starting_line + "Bye. Hope to see you again soon!" +  ending_line);
                 exit_bye = true;
             } else {
+                System.out.println(separation_line);
                 if (input.equals("list")) {
                     int temp = 1;
-                    System.out.println(separation_line);
+                    System.out.println(indent + "Here are the tasks in your list:");
                     while (task_arr[temp - 1] != null) {
-                        System.out.println(indent + temp + ". " + task_arr[temp - 1]);
+                        System.out.println(indent + temp + ".[" + task_arr[temp - 1].getStatusIcon() + "] " + task_arr[temp - 1].getDescription());
                         temp++;
                     }
-                    System.out.println(separation_line + "\n");
+                } else if (input.substring(0, 4).equals("done")) {
+                    int done_number = Integer.parseInt(input.substring(5, input.length()));
+                    task_arr[count - 1].markAsDone();
+                    System.out.println(indent + "Nice! I've marked this task as done:");
+                    System.out.println(indent + "  [\u2713] " + task_arr[count - 1].getDescription());
                 } else {
-                    System.out.println(starting_line + "added: " + input + ending_line);
-                    task_arr[count] = input;
+                    System.out.println(indent + "added: " + input);
+                    task_arr[count] = new Task(input);
                     count++;
                 }
+                System.out.println(separation_line + "\n");
                 //System.out.println(starting_line + input + ending_line);
             }
         }
