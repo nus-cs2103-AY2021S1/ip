769481980ce5e37882a4537ba7c738d1faeee02a
akgrenSoar <44816346+akgrenSoar@users.noreply.github.com>
2020-08-19 17:55:15 +0800

    Level-1 (v2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,4 @@
-import chatbot.ChatbotEcho;
+import java.util.Scanner;
 
 public class Duke {
 
@@ -8,14 +8,29 @@ public class Duke {
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String greeting = "Hello! I'm Echo\nLet's have a conversation!";
+    private static final String endingGreeting = "Bye. Hope to see you again soon!";
 
     public static void main(String[] args) {
         System.out.println("Hello from\n" + logo);
+        System.out.println(greeting);
+        Duke duke = new Duke();
+        duke.run();
+        System.out.println(endingGreeting);
+    }
 
-        String greeting = "Hello! I'm Echo\nLet's have a conversation!";
-        String endingGreeting = "Bye. Hope to see you again soon!";
+    private void run() {
+        String input;
+        Scanner scanner = new Scanner(System.in);
 
-        ChatbotEcho chatbotEcho = new ChatbotEcho(greeting, System.in, endingGreeting);
-        chatbotEcho.run();
+        System.out.print("> ");
+        while(!(input = scanner.nextLine()).equals("bye")) {
+            echo(input);
+        }
+    }
+
+    private String echo(String input) {
+        System.out.print("Echo: " + input + "\n> ");
+        return input;
     }
 }
