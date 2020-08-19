401a9cd00b9c745590df95798f209a624f70f013
jiaqi20 <tjq888@gmail.com>
2020-08-19 11:39:58 +0800

    Level-3: Mark As Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,21 +11,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo + "\nHello! I'm Duke" + "\nWhat can I do for you?");
 
-        List<String> lst = new ArrayList<>();
+        List<Task> lst = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         boolean boo = true;
         while (boo){
             String str = sc.nextLine();
+            String[] strArr = str.split(" ");
             if (str.equals("bye")) {
                 boo = false;
                 System.out.println("Bye. Hope to see you again soon!");
             } else if (str.equals("list")) {
                 for (int i = 0; i < lst.size(); i++) {
-                    System.out.println((i + 1) + ". " + lst.get(i));
+                    System.out.println((i + 1) + ". " + lst.get(i).toString());
                 }
+            } else if (strArr[0].equals("done")) {
+                lst.get(Integer.parseInt(strArr[1]) - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done: \n" +
+                        lst.get(Integer.parseInt(strArr[1]) - 1).toString());
             } else {
-                lst.add(str);
-                System.out.println("added: " + str);
+                Task task = new Task(str);
+                lst.add(task);
+                System.out.println("added: " + task.toString());
             }
         }
         sc.close();
