62efd8ca825097597ea686e3b80c3de4eed9897a
rayasamhr <streamblr.harsh@gmail.com>
2020-08-19 10:43:06 +0800

    Supports adding tasks to a list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,6 +9,7 @@ public class Duke {
             "What can I do for you?";
     private static final String farewell = "Bye. Hope to see you again soon.";
     private boolean running = true;
+    private List<String> taskList = new ArrayList<>(100);
 
     public Duke() {
     }
@@ -20,20 +23,37 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
         Duke servingDuke = new Duke();
-        servingDuke.respondToInput(greeting);
+        System.out.println(servingDuke.constructMessage(greeting));
         while (servingDuke.canRun()) {
             String input = sc.nextLine();
             servingDuke.respondToInput(input);
         }
     }
 
-    private void respondToInput(String input) {
+    private void respondToInput(String rawInput) {
+        String input = rawInput.trim();
         if (input.equals("bye")) {
             running = false;
             System.out.println(constructMessage(farewell));
+        } else if (input.equals("list")) {
+            System.out.println(constructMessage(printAsString(taskList)));
         } else if (!input.isEmpty()){
-            System.out.println(constructMessage(input));
+            taskList.add(input);
+            System.out.println(constructMessage("added: " + input));
         }
+    }
+
+    private String printAsString(List<String> taskList) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 1; i <= taskList.size(); i++) {
+            res.append(i)
+                    .append(". ")
+                    .append(taskList.get(i - 1));
+            if (i != taskList.size()) {
+                res.append("\n");
+            }
+        }
+        return res.toString();
     }
 
     private boolean canRun() {
