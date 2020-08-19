b33740e794d9932bc374ee5769ad63c887ffb816
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-08-19 12:25:23 +0800

    Added convenience function for formatting output

--- src/main/java/Chatterbox.java
+++ src/main/java/Chatterbox.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 
 public class Chatterbox {
+    private static final String SEPARATOR = "++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+
+    private static String format(String s) {
+        return SEPARATOR + "\n" + s + "\n" + SEPARATOR;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello I'm Chatterbox. What can I do for you?");
 
         Scanner s = new Scanner(System.in);
         String input = s.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
-
+            System.out.println(format(input));
             input = s.nextLine();
         }
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Goodbye! Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(format("Goodbye! Hope to see you again soon!"));
     }
 }
+
