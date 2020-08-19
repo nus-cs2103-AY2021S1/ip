919d266202da4237b0ace566d987dbb3ec5e6333
Jun Hui Phoon <>
2020-08-19 18:58:57 +0800

    added Duke Level-2 functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,47 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     public static String duke = "Duke> ";
     public static String cmd = "Input> ";
+    public static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+
         Scanner sc = new Scanner(System.in);
         String userInput = "";
+        ArrayList<String> list = new ArrayList<>();
 
-        System.out.println(logo);
         startupMsg();
 
-        do {
+        while (true) {
             System.out.print(cmd);
             userInput = sc.nextLine();
-            System.out.println(duke + userInput);
+            if (userInput.equals("bye")) {
+                break;
+            }
 
-        } while(!userInput.equals("bye"));
+            if (!userInput.equals("list")) {
+                list.add(userInput);
+                System.out.println(duke + "I added '" + userInput + "' to your To-Do List");
+            } else {
+                int idx = 0;
+                System.out.println(duke + "Here's your To-Do List:");
+                for (String item : list) {
+                    System.out.println(++idx + ". " + item);
+                }
+            }
+        }
 
-        System.out.println(duke + "Bye!");
+        System.out.println(duke + "See you soon!");
     }
 
     private static void startupMsg() {
+        System.out.println(logo);
         System.out.println(duke + "Hi I'm Duke!");
         System.out.println(duke + "What can I do for you?");
     }
