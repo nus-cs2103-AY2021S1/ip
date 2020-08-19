38020fa8fdd6cc66a124907ad503b307fe289750
Ma-Yueran <mayuerantom@outlook.com>
2020-08-19 20:46:23 +0800

    Improve task list UI

--- src/ListCommand.java
+++ src/ListCommand.java
@@ -7,9 +7,14 @@ public class ListCommand extends Command {
     @Override
     public void execute(String str) {
         UIPrint.drawLine(UIPrint.star, 50);
+        System.out.println("Current tasks:\n");
 
         for (int i = 0; i < Duke.tasks.size(); i++) {
             System.out.println(i + 1 + ". " + Duke.tasks.get(i));
+        }
+
+        if (Duke.tasks.size() == 0) {
+            System.out.println("None");
         }
 
         UIPrint.drawLine(UIPrint.star, 50);
