739660f88547ad08f8c1e18bcfac066209715072
amylzting <amylimzting@gmail.com>
2020-08-19 15:00:23 +0800

    Add support for three types of Task in Dino
    Let's implement 3 subclasses of Task, namely:
    Todo, Deadline and Event. Todos are tasks without
    any date/time, Deadlines are tasks that need to be
    done before a specific date/time, and Events are
    tasks that start at a specific time and ends at a specific time.
    
    Inheritance is used to support multiple task types,
    as they are all tasks and there is similarity between them.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Rawr! I'm Dino ><\n" + "What can I do for you?"
-                + "\nInput a task to be added to your task list!"
+        System.out.println("Rawr! I'm Dino ><\n" + "Get started on your task list by entering a task!"
+                + "\nTo see how to format your task, input 'format'"
                 + "\nTo see your list of tasks, input 'list'."
                 + "\n____________________________________________________________");
         Scanner scanner = new Scanner(System.in);
@@ -14,22 +14,41 @@ public class Duke {
 
         while (!isBye) {
             String input = scanner.nextLine();
+            String[] inputWords = input.split(" ");
             if (input.equals("bye")) {
                 // Dino says bye
                 System.out.println("Rawr. Hope to see you again soon! ><"
                         + "\n____________________________________________________________");
                 isBye = true;
+            } else if (input.equals("format")) {
+                System.out.println("Formats for the three task types Todo, Deadline and Event,"
+                        + " are shown below.\n"
+                        + "Todo: 'todo <task description>'"
+                        + " (e.g., visit new theme park)\n"
+                        + "Deadline: 'deadline <task description>"
+                        + " /by <date/time>' (e.g., submit report by 11/10/2019 5pm)\n"
+                        + "Event: 'event <task description>"
+                        + " /at <date/start and end time>' "
+                        + "(e.g., team project meeting on 2/10/2019 2-4pm)"
+                        + "\n____________________________________________________________");
             } else if (input.equals("list")) {
-                // Dino lists out all items in list
-                System.out.println("Dino lists your tasks:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    int index = i + 1;
-                    System.out.println(index + ". " + tasks.get(i));
+                if(tasks.size() != 0) {
+                    // Dino lists out all items in list
+                    System.out.println("Dino lists your tasks:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        int index = i + 1;
+                        System.out.println(index + ". " + tasks.get(i));
+                    }
+                    System.out.println("To mark off a task after completion"
+                            + ", input 'done <task number>'.");
+                } else {
+                    System.out.println("Rawr! Dino could not find any items in your task list."
+                            + "\nGet started by entering a task. "
+                            + "Formats for a task can be found by entering 'format'.");
                 }
-                System.out.println("To mark off a task after completion, just input 'done <task number>'.");
                 System.out.println("____________________________________________________________");
-            } else if (input.split(" ")[0].equals("done") && input.split(" ").length == 2
-                    && input.split(" ")[1].matches("[0-9]+")) {
+            } else if (inputWords[0].equals("done") && inputWords.length == 2
+                    && inputWords[1].matches("[0-9]+")) {
                 // condition checks that user input is in the format "done X" where X is a numeric
 
                 int taskNumber = Integer.parseInt(input.split(" ")[1]);
@@ -48,11 +67,51 @@ public class Duke {
                 }
             } else {
                 // Dino adds task to list
-                Task task = new Task(input);
-                tasks.add(task);
-                System.out.println("Dino has added to your list of tasks: "
-                        + input
-                        + "\n____________________________________________________________");
+                try {
+                    switch (inputWords[0]) {
+                    case "todo":
+                        String task = input.substring(5);
+                        Todo todo = new Todo(task);
+                        tasks.add(todo);
+                        System.out.println("Dino has added to your list of tasks: \n"
+                                + todo
+                                + "\nNumber of tasks in list: " + tasks.size()
+                                + "\n____________________________________________________________");
+                        break;
+                    case "deadline":
+                        String[] taskBy = input.substring(9)
+                                .split("/by");
+                        Deadline deadline = new Deadline(taskBy[0], taskBy[1]);
+                        tasks.add(deadline);
+                        System.out.println("Dino has added to your list of tasks: \n"
+                                + deadline
+                                + "\nNumber of tasks in list: " + tasks.size()
+                                + "\n____________________________________________________________");
+                        break;
+                    case "event":
+                        String[] eventAt = input.substring(6)
+                                .split("/at");
+                        Event event = new Event(eventAt[0], eventAt[1]);
+                        tasks.add(event);
+                        System.out.println("Dino has added to your list of tasks: \n"
+                                + event
+                                + "\nNumber of tasks in list: " + tasks.size()
+                                + "\n____________________________________________________________");
+                        break;
+                    default:
+                        System.out.println("Rawr! Dino could not add your task. "
+                                + "Make sure your format is correct."
+                                + "\nFormats to input a task can be found by entering 'format'."
+                                + "\n____________________________________________________________");
+                        break;
+                    }
+                } catch(IndexOutOfBoundsException e) {
+                    // invalid task format entered
+                    System.out.println("Rawr! Dino could not add your task. "
+                            + "Make sure your format is correct."
+                            + "\nFormats to input a task can be found by entering 'format'."
+                            + "\n____________________________________________________________");
+                }
             }
         }
     }
