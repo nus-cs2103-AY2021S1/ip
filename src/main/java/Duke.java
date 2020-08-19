43915c4d0230132a48c59ef8237f6279491dfb2e
Ting Xiao <tigerting98@gmail.com>
2020-08-20 02:18:49 +0800

    Add delete task functionality and error handling for wrong number format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,33 +20,48 @@ public class Duke {
             String input = userInput.nextLine();
             String[] inputArray = input.split(" ");
             String firstWord = inputArray[0];
-            if (firstWord.toLowerCase().equals("bye")) { // for termination
+            try {
+                if (firstWord.toLowerCase().equals("bye")) { // for termination
 
-                System.out.println("Well I'll see you around, pardner!!");
-                break;
+                    System.out.println("Well I'll see you around, pardner!!");
+                    break;
 
-            } else if (firstWord.toLowerCase().equals("list")) { // to display list of tasks
+                } else if (firstWord.toLowerCase().equals("list")) { // to display list of tasks
 
-                System.out.println("Here's yer current list of thingymajigs");
+                    System.out.println("Here's yer current list of thingymajigs");
 
-                for(int i = 0; i < taskList.size();i++ ) {
-                    Task task = taskList.get(i);
-                    System.out.println(i + 1 + ". " + task.toString());
-                }
+                    for (int i = 0; i < taskList.size(); i++) {
+                        Task task = taskList.get(i);
+                        System.out.println(i + 1 + ". " + task.toString());
+                    }
 
-            } else if (firstWord.toLowerCase().equals("done")) {
-                int index = Integer.parseInt(inputArray[1]);
-                Task task = taskList.get(index - 1);
-                task.markAsDone();
-                System.out.println("Sure thing baws! This right here is marked as done!\n" + task.toString());
+                } else if (firstWord.toLowerCase().equals("done")) {
+                    int index = Integer.parseInt(inputArray[1]);
+                    Task task = taskList.get(index - 1);
+                    task.markAsDone();
+                    System.out.println("Sure thing baws! This right here is marked as done!\n" + task.toString());
 
-            } else { // to add task to list
-                try {
+                } else if (firstWord.toLowerCase().equals("delete")) {
+
+                    if (inputArray.length <= 1) throw new DukeException("Sorry, which item did you want me to strike off again?");
+                    try {
+                        int index = Integer.parseInt(inputArray[1]);
+                        if (index > taskList.size()) throw new DukeException("C'mon parder! That task doesn't exist cos you don't go so many!");
+                        Task toBeRemoved = taskList.get(index - 1);
+                        taskList.remove(toBeRemoved);
+                        System.out.println("Alrighty, I'm taking that one out:\n" + toBeRemoved.toString() + "\n" + "You've got a total of " + taskList.size() + " items right now.");
+                    } catch (NumberFormatException e) {
+                        throw new DukeException("I need a number not a word in this case. Could ya pass that by me one more time?");
+                    }
+
+                } else { // to add task to list
+
                     Task task;
                     int index;
                     switch (firstWord.toLowerCase()) {
                         case "todo":
-                            if (inputArray.length <= 1) throw new DukeException("Sorry, but I can't do anything if you don't give me the description of your todo!");
+                            if (inputArray.length <= 1)
+                                throw new DukeException("Sorry, but I can't do anything if you don't give me the description of your todo!");
                             String desc = stringCombiner(inputArray, 1, inputArray.length - 1);
                             task = new Todo(desc);
                             break;
@@ -54,16 +69,20 @@ public class Duke {
                             String[] eventSplit = input.split("/at");
                             index = indexFinder(inputArray, "/at");
                             if (index == 0) throw new DukeException("Think you forgot the /at keyword, pardner!");
-                            if (index == 1) throw new DukeException("I'm gonna need a description for this here event!");
-                            if (eventSplit.length == 1) throw new DukeException("I'm gonna need a date or time for this!");
+                            if (index == 1)
+                                throw new DukeException("I'm gonna need a description for this here event!");
+                            if (eventSplit.length == 1)
+                                throw new DukeException("I'm gonna need a date or time for this!");
                             task = new Event(stringCombiner(inputArray, 1, index - 1), eventSplit[1]);
                             break;
                         case "deadline":
                             String[] deadlineSplit = input.split("/by");
                             index = indexFinder(inputArray, "/by");
                             if (index == 0) throw new DukeException("Think you forgot the /by keyword, pardner!");
-                            if (index == 1) throw new DukeException("I'm gonna need a description for this here deadline!");
-                            if (deadlineSplit.length == 1) throw new DukeException("I'm gonna need a date or time for this!");
+                            if (index == 1)
+                                throw new DukeException("I'm gonna need a description for this here deadline!");
+                            if (deadlineSplit.length == 1)
+                                throw new DukeException("I'm gonna need a date or time for this!");
                             task = new Deadline(stringCombiner(inputArray, 1, index - 1), deadlineSplit[1]);
                             break;
                         default:
@@ -71,10 +90,9 @@ public class Duke {
                     }
                     taskList.add(task);
                     System.out.println("Alrighty, I'll put it on yer tab:\n" + task.toString() + "\n" + "You've got a total of " + taskList.size() + " items right now.");
-                } catch (DukeException e) {
-                    System.out.println(e.toString());
                 }
-
+            } catch (DukeException e) {
+                System.out.println(e.toString());
             }
         }
     }
