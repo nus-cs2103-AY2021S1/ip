bc97279fd667d3b7cd8f837e95d8179834f07f5c
porkeypine <huiling940@gmail.com>
2020-08-20 06:11:34 +0800

    Level 1 - implemented greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("My name? You don't need to know that. \nStop bothering me already... What do you want??");
+//        BufferedReader reader =
+//                new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        String command = sc.next();
+
+        Set<String> terminationCommands = new HashSet<String>();
+        terminationCommands.add("bye");
+        terminationCommands.add("toodles");
+        terminationCommands.add("farewell");
+        terminationCommands.add("sayonara");
+//        while (!command.equals("bye")) {
+        while (!terminationCommands.contains(command)) {
+            System.out.println(command);
+            command = sc.next();
+        }
+        System.out.println("Finally... don't come back if you can possibly help it, jeez.");
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
     }
 }
