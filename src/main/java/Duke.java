684ff03a54850b2e8690b83a927a526e90493c1d
KohHanMing <kohhanming1997@gmail.com>
2020-08-20 00:39:01 +0800

    Level-1 of duke.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,8 +7,23 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String divider = "___________________________________";
+        System.out.println(divider);
         System.out.println("Hello from\n" + logo);
-        System.out.println("Commit test");
-        System.out.println("Tag test");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        Scanner sc = new Scanner(System.in);
+
+        String input = "";
+
+        while(!input.toLowerCase().equals("bye")) {
+            input = sc.nextLine();
+            System.out.println(divider);
+            System.out.println(input);
+            System.out.println(divider);
+
+        }
+
+        System.out.println("Bye! See you around :)");
     }
 }
