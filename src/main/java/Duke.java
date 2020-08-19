9e89fa9a917d0516b71a5690722b07aff9dbe8d4
Raythx98 <65889977+raythx98@users.noreply.github.com>
2020-08-20 02:07:10 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void greet() {
+        String greeting = "Hello mah dud, itza handsome robo speakin\n" +
+                "What duh hell du yu wan?";
+        System.out.println(greeting);
+    }
+
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        greet();
+        String nextLine = sc.nextLine();
+        while (!nextLine.equals("bye")) {
+            System.out.println(nextLine);
+            nextLine = sc.nextLine();
+        }
+        System.out.println("Never come back,\n" +
+                "dun wanna see yu ever agin");
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        sc.close();
     }
 }
