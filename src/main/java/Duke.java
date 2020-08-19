823bcc020bbff1101ee6952a7fe5cc35a58d4bc7
GabrielTeo <gabrieltyx@gmail.com>
2020-08-20 01:29:56 +0800

    Added Greet, Echo, Exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void greetEchoExit() {
+        System.out.println("-----------------------------------------------------");
+        System.out.println("    " + "Hello! I'm Duke\n    What can I do for you?");
+        System.out.println("-----------------------------------------------------");
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("-----------------------------------------------------");
+                System.out.println("    " + "Bye. Hope to see you again soon!");
+                System.out.println("-----------------------------------------------------");
+                break;
+            } else {
+                System.out.println("-----------------------------------------------------");
+                System.out.println("    " + line);
+                System.out.println("-----------------------------------------------------");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greetEchoExit();
     }
 }
