c286486efc5dd48fc98666c7e478167661e21ec9
chan-j-d <chan.j.d.98@gmail.com>
2020-08-20 01:56:20 +0800

    Incorporated Task class to add Mark-as-Done functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static final String LINE_BREAK = "    ____________________________________________________________\n";
-    private static final String PRESPACING = "     ";
-    private static final String OPENING_MESSAGE = "Hello! I'm Duke\n" + PRESPACING + "What can I do for you?";
-    private static final String CLOSING_MESSAGE = "Bye. Hope to see you again soon!";
+    protected static final String LINE_BREAK = "    ____________________________________________________________\n";
+    protected static final String PRESPACING = "     ";
+    protected static final String OPENING_MESSAGE = "Hello! I'm Duke\n" + PRESPACING + "What can I do for you?";
+    protected static final String CLOSING_MESSAGE = "Bye. Hope to see you again soon!";
+    protected static final String MARKED_MESSAGE = "Nice! I've marked this task as done: ";
 
-    private static final String CLOSING_STRING = "bye";
-    private static final String LIST_STRING = "list";
+    protected static final String CLOSING_STRING = "bye";
+    protected static final String LIST_STRING = "list";
+    protected static final String DONE_STRING = "done";
 
-    private static final List<String> stringRecords = new ArrayList<String>();
+    protected static final List<Task> taskRecords = new ArrayList<Task>();
 
     public static void main(String[] args) {
         System.out.println(getOpeningText());
@@ -23,52 +25,56 @@ public class Duke {
 
         while (!userInput.equals(CLOSING_STRING)) {
             userInput = scanner.nextLine();
-            switch (userInput) {
-                case CLOSING_STRING:
-                    System.out.println(getClosingText());
-                    break;
-                case LIST_STRING:
-                    System.out.println(processString(getListString()));
-                    break;
-                default:
-                    stringRecords.add(userInput);
-                    System.out.println(processString(createAddedString(userInput)));
-                    break;
-
-
+            if (userInput.equals(CLOSING_STRING)) {
+                System.out.println(getClosingText());
+            } else if (userInput.equals(LIST_STRING)) {
+                System.out.println(processString(getListString()));
+            } else if (userInput.substring(0, 4).equals(DONE_STRING)) {
+                int index = Integer.parseInt(userInput.substring(5));
+                System.out.println(registerTaskDone(index));
+            } else {
+                taskRecords.add(new Task(userInput));
+                System.out.println(processString(createAddedString(userInput)));
             }
+
         }
 
 
     }
 
-    private static String getListString() {
+    protected static String registerTaskDone(int taskNumber) {
+        Task taskDone = taskRecords.get(taskNumber - 1);
+        taskDone.markDone();
+        return processString(MARKED_MESSAGE + '\n' + PRESPACING + "   " + taskDone);
+    }
+
+    protected static String getListString() {
         StringBuilder builder = new StringBuilder();
         int count = 1;
         boolean first = true;
-        for (String string : stringRecords) {
+        for (Task task : taskRecords) {
             if (!first) {
                 builder.append('\n' + PRESPACING);
             } else first = false;
-            builder.append(count++ + ". " + string);
+            builder.append(count++ + "." + task.toString());
         }
         return builder.toString();
     }
 
-    private static String createAddedString(String addedString) {
+    protected static String createAddedString(String addedString) {
         return "added: " + addedString;
     }
 
 
-    private static String processString(String string) {
+    protected static String processString(String string) {
         return LINE_BREAK  + PRESPACING + string + '\n' + LINE_BREAK;
     }
 
-    private static String getOpeningText() {
+    protected static String getOpeningText() {
         return processString(OPENING_MESSAGE);
     }
 
-    private static String getClosingText() {
+    protected static String getClosingText() {
         return processString(CLOSING_MESSAGE);
     }
 
