cd3f5f76c895cfd8d41faec26c9544f3c85f887d
Ting Xiao <tigerting98@gmail.com>
2020-08-20 02:03:12 +0800

    Add basic error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -46,25 +46,32 @@ public class Duke {
                     int index;
                     switch (firstWord.toLowerCase()) {
                         case "todo":
+                            if (inputArray.length <= 1) throw new DukeException("Sorry, but I can't do anything if you don't give me the description of your todo!");
                             String desc = stringCombiner(inputArray, 1, inputArray.length - 1);
                             task = new Todo(desc);
                             break;
                         case "event":
                             String[] eventSplit = input.split("/at");
                             index = indexFinder(inputArray, "/at");
+                            if (index == 0) throw new DukeException("Think you forgot the /at keyword, pardner!");
+                            if (index == 1) throw new DukeException("I'm gonna need a description for this here event!");
+                            if (eventSplit.length == 1) throw new DukeException("I'm gonna need a date or time for this!");
                             task = new Event(stringCombiner(inputArray, 1, index - 1), eventSplit[1]);
                             break;
                         case "deadline":
                             String[] deadlineSplit = input.split("/by");
                             index = indexFinder(inputArray, "/by");
+                            if (index == 0) throw new DukeException("Think you forgot the /by keyword, pardner!");
+                            if (index == 1) throw new DukeException("I'm gonna need a description for this here deadline!");
+                            if (deadlineSplit.length == 1) throw new DukeException("I'm gonna need a date or time for this!");
                             task = new Deadline(stringCombiner(inputArray, 1, index - 1), deadlineSplit[1]);
                             break;
                         default:
-                            throw new IllegalStateException("Unexpected value: " + firstWord.toLowerCase());
+                            throw new DukeException("Sorry, I didn't quite catch that!");
                     }
                     taskList.add(task);
                     System.out.println("Alrighty, I'll put it on yer tab:\n" + task.toString() + "\n" + "You've got a total of " + taskList.size() + " items right now.");
-                } catch (IllegalStateException e) {
+                } catch (DukeException e) {
                     System.out.println(e.toString());
                 }
 
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,11 @@
+public class DukeException extends Exception {
+
+    public DukeException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return this.getMessage();
+    }
+}
