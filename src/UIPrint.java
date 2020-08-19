9b4833944a65f80fe05892c0fb3761a2fe20c59f
Ma-Yueran <mayuerantom@outlook.com>
2020-08-19 19:51:59 +0800

    Change icons of tick & cross in UIPrint.java

--- src/UIPrint.java
+++ src/UIPrint.java
@@ -1,17 +1,23 @@
 public class UIPrint {
 
     public static char star = '*';
-    public static String tick = "[✓]";
-    public static String cross = "[✗]";
+    public static String tick = "[Done]";
+    public static String cross = "[Not Done]";
     public static String todoIcon = "[T]";
     public static String deadlineIcon = "[D]";
     public static String eventIcon = "[E]";
 
     public static void drawLine(char ch, int length) {
-        System.out.println();
+        System.out.println(getLine(ch, length));
+    }
+
+    public static String getLine(char ch, int length) {
+        String line = "\n";
         for (int i = 0; i < length; i++) {
-            System.out.print(ch);
+            line += ch;
         }
-        System.out.println();
+        line += "\n";
+
+        return line;
     }
 }
