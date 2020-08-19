4f9608879b04e8ce0a64b78e7ca0407898eeb46e
siddarth2824 <siddarth2824@gmail.com>
2020-08-19 22:40:15 +0800

    Add A-TextUiTesting: Automated Text UI Testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,11 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Duke.java
+
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Duke.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
+
+export LC_ALL=en_GB.UTF-8
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ../bin Duke < input.txt > ACTUAL.TXT
