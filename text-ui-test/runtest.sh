f4e1aac5a94967bd8526606208b99b2e197f1ccd
jonasngs <jonasngs30@gmail.com>
2020-08-19 21:10:58 +0800

    Added more inputs for automated text UI testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -19,8 +19,8 @@ then
     exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+# run the program, feed commands from input.txt file and redirect the output to the EXPECTED.TXT
+java -classpath ../bin Duke < input.txt > EXPECTED.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
