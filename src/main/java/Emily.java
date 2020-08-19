e253d38ebb5c6225b0ff22cdf39341df65c2526e
limhaishan <haishanlim@gmail.com>
2020-08-19 15:11:03 +0800

    improve layout of divider include exceptions catching invalid input and task with empty description simplified main method

--- src/main/java/Emily.java
+++ src/main/java/Emily.java
@@ -3,35 +3,34 @@ import java.util.Scanner;
 
 public class Emily {
 
-
-    public static void main(String[] args) {
+    static public void interacting() throws DukeException {
 
         Task current;
         Task[] store = new Task[100];
 
-        String divider = "---------------";
+        String divider = "    ---------------";
         int counter = 1;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello, I am Emily\n" +
-                "What can i do for you?\n"+
-                divider);
-
-
         String input = sc.nextLine();
-        try {
+
+            if(input.trim().isEmpty()){
+                throw new DukeException("input cannot be empty");
+            }
+
             while (!input.equals("bye")) {
 
                 if (input.equals("list")) {
 
-                    System.out.println("    " + divider);
+                    System.out.println(divider);
+
                     for (int i = 1; i < counter; i++) {
                         current = store[i];
                         String item = "    " + i + ". " + current;
 
                         System.out.println(item);
                     }
-                    System.out.println("    " + divider);
+                    System.out.println(divider);
 
 
                 } else if (input.contains("done")) {
@@ -43,46 +42,77 @@ public class Emily {
                     String item = "Nice work! I have marked the task as done:\n" +
                             "     " + current;
 
-                    System.out.println("    " + divider +
+                    System.out.println(divider +
                             "\n    " + item +
-                            "\n    " + divider);
+                            "\n" + divider);
 
 
-                } else { //case to add task
+                } else if(input.contains("todo") || input.contains("deadline") || input.contains("event")) { //case to add task
+
+                    String shorten = input.trim();
+
+                    //empty description
+                    if(shorten.equals("todo") || shorten.equals("deadline") || shorten.equals("event")){
+                        System.out.println(divider);
+                        throw new DukeException("The description of " + input + " cannot be empty");
+                    }
 
                     Task item = new Task("");
-                    System.out.println("    " + divider
+                    System.out.println(divider
                             + "\n    Got it! I have added this task: ");
 
-                    if (input.contains("todo")) {
-                        String describe = input.substring(5);
-                        item = new ToDos(describe);
+                        if (input.contains("todo")) {
+                            String describe = input.substring(5);
+                            item = new ToDos(describe);
 
-                    } else if (input.contains("deadline")){
-                        String describe = input.substring(9);
-                        String[] temp = describe.split("/by ");
-                        item = new Deadline(temp[0], temp[1]);
-                    } else if (input.contains("event")){
-                        String describe = input.substring(6);
-                        String[] temp = describe.split("/at ");
-                        item = new Event(temp[0], temp[1]);
-                    }
+                        } else if (input.contains("deadline")) {
+                            String describe = input.substring(9);
+                            String[] temp = describe.split("/by ");
+                            item = new Deadline(temp[0], temp[1]);
+                        } else if (input.contains("event")) {
+                            String describe = input.substring(6);
+                            String[] temp = describe.split("/at ");
+                            item = new Event(temp[0], temp[1]);
+                        }
 
                     store[counter] = item;
                     counter++;
 
                     System.out.println("        " + item);
                     System.out.println("    Now you have " + (counter-1) + " tasks in the list" +
-                            "\n    " + divider);
+                            "\n" + divider);
+
+
+                } else{
+                    //invalid input
+                    System.out.println(divider);
+                    throw new DukeException("I'm not sure what you mean");
                 }
 
                 input = sc.nextLine();
             }
 
-        System.out.println(divider + "\nBye~, hope to see you again!");
+    }
 
-        }catch(Exception e){
-            System.out.println(e);
+
+    public static void main(String[] args) throws DukeException {
+        String divider = "-------------------";
+
+
+
+        System.out.println("Hello, I am Emily\n" +
+                "What can i do for you?\n"+
+                divider);
+
+        try{
+            interacting();
+            System.out.println(divider + "\nBye~, hope to see you again!");
+        } catch(DukeException e){
+            System.out.println("OOPS! " + e.getMessage() + "\n" + divider);
+
         }
+
+
+
     }
 }
