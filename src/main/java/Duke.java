8c3f219c30a91119391f073b5e3cf0a50e134221
EthanTheGoondu <ethannoahrozario@gmail.com>
2020-08-19 13:08:17 +0800

    Level 2 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,29 +10,78 @@ public class Duke {
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
     static String line = "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-";
+    static TaskList taskList = new TaskList();
+
+
+    private static class Task {
+
+        private final String name;
+
+        Task(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+    }
+
+
+    private static class TaskList extends ArrayList<Task> {
+
+        public void printList() {
+            printWithLines(this.toString());
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder list = new StringBuilder();
+            int l = super.size();
+            for (int i = 0; i < l; i++) {
+                list.append(i + 1).append(". ").append(super.get(i).toString()).append("\n");
+            }
+            return list.toString();
+        }
+    }
+
 
     public static void main(String[] args) {
         printWithLines("Hello! My name is Duketh Puketh III, but you can call me\n" + logo +
-                "\n How may I help you today? :)");
+                "\n How may I help you today? :)\n");
         processInput();
-        printWithLines("Bye! I'll see you again next time!");
+        printWithLines("Bye! I'll see you again next time!\n");
     }
 
     private static void processInput() {
 
         String nextInput = input.nextLine();
-        boolean bye = nextInput.equals("bye");
+        while (!nextInput.equals("bye")) {
 
-        while (!bye) {
-            printWithLines(nextInput);
+            switch (nextInput) {
+                case "list":
+                    taskList.printList();
+                    break;
+                default:
+                    taskList.add(new Task(nextInput));
+                    printWithLines("added: " + nextInput + "\n");
+            }
             nextInput = input.nextLine();
-            bye = nextInput.equals("bye");
         }
 
     }
 
     private static void printWithLines(String output) {
-        System.out.println(line + "\n" + output + "\n" + line);
+        System.out.println(line + "\n" + output + line);
     }
+
+    private static void addToList() {
+
+    }
+
+
+
+
 
 }
