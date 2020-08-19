77403771560689c49a9ed6ac181f2e867e1313cf
cx0810 <chengxin0810@gmail.com>
2020-08-19 16:20:55 +0800

    add handling of errors such as incorrect inputs by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,6 +13,8 @@ public class Duke {
     private static final String DEADLINE = "deadline";
     private static final String TODO = "todo";
 
+    private static final String EMPTY = "";
+
 
     public static void main(String[] args) {
         greeting();
@@ -37,12 +39,15 @@ public class Duke {
                 displayList();
             } else {
                 String[] inputArr = userInput.split(" ");
-
-                if (inputArr[0].equals(DONE)) {
-                    int itemsIdx = Integer.parseInt(inputArr[1]) - 1;
-                    markAsDone(itemsIdx);
-                } else {
-                    addItem(userInput);
+                try {
+                    if (inputArr[0].equals(DONE)) {
+                        int itemsIdx = Integer.parseInt(inputArr[1]) - 1;
+                        markAsDone(itemsIdx);
+                    } else {
+                        addItem(userInput);
+                    }
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
                 }
             }
         }
@@ -65,7 +70,7 @@ public class Duke {
         }
     }
 
-    private static void addItem(String input) {
+    private static void addItem(String input) throws DukeException {
         String[] inputArr = input.split(" ", 2);
         String taskType = inputArr[0];
         String taskDescription = inputArr[1];
@@ -74,34 +79,50 @@ public class Duke {
 
         switch (taskType) {
             case TODO:
+                if (taskDescription.equals(EMPTY)) {
+                    throw new DukeException("Please key in a valid name for To Do");
+                }
                 newTask = new Todo(taskDescription);
                 break;
             case DEADLINE:
                 String[] deadlineArr = taskDescription.split("/by", 2);
+                if (deadlineArr.length != 2) {
+                    throw new DukeException("Please key in a valid name and date for the Deadline");
+                }
                 String deadlineName = deadlineArr[0];
                 String deadlineDate = deadlineArr[1];
+
+                if (deadlineDate.equals(EMPTY)) {
+                    throw new DukeException("Please key in a valid date for the Deadline");
+                }
                 newTask = new Deadline(deadlineName, deadlineDate);
                 break;
             case EVENT:
                 String[] eventArr = taskDescription.split("/at", 2);
+                if (eventArr.length != 2) {
+                    throw new DukeException("Please key in a valid name and date for the Event");
+                }
                 String eventName = eventArr[0];
                 String eventDuration = eventArr[1];
+
+                if (eventDuration.equals(EMPTY)) {
+                    throw new DukeException("Please key in a valid duration for the Event");
+                }
                 newTask = new Event(eventName, eventDuration);
                 break;
             default:
-                System.out.println("Task type does not exist.");
-                return;
+                throw new DukeException("I'm sorry, but I don't know what that means :-(");
         }
 
         items.add(newTask);
         String taskText = "Got it. I've added this task:" + "\n" + newTask + "\n";
-        String totalText = "Now you have " + items.size() + " tasks in the list.";
+        String totalText = "Now you have " + items.size() + " tasks in the list";
         System.out.println(taskText + totalText);
     }
 
-    private static void markAsDone(int itemsIdx) {
+    private static void markAsDone(int itemsIdx) throws DukeException{
         if (itemsIdx < 0 || itemsIdx > items.size() - 1) {
-            System.out.println("Sorry, task does not exist");
+            throw new DukeException("Sorry, the task does not exist");
         } else {
             items.get(itemsIdx).setDone();
             System.out.println("Nice! I've marked this task as done:");
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,12 @@
+public class DukeException extends Exception {
+
+    DukeException(String msg) {
+        super(msg);
+    }
+
+    @Override
+    public String toString() {
+        return "\uD83D\uDE40" + "OOPS!!! " + super.toString();
+    }
+
+}
