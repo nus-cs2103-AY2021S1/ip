099d4675d1399a2eedcbb1af46c4c3f341238779
SONGYI98 <e0392524@u.nus.edu>
2020-08-19 23:11:37 +0800

    Implement echo and exit features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String divider = "____________________________________________________________\n";
@@ -8,5 +10,17 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n\n";
         String introduction = "Hello! I'm Duke\nWhat can I do for you?\n";
         System.out.println(divider + logo + introduction + divider);
+
+        // Read input from input.txt
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String nextLine = sc.nextLine();
+            if (nextLine.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(nextLine);
+            }
+        }
     }
 }
