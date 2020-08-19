706d3bafff73f215e1e7b1c06496694f6dc1c741
David Liew <davidliew1999@gmail.com>
2020-08-19 20:39:43 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    List<String> list = new ArrayList<>();
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,16 +13,36 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hi! I'm Duke your friendly neighbourhood chat bot");
         System.out.println("What can i do for you?");
+        Duke duke = new Duke();
+        duke.run();
+
+    }
+
+    public void run() {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String input = sc.nextLine();
-            if (!input.equals("bye")) {
-                echo(input);
-            } else {
+            if (input.equals("list")) {
+                showList();
+            } else if (input.equals("bye")) {
                 System.out.println("        Bye have a good day!");
                 break;
+            } else {
+                add(input);
             }
+        }
+    }
 
+    public void add(String input) {
+        this.list.add(input);
+        System.out.println("        added: " + input);
+    }
+
+    public void showList() {
+        int counter = 1;
+        for (String s : list) {
+            System.out.println("        " + counter + ". " + s);
+            counter++;
         }
     }
 
