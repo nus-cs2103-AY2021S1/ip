85831d2d81bee3918f330d27c1e68bba6b793f9f
KOH-ZHE-HAO <lincolnkoh99@gmail.com>
2020-08-19 16:06:27 +0800

    Complete implementation of level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,10 +3,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Duke {
-    private static final String LINE_BREAK_BOT = "____________________________________________________________";
-    private static final String LINE_BREAK_USER = "************************************************************";
-    private static final String SPACE_BEFORE_LINE = "   ";
+    private static final String LINE_BREAK = "   ____________________________________________________________";
     private static final String SPACE_BEFORE_TEXT = "    ";
+    private static final String SPACE_FOR_ADDING = "     ";
 
     private static boolean isInteger (String str) {
         try {
@@ -21,52 +20,76 @@ public class Duke {
         List<Task> tasks = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+        System.out.println(LINE_BREAK);
         System.out.println(SPACE_BEFORE_TEXT + "Hey, I'm Emilia!" + "\n" +
                 SPACE_BEFORE_TEXT + "What can I do for you ♥ ?");
-        System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+        System.out.println(LINE_BREAK);
 
         while (sc.hasNext()) {
             String input = sc.nextLine().trim().toLowerCase();
-
+            System.out.println(LINE_BREAK);
             if (input.equals("bye")) {
-                System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT + "\n" + SPACE_BEFORE_TEXT +
-                        "Welcome back, master ♥ !" + "\n" + SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+                System.out.println(SPACE_BEFORE_TEXT + "Welcome back, master ♥ !" + "\n" +
+                        LINE_BREAK);
                 break;
 
             }
 
             if (input.equals("list")) {
-                System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_USER);
-                System.out.println(SPACE_BEFORE_LINE + "Please take a look at the tasks:");
+                System.out.println(SPACE_BEFORE_TEXT + "Please take a look at the tasks:");
                 int count = 1;
                 for (Task task : tasks) {
                     System.out.println(SPACE_BEFORE_TEXT + count + ". " + task);
                     count++;
                 }
-                System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+                System.out.println(LINE_BREAK);
                 continue;
             }
 
-            String[] process = input.split(" ");
+            String[] process = input.split(" ", 2);
             if (process[0].equals("done") && isInteger(process[1])) {
-                int index = Integer.parseInt(process[1]);
+                int index = Integer.parseInt(process[1]) - 1;
                 try {
                     Task task = tasks.get(index);
                     task.markAsDone();
-                    System.out.println(SPACE_BEFORE_LINE + "Understood, I've marked this task as done:" +
+                    System.out.println(SPACE_BEFORE_TEXT + "Understood, I've marked this task as done:" +
                             "\n" + task);
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("Sorry master, I can't seem to find the task...");
+                    System.out.println("Sorry, I can't seem to find the task...");
                 }
                 continue;
             }
 
-            Task current = new Task(input);
-            tasks.add(current);
-            System.out.println(SPACE_BEFORE_LINE + LINE_BREAK_USER + "\n" + SPACE_BEFORE_TEXT +
-                    "added: " + input + "\n" + SPACE_BEFORE_LINE + LINE_BREAK_BOT);
+            String firstWord = process[0];
+            Task current;
+            switch (firstWord) {
+                case "todo":
+                    current = new ToDo(process[1]);
+                    break;
+                case "deadline": {
+                    int byIndex = process[1].indexOf(" /by ");
+                    String deadline = process[1].substring(byIndex + 4).trim();
+                    String description = process[1].substring(0, byIndex).trim();
+                    current = new Deadline(description, deadline);
+                    break;
+                }
+                case "event": {
+                    int atIndex = process[1].indexOf(" /at ");
+                    String deadline = process[1].substring(atIndex + 4).trim();
+                    String description = process[1].substring(0, atIndex).trim();
+                    current = new Event(description, deadline);
+                    break;
+                }
+                default:
+                    System.out.println("Invalid input detected!");
+                    continue;
+            }
 
+            tasks.add(current);
+            System.out.println(SPACE_BEFORE_TEXT + "Understood! I've added this task: " +
+                    "\n" + SPACE_FOR_ADDING + current);
+            System.out.println(SPACE_BEFORE_TEXT + "You have " + tasks.size() + " tasks in your list now!");
+            System.out.println(LINE_BREAK);
         }
     }
 }
