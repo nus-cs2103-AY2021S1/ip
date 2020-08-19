14e9e7c61eeb0e85cb4fa624c363985397300c8e
iamjamestan <jamestanyuli@gmail.com>
2020-08-19 14:43:50 +0800

    level 6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,6 +15,7 @@ public class Duke {
                 "\tEvent: \"event <description> /at <time interval>\"\n" +
                 "Send \"list\" to see all tasks.\n" +
                 "Send \"done <item number>\" to mark an item as done\n" +
+                "Send \"delete <item number>\" to delete anj item from the list\n" +
                 "Send \"bye\" to end our conversation.");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
@@ -24,7 +25,7 @@ public class Duke {
                 switch (strings[0]) {
                     case "list":
                         if (tasks.isEmpty()) {
-                            throw new DukeException("Theres currently nothing in your list.");
+                            System.out.println("Theres currently nothing in your list.");
                         } else {
                             for (int i = 0; i < tasks.size(); i++) {
                                 System.out.printf("%d. %s%n", i + 1, tasks.get(i));
@@ -39,6 +40,17 @@ public class Duke {
                             System.out.println("\t" + tasks.get(itemNumber - 1));
                         } catch (NumberFormatException | IndexOutOfBoundsException ex) {
                             throw new DukeException("Please key in a valid number for \"done\"");
+                        }
+                        break;
+                    case "delete":
+                        try {
+                            int itemNumber = Integer.parseInt(input.split(" ")[1]);
+                            System.out.println("Noted. I've removed this task:");
+                            System.out.println("\t" + tasks.get(itemNumber - 1));
+                            tasks.remove(itemNumber - 1);
+                            System.out.println("You now have " + tasks.size() + " tasks in the list.");
+                        } catch (NumberFormatException | IndexOutOfBoundsException ex) {
+                            throw new DukeException("Please key in a valid number for \"delete\"");
                         }
                         break;
                     case "todo":
