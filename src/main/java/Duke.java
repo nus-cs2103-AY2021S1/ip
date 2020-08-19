a1483c97f8aec3b59e0dadcc9da6abda6d9d5b88
chan-j-d <chan.j.d.98@gmail.com>
2020-08-20 01:38:16 +0800

    Fixed the String parsing for list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static final String lineBreak = "    ____________________________________________________________\n";
-    private static final String preSpacing = "     ";
-    private static final String openingMessage = "Hello! I'm Duke\n" + preSpacing + "What can I do for you?";
-    private static final String closingMessage = "Bye. Hope to see you again soon!";
+    private static final String LINE_BREAK = "    ____________________________________________________________\n";
+    private static final String PRESPACING = "     ";
+    private static final String OPENING_MESSAGE = "Hello! I'm Duke\n" + PRESPACING + "What can I do for you?";
+    private static final String CLOSING_MESSAGE = "Bye. Hope to see you again soon!";
 
     private static final String CLOSING_STRING = "bye";
     private static final String LIST_STRING = "list";
@@ -22,16 +22,18 @@ public class Duke {
         String userInput = "";
 
         while (!userInput.equals(CLOSING_STRING)) {
-            userInput = scanner.next();
+            userInput = scanner.nextLine();
             switch (userInput) {
                 case CLOSING_STRING:
                     System.out.println(getClosingText());
                     break;
                 case LIST_STRING:
                     System.out.println(processString(getListString()));
+                    break;
                 default:
                     stringRecords.add(userInput);
                     System.out.println(processString(createAddedString(userInput)));
+                    break;
 
 
             }
@@ -43,7 +45,11 @@ public class Duke {
     private static String getListString() {
         StringBuilder builder = new StringBuilder();
         int count = 1;
+        boolean first = true;
         for (String string : stringRecords) {
+            if (!first) {
+                builder.append('\n' + PRESPACING);
+            } else first = false;
             builder.append(count++ + ". " + string);
         }
         return builder.toString();
@@ -55,15 +61,15 @@ public class Duke {
 
 
     private static String processString(String string) {
-        return lineBreak  + preSpacing + string + '\n' + lineBreak;
+        return LINE_BREAK  + PRESPACING + string + '\n' + LINE_BREAK;
     }
 
     private static String getOpeningText() {
-        return processString(openingMessage);
+        return processString(OPENING_MESSAGE);
     }
 
     private static String getClosingText() {
-        return processString(closingMessage);
+        return processString(CLOSING_MESSAGE);
     }
 
 
