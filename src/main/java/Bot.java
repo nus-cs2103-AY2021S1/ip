70f05884d83367ab438afb25d60fc3be397195fb
Gabriel Sim <gabrielsimbingyang@gmail.com>
2020-08-19 20:04:04 +0800

    Level-2

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,6 +5,9 @@ public class Bot {
     }
 
     String line = "____________________________________________________________";
+    String[] list = new String[100];
+    int listSize = 0;
+
 
     public void serve() {
         Scanner sc = new Scanner(System.in);
@@ -12,13 +15,29 @@ public class Bot {
             String s = sc.nextLine();
             if (s.equals("bye")) {
                 return;
+            } else if (s.equals("list")) {
+                returnListings(s);
             } else {
-                System.out.println(line + "\n" + s + "\n" + line);
-
+                addListings(s);
             }
 
-
         }
+    }
+
+    public void addListings(String listing) {
+        listing = "added: " + listing;
+        list[listSize] = listing;
+        listSize++;
+        System.out.println(line + "\n" + listing + "\n" + line);
+
+    }
+
+    public void returnListings(String listing) {
+        System.out.println(line);
+        for (int i = 0; i < listSize; i++) {
+            System.out.println((i + 1) + ". " + list[i]);
+        }
+        System.out.println(line);
     }
 
 
