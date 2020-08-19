8820f34cd5831a1fc288ed434d4f745675fa2077
nuovonatura <60500235+nuovonatura@users.noreply.github.com>
2020-08-20 05:06:53 +0800

    Level-2
    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -7,15 +8,22 @@ public class Duke {
         //Greetings.
         System.out.println("\tHello!\n\tI am Baymax, your personal idle time companion." +
                 "\n\tHow may I help you?");
-        //Echo
+        //Add List
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         while (running) {
             String text = scanner.nextLine();
             if (text.equals("bye")) {
-                text = "It was my pleasure assisting you.\n\tSee you next Time!";
+                System.out.println("\tIt was my pleasure assisting you.\n\tSee you next Time!");
                 running = false;
+            } else if (text.equals("list")) {
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println("\t" + i + ". " + list.get(i - 1));
+                }
+            } else {
+                list.add(text);
+                System.out.println("\tadded: " + text);
             }
-            System.out.println("\t" + text);
         }
     }
 }
