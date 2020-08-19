1828bef169042f5aed7d9eff4a34ee9ab8b989b6
fanminj <fanmin98@gmail.com>
2020-08-19 23:48:50 +0800

    Add DukeException.java to handle two types of errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,6 +9,48 @@ public class Duke {
     static final String EVENT = "event";
     static final String DEADLINE = "deadline";
 
+    protected static Task assignTask(String[] command) throws DukeException {
+        Task t;
+        String type = command[0];
+
+        if (command.length > 1) {
+            String description = command[1];
+            if (type.equals(DONE)) {
+                int taskNum = Integer.parseInt(description) - 1;
+                t = new Done(taskNum);
+            } else if (type.equals(TODO)) {
+                t = new ToDo(description);
+            } else if (type.equals(EVENT)) {
+                String[] s = description.split(" /at ");
+                String desc = s[0];
+                String at = s[1];
+                t = new Event(desc, at);
+            } else if (type.equals(DEADLINE)) {
+                String[] s = description.split(" /by ");
+                String desc = s[0];
+                String by = s[1];
+                t = new Deadline(desc, by);
+            } else {
+                throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :(\n");
+            }
+        } else {
+            if (type.equals(LIST)) {
+                t = new List();
+            } else if (type.equals(EXIT)) {
+                t = new Exit();
+            } else {
+                if (type.equals(DONE) || type.equals(TODO) || type.equals(EVENT) || type.equals(DEADLINE)) {
+                    String message = String.format("OOPS!!! The description of %s cannot be empty.\n", type);
+                    throw new DukeException(message);
+                } else {
+                    throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :(\n");
+                }
+            }
+        }
+
+        return t;
+    }
+
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -24,49 +66,18 @@ public class Duke {
         System.out.println(res);
 
         while (sc.hasNextLine()) {
-            Task t;
             String[] command = sc.nextLine().split(" ", 2);
-            String type = command[0];
 
-            if (command.length > 1) {
-                String description = command[1];
-
-                if (type.equals(DONE)) {
-                    int taskNum = Integer.parseInt(description) - 1;
-                    t = new Done(taskNum);
-                } else if (type.equals(TODO)) {
-                    t = new ToDo(description);
-                } else if (type.equals(EVENT)) {
-                    String[] s = description.split(" /at ");
-                    String desc = s[0];
-                    String at = s[1];
-                    t = new Event(desc, at);
-                } else if (type.equals(DEADLINE)) {
-                    String[] s = description.split(" /by ");
-                    String desc = s[0];
-                    String by = s[1];
-                    t = new Deadline(desc, by);
-                } else {
-                    // throw exception
-                    t = new Task("fail", "fail");
+            try {
+                Task t = assignTask(command);
+                echo.addTask(t);
+                String response = echo.replyUser();
+                System.out.println(response);
+                if (echo.toExit()) {
+                    break;
                 }
-            } else {
-                if (type.equals(LIST)) {
-                    t = new List();
-                } else if (type.equals(EXIT)) {
-                    t = new Exit();
-                } else {
-                    // throw exception
-                    t = new Task("fail", "fail");
-                }
-            }
-
-
-            echo.addTask(t);
-            String response = echo.replyUser();
-            System.out.println(response);
-            if (echo.toExit()) {
-                break;
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
 
