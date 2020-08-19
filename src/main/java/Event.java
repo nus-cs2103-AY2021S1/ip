ec5120977f0f4c28d3c38eb4881086424c0a84c7
yaoyuan <yaoyuan@u.nus.edu>
2020-08-19 22:27:15 +0800

    added Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+public class Event extends Task{
+    private String time;
+
+    public Event(String description, String time) {
+        super(description);
+        this.time = time;
+    }
+
+    public String toString() {
+        return "[E]" + super.toString()
+                + " (at: " + time + ")";
+    }
+}
