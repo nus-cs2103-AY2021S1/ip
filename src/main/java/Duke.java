325d47e0f287421817ccc1d862bbe097092888bc
Petrick Jerico <petrickjerico@gmail.com>
2020-08-19 22:39:06 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
     public static void displayMessage(String msg) {
-        System.out.println("\n––––––––––––––––––––– *** –––––––––––––––––––––");
-        System.out.println(msg);
         System.out.println("––––––––––––––––––––– *** –––––––––––––––––––––");
+        System.out.println(msg);
+        System.out.println("––––––––––––––––––––– *** –––––––––––––––––––––\n");
+    }
+
+    public static String getOrders(List<String> orders) {
+        int index = 1;
+        String orderMessage = "TO-DO LIST:";
+        for (String order : orders) {
+            orderMessage += String.format("\n   " + index + ". " + order);
+            index++;
+        }
+        return orderMessage;
     }
 
     public static void main(String[] args) {
+
+        List<String> orders = new ArrayList<>(100);
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -24,15 +38,28 @@ public class Duke {
         displayMessage(welcome);
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNext()) {
-            String order = sc.next();
+        while (sc.hasNextLine()) {
+
+            String order = sc.nextLine();
             if (order.equalsIgnoreCase("bye")) {
+
                 String goodbye = "Alright, see you soon!";
                 displayMessage(goodbye);
                 sc.close();
                 break;
+
+            } else if (order.equalsIgnoreCase("list")) {
+
+                displayMessage(getOrders(orders));
+
             } else {
-                displayMessage("You typed: " + order);
+
+                String orderMessage = "You typed: " + order + "\n"
+                        + "'" + order + "' added to list!";
+
+                orders.add(order);
+                displayMessage(orderMessage);
+
             }
         }
     }
