a06973877208a0b8877bf8bb9da5d8a97045a84f
ChengJiyuqing <chengjiyuqing@gmail.com>
2020-08-20 01:30:46 +0800

    Level-1, Greet, Echo, Exit
    Level-1, Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //Beginning of my own code:
+        System.out.println("Hello! Duke at your serve. Please name your request.");
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String userMessage = sc.nextLine();
+            if (!userMessage.equals("bye")) {
+                System.out.println(userMessage);
+            }
+
+            if (userMessage.equals("bye")) {
+                System.out.println("Bye! Nice serving you. Hope to see you again soon! :D");
+                break;
+            }
+        }
+
+
     }
 }
