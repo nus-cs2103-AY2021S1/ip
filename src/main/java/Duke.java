bc844d524fa82019437b096ae384d3fae266d499
Gabriel Sim <gabrielsimbingyang@gmail.com>
2020-08-19 18:54:23 +0800

    test

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("test");
     }
 }
