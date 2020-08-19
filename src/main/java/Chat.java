c18062f097856f2fdd9e09cebc96ea46dc4183ca
akgrenSoar <44816346+akgrenSoar@users.noreply.github.com>
2020-08-19 20:28:26 +0800

    Level-3

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,28 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Chat {
 
-    private final List<String> history;
+    private final List<Task> taskList;
+    Pattern donePattern = Pattern.compile("^done (\\d+)\\s*$");
 
     public Chat() {
-        this.history = new ArrayList<>(100);
-    }
-
-    private void echo(String input) {
-        System.out.println("Echo: " + input);
-    }
-
-    private void add(String input) {
-        this.history.add(input);
+        this.taskList = new ArrayList<>(100);
     }
 
     private void list() {
         int i = 0;
-        for (String item : this.history) {
-            System.out.println(++i + ". " + item);
+        for (Task item : this.taskList) {
+            System.out.println(++i + ". " + item.toString());
         }
+    }
+
+    private void markDone(Task task) {
+        task.setCompleted(true);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("    " + task.toString());
     }
 
     public void run() {
@@ -35,17 +36,26 @@ public class Chat {
             System.out.print("> ");
             input = scanner.nextLine();
 
-            if (input.equals("bye")) {
+            if (input.equals("bye")) { // Exit
                 break;
             }
 
-            if (input.equals("list")) {
+            if (input.equals("list")) { // List all tasks
                 list();
                 continue;
             }
 
-            add(input);
-            echo(input);
+            Matcher matcher = donePattern.matcher(input);
+            if (matcher.matches()) { // Mark task as completed
+                int taskIndex = Integer.parseInt(matcher.group(1)) - 1;
+                markDone(this.taskList.get(taskIndex)); // TODO: Catch IndexOutOfBoundsException
+                continue;
+            }
+
+            // Add new task and echo
+            Task task = new Task(input);
+            this.taskList.add(task);
+            System.out.println("Added: " + task.getDescription());
 
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+
+
+public class Task {
+
+    private String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return (completed ? "[✓] " : "[✗] ") + description;
+    }
+
+}
