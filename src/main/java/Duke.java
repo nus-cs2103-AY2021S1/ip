52ff3b30f0b48df2b88c70294607592d8272e665
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-08-19 18:40:34 +0800

    level2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Duke {
-    final static String LINE = "____________________________________________________________\n";
     final static String BYE = "aight imma head out\n";
     final static String WELCOME = "Hello! I'm Duke\nWhat can I do for you?\n";
+    final static String LINE = "____________________________________________________________\n";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,13 +13,42 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println(LINE + WELCOME + "\n" + LINE);
+        print(WELCOME);
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        while(!input.equals("bye")) {
-            System.out.println(LINE + input + "\n" + LINE);
+        while (!input.equals("bye")) {
+            if (input.isEmpty()) {
+                continue;
+            }
+            String[] words = input.split(" ");
+            String command = words[0];
+            switch (command) {
+                case "list":
+                    print_tasks(tasks);
+                    break;
+                default:
+                    Task newTask = new Task(input, false);
+                    print("added: " + newTask.toString());
+                    tasks.add(newTask);
+                    break;
+            }
+
+//            System.out.println(LINE + input + "\n" + LINE);
             input = scanner.nextLine();
         }
-        System.out.println(LINE + BYE + LINE);
+        print(BYE);
+    }
+
+    public static void print_tasks(ArrayList<Task> tasks){
+        System.out.print(LINE);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.print(i + ". " + tasks.get(i));
+        }
+        System.out.print(LINE);
+    }
+
+    public static void print(String str){
+        System.out.print(LINE + str + LINE);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    String text;
+    boolean done;
+    Task(String text, boolean done){
+        this.text = text;
+        this.done = done;
+    }
+    public String toString(){
+        return text + "\n";
+    }
+}
