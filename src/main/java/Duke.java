a545789d31658739fb47f37574cc21519b2fa1de
ameliatjy <e0425671@u.nus.edu>
2020-08-20 00:36:14 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Duke {
     public static ArrayList<Task> userInputs = new ArrayList<>();
 
-    public static void viewTasks() {
+    public static void viewTasks() throws DukeException {
         if (userInputs.size() == 0) { // user has not added any task
-            System.out.println("Nothing has been added to the list yet!");
+            throw new DukeException("Nothing has been added to the list yet!");
         } else {
             for (int i = 0; i < userInputs.size(); i++) {
                 String output = (i + 1) + ". " + userInputs.get(i).toString();
@@ -15,40 +15,42 @@ public class Duke {
         }
     }
 
-    public static void markDone(String inputMsg) {
+    public static void markDone(String inputMsg) throws DukeException {
         int taskNumber = Integer.valueOf(inputMsg.split(" ")[1]); // gets the done task number
         Task currTask = userInputs.get(taskNumber - 1);
         if (currTask.getStatus()) { // task has already marked done before
-            System.out.println("Task has already been completed earlier on!");
+            throw new DukeException("Task has already been completed earlier on!");
         } else {
             currTask.markAsComplete();
             System.out.println("Nice! I've marked this task as done:\n" + currTask.toString());
         }
     }
 
-    public static void addToList(String inputMsg, String actionType) {
-        String outputMsg = "";
-        if (actionType.equals("todo")) {
-            String taskName = inputMsg.substring(5);
-            Task newTask = new Todo(taskName, false);
-            userInputs.add(newTask);
-            outputMsg += "Got it. I've added this task:\n" + newTask.toString();
-        } else if (actionType.equals("deadline")) {
-            String taskName = inputMsg.split("/")[0].substring(9);
-            String deadline = inputMsg.split("/")[1].substring(3);
-            Task newTask = new Deadline(taskName, false, deadline);
-            userInputs.add(newTask);
-            outputMsg += "Got it. I've added this task:\n" + newTask.toString();
-        } else if (actionType.equals("event")) {
-            String taskName = inputMsg.split("/")[0].substring(6);
-            String deadline = inputMsg.split("/")[1].substring(3);
-            Task newTask = new Deadline(taskName, false, deadline);
-            userInputs.add(newTask);
-            outputMsg += "Got it. I've added this task:\n" + newTask.toString();
-        } else { // when user keys in unregistered action
-            outputMsg += "Specified action is not recognised.";
+    public static void addToList(String inputMsg, String actionType) throws DukeException {
+        Task newTask;
+        int numOfWords = inputMsg.split(" ").length;
+        if (numOfWords <= 1) {
+            throw new DukeException("Description of task cannot be empty!");
+        } else {
+            if (actionType.equals("todo")) {
+                String taskName = inputMsg.substring(5);
+                newTask = new Todo(taskName, false);
+            } else if (actionType.equals("deadline")) {
+                String taskName = inputMsg.split("/")[0].substring(9);
+                String deadline = inputMsg.split("/")[1].substring(3);
+                newTask = new Deadline(taskName, false, deadline);
+            } else if (actionType.equals("event")) {
+                String taskName = inputMsg.split("/")[0].substring(6);
+                String deadline = inputMsg.split("/")[1].substring(3);
+                newTask = new Deadline(taskName, false, deadline);
+            } else { // when user keys in unregistered action
+                throw new DukeException("Specified action is not recognised.");
+            }
         }
-        outputMsg += "\nYou have " + userInputs.size() + " tasks in the list.";
+        userInputs.add(newTask);
+        String outputMsg = "Got it. I've added this task:\n"
+                + newTask.toString()
+                + "\nYou have " + userInputs.size() + " tasks in the list.";
         System.out.println(outputMsg);
     }
 
@@ -66,11 +68,23 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (inputMsg.equals("list")) { // sees all tasks
-                viewTasks();
+                try {
+                    viewTasks();
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
             } else if (actionType.equals("done")) { // mark task as done
-                markDone(inputMsg);
+                try {
+                    markDone(inputMsg);
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
             } else { // add task to list
-                addToList(inputMsg, actionType);
+                try {
+                    addToList(inputMsg, actionType);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
             }
         }
         sc.close();
