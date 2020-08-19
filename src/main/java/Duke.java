b73ad3720a750671dae1135897e32da1f086eee8
Christopher Goh <chrisgzf@gmail.com>
2020-08-20 05:54:28 +0800

    feat: Add command echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm Duke!");
+        System.out.println("What can I do for you?");
+
+        String userInput;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("$ ");
+            userInput = scanner.nextLine();
+
+            switch(userInput) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    return;
+                default:
+                    System.out.println(userInput);
+            }
+        }
     }
 }
