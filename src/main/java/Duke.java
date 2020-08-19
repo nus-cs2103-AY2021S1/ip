bb13e545290308db1df44de4cc28745992cfa5b2
SONGYI98 <e0392524@u.nus.edu>
2020-08-19 23:44:14 +0800

    Implement add and list features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        // Create an empty list of tasks
+        ArrayList<Task> tasks = new ArrayList<>();
+
         String divider = "____________________________________________________________\n";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,17 +13,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n\n";
         String introduction = "Hello! I'm Duke\nWhat can I do for you?\n";
+        String add = "added: ";
+
+        // Print Duke's introduction
         System.out.println(divider + logo + introduction + divider);
 
         // Read input from input.txt
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String nextLine = sc.nextLine();
-            if (nextLine.equals("bye")) {
+            if (nextLine.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    int number = i + 1;
+                    System.out.println(number + ". " + tasks.get(i).getText());
+                }
+            } else if (nextLine.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else {
-                System.out.println(nextLine);
+                tasks.add(new Task(nextLine));
+                System.out.println(add + nextLine);
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+
+    private String text;
+
+    public Task(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
