ddf8bbf46734afa4bf844f7b27d0f982b9668f6f
zhizhizhizhizhi <zhizhi.xu@yahoo.com>
2020-08-20 01:56:55 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,66 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
-    static ArrayList<String> list = new ArrayList<>();
+    static class Task {
+        boolean completed = false;
+        String name;
+
+        Task(String name) {
+            this.name = name;
+        }
+        public void setCompleted() {
+            completed = true;
+        }
+
+        @Override
+        public String toString() {
+            return (completed ? "[✓]" : "[✗]") + " " + name;
+        }
+
+    }
+
+    static boolean isNum(String s) {
+        if (s == null) {
+            return false;
+        }
+        try {
+            int d = Integer.parseInt(s);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    static void print(String s) {
+        System.out.println("    ____________________________________________________________\n" +
+                s +
+                "    ____________________________________________________________\n");
+    }
+    static ArrayList<Task> list = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s;
-        System.out.println("    ____________________________________________________________\n" +
-                "     Hello! I'm Duke\n" +
-                "     What can I do for you?\n" +
-                "    ____________________________________________________________\n");
+        print("     Hello! I'm Duke\n" +
+                "     What can I do for you?\n");
         while(!(s = scanner.nextLine()).equals("bye")) {
-            if(s.equals("list")) {
+            String[] done = s.split(" ");
+            if (done.length == 2 && done[0].equals("done") && isNum(done[1])
+                    && Integer.parseInt(done[1]) <= list.size() && Integer.parseInt(done[1]) > 0) {
+                list.get(Integer.parseInt(done[1]) - 1).setCompleted();
+                print("     Nice! I've marked this task as done:\n" +
+                        "     " + list.get(Integer.parseInt(done[1]) - 1) + "\n");
+            } else if(s.equals("list")) {
                 String temp = "";
                 for(int i = 0; i < list.size(); i++) {
                     temp += "     " + (i+1) + ". " + list.get(i) + "\n";
                 }
-                System.out.println("    ____________________________________________________________\n" +
-                        temp +
-                        "    ____________________________________________________________\n");
+                print("     Here are the tasks in your list:\n" + temp);
             } else {
-                list.add(s);
-                System.out.println("    ____________________________________________________________\n" +
-                        "    added: " + s + "\n" +
-                        "    ____________________________________________________________\n");
+                list.add(new Task(s));
+                print("    added: " + s + "\n");
             }
         }
-        System.out.println("    ____________________________________________________________\n" +
-                "     Bye. Hope to see you again soon!\n" +
-                "    ____________________________________________________________");
+        print("     Bye. Hope to see you again soon!\n");
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
