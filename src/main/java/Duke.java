ea9e52cc636fcd4e9dcf51a97b99d0816826dc3d
David Liew <davidliew1999@gmail.com>
2020-08-19 20:24:14 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hi! I'm Duke your friendly neighbourhood chat bot");
+        System.out.println("What can i do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if (!input.equals("bye")) {
+                echo(input);
+            } else {
+                System.out.println("        Bye have a good day!");
+                break;
+            }
+
+        }
+    }
+
+    public static void echo(String input) {
+        System.out.println("        " + input);
     }
 }
