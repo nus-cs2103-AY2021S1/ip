4a639bc2634720d402fcc1108460c1c4102dd0a3
yu-ming-chen <chen.yuming.mao@gmail.com>
2020-08-19 15:15:45 +0800

    Level-4 completed, added Deadline, Todo and Event class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,31 +22,43 @@ public class Duke {
     }
 
     public void echo(String input) {
-        System.out.println("---------------------------");
+        System.out.println("------------------------------------");
         System.out.println(input);
-        System.out.println("---------------------------");
+        System.out.println("------------------------------------");
     }
 
     public void byeMessage() {
-        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-");
         System.out.println("Bye. Hope to see you again soon!!!");
-        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-");
     }
 
     public void run() {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
             String str = sc.nextLine();
-            String arr[] = str.split(" ", 100);
+            String arr[] = str.split(" ");
             String keyWord = arr[0];
             if (keyWord.equals("bye")) {
                 break;
             } else if (keyWord.equals("list")) {
                 showList();
-            } else if(str.length() >= 6 && keyWord.equals("todo")) {
-                Task task = new Task(str.substring(5));
+            } else if(keyWord.equals("todo")) {
+                Todo task = new Todo(str.substring(5));
                 addToList(task);
-            } else if(str.length() >= 6 && keyWord.equals("done")) {
+            } else if(keyWord.equals("deadline") && str.contains("/by")){
+                String holder[] = str.split("deadline")[1].split("/by ");
+                String description = holder[0].trim();
+                String by = holder[1].trim();
+                Deadline task = new Deadline(description, by);
+                addToList(task);
+            } else if (keyWord.equals("event") && str.contains("/at")){
+                String holder[] = str.split("event")[1].split("/at ");
+                String description = holder[0].trim();
+                String at = holder[1].trim();
+                Event task = new Event(description, at);
+                addToList(task);
+            } else if(keyWord.equals("done")) {
                 int index = Character.getNumericValue(str.charAt(5));
                 completeTask(index);
             }
@@ -56,27 +68,27 @@ public class Duke {
 
     public void completeTask(int index) {
         list.get(index-1).markAsDone();
-        System.out.println("---------------------------");
+        System.out.println("------------------------------------");
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(list.get(index-1).toString());
-        System.out.println("---------------------------");
+        System.out.println("------------------------------------");
     }
 
     public void addToList(Task task) {
         list.add(task);
-        System.out.println("---------------------------");
+        System.out.println("------------------------------------");
         System.out.println("Got it. I've added this task:");
         System.out.println(task.toString());
         System.out.println(String.format("Now you have %d tasks in the list.", list.size()));
-        System.out.println("---------------------------");
+        System.out.println("------------------------------------");
 
     }
     public void showList() {
-        System.out.println("---------------------------");
+        System.out.println("------------------------------------");
         for (int i = 0; i < list.size(); i++) {
             System.out.println(String.format("%d. ", i+1) + list.get(i));
         }
-        System.out.println("---------------------------");
+        System.out.println("------------------------------------");
     }
     public static void main(String[] args) {
         Duke bot = new Duke();
