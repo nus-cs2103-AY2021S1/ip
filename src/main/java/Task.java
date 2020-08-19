e0c95109d72ead587a5d952ee32636a5e6dc951e
xnmng <puaxuanmingpxm18@gmail.com>
2020-08-19 14:04:43 +0800

    implemented the task class to encapsulate the concept of a task

---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+// partial solution template copied from Extension: A-Classes
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+}
