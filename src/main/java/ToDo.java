12065c9579387b5f3a26324b0daab531ec8456f3
zeying99 <758504129@qq.com>
2020-08-20 01:10:47 +0800

    Revert "Revert "task 4 ToDos, Events, Deadlines (there can still have random task other than these three types)""
    This reverts commit d40037dbc2c5451a43e7bf171b7e6ca70a80c214.

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,13 @@
+public class ToDo extends Task {
+    protected String at;
+
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+
+}
