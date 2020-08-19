7b87611909e8647ba504f400ec2cfddf5907e807
zeying99 <758504129@qq.com>
2020-08-20 03:12:52 +0800

    Testing (However there is some bugs with random number appearing for no reasons. Need to clarify with TA)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,20 +3,28 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("    ____________________________________________________________\n" +
+        System.out.println(
+                "Hello from\n" +
+                        " ____        _        \n" +
+                        "|  _ \\ _   _| | _____ \n" +
+                        "| | | | | | | |/ / _ \\\n" +
+                        "| |_| | |_| |   <  __/\n" +
+                        "|____/ \\__,_|_|\\_\\___|\n" +
+                "____________________________________________________________\n" +
                 "     Hello! I'm Duke\n" +
                 "     What can I do for you?\n" +
-                "    ____________________________________________________________\n");
+                "____________________________________________________________\n");
         boolean stop = false;
         Userinput userinput = new Userinput();
-        while(!stop){
+        while(sc.hasNextLine()){
             String input = sc.nextLine();
             String output = userinput.getDukeResponse(input);
             System.out.println(output);
             stop = userinput.getTerminate();
-        }
-        if (stop) {
-            sc.close();
+            if(stop){
+                sc.close();
+                break;
+            }
         }
     }
 }
