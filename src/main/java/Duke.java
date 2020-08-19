789250596160c5b1e959aef946593a5f00d38195
hejl <hejl20150101@gmail.com>
2020-08-19 14:51:10 +0800

    Modified chatbot display format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-        String greeting = "Hello! I'm Duke\n" + "What can I do for you?";
+        String separation_line = "    ____________________________________________________________\n";
+        String indent = "     ";
+        String starting_line = separation_line + indent;
+        String ending_line = "\n" + separation_line;
+        String greeting = starting_line + "Hello! I'm Duke\n" + indent + "What can I do for you?" + ending_line;
         System.out.println(greeting);
 
         boolean exit_bye = false;
@@ -18,10 +22,10 @@ public class Duke {
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(starting_line + "Bye. Hope to see you again soon!" +  ending_line);
                 exit_bye = true;
             } else {
-                System.out.println(input);
+                System.out.println(starting_line + input + ending_line);
             }
         }
     }
