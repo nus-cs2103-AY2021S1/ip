23779e3785b27c9cdbfa05f47d7379678c4b8bfc
rgabelarde <rgabelarde@gmail.com>
2020-08-20 04:10:35 +0800

    Allowed duke to deal with errors such as incorrect inputs entered by the user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
         System.out.println("____________________________________________________________\n");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException{
         Scanner userInput = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
 
@@ -20,43 +20,63 @@ public class Duke {
         String input = userInput.nextLine();
 
         while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                printBorder();
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
-                }
-                printBorder();
-            } else if (input.contains("done")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                tasks.get(index).markAsDone();
-                printBorder();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(tasks.get(index));
-                printBorder();
-            } else {
-                printBorder();
-                System.out.println("Got it. I've added this task:");
-
-                if (input.contains("todo")) {
-                    Todo t = new Todo(input.substring(5));
-                    tasks.add(t);
-                    System.out.println(t);
-                } else {
-                    int due = input.indexOf("/");
-
-                    if (input.contains("deadline")) {
-                        Deadline dl = new Deadline(input.substring(9, due), input.substring(due + 4));
-                        tasks.add(dl);
-                    } else if (input.contains("event")) {
-                        Event e = new Event(input.substring(6, due), input.substring(due + 4));
-                        tasks.add(e);
+                if (input.equals("list")) {
+                    printBorder();
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
                     }
-                    System.out.println(tasks.get(tasks.size() - 1));
+                    printBorder();
+                } else if (input.contains("done")) {
+                    int index = Integer.parseInt(input.substring(5)) - 1;
+                    tasks.get(index).markAsDone();
+                    printBorder();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks.get(index));
+                    printBorder();
+                } else {
+                    printBorder();
+                    if (input.contains("todo")) {
+                        try {
+                            Todo t = new Todo(input.substring(5));
+                            tasks.add(t);
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println(t);
+                        } catch (Exception e) {
+                            throw new DukeException(" ☹ OOPS!!! The description of a todo cannot be empty.");
+                        }
+                    } else if (input.contains("deadline") || input.contains("event")){
+                        int due = input.indexOf("/");
+
+                        if (input.contains("deadline")) {
+                            try {
+                                Deadline dl = new Deadline(input.substring(9, due), input.substring(due + 4));
+                                System.out.println("Got it. I've added this task:");
+                                tasks.add(dl);
+                            } catch (Exception e) {
+                                throw new DukeException(" ☹ OOPS!!! The description of a deadline cannot be empty.");
+                            }
+                        } else if (input.contains("event")) {
+                            try {
+                                Event e = new Event(input.substring(6, due), input.substring(due + 4));
+                                System.out.println("Got it. I've added this task:");
+                                tasks.add(e);
+                            } catch (Exception e) {
+                                throw new DukeException(" ☹ OOPS!!! The description of a todo cannot be empty.");
+                            }
+                        }
+                        try {
+                            System.out.println(tasks.get(tasks.size() - 1));
+                            System.out.println("Now you have " + tasks.size() + " tasks in the list.\n");
+                        } catch (Exception e) {
+                            throw new DukeException("failed to provide task info sufficiently");
+                        }
+                    }
+                    else {
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
                 }
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.\n");
-                printBorder();
-            }
+            printBorder();
             input = userInput.nextLine();
         }
         printBorder();
