4b07d151cb643f566139ea9277fed53e9de81883
nicholasyeo <65457870+nicholasyeo@users.noreply.github.com>
2020-08-19 22:23:07 +0800

    level 1 implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public void greet() {
+        System.out.println("Hello! I'm Duke.");
+        System.out.println("What can I do for you?\n");
+    }
+
+    public void echo() {
+        boolean exit = false;
+        Scanner sc = new Scanner(System.in);
+        while (! exit) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                exit = true;
+                sc.close();
+            } else {
+                System.out.println(command + "\n");
+            }
+        }
+    }
+
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.greet();
+        duke.echo();
+        duke.exit();
     }
 }
