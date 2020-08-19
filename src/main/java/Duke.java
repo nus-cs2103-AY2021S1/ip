61da79bbb93091483eb5bf6ecda86d07f908f384
chan-j-d <chan.j.d.98@gmail.com>
2020-08-20 02:25:46 +0800

    Added ToDos, Deadlines and Events to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,10 +9,15 @@ public class Duke {
     protected static final String OPENING_MESSAGE = "Hello! I'm Duke\n" + PRESPACING + "What can I do for you?";
     protected static final String CLOSING_MESSAGE = "Bye. Hope to see you again soon!";
     protected static final String MARKED_MESSAGE = "Nice! I've marked this task as done: ";
+    protected static final String ADDED_MESSAGE = "Got it. I've added this task: ";
 
     protected static final String CLOSING_STRING = "bye";
     protected static final String LIST_STRING = "list";
     protected static final String DONE_STRING = "done";
+
+    protected static final String TO_DO = "todo";
+    protected static final String DEADLINE = "deadline";
+    protected static final String EVENT = "event";
 
     protected static final List<Task> taskRecords = new ArrayList<Task>();
 
@@ -60,6 +65,36 @@ public class Duke {
             builder.append(count++ + "." + task.toString());
         }
         return builder.toString();
+    }
+
+    protected static String addTask(String taskDescription) {
+        String[] details = taskDescription.split(" ", 2);
+        Task addedTask;
+        switch(details[0]) {
+            case TO_DO:
+                addedTask = new ToDos(details[1]);
+                break;
+            case DEADLINE:
+                addedTask = new Deadlines(details[1]);
+                break;
+            case EVENT:
+                addedTask = new Events(details[1]);
+                break;
+            default:
+                addedTask = new Task(taskDescription);
+        }
+
+        taskRecords.add(addedTask);
+        return processString(
+                ADDED_MESSAGE + '\n' +
+                        PRESPACING + "   " + addedTask + '\n' +
+                        PRESPACING + createTaskNumberCountMessage(taskRecords.size())
+        );
+
+    }
+
+    protected static String createTaskNumberCountMessage(int count) {
+        return "Now you have " + count + " tasks in the list.";
     }
 
     protected static String createAddedString(String addedString) {
