ccbc22a28cf316691a00b0bcd0816c3b1854bd01
yu-ming-chen <chen.yuming.mao@gmail.com>
2020-08-19 13:28:11 +0800

    Level-2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,15 @@
+package main.java;
+
 import java.util.*;
 
 public class Duke {
+
+    List<String> list;
+
+
+    public Duke(){
+        list = new ArrayList<>();
+    }
 
     public void sayHi() {
         String logo = " ____        _        \n"
@@ -28,12 +37,29 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
             String str = sc.nextLine();
-            if (str.equals("bye")) {
+            if (str.equalsIgnoreCase("bye")) {
                 break;
+            } else if (str.equalsIgnoreCase("list")) {
+                showList();
+            } else {
+                addToList(str);
             }
-            echo(str);
         }
         byeMessage();
+    }
+
+    public void addToList(String task) {
+        list.add(task);
+        System.out.println("---------------------------");
+        System.out.println("added: " + task);
+        System.out.println("---------------------------");
+    }
+    public void showList() {
+        System.out.println("---------------------------");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(String.format("%d. ", i+1) + list.get(i));
+        }
+        System.out.println("---------------------------");
     }
     public static void main(String[] args) {
         Duke bot = new Duke();
