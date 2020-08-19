d837b3da8986c1adbb2be066245fb5555c7f1c69
yaoyuan <yaoyuan@u.nus.edu>
2020-08-19 18:15:40 +0800

    Greet, Echo, Exit for Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    static String line = "—————————————————————————————————————————————————————————————————";
+
+    public static void greeting() {
+        String str = ("\t" + line + "\n"
+                + "\tHello! I'm Duke\n"
+                + "\tWhat can I do for you?\n"
+                + "\t" + line);
+        System.out.println(str);
     }
+
+    public static void repeat(String input){
+        if (input.equals("bye")) {
+            System.out.println("\t" + line + "\n"
+                    + "\tBye. Hope to see you again soon!\n"
+                    + "\t" + line);
+        } else {
+            System.out.println( "\t" + line + "\n"
+                    + "\t" + input + "\n"
+                    + "\t" + line);
+        }
+    }
+
+    public static void main(String[] args) {
+        greeting();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                repeat(input);
+                sc.close();
+                break;
+            } else {
+                repeat(input);
+            }
+        }
+    }
+
 }
