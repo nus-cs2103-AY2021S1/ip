23c12a6aa8adcf28276d61d5ffd22536b3c53fbd
Ma-Yueran <mayuerantom@outlook.com>
2020-08-19 20:44:44 +0800

    Make execute of DeadlineCommand.java and EventCommand.java throw IncorrectFormatException

--- src/DeadlineCommand.java
+++ src/DeadlineCommand.java
@@ -1,3 +1,6 @@
+import exception.IncorrectFormatException;
+import exception.NoDescriptionException;
+
 public class DeadlineCommand extends Command {
 
     public DeadlineCommand() {
@@ -7,8 +10,17 @@ public class DeadlineCommand extends Command {
     @Override
     public void execute(String str) {
         String[] splitStr = str.split(" /by ", 2);
+
+        if (splitStr.length != 2) {
+            String line = UIPrint.getLine(UIPrint.star, 50);
+            String errMessage =
+                    line + "\nPlease follow the format of deadline <task description> /by <deadline>\n" + line;
+            throw new IncorrectFormatException(errMessage);
+        }
+
         String description = splitStr[0];
         String deadline = splitStr[1];
+
         Deadline newDeadline = new Deadline(UIPrint.deadlineIcon, description, deadline);
 
         Duke.tasks.add(newDeadline);
--- src/EventCommand.java
+++ src/EventCommand.java
@@ -1,3 +1,6 @@
+import exception.IncorrectFormatException;
+import exception.NoDescriptionException;
+
 public class EventCommand extends Command {
 
     public EventCommand() {
@@ -7,8 +10,17 @@ public class EventCommand extends Command {
     @Override
     public void execute(String str) {
         String[] splitStr = str.split(" /at ", 2);
+
+        if (splitStr.length != 2) {
+            String line = UIPrint.getLine(UIPrint.star, 50);
+            String errMessage =
+                    line + "\nPlease follow the format of event <task description> /at <event time>\n" + line;
+            throw new IncorrectFormatException(errMessage);
+        }
+
         String description = splitStr[0];
         String time = splitStr[1];
+
         Event newEvent = new Event(UIPrint.eventIcon, description, time);
 
         Duke.tasks.add(newEvent);
