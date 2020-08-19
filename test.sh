ee01ee3dc7301f1a0e6f95e87d212c02b95d255d
zhiayang <zhiayang@nascentnoodle.com>
2020-08-19 14:02:12 +0800

    Add test script, remove old stuff

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+javac -target 11 -source 11 src/main/java/*.java -d bin/
+java -cp bin Main < text-ui-test/input.txt | diff -bu text-ui-test/EXPECTED.TXT -
+
+if [ $? -ne 0 ]; then
+	echo "test failed"
+	exit -1
+else
+	echo "test passed"
+	exit 0
+fi
