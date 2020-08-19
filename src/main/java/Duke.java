30886f91de51c6e927fb7997f59e7be7337482c5
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-08-19 12:06:48 +0800

    Added echo functionality
    Chatbot will now echo back any input typed in by the user, unless the word "bye" is typed, following which it will exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm Chatterbox. What can I do for you?");
+
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+
+            input = s.nextLine();
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Goodbye! Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
