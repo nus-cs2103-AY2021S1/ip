a18dcac959ab9d59e43a5a8a5d4fdc46eb25c48d
zhengweii <limzhengwei_99@hotmail.com>
2020-08-19 17:58:54 +0800

    Added greeting and echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+
+        echo();
+    }
+
+    private static void greet() {
+        System.out.println("Hello! I am Wish\n" + "What can I do for you?");
+    }
+
+    private static void echo() {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        while (!s.equals("bye")) {
+            System.out.println(s);
+            s = sc.nextLine();
+        }
+
+        System.out.println("Goodbye! All the best and see you again soon!");
     }
 }
