7bdb3195297b18e324ac303b34c0bc8128c04ea8
Ting Xiao <tigerting98@gmail.com>
2020-08-19 22:49:52 +0800

    Add task addition and task listing functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
+        ArrayList<String> toDoList = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -19,8 +21,15 @@ public class Duke {
             if (input.toLowerCase().equals("bye")) {
                 System.out.println("Well I'll see you around, pardner!!");
                 break;
+            } else if (input.toLowerCase().equals("list")) {
+                System.out.println("Here's yer current list of thingymajigs");
+                for(int i = 0; i < toDoList.size();i++ ) {
+                    String index = Integer.toString(i + 1);
+                    System.out.println(i + 1 + ". " + toDoList.get(i));
+                }
             } else {
-                System.out.println(input);
+                System.out.println("Alright, I'll put it on yer tab: " + input);
+                toDoList.add(input);
             }
         }
     }
