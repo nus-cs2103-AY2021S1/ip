501877b2d7243556ed81b2e996bda7257ad19b67
zeying99 <758504129@qq.com>
2020-08-20 01:09:58 +0800

    task 4 ToDos, Events, Deadlines (there can still have random task other than these three types)

--- src/main/java/Userinput.java
+++ src/main/java/Userinput.java
@@ -10,7 +10,91 @@ public class Userinput {
 
     String getDukeResponse(String input) {
         String[] words = input.split(" ");
-        if (!words[0].equals("done")) {
+        String taskGrammar = "task";
+        if (this.tasks.size() > 0) {
+            taskGrammar = "tasks";
+        }
+
+        if (words[0].equals("deadline")) {
+            //deadline cases
+            String deadlineTask = "";
+            String deadlineDate = "";
+            int byPosition = 0;
+            for (int i = 1;  i < words.length; i++) {
+                if (!words[i].equals("/by")) {
+                    deadlineTask = deadlineTask + words[i] + " ";
+                } else {
+                    byPosition = i;
+                    break;
+                }
+            }
+
+            for (int k = byPosition + 1; k < words.length; k++) {
+                if (k != words.length-1) {
+                    deadlineDate = deadlineDate + words[k] + " ";
+                } else {
+                    deadlineDate = deadlineDate + words[k];
+                }
+            }
+            Deadline newDeadline = new Deadline(deadlineTask,deadlineDate);
+            this.tasks.add(newDeadline);
+            String addNewDeadlineString = "____________________________________________________________\n" +
+                    "     Got it. I've added this task: \n" +
+                    "       " + newDeadline.toString() + "\n" +
+                    "     Now you have " + this.tasks.size() + " "+ taskGrammar + " in the list.\n" +
+                    "____________________________________________________________\n";
+            return addNewDeadlineString;
+
+        }  else if (words[0].equals("todo")) {
+            //todo case
+            String todoTask = "";
+            for (int i = 1; i < words.length; i++) {
+                todoTask = todoTask + words[i] + " ";
+            }
+            ToDo newToDo = new ToDo(todoTask);
+            this.tasks.add(newToDo);
+            String addNewToDo = "____________________________________________________________\n" +
+                    "     Got it. I've added this task: \n" +
+                    "       " + newToDo.toString() + "\n" +
+                    "     Now you have " + this.tasks.size() + " "+ taskGrammar + " in the list.\n" +
+                    "____________________________________________________________\n";
+            return addNewToDo;
+
+        } else if (words[0].equals("event")){
+            String eventTask = "";
+            String eventDate = "";
+            int atPosition = 0;
+            for (int i = 1;  i < words.length; i++) {
+                if (!words[i].equals("/at")) {
+                    eventTask = eventTask + words[i] + " ";
+                } else {
+                    atPosition = i;
+                    break;
+                }
+            }
+
+            for (int k = atPosition + 1; k < words.length; k++) {
+                if (k != words.length-1) {
+                    eventDate = eventDate + words[k] + " ";
+                } else {
+                    eventDate = eventDate + words[k];
+                }
+            }
+            Event newEvent = new Event(eventTask,eventDate);
+            this.tasks.add(newEvent);
+            String addNewEventString = "____________________________________________________________\n" +
+                    "     Got it. I've added this task: \n" +
+                    "       " + newEvent.toString() + "\n" +
+                    "     Now you have " + this.tasks.size() + " "+ taskGrammar + " in the list.\n" +
+                    "____________________________________________________________\n";
+            return addNewEventString;
+
+        } else if (words[0].equals("done")) {
+            //done cases
+            int number = Integer.parseInt(words[1])-1;
+            return this.tasks.get(number).markAsDone();
+        } else {
+            //normal list,bye,blah,normal task(not event, deadline or T0d0) cases
             String blahResponse = "____________________________________________________________\n" +
                     "blah\n" +
                     "____________________________________________________________";
@@ -20,10 +104,12 @@ public class Userinput {
             if (input.equals("list")) {
                 String listResponse = "____________________________________________________________\n" +
                         "Here are the tasks in your list:\n";
+
                 for (int i = 0; i < this.tasks.size(); i++) {
                     String number = Integer.toString(1 + i);
                     listResponse = listResponse + number + ". " + this.tasks.get(i).toString() + "\n";
                 }
+
                 listResponse = listResponse + "____________________________________________________________";
                 return listResponse;
             } else if (input.equals("blah")) {
@@ -37,9 +123,6 @@ public class Userinput {
                         "added: " + input + "\n" +
                         "____________________________________________________________\n";
             }
-        } else {
-            int number = Integer.parseInt(words[1]);
-            return this.tasks.get(number).markAsDone();
         }
     }
 
