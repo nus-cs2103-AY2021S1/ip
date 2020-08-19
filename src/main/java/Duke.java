e6c592556e46a2fdc80f06c7ec8264a99871a81f
amylzting <amylimzting@gmail.com>
2020-08-19 23:52:37 +0800

    Add DukeException error handling to Dino
    Let's handle errors that occur for each feature
    in Dino by throwing DukeException.
    Let's update the test cases in input.txt,
    to better test the functionality.
    
    DukeException represent exceptions specific to Dino.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static void printList(List<Task> tasks) {
+    public static void printList(List<Task> tasks) throws DukeException {
         if(tasks.size() != 0) {
             // Dino lists out all items in list
             System.out.println("Dino lists your tasks:");
@@ -15,31 +16,28 @@ public class Duke {
             System.out.println("To mark off a task after completion"
                     + ", input 'done <task number>'.");
         } else {
-            System.out.println("Rawr! Dino could not find any items in your task list."
+            throw new DukeException("Rawr! Dino could not find any items in your task list."
                     + "\nGet started by entering a task. "
                     + "Formats for a task can be found by entering 'format'.");
         }
-        System.out.println("____________________________________________________________");
     }
 
-    public static void markDone(List<Task> tasks, String input) {
+    public static void markDone(List<Task> tasks, String input) throws DukeException {
         int taskNumber = Integer.parseInt(input.split(" ")[1]);
         if (taskNumber > tasks.size() || taskNumber < 1) {
             // task number is not valid
-            System.out.println("Task " + taskNumber + " is not in your list of tasks!"
-                    + "\n____________________________________________________________");
+            throw new DukeException("Task " + taskNumber + " is not in your list of tasks!");
         } else {
             // Dino marks task as done
             int taskIndex = taskNumber - 1;
             Task doneTask = tasks.get(taskIndex);
             doneTask.markAsDone();
             System.out.println("Great! Dino has marked " + "Task " + taskNumber
-                    + " as done:\n" + doneTask
-                    + "\n____________________________________________________________");
+                    + " as done:\n" + doneTask);
         }
     }
 
-    public static void addTask(List<Task> tasks, String input, String[] inputWords) {
+    public static void addTask(List<Task> tasks, String input, String[] inputWords) throws DukeException {
         try {
             switch (inputWords[0]) {
             case "todo":
@@ -48,67 +46,93 @@ public class Duke {
                 tasks.add(todo);
                 System.out.println("Dino has added to your list of tasks:\n"
                         + todo
-                        + "\nNumber of tasks in list: " + tasks.size()
-                        + "\n____________________________________________________________");
+                        + "\nNumber of tasks in list: " + tasks.size());
                 break;
             case "deadline":
                 String[] taskBy = input.substring(9)
                         .split("/by");
-                Deadline deadline = new Deadline(taskBy[0], taskBy[1]);
-                tasks.add(deadline);
-                System.out.println("Dino has added to your list of tasks:\n"
-                        + deadline
-                        + "\nNumber of tasks in list: " + tasks.size()
-                        + "\n____________________________________________________________");
+                if(taskBy.length >= 2 && !taskBy[0].equals("")) {
+                    // condition checks that input has task description and date/time
+                    StringBuilder taskDeadline = new StringBuilder(taskBy[1]);
+                    // if there are more than one '/by' in input,
+                    // task deadline taken as string after first '/by'
+                    for(int i = 2; i < taskBy.length; i++) {
+                        taskDeadline.append("/by").append(taskBy[i]);
+                    }
+                    Deadline deadline = new Deadline(taskBy[0], taskDeadline.toString());
+                    tasks.add(deadline);
+                    System.out.println("Dino has added to your list of tasks:\n"
+                            + deadline
+                            + "\nNumber of tasks in list: " + tasks.size());
+                } else {
+                    throw new DukeException("Rawr! Dino could not add your task. "
+                            + "Make sure your format is correct."
+                            + "\nFormats to input a task can be found by entering 'format'.");
+                }
                 break;
             case "event":
                 String[] eventAt = input.substring(6)
                         .split("/at");
-                Event event = new Event(eventAt[0], eventAt[1]);
-                tasks.add(event);
-                System.out.println("Dino has added to your list of tasks:\n"
-                        + event
-                        + "\nNumber of tasks in list: " + tasks.size()
-                        + "\n____________________________________________________________");
+                if(eventAt.length >= 2 && !eventAt[0].equals("")) {
+                    // condition checks that input has task description and date/time
+                    StringBuilder eventTime = new StringBuilder(eventAt[1]);
+                    // if there are more than one '/at' in input,
+                    // task deadline taken as string after first '/at'
+                    for(int i = 2; i < eventAt.length; i++) {
+                        eventTime.append("/at").append(eventAt[i]);
+                    }
+                    Event event = new Event(eventAt[0], eventTime.toString());
+                    tasks.add(event);
+                    System.out.println("Dino has added to your list of tasks:\n"
+                            + event
+                            + "\nNumber of tasks in list: " + tasks.size());
+                } else {
+                    throw new DukeException("Rawr! Dino could not add your task. "
+                            + "Make sure your format is correct."
+                            + "\nFormats to input a task can be found by entering 'format'.");
+                }
                 break;
             default:
-                System.out.println("Rawr! Dino could not add your task. "
+                // when input is not a deadline, event or todo
+                throw new DukeException("Rawr! Dino could not add your task. "
                         + "Make sure your format is correct."
-                        + "\nFormats to input a task can be found by entering 'format'."
-                        + "\n____________________________________________________________");
-                break;
+                        + "\nFormats to input a task can be found by entering 'format'.");
             }
         } catch(IndexOutOfBoundsException e) {
             // invalid task format entered
-            System.out.println("Rawr! Dino could not add your task. "
+            throw new DukeException("Rawr! Dino could not add your task. "
                     + "Make sure your format is correct."
-                    + "\nFormats to input a task can be found by entering 'format'."
-                    + "\n____________________________________________________________");
+                    + "\nFormats to input a task can be found by entering 'format'.");
         }
     }
 
     public static void processInputs(List<Task> tasks, String input, String[] inputWords) {
-        if (input.equals("format")) {
-            System.out.println("Formats for the three task types Todo, Deadline and Event,"
-                    + " are shown below.\n"
-                    + "Todo: 'todo <task description>'"
-                    + " (e.g., visit new theme park)\n"
-                    + "Deadline: 'deadline <task description>"
-                    + " /by <date/time>' (e.g., submit report by 11/10/2019 5pm)\n"
-                    + "Event: 'event <task description>"
-                    + " /at <date/start and end time>' "
-                    + "(e.g., team project meeting on 2/10/2019 2-4pm)"
-                    + "\n____________________________________________________________");
-        } else if (input.equals("list")) {
-            printList(tasks);
-        } else if (inputWords[0].equals("done") && inputWords.length == 2
-                && inputWords[1].matches("[0-9]+")) {
-            // condition checks that user input is in the format "done X" where X is a numeric
-            markDone(tasks, input);
-        } else {
-            // Dino adds task to list
-            addTask(tasks, input, inputWords);
+        try {
+            if (input.equals("format")) {
+                System.out.println("Formats for the three task types Todo, Deadline and Event,"
+                        + " are shown below.\n"
+                        + "Todo: 'todo <task description>'"
+                        + " (e.g., visit new theme park)\n"
+                        + "Deadline: 'deadline <task description>"
+                        + " /by <date/time>' (e.g., submit report by 11/10/2019 5pm)\n"
+                        + "Event: 'event <task description>"
+                        + " /at <date/start and end time>' "
+                        + "(e.g., team project meeting on 2/10/2019 2-4pm)");
+            } else if (input.equals("list")) {
+                printList(tasks);
+            } else if (inputWords[0].equals("done") && inputWords.length == 2
+                    && inputWords[1].matches("[0-9]+")) {
+                // condition checks that user input is in the format "done X" where X is a numeric
+                markDone(tasks, input);
+            } else {
+                // Dino adds task to list
+                addTask(tasks, input, inputWords);
+            }
+
+        } catch(DukeException e) {
+            System.out.println(e.getMessage());
         }
+        System.out.println("____________________________________________________________");
     }
 
     public static void main(String[] args) {
--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,4 +1,5 @@
 public class DukeException extends Exception{
+
     public DukeException(String message) {
         super(message);
     }
