2c0dfde6cd45038b5449e936f72ac6bb96d7a8e2
hejl <hejl20150101@gmail.com>
2020-08-19 15:27:46 +0800

    Added Add, List features with slight modifications on chatbot chatting style

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,11 +10,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-        String separation_line = "    ____________________________________________________________\n";
+        String separation_line = "    ____________________________________________________________";
         String indent = "     ";
-        String starting_line = separation_line + indent;
-        String ending_line = "\n" + separation_line;
-        String greeting = starting_line + "Hello! I'm Duke\n" + indent + "What can I do for you?" + ending_line;
+        String starting_line = separation_line + "\n" + indent;
+        String ending_line = "\n" + separation_line + "\n";
+
+        String[] task_arr = new String[100];
+        int count = 0;
+
+        String greeting = starting_line + "Hello! This is J.A.R.V.I.S.\n" + indent + "How may I help you?" + ending_line;
         System.out.println(greeting);
 
         boolean exit_bye = false;
@@ -25,7 +29,20 @@ public class Duke {
                 System.out.println(starting_line + "Bye. Hope to see you again soon!" +  ending_line);
                 exit_bye = true;
             } else {
-                System.out.println(starting_line + input + ending_line);
+                if (input.equals("list")) {
+                    int temp = 1;
+                    System.out.println(separation_line);
+                    while (task_arr[temp - 1] != null) {
+                        System.out.println(indent + temp + ". " + task_arr[temp - 1]);
+                        temp++;
+                    }
+                    System.out.println(separation_line + "\n");
+                } else {
+                    System.out.println(starting_line + "added: " + input + ending_line);
+                    task_arr[count] = input;
+                    count++;
+                }
+                //System.out.println(starting_line + input + ending_line);
             }
         }
     }
