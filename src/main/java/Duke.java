bbc904f677ac256b5c49783bd26de741d781c0f6
Adithya Narayan Rangarajan Sreenivasan <e0426343@u.nus.edu>
2020-08-19 17:07:00 +0530

    Completed Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,13 +8,13 @@ public class Duke {
 
     private static ResourceBundle strings;
     private static Scanner scanner;
-    private static ArrayList<String> list;
+    private static ArrayList<Task> tasks;
 
     private static void initializeDuke() {
         // read the appropriate string resources
         strings = ResourceBundle.getBundle("resources.StringsBundle", Locale.ENGLISH);
         scanner = new Scanner(System.in);
-        list = new ArrayList<>();
+        tasks = new ArrayList<>();
     }
 
     private static void printWithDecoration(Object object) {
@@ -29,22 +29,29 @@ public class Duke {
         initializeDuke();
 
         printWithDecoration(strings.getString("output.greeting"));
-        String input;
+
+        String input, output;
 
         while (scanner.hasNext()) {
             input = scanner.nextLine();
 
             if (input.equals(strings.getString("command.list"))) {
-                String output = IntStream.range(0, list.size())
-                        .mapToObj(index -> (index + 1) + ". " + list.get(index) + "\n\t")
+                output = IntStream.range(0, tasks.size())
+                        .mapToObj(index -> (index + 1) + ". " + tasks.get(index) + "\t")
                         .reduce("", (x, y) -> x + y)
                         .strip();
+
+                printWithDecoration(output);
+            } else if (input.split(" ")[0].equals(strings.getString("command.done"))) {
+                int toMark = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks.get(toMark).markAsDone();
+                output = (strings.getString("output.done") + tasks.get(toMark)).strip();
 
                 printWithDecoration(output);
             } else if (input.equals(strings.getString("command.bye"))) {
                 break;
             } else {
-                list.add(input);
+                tasks.add(new Task(input));
                 printWithDecoration(strings.getString("output.added") + input);
             }
 
