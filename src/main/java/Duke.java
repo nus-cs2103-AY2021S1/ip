9c70b37b763408b5d86d2885183a15888b9715a4
Lim Jin Feng <ljfof98@gmail.com>
2020-08-19 16:28:47 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private final static String bar = "____________________________________________________________";
+    private final static String greeting = "Hello! I'm Duke\n"
+            + "What can I do for you?";
+    private final static String goodbye = "Bye. Hope to see you again soon!";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + Duke.logo);
+        System.out.println(Duke.bar);
+        System.out.println(Duke.greeting);
+        System.out.println(Duke.bar);
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(Duke.bar);
+            System.out.println(userInput);
+            System.out.println(Duke.bar);
+            userInput = sc.nextLine();
+        }
+        System.out.println(Duke.bar);
+        System.out.println(Duke.goodbye);
+        System.out.println(Duke.bar);
     }
 }
