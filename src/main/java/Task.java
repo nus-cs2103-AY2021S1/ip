3160d43d7652da9c2909498848753034863aa194
Clara Adora <claraadora@gmail.com>
2020-08-19 12:38:34 +0800

    fix: add  modifiers and supporting methods to Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,20 @@
 public class Task {
-    String todo;
-    boolean isDone;
+    protected final String description;
+    protected final boolean isDone;
 
-    public Task(String todo) {
-        this.todo = todo;
-        isDone = false;
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public Task markAsDone() {
+        Task task = new Task(this.description, true);
+        return task;
+    }
+
+    @Override
+    public String toString() {
+        String box = this.isDone ? "\u2713" : "\u2718";
+        return String.format("[%s] %s", box, this.description);
     }
 }
