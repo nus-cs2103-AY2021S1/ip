3018d3e9e03ac2ae3f262dd49d60f83ebfaa8ae9
pongzers <you@example.com>
2020-08-19 17:28:34 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String messageFormatter(String msg) {
+        return ("    ____________________________________________________________\n"
+                + "    " + msg + "\n"
+                + "    ____________________________________________________________\n");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf(messageFormatter(
+                "Hello! I'm Duke\n" +
+                "    What can I do for you?"));
+
+        while (scanner.hasNext()) {
+            String msg = scanner.next();
+            if (msg.equals("bye")) {
+                System.out.printf(messageFormatter("Bye. Hope to see you again soon!"));
+                break;
+            }
+            System.out.printf(messageFormatter(msg));
+        }
     }
 }
