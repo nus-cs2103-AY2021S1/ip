3fbe4205e4e3fdc100b031336c010591e7b46017
Aung Thuya Oo <thuyaoo200@gmail.com>
2020-08-19 11:55:13 +0800

    Complete level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,56 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        String introduction = "    ____________________________________________________________\n" +
+        List<String> textList = new ArrayList<>();
+        sayHello();
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            if (command.equals("list")) {
+                displayList(textList);
+            } else {
+                echoAddCommand(command);
+                textList.add(command);
+            }
+            command = scanner.nextLine();
+        }
+        sayBye();
+    }
+
+    private static void echoAddCommand(String command) {
+        System.out.println(wrapDivider("     added: " + command + "\n"));
+    }
+
+    private static void sayHello() {
+        System.out.println(wrapDivider(
                 "      ____        _        \n" +
                 "     |  _ \\ _   _| | _____ \n" +
                 "     | | | | | | | |/ / _ \\\n" +
                 "     | |_| | |_| |   <  __/\n" +
                 "     |____/ \\__,_|_|\\_\\___|\n" +
                 "     Hello! I'm Thuya\n" +
-                "     What may I do for you, sir/madam?\n" +
-                "    ____________________________________________________________\n";
-        System.out.println(introduction);
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.next();
-        while (!command.equals("bye")) {
-            System.out.println(
-                    "    ____________________________________________________________\n" +
-                    "     " + command +"\n" +
-                    "    ____________________________________________________________\n" );
-            command = scanner.next();
-        }
-        System.out.println(
-                "    ____________________________________________________________\n" +
-                "     Bye. Hope to see you again soon!\n" +
-                "    ____________________________________________________________\n" +
-                "\n"
+                "     What may I do for you, sir/madam?\n")
         );
+    }
+
+    private static void sayBye() {
+        System.out.println(wrapDivider("     Bye. Hope to see you again soon!\n"));
+    }
+
+    public static void displayList(List<String> textList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < textList.size(); i++) {
+            stringBuilder.append("     " + (i+1) + ". "+ textList.get(i) +"\n");
+        }
+        System.out.println(wrapDivider(stringBuilder.toString()));
+    }
+
+    private static String wrapDivider(String text) {
+        return  "    ____________________________________________________________\n" +
+                text +
+                "    ____________________________________________________________\n" ;
     }
 }
