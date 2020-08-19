4a182f1fd825b725f8df6d6ad26dcaad2edd3f92
CodyChew <chewchengyap@gmail.com>
2020-08-19 17:50:38 +0800

    Fix runtest.bat -now checks for tick/cross correctly

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,12 +1,14 @@
 @ECHO OFF
 
 
+
 REM create bin directory if it doesn't exist
 if not exist C:\Data\repos\CS2103\bin mkdir C:\Data\repos\CS2103\bin
 
 REM delete output from previous run
 del ACTUAL.TXT
 
+set JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 REM compile the code into the bin folder
 javac  -cp C:\Data\repos\CS2103\src\main\java -Xlint:none -d C:\Data\repos\CS2103\bin C:\Data\repos\CS2103\src\main\java\Duke.java
@@ -19,6 +21,7 @@ REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath C:\Data\repos\CS2103\bin Duke < input.txt > ACTUAL.TXT
+
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
