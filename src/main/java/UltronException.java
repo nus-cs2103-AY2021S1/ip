3586b118028d3dc9be3e7d94f4aff626224c50fd
Junhua <wenjh1998@gmail.com>
2020-08-19 12:54:35 +0800

    Updated UltronException to match it

--- src/main/java/UltronException.java
+++ src/main/java/UltronException.java
@@ -1,6 +1,14 @@
 public class UltronException extends Exception{
 
-    public UltronException(String message){
-        super(message);
+    public UltronException(String inputString, ExceptionType errorType){
+
+        //Call the superclass method
+        super(errorType.getMessage(inputString));
+    }
+
+    public UltronException(String inputString, String inputArguments, ExceptionType errorType){
+
+        //Call the superclass method
+        super(errorType.getMessage(inputString, inputArguments));
     }
 }
