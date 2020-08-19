3c9ded5ce9bde03042109c46c6346fbe1dcc8c9a
Xingjian <xingjian.chen@yahoo.com.sg>
2020-08-19 17:54:37 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * Duke class that contains the main logic for the chat bot DUKE.
+ */
 public class Duke {
 
     public static String lineDivider = "------------------------------------------";
 
+    /**
+     * Echos the message that the user put.
+     * @param message  A String message that user wants the chat bot to echo.
+     */
     public static void echo(String message) {
         System.out.println(lineDivider);
         System.out.println(message);
         System.out.println(lineDivider + "\n");
     }
 
+    /**
+     * Arrange list of task into a more readable format.
+     * @param list
+     * @return A more readable String that represents the contents in the task list.
+     */
     public static String showList(ArrayList<Task> list) {
         StringBuffer lst = new StringBuffer();
         lst.append("Here are the tasks in your list:\n");
@@ -25,6 +37,11 @@ public class Duke {
         return lst.toString();
     }
 
+    /**
+     * Check if an integer is represented in the String
+     * @param s  String input
+     * @return   True or False
+     */
     public static boolean isInt(String s) {
         try{
             int num = Integer.parseInt(s);
@@ -36,6 +53,10 @@ public class Duke {
         }
     }
 
+    /**
+     * Main logic for the chat bot
+     * @param args User input
+     */
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -76,8 +97,54 @@ public class Duke {
                  continue;
             }
 
-            list.add(new Task(msg));
-            echo("added: " + msg);
+            if (msgArray[0].equals("todo") || msgArray[0].equals("deadline") || msgArray[0].equals("event")) {
+                String taskS = " task ";
+                if (msgArray[0].equals("todo")) {
+                    String todoTask = msg.substring(5);
+                    ToDo todo = new ToDo(todoTask);
+                    list.add(todo);
+                    if (list.size() > 1) {
+                        taskS = " tasks ";
+                    }
+                    echo("Got it. I've added this task:\n"
+                            + todo.toString()
+                            + "\nNow you have "
+                            + list.size()
+                            + taskS
+                            + "in the list.");
+                } else if (msgArray[0].equals("deadline")) {
+                    String deadlineTask = msg.substring(9);
+                    String[] deadlineTaskArray = deadlineTask.split(" /by ");
+                    Deadline deadline = new Deadline(deadlineTaskArray[0], deadlineTaskArray[1]);
+                    list.add(deadline);
+                    if (list.size() > 1) {
+                        taskS = " tasks ";
+                    }
+                    echo("Got it. I've added this task:\n"
+                            + deadline.toString()
+                            + "\nNow you have "
+                            + list.size()
+                            + taskS
+                            + "in the list.");
+                } else {
+                    String eventTask = msg.substring(6);
+                    String[] eventTaskArray = eventTask.split(" /at ");
+                    Event event = new Event(eventTaskArray[0], eventTaskArray[1]);
+                    list.add(event);
+                    if (list.size() > 1) {
+                        taskS = " tasks ";
+                    }
+                    echo("Got it. I've added this task:\n"
+                            + event.toString()
+                            + "\nNow you have "
+                            + list.size()
+                            + taskS
+                            + "in the list.");
+                }
+                continue;
+            }
+
+            echo("Invalid input.");
         }
     }
 }
