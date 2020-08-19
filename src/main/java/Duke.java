fe2819cc5bb3d33b358994472b40564133ef8ef9
yaoyuan <yaoyuan@u.nus.edu>
2020-08-19 20:42:15 +0800

    Mark as Done for Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
 
     static String line = "—————————————————————————————————————————————————————————————————";
-    static String[] list = new String[100];
+    static Task[] list = new Task[100];
     static int index = 0;
 
     public static void greeting() {
@@ -30,7 +30,7 @@ public class Duke {
         }
     }
 
-    public static void addTask(String task) {
+    public static void addTask(Task task) {
         list[index] = task;
         index++;
         System.out.println("\t" + line + "\n"
@@ -39,11 +39,30 @@ public class Duke {
     }
 
     public static void showList(){
-        System.out.println("\t" + line);
+        System.out.println("\t" + line + "\n\tHere are the tasks in your list:");
         for (int i = 0; i < index; i++){
             System.out.println("\t" + (i+1) + ". " + list[i]);
         }
         System.out.println("\t" + line);
+    }
+
+    public static String firstWord(String str) {
+        return str.split(" ")[0]; // create array of words and return 0th word
+    }
+
+    public static int getTaskNum(String str) {
+        return Integer.parseInt(str.split(" ")[1]);
+    }
+
+    public static void markDone(int num) {
+        if (list[num-1] == null) {
+            System.out.println("Error :("); // error catch
+        } else {
+            list[num - 1].markDone();
+            System.out.println("\t" + line + "\n\tNice! I've marked this task as done:\n\t  "
+                    + list[num - 1]
+                    + "\n\t" + line);
+        }
     }
 
 
@@ -53,14 +72,19 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
+            // possible actions: type task, list, bye, done 2
             if (input.equals("bye")) {
                 bye();
                 sc.close();
                 break;
-            } else if (input.equals("list")){
+            } else if (input.equals("list")) {
                 showList();
+            } else if (firstWord(input).equals("done")) {
+                int taskNum = getTaskNum(input);
+                markDone(taskNum);
             } else {
-                addTask(input);
+                Task newTask = new Task(input);
+                addTask(newTask);
             }
         }
     }
