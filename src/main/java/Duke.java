74ed5b81b5e52a81dda773d12564f8c148dd24dc
tengjianling <tengjianling98@gmail.com>
2020-08-19 12:51:34 +0800

    Change while loop format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,7 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
         String str = "";
         System.out.println("    _________________________________");
         System.out.println("    Hello! I'm Duke");
@@ -18,59 +19,69 @@ public class Duke {
 
         ArrayList<Task> tasks = new ArrayList<>();
 
-        while (!str.equals("bye")) {
-            str = sc.nextLine();
+        //while (!str.equals("bye")) {
+        while (sc.hasNext()) {
+            str = sc.next();
             Task t = new Task(str);
 
-            System.out.println("    _________________________________");
 
+            if (str.isBlank()) {
+                // does not register as an entry
+            } else {
+                System.out.println("    _________________________________");
 
-            if (t.getDescription().equals("list")) {
-                System.out.println("    Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    String item = (i + 1) + ". " + tasks.get(i);
-                    System.out.println("    " + item);
+                if (t.getDescription().equals("list")) {
+                    System.out.println("    Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        String item = (i + 1) + ". " + tasks.get(i);
+                        System.out.println("    " + item);
+                    }
+
+                } else if (t.isTodo()) {
+                    ToDo todo = new ToDo(t.getDescription());
+                    tasks.add(todo);
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println("        " + todo);
+                    System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
+
+                } else if (t.isDeadline()) {
+                    String date = t.getDate();
+                    Deadline deadline = new Deadline(t.getDescription(), date);
+                    tasks.add(deadline);
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println("        " + deadline);
+                    System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
+
+                } else if (t.isEvent()) {
+                    String date = t.getDate();
+                    Event event = new Event(t.getDescription(), date);
+                    tasks.add(event);
+                    System.out.println("    Got it. I've added this task:");
+                    System.out.println("        " + event);
+                    System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
+
+                } else if (t.getFirstWord().equals("done")) {
+                    int taskNumber = t.getNumber();
+                    tasks.get(taskNumber - 1).markAsDone();
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println("    " + tasks.get(taskNumber - 1));
+
+                } else if (!str.equals("bye")) {
+                    System.out.println("    added: " + t.getDescription());
+                    tasks.add(t);
+                    break;
+                } else {
+
+                    System.out.println("    Bye. Hope to see you again soon!");
+                    System.out.println("    _________________________________");
+                    break;
                 }
 
-            } else if (t.isTodo()) {
-                ToDo todo = new ToDo(t.getDescription());
-                tasks.add(todo);
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("    " + todo);
-                System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
-
-            } else if (t.isDeadline()) {
-                String date = t.getDate();
-                Deadline deadline = new Deadline(t.getDescription(), date);
-                tasks.add(deadline);
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("    " + deadline);
-                System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
-
-            } else if (t.isEvent()) {
-                String date = t.getDate();
-                Event event = new Event(t.getDescription(), date);
-                tasks.add(event);
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("    " + event);
-                System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
-
-            } else if (t.getFirstWord().equals("done")) {
-                int taskNumber = t.getNumber();
-                tasks.get(taskNumber - 1).markAsDone();
-                System.out.println("    Nice! I've marked this task as done:");
-                System.out.println("    " + tasks.get(taskNumber - 1));
-
-            } else if (!str.equals("bye")) {
-                System.out.println("    added: " + t.getDescription());
-                tasks.add(t);
+                System.out.println("    _________________________________");
             }
-            System.out.println("    _________________________________");
+
         }
 
-        System.out.println("    _________________________________");
-        System.out.println("    Bye. Hope to see you again soon!");
-        System.out.println("    _________________________________");
 
     }
 }
