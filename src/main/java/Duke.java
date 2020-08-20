cc7c9a917869e3a0dfaca25bc3d320ffcbfe724b
zhengweii <limzhengwei_99@hotmail.com>
2020-08-20 08:58:12 +0800

    Added functionality to store and display text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         greet();
-
         echo();
     }
 
@@ -15,11 +15,31 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
 
+        ArrayList<String> database = new ArrayList<>(100);
         while (!s.equals("bye")) {
-            System.out.println(s);
+            if (s.equals("list")) {
+                list(database);
+            } else {
+                add(database, s);
+                System.out.println("Added item: " + s);
+            }
             s = sc.nextLine();
         }
 
         System.out.println("Goodbye! All the best and see you again soon!");
+    }
+
+    private static void add(ArrayList<String> database, String text) {
+        database.add(text);
+    }
+
+    private static void list(ArrayList<String> database) {
+        if (database.size() == 0) {
+            System.out.println("There are no items in the database");
+        }
+
+        for (int i = 0; i < database.size(); i++) {
+            System.out.println((i + 1) + ". " + database.get(i));
+        }
     }
 }
