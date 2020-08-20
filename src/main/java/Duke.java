4d52c689cffbc954614960a4149c62ce881d4753
Aaron S <aaronsgseah@gmail.comm>
2020-08-21 00:55:19 +0800

    Completed Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     private static final Scanner scanner = new Scanner(System.in);
+
     private static final String IDENT = "\t";
     private static final String LINE = IDENT + "------------------------------";
+    private static final String OUTPUT_FORMAT = LINE + "\n%s\n" + LINE;
     private static final String INITIAL_RESPONSE = "Hello! I'm Duke\n\tWhat can I do for you?";
     private static final String EXIT_INPUT = "bye";
     private static final String EXIT_RESPONSE = "Bye. Hope to see you again soon!";
+
+
+    private static final String LIST_COMMAND = "list";
+    private static final List<String> LIST = new ArrayList<>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,24 +24,38 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         String response;
-        responseOutput(INITIAL_RESPONSE);
+        printResponse(INITIAL_RESPONSE);
         while (true) {
             response = scanner.nextLine();
-            int output = responseOutput(response);
-            if (output == -1) {
+            if (response.equals(EXIT_INPUT)) {
+                printResponse(EXIT_RESPONSE);
                 break;
+            } else if (response.equals(LIST_COMMAND)) {
+                printList();
+            } else {
+                addToList(response);
+                printResponse("added: " + response);
             }
         }
     }
-    private static int responseOutput(String response) {
-        int output = 1;
-        if (response.equals(EXIT_INPUT)) {
-            response = EXIT_RESPONSE;
-            output = -1;
+
+    private static void printResponse(String response) {
+        System.out.println(LINE);
+        System.out.printf("%s%s\n", IDENT, response);
+        System.out.println(LINE);
+    }
+
+    private static void addToList(String response) {
+        LIST.add(response);
+    }
+
+    private static void printList() {
+        System.out.println(LINE);
+        int num = 0;
+        for (String output : LIST) {
+            num++;
+            System.out.printf("%s%d. %s\n", IDENT, num, output);
         }
         System.out.println(LINE);
-        System.out.printf("%s%s%n",IDENT, response);
-        System.out.println(LINE);
-        return output;
     }
 }
