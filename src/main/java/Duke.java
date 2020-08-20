5fa8ff1713e1d0ce7199b6dc1e7a4728ac5bd10d
taneuzin <taneuzin@u.nus.edu>
2020-08-20 14:19:37 +0800

    This is level 1, I hope im committing this correctly.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo + "\nHello im Eu Zin's Duke, he spent thursday afternoon creating me cuz he forgot abt the iP");
+
+        Duke.echo(new Scanner(System.in));
+    }
+
+    static void echo(Scanner scanner) {
+        String userInput = scanner.nextLine();
+        String borders = "\n\\   / \\   / \\   / \\   / im not very creative \\   / \\   / \\   / \\   /\n \\ /   \\ /   \\ /   \\ /      EuZin's Duke      \\ /   \\ /   \\ /   \\ /\n";
+        if (userInput.equals("bye")) {
+            System.out.println(borders + "\n" + "Bye. Hope to see you again soon!" + "\n" + borders);
+        } else {
+            System.out.println(borders + "\n" + userInput + "\n" + borders);
+            Duke.echo((new Scanner(System.in)));
+        }
     }
 }
