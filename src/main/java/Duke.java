b81969cfe7efc6c3c860e916018f78f436b43c8a
Tan Yan Lyn <e0424594@u.nus.edu>
2020-08-20 12:54:08 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,76 +3,89 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws DukeException {
+        try {
+            String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+            System.out.println("Hello from\n" + logo);
 
-        System.out.println("Hello! I'm Duke \n"
-                + "What can I do for you?");
-        Scanner sc = new Scanner(System.in);
-        List<Input> inputs = new ArrayList<>();
-        while (true) {
-            String nextLine = sc.nextLine();
-            if (nextLine.startsWith("done")) {
-                int numTaskDone = Integer.valueOf(nextLine.substring(5));
-                System.out.println("Nice! I've marked this task as done:");
-                if (numTaskDone <= inputs.size()) {
-                    Input inputType = inputs.get(numTaskDone -1);
+            System.out.println("Hello! I'm Duke \n"
+                    + "What can I do for you?");
+            Scanner sc = new Scanner(System.in);
+            List<Input> inputs = new ArrayList<>();
+            while (true) {
+                String nextLine = sc.nextLine();
+                if (nextLine.startsWith("done")) {
+                    if (nextLine.equals("done") || nextLine.equals("done ")) {
+                        throw new DukeException("OOPS!!! The description of done cannot be empty.");
+                    }
+                    int numTaskDone = Integer.valueOf(nextLine.substring(5));
+                    if (numTaskDone > inputs.size()) {
+                        throw new DukeException("OOPS!!! Task does not exist.");
+                    }
+                    System.out.println("Nice! I've marked this task as done:");
+                    Input inputType = inputs.get(numTaskDone - 1);
                     inputType.taskDone();
                     System.out.println("[/] " + inputType.content + " " + inputType.time);
                 } else {
-                }
-            } else {
-                if (nextLine.startsWith("todo")) {
-                    Todo todo = new Todo(nextLine.substring(5));
-                    inputs.add(todo);
-                    int count = inputs.size();
-                    System.out.println("Got it. I've added this task: \n" + "  [T][x] " + todo.content +
-                            "\n Now you have " + count + " tasks in the list");
-                }
-                else if (nextLine.startsWith("deadline")) {
-                    int charLoc = nextLine.indexOf("/by");
-                    Deadline deadline = new Deadline(nextLine.substring(9, charLoc), nextLine.substring(charLoc + 3));
-                    inputs.add(deadline);
-                    int count = inputs.size();
-                    System.out.println("Got it. I've added this task: \n" + "  [D][x] " + deadline.content +
-                            deadline.time + "\n Now you have " + count + " tasks in the list");
-                }
-                else if (nextLine.startsWith("event")) {
-                    int charLoc = nextLine.indexOf("/at");
-                    Event event = new Event(nextLine.substring(6, charLoc), nextLine.substring(charLoc + 3));
-                    inputs.add(event);
-                    int count = inputs.size();
-                    System.out.println("Got it. I've added this task: \n" + "  [E][x] " + event.content +
-                            event.time + "\n Now you have " + count + " tasks in the list");
-
-                }
-                else if (nextLine.equals("list")) {
-                    System.out.println("Here are the tasks in your list:");
-                    int len = inputs.size();
-                    for (int i = 1; i <= len; i++) {
-                        Input inputType = inputs.get(i - 1);
-                        if (inputType.done) {
-                            System.out.println(i + ". " + inputType.id + "[/] " + inputType.content + " " +
-                                     inputType.time);
-                        } else {
-                            System.out.println(i + ". " + inputType.id + "[x] " + inputType.content + " " +
-                                    inputType.time);
+                    if (nextLine.startsWith("todo")) {
+                        if (nextLine.equals("todo") || nextLine.equals("todo ")) {
+                            throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
                         }
+                        Todo todo = new Todo(nextLine.substring(5));
+                        inputs.add(todo);
+                        int count = inputs.size();
+                        System.out.println("Got it. I've added this task: \n" + "  [T][x] " + todo.content +
+                                "\n Now you have " + count + " tasks in the list");
+                    } else if (nextLine.startsWith("deadline")) {
+                        if (nextLine.equals("deadline") || nextLine.equals("deadline ")) {
+                            throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                        }
+                        int charLoc = nextLine.indexOf("/by");
+                        Deadline deadline = new Deadline(nextLine.substring(9, charLoc), nextLine.substring(charLoc + 3));
+                        inputs.add(deadline);
+                        int count = inputs.size();
+                        System.out.println("Got it. I've added this task: \n" + "  [D][x] " + deadline.content +
+                                deadline.time + "\n Now you have " + count + " tasks in the list");
+                    } else if (nextLine.startsWith("event")) {
+                        if (nextLine.equals("event") || nextLine.equals("event ")) {
+                            throw new DukeException("OOPS!!! The description of an event cannot be empty.");
+                        }
+                        int charLoc = nextLine.indexOf("/at");
+                        Event event = new Event(nextLine.substring(6, charLoc), nextLine.substring(charLoc + 3));
+                        inputs.add(event);
+                        int count = inputs.size();
+                        System.out.println("Got it. I've added this task: \n" + "  [E][x] " + event.content +
+                                event.time + "\n Now you have " + count + " tasks in the list");
+
+                    } else if (nextLine.equals("list")) {
+                        System.out.println("Here are the tasks in your list:");
+                        int len = inputs.size();
+                        for (int i = 1; i <= len; i++) {
+                            Input inputType = inputs.get(i - 1);
+                            if (inputType.done) {
+                                System.out.println(i + ". " + inputType.id + "[/] " + inputType.content + " " +
+                                        inputType.time);
+                            } else {
+                                System.out.println(i + ". " + inputType.id + "[x] " + inputType.content + " " +
+                                        inputType.time);
+                            }
+                        }
+                    } else if (nextLine.equals("bye")) {
+                        System.out.println("Bye. Hope to see you again soon!");
+                        break;
+                    } else {
+                        throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                     }
-                }
-                else if (nextLine.equals("bye")) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
-                    }
-                else {}
                 }
             }
-
+        }
+        catch (DukeException e) {
+            System.out.println(e.msg);
+        }
     }
         
     public static class Input {
@@ -139,9 +152,21 @@ public class Duke {
             this.id = "[E]";
         }
     }
-
-
-
-
-
+    
+    
+    public static class DukeException extends Exception {
+        String msg; 
+        
+        DukeException(String msg) {
+            this.msg = msg;
+        }
     }
+    
+
+}
+
+
+
+
+
+    
