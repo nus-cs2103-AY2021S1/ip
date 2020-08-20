0e763f366efc94a58b00e6e7a953dc6f80211090
GabrielTeo <gabrieltyx@gmail.com>
2020-08-20 15:01:02 +0800

    add markAsDone function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,35 +3,73 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static List<String> tasks = new ArrayList<>();
+    public static String border = "-----------------------------------------------------";
+    public static String indentation = "    ";
+    public static List<Task> tasks = new ArrayList<>();
 
     public static void interact() {
-        System.out.println("-----------------------------------------------------");
-        System.out.println("    " + "Hello! I'm Duke\n    What can I do for you?");
-        System.out.println("-----------------------------------------------------");
-
+        greet();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
             if (line.equals("bye")) {
-                System.out.println("-----------------------------------------------------");
-                System.out.println("    " + "Bye. Hope to see you again soon!");
-                System.out.println("-----------------------------------------------------");
+                exit();
                 break;
             } else if (line.equals("list")) {
-                System.out.println("-----------------------------------------------------");
-                for (String task : tasks) {
-                    int index = tasks.indexOf(task) + 1;
-                    System.out.println("    " + index + ". " + task);
-                }
-                System.out.println("-----------------------------------------------------");
+                list();
             } else {
-                System.out.println("-----------------------------------------------------");
-                tasks.add(line);
-                System.out.println("    " + "added: " + line);
-                System.out.println("-----------------------------------------------------");
+                String[] arrOfStr = line.split(" ", 0);
+                if (arrOfStr[0].equals("done")) {
+                    int index = Integer.parseInt(arrOfStr[1]) - 1;
+                    markDone(index);
+                } else {
+                    add(line);
+                }
             }
         }
+    }
+
+    public static void printBorder() {
+        System.out.println(indentation + border);
+    }
+
+    public static void greet() {
+        printBorder();
+        System.out.println(indentation + "Hello! I'm Duke\n    What can I do for you?");
+        printBorder();
+    }
+
+    public static void exit() {
+        printBorder();
+        System.out.println(indentation + "Bye. Hope to see you again soon!");
+        printBorder();
+    }
+
+    public static void list() {
+        printBorder();
+        System.out.println(indentation + "Here are the tasks in your list:");
+        for (Task task : tasks) {
+            int index = tasks.indexOf(task) + 1;
+            System.out.println(indentation + index + ".[" + task.getStatusIcon() + "] " + task.description);
+        }
+        printBorder();
+    }
+
+    public static void add(String line) {
+        printBorder();
+        tasks.add(new Task(line));
+        System.out.println(indentation + "added: " + line);
+        printBorder();
+    }
+
+    public static void markDone(int index) {
+        Task task = tasks.get(index);
+        Task newTask = task.markAsDone();
+        tasks.set(index, newTask);
+        printBorder();
+        System.out.println(indentation + "Nice! I've marked this task as done:");
+        System.out.println(indentation + indentation + "[" + newTask.getStatusIcon() + "] " + newTask.description);
+        printBorder();
     }
 
     public static void main(String[] args) {
