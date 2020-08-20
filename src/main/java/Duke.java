336d06045179f80eec1e4048527ef111f0418dd6
WeiJie96 <fongweijie96@u.nus.edu>
2020-08-20 22:22:03 +0800

    Add, List Added arraylist to ask tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,32 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
+    public static ArrayList<Task> taskList = new ArrayList<>();
 
     /*
      * Prints Duke's welcome message
      */
     private static void welcome() {
         printOutput("     Hello! I'm Duke\n" +
-                        "     What can I do for you?");
+                        "     What can I do for you?", true);
     }
 
     /*
      * Wraps input in dashes
      */
-    private static void printOutput(String input) {
-        System.out.println("    ____________________________________________________________\n" +
-                input + "\n" +
-                "    ____________________________________________________________");
+    private static void printOutput(String input, boolean lastNewLine) {
+        if (lastNewLine) {
+            System.out.println("    ____________________________________________________________\n" +
+                    input + "\n" +
+                    "    ____________________________________________________________");
+
+        } else {
+            System.out.println("    ____________________________________________________________\n" +
+                    input +
+                    "    ____________________________________________________________");
+        }
     }
 
     /*
@@ -24,12 +34,19 @@ public class Duke {
      * @return: returns boolean of whether or not to continue
      */
     private static boolean inputHandler(String input) {
-        switch(input) {
+        String action = input.split(" ")[0];
+        switch(action) {
+            case "list":
+                listTasks();
+                return true;
+            case "done":
+                done(input);
+                return true;
             case "bye":
                 exit();
                 return false;
             default:
-                printOutput("     " + input);
+                handleTask(input);
                 return true;
         }
     }
@@ -38,7 +55,33 @@ public class Duke {
      * Does all necessary actions before exiting
      */
     private static void exit() {
-        printOutput("     Bye. Hope to see you again soon!");
+        printOutput("     Bye. Hope to see you again soon!", true);
+    }
+
+    private static void handleTask(String input) {
+        Task task = new Task(input);
+        taskList.add(task);
+        printOutput("     added: " + task.getDescription(), true);
+    }
+
+    private static void listTasks() {
+        String output = "     Here are the tasks in your list:\n";
+        int index = 1;
+        for (Task task : taskList) {
+            output += "     " + index + "." + task.strTask() + "\n";
+            index++;
+        }
+        printOutput(output, false);
+    }
+
+    private static void done(String input) {
+        String strIndex = input.split(" ")[1];
+        int index = Integer.parseInt(strIndex);
+        Task task = taskList.get(index - 1);
+        task.setDone(true);
+        String output = "     Nice! I've marked this task as done: \n" +
+                                "       " + task.strTask();
+        printOutput(output, true);
     }
 
     /*
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public String strTask() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
+}
