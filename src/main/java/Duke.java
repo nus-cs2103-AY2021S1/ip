2777c68324092792e923c1d863a177ce7ae49440
FooJingYi <foojingyi@gmail.com>
2020-08-20 15:09:18 +0800

    Created user input feature and respond to user inputs, deleted default output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        Message start = new Message("start");
+        start.reply();
+
+        while (sc.hasNext()) {
+            Message msg = new Message(sc.next());
+            msg.reply();
+            if (msg.getCmd() == Command.BYE) {
+                break;
+            }
+        }
     }
 }
