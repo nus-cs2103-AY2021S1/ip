b144918c2f44b659bc83d97f4cf8d13d80bdc53a
m0nggh <malcolmong27@gmail.com>
2020-08-20 23:02:31 +0800

    A-Enums Completed, Updated UiTesting for delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -37,70 +36,42 @@ public class Duke {
 
     // main driver function for duke to tackle commands
     private static void askDuke(String command, String afterCommand) throws DukeException {
-        // handle all different commands
-        if (command.equals("bye")) { // 1. bye
-            exit();
-        } else if (command.equals("list")) { // 2. list
-            displayList();
-        } else if (command.equals("done")){ // 3. done (needs a valid number)
-            int taskNo = Integer.parseInt(afterCommand) - 1;
-            if (taskNo >= tasks.size() || taskNo < 0) {
-                throw new DukeException("Please enter a valid task no!");
-            }
-            tasks.get(taskNo).markAsCompleted();
-            displayCompletedTask(tasks.get(taskNo));
-        } else if (command.equals("todo")) { // 4. to do (needs an after command)
-            if (afterCommand == null) {
-                throw new DukeException("Please do not leave the todo description empty!");
-            }
-            addOnToList(new ToDo(afterCommand));
-        } else if (command.equals("deadline")) { // 5. deadline (needs an after command)
-            if (afterCommand == null) {
-                throw new DukeException("Please do not leave the deadline description empty!");
-            }
-            // first chunk is the deadline details, second chunk is by when
-            String[] splittedDeadline = afterCommand.split("/");
-
-            // teach the user the format for the deadline
-            if (splittedDeadline.length == 1) {
-                throw new DukeException("Format of deadline recording: deadline keyword" +
-                        ", deadline instructions, forward slash, by keyword with a colon, specific date/time)"
-                        + "\n e.g. deadline return book /by Sunday");
-            }
-
-            String details = splittedDeadline[0].trim();
-            String by = splittedDeadline[1].split("\\s", 2)[1];
-            addOnToList(new Deadline(details, by));
-        } else if (command.equals("event")) { // 6. event (needs an after command)
-            if (afterCommand == null) {
-                throw new DukeException("Please do not leave the event description empty!");
-            }
-            // first chunk is the event details, second chunk is at where
-            String[] splittedEvent = afterCommand.split(("/"));
-
-            // teach the user the format for the deadline
-            if (splittedEvent.length == 1) {
-                throw new DukeException("Format of event recording: event keyword" +
-                        ", event instructions, forward slash, at keyword with a colon, start/end time)"
-                        + "\n e.g. project meeting /at Mon 2-4pm");
-            }
-
-            String details = splittedEvent[0].trim();
-            String at = splittedEvent[1].split("\\s", 2)[1];
-            addOnToList(new Event(details, at));
-        } else if (command.equals("delete")) { // 7. delete (needs a valid number)
-            int taskNo = Integer.parseInt(afterCommand) - 1;
-            if (taskNo >= tasks.size() || taskNo < 0) {
-                throw new DukeException("Please enter a valid task no!");
-            }
-            displayDeletedTask(taskNo);
+        Command commandType;
+        try {
+            commandType = Command.valueOf(command.toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            commandType = Command.UNIDENTIFIED;
         }
-        else {
-            // if a bad command is thrown at Duke
-            throw new DukeException("Please key in a command I understand!");
+
+        // handle all different commands using switch and enum instead; organised the methods
+        // to make the code look neater
+        switch (commandType) {
+            case BYE:
+                exit();
+                break;
+            case LIST:
+                displayList();
+                break;
+            case DONE:
+                recordDoneTask(afterCommand);
+                break;
+            case TODO:
+                recordToDoTask(afterCommand);
+                break;
+            case DEADLINE:
+                recordDeadlineTask(afterCommand);
+                break;
+            case EVENT:
+                recordEventTask(afterCommand);
+                break;
+            case DELETE:
+                deleteTask(afterCommand);
+                break;
+            case UNIDENTIFIED:
+                // if a bad command is thrown at Duke
+                throw new DukeException("Please key in a command I understand!");
         }
     }
-
 
     private static void greet(String logo) {
        format("Hello! I'm\n" + logo + "\n" +
@@ -136,8 +107,75 @@ public class Duke {
         format(sb.toString());
     }
 
+    private static void recordDoneTask(String afterCommand) throws DukeException {
+        // needs a valid number
+        int taskNo = Integer.parseInt(afterCommand) - 1;
+        if (taskNo >= tasks.size() || taskNo < 0) {
+            throw new DukeException("Please enter a valid task no!");
+        }
+        tasks.get(taskNo).markAsCompleted();
+        displayCompletedTask(tasks.get(taskNo));
+    }
+
     private static void displayCompletedTask(Task task) {
         format("Nice! I've marked this task as done:\n" + task);
+    }
+
+    private static void recordToDoTask(String afterCommand) throws DukeException {
+        // needs an after command
+        if (afterCommand == null) {
+            throw new DukeException("Please do not leave the todo description empty!");
+        }
+        addOnToList(new ToDo(afterCommand));
+    }
+
+    private static void recordDeadlineTask(String afterCommand) throws DukeException {
+        // needs an after command
+        if (afterCommand == null) {
+            throw new DukeException("Please do not leave the deadline description empty!");
+        }
+        // first chunk is the deadline details, second chunk is by when
+        String[] splittedDeadline = afterCommand.split("/");
+
+        // teach the user the format for the deadline
+        if (splittedDeadline.length == 1) {
+            throw new DukeException("Format of deadline recording: deadline keyword" +
+                    ", deadline instructions, forward slash, by keyword with a colon, specific date/time)"
+                    + "\n e.g. deadline return book /by Sunday");
+        }
+
+        String details = splittedDeadline[0].trim();
+        String by = splittedDeadline[1].split("\\s", 2)[1];
+        addOnToList(new Deadline(details, by));
+    }
+
+    private static void recordEventTask(String afterCommand) throws DukeException {
+        // needs an after command
+        if (afterCommand == null) {
+            throw new DukeException("Please do not leave the event description empty!");
+        }
+        // first chunk is the event details, second chunk is at where
+        String[] splittedEvent = afterCommand.split(("/"));
+
+        // teach the user the format for the deadline
+        if (splittedEvent.length == 1) {
+            throw new DukeException("Format of event recording: event keyword" +
+                    ", event instructions, forward slash, at keyword with a colon, start/end time)"
+                    + "\n e.g. project meeting /at Mon 2-4pm");
+        }
+
+        String details = splittedEvent[0].trim();
+        String at = splittedEvent[1].split("\\s", 2)[1];
+        addOnToList(new Event(details, at));
+    }
+
+    private static void deleteTask(String afterCommand) throws DukeException {
+        // needs a valid number
+        int taskNo = Integer.parseInt(afterCommand) - 1;
+        if (taskNo >= tasks.size() || taskNo < 0) {
+            throw new DukeException("Please enter a valid task no!");
+        }
+        displayDeletedTask(taskNo);
     }
 
     private static void displayDeletedTask(int index) {
