1d5242ca1f0d2e2e88d934bc54014f92910ee166
Muhammad Syafiq Bin Abas <syafiqabs7@gmail.com>
2020-08-20 19:53:00 +0800

    add Level-3

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
