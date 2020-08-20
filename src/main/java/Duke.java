1bd1000f2f9aac58490fd7188cfbd922e6e7dc44
porkeypine <huiling940@gmail.com>
2020-08-20 23:11:31 +0800

    A-Enums - implemented enums for Duke's character lines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,9 +8,23 @@ import java.util.Set;
 import static java.lang.Integer.parseInt;
 
 public class Duke {
-//    private enum Commands {
-//        LIST, DONE, TODO, DEADLINE, EVENT, BYE
-//    }
+    private enum Speaking {
+        GREET("My name? You don't need to know that. \nStop bothering me already... What do you want??"),
+        BYE("Finally... don't come back if you can possibly help it, please."),
+        LIST("I wouldn't bother doing them if I were you."),
+        DONE("Oh goody... You actually accomplished something!!\n  "),
+        ADD("You're making me feel tired... But if you insist:\n  "),
+        DELETE("Oh, getting lazy are we? I approve. I've removed this:\n  "),
+        EXCEPTION("Jesus, what are you doing?"),
+        INVALIDINPUT("Invalid input, dummy."),
+        INVALIDCOMMAND("I don't understand what you mean.");
+
+        private final String line;
+
+        private Speaking(String line) {
+            this.line = line;
+        }
+    }
 
     public static String getFirstWord(String command) {
         return command.contains(" ") ? command.split(" ")[0] : command;
@@ -34,25 +48,24 @@ public class Duke {
                     for (int i = 0; i < tasks.size(); i++) {
                         System.out.println(i + 1 + ". " + tasks.get(i));
                     }
-                    System.out.println("I wouldn't bother doing them if I were you.");
+                    System.out.println(Speaking.LIST.line);
                 } else if (getFirstWord(command).equals("done")) {
                     try {
                         int taskNumber = parseInt(command.split(" ")[1]) - 1;
                         tasks.get(taskNumber).markAsDone();
-                        System.out.println("Oh goody... You actually accomplished something!!\n" +
-                                tasks.get(taskNumber));
+                        System.out.println(Speaking.DONE.line + tasks.get(taskNumber));
                     } catch (Exception e) {
-                        throw new DukeException("Invalid input, dummy. Specify the task number correctly.");
+                        throw new DukeException(Speaking.INVALIDINPUT.line + " Specify the task number correctly.");
                     }
                 } else if (getFirstWord(command).equals("todo")) {
                     try {
                         String toAdd = command.split(" ", 2)[1];
                         tasks.add(new Todo(toAdd));
                         int length = tasks.size();
-                        System.out.println("You're making me feel tired... But if you insist:\n  " + tasks.get(length-1) +
+                        System.out.println(Speaking.ADD.line + tasks.get(length-1) +
                                 "\nYou now have " + length + (length == 1 ? " thing" : " things") + " in your list");
                     } catch (Exception e) {
-                        throw new DukeException("Invalid input, dummy. Did you put your task after a space?");
+                        throw new DukeException(Speaking.INVALIDINPUT.line + " Did you put your task after a space?");
                     }
                 } else if (getFirstWord(command).equals("deadline")) {
                     try {
@@ -60,10 +73,10 @@ public class Duke {
                         String[] descAndDeadline = wholeTask.split(" /by ");
                         tasks.add(new Deadline(descAndDeadline[0], descAndDeadline[1]));
                         int length = tasks.size();
-                        System.out.println("You're making me feel tired... But if you insist:\n  " + tasks.get(length-1) +
+                        System.out.println(Speaking.ADD.line + tasks.get(length-1) +
                                 "\nYou now have " + length + (length == 1 ? " thing" : " things") + " in your list");
                     } catch (Exception e) {
-                        throw new DukeException("Invalid input, dummy. Did you put a task before and deadline after ' /by '?");
+                        throw new DukeException(Speaking.INVALIDINPUT.line + " Did you put a task before and deadline after ' /by '?");
                     }
                 } else if (getFirstWord(command).equals("event")) {
                     try {
@@ -71,10 +84,10 @@ public class Duke {
                         String[] descAndTime = wholeTask.split(" /at ");
                         tasks.add(new Event(descAndTime[0], descAndTime[1]));
                         int length = tasks.size();
-                        System.out.println("You're making me feel tired... But if you insist:\n  " + tasks.get(length-1) +
+                        System.out.println(Speaking.ADD.line + tasks.get(length-1) +
                                 "\nYou now have " + length + (length == 1 ? " thing" : " things") + " in your list");
                     } catch (Exception e) {
-                        throw new DukeException("Invalid input, dummy. Did you put a task before and time after ' /at '?");
+                        throw new DukeException(Speaking.INVALIDINPUT.line + " Did you put a task before and time after ' /at '?");
                     }
                 } else if (getFirstWord(command).equals("delete")) {
                     try {
@@ -82,26 +95,26 @@ public class Duke {
                         Task deletedTask = tasks.get(taskNumber);
                         tasks.remove(taskNumber);
                         int length = tasks.size();
-                        System.out.println("Oh, getting lazy are we? I approve. I've removed this:\n  " + deletedTask +
+                        System.out.println(Speaking.DELETE.line + deletedTask +
                                 "\nYou now have " + length + (length == 1 ? " thing" : " things") + " in your list");
                     } catch (Exception e) {
-                        throw new DukeException("Invalid input, dummy. Specify the task number correctly.");
+                        throw new DukeException(Speaking.INVALIDINPUT.line + " Specify the task number correctly.");
                     }
                 } else {
-                    throw new DukeException("I don't understand what you mean.");
+                    throw new DukeException(Speaking.INVALIDCOMMAND.line);
                 }
                 command = sc.nextLine();
                 command = command.trim();
             }
-            System.out.println("Finally... don't come back if you can possibly help it, please.");
+            System.out.println(Speaking.BYE.line);
         } catch (DukeException e) {
-            System.out.println("Jesus, what are you doing? " + e.getMessage());
+            System.out.println(Speaking.EXCEPTION.line + " " + e.getMessage());
             readCommands(tasks);
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("My name? You don't need to know that. \nStop bothering me already... What do you want??");
+        System.out.println(Speaking.GREET.line);
         readCommands(new ArrayList<Task>());
     }
 }
