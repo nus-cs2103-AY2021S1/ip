7fe9c52b6fceb0152edb7dedb1a51ec1a18bfede
xnmng <puaxuanmingpxm18@gmail.com>
2020-08-20 20:26:04 +0800

    implemented Automated Text UI Testing

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,7 +10,7 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "✓" : "✘"); //return tick or X symbols
     }
 
     public void markAsDone() {
