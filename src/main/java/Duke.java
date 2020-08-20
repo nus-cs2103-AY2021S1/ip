dae8c04a7c7cbf33782b2c5c228e11eac6cf0114
teekoksiang <teekoksiang89@gmail.com>
2020-08-20 19:42:50 +0800

    Implement delete task feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,45 +27,49 @@ public class Duke {
     }
 
     private static void addTask(String command) {
-        List<String> words = Arrays.asList(command.split(" "));
-        String taskType = words.get(0);
-        Task task;
-        if (taskType.equalsIgnoreCase("todo")) {
-            if (words.size() <= 1) {
-                throw new IllegalArgumentException(":( Oops!!! The description of a Todo task cannot be empty. :-(");
+        try {
+            List<String> words = Arrays.asList(command.split(" "));
+            String taskType = words.get(0);
+            Task task;
+            if (taskType.equalsIgnoreCase("todo")) {
+                if (words.size() <= 1) {
+                    throw new IllegalArgumentException(":( Oops!!! The description of a Todo task cannot be empty. :-(");
+                }
+                String taskDescription = combineWords(words.subList(1, words.size()));
+                task = new Todo(taskDescription);
+            } else if (taskType.equalsIgnoreCase("deadline")) {
+                int index = words.indexOf("/by");
+                if (words.size() <= 1 || index == 1) {
+                    throw new IllegalArgumentException(":( Oops!!! The description of a Deadline task cannot be empty. :-(");
+                }
+                if (index == -1 || words.size() - index <= 1) {
+                    throw new IllegalArgumentException(":( Oops!!! Please type \"deadline [task description] /by [deadline] \" to add a Deadline task");
+                }
+                String taskDescription = combineWords(words.subList(1, index));
+                String by = combineWords(words.subList(index + 1, words.size()));
+                task = new Deadline(taskDescription, by);
+            } else if (taskType.equalsIgnoreCase("event")) {
+                int index = words.indexOf("/at");
+                if (words.size() <= 1 || index == 1) {
+                    throw new IllegalArgumentException(":( Oops!!! The description of a Event task cannot be empty. :-(");
+                }
+                if (index == -1 || words.size() - index <= 1) {
+                    throw new IllegalArgumentException(":( Oops!!! Please type \"event [task description] /at [event time] \" to add a Event task");
+                }
+                String taskDescription = combineWords(words.subList(1, index));
+                String at = combineWords(words.subList(index + 1, words.size()));
+                task = new Event(taskDescription, at);
+            } else {
+                throw new IllegalArgumentException(":( Oops!!! I'm sorry, but I don't know what that means :-(");
             }
-            String taskDescription = combineWords(words.subList(1, words.size()));
-            task = new Todo(taskDescription);
-        } else if (taskType.equalsIgnoreCase("deadline")) {
-            if (words.size() <= 1) {
-                throw new IllegalArgumentException(":( Oops!!! The description of a Deadline task cannot be empty. :-(");
-            }
-            int index = words.indexOf("/by");
-            if (index == -1 || words.size() - index <= 1) {
-                throw new IllegalArgumentException(":( Oops!!! Please type \"deadline [task description] /by [deadline] \" to add a Deadline task");
-            }
-            String taskDescription = combineWords(words.subList(1, index));
-            String by = combineWords(words.subList(index + 1, words.size()));
-            task = new Deadline(taskDescription, by);
-        } else if (taskType.equalsIgnoreCase("event")) {
-            if (words.size() <= 1) {
-                throw new IllegalArgumentException(":( Oops!!! The description of a Event task cannot be empty. :-(");
-            }
-            int index = words.indexOf("/at");
-            if (index == -1 || words.size() - index <= 1) {
-                throw new IllegalArgumentException(":( Oops!!! Please type \"event [task description] /at [event time] \" to add a Event task");
-            }
-            String taskDescription = combineWords(words.subList(1, index));
-            String at = combineWords(words.subList(index + 1, words.size()));
-            task = new Event(taskDescription, at);
-        } else {
-            throw new IllegalArgumentException(":( Oops!!! I'm sorry, but I don't know what that means :-(");
+            taskList.add(task);
+            String message = "Got it. I've added this task:";
+            message += "\n\t\t".concat(task.toString());
+            message += String.format("\n\tNow you have %d tasks in the list.", taskList.size());
+            printResponse(message);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            printResponse(illegalArgumentException.getLocalizedMessage());
         }
-        taskList.add(task);
-        String message = "Got it. I've added this task:";
-        message += "\n\t\t".concat(task.toString());
-        message += String.format("\n\tNow you have %d tasks in the list.", taskList.size());
-        printResponse(message);
     }
 
     private static void listTasks() {
@@ -85,7 +89,7 @@ public class Duke {
     private static int extractTaskNumber(String command) {
         List<String> words = Arrays.asList(command.split(" "));
         if (words.size() != 2) {
-            throw new IllegalArgumentException("Please type \"done [task number]\" to mark task as done");
+            throw new IllegalArgumentException("Please type \"[action] [task number]\"");
         } else {
             // convert the task number into int
             int taskNumber = Integer.parseInt(words.get(1));
@@ -96,16 +100,36 @@ public class Duke {
     private static void doneTask(String command) {
         try {
             int taskNumber = extractTaskNumber(command);
-            if (taskNumber > taskList.size()) {
+            if (taskNumber > taskList.size() || taskNumber < 1) {
                 throw new IllegalArgumentException("The task number is not found");
             }
             taskList.get(taskNumber - 1).markDone();
-            String doneMessage = "Nice! I've marked this task as done:";
-            doneMessage += String.format("\n\t\t".concat(taskList.get(taskNumber - 1).toString()));
-            printResponse(doneMessage);
+            String response = "Nice! I've marked this task as done:";
+            response += String.format("\n\t\t".concat(taskList.get(taskNumber - 1).toString()));
+            printResponse(response);
         } catch (IllegalArgumentException illegalArgumentException) {
             if (illegalArgumentException instanceof NumberFormatException) {
                 printResponse("Please type \"done [task number]\" to mark task as done");
+            } else {
+                printResponse(illegalArgumentException.getLocalizedMessage());
+            }
+        }
+    }
+
+    private static void deleteTask(String command) {
+        try {
+            int taskNumber = extractTaskNumber(command);
+            if (taskNumber > taskList.size() || taskNumber < 1) {
+                throw new IllegalArgumentException("The task number is not found");
+            }
+            String response = "Noted. I've removed this task:";
+            response += String.format("\n\t\t".concat(taskList.get(taskNumber - 1).toString()));
+            taskList.remove(taskNumber - 1);
+            response += String.format("\n\tNow you have %d tasks in the list.", taskList.size());
+            printResponse(response);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            if (illegalArgumentException instanceof NumberFormatException) {
+                printResponse("Please type \"[action] [task number]\"");
             } else {
                 printResponse(illegalArgumentException.getLocalizedMessage());
             }
@@ -121,16 +145,13 @@ public class Duke {
                 listTasks();
             } else if (command.startsWith("done")) {
                 doneTask(command);
+            } else if (command.startsWith("delete")) {
+                deleteTask(command);
             } else {
-                try {
-                    addTask(command);
-                } catch (IllegalArgumentException illegalArgumentException) {
-                    printResponse(illegalArgumentException.getLocalizedMessage());
-                }
+                addTask(command);
             }
             command = scanner.nextLine();
         }
-
         scanner.close();
     }
 
