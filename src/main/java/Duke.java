f430bf20d60ba4ff11b60fcae95a75035c753828
yanbingtao <yanbingtao963@Gmail.com>
2020-08-20 17:02:30 +0800

    Add comments for Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,7 @@ public class Duke {
         Feedbacks.greetingMsg();
         String inputMsg = sc.nextLine();
 
+        // The program will continue looping if the user didn't enter "bye" to terminate the program
         while (!inputMsg.equals("bye")) {
             if (inputMsg.equals("list")) {
                 Feedbacks.getAllTasksMsg(tasks);
@@ -20,8 +21,10 @@ public class Duke {
                     int index = Integer.parseInt(inputMsg.split("done ")[1]);
                     Task.doneTask(index, tasks);
                 } catch (IndexOutOfBoundsException e) {
+                    // if the user doesn't type the index after the keyword "done"
                     Warnings.invalidDoneTaskIndex(tasks.size());
                 } catch (NumberFormatException e) {
+                    // if the user doesn't key in a valid index after keyword "done"
                     Warnings.invalidDoneTaskIndex(tasks.size());
                 }
             } else if (inputMsg.startsWith("todo")) {
@@ -30,6 +33,7 @@ public class Duke {
                     taskTitle = inputMsg.split("todo ")[1];
                     ToDos.addTodoTask(taskTitle, tasks);
                 } catch (ArrayIndexOutOfBoundsException e) {
+                    // if the user doesn't type the the task description after the keyword "todo"
                     Warnings.invalidToDo();
                 }
             } else if (inputMsg.startsWith("deadline")) {
@@ -40,6 +44,7 @@ public class Duke {
                     deadlineTime = inputMsg.split("deadline ")[1].split("/by")[1];
                     Deadlines.addDeadlineTask(taskTitle, deadlineTime, tasks);
                 } catch (ArrayIndexOutOfBoundsException e) {
+                    // if the user doesn't follow the correct format after the keyword "deadline"
                     Warnings.invalidDeadline();
                 }
             } else if (inputMsg.startsWith("event")) {
@@ -48,6 +53,7 @@ public class Duke {
                     String eventTime = inputMsg.split("event ")[1].split("/at")[1];
                     Events.addEventTask(taskTitle, eventTime, tasks);
                 } catch (ArrayIndexOutOfBoundsException e) {
+                    // if the user doesn't follow the correct format after the keyword "event"
                     Warnings.invalidEvent();
                 }
             } else if (inputMsg.startsWith("delete")) {
@@ -55,16 +61,21 @@ public class Duke {
                     int index = Integer.parseInt(inputMsg.split("delete ")[1]);
                     Task.deleteTask(index, tasks);
                 } catch (IndexOutOfBoundsException e) {
+                    // if the user doesn't type the index after the keyword "delete"
                     Warnings.invalidDelete(tasks.size());
                 } catch (NumberFormatException e) {
+                    // if the user doesn't key in a valid index after keyword "delete"
                     Warnings.invalidDelete(tasks.size());
                 }
 
             } else {
+                // if the user randomly enter any other commands which are not inside the command list
                 Warnings.invalidInput();
             }
+            // waiting for user to key in the next request
             inputMsg = sc.nextLine();
         }
+        // say bye to the user
         Feedbacks.byeMsg();
     }
 }
