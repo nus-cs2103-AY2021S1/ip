0c09eb1dcaf44ae25f79342bfc144152d5c6b3bb
Eryn <yeryeon@gmail.com>
2020-08-20 13:06:51 +0900

    Complete Level-1 Greet, Echo, Exit
    A skeletal version of Wyre that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String border = "\n^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^\n";
+        Scanner sc = new Scanner(System.in);
+        String command;
+
         String logo = " _       _ \n"
                 + "| |  _  | |_   _ ____ ___\n"
                 + "| | | | | | |_/ |  _ \\  _ \\ \n"
                 + "| |_| |_| |\\___ |    <  __/\n"
                 + "\\___/\\___/ \\____|_| \\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(border + "Hi! I'm Wyre, your Personal Assistant Chatbot! :>\nWhat can I do for you today?" + border);
+        while(true) {
+            command = sc.next();
+            if(command.equals("bye")) {
+                System.out.println(border + "Bye. Hope to see you again!" + border);
+                break;
+            } else {
+                System.out.println(border + command + border);
+            }
+        }
+
     }
 }
