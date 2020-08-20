8bba5d5d202674e84c42e0e0faa4a47adea3213a
yuanxing-y <zyx19991208@gmail.com>
2020-08-20 20:57:59 +0800

    Added Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,16 +7,19 @@ public class Duke {
 
     public static String horizontal_line = "____________________________________________________________";
 
+    public static void printWithBorder(String content) {
+        System.out.println(horizontal_line);
+        System.out.println(content);
+        System.out.println(horizontal_line);
+    }
+
     public static void greeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
-        System.out.println(horizontal_line);
-        System.out.println("Hello from\n" + logo);
-        System.out.println(horizontal_line);
+        printWithBorder("Hello from\n" + logo);
     }
 
     public static void showList() {
@@ -29,26 +32,48 @@ public class Duke {
         System.out.println(horizontal_line);
     }
 
+    public static void addTaskMsg(Task task) {
+        printWithBorder("Got it. I've added this task: \n "
+                + task + "\n"
+                + String.format("Now you have %d tasks in the list.", taskList.size()));
+    }
+
     public static void response(String input) {
-        if (input.equals("bye")) {
-            System.out.println(horizontal_line);
-            System.out.println("Bye. Hope to see you again soon!");
-            System.out.println(horizontal_line);
-        } else if (input.equals("list")) {
-            showList();
-        } else if (input.split(" ").length == 2 && input.split(" ")[0].equals("done")){
-            int doneIndex = Integer.parseInt(input.split(" ")[1]);
-            Task task = taskList.get(doneIndex - 1);
-            task.markAsDone();
-            System.out.println(horizontal_line);
-            System.out.println("Nice! I've marked this task as done: ");
-            System.out.println(task);
-            System.out.println(horizontal_line);
-        } else {
-            taskList.add(new Task(input));
-            System.out.println(horizontal_line);
-            System.out.println("added: "+ input);
-            System.out.println(horizontal_line);
+        String[] words = input.split(" ");
+        String command = words[0];
+        if (words.length == 1) {
+            if (command.equals("bye")) {
+                printWithBorder("Bye. Hope to see you again soon!");
+            }
+            if (command.equals("list")) {
+                showList();
+            }
+        }
+        if (words.length > 1) {
+            String description = input.split(" ", 2)[1];
+            if (command.equals("done")) {
+                int doneIndex = Integer.parseInt(description);
+                Task task = taskList.get(doneIndex - 1);
+                task.markAsDone();
+                printWithBorder("Nice! I've marked this task as done: \n" + task);
+            }
+            if (command.equals("todo")) {
+                ToDo todo = new ToDo(description);
+                taskList.add(todo);
+                addTaskMsg(todo);
+            }
+            if (command.equals("deadline")) {
+                String[] contentAndTime = description.split(" /by ");
+                Deadline ddl = new Deadline(contentAndTime[0], contentAndTime[1]);
+                taskList.add(ddl);
+                addTaskMsg(ddl);
+            }
+            if (command.equals("event")) {
+                String[] contentAndTime = description.split(" /at ");
+                Event e = new Event(contentAndTime[0], contentAndTime[1]);
+                taskList.add(e);
+                addTaskMsg(e);
+            }
         }
     }
 
