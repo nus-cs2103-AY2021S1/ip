dc0b053bbd4e199d3e010d98098a12dbcd18c266
Zhang Yunjie <e0421414@u.nus.edu>
2020-08-20 18:49:13 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,11 +13,10 @@ public class Duke {
 
         String line = "____________________________________________________________\n";
         String greeting = "Hello! I'm Duke from the chat bot universe ~ Nice to meet you (●´∀｀●) \n" +
-                           "You are the first human I've ever met. Say something to me!\n";
+                           "I'll be your task manager from now onwards.\n";
         String bye = "Awwww, I guess you are gonna leave... \n" +
-                "Human language is so fun! " +
-                "But it's time for me to sleep again (yesss I'm just a little sleepy chat bot xD )\n" +
-                "Nudge me if you wanna talk again! Have a nice day ❤️\n ";
+                "I'll keep track of your tasks nicely. \n" +
+                "Text me if you wanna talk again! Have a nice day ❤️\n ";
 
         System.out.println(line + greeting + line);
         Scanner sc = new Scanner(System.in);
@@ -26,17 +25,28 @@ public class Duke {
 
         while (sc.hasNextLine()) {
             String temp = sc.nextLine();
-            String[] pieces = temp.split("\\s+");
+            //String[] pieces = temp.split("\\s+");
+            String [] pieces = temp.split(" ", 2);
             if (temp.equals("bye")) {
                 System.out.println(line + bye + line);
                 return;
             } else if (temp.equals("list")) {
                 System.out.println(line + "Here are the tasks in your list: \n");
                 for (int i = 1; i < count + 1; i++) {
-                    Task cur = list[i-1];
-                    System.out.println("" + i + ". [" + cur.getStatusIcon()+ "] " + cur );
+                    Task cur = list[i - 1];
+                    String type = "";
+                    if (cur instanceof ToDo) {
+                        type = "[T]";
+                    } else if (cur instanceof Deadline) {
+                        type = "[D]";
+                    } else {
+                        type = "[E]";
+                    }
+                    System.out.println("" + i + "." + type + "[" + cur.getStatusIcon() + "] " + cur);
                 }
                 System.out.println(line);
+
+
             } else if (pieces[0].equals("done")){
                 int task = Integer.valueOf(pieces[1]);
                 Task cur = list[task-1];
@@ -46,19 +56,45 @@ public class Duke {
                         "[" + cur.getStatusIcon()+ "] " + cur);
                 System.out.println(line);
 
-
-
             } else {
-                System.out.println(line);
-                System.out.println("added: " + temp);
-                System.out.println(line);
-                list[count] = new Task(temp);
+                String s = "";
+                String[] array = new String[1];
+                Task t = new Task("");
+                switch(pieces[0]) {
+                    case "todo":
+                        t = new ToDo(pieces[1]);
+                        s = "[T][\u2718] " + t;
+                        break;
+
+                    case "deadline":
+                        array = pieces[1].split("/by");
+                        t = new Deadline(array[0], array[1]);
+                        s = "[D][\u2718] " + t;
+                        break;
+
+                    case "event":
+                        array = pieces[1].split("/at");
+                        t = new Event(array[0], array[1]);
+                        s = "[E][\u2718] " + t;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                list[count] = t;
                 count++;
+
+                System.out.println(line);
+                System.out.println("Got it. I've added this task: \n" + s );
+                System.out.println("Now you have " + count + " tasks in the list. ");
+                System.out.println(line);
+
             }
                 //echo
                 //System.out.println(line + temp + "\n" + line);
-            }
-
         }
+
     }
+}
 
---
+++ src/main/java/ToDo.java
@@ -0,0 +1,7 @@
+public class ToDo extends Task {
+
+    public ToDo(String description) {
+        super(description);
+    }
+
+}
