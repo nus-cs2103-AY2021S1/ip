5ee3f009a5459872ead04e476b4c53a006b424d2
Xue Yong <xueyong26@hotmail.com>
2020-08-20 17:09:11 +0800

    Added Add, List functionality to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,41 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static final String line = "____________________________________________________________\n";
     private static final String bye = "bye";
+    private static final String list = "list";
+    private ArrayList<String> input;
 
+    private Duke() {
+        input = new ArrayList<>();
+    }
 
     public void init() {
         Scanner sc = new Scanner(System.in);
-        String command = sc.next();
+        String command = sc.nextLine();
         while (!command.equals(bye)) {
-            System.out.println(line + command + "\n" + line);
-            command = sc.next();
+            if (command.equals(list)) {
+                display();
+            } else {
+                add(command);
+            }
+            command = sc.nextLine();
         }
         System.out.println(line + "Cya!!" + "\n" + line);
+    }
+
+    private void display() {
+        System.out.println(line);
+        for (int i = 0; i < input.size(); i++) {
+            System.out.println(String.valueOf(i+1) + ". " + input.get(i) + "\n");
+        }
+        System.out.println(line);
+    }
+
+    private void add(String reminder) {
+        input.add(reminder);
+        System.out.println(line + "added: " + reminder + "\n" + line);
     }
 
     public static void main(String[] args) {
