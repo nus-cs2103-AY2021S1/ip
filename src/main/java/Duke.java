f5032652005f7265144256f261735ea2eac2a616
teekoksiang <teekoksiang89@gmail.com>
2020-08-20 12:26:45 +0800

    Implement add and list chatbot actions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,8 +7,31 @@ public class Duke {
     private static final String EXIT_MESSAGE = "Bye. See you again soon!";
     private static final String DIVIDER = "\n\t-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n";
 
+    private static final List<String> actionList = new ArrayList<>();
+
     private static void printResponse(String message) {
         System.out.println(DIVIDER.concat("\t".concat(message.concat("\n"))).concat(DIVIDER));
+    }
+
+    private static void addAction(String action) {
+        actionList.add(action);
+        printResponse("added: ".concat(action));
+    }
+
+    private static void listActions() {
+        if (actionList.size() == 0) {
+            printResponse("No action added yet...");
+            return;
+        }
+        StringBuilder actions = new StringBuilder();
+        for (int i = 0; i < actionList.size(); i++) {
+            String action = String.format("%d. %s", (i + 1), actionList.get(i));
+            if (i > 0) {
+                action = "\n\t".concat(action);
+            }
+            actions.append(action);
+        }
+        printResponse(actions.toString());
     }
 
     private static void listenCommand() {
@@ -14,7 +39,11 @@ public class Duke {
         String command = scanner.nextLine();
 
         while (!command.equalsIgnoreCase("bye")) {
-            printResponse(command);
+            if (command.equalsIgnoreCase("list")) {
+                listActions();
+            } else {
+                addAction(command);
+            }
             command = scanner.nextLine();
         }
 
