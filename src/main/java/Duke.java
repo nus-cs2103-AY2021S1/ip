f9aa9aaf99104bc6f37e2746ac6807b32374f9da
MerlinLim <merlinlim98@gmail.com>
2020-08-20 23:41:37 +0800

    Adds todo, deadline and event class
    Todo deadline and event class are subclasses of Task.
    Todo class is for tasks with no date/time attached
    Deadline class is for tasks with a data/time to indicate when the
    dealine is.
    Event class is for tasks with date/time to indicate when it starts and
    when it ends.
    
    Data/time are strings.
    
    The main includes todo, deadline and event features. If user specifies
    which type of task to do, the main will handle each type accordingly.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,7 +24,7 @@ public class Duke {
                 for (int i = 0; i < list.size(); i++) {
                     System.out.println((1 + i) + "." + list.get(i));
                 }
-            } else if (input_split[0].equals("done") && input_split.length == 2){
+            } else if (input_split[0].equals("done") && input_split.length == 2) {
                 // Marks number as done
                 int index;
                 // Parse int
@@ -34,12 +34,28 @@ public class Duke {
                 Task task = list.get(index);
                 task.markDone();
                 System.out.println("Nice! I've marked this task as done:\n" + task);
-
             } else {
                 // Adds item to list
-                Task newTask = new Task(input);
-                list.add(newTask);
-                System.out.println("added: " + input);
+                String type = input_split[0];
+                String name;
+                if (type.equals("todo")){
+                    name = input.split("todo ")[1];
+                    list.add(new ToDo(name));
+                }else if(type.equals("deadline")){
+                    name = input.split(" /by ")[0].split("deadline ")[1];
+                    String by = input.split(" /by ")[1];
+                    list.add(new Deadline(name, by));
+                }else if(type.equals("event")){
+                    name = input.split(" /at ")[0].split("event ")[1];
+                    String at = input.split(" /at ")[1];
+                    list.add(new Event(name, at));
+                }else{
+                    list.add(new Task(input));
+                    name = input;
+                }
+
+                System.out.println("Go it. I've added this task:\n" + list.get(list.size()-1).toString());
+                System.out.println("Now you have " + list.size() + " tasks in the list.");
             }
         }
 
@@ -52,8 +68,9 @@ public class Duke {
 }
 
 class Task {
-    private String name;
-    private boolean done;
+    protected String name;
+    protected boolean done;
+
     public Task(String name) {
         this.name = name;
         this.done = false;
@@ -74,5 +91,44 @@ class Task {
     public String toString(){
         String symbol = done ? "\u2713" : "\u2718";
         return "[" + symbol + "] " + name;
+    }
+}
+
+class ToDo extends Task {
+    public ToDo(String name){
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String name, String by) {
+        super(name);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    protected String at;
+
+    public Event (String name, String at) {
+        super(name);
+        this.at = at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + at + ")";
     }
 }
