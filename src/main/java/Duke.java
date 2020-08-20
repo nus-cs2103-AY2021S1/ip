ae618f8abb39a8054134e02640ead906dba5208c
kaitlynng <Kaitlyn.nky@gmail.com>
2020-08-20 11:45:36 +0800

    Done level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,14 @@ import java.util.ArrayList;
 
 public class Duke {
 
+    private static final String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String botName = "Duke";
+    private static final String userName = "You";
+
     private static final String line = "------------------------------------------------------------------";
 
     //commands
@@ -10,24 +18,19 @@ public class Duke {
     private static final String CMD_LIST = "list";
     private static final String CMD_DONE = "done";
 
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String botName = "Duke";
-        String userName = "You";
+    private static final String CMD_TODO = "todo";
+    private static final String CMD_DEADLINE = "deadline";
+    private static final String CMD_EVENT = "event";
 
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         Boolean running = true;
 
         ArrayList<Task> tasks = new ArrayList<>();
 
-        System.out.println("привет, меня зовут \n" + logo + "\n\n" +
-                "Приветствую мою Родину, Мать Россию.\n" +
-                "Что ты хочешь?\n" + line);
+        System.out.println("Hi, my nmae is \n" + logo + "\n\n" +
+                "How can I hlep you taody?\n" + line);
 
         while (running) {
             System.out.print(userName + " said: ");
@@ -36,17 +39,51 @@ public class Duke {
             //split command
             int space_idx = userInput.indexOf(' ');
             String command = space_idx  == -1 ? userInput : userInput.substring(0, space_idx);
-            String rest = space_idx == -1 ? "" : userInput.substring(space_idx + 1);
+            String rest = space_idx == -1 ? "" : userInput.substring(space_idx + 1).trim();
+
+            int date_idx;
 
             switch(command) {
                 case CMD_EXIT:
                     running = false;
                     break;
+
+                case CMD_TODO:
+                    tasks.add(new Todo(rest));
+                    System.out.println(fmtMsg("I hvae adedd a new tsak:\n  " + tasks.get(tasks.size() - 1) +
+                                              "\nYou now hvae " +  tasks.size() + " tskas in yuor lsit."));
+                    break;
+
+                case CMD_DEADLINE:
+                    date_idx = rest.indexOf("/by");
+                    if (date_idx == -1) {
+                        System.out.println(fmtMsg("You need to sepifcy a dtae or tmie."));
+                    } else {
+                        tasks.add(new Deadline(rest.substring(0, date_idx).trim(),
+                                rest.substring(date_idx + 3).trim()));
+                        System.out.println(fmtMsg("I hvae adedd a new tsak:\n  " + tasks.get(tasks.size() - 1) +
+                                "\nYou now hvae " +  tasks.size() + " tskas in yuor lsit."));
+                    }
+
+                    break;
+
+                case CMD_EVENT:
+                    date_idx = rest.indexOf("/at");
+                    if (date_idx == -1) {
+                        System.out.println(fmtMsg("You need to sepifcy a dtae or tmie."));
+                    } else {
+                        tasks.add(new Event(rest.substring(0, date_idx).trim(),
+                                rest.substring(date_idx + 3).trim()));
+                        System.out.println(fmtMsg("I hvae adedd a new tsak:\n  " + tasks.get(tasks.size() - 1) +
+                                "\nYou now hvae " +  tasks.size() + " tskas in yuor lsit."));
+                    }
+                    break;
+
                 case CMD_LIST:
                     if (tasks.isEmpty()) {
-                        System.out.println(fmtMsg("В списке нет ничего глупого."));
+                        System.out.println(fmtMsg("You dno't hvae any tsaks ctrunelry."));
                     } else {
-                        String msg = "Вот что в списке:";
+                        String msg = "\nHree are yuor tkass:";
                         for (int i = 0; i < tasks.size(); i++) {
                             msg += "\n" +(i + 1) + ". " + tasks.get(i);
                         }
@@ -58,27 +95,26 @@ public class Duke {
                     try {
                         int idx = Integer.parseInt(rest) - 1;
                         if (idx < 0 || idx >= tasks.size()) {
-                            System.out.println(fmtMsg("Нет такой задачи, тупица."));
+                            System.out.println(fmtMsg("Yuo dno't hvae a tsak wtih tihs lbeal."));
                         } else {
                             tasks.get(idx).setDone(true);
-                            System.out.println(fmtMsg("Наконец-то натворил ты, ленивая задница.\n" +
+                            System.out.println(fmtMsg("Mkread tsak as cteopmle.\n" +
                                                       "  " + tasks.get(idx)));
                         }
                     } catch (NumberFormatException e) {
-                        System.out.println(fmtMsg("Введите число после команды, тупица."));
+                        System.out.println(fmtMsg("Psaele sfiepcy the iednx of the tsak you cpmolteed."));
                     }
                     break;
 
                 default:
-                    tasks.add(new Task(userInput));
-                    System.out.println(fmtMsg(botName + " said: " + "Я добавил " + userInput + " в список."));
+                    System.out.println(fmtMsg("Nteod wtih tnakhs."));
             }
         }
 
-        System.out.println(fmtMsg(botName + " said: Пока, надеюсь никогда больше не увидеть тебя!"));
+        System.out.println(fmtMsg("Bye, hpoe to nveer see you aiagn."));
     }
 
     public static String fmtMsg(String msg) {
-        return line + "\n" + msg + "\n" + line;
+        return line + "\n" + botName + " said: " + msg + "\n" + line;
     }
 }
---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+public class Event extends Task {
+    protected String by;
+
+    public Event(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + by + ")";
+    }
+}
