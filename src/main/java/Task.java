a12d24794af7dca737392bd4e80a3a23a90a175d
FooJingYi <foojingyi@comp.nus.edu.sg>
2020-08-20 16:35:39 +0800

    Added Task.java class, adapting from example given

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+//Adapted from https://nus-cs2103-ay2021s1.github.io/website/schedule/week2/project.html
+public class Task {
+    private final String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "\u2713" : "\u2718"; //return tick or cross symbols
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+}
