85d1cacf19326e148db4b90e8d6e501297711de0
wengfaing <wengfaing@gmail.com>
2020-08-20 23:59:19 +0800

    TextUITest

--- src/Event.java
+++ src/Event.java
@@ -8,7 +8,7 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at:" + at + ")";
+    return "[E]" + super.toString() + " (at:" + at + ")";
     }
 
 }
