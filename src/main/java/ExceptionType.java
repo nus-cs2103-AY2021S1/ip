304a7ed4350d022fbf09b2cdae5771634076f797
Junhua <wenjh1998@gmail.com>
2020-08-20 15:15:35 +0800

    Added an IOException to the Exception type

--- src/main/java/ExceptionType.java
+++ src/main/java/ExceptionType.java
@@ -2,7 +2,8 @@ public enum ExceptionType {
     NO_ARGUMENTS_SUPPLIED("No arguments supplied for %s"),
     INVALID_ARGUMENT("Invalid value of %s with '%s'"),
     INVALID_NUMBER("'%s' is not a valid number"),
-    INVALID_COMMAND("Invalid command '%s'");
+    INVALID_COMMAND("Invalid command '%s'"),
+    IO_EXCEPTION("How do I not have permission for %s");
 
     //Store the error message
     private final String errorMessage;
