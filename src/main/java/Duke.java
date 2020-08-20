454fbfc45f27c5d2b07da88216a1b407abb27d1f
jordanyoong <jordanyoongjiaen@gmail.com>
2020-08-20 20:50:51 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        chat();
+    }
+
+    public static void chat() {
         String start = "Hello! I'm Duke \nWhat can I do for you?";
         System.out.println(start);
         Scanner sc = new Scanner(System.in);
@@ -11,6 +15,7 @@ public class Duke {
         while (true) {
             String in = sc.nextLine();
             if (in.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (in.equals("list")) {
                 for (int j = 0; j < arr.size(); j++) {
@@ -24,6 +29,5 @@ public class Duke {
                 System.out.println(toPrint);
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
