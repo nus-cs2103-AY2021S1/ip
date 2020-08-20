79a584d4faa1cc18c0acb354cebd4c3a8b577b69
lebencwb <46550747+lebencwb@users.noreply.github.com>
2020-08-20 22:26:48 +0800

    updated Level-1 of Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String textFormat(String str) {
+        return "\t" + "-------------------------------------------------------\n\t"
+                + str + "\n"
+                + "\t" + "-------------------------------------------------------\n";
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.print(textFormat("Hello! I'm Duke\n" + "\t" + "What can I do for you?"));
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(textFormat("Bye. Hope to see you again soon!"));
+                scanner.close();
+                break;
+            }
+            System.out.println(textFormat(userInput));
+        }
     }
 }
