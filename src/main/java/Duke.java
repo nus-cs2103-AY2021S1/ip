7345f13618f927d1c98399243be0e43e6a5f2e89
minhhhnguyen2000 <minh.hh.nguyen.2000@gmail.com>
2020-08-20 20:28:44 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,7 +25,7 @@ public class Duke {
                     System.out.println(i + "." + tasks[i - 1].getStatus());
                 }
             }
-            else if(cmd.substring(0, 5).equals("done ")) {
+            else if(cmd.length() >= 4 && cmd.substring(0, 4).equals("done")) {
                 int c = 0;
                 for(int i = 5; i < cmd.length(); ++i) {
                     c = c * 10 + cmd.charAt(i) - '0';
@@ -34,47 +34,71 @@ public class Duke {
                 tasks[c - 1].done();
                 System.out.println(tasks[c - 1].getStatus());
             }
-            else if(cmd.substring(0, 4).equals("todo")) {
-                Todo tmp = new Todo(cmd.substring(5));
-                System.out.println("Got it. I've added this task: ");
-                System.out.println("  " + tmp.getStatus());
-                tasks[numTask] = tmp;
-                numTask++;
-                System.out.println("Now you have " + numTask + " tasks in the list.");
-            }
-            else if(cmd.substring(0, 8).equals("deadline")) {
-                String getName = "", getDeadline = "";
-                for(int i = 0; i < cmd.length(); ++i) {
-                    if(cmd.charAt(i) == '/' && cmd.charAt(i + 1) == 'b' && cmd.charAt(i + 2) == 'y') {
-                        getDeadline = cmd.substring(i + 4);
-                        getName = cmd.substring(9, i - 1);
-                        break;
-                    }
+            else if(cmd.length() >= 4 && cmd.substring(0, 4).equals("todo")) {
+                try {
+                    checkCmd(cmd, 4, "☹ OOPS!!! The description of a todo cannot be empty.");
+                    String getName = cmd.substring(5);
+                    Todo tmp = new Todo(getName);
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("  " + tmp.getStatus());
+                    tasks[numTask] = tmp;
+                    numTask++;
+                    System.out.println("Now you have " + numTask + " tasks in the list.");
                 }
-                Deadline tmp = new Deadline(getName, getDeadline);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println("  " + tmp.getStatus());
-                tasks[numTask] = tmp;
-                numTask++;
-                System.out.println("Now you have " + numTask + " tasks in the list.");
-            }
-            else if(cmd.substring(0, 5).equals("event")) {
-                String getName = "", getTime = "";
-                for(int i = 0; i < cmd.length(); ++i) {
-                    if(cmd.charAt(i) == '/' && cmd.charAt(i + 1) == 'a' && cmd.charAt(i + 2) == 't') {
-                        getTime = cmd.substring(i + 4);
-                        getName = cmd.substring(6, i - 1);
-                        break;
-                    }
+                catch (DukeException ex) {
+                    System.out.println(ex.getMessage());
                 }
-                Event tmp = new Event(getName, getTime);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println("  " + tmp.getStatus());
-                tasks[numTask] = tmp;
-                numTask++;
-                System.out.println("Now you have " + numTask + " tasks in the list.");
-                //
             }
+            else if(cmd.length() >= 8 &&cmd.substring(0, 8).equals("deadline")) {
+                try {
+                    checkCmd(cmd, 8, "☹ OOPS!!! The description of a deadline cannot be empty.");
+                    String getName = "", getDeadline = "";
+                    for (int i = 0; i < cmd.length(); ++i) {
+                        if (cmd.charAt(i) == '/' && cmd.charAt(i + 1) == 'b' && cmd.charAt(i + 2) == 'y') {
+                            getDeadline = cmd.substring(i + 4);
+                            getName = cmd.substring(9, i - 1);
+                            break;
+                        }
+                    }
+                    Deadline tmp = new Deadline(getName, getDeadline);
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("  " + tmp.getStatus());
+                    tasks[numTask] = tmp;
+                    numTask++;
+                    System.out.println("Now you have " + numTask + " tasks in the list.");
+                }
+                catch (DukeException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+            else if(cmd.length() >= 5 &&cmd.substring(0, 5).equals("event")) {
+                try {
+                    checkCmd(cmd, 5, "☹ OOPS!!! The description of a event cannot be empty.");
+                    String getName = "", getTime = "";
+                    for (int i = 0; i < cmd.length(); ++i) {
+                        if (cmd.charAt(i) == '/' && cmd.charAt(i + 1) == 'a' && cmd.charAt(i + 2) == 't') {
+                            getTime = cmd.substring(i + 4);
+                            getName = cmd.substring(6, i - 1);
+                            break;
+                        }
+                    }
+                    Event tmp = new Event(getName, getTime);
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("  " + tmp.getStatus());
+                    tasks[numTask] = tmp;
+                    numTask++;
+                    System.out.println("Now you have " + numTask + " tasks in the list.");
+                    //
+                }
+                catch (DukeException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+            else System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
+    }
+
+    public static void checkCmd(String cmd, int len, String Ex) throws DukeException {
+        if(cmd.length() == len) throw new DukeException(Ex);
     }
 }
