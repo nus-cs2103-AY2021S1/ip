79d4d42f6142d920e596dc44b9a84d839837fe05
Jerry Ho <jerryho@u.nus.edu>
2020-08-20 20:41:15 +0800

    Add Task Class

---
+++ src/main/java/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+}
