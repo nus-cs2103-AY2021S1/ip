dae800dd85c5f53837c6283e5373ae8d90ff09e0
Gabriel Tan <58500317+gabztcr@users.noreply.github.com>
2020-08-20 17:38:15 +0800

    Add greeting, echo and exit for Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("————————————————————————————————————————————————————————————");
+        System.out.println("Hello! I'm Duke! \nWhat can I do for you?");
+        System.out.println("————————————————————————————————————————————————————————————");
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println("————————————————————————————————————————————————————————————");
+            System.out.println(command);
+            System.out.println("————————————————————————————————————————————————————————————");
+            command = sc.nextLine();
+        }
+
+        System.out.println("————————————————————————————————————————————————————————————");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("————————————————————————————————————————————————————————————");
+        sc.close();
     }
 }
