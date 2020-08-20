766567b27a9a4a2c1db76d9ef32c1f656c0672d3
Gabriel Tan <58500317+gabztcr@users.noreply.github.com>
2020-08-20 23:22:50 +0800

    Handle exceptions and errors for Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,29 +4,6 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static boolean isNumber(char c) {
-        int c_int = Character.getNumericValue(c);
-        return c_int <= 9 && c_int >= 0;
-    }
-
-    public static boolean isMarkingDone(String command) {
-        String[] lst = command.split(" ");
-
-        if (lst.length == 1 || !lst[0].equals("done")) {
-            return false;
-        } else {
-            String secondElement = lst[1];
-
-            if (secondElement.length() == 2) {
-                return isNumber(secondElement.charAt(0)) && isNumber(secondElement.charAt(1));
-            } else if (secondElement.length() == 1) {
-                return isNumber(secondElement.charAt(0));
-            } else {
-                return false;
-            }
-        }
-    }
-
     public static String numTasks(List<Task> lst) {
         int numTasks = lst.size();
         return numTasks == 1 ? "1 task" : numTasks + " tasks";
@@ -47,54 +24,54 @@ public class Duke {
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println(i + 1 + "." + tasks.get(i).toString());
                 }
-            } else if (isMarkingDone(command)) {
-                int taskId = Integer.parseInt(command.split(" ")[1]) - 1;
-                if (taskId < tasks.size()) {
-                    tasks.get(taskId).markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks.get(taskId).toString());
-                } else {
-                    System.out.println("Task does not exist; Try another task number.");
-                }
             } else {
                 String[] command_lst = command.split(" ", 2);
-
-                if (command_lst[0].equals("todo")) {
-                    if (command_lst.length == 2) {
+                
+                if (command_lst[0].equals("done")) {
+                    try {
+                        int taskId = Integer.parseInt(command.split(" ")[1]) - 1;
+                        tasks.get(taskId).markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(tasks.get(taskId).toString());
+                    } catch (Exception e) {
+                        new DukeException("invalidMarkingDone");
+                    }
+                } else if (command_lst[0].equals("todo")) {
+                    try {
                         Task newTask = new ToDo(command_lst[1]);
                         tasks.add(newTask);
                         System.out.println("Got it. I've added this task:\n" + newTask.toString());
                         System.out.println("Now you have " + numTasks(tasks) + " in the list.");
-                    } else {
-                        System.out.println("You have entered an invalid todo.");
+                    } catch (Exception e) {
+                        new DukeException("invalidTodo");
                     }
                 } else if (command_lst[0].equals("deadline")) {
-                    if (command_lst[1].contains("/by")) {
+                    try {
                         String desc = command_lst[1].split(" /by ")[0];
                         String by = command_lst[1].split(" /by ")[1];
                         Task newTask = new Deadline(desc, by);
                         tasks.add(newTask);
                         System.out.println("Got it. I've added this task: \n" + newTask.toString());
                         System.out.println("Now you have " + numTasks(tasks) + " in the list.");
-                    } else {
-                        System.out.println("You have entered an invalid deadline.");
+                    } catch (Exception e) {
+                        new DukeException("invalidDeadline");
                     }
                 } else if (command_lst[0].equals("event")) {
-                    if (command_lst[1].contains("/at")) {
+                    try {
                         String desc = command_lst[1].split(" /at ")[0];
                         String at = command_lst[1].split(" /at ")[1];
                         Task newTask = new Event(desc, at);
                         tasks.add(newTask);
                         System.out.println("Got it. I've added this task: \n" + newTask.toString());
                         System.out.println("Now you have " + numTasks(tasks) + " in the list.");
-                    } else {
-                        System.out.println("You have entered an invalid event.");
+                    } catch (Exception e) {
+                        new DukeException("invalidEvent");
                     }
                 } else {
-                    System.out.println("You have entered an invalid command.");
+                    new DukeException("invalidCommand");
                 }
             }
-
+            
             System.out.println("————————————————————————————————————————————————————————————");
             command = sc.nextLine();
         }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,16 @@
+public class DukeException extends Exception {
+    public DukeException(String errorType) {
+        String errorMessage = errorType.equals("invalidCommand") 
+                ? "☹ OOPS!!! I'm sorry, but I don't know what that means :-("
+                : errorType.equals("invalidMarkingDone") 
+                ? "☹ OOPS!!! Command to mark a task done is formatted wrongly." 
+                : errorType.equals("invalidTodo")
+                ? "☹ OOPS!!! The description of a todo cannot be empty."
+                : errorType.equals("invalidDeadline")
+                ? "☹ OOPS!!! The deadline is formatted wrongly."
+                : errorType.equals("invalidEvent")
+                ? "☹ OOPS!!! The event is formatted wrongly."
+                : "Unknown error. Try something else.";
+        System.out.println(errorMessage);
+    }
+}
