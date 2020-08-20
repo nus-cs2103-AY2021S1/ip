64333664f84a20148cf4f93fd80ec9ddc7a7d00b
dearvae <lovevae7@gmail.com>
2020-08-20 15:03:50 +0800

    feat(main):Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greeting();
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        String input = scanner.nextLine();  // Read user input
+        while(!input.equals("bye")) {
+            System.out.println(input);  // Output user input
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
+
+    public static void greeting() {
+        System.out.println("Hello! I'm KK\n" +
+                " What can I do for you?");
+    }
+
 }
