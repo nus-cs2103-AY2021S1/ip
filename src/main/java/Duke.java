8c5d19dec301bf8ff95a5b1e34d520ffc2734370
Muhammad Syafiq Bin Abas <syafiqabs7@gmail.com>
2020-08-20 20:03:08 +0800

    Revert "add Level-3"
    This reverts commit 1d5242ca1f0d2e2e88d934bc54014f92910ee166.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,27 +6,20 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
 
-        List<Task> tasks = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        Task task = new Task(sc.nextLine());
+        String input = sc.nextLine();
 
-        while (!(task.toString().equals("bye"))) {
-            if (task.toString().equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". [" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i));
+        while (!(input.equals("bye"))) {
+            if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                        System.out.println((i+1) + ". " + list.get(i));
                 }
-                task = new Task(sc.nextLine());
-            } else if (task.toString().contains("done")) {
-                String string = task.toString();
-                int index = Integer.parseInt(string.replaceAll("\\D",""));
-                tasks.get(index - 1).markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n  [" + tasks.get(index - 1).getStatusIcon() + "] " + tasks.get(index - 1));
-                task = new Task(sc.nextLine());
+                input = sc.nextLine();
             } else {
-                System.out.println("added: " + task);
-                tasks.add(task);
-                task = new Task(sc.nextLine());
+                System.out.println("added: " + input);
+                list.add(input);
+                input = sc.nextLine();
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
