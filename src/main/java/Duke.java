de67af20af527e63a4d5a466c904da79e8721ce8
Perpetual09 <jackgao0309@gmail.com>
2020-08-20 13:18:08 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String res = "";
+        List<String> data = new ArrayList<String>();
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Duke");
         System.out.println("     What can I do for you?");
         System.out.println("    ____________________________________________________________");
-        while(true) {
+        while (true) {
             res = sc.nextLine();
-            if(res.equals("bye")) {
+            if (res.equals("bye")) {
                 break;
-            } else {
+            } else if (res.equals("list")) {
                 System.out.println("    ____________________________________________________________");
-                System.out.printf("     %s\n", res);
+                for (int i = 0; i < data.size(); i++) {
+                    System.out.printf("     %d. %s\n", i + 1, data.get(i));
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                data.add(res);
+                System.out.println("    ____________________________________________________________");
+                System.out.printf("     added: %s\n", res);
                 System.out.println("    ____________________________________________________________");
             }
         }
@@ -21,4 +31,6 @@ public class Duke {
         System.out.println("     Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
     }
+
+
 }
