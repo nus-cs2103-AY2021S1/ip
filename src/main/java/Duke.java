9e04eaa2a7b28c38c963a93c776a996da58b73db
Xue Yong <xueyong26@hotmail.com>
2020-08-20 17:50:21 +0800

    Added task.java to track tasks and the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,8 @@ public class Duke {
     private static final String line = "____________________________________________________________\n";
     private static final String bye = "bye";
     private static final String list = "list";
-    private ArrayList<String> input;
+    private static final String done = "done";
+    private ArrayList<Task> input;
 
     private Duke() {
         input = new ArrayList<>();
@@ -14,28 +15,40 @@ public class Duke {
     public void init() {
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
+        String[] commands = command.split(" ");
         while (!command.equals(bye)) {
             if (command.equals(list)) {
                 display();
+            } else if (commands[0].equals(done) && commands.length == 2) {
+                updateTask(Integer.valueOf(commands[1]));
             } else {
                 add(command);
             }
             command = sc.nextLine();
+            commands = command.split(" ");
         }
         System.out.println(line + "Cya!!" + "\n" + line);
     }
 
     private void display() {
         System.out.println(line);
+        System.out.println("Here are the tasks in your list:\n");
         for (int i = 0; i < input.size(); i++) {
-            System.out.println(String.valueOf(i+1) + ". " + input.get(i) + "\n");
+            System.out.println(String.valueOf(i+1) + "." + input.get(i).getDescription() + "\n");
         }
         System.out.println(line);
     }
 
-    private void add(String reminder) {
-        input.add(reminder);
-        System.out.println(line + "added: " + reminder + "\n" + line);
+    private void add(String task) {
+        input.add(new Task(task));
+        System.out.println(line + "added: " + task + "\n" + line);
+    }
+    private void updateTask(int index) {
+        input.get(index - 1).markAsDone();
+        System.out.println(line);
+        System.out.println("Solid bro!! I've marked this task as done:\n");
+        System.out.println(input.get(index -1 ).getDescription() + "\n");
+        System.out.println(line);
     }
 
     public static void main(String[] args) {
