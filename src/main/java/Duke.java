f075fbf4ed46712bad2259ee26fe821f59c1f927
MerlinLim <merlinlim98@gmail.com>
2020-08-20 22:20:10 +0800

    Create Task class and includes done feature
    Task class has two attributes, String name and boolean done. markDone
    sets done = True and getName gets name. An overriden string method
    returns the object's name and a symbol to indicate if the Task has been
    done or not done.
    
    The main method now includes the command "done" to allow the user to
    mark tasks that are done. E.g "done 1" will mark the first task done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,22 +10,35 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
         // Creates list
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         // Echo
         while(sc.hasNextLine()) {
             String input = sc.nextLine();
+            String[] input_split = input.split(" ");
             if (input.equals("bye")){
                 // Stops taking inputs
                 break;
             } else if( input.equals("list") ) {
                 // returns list of items
-                for (int i = 0; i<list.size(); i++){
-                    System.out.println( (1 + i) + ". " + list.get(i));
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((1 + i) + "." + list.get(i));
                 }
+            } else if (input_split[0].equals("done") && input_split.length == 2){
+                // Marks number as done
+                int index;
+                // Parse int
+                index = Integer.parseInt(input_split[1]) - 1;
+
+                // Mark task as done
+                Task task = list.get(index);
+                task.markDone();
+                System.out.println("Nice! I've marked this task as done:\n" + task);
+
             } else {
                 // Adds item to list
-                list.add(input);
+                Task newTask = new Task(input);
+                list.add(newTask);
                 System.out.println("added: " + input);
             }
         }
@@ -36,4 +49,30 @@ public class Duke {
         sc.close();
     }
 
+}
+
+class Task {
+    private String name;
+    private boolean done;
+    public Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+
+    public void markDone(){
+        this.done = true;
+    }
+
+    public boolean getDone(){
+        return this.done;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String toString(){
+        String symbol = done ? "\u2713" : "\u2718";
+        return "[" + symbol + "] " + name;
+    }
 }
