ebd9ad335eece2482663d614eabeda11c3ee9394
FooJingYi <foojingyi@comp.nus.edu.sg>
2020-08-20 17:38:50 +0800

    Changed switch statements to if statements in Message.java, updated reply() for NOT_FOUND command

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -38,41 +38,34 @@ public class Message {
 
     public void reply() {
         System.out.println("____________________________________________________________");
-        switch (cmd) {
-            case START:
-                System.out.println("Eh what's up! I'm Meimei" +
-                        "\nWhat you want ah?");
-                break;
-            case LIST:
-                System.out.println("Na, here is your list lah:");
-                int counter = 0;
-                for (Task item : list) {
-                    counter++;
-                    System.out.println(counter +
-                            "." +
-                            item.toString());
-                }
-                break;
-            case DONE:
-                String[] words = message.split(" ");
-                try {
-                    int index = Integer.parseInt(words[1]);
-                    Task item = list.get(index - 1);
-                    item.markDone();
-                    System.out.println("Can, I help you mark this as done liao:" +
-                            "\n  " +
-                            item.toString());
-                } catch (NumberFormatException e) {
-                    System.out.println("Cannot find leh. Try typing \"done {index of list item}\".");
-                }
-                break;
-            case BYE:
-                System.out.println("Ok bye bye! C u again :P");
-                break;
-            case NOT_FOUND:
-                this.list.add(new Task(message));
-                System.out.println("I cannot find this command leh. Try sth else?");
-                break;
+        if (cmd == Command.START) {
+            System.out.println("Eh what's up! I'm Meimei" +
+                    "\nWhat you want ah?");
+        } else if (cmd == Command.LIST) {
+            System.out.println("Na, here is your list lah:");
+            int counter = 0;
+            for (Task item : list) {
+                counter++;
+                System.out.println(counter +
+                        "." +
+                        item.toString());
+            }
+        } else if (cmd == Command.DONE) {
+            String[] words = message.split(" ");
+            try {
+                int index = Integer.parseInt(words[1]);
+                Task item = list.get(index - 1);
+                item.markDone();
+                System.out.println("Can, I help you mark this as done liao:" +
+                        "\n  " +
+                        item.toString());
+            } catch (NumberFormatException e) {
+                System.out.println("Cannot find leh. Try typing \"done {index of list item}\".");
+            }
+        } else if (cmd == Command.BYE) {
+            System.out.println("Ok bye bye! C u again :P");
+        } else if (cmd == Command.NOT_FOUND) {
+            System.out.println("I cannot find this command leh. Try sth else?");
         }
         System.out.println("____________________________________________________________");
     }
