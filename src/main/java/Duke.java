80b8cf503457c19c3782302e841dc96ffe19c77f
Eryn <yeryeon@gmail.com>
2020-08-20 18:25:02 +0900

    Complete Level-5 Handle Errors
    Teach Wyre to deal with errors such as incorrect inputs entered by the user.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,9 +9,10 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String border = "\n^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^\n";
+        String border = "\n^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^\n";
         Scanner sc = new Scanner(System.in);
         String command;
+        //String original;
         ArrayList<Task> tList = new ArrayList<>();
 
         String logo = " _       _ \n"
@@ -21,46 +22,69 @@ public class Duke {
                 + "\\___/\\___/ \\____|_| \\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println(border + "Hi! I'm Wyre, your Personal Assistant Chatbot! :>\nWhat can I do for you today?" + border);
+        System.out.println(border + "Hi! I'm Wyre, your Personal Assistant Chatbot! :>\nWhat can I do for you today?"
+                + border);
         while(true) {
-            command = sc.nextLine();
-            if(command.equals("bye")) {
-                System.out.println(border + "Bye. Hope to see you again!" + border);
-                break;
-            } else if (command.equals("list")) {
-                System.out.println(border + "Here are the task(s) in your list:\n");
-                for (int i = 0; i < tList.size(); i++) {
-                    System.out.println("\t" + (i + 1) + ". " + tList.get(i));
+            try {
+                command = sc.nextLine();
+                //command = original.replaceAll("\\s", ""); // remove all spaces from command
+
+                if (command.equals("bye")) {
+                    System.out.println(border + "Bye. Hope to see you again!" + border);
+                    break;
+                } else if (command.equals("list")) {
+                    System.out.println(border + "Here are the task(s) in your list:\n");
+                    for (int i = 0; i < tList.size(); i++) {
+                        System.out.println("\t" + (i + 1) + ". " + tList.get(i));
+                    }
+                    System.out.println(border);
+                } else if (command.substring(0, 4).equals("done")) {
+                    int index = Integer.parseInt(command.substring(5)) - 1;
+                    tList.get(index).setStatus(true);
+                    System.out.println(border + "Well done! I've marked this task as done:");
+                    System.out.println("\t" + tList.get(index) + border);
+                } else if (command.substring(0, 4).equals("todo")) {
+                    String name = command.substring(5);
+                    if (name.isEmpty()) {
+                        System.out.println("Naw, you can't have a todo with an empty name!");
+                    } else {
+                        ToDo t = new ToDo(name, false);
+                        tList.add(t);
+                        System.out.println(border + "Wyre at your service. I've added the task:\n\t" + t);
+                        System.out.println("Now you have " + tList.size() + " task(s) in the list." + border);
+                    }
+                } else if (command.substring(0, 5).equals("event")) {
+                    int escapeIndex = command.lastIndexOf("/");
+                    String name = command.substring(6, escapeIndex);
+
+                    if (name.isEmpty()) {
+                        System.out.println("Naw, you can't have a event with an empty name!");
+                    } else {
+                        Event e = new Event(name, false, command.substring(escapeIndex + 4));
+                        tList.add(e);
+                        System.out.println(border + "Wyre at your service. I've added the task:\n\t" + e);
+                        System.out.println("Now you have " + tList.size() + " task(s) in the list." + border);
+                    }
+                } else if (command.substring(0, 8).equals("deadline")) {
+                    int escapeIndex = command.lastIndexOf("/");
+                    String name = command.substring(9, escapeIndex);
+
+                    if (name.isEmpty()) {
+                        System.out.println("Naw, you can't have a deadline with an empty name!");
+                    } else {
+                        Deadline d = new Deadline(name, false, command.substring(escapeIndex + 4));
+                        tList.add(d);
+                        System.out.println(border + "Wyre at your service. I've added the task:\n\t" + d);
+                        System.out.println("Now you have " + tList.size() + " task(s) in the list." + border);
+                    }
+                } else {
+                    System.out.println(border + "Naw, this isn't an accepted command!\nAvailable commands: bye, list," +
+                            " done, todo, deadline, event" + border);
                 }
-                System.out.println(border);
-            } else if (command.substring(0,4).equals("done")) {
-                int index = Integer.parseInt(command.substring(5)) - 1;
-                tList.get(index).setStatus(true);
-                System.out.println(border + "Well done! I've marked this task as done:");
-                System.out.println("\t" + tList.get(index) + border);
-            } else if (command.substring(0,4).equals("todo")) {
-                ToDo t = new ToDo(command.substring(5), false);
-                tList.add(t);
-                System.out.println(border + "Wyre at your service. I've added the task:\n\t" + t);
-                System.out.println("Now you have " + tList.size() + " task(s) in the list.\n" + border);
-            } else if (command.substring(0,8).equals("deadline")) {
-                int escapeIndex = command.lastIndexOf("/");
-                Deadline d = new Deadline(command.substring(9, escapeIndex - 1), false, command.substring(escapeIndex + 4));
-                tList.add(d);
-                System.out.println(border + "Wyre at your service. I've added the task:\n\t" + d);
-                System.out.println("Now you have " + tList.size() + " task(s) in the list.\n" + border);
-            } else if (command.substring(0,5).equals("event")) {
-                int escapeIndex = command.lastIndexOf("/");
-                Event e = new Event(command.substring(6, escapeIndex - 1), false, command.substring(escapeIndex + 4));
-                tList.add(e);
-                System.out.println(border + "Wyre at your service. I've added the task:\n\t" + e);
-                System.out.println("Now you have " + tList.size() + " task(s) in the list.\n" + border);
-            } else {
-                /*
-                tList.add(new Task(command, false));
-                System.out.println(border + "added: " + command + border);*/
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.println(border + "Naw fam, not a legal command. :'(\nAvailable commands: bye, list, " +
+                        "done, todo, deadline, event" + border);
             }
         }
-
     }
 }
