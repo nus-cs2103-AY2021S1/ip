521263b7eaf03aa097344ea567b3b254aa2a86db
NicholasCanete <nicholasgcc@gmail.com>
2020-08-20 19:37:39 +0800

    Level 2: Added todo list function to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Greet();
+
+        ArrayList<String> todo = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
        while (!input.equals("bye")) {
-           Echo(input);
-           input = sc.next();
+           add(input);
+           todo.add(input);
+           input = sc.nextLine();
+           if (input.equals("list")) {
+                printList(todo);
+                input = sc.nextLine();
+           }
        }
        Bye();
 
@@ -18,9 +26,19 @@ public class Duke {
         System.out.println("What can I do for you?");
     }
 
-    public static void Echo(String input) {
+    public static void printList(ArrayList<String> todo) {
+        int taskNum = 0;
         System.out.println("    ____________________________________________________________");
-        System.out.println("     " + input);
+        for (String task : todo) {
+            taskNum++;
+            System.out.println("     " + taskNum + ". " + task);
+        }
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void add(String todo) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     added: " + todo);
         System.out.println("    ____________________________________________________________");
     }
 
