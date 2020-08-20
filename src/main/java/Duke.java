7a0143e71fbe35c6298b815b800387a8ee695b18
GabrielTeo <gabrieltyx@gmail.com>
2020-08-20 13:54:02 +0800

    rename and add listing function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static void greetEchoExit() {
+    public static List<String> tasks = new ArrayList<>();
+
+    public static void interact() {
         System.out.println("-----------------------------------------------------");
         System.out.println("    " + "Hello! I'm Duke\n    What can I do for you?");
         System.out.println("-----------------------------------------------------");
@@ -14,15 +18,23 @@ public class Duke {
                 System.out.println("    " + "Bye. Hope to see you again soon!");
                 System.out.println("-----------------------------------------------------");
                 break;
+            } else if (line.equals("list")) {
+                System.out.println("-----------------------------------------------------");
+                for (String task : tasks) {
+                    int index = tasks.indexOf(task) + 1;
+                    System.out.println("    " + index + ". " + task);
+                }
+                System.out.println("-----------------------------------------------------");
             } else {
                 System.out.println("-----------------------------------------------------");
-                System.out.println("    " + line);
+                tasks.add(line);
+                System.out.println("    " + "added: " + line);
                 System.out.println("-----------------------------------------------------");
             }
         }
     }
 
     public static void main(String[] args) {
-        greetEchoExit();
+        interact();
     }
 }
