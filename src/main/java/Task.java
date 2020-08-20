08924f40e9a2fa5ce43e0161d401bc8f274c3ae3
Eryn <yeryeon@gmail.com>
2020-08-20 16:00:14 +0900

    Update Task Class
    Add necessary getters and setters and hide the private variables

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,24 @@
 package main.java;
 
 public class Task {
-    String name;
-    Boolean isComplete;
+    private String name;
+    private Boolean isDone;
 
     public Task (String s, Boolean b) {
         name = s;
-        isComplete = b;
+        isDone = b;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean isDone() {
+        return isDone;
+    }
+
+    public void setStatus(boolean b) {
+        isDone = b;
+    }
+
 }
