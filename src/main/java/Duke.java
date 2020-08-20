e210c96f2fb1355068a4068cad78f9dd7e178d94
teekoksiang <teekoksiang89@gmail.com>
2020-08-20 15:16:22 +0800

    Implement done chatbot action

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,31 +8,62 @@ public class Duke {
     private static final String EXIT_MESSAGE = "Bye. See you again soon!";
     private static final String DIVIDER = "\n\t-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n";
 
-    private static final List<String> actionList = new ArrayList<>();
+    private static final List<Task> taskList = new ArrayList<>();
 
     private static void printResponse(String message) {
         System.out.println(DIVIDER.concat("\t".concat(message.concat("\n"))).concat(DIVIDER));
     }
 
-    private static void addAction(String action) {
-        actionList.add(action);
-        printResponse("added: ".concat(action));
+    private static void addTask(String taskDescription) {
+        Task task = new Task(taskDescription);
+        taskList.add(task);
+        printResponse("added: ".concat(taskDescription));
     }
 
-    private static void listActions() {
-        if (actionList.size() == 0) {
-            printResponse("No action added yet...");
+    private static void listTasks() {
+        if (taskList.size() == 0) {
+            printResponse("No task added yet...");
             return;
         }
-        StringBuilder actions = new StringBuilder();
-        for (int i = 0; i < actionList.size(); i++) {
-            String action = String.format("%d. %s", (i + 1), actionList.get(i));
+        StringBuilder tasks = new StringBuilder();
+        for (int i = 0; i < taskList.size(); i++) {
+            String task = String.format("%d. %s", (i + 1), taskList.get(i));
             if (i > 0) {
-                action = "\n\t".concat(action);
+                task = "\n\t".concat(task);
             }
-            actions.append(action);
+            tasks.append(task);
         }
-        printResponse(actions.toString());
+        printResponse(tasks.toString());
+    }
+
+    private static int extractTaskNumber(String command) {
+        List<String> words = Arrays.asList(command.split(" "));
+        if (words.size() != 2) {
+            throw new IllegalArgumentException("Please type \"done [task number]\" to mark task as done");
+        } else {
+            // convert the task number into int
+            int taskNumber = Integer.parseInt(words.get(1));
+            return taskNumber;
+        }
+    }
+
+    private static void doneTask(String command) {
+        try {
+            int taskNumber = extractTaskNumber(command);
+            if (taskNumber > taskList.size()) {
+                throw new IllegalArgumentException("The task number is not found");
+            }
+            taskList.get(taskNumber - 1).markDone();
+            String doneMessage = "Nice! I've marked this task as done:";
+            doneMessage += String.format("\n\t\t".concat(taskList.get(taskNumber - 1).toString()));
+            printResponse(doneMessage);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            if (illegalArgumentException instanceof NumberFormatException) {
+                printResponse("Please type \"done [task number]\" to mark task as done");
+            } else {
+                printResponse(illegalArgumentException.getLocalizedMessage());
+            }
+        }
     }
 
     private static void listenCommand() {
@@ -40,9 +72,11 @@ public class Duke {
 
         while (!command.equalsIgnoreCase("bye")) {
             if (command.equalsIgnoreCase("list")) {
-                listActions();
+                listTasks();
+            } else if (command.startsWith("done")) {
+                doneTask(command);
             } else {
-                addAction(command);
+                addTask(command);
             }
             command = scanner.nextLine();
         }
