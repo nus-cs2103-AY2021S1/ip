1b1cdac2d0d1e5470d698844f68951593b35433f
Javier <javierlimja96@gmail.com>
2020-08-20 18:12:47 +0800

    done is updated. delete method is added. Main method cleaned up.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -38,13 +38,7 @@ public class Duke {
                 isRunning = bye();
             }
             else if(getWord(input).equals("done")){ //incomplete
-                num = sc.nextInt();
-                task = list.get(num-1).done();
-                list.add(num-1, task); //update task in list to done
-                printLine();
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(" " + task.getDoneString() + task.getString());
-                printLine();
+                done(input, list);
 
             }
             else if(getWord(input).equals("todo")){
@@ -55,6 +49,9 @@ public class Duke {
             }
             else if(getWord(input).equals("event")) {
                 event(input, list);
+            }
+            else if(getWord(input).equals("delete")) {
+                delete(input, list);
             }
             else{
                 error();
@@ -136,7 +133,7 @@ public class Duke {
         }else {
             int index = input.indexOf("/at");
             if (index == -1) {
-                System.out.println("☹ OOPS!!! The description of a event must have a indicated deadline.");
+                System.out.println("☹ OOPS!!! The description of an event must have a indicated deadline.");
             } else {
                 StringBuilder stringBuilder = new StringBuilder();
                 stringBuilder.append(input.substring(0, index)).append("(at:").append(input.substring(index + 3)).append(")");
@@ -146,6 +143,73 @@ public class Duke {
                 System.out.println("Got it. I've added this task: ");
                 System.out.println(" " + task.getTypeString() + task.getDoneString() + input);
                 System.out.println("Now you have " + list.size() + " tasks in the list.");
+            }
+        }
+        printLine();
+    }
+
+    public static void done(String input, ArrayList<Task> list){
+        input = input.substring(4);
+        printLine();
+        if(input.isEmpty()){
+            System.out.println("☹ OOPS!!! Please indicate which task is done.");
+        }else{
+            input = input.substring(1);
+            boolean isInteger = true;
+            for(int i = 0; i < input.length(); i++){
+                if(!Character.isDigit(input.charAt(i))){
+                    isInteger = false;
+                    break;
+                }
+            }
+            if(isInteger){
+                int tasknumber = Integer.parseInt(input);
+                if( (tasknumber == 0) ||(tasknumber > list.size()) ){
+                    System.out.println("☹ OOPS!!! Task number is not found in the list.");
+                }
+                else{
+                    Task task = list.remove(tasknumber - 1);
+                    Task newTask = new Task(task.taskType, true, task.toString());
+                    list.add(tasknumber-1, newTask);
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(" " + newTask.getTypeString() + newTask.getDoneString() + newTask.toString());
+                }
+            }
+            else{
+                System.out.println("☹ OOPS!!! Incorrect entry for finished task.");
+            }
+        }
+        printLine();
+    }
+
+    public static void delete(String input, ArrayList<Task> list){
+        input = input.substring(6);
+        printLine();
+        if(input.isEmpty()){
+            System.out.println("☹ OOPS!!! Please indicate which task is done.");
+        }else{
+            input = input.substring(1);
+            boolean isInteger = true;
+            for(int i = 0; i < input.length(); i++){
+                if(!Character.isDigit(input.charAt(i))){
+                    isInteger = false;
+                    break;
+                }
+            }
+            if(isInteger){
+                int tasknumber = Integer.parseInt(input);
+                if( (tasknumber == 0) ||(tasknumber > list.size()) ){
+                    System.out.println("☹ OOPS!!! Task number is not found in the list.");
+                }
+                else{
+                    Task task = list.remove(tasknumber - 1);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(" " + task.getTypeString() + task.getDoneString() + task.toString());
+                    System.out.println("Now you have " + list.size() + " tasks in the list.");
+                }
+            }
+            else{
+                System.out.println("☹ OOPS!!! Incorrect entry for finished task.");
             }
         }
         printLine();
@@ -201,6 +265,10 @@ class Task{
         else{
             string = "[E]";
         }
+        return string;
+    }
+
+    public String toString(){
         return string;
     }
 }
