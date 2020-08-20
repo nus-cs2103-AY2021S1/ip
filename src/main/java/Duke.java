aaf6c022c3e6c4ec8117fa0f7b98999a77ce51f3
Javier <javierlimja96@gmail.com>
2020-08-20 16:47:13 +0800

    placed Task and TaskType into Duke class for automated testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,7 +14,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n"*/
-                + " Hello I'm Duke \n"
+                + " Hello I'm Duke\n"
                 + " What can I do for you?\n"
                 + "____________________________________________________________\n";
         System.out.println(logo);
@@ -96,4 +96,52 @@ public class Duke {
             }
         }
     }
+}
+class Task{
+    TaskType taskType;
+    boolean isDone;
+    String string;
+
+    public Task(TaskType taskType, boolean isDone, String string){
+        this.taskType = taskType;
+        this.isDone = isDone;
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+    public String getDoneString(){
+        String string;
+        if(isDone){
+            string = "[✓]";
+        }
+        else{
+            string = "[✗]";
+        }
+        return string;
+    }
+
+    public Task done(){
+        return new Task(taskType, true, string);
+    }
+
+    public String getTypeString(){
+        String string;
+        if(taskType.equals(TaskType.TODO)){
+            string = "[T]";
+        }
+        else if(taskType.equals(TaskType.DEADLINE)){
+            string = "[D]";
+        }
+        else{
+            string = "[E]";
+        }
+        return string;
+    }
+}
+enum TaskType{
+    TODO,
+    DEADLINE,
+    EVENT;
 }
