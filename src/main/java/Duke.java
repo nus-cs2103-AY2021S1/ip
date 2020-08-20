88611b4d34aa3ba840d60daf72534d81f0d25303
Daniel Adi <daniel@adip.me>
2020-08-20 15:47:26 +0800

    add greet, echo, exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,16 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello from\n" + logo);
+        while (sc.hasNext()) {
+            String command = sc.nextLine();
+            if (command.equals("bye")){
+                break;
+            }
+            System.out.println("\n" + command);
+        }
+        System.out.println("Goodbye! See you again soon!");
     }
 }
