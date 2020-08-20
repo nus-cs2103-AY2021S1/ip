d42ec759a4c86308d1365ce27f0e564cf3266b85
Raythx98 <65889977+raythx98@users.noreply.github.com>
2020-08-20 14:19:49 +0800

    Level-1 improved

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,24 +8,43 @@ public class Duke {
         System.out.println(greeting);
     }
 
+    private static void farewell() {
+        String farewell = "Never come back,\n" +
+                "dun wanna see yu ever agin";
+        System.out.println(farewell);
+    }
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         greet();
         String nextLine = sc.nextLine();
-        while (!nextLine.equals("bye")) {
-            System.out.println(nextLine);
-            nextLine = sc.nextLine();
+        boolean cont = true;
+        while (cont) {
+            switch (nextLine) {
+                case "bye":
+                    farewell();
+                    cont = false;
+                    break;
+//                case "bye":
+//                    //someth
+//                    break;
+//                case "bye":
+//                    //someth
+//                    break;
+                default:
+                    System.out.println(nextLine);
+                    nextLine = sc.nextLine();
+
+            }
         }
-        System.out.println("Never come back,\n" +
-                "dun wanna see yu ever agin");
+        sc.close();
+    }
+}
+
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        sc.close();
-    }
-}
