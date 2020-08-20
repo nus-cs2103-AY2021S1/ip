75695421f273a469b7eddb1d7ba33e3a0e99d1c5
taneuzin <taneuzin@u.nus.edu>
2020-08-20 22:32:40 +0800

    Added Automated Text UI Testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,14 +12,17 @@ public class Duke {
         System.out.println(logo + "\nHello im Eu Zin's Duke, he spent thursday afternoon creating me cuz he forgot abt the iP");
 
         ArrayList<Task> taskList = new ArrayList<>();
-        Duke.response(new Scanner(System.in), taskList);
+        try {
+            Duke.response(new Scanner(System.in), taskList);
+        } catch (DukeException e) {
+            System.out.println(e.toString());
+        }
     }
 
-    static void response(Scanner scanner, ArrayList<Task> taskList) {
+    static void response(Scanner scanner, ArrayList<Task> taskList) throws DukeException {
         String userInput = scanner.nextLine();
-        String borders = "\n\n\\   / \\   / \\   / \\   / im not very creative \\   / \\   / \\   / \\   /\n \\ /   \\ /   \\ /   \\ /      EuZin's Duke      \\ /   \\ /   \\ /   \\ /\n\n";
+        String borders = "\n\\   / \\   / \\   / \\   / im not very creative \\   / \\   / \\   / \\   /\n \\ /   \\ /   \\ /   \\ /      EuZin's Duke      \\ /   \\ /   \\ /   \\ /\n\n";
         String addedMessage = "ok can i've added it\n";
-        String countMessage = "Now got " + taskList.size() + " task in the list\n";
         if (userInput.equals("bye")) {
             System.out.println(borders + "Bye. Hope to see you again soon!" + borders);
         } else if (userInput.equals("list")) {
@@ -31,36 +34,38 @@ public class Duke {
                 returnString += "\n" + (counter + 1) + ". " + thisTask.toString();
                 counter++;
             }
-            System.out.println(returnString + borders);
-            response(new Scanner(System.in), taskList);
+            System.out.println(returnString + "\n" + borders);
+            response(scanner, taskList);
         } else if (userInput.startsWith("done")) {
             String returnString = borders + "ok sure good job i guess\n";
             int taskDone = Integer.parseInt(userInput.substring(5));
             Task thisTask = taskList.get(taskDone - 1);
             thisTask.done();
-            returnString += thisTask.getStatusIcon() + " " + thisTask.toString();
-            System.out.println(returnString + borders);
-            response(new Scanner(System.in), taskList);
+            returnString += thisTask.toString();
+            System.out.println(returnString + "\n" + borders);
+            response(scanner, taskList);
         } else if (userInput.startsWith("todo")) {
             Task thisTask = new Task(userInput);
             taskList.add(thisTask);
-            System.out.println(borders + "added: " + userInput + borders);
-            System.out.println(borders + addedMessage + thisTask.toString() + "\n" + countMessage + borders);
-            Duke.response((new Scanner(System.in)), taskList);
+            System.out.println(borders + addedMessage + thisTask.toString().replace("todo ","") + "\n" +
+                    "Now got " + taskList.size() + " task in the list\n" + borders);
+            Duke.response(scanner, taskList);
         } else if (userInput.startsWith("deadline")) {
             String[] StringArr = userInput.split(" /by");
-            Task thisTask = new Deadline(StringArr[0].replace("deadline", ""), StringArr[1]);
+            Task thisTask = new Deadline(StringArr[0].replace("deadline ", ""), StringArr[1]);
             taskList.add(thisTask);
-            System.out.println(borders + addedMessage + thisTask.toString() + "\n" + countMessage + borders);
-            Duke.response((new Scanner(System.in)), taskList);
+            System.out.println(borders + addedMessage + thisTask.toString() + "\n" +
+                    "Now got " + taskList.size() + " task in the list\n" + borders);
+            Duke.response(scanner, taskList);
         } else if (userInput.startsWith("event")) {
             String[] StringArr = userInput.split(" /at");
-            Task thisTask = new Event(StringArr[0].replace("event", ""), StringArr[1]);
+            Task thisTask = new Event(StringArr[0].replace("event ", ""), StringArr[1]);
             taskList.add(thisTask);
-            System.out.println(borders + addedMessage + thisTask.toString() + "\n" + countMessage + borders);
-            Duke.response((new Scanner(System.in)), taskList);
+            System.out.println(borders + addedMessage + thisTask.toString() + "\n" +
+                    "Now got " + taskList.size() + " task in the list\n" + borders);
+            Duke.response(scanner, taskList);
         } else {
-            Duke.response((new Scanner(System.in)), taskList);
+            throw new DukeException();
         }
     }
 }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,7 @@
+public class DukeException extends Throwable {
+
+    public String toString() {
+        return "Duke is too dumb, Duke dunno what you mean";
+    }
+
+}
