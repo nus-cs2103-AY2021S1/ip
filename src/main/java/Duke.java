52872bf0c6cda935a7829d4c146064fdc11b7ac8
FooJingYi <foojingyi@comp.nus.edu.sg>
2020-08-20 15:49:53 +0800

    Added a HashSet "list" to keep all added items

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        HashSet<String> list = new HashSet<>();
 
         Message start = new Message("start");
         start.reply();
