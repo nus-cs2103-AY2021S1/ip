3527332f103d583ffcf0dcf58bbecd3178708392
koonweee <34903259+koonweee@users.noreply.github.com>
2020-08-21 01:31:34 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static Integer Intger;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,20 +15,39 @@ public class Duke {
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         Scanner scan = new Scanner(System.in);
-        ArrayList<String> storage = new ArrayList<>();
+        ArrayList<Task> storage = new ArrayList<>();
         String in = scan.nextLine();
         while(!in.equals("bye")) {
             if (in.equals("list")) {
+                System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0; i < storage.size(); i++) {
-                    System.out.println("\t" + (i+1) + ". " + storage.get(i));
+                    System.out.println("\t" + (i + 1) + "." + storage.get(i));
                 }
+            } else if (in.contains("done ")) {
+                int task = Intger.parseInt(in.replace("done ", "")) - 1;
+                storage.get(task).done = true;
+                System.out.println("\tNice! I've marked this task as done:\n\t" + storage.get(task));
             } else {
                 System.out.println("\tadded: " + in);
-                storage.add(in);
+                storage.add(new Task(in));
             }
             in = scan.nextLine();
         }
         System.out.println("\tBye. Hope to see you again soon!");
 
+    }
+}
+
+class Task {
+    String task;
+    boolean done;
+    Task(String task) {
+        this.task = task;
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        return done ? ("[✓] " + task) : ("[✗] " + task);
     }
 }
