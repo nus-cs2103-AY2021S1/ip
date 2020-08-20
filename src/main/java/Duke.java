f9fee53dcf5cf22427fa032ee5acabda2aaf54da
LAPTOP-S8CB0H15\Iqbal <iq_bal97@hotmail.com>
2020-08-20 23:16:40 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,22 +8,36 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         List<Task> tasks = new ArrayList<>();
         Task task = new Task(sc.nextLine());
-        while (!(task.toString().equals("bye"))) {
-            if (task.toString().equals("list")) {
+        while (!(task.getDescription().equals("bye"))) {
+            if (task.getDescription().equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= tasks.size(); i++) {
-                    System.out.println(i + ".[" + tasks.get(i - 1).getStatusIcon() + "] " + tasks.get(i - 1));
+                    System.out.println(i + "." + tasks.get(i - 1));
                 }
                 task = new Task(sc.nextLine());
-            } else if (task.toString().contains("done")) {
-                String string = task.toString();
+            } else if (task.getDescription().contains("done")) {
+                String string = task.getDescription();
                 int index = Integer.parseInt(string.replaceAll("\\D", ""));
                 tasks.get(index - 1).markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n  [" + tasks.get(index - 1).getStatusIcon() + "] " + tasks.get(index - 1));
+                System.out.println("Nice! I've marked this task as done:\n  " + tasks.get(index - 1));
                 task = new Task(sc.nextLine());
             } else {
-                tasks.add(task);
-                System.out.println("added: " + task);
+                if (task.getDescription().contains("todo")) {
+                    String str = task.getDescription().replaceFirst("todo","").strip();
+                    task = new ToDo(str);
+                    tasks.add(task);
+                } else if (task.getDescription().contains("deadline")) {
+                    String str = task.getDescription().replaceFirst("deadline","");
+                    String[] strings = str.split("/by");
+                    task = new Deadline(strings[0].strip(), strings[1].strip());
+                    tasks.add(task);
+                } else if (task.getDescription().contains("event")) {
+                    String str = task.getDescription().replaceFirst("event","");
+                    String[] strings = str.split("/at");
+                    task = new Event(strings[0].strip(), strings[1].strip());
+                    tasks.add(task);
+                }
+                System.out.println("Got it. I've added this task:\n  " + task + "\nNow you have " + tasks.size() + " tasks in the list.");
                 task = new Task(sc.nextLine());
             }
         }
