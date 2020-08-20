affd926d86aa161f8e75e983ba44b7c58cd8eece
Jerry Ho <jerryho@u.nus.edu>
2020-08-20 20:38:57 +0800

    Add ability to store text and display back to user when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,14 +7,20 @@ public class Duke {
         System.out.println(greetings);
 
         ArrayList<String> lst = new ArrayList<>();
-        
+
         Scanner sc = new Scanner(System.in);
         while (true) {
             String str = sc.nextLine();
             if (str.equals("bye")) {
                 break;
+            } else if (str.equals("list")) {
+                for (int i = 0; i < lst.size(); i ++) {
+                    String num = Integer.toString(i + 1);
+                    System.out.println(num + ". " + lst.get(i));
+                }
             } else {
-                System.out.println(str);
+                lst.add(str);
+                System.out.println("added: " + str);
             }
         }
         String goodbyeMessage = "Bye. Hope to see you again soon!";
