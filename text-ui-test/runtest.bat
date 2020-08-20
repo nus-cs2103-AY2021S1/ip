df0d4ba16f43e5b6b795414ab911f5c7598dfdb1
unknown <schoolex@hotmail.com>
2020-08-20 13:59:20 +0800

    Updated automated testing script(unix)

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -18,4 +18,4 @@ REM run the program, feed commands from input.txt file and redirect the output t
 java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
-FC ACTUAL.TXT EXPECTED.TXT
+FC ACTUAL.TXT EXPECTED-UNIX.TXT
