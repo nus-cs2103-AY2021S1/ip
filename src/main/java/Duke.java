0f9829f5f010656699ca2ece8c486de3b3407134
Noorul Azlina <>
2020-08-20 22:23:19 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,19 +13,32 @@ public class Duke {
             } while (sc.hasNextLine());
         }
     }
-
-    public static void main(String[] args) {
-        scan();
-        System.out.println("  ____________________________________________________________\n" + "  Hello! I'm Duke\n" + "  What can I do for you?\n"
-                + "  ____________________________________________________________");
+    public static void output(){
         for(String string : todos){
             System.out.println("\n" + string + "\n  ____________________________________________________________");
             if(string.equals("bye")){
                 System.out.println("  Bye. Hope to see you again soon!\n" + "  ____________________________________________________________");
                 break;
             }
-            System.out.println("  " + string + "\n" +
-                    "  ____________________________________________________________");
+            if(string.equals("list")){
+                for(int i = 0; i < todos.size(); i++){
+                    if(!todos.get(i).equals("list") && !todos.get(i).equals("bye")) {
+                        System.out.println("  " + (i + 1) + ". " + todos.get(i));
+                    }
+
+                }
+                System.out.println("\n  ____________________________________________________________");
+            }else {
+                System.out.println("  " + "added: " + string + "\n" +
+                        "  ____________________________________________________________");
+            }
         }
+
+    }
+    public static void main(String[] args) {
+        scan();
+        System.out.println("  ____________________________________________________________\n" + "  Hello! I'm Duke\n" + "  What can I do for you?\n"
+                + "  ____________________________________________________________");
+        output();
     }
 }
