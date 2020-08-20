b042dce256ecbce23b2e3fad6e1543c294c9bacb
lebencwb <46550747+lebencwb@users.noreply.github.com>
2020-08-21 00:52:58 +0800

    updated Level-4 of Duke.java, added Deadline.java, Event.java, ToDo.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,26 +19,59 @@ public class Duke {
                 break;
             }
             if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("\tHere are the tasks in your list:");
                 int index = 1;
                 for (Task task : list) {
                     System.out.println("\t" + index++ + ". " + task.toString());
                 }
+                if (index == 1) {
+                    System.out.println("\tYou have no tasks in your list :)");
+                }
             } else {
                 Scanner scanner2 = new Scanner(userInput);
-                if (scanner2.next().equalsIgnoreCase("done")) {
+                String cmd = scanner2.next();
+                if (cmd.equalsIgnoreCase("done")) {
                     int index = scanner2.nextInt();
                     Task task = list.get(index - 1);
                     task.markAsDone();
                     System.out.println("\tNice! I've marked this as done:");
                     System.out.println("\t  " + task.toString());
-                } else{
-                    list.add(new Task(userInput));
-                    System.out.println("\tadded: " + userInput);
+                } else {
+                    System.out.println("\tGot it. I've added this task:");
+                    if (cmd.equalsIgnoreCase("todo")) {
+                        ToDo todo = new ToDo(scanner2.nextLine());
+                        list.add(todo);
+                        System.out.println("\t  " + todo.toString());
+                    }
+                    else if (cmd.equalsIgnoreCase("deadline")) {
+                        scanner2.useDelimiter("( /by )");
+                        String description = scanner2.next();
+                        scanner2.reset();
+                        scanner2.next();
+                        String by = scanner2.nextLine().trim();
+                        Deadline deadline = new Deadline(description, by);
+                        list.add(deadline);
+                        System.out.println("\t  " + deadline.toString());
+                    }
+                    else if (cmd.equalsIgnoreCase("event")) {
+                        scanner2.useDelimiter("( /at )");
+                        String description = scanner2.next();
+                        scanner2.reset();
+                        scanner2.next();
+                        String at = scanner2.nextLine().trim();
+                        Event event = new Event(description, at);
+                        list.add(event);
+                        System.out.println("\t  " + event.toString());
+                    } else {
+                        Task task = new Task(userInput);
+                        list.add(task);
+                        System.out.println("\t  " + task.toString());
+                    }
+                    System.out.println("\tNow you have " + list.size() + " task(s) in the list.");
                 }
             }
             System.out.println(horizontalLine);
         }
     }
-
 }
 
