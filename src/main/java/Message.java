77511f4a87d3d0cb3751ec08c9b2b9f26d3c6a30
FooJingYi <foojingyi@gmail.com>
2020-08-20 15:00:13 +0800

    Add Message.java with constructor, reply() method, and enum for command types

---
+++ src/main/java/Message.java
@@ -0,0 +1,45 @@
+enum Command {
+    START,
+    BYE,
+    ECHO
+}
+
+public class Message {
+    private final String message;
+    private Command cmd;
+
+    // public constructor
+    public Message(String message) {
+        this.message = message;
+        String[] words = message.split(" ");
+        String cmdString = words[0];
+
+        for (Command c : Command.values()) {
+            if (c.toString().equalsIgnoreCase(cmdString)) {
+                this.cmd = c;
+                break;
+            }
+        }
+
+        if (this.cmd == null) { // if not a recognised command
+            this.cmd = Command.ECHO;
+        }
+    }
+
+    public void reply() {
+        System.out.println("____________________________________________________________");
+        switch (cmd) {
+            case START:
+                System.out.println("Eh what's up! I'm Meimei" +
+                        "\nWhat you want ah?");
+                break;
+            case BYE:
+                System.out.println("Ok bye bye! C u again :P");
+                break;
+            case ECHO:
+                System.out.println(message);
+                break;
+        }
+        System.out.println("____________________________________________________________");
+    }
+}
