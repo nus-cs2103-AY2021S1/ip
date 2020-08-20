00627881bef210bcf61deca2c5038bb8c815a610
MerlinLim <merlinlim98@gmail.com>
2020-08-20 19:01:22 +0800

    Add text, List text
    Chat Bot adds whatever text input to an ArrayList.
    When User types "list", Chat Bot will display all the text the
    user has entered in chronological order.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         // Greet
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
+        // Creates list
+        ArrayList<String> list = new ArrayList<>();
+
         // Echo
         while(sc.hasNextLine()) {
             String input = sc.nextLine();
             if (input.equals("bye")){
+                // Stops taking inputs
                 break;
+            } else if( input.equals("list") ) {
+                // returns list of items
+                for (int i = 0; i<list.size(); i++){
+                    System.out.println( (1 + i) + ". " + list.get(i));
+                }
             } else {
-                System.out.println(input);
+                // Adds item to list
+                list.add(input);
+                System.out.println("added: " + input);
             }
         }
 
