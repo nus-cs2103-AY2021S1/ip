62b5a20fda3b62624338c3fd97edb184db13db45
Xue Yong <xueyong26@hotmail.com>
2020-08-20 22:05:18 +0800

    Refactored Code and Added ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,54 +1,100 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 public class Duke {
     private static final String line = "____________________________________________________________\n";
     private static final String bye = "bye";
     private static final String list = "list";
     private static final String done = "done";
-    private ArrayList<Task> input;
+    private static final String todo = "todo";
+    private static final String deadline = "deadline";
+    private static final String event = "event";
+    private ArrayList<Task> inputList;
 
     private Duke() {
-        input = new ArrayList<>();
+        inputList = new ArrayList<>();
     }
 
     public void init() {
         Scanner sc = new Scanner(System.in);
-        String command = sc.nextLine();
-        String[] commands = command.split(" ");
-        while (!command.equals(bye)) {
-            if (command.equals(list)) {
-                display();
-            } else if (commands[0].equals(done) && commands.length == 2) {
-                updateTask(Integer.valueOf(commands[1]));
+        while (sc.hasNextLine()) {
+            String command = sc.nextLine();
+            if (command.equals(bye)) {
+                System.out.println(template("Cya!!"));
+                return;
             } else {
-                add(command);
+                String reply = inputHandler(command);
+                System.out.println(template(reply));
             }
-            command = sc.nextLine();
-            commands = command.split(" ");
         }
-        System.out.println(line + "Cya!!" + "\n" + line);
     }
 
-    private void display() {
-        System.out.println(line);
-        System.out.println("Here are the tasks in your list:\n");
-        for (int i = 0; i < input.size(); i++) {
-            System.out.println(String.valueOf(i+1) + "." + input.get(i).getDescription() + "\n");
-        }
-        System.out.println(line);
+    private String template(String reply) {
+        return line + reply + "\n" + line;
     }
 
-    private void add(String task) {
-        input.add(new Task(task));
-        System.out.println(line + "added: " + task + "\n" + line);
+    private String inputHandler(String input) {
+        String[] commands = input.split(" ", 2);
+        if (commands[0].equals(list)) {
+            return display();
+        } else if (commands[0].equals(done) && commands.length == 2) {
+            return updateTask(Integer.valueOf(commands[1]));
+        } else if (commands[0].equals(todo)) {
+            return addTodo(input);
+        } else if (commands[0].equals(event)) {
+            String[] arr = commands[1].split("/at");
+            return addEvent(arr[0], arr[1]);
+        } else if (commands[0].equals(deadline)) {
+            String[] arr = commands[1].split("/by");
+            return addDeadline(arr[0], arr[1]);
+        } else {
+            return "Invalid input";
+        }
     }
-    private void updateTask(int index) {
-        input.get(index - 1).markAsDone();
-        System.out.println(line);
-        System.out.println("Solid bro!! I've marked this task as done:\n");
-        System.out.println(input.get(index -1 ).getDescription() + "\n");
-        System.out.println(line);
+
+    private String display() {
+        StringBuilder str = new StringBuilder();
+        str.append("Here are the tasks in your list:\n");
+        for (int i = 0; i < inputList.size(); i++) {
+            str.append(String.valueOf(i+1) + "." + inputList.get(i).toString() + "\n");
+        }
+        return str.toString();
+    }
+
+    private String addTodo(String task) {
+        StringBuilder str = new StringBuilder();
+        Todo current = new Todo(task);
+        inputList.add(current);
+        str.append("Got it bro, I've added this task:\n  ").append(current.toString() + "\n").append(
+                "Now you have ").append(inputList.size()).append(" tasks in the list.");
+        return str.toString();
+    }
+
+    private String addDeadline(String task, String by) {
+        StringBuilder str = new StringBuilder();
+        Deadline current = new Deadline(task, by);
+        inputList.add(current);
+        str.append("Got it bro, I've added this task:\n  ").append(current.toString() + "\n").append(
+                "Now you have ").append(inputList.size()).append(" tasks in the list.");
+        return str.toString();
+    }
+
+    private String addEvent(String task, String at) {
+        StringBuilder str = new StringBuilder();
+        Event current = new Event(task, at);
+        inputList.add(current);
+        str.append("Got it bro, I've added this task:\n  ").append(current.toString() + "\n").append(
+                "Now you have ").append(inputList.size()).append(" tasks in the list.");
+        return str.toString();
+    }
+
+    private String updateTask(int index) {
+        inputList.get(index - 1).markAsDone();
+        StringBuilder str = new StringBuilder();
+        str.append("Solid bro!! I've marked this task as done:\n").append(
+                inputList.get(index -1 ).toString() + "\n");
+        return str.toString();
     }
 
     public static void main(String[] args) {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,16 +7,20 @@ public class Task {
         this.isDone = false;
     }
 
-    public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-    }
-
-    public String getDescription() {
-        return "[" + getStatusIcon() + "] " + description;
-    }
-
     public void markAsDone() {
         this.isDone = true;
     }
 
+    public void unMarkAsDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "✓" : "✗"); //return tick or X symbols
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
