288b55080173e420cdfb143883b5167b41f0a45d
Jace Tan <jace1711@gmail.com>
2020-08-20 23:44:54 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Yoo ( ﾟ▽ﾟ)/ \nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        ArrayList<String> al = new ArrayList<>();
+
+        String input = sc.nextLine();
         while(!input.equals("bye")) {
-            System.out.println(input);
-            input = sc.next();
+
+            if(input.equals("list")) {
+                System.out.println("Here's your list!");
+                for(int i = 1; i <= al.size(); i ++) {
+                    System.out.println(i + ". " + al.get(i - 1));
+                }
+            } else {
+                System.out.println("I've added [" + input + "] !");
+                al.add(input);
+            }
+            input = sc.nextLine();
         }
         System.out.println("Bye! Come back soon ( ･ω･)ﾉ");
+        sc.close();
     }
 }
