f311f840e071ed006402499afda725561fe9a8da
geoboom <leegeorgie52@gmail.com>
2020-08-20 21:36:24 +0800

    Rewrite test script to account for packages

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+ENTRY_FILENAME='Runner'
 
 # create bin directory if it doesn't exist
 if [ ! -d "../bin" ]
@@ -13,21 +14,17 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Duke.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/duke/runner/Runner.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
-
-# convert to UNIX format
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+java -classpath ../bin duke.runner.Runner < input.txt > ACTUAL.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
+diff ACTUAL.TXT EXPECTED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
