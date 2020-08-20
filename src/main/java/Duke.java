ed6a54d8be7505defd91b20765b8edea8d168f7a
LAPTOP-S8CB0H15\Iqbal <iq_bal97@hotmail.com>
2020-08-20 20:22:27 +0800

    add Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,18 +6,25 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);    // Create a Scanner object
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
-        String input = sc.nextLine();
-        List<String> list = new ArrayList<>();
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                for (int i = 1; i <= list.size(); i++) {
-                    System.out.println(i + ". " + list.get(i - 1));
+        List<Task> tasks = new ArrayList<>();
+        Task task = new Task(sc.nextLine());
+        while (!(task.toString().equals("bye"))) {
+            if (task.toString().equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 1; i <= tasks.size(); i++) {
+                    System.out.println(i + ".[" + tasks.get(i - 1).getStatusIcon() + "] " + tasks.get(i - 1));
                 }
-                input = sc.nextLine();
+                task = new Task(sc.nextLine());
+            } else if (task.toString().contains("done")) {
+                String string = task.toString();
+                int index = Integer.parseInt(string.replaceAll("\\D", ""));
+                tasks.get(index - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n  [" + tasks.get(index - 1).getStatusIcon() + "] " + tasks.get(index - 1));
+                task = new Task(sc.nextLine());
             } else {
-                list.add(input);
-                System.out.println("added: " + input);
-                input = sc.nextLine();
+                tasks.add(task);
+                System.out.println("added: " + task);
+                task = new Task(sc.nextLine());
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
