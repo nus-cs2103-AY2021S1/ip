b9d75339720cfa25e36e1440a411f1d389c3b34c
hoperawr <hopeleong@gmail.com>
2020-08-20 19:31:50 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    static String[] list = new String[100];
+    static int index = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,13 +18,30 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()) {
             String input = sc.nextLine();
-            if (input.equals("bye")){
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                printlist();
             } else {
-                System.out.println(input);
+                add(input);
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
+
+    public static void add(String input){
+        list[index] = input;
+        index += 1;
+        System.out.println("added: " + input);
+    }
+
+    public static void printlist(){
+        int tempindex = 1;
+        for (int k =0; list[k] != null; k++ ){
+            System.out.println(tempindex + ". " + list[k]);
+            tempindex += 1;
+        }
+    }
+
 
 }
