bf1a399b61d01490b9ae94b5d53f309007b84219
Xue Yong <xueyong26@hotmail.com>
2020-08-20 13:55:44 +0800

    Added Greet, Echo, Exit in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String line = "____________________________________________________________\n";
+    private static final String bye = "bye";
+
+
+    public void init() {
+        Scanner sc = new Scanner(System.in);
+        String command = sc.next();
+        while (!command.equals(bye)) {
+            System.out.println(line + command + "\n" + line);
+            command = sc.next();
+        }
+        System.out.println(line + "Cya!!" + "\n" + line);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcome = "Yo I'm Dood!!\nAnything I can do for you?\n";
+        System.out.println(line + welcome + line);
+        Duke bot = new Duke();
+        bot.init();
     }
 }
