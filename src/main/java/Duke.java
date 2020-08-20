759dc3158e79e026b8b5bd50a7d47dfa51ef53f7
Theodore Leebrant <theodoreleebrant@gmail.com>
2020-08-20 18:24:04 +0800

    Level 6 submission

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,45 +2,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void printMessage(String msg) {
-        System.out.println("    ____________________________________________________________");
-        System.out.print(msg);
-        System.out.println("    ____________________________________________________________");
-    }
-
-    public static void initialMessage() {
-        String toPrint = "    Hello! I'm Duke the Bad Dragon.\n" + "    What can I do for you?\n";
-        printMessage(toPrint);
-    }
-
-    public static void exitMessage() {
-        printMessage("     Bye. Hope to see you again soon!\n");
-    }
-
-    public static void printDone(String msg) {
-        String toPrint = "     Nice! I've marked this task as done:" + "\n" + msg;
-        printMessage(toPrint);
-    }
-
-    public static void printList(String msg) {
-        String toPrint = "     Here are the tasks in your list:\n" + msg;
-        printMessage(toPrint);
-    }
-
-    public static void printAdd(Pair<String, Integer> msg) {
-        String toPrint = "     Got it. I've added this task:\n"
-                + "       " + msg.getT()
-                + "     Now you have " + msg.getU() + " tasks in the list.\n";
-        printMessage(toPrint);
-    }
-
-    public static void printDelete(Pair<String, Integer> msg) {
-        String toPrint = "     Noted. I've removed this task: \n"
-                + "       " + msg.getT()
-                + "     Now you have " + msg.getU() + " tasks in the list.\n";
-        printMessage(toPrint);
-    }
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -48,7 +9,7 @@ public class Duke {
         Parser parser = new Parser();
 
         // TODO: 20/8/20 Improve runtime: keep an internal counter of task (for done.*)
-        initialMessage();
+        Printer.initialMessage();
         while(true) {
             try {
                 String echo = sc.nextLine();
@@ -56,31 +17,25 @@ public class Duke {
                 // Command Handling
                 // Exit
                 if (echo.equals("bye")) {
-                    exitMessage();
+                    Printer.exitMessage();
                     break;
                 }
 
                 // Querying items
                 else if (echo.matches("list\\s*")) {
-                    printList(tasks.printTodoList());
+                    Printer.printList(tasks.printTodoList());
                 }
 
                 // Checks if it matches done and an integer
                 else if (echo.matches("done.*")) {
-                    // Strip the done, leaving the integer
-                    // TODO: 20/8/20 Cleanup
-                    int index = 0;
-                    index = parser.parseDone(echo, tasks.length());
-                    printDone(tasks.markAsDone(index));
+                    int index = parser.parseDone(echo, tasks.length());
+                    Printer.printDone(tasks.markAsDone(index));
                 }
 
                 // Checks if it matches done and an integer
                 else if (echo.matches("delete.*")) {
-                    // Strip the done, leaving the integer
-                    // TODO: 20/8/20 Cleanup
-                    int index = 0;
-                    index = parser.parseDelete(echo, tasks.length());
-                    printDelete(tasks.delete(index));
+                    int index = parser.parseDelete(echo, tasks.length());
+                    Printer.printDelete(tasks.delete(index));
                 }
 
                 // Add items
@@ -90,13 +45,13 @@ public class Duke {
                     TaskType type = res.getT();
                     switch (type) {
                         case TODO:
-                            printAdd(tasks.add(new ToDo(description.get(0))));
+                            Printer.printAdd(tasks.add(new ToDo(description.get(0))));
                             break;
                         case DEADLINE:
-                            printAdd(tasks.add(new Deadline(description.get(0), description.get(1))));
+                            Printer.printAdd(tasks.add(new Deadline(description.get(0), description.get(1))));
                             break;
                         case EVENT:
-                            printAdd(tasks.add(new Event(description.get(0), description.get(1))));
+                            Printer.printAdd(tasks.add(new Event(description.get(0), description.get(1))));
                             break;
                         case NONE:
                             break;
---
+++ src/main/java/Printer.java
@@ -0,0 +1,42 @@
+public class Printer {
+    public Printer() { }
+
+    public static void printMessage(String msg) {
+        System.out.println("    ____________________________________________________________");
+        System.out.print(msg);
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void initialMessage() {
+        String toPrint = "    Hello! I'm Duke the Bad Dragon.\n" + "    What can I do for you?\n";
+        printMessage(toPrint);
+    }
+
+    public static void exitMessage() {
+        printMessage("     Bye. Hope to see you again soon!\n");
+    }
+
+    public static void printDone(String msg) {
+        String toPrint = "     Nice! I've marked this task as done:" + "\n" + msg;
+        printMessage(toPrint);
+    }
+
+    public static void printList(String msg) {
+        String toPrint = "     Here are the tasks in your list:\n" + msg;
+        printMessage(toPrint);
+    }
+
+    public static void printAdd(Pair<String, Integer> msg) {
+        String toPrint = "     Got it. I've added this task:\n"
+                + "       " + msg.getT()
+                + "     Now you have " + msg.getU() + " tasks in the list.\n";
+        printMessage(toPrint);
+    }
+
+    public static void printDelete(Pair<String, Integer> msg) {
+        String toPrint = "     Noted. I've removed this task: \n"
+                + "       " + msg.getT()
+                + "     Now you have " + msg.getU() + " tasks in the list.\n";
+        printMessage(toPrint);
+    }
+}
