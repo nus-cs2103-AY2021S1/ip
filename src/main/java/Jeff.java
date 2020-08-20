223ca07125cd472d51e4f59d706e5c4158e5224c
Wayne <>
2020-08-20 15:23:58 +0800

    Level 2. Add, List

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -4,28 +4,39 @@ public class Jeff {
 
     public static void main(String[] args) {
 
+        String[] rubbishBin = new String[100];
+        String add = "added: ";
+        int counter = 0;
+
         Scanner Sc = new Scanner(System.in);
         String line = "____________________________\n"
-                     +"____________________________";
-
+                +"____________________________";
 
         String logo = "****** ****** ****** ******\n"
-                     +"   *   *      *      *\n"
-                     +"   *   ****** ****** ******\n"
-                     +"*  *   *      *      *\n"
-                     +"***    ****** *      *\n";
+                +"   *   *      *      *\n"
+                +"   *   ****** ****** ******\n"
+                +"*  *   *      *      *\n"
+                +"***    ****** *      *\n";
         System.out.println("My name is\n" + logo);
         System.out.println("What do you want?");
         System.out.println(line);
 
         while(true){
-            String input = Sc.next();
+            String input = Sc.nextLine();
 
             if(input.equals("bye")){
                 System.out.println("Bye, Have a great time! ");
                 break;
             }else{
-                System.out.println(input);
+                if(input.equals("list")){
+                    for(int i=0; i<counter; i++){
+                        System.out.println( i+1 +"."+" "+ rubbishBin[i]);
+                    }
+                }else{
+                    rubbishBin[counter] = input;
+                    System.out.println(add + rubbishBin[counter]);
+                    counter++;
+                }
             }
 
             System.out.println(line);
@@ -33,4 +44,3 @@ public class Jeff {
         }
     }
 }
-
