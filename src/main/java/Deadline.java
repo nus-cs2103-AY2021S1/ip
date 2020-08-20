ec28ac6e0c4bb151e8b92acdb46bf9b2bbcf833c
zhengweii <limzhengwei_99@hotmail.com>
2020-08-20 16:14:23 +0800

    Set up automated text UI testing

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,6 +8,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + this.by + ")";
     }
 }
