9b7ea09c1f21da2dbaef291e486be72a007d9e49
joshruien <joshruien@gmail.com>
2020-08-20 17:57:20 +0800

    Modified runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,21 +13,21 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Duke.java
+if ! javac -cp /Users/joshua/Desktop/ip/src -Xlint:none -d /Users/joshua/Desktop/ip/bin /Users/joshua/Desktop/ip/src/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath /Users/joshua/Desktop/ip/bin Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
+diff ACTUAL.TXT EXPECTED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
