a0828cbce97d2a1f02de383fa816151de288ad3e
Ringo1225 <caoqin@Caos-MacBook-Pro.local>
2020-08-20 22:11:49 +0800

    Added features for Greet, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("\n\n");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n\n");
+        String input = sc.next();
+
+        while (!input.equals(("bye"))) {
+            System.out.println("\n" + input + "\n");
+            input = sc.next();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                return;
+            }
+        }
     }
 }
