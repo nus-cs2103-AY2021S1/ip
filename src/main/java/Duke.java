a97cea3c2f12cb3d69bdf5ecfb0e1f61f2de9e89
Jerry Ho <jerryho@u.nus.edu>
2020-08-20 23:16:29 +0800

    Seperate mainLogic from main() and create a createTask method. Add basic Exceptionhandling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,13 +2,44 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
-        String greetings = "Hello! I'm Duke\nWhat can I do for you?";
-        System.out.println(greetings);
 
+    public static Task createTask(String firstWord, String input) throws DukeException {
+        Task newTask;
+        if (firstWord.equals("todo")) {
+            String[] arr = input.split("todo ");
+            if (arr.length == 1) {
+                throw new DukeException("The description of a todo cannot be empty.");
+            } else {
+                String tsk = arr[1];
+                newTask = new ToDo(tsk);
+            }
+        } else if (firstWord.equals("deadline")) {
+            String[] arr = input.split("deadline ");
+            if (arr.length == 1) {
+                throw new DukeException("The description of a deadline cannot be empty.");
+            } else {
+                String[] split = input.split("/by ");
+                String deadline = split[1];
+                String tsk = split[0].split("deadline ")[1];
+                newTask = new Deadline(tsk, deadline);
+            }
+
+        } else {
+            String[] arr = input.split("event ");
+            if (arr.length == 1) {
+                throw new DukeException("The description of an event cannot be empty.");
+            } else {
+                String[] split = input.split("/at ");
+                String at = split[1];
+                String tsk = split[0].split("event ")[1];
+                newTask = new Event(tsk, at);
+            }
+        }
+        return newTask;
+    }
+    public static void mainLogic(Scanner sc) throws DukeException {
         ArrayList<Task> lst = new ArrayList<>();
 
-        Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
             String firstWord = input.split(" ")[0];
@@ -30,29 +61,31 @@ public class Duke {
                 System.out.println("Nice! I've marked this task as done:\n  " + selectedTask);
             } else if (firstWord.matches("todo|deadline|event")) {
                 Task newTask;
-                if (firstWord.equals("todo")) {
-                    String tsk = input.split("todo ")[1];
-                    newTask = new ToDo(tsk);
-                } else if (firstWord.equals("deadline")) {
-                    String[] split = input.split("/by ");
-                    String deadline = split[1];
-                    String tsk = split[0].split("deadline ")[1];
-                    newTask = new Deadline(tsk, deadline);
-                } else {
-                    String[] split = input.split("/at ");
-                    String at = split[1];
-                    String tsk = split[0].split("event ")[1];
-                    newTask = new Event(tsk, at);
+                try {
+                    newTask = createTask(firstWord, input);
+                } catch (DukeException e) {
+                    throw e;
                 }
                 lst.add(newTask);
                 System.out.println("Got it. I've added this task:\n  " + newTask);
                 System.out.println("Now you have " + lst.size() + " tasks in the list.");
             } else {
-                lst.add(new Task(input));
-                System.out.println("added: " + input);
+                 throw new DukeException("I'm sorry, but I don't know what that means :-(");
             }
         }
+
         String goodbyeMessage = "Bye. Hope to see you again soon!";
         System.out.println(goodbyeMessage);
+    }
+    public static void main(String[] args) {
+        String greetings = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(greetings);
+
+        Scanner sc = new Scanner(System.in);
+        try {
+            mainLogic(sc);
+        } catch (DukeException e) {
+            System.out.println("☹ OOPS!!! " + e.getMessage());
+        }
     }
 }
