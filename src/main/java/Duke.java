a1013c7aa06fda478bf524f266bd7a7740e36945
Muhammad Syafiq Bin Abas <syafiqabs7@gmail.com>
2020-08-20 17:29:35 +0800

    add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while (!(input.equals("bye"))) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
