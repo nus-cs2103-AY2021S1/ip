c5638aa42de7c5b50b0ede8af39a79e019861c7e
jialin7878 <cjl@u.nus.edu>
2020-08-20 13:55:45 +0800

    Level-2

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
-public class DukeException {
+public class DukeException extends java.lang.Exception {
+    public DukeException(String errorMsg) {
+        super(errorMsg);
+    }
 }
