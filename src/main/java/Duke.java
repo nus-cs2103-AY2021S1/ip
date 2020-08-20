a240abd7410462919c47c9cdfb21593852d70a2e
Jerry Ho <jerryho@u.nus.edu>
2020-08-20 20:31:36 +0800

    Import ArrayList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         String greetings = "Hello! I'm Duke\nWhat can I do for you?";
         System.out.println(greetings);
+
+        ArrayList<String> lst = new ArrayList<>();
+        
         Scanner sc = new Scanner(System.in);
         while (true) {
             String str = sc.nextLine();
