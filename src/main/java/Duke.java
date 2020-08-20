81117a493d6658343bc10990cc62190ebfe6246c
duynguyen24501 <duynguyen24501@gmail.com>
2020-08-20 20:07:23 +0800

    Update A-Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Duke {
+    public enum Action {
+        BYE("bye"), LIST("list"), DONE("done"), TODO("todo"), DEADLINE("deadline"), EVENT("event"), DELETE("delete");
+        public String value;
+        Action(String value) {
+            this.value = value;
+        }
+    }
     public static void main(String[] args) throws DukeException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -21,21 +28,21 @@ public class Duke {
 
         while (true) {
             String userInput = sc.nextLine();
-            if (userInput.equals("bye")) {
+            if (userInput.equals(Action.BYE.value)) {
                 System.out.println("    ______________________________________");
                 System.out.println("        Bye. Hope to see you again soon!");
                 System.out.println("    ______________________________________");
                 break;
 
-            } else if (userInput.equals("list")) {
+            } else if (userInput.equals(Action.LIST.value)) {
                 System.out.println("    ______________________________________");
                 System.out.println("        Here are the tasks in your list:");
                 for(int i=0; i<list.size(); i++) {
-                    System.out.println("        " + (i+1) + "." + list.get(i));
+                    System.out.println("            " + (i+1) + "." + list.get(i));
                 }
                 System.out.println("    ______________________________________");
 
-            }  else if (userInput.startsWith("done")) {
+            }  else if (userInput.startsWith(Action.DONE.value)) {
                 try {
                     int taskPosition = Integer.parseInt(userInput.substring(5));
                     list.get(taskPosition - 1).markAsDone();
@@ -47,9 +54,13 @@ public class Duke {
                     System.out.println("    ______________________________________");
                     System.out.println("        ☹ OOPS!!! The description of a done cannot be empty or in wrong format");
                     System.out.println("    ______________________________________");
+                } catch (NumberFormatException e) {
+                    System.out.println("    ______________________________________");
+                    System.out.println("        ☹ OOPS!!! The description of a done cannot be empty or in wrong format");
+                    System.out.println("    ______________________________________");
                 }
 
-            } else if (userInput.startsWith("todo")) {
+            } else if (userInput.startsWith(Action.TODO.value)) {
                 try {
                     String description = userInput.substring(5);
                     Task toDo = new Todo(description);
@@ -65,7 +76,7 @@ public class Duke {
                     System.out.println("    ______________________________________");
                 }
 
-            } else if (userInput.startsWith("deadline")) {
+            } else if (userInput.startsWith(Action.DEADLINE.value)) {
                 try {
                     int spacePosition = userInput.indexOf(" ");
                     int keywordPosition = userInput.indexOf("/by");
@@ -83,7 +94,7 @@ public class Duke {
                     System.out.println("        ☹ OOPS!!! The description of a deadline cannot be empty or in wrong format.");
                     System.out.println("    ______________________________________");
                 }
-            } else if (userInput.startsWith("event")) {
+            } else if (userInput.startsWith(Action.EVENT.value)) {
                 try {
                     int spacePosition = userInput.indexOf(" ");
                     int keywordPosition = userInput.indexOf("/at");
@@ -101,7 +112,8 @@ public class Duke {
                     System.out.println("        ☹ OOPS!!! The description of an event cannot be empty or in wrong format.");
                     System.out.println("    ______________________________________");
                 }
-            } else if (userInput.startsWith("delete")) {
+                
+            } else if (userInput.startsWith(Action.DELETE.value)) {
                 try {
                     int taskPosition = Integer.parseInt(userInput.substring(7));
                     System.out.println("    ______________________________________");
