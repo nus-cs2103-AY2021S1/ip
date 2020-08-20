355bad81bd291873b8e83ffff398d9fca37ecbca
HoPinXian <hopinxian@gmail.com>
2020-08-20 11:44:39 +0800

    Add add, list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -11,13 +12,28 @@ public class Duke {
         // Reply Loop
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        ArrayList<String> taskList = new ArrayList<>();
 
         while(!input.equals("bye")) {
-            String reply = "\nI have received your command as follows:\n"
-                    + input
-                    + "\n";
-            System.out.println(reply);
+            //List all tasks
+            if (input.equals("list")) {
+                String listString = "\nHere are your list of tasks, Master.\n"
+                        + "You have " + taskList.size() + " tasks in total.\n";
 
+                int index = 0;
+                for (String task: taskList) {
+                    index++;
+                    listString += "\n" + index + ": " + task;
+                }
+
+                System.out.println(listString + "\n");
+            } else {
+                //Add to list
+                taskList.add(input);
+                String reply = "\nI have added your command as follows:\n"
+                        + "Added: " + input + "\n";
+                System.out.println(reply);
+            }
             input = sc.nextLine();
         }
 
