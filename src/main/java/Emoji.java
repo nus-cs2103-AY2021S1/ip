420862fec8f51e4d63eda62fbc61d3928f8a99cd
xz0127 <xinzhe0127@gmail.com>
2020-08-20 22:51:37 +0800

    changed source root

--- src/main/java/Emoji.java
+++ src/main/java/Emoji.java
@@ -1,5 +1,3 @@
-package main.java;
-
 public enum Emoji {
 
     CHICKEN(0x1F423),
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,5 +1,3 @@
-package main.java;
-
 public class Task {
     protected String taskDescription;
     protected boolean isDone;
