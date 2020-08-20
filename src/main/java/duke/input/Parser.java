1b93bbe7b61e525f37ed2456e40b99d883b7c48b
geoboom <leegeorgie52@gmail.com>
2020-08-20 21:44:19 +0800

    Add some javadocs

--- src/main/java/duke/input/Parser.java
+++ src/main/java/duke/input/Parser.java
@@ -3,15 +3,27 @@ package duke.input;
 import duke.duke.Duke;
 import java.util.List;
 
+/**
+ * duke.input.Parser is the class that handles input parsing.
+ * Given a string, it should determine what command to execute and execute it.
+ */
 public class Parser {
 
   public static final String CMD_TERMINATE = "bye";
   public static final String CMD_TASK_LIST = "list";
 
+  /**
+   * @param s command supplied
+   * @return true if it's an exit command, ignoring case
+   */
   public static boolean isExitCommand(String s) {
     return s.toLowerCase().equals(CMD_TERMINATE);
   }
 
+  /**
+   * @param s command supplied
+   * @return a list of string (tasks) corresponding to command execution output
+   */
   public static List<String> runCommand(String s) {
     String[] args = s.split("\\s+", 2);
 
--- src/main/java/duke/runner/Runner.java
+++ src/main/java/duke/runner/Runner.java
@@ -3,12 +3,20 @@ package duke.runner;
 import duke.input.Parser;
 import java.util.Scanner;
 
+/**
+ * Class that handles the "event loop" of the CLI program, terminating
+ * when a termination command is detected.
+ */
 public class Runner {
 
   public static void main(String[] args) {
     run();
   }
 
+  /**
+   * Prints hello, goodbye, nd output of command executions.
+   * Also runs the event loop.
+   */
   public static void run() {
     System.out.println("Hi. Duke here.\nHow can I help you?");
     Scanner sc = new Scanner(System.in);
