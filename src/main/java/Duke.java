7f149883f3731a04df9793dcf5b39abb80cff114
MerlinLim <merlinlim98@gmail.com>
2020-08-20 18:37:53 +0800

    Add Greet, Echo and Exit
    Chat bot starts by greeting the user and echos user's inputs.
    When user types, "bye" the bot exits and stops.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        // Greet
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        // Echo
+        while(sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")){
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        // Exit
+        System.out.println("Bye. Hope to see you again soon!");
+
+        sc.close();
     }
+
 }
