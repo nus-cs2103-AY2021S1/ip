d541d4a7670ca73862b462e301358247d087a718
nicholasyeo <65457870+nicholasyeo@users.noreply.github.com>
2020-08-20 21:14:21 +0800

    added exception handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,58 +4,78 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    private final List<Task> tasks;
+    private static List<Task> tasks = new ArrayList<>();
 
-    public Duke() {
-        this.tasks = new ArrayList<>();
-    }
-
-    public void greet() {
+    public static void greet() {
         System.out.println("Hello! I'm Duke.");
         System.out.println("What can I do for you?\n");
     }
 
-    public void handleInput() {
+    public static void handleInput() {
         boolean exit = false;
         Scanner sc = new Scanner(System.in);
-        while (! exit) {
-            String command = sc.nextLine();
-            if (command.equals("bye")) { // Exits while loop
-                exit = true;
-                sc.close();
-            } else if (command.equals("list")) {
-                this.displayTasks();
-            } else if (command.startsWith("done")) {
-                int position = Integer.parseInt(command.substring(5));
-                this.markTaskAsDone(position);
-            } else if (command.startsWith("todo")) {
-                String description = command.substring(5);
-                Task task = new Todo(description);
-                this.addTask(task);
-            } else if (command.startsWith("deadline")) {
-                String[] deadline = command.substring(9).split("/by");
-                String description = deadline[0].trim();
-                String by = deadline[1].trim();
-                Task task = new Deadline(description, by);
-                this.addTask(task);
-            } else if (command.startsWith("event")) {
-                String[] event = command.substring(6).split("/at");
-                String description = event[0].trim();
-                String at = event[1].trim();
-                Task task = new Event(description, at);
-                this.addTask(task);
+        String command;
+        while (!exit) {
+            try {
+                command = sc.nextLine();
+                String errorMessage = "";
+                try {
+                    if (command.equals("bye")) { // Exits while loop
+                        exit = true;
+                        sc.close();
+                    } else if (command.equals("list")) {
+                        displayTasks();
+                    } else if (command.startsWith("done")) {
+                        int position = Integer.parseInt(command.substring(5));
+                        markTaskAsDone(position);
+                    } else if (command.startsWith("todo")) {
+                        String description = command.substring(5);
+                        Task task = new Todo(description);
+                        addTask(task);
+                    } else if (command.startsWith("deadline")) {
+                        String[] deadline = command.substring(9).split("/by");
+                        String description = deadline[0].trim();
+                        String by = deadline[1].trim();
+                        Task task = new Deadline(description, by);
+                        addTask(task);
+                    } else if (command.startsWith("event")) {
+                        String[] event = command.substring(6).split("/at");
+                        String description = event[0].trim();
+                        String at = event[1].trim();
+                        Task task = new Event(description, at);
+                        addTask(task);
+                    } else {
+                        errorMessage = "Sorry! I don't know what that means...";
+                        throw new DukeException(errorMessage);
+                    }
+                } catch (StringIndexOutOfBoundsException e) {
+                    if (isTask(command)) {
+                        errorMessage = "OOPS!!! Description of a task cannot be empty :(";
+                    } else if (command.startsWith("done")) {
+                        errorMessage = "Missing task number! Please ensure to key in the task number :)";
+                    }
+                    throw new DukeException(errorMessage);
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
 
-    public void addTask(Task task) {
-        System.out.println("Okay! Task added for you!");
+    public static boolean isTask(String command) {
+        return command.startsWith("todo") 
+            || command.startsWith("deadline")
+            || command.startsWith("event");
+    }
+
+    public static void addTask(Task task) {
         tasks.add(task);
+        System.out.println("Okay! Task added for you!");
         System.out.println(task);
         System.out.println("Now you have " + tasks.size() + " task(s) in the list." + "\n");
     }
 
-    public void displayTasks() {
+    public static void displayTasks() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println(i + 1 + ". " + tasks.get(i));
@@ -63,21 +83,20 @@ public class Duke {
         System.out.println("");
     }
 
-    public void markTaskAsDone(int position) {
+    public static void markTaskAsDone(int position) {
         Task task = tasks.get(position - 1);
         task.markAsDone();
         System.out.println("Good job! I've marked this task as done:");
         System.out.println(task + "\n");
     }
 
-    public void exit() {
+    public static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
     }
-    
+
     public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.greet();
-        duke.handleInput();
-        duke.exit();
+        greet();
+        handleInput();
+        exit();
     }
 }
