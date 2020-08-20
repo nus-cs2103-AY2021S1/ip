e076acbfd45bbe357596b290cc2161daaefac166
nuovonatura <60500235+nuovonatura@users.noreply.github.com>
2020-08-20 19:12:14 +0800

    Level-3 prototype
    Standard level-3 implementation without error handling and customized behaviors.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,19 +10,29 @@ public class Duke {
                 "\n\tHow may I help you?");
         //Add List
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         while (running) {
             String text = scanner.nextLine();
             if (text.equals("bye")) {
                 System.out.println("\tIt was my pleasure assisting you.\n\tSee you next Time!");
                 running = false;
             } else if (text.equals("list")) {
+                System.out.println("\tHere are the tasks in your list:");
                 for (int i = 1; i <= list.size(); i++) {
                     System.out.println("\t" + i + ". " + list.get(i - 1));
                 }
+            } else if (text.contains("done")) {
+                int taskNo = Integer.parseInt(text.substring(5));
+                if (taskNo > list.size()) {
+
+                }
+                Task task = list.get(taskNo - 1);
+                task.setDone();
+                System.out.println("\tYou have finished this task!\n\t" + task + "\n\tLet's move on to the next one!");
             } else {
-                list.add(text);
-                System.out.println("\tadded: " + text);
+                Task newTask = new Task(text);
+                list.add(newTask);
+                System.out.println("\tadded: " + newTask);
             }
         }
     }
