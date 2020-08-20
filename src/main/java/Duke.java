5ef951e9c79aa8a9c17808d1ce232995d42429f9
nuovonatura <60500235+nuovonatura@users.noreply.github.com>
2020-08-20 22:53:42 +0800

    Level-5
    Implement Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,6 +12,76 @@ public class Duke {
         }
     }
 
+    public static String[] command(String text) throws DukeException {
+        String commandList = "bye|list|done[\\s]+[\\d]+|todo.*|deadline.*|event.*";
+        if (text.matches(commandList)) {
+            String[] commandParaPair = text.split(" ", 2);
+            return commandParaPair;
+        } else {
+            throw new DukeException("\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+    }
+
+    public static int[] execute(String[] commandParaPair, ArrayList<Task> list, boolean running, int activeTasks)
+            throws DukeException {
+        switch (commandParaPair[0]) {
+            case "bye":
+                System.out.println("\tIt was my pleasure assisting you.\n\tSee you next Time!");
+                running = false;
+                break;
+            case "list":
+                System.out.println("\tHere are the tasks in your list:");
+                printList(list, true);
+                break;
+            case "done":
+                int taskNo = Integer.parseInt(commandParaPair[1].replaceAll("[\\s]+", ""));
+                if (taskNo > list.size()) { //If requested index is out of bound.
+                    throw new DukeException("\tYou don't have that many tasks!!!");
+                } else { //If index can be found, set the task to be done and move it to the last.
+                    Task task = list.get(taskNo - 1);
+                    list.remove(task);
+                    task.setDone();
+                    System.out.println("\tYou have finished this task!\n\t" + task + "\n\tLet's move on to the next one!");
+                    printList(list, false);
+                    list.add(task);
+                    activeTasks--;
+                }
+                break;
+            case "todo":
+            case "deadline":
+            case "event":
+                if (commandParaPair.length < 2) {
+                    throw new DukeException("\t☹ OOPS!!! The description of a task cannot be empty.");
+                } else {
+                    Task newTask;
+                    if (commandParaPair[0].equals("todo")) {
+                        newTask = new Todo(commandParaPair[1]);
+                    } else {
+                        String para = commandParaPair[1];
+                        String[] contentTimePair = para.split(" /by | /at ", 2);
+                        if (contentTimePair.length < 2) {
+                            throw new DukeException("\t☹ OOPS!!! The time of a deadline / event cannot be empty.");
+                        } else {
+                            if (commandParaPair[0].equals("deadline")) {
+                                newTask = new Deadline(contentTimePair[0], contentTimePair[1]);
+                            } else {
+                                newTask = new Event(contentTimePair[0], contentTimePair[1]);
+                            }
+                        }
+                    }
+
+                    list.add(activeTasks, newTask);
+                    activeTasks++;
+                    System.out.println("\tadded: " + newTask);
+                    System.out.println("\tYou have " + list.size() + " tasks, " + activeTasks + " undone!");
+                    printList(list, true);
+                }
+                break;
+        }
+        int[] pair = new int[]{running ? 1 : 0, activeTasks};
+        return pair;
+    }
+
     public static void main(String[] args) {
         //To check if the conversation has ended.
         boolean running = true;
@@ -26,47 +96,12 @@ public class Duke {
 
         while (running) {
             String text = scanner.nextLine();
-            if (text.matches("bye")) { //If user wants to leave the chat bot.
-                System.out.println("\tIt was my pleasure assisting you.\n\tSee you next Time!");
-                running = false;
-            } else if (text.matches("list")) { //If user requests for the list of tasks.
-                System.out.println("\tHere are the tasks in your list:");
-                printList(list, true);
-            } else if (text.matches("(done)([\\s]+[\\d]+)")) { //If user wants to set a task to be done
-                int taskNo = Integer.parseInt(text.replaceAll("done[\\s]+", ""));
-                if (taskNo > list.size()) { //If requested index is out of bound.
-                    System.out.println("\tYou don't have that many tasks!!!");
-                } else { //If index can be found, set the task to be done and move it to the last.
-                    Task task = list.get(taskNo - 1);
-                    list.remove(task);
-                    task.setDone();
-                    System.out.println("\tYou have finished this task!\n\t" + task + "\n\tLet's move on to the next one!");
-                    printList(list, false);
-                    list.add(task);
-                    noOfTasks--;
-                }
-            } else {
-                Task newTask;
-                String newText;
-                if (text.matches("(todo .+)")) {
-                    newText = text.replace("todo ", "");
-                    newTask = new Todo(newText);
-                } else if (text.matches("deadline .+")) {
-                    newText = text.replace("deadline ", "");
-                    String pair[] = newText.split(" /by ", 2);
-                    newTask = new Deadline(pair[0], pair[1]);
-                } else if (text.matches("event .+")) {
-                    newText = text.replace("event ", "");
-                    String pair[] = newText.split(" /at ", 2);
-                    newTask = new Event(pair[0], pair[1]);
-                } else { //Echo
-                    newTask = new Task(text);
-                }
-                list.add(noOfTasks, newTask);
-                noOfTasks++;
-                System.out.println("\tadded: " + newTask);
-                System.out.println("\tYou have " + list.size() + " tasks, " + noOfTasks + " undone!");
-                printList(list, true);
+            try {
+                int[] pair = execute(command(text), list, running, noOfTasks);
+                running = pair[0] == 1;
+                noOfTasks = pair[1];
+            } catch (DukeException error) {
+                System.out.println(error.getMessage());
             }
         }
     }
