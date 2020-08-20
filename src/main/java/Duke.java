7952fb8f6963983a197372b19cd4ebffdec2917d
gan-jw <e0425717@u.nus.edu>
2020-08-20 16:04:29 +0800

    Implemented a skeletal version of DeD that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+package main.java;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        boolean check = true;
+        Scanner sc = new Scanner(System.in);
+        String border = "____________________________________________________________\n";
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _______________ \n"
+//                + "| | | | | | | |/ / _ \\/ _ \\/ _ \\\n"
+//                + "| |_| | |_| |   <  __/| __/| __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\\___|\\___|\n";
+        String logo = " ____        ____  \n"
+                + "|  _ \\  ___ |  _ \\\n"
+                + "| | | |/ _ \\| | | |\n"
+                + "| |_| || __/| |_| |\n"
+                + "|____/ \\___||____/\n";
+        System.out.println("Hello I am\n" + logo + "\n" + "What can I do for you? :3");
+        while (check) {
+            String input = sc.nextLine();
+            switch (input) {
+                case "list":
+                    System.out.println(border + "list\n" + border);
+                    break;
+                case "uwu":
+                    System.out.println(border + "owo\n" + border);
+                    break;
+                case "exit":
+                    System.out.println(border + "bb cya again!\n" + border);
+                    check = false;
+                    break;
+                default:
+                    System.out.println(border + "I don't recognise this input :v please try again.\n" + border);
+            }
+        }
     }
 }
