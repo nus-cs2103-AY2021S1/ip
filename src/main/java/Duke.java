8793ed98100ea6bdc5cce483e405bd1653e41674
teng.reuben <reubentengjw@gmail.com>
2020-08-20 17:04:23 +0800

    Minor changes to allow for easier testing, new errors caught

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -80,11 +80,12 @@ public class Duke {
 
     private static final Messages[] ERROR_MESSAGES = {Messages.WRONG_INPUT, Messages.MISSING_INPUT,
         Messages.MISSING_INPUT, Messages.DONE_ERROR};
-
     private static List<Task> thingsOnList = new ArrayList<>();
+
     protected static void printLine() {
         System.out.println("    ************************************************************");
     }
+
     private static void viewList() {
         if (thingsOnList.size() == 0) {
             System.out.println("    It's getting a little lonely here Chief. No tasks here.");
@@ -99,63 +100,70 @@ public class Duke {
         return ERROR_MESSAGES[rnd].toString();
     }
     protected static void Echo() throws DukeExceptions {
-        int startingSize = thingsOnList.size();
+        //startingSize is used to keep track if items are added to the list
+        int startingSize;
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        if (!input.isEmpty()) {
-            printLine();
-            if (input.toLowerCase().equals(Messages.BYE.toString())) {
-                System.out.println(Messages.GOODBYE_MSG.toString());
-            } else if (input.toLowerCase().equals(Messages.LIST.toString())) {
-                viewList();
-            } else if (!input.isEmpty()) {
-                int spaceIndex = input.indexOf(" ");
-                if (spaceIndex != -1 && spaceIndex != input.length() - 1 && (input.contains(Messages.DELETE.toString())
-                    || input.contains(Messages.DONE.toString()))) {
-                    try {
-                        int x = Integer.parseInt(input.substring(spaceIndex + 1)) - 1;
-                        if (x + 1 > thingsOnList.size()) {
-                            throw new DukeExceptions("    This task doesn't exist Chief!");
+        while (sc.hasNextLine()) {
+            startingSize = thingsOnList.size();
+            String input = sc.nextLine();
+            if (!input.isEmpty()) {
+                printLine();
+                if (input.equals(Messages.BYE.toString())) {
+                    System.out.println(Messages.GOODBYE_MSG.toString());
+                } else if (input.equals(Messages.LIST.toString())) {
+                    viewList();
+                } else if (!input.isEmpty()) {
+                    int spaceIndex = input.indexOf(" ");
+                    if (spaceIndex != -1 && spaceIndex != input.length() - 1 && (input.contains(Messages.DELETE.toString())
+                            || input.contains(Messages.DONE.toString()))) {
+                        try {
+                            int x = Integer.parseInt(input.substring(spaceIndex + 1)) - 1;
+                            if (x + 1 > thingsOnList.size()) {
+                                throw new DukeExceptions("    This task doesn't exist Chief!");
+                            }
+                            if (input.substring(0, spaceIndex).equals(Messages.DONE.toString())) {
+                                System.out.println("    Mission accomplished.");
+                                thingsOnList.get(x).markAsDone();
+                                viewList();
+                            } else if (input.substring(0, spaceIndex).equals(Messages.DELETE.toString())) {
+                                System.out.println("    Roger. I'm removing the task: " + thingsOnList.get(x));
+                                thingsOnList.remove(x);
+                            }
+                        } catch (NumberFormatException e) {
+                            throw new DukeExceptions("    That's not a task number.");
                         }
-                        if (input.substring(0, spaceIndex).equals(Messages.DONE.toString())) {
-                            System.out.println("    Mission accomplished.");
-                            thingsOnList.get(x).markAsDone();
-                            viewList();
-                        } else if (input.substring(0, spaceIndex).equals(Messages.DELETE.toString())) {
-                            System.out.println("    Roger. I'm removing the task: " + thingsOnList.get(x));
-                            thingsOnList.remove(x);
-                        }
-                    } catch (NumberFormatException e) {
-                        throw new DukeExceptions("    That's not a task number.");
-                    }
-                } else {
-                    int cmdIndex = input.indexOf("/");
-                    if (cmdIndex == 0 || (input.contains("todo") && input.length() == 4)) {
-                    } else if (cmdIndex != -1 && cmdIndex != input.length() - 1) {
-                        String cmd = input.substring(cmdIndex, cmdIndex + 3);
-                        if (cmd.equals("/by")) {
-                            thingsOnList.add(new Deadlines(input));
-                        } else if (cmd.equals("/at")) {
-                            thingsOnList.add(new Events(input));
-                        }
-                    } else if (input.contains("todo") && !(input.substring(input.length() - 4).contains("todo"))) {
-                        thingsOnList.add(new ToDos(input));
-                    }
-                    if (thingsOnList.size() == startingSize) {
-                        throw new DukeExceptions("    There has been an incorrect use of a command.");
                     } else {
-                        System.out.println("    Roger. I've added this task:\n    " +
-                                thingsOnList.get(thingsOnList.size() - 1) + "\n    " +
-                                "Now you have " + thingsOnList.size() + " tasks in the list.");
+                        int cmdIndex = input.indexOf("/");
+                        if (cmdIndex == 0 || (input.contains("todo") && input.length() == 4)) {
+                            throw new DukeExceptions("    There's no content for this command");
+                        } else if (cmdIndex != -1 && cmdIndex != input.length() - 1) {
+                            String cmd = input.substring(cmdIndex, cmdIndex + 3);
+                            if (cmd.equals("/by")) {
+                                thingsOnList.add(new Deadlines(input));
+                            } else if (cmd.equals("/at")) {
+                                thingsOnList.add(new Events(input));
+                            }
+                        } else if (input.contains("todo") && !(input.substring(input.length() - 4).contains("todo"))) {
+                            if (input.substring(5).isEmpty()) {
+                                throw new DukeExceptions("    Can't make a todo without something to do, Chief.");
+                            }
+                            thingsOnList.add(new ToDos(input));
+                        }
+                        if (thingsOnList.size() == startingSize) {
+                            throw new DukeExceptions("    There has been an incorrect use of a command.");
+                        } else {
+                            System.out.println("    Roger. I've added this task:\n    " +
+                                    thingsOnList.get(thingsOnList.size() - 1) + "\n    " +
+                                    "Now you have " + thingsOnList.size() + " tasks in the list.");
+                        }
                     }
                 }
+                printLine();
             }
-            printLine();
-        }
-        if (!input.equals(Messages.BYE.toString())) {
-            Echo();
-        } else {
-            sc.close();
+            if (input.equals(Messages.BYE.toString())) {
+                sc.close();
+                break;
+            }
         }
     }
     public static void main(String[] args) {
--- src/main/java/DukeExceptions.java
+++ src/main/java/DukeExceptions.java
@@ -1,4 +1,18 @@
 package main.java;
 
-public class DukeExceptions {
+public class DukeExceptions extends Exception {
+    public DukeExceptions(String message) {
+        super(message);
+    }
+
+    protected void continueTrying() {
+        try {
+            Duke.Echo();
+        } catch (DukeExceptions e) {
+            System.out.println(e.getMessage());
+            System.out.println(Duke.getWrongInput());
+            Duke.printLine();
+            continueTrying();
+        }
+    }
 }
