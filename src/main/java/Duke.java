cba56e953cb5e1ccdf85a0966461fc88513fa9c8
teekoksiang <teekoksiang89@gmail.com>
2020-08-20 11:53:45 +0800

    Implement greet, echo and exit chatbot actions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String GREET_MESSAGE = "Hello! I'm AiBot :)\n\tEnter the command you would like to do\n\tEnter 'bye' to exit";
+    private static final String EXIT_MESSAGE = "Bye. See you again soon!";
+    private static final String DIVIDER = "\n\t-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n";
+
+    private static void printResponse(String message) {
+        System.out.println(DIVIDER.concat("\t".concat(message.concat("\n"))).concat(DIVIDER));
+    }
+
+    private static void listenCommand() {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (!command.equalsIgnoreCase("bye")) {
+            printResponse(command);
+            command = scanner.nextLine();
+        }
+
+        scanner.close();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printResponse(GREET_MESSAGE);
+        listenCommand();
+        printResponse(EXIT_MESSAGE);
     }
 }
