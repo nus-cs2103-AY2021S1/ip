5577ffb60c5844ec057ef07e2dcbe7b5b32435ce
zhengweii <limzhengwei_99@hotmail.com>
2020-08-20 10:10:04 +0800

    Add support for tracking todos, deadlines and events

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,36 +16,80 @@ public class Duke {
         String s = sc.next();
 
         ArrayList<Task> database = new ArrayList<>(100);
+        String description;
         while (!s.equals("bye")) {
-            if (s.equals("list")) {
-                list(database);
-            } else if (s.equals("done")) {
-                if (database.size() != 0) {
-                    int index = sc.nextInt();
-                    database.get(index - 1).markAsDone();
-                    System.out.println("Nice! I have marked this task as done!");
-                } else {
-                    System.out.println("There are no tasks in the database to mark as done");
-                }
-            } else {
-                add(database, s);
-                System.out.println("Added item: " + s);
+            switch (s) {
+                case "list":
+                    list(database);
+                    break;
+
+                case "done":
+                    if (database.size() != 0) {
+                        int index = sc.nextInt();
+                        database.get(index - 1).markAsDone();
+                        System.out.println("Nice! I have marked this task as done!");
+                    } else {
+                        System.out.println("There are no tasks in the database to mark as done");
+                    }
+                    break;
+
+                case "todo":
+                    description = sc.nextLine();
+                    ToDo toDo = new ToDo(description);
+                    add(database, toDo);
+                    break;
+
+                case "deadline":
+                case "event":
+                    String toParse = sc.nextLine();
+                    if (!toParse.equals("")) {
+                        try {
+                            if (s.equals("deadline")) {
+                                String[] parsedStrings = toParse.split("/by ");
+                                Deadline deadline = new Deadline(parsedStrings[0], parsedStrings[1]);
+                                add(database, deadline);
+                            } else {
+                                String[] parsedStrings = toParse.split("/at ");
+                                Event event = new Event(parsedStrings[0], parsedStrings[1]);
+                                add(database, event);
+                            }
+                        } catch (Exception ArrayIndexOutOfBoundsException) {
+                            System.out.println("Please input " +
+                                    (s.equals("deadline") ? "the deadline" : "start and end timing") +
+                                    " in a valid format!");
+                        }
+                    } else {
+                        System.out.println("Description and " +
+                                (s.equals("deadline") ? "the deadline" : "the start and end time") +
+                                " of the task cannot be empty");
+                    }
+                    break;
+
+                default:
+                    System.out.println("Oops! No such command exists. Please enter a valid command");
+                    break;
             }
+
             s = sc.next();
         }
 
         System.out.println("Goodbye! All the best and see you again soon!");
     }
 
-    private static void add(ArrayList<Task> database, String text) {
-        database.add(new Task(text));
+    private static void add(ArrayList<Task> database, Task task) {
+        database.add(task);
+        System.out.println("Looking good! I have added this task:");
+        System.out.println(task);
+        System.out.println("Now you have " + database.size() + " tasks in your list");
     }
 
     private static void list(ArrayList<Task> database) {
         if (database.size() == 0) {
             System.out.println("There are no tasks in the database");
+            return;
         }
 
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < database.size(); i++) {
             System.out.println((i + 1) + ". " + database.get(i));
         }
