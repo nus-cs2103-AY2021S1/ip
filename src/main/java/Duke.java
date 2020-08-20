6d55428d3e24f700a8cc9cbb9b9be217957f867d
Wang Zijun <wzj97@hotmail.com>
2020-08-20 22:04:37 +0800

    added command echo and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    public void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+
+    public void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        //initialize Duke and send welcome message
+        Duke duke = new Duke();
+        duke.greet();
+
+        //input loop
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                break;
+            }
+            System.out.println(s);
+        }
+
+        //send exit message
+        duke.bye();
+
     }
 }
