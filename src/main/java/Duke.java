b6fd68c655fd4644966e4eda82a6ba3bbf13067a
Daniel Adi <daniel@adip.me>
2020-08-20 17:30:06 +0800

    add add and list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,16 +6,32 @@ public class Duke {
         String welcome = "Hello. I am Claude! What may I do for you today?";
         String goodbye = "Goodbye! Hope to see you again soon!";
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>();
 
         System.out.println(welcome);
         while (sc.hasNext()) {
             String command = sc.nextLine();
-            if (command.equals("bye")){
+            if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                System.out.println(printList(taskList));
+            } else {
+                taskList.add(command);
+                System.out.println("\n Task added: " + command);
             }
-            System.out.println("\n" + command);
         }
-
         System.out.println(goodbye);
+    }
+
+    private static String printList(ArrayList<String> al) {
+        if (al.isEmpty()) {
+            return "List is empty!";
+        } else {
+            String s = "";
+            for (int i = 0; i < al.size(); i++) {
+                s = s + String.valueOf(i+1) + ". " + al.get(i) + "\n";
+            }
+            return s;
+        }
     }
 }
