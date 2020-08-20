27b4fd59ab19229afa212ab6c842847241bc9fc8
hoperawr <hopeleong@gmail.com>
2020-08-20 19:12:08 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        bot();
     }
+
+    public static void bot(){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")){
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
 }
