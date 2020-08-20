1108d20a25b54eed4812b701b3fb52705f6cecb6
solkim-83 <65942359+solkim-83@users.noreply.github.com>
2020-08-21 00:27:56 +0800

    Revised testing of UI testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) throws DukeException, IOException {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+        String logo = " ____        _\n"
+                + "|  _ \\ _   _| | _____\n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
 
         System.out.println("-------------------------------------------\n" +
                 "Hello! I'm Duke\n" + "What can I do for you?\n" +
