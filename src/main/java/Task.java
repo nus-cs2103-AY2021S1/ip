9eec2ef7d4dfa1953d1d27cc8f4a021caa982781
lebencwb <46550747+lebencwb@users.noreply.github.com>
2020-08-20 23:43:44 +0800

    separated Task from Duke.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
