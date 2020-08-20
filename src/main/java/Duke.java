5e8c2c8b77545f1cff8b50dc43dbf941eac08b54
gan-jw <e0425717@u.nus.edu>
2020-08-20 16:23:31 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,38 +1,42 @@
 package main.java;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         boolean check = true;
         Scanner sc = new Scanner(System.in);
-        String border = "____________________________________________________________\n";
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _______________ \n"
-//                + "| | | | | | | |/ / _ \\/ _ \\/ _ \\\n"
-//                + "| |_| | |_| |   <  __/| __/| __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\\___|\\___|\n";
+        ArrayList<String> storedItems = new ArrayList<>();
+        String border = "____________________________________________________________";
+
         String logo = " ____        ____  \n"
                 + "|  _ \\  ___ |  _ \\\n"
                 + "| | | |/ _ \\| | | |\n"
                 + "| |_| || __/| |_| |\n"
                 + "|____/ \\___||____/\n";
-        System.out.println("Hello I am\n" + logo + "\n" + "What can I do for you? :3");
+        System.out.println("Hello I am\n" + logo + "\n" + "Feed me some input! :3\n");
+
         while (check) {
             String input = sc.nextLine();
             switch (input) {
                 case "list":
-                    System.out.println(border + "list\n" + border);
+                    System.out.println(border);
+                    for (int i = 0; i < storedItems.size(); i ++) {
+                        System.out.println(String.format("%d. %s", i + 1, storedItems.get(i)));
+                    }
+                    System.out.println(border);
                     break;
                 case "uwu":
-                    System.out.println(border + "owo\n" + border);
+                    System.out.println(border + "\n" + "owo\n" + border);
                     break;
                 case "exit":
-                    System.out.println(border + "bb cya again!\n" + border);
+                    System.out.println(border + "\n" + "bb cya again!\n" + border);
                     check = false;
                     break;
                 default:
-                    System.out.println(border + "I don't recognise this input :v please try again.\n" + border);
+                    storedItems.add(input);
+                    System.out.println(border + "\n" + "*Gobble gobble* " + input + " has been eated OwO\n" + border);
             }
         }
     }
