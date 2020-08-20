42e83832756d6b64491343a946ee086214d25238
gohyeeloon <goh.yeeloon@gmail.com>
2020-08-20 17:55:52 +0800

    trying to figure out testing 2.0

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp /Users/yeeloon/Desktop/2103/ip/src/main/java -Xlint:none -d /Users/yeeloon/Desktop/2103/bin
+if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/Duke.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath /Users/yeeloon/Desktop/2103/bin Duke.java < input.txt > ACTUAL.TXT
+java -classpath ../bin Duke < input.txt > ACTUAL.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED.TXT
