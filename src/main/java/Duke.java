1beafcdf0ccc68fff29585a25e76ebe252d122a7
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-08-20 19:51:53 +0800

    Add extra exception handling and testing coverage

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -31,47 +31,79 @@ public class Duke {
             // blocks program until input is received
             String newInput = inputScanner.nextLine();
             // then prints the input
+            
+            //trim the input to remove leading and ending whitespace
+            newInput = newInput.trim();
 
             try {
                 if (newInput.equals("bye")) {
                     quitProgram = true;
                 } else if (newInput.equals("list")) {
                     out.println(taskList.getAllTasksAsString());
-                } else if (newInput.length() > 5 && newInput.substring(0, 5).equals("done ")) {
-                    int taskIndex = Integer.parseInt(newInput.substring(5));    // this is not corrected for 0 index
-                    Task completedTask = taskList.getTask(taskIndex);
-                    completedTask.markAsDone();
+                } else if (newInput.indexOf("done") == 0) {
+                    if (newInput.length() == 4) {
+                        throw new DukeException("OOPS!!! Please specify a task to mark as complete.");
+                    } else {
+                        int taskIndex = Integer.parseInt(newInput.substring(5));    // this is not corrected for 0 index
+                        Task completedTask = taskList.getTask(taskIndex);
+                        completedTask.markAsDone();
 
-                    out.println("Nice! I've marked this task as done: ");
-                    out.println(completedTask);
-                } else if (newInput.indexOf("delete ") == 0) {
-                    int taskIndex = Integer.parseInt(newInput.substring(7));
+                        out.println("Nice! I've marked this task as done: ");
+                        out.println(completedTask);
+                    }
+                } else if (newInput.indexOf("delete") == 0) {
+                    if (newInput.length() == 6) {
+                        throw new DukeException(" OOPS!!! Please specify a task to delete.");
+                    } else {
+                        try {
+                            int taskIndex = Integer.parseInt(newInput.substring(7));
+                            
+                            Task removedTask = taskList.getTask(taskIndex);
+                            taskList.deleteTask(taskIndex);
 
-                    Task removedTask = taskList.getTask(taskIndex);
-                    taskList.deleteTask(taskIndex);
-
-                    out.println("Noted. I've removed this task: ");
-                    out.println(removedTask);
-                    out.println("Now you have " + taskList.numTasks() + " tasks in the list.");
+                            out.println("Noted. I've removed this task: ");
+                            out.println(removedTask);
+                            out.println("Now you have " + taskList.numTasks() + " tasks in the list.");
+                        } catch (NumberFormatException e) {
+                            throw new DukeException(" OOPS!!! Please specify a valid task to delete.");
+                        }
+                    }
                 } else {
                     // use indexOf() method to find substring
                     Task newTask = new Task("");    // this is to make the compiler happy
 
-                    if (newInput.indexOf("event ") == 0) {
-                        int indexOfAtKeyword = newInput.indexOf(" /at ");
-                        String eventDesc = newInput.substring(6, indexOfAtKeyword);
-                        String eventTime = newInput.substring(indexOfAtKeyword + 5);
+                    if (newInput.indexOf("event") == 0) {
+                        if (newInput.length() <= 5) {
+                            throw new DukeException(" OOPS!!! The description of an event cannot be empty.");
+                        } else {
+                            int indexOfAtKeyword = newInput.indexOf(" /at ");
+                            
+                            if (indexOfAtKeyword == -1) {
+                                throw new DukeException(" OOPS!!! Please specify the event time.");
+                            } else {
+                                String eventDesc = newInput.substring(6, indexOfAtKeyword);
+                                String eventTime = newInput.substring(indexOfAtKeyword + 5);
 
-                        newTask = new Event(eventDesc, eventTime);
-                        taskList.addTask(newTask);
+                                newTask = new Event(eventDesc, eventTime);
+                                taskList.addTask(newTask);
+                            }
+                        }
+                    } else if (newInput.indexOf("deadline") == 0) {
+                        if (newInput.length() <= 8) {
+                            throw new DukeException(" OOPS!!! The description of a deadline cannot be empty.");
+                        } else {
+                            int indexOfByKeyword = newInput.indexOf(" /by ");
+                            
+                            if (indexOfByKeyword == -1) {
+                                throw new DukeException(" OOPS!!! Please specify the deadline time.");
+                            } else {
+                                String deadlineDesc = newInput.substring(9, indexOfByKeyword);
+                                String deadlineTime = newInput.substring(indexOfByKeyword + 5);
 
-                    } else if (newInput.indexOf("deadline ") == 0) {
-                        int indexOfByKeyword = newInput.indexOf(" /by ");
-                        String deadlineDesc = newInput.substring(9, indexOfByKeyword);
-                        String deadlineTime = newInput.substring(indexOfByKeyword + 5);
-
-                        newTask = new Deadline(deadlineDesc, deadlineTime);
-                        taskList.addTask(newTask);
+                                newTask = new Deadline(deadlineDesc, deadlineTime);
+                                taskList.addTask(newTask);
+                            }
+                        }
                     } else if (newInput.indexOf("todo") == 0){
                         if (newInput.length() <= 4 || newInput.substring(5).trim().length() == 0) {
                             throw new DukeException(" OOPS!!! The description of a todo cannot be empty.");
