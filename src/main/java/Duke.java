2c6280c4f71fd41a8905dd0b6ff8a16f7cb75457
dearvae <lovevae7@gmail.com>
2020-08-20 16:38:41 +0800

    feat(main):Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static List<String> lists = new ArrayList<>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,14 +15,29 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         greeting();
+
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         String input = scanner.nextLine();  // Read user input
-        while(!input.equals("bye")) {
-            System.out.println(input);  // Output user input
-            input = scanner.nextLine();
+        while(true) {
+            if(input.equals("bye")){
+                break;
+            } else if(input.equals("list")) {
+                displayList();
+                input = scanner.nextLine();
+            } else {
+                System.out.println("added: " + input);  // Output user input
+                lists.add(input);
+                input = scanner.nextLine();
+            }
         }
 
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void displayList() {
+        for(int i = 0; i < lists.size(); i++ ) {
+            System.out.println(i+1 + ". " + lists.get(i));
+        }
     }
 
     public static void greeting() {
