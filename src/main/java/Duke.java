10ed662b1ab1d778baf3bd3011e1c1f70bf13927
lebencwb <46550747+lebencwb@users.noreply.github.com>
2020-08-20 23:00:08 +0800

    "

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String textFormat(String str) {
-        return "\t" + "-------------------------------------------------------\n\t"
-                + str + "\n"
-                + "\t" + "-------------------------------------------------------\n";
-    }
     public static void main(String[] args) {
-        System.out.print(textFormat("Hello! I'm Duke\n" + "\t" + "What can I do for you?"));
+        String horizontalLine = "\t-------------------------------------------------------";
+        System.out.println(horizontalLine);
+        System.out.println("\tHello! I'm Duke\n" + "\t" + "What can I do for you?");
+        System.out.println(horizontalLine);
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int index = 0;
         while (scanner.hasNext()) {
+            System.out.println(horizontalLine);
             String userInput = scanner.nextLine();
-            if (userInput.equals("bye")) {
-                System.out.println(textFormat("Bye. Hope to see you again soon!"));
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
                 scanner.close();
                 break;
             }
-            System.out.println(textFormat(userInput));
+            if (userInput.equalsIgnoreCase("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println("\t" + list[i]);
+                }
+                System.out.println(horizontalLine);
+            } else {
+                list[index] = String.format("%d. %s", ++index, userInput);
+                System.out.println("\tadded: " + userInput);
+                System.out.println(horizontalLine);
+            }
         }
     }
 }
