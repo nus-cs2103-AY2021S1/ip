51302f24a3f54a70c98e1d1f4998bf57061b81f5
lll-jy <E0376997@u.nus.edu>
2020-08-21 16:29:34 +0800

    Copy isDoneCommand to Parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,22 @@
 public class Parser {
+    public static int isDoneCommand(String cmd, int count) throws InvalidCommandException {
+        if (cmd.startsWith("done ")) {
+            if (cmd.length() < 6) {
+                throw new InvalidCommandException("\u2639 OOPS!!! The task to mark as done cannot be empty.");
+            }
+            try {
+                int n = Integer.parseInt(cmd.substring(5));
+                if (n < 1) {
+                    throw new InvalidCommandException("\u2639 OOPS!!! The task index should be a positive integer.");
+                } else if (n > count) {
+                    throw new InvalidCommandException("\u2639 OOPS!!! The task index does not exist.");
+                }
+                return n;
+            } catch (NumberFormatException e) {
+                throw new InvalidCommandException("\u2639 OOPS!!! The task index should be a number.");
+            }
+        } else {
+            return -1;
+        }
+    }
 }
