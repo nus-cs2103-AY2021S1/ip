62f18c440c6e289f458fb78ad42baf9d8b3a243c
zhaojj2209 <zhaojj2000@gmail.com>
2020-08-22 00:35:24 +0800

    fix switch indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -52,74 +52,74 @@ public class Duke {
                 next = sc.nextLine();
                 actionType action = getAction(next);
                 switch(action) {
-                    case QUIT:
-                        System.out.println("Bye! :>");
-                        sc.close();
-                        System.exit(0);
-                        break;
-                    case LIST:
-                        if (list.size() == 0) {
-                            System.out.println("List is empty");
-                        } else {
-                            System.out.println("Items in list:");
-                            for (int i = 0; i < list.size(); i++) {
-                                System.out.println((i + 1) + ". " + list.get(i).toString());
-                            }
+                case QUIT:
+                    System.out.println("Bye! :>");
+                    sc.close();
+                    System.exit(0);
+                    break;
+                case LIST:
+                    if (list.size() == 0) {
+                        System.out.println("List is empty");
+                    } else {
+                        System.out.println("Items in list:");
+                        for (int i = 0; i < list.size(); i++) {
+                            System.out.println((i + 1) + ". " + list.get(i).toString());
                         }
-                        break;
-                    case MARK_DONE:
-                        int taskNo = Integer.parseInt(next.substring(5));
-                        if (taskNo > list.size()) {
-                            throw new DukeException("Task does not exist _(´ཀ`」 ∠)_");
-                        } else {
-                            Task completedTask = list.get(taskNo - 1);
-                            completedTask.markAsDone();
-                            System.out.println("Task marked complete:");
-                            System.out.println(completedTask.toString());
-                        }
-                        break;
-                    case DELETE:
-                        int deleteNo = Integer.parseInt(next.substring(7));
-                        if (deleteNo > list.size()) {
-                            throw new DukeException("Task does not exist _(´ཀ`」 ∠)_");
-                        } else {
-                            Task deletedTask = list.remove(deleteNo - 1);
-                            System.out.println("Task deleted:");
-                            System.out.println(deletedTask.toString());
-                        }
-                        break;
-                    case ADD_TODO:
-                        if (next.length() < 6) {
-                            throw new DukeException("Task cannot be empty _(´ཀ`」 ∠)_");
-                        } else {
-                            Task newTodo = new ToDo(next.substring(5));
-                            list.add(newTodo);
-                            System.out.println("Added: " + newTodo.toString());
-                            System.out.println("Total tasks: " + list.size());
-                        }
-                        break;
-                    case ADD_EVENT:
-                        if (next.length() < 7) {
-                            throw new DukeException("Event cannot be empty _(´ཀ`」 ∠)_");
-                        } else {
-                            Task newEvent = new Event(next.substring(6));
-                            list.add(newEvent);
-                            System.out.println("Added: " + newEvent.toString());
-                            System.out.println("Total tasks: " + list.size());
-                        }
-                        break;
-                    case ADD_DEADLINE:
-                        if (next.length() < 10) {
-                            throw new DukeException("Deadline cannot be empty _(´ཀ`」 ∠)_");
-                        } else {
-                            Task newDeadline = new Deadline(next.substring(9));
-                            list.add(newDeadline);
-                            System.out.println("Added: " + newDeadline.toString());
-                            System.out.println("Total tasks: " + list.size());
-                        }
-                        break;
-                    case WRONG_INPUT:
-                        throw new DukeException("I have no idea what that means ¯\\_(ツ)_/¯");
+                    }
+                    break;
+                case MARK_DONE:
+                    int taskNo = Integer.parseInt(next.substring(5));
+                    if (taskNo > list.size()) {
+                        throw new DukeException("Task does not exist _(´ཀ`」 ∠)_");
+                    } else {
+                        Task completedTask = list.get(taskNo - 1);
+                        completedTask.markAsDone();
+                        System.out.println("Task marked complete:");
+                        System.out.println(completedTask.toString());
+                    }
+                    break;
+                case DELETE:
+                    int deleteNo = Integer.parseInt(next.substring(7));
+                    if (deleteNo > list.size()) {
+                        throw new DukeException("Task does not exist _(´ཀ`」 ∠)_");
+                    } else {
+                        Task deletedTask = list.remove(deleteNo - 1);
+                        System.out.println("Task deleted:");
+                        System.out.println(deletedTask.toString());
+                    }
+                    break;
+                case ADD_TODO:
+                    if (next.length() < 6) {
+                        throw new DukeException("Task cannot be empty _(´ཀ`」 ∠)_");
+                    } else {
+                        Task newTodo = new ToDo(next.substring(5));
+                        list.add(newTodo);
+                        System.out.println("Added: " + newTodo.toString());
+                        System.out.println("Total tasks: " + list.size());
+                    }
+                    break;
+                case ADD_EVENT:
+                    if (next.length() < 7) {
+                        throw new DukeException("Event cannot be empty _(´ཀ`」 ∠)_");
+                    } else {
+                        Task newEvent = new Event(next.substring(6));
+                        list.add(newEvent);
+                        System.out.println("Added: " + newEvent.toString());
+                        System.out.println("Total tasks: " + list.size());
+                    }
+                    break;
+                case ADD_DEADLINE:
+                    if (next.length() < 10) {
+                        throw new DukeException("Deadline cannot be empty _(´ཀ`」 ∠)_");
+                    } else {
+                        Task newDeadline = new Deadline(next.substring(9));
+                        list.add(newDeadline);
+                        System.out.println("Added: " + newDeadline.toString());
+                        System.out.println("Total tasks: " + list.size());
+                    }
+                    break;
+                case WRONG_INPUT:
+                    throw new DukeException("I have no idea what that means ¯\\_(ツ)_/¯");
                 }
             }
         } catch (Exception e) {
