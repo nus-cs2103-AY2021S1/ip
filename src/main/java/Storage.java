ed91b86724fcb651af38a18925afc9ff88802fb8
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-08-21 18:27:54 +0800

    Add Storage class to read and write to .txt storage file

---
+++ src/main/java/Storage.java
@@ -0,0 +1,46 @@
+import javax.sound.midi.Soundbank;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+
+    private final String storagePath = "data/duke.txt";
+
+    public String[] getTasks() {
+        File storageFile = new File(storagePath);
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(storageFile);
+            while (scanner.hasNext()) {
+                lines.add(scanner.nextLine());
+            }
+            return lines.toArray(new String[0]);
+        } catch (FileNotFoundException e) {
+            System.out.println("Storage file not found");
+            return new String[0];
+        }
+    }
+
+    public void addTasks(String taskLine) {
+        try {
+            FileWriter storageWriter = new FileWriter(storagePath, true);
+            storageWriter.write(taskLine);
+            storageWriter.close();
+        } catch (IOException e) {
+            System.out.println("Storage file not found");
+        }
+    }
+
+    public static void main(String[] args) {
+        // temporary tests
+        Storage s = new Storage();
+        String[] result = s.getTasks();
+        for (String str : result) {
+            System.out.println(str);
+        }
+    }
+}
