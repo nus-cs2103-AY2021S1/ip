551365434352b7f2f12ba6dab7beec94d5a7f2ba
lll-jy <E0376997@u.nus.edu>
2020-08-21 17:13:09 +0800

    Delete unnecesarry imports

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Duke {
     public static void main(String[] args) throws IOException, InvalidCommandException {
