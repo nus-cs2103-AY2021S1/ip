67dcf8b54c41abfbcf7adff3dbc4b1aefe7bf5a5
lll-jy <E0376997@u.nus.edu>
2020-08-21 17:09:47 +0800

    Crate print method

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class TaskList extends ArrayList<Task> {
+    int count;
 
+    public void print(Predicate<Task> predicate) {
+        for (int i = 0; i < count; i++) {
+            Task task = get(i);
+            if (predicate.test(task)) {
+                System.out.println("\t  " + (i + 1) + "." + task);
+            }
+        }
+    }
 }
