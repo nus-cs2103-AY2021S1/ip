eb5ee99f5b9f896e2d24a09c2f3732db67a21b9e
lll-jy <E0376997@u.nus.edu>
2020-08-21 16:03:33 +0800

    Read from the file

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,15 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Storage {
     private final String filePath;
     public final List<Task> list;
 
-    public Storage(String filePath) {
+    public Storage(String filePath) throws FileNotFoundException {
         this.filePath = filePath;
         list = new ArrayList<>();
+        File file = new File(filePath);
+        Scanner sc = new Scanner(file);
+        while (sc.hasNext()) {
+            String taskType = sc.next();
+            int done = sc.nextInt();
+            String description = sc.next();
+            description = sc.nextLine();
+            Task toAdd;
+            if (taskType == "T") {
+                toAdd = new ToDo(description);
+            } else {
+                int position = description.indexOf('|');
+                String time = description.substring(position + 6);
+                description = description.substring(0, position - 1);
+                if (taskType == "D") {
+                    toAdd = new Deadline(description, time);
+                } else if (taskType == "E") {
+                    toAdd = new Event(description, time);
+                }
+            }
+            if (done == 1) {
+                toAdd.markAsDone();
+            }
+            list.add(toAdd);
+        }
     }
 
     public void addToList(Task task) throws InvalidCommandException {
