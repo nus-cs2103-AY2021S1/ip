1e20eb928419b63725cf239a142c3994b46ae4a2
zhiayang <zhiayang@nascentnoodle.com>
2020-08-22 01:38:47 +0800

    Add the Database class, and allow loading Tasks from disk

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,27 @@
 // Copyright (c) 2020, zhiayang, Apache License 2.0.
 
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
 
+    private static final String BOT_NAME  = "ikurabowl";
+    private static final String DB_PATH   = "data/tasks.txt";
+
     public static void main(String[] args) {
-        var bot = new Bot("ikurabowl");
+        var bot = new Bot(BOT_NAME);
+
+        var db = new Database(DB_PATH);
+
+        try {
+            bot.setTasks(db.loadTasks());
+        } catch (IOException e) {
+            System.out.printf("error occured while reading/creating the task list:\n%s\n", e);
+            System.exit(1);
+        } catch (InvalidDatabaseException e) {
+            System.out.printf("malformed line while reading task list:\n%s\n", e);
+            System.exit(1);
+        }
 
         var sc = new Scanner(System.in);
 
