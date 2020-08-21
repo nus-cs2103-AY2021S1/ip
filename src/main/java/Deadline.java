72e785b577613716ea31b0acf9d212e34c60d591
gohsiauchiak <gsc928@gmail.com>
2020-08-21 17:04:08 +0800

    Make Task an abstract class and add toTaskData method
    Task is a normal class.
    
    There are some methods like toTaskData that are implemented
    differently depending on the subclass of Task.
    
    Let's make Task an abstract class to provide this common
    functionality across the subclasses while also allowing Task to
    have default methods.
    
    Using an abstract class is preferable over an interface because
    the subclasses have an "is-a" relationship to Task, instead of a
    "can-do" relationship.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,11 +1,17 @@
 public class Deadline extends Task {
     private static final String STRING_FORMAT = "[D][%s] %s (by: %s)";
+    private static final String TASK_DATA_FORMAT = "%s|%d|%s|%s";
 
     protected String dueDate;
 
     public Deadline(String description, boolean isDone, String dueDate) {
         super(description, isDone);
         this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toTaskData() {
+        return String.format(Deadline.TASK_DATA_FORMAT, "D", isDone ? 1 : 0, description, dueDate);
     }
 
     @Override
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,17 @@
 public class Event extends Task {
     private static final String STRING_FORMAT = "[E][%s] %s (at: %s)";
+    private static final String TASK_DATA_FORMAT = "%s|%d|%s|%s";
 
     protected String eventTime;
 
     public Event(String description, boolean isDone, String eventTime) {
         super(description, isDone);
         this.eventTime = eventTime;
+    }
+
+    @Override
+    public String toTaskData() {
+        return String.format(Event.TASK_DATA_FORMAT, "E", isDone ? 1 : 0, description, eventTime);
     }
 
     @Override
