e518cc96a990ef6112fd2e3b8dff250579a320bb
lll-jy <E0376997@u.nus.edu>
2020-08-21 19:30:22 +0800

    Execute DoneCommand

--- src/main/java/DoneCommand.java
+++ src/main/java/DoneCommand.java
@@ -2,4 +2,15 @@ public class DoneCommand extends Command {
     public DoneCommand(String input) {
         super(input);
     }
+
+    @Override
+    public void execute(Ui ui, TaskList list, Storage storage) throws InvalidCommandException {
+        int m = Parser.isValidDelete(input, list.count) - 1;
+        Task toDelete = list.get(m);
+        list.remove(toDelete);
+        storage.deleteTask(list);
+        ui.output("Noted. I've removed this task:\n\t    " + toDelete +
+                "\n\t  Now you have " + list.size());
+        list.count--;
+    }
 }
