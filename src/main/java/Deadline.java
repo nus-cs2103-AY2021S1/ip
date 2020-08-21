ecf267005f14adec2af5ec6a4a93c25987c9b38d
lll-jy <E0376997@u.nus.edu>
2020-08-21 12:29:57 +0800

    Change output date format of deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -15,6 +15,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + byTime + ")";
+        return "[D]" + super.toString() + " (by: " + byTime.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
