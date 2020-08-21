7b8fdb6f44fc8f165d3ad14f98278a8b507bbffa
lll-jy <E0376997@u.nus.edu>
2020-08-21 19:38:27 +0800

    Fix typo: isValidDelete -> isValidDone

--- src/main/java/DoneCommand.java
+++ src/main/java/DoneCommand.java
@@ -5,7 +5,7 @@ public class DoneCommand extends Command {
 
     @Override
     public void execute(Ui ui, TaskList list, Storage storage) throws InvalidCommandException {
-        int m = Parser.isValidDelete(input, list.count) - 1;
+        int m = Parser.isValidDone(input, list.count) - 1;
         Task toDelete = list.get(m);
         list.remove(toDelete);
         storage.deleteTask(list);
