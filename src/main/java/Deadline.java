5ba463285b5c708d1aee7e42a1319af8a886a6e2
khenghun <e0406137@u.nus.edu>
2020-08-21 20:34:49 +0800

    Level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,8 @@
-public class Deadline extends Task {
-
-    protected String by;
+public class Deadline extends TimedTask {
 
     public Deadline(String description, String by) {
-        super(description);
-        this.by = by.trim();
-    }
-
-    @Override
-    public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        super(description, by);
+        this.connecting = " (by: ";
+        this.firstLetter = "[D]";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,8 @@
-public class Event extends Task {
-
-    protected String at;
+public class Event extends TimedTask {
 
     public Event(String description, String at) {
-        super(description);
-        this.at = at.trim();
-    }
-
-    @Override
-    public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        super(description,at);
+        this.connecting = " (at: ";
+        this.firstLetter = "[E]";
     }
 }
