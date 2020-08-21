f57b9eb752b27911222fcfdaca4f461d0cf74184
Robinho98 <ho.robin@yahoo.com.sg>
2020-08-21 21:33:39 +0800

    Add date formatting into deadline and event class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Deadline task that inherits from Task class, and has an additional condition, which is when it is due by.
  */
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     /**
      * Creates a new Deadline object
      *
      * @param description details about the Deadline
-     * @param by time/date the deadline is due by
+     * @param by date the deadline is due by in yyyy-mm-dd format
      * @return Deadline with a corresponding description and sets it as uncompleted.
      */
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by.trim());
     }
 
     /**
@@ -23,6 +26,6 @@ public class Deadline extends Task {
      */
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Event task that inherits from Task class, and has an additional condition, which is when it is held at.
  */
 public class Event extends Task {
-    protected String at;
+    protected LocalDate at;
 
     /**
      * Creates a new Event object
      *
      * @param description details about the Event
-     * @param at time/date the event is held at
+     * @param at date the event is held at in yyyy-mm-dd format
      * @return Event with a corresponding description and sets it as uncompleted.
      */
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        this.at = LocalDate.parse(at.trim());
     }
 
     /**
@@ -23,6 +26,6 @@ public class Event extends Task {
      */
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + at.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ")";
     }
 }
