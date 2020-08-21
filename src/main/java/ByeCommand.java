f76cf1deef7c355c70435b01cdc1f47d938a7748
lll-jy <E0376997@u.nus.edu>
2020-08-21 19:14:36 +0800

    Execute ByeCommand

--- src/main/java/ByeCommand.java
+++ src/main/java/ByeCommand.java
@@ -1,2 +1,6 @@
-public class ByeCommand {
+public class ByeCommand extends Command {
+    @Override
+    public void execute(Ui ui, TaskList list) {
+        ui.printList(list, t -> true, "");
+    }
 }
