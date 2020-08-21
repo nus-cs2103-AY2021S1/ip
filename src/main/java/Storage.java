939bfe1616a6e0f150132ad4eecfd337bce3be9f
lll-jy <E0376997@u.nus.edu>
2020-08-21 15:37:17 +0800

    Copy addToList to Storage

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,7 +1,20 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Storage {
     private final String filePath;
 
     public Storage(String filePath) {
         this.filePath = filePath;
+    }
+
+    public void addToList(Task task) throws InvalidCommandException {
+        try {
+            FileWriter fw = new FileWriter(filePath, true);
+            fw.write(task.output());
+            fw.close();
+        } catch (IOException e) {
+            throw new InvalidCommandException(e.getMessage());
+        }
     }
 }
