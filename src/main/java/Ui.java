4d302f0265c1e76acaf1e13f6f4f22be3d39ff0b
lll-jy <E0376997@u.nus.edu>
2020-08-21 14:51:55 +0800

    Copy output method for Ui

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,5 +1,11 @@
 public class Ui {
+    private static final String horizontalLine = "\t=================================================================================";
+
     public void print(String str) {
         System.out.println(str);
+    }
+
+    private String output(String message) {
+        return horizontalLine + "\n\t  " + message + "\n" + horizontalLine + "\n";
     }
 }
