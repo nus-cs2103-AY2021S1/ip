ee96dc47820bd141760186a83cb6fe3921876920
lll-jy <E0376997@u.nus.edu>
2020-08-21 19:24:22 +0800

    Add constructro to ListCommand

--- src/main/java/ListCommand.java
+++ src/main/java/ListCommand.java
@@ -1,4 +1,8 @@
 public class ListCommand extends Command {
+    public ListCommand(String input) {
+        super(input);
+    }
+
     @Override
     public void execute(Ui ui, TaskList list, Storage storage) {
         ui.printList(list, t -> true, "");
