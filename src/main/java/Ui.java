ff591a7ec569798c1d3350d74f6e6f1a9a480aaf
lll-jy <E0376997@u.nus.edu>
2020-08-21 14:53:06 +0800

    Copy printList method for Ui

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Ui {
     private static final String horizontalLine = "\t=================================================================================";
 
@@ -7,5 +10,16 @@ public class Ui {
 
     private String output(String message) {
         return horizontalLine + "\n\t  " + message + "\n" + horizontalLine + "\n";
+    }
+
+    private static void printList(int count, List<Task> list, Predicate<Task> predicate, String note) {
+        System.out.println(horizontalLine + "\n\t  " + "Here are the tasks " + note + "in your list:");
+        for (int i = 0; i < count; i++) {
+            Task task = list.get(i);
+            if (predicate.test(task)) {
+                System.out.println("\t  " + (i + 1) + "." + list.get(i));
+            }
+        }
+        System.out.println(horizontalLine + "\n");
     }
 }
