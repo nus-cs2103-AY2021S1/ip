4aa09e2c51b35d4714626356e9eb414741decdbb
lll-jy <E0376997@u.nus.edu>
2020-08-21 19:34:56 +0800

    Execute AddCommand

--- src/main/java/AddCommand.java
+++ src/main/java/AddCommand.java
@@ -2,4 +2,14 @@ public class AddCommand extends Command {
     public AddCommand(String input) {
         super(input);
     }
+
+    @Override
+    public void execute(Ui ui, TaskList list, Storage storage) throws InvalidCommandException {
+        Task task = Parser.generate(input);
+        storage.addToList(task);
+        list.add(list.count++, task);
+        String temp = list.count <= 1 ? " task" : " tasks";
+        ui.output("Got it. I've added this task:\n\t    " + task +
+                "\n\t  Now you have " + list.count + temp + " in the list.");
+    }
 }
