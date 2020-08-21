33af0a6a1e589e616a2db5c945536c563f37a143
lll-jy <E0376997@u.nus.edu>
2020-08-21 19:21:20 +0800

    Change Command constructor to have an input command string

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,8 +1,10 @@
 public abstract class Command {
     public boolean isExit;
+    private final String input;
 
-    public Command() {
+    public Command(String input) {
         isExit = false;
+        this.input = input;
     }
 
     abstract public void execute(Ui ui, TaskList list, Storage storage);
