9792c6c8e8e7fbe9675ecd95378ee0ffac16a7dd
lll-jy <E0376997@u.nus.edu>
2020-08-21 19:32:42 +0800

    Execute HappensCommand

--- src/main/java/HappensCommand.java
+++ src/main/java/HappensCommand.java
@@ -1,5 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class HappensCommand extends Command {
     public HappensCommand(String input) {
         super(input);
+    }
+
+    @Override
+    public void execute(Ui ui, TaskList list, Storage storage) throws InvalidCommandException {
+        try {
+            LocalDate date = LocalDate.parse(input.substring(11),
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            ui.printList(list, t -> t.happenOnDate(date), "happening on " +
+                    date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " ");
+        } catch (Exception e) {
+            throw new InvalidCommandException("Invalid date format. Please use yyyy-MM-dd");
+        }
     }
 }
