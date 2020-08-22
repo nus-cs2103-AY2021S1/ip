43ef62a1c475edd2d7b40b3b872e5ae33597c2a2
Zhengyang Lin <nauw1010@gmail.com>
2020-08-23 04:23:21 +0800

    Update Using LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task{
     protected String by;
+    protected LocalDate byDate;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        try{
+            byDate = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            byDate = null;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: "
+                + (byDate == null ? by : byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")))
+                + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task{
     protected String at;
+    protected LocalDate atDate;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        try{
+            atDate = LocalDate.parse(at);
+        } catch (DateTimeParseException e) {
+            atDate = null;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[D]" + super.toString() + " (by: "
+                + (atDate == null ? at : atDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")))
+                + ")";
     }
 }
