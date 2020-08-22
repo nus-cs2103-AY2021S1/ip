e6ba87898ef5c7efd4032d9731683a17d7f2074a
Lye Yi Xian <lyeyx999@hotmail.com>
2020-08-23 00:44:09 +0800

    Update class Deadline to account for actual date format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-  private String by;
+    private LocalDate deadlineDate;
 
-  public Deadline(String description, String by) {
-    super(description);
-    this.by = by;
-  }
+    public Deadline(String description, String by) {
+        super(description);
+        this.deadlineDate = LocalDate.parse(by);
+    }
 
-  @Override
-  public String toString() {
-    return "[D]" + super.toString() + " (by: " + by + ")";
-  }
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + deadlineDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+    }
 }
