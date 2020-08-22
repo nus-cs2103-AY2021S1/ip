4613ed899d0ad1966debb7a46128e7bcd2f3cd67
Sigmund <sigmundchianasta@gmail.com>
2020-08-22 21:38:13 +0800

    Add save functions to and from /save.txt

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,6 +8,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[DEADLINE]" + super.toString() + " | by: " + deadlineTime;
+        return "[DEADLINE] " + super.toString() + " | by: " + deadlineTime;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,6 +8,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[EVENT]" + super.toString() + " | at: " + eventTime;
+        return "[EVENT] " + super.toString() + " | at: " + eventTime;
     }
 }
