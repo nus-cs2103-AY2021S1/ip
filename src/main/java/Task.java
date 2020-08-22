5e640434b6295489288d2d779eeb6efbc69e104f
marcus <marcus2k>
2020-08-22 22:04:55 +0800

    Change Task members to protected

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,15 +2,15 @@ public abstract class Task {
     protected String description;
     protected boolean done = false;
 
-    public Task(String description) {
+    protected Task(String description) {
         this.description = description;
     }
 
-    public boolean isDone() {
+    protected boolean isDone() {
         return this.done;
     }
 
-    public Task markAsDone() {
+    protected Task markAsDone() {
         this.done = true;
         return this;
     }
