508493e8b9434e276c250c97d27b9688c1f0c8d7
lll-jy <E0376997@u.nus.edu>
2020-08-22 15:48:16 +0800

    Use java.Scanner to deal with happen command

--- src/main/java/duke/command/HappenCommand.java
+++ src/main/java/duke/command/HappenCommand.java
@@ -5,6 +5,7 @@ import duke.task.Task;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class HappenCommand extends Command {
     public HappenCommand(String input) {
@@ -14,28 +15,39 @@ public class HappenCommand extends Command {
     @Override
     public void execute(Ui ui, TaskList list, Storage storage) throws InvalidCommandException {
         String description = input.substring(7);
+        Scanner sc = new Scanner(description);
         try {
-            if (description.equals("on today")) {
-                ui.printList(list, Task::happenToday, "happening today ");
-            } else if (description.startsWith("on ")) {
-                LocalDate date = LocalDate.parse(description.substring(3),
-                        DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                ui.printList(list, t -> t.happenOnDate(date), "happening on " +
-                        date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " ");
-            } else if (description.equals("before today")) {
-                ui.printList(list, Task::happenBeforeToday, "happening before today ");
-            } else if (description.startsWith("before ")) {
-                LocalDate date = LocalDate.parse(description.substring(7),
-                        DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                ui.printList(list, t -> t.happenBeforeDate(date), "happening before " +
-                        date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " ");
-            } else if (description.equals("after today")) {
-                ui.printList(list, Task::happenAfterToday, "happening after today ");
-            } else if (description.startsWith("after ")) {
-                LocalDate date = LocalDate.parse(description.substring(6),
-                        DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                ui.printList(list, t -> t.happenAfterDate(date), "happening after " +
-                        date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " ");
+            String type, detail;
+            try {
+                type = sc.next();
+                detail = sc.next();
+            } catch (Exception e) {
+                throw new InvalidCommandException("Invalid happen command input");
+            }
+            if (type.equals("on") && !sc.hasNext()) {
+                if (detail.equals("today")) {
+                    ui.printList(list, Task::happenToday, "happening today ");
+                } else {
+                    LocalDate date = LocalDate.parse(detail, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                    ui.printList(list, t -> t.happenOnDate(date), "happening on " +
+                            date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " ");
+                }
+            } else if (type.equals("before") && !sc.hasNext()) {
+                if (detail.equals("today")) {
+                    ui.printList(list, Task::happenBeforeToday, "happening before today ");
+                } else {
+                    LocalDate date = LocalDate.parse(detail, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                    ui.printList(list, t -> t.happenBeforeDate(date), "happening before " +
+                            date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " ");
+                }
+            } else if (type.equals("after") && !sc.hasNext()) {
+                if (detail.equals("today")) {
+                    ui.printList(list, Task::happenAfterToday, "happening after today ");
+                } else {
+                    LocalDate date = LocalDate.parse(detail, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                    ui.printList(list, t -> t.happenAfterDate(date), "happening after " +
+                            date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " ");
+                }
             } else {
                 throw new InvalidCommandException("Invalid happen command input");
             }
