cecfeadc164787ef95aca0a01e693ca041b31471
Sigmund <sigmundchianasta@gmail.com>
2020-08-22 21:53:15 +0800

    Modify dates to use java.time.LocalDate and not Strings

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    String deadlineTime;
+    LocalDate deadlineTime;
 
     Deadline(String taskName, String deadlineTime) throws DukeException {
         super (taskName);
-        this.deadlineTime = deadlineTime.trim();
+        // ISO_LOCAL_DATE format, no conversion needed (yyyy-mm-dd)
+        try {
+            this.deadlineTime = LocalDate.parse(deadlineTime.trim());
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Please write down our dates in the standard format, yyyy-mm-dd.");
+        }
     }
 
     @Override
     public String toString() {
-        return "[DEADLINE]" + super.toString() + " | by: " + deadlineTime;
+        return "[DEADLINE]" + super.toString() + " | by: " + deadlineTime.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
     }
 }
