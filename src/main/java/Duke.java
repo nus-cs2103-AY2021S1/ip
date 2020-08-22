1fdd9a8795b00adc4efadf90c811e8909116073a
MatthiasLHK <59341741+MatthiasLHK@users.noreply.github.com>
2020-08-23 00:06:00 +0800

    Default master

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,20 +2,11 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        Scanner sc = new Scanner(System.in);
-        String echo = sc.nextLine();
-        while(!echo.equals("bye")){
-            System.out.println(echo);
-            echo = sc.nextLine();
-        }
-        System.out.println("Bye. Hope to see you again soon!");
+       String logo = " ____        _        \n"
+               + "|  _ \\ _   _| | _____ \n"
+               + "| | | | | | | |/ / _ \\\n"
+               + "| |_| | |_| |   <  __/\n"
+               + "|____/ \\__,_|_|\\_\\___|\n";
+       System.out.println("Hello from\n" + logo);
     }
 }
