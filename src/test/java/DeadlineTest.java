cb9bce5bba0cacd76d95c328805d2644313da659
davidsqf <davidsongqifeng@gmail.com>
2020-08-22 11:42:43 +0800

    add a few JUnit tests

---
+++ src/test/java/DeadlineTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    @Test
+    public void toStringTest() {
+        assertEquals("[D][\u2718] buy gift(Apr 28 2021)",
+                new Deadline("buy gift", LocalDate.parse("2021-04-28")));
+    }
+
+}
---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void byeTest() throws IOException {
+        assertEquals("    Bye-bye, see you next time!", Parser.process("bye"));
+    }
+}
