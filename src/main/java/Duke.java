3deb8e922f416ab3b0b2df1a8f5818547f32d49a
theyifan <zhang.yifan@u.nus.edu>
2020-08-22 16:19:55 +0800

    Correct indentation for switch case

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,60 +15,60 @@ public class Duke {
             try {
                 CommandType command = convertInputToEnum(inputs[0].toLowerCase());
                 switch (command) {
-                    case BYE:
-                        Print.formatPrint("Bye! Hope to see you again soon! ");
-                        return;
-                    case LIST:
-                        Task.printList();
-                        break;
-                    case DONE:
-                        if (inputs.length <= 1) {
-                            throw new DukeException("OOPS! Task number cannot be empty for done action!");
-                        }
-                        try {
-                            Task.markTaskAsDone(Integer.parseInt(inputs[1]));
-                        } catch (NumberFormatException | IndexOutOfBoundsException ex) {
-                            throw new DukeException("OOPS! Task number is invalid!");
-                        }
-                        break;
-                    case DELETE:
-                        if (inputs.length <= 1) {
-                            throw new DukeException("OOPS! Task number cannot be empty for delete action!");
-                        }
-                        try {
-                            Task.deleteTask(Integer.parseInt(inputs[1]));
-                        } catch (NumberFormatException | IndexOutOfBoundsException ex) {
-                            throw new DukeException("OOPS! Task number is invalid!");
-                        }
-                        break;
-                    case TODO:
-                        if (inputs.length <= 1) {
-                            throw new DukeException("OOPS! The description of a todo cannot be empty!");
-                        }
-                        Task.addTask(new ToDo(inputs[1]));
-                        break;
-                    case DEADLINE:
-                        if (inputs.length <= 1) {
-                            throw new DukeException("OOPS! The description of a deadline cannot be empty!");
-                        }
-                        String[] deadlineDetails = inputs[1].split(" /by ", 2);
-                        if (deadlineDetails.length <= 1) {
-                            throw new DukeException("OOPS! The date of a deadline cannot be empty!");
-                        }
-                        Task.addTask(new Deadline(deadlineDetails[0], deadlineDetails[1]));
-                        break;
-                    case EVENT:
-                        if (inputs.length <= 1) {
-                            throw new DukeException("OOPS! The description of an event cannot be empty!");
-                        }
-                        String[] eventDetails = inputs[1].split(" /at ", 2);
-                        if (eventDetails.length <= 1) {
-                            throw new DukeException("OOPS! The time of an event cannot be empty!");
-                        }
-                        Task.addTask(new Event(eventDetails[0], eventDetails[1]));
-                        break;
-                    default:
-                        throw new DukeException("OOPS! I'm sorry, but I don't know what that means :-(");
+                case BYE:
+                    Print.formatPrint("Bye! Hope to see you again soon! ");
+                    return;
+                case LIST:
+                    Task.printList();
+                    break;
+                case DONE:
+                    if (inputs.length <= 1) {
+                        throw new DukeException("OOPS! Task number cannot be empty for done action!");
+                    }
+                    try {
+                        Task.markTaskAsDone(Integer.parseInt(inputs[1]));
+                    } catch (NumberFormatException | IndexOutOfBoundsException ex) {
+                        throw new DukeException("OOPS! Task number is invalid!");
+                    }
+                    break;
+                case DELETE:
+                    if (inputs.length <= 1) {
+                        throw new DukeException("OOPS! Task number cannot be empty for delete action!");
+                    }
+                    try {
+                        Task.deleteTask(Integer.parseInt(inputs[1]));
+                    } catch (NumberFormatException | IndexOutOfBoundsException ex) {
+                        throw new DukeException("OOPS! Task number is invalid!");
+                    }
+                    break;
+                case TODO:
+                    if (inputs.length <= 1) {
+                        throw new DukeException("OOPS! The description of a todo cannot be empty!");
+                    }
+                    Task.addTask(new ToDo(inputs[1]));
+                    break;
+                case DEADLINE:
+                    if (inputs.length <= 1) {
+                        throw new DukeException("OOPS! The description of a deadline cannot be empty!");
+                    }
+                    String[] deadlineDetails = inputs[1].split(" /by ", 2);
+                    if (deadlineDetails.length <= 1) {
+                        throw new DukeException("OOPS! The date of a deadline cannot be empty!");
+                    }
+                    Task.addTask(new Deadline(deadlineDetails[0], deadlineDetails[1]));
+                    break;
+                case EVENT:
+                    if (inputs.length <= 1) {
+                        throw new DukeException("OOPS! The description of an event cannot be empty!");
+                    }
+                    String[] eventDetails = inputs[1].split(" /at ", 2);
+                    if (eventDetails.length <= 1) {
+                        throw new DukeException("OOPS! The time of an event cannot be empty!");
+                    }
+                    Task.addTask(new Event(eventDetails[0], eventDetails[1]));
+                    break;
+                default:
+                    throw new DukeException("OOPS! I'm sorry, but I don't know what that means :-(");
                 }
             } catch (DukeException e) {
                 Print.formatPrint(e.getMessage());
@@ -80,22 +80,22 @@ public class Duke {
 
     private static CommandType convertInputToEnum(String input) throws DukeException {
         switch (input) {
-            case "bye":
-                return CommandType.BYE;
-            case "list":
-                return CommandType.LIST;
-            case "done":
-                return CommandType.DONE;
-            case "delete":
-                return CommandType.DELETE;
-            case "todo":
-                return CommandType.TODO;
-            case "deadline":
-                return CommandType.DEADLINE;
-            case "event":
-                return CommandType.EVENT;
-            default:
-                throw new DukeException("OOPS! I'm sorry, but I don't know what that means :-(");
+        case "bye":
+            return CommandType.BYE;
+        case "list":
+            return CommandType.LIST;
+        case "done":
+            return CommandType.DONE;
+        case "delete":
+            return CommandType.DELETE;
+        case "todo":
+            return CommandType.TODO;
+        case "deadline":
+            return CommandType.DEADLINE;
+        case "event":
+            return CommandType.EVENT;
+        default:
+            throw new DukeException("OOPS! I'm sorry, but I don't know what that means :-(");
         }
     }
 }
