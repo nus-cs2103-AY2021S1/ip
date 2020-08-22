0240c52fc00e096da7887b3da92864007f4a3a65
rishi <rishi5154@gmail.com>
2020-08-22 16:54:09 +0800

    Level 7: Save

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,20 @@
 public class Deadline extends Task {
-    public Deadline(String description, int index) {
-        super(description, index);
+    public Deadline(String description, int index, boolean isDone) {
+        super(description, index, isDone);
+        super.type = TaskType.DEADLINE;
     }
     @Override
     public String getStatusWithIndex() {
         int idx = this.description.indexOf('/');
         String task = this.description.substring(0, idx);
         String end = new StringBuilder(this.description.substring(idx + 1)).insert(2,':').toString();
-        return String.format("%s. %s%s%s(%s)", index, TaskType.DEADLINE, isDone ? super.done : super.start, task, end);
+        return String.format("%s. %s%s%s(%s)", index, super.type, isDone ? super.done : super.start, task, end);
     }
     @Override
     public String toString() {
         int idx = this.description.indexOf('/');
         String task = this.description.substring(0, idx);
         String end = new StringBuilder(this.description.substring(idx + 1)).insert(2,':').toString();
-        return String.format("%s%s%s(%s)", TaskType.DEADLINE, isDone ? super.done : super.start, task, end);
+        return String.format("%s%s%s(%s)", super.type, isDone ? super.done : super.start, task, end);
     }
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static List<Task> todoList = new ArrayList<>();
+    public static List<Task> todoList; //= new ArrayList<>();
     public static String line = "---------------------------------------------------";
+    public static Data data;
+
+    public Duke(String path) {
+        try {
+            data = new Data(path);
+            //System.out.println("Before calling load");
+            todoList = data.loadData();
+        } catch (IOException e) {
+            System.out.println("FAILURE: Unable to load data from local drive.");
+            todoList = new ArrayList<>();
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,6 +30,70 @@ public class Duke {
                 "What can I do for you?\n";
         //System.out.println(logo + "\n" + welcome);
         System.out.println(welcome);
+//        Scanner scan = new Scanner(System.in);
+//        String word = scan.nextLine();
+//        int len = 0;
+//        while (!word.equals("bye")) {
+//            len = word.length();
+//            if (word.equals("list")) {
+//                System.out.println(line);
+//                for (Task task : todoList) {
+//                    System.out.println(task.getStatusWithIndex());
+//                }
+//            } else if (len >= 4 && word.substring(0,4).equals("todo")) {
+//                processTask(word);
+//            }
+////                try {
+////                    //String todo = word.substring(5);
+////                    Todo toDo = storeTodo(word);
+////                    System.out.println(toDo == null ? "Failed!" : line + "\nGot it. I've have added this task:\n   " + toDo.toString()
+////                            + "\nNow you have " + todoList.size() + " tasks in the list." );
+////                } catch (EmptyDukeException e) {
+////                    System.out.println(e.toString());
+////                }
+//            else if (len >= 8 && word.substring(0,8).equals("deadline")) {
+//                processTask(word);
+////                try {
+////                    Deadline toDo = storeDeadline(word);
+////                    System.out.println(toDo == null ? "Failed!" : line + "\nGot it. I've have added this task:\n   " + toDo.toString()
+////                            + "\nNow you have " + todoList.size() + " tasks in the list." );
+////                } catch (EmptyDukeException e) {
+////                    System.out.println(e.toString());
+////                }
+//            } else if (len >= 5 && word.substring(0,5).equals("event")) {
+//                processTask(word);
+////                try {
+////                    Event toDo = storeEvent(word);
+////                    System.out.println(toDo == null ? "Failed!" : line + "\nGot it. I've have added this task:\n   " + toDo.toString()
+////                            + "\nNow you have " + todoList.size() + " tasks in the list." );
+////                } catch (EmptyDukeException e) {
+////                    System.out.println(e.toString());
+////                }
+//            } else if (len > 4 && word.substring(0,5).equals("done ")) {
+//                int taskNo = Character.getNumericValue(word.charAt(5)) - 1;
+//                Task task = todoList.get(taskNo);
+//                task.isDone = true;
+//                System.out.println(line + "\nNice! I have marked this task as done: \n  " + task.toString());
+//            } else {
+//                if (len > 6 && word.substring(0,7).equals("delete ")) {
+//                    int taskNo = Character.getNumericValue(word.charAt(7)) - 1;
+//                    delete(taskNo);
+//                } else {
+//                    try {
+//                        storeTask(word);
+//                        System.out.println(line + "\nadded: " + word);
+//                    } catch (BlahException e){
+//                        System.out.println(e.toString());
+//                    }
+//                }
+//            }
+//            word = scan.nextLine();
+//        }
+//        System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
+        new Duke("data/duke.txt").runDuke();
+    }
+
+    public void runDuke(){
         Scanner scan = new Scanner(System.in);
         String word = scan.nextLine();
         int len = 0;
@@ -25,7 +104,7 @@ public class Duke {
                 for (Task task : todoList) {
                     System.out.println(task.getStatusWithIndex());
                 }
-            } else if (len >= 4 && word.substring(0,4).equals("todo")) {
+            } else if (word.startsWith("todo")/*len >= 4 && word.substring(0,4).equals("todo")*/) {
                 processTask(word);
             }
 //                try {
@@ -36,7 +115,7 @@ public class Duke {
 //                } catch (EmptyDukeException e) {
 //                    System.out.println(e.toString());
 //                }
-            else if (len >= 8 && word.substring(0,8).equals("deadline")) {
+            else if (word.startsWith("deadline") /*len >= 8 && word.substring(0,8).equals("deadline")*/) {
                 processTask(word);
 //                try {
 //                    Deadline toDo = storeDeadline(word);
@@ -45,7 +124,7 @@ public class Duke {
 //                } catch (EmptyDukeException e) {
 //                    System.out.println(e.toString());
 //                }
-            } else if (len >= 5 && word.substring(0,5).equals("event")) {
+            } else if (word.startsWith("event") /*len >= 5 && word.substring(0,5).equals("event")*/) {
                 processTask(word);
 //                try {
 //                    Event toDo = storeEvent(word);
@@ -75,6 +154,11 @@ public class Duke {
             word = scan.nextLine();
         }
         System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
+        try {
+            data.save(todoList);
+        } catch (IOException e) {
+            System.out.println("FAILURE: Could not save data to an appropriate directory.");
+        }
     }
 
     public static void storeTask(String item) throws BlahException {
@@ -95,7 +179,7 @@ public class Duke {
         } else if (todo.length() <= 4) {
             throw new EmptyDukeException("The description of your todo is empty.");
         } else {
-            Todo curr = new Todo(todo.substring(5), count);
+            Todo curr = new Todo(todo.substring(5), count, false);
             todoList.add(curr);
             return curr;
         }
@@ -108,7 +192,7 @@ public class Duke {
         } else if (deadline.length() <= 8) {
             throw new EmptyDukeException("The description of your deadline is empty.");
         } else {
-            Deadline curr = new Deadline(deadline.substring(9), count);
+            Deadline curr = new Deadline(deadline.substring(9), count, false);
             todoList.add(curr);
             return curr;
         }
@@ -121,7 +205,7 @@ public class Duke {
         } else if (event.length() <= 5) {
             throw new EmptyDukeException("The description of your event is empty.");
         } else {
-            Event curr = new Event(event.substring(6), count);
+            Event curr = new Event(event.substring(6), count, false);
             todoList.add(curr);
             return curr;
         }
