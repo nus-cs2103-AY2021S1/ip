dd99a95d2d9a4fb92b879702c11357de3362dfe9
Lingy12 <russellgy1229@gmail.com>
2020-08-22 11:06:09 +0800

    Support save user data to hard drive

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -7,6 +7,17 @@ public class Event extends Task{
         this.at = at;
     }
 
+    public Event(boolean isDone,String description, String at) {
+        super(description);
+        this.at = at;
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toFileStringFormat() {
+        return String.format("E / %d / %s / %s",isDone ? 1 : 0, this.desciption,this.at);
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (at: " + at + ")";
