d03439b4d8fbff7d8a8511f9132f28dfb4e7b19b
rizhaow <rizhaow@gmail.com>
2020-08-22 18:14:50 +0800

    feat(level8): add list date command to print deadlines/events occurring on a specific date

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,15 +2,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
-    private LocalDate date;
-
     public Event(String name, LocalDate date) {
-        super(name, "[E]");
-        this.date = date;
+        super(name, "[E]", date);
     }
 
     @Override
     public String toString() {
-        return String.format("%s (at: %s)", super.toString(), this.date.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
+        return String.format("%s (at: %s)", super.toString(), super.getDate().get().format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
