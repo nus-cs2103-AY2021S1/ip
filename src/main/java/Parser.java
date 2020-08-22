6b0eab5aae19411f7a0cd2e75fe404e5f9c13346
Rayson Koh <rayson.rk.koh@gmail.com>
2020-08-22 11:30:14 +0800

    Refactored StringUtils to Parser and Ui

---
+++ src/main/java/Parser.java
@@ -0,0 +1,130 @@
+import java.time.LocalDate;
+
+public class Parser {
+    public static Command parseInput(String input) throws DukeException {
+        validateNotEmpty(input);
+
+        String[] segments = input.split(" ");
+        String cmd = segments[0].trim();
+
+        // CHECK FOR NON-TASKS-RELATED COMMANDS
+        switch (cmd) {
+            case "bye":
+                return new ByeCommand();
+            case "save":
+                return new SaveCommand();
+            case "list":
+                return new ListCommand();
+            case "done":
+                return new DoneCommand(Integer.parseInt(segments[1].trim()));
+            case "delete":
+                return new DeleteCommand(Integer.parseInt(segments[1].trim()));
+            case "todo":
+            case "deadline":
+            case "event":
+                break;
+            default:
+                throw new DukeException("Aww! The first word of your input is wrong!");
+        }
+
+        String[] parsedSegments = parseSegments(input, cmd, getTaskBreakPt(cmd));
+        // CHECK FOR TASKS-RELATED COMMANDS
+        switch (cmd) {
+            case "todo":
+                return new TodoCommand(parsedSegments[0]);
+            case "deadline":
+                return new DeadlineCommand(parsedSegments[0], parsedSegments[1]);
+            case "event":
+                return new EventCommand(parsedSegments[0], parsedSegments[1]);
+            default:
+                throw new DukeException("Aww! The first word of your input is wrong!");
+        }
+    }
+
+    public static Task parseTaskData(String data) throws DukeException {
+        String[] segments = data.split("\\|");
+        Task task;
+        boolean isDone = segments[1].equals("1");
+
+        switch (segments[0]) {
+            case "T":
+                task = new TodoTask(segments[2], isDone);
+                break;
+            case "D":
+                task = new DeadlineTask(segments[2], isDone, LocalDate.parse(segments[3]));
+                break;
+            case "E":
+                task = new EventTask(segments[2], isDone, LocalDate.parse(segments[3]));
+                break;
+            default:
+                task = null;
+        }
+        return task;
+    }
+
+    private static String[] parseSegments(String input, String cmd, String breakPt) throws DukeException {
+        checkIllegalChar(input);
+
+        int size = 2;
+        if (breakPt.equals("")) {
+            size = 1;
+        }
+        String[] res = new String[size];
+
+        String[] splitByBP1 = input.split(cmd);
+        if (splitByBP1.length <= 1) {
+            throw new DukeException(cmd + " description cannot be empty!");
+        }
+
+        String description = splitByBP1[1].trim();
+        if (description.equals("")) {
+            throw new DukeException(cmd + " description cannot be empty!");
+        }
+
+        if (size == 1) {
+            res[0] = description;
+            return res;
+        }
+
+        String[] splitByBP2 = description.split(breakPt);
+        if (!description.contains(breakPt)) {
+            throw new DukeException(breakPt + " keyword must be in input for all " + cmd + " commands!");
+        }
+
+        if (splitByBP2.length <= 1 || splitByBP2[1].trim().equals("")) {
+            throw new DukeException("Content after " + breakPt + " should not be empty!");
+        }
+
+        res[0] = splitByBP2[0].trim();
+        res[1] = splitByBP2[1].trim();
+
+        return res;
+    }
+
+    private static void checkIllegalChar(String input) throws DukeException {
+        char[] illegalChar = {'|'};
+        for (char c : illegalChar) {
+            if (input.indexOf(c) != -1) {
+                throw new DukeException("Illegal Character: " + c);
+            }
+        }
+    }
+
+    private static void validateNotEmpty(String input) throws DukeException {
+        if (input.equals("")) {
+            throw new DukeException("Input cannot be empty!");
+        }
+    }
+
+    private static String getTaskBreakPt(String taskName) {
+        switch (taskName) {
+            case "deadline":
+                return "/by";
+            case "event":
+                return "/at";
+            default:
+                return "";
+        }
+
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ui {
+    private final Scanner sc;
+    private final static String ERRORBORDER = "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    private final static String BORDER = "===================================================";
+    private final static String INDENT = "    ";
+
+    public Ui() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public String getUserInput() {
+        return sc.nextLine();
+    }
+
+    public void printGreeting() {
+        ArrayList<String> welcomeTextBlock = new ArrayList<>(List.of(
+                "Hello, my name is ",
+                " ____        _        ",
+                "|  _ \\ _   _| | _____ ",
+                "| | | | | | | |/ / _ \\",
+                "| |_| | |_| |   <  __/",
+                "|____/ \\__,_|_|\\_\\___|",
+                "How may I help you?"
+        ));
+
+        printWithWrapper(welcomeTextBlock, false, false);
+    }
+
+    public void printExit() {
+        printWithWrapper(new ArrayList<>(List.of("Bye bye! Hope to see you again soon!")), false, false);
+    }
+
+    private void printBorder(boolean isError) {
+        System.out.println((isError ? ERRORBORDER : BORDER));
+    }
+
+    public <T> void printWithWrapper(ArrayList<T> toPrint, boolean withNumbering, boolean isError) {
+        printBorder(isError);
+        for (int i = 0; i < toPrint.size(); i++) {
+            if (toPrint.get(i) == null) {
+                break;
+            }
+
+            String prepend = INDENT;
+            if (withNumbering) {
+                prepend += (i + 1) + ". ";
+            }
+            System.out.println(prepend + toPrint.get(i).toString());
+        }
+        printBorder(isError);
+    }
+}
