e27aa19eaefe694d6bd2dec413622544946f39f4
lll-jy <E0376997@u.nus.edu>
2020-08-22 20:10:37 +0800

    Add event happenToday and happenBeforeDate test

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -28,4 +28,29 @@ public class EventTest {
             fail();
         }
     }
+
+    @Test
+    public void happenTodayTest() {
+        try {
+            Event e1 = new Event("Project meeting", "2020-08-22 11:30");
+            assertEquals(true, e1.happenToday());
+
+            Event e2 = new Event("Project meeting", "2020-08-24 11:30");
+            assertEquals(false, e2.happenToday());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void happenBeforeDateTest() {
+        try {
+            Event e = new Event("Project meeting", "2020-09-03 11:30");
+            assertEquals(true, e.happenBeforeDate(LocalDate.parse("2020-09-05")));
+            assertEquals(false, e.happenBeforeDate(LocalDate.parse("2020-09-01")));
+            assertEquals(false, e.happenBeforeDate(LocalDate.parse("2020-09-03")));
+        } catch (Exception e) {
+            fail();
+        }
+    }
 }
