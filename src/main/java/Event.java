a819a0e08056c4059da15f93581dd40f3993a2d7
yanchenglee98 <yanchenglee@gmail.com>
2020-08-22 09:52:00 +0800

    Use LocalDate and LocalTime

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,27 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Event extends Task {
 
-    String date;
+    LocalDate date;
+    LocalTime startTime;
+    LocalTime endTime;
 
     public Event(String taskName, String date) {
         super(taskName);
-        this.date = date;
+        String[] arr = date.split(" ");
+        String eventDate = arr[0];
+        String[] eventTime = arr[1].split("-");
+        String eventStartTime = eventTime[0];
+        String eventEndTime = eventTime[1];
+        this.date = LocalDate.parse(eventDate);
+        this.startTime = LocalTime.parse(eventStartTime);
+        this.endTime = LocalTime.parse(eventEndTime);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + date + ")";
+        return "[E]" + super.toString() + "(at: " + date + " " + startTime + " - " + endTime + ")";
     }
 
 }
