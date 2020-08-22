3455b37ea1e15be08c900741fdaae8783b1009ba
boundtotheearth <farrellnah@gmail.com>
2020-08-22 15:51:20 +0800

    Extract UI Features
    User I/O is being handled in the main program
    
    Extracting user I/O to its own class will make
    it easier to manage
    
    Let's create anew UIManager class to handle
    user I/O

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,20 @@
 package main.java;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     static TaskList taskList;
     static DataManager dataManager = new DataManager();
+    static UiManager uiManager = new UiManager();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String logo =
-                " _\n" +
-                        "//\\\n" +
-                        "V  \\\n" +
-                        " \\  \\_\n" +
-                        "  \\,'.`-.\n" +
-                        "   |\\ `. `.\n" +
-                        "   ( \\  `. `-.                        _,.-:\\\n" +
-                        "    \\ \\   `.  `-._             __..--' ,-';/\n" +
-                        "     \\ `.   `-.   `-..___..---'   _.--' ,'/\n" +
-                        "      `. `.    `-._        __..--'    ,' /\n" +
-                        "        `. `-_     ``--..''       _.-' ,'\n" +
-                        "          `-_ `-.___        __,--'   ,'\n" +
-                        "             `-.__  `----\"\"\"    __.-'\n" +
-                        "                   `--..____..--'";
-
-        System.out.println(logo);
-        printInWindow("Hello, I'm a banana.\nWhat can I do for you?");
+        uiManager.displayStartMessage();
 
         try {
             taskList = dataManager.load();
 
-            while (sc.hasNextLine()) {
-                String input = sc.nextLine();
+            while (uiManager.hasUserInput()) {
+                String input = uiManager.getNextInput();
                 String[] splitInput = input.split(" ", 2);
 
                 CommandType command = CommandType.enumFromString(splitInput[0]);
@@ -44,7 +25,7 @@ public class Duke {
                 }
             }
         } catch (DukeException exception) {
-            printInWindow(exception.getMessage());
+            uiManager.printInWindow(exception.getMessage());
         }
     }
 
@@ -54,39 +35,39 @@ public class Duke {
             ToDo todo = new ToDo(parameters);
             taskList.addTask(todo);
             dataManager.save(taskList);
-            printInWindow("Added: " + todo.toString());
+            uiManager.printInWindow("Added: " + todo.toString());
             break;
         case EVENT:
             String[] eventParameters = parameters.split(" /at ");
             Event event = new Event(eventParameters[0], LocalDate.parse(eventParameters[1]));
             taskList.addTask(event);
             dataManager.save(taskList);
-            printInWindow("Added: " + event.toString());
+            uiManager.printInWindow("Added: " + event.toString());
             break;
         case DEADLINE:
             String[] deadlineParameters = parameters.split(" /by ");
             Deadline deadline = new Deadline(deadlineParameters[0], LocalDate.parse(deadlineParameters[1]));
             taskList.addTask(deadline);
             dataManager.save(taskList);
-            printInWindow("Added: " + deadline.toString());
+            uiManager.printInWindow("Added: " + deadline.toString());
             break;
         case DONE:
             int doneNumber = Integer.parseInt(parameters);
             taskList.updateDone(doneNumber);
             dataManager.save(taskList);
-            printInWindow("Nice! I've marked the this as done.\n" + taskList.getTask(doneNumber).toString());
+            uiManager.printInWindow("Nice! I've marked the this as done.\n" + taskList.getTask(doneNumber).toString());
             break;
         case LIST:
-            printInWindow(taskList.toString());
+            uiManager.printInWindow(taskList.toString());
             break;
         case DELETE:
             int deleteNumber = Integer.parseInt(parameters);
-            printInWindow("I've removed this task:\n" + taskList.getTask(deleteNumber).toString());
+            uiManager.printInWindow("I've removed this task:\n" + taskList.getTask(deleteNumber).toString());
             taskList.deleteTask(deleteNumber);
             dataManager.save(taskList);
             break;
         case BYE:
-            printInWindow("Bye. Hope to see you again soon!");
+            uiManager.printInWindow("Bye. Hope to see you again soon!");
             return 0;
         case UNDEFINED:
             throw new DukeException("I don't know what that means!");
@@ -94,12 +75,5 @@ public class Duke {
             throw new DukeException("This command has not yet been implemented!");
         }
         return 1;
-    }
-
-    public static void printInWindow(String text) {
-        String divider = "---------------------------------------------";
-        System.out.println(divider);
-        System.out.println(text);
-        System.out.println(divider);
     }
 }
---
+++ src/main/java/UiManager.java
@@ -0,0 +1,43 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class UiManager {
+    final String logo =
+            " _\n" +
+            "//\\\n" +
+            "V  \\\n" +
+            " \\  \\_\n" +
+            "  \\,'.`-.\n" +
+            "   |\\ `. `.\n" +
+            "   ( \\  `. `-.                        _,.-:\\\n" +
+            "    \\ \\   `.  `-._             __..--' ,-';/\n" +
+            "     \\ `.   `-.   `-..___..---'   _.--' ,'/\n" +
+            "      `. `.    `-._        __..--'    ,' /\n" +
+            "        `. `-_     ``--..''       _.-' ,'\n" +
+            "          `-_ `-.___        __,--'   ,'\n" +
+            "             `-.__  `----\"\"\"    __.-'\n" +
+            "                   `--..____..--'";
+
+    Scanner scanner = new Scanner(System.in);
+
+    public boolean hasUserInput() {
+        return scanner.hasNextLine();
+    }
+
+    public String getNextInput() {
+        return scanner.nextLine();
+    }
+
+    public void displayStartMessage() {
+        System.out.println(logo);
+        printInWindow("Hello, I'm a banana.\nWhat can I do for you?");
+    }
+
+    public void printInWindow(String text) {
+        String divider = "---------------------------------------------";
+        System.out.println(divider);
+        System.out.println(text);
+        System.out.println(divider);
+    }
+}
