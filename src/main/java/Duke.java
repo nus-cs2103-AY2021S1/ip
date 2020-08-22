48a69e1e1b14747c81273ac1a77ab5a9ec5cd116
Preston Toh <prestontoh@gmail.com>
2020-08-23 00:43:31 +0800

    Improve date support for Level-8

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -100,7 +100,7 @@ public class Duke {
             try {
 
                 String[] deadlineSplit = valArr[1].split("/by");
-                Deadline newTask = new Deadline(deadlineSplit[0], deadlineSplit[1]);
+                Deadline newTask = new Deadline(deadlineSplit[0], deadlineSplit[1].strip());
                 tasks.add(newTask);
                 System.out.println("Got it~ I've added this task:\n"
                         + newTask + "\n" + "You now have " + tasks.size() + " tasks in the list~");
@@ -116,7 +116,7 @@ public class Duke {
         } else {
             try {
                 String[] eventSplit = valArr[1].split("/at");
-                Event newTask = new Event(eventSplit[0], eventSplit[1]);
+                Event newTask = new Event(eventSplit[0], eventSplit[1].strip());
                 tasks.add(newTask);
                 System.out.println("Got it~ I've added this task:\n"
                         + newTask + "\n" + "You now have " + tasks.size() + " tasks in the list~");
