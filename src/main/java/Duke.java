6522d4d5b97c7a0a8f8007ef7462433e4a1e5ec9
Sean Tay <seantaysl@gmail.com>
2020-08-22 22:56:56 +0800

    Fix naming

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
 public class Duke {
-    private Storage storage;
-    private TaskList taskList;
-    private Ui ui;
-
+    private final Storage storage;
+    private final TaskList taskList;
+    private final Ui ui;
 
     public Duke(String name) {
         this.ui = new Ui();
         this.taskList = new TaskList();
         this.storage = new Storage(taskList, name);
     }
+
     public void run() {
         TaskList taskList = this.taskList;
         Scanner scan = new Scanner(System.in);
         Storage storage = this.storage;
-        String border = ui.getBorder();
+        String border = Ui.getBorder();
+
         ui.startUp(taskList, storage);
+
         while (scan.hasNext()) {
             String test = scan.next();
             if (ui.checkBye(test)) {
@@ -24,25 +26,25 @@ public class Duke {
                 break;
             } else {
                 String next = scan.nextLine().replaceFirst(" ", "");
-                if (TaskList.checkList(test)) {
-                    TaskList.displayList();
-                } else if (TaskList.checkDone(test)) {
+                if (taskList.checkList(test)) {
+                    taskList.displayList();
+                } else if (taskList.checkDone(test)) {
                     try {
-                        TaskList.doneTask(next);
+                        taskList.doneTask(next);
                         storage.updateFile();
                     } catch (DukeException e) {
                         System.out.println(border + e.getMessage() + "\n" + border);
                     }
-                } else if (TaskList.checkDel(test)) {
+                } else if (taskList.checkDel(test)) {
                     try {
-                        TaskList.delTask(next);
+                        taskList.delTask(next);
                         storage.updateFile();
                     } catch (DukeException e) {
                         System.out.println(border + e.getMessage() + "\n" + border);
                     }
                 } else {
                     try {
-                        TaskList.addTask(test, next);
+                        taskList.addTask(test, next);
                         storage.updateFile();
                     } catch (DukeException e) {
                         System.out.println(border + e.getMessage() + "\n" + border);
@@ -52,6 +54,7 @@ public class Duke {
         }
         scan.close();
     }
+
     public static void main(String[] args) {
         new Duke("duke").run();
     }
