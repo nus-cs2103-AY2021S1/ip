85a0e740382032ede0715853f74a666282217e8c
Dora Heng <e0425696@u.nus.edu>
2020-08-22 21:49:13 +0800

    Edit Parser.java

--- src/main/java/duke/Parser/Parser.java
+++ src/main/java/duke/Parser/Parser.java
@@ -6,6 +6,7 @@ import duke.Command.ExitCommand;
 import duke.Command.ListCommand;
 import duke.Command.DeleteCommand;
 import duke.Command.AddCommand;
+import duke.Command.FindCommand;
 
 public class Parser {
 
@@ -26,6 +27,9 @@ public class Parser {
         } else if (str.contains("delete")) {
             int taskIndex = Integer.parseInt(str.split("\\s+")[1]);
             return new DeleteCommand(taskIndex);
+        } else if (str.contains("find")) {
+            String keyword = str.substring(5).trim();
+            return new FindCommand(keyword);
         } else {
             return new AddCommand(str);
         }
