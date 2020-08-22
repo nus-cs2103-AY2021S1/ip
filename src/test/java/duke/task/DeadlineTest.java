bc9e09bee45f4b1cf662c48c8955cacfc682bf5d
lll-jy <E0376997@u.nus.edu>
2020-08-22 20:49:04 +0800

    Add deadline happenToday JUnit test

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -3,6 +3,8 @@ package duke.task;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -24,6 +26,23 @@ public class DeadlineTest {
             Deadline d = new Deadline("Assignment 1", "2020-09-01");
             assertEquals(true, d.happenOnDate(LocalDate.parse("2020-09-01")));
             assertEquals(false, d.happenOnDate(LocalDate.parse("2020-09-04")));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void happenTodayTest() {
+        try {
+            LocalDate today = LocalDate.now();
+            String td = today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            Deadline d1 = new Deadline("Assignment 1", td);
+            assertEquals(true, d1.happenToday());
+
+            LocalDate newDate = LocalDate.now().plusDays(2);
+            String newStr = newDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            Deadline d2 = new Deadline("Assignment 1", newStr);
+            assertEquals(false, d2.happenToday());
         } catch (Exception e) {
             fail();
         }
