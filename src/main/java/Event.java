058aacee35271b599881c1441eeda194c1ae602f
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-08-22 21:52:18 +0800

    Add TimedTask abstract class
    Deadline and Event both have common behaviours (attributes and methods),
    causing code duplication.
    
    By extracting the common behaviours into an abstract parent class
    reduces code duplication and allows for use in polymorphism (e.g. to get
    a collection of tasks for a specific date).
    
    Let's combine the common attributes into an abstract parent class called
    TimedTask from which Event and Deadline will inherit from.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,22 +1,11 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-public class Event extends Task {
-    private LocalDateTime datetime;
-    private static final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy kk:mm");
-    private static final DateTimeFormatter printFormatter = DateTimeFormatter.ofPattern("d MMM yyyy hh.mma");
+public class Event extends TimedTask {
 
     public Event(String description, String datetime) {
-        super(description);
-        this.datetime = LocalDateTime.parse(datetime, Event.inputFormatter);
-    }
-
-    private String datetimeString() {
-        return "(at: " + this.datetime.format(Event.printFormatter) + ")";
+        super(description, datetime);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " " + this.datetimeString();
+        return "[E]" + super.toString() + " (at: " + super.datetimeString() + ")";
     }
 }
