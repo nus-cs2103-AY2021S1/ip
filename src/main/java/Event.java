9c4abe1b3a0c044aee8ce0ac247a974ef7fef1af
rizhaow <rizhaow@gmail.com>
2020-08-22 17:13:38 +0800

    feat(level8): Store actual date object in Event and Deadline class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,16 @@
-public class Event extends Task {
-    private String date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String name, String date) {
+public class Event extends Task {
+    private LocalDate date;
+
+    public Event(String name, LocalDate date) {
         super(name, "[E]");
         this.date = date;
     }
 
     @Override
     public String toString() {
-        return String.format("%s (at: %s)", super.toString(), this.date);
+        return String.format("%s (at: %s)", super.toString(), this.date.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
