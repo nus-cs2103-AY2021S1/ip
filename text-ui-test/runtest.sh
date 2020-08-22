bbdf3014e2af64744ac0c52a8be8bf7345277c1e
sebastiantoh <sebas.tsj.98@gmail.com>
2020-08-22 11:14:48 +0800

    Update build command in runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -18,11 +18,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if
-  ! (
-    find ../src/main/java -name "*.java" >sources.txt
-    javac -cp ../src -Xlint:none -d ../bin @sources.txt
-  )
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
