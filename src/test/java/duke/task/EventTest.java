e38cb4e6d0773f37cba43d27560fe47ce49635a0
lll-jy <E0376997@u.nus.edu>
2020-08-22 20:03:03 +0800

    Create EventTest

---
+++ src/test/java/duke/task/EventTest.java
@@ -0,0 +1,21 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class EventTest {
+    @Test
+    public void happenOnDateTest() {
+        try {
+            Event e = new Event("Project meeting", "2020-09-03 11:30");
+            assertEquals(true, e.happenOnDate(LocalDate.parse("2020-09-03")));
+            assertEquals(false, e.happenOnDate(LocalDate.parse("2020-09-04")));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+}
