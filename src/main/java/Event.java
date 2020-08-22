6166110c70dd8453329709623392bc2b7031e245
joshualiangxy <joshualiang.xy@gmail.com>
2020-08-22 18:04:25 +0800

    Add proper support for datetime and update tests

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,19 @@
-public class Event extends Task {
-    private final String time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String name, String time) {
+public class Event extends Task {
+    private final LocalDateTime time;
+    private final DateTimeFormatter formatter;
+
+    public Event(String name, LocalDateTime time) {
         super(name);
         this.time = time;
+        formatter = DateTimeFormatter.ofPattern("EEEE, dd MMM yyyy, h:mma");
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (at: %s)", super.toString(), time);
+        return String.format("[E]%s (at: %s)", super.toString(),
+                time.format(formatter));
     }
 }
