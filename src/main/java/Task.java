705222daea9eb484d9365c8813f0ae96f368ab02
kelvinvin <kelvinw0ng@hotmail.com>
2020-08-22 15:25:34 +0800

    Change symbols for a Task's status from Tick/Cross to 0/1

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,7 +10,7 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "Tick" : "Cross"); //return tick or X symbols
+        return (isDone ? "1" : "0"); //return tick or X symbols
     }
 
     public void markAsDone() {
