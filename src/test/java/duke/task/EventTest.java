d4a768ebd8bc4df664a22320409ce472bb1ce72f
lll-jy <E0376997@u.nus.edu>
2020-08-22 20:05:35 +0800

    Test creating new Event

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -9,6 +9,16 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class EventTest {
     @Test
+    public void newEventTest() {
+        try {
+            Event e = new Event("Project meeting", "2020-09-03 11");
+            fail();
+        } catch (Exception e) {
+            assertEquals("Invalid input datetime, please input as yyyy-MM-dd HH:mm.", e.getMessage());
+        }
+    }
+
+    @Test
     public void happenOnDateTest() {
         try {
             Event e = new Event("Project meeting", "2020-09-03 11:30");
