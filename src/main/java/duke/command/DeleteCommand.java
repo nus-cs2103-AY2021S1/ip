4d93ad93ad7015411e0f6b5969bc54d5337ec764
lll-jy <E0376997@u.nus.edu>
2020-08-22 14:57:20 +0800

    Use size() method instead of count variable in TaskList

--- src/main/java/duke/command/DeleteCommand.java
+++ src/main/java/duke/command/DeleteCommand.java
@@ -10,13 +10,12 @@ public class DeleteCommand extends Command {
 
     @Override
     public void execute(Ui ui, TaskList list, Storage storage) throws InvalidCommandException {
-        int count = list.count;
+        int count = list.size();
         int m = Parser.isValidDelete(input, count) - 1;
         Task toDelete = list.get(m);
         list.remove(toDelete);
         storage.deleteTask(list);
         ui.output("Noted. I've removed this task:\n\t    " + toDelete +
                 "\n\t  Now you have " + list.size());
-        list.count--;
     }
 }
--- src/main/java/duke/command/DoneCommand.java
+++ src/main/java/duke/command/DoneCommand.java
@@ -9,7 +9,7 @@ public class DoneCommand extends Command {
 
     @Override
     public void execute(Ui ui, TaskList list, Storage storage) throws InvalidCommandException {
-        int n = Parser.isValidDone(input, list.count) - 1;
+        int n = Parser.isValidDone(input, list.size()) - 1;
         list.get(n).markAsDone();
         ui.output("Nice! I've marked this task as done:\n\t    " + list.get(n));
     }
