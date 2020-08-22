d98605517027dd839788bc6de7755a3e571885c7
yanchenglee98 <yanchenglee@gmail.com>
2020-08-22 10:16:17 +0800

    Integrate level 7 and 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -5,17 +5,17 @@ public class Deadline extends Task {
     LocalDate date;
 
     public Deadline(String taskName, String date) {
-        super(taskName);
+        super(taskName.stripTrailing());
         this.date = LocalDate.parse(date);
     }
 
     @Override
     public String getInfo() {
-        return String.format("D | %d | %s | %s\n", isDone ? 1 : 0, taskName, date);
+        return String.format("D | %d | %s | %s\n", isDone ? 1 : 0, taskName, date.toString());
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + date + ")";
+        return "[D]" + super.toString() + " (by: " + date + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,7 +8,7 @@ public class Event extends Task {
     LocalTime endTime;
 
     public Event(String taskName, String date) {
-        super(taskName);
+        super(taskName.stripTrailing());
         String[] arr = date.split(" ");
         String eventDate = arr[0];
         String[] eventTime = arr[1].split("-");
@@ -21,7 +21,7 @@ public class Event extends Task {
 
     @Override
     public String getInfo() {
-        return String.format("E | %d | %s | %s\n", isDone ? 1 : 0, taskName, date);
+        return String.format("E | %d | %s | %s %s-%s\n", isDone ? 1 : 0, taskName, date.toString(), startTime.toString(), endTime.toString());
     }
 
     @Override
