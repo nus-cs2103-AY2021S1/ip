e575624e46d4ce793d28aaea8a24d58daf83900c
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-08-22 21:14:31 +0800

    Add DateTime attributes to Event and Deadline

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String datetime;
+    private LocalDateTime datetime;
+    private static final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy kk:mm");
+    private static final DateTimeFormatter printFormatter = DateTimeFormatter.ofPattern("d MMM yyyy hh.mma");
 
     public Event(String description, String datetime) {
         super(description);
-        this.datetime = datetime;
+        this.datetime = LocalDateTime.parse(datetime, Event.inputFormatter);
+    }
+
+    private String datetimeString() {
+        return "(at: " + this.datetime.format(Event.printFormatter) + ")";
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + datetime + ")";
+        return "[E]" + super.toString() + " " + this.datetimeString();
     }
 }
