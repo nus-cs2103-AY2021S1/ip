6f4e72868bb29a0e795b17d90f34db3c2d23cbd7
pr4aveen <ajjuu98@gmail.com>
2020-08-22 15:19:47 +0800

    Add ability to parse date and time
    Program saves event and deadline task's date and time as a string. This
    prevents the program from performing more advanced operations on the
    tasks such as searching by date.
    
    Let's allow the program to parse and store the date and time of events
    and deadlines using the yyyy-MM-dd[ HH[:]mm] format, where the square
    brackets represent optional parameters.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,13 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
 
-    private String deadline;
+    private LocalDate deadlineDate;
+    private LocalTime deadlineTime;
 
     public static Deadline createNewDeadline(String argument) throws DukeException {
         String[] deadlineArguments = argument.split(" /by ");
@@ -14,22 +21,51 @@ public class Deadline extends Task {
             throw new DukeException("Deadline name cannot be blank!");
         }
 
-        String deadlineTime = deadlineArguments[1];
-        if (deadlineTime.isBlank()) {
+        String deadlineDateTime = deadlineArguments[1];
+        if (deadlineDateTime.isBlank()) {
             throw new DukeException("Deadline time cannot be blank!");
         }
 
-        return new Deadline(deadlineName, deadlineTime);
+        String[] datetime = deadlineDateTime.split(" ");
+
+        LocalDate deadlineDate;
+        LocalTime deadlineTime = null;
+
+        try {
+            deadlineDate = LocalDate.parse(datetime[0]);
+
+            if (datetime.length == 2) {
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH[:]mm");
+                deadlineTime = LocalTime.parse(datetime[1], dtf);
+            }
+
+        } catch (DateTimeParseException e) {
+            throw new DukeException("DateTime format is invalid.");
+        }
+
+        return new Deadline(deadlineName, deadlineDate, deadlineTime);
 
     }
 
-    private Deadline(String taskName, String deadline) {
+    private Deadline(String taskName, LocalDate deadlineDate, LocalTime deadlineTime) {
         super(taskName);
-        this.deadline = deadline;
+        this.deadlineDate = deadlineDate;
+        this.deadlineTime = deadlineTime;
+    }
+
+    private String printDateTime() {
+
+        String output = deadlineDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+
+        if (deadlineTime != null) {
+            output += ", " + deadlineTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
+        }
+
+        return output;
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), deadline);
+        return String.format("[D]%s (by: %s)", super.toString(), printDateTime());
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,6 +1,13 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+
 public class Event extends Task {
 
-    private String startTime;
+    private LocalDate startDate;
+    private LocalTime startTime;
 
     public static Event createNewEvent(String argument) throws DukeException {
         String[] eventArguments = argument.split(" /at ");
@@ -14,21 +21,50 @@ public class Event extends Task {
             throw new DukeException("Event name cannot be blank!");
         }
 
-        String startTime = eventArguments[1];
-        if (startTime.isBlank()) {
+        String startDateTime = eventArguments[1];
+        if (startDateTime.isBlank()) {
             throw new DukeException("Event time cannot be blank!");
         }
+        
+        String[] datetime = startDateTime.split(" ");
+        
+        LocalDate startDate;
+        LocalTime startTime = null;
+        
+        try {
+            startDate = LocalDate.parse(datetime[0]);
 
-        return new Event(eventName, startTime);
+            if (datetime.length == 2) {
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH[:]mm");
+                startTime = LocalTime.parse(datetime[1], dtf);
+            }
+            
+        } catch (DateTimeParseException e) {
+            throw new DukeException("DateTime format is invalid.");
+        }
+
+        return new Event(eventName, startDate, startTime);
+    }
+    
+    private String printDateTime() {
+        
+        String output = startDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        
+        if (startTime != null) {
+            output += ", " + startTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
+        }
+        
+        return output;
     }
 
-    private Event(String taskName, String startTime) {
+    private Event(String taskName, LocalDate startDate, LocalTime startTime) {
         super(taskName);
+        this.startDate = startDate;
         this.startTime = startTime;
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (at: %s)", super.toString(), startTime);
+        return String.format("[E]%s (at: %s)", super.toString(), printDateTime());
     }
 }
