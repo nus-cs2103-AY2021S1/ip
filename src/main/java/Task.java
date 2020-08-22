0d732b52e5687b4bf874168168bd87078a240197
limkoonkiat <lim.koonkiat42@gmail.com>
2020-08-22 23:36:37 +0800

    Add dates of the format yyyy-mm-dd to deadlines / events and add view yyyy-mm-dd command to print deadlines/events occurring on a specific date

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -15,6 +15,10 @@ public class Task {
         this.isDone = true;
     }
 
+    public String getDescription() {
+        return this.description;
+    }
+
     @Override
     public String toString() {
         return this.getStatusIcon() + " " + this.description;
