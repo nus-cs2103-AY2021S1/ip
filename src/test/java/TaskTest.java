de222ed073804f854f84e933b16bce4bc72ac4f0
sogggy <songyu98@gmail.com>
2020-08-23 11:20:31 +0800

    Add dummy JUnit test

---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TaskTest {
+
+    //test that date is reformatted properly
+    @Test
+    public void testReformatDate() {
+        String res = "";
+    }
+}
