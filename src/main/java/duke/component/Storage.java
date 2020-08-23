29337baa0fb64b127ce6628c924aff44d0d709ce
lll-jy <E0376997@u.nus.edu>
2020-08-23 16:40:05 +0800

    Add JavaDoc for addToList method of Storage

--- src/main/java/duke/component/Storage.java
+++ src/main/java/duke/component/Storage.java
@@ -10,6 +10,11 @@ public interface Storage {
      */
     TaskList getList();
 
+    /**
+     * Writes the task to the storage file.
+     * @param task the task that is to be written in the task
+     * @throws InvalidCommandException should never been thrown unless the file path is not working
+     */
     void addToList(Task task) throws InvalidCommandException;
 
     void reWrite(TaskList list) throws InvalidCommandException;
