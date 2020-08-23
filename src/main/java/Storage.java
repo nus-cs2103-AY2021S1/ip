6b74578a8d09f9ea31f887c566f0995c34be6bf8
SherryWu178 <sherry.wu178@gmail.com>
2020-08-23 22:34:32 +0800

    Fix data.txt not found situation

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -5,7 +5,6 @@ import java.nio.file.Paths;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class Storage {
     private final String filePath = "/data.txt";
@@ -17,30 +16,32 @@ public class Storage {
         try {
             ArrayList<Task> tasks = new ArrayList<>();
             String cwd = System.getProperty("user.dir");
-            List<String> allLines = Files.readAllLines(Paths.get(cwd + filePath));
-            for (String line : allLines) {
-                String[] info = line.split(" \\| ");
+            if (Files.exists(Paths.get(cwd + filePath))) {
+                List<String> allLines = Files.readAllLines(Paths.get(cwd + filePath));
+                for (String line : allLines) {
+                    String[] info = line.split(" \\| ");
 
-                String type = info[0];
-                int complete = Integer.parseInt(info[1]);
-                String title = info[2];
-                switch(type) {
-                    case "T":
-                        Todo newTodo = new Todo(title, complete);
-                        tasks.add(newTodo);
-                        break;
-                    case "D":
-                        String deadline = info[3];
-                        LocalDate deadlineInLocalDate = LocalDate.parse(deadline);
-                        Deadline newDeadline = new Deadline(title, complete, deadlineInLocalDate);
-                        tasks.add(newDeadline);
-                        break;
-                    case "E":
-                        String time = info[3];
-                        LocalDate timeInLocalDate = LocalDate.parse(time);
-                        Event newEvent = new Event(title, complete, timeInLocalDate);
-                        tasks.add(newEvent);
-                        break;
+                    String type = info[0];
+                    int complete = Integer.parseInt(info[1]);
+                    String title = info[2];
+                    switch(type) {
+                        case "T":
+                            Todo newTodo = new Todo(title, complete);
+                            tasks.add(newTodo);
+                            break;
+                        case "D":
+                            String deadline = info[3];
+                            LocalDate deadlineInLocalDate = LocalDate.parse(deadline);
+                            Deadline newDeadline = new Deadline(title, complete, deadlineInLocalDate);
+                            tasks.add(newDeadline);
+                            break;
+                        case "E":
+                            String time = info[3];
+                            LocalDate timeInLocalDate = LocalDate.parse(time);
+                            Event newEvent = new Event(title, complete, timeInLocalDate);
+                            tasks.add(newEvent);
+                            break;
+                    }
                 }
             }
             return tasks;
