ff15262a393727368495be3c0858fd0c91a6a33e
lll-jy <E0376997@u.nus.edu>
2020-08-23 18:50:38 +0800

    Add isExit JUnit test for FindCommand

--- src/test/java/duke/command/FindCommandTest.java
+++ src/test/java/duke/command/FindCommandTest.java
@@ -1,5 +1,13 @@
 package duke.command;
 
-public class FindCommandTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class FindCommandTest {
+    @Test
+    public void isExit__alwaysFalse() {
+        assertFalse(new FindCommand("find this").isExit());
+        assertFalse(new FindCommand("find ").isExit());
+    }
 }
