6df36a15103e70f3f1f1ca12f400b8dc3ee52c67
Royxuzeng <e0376917@u.nus.edu>
2020-08-23 23:43:01 +0800

    Add level 8
    Accept dates in format yyyy-mm-dd and store them in LocalDate and print the dates in the format of MMM dd yyyy.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -76,11 +77,54 @@ public class Duke {
                     if (type.equals("deadline")) {
                         String description = string1.substring(string1.indexOf(' ') + 1, string1.indexOf('/') - 1);
                         String by = string1.substring(string1.indexOf("/by") + 4);
-                        lists.add(new Deadline(description, by));
+
+                        String s = "";
+
+                        if (by.charAt(0) >= '0' && by.charAt(0) <= '9') {
+                            if (by.charAt(1) == '/') {
+                                s = s + by.substring(5, 9);
+                                s = s + '-';
+                                s = s + by.substring(2, 4);
+                                s = s + '-';
+                                s = s + '0' + by.substring(0,1);
+                            } else {
+                                s = s + by.substring(6, 10);
+                                s = s + '-';
+                                s = s + by.substring(3, 5);
+                                s = s + '-';
+                                s = s + '0' + by.substring(0, 2);
+                            }
+                            LocalDate date = LocalDate.parse(s);
+                            lists.add(new Deadline(description, by, date));
+                        } else {
+                            lists.add(new Deadline(description, by));
+                        }
+
                     } else if (type.equals("event")) {
                         String time = string1.substring(string1.indexOf("/at") + 4);
                         String description = string1.substring(string1.indexOf(' ') + 1, string1.indexOf('/') - 1);
-                        lists.add(new Event(description, time));
+
+                        String s = "";
+
+                        if (time.charAt(0) >= '0' && time.charAt(0) <= '9') {
+                            if (time.charAt(1) == '/') {
+                                s = s + time.substring(5, 9);
+                                s = s + '-';
+                                s = s + time.substring(2, 4);
+                                s = s + '-';
+                                s = s + '0' + time.substring(0,1);
+                            } else {
+                                s = s + time.substring(6, 10);
+                                s = s + '-';
+                                s = s + time.substring(3, 5);
+                                s = s + '-';
+                                s = s + '0' + time.substring(0, 2);
+                            }
+                            LocalDate date = LocalDate.parse(s);
+                            lists.add(new Event(description, time, date));
+                        } else {
+                            lists.add(new Event(description, time));
+                        }
                     } else if (type.equals("todo")) {
                         String description = string1.substring(string1.indexOf(' ') + 1);
                         lists.add(new ToDo(description));
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,29 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
     protected String time;
+    protected LocalDate date;
 
     public Event(String description, String time) {
         super(description);
         this.time = time;
+        this.date = null;
     }
+
+    public Event(String description, String time, LocalDate date) {
+        super(description);
+        this.time = time;
+        this.date = date;
+    }
+
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + time + ")";
+        if (date == null) {
+            return "[E]" + super.toString() + " (at: " + time + ")";
+        } else {
+            return "[E]" + super.toString() + " (at: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+        }
     }
 }
