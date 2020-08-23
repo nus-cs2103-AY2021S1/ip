161e45720c81d498957c89ecab1e66f19bb5a109
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2020-08-23 23:39:11 +0800

    Replace datetime string with actual datetime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,25 @@
-public class Deadline extends Task {
-    String datetimeDue;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Deadline(String content, String datetimeDue) throws DukeException {
+public class Deadline extends Task {
+    LocalDateTime datetimeDue;
+    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public Deadline(String content, String datetimeDueString) throws DukeException {
         super(content);
-        this.datetimeDue = datetimeDue;
+        if (datetimeDueString.replace(" ", "").equals("")) {
+            throw new DukeException("Event datetime cannot be empty.");
+        }
+        try {
+            this.datetimeDue = LocalDateTime.parse(datetimeDueString, formatter);
+        } catch (DateTimeParseException exception) {
+            throw new DukeException("Invalid datetime specified (Needs to be yyyy-MM-dd HH:mm).");
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), datetimeDue);
+        return String.format("[D]%s (by: %s)", super.toString(), datetimeDue.format(formatter));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,26 @@
-public class Event extends Task {
-    String datetime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Event(String content, String datetime) throws DukeException{
+public class Event extends Task {
+    LocalDateTime datetime;
+    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public Event(String content, String datetimeString) throws DukeException{
         super(content);
-        if (datetime.replace(" ", "").equals("")) {
+        if (datetimeString.replace(" ", "").equals("")) {
             throw new DukeException("Event datetime cannot be empty.");
         }
-        this.datetime = datetime;
+        try {
+            this.datetime = LocalDateTime.parse(datetimeString, formatter);
+        } catch (DateTimeParseException exception) {
+            throw new DukeException("Invalid datetime specified (Needs to be yyyy-MM-dd HH:mm).");
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (at: %s)", super.toString(), datetime);
+
+        return String.format("[E]%s (at: %s)", super.toString(), datetime.format(formatter));
     }
 }
