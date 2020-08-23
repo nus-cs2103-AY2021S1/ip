ab142c95d02b42a2d09080df66ced2f49ef60931
sgn00 <37658877+sgn00@users.noreply.github.com>
2020-08-23 16:46:35 +0800

    Add LocalDate implementation

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    private String date;
+    private LocalDate date;
 
-    public Deadline(String name, String date) {
+    public Deadline(String name, LocalDate date) {
         super(name);
         this.date = date;
     }
 
     @Override
     public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        String date = formatter.format(this.date);
         return String.format("[D]%s (by: %s)", super.toString(), date);
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    private String date;
+    private LocalDate date;
 
-    public Event(String name, String date) {
+    public Event(String name, LocalDate date) {
         super(name);
         this.date = date;
     }
 
     @Override
     public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        String date = formatter.format(this.date);
         return String.format("[E]%s (at: %s)", super.toString(), date);
     }
 }
