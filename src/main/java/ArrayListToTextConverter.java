8b138d37caebd47c6c5a9e8e5a57926bceb10859
moyj01 <50520213+moyj01@users.noreply.github.com>
2020-08-23 18:51:01 +0800

    Add save to disc and read from disc functionality

---
+++ src/main/java/ArrayListToTextConverter.java
@@ -0,0 +1,20 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+public class ArrayListToTextConverter {
+
+    public static void convertArrayListToText(List<Task> tasks) {
+        try {
+            FileOutputStream writeData = new FileOutputStream("./taskdata.txt");
+            ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
+            writeStream.writeObject(tasks);
+            writeStream.flush();
+            writeStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,15 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static void main(String[] args) throws DukeException {
+    public static void main(String[] args) throws DukeException, IOException {
         String logo = "Duke Melvin";
-//
-//                " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo + "\n" + "What can I do for you?");
 
-        List<Task> ls = new ArrayList<>();
+        List<Task> ls = TextToArrayListConverter.readFile();
         String line = "";
         Scanner in = new Scanner(System.in);
 
@@ -25,13 +20,12 @@ public class Duke {
                 switch (line) {
                     case "bye":
                         System.out.println("Bye. Hope to see you again soon!");
+                        ArrayListToTextConverter.convertArrayListToText(ls);
                         break;
                     case "list":
-                        System.out.println("____________________________________________________________");
                         for (int x = 0; x < ls.size(); x++) {
                             System.out.println(x + 1 + ":" + ls.get(x).toString());
                         }
-                        System.out.println("____________________________________________________________");
                         break;
                     default:
                         switch (firstArg) {
@@ -41,29 +35,30 @@ public class Duke {
                                     throw new InvalidDoneCommandException();
                                 } else {
                                     int itemToBeMarkedAsDone = Integer.parseInt(doneCommandArray[1]);
-                                    if (itemToBeMarkedAsDone > ls.size() || itemToBeMarkedAsDone <=0) {
+                                    if (itemToBeMarkedAsDone > ls.size() || itemToBeMarkedAsDone <= 0) {
                                         throw new InvalidDoneCommandException();
                                     } else {
                                         ls.get(itemToBeMarkedAsDone - 1).markAsDone();
+                                        ArrayListToTextConverter.convertArrayListToText(ls);
                                     }
                                 }
                                 break;
                             case "delete":
                                 String[] deleteCommandArray = line.split(" ");
-                                if(deleteCommandArray.length <2){
+                                if (deleteCommandArray.length < 2) {
                                     throw new InvalidDeleteCommandException();
-                                }else{
+                                } else {
                                     int itemToBeDeleted = Integer.parseInt(deleteCommandArray[1]);
-                                    if(itemToBeDeleted > ls.size() || itemToBeDeleted <=0){
+                                    if (itemToBeDeleted > ls.size() || itemToBeDeleted <= 0) {
                                         throw new InvalidDeleteCommandException();
-                                    }else{
-                                        Task item = ls.get(itemToBeDeleted-1);
-                                        ls.remove(itemToBeDeleted-1);
-                                        System.out.println("____________________________________________________________");
+                                    } else {
+                                        Task item = ls.get(itemToBeDeleted - 1);
+                                        ls.remove(itemToBeDeleted - 1);
                                         System.out.println("Noted. I've removed this task:");
                                         System.out.println(item.toString());
                                         System.out.format("Now you have %d tasks in the list\n", ls.size());
-                                        System.out.println("____________________________________________________________");
+
+                                        ArrayListToTextConverter.convertArrayListToText(ls);
 
                                     }
                                 }
@@ -78,11 +73,11 @@ public class Duke {
                                     String todoString = line.substring(5);
                                     ToDo todo = new ToDo(todoString);
                                     ls.add(todo);
-                                    System.out.println("____________________________________________________________");
                                     System.out.println("Got it. I've added this task:");
                                     System.out.println(todo.toString());
                                     System.out.format("Now you have %d tasks in the list\n", ls.size());
-                                    System.out.println("____________________________________________________________");
+                                    ArrayListToTextConverter.convertArrayListToText(ls);
+
                                     break;
                                 }
 
@@ -93,11 +88,11 @@ public class Duke {
                                 String deadlineString = line.split("/")[0].substring(9);
                                 Deadline deadline = new Deadline(deadlineString, by2);
                                 ls.add(deadline);
-                                System.out.println("____________________________________________________________");
                                 System.out.println(" Got it. I've added this task:");
                                 System.out.println(deadline.toString());
                                 System.out.format("Now you have %d tasks in the list\n", ls.size());
-                                System.out.println("____________________________________________________________");
+                                ArrayListToTextConverter.convertArrayListToText(ls);
+
                                 break;
 
                             case "event":
@@ -107,11 +102,11 @@ public class Duke {
                                 String eventString = line.split("/")[0].substring(6);
                                 Event event = new Event(eventString, at2);
                                 ls.add(event);
-                                System.out.println("____________________________________________________________");
-                                System.out.println(" Got it. I've added this task:");
+                                System.out.println("Got it. I've added this task:");
                                 System.out.println(event.toString());
                                 System.out.format("Now you have %d tasks in the list\n", ls.size());
-                                System.out.println("____________________________________________________________");
+                                ArrayListToTextConverter.convertArrayListToText(ls);
+
                                 break;
 
                             default:
@@ -121,8 +116,11 @@ public class Duke {
                 }
             } catch (DukeException exception) {
                 System.out.println(exception.getMessage());
-            }
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
 
+
+            }
 
         }
     }
