2d4e39574c89308909991addf5a0a20eb476aae5
Sean Tay <seantaysl@gmail.com>
2020-08-23 20:38:40 +0800

    Fix styling issues for UI.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -9,30 +9,28 @@ public class Ui {
         storage.fileCheck();
         if (!storage.getFile().exists() || storage.getFile().length() == 0) {
             System.out.println(
-                    BORDER + "Hello! I'm Duke\n"
-                            + "What can I do for you?\n" + BORDER
+                BORDER + "Hello! I'm Duke\n"
+                    + "What can I do for you?\n" + BORDER
             );
         } else {
             System.out.println(
-                    BORDER + "Well come back!\n" + "You still have "
-                            + taskList.getList().size() + " tasks left to clear.\n" + BORDER
+                BORDER + "Well come back!\n" + "You still have "
+                    + taskList.getList().size() + " tasks left to clear.\n" + BORDER
             );
         }
     }
 
     public void addTaskLine(Task toAdd, int size) {
         System.out.println(
-                BORDER + "Got it. I've added this task:\n"
-                + "  " + toAdd + "\n"
-                + "Now you have " + size + " tasks in the list.\n"
-                + BORDER);
+            BORDER + "Got it. I've added this task:\n" + "  " + toAdd + "\n"
+                + "Now you have " + size + " tasks in the list.\n" + BORDER
+        );
     }
 
     public void removeTaskLine(Task toRemove, int size) {
         System.out.println(
-                BORDER + "Noted. I've removed this task:\n" + "  "
-                        + toRemove + "\n"
-                        + "Now you have " + size + " tasks in the list.\n" + BORDER
+                BORDER + "Noted. I've removed this task:\n" + "  " + toRemove + "\n"
+                    + "Now you have " + size + " tasks in the list.\n" + BORDER
         );
     }
 
