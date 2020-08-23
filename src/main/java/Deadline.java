1f69cdb32c6c2b2c5c36c69f214abe724641cdf2
jiaweiteo <jiawei.teo@hotmail.com>
2020-08-23 21:48:04 +0800

    Level-8: Dates and Times
    Implemented Java LocalDate for Duke to regconise dates. Store dates as a
    LocalDate object and print them in MMM dd yyyy format.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,21 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
+    protected LocalDate localDate;
     protected String due;
 
     public Deadline(String description, String due) {
         super(description);
+        LocalDate d = LocalDate.parse(due);
+        this.localDate = d;
+        String convertedDate = d.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        this.due = convertedDate;
+    }
+
+    public Deadline(String task, boolean isDone, String due) {
+        super(task, isDone);
         this.due = due;
     }
 
