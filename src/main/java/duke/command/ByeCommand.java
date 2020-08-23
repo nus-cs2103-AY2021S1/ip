3b398d2420bd5d6e5cb323466f502bbab2440565
lll-jy <E0376997@u.nus.edu>
2020-08-23 15:58:37 +0800

    Add JavaDoc for execute method in ByeCommand

--- src/main/java/duke/command/ByeCommand.java
+++ src/main/java/duke/command/ByeCommand.java
@@ -12,6 +12,13 @@ public class ByeCommand extends Command {
         return true;
     }
 
+    /**
+     * Executes the command, prints the current list on ui.
+     * @param ui The user interface object that is currently running.
+     * @param list The current list of tasks.
+     * @param storage The storage-writing object that is currently running.
+     * @return The string "bye".
+     */
     @Override
     public String execute(Ui ui, TaskList list, Storage storage) {
         ui.printList(list, t -> true, "");
