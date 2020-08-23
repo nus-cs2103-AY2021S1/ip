8a903eb93bbe5f249e428dfed48433bdaea44ec5
lll-jy <E0376997@u.nus.edu>
2020-08-23 10:33:29 +0800

    Add todo output JUnit test

--- src/test/java/duke/task/ToDoTest.java
+++ src/test/java/duke/task/ToDoTest.java
@@ -2,6 +2,7 @@ package duke.task;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ToDoTest {
@@ -9,6 +10,19 @@ public class ToDoTest {
     public void constructor_anyInput_noException() {
         try {
             ToDo t = new ToDo("return books");
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void outputTest() {
+        try {
+            ToDo t = new ToDo("return books");
+            assertEquals("T | 0 | return books\n", t.output());
+
+            t.markAsDone();
+            assertEquals("T | 1 | return books\n", t.output());
         } catch (Exception e) {
             fail();
         }
