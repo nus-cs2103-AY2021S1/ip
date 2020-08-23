618196cad204d56c3845efd1b5a1b9056aac3f82
Wincent <wincenttsjoi@gmail.com>
2020-08-23 20:04:05 +0800

    Add javadoc for all exceptions

--- src/main/java/exceptions/DukeException.java
+++ src/main/java/exceptions/DukeException.java
@@ -1,7 +1,15 @@
 package exceptions;
 
+/**
+ * If there is any error related to duke chatbot,
+ * <tt>DukeException</tt> will be thrown.
+ */
 public class DukeException extends Exception {
 
+    /**
+     * Same message will be thrown to user in DukeException.
+     * @param message message that will be thrown to user
+     */
     public DukeException(String message) {
         super(message);
     }
--- src/main/java/exceptions/DukeInvalidMessageException.java
+++ src/main/java/exceptions/DukeInvalidMessageException.java
@@ -1,7 +1,17 @@
 package exceptions;
 
+/**
+ * If a command entered does not have a valid description,
+ * <tt>DukeInvalidMessageException</tt> will be thrown.
+ */
 public class DukeInvalidMessageException extends DukeException {
 
+    /**
+     * Specific message that will be sent to user
+     * when description of command is invalid.
+     * An example would be putting done or delete description which
+     * does not exist in the list.
+     */
     public DukeInvalidMessageException() {
         super("Sorry Boss! The description that you put is invalid..");
     }
