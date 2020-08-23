1414cdae1c8efcc779ef79d95ded3fb78576e20b
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-08-23 13:26:01 +0800

    Convert Command to an abstract class

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,41 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
+public abstract class Command {
 
-public enum Command {
-    BYE("bye"),
-    LIST("list"),
-    DONE("done"),
-    DELETE("delete"),
-    DEADLINE("deadline"),
-    EVENT("event"),
-    TODO("todo"),
-    UNKNOWN("unknown");
+    abstract void execute(TaskList tasks, Ui ui, Storage storage);
 
-    private final String commandName;
-    private static Map<String, Command> commandMap;
+    abstract boolean isExit();
 
-    Command(String commandName) {
-        this.commandName = commandName;
-    }
-
-    static {
-        final Map<String, Command> strToCommand = new HashMap<>();
-        for (Command c : Command.values()) {
-            strToCommand.put(c.toString(), c);
-        }
-        Command.commandMap = strToCommand;
-    }
-
-    public static Command getCommand(String command) {
-        if(!commandMap.containsKey(command)) {
-            return Command.UNKNOWN;
-        } else {
-            return commandMap.get(command);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return this.commandName;
-    }
 }
