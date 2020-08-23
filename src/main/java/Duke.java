164a334ce9af1fb5c55a3d897b3962fd04bd9a38
amylzting <amylimzting@gmail.com>
2020-08-23 16:44:34 +0800

    Add ability for Dino to load task data
    Let's add the ability for Dino to:
    * create a directory data to store user data
    * create a file to store user task list
    * load data from user task list
    
    This helps to keep track of user's task data
    when they use Dino again.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -157,6 +159,77 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
+    public static void loadData(List<Task> tasks) throws DukeException {
+        //read Duke's data file and load tasks into tasks list
+        try {
+            File dir = new File("./data");
+            File taskFile = new File("./data/tasklist.txt");
+            if(dir.mkdir()) {
+                System.out.println("Welcome. Dino has created a new directory " +
+                        "to store your data.");
+                if(taskFile.createNewFile()) {
+                    System.out.println("Dino has successfully created a new file to store your task list.");
+                } else {
+                    System.out.println("Dino could not create a new file to store your task list.");
+                }
+            } else if(taskFile.createNewFile()) {
+                System.out.println("Welcome. Dino has successfully created " +
+                        "a new file to store your task list.");
+            } else {
+                Scanner scan = new Scanner(taskFile);
+                while (scan.hasNext()) {
+                    String taskTypeAndDone = scan.next();
+                    String taskDesc = scan.nextLine().substring(1);
+                    char taskType = taskTypeAndDone.charAt(1);
+                    char taskDone = taskTypeAndDone.charAt(4);
+                    switch (taskType) {
+                    case 'T':
+                        Todo todo = new Todo(taskDesc);
+                        if(taskDone == '✓') {
+                            todo.markAsDone();
+                        }
+                        tasks.add(todo);
+                        break;
+
+                    case 'D':
+                        String[] taskBy = taskDesc.split("by:");
+                        String task = taskBy[0].substring(0, taskBy[0].length() - 2);
+                        String by = taskBy[1].substring(1, taskBy[1].length() - 1);
+                        Deadline deadline = new Deadline(task, by);
+                        if(taskDone == '✓') {
+                            deadline.markAsDone();
+                        }
+                        tasks.add(deadline);
+                        break;
+
+                    case 'E':
+                        String[] eventAt = taskDesc.split("at:");
+                        String eventTask = eventAt[0].substring(0, eventAt[0].length() - 2);
+                        String at = eventAt[1].substring(1, eventAt[1].length() - 1);
+                        Event event = new Event(eventTask, at);
+                        if(taskDone == '✓') {
+                            event.markAsDone();
+                        }
+                        tasks.add(event);
+                        break;
+                    }
+                }
+                System.out.println("Welcome back. Dino has successfully loaded your task data.");
+            }
+        } catch(IOException e) {
+            throw new DukeException("Dino could not load your task data.");
+        }
+    }
+
+    public static void run(List<Task> tasks) {
+        try {
+            loadData(tasks);
+        } catch(DukeException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____\n"
                 + "|  _ \\ _ _____  ___\n"
@@ -175,6 +248,7 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         boolean isBye = false;
         List<Task> tasks = new ArrayList<>();
+        run(tasks);
 
         while (!isBye && scanner.hasNextLine()) {
             String input = scanner.nextLine();
