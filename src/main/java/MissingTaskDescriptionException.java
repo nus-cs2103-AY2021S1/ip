0fa0114299a8fd55ace3249966d962b71a4ca985
onglijin <onglijinlj@gmail.com>
2020-08-23 23:49:50 +0800

    Update Exception Files

--- src/main/java/MissingTaskDescriptionException.java
+++ src/main/java/MissingTaskDescriptionException.java
@@ -1,10 +1,6 @@
-public class MissingTaskDescriptionException extends Exception{
-    public MissingTaskDescriptionException() {
-        String horizontalLine = "_______________________________________________________";
-        System.out.println(horizontalLine
-                + "\r\n"
-                + "Oops! The description cannot be empty :("
-                + "\r\n"
-                + horizontalLine);
+public class MissingTaskDescriptionException extends Exception {
+    
+    public MissingTaskDescriptionException(String message) {
+        super(message);
     }
 }
