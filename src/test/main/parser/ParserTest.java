537c94c1825f60582f7e402b243c06487240052e
joshualiangxy <joshualiang.xy@gmail.com>
2020-08-24 01:53:44 +0800

    Configure jar file

--- src/test/main/parser/ParserTest.java
+++ src/test/main/parser/ParserTest.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParserTest {
-
     @Nested
     @DisplayName("bye command")
     class Bye {
