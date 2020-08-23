b3cebcf719338e261380275010838c7a618b858a
leeweiminsg <lee.wei.min@u.nus.edu>
2020-08-23 18:38:35 +0800

    Complete A-JUnit

---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void isTaskModificationF_done_isTaskModification(){
+        boolean isTaskModification = Parser.isTaskModification("done");
+
+        assertEquals(isTaskModification, true);
+    }
+
+    @Test
+    public void isTaskModificationF_delete_isTaskModification(){
+        boolean isTaskModification = Parser.isTaskModification("delete");
+
+        assertEquals(isTaskModification, true);
+    }
+
+    @Test
+    public void isTaskModificationF_todo_isNotTaskModification(){
+        boolean isTaskModification = Parser.isTaskModification("todo");
+
+        assertEquals(isTaskModification, false);
+    }
+}
