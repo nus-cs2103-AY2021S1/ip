514c44b06c7ed43f5fbf96c69b57bbd7a206e962
lll-jy <E0376997@u.nus.edu>
2020-08-23 12:42:24 +0800

    Add isExit JUnit test for ByeCommand

--- src/test/java/duke/command/ByeCommandTest.java
+++ src/test/java/duke/command/ByeCommandTest.java
@@ -1,4 +1,13 @@
 package duke.command;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ByeCommandTest {
+    @Test
+    public void isExit_alwaysTrue() {
+        ByeCommand b = new ByeCommand("bye");
+        assertTrue(b.isExit());
+    }
 }
