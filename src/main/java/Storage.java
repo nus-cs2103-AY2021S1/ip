0c00c73945f3d272bc14958df24c90e82346a265
Lee Ming De <leemingde@gmail.com>
2020-08-23 10:22:19 +0800

    Handle case for folder not existing

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -4,44 +4,54 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Storage {
 
+
     public static ArrayList<Task> getPreviousTask(ArrayList<Task> taskList) {
-        java.nio.file.Path path = java.nio.file.Paths.get("data").resolve("duke.txt");
+        java.nio.file.Path directory = java.nio.file.Paths.get("data");
         try {
-            File file = new File(path.toString());
-            Scanner sc = new Scanner(file);
-            while (sc.hasNext()) {
-                String[] task = sc.nextLine().split("\\|");
-                switch (task[0].trim()) {
-                case "T":
-                    ToDo todo = new ToDo(task[2].trim());
-                    if (task[1].equals("1")) {
-                        todo.doneTask();
+            if (!Files.exists(directory)) {
+                Files.createDirectory(directory);
+                java.nio.file.Path path = java.nio.file.Paths.get("data").resolve("duke.txt");
+                if (!Files.exists(path)) {
+                    File file = new File(path.toString());
+                    file.createNewFile();
+                }
+                File file = new File(path.toString());
+                Scanner sc = new Scanner(file);
+                while (sc.hasNext()) {
+                    String[] task = sc.nextLine().split("\\|");
+                    switch (task[0].trim()) {
+                    case "T":
+                        ToDo todo = new ToDo(task[2].trim());
+                        if (task[1].equals("1")) {
+                            todo.doneTask();
+                        }
+                        taskList.add(todo);
+                        break;
+                    case "D":
+                        Deadline deadline = new Deadline(task[2].trim(), task[3].trim());
+                        if (task[1].equals("1")) {
+                            deadline.doneTask();
+                        }
+                        taskList.add(deadline);
+                        break;
+                    case "E":
+                        Event event = new Event(task[2].trim(), task[3].trim());
+                        if (task[1].equals("1")) {
+                            event.doneTask();
+                        }
+                        taskList.add(event);
+                        break;
                     }
-                    taskList.add(todo);
-                    break;
-                case "D":
-                    Deadline deadline = new Deadline(task[2].trim(), task[3].trim());
-                    if (task[1].equals("1")) {
-                        deadline.doneTask();
-                    }
-                    taskList.add(deadline);
-                    break;
-                case "E":
-                    Event event = new Event(task[2].trim(), task[3].trim());
-                    if (task[1].equals("1")) {
-                        event.doneTask();
-                    }
-                    taskList.add(event);
-                    break;
                 }
             }
-        } catch (FileNotFoundException e) {
-            File file = new File(path.toString());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
         return taskList;
     }
