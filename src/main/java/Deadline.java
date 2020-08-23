321cdc052a562b0e7ea145f92a355dd82592a786
BreadPeanutButter <joonjie98@gmail.com>
2020-08-23 20:24:07 +0800

    Implement Level 8 (Deadline date)
    Accept deadline dates in dd-MM-yyyy format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,7 +1,10 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
 
     private final String task;
-    private final String deadline;
+    private final LocalDate deadline;
     private static final String errorMessage = "OOPS!!! The description of a deadline cannot be empty.\n";
 
     protected Deadline(String command) throws DukeException{
@@ -10,7 +13,8 @@ public class Deadline extends Task{
         } else {
             int index = command.indexOf("/by");
             this.task = command.substring(9, index - 1);
-            this.deadline = command.substring(index + 4);
+            this.deadline = LocalDate.parse(command.substring(index + 4),
+                    DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         }
     }
 
@@ -24,7 +28,7 @@ public class Deadline extends Task{
         return String.format("[D][%s] %s (by: %s)",
                 super.isDone() ? "\u2713" : "\u2717",
                 getTask(),
-                deadline);
+                deadline.format(DateTimeFormatter.ofPattern("d MMM yyyy")));
     }
 
 }
