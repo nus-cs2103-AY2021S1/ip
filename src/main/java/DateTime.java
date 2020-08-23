1872ca8fa48218f8045fa73c0501e90d1cb70231
junlong4321 <desmondwongjunlong@yahoo.com.sg>
2020-08-23 17:52:07 +0800

    Duke Level 8. Dates and Times

---
+++ src/main/java/DateTime.java
@@ -0,0 +1,35 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.Arrays;
+
+public class DateTime {
+
+    public static String formatDateTime(String input) {
+        // Make a copy of the input string
+        String result = input;
+        // Format the following date and time formats
+        List<String> formatStrings = Arrays.asList(
+                "dd/MM/yyyy HHmm", "d/MM/yyyy HHmm",
+                "dd-MM-yyyy HHmm", "d-MM-yyyy HHmm",
+                "dd/M/yyyy HHmm", "d/M/yyyy HHmm",
+                "dd-M-yyyy HHmm", "d-M-yyyy HHmm");
+
+        DateTimeFormatter formatter;
+        LocalDateTime dateTime;
+
+        // Try to fit the input string into a format in formatString
+        for (String formatString : formatStrings) {
+            try {
+                formatter = DateTimeFormatter.ofPattern(formatString);
+                dateTime = LocalDateTime.parse(result, formatter);
+                // E.g Convert 2/12/2019 1800 to Monday, December 02, 2019 06:00 PM
+                result = dateTime.format(DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy hh:mm a"));
+            } catch (DateTimeParseException e) {
+                // Pass and don't format input if the date and time format does not match any element in formatString
+            }
+        }
+        return result;
+    }
+}
--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -9,6 +9,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + DateTime.formatDateTime(by) + ")";
     }
 }
