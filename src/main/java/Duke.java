b1e37a265673cc896ddf280764230966f2a94e3b
Jaylenlee <e0424677@u.nus.edu>
2020-08-23 21:12:41 +0800

    Change path from absolute to relative. Add save task function.
    Access to path change allows different system other than Windows to access.
    Save task function allows data to be loaded into harddisk and resume on next run.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,6 +8,10 @@ import Task.Task;
 import Task.ToDoTask;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
@@ -43,9 +47,13 @@ public class Duke {
 
     public static void interact(Scanner sc) {
         HashMap<String, BiConsumer<String, List<Task>>> map = setUp();
-        List<Task> list = FileToTaskListConverter.convert(new File("C:/Users/Jaylen/Downloads/CS2103 IP/data/Duke.txt"));
-        String command = sc.nextLine();
 
+        String home = System.getProperty("user.home");
+        Path path = Paths.get(home, "Downloads", "CS2103 IP", "data", "duke.txt");
+        File file = path.toFile();
+        List<Task> list = FileToTaskListConverter.convert(file);
+
+        String command = sc.nextLine();
         while (!command.equals("bye")) {
             BiConsumer<String, List<Task>> action =  map.get(command.replaceAll(" .*", ""));
 
@@ -60,11 +68,27 @@ public class Duke {
             }
             command = sc.nextLine();
         }
-
+        saveToFile(list, path.toFile());
         System.out.println("    ____________________________________________________________\n"
                 + "     Bye. Hope to see you again soon!\n"
                 + "    ____________________________________________________________\n"
         );
+    }
+
+    public static void saveToFile(List<Task> list, File file) {
+        try {
+            FileWriter fw = new FileWriter(file);
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < list.size(); i++) {
+                Task task = list.get(i);
+                sb.append(task.toString());
+                sb.append("\n");
+            }
+            fw.write(sb.toString());
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Unable to save");
+        }
     }
 
     public static void addCommand(List<Task> list, String command) {
