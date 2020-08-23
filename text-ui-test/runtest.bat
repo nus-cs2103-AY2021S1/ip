9c8504c2803972043a81f8cfe3a8ee62ee72e4d9
onglijin <onglijinlj@gmail.com>
2020-08-23 15:20:27 +0800

    Update test files for level 1

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,17 +1,13 @@
-  
 @ECHO OFF
 
-SET MY_PATH = "C:\Users\Li Jin\Desktop\NUS\Modules\Y2S1\CS2103\iP"
-
 REM create bin directory if it doesn't exist
-if not exist "%MY_PATH%\bin" mkdir "%MY_PATH%\bin"
+if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
-
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -19,7 +15,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath "%MY_PATH%\bin" Duke < input.txt > ACTUAL.TXT
+java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
