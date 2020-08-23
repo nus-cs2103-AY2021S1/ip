b179018f50545f09513d707efea4f1ea95e78bb2
kimberly ong <e0424807@u.nus.edu>
2020-08-23 11:08:45 +0800

    Add Level-8: Dates and Times

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,14 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class DeadlineTask extends Task {
-    String deadline;
+    LocalDateTime deadline;
 
     DeadlineTask(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+            this.deadline = LocalDateTime.parse(deadline, formatter);
+        } catch (DateTimeParseException exception) {
+                System.out.println(exception);
+        }
+
     }
 
     @Override
     public String toString() {
-        return "[D][" + getStatusIcon() + "] " + description + " (by: " + deadline + ")";
+        return "[D][" + getStatusIcon() + "] " + description + " (by: " +
+                deadline.format(DateTimeFormatter.ofPattern("d MM yyyy, hh:mm a")) + ")";
     }
 
 }
--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,14 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class EventTask extends Task {
-    String timing;
+    LocalDateTime timing;
 
     EventTask(String description, String timing) {
         super(description);
-        this.timing = timing;
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+            this.timing = LocalDateTime.parse(timing, formatter);
+        } catch (DateTimeParseException exception) {
+            System.out.println(exception);
+        }
     }
 
     @Override
     public String toString() {
-        return "[E][" + getStatusIcon() + "] " + description + " (at: " + timing + ")";
+        return "[E][" + getStatusIcon() + "] " + description + " (by: " +
+                timing.format(DateTimeFormatter.ofPattern("d MM yyyy, hh:mm a")) + ")";
     }
 
 }
