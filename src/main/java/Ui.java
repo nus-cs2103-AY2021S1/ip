ed36652f3636f243c09bf6640b61613b3ce350f2
chewypiano <gimikamil@gmail.com>
2020-08-23 17:24:21 +0800

    Refactor Code Part 1 - Add Ui Class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Ui {
+
+    private Scanner sc;
+    
+    public Ui() {
+        sc = new Scanner(System.in);
+    }
+
+    // format horizontal line to create divisions
+    public String div() {
+        return "\n______________________________"
+            + "________________________________\n\n";
+    }
+
+    public String greet() {
+        return "                      █████████\n"
+            +  "  ███████          ███        ███\n"
+            +  "  █      █       ███             ███\n"
+            +  "   █      █    ██                   ██\n"
+            +  "    █     █   ██     ██      ██     ███          Hey! I'm Jimmy,\n"
+            +  "     █   █   █      ████    ████      ██     your personal assistant!\n"
+            +  "   █████████████                      ██\n"
+            +  "   █            █         █           ██   What can I do for you today?\n"
+            +  " ██             █   ██          ██    ██\n"
+            +  "██   ███████████     ██        ██     ██\n"
+            +  "█               █      ████████       ██\n"
+            +  "██              █                    ██\n"
+            +  " █   ███████████                   ██\n"
+            +  " ██          ████                 █\n"
+            +  "  ████████████   █████████████████";
+    }
+
+    public String process() {
+        return this.sc.nextLine();
+    }
+
+    // printing messages for user to view
+    public void speak(String input) {
+        System.out.println(this.div() +
+            input + this.div());
+    }
+}
