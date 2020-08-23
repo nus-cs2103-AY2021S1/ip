d1b60564f49f50d93aaf705c6864ff885e6a155b
Wincent <wincenttsjoi@gmail.com>
2020-08-23 20:28:11 +0800

    Add task javadoc

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,21 +1,40 @@
+/**
+ * Task contains the description of command by user and
+ * the state of task, whether it is done or not done.
+ */
 public class Task {
 
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructor of task, item initialized as not done by default.
+     * @param description description of task command
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon depending on done status of task.
+     * @return string icon of tick or cross
+     */
     public String getStatusIcon() {
         return (isDone ? "✓" : "✘"); //return tick or X symbols
     }
 
+    /**
+     * Marks task status to done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Returns 1 if status is done, 0 if not done.
+     * @return
+     */
     public String toSaveString() {
         if (isDone) {
             return "1";
