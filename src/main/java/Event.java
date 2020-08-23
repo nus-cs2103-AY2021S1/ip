005cc0c81c11d50a686c4d07dbde43a1058dad3d
yongping827 <yongping827@users.noreply.github.com>
2020-08-23 23:23:28 +0800

    Add: Level 8. Dates and Times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,11 +2,8 @@ package main.java;
 
 class Event extends Task {
 
-    final String dateTime;
-
-    Event(String description, String datetime) {
-        super(description);
-        this.dateTime = datetime;
+    Event(String description, String dateTimeString) {
+        super(description, dateTimeString);
     }
 
     @Override
