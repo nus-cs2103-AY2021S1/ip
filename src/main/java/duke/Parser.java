ea34aa680baef83c4dad3efcbce61c231d8cf0fe
sebastiantoh <sebas.tsj.98@gmail.com>
2020-08-23 12:39:12 +0800

    Fix constants naming

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -17,11 +17,12 @@ import duke.task.Event;
 import duke.task.Todo;
 
 public class Parser {
-    private final static String failedToMarkTaskAsComplete = "Failed to mark task as complete!";
-    private final static String failedToDeleteTask = "Failed to delete task!";
-    private final static String failedToCreateTask = "Failed to create task!";
-    private final static String failedToCreateDeadline = "Failed to create Deadline task!";
-    private final static String failedToCreateEvent = "Failed to create Event task!";
+    private final static String FAILED_TO_MARK_TASK_AS_COMPLETE =
+            "Failed to mark task as complete!";
+    private final static String FAILED_TO_DELETE_TASK = "Failed to delete task!";
+    private final static String FAILED_TO_CREATE_TASK = "Failed to create task!";
+    private final static String FAILED_TO_CREATE_DEADLINE_TASK = "Failed to create Deadline task!";
+    private final static String FAILED_TO_CREATE_EVENT_TASK = "Failed to create Event task!";
 
     static Command parse(String fullCommand) throws DukeException {
         String[] commandInputs = fullCommand.trim().split(" ", 2);
@@ -49,7 +50,7 @@ public class Parser {
 
             case "done":
                 if (commandInputs.length < 2) {
-                    throw new MissingTaskIdException(failedToMarkTaskAsComplete);
+                    throw new MissingTaskIdException(FAILED_TO_MARK_TASK_AS_COMPLETE);
                 }
 
                 commandDetails = commandInputs[1].trim();
@@ -58,12 +59,12 @@ public class Parser {
                     int taskId = Integer.parseInt(commandDetails);
                     return new DoneCommand(taskId);
                 } catch (NumberFormatException e) {
-                    throw new InvalidTaskIdException(failedToMarkTaskAsComplete);
+                    throw new InvalidTaskIdException(FAILED_TO_MARK_TASK_AS_COMPLETE);
                 }
 
             case "delete":
                 if (commandInputs.length < 2) {
-                    throw new MissingTaskIdException(failedToDeleteTask);
+                    throw new MissingTaskIdException(FAILED_TO_DELETE_TASK);
                 }
 
                 commandDetails = commandInputs[1].trim();
@@ -72,14 +73,14 @@ public class Parser {
                     int taskId = Integer.parseInt(commandDetails);
                     return new DeleteCommand(taskId);
                 } catch (NumberFormatException e) {
-                    throw new InvalidTaskIdException(failedToDeleteTask);
+                    throw new InvalidTaskIdException(FAILED_TO_DELETE_TASK);
                 }
 
             case "todo":
             case "deadline":
             case "event":
                 if (commandInputs.length < 2) {
-                    throw new MissingTaskDetailsException(failedToCreateTask);
+                    throw new MissingTaskDetailsException(FAILED_TO_CREATE_TASK);
                 }
 
                 commandDetails = commandInputs[1].trim();
@@ -90,7 +91,7 @@ public class Parser {
                     String[] deadlineDetails = commandDetails.split("/by", 2);
 
                     if (deadlineDetails.length < 2) {
-                        throw new MissingTaskDetailsException(failedToCreateDeadline,
+                        throw new MissingTaskDetailsException(FAILED_TO_CREATE_DEADLINE_TASK,
                                 "No deadline was specified!");
                     }
 
@@ -103,7 +104,7 @@ public class Parser {
                     String[] eventDetails = commandDetails.split("/at", 2);
 
                     if (eventDetails.length < 2) {
-                        throw new MissingTaskDetailsException(failedToCreateEvent,
+                        throw new MissingTaskDetailsException(FAILED_TO_CREATE_EVENT_TASK,
                                 "No date was specified!");
                     }
 
