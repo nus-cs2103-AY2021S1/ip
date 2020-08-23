cd1c36ce7526743f75922730afcd9219fec6a77f
rishi <rishi5154@gmail.com>
2020-08-23 15:08:05 +0800

    A-MoreOOP: Add Parser class

---
+++ src/main/java/Parser.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Parser {
+
+    public Scanner scan;
+
+    public Parser() {
+        this.scan = new Scanner(System.in);
+    }
+
+    public Commands analyse(String command) {
+        if (command.startsWith("list")) {
+            return Commands.LIST;
+        } else if (command.startsWith("todo")) {
+            return Commands.TODO;
+        } else if (command.startsWith("deadline")) {
+            return Commands.DEADLINE;
+        } else if (command.startsWith("event")) {
+            return Commands.EVENT;
+        } else if (command.startsWith("delete")) {
+            return Commands.DELETE;
+        } else if (command.startsWith("blah")) {
+            return Commands.BLAH;
+        } else if (command.startsWith("done")) {
+            return Commands.DONE;
+        } else {
+            return Commands.TASK;
+        }
+    }
+}
