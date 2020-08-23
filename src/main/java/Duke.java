4283653494b7bcc781cb36b91543970d7e693301
g-erm <tanyuting.gt@gmail.com>
2020-08-23 16:51:41 +0800

    Tidy up code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    int taskSize;
+    ArrayList<Task> taskList;
+
+    public Duke() {
+        this.taskSize = 0;
+        this.taskList = new ArrayList<>();
+    }
+
+    public Duke(int taskSize, ArrayList<Task> taskList) {
+        this.taskSize = taskSize;
+        this.taskList = taskList;
+    }
+
+    public static void greeting() {
         String logo = " ____   ____\n"
                 + "|  _ \\ |  _ \\\n"
                 + "| | | || | | |\n"
@@ -10,6 +23,104 @@ public class Duke {
                 + "|____/ |____/\n";
         System.out.println("Hi! I'm\n" + logo + "How can I help you? :)\n"
                 + "_________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("You're leaving? Bye :( Come back soon!"
+                + "\n_________________________________________");
+    }
+
+    public static void list(Duke dd) {
+        System.out.println("Here is your current list of task(s)!");
+        int curr = 0;
+        while (curr < dd.taskSize) {
+            System.out.println((curr + 1) + ". " + dd.taskList.get(curr));
+            curr += 1;
+        }
+    }
+
+    public static void addTodo(Duke dd, String input) {
+        input = input.substring(5);
+        dd.taskList.add(new Todo(input));
+        System.out.println("Ok, To-do added:\n  " + dd.taskList.get(dd.taskSize));
+        dd.taskSize += 1;
+        System.out.println("You now have " + dd.taskSize + " task(s) in your list!");
+    }
+
+    public static void addDeadline(Duke dd, String input) {
+        input = input.substring(9);
+        String[] temp = input.split(" /by "); // create array of [task desc, task date]
+
+        if (temp.length == 2) {
+            // valid
+            dd.taskList.add(new Deadline(temp[0], temp[1]));
+            System.out.println("Ok, Deadline added:\n  " + dd.taskList.get(dd.taskSize));
+            dd.taskSize += 1;
+            System.out.println("You now have " + dd.taskSize + " task(s) in your list!");
+        } else {
+            // no valid date
+            System.out.println("Due date not detected, try again!");
+        }
+    }
+
+    public static void addEvent(Duke dd, String input) {
+        input = input.substring(6);
+        String[] temp = input.split(" /at "); // create array of [task desc, task date]
+
+        if (temp.length == 2) {
+            // valid
+            dd.taskList.add(new Event(temp[0], temp[1]));
+            System.out.println("Ok, Event added:\n  " + dd.taskList.get(dd.taskSize));
+            dd.taskSize += 1;
+            System.out.println("You now have " + dd.taskSize + " task(s) in your list!");
+        } else {
+            // no valid date
+            System.out.println("Event date not detected, try again!");
+        }
+    }
+
+    public static void done(Duke dd, String input) {
+        String taskStr = input.substring(5);
+        int taskNum = 0;
+
+        try {
+            taskNum = Integer.parseInt(taskStr);
+        }
+        catch (NumberFormatException ignored) {
+        }
+
+        if (taskNum > 0 && taskNum <= dd.taskSize) {
+            dd.taskList.get(taskNum - 1).markAsDone();
+            System.out.println("Wow!! Good job!!\n  " + dd.taskList.get(taskNum - 1));
+        }
+        else {
+            System.out.println("hmm.. I don't think thats a valid task, try again?");
+        }
+    }
+
+    public static void delete(Duke dd, String input) {
+        String delStr = input.substring(7);
+        int delNum = 0;
+
+        try {
+            delNum = Integer.parseInt(delStr);
+        }
+        catch (NumberFormatException ignored) {
+        }
+
+        if (delNum > 0 && delNum <= dd.taskSize) {
+            System.out.println("Alright! I've deleted the task:\n  " + dd.taskList.get(delNum - 1));
+            dd.taskList.remove(delNum-1);
+            dd.taskSize -= 1;
+            System.out.println("You now have " + dd.taskSize + " task(s) in your list!");
+        }
+        else {
+            System.out.println("hmm.. I don't think thats a valid task, try again?");
+        }
+    }
+
+    public static void main(String[] args) {
+        greeting();
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
@@ -17,105 +128,32 @@ public class Duke {
         String bye = "bye";
         String list = "list";
 
-        ArrayList<Task> taskList = new ArrayList<Task>();
-        int taskSize = 0;
+        Duke dd = new Duke();
 
         if (input.equals(bye)) {
-            // exit
-            System.out.println("You're leaving? Bye :( Come back soon!"
-                    + "\n_________________________________________");
+            exit();
         }
         else {
             // does not exit
             while (!input.equals(bye)) {
                 if (input.equals(list)) {
-                    System.out.println("Here is your current list of task(s)!");
-                    int curr = 0;
-                    while (curr < taskSize) {
-                        System.out.println((curr + 1) + ". " + taskList.get(curr));
-                        curr += 1;
-                    }
+                    list(dd);
                 }
                 // input tasks
                 else if (input.startsWith("todo")) {
-                    // to-do activity
-                    input = input.substring(5);
-                    taskList.add(new Todo(input));
-                    System.out.println("Ok, To-do added:\n  " + taskList.get(taskSize));
-                    taskSize += 1;
-                    System.out.println("You now have " + taskSize + " task(s) in your list!");
+                    addTodo(dd, input);
                 }
                 else if (input.startsWith("deadline")) {
-                    // deadline
-                    input = input.substring(9);
-                    String[] temp = input.split(" /by "); // create array of [task desc, task date]
-
-                    if (temp.length == 2) {
-                        // valid
-                        taskList.add(new Deadline(temp[0], temp[1]));
-                        System.out.println("Ok, Deadline added:\n  " + taskList.get(taskSize));
-                        taskSize += 1;
-                        System.out.println("You now have " + taskSize + " task(s) in your list!");
-                    } else {
-                        // no valid date
-                        System.out.println("Due date not detected, try again!");
-                    }
+                    addDeadline(dd, input);
                 }
                 else if (input.startsWith("event")) {
-                    // event
-                    input = input.substring(6);
-                    String[] temp = input.split(" /at "); // create array of [task desc, task date]
-
-                    if (temp.length == 2) {
-                        // valid
-                        taskList.add(new Event(temp[0], temp[1]));
-                        System.out.println("Ok, Event added:\n  " + taskList.get(taskSize));
-                        taskSize += 1;
-                        System.out.println("You now have " + taskSize + " task(s) in your list!");
-                    } else {
-                        // no valid date
-                        System.out.println("Event date not detected, try again!");
-                    }
+                    addEvent(dd, input);
                 }
-                // check for done activity
                 else if (input.startsWith("done")) {
-                    String taskStr = input.substring(5);
-                    int taskNum = 0;
-
-                    try {
-                        taskNum = Integer.parseInt(taskStr);
-                    }
-                    catch (NumberFormatException ignored) {
-                    }
-
-                    if (taskNum > 0 && taskNum <= taskSize) {
-                        taskList.get(taskNum - 1).markAsDone();
-                        System.out.println("Wow!! Good job!!\n  " + taskList.get(taskNum - 1));
-                    }
-                    else {
-                        System.out.println("hmm.. I don't think thats a valid task, try again?");
-                    }
+                    done(dd, input);
                 }
-                // check for delete activity
                 else if (input.startsWith("delete")) {
-                    String delStr = input.substring(7);
-                    int delNum = 0;
-
-                    try {
-                        delNum = Integer.parseInt(delStr);
-                    }
-                    catch (NumberFormatException ignored) {
-                    }
-
-                    if (delNum > 0 && delNum <= taskSize) {
-                        System.out.println("Alright! I've deleted the task:\n  " + taskList.get(delNum - 1));
-                        taskList.remove(delNum-1);
-                        taskSize -= 1;
-                        System.out.println("You now have " + taskSize + " task(s) in your list!");
-                    }
-                    else {
-                        System.out.println("hmm.. I don't think thats a valid task, try again?");
-                    }
+                    delete(dd, input);
                 }
                 else {
                     // not valid task
@@ -124,9 +162,7 @@ public class Duke {
                 System.out.println("_________________________________________");
                 input = sc.nextLine();
             }
-
-            System.out.println("You're leaving? Bye :( Come back soon!"
-                    + "\n_________________________________________");
+            exit();
         }
     }
 }
