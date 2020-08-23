ff260b348b22afd2044cd75fcddbba7a60c32a0a
TianYong-Goh <goh_ty@users.noreply.github.com>
2020-08-23 19:10:42 +0800

    Change File Name

---
+++ src/main/java/Mug.java
@@ -0,0 +1,33 @@
+import Storage.Storage;
+import Tasks.TaskList;
+import Ui.Ui;
+
+import java.util.Scanner;
+
+public class Mug {
+
+    private final Ui ui;
+    private final TaskList tasks;
+
+    public Mug(String filePath) {
+        this.ui = new Ui();
+        Storage storage = new Storage(filePath);
+        this.tasks = new TaskList(storage);
+    }
+
+    public void run() {
+        this.ui.welcome();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+           String input = sc.nextLine();
+           System.out.println(this.ui.readCommand(input, this.tasks));
+           if(input.trim().equals("bye")) {
+               break;
+           }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Mug("mug.txt").run();
+    }
+}
