b5b2632e7d05752a6cfc240fec80fce19ea894ec
lll-jy <E0376997@u.nus.edu>
2020-08-23 16:50:46 +0800

    Add JavaDoc for print method of Ui

--- src/main/java/duke/component/Ui.java
+++ src/main/java/duke/component/Ui.java
@@ -16,6 +16,10 @@ public class Ui {
         sc = new Scanner(System.in);
     }
 
+    /**
+     * Prints the given string.
+     * @param str the string to print
+     */
     public void print(String str) {
         System.out.println(str);
     }
