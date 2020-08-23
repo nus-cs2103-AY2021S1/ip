03523e01e51e354e65ca8f2ddb242611e200bc41
lll-jy <E0376997@u.nus.edu>
2020-08-23 12:46:42 +0800

    Add isExit JUnit test for AddCommand

--- src/test/java/duke/command/AddCommandTest.java
+++ src/test/java/duke/command/AddCommandTest.java
@@ -1,4 +1,15 @@
 package duke.command;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class AddCommandTest {
+    @Test
+    public void isExit_alwaysFalse() {
+        assertFalse(new AddCommand("anything").isExit());
+        assertFalse(new AddCommand("todo anything").isExit());
+        assertFalse(new AddCommand("event anything").isExit());
+        assertFalse(new AddCommand("deadline anything").isExit());
+    }
 }
