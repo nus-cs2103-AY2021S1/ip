6a90d9b8d4fa84240db80efdf13001044b61efc6
dianneloh9 <dianneloh9@gmail.com>
2020-08-23 21:52:13 +0800

    Update test script

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,9 +12,15 @@ then
     rm ACTUAL.TXT
 fi
 
+# delete data from prevous run
+if [ -e "./data/duke.txt" ]
+then
+    rm ./data/duke.txt
+fi
+
 # compile the code into the bin folder, terminates if error occurred
 
-if ! javac -cp ../src/main/java/duke -Xlint:none -d ../bin ../src/main/java/duke/Duke.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/duke/Duke.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
@@ -22,8 +28,8 @@ fi
 
 export LC_ALL=en_GB.UTF-8
 
-# run the program, feed duke.commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin duke/Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
