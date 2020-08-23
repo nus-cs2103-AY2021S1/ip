f9c5ecba27b2a277862b34b6e4ddaaa5c9b46cd2
lll-jy <E0376997@u.nus.edu>
2020-08-23 17:45:11 +0800

    Create findCommand equals method

--- src/main/java/duke/command/findCommand.java
+++ src/main/java/duke/command/findCommand.java
@@ -8,4 +8,20 @@ public class findCommand extends Command {
     public findCommand(String input) {
         super(input);
     }
+
+    /**
+     * Checks whether the given command equals this one.
+     * @param obj the object to compare
+     * @return true if obj is a FindCommand and command input is the same
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        } else if (obj instanceof findCommand) {
+            return input.equals(((findCommand) obj).input);
+        } else {
+            return false;
+        }
+    }
 }
