6626092ec769e7387de0d0f47e286487b7b989df
lll-jy <E0376997@u.nus.edu>
2020-08-23 13:37:44 +0800

    Add isExit JUnit test for DeleteCommand

--- src/test/java/duke/command/DeleteCommandTest.java
+++ src/test/java/duke/command/DeleteCommandTest.java
@@ -1,4 +1,16 @@
 package duke.command;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class DeleteCommandTest {
+    @Test
+    public void isExit_alwaysFalse() {
+        assertFalse(new DeleteCommand("delete ").isExit());
+        assertFalse(new DeleteCommand("delete 3").isExit());
+        assertFalse(new DeleteCommand("delete 0").isExit());
+        assertFalse(new DeleteCommand("delete -3").isExit());
+        assertFalse(new DeleteCommand("delete anything").isExit());
+    }
 }
