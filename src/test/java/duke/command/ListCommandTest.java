e8c11428ef7f6d506a553ec530dc7b9caea52401
lll-jy <E0376997@u.nus.edu>
2020-08-23 13:11:27 +0800

    Add execute JUnit test for ListCommand

--- src/test/java/duke/command/ListCommandTest.java
+++ src/test/java/duke/command/ListCommandTest.java
@@ -1,12 +1,26 @@
 package duke.command;
 
+import duke.component.Storage;
+import duke.component.StorageStub;
+import duke.component.TaskList;
+import duke.component.Ui;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ListCommandTest {
     @Test
     public void isExit_alwaysFalse() {
         assertFalse(new ListCommand("list").isExit());
+    }
+
+    @Test
+    public void execute_nothingWrong() {
+        Ui ui = new Ui();
+        Storage storage = new StorageStub();
+        TaskList list = storage.getList();
+        ListCommand b = new ListCommand("list");
+        assertEquals("list", b.execute(ui, list, storage));
     }
 }
