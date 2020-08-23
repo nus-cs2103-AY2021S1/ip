fdd3e061bab48bc005e2389e406fff27e8255f69
lll-jy <E0376997@u.nus.edu>
2020-08-23 14:05:10 +0800

    Add isExit JUnit test for HappenCommand

--- src/test/java/duke/command/HappenCommandTest.java
+++ src/test/java/duke/command/HappenCommandTest.java
@@ -1,4 +1,12 @@
 package duke.command;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class HappenCommandTest {
+    @Test
+    public void isExit_alwaysFalse() {
+        assertFalse(new HappenCommand("happen on today").isExit());
+    }
 }
