b24be01f0c2b73bbb29e10f8bcc7940496d52517
lll-jy <E0376997@u.nus.edu>
2020-08-23 13:24:29 +0800

    Change unrecognized command test to all invalid command test

--- src/test/java/duke/command/AddCommandTest.java
+++ src/test/java/duke/command/AddCommandTest.java
@@ -1,9 +1,7 @@
 package duke.command;
 
-import duke.component.Storage;
-import duke.component.StorageStub;
-import duke.component.TaskList;
-import duke.component.Ui;
+import duke.component.*;
+import duke.task.ToDo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,34 +15,39 @@ public class AddCommandTest {
         assertFalse(new AddCommand("deadline anything").isExit());
     }
 
-    @Test
-    public void execute_unrecognizedCommand_throwException() {
-        Ui ui = new Ui();
-        Storage storage = new StorageStub();
-        TaskList list = storage.getList();
-
-        AddCommand a = new AddCommand("bash");
+    public void executeExceptionHelper(String s, Ui ui, TaskList list, Storage storage) {
+        AddCommand a = new AddCommand(s);
         try {
             a.execute(ui, list, storage);
             fail();
         } catch (Exception e) {
-            assertEquals("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(", e.getMessage());
+            try {
+                Parser.generate(s);
+            } catch (Exception e2) {
+                assertEquals(e2.getMessage(), e.getMessage());
+            }
         }
+    }
 
-        AddCommand b = new AddCommand("done");
-        try {
-            b.execute(ui, list, storage);
-            fail();
-        } catch (Exception e) {
-            assertEquals("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(", e.getMessage());
-        }
+    @Test
+    public void execute_invalidCommand_throwException() {
+        Ui ui = new Ui();
+        Storage storage = new StorageStub();
+        TaskList list = storage.getList();
+        list.add(new ToDo("hello"));
 
-        AddCommand c = new AddCommand("happen");
-        try {
-            c.execute(ui, list, storage);
-            fail();
-        } catch (Exception e) {
-            assertEquals("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(", e.getMessage());
-        }
+        executeExceptionHelper("bash", ui, list, storage);
+        executeExceptionHelper("done", ui, list, storage);
+        executeExceptionHelper("delete", ui, list, storage);
+        executeExceptionHelper("done 3", ui, list, storage);
+        executeExceptionHelper("done 0", ui, list, storage);
+        executeExceptionHelper("done -1", ui, list, storage);
+        executeExceptionHelper("done 1", ui, list, storage);
+        executeExceptionHelper("done 1", ui, list, storage);
+        executeExceptionHelper("happen", ui, list, storage);
+        executeExceptionHelper("todo", ui, list, storage);
+        executeExceptionHelper("deadline do something", ui, list, storage);
+        executeExceptionHelper("event meeting /at", ui, list, storage);
+        executeExceptionHelper("event meeting /at 2020-08-09", ui, list, storage);
     }
 }
