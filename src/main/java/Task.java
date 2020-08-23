dbab8ed4306008e8d510381faade61b69beec981
amylzting <amylimzting@gmail.com>
2020-08-24 00:48:42 +0800

    Change markAsDone symbol in Task class
    Let's change the symbol denoting whether a
    task is done from tick and cross to 1 and 0.
    
    This takes away any potential bugs caused
    from symbol.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,7 +8,7 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "✓" : "✘"); //return tick or X symbols
+        return (isDone ? "1" : "0"); //return tick or X symbols
     }
 
     public void markAsDone() {
