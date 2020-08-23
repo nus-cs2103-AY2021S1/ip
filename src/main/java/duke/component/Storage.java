7e542c2d9b80728c7ac12075d928dff4961c9258
lll-jy <E0376997@u.nus.edu>
2020-08-23 16:41:25 +0800

    Add JavaDoc for reWrite method of Storage

--- src/main/java/duke/component/Storage.java
+++ src/main/java/duke/component/Storage.java
@@ -17,5 +17,10 @@ public interface Storage {
      */
     void addToList(Task task) throws InvalidCommandException;
 
+    /**
+     * Re-writes the storage file because of deletion or marking-as-done executed.
+     * @param list the new task list used for updating the storage file
+     * @throws InvalidCommandException should never been thrown unless the file path is not working
+     */
     void reWrite(TaskList list) throws InvalidCommandException;
 }
