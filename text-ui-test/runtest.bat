bdaec9329add2586eac216a0345469680511abc9
Sean <e0406739@u.nus.edu>
2020-08-24 00:32:13 +0800

    -Retrieve deleted files

---
+++ text-ui-test/runtest.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+REM create bin directory if it doesn't exist
+if not exist ..\bin mkdir ..\bin
+
+REM delete output from previous run
+del ACTUAL.TXT
+
+REM compile the code into the bin folder
+javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\duke.Duke.java
+IF ERRORLEVEL 1 (
+    echo ********** BUILD FAILURE **********
+    exit /b 1
+)
+REM no error here, errorlevel == 0
+
+REM run the program, feed duke.commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ..\bin duke.Duke < input.txt > ACTUAL.TXT
+
+REM compare the output to the expected output
+FC ACTUAL.TXT EXPECTED.TXT
