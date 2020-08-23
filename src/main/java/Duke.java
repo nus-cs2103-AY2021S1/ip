86e7361b9033b5b5adf3bc0a8310c600bbd03950
ktaekwon000 <ktaekwon000@gmail.com>
2020-08-23 22:12:36 +0800

    Remove indentation from output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,23 +12,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String LINE = "    ____________________________________________________________";
+    private static final String LINE = "____________________________________________________________";
     private static final String WELCOME_MESSAGE = "Hello, I'm Duke, your personal assistant!\n"
-        + "     What can I do for you?";
+        + "What can I do for you?";
     private static final String GOODBYE_MESSAGE = "Bye. Hope to see you again soon! :)";
 
     private static ArrayList<Task> list;
 
     private static String makeWrappedString(String txt) {
-        return LINE + "\n     " + txt + "\n" + LINE;
+        return LINE + "\n" + txt + "\n" + LINE;
     }
 
     private static void addToList(Task task) {
         list.add(task);
         System.out.println(LINE
-            + "\n     Sure, I've added this task to your list:\n"
-            + "       " + task
-            + "\n     You now have " + list.size() + " task(s) in the list!\n"
+            + "\nSure, I've added this task to your list:\n"
+            + task
+            + "\nYou now have " + list.size() + " task(s) in the list!\n"
             + LINE);
     }
 
@@ -90,7 +90,7 @@ public class Duke {
                     if (indexOfBy < 0 || input.substring(indexOfBy + 5).split(" ").length <= 0) {
                         throw new IllegalArgumentException(
                             "A date needs to be provided with /by when adding a deadline.\n"
-                                + "     Usage: deadline return book /by 2020-12-30");
+                                + "Usage: deadline return book /by 2020-12-30");
                     }
                     addToList(new Deadline(input.substring(9, indexOfBy), input.substring(indexOfBy + 5)));
                     break;
@@ -101,17 +101,17 @@ public class Duke {
                     }
                     if (indexOfAt < 0 || input.substring(indexOfAt + 5).split(" ").length <= 0) {
                         throw new IllegalArgumentException("A date needs to be provided with /at when adding a event.\n"
-                            + "     Usage: event library renewal /at 2020-12-31");
+                            + "Usage: event library renewal /at 2020-12-31");
                     }
                     addToList(new Event(input.substring(6, indexOfAt), input.substring(indexOfAt + 5)));
                     break;
                 case "list":
-                    System.out.println(LINE + "\n     Here are the tasks in your list:");
+                    System.out.println(LINE + "\nHere are the tasks in your list:");
                     if (list.size() == 0) {
-                        System.out.println("     Your list is empty. How about adding some tasks?");
+                        System.out.println("Your list is empty. How about adding some tasks?");
                     } else {
                         for (int i = 0; i < list.size(); i++) {
-                            System.out.println("     " + (i + 1) + ": " + list.get(i));
+                            System.out.println("" + (i + 1) + ": " + list.get(i));
                         }
                     }
                     System.out.println(LINE);
@@ -122,16 +122,16 @@ public class Duke {
                     }
                     if (Integer.parseInt(input.split(" ")[1]) > list.size()) {
                         throw new IllegalArgumentException(
-                            "The index provided is too large.\n     Run list to see your list of tasks.");
+                            "The index provided is too large.\nRun list to see your list of tasks.");
                     }
                     if (Integer.parseInt(input.split(" ")[1]) < 1) {
                         throw new IllegalArgumentException(
-                            "The index provided is too small.\n     Input an integer that is 1 or greater.");
+                            "The index provided is too small.\nInput an integer that is 1 or greater.");
                     }
                     Task doneTask = list.get(Integer.parseInt(input.split(" ")[1]) - 1);
                     doneTask.setDone(true);
                     System.out.println(
-                        makeWrappedString("Great Job! I've marked this task as done:\n       " + doneTask));
+                        makeWrappedString("Great Job! I've marked this task as done:\n" + doneTask));
                     break;
                 case "delete":
                     if (input.split(" ").length < 2 || !input.split(" ")[1].matches("\\d+")) {
@@ -139,27 +139,27 @@ public class Duke {
                     }
                     if (Integer.parseInt(input.split(" ")[1]) > list.size()) {
                         throw new IllegalArgumentException(
-                            "The index provided is too large.\n     Run list to see your list of tasks.");
+                            "The index provided is too large.\nRun list to see your list of tasks.");
                     }
                     if (Integer.parseInt(input.split(" ")[1]) < 1) {
                         throw new IllegalArgumentException(
-                            "The index provided is too small.\n     Input an integer that is 1 or greater.");
+                            "The index provided is too small.\nInput an integer that is 1 or greater.");
                     }
                     Task deleted = list.remove(Integer.parseInt(input.split(" ")[1]) - 1);
                     System.out.println(LINE
-                        + "\n     Sure, I've deleted this task from your list:\n"
-                        + "       " + deleted
-                        + "\n     You now have " + list.size() + " task(s) in the list.\n"
+                        + "\nSure, I've deleted this task from your list:\n"
+                        + deleted
+                        + "\nYou now have " + list.size() + " task(s) in the list.\n"
                         + LINE);
                     break;
                 default:
                     throw new IllegalArgumentException("Your input must start with one of the following:\n"
-                        + "     \"todo\", \"deadline\", \"event\", \"list\", \"done\" or \"delete\"."
+                        + "\"todo\", \"deadline\", \"event\", \"list\", \"done\" or \"delete\"."
                     );
                     // break;
                 }
             } catch (IllegalArgumentException e) {
-                System.out.println(makeWrappedString("Error! We had a problem understanding your message.\n     "
+                System.out.println(makeWrappedString("Error! We had a problem understanding your message.\n"
                     + e.getMessage()));
             }
             input = sc.nextLine();
