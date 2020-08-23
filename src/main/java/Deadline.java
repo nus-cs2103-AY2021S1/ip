707dcabb8ca514b08b9641fbac66322c2cc13c36
jazerler <lerjazer@gmail.com>
2020-08-23 20:41:04 +0800

    Add dates and times with LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
 
-    String time;
+    LocalDate time;
 
-    Deadline(String name, String time) {
+    Deadline(String name, LocalDate time) {
         super(name);
         this.time = time;
     }
 
     @Override
     public String toString() {
-        return String.format("[D] %s (by: %s)", super.toString(), time);
+        return String.format("[D] %s (by: %s)", super.toString(), time.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends  Task{
 
-    String time;
+    LocalDate time;
 
-    Event(String name, String time) {
+    Event(String name, LocalDate time) {
         super(name);
         this.time = time;
     }
 
     @Override
     public String toString() {
-        return String.format("[E] %s (at: %s)", super.toString(), time);
+        return String.format("[E] %s (at: %s)", super.toString(), time.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
