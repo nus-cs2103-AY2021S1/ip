3d99c238320d79aaec7ce6743fad6b3a739aba1a
zhaojj2209 <zhaojj2000@gmail.com>
2020-08-23 23:13:00 +0800

    Implement level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    String deadline;
+    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+    LocalDate deadline;
+
     public Deadline(String description) {
         super(description.split(" /by ")[0]);
-        this.deadline = description.split(" /by ")[1];
+        this.deadline = LocalDate.parse(description.split(" /by ")[1]);
     }
 
     @Override
     public String toString() {
-        return "[D][" + super.getStatusIcon() + "] " + super.description + " (by: " + this.deadline + ")";
+        return "[D][" + super.getStatusIcon() + "] " + super.description + " (by: " + this.deadline.format(formatter) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    String eventTime;
+    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+    LocalDate eventTime;
+
     public Event(String description) {
         super(description.split(" /at ")[0]);
-        this.eventTime = description.split(" /at ")[1];
+        this.eventTime = LocalDate.parse(description.split(" /at ")[1]);
     }
 
     @Override
     public String toString() {
-        return "[E][" + super.getStatusIcon() + "] " + super.description + " (at: " + this.eventTime + ")";
+        return "[E][" + super.getStatusIcon() + "] " + super.description + " (at: " + this.eventTime.format(formatter) + ")";
     }
 }
