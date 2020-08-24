ced4a36cb597a3f4cbe59223dbee56dbf55320f9
chan-j-d <chan.j.d.98@gmail.com>
2020-08-25 01:33:26 +0800

    Create ListCommand.java

---
+++ src/main/java/ListCommand.java
@@ -0,0 +1,15 @@
+public class ListCommand extends Command {
+
+    protected static final String LIST_HEADER = "Here are the tasks in your list: ";
+
+    public static ListCommand create() {
+        return new ListCommand();
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) {
+        ui.outputMessage(LIST_HEADER + '\n' + taskList.toString());
+    }
+
+
+}
