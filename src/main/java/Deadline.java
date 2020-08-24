93b250ec7b6bcd40daf153127cc85cdbd8d35180
khoodehui <dehui@Khoos-MacBook-Pro.local>
2020-08-25 00:42:32 +0800

    Complete Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,42 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
-    protected String by;
+    
+    private String by;
+    private LocalDate taskDate;
+    private LocalTime taskTime;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        String[] dateArguments = by.split(" ");
+        
+        try {
+            taskDate = LocalDate.parse(dateArguments[0]);
+            
+            if (dateArguments.length == 2) {
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH[:]mm");
+                taskTime = LocalTime.parse(dateArguments[1], dtf);
+            }
+        } catch (DateTimeParseException e) {
+            System.out.println("Date and time format is invalid.");
+        }
+    }
+
+    private String formatTaskTime() {
+        String output = taskDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        if (taskTime != null) {
+            output += ", " + taskTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
+        }
+        return output;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: )" + formatTaskTime();
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,42 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+
 public class Event extends Task {
-    protected String at;
+    
+    private String at;
+    private LocalDate taskDate;
+    private LocalTime taskTime;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        String[] dateArguments = at.split(" ");
+        
+        try {
+            taskDate = LocalDate.parse(dateArguments[0]);
+
+            if (dateArguments.length == 2) {
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH[:]mm");
+                taskTime = LocalTime.parse(dateArguments[1], dtf);
+            }
+        } catch (DateTimeParseException e) {
+            System.out.println("Date and time format is invalid.");
+        }
+    }
+
+    private String formatTaskTime() {
+        String output = taskDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        if (taskTime != null) {
+            output += ", " + taskTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
+        }
+        return output;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: )" + formatTaskTime();
     }
 }
