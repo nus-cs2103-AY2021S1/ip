0cfb89ec24e514133ef1b00255217646fad659a5
Uriel Tan <urieltan3@gmail.com>
2020-08-25 02:22:38 +0800

    Add date parsing to Event

--- src/main/java/task/Event.java
+++ src/main/java/task/Event.java
@@ -1,7 +1,13 @@
 package task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
     String time;
+    LocalDate dateTime;
+    boolean isParsedDate;
+
     public Event(String line) throws EmptyStringException{
         super();
         if(line.isBlank()){
@@ -10,11 +16,23 @@ public class Event extends Task{
         String[] command = line.split(" \\/at ");
         this.item = command[0];
         this.time = command[1];
+        this.isParsedDate = false;
+        try {
+            this.dateTime = LocalDate.parse(this.time);
+            this.isParsedDate = true;
+        }
+        catch (Exception e){
+            //Date failed to parse
+        }
         taskType = "E";
     }
 
     @Override
     public String toString() {
-        return super.toString() + " (at: " + time + ")";
+        String dateString = isParsedDate ? dateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                : time;
+        return super.toString() + " (at: "
+                + dateString
+                + ")";
     }
 }
