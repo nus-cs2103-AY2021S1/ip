06c9d80553fc2b0aa13025719cc570dd53c5b3c7
lowjiefeng <lowjiefeng1998@gmail.com>
2020-08-25 01:56:59 +0800

    Add UI.java for UI logic

---
+++ src/main/java/UI.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class UI {
+    private static final String DIVIDER = "-----------------------------------------------------";
+    private static final String HELLO_MESSAGE = "Greetings! I am Duke.\nWhat can I do for you?";
+    private static final String EXIT_MESSAGE = "Bye! See you around!";
+    private static final String ERROR_MESSAGE = "Hmm I didn't quite catch that.";
+
+    private final Scanner scanner;
+
+    public UI() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showLine() {
+        System.out.println(DIVIDER);
+    }
+
+    public void showHelloMessage() {
+        System.out.println(HELLO_MESSAGE);
+    }
+
+    public void showExitMessage() {
+        System.out.println(EXIT_MESSAGE);
+    }
+
+    public void showErrorMessage(String message) {
+        System.out.println(ERROR_MESSAGE + "\n" + message);
+    }
+
+    public String readCommand() throws UIException {
+        if (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (!line.equals("")) {
+                return line;
+            } else {
+                throw new UIException("Input cannot be empty.");
+            }
+        } else {
+            throw new UIException("Invalid call to readCommand");
+        }
+    }
+
+    public void closeUI() {
+        scanner.close();
+    }
+}
---
+++ src/main/java/UIException.java
@@ -0,0 +1,5 @@
+public class UIException extends Exception {
+    public UIException (String message) {
+        super(message);
+    }
+}
