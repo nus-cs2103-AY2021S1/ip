34270dfdc6f1169a4dc7ca0a62588240a4d497fa
ktaekwon000 <ktaekwon000@gmail.com>
2020-08-24 22:02:39 +0800

    Implement Storage

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,53 +1,12 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Duke {
     private static TaskList list;
-
-    private static void makeDataFile() throws IOException {
-        File file = Paths.get(".", "data", "duke.data").toFile();
-        list = new TaskList();
-        FileOutputStream fos = new FileOutputStream(file);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(list);
-        oos.close();
-    }
+    private static Storage storage;
 
     public static void main(String[] args) {
         Ui.printGreeting();
 
-        try {
-            File file = Paths.get(".", "data", "duke.data").toFile();
-            //@@author ktaekwon000-reused
-            //Reused from https://stackoverflow.com/a/16111797 with minor modifications
-            FileInputStream fis = new FileInputStream(file);
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            list = (TaskList) ois.readObject();
-            ois.close();
-            //@@author
-        } catch (IOException | ClassNotFoundException e1) {
-            try {
-                makeDataFile();
-            } catch (FileNotFoundException e2) {
-                try {
-                    Path dataFolder = Paths.get(".", "data");
-                    Files.createDirectories(dataFolder);
-                    makeDataFile();
-                } catch (IOException e) {
-                    Ui.print(e.getMessage());
-                }
-            } catch (IOException e) {
-                Ui.print("There was an error initialising your save file.\n" + e);
-            }
-        }
+        storage = new Storage(".//data//duke.data");
+        list = storage.loadFile();
 
         Ui ui = new Ui();
 
@@ -60,18 +19,7 @@ public class Duke {
             command = Parser.parse(input);
         }
 
-        try {
-            File file = Paths.get(".", "data", "duke.data").toFile();
-            //@@author ktaekwon000-reused
-            //Reused from https://stackoverflow.com/a/16111797 with minor modifications
-            FileOutputStream fos = new FileOutputStream(file);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(list);
-            oos.close();
-            //@@author
-        } catch (IOException e) {
-            Ui.print("There was an error saving your data.\n" + e.getMessage());
-        }
+        storage.saveFile(list);
 
         Ui.printGoodbye();
     }
---
+++ src/main/java/Storage.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Storage {
+    private final Path path;
+
+    public Storage(String filePath) {
+        path = Paths.get(filePath);
+    }
+
+    private TaskList makeDataFile() throws IOException {
+        File file = path.toFile();
+        TaskList list = new TaskList();
+        FileOutputStream fos = new FileOutputStream(file);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(list);
+        oos.close();
+
+        return list;
+    }
+
+    public TaskList loadFile() {
+        TaskList list = null;
+        try {
+            File file = path.toFile();
+            //@@author ktaekwon000-reused
+            //Reused from https://stackoverflow.com/a/16111797 with minor modifications
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            list = (TaskList) ois.readObject();
+            ois.close();
+            //@@author
+        } catch (IOException | ClassNotFoundException e1) {
+            try {
+                list = makeDataFile();
+            } catch (FileNotFoundException e2) {
+                try {
+                    Files.createDirectories(path.getParent());
+                    list = makeDataFile();
+                } catch (IOException e) {
+                    Ui.print(e.getMessage());
+                }
+            } catch (IOException e) {
+                Ui.print("There was an error initialising your save file.\n" + e);
+            }
+        }
+
+        if (list == null) {
+            Ui.print("Running program without initializing a save.\nThis may cause errors when closing the program.");
+            return new TaskList();
+        } else {
+            return list;
+        }
+    }
+
+    public void saveFile(TaskList list) {
+        try {
+            File file = Paths.get(".//data//duke.data").toFile();
+            //@@author ktaekwon000-reused
+            //Reused from https://stackoverflow.com/a/16111797 with minor modifications
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(list);
+            oos.close();
+            //@@author
+        } catch (IOException e) {
+            Ui.print("There was an error saving your data.\n" + e.getMessage());
+        }
+    }
+}
