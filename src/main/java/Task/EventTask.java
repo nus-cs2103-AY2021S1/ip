413b202f53d99cdcf05e92b18435ba7c7e759b4e
Jaylenlee <e0424677@u.nus.edu>
2020-08-24 22:41:15 +0800

    Change time attribute
    EventTask now contains LocalDate and LocalTime instead of String for time.
    String time parameter for constructor now requires a specified format. ("YYYY-MM-DD HHmm-HHmm")
    Date and Time are in "dd MMM yyyy" and "kk:mm" format respectively.
    
    This allows for a more accurate time representation instead of a String variable of anything. Improves program logic.

--- src/main/java/Task/EventTask.java
+++ src/main/java/Task/EventTask.java
@@ -1,15 +1,76 @@
 package Task;
 
-public class EventTask extends Task {
-    protected String time;
+import Exception.DateTimeInvalidFormatException;
 
-    public EventTask(String description, String time) {
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.PatternSyntaxException;
+
+public class EventTask extends Task {
+    protected LocalDate date;
+    protected LocalTime startTime;
+    protected LocalTime endTime;
+    protected static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("kk:mm");
+    protected static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd MMM yyyy");
+
+    public EventTask(String description, String dateTimeString) throws DateTimeInvalidFormatException {
         super(description);
-        this.time = time;
+
+        try {
+            // dateTimeString will be in  "YYYY-MM-DD HHmm-HHmm" format
+            String[] arr = dateTimeString.split(" ");
+            int year = Integer.parseInt(arr[0].substring(0, 4));
+            int mth = Integer.parseInt(arr[0].substring(5, 7));
+            int day = Integer.parseInt(arr[0].substring(8));
+
+            int startHour = Integer.parseInt(arr[1].substring(0, 2));
+            int startMin = Integer.parseInt(arr[1].substring(2, 4));
+
+            int endHour = Integer.parseInt(arr[1].substring(5, 7));
+            int endMin = Integer.parseInt(arr[1].substring(7));
+
+            if (endHour < startHour || (endHour == startHour && endMin < startMin)) {
+                throw new DateTimeInvalidFormatException(
+                        "Action invalid. End Time less than Start Time.\n"
+                );
+            }
+
+            LocalDate date = LocalDate.of(year, mth, day);
+            LocalTime startTime = LocalTime.of(startHour, startMin);
+            LocalTime endTime = LocalTime.of(endHour, endMin);
+
+            this.date = date;
+            this.startTime = startTime;
+            this.endTime = endTime;
+        } catch(NumberFormatException nfe) {
+            throw new DateTimeInvalidFormatException(
+                    "Action invalid. Date and Time Format incorrect.\n"
+                            + "     Correct Format: YYYY-MM-DD HHmm-HHmm"
+            );
+        } catch (IndexOutOfBoundsException ioobe) {
+            throw new DateTimeInvalidFormatException(
+                    "Action invalid. Date and Time Format incorrect.\n"
+                            + "     Correct Format: YYYY-MM-DD HHmm-HHmm"
+            );
+        } catch (PatternSyntaxException pse) {
+            throw new DateTimeInvalidFormatException(
+                    "Action invalid. Date and Time Format incorrect.\n"
+                            + "     Correct Format: YYYY-MM-DD HHmm-HHmm"
+            );
+        } catch (DateTimeException dte) { // invalid date time eg 9999-13-32 2400
+            throw new DateTimeInvalidFormatException(
+                    "Action invalid. Date and Time value invalid.\n"
+            );
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.time + ")";
+        return "[E]" + super.toString() + " (at: "
+                + this.date.format(EventTask.DATE_FORMAT) + " "
+                + this.startTime .format(EventTask.TIME_FORMAT)+ "-"
+                + this.endTime.format(EventTask.TIME_FORMAT) + ")";
     }
 }
