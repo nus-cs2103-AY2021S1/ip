df576933d9747081350b85c9c22210263886fa38
chan-j-d <chan.j.d.98@gmail.com>
2020-08-25 01:34:00 +0800

    Create CommandParser.java

---
+++ src/main/java/CommandParser.java
@@ -0,0 +1,27 @@
+public class CommandParser {
+
+    protected static final String INDICATOR_CLOSING = "bye";
+    protected static final String INDICATOR_LIST = "list";
+    protected static final String INDICATOR_DONE = "done";
+    protected static final String INDICATOR_DELETE = "delete";
+
+    public static Command parse(String userInput) {
+        String firstWord = userInput.split(" ")[0];
+        if (userInput.equals(INDICATOR_CLOSING)) {
+            return ExitCommand.create();
+        } else if (userInput.equals(INDICATOR_LIST)) {
+            return ListCommand.create();
+        } else if (firstWord.equals(INDICATOR_DONE)) {
+            return DoneCommand.parse(userInput);
+        } else if (firstWord.equals(INDICATOR_DELETE)) {
+            return DeleteCommand.parse(userInput);
+        } else {
+            return AddCommand.parse(userInput);
+        }
+
+    }
+
+
+
+}
+
