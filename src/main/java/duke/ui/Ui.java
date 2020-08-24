6e5aba0dc2cde979e75768b65e09113716db7250
ruixuantan <ruixuantan@yahoo.com.sg>
2020-08-24 20:25:42 +0800

    Add java docs for ui class

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -2,6 +2,9 @@ package duke.ui;
 
 import java.util.Scanner;
 
+/**
+ * Represents the user interface class.
+ */
 public class Ui {
     private final Scanner scanner;
     private static final String DIVIDER = "---------------------------------------------------------";
@@ -11,27 +14,48 @@ public class Ui {
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
 
+    /**
+     * Construtor method.
+     * Initialises a Scanner object that reads in input from the user.
+     */
     public Ui() {
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Prints out a blank line.
+     */
     public void showBlankLine() {
         System.out.println();
     }
 
+    /**
+     * Prints the starting message of Duke.
+     */
     public void showStartMessage() {
         String message = "Greetings, what may I do for you?";
         System.out.println(LOGO + message);
     }
 
+    /**
+     * Prints out the divider line in between responses from Duke.
+     */
     public void showDivider() {
         System.out.println(DIVIDER);
     }
 
+    /**
+     * Prints out the message from Duke, after a user has input a command.
+     * @param msg the message to be printed.
+     */
     public void showStatus(String msg) {
         System.out.println(msg);
     }
 
+    /**
+     * Reads in the user input as a line.
+     * @return the String of the user input.
+     */
     public String readUserInput() {
         return this.scanner.nextLine();
     }
