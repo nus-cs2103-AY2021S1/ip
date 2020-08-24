6a2943f66fa0a04494d4b37096870b5c19533289
kaichaoang <kaichaoa@gmail.com>
2020-08-25 04:24:03 +0800

    Modify class variable to LocalDate and LocalTime
    Let's change the class String variable to LocalDate and LocalTime
    variable so that Duke can understand the input as date and time
    instead of just treating it as a String.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,37 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String dateAndOrTime;
+    protected LocalDate localDate;
+    protected LocalTime localTime;
 
-    Deadline(String description, String dateAndOrTime) {
+    Deadline(String description, String localDate, String localTime) {
         super(description);
-        this.dateAndOrTime = dateAndOrTime;
+        this.localDate = localDate != null ? LocalDate.parse(localDate) : null;
+        this.localTime = localTime != null ? LocalTime.parse(localTime) : null;
+    }
+
+    public boolean hasDate() {
+        return this.localDate != null;
+    }
+
+    public boolean hasTime() {
+        return this.localTime != null;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + dateAndOrTime + ")";
+        String toReturn = "[D]" + super.toString();
+        if (this.hasDate()) {
+            if (this.hasTime()) {
+                toReturn += "(at: " + this.localDate.format((DateTimeFormatter.ofPattern("MMM dd yyyy")))
+                                + " " + this.localTime + ")";
+            } else {
+                toReturn +="(at: " + this.localDate.format((DateTimeFormatter.ofPattern("MMM dd yyyy"))) + ")";
+            }
+        }
+        return toReturn;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,38 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String dateAndOrTime;
+    protected LocalDate localDate;
+    protected LocalTime localTime;
 
-    Event(String description, String dateAndOrTime) {
+    Event(String description, String localDate, String localTime) {
         super(description);
-        this.dateAndOrTime = dateAndOrTime;
+        this.localDate = localDate != "" ? LocalDate.parse(localDate) : null;
+        this.localTime = localTime != "" ? LocalTime.parse(localTime) : null;
+    }
+
+    public boolean hasDate() {
+        return this.localDate != null;
+    }
+
+    public boolean hasTime() {
+        return this.localTime != null;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + dateAndOrTime + ")";
+        String toReturn = "[E]" + super.toString();
+        if (this.hasDate()) {
+            if (this.hasTime()) {
+                toReturn += "(at: " + this.localDate.format((DateTimeFormatter.ofPattern("MMM dd yyyy")))
+                                + " " + this.localTime + ")";
+            } else {
+                toReturn +="(at: " + this.localDate.format((DateTimeFormatter.ofPattern("MMM dd yyyy"))) + ")";
+            }
+        }
+        return toReturn;
     }
+
 }
