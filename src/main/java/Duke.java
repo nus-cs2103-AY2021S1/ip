e16b0a40ab80e796eca152bc7cbadb16a2744667
sudogene <andywubby@gmail.com>
2020-08-24 10:06:43 +0800

    fix level 8 logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -384,7 +384,7 @@ public class Duke {
      * @param args optional and will be treated as the first user input.
      */
     public static void main(String[] args) {
-        String lvl = "7";
+        String lvl = "8";
         String logo = "                                                 _     _\n"
                 + " _______  _______  _______  _______  __   __    (c).-.(c)\n"
                 + "|       ||       ||  _    ||  _    ||  | |  |    / ._. \\ \n"
