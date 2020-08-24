fcc0b65ac6d154e7fdce530fcc3d25569c1fc740
luo-git <lluoyi.lu@gmail.com>
2020-08-24 20:54:36 +0800

    Add Main driver class

---
+++ src/luoyi/duke/Main.java
@@ -0,0 +1,30 @@
+package luoyi.duke;
+
+import luoyi.duke.data.Duke;
+import luoyi.duke.data.IDuke;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        IDuke bot = Duke.getDuke("data/duke.txt");
+        Scanner sc = new Scanner(System.in);
+
+        // Greet user
+        bot.greet();
+
+        // Handle commands until user types bye
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                break;
+            } else if (!s.matches("\\s*")) {
+                // Ignore white spaces or empty lines
+                bot = bot.handleCommand(s);
+            }
+        }
+
+        // Exit bot
+        bot.bye();
+    }
+}
