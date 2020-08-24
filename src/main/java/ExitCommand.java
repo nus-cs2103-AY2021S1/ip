4579ad692233049217a371cebc75c4930876e75a
chan-j-d <chan.j.d.98@gmail.com>
2020-08-25 01:33:16 +0800

    Create ExitCommand.java

---
+++ src/main/java/ExitCommand.java
@@ -0,0 +1,24 @@
+public class ExitCommand extends Command {
+
+    protected static final String CLOSING_MESSAGE = "Bye. Hope to see you again soon!";
+
+    public static ExitCommand create() {
+        return new ExitCommand();
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) {
+        ui.outputMessage(getClosingText());
+    }
+
+    private String getClosingText() {
+        return CLOSING_MESSAGE;
+    }
+
+    @Override
+    public boolean isExit() {
+        return true;
+    }
+
+
+}
