c4a22cc7f5865eaad7b90fdd0b95069f75a64aee
solkim-83 <65942359+solkim-83@users.noreply.github.com>
2020-08-24 22:44:13 +0800

    Add Date and Time function (Level-8)

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,42 +1,15 @@
-import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.Locale;
+public class Deadline extends Task {
 
-
-public class Deadline extends Task implements Serializable {
-    protected LocalDateTime deadlineDateTime;
-    protected String deadlineName;
+   protected String by;
 
     public Deadline(String description) {
         super(description);
-        String[] descriptionArray = description.split("/by");
-        if(descriptionArray.length == 1) { //no "/at" present
-            throw new IllegalArgumentException("Invalid input, no deadline stated");
-        } else if(descriptionArray.length > 2) {
-            throw new IllegalArgumentException("Invalid input, multiple deadlines stated");
-        }
-        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern(" dd/MM/yyyy HH:mm");
-        deadlineDateTime = LocalDateTime.parse(descriptionArray[1], inputFormat);
-        deadlineName = descriptionArray[0];
     }
 
-
-    public String getDeadlineDateTime() throws DateTimeParseException {
-        DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("dd-MMM-yyyy hh:mm a", Locale.ENGLISH);
-        return deadlineDateTime.format(outputFormat);
-    }
-
-//    public void getDeadlineOnThisDay(String deadlineDate) {
-//        if(deadlineDate == getDeadlineDateTime()) {
-//            System.out.println(taskName);
-//        }
-//    }
+    String[] stringSegments = description.split("/by", 2);
 
     @Override
     public String toString() {
-            return "[D]" + "[" + getStatusIcon() + "] " + deadlineName + " (by: " + getDeadlineDateTime() + ")";
+        return "[D]" + "[" + getStatusIcon() + "] " + stringSegments[0] + " (by: " + stringSegments[1] + ")";
     }
 }
--- src/main/java/TodoCommand.java
+++ src/main/java/TodoCommand.java
@@ -6,9 +6,6 @@ public class TodoCommand extends Command {
             throw new IllegalArgumentException("-------------------------------------------\n" +
                                                     "☹ OOPS!!! The description of a todo cannot be empty. Try again!\n"
                                                             +"-------------------------------------------");
-        } else {
-            System.out.println("-------------------------------------------\n" +
-                    "Got it. I've added this task:");
         }
     }
 }
