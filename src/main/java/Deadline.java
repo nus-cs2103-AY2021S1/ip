cf75a4cac72976546f6c2a1b529ce261edb2bc99
shermzlim <shermz.lim.jh@gmail.com>
2020-08-24 22:59:59 +0800

    Allow deadline and event to understand date and time input

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,43 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
     protected String by;
+    protected LocalDate byDate;
+    protected LocalDateTime byDateTime;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        this.setByDateAndTime();
+    }
+
+    private void setByDateAndTime() {
+        try {
+            this.byDateTime = LocalDateTime.parse(this.by);
+            this.byDate = this.byDateTime.toLocalDate();
+        } catch (DateTimeParseException e) {
+            try {
+                this.byDate = LocalDate.parse(this.by);
+            } catch (DateTimeParseException ignored) { }
+        }
+    }
+
+    private String getFormattedBy() {
+        if (this.byDateTime != null) {
+            return this.byDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+        } else if (this.byDate != null) {
+            return this.byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } else {
+            return this.by;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + getFormattedBy() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,43 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+
 public class Event extends Task {
     protected String at;
+    protected LocalDate atDate;
+    protected LocalDateTime atDateTime;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        this.setAtDateAndTime();
+    }
+
+    private void setAtDateAndTime() {
+        try {
+            this.atDateTime = LocalDateTime.parse(this.at);
+            this.atDate = this.atDateTime.toLocalDate();
+        } catch (DateTimeParseException e) {
+            try {
+                this.atDate = LocalDate.parse(this.at);
+            } catch (DateTimeParseException ignored) { }
+        }
+    }
+
+    private String getFormattedAt() {
+        if (this.atDateTime != null) {
+            return this.atDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+        } else if (this.atDate != null) {
+            return this.atDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } else {
+            return this.at;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + this.getFormattedAt() + ")";
     }
 }
