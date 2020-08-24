e2d8178a6176ad917c197f7343ef5f605e860c81
Junhua <wenjh1998@gmail.com>
2020-08-24 11:05:19 +0800

    Create DeleteCommand.java

---
+++ src/main/java/DeleteCommand.java
@@ -0,0 +1,36 @@
+public class DeleteCommand extends Command{
+
+    private String arguments;
+
+    public DeleteCommand(String arguments){
+        super(false, arguments);
+    }
+
+    @Override
+    public void execute(TaskList taskList, UI ui, Storage storage) throws UltronException{
+
+        //Initialise index
+        int index = Parser.parseInteger(this.getArguments());
+
+        //Check if the index is out of range
+        if (index >= taskList.size()) {
+
+            //Throw an Ultron exception if it is out of range
+            throw new UltronException("delete",
+                    Integer.toString(index + 1),
+                    ExceptionType.INVALID_ARGUMENT);
+        }
+
+        //Get the task
+        Task tsk = taskList.get(index);
+
+        //Remove the task
+        taskList.remove(index);
+
+        //Print the delete message
+        System.out.printf("What are you doing removing this?!?!\n  "
+                        + "%s\nNow you have %d burdens%n",
+                tsk,
+                taskList.size());
+    }
+}
