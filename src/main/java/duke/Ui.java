2ae5193801e774a8b5fa4e5d6b74eb9a2280b14c
Lim Wei Quan, Ernest <ernestlim8@gmail.com>
2020-08-24 16:19:20 +0800

    Rename branch

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -7,7 +7,7 @@ import task.Task;
 public class Ui {
   private String line = "____________________________________________________________";
   String welcomeMessage = "Hello! I'm Duke\n What can I do for you?";
-  String goodbyeMessage = "Bye! Message me anytime!!";
+  String goodbyeMessage = "Bye! Message me anytime!";
   Scanner sc = new Scanner(System.in);
 
   public void showLine() {
