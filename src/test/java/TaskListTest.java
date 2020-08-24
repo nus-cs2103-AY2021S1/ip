8f9607b093b2d6330f4d907eb24b75da7365f5fa
Tan Zhuo Yao <e0406711@u.nus.edu>
2020-08-24 21:22:55 +0800

    Add JUnit testing files

---
+++ src/test/java/TaskListTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TaskListTest {
+    @Test
+    public void testEmptyTaskList(){
+        assertEquals("", new TaskList().iterateToDo());
+    }
+
+    @Test
+    public void testFilledTaskList() {
+        List<Task> list = new ArrayList<>();
+        list.add(new Todo(false, 1, "Wash clothes"));
+        assertEquals("1. [T][✗] Wash clothes\n", new TaskList(list).iterateToDo());
+    }
+
+    @Test
+    public void testDeleteTask() {
+        List<Task> list = new ArrayList<>();
+        list.add(new Todo(false, 1, "Wash clothes"));
+        list.add(new Deadline(false, 2, "Do testing", "2020-10-10"));
+        TaskList test = new TaskList(list);
+        test.deleteTask(1);
+        assertEquals("1. [D][✗] Do testing (by: 10 Oct 2020)\n", test.iterateToDo());
+    }
+}
---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+        public void testTodo() {
+            Task test = new Todo(false, 1, "Wash Clothes");
+            assertEquals("[T][✗] Wash Clothes", test.toString());
+    }
+
+    @Test
+    public void testDeadline() {
+        Task test = new Deadline(false, 1, "Wash Clothes", "2020-10-23");
+        assertEquals("[D][✗] Wash Clothes (by: 23 Oct 2020)", test.toString());
+    }
+
+    @Test
+    public void testMarkDone() {
+        Task test = new Event(false, 1, "Wash Car", "2020-11-19");
+        test = test.markDone();
+        assertEquals("[E][✓] Wash Car (at: 19 Nov 2020)", test.toString());
+    }
+}
