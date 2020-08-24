3e8259b3a2add7cdbc71d326f57c997bda502c6a
Peh Jun Siang <austin9090@hotmail.com>
2020-08-25 03:01:03 +0800

    Add TaskList to hold a list of Tasks

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,28 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private ArrayList<Task> taskList = new ArrayList<>();
+
+    public int size(){
+        return taskList.size();
+    }
+
+    public boolean addAll(List<Task> list){
+        return taskList.addAll(list);
+    }
+
+    public boolean add(Task task){
+        return taskList.add(task);
+    }
+
+    public Task get(int index){
+        return taskList.get(index);
+    }
+
+    public Task delete(int position){
+        return taskList.remove(position);
+    }
+}
