fa1e9169a8007d9d15be2294140928d228b69eb5
shermzlim <shermz.lim.jh@gmail.com>
2020-08-25 02:07:50 +0800

    Refactor all user interactions into Ui class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,89 +1,64 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.List;
 import java.util.Arrays;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
-import java.time.format.DateTimeFormatter;
 
 public class Duke {
+
     private Storage storage;
     private TaskList tasks;
+    private Ui ui;
 
     public Duke(String filePath) {
+        this.ui = new Ui();
         this.storage = new Storage(filePath);
         try {
             this.tasks = new TaskList(this.storage.load());
         } catch (DukeException e) {
-            System.out.println(e.getMessage());
+            ui.showLoadingError();
             this.tasks = new TaskList();
         }
     }
 
     public static void main(String[] args) {
-        String name = "Omega";
-        Duke.printHorizontalLine();
-        System.out.println("Hi! I am " + name + ", your personal assistant.");
-        System.out.println("How may I help you today?");
-        Duke.printHorizontalLine();
-        new Duke("data/duke.txt").interactWithUser();
+        new Duke("data/duke.txt").run();
     }
 
-    private void interactWithUser() {
+    public void run() {
+        ui.showWelcome();
         boolean exitProgram = false;
-        Scanner scn = new Scanner(System.in);
         while (!exitProgram) {
-            System.out.println();
-            if (!scn.hasNext()) {
-                exitProgram = true;
-            } else {
-                String userInput = scn.nextLine();
-                String[] userInputArgs = userInput.split(" ");
-                Duke.printHorizontalLine();
-                try {
-                    if (userInput.equals(Command.BYE.getName())) {
-                        System.out.println("Goodbye! Shutting down now...");
-                        exitProgram = true;
-                    } else if (userInputArgs[0].equals(Command.LIST.getName())) {
-                        List<Task> taskList;
-                        try {
-                            LocalDate date = LocalDate.parse(userInputArgs[1]);
-                            System.out.println(String.format(
-                                    "Here are the tasks in your list on %s:",
-                                    date.format(DateTimeFormatter.ofPattern("MMM d yyyy"))));
-                            taskList = this.tasks.getListOfTasks(date);
-                        } catch (ArrayIndexOutOfBoundsException | DateTimeParseException e) {
-                            System.out.println("Here are the tasks in your list:");
-                            taskList = this.tasks.getListOfTasks();
-                        }
-                        int idx = 1;
-                        for (Task task : taskList) {
-                            System.out.println(String.format("%d.%s", idx, task.toString()));
-                            idx += 1;
-                        }
-                    } else {
-                        if (userInputArgs[0].equals(Command.DONE.getName())) {
-                            Task task = markTaskDone(userInputArgs);
-                            System.out.println("Nice! I've marked this task as done:");
-                            Duke.printWithIndent(task.toString());
-                        } else if (userInputArgs[0].equals(Command.DELETE.getName())) {
-                            Task task = deleteTask(userInputArgs);
-                            System.out.println("Noted. I've removed this task:");
-                            Duke.printWithIndent(task.toString());
-                            System.out.println(String.format("Now you have %d tasks in the list.", this.tasks.size()));
-                        } else {
-                            Task task = createTask(userInputArgs);
-                            this.tasks.addTask(task);
-                            System.out.println("Got it. I've added this task:");
-                            Duke.printWithIndent(task.toString());
-                            System.out.println(String.format("Now you have %d tasks in the list.", this.tasks.size()));
-                        }
-                        storage.saveTaskList(this.tasks);
+            try {
+                ui.showBlankLine();
+                String fullCommand = ui.readCommand();
+                ui.showLine();
+                String[] userInputArgs = fullCommand.split(" ");
+                if (fullCommand.equals(Command.BYE.getName())) {
+                    exitProgram = true;
+                    ui.showGoodbye();
+                } else if (userInputArgs[0].equals(Command.LIST.getName())) {
+                    try {
+                        LocalDate date = LocalDate.parse(userInputArgs[1]);
+                        ui.showTasksOnDate(this.tasks.getListOfTasks(date), date);
+                    } catch (ArrayIndexOutOfBoundsException | DateTimeParseException e) {
+                        ui.showAllTasks(this.tasks.getListOfTasks());
                     }
-                } catch (DukeException err) {
-                    System.out.println(err.getMessage());
+                } else {
+                    if (userInputArgs[0].equals(Command.DONE.getName())) {
+                        ui.showTaskDone(markTaskDone(userInputArgs));
+                    } else if (userInputArgs[0].equals(Command.DELETE.getName())) {
+                        Task task = deleteTask(userInputArgs);
+                        ui.showTaskDeleted(task, this.tasks.size());
+                    } else {
+                        Task task = createTask(userInputArgs);
+                        this.tasks.addTask(task);
+                        ui.showTaskAdded(task, this.tasks.size());
+                    }
+                    storage.saveTaskList(this.tasks);
                 }
-                Duke.printHorizontalLine();
+            } catch (DukeException err) {
+                ui.showError(err.getMessage());
+            } finally {
+                ui.showLine();
             }
         }
     }
@@ -148,14 +123,5 @@ public class Duke {
 
     private static String reassembleString(String[] arr, int from, int to) {
         return String.join(" ", Arrays.copyOfRange(arr, from, to));
-    }
-
-    private static void printHorizontalLine() {
-        String horizontalLine = "---------------------------------------------";
-        System.out.println(horizontalLine);
-    }
-
-    private static void printWithIndent(String str) {
-        System.out.println("  " + str);
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,92 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.util.List;
+
+public class Ui {
+    Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showWelcome() {
+        String name = "Omega";
+        printHorizontalLine();
+        System.out.println("Hi! I am " + name + ", your personal assistant.");
+        System.out.println("How may I help you today?");
+        printHorizontalLine();
+    }
+
+    public void showGoodbye() {
+        System.out.println("Goodbye! Shutting down now...");
+    }
+
+    public void showLoadingError() {
+        printHorizontalLine();
+        System.out.println("Sorry, there is an error loading the data");
+        printHorizontalLine();
+    }
+
+    public void showError(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public void showAllTasks(List<Task> taskList) {
+        System.out.println("Here are the tasks in your list:");
+        showTasks(taskList);
+    }
+
+    public void showTasksOnDate(List<Task> taskList, LocalDate date) {
+        System.out.println(String.format(
+                "Here are the tasks in your list on %s:",
+                date.format(DateTimeFormatter.ofPattern("MMM d yyyy"))));
+        showTasks(taskList);
+    }
+
+    public void showTasks(List<Task> taskList) {
+        int idx = 1;
+        for (Task task : taskList) {
+            System.out.println(String.format("%d.%s", idx, task.toString()));
+            idx += 1;
+        }
+    }
+
+    public void showTaskDone(Task task) {
+        System.out.println("Nice! I've marked this task as done:");
+        printWithIndent(task.toString());
+    }
+
+    public void showTaskDeleted(Task task, int numTasksLeft) {
+        System.out.println("Noted. I've removed this task:");
+        printWithIndent(task.toString());
+        System.out.println(String.format("Now you have %d tasks in the list.", numTasksLeft));
+    }
+
+    public void showTaskAdded(Task task, int numTasksLeft) {
+        System.out.println("Got it. I've added this task:");
+        printWithIndent(task.toString());
+        System.out.println(String.format("Now you have %d tasks in the list.", numTasksLeft));
+    }
+
+    public void showBlankLine() {
+        System.out.println();
+    }
+
+    public void showLine() {
+        this.printHorizontalLine();
+    }
+
+    private void printHorizontalLine() {
+        String horizontalLine = "---------------------------------------------";
+        System.out.println(horizontalLine);
+    }
+
+    private void printWithIndent(String str) {
+        System.out.println("  " + str);
+    }
+}
