e90d98a7929baeac864917a2038186bd4e087d29
Jaylenlee <e0424677@u.nus.edu>
2020-08-24 22:33:07 +0800

    Add DateTimeInvalidFormatException
    DateTimeInvalidFormatException is an exception when the input date time format differ from expected.
    It extends from InvalidActionException as a form of invalid action.

---
+++ src/main/java/Exception/DateTimeInvalidFormatException.java
@@ -0,0 +1,12 @@
+package Exception;
+
+public class DateTimeInvalidFormatException extends InvalidActionException {
+
+    public DateTimeInvalidFormatException() {
+        super("Action invalid. Date and Time Format incorrect.");
+    }
+
+    public DateTimeInvalidFormatException(String message) {
+        super(message);
+    }
+}
