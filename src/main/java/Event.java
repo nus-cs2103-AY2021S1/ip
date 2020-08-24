544f71e1c6ec22ff58858f8fc20634862241032d
Zhu Yuxuan <e0424709@u.nus.edu>
2020-08-24 22:28:48 +0800

    Modify Deadline.java, Duke.java, Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,6 +8,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + duration + ")";
+        return "[E]" + super.toString() + " (at: " + duration + ")";
     }
 }
