22173959952bb111ca8dcb87b90a1c745a1d6f01
Junhua <wenjh1998@gmail.com>
2020-08-24 11:05:23 +0800

    Create HelpCommand.java

---
+++ src/main/java/HelpCommand.java
@@ -0,0 +1,23 @@
+public class HelpCommand extends Command {
+
+    public HelpCommand(String arguments){
+        super(false, arguments);
+    }
+
+    @Override
+    public void execute(TaskList taskList, UI ui, Storage storage) throws UltronException {
+
+        if (this.getArguments().trim().length() > 0){
+            throw new UltronException("list", ExceptionType.TOO_MUCH_ARGUMENTS);
+        }
+
+        System.out.println("Heh I guess I could help an insect like you:\n"
+                + "- help                      : Get help for the commands\n"
+                + "- todo (name)               : Adds a todo to the list\n"
+                + "- event (name) /at (date)   : Adds an event at date\n"
+                + "- deadline (name) /by (date): "
+                + "Adds a deadline which expires by date\n"
+                + "- delete (number)           : Removes a todo from the list\n"
+        );
+    }
+}
