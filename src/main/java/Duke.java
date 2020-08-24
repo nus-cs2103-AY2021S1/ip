cb47b49286da2872a8aa879fae68f0ec946c5c56
Penn Han <leepennhan98@gmail.com>
2020-08-24 21:31:43 +0800

    Create properties in Duke Class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
+
+    private DukeFile dukeFile;
+    private ArrayList<Task> arraylst;
+    private Processor processor;
+
+    public Duke(String filePath) {
+        this.arraylst = new ArrayList<>();
+        this.dukeFile = DukeFile.createDukeFile(filePath);
+        this.processor = new Processor();
+    }
+
+    public void run() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -12,29 +23,25 @@ public class Duke {
         System.out.println("Initializing\n" + logo);
         System.out.println("Yo what's up man, it's ya boi DUKE \n" + "What can I do for you today, Sir?");
 
-        ArrayList<Task> arraylst = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        Processor processor = new Processor();
-        DukeFile dukeFile = DukeFile.createDukeFile("Saved");
-
         while (sc.hasNext()) {
             String cmd = sc.nextLine();
             if (!cmd.equals("bye")) {
                 try {
                     String[] stringarr = cmd.split(" ");
                     if (stringarr[0].equals("list")) {
-                        processor.processorList(arraylst);
+                        this.processor.processorList(arraylst);
                     } else if (stringarr[0].equals("done")) {
                         int index = Integer.parseInt(stringarr[1]);
-                        String record = processor.processorDone(arraylst, index);
-                        dukeFile.updateRecord(record, index);
+                        String record = this.processor.processorDone(arraylst, index);
+                        this.dukeFile.updateRecord(record, index);
                     } else if (stringarr[0].equals("delete")) {
                         int index = Integer.parseInt(stringarr[1]);
-                        processor.processorDelete(arraylst, index);
-                        dukeFile.deleteRecord(index);
+                        this.processor.processorDelete(arraylst, index);
+                        this.dukeFile.deleteRecord(index);
                     } else {
-                        String record = processor.processorAdd(cmd, arraylst);
-                        dukeFile.saveRecord(record);
+                        String record = this.processor.processorAdd(cmd, arraylst);
+                        this.dukeFile.saveRecord(record);
                     }
                 } catch (DukeException e) {
                     System.out.println(e.getMessage());
@@ -50,5 +57,10 @@ public class Duke {
             System.out.println("Sorry an unexpected error occurred.");
             e.printStackTrace();
         }
+    };
+
+
+    public static void main(String[] args) {
+        new Duke("Saved").run();
     }
 }
