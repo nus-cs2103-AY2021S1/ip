2a30cd21dfa1592cb55eb91d44307dff766d9fdc
chan-j-d <chan.j.d.98@gmail.com>
2020-08-25 01:12:30 +0800

    Update AddCommand to incorporate TaskParser

---
+++ src/main/java/AddCommand.java
@@ -0,0 +1,29 @@
+public class AddCommand implements Command {
+
+    protected static final String ADDED_MESSAGE = "Got it. I've added this task: ";
+
+    private Task task;
+
+    private AddCommand(Task task) {
+        this.task = task;
+    }
+
+    public static AddCommand parse(String taskCommand) {
+        return new AddCommand(TaskParser.parse(taskCommand));
+    }
+
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) {
+        taskList.add(task);
+        ui.outputMessage(createAddMessage(task, taskList));
+        storage.updateFile(taskList);
+    }
+
+    private String createAddMessage(Task task, TaskList taskList) {
+        return ADDED_MESSAGE + '\n'
+                + "   " + task + '\n'
+                + taskList.createTaskNumberCountMessage();
+    }
+
+}
