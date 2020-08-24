961e80012524fbb384c21e77b1d0900257870128
MatthiasLHK <matthiasli1998@hotmail.com>
2020-08-24 16:03:16 +0800

    Test commit

--- src/main/java/task.java
+++ src/main/java/task.java
@@ -35,7 +35,7 @@ public class task {
         else{
             done = "[X]";
         }
-        return this.index + ". " + done + " " + this.name;
+        return done + " " + this.name;
     }
 
     public String read2(){
@@ -57,6 +57,5 @@ public class task {
         System.out.println(line1);
         System.out.println(line2);
         System.out.println(line3);
-         
     }
 }
