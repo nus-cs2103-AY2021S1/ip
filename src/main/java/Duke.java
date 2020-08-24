d4da551eedac2366f3cf6848caa5e9b07e57a09f
augustinekau <augustinekau@gmail.com>
2020-08-24 23:29:18 +0800

    Correct code to align with Java Coding Standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,7 +16,7 @@ public class Duke {
         System.out.println("Hello! I'm Dude\n" + "What can I do for you today?");
         System.out.println(horizontalDiv);
         WhileLoop:
-        while(in.hasNextLine()) {
+        while (in.hasNextLine()) {
             String str = in.nextLine();
             String[] arr = str.split(" ", 2);
 
@@ -24,47 +24,69 @@ public class Duke {
             Command cmd = Command.valueOf(upperCaseCmd);
 
             switch (cmd) {
-                case BYE:
-                    System.out.println(horizontalDiv);
-                    System.out.println("Bye! Hope to see you again soon!");
-                    System.out.println(horizontalDiv);
-                    break WhileLoop;
+            case BYE:
+                System.out.println(horizontalDiv);
+                System.out.println("Bye! Hope to see you again soon!");
+                System.out.println(horizontalDiv);
+                break WhileLoop;
 
-                case LIST:
+            case LIST:
+                System.out.println(horizontalDiv);
+                if (ls.isEmpty()) {
+                    System.out.println("Congratulations! You have currently no task.");
+                } else {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < ls.size(); i++) {
+                        Task task = ls.get(i);
+                        int num = i + 1;
+                        System.out.println(num + ". " + task.toString());
+                    }
+                }
+                System.out.println(horizontalDiv);
+                break;
+
+            case DONE:
+                try {
+                    int numToBeMarkedAsDone = Integer.parseInt(str.substring(str.length() - 1)) - 1;
+                    Task tsk = ls.get(numToBeMarkedAsDone);
+                    tsk.markAsDone();
+                    ls.set(numToBeMarkedAsDone, tsk);
                     System.out.println(horizontalDiv);
-                    if (ls.isEmpty()) {
-                        System.out.println("Congratulations! You have currently no task.");
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(tsk.toString());
+                    System.out.println(horizontalDiv);
+                } catch (Exception ex) {
+                    System.out.println(horizontalDiv);
+                    System.out.println("Sorry! The number does not exist in the list!");
+                    System.out.println(horizontalDiv);
+                }
+                break;
+
+            case TODO:
+                try {
+                    Task newTask = new Todo(arr[1]);
+                    ls.add(newTask);
+                    System.out.println(horizontalDiv);
+                    System.out.println("Got it. I've added this task: \n" + newTask.toString());
+                    if(ls.size() > 1) {
+                        System.out.println("Now you have " + ls.size() + " tasks in the list.");
                     } else {
-                        System.out.println("Here are the tasks in your list:");
-                        for (int i = 0; i < ls.size(); i++) {
-                            Task task = ls.get(i);
-                            int num = i + 1;
-                            System.out.println(num + ". " + task.toString());
-                        }
+                        System.out.println("Now you have " + ls.size() + " task in the list.");
                     }
                     System.out.println(horizontalDiv);
-                    break;
+                } catch (Exception ex) {
+                    System.out.println(horizontalDiv);
+                    System.out.println("Sorry! The description of todo cannot be empty!!");
+                    System.out.println(horizontalDiv);
+                }
+                break;
 
-                case DONE:
+            case DEADLINE:
+                try {
+                    String secondStr = arr[1];
+                    String[] arrOfStr = secondStr.split(" /by ", 2);
                     try {
-                        int numToBeMarkedAsDone = Integer.parseInt(str.substring(str.length() - 1)) - 1;
-                        Task tsk = ls.get(numToBeMarkedAsDone);
-                        tsk.markAsDone();
-                        ls.set(numToBeMarkedAsDone, tsk);
-                        System.out.println(horizontalDiv);
-                        System.out.println("Nice! I've marked this task as done: ");
-                        System.out.println(tsk.toString());
-                        System.out.println(horizontalDiv);
-                    } catch (Exception ex) {
-                        System.out.println(horizontalDiv);
-                        System.out.println("Sorry! The number does not exist in the list!");
-                        System.out.println(horizontalDiv);
-                    }
-                    break;
-
-                case TODO:
-                    try {
-                        Task newTask = new Todo(arr[1]);
+                        Task newTask = new Deadline(arrOfStr[0], arrOfStr[1]);
                         ls.add(newTask);
                         System.out.println(horizontalDiv);
                         System.out.println("Got it. I've added this task: \n" + newTask.toString());
@@ -76,72 +98,25 @@ public class Duke {
                         System.out.println(horizontalDiv);
                     } catch (Exception ex) {
                         System.out.println(horizontalDiv);
-                        System.out.println("Sorry! The description of todo cannot be empty!!");
+                        System.out.println("Sorry! Please enter a date for the deadline using the command '/by'!");
                         System.out.println(horizontalDiv);
                     }
-                    break;
+                } catch (Exception ex) {
+                    System.out.println(horizontalDiv);
+                    System.out.println("Sorry! The description of deadline cannot be empty!");
+                    System.out.println(horizontalDiv);
+                }
+                break;
 
-                case DEADLINE:
+            case EVENT:
+                try {
+                    String secondStr = arr[1];
+                    String[] arrOfStr = secondStr.split(" /at ", 2);
                     try {
-                        String secondStr = arr[1];
-                        String[] arrOfStr = secondStr.split(" /by ", 2);
-                        try {
-                            Task newTask = new Deadline(arrOfStr[0], arrOfStr[1]);
-                            ls.add(newTask);
-                            System.out.println(horizontalDiv);
-                            System.out.println("Got it. I've added this task: \n" + newTask.toString());
-                            if(ls.size() > 1) {
-                                System.out.println("Now you have " + ls.size() + " tasks in the list.");
-                            } else {
-                                System.out.println("Now you have " + ls.size() + " task in the list.");
-                            }
-                            System.out.println(horizontalDiv);
-                        } catch (Exception ex) {
-                            System.out.println(horizontalDiv);
-                            System.out.println("Sorry! Please enter a date for the deadline using the command '/by'!");
-                            System.out.println(horizontalDiv);
-                        }
-                    } catch (Exception ex) {
+                        Task newTask = new Event(arrOfStr[0], arrOfStr[1]);
+                        ls.add(newTask);
                         System.out.println(horizontalDiv);
-                        System.out.println("Sorry! The description of deadline cannot be empty!");
-                        System.out.println(horizontalDiv);
-                    }
-                    break;
-
-                case EVENT:
-                    try {
-                        String secondStr = arr[1];
-                        String[] arrOfStr = secondStr.split(" /at ", 2);
-                        try {
-                            Task newTask = new Event(arrOfStr[0], arrOfStr[1]);
-                            ls.add(newTask);
-                            System.out.println(horizontalDiv);
-                            System.out.println("Got it. I've added this task: \n" + newTask.toString());
-                            if (ls.size() > 1) {
-                                System.out.println("Now you have " + ls.size() + " tasks in the list.");
-                            } else {
-                                System.out.println("Now you have " + ls.size() + " task in the list.");
-                            }
-                            System.out.println(horizontalDiv);
-                        } catch (Exception ex) {
-                            System.out.println(horizontalDiv);
-                            System.out.println("Sorry! Please enter a duration for the event using the command '/at'!");
-                            System.out.println(horizontalDiv);
-                        }
-                    } catch (Exception ex) {
-                        System.out.println(horizontalDiv);
-                        System.out.println("Sorry! The description of event cannot be empty!");
-                        System.out.println(horizontalDiv);
-                    }
-                    break;
-                case DELETE:
-                    try {
-                        int numToBeDeleted = Integer.parseInt(str.substring(str.length() - 1)) - 1;
-                        Task tsk = ls.get(numToBeDeleted);
-                        ls.remove(numToBeDeleted);
-                        System.out.println(horizontalDiv);
-                        System.out.println("Successfully deleted this task:");
-                        System.out.println(tsk.toString());
+                        System.out.println("Got it. I've added this task: \n" + newTask.toString());
                         if (ls.size() > 1) {
                             System.out.println("Now you have " + ls.size() + " tasks in the list.");
                         } else {
@@ -150,14 +125,39 @@ public class Duke {
                         System.out.println(horizontalDiv);
                     } catch (Exception ex) {
                         System.out.println(horizontalDiv);
-                        System.out.println("Sorry! The number to be deleted does not exist in the list!");
+                        System.out.println("Sorry! Please enter a duration for the event using the command '/at'!");
                         System.out.println(horizontalDiv);
                     }
-                    break;
-                default:
+                } catch (Exception ex) {
                     System.out.println(horizontalDiv);
-                    System.out.println("Sorry! But I don't know what that means!");
+                    System.out.println("Sorry! The description of event cannot be empty!");
                     System.out.println(horizontalDiv);
+                }
+                break;
+            case DELETE:
+                try {
+                    int numToBeDeleted = Integer.parseInt(str.substring(str.length() - 1)) - 1;
+                    Task tsk = ls.get(numToBeDeleted);
+                    ls.remove(numToBeDeleted);
+                    System.out.println(horizontalDiv);
+                    System.out.println("Successfully deleted this task:");
+                    System.out.println(tsk.toString());
+                    if (ls.size() > 1) {
+                        System.out.println("Now you have " + ls.size() + " tasks in the list.");
+                    } else {
+                        System.out.println("Now you have " + ls.size() + " task in the list.");
+                    }
+                    System.out.println(horizontalDiv);
+                } catch (Exception ex) {
+                    System.out.println(horizontalDiv);
+                    System.out.println("Sorry! The number to be deleted does not exist in the list!");
+                    System.out.println(horizontalDiv);
+                }
+                break;
+            default:
+                System.out.println(horizontalDiv);
+                System.out.println("Sorry! But I don't know what that means!");
+                System.out.println(horizontalDiv);
             }
         }
     }
