1b7843a49c5f2161e636dd047c0337d75c8df8bf
jazerler <lerjazer@gmail.com>
2020-08-24 23:17:17 +0800

    Add test directory

---
+++ src/test/java/DukeTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+
+	@Test
+	public void dummyTest() {
+		assertEquals(2, 2);
+	}
+
+}
