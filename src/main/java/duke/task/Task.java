14383a1f74325eeb218e837858cf22a89f66599a
rizhaow <rizhaow@gmail.com>
2020-08-24 12:03:19 +0800

    fix: Fix task type tag of deadline task
    Refactor task type tags to a separate enum for better visibility

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -4,42 +4,42 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 public class Task {
-    private static final String DONE = "[x]";
-    private static final String NOT_DONE = "[ ]";
+  private static final String DONE = "[x]";
+  private static final String NOT_DONE = "[ ]";
 
-    private final String name;
-    private String type;
-    private boolean isDone;
-    private Optional<LocalDate> date;
+  private final String name;
+  private TaskType taskType;
+  private boolean isDone;
+  private Optional<LocalDate> date;
 
-    public Task(String name, String type) {
-        this.name = name;
-        this.isDone = false;
-        this.type = type;
-        this.date = Optional.empty();
-    }
+  public Task(String name, TaskType type) {
+    this.name = name;
+    this.isDone = false;
+    this.taskType = type;
+    this.date = Optional.empty();
+  }
 
-    public Task(String name, String type, LocalDate date) {
-        this.name = name;
-        this.isDone = false;
-        this.type = type;
-        this.date = Optional.of(date);
-    }
+  public Task(String name, TaskType type, LocalDate date) {
+    this.name = name;
+    this.isDone = false;
+    this.taskType = type;
+    this.date = Optional.of(date);
+  }
 
-    private String getStatus() {
-        return this.isDone ? Task.DONE : Task.NOT_DONE;
-    }
+  public String getStatus() {
+    return this.isDone ? Task.DONE : Task.NOT_DONE;
+  }
 
-    public Optional<LocalDate> getDate() {
-        return this.date;
-    }
+  public Optional<LocalDate> getDate() {
+    return this.date;
+  }
 
-    public void setDone(boolean isDone) {
-        this.isDone = isDone;
-    }
+  public void setDone(boolean isDone) {
+    this.isDone = isDone;
+  }
 
-    @Override
-    public String toString() {
-        return String.format("%s%s %s", this.type, this.getStatus(), this.name);
-    }
+  @Override
+  public String toString() {
+    return String.format("%s%s %s", this.taskType, this.getStatus(), this.name);
+  }
 }
