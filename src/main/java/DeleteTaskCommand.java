1b5687a1bbc9668f29696a54038f90663abb0ce4
yongping827 <yongping827@users.noreply.github.com>
2020-08-24 14:00:24 +0800

    Add methods in classes DeleteTaskCommand and Parser

--- src/main/java/DeleteTaskCommand.java
+++ src/main/java/DeleteTaskCommand.java
@@ -3,6 +3,6 @@ package main.java;
 class DeleteTaskCommand extends Command {
     @Override
     protected void execute(Storage storage, TaskList tasks, Ui ui) {
-        
+
     }
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,4 +1,30 @@
 package main.java;
 
 class Parser {
+
+    Command parse(String input) {
+
+        String[] prefixParser = input.split(" ", 2);
+        String prefix = prefixParser[0];
+        String body = prefixParser[1];
+
+        switch(prefix) {
+            case("bye"):
+                return new ExitCommand();
+            case("deadline"):
+                return new CreateDeadlineCommand();
+            case("delete"):
+                return new DeleteTaskCommand();
+            case("done"):
+                return new CompleteTaskCommand();
+            case("event"):
+                return new CreateEventCommand();
+            case("list"):
+                return new ListTasksCommand();
+            case("todo"):
+                return new CreateTodoCommand();
+            default:
+                return new InvalidCommand();
+        }
+    }
 }
