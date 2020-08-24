abc569ea2977563371e398bbac88179c900f7df7
Junhua <wenjh1998@gmail.com>
2020-08-24 11:05:18 +0800

    Create ByeCommand.java

---
+++ src/main/java/ByeCommand.java
@@ -0,0 +1,19 @@
+public class ByeCommand extends Command{
+
+    public ByeCommand(String arguments){
+        super(true, arguments);
+    }
+
+    @Override
+    public void execute(TaskList taskList, UI ui, Storage storage) throws UltronException {
+
+        if (this.getArguments().trim().length() > 0){
+            throw new UltronException("bye", ExceptionType.TOO_MUCH_ARGUMENTS);
+        }
+
+        //Save the storage
+        storage.writeAll(taskList.getList());
+
+        System.out.printf("Clearly you were not worth my time.\n");
+    }
+}
