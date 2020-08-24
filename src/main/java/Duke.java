8f8b564e85009a8c85671c22a15ef71cfca66cfc
kaichaoang <kaichaoa@gmail.com>
2020-08-25 05:00:47 +0800

    Update missing methods from branch-Level-8
    There were some methods there were missing in the
    master's Duke class after branch-Level-8 was merged
    into.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Arrays;
 import java.io.IOException;
 import java.io.File;
 import java.io.BufferedReader;
@@ -46,8 +47,8 @@ public class Duke {
                     System.out.println("list: displays a sequential view of past inputs\n" +
                             "done <task number>: denotes a task as done by checking it\n" +
                             "delete <task number>: deletes an existing task\n" +
-                            "deadline <description> /by <date/time>: adds a deadline with desired date/time\n" +
-                            "event <description> /at <date/time>: adds an event with desired date/time\n" +
+                            "deadline <description> /by <YYYY-MM-DD> <HH:MM>: adds a deadline with desired date/time\n" +
+                            "event <description> /at <YYYY-MM-DD> <HH:MM>: adds an event with desired date/time\n" +
                             "todo <description>: adds a todo task\n" +
                             "bye: terminates program");
                 } else if (keyWord.equals("list")) {
@@ -89,11 +90,11 @@ public class Duke {
                         pastInputs.add(toDo);
                         System.out.println("Duke added into your task list:\n" + toDo);
                     } else if (keyWord.equals("deadline")) {
-                        Deadline deadline = new Deadline(data[0], data[1]);
+                        Deadline deadline = new Deadline(data[0], data[1], data[2]);
                         pastInputs.add(deadline);
                         System.out.println("Duke added into your task list:\n" + deadline);
                     } else if (keyWord.equals("event")) {
-                        Event event = new Event(data[0], data[1]);
+                        Event event = new Event(data[0], data[1], data[2]);
                         pastInputs.add(event);
                         System.out.println("Duke added into your task list:\n" + event);
                     }
@@ -132,8 +133,8 @@ public class Duke {
         return word;
     }
 
-    //returns an array of description and date/time(if applicable)
-    public static String[] processInput(String[] array) throws EmptyTaskException {
+    //returns an array with index 0:description, index 1:date (if applicable), index 2:time (if applicable)
+    public static String[] processInput(String[] array) throws DukeException {
         if (array.length <= 1) {
             if (array[0].equals("event")) {
                 throw new EmptyTaskException("☹ OOPS!!! The description of a event cannot be empty.");
@@ -146,48 +147,92 @@ public class Duke {
             }
         } else if (array[0].equals("event")) {
             String des = "";
-            String dateAndOrTime = "";
+            String date = "";
+            String time = "";
+
             boolean toBreak = false;
+            int pivotIndex = 0;
             for (int i = 1; i < array.length; i++) {
                 if (array[i].equals("/at")) {
                     //sets the breaking point of input
                     toBreak = true;
-                } else if (!toBreak) {
-                    des += array[i] + " ";
+                    pivotIndex = i;
+                    break;
                 } else {
-                    dateAndOrTime += array[i];
-                    if (i != array.length - 1) {
-                        dateAndOrTime += " ";
-                    }
+                    //before breaking point
+                    des += array[i] + " ";
                 }
             }
-            //index 0 is description, index 1 is date/time
-            return new String[]{des, dateAndOrTime};
-
+            String[] dateTimeArr = Arrays.copyOfRange(array, pivotIndex + 1, array.length);
+            //after breaking point, there should only be a maximum of 2 items remaining
+            //first item is date, second item is time
+            if (dateTimeArr.length > 2) {
+                throw new DukeException("Please enter a valid format");
+            } else {
+                if (dateTimeArr.length == 1) {
+                    //only has date but no time
+                    date = dateTimeArr[0];
+                } else if (dateTimeArr.length == 2) {
+                    //has date and time
+                    date = dateTimeArr[0];
+                    time = dateTimeArr[1];
+                }
+            }
+            //index 0 is description, index 1 is date, index 2 is time
+            if (toBreak && isValidDate(date) && isValidTime(time)) {
+                return new String[]{des, date, time};
+            } else {
+                //exception is thrown when the format is off, since there is no breaking point, or
+                //if the input date or time is wrong
+                throw new DukeException("Please use the correct format and include the keyword: /at");
+            }
         } else if (array[0].equals("deadline")) {
             String des = "";
-            String dateAndOrTime = "";
+            String date = "";
+            String time = "";
+
             boolean toBreak = false;
+            int pivotIndex = 0;
             for (int i = 1; i < array.length; i++) {
-                if (array[i].equals("/by")) {
+                if (array[i].equals("/at")) {
                     //sets the breaking point of input
                     toBreak = true;
-                } else if (!toBreak) {
-                    des += array[i] + " ";
+                    pivotIndex = i;
+                    break;
                 } else {
-                    dateAndOrTime += array[i];
-                    if (i != array.length - 1) {
-                        dateAndOrTime += " ";
-                    }
+                    //before breaking point
+                    des += array[i] + " ";
                 }
             }
-            //index 0 is description, index 1 is date/time
-            return new String[]{des, dateAndOrTime};
+            String[] dateTimeArr = Arrays.copyOfRange(array, pivotIndex + 1, array.length);
+            //after breaking point, there should only be a maximum of 2 items remaining
+            //first item is date, second item is time
+            if (dateTimeArr.length > 2) {
+                throw new DukeException("Please enter a valid format");
+            } else {
+                if (dateTimeArr.length == 1) {
+                    //only has date but no time
+                    date = dateTimeArr[0];
+                } else if (dateTimeArr.length == 2) {
+                    //has date and time
+                    date = dateTimeArr[0];
+                    time = dateTimeArr[1];
+                }
+            }
+            //index 0 is description, index 1 is date, index 2 is time
+            if (toBreak && isValidDate(date) && isValidTime(time)) {
+                return new String[]{des, date, time};
+            } else {
+                //exception is thrown when the format is off, since there is no breaking point, or
+                //if the input date or time is wrong
+                throw new DukeException("Please use the correct format and include the keyword: /by");
+            }
         } else if (array[0].equals("todo")) {
             String des = "";
             for (int i = 1; i < array.length; i++) {
                 des += array[i];
                 if (i != array.length - 1) {
+                    //adds a white space in between each word
                     des += " ";
                 }
             }
@@ -261,12 +306,21 @@ public class Duke {
 
     //parses and reads the given string and returns a task
     public static Task taskParser(String string) {
-        //<type>!@%<status>!@%<description>!@%<date/time(if applicable)>
+        //<type>!@%<status>!@%<description>!@%<date>(if applicable)!@%<time>(if applicable)
         String[] data = string.split("!@%");
         String type = data[0];
         String status = data[1];
         String description = data[2];
-        String dateOrTime = data.length > 3 ? data[3] : "";
+        String date = "";
+        String time = "";
+
+        //checks if the data consists of date and time using the split array size
+        if (data.length == 4) {
+            date = data[3];
+        } else if (data.length == 5) {
+            date = data[3];
+            time = data[4];
+        }
 
         //returns a task based on type, marks a task as done if status is 1
         if (type.equals("T")) {
@@ -276,13 +330,13 @@ public class Duke {
             }
             return todo;
         } else if (type.equals("D")) {
-            Deadline deadline = new Deadline(description, dateOrTime);
+            Deadline deadline = new Deadline(description, date, time);
             if (status.equals("1")) {
                 deadline.markDone();
             }
             return deadline;
         } else {
-            Event event = new Event(description, dateOrTime);
+            Event event = new Event(description, date, time);
             if (status.equals("1")) {
                 event.markDone();
             }
@@ -296,7 +350,7 @@ public class Duke {
             //create temp text file
             String tempDir = "./data/temp.txt";
             File temp = new File(tempDir);
-            
+
             File oldFile = new File(TASKS_DIRECTORY);
             if (temp.createNewFile()) {
                 BufferedWriter output = new BufferedWriter(new FileWriter(temp, true));
@@ -308,15 +362,17 @@ public class Duke {
                     //so that our parser can read
                     if (curr instanceof ToDo) {
                         ToDo todo = (ToDo) curr;
-                        toAppend = "T!@%" + (todo.isDone ? "1!@%" :"0!@%") + todo.description + "!@%";
+                        toAppend = "T!@%" + (todo.isDone ? "1!@%" : "0!@%") + todo.description + "!@%";
                     } else if (curr instanceof Deadline) {
                         Deadline deadline = (Deadline) curr;
-                        toAppend = "D!@%" + (deadline.isDone ? "1!@%" :"0!@%") + deadline.description + "!@%"
-                                + deadline.dateAndOrTime;
+                        toAppend = "D!@%" + (deadline.isDone ? "1!@%" : "0!@%") + deadline.description + "!@%"
+                                + (deadline.localDate != null ? deadline.localDate : "") + "!@%"
+                                + (deadline.localTime != null ? deadline.localTime : "");
                     } else {
                         Event event = (Event) curr;
-                        toAppend = "E!@%" + (event.isDone ? "1!@%" :"0!@%") + event.description + "!@%"
-                                    + event.dateAndOrTime;
+                        toAppend = "E!@%" + (event.isDone ? "1!@%" : "0!@%") + event.description + "!@%"
+                                + (event.localDate != null ? event.localDate : "") + "!@%"
+                                + (event.localTime != null ? event.localTime : "");
                     }
                     output.write(toAppend);
                     output.newLine();
@@ -324,8 +380,8 @@ public class Duke {
                 output.close();
 
                 //checks if old file is deleted and new file is renamed
-                if(oldFile.delete()) {
-                    if(!temp.renameTo(oldFile)) {
+                if (oldFile.delete()) {
+                    if (!temp.renameTo(oldFile)) {
                         System.out.println("Error in renaming new file.");
                     }
                 } else {
@@ -337,5 +393,41 @@ public class Duke {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    //checks the format of a given date and checks if it is acceptable
+    public static boolean isValidDate(String date) {
+        if (date.equals("")) {
+            return true;
+        }
+        String[] splitDate = date.split("-");
+        if (splitDate.length == 3) {
+            String year = splitDate[0];
+            String month = splitDate[1];
+            String day = splitDate[2];
+            if (year.length() == 4 && month.length() == 2 && day.length() == 2 && Integer.parseInt(month) <= 12
+                    && Integer.parseInt(month) >= 1 && Integer.parseInt(day) >= 1 && Integer.parseInt(day) <= 31) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //checks the format of a given time and checks if it is acceptable
+    public static boolean isValidTime(String time) {
+        if (time.equals("")) {
+            return true;
+        }
+        String[] splitTime = time.split(":");
+        if (splitTime.length == 2) {
+            String hour = splitTime[0];
+            String minutes = splitTime[1];
+            if (hour.length() == 2 && minutes.length() == 2 && Integer.parseInt(hour) <= 24
+                    && Integer.parseInt(hour) >= 0 && Integer.parseInt(minutes) >= 0
+                    && Integer.parseInt(minutes) <= 60) {
+                return true;
+            }
+        }
+        return false;
     }
 }
