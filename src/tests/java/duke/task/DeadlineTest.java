6d933ecc88a8dd3c3def11ef0432608a7d84ee8b
siddarth2824 <siddarth2824@gmail.com>
2020-08-25 19:36:18 +0800

    Add test for Deadline class

--- src/tests/java/duke/task/DeadlineTest.java
+++ src/tests/java/duke/task/DeadlineTest.java
@@ -1,4 +1,64 @@
 package duke.task;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DeadlineTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void test_toDataFileFormat_notDone(){
+        String taskDescription = "Test";
+        DukeDateTime dateTime = new DukeDateTime(LocalDateTime.parse("2020-10-24T16:00"), true);
+        Deadline testDeadline = new Deadline(taskDescription, dateTime);
+        String expectedOutput = "D | 0 | " + taskDescription + " | 24 Oct 2020 4:00 PM";
+        assertEquals(expectedOutput, testDeadline.toDataFileFormat());
+    }
+
+    @Test
+    public void test_toDataFileFormat_done(){
+        String taskDescription = "Test";
+        DukeDateTime dateTime = new DukeDateTime(LocalDateTime.parse("2020-10-24T16:00"), true);
+        Deadline testDeadline = new Deadline(taskDescription, dateTime);
+        testDeadline.markTaskAsDone();
+        String expectedOutput = "D | 1 | " + taskDescription + " | 24 Oct 2020 4:00 PM";
+        assertEquals(expectedOutput, testDeadline.toDataFileFormat());
+    }
+
+    @Test
+    public void test_toString_notDone(){
+        String taskDescription = "Test";
+        DukeDateTime dateTime = new DukeDateTime(LocalDateTime.parse("2020-10-24T16:00"), true);
+        Deadline testDeadline = new Deadline(taskDescription, dateTime);
+        String expectedOutput = "[D][\u2718] " + taskDescription + " (by: 24 Oct 2020 4:00 PM)";
+        assertEquals(expectedOutput, testDeadline.toString());
+    }
+
+    @Test
+    public void test_toString_done(){
+        String taskDescription = "Test";
+        DukeDateTime dateTime = new DukeDateTime(LocalDateTime.parse("2020-10-24T16:00"), true);
+        Deadline testDeadline = new Deadline(taskDescription, dateTime);
+        testDeadline.markTaskAsDone();
+        String expectedOutput = "[D][\u2713] " + taskDescription + " (by: 24 Oct 2020 4:00 PM)";
+        assertEquals(expectedOutput, testDeadline.toString());
+    }
 }
