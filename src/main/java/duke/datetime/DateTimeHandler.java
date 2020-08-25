cf0b07676f986361ba15d876c2f84a8a999aee32
chan-j-d <chan.j.d.98@gmail.com>
2020-08-26 02:58:09 +0800

    Add javadocs to duke.datetime classes

--- src/main/java/duke/datetime/DateTimeHandler.java
+++ src/main/java/duke/datetime/DateTimeHandler.java
@@ -8,15 +8,27 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Class specifically meant to handle string inputs and convert them into LocalDateTime objects.
+ */
 public class DateTimeHandler {
 
+    /** String representing standard datetime format used */
     public static final String STANDARD_DATETIME_FORMAT_STRING = "dd-MM-uuuu HHmm";
+    /** String representing standard time format */
     public static final String STANDARD_2400_FORMAT_STRING = "HHmm";
+    /** DateTimeFormatter object of standard datetime format */
     public static final DateTimeFormatter STANDARD_DATETIME_FORMAT = DateTimeFormatter.ofPattern(
             STANDARD_DATETIME_FORMAT_STRING);
+    /** DateTimeFormatter object of standard time format */
     public static final DateTimeFormatter STANDARD_2400_FORMAT = DateTimeFormatter.ofPattern(
             STANDARD_2400_FORMAT_STRING);
 
+    /**
+     * Parses a string be converted into a single LocalDateTime object.
+     * @param dateTime string to be converted into LocalDateTime object
+     * @return LocalDateTime object from the details given
+     */
     public static LocalDateTime parseDateTime(String dateTime) {
         try {
             return LocalDateTime.parse(dateTime, STANDARD_DATETIME_FORMAT);
@@ -26,6 +38,12 @@ public class DateTimeHandler {
         }
     }
 
+    /**
+     * Parses a string be converted into a pair of start and end timings.
+     * Besides invalid format, this method checks that the latter timing is later than the earlier timing.
+     * @param eventTiming string to be converted into a pair of LocalDateTime objects
+     * @return pair of LocalDateTime objects from the details given
+     */
     public static Pair<LocalDateTime, LocalDateTime> parseEventTimings(String eventTiming) {
         int acceptableLength1 = STANDARD_DATETIME_FORMAT_STRING.length() + 1 + STANDARD_2400_FORMAT_STRING.length();
         int acceptableLength2 = STANDARD_DATETIME_FORMAT_STRING.length() * 2 + 1;
@@ -57,14 +75,6 @@ public class DateTimeHandler {
                     + STANDARD_DATETIME_FORMAT_STRING + "-" + STANDARD_2400_FORMAT_STRING + " (24hr)");
 
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println(parseDateTime("24-09-2000 1800"));
-        System.out.println(parseEventTimings("24-09-2000 1800-2000"));
-        System.out.println(parseEventTimings("24-09-2000 1800-26-09-2000 2000"));
-        System.out.println(parseEventTimings("24-09-2000 180026-09-2000 2000"));
-
     }
 
 
