da7ebc4394e969937fcfafcbc4853e9145eb386a
augustinekau <augustinekau@gmail.com>
2020-08-25 15:16:22 +0800

    Add feature for Deadline to understand date and time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,24 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate date;
+    protected LocalTime time;
+
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        String[] str = by.split(" ", 2);
+        this.date = LocalDate.parse(str[0]);
+        this.time = LocalTime.parse(str[1]);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: "
+                + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " "
+                + time.format(DateTimeFormatter.ofPattern("hh:mm a")) + ")";
     }
 }
