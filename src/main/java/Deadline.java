44cfda02875a8be99bebc91fe334f7fe956e196a
NicholasCanete <nicholasgcc@gmail.com>
2020-08-25 23:14:33 +0800

    Level 7 - modified task and its subclasses, added file writing functionality to Duke

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,7 +1,29 @@
 public class Deadline extends Task {
 
+    private String identifier;
+
     public Deadline(String description) {
         super(description);
+        this.identifier = "D";
+    }
+
+    @Override
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    //separates date from description
+    @Override
+    public String getDescription() {
+        String fullDescription = super.getDescription();
+        String[] desArray = fullDescription.split("/", 2);
+        return desArray[0];
+    }
+
+    public String getDate() {
+        String[] desArray = this.description.split("/", 2);
+        String[] timeArray = desArray[1].split(" ", 2);
+        return timeArray[1];
     }
 
     @Override
