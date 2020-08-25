6d40b6cb2e1c28f0241f731797d295bae8ce3f93
Foo Kai En <kaien.foo@gmail.com>
2020-08-25 17:13:02 +0800

    Store dates and times as LocalDateTime objects instead of Strings

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,11 +1,25 @@
-public class Deadline extends Task {
-    private String by;
-    protected static final String FORMAT = "deadline <task> /by <deadline>";
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    Deadline(String command) {
+public class Deadline extends Task {
+    private LocalDateTime by;
+    protected static final String FORMAT = "deadline <description> /by <D-M-YYYY HHmm> (eg. 17-3-2020 0945 "
+            + "or 3-4-2020 with no time specified)";
+    private static final String NO_TIME_FLAG = String.format(" 0%.0f", Math.PI * Math.pow(10, 10));
+
+    Deadline(String command) throws InvalidFormatException {
         super(extractNameFromCommand(command));
         int index = command.indexOf("/by ") + 4;
-        by = command.substring(index);
+        String dateStr = command.substring(index).strip();
+        if (dateStr.length() < 13) {
+            dateStr += NO_TIME_FLAG;
+        }
+        try {
+            by = LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("d-M-yyyy HHmm[ssn]"));
+        } catch (DateTimeParseException e) {
+            throw new InvalidFormatException(FORMAT);
+        }
     }
 
     static String extractNameFromCommand(String command) {
@@ -15,6 +29,15 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return String.format("[%c] [%s] %s (by: %s)", 'D', getStatusIcon(), description, by);
+        String format = "d MMM yyyy";
+        if (by.getHour() != 3 || by.getMinute() != 14 || by.getSecond() != 15 || by.getNano() != 926536) {
+            if (by.getMinute() == 0) {
+                format += " ha";
+            } else {
+                format += " h.mma";
+            }
+        }
+        String byStr = by.format(DateTimeFormatter.ofPattern(format));
+        return String.format("[%c] [%s] %s (by: %s)", 'D', getStatusIcon(), description, byStr);
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,25 @@
-public class Event extends Task {
-    private String at;
-    protected static final String FORMAT = "event <event> /at <time>";
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    Event(String command) {
+public class Event extends Task {
+    private LocalDateTime at;
+    protected static final String FORMAT = "event <description> /at <D-M-YYYY HHmm> (eg. 17-3-2020 0945 "
+            + "or 3-4-2020 with no time specified)";
+    private static final String NO_TIME_FLAG = String.format(" 0%.0f", Math.PI * Math.pow(10, 10));
+
+    Event(String command) throws InvalidFormatException {
         super(extractNameFromCommand(command));
         int index = command.indexOf("/at ") + 4;
-        at = command.substring(index);
+        String dateStr = command.substring(index).strip();
+        if (dateStr.length() < 13) {
+            dateStr += NO_TIME_FLAG;
+        }
+        try {
+            at = LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("d-M-yyyy HHmm[ssn]"));
+        } catch (DateTimeParseException e) {
+            throw new InvalidFormatException(FORMAT);
+        }
     }
 
     static String extractNameFromCommand(String command) {
@@ -15,6 +29,15 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return String.format("[%c] [%s] %s (at: %s)", 'E', getStatusIcon(), description, at);
+        String format = "d MMM yyyy";
+        if (at.getHour() != 3 || at.getMinute() != 14 || at.getSecond() != 15 || at.getNano() != 926536) {
+            if (at.getMinute() == 0) {
+                format += " ha";
+            } else {
+                format += " h.mma";
+            }
+        }
+        String atStr = at.format(DateTimeFormatter.ofPattern(format));
+        return String.format("[%c] [%s] %s (at: %s)", 'E', getStatusIcon(), description, atStr);
     }
 }
