86ab38118481c70791d18ba2fc2eff1a1e4ed92b
JoeyToh <joeytoh29@gmail.com>
2020-08-26 03:23:08 +0800

    Implement Level 8 (Dates and Times)

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     protected String date;
+    private LocalDate localDate;
 
     public Deadline(String task, String date) {
         super(task);
         this.date = date;
+        localDate = LocalDate.parse(date);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + date + ")";
+        return "[D]" + super.toString() + " (by: " + localDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
