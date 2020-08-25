1b3a24596b78b4de45233a3e96b22ef562075f4d
PhongTran98 <giaphong8127@gmail.com>
2020-08-25 14:51:54 +0800

    Add Java Doc

--- src/main/java/command/Command.java
+++ src/main/java/command/Command.java
@@ -8,10 +8,19 @@ import java.util.List;
 public abstract class Command {
     List<String> input;
 
+    /**
+     * Creates a new command with user's input.
+     * @param input
+     */
     public Command(List<String> input) {
         this.input = input;
     }
 
+    /**
+     * Runs the command.
+     * @param taskList the current list of tasks
+     * @throws UserException
+     */
     abstract public void run(TaskList taskList) throws UserException;
 }
 
