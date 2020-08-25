ab65176068b3013e7281c55a112ddb30d4a236ac
Ben-Hanan <benhanan98@gmail.com>
2020-08-25 13:20:04 +0800

    A-Jar: Create a JAR File

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -5,9 +5,10 @@ if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 del ACTUAL.TXT
+del data
 
 REM compile the code into the bin folder
-javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\duke\*.java
 
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
