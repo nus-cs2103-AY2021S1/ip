9f79d8c1cc751f7ec035551672632a5421d6da20
Jaylenlee <e0424677@u.nus.edu>
2020-08-25 10:00:30 +0800

    Add FileCorruptedException
    FileCorruptedException is thrown when the date stored in files are not formatted as follows:
    
    type//doneStatus//description of action//date( if applicable)//time( if applicable)

---
+++ src/main/java/Exception/FileCorruptedException.java
@@ -0,0 +1,11 @@
+package Exception;
+
+public class FileCorruptedException extends DukeException {
+    private static final String DESCRIPTION = "File is corrupted. Unable to convert file into list of Tasks.";
+    public FileCorruptedException() {
+        super(DESCRIPTION);
+    }
+    public FileCorruptedException(String message) {
+        super(message);
+    }
+}
