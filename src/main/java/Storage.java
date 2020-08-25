4976d41c3771dc7a1e9b191da2c9398a859cdf7e
csiongn <>
2020-08-25 20:12:51 +0800

    A-CodingStandard: Tweak the code to comply with coding standard

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -14,29 +14,39 @@ public class Storage {
 
     /**
      * Creates a storage object that reads and writes to a file at a specified filepath.
-     * @param filePath file path of file to read and write to
+     *
+     * @param filePath file path of file to read and write to.
      */
     public Storage(String filePath) {
         this.filePath = filePath;
     }
 
     /**
-     * Checks that file we use to store data exists and creates the file if it does not.
+     * Checks that whether the file exists.
+     *
+     * @return Boolean representing whether the file exists.
      */
-    public boolean checkOrCreate() throws IOException {
+    public boolean doesFileExist() {
         File file = new File(this.filePath);
-        boolean createdDirectory = file.getParentFile().mkdirs();
-        boolean createdFile = file.createNewFile();
-        return createdDirectory || createdFile;
+        return file.exists();
     }
 
     /**
-     * Saves a list to the file. Creates the file if it does not exist
-     * @param lst TaskList to save to the file
+     * Creates the file
      */
-    public void save(TaskList lst){
+    public void createFile() throws IOException {
+        File file = new File(this.filePath);
+        boolean createdDirectory = file.getParentFile().mkdirs();
+        boolean createdFile = file.createNewFile();
+    }
+
+    /**
+     * Saves a list to the file. Creates the file if it does not exist.
+     *
+     * @param lst TaskList to save to the file.
+     */
+    public void save(TaskList lst) {
         try {
-            checkOrCreate();
             FileOutputStream fileOutputStream = new FileOutputStream(this.filePath);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(lst);
@@ -49,21 +59,22 @@ public class Storage {
     /**
      * Reads from the file containing a TaskList and returns it. If file does not exist, returns an
      * empty file.
-     * @return the TaskList read from the file, otherwise an empty TaskList
+     *
+     * @return The TaskList read from the file if one is read, otherwise an empty TaskList.
      */
     public TaskList read() {
         try {
-            if (checkOrCreate()) {
-                System.out.println("File does not exist, creating file");
-                return new TaskList();
-            } else {
-                System.out.println("Todo file found, reading file");
+            if (doesFileExist()) {
                 FileInputStream fileInputStream = new FileInputStream(this.filePath);
                 ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
                 TaskList lst = (TaskList) objectInputStream.readObject();
                 objectInputStream.close();
                 return lst;
+            } else {
+                createFile();
+                return new TaskList();
             }
+
         } catch (IOException | ClassNotFoundException e) {
             System.out.println("Error reading file");
             e.printStackTrace();
