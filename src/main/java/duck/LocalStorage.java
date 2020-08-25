6100904ee8efac67257b2d2f195e8a6720514ada
tankangliang <tankangliang98@gmail.com>
2020-08-25 09:43:33 +0800

    Add Storage interface and local implementation

---
+++ src/main/java/duck/LocalStorage.java
@@ -0,0 +1,41 @@
+package duck;
+
+import duck.exception.DuckException;
+import duck.task.TaskList;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class LocalStorage implements Storage{
+    private String filePath;
+
+    public LocalStorage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void save(TaskList taskList) throws DuckException {
+        try {
+            FileOutputStream file = new FileOutputStream(filePath);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+            out.writeObject(taskList);
+            out.close();
+            file.close();
+        } catch (IOException i) {
+            throw new DuckException("Failed to save file");
+        }
+    }
+
+    public TaskList load() throws DuckException {
+        try {
+            FileInputStream file = new FileInputStream(filePath);
+            ObjectInputStream in = new ObjectInputStream(file);
+            TaskList taskList = (TaskList) in.readObject();
+            return taskList;
+        } catch (IOException | ClassNotFoundException e) {
+            throw new DuckException("Failed to load file");
+        }
+    }
+}
