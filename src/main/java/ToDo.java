00097b1df5cb24b2130a87fcc030b51b91a67304
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-08-25 21:54:30 +0800

    Override Task fromString function

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -7,4 +7,15 @@ public class ToDo extends Task {
     public String toString() {
         return "[T]" + super.toString();
     }
+
+    @Override
+    public Task fromString(String taskString) {
+        boolean isDone = taskString.split(" ")[0].equals("Done");
+        String description = taskString.split(" ")[1];
+        Task t = new ToDo(description);
+        if (isDone) {
+            t.setDone();
+        }
+        return t;
+    }
 }
