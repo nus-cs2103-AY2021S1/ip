41fda95e148f1ce1fc6659d09d29ba0e8de62d17
Sean Chan Ding Chao <dcchan98@gmail.com>
2020-08-25 15:41:00 +0800

    Java Docs added

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.io.Serializable;
 
 
+/**
+ * Task class that stores what to do, type, deadline and if finished
+ */
 public class Task implements Serializable {
 
     private String type;// can be [T] [E] or [D]
@@ -11,27 +14,53 @@ public class Task implements Serializable {
     private String details;
     private String deadLine = null;
 
+
     public LocalDate localDeadline;
 
+    /**
+     * Tells if tasks is finish
+     * @return boolean of whether task is finished
+     */
     public boolean finished(){
         return this.done;
     }
 
+    /**
+     * Sets done to true. Used when finished task
+     */
     public void setDone(){
         this.done=true;
     }
 
+    /**
+     * Constructor to create task
+     * @param type string value of ["T"] etc
+     * @param details consists of task information
+     */
     public Task(String type, String details) {
 
         this.type = type;
         this.details = details;
     }
 
+    /**
+     * Constructor to create task
+     * @param type string value of ["T"] etc
+     * @param details consists of task information
+     * @param deadLine deadline of task
+     */
     public Task(String type, String details,String deadLine)  {
         this.type = type;
         this.details = details;
         this.deadLine = deadLine;
     }
+
+    /**
+     * Constructor to create task
+     * @param type string value of ["T"] etc
+     * @param details consists of task information
+     * @param localDeadline deadline of task in LocalDate instance
+     */
     public Task(String type, String details,LocalDate localDeadline)  {
         this.type = type;
         this.details = details;
@@ -41,6 +70,10 @@ public class Task implements Serializable {
 
     }
 
+    /**
+     * properly formats string value
+     * @returns string of all relevant information of myTasks
+     */
     @Override
     public String toString() {
         String checkbox = done? "[✓]":"[✗]" ;
