3a8919c68f7045126dda975aba2c671ab592fcaf
gsmoon97 <gsmoon97@gmail.com>
2020-08-25 21:36:22 +0900

    A-JUnit

---
+++ src/test/java/LukeTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LukeTest {
+    @Test
+    public void dummyTest1(){
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void dummyTest2(){
+        assertEquals(2, 2);
+    }
+}
