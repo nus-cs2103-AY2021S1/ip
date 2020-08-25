b7a5c4545adff38d98cda9eb709c8d10a2d8fefe
chan-j-d <chan.j.d.98@gmail.com>
2020-08-26 03:22:29 +0800

    Add javadocs to duke package files

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -12,10 +12,19 @@ import duke.util.TaskList;
  */
 public class Duke {
 
+    /** list of tasks to keep track of */
     protected TaskList taskList;
+    /** storage object handling saving and editing of the save file in the hard drive */
     protected Storage storage;
+    /** ui object in charge of direct interaction with the user */
     protected Ui ui;
 
+    /**
+     * Standard constructor initialising the Duke bot.
+     * It will attempt to search for a pre-saved save file of a previous task list.
+     * If found, it loads up that task list.
+     * Otherwise it creates its own which will be saved and able to be opened by future Duke bots.
+     */
     public Duke() {
         storage = Storage.init();
         taskList = storage.readStoredData();
@@ -25,10 +34,13 @@ public class Duke {
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.run();
-
     }
 
-    protected void run() {
+    /**
+     * Runs the duke bot.
+     * It continuously queries the ui for the next user response and responds appropriately.
+     */
+    public void run() {
         ui.startup();
 
         boolean isExit = false;
--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -2,31 +2,56 @@ package duke;
 
 import java.util.Scanner;
 
+/**
+ * Standard user-interface class that handles interactions between the user and the engine.
+ */
 public class Ui {
 
+    //Constants used when formatting the bot's message
     protected static final String LINE_BREAK = "    ____________________________________________________________\n";
     protected static final String PRESPACING = "     ";
 
+    /** Standard opening message sent */
     protected static final String OPENING_MESSAGE = "Hello! I'm Duke\nWhat can I do for you?";
 
+    //Scanner object used to obtain response from the System
     private Scanner scanner;
 
+    /**
+     * Standard constructor that initialises a UI object.
+     */
     public Ui() {
         scanner = new Scanner(System.in);
     }
 
+    /**
+     * Queries the user for the next command in the form of a string.
+     * @return string provided by the user
+     */
     public String getCommand() {
         return scanner.nextLine();
     }
 
+    /**
+     * Notifies the user with the given message
+     * @param message message to be sent
+     */
     public void outputMessage(String message) {
         System.out.println(processString(message));
     }
 
+    /**
+     * Message sent to user when the bot starts up.
+     */
     public void startup() {
         outputMessage(getOpeningText());
     }
 
+    /**
+     * String formatter that customises messages to be sent to the user.
+     * @param string message to be sent
+     * @return formatted message that will be sent
+     */
     protected static String processString(String string) {
         return LINE_BREAK  + PRESPACING
                 + string.replaceAll("\n", '\n' + PRESPACING)
@@ -34,6 +59,7 @@ public class Ui {
     }
 
 
+    /** Method used to obtain the opening text when the bot starts up */
     private String getOpeningText() {
         return OPENING_MESSAGE;
     }
