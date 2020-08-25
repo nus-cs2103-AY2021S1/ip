09e2a6b19cdbdf221c2e399b1fc338f92fd21bc2
Thai Duong <htduong292001@gmail.com>
2020-08-25 15:37:44 +0800

    Add javadoc for classes in duke.exception package

--- src/main/java/duke/exception/InadequateCommandException.java
+++ src/main/java/duke/exception/InadequateCommandException.java
@@ -1,15 +1,28 @@
 package duke.exception;
 
+/**
+ * Throws when a command that create task is input but missing some supporting info
+ */
 public class InadequateCommandException extends DukeException {
     private String typeOfTask;
     private String[] missingSections;
 
+    /**
+     * Creates an <code>InadequateCommandException</code> object.
+     * @param typeOfTask The type of the task intended to create (<code>Todo</code>, <code>Deadline</code>,
+     *                   or <code>Event</code>)
+     * @param missingSections The missing information sections in the input
+     */
     public InadequateCommandException(String typeOfTask, String[] missingSections) {
         super("");
         this.typeOfTask = typeOfTask;
         this.missingSections = missingSections;
     }
 
+    /**
+     * Returns the message of the exception
+     * @return The message describing the missing sections.
+     */
     @Override
     public String getMessage() {
         String listing = "";
