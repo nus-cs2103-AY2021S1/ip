421c70c2ea555a484b9a3b1ac5fec3250dee08d3
augustinekau <augustinekau@gmail.com>
2020-08-26 01:55:26 +0800

    JavaDoc: Todo

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,8 +1,20 @@
+/**
+ * <h>To-do Task Type</h>
+ * This class is a type of tasks without any date/time attached to it.
+ */
 public class Todo extends Task{
+    /**
+     * Constructor of to-do class
+     * @param description Name of the task input by user.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Overridden toString method to output name, type and status of task.
+     * @return String This returns a string.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
