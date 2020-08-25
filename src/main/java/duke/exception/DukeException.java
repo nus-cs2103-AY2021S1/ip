2c9c491f5a78793cecba94fabc953fb7296ed817
cwenling <e0425906@u.nus.edu>
2020-08-26 00:43:12 +0800

    Add javadocs for Ui, Parser, Storage, DukeException

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,7 +1,15 @@
 package duke.exception;
 
+/**
+ * Represents DukeExceptions used in Duke.
+ */
 public class DukeException extends Exception {
 
+    /**
+     * Creates a DukeException with a specified message.
+     *
+     * @param errorMessage Error message to be shown.
+     */
     public DukeException(String errorMessage) {
         super(errorMessage);
     }
--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -2,6 +2,9 @@ package duke.ui;
 
 import java.util.Scanner;
 
+/**
+ * Represents the Ui dealing with interactions with the user.
+ */
 public class Ui {
 
     private static final String DIVIDER =
@@ -20,31 +23,57 @@ public class Ui {
     private static final String GOODBYE =
             "\t Good nite! Hope you have a good night's sleep. See you soon!\n";
 
+    /**
+     * Creates a Ui for interacting with the user.
+     */
     public Ui() {
     }
 
+    /**
+     * Displays the welcome message when user starts up Duke.
+     */
     public void showWelcome() {
         System.out.println(LOGO);
         System.out.println(DIVIDER + GREETING + DIVIDER);
     }
 
+    /**
+     * Displays the farewell message when user exits Duke.
+     */
     public void showFarewell() {
         System.out.print(DIVIDER + GOODBYE + DIVIDER);
     }
 
+    /**
+     * Displays a formatted error message if a DukeException occurs.
+     *
+     * @param errorMessage Error message of the DukeException.
+     */
     public void showError(String errorMessage) {
         String enhancedMessage = "\t :-( OOPS!!! " + errorMessage + "\n";
         System.out.println(DIVIDER + enhancedMessage + DIVIDER);
     }
 
+    /**
+     * Displays a formatted message showing execution of a command.
+     *
+     * @param actionMessage Message to be shown to user.
+     */
     public void showAction(String actionMessage) {
         System.out.println(DIVIDER + actionMessage + DIVIDER);
     }
 
+    /**
+     * Displays a loading error if unable to load tasks from text file.
+     */
     public void showLoadingError() {
         System.out.println("Unable to load Tasklist");
     }
 
+    /**
+     * Scans for user input.
+     * @return String of text that user has input.
+     */
     public String readCommand() {
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
