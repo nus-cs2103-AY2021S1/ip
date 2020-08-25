bd033606f804deaf8caba8ec0eda157a16618209
aizatazhar <aizat.azharr@gmail.com>
2020-08-25 19:13:30 +0800

    Add Javadocs for tasks

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -1,5 +1,8 @@
 package duke.task;
 
+/**
+ * Abstract class that models a task.
+ */
 public abstract class Task {
     protected String description;
     protected boolean isDone;
@@ -9,14 +12,27 @@ public abstract class Task {
         this.isDone = false;
     }
 
+    /**
+     * Returns icon representing whether the task is done or not.
+     * 
+     * @return Icon representing whether the task is done or not.
+     */
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
+    /**
+     * Sets the task to be done.
+     */
     public void completeTask() {
         this.isDone = true;
     }
 
+    /**
+     * Returns a String representation of the task for saving.
+     * 
+     * @return String representation of the task for saving purposes.
+     */
     public abstract String toSaveFormat();
     
     @Override
--- src/main/java/duke/task/ToDo.java
+++ src/main/java/duke/task/ToDo.java
@@ -1,10 +1,18 @@
 package duke.task;
 
+/**
+ * Models a todo task.
+ */
 public class ToDo extends Task {
     public ToDo(String description) {
         super(description);
     }
 
+    /**
+     * Returns icon representing whether the task is done or not.
+     *
+     * @return Icon representing whether the task is done or not.
+     */
     @Override
     public String toSaveFormat() {
         return String.format("T | %d | %s", this.isDone ? 1 : 0, this.description);
