2370b5cb44ff49e8dbbddaa73b4a00feb4b3ed74
augustinekau <augustinekau@gmail.com>
2020-08-26 02:02:27 +0800

    Tweak Duke class to comply with coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -39,122 +39,114 @@ public class Duke {
             }
 
             switch (cmd) {
-                case BYE:
-                    ui.showBye();
-                    // rewrite the file to update latest changes
-                    Storage.saveFile(this.storage.file, ls);
-                    break WhileLoop;
-
-                case LIST:
+            case BYE:
+                ui.showBye();
+                // rewrite the file to update latest changes
+                Storage.saveFile(this.storage.file, ls);
+                break WhileLoop;
+            case LIST:
+                if (ls.isEmpty()) {
+                    ui.showListNoTask();
+                } else {
+                    ui.showListTask();
+                    for (int i = 0; i < ls.size(); i++) {
+                        Task task = ls.get(i);
+                        int num = i + 1;
+                        System.out.println(num + ". " + task.toString());
+                    }
+                    ui.horizontalDiv();
+                }
+                break;
+            case DONE:
+                try {
+                    int numToBeMarkedAsDone = Parser.parseInt(str);
+                    Task tsk = ls.get(numToBeMarkedAsDone);
+                    tsk.markAsDone();
+                    ls.set(numToBeMarkedAsDone, tsk);
+                    ui.showDoneMsg(tsk.toString());
+                } catch (Exception ex) {
+                    ui.showDoneError();
+                }
+                break;
+            case TODO:
+                try {
+                    Task newTask = new Todo(arr[1]);
+                    ls.add(newTask);
+                    ui.showTodoMsg(ls, newTask);
+                } catch (Exception ex) {
+                    ui.showTodoError();
+                }
+                break;
+            case DEADLINE:
+                try {
+                    String[] arrOfStr = Parser.parse(arr, 1);
+                    try {
+                        Task newTask = new Deadline(arrOfStr[0], arrOfStr[1]);
+                        ls.add(newTask);
+                        ui.showDeadlineEventMsg(ls, newTask);
+                    } catch (Exception ex) {
+                        ui.showDeadlineFormatError();
+                    }
+                } catch (Exception ex) {
+                    ui.showDeadlineError();
+                }
+                break;
+            case EVENT:
+                try {
+                    String[] arrOfStr = Parser.parse(arr, 2);
+                    try {
+                        Task newTask = new Event(arrOfStr[0], arrOfStr[1]);
+                        ls.add(newTask);
+                        ui.showDeadlineEventMsg(ls, newTask);
+                    } catch (Exception ex) {
+                        ui.showEventFormatError();
+                    }
+                } catch (Exception ex) {
+                    ui.showEventError();
+                }
+                break;
+            case DELETE:
+                try {
+                    int numToBeDeleted = Parser.parseInt(str);
+                    Task tsk = ls.get(numToBeDeleted);
+                    ls.remove(numToBeDeleted);
+                    ui.showDeleteMsg(ls, tsk);
+                } catch (Exception ex) {
+                    ui.showDeleteError();
+                }
+                break;
+            case CHECK:
+                try {
+                    String checkDate = arr[1];
+                    LocalDate date = LocalDate.parse(checkDate);
                     if (ls.isEmpty()) {
-                        ui.showListNoTask();
+                        ui.showCheckNoTask();
                     } else {
-                        ui.showListTask();
+                        ui.horizontalDiv();
+                        int counter = 0;
                         for (int i = 0; i < ls.size(); i++) {
                             Task task = ls.get(i);
-                            int num = i + 1;
-                            System.out.println(num + ". " + task.toString());
-                        }
-                        ui.horizontalDiv();
-                    }
-                    break;
-
-                case DONE:
-                    try {
-                        int numToBeMarkedAsDone = Parser.parseInt(str);
-                        Task tsk = ls.get(numToBeMarkedAsDone);
-                        tsk.markAsDone();
-                        ls.set(numToBeMarkedAsDone, tsk);
-                        ui.showDoneMsg(tsk.toString());
-                    } catch (Exception ex) {
-                        ui.showDoneError();
-                    }
-                    break;
-
-                case TODO:
-                    try {
-                        Task newTask = new Todo(arr[1]);
-                        ls.add(newTask);
-                        ui.showTodoMsg(ls, newTask);
-                    } catch (Exception ex) {
-                        ui.showTodoError();
-                    }
-                    break;
-
-                case DEADLINE:
-                    try {
-                        String[] arrOfStr = Parser.parse(arr, 1);
-                        try {
-                            Task newTask = new Deadline(arrOfStr[0], arrOfStr[1]);
-                            ls.add(newTask);
-                            ui.showDeadlineEventMsg(ls, newTask);
-                        } catch (Exception ex) {
-                            ui.showDeadlineFormatError();
-                        }
-                    } catch (Exception ex) {
-                        ui.showDeadlineError();
-                    }
-                    break;
-
-                case EVENT:
-                    try {
-                        String[] arrOfStr = Parser.parse(arr, 2);
-                        try {
-                            Task newTask = new Event(arrOfStr[0], arrOfStr[1]);
-                            ls.add(newTask);
-                            ui.showDeadlineEventMsg(ls, newTask);
-                        } catch (Exception ex) {
-                            ui.showEventFormatError();
-                        }
-                    } catch (Exception ex) {
-                        ui.showEventError();
-                    }
-                    break;
-                    
-                case DELETE:
-                    try {
-                        int numToBeDeleted = Parser.parseInt(str);
-                        Task tsk = ls.get(numToBeDeleted);
-                        ls.remove(numToBeDeleted);
-                        ui.showDeleteMsg(ls, tsk);
-                    } catch (Exception ex) {
-                        ui.showDeleteError();
-                    }
-                    break;
-                    
-                case CHECK:
-                    try {
-                        String checkDate = arr[1];
-                        LocalDate date = LocalDate.parse(checkDate);
-                        if (ls.isEmpty()) {
-                            ui.showCheckNoTask();
-                        } else {
-                            ui.horizontalDiv();
-                            int counter = 0;
-                            for (int i = 0; i < ls.size(); i++) {
-                                Task task = ls.get(i);
-                                if (task instanceof Event) {
-                                    if (((Event) task).getAt().equals(date) && !task.isDone) {
-                                        counter += 1;
-                                        System.out.println(counter + ". " + task.toString());
-                                    }
-                                } else if (task instanceof Deadline && !task.isDone) {
-                                    if (((Deadline) task).getDate().equals(date)) {
-                                        counter += 1;
-                                        System.out.println(counter + ". " + task.toString());
-                                    }
+                            if (task instanceof Event) {
+                                if (((Event) task).getAt().equals(date) && !task.isDone) {
+                                    counter += 1;
+                                    System.out.println(counter + ". " + task.toString());
+                                }
+                            } else if (task instanceof Deadline && !task.isDone) {
+                                if (((Deadline) task).getDate().equals(date)) {
+                                    counter += 1;
+                                    System.out.println(counter + ". " + task.toString());
                                 }
                             }
-                            ui.showCheckTask(counter, date);
                         }
-                    } catch (Exception ex) {
-                        ui.showCheckError();
+                        ui.showCheckTask(counter, date);
                     }
-                    break;
-                    
-                default:
-                    ui.invalidInput();
-                    break;
+                } catch (Exception ex) {
+                    ui.showCheckError();
+                }
+                break;
+            default:
+                ui.invalidInput();
+                break;
             }
         }
     }
