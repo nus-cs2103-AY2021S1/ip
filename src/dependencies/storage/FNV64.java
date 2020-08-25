dc2238c1046c2ee77198665b0f57204f5394466d
KenGondor <kengondorleh@gmail.com>
2020-08-25 17:01:50 +0800

    Add package for Duke and Ui. Add Javadocs for FNV class for storage module

--- src/dependencies/storage/FNV64.java
+++ src/dependencies/storage/FNV64.java
@@ -4,10 +4,16 @@ package dependencies.storage;
 /**
  * Fowler-No-Voll hash utility function.
  */
-class FNV64 {
+public class FNV64 {
     private static final long PRIME = 0x100000001b3L;
     private static final long OFFSET = 0xcbf29ce484222325L;
 
+    /**
+     * Hash a string.
+     *
+     * @param k hash a string
+     * @return a 64 bit hash
+     */
     public static long hash(String k) {
         long ret = OFFSET;
         int l = k.length();
