21fb4f77b63c147ec1d9fad6dd51ffac90cafafb
duckmoon99 <e0407816@u.nus.edu>
2020-08-25 19:11:04 +0800

    Fix save string for deadline and event tasks

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -13,6 +13,6 @@ public class DeadlineTask extends Task {
 
     @Override
     public String toSaveString() {
-        return String.format("T @@ %d @@ %s @@ %s", isDone ? 1 : 0, desc, deadline);
+        return String.format("D @@ %d @@ %s @@ %s", isDone ? 1 : 0, desc, deadline);
     }
 }
--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -13,6 +13,6 @@ public class EventTask extends Task {
 
     @Override
     public String toSaveString() {
-        return String.format("T @@ %d @@ %s @@ %s", isDone ? 1 : 0, desc, period);
+        return String.format("E @@ %d @@ %s @@ %s", isDone ? 1 : 0, desc, period);
     }
 }
