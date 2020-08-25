d89f3e2e71a8c0103742a38eba4f09fbb11f654f
Muhammad Syafiq Bin Abas <syafiqabs7@gmail.com>
2020-08-25 16:15:55 +0800

    add Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,45 +4,65 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("Hello! I'm Duke\n" + "How can I help you?");
 
-        List<Task> tasks = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        Task task = new Task(sc.nextLine());
+        List<Task> tasks = new ArrayList<>();
 
-        while (!(task.getDescription().equals("bye"))) {
-            if (task.getDescription().equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + "." + tasks.get(i));
+        while (true) {
+            try {
+                String command = sc.nextLine();
+                String[] commandArr = command.split(" ", 2);
+                Task task;
+                String[] strings;
+                if (commandArr[0].equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
                 }
-                task = new Task(sc.nextLine());
-            } else if (task.getDescription().contains("done")) {
-                String string = task.getDescription();
-                int index = Integer.parseInt(string.replaceAll("\\D",""));
-                tasks.get(index - 1).markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n " + tasks.get(index - 1));
-                task = new Task(sc.nextLine());
-            } else {
-                if (task.getDescription().contains("todo")) {
-                    String string = task.getDescription().replaceFirst("todo","").strip();
-                    task = new ToDos(string);
-                    tasks.add(task);
-                } else if (task.getDescription().contains("deadline")) {
-                    String string = task.getDescription().replaceFirst("deadline","");
-                    String[] strings = string.split("/by");
-                    task = new Deadlines(strings[0].strip(), strings[1].strip());
-                    tasks.add(task);
-                } else if (task.getDescription().contains("event")) {
-                    String string = task.getDescription().replaceFirst("event","");
-                    String[] strings = string.split("/at");
-                    task = new Events(strings[0].strip(), strings[1].strip());
-                    tasks.add(task);
+                switch (commandArr[0]) {
+                    case "todo":
+                        if (commandArr.length == 1 || commandArr[1].strip().equals("")) {
+                            throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                        }
+                        task = new ToDos(commandArr[1]);
+                        tasks.add(task);
+                        System.out.println("Got it. I've added this task:\n " + task + "\nNow you have " + tasks.size() + " tasks in the list.");
+                        break;
+                    case "deadline":
+                        if (commandArr.length == 1 || commandArr[1].strip().equals("")) {
+                            throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                        }
+                        strings = commandArr[1].split("/by");
+                        task = new Deadlines(strings[0].strip(), strings[1].strip());
+                        tasks.add(task);
+                        System.out.println("Got it. I've added this task:\n " + task + "\nNow you have " + tasks.size() + " tasks in the list.");
+                        break;
+                    case "event":
+                        if (commandArr.length == 1 || commandArr[1].strip().equals("")) {
+                            throw new DukeException("OOPS!!! The description of an event cannot be empty.");
+                        }
+                        strings = commandArr[1].split("/at");
+                        task = new Events(strings[0].strip(), strings[1].strip());
+                        tasks.add(task);
+                        System.out.println("Got it. I've added this task:\n " + task + "\nNow you have " + tasks.size() + " tasks in the list.");
+                        break;
+                    case "list":
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println((i + 1) + "." + tasks.get(i));
+                        }
+                        break;
+                    case "done":
+                        int index = Integer.parseInt(commandArr[1]) - 1;
+                        tasks.get(index).markAsDone();
+                        System.out.println("Nice! I've marked this task as done:\n " + tasks.get(index));
+                        break;
+                    default:
+                        throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-                System.out.println("Got it. I've added this task:\n " + task + "\nNow you have " + tasks.size() + " tasks in the list.");
-                task = new Task(sc.nextLine());
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
