f2450d65771ef22657b0ab49cc3211a14c924536
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-08-25 22:05:21 +0800

    Add override fromString

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -10,7 +10,7 @@ public class ToDo extends Task {
 
     @Override
     public Task fromString(String taskString) {
-        boolean isDone = taskString.split(" ")[0].equals("Done");
+        boolean isDone = taskString.split(" ")[0].equals("[Done]");
         String description = taskString.split(" ")[1];
         Task t = new ToDo(description);
         if (isDone) {
