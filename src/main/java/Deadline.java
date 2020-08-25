ef16fdf6cb75b70f471fb6f20989f8a14c188a04
Tan Su Yin <e0425292@u.nus.edu>
2020-08-25 23:51:54 +0800

    Add proper date and time feature

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,36 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Deadline extends Task {
-    private String dateAndTime;
+    private LocalDate date;
+    private Date time = null;
+    SimpleDateFormat timeFormatter = new SimpleDateFormat("h:mm a");
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MMM d yyyy");
 
-    public Deadline(String description) {
+    public Deadline(String description) throws DukeException {
         super(description.split("/by ")[0]);
-        this.dateAndTime = description.split("/by ")[1];
-
+        try {
+            String[] dateAndTime = description.split("/by ")[1].split(" ");
+            this.date = LocalDate.parse(dateAndTime[0]);
+            System.out.println(this.date);
+            if (dateAndTime.length == 2) {
+                SimpleDateFormat parseFormat = new SimpleDateFormat("hh:mm");
+                this.time = parseFormat.parse(dateAndTime[1]);
+            }
+        } catch (Exception e) {
+            throw new DukeException("Please input a valid date and time in the format yyyy-mm-dd HH:MM");
+        }
+        
     }
 
     public String dateAndTimeBracket() {
-        return String.format("(by: %s)", this.dateAndTime);
+        if (this.time == null) {
+            return String.format("(by: %s)", this.date.format(dateFormatter));
+        } else {
+            return String.format("(by: %s, %s)", this.date.format(dateFormatter), timeFormatter.format(this.time));
+        }
     }
 
     @Override
