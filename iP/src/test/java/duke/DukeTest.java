a4459fd53243764d06bb246c94eb90a2b67c2d3f
Sushi <54584459+yuki-cell@users.noreply.github.com>
2020-08-25 12:50:23 +0800

    commit A-JUnit(newer)

--- iP/src/test/java/duke/DukeTest.java
+++ iP/src/test/java/duke/DukeTest.java
@@ -1,4 +1,26 @@
 package duke;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DukeTest {
+    @Test
+    public void addTodo() throws IOException, DukeException {
+        Duke duke = new Duke("data/duke.txt");
+        assertEquals(0, duke.tasks.taskList.size());
+        duke.user_input_handler("todo homework", false);
+        assertEquals( 1, duke.tasks.taskList.size());
+    }
+
+    @Test
+    public void addAndDeleteTodo() throws IOException, DukeException {
+        Duke duke = new Duke("data/duke.txt");
+        duke.user_input_handler("todo homework", false);
+        assertEquals( 1, duke.tasks.taskList.size());
+        duke.user_input_handler("delete 1", false);
+        assertEquals( 0, duke.tasks.taskList.size());
+    }
 }
