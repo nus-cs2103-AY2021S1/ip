37f4c20754780ca8458d291702caa8d14c17a7f2
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-08-25 18:04:05 +0800

    Modify dates and time storage to use LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
+    protected static DateTimeFormatter dateTimeInputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    protected static DateTimeFormatter dateTimeOutputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm");
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, dateTimeInputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(dateTimeOutputFormatter) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String timeOfEvent;
+    protected LocalDateTime timeOfEvent;
+    protected static DateTimeFormatter dateTimeInputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    protected static DateTimeFormatter dateTimeOutputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm");
+    
 
     public Event(String description, String timeOfEvent) {
         super(description);
-        this.timeOfEvent = timeOfEvent;
+        this.timeOfEvent = LocalDateTime.parse(timeOfEvent, dateTimeInputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + timeOfEvent + ")";
+        return "[E]" + super.toString() + " (at: " + timeOfEvent.format(dateTimeOutputFormatter) + ")";
     }
 }
