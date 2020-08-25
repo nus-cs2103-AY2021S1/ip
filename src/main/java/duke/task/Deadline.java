0d5e5e85c8f9a4cd1f42a158fb37335895cb412a
Wang Zijun <wzj97@hotmail.com>
2020-08-26 01:47:38 +0800

    Add JavaDoc to duke.task classes

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -4,25 +4,56 @@ import duke.time.TimePoint;
 
 import java.util.HashMap;
 
+/**
+ * Class that represents a <code>Task</code> with a deadline.
+ */
 public class Deadline extends Task {
 
+    /**
+     * Deadline of task.
+     */
     private TimePoint deadline;
 
+    /**
+     * Creates an initialised <code>Deadline</code> with name and deadline.
+     *
+     * @param name Name of <code>Deadline</code>.
+     * @param deadline Deadline of <code>Deadline</code> task.
+     */
     public Deadline(String name, String deadline) {
         super(name);
         this.deadline = TimePoint.of(deadline);
     }
 
+    /**
+     * Creates a fully detailed <code>Deadline</code>.
+     * For creating <code>Deadline</code> from save file.
+     *
+     * @param name Name of <code>Deadline</code>.
+     * @param deadline Deadline of <code>Deadline</code> task.
+     * @param done Whether <code>Deadline</code> has been done.
+     */
     public Deadline(String name, String deadline, boolean done) {
         super(name, done);
         this.deadline = TimePoint.of(deadline);
     }
 
+    /**
+     * Returns String formatted for representation of <code>Deadline</code> for display.
+     *
+     * @return Formatted String.
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.deadline.toString() + ")";
     }
 
+    /**
+     * Converts <code>Deadline</code> to <code>HashMap</code> representation.
+     * Used for further processing to save file string.
+     *
+     * @return HashMap representation of properties.
+     */
     @Override
     public HashMap<String, String> convertToHashMap() {
         HashMap<String, String> dict = super.convertToHashMap();
--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -4,25 +4,56 @@ import duke.time.TimePoint;
 
 import java.util.HashMap;
 
+/**
+ * Class that represents an event that occurs at some time.
+ */
 public class Event extends Task {
 
+    /**
+     * Represents time at which event occurs.
+     */
     private TimePoint when;
 
+    /**
+     * Creates an initialised <code>Event</code> with name and time.
+     *
+     * @param name Name of <code>Event</code>.
+     * @param when Time when <code>Event</code> occurs.
+     */
     public Event(String name, String when) {
         super(name);
         this.when = TimePoint.of(when);
     }
 
+    /**
+     * Creates a fully detailed <code>Event</code>.
+     * For creating <code>Event</code> from save file.
+     *
+     * @param name Name of <code>Event</code>.
+     * @param when Time when <code>Event</code> occurs.
+     * @param done Whether <code>Event</code> has been done.
+     */
     public Event(String name, String when, boolean done) {
         super(name, done);
         this.when = TimePoint.of(when);
     }
 
+    /**
+     * Returns String formatted for representation of <code>Event</code> for display.
+     *
+     * @return Formatted String.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (at: " + this.when.toString() + ")";
     }
 
+    /**
+     * Converts <code>Event</code> to <code>HashMap</code> representation.
+     * Used for further processing to save file string.
+     *
+     * @return HashMap representation of properties.
+     */
     @Override
     public HashMap<String, String> convertToHashMap() {
         HashMap<String, String> dict = super.convertToHashMap();
