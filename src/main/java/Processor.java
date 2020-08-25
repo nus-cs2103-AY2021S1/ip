b7123c5a41ebaca629e45cdf15eb65bd2bbf563c
Penn Han <leepennhan98@gmail.com>
2020-08-25 14:25:16 +0800

    Align coding standard for Processor Class

--- src/main/java/Processor.java
+++ src/main/java/Processor.java
@@ -6,70 +6,70 @@ public class Processor {
 
     public Processor() {}
 
-    public void process(String command, ArrayList<Task> arraylst, DukeFile dukeFile) throws DukeException {
+    public void process(String command, ArrayList<Task> arrayLst, DukeFile dukeFile) throws DukeException {
         String[] stringarr = command.split(" ");
         if (stringarr[0].equals("list")) {
-            processorList(arraylst);
+            processorList(arrayLst);
         } else if (stringarr[0].equals("done")) {
             int index = Integer.parseInt(stringarr[1]);
-            String record = processorDone(arraylst, index);
+            String record = processorDone(arrayLst, index);
             dukeFile.updateRecord(record, index);
         } else if (stringarr[0].equals("delete")) {
             int index = Integer.parseInt(stringarr[1]);
-            processorDelete(arraylst, index);
+            processorDelete(arrayLst, index);
             dukeFile.deleteRecord(index);
         } else if (stringarr[0].equals("find")) {
             String key = stringarr[1];
-            processorFind(arraylst, key);
+            processorFind(arrayLst, key);
         } else {
-            String record = processorAdd(command, arraylst);
+            String record = processorAdd(command, arrayLst);
             dukeFile.saveRecord(record);
         }
     }
 
-    public void processorFind(ArrayList<Task> arraylst, String key) {
+    public void processorFind(ArrayList<Task> arrayLst, String key) {
         int counter = 1;
         System.out.println("_________________________________________\n" + "Here are the matching tasks in your list:");
-        for (int i = 0; i < arraylst.size(); i++) {
-            if (arraylst.get(i).getTask().contains(key)) {
-                System.out.println(counter + "." + arraylst.get(i).toString());
+        for (int i = 0; i < arrayLst.size(); i++) {
+            if (arrayLst.get(i).getTask().contains(key)) {
+                System.out.println(counter + "." + arrayLst.get(i).toString());
                 counter++;
             }
         }
         System.out.println("_________________________________________");
     }
 
-    public void processorList(ArrayList<Task> arraylst) {
+    public void processorList(ArrayList<Task> arrayLst) {
         System.out.println("_________________________________________\n" + "Here are the tasks in your list:");
-        for (int i = 0; i < arraylst.size(); i++) {
+        for (int i = 0; i < arrayLst.size(); i++) {
             int index = i+1;
-            System.out.println(index + "." + arraylst.get(i).toString());
+            System.out.println(index + "." + arrayLst.get(i).toString());
         }
         System.out.println("_________________________________________");
     }
 
-    public String processorDone(ArrayList<Task> arraylst, int index) {
+    public String processorDone(ArrayList<Task> arrayLst, int index) {
         int i = index - 1;
-        arraylst.get(i).setDone();
+        arrayLst.get(i).setDone();
         System.out.println("_________________________________________");
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println(index + "." + arraylst.get(i).toString());
+        System.out.println(index + "." + arrayLst.get(i).toString());
         System.out.println("_________________________________________");
-        return arraylst.get(i).toString();
+        return arrayLst.get(i).toString();
     }
 
-    public void processorDelete(ArrayList<Task> arraylst, int index) {
+    public void processorDelete(ArrayList<Task> arrayLst, int index) {
         int i = index - 1;
-        Task removed_task = arraylst.get(i);
-        arraylst.remove(i);
+        Task removed_task = arrayLst.get(i);
+        arrayLst.remove(i);
         System.out.println("_________________________________________");
         System.out.println("Noted, I've removed this task:");
         System.out.println(removed_task.toString());
-        System.out.println("Now you have " + arraylst.size() + " tasks in the list.");
+        System.out.println("Now you have " + arrayLst.size() + " tasks in the list.");
         System.out.println("_________________________________________");
     }
 
-    public String processorAdd(String cmd, ArrayList<Task> arraylst) throws DukeException {
+    public String processorAdd(String cmd, ArrayList<Task> arrayLst) throws DukeException {
         String[] stringarr = cmd.split(" ", 2);
         if (stringarr[0].equals("todo")) {
             if (stringarr.length <= 1) {
@@ -77,7 +77,7 @@ public class Processor {
                 throw new DukeException("_________________________________________\n" + message + "\n_________________________________________");
             } else {
                 Todo todo = new Todo(stringarr[1]);
-                arraylst.add(todo);
+                arrayLst.add(todo);
             }
         } else if (stringarr[0].equals("deadline")) {
             if (stringarr.length <= 1) {
@@ -87,7 +87,7 @@ public class Processor {
                 String[] secondarr = stringarr[1].split("/by", 2);
                 LocalDate date = LocalDate.parse(secondarr[1].trim());
                 Deadline deadline = new Deadline(secondarr[0], date);
-                arraylst.add(deadline);
+                arrayLst.add(deadline);
             }
         } else if (stringarr[0].equals("event")) {
             if (stringarr.length <= 1) {
@@ -96,18 +96,18 @@ public class Processor {
             } else {
                 String[] secondarr = stringarr[1].split("/at", 2);
                 Event event = new Event(secondarr[0], secondarr[1]);
-                arraylst.add(event);
+                arrayLst.add(event);
             }
         } else {
             String message = "OOPS!!! I'm sorry, but I don't know what that means :-(";
             throw new DukeException("_________________________________________\n" + message + "\n_________________________________________");
         }
-        int lastelem = arraylst.size() - 1;
-        Task latesttask = arraylst.get(lastelem);
+        int lastelem = arrayLst.size() - 1;
+        Task latesttask = arrayLst.get(lastelem);
         System.out.println("_________________________________________");
         System.out.println("Got it. I've added this task:");
         System.out.println(latesttask.toString());
-        System.out.println("Now you have " + arraylst.size() + " tasks in the list.");
+        System.out.println("Now you have " + arrayLst.size() + " tasks in the list.");
         System.out.println("_________________________________________");
         return latesttask.toString();
     }
