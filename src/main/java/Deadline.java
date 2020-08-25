5aa701584581fe9680924c36ea66d4af52dd0d53
Mehak Virdi <Mehak@mehaks-mbp.lan>
2020-08-25 20:13:14 +0800

    Level 8 draft 1 on branch

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " +
+                by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
