b6805c61bc9abae0a045ff681ead3ea2929270e8
aizatazhar <aizat.azharr@gmail.com>
2020-08-25 19:08:40 +0800

    Add Javadocs for exceptions

--- src/main/java/duke/exception/InvalidDeleteIndexException.java
+++ src/main/java/duke/exception/InvalidDeleteIndexException.java
@@ -1,5 +1,8 @@
 package duke.exception;
 
+/**
+ * Exception thrown when a delete command uses an invalid index.
+ */
 public class InvalidDeleteIndexException extends DukeException {
     public InvalidDeleteIndexException(int tasksSize) {
         super(String.format("No such task :(\nYou have %d tasks.", tasksSize));
--- src/main/java/duke/exception/InvalidDoneIndexException.java
+++ src/main/java/duke/exception/InvalidDoneIndexException.java
@@ -1,5 +1,8 @@
 package duke.exception;
 
+/**
+ * Exception thrown when a done command uses an invalid index.
+ */
 public class InvalidDoneIndexException extends DukeException {
     public InvalidDoneIndexException(int tasksSize) {
         super(String.format("No such task :(\nYou have %d tasks.", tasksSize));
