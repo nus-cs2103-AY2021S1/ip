c74f3d665a2d7265a333082b9b97a9342ce6aabb
Ashley-Lau <ashleylau451@gmail.com>
2020-08-26 03:04:02 +0800

    Add junit testing for Parser and Event class

---
+++ src/test/java/Parser/ParserTest.java
@@ -0,0 +1,63 @@
+package Parser;
+
+import Commands.ByeCommand;
+import Commands.DeleteCommand;
+import Commands.DoneCommand;
+import Commands.ListCommand;
+import Commands.AddCommand;
+import Duke.Parser;
+import Exceptions.InvalidCommandException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+
+    @Test
+    @DisplayName("Testing if ByeCommand is being outputted when bye is inputted")
+    public void parseByeCommand() throws InvalidCommandException {
+        assertEquals(Parser.parse("bye").getClass(), ByeCommand.class);
+    }
+
+    @Test
+    @DisplayName("Testing if ListCommand is being outputted when list is inputted")
+    public void parseListCommand() throws InvalidCommandException {
+        assertEquals(Parser.parse("list").getClass(), ListCommand.class);
+    }
+
+    @Test
+    @DisplayName("Testing if DoneCommand is being outputted when done is inputted")
+    public void parseDoneCommand() throws InvalidCommandException {
+        assertEquals(Parser.parse("done 2").getClass(), DoneCommand.class);
+    }
+
+    @Test
+    @DisplayName("Testing if DeleteCommand is being outputted when delete is inputted")
+    public void parseDeleteCommand() throws InvalidCommandException {
+        assertEquals(Parser.parse("delete").getClass(), DeleteCommand.class);
+    }
+
+    @Test
+    @DisplayName("Testing if AddCommand is being outputted when the deadline, event and todo command is inputted")
+    public void parseAddCommand() throws InvalidCommandException {
+        assertEquals(Parser.parse("event read book").getClass(), AddCommand.class);
+        assertEquals(Parser.parse("todo read book").getClass(), AddCommand.class);
+        assertEquals(Parser.parse("deadline read book").getClass(), AddCommand.class);
+    }
+
+    @Test
+    @DisplayName("Testing if isTask, isDeadline, isToDo and isEvent methods are working")
+    public void parseMiscellaneousCommand() throws InvalidCommandException {
+        assertTrue(Parser.isEventCommand("event"));
+        assertTrue(Parser.isDeadlineCommand("deadline"));
+        assertTrue(Parser.isToDoCommand("todo"));
+        assertTrue(Parser.isTask("todo"));
+    }
+
+    @Test
+    @DisplayName("Testing if InvalidCommandException is thrown if a invalid command is inputted")
+    public void parseInvalidCommand() {
+        assertThrows(InvalidCommandException.class, () -> Parser.parse("blah"));
+    }
+}
---
+++ src/test/java/Tasks/EventTester.java
@@ -0,0 +1,28 @@
+package Tasks;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTester {
+
+    @Test
+    @DisplayName("Testing getBy method in Event class")
+    public void testGetByTiming() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime testDate = LocalDateTime.parse("2020-02-02 12:12", formatter);
+        assertEquals(new Event("test", testDate).getBy(), "2020-02-02 12:12");
+    }
+
+    @Test
+    @DisplayName("Testing toString method in Event class")
+    public void testToString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime testDate = LocalDateTime.parse("2020-12-12 12:12", formatter);
+        assertEquals(new Event("read book", testDate).toString(), "[E][✘] read book (at: Dec 12 2020 12:12)");
+    }
+}
