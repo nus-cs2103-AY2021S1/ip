4301e59185c884f7892c2903b35f74e5896477c2
ameliatjy <e0425671@u.nus.edu>
2020-08-26 00:55:43 +0800

    Level-8 Change input deadline type

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,17 @@
-public class Deadline extends Task {
-    private String date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    Deadline(String taskName, boolean isCompleted, String date) {
+public class Deadline extends Task {
+    private LocalDateTime date;
+
+    Deadline(String taskName, boolean isCompleted, LocalDateTime date) {
         super(taskName, isCompleted);
         this.date = date;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + date + ")";
+        return "[D]" + super.toString() + "(by: " + date.format(DateTimeFormatter.ofPattern("d MMMM yyyy h:mm a")) + ")";
     }
 }
