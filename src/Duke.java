9cf38c1929ea09178b6c7caf4798271abe4c9bc0
Ma-Yueran <mayuerantom@outlook.com>
2020-08-26 00:27:07 +0800

    Use loadSavedTasks & saveCurrentTasks in Duke.java

--- src/Duke.java
+++ src/Duke.java
@@ -2,14 +2,19 @@ import input.UserInput;
 import function.DukeFunction;
 import data.DukeData;
 
+import java.io.IOException;
+
 public class Duke {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        DukeFunction.loadSavedTasks();
         DukeFunction.greet();
 
         while (!DukeData.exitLoop) {
             String inputLine = UserInput.getOneLine();
             DukeFunction.checkCommand(inputLine);
         }
+
+        DukeFunction.saveCurrentTasks();
     }
 }
