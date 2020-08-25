9a3bcb1a536d9fad10b31f0388a9535c8b1652ac
KenGondor <kengondorleh@gmail.com>
2020-08-25 17:47:56 +0800

    Refactored code in runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -9,7 +9,7 @@ del /S .\data\*
 
 
 REM compile the code into the bin folder
-javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\duke\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
