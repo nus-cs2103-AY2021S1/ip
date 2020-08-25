b5a7c589df297bfc8b4117377a2bd6a20abadeab
LAPTOP-S0GSC5AQ\kormi <kormingsoon@gmail.com>
2020-08-25 11:46:27 +0800

    Add UserInterface to handle printing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,11 +6,13 @@ public class Duke {
 
     private Storage storage;
     private Tasklist taskList;
+    private UserInterface ui;
 
     // constructor for the chat bot
     public Duke() {
         storage = new Storage();
         taskList = new Tasklist(storage);
+        ui = new UserInterface();
     }
 
     public static void main(String[] args) {
@@ -21,39 +23,18 @@ public class Duke {
         Duke duke = new Duke();
         duke.initialise();
         duke.userCommand();
+
+        // when bot stops
         duke.exit();
     }
 
     public void initialise() {
-        String logo = "       \\:.             .:/\n" +
-                "        \\``._________.''/ \n" +
-                "         \\             / \n" +
-                " .--.--, / .':.   .':. \\\n" +
-                "/__:  /  | '::' . '::' |\n" +
-                "   / /   |`.   ._.   .'|\n" +
-                "  / /    |.'         '.|\n" +
-                " /___-_-,|.\\  \\   /  /.|\n" +
-                "      // |''\\.;   ;,/ '|\n" +
-                "      `==|:=         =:|\n" +
-                "         `.          .'\n" +
-                "           :-._____.-:\n" +
-                "          `''       `''\n";
-
-        String startingMessage = "Pikachu: Hello, I am Pikachu! My pika service creates a to-do list for you!\n\n" +
-                "1. type 'list' and I list all that you said, along if it is completed\n" +
-                "2. type either 'event', 'deadline', 'todo', followed by the task!\n" +
-                "   2.1. if 'event', type the task followed by a '/at <duration>' to indicate duration\n" +
-                "   2.2. if 'deadline', type the task followed by a '/by <deadline>' to indicate deadline\n" +
-                "   2.3. if 'delete' type the task followed by a number within the list index to delete\n" +
-                "   2.4. if 'done' type the task followed by a number within the list index to mark it\n" +
-                "3. type 'done x' where x is the index of the item you want to be indicated done\n" +
-                "4. or you can say 'bye' to end us </3 Type your command:\n";
         try {
+            ui.welcomeMessage();
             taskList.loadList();
         } catch (IOException e) {
             System.out.print(e.getStackTrace());
         }
-        System.out.print(logo + startingMessage);
     }
 
     public void userCommand() {
@@ -84,19 +65,19 @@ public class Duke {
                 input = sc.nextLine();
             }
         } catch (DukeCommandException | DukeIndexException | DukeTaskException | DukeListException e) {
-            System.out.print(String.format("    ERROR: %s\n", e.getMessage()));
+            ui.printError(e.getMessage());
         } catch (DateTimeParseException e) {
-            System.out.print(String.format("  ERROR: INPUT DATE TIME FORMAT IS WRONG. \n"));
+            ui.printError("INPUT DATE TIME FORMAT IS WRONG");
         } catch (IOException e) {
-            System.out.print(e.getStackTrace());
+            ui.printError(e.getMessage());
         }
     }
 
     public void commandList() throws DukeListException {
         if (taskList.getTaskSize() != 0) {
-            System.out.print("Retrieving your list, patient ah!\n");
+            ui.listTask();
             for (int i = 0; i < taskList.getTaskSize(); i++) {
-                System.out.print(String.format("   %2d. %s\n", i + 1, taskList.get(i)));
+                ui.printTask(i + 1, taskList.get(i).toString());
             }
         } else {
             throw new DukeListException("Your list is empty.");
@@ -112,11 +93,9 @@ public class Duke {
 
         if (mode.equals("done")) {
             taskList.makeTaskDone(index);
-            System.out.print(String.format("    Swee la, task done liao!:\n" +
-                    "       %s\n", taskList.get(index)));
+            ui.printDone(taskList.get(index).toString());
         } else {
-            System.out.print(String.format("    Delete liao boss:\n" +
-                    "       %s\n    Remaining Tasks: %d\n", taskList.get(index), taskList.getTaskSize()));
+            ui.printDelete(taskList.get(index).toString(), taskList.getTaskSize());
             taskList.removeTask(index);
         }
 
@@ -143,11 +122,8 @@ public class Duke {
             }
 
             taskList.addTask(newTask);
+            ui.printAddTask(newTask.toString(), taskList.getTaskSize());
 
-            System.out.print("    Steady! I add... wait ah...\n");
-            System.out.print(String.format("        ADDED: %s\n", newTask));
-            System.out.print(String.format("    Now you got %d tasks in list, " +
-                    "don't procrastinate hor\n", size));
         } catch (IndexOutOfBoundsException e) {
             String errorMessage = "You might have left your message or duration empty.";
             throw new DukeTaskException(errorMessage);
@@ -159,33 +135,7 @@ public class Duke {
     }
 
     public void exit() {
-        String exitMessage = "Pikachu: Pika byebye! THUNDERBOLT!\n";
-        String bolt = "\n" +
-                "                  .-~*~--,.   .-.\n" +
-                "          .-~-. ./OOOOOOOOO\\.'OOO`9~~-.\n" +
-                "        .`OOOOOO.OOM.OLSONOOOOO@@OOOOOO\\\n" +
-                "       /OOOO@@@OO@@@OO@@@OOO@@@@@@@@OOOO`.\n" +
-                "       |OO@@@WWWW@@@@OOWWW@WWWW@@@@@@@OOOO).\n" +
-                "     .-'OO@@@@WW@@@W@WWWWWWWWOOWW@@@@@OOOOOO}\n" +
-                "    /OOO@@O@@@@W@@@@@OOWWWWWOOWOO@@@OOO@@@OO|\n" +
-                "   lOOO@@@OO@@@WWWWWWW\\OWWWO\\WWWOOOOOO@@@O.'\n" +
-                "    \\OOO@@@OOO@@@@@@OOW\\     \\WWWW@@@@@@@O'.\n" +
-                "     `,OO@@@OOOOOOOOOOWW\\     \\WWWW@@@@@@OOO)\n" +
-                "      \\,O@@@@@OOOOOOWWWWW\\     \\WW@@@@@OOOO.'\n" +
-                "        `~c~8~@@@@WWW@@W\\       \\WOO|\\UO-~'\n" +
-                "             (OWWWWWW@/\\W\\    ___\\WO)\n" +
-                "               `~-~''     \\   \\WW=*'\n" +
-                "                         __\\   \\\n" +
-                "                         \\      \\\n" +
-                "                          \\    __\\\n" +
-                "                           \\  \\\n" +
-                "                            \\ \\\n" +
-                "                             \\ \\\n" +
-                "                              \\\\\n" +
-                "                               \\\\\n" +
-                "                                \\\n" +
-                "                                 \\\n";
-        System.out.print(exitMessage + bolt);
+        ui.exitMessage();
     }
 
 }
---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,97 @@
+public class UserInterface {
+
+    private static final String LOGO =
+            "       \\:.             .:/\n" +
+            "        \\``._________.''/ \n" +
+            "         \\             / \n" +
+            " .--.--, / .':.   .':. \\\n" +
+            "/__:  /  | '::' . '::' |\n" +
+            "   / /   |`.   ._.   .'|\n" +
+            "  / /    |.'         '.|\n" +
+            " /___-_-,|.\\  \\   /  /.|\n" +
+            "      // |''\\.;   ;,/ '|\n" +
+            "      `==|:=         =:|\n" +
+            "         `.          .'\n" +
+            "           :-._____.-:\n" +
+            "          `''       `''\n";
+
+    private static final String thunderbolt =
+            "                  .-~*~--,.   .-.\n" +
+            "          .-~-. ./OOOOOOOOO\\.'OOO`9~~-.\n" +
+            "        .`OOOOOO.OOM.OLSONOOOOO@@OOOOOO\\\n" +
+            "       /OOOO@@@OO@@@OO@@@OOO@@@@@@@@OOOO`.\n" +
+            "       |OO@@@WWWW@@@@OOWWW@WWWW@@@@@@@OOOO).\n" +
+            "     .-'OO@@@@WW@@@W@WWWWWWWWOOWW@@@@@OOOOOO}\n" +
+            "    /OOO@@O@@@@W@@@@@OOWWWWWOOWOO@@@OOO@@@OO|\n" +
+            "   lOOO@@@OO@@@WWWWWWW\\OWWWO\\WWWOOOOOO@@@O.'\n" +
+            "    \\OOO@@@OOO@@@@@@OOW\\     \\WWWW@@@@@@@O'.\n" +
+            "     `,OO@@@OOOOOOOOOOWW\\     \\WWWW@@@@@@OOO)\n" +
+            "      \\,O@@@@@OOOOOOWWWWW\\     \\WW@@@@@OOOO.'\n" +
+            "        `~c~8~@@@@WWW@@W\\       \\WOO|\\UO-~'\n" +
+            "             (OWWWWWW@/\\W\\    ___\\WO)\n" +
+            "               `~-~''     \\   \\WW=*'\n" +
+            "                         __\\   \\\n" +
+            "                         \\      \\\n" +
+            "                          \\    __\\\n" +
+            "                           \\  \\\n" +
+            "                            \\ \\\n" +
+            "                             \\ \\\n" +
+            "                              \\\\\n" +
+            "                               \\\\\n" +
+            "                                \\\n" +
+            "                                 \\\n";
+
+    private static final String BORDER = "      ";
+
+    private static final String ERROR = "    ERROR: ";
+
+    private static final String newLine = "\n";
+
+    public void printAddTask(String task, int totalNumber) {
+        System.out.print(BORDER + "Steady! I add... wait ah.." + newLine);
+        System.out.print(BORDER + BORDER + "ADDED: " + task + newLine);
+        System.out.print(BORDER + "Now you got " + totalNumber + " tasks" + newLine);
+    }
+
+    public void listTask() {
+        System.out.print(BORDER + "Retrieving your list, patient ah!\n");
+    }
+
+    public void printTask(int listNumber, String task) {
+        String toPrint = String.format("%2d. %s\n", listNumber, task);
+        System.out.print(BORDER + toPrint);
+    }
+
+    public void printDone(String task) {
+        System.out.print(BORDER + "Swee la, task done liao:" + newLine);
+        System.out.print(BORDER + BORDER + task + newLine);
+    }
+
+    public void printDelete(String task, int remaining) {
+        System.out.print(BORDER + "Delete liao boss:" + newLine);
+        System.out.print(BORDER + "Remaining Tasks: " + remaining);
+    }
+
+    public void printError(String errorMessage) {
+        System.out.print(ERROR + errorMessage + newLine);
+    }
+
+    public void welcomeMessage() {
+        String startingMessage = "Pikachu: Hello, I am Pikachu! My pika service creates a to-do list for you!\n\n" +
+                "1. type 'list' and I list all that you said, along if it is completed\n" +
+                "2. type either 'event', 'deadline', 'todo', followed by the task!\n" +
+                "   2.1. if 'event', type the task followed by a '/at <duration>' to indicate duration\n" +
+                "   2.2. if 'deadline', type the task followed by a '/by <deadline>' to indicate deadline\n" +
+                "   2.3. if 'delete' type the task followed by a number within the list index to delete\n" +
+                "   2.4. if 'done' type the task followed by a number within the list index to mark it\n" +
+                "3. type 'done x' where x is the index of the item you want to be indicated done\n" +
+                "4. or you can say 'bye' to end us </3 Type your command:\n";
+        System.out.print(LOGO + startingMessage);
+    }
+
+    public void exitMessage() {
+        String exitMessage = "Pikachu: Pika byebye! THUNDERBOLT!\n";
+        System.out.print(thunderbolt + exitMessage);
+    }
+
+}
