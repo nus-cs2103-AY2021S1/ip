d000fe9041b420582c2af4800204cc90ca1fba60
koonweee <34903259+koonweee@users.noreply.github.com>
2020-08-26 02:40:13 +0800

    Add JUnit Tests

--- src/test/java/TodoTest.java
+++ src/test/java/TodoTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoTest {
+    @Test
+    public void testTodo() {
+        Todo testTodo = new Todo("Todo 1");
+        assertEquals("[T][✗] Todo 1",testTodo.toString());
+    }
+    @Test
+    public void testTodoDone() {
+        Todo testTodo = new Todo("Todo 1");
+        testTodo.setDone();
+        assertEquals("[T][✓] Todo 1",testTodo.toString());
+    }
 }
