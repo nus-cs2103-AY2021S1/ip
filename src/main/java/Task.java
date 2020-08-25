4d5631fef786b62c2d3ed07a8392f252cf103696
Su <kkangs0226@gmail.com>
2020-08-26 01:20:40 +0800

    Add Level 7 save feature
    Any changes made to the list of tasks does not get saved, and every run
    of the program would give an empty list at the start, which is not
    useful to the user.
    
    Adding save feature would allow the program to save the changes to
    the list of tasks from the previous run of the program.
    
    Let's
    * save the list of tasks in a duke.txt file under data folder
      every time there are changes to the list of tasks
    * read data from the duke.txt file at the start of the program
      run so that the data from previous program can be called and
      used in the program

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,6 +19,10 @@ public class Task {
         this.isDone = true;
     }
 
+    public String toFileString() {
+        return "Error";
+    }
+
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -5,6 +5,12 @@ public class Todo extends Task{
     }
 
     @Override
+    public String toFileString() {
+        String doneInteger = isDone ? "1" : "0";
+        return "T | " + doneInteger + " | " + this.description;
+    }
+
+    @Override
     public String toString() {
         return "[T]" + super.toString();
     }
