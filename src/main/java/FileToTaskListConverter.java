9f9387d1e01e0616382acc192793110283e40244
Jaylenlee <e0424677@u.nus.edu>
2020-08-25 10:03:29 +0800

    Change File Conversion function
    FileToTaskListConverter now extracts data from file and build task list only if data stored is formatted in:
    type//doneStatus//description of action//date (if applicable)//time (if applicable)
    
    else a FileCorruptedException will be thrown.

--- src/main/java/FileToTaskListConverter.java
+++ src/main/java/FileToTaskListConverter.java
@@ -1,3 +1,5 @@
+import Exception.DukeException;
+import Exception.FileCorruptedException;
 import Task.DeadlineTask;
 import Task.EventTask;
 import Task.Task;
@@ -6,12 +8,13 @@ import Task.ToDoTask;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.PatternSyntaxException;
 
 public abstract class FileToTaskListConverter {
-    public static List<Task> convert(File data) {
+    public static List<Task> convert(File data) throws FileCorruptedException {
         List<String> dataList = loadStringData(data);
         List<Task> list = new ArrayList<>();
 
@@ -48,25 +51,29 @@ public abstract class FileToTaskListConverter {
         }
     }
 
-    private static Task getTaskFromData(String info) {
+    private static Task getTaskFromData(String info) throws FileCorruptedException {
+        try {
+            String[] words = info.split("//");
+            char type = words[0].charAt(0);
+            boolean isDone = words[1].equals("✓");
+            String description = words[2];
 
-        char type = info.charAt(1);
-        boolean isDone = Character.toString(info.charAt(4)).equals("✓");
-        int indexOfAction = info.indexOf(" ") + 1;
-        int indexOfOpenBrackets = info.indexOf("(");
-        int indexOfCloseBrackets = info.indexOf(")");
-
-        switch (type) {
-        case 'T':
-            return new ToDoTask(info.substring(indexOfAction), isDone);
-        case 'D':
-            return new DeadlineTask(info.substring(indexOfAction, indexOfOpenBrackets - 1),
-                    info.substring(indexOfOpenBrackets + 5, indexOfCloseBrackets), isDone);
-        case 'E':
-            return new EventTask(info.substring(indexOfAction, indexOfOpenBrackets - 1),
-                    info.substring(indexOfOpenBrackets + 5, indexOfCloseBrackets), isDone);
-        default:
-            return new Task("No Task !!!!");
+            switch (type) {
+            case 'T':
+                return new ToDoTask(description, isDone);
+            case 'D':
+                return new DeadlineTask(description, words[3] + " " + words[4], isDone);
+            case 'E':
+                return new EventTask(description, words[3] + " " + words[4], isDone);
+            default:
+                return new Task("No Task !!!!");
+            }
+        } catch (DukeException de) {
+            throw new FileCorruptedException();
+        } catch (IndexOutOfBoundsException ioobe) {
+            throw new FileCorruptedException();
+        } catch (PatternSyntaxException pse) {
+            throw new FileCorruptedException();
         }
     }
 }
