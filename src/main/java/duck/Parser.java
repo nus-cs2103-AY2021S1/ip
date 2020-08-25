6eb9599258f0afae4a13710e38fa745755f13818
tankangliang <tankangliang98@gmail.com>
2020-08-25 19:55:15 +0800

    Add JavaDoc for main duck package

--- src/main/java/duck/Parser.java
+++ src/main/java/duck/Parser.java
@@ -7,15 +7,35 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.Date;
 
+/**
+ * Parser is a helper class that contains static methods for
+ * parsing the various components from the user input.
+ */
 public class Parser {
     private static final String[] dateSeparators = {"/at", "/by"};
 
+    /**
+     * Returns the Option enumerator based on the first word separated
+     * by whitespace.
+     *
+     * @param input Input from user.
+     * @return Option enumerator.
+     */
     public static Option parseOption(String input) {
         String[] inputSplit = input.split("\\s+");
         Option option = Option.from(inputSplit[0]);
         return option;
     }
 
+    /**
+     * Returns the description portion of the user input.
+     * The first check is for the presence of any date and strips the date portion away.
+     * The second check is for a valid description length.
+     *
+     * @param input Input from user.
+     * @return Description portion of user input.
+     * @throws DuckException If input is empty.
+     */
     public static String parseDescription(String input) throws DuckException {
         input = input.strip();
         for (String separator : Parser.dateSeparators) {
@@ -31,6 +51,16 @@ public class Parser {
         return input;
     }
 
+    /**
+     * Returns the date portion of the user input.
+     * Strips away the input before the separators including the separator.
+     * Checks for a valid date length before parsing with LocalDate.
+     * If no valid date is obtained, an exception is thrown.
+     *
+     * @param input Input from user.
+     * @return Date of LocalDate class.
+     * @throws DuckException If date field is empty or not in the correct format.
+     */
     public static LocalDate parseDate(String input) throws DuckException {
         String date = "";
         for (String separator : Parser.dateSeparators) {
@@ -48,10 +78,17 @@ public class Parser {
         } catch (DateTimeParseException e) {
             throw new DuckException("Date format not supported. Use \"yyyy-mm-dd\", E.g. 2020-02-02");
         }
-
-
     }
 
+    /**
+     * Returns the task number portion of the user input.
+     * Task number should be the second portion delimited by whitespace.
+     * A valid input with a task number should only have 2 words, the option and task number.
+     *
+     * @param input Input from user.
+     * @return Task number of type int
+     * @throws DuckException If input is invalid or task number portion is not a number.
+     */
     public static int parseTaskNumber(String input) throws DuckException {
         String[] inputSplit = input.split("\\s+");
         if (inputSplit.length != 2) {
