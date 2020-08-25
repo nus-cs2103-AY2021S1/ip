a3c7e1c9a85296f8e765cf665bd5f2c3448d87bf
Sean Chan Ding Chao <dcchan98@gmail.com>
2020-08-25 11:12:59 +0800

    Parser added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,11 +5,15 @@ public class Duke {
     public static void main(String[] args) {
         Ui myDukeBot = new Ui();
         Storage myStorage = new Storage();
+        Parser myParser = new Parser();
+
         myDukeBot.greeting();
         myStorage.createDirectory("ToDo");
         myStorage.populateList(myDukeBot);
         myDukeBot.list();
-        myDukeBot.listener();
+
+        myParser.listener(myDukeBot);
+
         myStorage.updateDirectory(myDukeBot);
         System.out.println("End");
 
---
+++ src/main/java/Parser.java
@@ -0,0 +1,108 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Parser {
+
+    public void listener(Ui myUi) {
+        Scanner fetch = new Scanner(System.in);
+        String currInput = fetch.nextLine();
+
+        while (true) {
+            myUi.horizontalRule();
+            String[] fullArg = currInput.split("/");
+            String[] args1 = fullArg[0].split(" ");
+
+
+            if (currInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                myUi.horizontalRule();
+                break;
+            }
+
+            if (args1[0].equals("todo")) {
+                String task = "";
+                for (int i = 1; i < args1.length; i++) {
+                    task += args1[i] + " ";
+                }
+                try {
+                    myUi.addTask("[T]",task);
+                }
+                catch (InSuffArgsException E){
+                    System.out.println("☹ OOPS!!! The description of a " + args1[0] + " cannot be empty.");
+                }
+                myUi.numTask();
+            } else if (args1[0].equals("delete")) {
+                myUi.deleteTask(Integer.parseInt(args1[1]) - 1);
+            } else if (args1[0].equals("deadline")) {
+                String task = "";
+                for (int i = 1; i < args1.length; i++) {
+                    task += args1[i] + " ";
+                }
+                try {
+                    if(fullArg.length==1){
+                        throw new InSuffArgsException();
+                    }
+                    if(fullArg.length==4){
+                        String day = Integer.parseInt(fullArg[1].substring(fullArg[1].length()-1))+"";
+                        if(day.length()==1){
+                            day= "0"+day;
+                        }
+                        String month = Integer.parseInt(fullArg[2])+"";
+                        if(month.length()==1){
+                            month= "0"+month;
+                        }
+                        int year = Integer.parseInt(fullArg[3].split(" ")[0]);
+                        //int time = Integer.parseInt(fullArg[3].split(" ")[1]);
+
+                        //System.out.println(time);
+
+                        LocalDate d1 = LocalDate.parse(year+"-"+month+"-"+day);
+                        myUi.addTask("[D]",task,d1);
+
+                    }else{
+                        myUi.addTask("[D]",task ,fullArg[1]);
+                    }
+
+                }
+                catch (InSuffArgsException E){
+                    System.out.println("☹ OOPS!!! The description of a " + args1[0] + " cannot be empty.");
+                }
+
+                myUi.numTask();
+
+            } else if (args1[0].equals("event")) {
+
+                String task = "";
+                for (int i = 1; i < args1.length; i++) {
+                    task += args1[i] + " ";
+                }
+                try {
+                    if(fullArg.length==1){
+                        throw new InSuffArgsException();
+                    }
+                    myUi.addTask("[E]",task ,fullArg[1]);
+                }
+                catch (InSuffArgsException E){
+                    System.out.println("☹ OOPS!!! The description of a " + args1[0] + " cannot be empty.");
+                }
+                myUi.numTask();
+
+            } else if (currInput.equals("list")) {
+                myUi.list();
+            } else if (args1[0].equals("done")) {
+                int index = Integer.parseInt(args1[1]) - 1;
+                myUi.getTasks().get(index).setDone();
+                System.out.println("Nice I've marked this tasks as done");
+                System.out.println( myUi.getTasks().get(index));
+            } else {
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
+            myUi.horizontalRule();
+
+            currInput = fetch.nextLine();
+        }
+
+
+    }
+
+}
