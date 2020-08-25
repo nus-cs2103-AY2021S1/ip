4bf62d7886cf7a31ec1f66c4a6a3524efdc0a96e
chshen1998 <chewhoashen@gmail.com>
2020-08-25 13:15:25 +0800

    Add LocalDateTime to Tasks

--- src/main/java/Task/Deadline.java
+++ src/main/java/Task/Deadline.java
@@ -1,17 +1,17 @@
 package Task;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class Deadline extends Task {
     protected String by;
-    protected LocalDate byDate;
+    protected LocalDateTime byDate;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
         try {
-            this.byDate = LocalDate.parse(by);
+            this.byDate = LocalDateTime.parse(reformatedDateTime());
         } catch (DateTimeParseException e) {
             this.byDate = null;
         }
@@ -25,5 +25,15 @@ public class Deadline extends Task {
             return "[D]" + icon + " " + this.description + " (by: "
                     + this.byDate.format(DateTimeFormatter.ofPattern("MMM d yyy")) + ")";
         }
+    }
+
+    private String reformatedDateTime() {
+        String[] bySplit = this.by.split(" ", 2);
+        String date = bySplit[0];
+        String[] dateSplit = date.split("/", 3);
+        String time = bySplit[1];
+        String hour = time.substring(0, 2);
+        String minute = time.substring(2);
+        return dateSplit[2] + "-" + dateSplit[1] + "-" + dateSplit[0] + "T" + hour + ":" + minute + ":00";
     }
 }
--- src/main/java/Task/Event.java
+++ src/main/java/Task/Event.java
@@ -1,30 +1,40 @@
 package Task;
 import java.time.DateTimeException;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class Event extends Task {
     protected String at;
-    protected LocalDate atDate;
+    protected LocalDateTime atDateTime;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
         try {
-            this.atDate = LocalDate.parse(at);
+            this.atDateTime = LocalDateTime.parse(reformatedDateTime());
         } catch (DateTimeParseException e) {
-            this.atDate = null;
+            this.atDateTime = null;
         }
     }
 
     public String toString() {
         String icon = this.completed ? "[" + "\u2713" + "]" : "[" + "\u2718" + "]";
-        if (atDate == null) {
+        if (atDateTime == null) {
             return "[E]" + icon + " " + this.description + " (at: " + this.at + ")";
         } else {
             return "[E]" + icon + " " + this.description + " (at: "
-                    + this.atDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+                    + this.atDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
         }
+    }
+
+    private String reformatedDateTime() {
+        String[] bySplit = this.at.split(" ", 2);
+        String date = bySplit[0];
+        String[] dateSplit = date.split("/", 3);
+        String time = bySplit[1];
+        String hour = time.substring(0, 2);
+        String minute = time.substring(2);
+        return dateSplit[2] + "-" + dateSplit[1] + "-" + dateSplit[0] + "T" + hour + ":" + minute + ":00";
     }
 }
