ebdaf881863724f6593e00bc120ee9f2faba087b
gene <iamgenechua@gmail.com>
2020-08-25 14:20:59 +0800

    Update runtest.sh with clear command

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/duke/*.java
+if ! javac -cp ../src -Xlint:none -d ../bin/ ../src/main/java/duke/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin duke.Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 # cp EXPECTED.TXT EXPECTED-UNIX.TXT
