c3e942aa051a8a404af97d24a6feba16e6f4aed6
xxzz-tt <tanxuanzhi@gmail.com>
2020-08-25 23:42:47 +0800

    Follow the Coding Standard

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -58,93 +58,91 @@ public class Parser {
                 firstWord = s;
             }
             switch (firstWord) {
-                case "bye":
-                    exit();
-                    break;
-                case "list":
-                    generateList();
-                    break;
-
-                case "done":
-                    char x = s.charAt(s.length() - 1);
-                    int i = Character.getNumericValue(x);
-                    Task t = taskList.getList().get(i - 1);
-                    t.markAsDone();
-                    taskList.getList().set(i - 1, t);
+            case "bye":
+                exit();
+                break;
+            case "list":
+                generateList();
+                break;
+            case "done":
+                char x = s.charAt(s.length() - 1);
+                int i = Character.getNumericValue(x);
+                Task t = taskList.getList().get(i - 1);
+                t.markAsDone();
+                taskList.getList().set(i - 1, t);
+                System.out.println(line);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(t.printTask());
+                System.out.println(line);
+                break;
+            case "delete":
+                char y = s.charAt(s.length() - 1);
+                int k = Character.getNumericValue(y);
+                System.out.println(line);
+                System.out.println("Noted. I've removed this task:");
+                System.out.println(taskList.getList().get(k - 1).printTask());
+                taskList.delete(k);
+                System.out.println("Now you have " + taskList.getList().size() +
+                        (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list");
+                System.out.println(line);
+                break;
+            case "deadline":
+                if (s.length() != "deadline".length()) {
+                    String[] value = s.split(" /by ");
+                    String date = value[1];
+                    DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+                    DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
+                    LocalDateTime dateTime = LocalDateTime.parse(date, inputFormat);
+                    dateTime.format(outputFormat);
+                    Deadline deadline = new Deadline(value[0].replace("deadline ", ""), dateTime);
+//                list.add(deadline);
+                    taskList.addTask(deadline);
+                    storage.saveTask(deadline);
                     System.out.println(line);
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(t.printTask());
-                    System.out.println(line);
-
-                    break;
-                case "delete":
-                    char y = s.charAt(s.length() - 1);
-                    int k = Character.getNumericValue(y);
-                    System.out.println(line);
-                    System.out.println("Noted. I've removed this task:");
-                    System.out.println(taskList.getList().get(k - 1).printTask());
-                    taskList.delete(k);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(deadline.printTask());
                     System.out.println("Now you have " + taskList.getList().size() +
                             (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list");
                     System.out.println(line);
                     break;
-                case "deadline":
-                    if (s.length() != "deadline".length()) {
-                        String[] value = s.split(" /by ");
-                        String date = value[1];
-                        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
-                        DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
-                        LocalDateTime dateTime = LocalDateTime.parse(date, inputFormat);
-                        dateTime.format(outputFormat);
-                        Deadline deadline = new Deadline(value[0].replace("deadline ", ""), dateTime);
-//                list.add(deadline);
-                        taskList.addTask(deadline);
-                        storage.saveTask(deadline);
-                        System.out.println(line);
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(deadline.printTask());
-                        System.out.println("Now you have " + taskList.getList().size() +
-                                (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list");
-                        System.out.println(line);
-                        break;
-                    } else {
-                        throw new IncorrectInputException("☹ OOPS!!! The description of an deadline cannot be empty.");
-                    }
-                case "todo":
-                    if (s.length() != "todo".length()) {
-                        ToDo toDo = new ToDo(s.replace("todo ", ""));
+                } else {
+                    throw new IncorrectInputException("☹ OOPS!!! The description of an deadline cannot be empty.");
+                }
+            case "todo":
+                if (s.length() != "todo".length()) {
+                    ToDo toDo = new ToDo(s.replace("todo ", ""));
 //                list.add(toDo);
-                        taskList.addTask(toDo);
-                        storage.saveTask(toDo);
-                        System.out.println(line);
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(toDo.printTask());
-                        System.out.println("Now you have " + taskList.getList().size() +
-                                (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list");
-                        System.out.println(line);
-                        break;
-                    } else {
-                        throw new IncorrectInputException("☹ OOPS!!! The description of a todo cannot be empty.");
-                    }
-                case "event":
-                    if (s.length() != "event".length()) {
-                        String[] value = s.split(" /at ");
-                        Event event = new Event(value[0].replace("event ", ""), value[1]);
+                    taskList.addTask(toDo);
+                    storage.saveTask(toDo);
+                    System.out.println(line);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(toDo.printTask());
+                    System.out.println("Now you have " + taskList.getList().size() +
+                            (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list");
+                    System.out.println(line);
+                    break;
+                } else {
+                    throw new IncorrectInputException("☹ OOPS!!! The description of a todo cannot be empty.");
+                }
+            case "event":
+                if (s.length() != "event".length()) {
+                    String[] value = s.split(" /at ");
+                    Event event = new Event(value[0].replace("event ", ""), value[1]);
 //                list.add(event);
-                        taskList.addTask(event);
-                        storage.saveTask(event);
-                        System.out.println(line);
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(event.printTask());
-                        System.out.println("Now you have " + taskList.getList().size() +
-                                (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list");
-                        System.out.println(line);
-                        break;
-                    } else {
-                        throw new IncorrectInputException("☹ OOPS!!! The description of an event cannot be empty.");
-                    }
-                default:
-                    throw new IncorrectInputException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    taskList.addTask(event);
+                    storage.saveTask(event);
+                    System.out.println(line);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(event.printTask());
+                    System.out.println("Now you have " + taskList.getList().size() +
+                            (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list");
+                    System.out.println(line);
+                    break;
+                } else {
+                    throw new IncorrectInputException("☹ OOPS!!! The description of an event cannot be empty.");
+                }
+            default:
+                throw new IncorrectInputException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
