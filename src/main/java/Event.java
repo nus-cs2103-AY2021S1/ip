77a5a15bcba4674b7ec8ee9ecdfb52bdc7abe25c
Penn Han <leepennhan98@gmail.com>
2020-08-25 14:19:33 +0800

    Javadoc for Event Class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,3 +1,14 @@
+/**
+ * <h1> Event Task class </h1>
+ *
+ * The event class is a child class of Task
+ * It contains an extra property: event that indicates
+ * the date that a task is happening.
+ *
+ * @author Lee Penn Han
+ * @version 1.0
+ * @since 2020-08-25
+ */
 public class Event extends Task {
 
     protected String event;
