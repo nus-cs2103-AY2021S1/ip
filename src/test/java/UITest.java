7d1ae75692631cedbd389298d9d3a52c542c2734
WaiLok <e0323018@u.nus.edu>
2020-08-25 22:49:14 +0800

    A-JUnit

---
+++ src/test/java/UITest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class UITest {
+    @Test
+    public void MessagePrintTest(){
+            String testString = "☹ OOPS!!! The description of a Deadline cannot be empty.";
+            UI ui = new UI();
+    }
+}
