f64d33734193288859fff6804670c8d950a2d1a8
chan-j-d <chan.j.d.98@gmail.com>
2020-08-26 03:35:59 +0800

    Create FindCommand.java

---
+++ src/main/java/duke/command/FindCommand.java
@@ -0,0 +1,50 @@
+package duke.command;
+
+import duke.Ui;
+import duke.storage.Storage;
+import duke.task.Task;
+import duke.util.TaskList;
+
+public class FindCommand extends Command {
+
+    protected static final String FOUND_MESSAGE = "Here are the matching tasks in your list:";
+    protected static final String NONE_FOUND_MESSAGE = "It seems there were no matches!";
+
+    protected final String searchString;
+
+    private FindCommand(String searchString) {
+        this.searchString = searchString;
+    }
+
+    public static FindCommand parse(String searchString) {
+        return new FindCommand(searchString);
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) {
+        TaskList matchingTasks = getMatchingTasks(searchString, taskList);
+        ui.outputMessage(createFindMessage(matchingTasks));
+    }
+
+    protected TaskList getMatchingTasks(String searchString, TaskList taskList) {
+        TaskList matchingList = new TaskList();
+        for (Task task : taskList) {
+            if (task.getTaskDescription().contains(searchString)) {
+                matchingList.add(task);
+            }
+        }
+        return matchingList;
+    }
+
+    protected String createFindMessage(TaskList taskList) {
+        if (taskList.size() == 0) {
+            return NONE_FOUND_MESSAGE;
+        } else {
+            return FOUND_MESSAGE + '\n' + taskList.toString();
+        }
+    }
+
+
+
+
+}
