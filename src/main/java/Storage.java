05f90cd6b83f74e67bb4605334086a7c5bba1b14
Nahoyhp <phyohan@hotmail.com>
2020-08-25 11:13:05 +0800

    Create Storage and Ui class.

---
+++ src/main/java/Storage.java
@@ -0,0 +1,78 @@
+package main.java;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+    private final File file;
+
+    public Storage(String filePath) {
+        this.file = new File(filePath);
+    }
+
+    public void saveFile(String data) throws DukeException {
+        try {
+            FileWriter fw = new FileWriter(file);
+            fw.write(data);
+            fw.close();
+        } catch (IOException err) {
+            throw new DukeException("File Path is a directory -OR- Can't create file at location");
+        }
+    }
+
+    public ArrayList<Task> loadFile() throws DukeException {
+        try {
+            ArrayList<Task> loadedTask = new ArrayList<>();
+            Scanner scanner = new Scanner(this.file);
+            String[] dataRead;
+            while (scanner.hasNext()) {
+                dataRead = readSavedData(scanner.nextLine());
+                Task newTask = createSavedTask(dataRead);
+                loadedTask.add(newTask);
+            }
+            return loadedTask;
+        } catch (FileNotFoundException err) {
+            throw new DukeException("File Not Found");
+        }
+    }
+
+    private String[] readSavedData(String inputLine) {
+        String[] parts = inputLine.split(" \\| ");
+        ArrayList<String> result = new ArrayList<>();
+        for (String part : parts) {
+            result.add(part.trim());
+        }
+        System.out.println(result);
+        return result.toArray(parts);
+    }
+
+    private Task createSavedTask(String[] args) throws DukeException {
+        Task newTask;
+        try{
+            switch(args[0]) {
+            case "T":
+                newTask = new ToDo(args[2]);
+                break;
+            case "D":
+                newTask = new Deadline(args[2], args[3]);
+                break;
+            case "E":
+                newTask = new Event(args[2], args[3]);
+                break;
+            default:
+                throw new DukeException("Error: Saved File is badly corrupted");
+            }
+        } catch (Exception err) {
+            throw new DukeException("Error: Saved File is badly corrupted");
+        }
+
+        if (args[1].equals("1")) {
+            newTask.setDone();
+        }
+        return newTask;
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,55 @@
+package main.java;
+
+public class Ui {
+
+    public Ui() {
+    }
+
+    public void show(String input) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println(input);
+        System.out.println(line);
+    }
+
+    public void showWelcome() {
+        show("Duke at your service. How may I help?");
+    }
+
+    public void showLoadingError() {
+        showError("Can't load as file is missing\n" +
+                "If it is your first time opening, ignore this message.");
+    }
+
+    public void showTaskAdded(String taskDetail, int taskCount) {
+        String first = "Got it. I've added this task:\n";
+        String second = "    " + taskDetail + "\n";
+        String third = String.format("Now you have %d tasks in the list", taskCount);
+        show(first + second + third);
+    }
+
+    public void showTaskDone(String taskDetail) {
+        show("Nice! I have marked this task as done:\n" +
+                taskDetail);
+    }
+
+    public void showTasks(String taskDetails) {
+        show("Here are the tasks in your list\n" + taskDetails);
+    }
+
+    public void showDeletedTasks(String taskDetail) {
+        show("Alright! I have deleted this task:\n" + taskDetail);
+    }
+
+    public void showError(String err) {
+        String line = "************************************************************";
+        System.out.println(line);
+        System.out.println(err);
+        System.out.println(line);
+    }
+
+    public void showBye() {
+        show("Bye. See you again");
+    }
+
+}
