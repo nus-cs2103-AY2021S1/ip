85e9d7dc3a5c37ceb19eda72e15d623fb8dc3296
yeohhq <yeohhuiqing00@gmail.com>
2020-08-26 01:24:48 +0800

    Give users a way to find a task by searching for a keyword.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -74,6 +74,8 @@ public class Parser {
                 }
             case "bye":
                 return new endCommand(tasks);
+            case "find":
+                return new findCommand(tasks, description);
 
             default:
                 duke.ui.badInput();
