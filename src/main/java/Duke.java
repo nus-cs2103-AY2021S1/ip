5242b8c6c745f47f380f334c2eb7d186120bf6dc
lebencwb <46550747+lebencwb@users.noreply.github.com>
2020-08-25 22:15:23 +0800

    move list mutation actions into separate method from main, add constant variables

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,110 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+import java.io.File;
+
 public class Duke {
+    private static final String HORIZONTAL_LINE =
+            "\t-------------------------------------------------------";
+    private static final String STANDARD_GREETING = HORIZONTAL_LINE
+            + "\n\tHello! I'm Duke\n" + "\t" + "What can I do for you?\n"
+            + HORIZONTAL_LINE;
+    private static final String GOODBYE_MESSAGE = "\tBye. Hope to see you again soon!";
+    private static final String ERROR_PREFIX = "\t\u2639" + " OOPS!!! ";
+    private static final String CURRENT_TASKS = "\tNow you have %d task(s) in the list.";
+    private static final String INVALID_TASK = ERROR_PREFIX + "Sorry, that is not a valid task.";
+    private static final ArrayList<Task> list = new ArrayList<>();
+
+    private static void updateList(String userInput) {
+        Scanner inputScanner = new Scanner(userInput);
+        String command = inputScanner.next();
+
+        switch (command.toLowerCase()) {
+        case "done":
+            int index = inputScanner.nextInt();
+            try {
+                Task task = list.get(index - 1);
+                task.markAsDone();
+                System.out.println("\tNice! I've marked this as done:");
+                System.out.println("\t  " + task.toString());
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(INVALID_TASK);
+            }
+            break;
+        case "delete":
+            index = inputScanner.nextInt();
+            try {
+                System.out.println("\tNoted. I've removed this task:");
+                System.out.println("\t" + list.remove(index - 1).toString());
+                System.out.println(String.format(CURRENT_TASKS, list.size()));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(INVALID_TASK);
+            }
+            break;
+        case "todo":
+            if (!inputScanner.hasNext()) {
+                System.out.println(ERROR_PREFIX
+                        + "The description of a todo cannot be empty.");
+            } else {
+                System.out.println("\tGot it. I've added this task:");
+                ToDo todo = new ToDo(inputScanner.nextLine());
+                list.add(todo);
+                System.out.println("\t  " + todo.toString());
+                System.out.println(String.format(CURRENT_TASKS, list.size()));
+            }
+            break;
+        case "deadline":
+            if (!inputScanner.hasNext()) {
+                System.out.println(ERROR_PREFIX
+                        + "The description of a deadline cannot be empty.");
+            } else {
+                System.out.println("\tGot it. I've added this task:");
+                inputScanner.useDelimiter("( /by )");
+                String description = inputScanner.next();
+                inputScanner.reset();
+                inputScanner.next();
+                String by = inputScanner.nextLine().trim();
+                Deadline deadline = new Deadline(description, by);
+                list.add(deadline);
+                System.out.println("\t  " + deadline.toString());
+                System.out.println(String.format(CURRENT_TASKS, list.size()));
+            }
+            break;
+        case "event":
+            if (!inputScanner.hasNext()) {
+                System.out.println(ERROR_PREFIX
+                        + "The description of an event cannot be empty.");
+            } else {
+                System.out.println("\tGot it. I've added this task:");
+                inputScanner.useDelimiter("( /at )");
+                String description = inputScanner.next();
+                inputScanner.reset();
+                inputScanner.next();
+                String at = inputScanner.nextLine().trim();
+                Event event = new Event(description, at);
+                list.add(event);
+                System.out.println("\t  " + event.toString());
+                System.out.println(String.format(CURRENT_TASKS, list.size()));
+            }
+            break;
+        default:
+            System.out.println(ERROR_PREFIX
+                    + "I'm sorry, but I don't know what that means :-(");
+            break;
+        }
+    }
+
     public static void main(String[] args) {
-        String horizontalLine = "\t-------------------------------------------------------";
-        String errorPrefix = "\t\u2639" + " OOPS!!! ";
-        System.out.println(horizontalLine);
-        System.out.println("\tHello! I'm Duke\n" + "\t" + "What can I do for you?");
-        System.out.println(horizontalLine);
+        System.out.println(STANDARD_GREETING);
+
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> list = new ArrayList<>();
+
         while (scanner.hasNext()) {
-            System.out.println(horizontalLine);
+            System.out.println(HORIZONTAL_LINE);
             String userInput = scanner.nextLine();
             if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("\tBye. Hope to see you again soon!");
-                System.out.println(horizontalLine);
+                System.out.println(GOODBYE_MESSAGE);
+                System.out.println(HORIZONTAL_LINE);
                 scanner.close();
                 break;
             }
@@ -29,82 +118,9 @@ public class Duke {
                     System.out.println("\tYou have no tasks in your list :)");
                 }
             } else {
-                Scanner scanner2 = new Scanner(userInput);
-                String cmd = scanner2.next();
-                if (cmd.equalsIgnoreCase("done")) {
-                    int index = scanner2.nextInt();
-                    try {
-                        Task task = list.get(index - 1);
-                        task.markAsDone();
-                        System.out.println("\tNice! I've marked this as done:");
-                        System.out.println("\t  " + task.toString());
-                    } catch (IndexOutOfBoundsException e) {
-                        System.out.println(errorPrefix + "Sorry, that is not a valid task.");
-                    }
-
-                }
-                else if (cmd.equalsIgnoreCase("delete")) {
-                    int index = scanner2.nextInt();
-                    try {
-                        System.out.println("\tNoted. I've removed this task:");
-                        System.out.println("\t" + list.remove(index - 1).toString());
-                        System.out.println("\tNow you have " + list.size() + " task(s) in the list.");
-                    } catch (IndexOutOfBoundsException e) {
-                        System.out.println(errorPrefix + "Sorry, that is not a valid task.");
-                    }
-                }
-                else if (cmd.equalsIgnoreCase("todo")) {
-                    if (!scanner2.hasNext()) {
-                        System.out.println(errorPrefix
-                                + "The description of a todo cannot be empty.");
-                    } else {
-                        System.out.println("\tGot it. I've added this task:");
-                        ToDo todo = new ToDo(scanner2.nextLine());
-                        list.add(todo);
-                        System.out.println("\t  " + todo.toString());
-                        System.out.println("\tNow you have " + list.size() + " task(s) in the list.");
-                    }
-                }
-                else if (cmd.equalsIgnoreCase("deadline")) {
-                    if (!scanner2.hasNext()) {
-                        System.out.println(errorPrefix
-                                + "The description of a deadline cannot be empty.");
-                    } else {
-                        System.out.println("\tGot it. I've added this task:");
-                        scanner2.useDelimiter("( /by )");
-                        String description = scanner2.next();
-                        scanner2.reset();
-                        scanner2.next();
-                        String by = scanner2.nextLine().trim();
-                        Deadline deadline = new Deadline(description, by);
-                        list.add(deadline);
-                        System.out.println("\t  " + deadline.toString());
-                        System.out.println("\tNow you have " + list.size() + " task(s) in the list.");
-                    }
-                }
-                else if (cmd.equalsIgnoreCase("event")) {
-                    if (!scanner2.hasNext()) {
-                        System.out.println(errorPrefix
-                                + "The description of an event cannot be empty.");
-                    } else {
-                        System.out.println("\tGot it. I've added this task:");
-                        scanner2.useDelimiter("( /at )");
-                        String description = scanner2.next();
-                        scanner2.reset();
-                        scanner2.next();
-                        String at = scanner2.nextLine().trim();
-                        Event event = new Event(description, at);
-                        list.add(event);
-                        System.out.println("\t  " + event.toString());
-                        System.out.println("\tNow you have " + list.size() + " task(s) in the list.");
-                    }
-                }
-                else {
-                    System.out.println(errorPrefix
-                            + "I'm sorry, but I don't know what that means :-(");
-                }
+                updateList(userInput);
             }
-            System.out.println(horizontalLine);
+            System.out.println(HORIZONTAL_LINE);
         }
     }
 }
