6001e2eb859ce70a5b8ffad77d18410d28c9a9e5
damithc <>
2020-08-25 17:23:26 +0800

    Improve runtest.* scripts
    * Improve the javac command to work for multiple java files.
    * In runtest.bat, delete ACTUAL.TXT only if it exists.

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -4,10 +4,10 @@ REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
-del ACTUAL.TXT
+if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\Duke.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
