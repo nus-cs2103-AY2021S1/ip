8f795b966695de7bee984c03e95e4d83fe7b8670
CodyChew <chewchengyap@gmail.com>
2020-08-25 18:02:15 +0800

    Update deadline and event command to read Date and Time
    Represent DataTime as an object rather than a string for manipulation.
    
    Let's store a LocalDateTime obejct for the Deadline and Event task.
    
    Able to format its output easily.
    
    Reference to validation for correct dates and time format
    https://stackoverflow.com/questions/2149680/regex-date-format-validation-on-java
    https://www.geeksforgeeks.org/how-to-validate-time-in-24-hour-format-using-regular-expression/

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,17 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm")) + ")";
     }
 }
