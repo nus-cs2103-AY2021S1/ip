3166f540e47b1dec3b780cd2ff35b434540ca723
Jing Yen Loh <lohjingyen@gmail.com>
2020-08-26 11:53:50 +0800

    Extract out parsing logic for Todo and Event
    into their own parsing methods, for consistency and future.

--- src/main/java/TaskParser.java
+++ src/main/java/TaskParser.java
@@ -11,24 +11,32 @@ public class TaskParser {
 
         switch (taskType) {
         case "TODO": {
-            return new TodoTask(isDone, taskName);
+            return TaskParser.parseTodo(isDone, taskName);
         }
         case "DEADLINE": {
             String deadlineStr = tokens[3];
             return TaskParser.parseDeadline(isDone, taskName, deadlineStr);
         }
         case "EVENT": {
-            String timeRange = tokens[3];
-            return new EventTask(taskName, timeRange);
+            String timeRangeStr = tokens[3];
+            return TaskParser.parseEvent(isDone, taskName, timeRangeStr);
         }
         default:
             throw new InvalidInputException("Unable to parse task");
         }
     }
+
+    public static EventTask parseEvent(boolean isDone, String taskName, String timeRangeStr) {
+        return new EventTask(isDone, taskName, timeRangeStr);
+    }
+
     public static DeadlineTask parseDeadline(boolean isDone, String taskName, String deadlineStr) {
         // TODO: 26/8/20 Handle DateTimeParseException
         LocalDate deadline = LocalDate.parse(deadlineStr);
         return new DeadlineTask(isDone, taskName, deadline);
     }
 
+    public static TodoTask parseTodo(boolean isDone, String taskName) {
+        return new TodoTask(isDone, taskName);
+    }
 }
