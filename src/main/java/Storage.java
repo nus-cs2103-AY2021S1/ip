6b25dfc6dbf3e1301dc44ee5d349d7df9bf5e394
Xue Yong <xueyong26@hotmail.com>
2020-08-26 23:16:19 +0800

    Fix bug from merge with branch-Level-8
    Modify Storage to fix invalid type bug

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -3,11 +3,14 @@ import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import java.time.DateTimeException;
 import java.util.function.Function;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import java.lang.StringBuilder;
+
+import java.time.LocalDate;
 
 public class Storage {
     private String filePath;
@@ -44,17 +47,21 @@ public class Storage {
     private ArrayList<Task> lineConverter(ArrayList<String> lineList) throws CorruptedFileException {
         ArrayList<Task> taskList = new ArrayList<>();
         for (String s : lineList)  {
-            String[] temp = s.split(" \\| ");
-            if (!temp[1].equals("1") && !temp[1].equals("0")) {
-                throw new CorruptedFileException("Sorry, the input file is corrupted.");
-            }
-            if (temp[0].equals("T")) {
-                taskList.add(new Todo(temp[2], func.apply(temp[1])));
-            } else if (temp[0].equals("D")) {
-                taskList.add(new Deadline(temp[2], func.apply(temp[1]), temp[3]));
-            } else if (temp[0].equals("E")) {
-                taskList.add(new Event(temp[2], func.apply(temp[1]), temp[3]));
-            } else {
+            try {
+                String[] temp = s.split(" \\| ");
+                if (!temp[1].equals("1") && !temp[1].equals("0")) {
+                    throw new CorruptedFileException("Sorry, the input file is corrupted.");
+                }
+                if (temp[0].equals("T")) {
+                    taskList.add(new Todo(temp[2], func.apply(temp[1])));
+                } else if (temp[0].equals("D")) {
+                    taskList.add(new Deadline(temp[2], func.apply(temp[1]), LocalDate.parse(temp[3])));
+                } else if (temp[0].equals("E")) {
+                    taskList.add(new Event(temp[2], func.apply(temp[1]), LocalDate.parse(temp[3])));
+                } else {
+                    throw new CorruptedFileException("Sorry, the input file is corrupted.");
+                }
+            } catch (DateTimeException e) {
                 throw new CorruptedFileException("Sorry, the input file is corrupted.");
             }
         }
