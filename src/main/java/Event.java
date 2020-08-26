9a3b04d33e798cbbeea23187b9ae8504737afe13
joshtyf <tan.yin.feng@hotmail.com>
2020-08-26 16:12:39 +0800

    Document Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,10 +3,21 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * <code>Event</code> inherits from <code>Task</code>
+ * and is used to represent an event.
+ */
 class Event extends Task {
     private Date start;
     private Date end;
 
+    /**
+     * Constructor to create an event object.
+     * @param name the name of the event
+     * @param start the start time of the event in the format "dd MM yyyy HH:mm"
+     * @param end the end time of the event in the format "dd MM yyyy HH:mm"
+     * @throws DukeException if the format of the given dates are wrong
+     */
     Event(String name, String start, String end) throws DukeException {
         super(name);
         SimpleDateFormat formatter = new SimpleDateFormat("dd MM yyyy HH:mm");
@@ -18,6 +29,11 @@ class Event extends Task {
         }
     }
 
+    /**
+     * Returns a comma seperated <code>String</code> containing the relevant information
+     * about this event. This information will then be saved in a text file.
+     * @return the comma seperated text of this event
+     */
     @Override
     public String saveText() {
         String completeStatus = super.isCompleted() ? "1" : "0";
@@ -25,6 +41,10 @@ class Event extends Task {
 ;        return "E," + completeStatus + "," + super.getName() + "," + dateFormat.format(start) + "," + dateFormat.format(end);
     }
 
+    /**
+     * <code>String</code> represenation of this event object.
+     * @return the string representation this object
+     */
     @Override
     public String toString() {
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
