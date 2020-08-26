67fa366113e894d5ab95d2ea355c628b638d2373
NiMadeNai <e0424663@u.nus.edu>
2020-08-26 23:52:51 +0800

    A-JavaDocs

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,6 +5,11 @@ import duke.Storage.Storage;
 import duke.TaskList.TaskList;
 import duke.Ui.Ui;
 
+/**
+ * Duke is the name of this program. It is an CLI app that reads and save
+ * the user Tasks. The user can use it to keep tabs on their tasks and
+ * and can mark them as done when they deem fit.
+ */
 public class Duke {
 
     private final static String directory =  System.getProperty("user.dir");
@@ -19,7 +24,9 @@ public class Duke {
         this.tasks = new TaskList();
 
     }
-
+    /**
+     * Run the programme, using a Duke Object
+     */
     public void run(){
         this.ui.startMessage();
         storage.loadTasks(this.tasks);
