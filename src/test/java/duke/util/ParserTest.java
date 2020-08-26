f8ab60ade3529da47fd0a809ef4b9ff2790225e9
Wong-ZZ <wongzhengzhi@gmail.com>
2020-08-27 00:51:27 +0800

    Update text-ui-test and JUnit tests

--- src/test/java/duke/util/ParserTest.java
+++ src/test/java/duke/util/ParserTest.java
@@ -10,32 +10,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ParserTest {
     @Test
     public void testParseCmdWord() {
-        CommandType exit = Parser.parseCmdWord("bye");
-        assertEquals(CommandType.Exit, exit);
-
-        CommandType list = Parser.parseCmdWord("list");
-        assertEquals(CommandType.List, list);
-
-        CommandType done = Parser.parseCmdWord("done 3");
-        assertEquals(CommandType.Done, done);
+        CommandType deadline = Parser.parseCmdWord("deadline lorem ipsum");
+        assertEquals(CommandType.Task, deadline);
 
         CommandType delete = Parser.parseCmdWord("delete 1");
         assertEquals(CommandType.Delete, delete);
 
+        CommandType done = Parser.parseCmdWord("done 3");
+        assertEquals(CommandType.Done, done);
+
         CommandType due = Parser.parseCmdWord("due 2020-08-09");
         assertEquals(CommandType.Due, due);
 
-        CommandType deadline = Parser.parseCmdWord("deadline lorem ipsum");
-        assertEquals(CommandType.Task, deadline);
+        CommandType exit = Parser.parseCmdWord("bye");
+        assertEquals(CommandType.Exit, exit);
 
         CommandType event = Parser.parseCmdWord("event lorem ipsum");
         assertEquals(CommandType.Task, event);
 
-        CommandType todo = Parser.parseCmdWord("todo lorem ipsum");
-        assertEquals(CommandType.Task, todo);
+        CommandType find = Parser.parseCmdWord("find");
+        assertEquals(CommandType.Find, find);
 
         CommandType invalid = Parser.parseCmdWord("something");
         assertEquals(CommandType.Invalid, invalid);
+
+        CommandType list = Parser.parseCmdWord("list");
+        assertEquals(CommandType.List, list);
+
+        CommandType todo = Parser.parseCmdWord("todo lorem ipsum");
+        assertEquals(CommandType.Task, todo);
     }
 
     @Test
