5c9ff27f26e6fa3da250dba139003740e10417d1
ethan-l-m-e <cs53hv36sw@privaterelay.appleid.com>
2020-08-27 00:28:36 +0800

    Add data for duke to .gitignore

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -5,6 +5,7 @@ if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 del ACTUAL.TXT
+del data
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\Duke.java
