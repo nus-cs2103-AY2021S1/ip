50b9a4784e6817ea9972904e2aa91edc72185cbc
rgabelarde <rgabelarde@gmail.com>
2020-08-26 16:48:44 +0800

    Remove wildcard for library imports, specify libraries

--- src/Storage.java
+++ src/Storage.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
