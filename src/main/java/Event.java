c89129af9bde67f67adac7ec87939971a07a07f3
jetnew <notesjet@gmail.com>
2020-08-26 21:52:08 +0800

    Remove google-java-format

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,38 +2,38 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
-  LocalDate taskAt;
+    LocalDate taskAt;
 
-  Event(String taskName, LocalDate taskAt) {
-    super(taskName);
-    this.taskAt = taskAt;
-  }
-
-  @Override
-  public String toString() {
-    return "[E]"
-        + super.toString()
-        + " (at: "
-        + this.taskAt.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))
-        + ")";
-  }
-
-  @Override
-  public String toFileFormat() {
-    return "E|" + (this.isDone ? "1" : "0") + "|" + this.taskName + "|" + this.taskAt;
-  }
-
-  /**
-   * Loads the file format String representation of Event task.
-   *
-   * @return Event loaded from file format representation
-   */
-  static Event fromFileFormat(String fileFormatString) {
-    String[] tokens = fileFormatString.split("\\|");
-    Event loaded = new Event(tokens[2], LocalDate.parse(tokens[3]));
-    if (tokens[1].equals("1")) {
-      loaded.setDone();
+    Event(String taskName, LocalDate taskAt) {
+        super(taskName);
+        this.taskAt = taskAt;
     }
-    return loaded;
-  }
+
+    @Override
+    public String toString() {
+        return "[E]"
+                + super.toString()
+                + " (at: "
+                + this.taskAt.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))
+                + ")";
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "E|" + (this.isDone ? "1" : "0") + "|" + this.taskName + "|" + this.taskAt;
+    }
+
+    /**
+     * Loads the file format String representation of Event task.
+     *
+     * @return Event loaded from file format representation
+     */
+    static Event fromFileFormat(String fileFormatString) {
+        String[] tokens = fileFormatString.split("\\|");
+        Event loaded = new Event(tokens[2], LocalDate.parse(tokens[3]));
+        if (tokens[1].equals("1")) {
+            loaded.setDone();
+        }
+        return loaded;
+    }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,56 +1,61 @@
 import java.util.ArrayList;
 
 public class TaskList {
-  ArrayList<Task> tasks;
-  /* Constructor for loading past task list from storage. */
-  public TaskList(ArrayList<Task> taskList) {
-    this.tasks = taskList;
-  }
+    ArrayList<Task> tasks;
 
-  /* Constructor for initialising new task list. */
-  public TaskList() {
-    this.tasks = new ArrayList<>();
-  }
-
-  /** List tasks to system output. */
-  public void listTasks() {
-    if (tasks.isEmpty()) {
-      System.out.println("You have no remaining tasks! Cheers!");
-    } else {
-      System.out.println("Here are the tasks in your list:");
-      for (int i = 0; i < tasks.size(); i++) {
-        System.out.println(i + 1 + "." + tasks.get(i));
-      }
+    /* Constructor for loading past task list from storage. */
+    public TaskList(ArrayList<Task> taskList) {
+        this.tasks = taskList;
     }
-  }
 
-  /** Add a task to list of tasks. */
-  public void addTask(Task task) {
-    this.tasks.add(task);
-    System.out.println("Got it. I've added this task:");
-    System.out.println("  " + tasks.get(tasks.size() - 1));
-    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-  }
+    /* Constructor for initialising new task list. */
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
 
-  /**
-   * Set the task with given index as done.
-   *
-   * @param id index of task to be marked as done
-   */
-  public void setDone(int id) {
-    this.tasks.get(id).setDone();
-    System.out.println("Nice! I've marked this task as done:");
-    System.out.println("  " + tasks.get(id));
-  }
+    /**
+     * List tasks to system output.
+     */
+    public void listTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("You have no remaining tasks! Cheers!");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(i + 1 + "." + tasks.get(i));
+            }
+        }
+    }
 
-  /**
-   * Delete the task with given index from the task list.
-   *
-   * @param id index of task to be deleted
-   */
-  public void deleteTask(int id) {
-    Task deletedTask = this.tasks.remove(id);
-    System.out.println("Noted. I have removed this task:");
-    System.out.println("  " + deletedTask);
-  }
+    /**
+     * Add a task to list of tasks.
+     */
+    public void addTask(Task task) {
+        this.tasks.add(task);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+    }
+
+    /**
+     * Set the task with given index as done.
+     *
+     * @param id index of task to be marked as done
+     */
+    public void setDone(int id) {
+        this.tasks.get(id).setDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + tasks.get(id));
+    }
+
+    /**
+     * Delete the task with given index from the task list.
+     *
+     * @param id index of task to be deleted
+     */
+    public void deleteTask(int id) {
+        Task deletedTask = this.tasks.remove(id);
+        System.out.println("Noted. I have removed this task:");
+        System.out.println("  " + deletedTask);
+    }
 }
