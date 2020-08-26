b6655bf18a8fe6fa25792cf59de3cd16fbb14d2b
Ma-Yueran <mayuerantom@outlook.com>
2020-08-26 15:33:49 +0800

    Add TimeFormat.java

---
+++ src/time/TimeFormat.java
@@ -0,0 +1,17 @@
+package time;
+
+import java.time.format.DateTimeFormatter;
+
+public class TimeFormat {
+    public static final DateTimeFormatter dateFormatter =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public static final DateTimeFormatter timeFormatter1 =
+            DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    public static final DateTimeFormatter timeFormatter2 =
+            DateTimeFormatter.ofPattern("HH:mm");
+
+    public static final DateTimeFormatter dateTimeFormatter =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+}
