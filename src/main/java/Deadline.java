4c5a02f6b6337292ad2024e07e7b5280572a7b1d
KohHanMing <kohhanming1997@gmail.com>
2020-08-26 16:00:48 +0800

    Add proper date and time formats.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,47 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class Deadline extends Task{
-    protected String dateAndTime;
+    protected LocalDate date;
+    protected Date time;
+
     Deadline(String description, String dateAndTime, TaskType taskType) {
         super(description, taskType);
-        this.dateAndTime = dateAndTime;
+        String[] dateAndTimeSplit = dateAndTime.split(" ");
+        String[] dateSplit = dateAndTimeSplit[0].split("/");
+        this.date = LocalDate.of(Integer.parseInt(dateSplit[2]),
+                Integer.parseInt(dateSplit[1]),
+                Integer.parseInt(dateSplit[0]));
+        String timeUnformatted = dateAndTimeSplit[1];
+        String timeFormatted = timeUnformatted.substring(0, 2)
+                + ":"
+                + timeUnformatted.substring(2, 4);
+        SimpleDateFormat _24HourFormat = new SimpleDateFormat("HH:mm");
+        try {
+            this.time = _24HourFormat.parse(timeFormatted);
+        } catch (ParseException e) {
+            System.out.println("Hmm that didn't work... Is your time in HHMM format?");
+        }
+    }
+
+    public String printDate() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
+        return dateTimeFormatter.format(date);
+    }
+
+    public String printTime() {
+        SimpleDateFormat _12HourFormat = new SimpleDateFormat("hh:mm a");
+        return _12HourFormat.format(time);
     }
 
     @Override
     public String toString() {
-        return "[Deadline]" + super.toString() + "(by: " + dateAndTime + ")";
+
+        return "[Deadline]"
+                + super.toString()
+                + "(by: " + this.printDate() + " " + this.printTime() + ")";
     }
 }
