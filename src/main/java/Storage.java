603e0a7a5f97a6188f10caf5a8093ef47239db1d
xinyue <xinyue98.zhang@gmail.com>
2020-08-27 00:15:19 +0800

    A-JavaDoc

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -8,22 +8,28 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Storage {
-    
+
     private final String filePath;
-    
+
     Storage(String filePath) {
         this.filePath = filePath;
     }
-    
+
+    /**
+     * Reads the tasks from the local storage file and creates a list of Tasks.
+     *
+     * @return a list of Tasks stored in the local storage file.
+     * @throws DukeException if the local storage file cannot be created or loaded.
+     */
     ArrayList<Task> load() throws DukeException {
         ArrayList<Task> taskList = new ArrayList<>();
-        
+
         try {
             new File(filePath).createNewFile();
         } catch (IOException e) {
             throw new DukeException("☹ OOPS!!! There was an error creating a save file.");
         }
-        
+
         try {
             Scanner fileReader = new Scanner(new File(filePath));
             while (fileReader.hasNextLine()) {
@@ -37,6 +43,13 @@ public class Storage {
         }
     }
 
+    /**
+     * Converts a string into a Task.
+     *
+     * @param s string description of a Task.
+     * @return a Task described by the input string.
+     * @throws DukeException if input string is in the wrong format.
+     */
     private Task getTask(String s) throws DukeException {
         try {
             Task currentTask;
@@ -63,7 +76,13 @@ public class Storage {
             throw new DukeException();
         }
     }
-    
+
+    /**
+     * Updates the local storage file and stores the list of Tasks.
+     *
+     * @param taskList list of Tasks to be stored.
+     * @throws DukeException if local storage file cannot be accessed.
+     */
     void updateTasks(TaskList taskList) throws DukeException {
         try {
             FileWriter fileWriter = new FileWriter(filePath);
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
 
 public class TaskList {
-    
+
     private final ArrayList<Task> myTaskList;
-    
+
     TaskList() {
         myTaskList = new ArrayList<>();
     }
-    
+
     TaskList(ArrayList<Task> taskList) {
         this.myTaskList = taskList;
     }
-    
+
     ArrayList<Task> getMyTaskList() {
         return myTaskList;
     }
-    
+
     int getListLength() {
         return myTaskList.size();
     }
@@ -23,7 +23,14 @@ public class TaskList {
     public void addNewTask(Task newTask) {
         myTaskList.add(newTask);
     }
-    
+
+    /**
+     * Marks a task as done. Throws an exception if the task index is invalid.
+     *
+     * @param taskIndex index of task to be marked as done.
+     * @return the done task.
+     * @throws DukeException if index is invalid.
+     */
     public Task doneTask(int taskIndex) throws DukeException {
         try {
             Task temp = myTaskList.get(taskIndex - 1);
@@ -33,7 +40,14 @@ public class TaskList {
             throw new InvalidTaskIndexException("done");
         }
     }
-    
+
+    /**
+     * Deletes a task from the list. Throws an exception if the task index is invalid.
+     *
+     * @param taskIndex index of task to be deleted.
+     * @return the deleted task.
+     * @throws DukeException if index is invalid.
+     */
     public Task deleteTask(int taskIndex) throws DukeException {
         try {
             Task temp = myTaskList.get(taskIndex - 1);
@@ -42,6 +56,23 @@ public class TaskList {
         } catch (IndexOutOfBoundsException e) {
             throw new InvalidTaskIndexException("delete");
         }
+    }
+
+    /**
+     * Finds all the tasks with description that contains the input.
+     *
+     * @param input the required keyword to be searched for.
+     * @return a string describing the list of tasks that contains the input in their description.
+     */
+
+    String findTask(String input) {
+        ArrayList<Task> tasks = new ArrayList<>();
+        for (Task t : myTaskList) {
+            if (t.description.contains(input)) {
+                tasks.add(t);
+            }
+        }
+        return new TaskList(tasks).toString();
     }
 
     @Override
