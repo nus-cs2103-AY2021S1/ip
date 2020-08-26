9fb94ca9bf0701d94b887f4801c033c5d4042712
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-08-26 09:04:40 +0800

    Remove unused method from TaskList

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -37,11 +37,4 @@ public class TaskList {
         }
         return output;
     }
-
-    public void displayTasks() {
-        System.out.println("Here are the tasks in your list: ");
-        for (int i = 0; i < taskList.size(); i++) {
-            System.out.println((i + 1) + ". " + taskList.get(i));
-        }
-    }
 }
