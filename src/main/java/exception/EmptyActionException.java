7d4f247c4576f6bd9834cead4bc6b31390ff762f
Jaylenlee <e0424677@u.nus.edu>
2020-08-26 10:24:26 +0800

    Add JavaDoc to exception package

--- src/main/java/exception/EmptyActionException.java
+++ src/main/java/exception/EmptyActionException.java
@@ -1,7 +1,19 @@
 package exception;
 
-public class EmptyActionException extends DukeException {
+/**
+ * Encapsulates the message of an exception or error related to Duke's operation
+ *  on empty command or action.
+ *
+ * <p>The 'EmptyActionException' supports operators, supported include: </p>
+ *
+ * <p> (i) Getters to error message </p>
+ */
+public class EmptyActionException extends exception.DukeException {
     private static final String DESCRIPTION = "The description of a command cannot be empty.";
+
+    /**
+     * Constructor of this object.
+     */
     public EmptyActionException() {
         super(DESCRIPTION);
     }
--- src/main/java/exception/InvalidCommandException.java
+++ src/main/java/exception/InvalidCommandException.java
@@ -1,7 +1,19 @@
 package exception;
 
+/**
+ * Encapsulates the message of an exception or error related to Duke's operation on
+ * invalid commands.
+ *
+ * <p>The 'InvalidCommandException' supports operators, supported include: </p>
+ *
+ * <p> (i) Getters to error message </p>
+ */
 public class InvalidCommandException extends DukeException {
     private static final String DESCRIPTION = "I'm sorry, but I don't know what that means :-(";
+
+    /**
+     * Constructor of this object.
+     */
     public InvalidCommandException() {
         super(DESCRIPTION);
     }
