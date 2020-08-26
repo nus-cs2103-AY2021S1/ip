962599e8f2050562a6b140837caf5454b5a61606
Raythx98 <65889977+raythx98@users.noreply.github.com>
2020-08-26 20:31:20 +0800

    Add JUnit test

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -1,11 +1,58 @@
 package duke;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
     @Test
-    public void dummyTest(){
-        assertEquals(2, 2);
+    public void parse_glibberish_success() {
+
+        new Parser().parse(new TaskList(), "asdfgh qwert");
+
+        String expectedResult = "duke.exception.UnknownCommandException: Don't understand...";
+        String actualResult = outputStreamCaptor.toString().trim();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void parse_byeEntry_success() {
+
+        boolean actualResult =  new Parser().parse(new TaskList(), "bye");
+
+        assertTrue(actualResult);
+    }
+
+    @Test
+    public void parse_showEmptyList_success() {
+
+        new Parser().parse(new TaskList(), "list");
+
+        String expectedResult = "Here yur tasks faggit: \n" +
+                "empty";
+        String actualResult = outputStreamCaptor.toString().trim();
+
+        assertEquals(expectedResult, actualResult);
     }
 }
--- src/test/java/duke/TaskListTest.java
+++ src/test/java/duke/TaskListTest.java
@@ -1,4 +1,66 @@
 package duke;
 
+import duke.task.Task;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TaskListTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    TaskList tasks;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        tasks = new TaskList();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void addTask_emptyList_success() {
+
+        String actualResult = new TaskList().addTask(Task.TaskType.TODOS, "CS2103T Exams", "2020-12-31 2021");
+
+        String expectedResult = "Got it, here yur task bij\n" +
+                "[T][✘] CS2103T Exams (by: 2020-12-31 2021)\n" +
+                "Now you have 1 tasks in the list.";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void deleteTask_singleList_success() {
+
+        TaskList tasks = new TaskList();
+        tasks.addTask(Task.TaskType.TODOS, "CS2103T Exams", "2020-12-31 2021");
+        String actualResult = tasks.removeTask(0);
+
+        String expectedResult = "[T][✘] CS2103T Exams (by: 2020-12-31 2021)";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void getSize_emptyList_success() {
+
+        TaskList tasks = new TaskList();
+//        tasks.addTask(Task.TaskType.TODOS, "CS2103T Exams", "2020-12-31 2021");
+        int actualResult = tasks.getSize();
+        int expectedResult = 0;
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
