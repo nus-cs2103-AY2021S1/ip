04973b0dfb8b7cca491b586ded5a04f857664af5
Jeanne Toh <jeannetoh99@gmail.com>
2020-08-26 23:50:53 +0800

    Add Ui.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,31 +2,20 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Duke {
-    private static Storage storage;
-    private static TaskList taskList;
-    private static final String starline = "**************************************************************************";
+    private Storage storage;
+    private TaskList taskList;
     private static final String dashline = "--------------------------------------------------------------------------";
-    private static final String logo =
-              " ____        _   \n"
-            + "|  _ \\ _   _| | _____\n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|";
-
-    public static void greet() {
-        System.out.println(starline +
-                "\nWelcome! I am\n" + logo +
-                "\nHere are some magic words to get you going:" +
-                "\nTo add a todo, say 'todo <task description>'." +
-                "\nTo add a deadline, say 'deadline <task description> /by <yyyy-mm-dd>'." +
-                "\nTo add an event, say 'event <event description> /at <event location>'" +
-                "\nTo view your tasks, say 'list'." +
-                "\nTo check off a task, say 'done <task number>'." +
-                "\nTo leave, say 'bye'.\n" +
-                starline);
+    
+    public Duke(String filePath) {
+        try {
+            storage = new Storage(filePath);
+            taskList = new TaskList(storage.load());
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: file not created. " + e);
+        }
     }
 
-    public static void awaitInputCommand() {
+    public void awaitInputCommand() {
         Scanner sc = new Scanner(System.in);
         String next = sc.nextLine();
 
@@ -36,19 +25,19 @@ public class Duke {
 
             // "list" prints the task list
             if (next.equals("list")) {
-                taskList.list();
+                this.taskList.list();
 
             // "done" checks off boxes, need to check for input errors
             } else if (splitNext[0].equals("done")) {
                 try {
-                    taskList.markTaskAsDone(splitNext[1]);
+                    this.taskList.markTaskAsDone(splitNext[1]);
                 } catch (IndexOutOfBoundsException ex) {
                     System.out.println(dashline + "\n\u2639 Please indicate which task you'd like to check off!");
                 }
             // to "delete" tasks from the taskList
             } else if (splitNext[0].equals("delete")) {
                 try {
-                    taskList.deleteTask(splitNext[1]);
+                    this.taskList.deleteTask(splitNext[1]);
                 } catch (IndexOutOfBoundsException ex) {
                     System.out.println(dashline + "\n\u2639 Please indicate which task you'd like to delete!");
                 }
@@ -56,7 +45,7 @@ public class Duke {
             // for ToDos, Deadlines, Events
             } else if (splitNext[0].equals("todo") || splitNext[0].equals("deadline") || splitNext[0].equals("event")){
                 try {
-                    taskList.add(next, true);
+                    this.taskList.add(next, true);
                 } catch (IllegalArgumentException ex) {
                     System.out.println(dashline + "\n\u2639 " + ex.getMessage() + "\n" + dashline);
                 }
@@ -67,37 +56,16 @@ public class Duke {
         }
         sc.close();
     }
-
-    public static void exit() {
-        System.out.println(
-                        " _____  ___  ___  _____\n" +
-                        "|  __ \\ \\  \\/  / |  ___|\n" +
-                        "| | / /  \\    /  |  |__\n" +
-                        "| | \\ \\   |  |   |  ___|\n" +
-                        "| |_/ /   |  |   |  |__\n" +
-                        "|____/    |__|   |_____|\n"
-        );
-        System.out.println("Hope you have a productive day ahead! :))");
-    }
     
-    public static void initialise() {
-        try {
-            storage = new Storage("./Data/saved-tasks.txt");
-            taskList = new TaskList(storage.load());
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: file not created. " + e);
-        }
-    }
-    
-    public static void save() {
-        storage.save(taskList);
+    public void save() {
+        this.storage.save(this.taskList);
     }
 
     public static void main(String[] args) {
-        initialise();
-        greet();
-        awaitInputCommand();
-        save();
-        exit();
+        Duke duke = new Duke("./Data/saved-tasks.txt");
+        Ui.greet();
+        duke.awaitInputCommand();
+        duke.save();
+        Ui.exit();
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,34 @@
+public class Ui {
+    private static final String starline = "**************************************************************************";
+    private static final String logo =
+            " ____        _   \n"
+                    + "|  _ \\ _   _| | _____\n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|";
+
+    public static void greet() {
+        System.out.println(starline +
+                "\nWelcome! I am\n" + logo +
+                "\nHere are some magic words to get you going:" +
+                "\nTo add a todo, say 'todo <task description>'." +
+                "\nTo add a deadline, say 'deadline <task description> /by <yyyy-mm-dd>'." +
+                "\nTo add an event, say 'event <event description> /at <event location>'" +
+                "\nTo view your tasks, say 'list'." +
+                "\nTo check off a task, say 'done <task number>'." +
+                "\nTo leave, say 'bye'.\n" +
+                starline);
+    }
+
+    public static void exit() {
+        System.out.println(
+                " _____  ___  ___  _____\n" +
+                        "|  __ \\ \\  \\/  / |  ___|\n" +
+                        "| | / /  \\    /  |  |__\n" +
+                        "| | \\ \\   |  |   |  ___|\n" +
+                        "| |_/ /   |  |   |  |__\n" +
+                        "|____/    |__|   |_____|\n"
+        );
+        System.out.println("Hope you have a productive day ahead! :))");
+    }
+}
