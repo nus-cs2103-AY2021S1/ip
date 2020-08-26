3287d6773d37286552aac028428ed40343d84404
Licheng-Wu <wlc.licheng@gmail.com>
2020-08-26 11:50:45 +0800

    Package the App as a JAR file

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -1,6 +1,14 @@
 package duke;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import java.util.Scanner;
 
 public class Storage {
