edee0cb5bbd388ab39183bee18bb29e3cfc2874a
Trav1sT <travtoh@gmail.com>
2020-08-26 17:23:12 +0800

    Level 8 - Add LocalDateTime to Event and Deadline

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String timePeriod;
+    private LocalDateTime timePeriod;
 
     public Event(String name, String timePeriod) {
         super(name);
-        this.timePeriod = timePeriod;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.timePeriod = LocalDateTime.parse(timePeriod, formatter);
     }
 
     @Override
     public String toString() {
         String symbol = this.done ? "✓" : "✘";
-        return String.format("[E]%s (at: %s)", super.toString(), this.timePeriod);
+        return String.format("[E]%s (at: %s)", super.toString(),
+                timePeriod.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a")));
     }
 }
