81af4978ea444c4acbd639ec691f7b21b16f8b7f
Joe <jteddysanjaya@gmail.com>
2020-08-26 22:30:11 +0800

    Add CRUD method to UserData

--- src/main/java/UserData.java
+++ src/main/java/UserData.java
@@ -4,34 +4,79 @@ import java.util.List;
 
 public class UserData {
 
-    List<Task> tasks;
+    List<Task> tasks = new ArrayList<>();
     File dataFolder = new File("./data");
     File data = new File("./data/duke.txt");
 
-    public UserData() throws IOException {
+    public UserData() {
         if (!dataFolder.exists()) dataFolder.mkdir();
-        boolean isNew = data.createNewFile();
-        if (!isNew)
-            read();
-        else
-            tasks = new ArrayList<>();
+        boolean isNew = false;
+        try {
+            isNew = data.createNewFile();
+            if (!isNew) read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // CREATE
     public void create(Task task) {
-
+        try {
+            FileWriter fw = new FileWriter(data, true);
+            System.out.println(task.toData());
+            fw.write(task.toData() + "\n");
+            fw.close();
+            tasks.add(task);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // READ
-    public void read() throws IOException {
+    private void read() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(data));
         String st;
         while ((st = br.readLine()) != null) {
             Task task = Task.fromData(st);
-            System.out.println(task.toString());
+            tasks.add(task);
         }
     }
 
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public int taskSize() {
+        return tasks.size();
+    }
+
     // UPDATE
+    public Task markAsDone(int taskId) {
+        Task task = tasks.get(taskId).markAsDone();
+        tasks.set(taskId, task);
+        update();
+        return task;
+    }
+
+    private void update() {
+        try {
+            FileWriter fw = new FileWriter(data);
+            for (Task task : tasks) {
+                fw.write(task.toData() + "\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // DELETE
+    public Task delete(int taskId) {
+        Task task = tasks.get(taskId);
+        tasks.remove(taskId);
+        update();
+        return task;
+    }
+
 
 }
