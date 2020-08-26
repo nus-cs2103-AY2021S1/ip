719b1a602cf629d2c8e3a6fcc08cbca05708c894
vanGoghhh <blackznigelng@gmail.com>
2020-08-26 13:55:32 +0800

    Add javadocs

--- src/main/java/FindCommand.java
+++ src/main/java/FindCommand.java
@@ -1,13 +1,28 @@
 import java.util.ArrayList;
 
+/**
+ * Class to handle a find command entered by user.
+ * @author vanGoghhh
+ */
+
 public class FindCommand extends Command {
 
     private String command;
 
+    /**
+     * Constructor for findcommand object.
+     * @param command Complete line of user command.
+     */
     public FindCommand(String command) {
         this.command = command;
     }
 
+    /**
+     * Method to execute when a find command is entered.
+     * @param tasks TaskList containing all the tasks
+     * @param dukeUI UI to print string responses by the bot.
+     * @throws DukeException
+     */
     @Override
     void execute(TaskList tasks, UI dukeUI) throws DukeException {
         try {
@@ -20,6 +35,10 @@ public class FindCommand extends Command {
         }
     }
 
+    /**
+     * Method to tell bot to end current session or not.
+     * @return false to not end session.
+     */
     @Override
     boolean isExit() {
         return false;
