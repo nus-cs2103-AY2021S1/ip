e79282dcde777f0d08cdea468677a24985a439d4
w-yeehong <e0418195@comp.nus.edu.sg>
2020-08-26 21:35:25 +0800

    Add JUnit tests

---
+++ src/test/java/rogue/logic/directives/ActionTest.java
@@ -0,0 +1,26 @@
+package rogue.logic.directives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ActionTest {
+    @Test
+    public void getAction_validAction_success() {
+        assertEquals(Action.getAction("event"), Action.ADD_EVENT);
+        assertEquals(Action.getAction("todo"), Action.ADD_TODO);
+        assertEquals(Action.getAction("deadline"), Action.ADD_DEADLINE);
+        assertEquals(Action.getAction("done"), Action.MARK_AS_DONE);
+        assertEquals(Action.getAction("list"), Action.LIST);
+        assertEquals(Action.getAction("delete"), Action.DELETE);
+        assertEquals(Action.getAction("done"), Action.MARK_AS_DONE);
+        assertEquals(Action.getAction("bye"), Action.EXIT);
+    }
+
+    @Test
+    public void getAction_invalidAction_returnInvalid() {
+        assertEquals(Action.getAction(""), Action.INVALID);
+        assertEquals(Action.getAction("test"), Action.INVALID);
+        assertEquals(Action.getAction("123"), Action.INVALID);
+    }
+}
---
+++ src/test/java/rogue/logic/parser/ParserTest.java
@@ -0,0 +1,24 @@
+package rogue.logic.parser;
+
+import org.junit.jupiter.api.Test;
+
+import rogue.commons.exceptions.IncorrectArgumentException;
+import rogue.logic.parser.exceptions.UnknownCommandException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParserTest {
+    @Test
+    public void createExe_emptyInput_exceptionThrown() {
+        assertThrows(IncorrectArgumentException.class, () -> {
+            Parser.createExe("");
+        });
+    }
+
+    @Test
+    public void createExe_invalidInput_exceptionThrown() {
+        assertThrows(UnknownCommandException.class, () -> {
+            Parser.createExe("create todo borrow book");
+        });
+    }
+}
