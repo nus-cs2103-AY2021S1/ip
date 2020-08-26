ba97e29213f76e9ebf7f8cec193f7c14d06bf73a
joshtyf <tan.yin.feng@hotmail.com>
2020-08-26 16:12:21 +0800

    Document ErrorCommand.java

--- src/main/java/ErrorCommand.java
+++ src/main/java/ErrorCommand.java
@@ -1,5 +1,14 @@
+/**
+ * <code>ErrorCommand</code> inherits from the base class <code>Command</code>
+ * and will handle the situation when the user gives an invalid command.
+ */
 public class ErrorCommand extends Command {
+    /**
+     * Prints out "Oops, you gave an invalid command.
+     * @return <code>true</code>
+     */
     public boolean execute() {
+        // TODO: refactor this to the ui class
         System.out.println("Oops, you gave an invalid command");
         return true;
     }
