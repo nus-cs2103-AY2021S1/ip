c9ea62ce538303ec50a6c0ee61cfd01803871c97
AudreyFelicio <audreyanwar@gmail.com>
2020-08-26 16:03:25 +0800

    Fix missing import statement

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -6,6 +6,7 @@ import duke.command.DeadlineCommand;
 import duke.command.DeleteCommand;
 import duke.command.DoneCommand;
 import duke.command.EventCommand;
+import duke.command.FindCommand;
 import duke.command.ListCommand;
 import duke.command.ToDoCommand;
 import duke.exception.DukeException;
