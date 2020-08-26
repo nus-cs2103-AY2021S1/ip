732f4708c7046e515a6eefc866bfed075aeed524
Petrick Jerico <petrickjerico@gmail.com>
2020-08-27 05:26:11 +0800

    A-Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,52 +1,77 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.*;
 
 public class Duke {
 
+    public enum TaskType {
+        TODO("todo") {
+            @Override
+            public void addToTasks(List<Task> tasks, String taskName, String taskDate) throws DukeException {
+                if (taskName.isBlank()) {
+                    throw new NullTaskNameException("todo");
+                }
+                tasks.add(new ToDo(taskName));
+            }
+        },
+        DEADLINE("deadline") {
+            @Override
+            public void addToTasks(List<Task> tasks, String taskName, String taskDate) throws DukeException {
+                if (taskName.isBlank()) {
+                    throw new NullTaskNameException("deadline");
+                }
+                if (taskDate.isBlank()) {
+                    throw new NullTaskDateException("deadline");
+                }
+                tasks.add(new Deadline(taskName, taskDate));
+            }
+        },
+        EVENT("event") {
+            @Override
+            public void addToTasks(List<Task> tasks, String taskName, String taskDate) throws DukeException {
+                if (taskName.isBlank()) {
+                    throw new NullTaskNameException("event");
+                }
+                if (taskDate.isBlank()) {
+                    throw new NullTaskDateException("event");
+                }
+                tasks.add(new Event(taskName, taskDate));
+            }
+        };
+
+        private static final Map<String, TaskType> nameToValueMap = new HashMap<String, TaskType>();
+        private final String lowerCase;
+
+        TaskType(String lowerCase) {
+            this.lowerCase = lowerCase;
+        }
+
+        public String getLowerCase() {
+            return lowerCase;
+        }
+
+        static {
+            for (TaskType type : EnumSet.allOf(TaskType.class)) {
+                nameToValueMap.put(type.getLowerCase(), type);
+            }
+        }
+
+        public static boolean isMember(String name) {
+            return nameToValueMap.containsKey(name);
+        }
+
+        public static TaskType getTaskType(String lowerCase) {
+            return nameToValueMap.get(lowerCase);
+        }
+
+        public abstract void addToTasks(List<Task> tasks, String taskName, String taskDate) throws DukeException;
+    }
+
     public static void displayStarLine() {
         System.out.println("––––––––––––––––––––– *** –––––––––––––––––––––");
     }
-
     public static void displayMessage(String message) {
         displayStarLine();
         System.out.println(message);
-        displayStarLine();
-        System.out.println();
-    }
-
-    public static void welcome() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-
-        String welcome = "Duke at your service!\n" +
-                // logo + "\n" +
-                // commented out due to runtest failures, for some reason.
-                "How can I help you?\n" +
-                "Type in your orders below.\n\n" +
-                "(command list: 'list', 'deadline', 'event', 'todo', 'done', 'bye')";
-
-        displayMessage(welcome);
-    }
-
-    public static void bye() {
-        displayMessage("Alright, see you soon!");
-    }
-
-    public static void list(List<Task> tasks, int pending) {
-        int currId = 1;
-
-        displayStarLine();
-        System.out.println("TO-DO LIST:");
-        System.out.println(String.format("%d pending", pending));
-        for (Task t : tasks) {
-            System.out.println(String.format("   %d. %s", currId, t));
-            currId++;
-        }
         displayStarLine();
     }
 
@@ -71,6 +96,38 @@ public class Duke {
         return id - 1;
     }
 
+    public static void welcome() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String welcome = "Duke at your service!\n" +
+                // logo + "\n" +
+                // commented out due to runtest failures, for some reason.
+                "How can I help you?\n" +
+                "Type in your orders below.\n\n" +
+                "(command list: 'list', 'deadline', 'event', 'todo', 'done', 'bye')";
+
+        displayMessage(welcome);
+        System.out.println();
+    }
+    public static void bye() {
+        displayMessage("Alright, see you soon!");
+    }
+    public static void list(List<Task> tasks, int pending) {
+        int currId = 1;
+
+        displayStarLine();
+        System.out.println("TO-DO LIST:");
+        System.out.println(String.format("%d pending", pending));
+        for (Task t : tasks) {
+            System.out.println(String.format("   %d. %s", currId, t));
+            currId++;
+        }
+        displayStarLine();
+    }
     public static void done(List<Task> tasks, String commandLine) throws DukeException {
         int target = getNumIfValid(tasks, commandLine);
         Task targetTask = tasks.get(target);
@@ -84,7 +141,6 @@ public class Duke {
         System.out.println(tasks.get(target));
         displayStarLine();
     }
-
     public static boolean delete(List<Task> tasks, String commandLine) throws DukeException {
         int target = getNumIfValid(tasks, commandLine);
         Task targetTask = tasks.get(target);
@@ -96,6 +152,29 @@ public class Duke {
         displayStarLine();
 
         return !targetTask.isDone;
+    }
+    public static void add (List<Task> tasks, String commandLine) throws DukeException {
+        String[] inputs = commandLine.split("/");
+        String[] taskDetails = inputs[0].split(" ", 2);
+        String taskType = taskDetails[0];
+
+        if (!TaskType.isMember(taskType)) {
+            throw new DukeException(taskType);
+        } else {
+            TaskType task = TaskType.getTaskType(taskType);
+            String taskName = taskDetails.length == 1 ? "" : taskDetails[1].trim();
+            String taskDate = inputs.length == 1 ? "" : inputs[1].split(" ", 2)[1];
+            task.addToTasks(tasks, taskName, taskDate);
+
+            int size = tasks.size();
+
+            displayStarLine();
+            System.out.println("'" + taskName + "' added to list!");
+            System.out.println(tasks.get(size - 1));
+            System.out.println("\nYou now have " + size + " task(s) in your list.\n");
+            System.out.println("(Use 'list' command to see your updated list.)");
+            displayStarLine();
+        }
     }
 
     public static void main(String[] args) {
@@ -121,7 +200,7 @@ public class Duke {
                     done(tasks, commandLine);
                     pending--;
                 } catch (DukeException e) {
-                    System.out.println(e);
+                    displayMessage(e.toString());
                 }
             } else if (commandLine.startsWith("delete")) {
                 try {
@@ -130,46 +209,14 @@ public class Duke {
                         pending--;
                     }
                 } catch (DukeException e) {
-                    System.out.println(e);
+                    displayMessage(e.toString());
                 }
             } else {
                 try {
-                    String[] inputs = commandLine.split("/");
-                    String[] taskDetails = inputs[0].split(" ", 2);
-                    String taskType = taskDetails[0];
-                    String taskName = taskDetails.length == 1 ? "" : taskDetails[1].trim();
-                    String taskDate = inputs.length == 1 ? "" : inputs[1].split(" ", 2)[1];
-
-                    switch (taskType) {
-                        case "deadline":
-                            if (taskName.isBlank()) throw new NullTaskNameException("deadline");
-                            if (taskDate.isBlank()) throw new NullTaskDateException("deadline");
-                            tasks.add(new Deadline(taskName, taskDate));
-                            break;
-                        case "event":
-                            if (taskName.isBlank()) throw new NullTaskNameException("event");
-                            if (taskDate.isBlank()) throw new NullTaskDateException("event");
-                            tasks.add(new Event(taskName, taskDate));
-                            break;
-                        case "todo":
-                            if (taskName.isBlank()) throw new NullTaskNameException("todo");
-                            tasks.add(new ToDo(taskName));
-                            break;
-                        default:
-                            throw new DukeException(taskType);
-                    }
-
-                    int size = tasks.size();
+                    add(tasks, commandLine);
                     pending++;
-
-                    displayStarLine();
-                    System.out.println("'" + taskName + "' added to list!");
-                    System.out.println(tasks.get(size - 1));
-                    System.out.println("\nYou now have " + size + " task(s) in your list.\n");
-                    System.out.println("(Use 'list' command to see your updated list.)");
-                    displayStarLine();
                 } catch (DukeException e) {
-                    System.out.println(e);
+                    displayMessage(e.toString());
                 }
             }
             System.out.println();
