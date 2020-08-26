af2fd033a604265aee54e416e2aa51f8e244143f
Wong-ZZ <wongzhengzhi@gmail.com>
2020-08-26 18:22:28 +0800

    Update Ui fields to be final

--- src/main/java/duke/util/Ui.java
+++ src/main/java/duke/util/Ui.java
@@ -1,8 +1,8 @@
 package duke.util;
 
 public class Ui {
-    private static String DIVIDER = "____________________________________________________________";
-    private static String ERROR_HEADER = "___________________________ERROR!___________________________";
+    private final static String DIVIDER = "____________________________________________________________";
+    private final static String ERROR_HEADER = "___________________________ERROR!___________________________";
 
     public void print(String msg) {
         System.out.print(DIVIDER + "\n" + msg + "\n" + DIVIDER + "\n");
