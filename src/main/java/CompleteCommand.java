449a271a32d75f89215d913c838142a567501f2c
joshtyf <tan.yin.feng@hotmail.com>
2020-08-26 16:10:15 +0800

    Document CompleteCommand.java

--- src/main/java/CompleteCommand.java
+++ src/main/java/CompleteCommand.java
@@ -1,4 +1,19 @@
+/**
+ * <code>CompleteCommand</code> inherits from the base class <code>Command</code>
+ * and will handle the job of marking tasks as complete.
+ */
 public class CompleteCommand extends Command {
+    /**
+     * Marks a task as complete.
+     * Using the <code>Ui</code> object in the parent class, it prints out
+     * the user interface to ask for the number of the task to be completed.
+     * It uses the <code>Scanner</code> object in the parent class to receive the number.
+     * It uses the <code>TaskManager</code> object in the parent class and calls
+     * its <code>markDone</code> method with the task number passed as an argument to mark
+     * the task as completed.
+     * @return <code>true</code>
+     * @throws DukeException if an invalid task number was given by the user.
+     */
     public boolean execute() throws DukeException {
         ui.askTaskNumToComplete();
         int taskNum = Integer.parseInt(sc.nextLine());
