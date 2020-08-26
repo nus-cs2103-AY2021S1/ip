68b0c21cacb3931e93f38e7474ca7754b74c59bb
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-08-26 14:02:02 +0800

    Lint TaskList.java

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,7 +1,7 @@
 import java.time.format.DateTimeParseException;
+import java.io.Serializable;
 import java.util.List;
 import java.util.ArrayList;
-import java.io.Serializable;
 
 public class TaskList implements Serializable {
     private final List<Task> itemList = new ArrayList<>();
@@ -9,36 +9,36 @@ public class TaskList implements Serializable {
     Task addItem(String item) throws DukeException {
         String type = getItemType(item);
         Task taskToAdd = null;
-        switch(type) {
-            case "todo":
-                if (item.trim().equals("todo")) {
-                    throw new DukeException("Todos must have non-empty " +
-                                                    "descriptions!");
-                }
 
-                String todoDescription = item.split("todo")[1].trim();
-                taskToAdd = new Todo(todoDescription);
-                break;
-            case "deadline":
-                try {
-                    taskToAdd = new Deadline(
-                            getItemDescription(item, type),
-                            getItemParameter(item, type)
-                    );
-                    break;
-                } catch (DateTimeParseException e) {
-                    throw new DukeException("Invalid date entered");
-                }
-            case "event":
-                taskToAdd = new Event(
+        switch(type) {
+        case "todo":
+            if (item.trim().equals("todo")) {
+                throw new DukeException("Todos must have non-empty "
+                                                + "descriptions!");
+            }
+
+            String todoDescription = item.split("todo")[1].trim();
+            taskToAdd = new Todo(todoDescription);
+            break;
+        case "deadline":
+            try {
+                taskToAdd = new Deadline(
                         getItemDescription(item, type),
                         getItemParameter(item, type)
                 );
                 break;
-            default:
-                throw new DukeException("Unrecognized task/command.");
+            } catch (DateTimeParseException e) {
+                throw new DukeException("Invalid date entered");
+            }
+        case "event":
+            taskToAdd = new Event(
+                    getItemDescription(item, type),
+                    getItemParameter(item, type)
+            );
+            break;
+        default:
+            throw new DukeException("Unrecognized task/command.");
         }
-
 
         itemList.add(taskToAdd);
         return taskToAdd;
@@ -68,18 +68,18 @@ public class TaskList implements Serializable {
 
     void markAsDone(int itemIndex) throws IllegalArgumentException {
         if (itemIndex > itemList.size()) {
-            throw new IllegalArgumentException("Item #" + itemIndex + " does " +
-                                                       "not exist and cannot " +
-                                                       "be marked as done.");
+            throw new IllegalArgumentException("Item #" + itemIndex + " does "
+                                                       + "not exist and cannot "
+                                                       + "be marked as done.");
         }
         itemList.get(itemIndex - 1).markAsDone();
     }
 
     void removeItem(int itemIndex) throws IllegalArgumentException {
         if (itemIndex > itemList.size()) {
-            throw new IllegalArgumentException("Item #" + itemIndex + " does " +
-                                                       "not exist and cannot " +
-                                                       "be removed.");
+            throw new IllegalArgumentException("Item #" + itemIndex + " does "
+                                                       + "not exist and cannot "
+                                                       + "be removed.");
         }
         Task removed = itemList.remove(itemIndex - 1);
         Ui.showSuccessfulRemoval(removed);
