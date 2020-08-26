4f7b5a8e26e498e9f744b14b479eb48975cec5e1
Diwu-Yi <59159857+Diwu-Yi@users.noreply.github.com>
2020-08-26 19:45:15 +0800

    Formatted to meet coding standard

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ParserTest {
     @Test
-    public void invalidArgumentTest(){
+    public void parser_invalidArgument(){
         try{
             Parser pr = new Parser();
             String input = "deadline ";
