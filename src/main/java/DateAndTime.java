69ba919a0fabe1de75b96bfc69ac7df9135c6280
ureshiiYing <tan.ying.hui@u.nus.edu>
2020-08-26 14:42:19 +0800

    Implemented Level 8
    Created a DateAndTime class which allows PandaBot to understand date and time in the format dd/mm/yyyy hhmm
    Edited the Deadline behaviour such that Deadlines can accept both formatted and unformatted input as due by dates

---
+++ src/main/java/DateAndTime.java
@@ -0,0 +1,52 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+// Allows PandaBot to understand date and time  in the format of dd/mm/yyyy hhmm
+public class DateAndTime {
+    private final LocalDate date;
+    private final LocalTime time;
+    
+    public DateAndTime (String date, String time) throws DateTimeParseException {
+        this.date = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        this.time = LocalTime.parse(time, DateTimeFormatter.ofPattern("HHmm"));
+    }
+    
+    private String parseDate(LocalDate input) {
+        // Day of Month Counter
+        String ending;
+        int day = date.getDayOfMonth();
+        switch (day % 10) {
+        case 1:
+            ending = "st";
+            break;
+        case 2:
+            ending = "nd";
+            break;
+        case 3:
+            ending = "rd";
+            break;
+        default:
+            ending = "th";
+            break;
+        }
+        
+        return day + ending + " of " + date.format(DateTimeFormatter.ofPattern("MMMM yyyy"));
+    }
+    
+    private String parseTime(LocalTime input) {
+        if (input.getMinute() == 0) {
+            return time.format(DateTimeFormatter.ofPattern("ha"));
+        } else {
+            return time.format(DateTimeFormatter.ofPattern("h.mma"));
+        }
+    }
+    
+    // print date and time 
+    @Override
+    public String toString() {
+        return parseDate(date) + ", " + parseTime(time);
+    }
+    
+}
---
+++ src/main/java/PandaBotInvalidArgumentFormatException.java
@@ -0,0 +1,5 @@
+public class PandaBotInvalidArgumentFormatException extends PandaBotException {
+    public PandaBotInvalidArgumentFormatException(String msg) {
+        super("Invalid format given: " + msg);
+    }
+}
