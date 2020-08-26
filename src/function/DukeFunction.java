d66fcef2867cc3153af17fd5e05e806b575db1ae
Ma-Yueran <mayuerantom@outlook.com>
2020-08-26 16:17:19 +0800

    Move ui related functions in DukeFunction.java to Ui.java

--- src/function/DukeFunction.java
+++ src/function/DukeFunction.java
@@ -1,11 +1,9 @@
 package function;
 
-import ui.UIPrint;
-import task.Task;
 import command.Command;
 import data.DukeData;
 import data.DukeCommandSet;
-import data.TaskSave;
+import storage.TaskStorage;
 
 import exception.IncorrectFormatException;
 import exception.InvalidIndexException;
@@ -15,78 +13,6 @@ import exception.UnknownCommandException;
 import java.io.IOException;
 
 public class DukeFunction {
-
-    public static void greet() {
-        System.out.println(UIPrint.logo);
-
-        UIPrint.drawLine(UIPrint.star, 50);
-
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-
-        UIPrint.drawLine(UIPrint.star, 50);
-    }
-
-    public static void echo(String str) {
-        UIPrint.drawLine(UIPrint.star, 50);
-        System.out.println(str);
-        UIPrint.drawLine(UIPrint.star, 50);
-    }
-
-    public static void reportCurrentTasks() {
-        UIPrint.drawLine(UIPrint.star, 50);
-
-        System.out.println("Current tasks:\n");
-
-        for (int i = 0; i < DukeData.tasks.size(); i++) {
-            System.out.println(i + 1 + ". " + DukeData.tasks.get(i));
-        }
-
-        if (DukeData.tasks.size() == 0) {
-            System.out.println("None");
-        }
-
-        UIPrint.drawLine(UIPrint.star, 50);
-    }
-
-    public static void reportNewTask(Task task) {
-        UIPrint.drawLine(UIPrint.star, 50);
-
-        System.out.println("Got it. I've added this task: ");
-        System.out.println(task);
-        System.out.println("Now you have " + DukeData.tasks.size() + " tasks in the list.");
-
-        UIPrint.drawLine(UIPrint.star, 50);
-    }
-
-    public static void reportDoneTask(Task task) {
-        UIPrint.drawLine(UIPrint.star, 50);
-
-        System.out.println("Nice, I've marked this task as done:");
-        System.out.println(task);
-
-        UIPrint.drawLine(UIPrint.star, 50);
-    }
-
-    public static void reportDeleteTask(Task task) {
-        UIPrint.drawLine(UIPrint.star, 50);
-
-        System.out.println("Noted. I've removed this task: ");
-        System.out.println(task);
-        System.out.println("Now you have " + DukeData.tasks.size() + " tasks in the list");
-
-        UIPrint.drawLine(UIPrint.star, 50);
-    }
-
-    public static void reportExit() {
-        UIPrint.drawLine(UIPrint.star, 50);
-
-        String exitWords = "Bye, hope to see you again soon!";
-        System.out.println(exitWords);
-
-        UIPrint.drawLine(UIPrint.star, 50);
-    }
-
     public static void checkCommand(String str) {
         String[] inputParts = str.split(" ", 2);
         String possibleCommand = inputParts[0];
@@ -109,10 +35,10 @@ public class DukeFunction {
     }
 
     public static void loadSavedTasks() throws IOException {
-        DukeData.tasks = TaskSave.getInstance().getSavedTasks();
+        DukeData.tasks = TaskStorage.getInstance().getSavedTasks();
     }
 
     public static void saveCurrentTasks() throws IOException {
-        TaskSave.getInstance().saveTasks(DukeData.tasks);
+        TaskStorage.getInstance().saveTasks(DukeData.tasks);
     }
 }
---
+++ src/ui/Ui.java
@@ -0,0 +1,77 @@
+package ui;
+
+import data.DukeData;
+import task.Task;
+
+public class Ui {
+    public static void greet() {
+        System.out.println(UIPrint.logo);
+
+        UIPrint.drawLine(UIPrint.star, 50);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        UIPrint.drawLine(UIPrint.star, 50);
+    }
+
+    public static void echo(String str) {
+        UIPrint.drawLine(UIPrint.star, 50);
+        System.out.println(str);
+        UIPrint.drawLine(UIPrint.star, 50);
+    }
+
+    public static void reportCurrentTasks() {
+        UIPrint.drawLine(UIPrint.star, 50);
+
+        System.out.println("Current tasks:\n");
+
+        for (int i = 0; i < DukeData.tasks.size(); i++) {
+            System.out.println(i + 1 + ". " + DukeData.tasks.get(i));
+        }
+
+        if (DukeData.tasks.size() == 0) {
+            System.out.println("None");
+        }
+
+        UIPrint.drawLine(UIPrint.star, 50);
+    }
+
+    public static void reportNewTask(Task task) {
+        UIPrint.drawLine(UIPrint.star, 50);
+
+        System.out.println("Got it. I've added this task: ");
+        System.out.println(task);
+        System.out.println("Now you have " + DukeData.tasks.size() + " tasks in the list.");
+
+        UIPrint.drawLine(UIPrint.star, 50);
+    }
+
+    public static void reportDoneTask(Task task) {
+        UIPrint.drawLine(UIPrint.star, 50);
+
+        System.out.println("Nice, I've marked this task as done:");
+        System.out.println(task);
+
+        UIPrint.drawLine(UIPrint.star, 50);
+    }
+
+    public static void reportDeleteTask(Task task) {
+        UIPrint.drawLine(UIPrint.star, 50);
+
+        System.out.println("Noted. I've removed this task: ");
+        System.out.println(task);
+        System.out.println("Now you have " + DukeData.tasks.size() + " tasks in the list");
+
+        UIPrint.drawLine(UIPrint.star, 50);
+    }
+
+    public static void reportExit() {
+        UIPrint.drawLine(UIPrint.star, 50);
+
+        String exitWords = "Bye, hope to see you again soon!";
+        System.out.println(exitWords);
+
+        UIPrint.drawLine(UIPrint.star, 50);
+    }
+}
