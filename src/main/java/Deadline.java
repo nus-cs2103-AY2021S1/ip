284cb3553170c5dbfad208aefcea2060dcbe3da6
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-08-26 21:28:57 +0800

    Update Deadline class to store date and time as LocalDate and LocalTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    //protected String by;
+    protected LocalDate byDate;
+    protected LocalTime byTime;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.byDate = LocalDate.parse(by.split(" ")[0]);
+        this.byTime = LocalTime.parse(by.split(" ")[1]);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("ha");
+        return "[D]" + super.toString() + " (by: " + dateFormatter.format(byDate) + " " + timeFormatter.format(byTime) + ")";
     }
 }
