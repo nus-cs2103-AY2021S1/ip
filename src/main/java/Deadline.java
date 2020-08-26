c5b6ab142abdba0298d9c28b30b1b2b82f230799
joshtyf <tan.yin.feng@hotmail.com>
2020-08-26 16:10:31 +0800

    Document Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -3,9 +3,20 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * <code>Deadline</code> inherits from <code>Task</code>
+ * and is used to represent a deadline.
+ */
 class Deadline extends Task {
     private Date dueDate;
 
+    /**
+     * Constructor for creating a new <code>Deadline</code> object.
+     * Requires a <code>name</code> and a <code>dueDate</code>.
+     * @param name the name of this object
+     * @param dueDate the due date of this object in the format of "dd MM yyyy"
+     * @throws DukeException if the date format given is invalid
+     */
     Deadline(String name, String dueDate) throws DukeException {
         super(name);
         SimpleDateFormat formatter = new SimpleDateFormat("dd MM yyyy");
@@ -16,6 +27,11 @@ class Deadline extends Task {
         }
     }
 
+    /**
+     * Returns a comma seperated <code>String</code> containing the relevant information
+     * about this deadline. This information will then be saved in a text file.
+     * @return the comma seperated text of this deadline
+     */
     @Override
     public String saveText() {
         String completeStatus = super.isCompleted() ? "1" : "0";
@@ -23,6 +39,10 @@ class Deadline extends Task {
 ;        return "D," + completeStatus + "," + super.getName() + "," + dateFormat.format(dueDate);
     }
 
+    /**
+     * Returns the string representation of this deadline.
+     * @return a string representation of this deadline
+     */
     @Override
     public String toString() {
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
