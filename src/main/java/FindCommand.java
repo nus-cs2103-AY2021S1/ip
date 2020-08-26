982767d6ed8f24a7bd09f1f6dcf0c84d740250a0
joshtyf <tan.yin.feng@hotmail.com>
2020-08-26 17:07:19 +0800

    Add FindCommand.java
    FindCommand is a new command that will be used to search for tasks with
    a particular keyword.

---
+++ src/main/java/FindCommand.java
@@ -0,0 +1,22 @@
+/**
+ * <code>FindCommand</code> inherits from the base class <code>Command</code>.
+ * It handles the execution of finding tasks given a particular keyword.
+ */
+public class FindCommand extends Command{
+
+    /**
+     * Asks for a keyword through the <code>Ui</code> object and receives the user
+     * input through the <code>Scanner</code> object from the parent class.
+     * It then calls the method <code>findTask</code> with the <code>keyword</code>
+     * passed as an argument. It then prints out the result.
+     * @return <code>true</code>
+     */
+    @Override
+    public boolean execute() {
+        ui.askForKeyword();
+        String keyword = sc.nextLine();
+        String result = tm.findTask(keyword);
+        ui.print(result);
+        return true;
+    }
+}
