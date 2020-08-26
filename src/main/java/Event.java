311d00fed9203c17d56bc2458bcb40eec8cdb01e
gohyeeloon <goh.yeeloon@gmail.com>
2020-08-26 17:54:32 +0800

    add ability for Duke to understand dates instead of treating them as strings

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String eventTime;
+    protected LocalDate eventTime;
 
     public Event(String event) {
         super(event.substring(6, event.indexOf("/")-1));
-        this.eventTime = "at: " + event.substring(event.indexOf("/")+4);
+        this.eventTime = LocalDate.parse(event.substring(event.indexOf("/")+4));
     }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("[E]").append(super.toString()).append(" (").append(this.eventTime).append(")");
+        sb.append("[E]")
+                .append(super.toString())
+                .append(" (").append("at: ")
+                .append(this.eventTime.format(DateTimeFormatter.ofPattern("MMM d yyyy")))
+                .append(")");
         return sb.toString();
     }
 }
