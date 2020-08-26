1809bd8c6be9e846a174062d376d5e36b20d514b
Thai Duong <htduong292001@gmail.com>
2020-08-26 22:21:06 +0800

    Change all the methods in Ui class from static to non-static
    The methods are static
    
    They don't need to be static

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -11,10 +11,12 @@ import java.util.Scanner;
 public class Ui {
     private static String horizontalLine = "________________________________________";
 
+    private Scanner sc = new Scanner(System.in);
+
     /**
      * Prints the welcome message
      */
-    public static void showWelcome() {
+    public void showWelcome() {
         String logo = " ____        _" + System.lineSeparator()
                 + "|  _ \\ _   _| | _____" + System.lineSeparator()
                 + "| | | | | | | |/ / _ \\" + System.lineSeparator()
@@ -31,8 +33,7 @@ public class Ui {
      * Waits for the user to insert command and reads it.
      * @return A string represents the user's input
      */
-    public static String requestCommand() {
-        Scanner sc = new Scanner(System.in);
+    public String requestCommand() {
         System.out.println(horizontalLine);
         System.out.println("Your command:");
         String input = sc.nextLine();
@@ -44,7 +45,7 @@ public class Ui {
      * Shows the items in the task.
      * @param tasks The list of items to be shown
      */
-    public static void showTasks(TaskList tasks) {
+    public void showTasks(TaskList tasks) {
         String lazyHumanBash = "You have nothing in your list. Find something to do you human!";
         String showTasksMsg = "Here are the task(s) in your list:";
         if (tasks.size() == 0) {
@@ -58,7 +59,7 @@ public class Ui {
     /**
      * Notifies the user that the file cannot be loaded.
      */
-    public static void showFileLoadingError() {
+    public void showFileLoadingError() {
         System.out.println("Data cannot be loaded");
     }
 
@@ -66,14 +67,14 @@ public class Ui {
      * Prints out any error message that passed in 
      * @param errorMessage The error message to be printed 
      */
-    public static void showError(String errorMessage) {
+    public void showError(String errorMessage) {
         System.out.println(errorMessage);
     }
 
     /**
      * Says goodbye to the user.
      */
-    public static void showGoodbye() {
+    public void showGoodbye() {
         System.out.println("Bye human. See you again soon!");
     }
 
@@ -81,7 +82,7 @@ public class Ui {
      * Prints out the number of item in the task.
      * @param tasks The task
      */
-    public static void taskReport(TaskList tasks) {
+    public void taskReport(TaskList tasks) {
         System.out.println("You have " + tasks.size() + " task(s) in your list");
     }
 
@@ -90,7 +91,7 @@ public class Ui {
      * @param task The deleted task
      * @param tasks The remaining task list
      */
-    public static void showDeleteTask(Task task, TaskList tasks) {
+    public void showDeleteTask(Task task, TaskList tasks) {
         System.out.println("Noted. I've deleted this task:");
         System.out.println(task);
         taskReport(tasks);
@@ -100,7 +101,7 @@ public class Ui {
      * Notifies the user after successfully marking a task as done.
      * @param task The task being marked as done
      */
-    public static void showMarkAsDoneTask(Task task) {
+    public void showMarkAsDoneTask(Task task) {
         System.out.println("Good job bud! I've marked this task as done:");
         System.out.println(task);
     }
@@ -108,7 +109,7 @@ public class Ui {
     /**
      * Notifies the user whenever the system cannot understand the input command.
      */
-    public static void showUnrecognizedCommandMessage() {
+    public void showUnrecognizedCommandMessage() {
         System.out.println("I don't understand a single word you say, human!");
         System.out.println("Speak robot language pls");
     }
@@ -118,7 +119,7 @@ public class Ui {
      * @param task The added task
      * @param tasks The task list
      */
-    public static void showAddSuccessful(Task task, TaskList tasks) {
+    public void showAddSuccessful(Task task, TaskList tasks) {
         System.out.println("Got it. I've added this task:");
         System.out.println(task);
         taskReport(tasks);
@@ -128,7 +129,7 @@ public class Ui {
      * Show the user the result of a find command.
      * @param tasks The list of tasks that fulfill the requirement
      */
-    public static void showFindResult(TaskList tasks) {
+    public void showFindResult(TaskList tasks) {
         System.out.println("Here are the matching task(s) in your list: ");
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println(tasks.get(i));
