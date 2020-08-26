31406c739621923b44dbba272ab828182d8512df
Jeanne Toh <jeannetoh99@gmail.com>
2020-08-26 21:47:12 +0800

    Add formatted dates to Deadlines
    The dates in deadlines are stored as Strings.
    
    Storing dates as Strings is less ideal as it does not allow for sorting, etc.
    
    Let's store deadline dates as a java.time.LocalDate instead.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
     
     static {
-        format = "deadline <task description> /by <task deadline>";
+        format = "deadline <task description> /by <yyyy-mm-dd>";
     }
     
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("OOPS! The deadline in an incorrect format! " +
+                    "Please indicate the date as <yyyy-mm-dd>");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + this.by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
