94cf4544118b9ff88e60666d36150b6dd7dd3402
Petrick Jerico <petrickjerico@gmail.com>
2020-08-27 03:47:01 +0800

    Refactor code in Duke.class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,14 +5,18 @@ import java.util.regex.*;
 
 public class Duke {
 
-    public static void displayBar() {
+    public static void displayStarLine() {
         System.out.println("––––––––––––––––––––– *** –––––––––––––––––––––");
     }
 
-    public static void main(String[] args) {
+    public static void displayMessage(String message) {
+        displayStarLine();
+        System.out.println(message);
+        displayStarLine();
+        System.out.println();
+    }
 
-        List<Task> tasks = new ArrayList<>(100);
-
+    public static void welcome() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -26,103 +30,114 @@ public class Duke {
                 "Type in your orders below.\n\n" +
                 "(command list: 'list', 'deadline', 'event', 'todo', 'done', 'bye')";
 
-        displayBar();
-        System.out.println(welcome);
-        displayBar();
-        System.out.println();
+        displayMessage(welcome);
+    }
+
+    public static void bye() {
+        displayMessage("Alright, see you soon!");
+    }
+
+    public static void list(List<Task> tasks, int pending) {
+        int currId = 1;
+
+        displayStarLine();
+        System.out.println("TO-DO LIST:");
+        System.out.println(String.format("%d pending", pending));
+        for (Task t : tasks) {
+            System.out.println(String.format("   %d. %s", currId, t));
+            currId++;
+        }
+        displayStarLine();
+    }
+
+    public static int getNumIfValid(List<Task> tasks, String commandLine) throws DukeException {
+
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(commandLine);
+        Integer id = m.find() ? Integer.parseInt(m.group()) : null;
+
+        if (tasks.isEmpty()) {
+            throw new EmptyTasksException(commandLine);
+        }
+
+        if (id == null) {
+            throw new NullDoneIndexException(commandLine);
+        }
+
+        if (id < 1 || id > tasks.size()) {
+            throw new RangeIndexException(commandLine);
+        }
+
+        return id - 1;
+    }
+
+    public static void done(List<Task> tasks, String commandLine) throws DukeException {
+        int target = getNumIfValid(tasks, commandLine);
+        Task targetTask = tasks.get(target);
+        if (targetTask.isDone) {
+            throw new AlreadyDoneIndexException(commandLine);
+        }
+        tasks.get(target).markAsDone();
+
+        displayStarLine();
+        System.out.println("Good job! This task is now marked done:");
+        System.out.println(tasks.get(target));
+        displayStarLine();
+    }
+
+    public static boolean delete(List<Task> tasks, String commandLine) throws DukeException {
+        int target = getNumIfValid(tasks, commandLine);
+        Task targetTask = tasks.get(target);
+        tasks.remove(target);
+
+        displayStarLine();
+        System.out.println("Alright! This task is now deleted:");
+        System.out.println(targetTask);
+        displayStarLine();
+
+        return !targetTask.isDone;
+    }
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int done = 0;
+        List<Task> tasks = new ArrayList<>(100);
+        int pending = 0;
+
+        welcome();
 
         while (sc.hasNextLine()) {
 
-            displayBar();
+            String commandLine = sc.nextLine();
 
-            String task = sc.nextLine();
-            if (task.equals("bye")) {
-
-                System.out.println("Alright, see you soon!");
+            if (commandLine.startsWith("bye")) {
+                bye();
                 sc.close();
-                displayBar();
                 break;
-
-            } else if (task.equals("list")) {
-
-                int currId = 1;
-
-                System.out.println("TO-DO LIST:");
-                System.out.println(String.format("%d pending", tasks.size() - done));
-                for (Task t : tasks) {
-                    System.out.println(String.format("   %d. %s", currId, t));
-                    currId++;
-                }
-
-            } else if (task.startsWith("done")){
-
-                Pattern p = Pattern.compile("\\d+");
-                Matcher m = p.matcher(task);
-                Integer id = m.find() ? Integer.parseInt(m.group()) : null;
-
+            } else if (commandLine.startsWith("list")) {
+                list(tasks, pending);
+            } else if (commandLine.startsWith("done")){
                 try {
-                    if (tasks.isEmpty()) {
-                        throw new EmptyTasksException(task);
-                    }
-
-                    if (id == null) {
-                        throw new NullDoneIndexException(task);
-                    }
-
-                    if (id < 1 || id > tasks.size()) {
-                        throw new RangeIndexException(task);
-                    }
-
-                    int target = id - 1;
-
-                    if (tasks.get(target).isDone) {
-                        throw new AlreadyDoneIndexException(task);
-                    }
-
-                    tasks.get(target).markAsDone();
-                    done++;
-                    System.out.println("Good job! This task is now marked done:");
-                    System.out.println(tasks.get(target));
-                } catch (NullDoneIndexException | RangeIndexException | AlreadyDoneIndexException | EmptyTasksException e) {
+                    done(tasks, commandLine);
+                    pending--;
+                } catch (DukeException e) {
                     System.out.println(e);
                 }
-
-            } else if (task.startsWith("delete")) {
-                Pattern p = Pattern.compile("\\d+");
-                Matcher m = p.matcher(task);
-                Integer id = m.find() ? Integer.parseInt(m.group()) : null;
-
+            } else if (commandLine.startsWith("delete")) {
                 try {
-                    if (tasks.isEmpty()) {
-                        throw new EmptyTasksException(task);
+                    boolean pendingIsDeleted = delete(tasks, commandLine);
+                    if (pendingIsDeleted) {
+                        pending--;
                     }
-
-                    if (id == null) {
-                        throw new NullDoneIndexException(task);
-                    }
-
-                    if (id < 1 || id > tasks.size()) {
-                        throw new RangeIndexException(task);
-                    }
-
-                    int target = id - 1;
-
-                    System.out.println("Alright! This task is now deleted:");
-                    System.out.println(tasks.get(target));
-                    tasks.remove(target);
-                } catch (NullDoneIndexException | RangeIndexException | EmptyTasksException e) {
+                } catch (DukeException e) {
                     System.out.println(e);
                 }
             } else {
-
                 try {
-                    String[] inputs = task.split("/");
+                    String[] inputs = commandLine.split("/");
                     String[] taskDetails = inputs[0].split(" ", 2);
                     String taskType = taskDetails[0];
-                    String taskName = taskDetails.length == 1 ? "" : taskDetails[1];
+                    String taskName = taskDetails.length == 1 ? "" : taskDetails[1].trim();
                     String taskDate = inputs.length == 1 ? "" : inputs[1].split(" ", 2)[1];
 
                     switch (taskType) {
@@ -145,19 +160,18 @@ public class Duke {
                     }
 
                     int size = tasks.size();
+                    pending++;
 
+                    displayStarLine();
                     System.out.println("'" + taskName + "' added to list!");
                     System.out.println(tasks.get(size - 1));
                     System.out.println("\nYou now have " + size + " task(s) in your list.\n");
                     System.out.println("(Use 'list' command to see your updated list.)");
+                    displayStarLine();
                 } catch (DukeException e) {
                     System.out.println(e);
-                } catch (Exception e) {
-                    System.out.println("I can't parse your order properly!");
                 }
             }
-
-            displayBar();
             System.out.println();
         }
     }
