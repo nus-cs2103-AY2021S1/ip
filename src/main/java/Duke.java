0fcdbd34f4ce9033e05285647b7536e481d0586d
gohyeeloon <goh.yeeloon@gmail.com>
2020-08-26 15:06:14 +0800

    create path for Duke to store tasks in .txt file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Duke {
     public static void main(String[] args) {
+        File f = new File("data");
+        if (!f.exists()) {
+            f.mkdir();
+        }
+
+        try {
+            File duke = new File("data/duke.txt");
+            if (!duke.exists()) {
+                duke.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println(new GreetCommand().execute());
         Responder.responder();
     }
