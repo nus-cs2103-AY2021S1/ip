6675a5ca645d27ce513cf6a93bed133891b37d9d
Kwek Min Yih <minyihkwek@gmail.com>
2020-08-26 16:32:21 +0800

    Add tests for Task class

---
+++ src/test/java/duke/task/TaskTest.java
@@ -0,0 +1,28 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void createNewTaskTest(){
+        Task newTask = Task.newTask("Exercise");
+        assertEquals(newTask.getTaskName(), "Exercise", "Task name is incorrect");
+        assertEquals(newTask.getIsCompleted(), false, "Task completion status is incorrect");
+    }
+
+    @Test
+    public void createExistingTaskTest() {
+        Task existingTask = Task.existingTask("Sleep", true);
+        assertEquals(existingTask.getTaskName(), "Sleep", "Task name is incorrect");
+        assertEquals(existingTask.getIsCompleted(), true, "Task completion status is incorrect");
+    }
+
+    @Test
+    public void markAsDoneTest(){
+        Task newTask = Task.newTask("Homework").markAsDone();
+        assertEquals(newTask.getIsCompleted(), true,
+                "Task completion status is incorrect after marking as done");
+    }
+}
