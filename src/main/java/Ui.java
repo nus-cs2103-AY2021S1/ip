1536c16e06743b94e5ce0a1298b4a483b02a9af3
Seow Alex <seowalex@hotmail.com>
2020-08-26 20:34:43 +0800

    Change error prompt colour

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -14,23 +14,29 @@ public class Ui {
         );
     }
 
-    public void showPrompt(String promptText) {
+    public void showPrompt(String promptText, boolean isError) {
+        String COLOR_START = isError ? "\033[0;31m" : "";
+        String COLOR_END = isError ? "\033[0m" : "";
         String HORIZONTAL_LINE = "____________________________________________________________";
         String INDENTATION = "    ";
         String[] lines = promptText.split("[\\r\\n]+");
-        StringBuilder output = new StringBuilder(INDENTATION + HORIZONTAL_LINE + '\n');
+        StringBuilder output = new StringBuilder(COLOR_START + INDENTATION + HORIZONTAL_LINE + '\n');
 
         for (String line : lines) {
             output.append(INDENTATION).append(line).append('\n');
         }
 
-        output.append(INDENTATION).append(HORIZONTAL_LINE).append('\n');
+        output.append(INDENTATION).append(HORIZONTAL_LINE).append(COLOR_END).append('\n');
 
         System.out.println(output.toString());
     }
 
+    public void showPrompt(String promptText) {
+        showPrompt(promptText, false);
+    }
+
     public void showError(String errorText) {
-        showPrompt(errorText);
+        showPrompt(errorText, true);
     }
 
     public String readCommand() {
