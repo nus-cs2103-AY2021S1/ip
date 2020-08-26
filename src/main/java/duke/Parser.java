77334dc6cce34ec0569014f9dce1626e353221c7
Zhu Yuxuan <e0424709@u.nus.edu>
2020-08-26 21:26:39 +0800

    Modify Parser.java

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -5,31 +5,35 @@ import duke.task.TaskType;
 
 public class Parser {
 
+    public static boolean isEmpty(String input, int x) {
+        return input.length() <= x || input.substring(x).trim().isEmpty();
+    }
+
     public static Command parse(String input) throws DukeException {
         if (input.equals("bye")) {
             return new ExitCommand();
         } else if (input.trim().equals("list")) {
             return new ListCommand();
         } else if (input.startsWith("done ") || input.equals("done")) {
-            if (input.length() < 6 || input.substring(5).trim().isEmpty()) {
+            if (isEmpty(input, 5)) {
                 throw new DukeException("Which task do you want to mark as done?");
             }
             String item = input.substring(5).trim();
             return new DoneCommand(item);
         } else if (input.startsWith("delete ") || input.equals("delete")) {
-            if (input.length() < 8 || input.substring(7).trim().isEmpty()) {
+            if (isEmpty(input, 7)) {
                 throw new DukeException("Which task do you want to delete?");
             }
             String item = input.substring(7).trim();
             return new DeleteCommand(item);
         } else if (input.startsWith("todo ") || input.equals("todo")) {
-            if (input.length() < 6 || input.substring(5).trim().isEmpty()) {
+            if (isEmpty(input, 5)) {
                 throw new DukeException("The description of a todo cannot be empty.");
             }
             String name = input.substring(5).trim();
             return new AddCommand(TaskType.TODO, name);
         } else if (input.startsWith("deadline ") || input.equals("deadline")) {
-            if (input.length() < 10 || input.substring(9).trim().isEmpty()) {
+            if (isEmpty(input, 9)) {
                 throw new DukeException("The description of a deadline cannot be empty.");
             }
             String details = input.substring(9).trim();
@@ -42,7 +46,7 @@ public class Parser {
             String time = split[1];
             return new AddCommand(TaskType.DEADLINE, name, time);
         } else if (input.startsWith("event ") || input.equals("event")) {
-            if (input.length() < 7 || input.substring(6).trim().isEmpty()) {
+            if (isEmpty(input, 6)) {
                 throw new DukeException("The description of a event cannot be empty.");
             }
             String details = input.substring(6).trim();
