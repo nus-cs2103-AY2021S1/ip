129493eb7980eed181b552755bff9391df09fef2
chshen1998 <chewhoashen@gmail.com>
2020-08-27 18:17:43 +0800

    Add testCommands testIncompleteCommand testExits

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -14,17 +14,41 @@ public class Parser {
         } else if (userCommand.equals("list")) {  // For viewing items in to do list
             return new ListCommand();
         } else if (userCommand.equals("done")) {  // For marking items in the to do list as done
-            return new DoneCommand(inputSplit[1]);
+            try {
+                return new DoneCommand(inputSplit[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException(LINE + "Invalid input! Please specify which task you have completed! \n" + LINE);
+            }
         } else if (userCommand.equals("todo")) { // Add new to do task
-            return new TodoCommand(inputSplit[1]);
+            try {
+                return new TodoCommand(inputSplit[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException(LINE + "Invalid input! Please specify your todo description! \n" + LINE);
+            }
         } else if (userCommand.equals("deadline")) { // Add new deadline
-            return new DeadlineCommand(inputSplit[1]);
+            try {
+                return new DeadlineCommand(inputSplit[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException(LINE + "Invalid input! Please specify your deadline description and details! \n" + LINE);
+            }
         } else if (userCommand.equals("event")) { // Add new event
-            return new EventCommand(inputSplit[1]);
+            try {
+                return new EventCommand(inputSplit[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException(LINE + "Invalid input! Please specify your event description and details! \n" + LINE);
+            }
         } else if (userCommand.equals("delete")) { // Delete task
-            return new DeleteCommand(inputSplit[1]);
+            try {
+                return new DeleteCommand(inputSplit[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException(LINE + "Invalid input! Please specify which task you want to delete! \n" + LINE);
+            }
         } else if (userCommand.equals("filter")) { // Filter taskList
-            return new FilterCommand(inputSplit[1]);
+            try {
+                return new FilterCommand(inputSplit[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException(LINE + "Invalid input! Please specify which date you want to filter! \n" + LINE);
+            }
         } else {
             return new InvalidCommand();
         }
--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,13 +1,40 @@
+import command.*;
 import exception.DukeException;
 import org.junit.Test;
 import parser.Parser;
+import task.Deadline;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class DukeTest {
     @Test
-    public void testWrongCommand() throws DukeException {
-        //assertTrue(Parser.parse("Wrong Command") instanceof DukeException);
+    public void testCommands() throws DukeException {
+        assertTrue(Parser.parse("Wrong Command") instanceof InvalidCommand);
+        assertTrue(Parser.parse("list") instanceof ListCommand);
+        assertTrue(Parser.parse("done 1") instanceof DoneCommand);
+        assertTrue(Parser.parse("delete 1") instanceof DeleteCommand);
+        assertTrue(Parser.parse("filter 20/03/2020") instanceof FilterCommand);
+        assertTrue(Parser.parse("todo t1") instanceof TodoCommand);
+        assertTrue(Parser.parse("deadline d1 /by 20/02/2020 1200") instanceof DeadlineCommand);
+        assertTrue(Parser.parse("event e1 /at 20/02/2020 1200") instanceof EventCommand);
+    }
+
+    @Test(expected = DukeException.class)
+    public void testIncompleteCommand() throws DukeException {
+        Parser.parse("done");
+    }
+
+    @Test
+    public void testExit() {
+        assertEquals(new ByeCommand().isExit(), true);
+        assertEquals(new ListCommand().isExit(), false);
+        assertEquals(new InvalidCommand().isExit(), false);
+        assertEquals(new DeadlineCommand("test").isExit(), false);
+        assertEquals(new EventCommand("test").isExit(), false);
+        assertEquals(new TodoCommand("test").isExit(), false);
+        assertEquals(new DoneCommand("test").isExit(), false);
+        assertEquals(new FilterCommand("test").isExit(), false);
+        assertEquals(new DeleteCommand("test").isExit(), false);
     }
 }
