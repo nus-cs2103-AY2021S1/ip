52448e8eca92ebdf0bbd9730d784c91affde80d9
wang-jun-hao <wangjunhao98@gmail.com>
2020-08-27 23:26:59 +0800

    A-Jar

--- src/main/java/NoFolderException.java
+++ src/main/java/NoFolderException.java
@@ -1,5 +1,5 @@
 public class NoFolderException extends DukeException {
     NoFolderException() {
-        super("Folder does not exists yet, new 'data' folder created in project root");
+        super("Folder does not exists yet, new 'data' folder created under root");
     }
 }
--- src/main/java/NoSavedFileException.java
+++ src/main/java/NoSavedFileException.java
@@ -1,5 +1,5 @@
 public class NoSavedFileException extends DukeException {
     NoSavedFileException() {
-        super("No save file for tasks found. Automatically created new save file under project_root/data");
+        super("No save file for tasks found. Automatically created new save file under root/data");
     }
 }
