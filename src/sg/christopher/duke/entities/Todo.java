6b329a1fdebcfee2b11b9bed202e3aee4e4a096b
Christopher Goh <chrisgzf@gmail.com>
2020-08-27 21:43:31 +0800

    docs: Add javadocs for Todo

--- src/sg/christopher/duke/entities/Todo.java
+++ src/sg/christopher/duke/entities/Todo.java
@@ -1,6 +1,14 @@
+/**
+ * Wrapper class for Todo tasks.
+ */
 package sg.christopher.duke.entities;
 
 public class Todo extends Task {
+    /**
+     * Creates a new Todo task.
+     *
+     * @param description description of Todo
+     */
     public Todo(String description) {
         super(description);
     }
