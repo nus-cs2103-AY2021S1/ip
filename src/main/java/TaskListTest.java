99704fc06a721b05b40ff04bfed4d81b7b1280eb
divakarmal <malhotradivakar@gmail.com>
2020-08-27 19:08:18 +0800

    Added jUnit test cases

---
+++ src/main/java/TaskListTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class TaskListTest {
+
+    @Test
+    public void taskList_addTask_taskAdded() {
+        TaskList list = new TaskList();
+        list.addTask(new TodoTask("description", false));
+        assertEquals(list.getSize(), 1);
+        assertEquals(list.getTask(1).toString(), "[T][✗] description");
+    }
+
+    @Test
+    public void taskList_deleteTask_taskDeleted() {
+        var list = new TaskList();
+        list.addTask(new TodoTask("task1", false));
+        list.addTask(new TodoTask("task2", false));
+        list.deleteTask(1);
+        assertEquals(list.getSize(), 1);
+    }
+
+    @Test
+    public void taskList_addDeadlineTask_taskAdded() {
+        TaskList list = new TaskList();
+        list.addTask(new DeadlineTask("description", false, "2019-05-06"));
+        assertEquals(list.getSize(), 1);
+        assertEquals(list.getTask(1).toString(), "[D][✗] description(by: May 6 2019)");
+    }
+
+}
