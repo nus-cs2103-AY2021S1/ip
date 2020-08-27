8ba48ac5c684ad22eddec6c480d596cc4088b7f0
royleochan <royleochan@gmail.com>
2020-08-27 19:28:50 +0800

    Add javadoc for find command

--- src/main/java/command/FindCommand.java
+++ src/main/java/command/FindCommand.java
@@ -7,13 +7,30 @@ import util.Ui;
 
 import java.util.List;
 
+/**
+ * Represents the find command. The find command searches for tasks based on the query and lists them.
+ */
 public class FindCommand extends Command{
+    /** Query of the find command. */
     private final String query;
-    
+
+    /**
+     * Creates a new Find command. 
+     *
+     * @param query Query for the find command to search for.
+     */
     public FindCommand(String query) {
         this.query = query;
     }
-    
+
+    /**
+     * Executes the find command. The execution involves finding tasks based on the query and listing out the search
+     * results.
+     *
+     * @param lst List containing the current tasks.
+     * @param ui Ui allows execute to carry out ui methods to print to the console.
+     * @param storage Storage allows execute to write and read files.
+     */
     public void execute(TaskList lst, Ui ui, Storage storage) {
         List<Task> filteredTasks = lst.filter(query);
         ui.showFindStatement(filteredTasks.size() <= 0);
