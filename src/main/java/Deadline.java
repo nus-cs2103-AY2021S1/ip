a797a8f3a62a9cc06f942aa5c4c0eae22cecd09a
luciatirta <luciatirtag@gmail.com>
2020-08-27 18:49:10 +0800

    Add LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,29 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 	protected final String by;
+	protected LocalDate date;
 
 	public Deadline(String description, String by) {
 		super(description);
 		this.by = by;
+		try {
+			this.date = LocalDate.parse(by);
+		} catch (DateTimeParseException ex){
+			this.date = null;
+		}
 	}
 
 	public Deadline(String description, boolean isDone, String by) {
 		super(description, isDone);
 		this.by = by;
+		try {
+			this.date = LocalDate.parse(by);
+		} catch (DateTimeParseException ex){
+			this.date = null;
+		}
 	}
 
 	@Override
@@ -18,6 +33,7 @@ public class Deadline extends Task {
 
 	@Override
 	public String toString() {
-		return "[D]" + super.toString() + " (by: " + by + ")";
+		return "[D]" + super.toString() + " (by: " + (date != null ?
+			(date.getDayOfWeek() + ", " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy"))) : by) + ")";
 	}
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,30 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
 	protected final String at;
+	protected LocalDate date;
+
 
 	public Event(String description, String at) {
 		super(description);
 		this.at = at;
+		try {
+			this.date = LocalDate.parse(at);
+		} catch (DateTimeParseException ex){
+			this.date = null;
+		}
 	}
 
 	public Event(String description, boolean isDone, String at) {
 		super(description, isDone);
 		this.at = at;
+		try {
+			this.date = LocalDate.parse(at);
+		} catch (DateTimeParseException ex){
+			this.date = null;
+		}
 	}
 
 	@Override
@@ -18,6 +34,7 @@ public class Event extends Task {
 
 	@Override
 	public String toString() {
-		return "[E]" + super.toString() + " (at: " + at + ")";
+		return "[E]" + super.toString() + " (at: " + (date != null ?
+			(date.getDayOfWeek() + ", " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy"))) : at) + ")";
 	}
 }
