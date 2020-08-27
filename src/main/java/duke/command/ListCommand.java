a206e378fb5c197e491d39e471c16f636872ae3e
Joe <jteddysanjaya@gmail.com>
2020-08-27 23:46:50 +0800

    Add JavaDoc to Command classes

--- src/main/java/duke/command/ListCommand.java
+++ src/main/java/duke/command/ListCommand.java
@@ -3,7 +3,18 @@ package duke.command;
 import duke.TaskList;
 import duke.Ui;
 
+/**
+ * Represents a list command.
+ */
 public class ListCommand implements Command {
+
+    /**
+     * Shows all the tasks that are in the list.
+     *
+     * @param ts
+     * @param ui
+     * @param input
+     */
     @Override
     public void execute(TaskList ts, Ui ui, String input) {
         ui.list();
--- src/main/java/duke/command/QuitCommand.java
+++ src/main/java/duke/command/QuitCommand.java
@@ -3,7 +3,18 @@ package duke.command;
 import duke.TaskList;
 import duke.Ui;
 
+/**
+ * Represents a quit command.
+ */
 public class QuitCommand implements Command {
+
+    /**
+     * Shows a goodbye message before exiting the program.
+     *
+     * @param ts
+     * @param ui
+     * @param input
+     */
     @Override
     public void execute(TaskList ts, Ui ui, String input) {
         ui.quit();
