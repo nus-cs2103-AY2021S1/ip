b8f22dfe3493d9baf34630df85f24d8d65a507da
Theodore Leebrant <theodoreleebrant@gmail.com>
2020-08-27 23:32:00 +0800

    remove todo

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -3,8 +3,7 @@ package duke;
 public class Task {
     boolean done;
     public String description;
-
-    // TODO: 17/8/20 make a toString 
+    
     public Task(String description) {
         this.done = false;
         this.description = description;
