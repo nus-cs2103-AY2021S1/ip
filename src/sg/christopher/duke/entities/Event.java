7c2d23dd1815926e82badf0f0be8d8fa4ddc9cb7
Christopher Goh <chrisgzf@gmail.com>
2020-08-27 21:44:35 +0800

    docs: Add javadocs for Event

--- src/sg/christopher/duke/entities/Event.java
+++ src/sg/christopher/duke/entities/Event.java
@@ -1,6 +1,15 @@
+/**
+ * Wrapper class for Event tasks.
+ */
 package sg.christopher.duke.entities;
 
 public class Event extends Task {
+    /**
+     * Creates a new Event task.
+     *
+     * @param description description of task
+     * @param dateTime time that task needs to be done at
+     */
     public Event(String description, String dateTime) {
         super(description);
         this.dateTime = dateTime;
