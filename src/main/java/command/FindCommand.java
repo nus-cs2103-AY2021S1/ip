80fd7b8eea44d8cee0863a8a7121517d116040bf
chshen1998 <chewhoashen@gmail.com>
2020-08-27 22:26:01 +0800

    Add FindCommand

---
+++ src/main/java/command/FindCommand.java
@@ -0,0 +1,36 @@
+package command;
+
+import exception.DukeException;
+
+import storage.Storage;
+
+import tasklist.TaskList;
+
+import ui.Ui;
+
+public class FindCommand extends Command {
+    private String keyword;
+
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
+        try {
+            String output = "";
+            for (int i = 1; i <= taskList.size(); i++) {
+                if (taskList.get(i - 1).toString().contains(keyword)) {
+                    output = output + i + ". " + taskList.get(i - 1) + "\n";
+                }
+            }
+            System.out.println(ui.LINE + "Here are your task that matches your search: \n" + output + ui.LINE);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException(ui.LINE + "Invalid input! Please specify the keyword to find! \n" + ui.LINE);
+        }
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
