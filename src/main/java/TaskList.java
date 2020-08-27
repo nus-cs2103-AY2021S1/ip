f5898d5f7db263c7902df1bd3fda21a7bbd160c7
Joe <jteddysanjaya@gmail.com>
2020-08-27 15:30:48 +0800

    Extract all adding/deleting/modifying to TaskList Class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+
+    List<Task> tasks;
+    Storage storage = new Storage();
+
+    public TaskList() {
+        tasks = storage.getTasks();
+    }
+
+    // CREATE
+    public void add(Task task) {
+        storage.add(task);
+        tasks.add(task);
+    }
+
+    // READ
+    public List<Task> getTasks() {
+        return tasks;
+    }
+    public int taskSize() {
+        return tasks.size();
+    }
+
+    // UPDATE
+    public Task markAsDone(int taskId) {
+        Task task = tasks.get(taskId).markAsDone();
+        tasks.set(taskId, task);
+        storage.update();
+        return task;
+    }
+
+    // DELETE
+    public Task delete(int taskId) {
+        Task task = tasks.get(taskId);
+        tasks.remove(taskId);
+        storage.update();
+        return task;
+    }
+
+}
