8f194f97c3ea4b6b1eca00395069a52deba398f3
Ma-Yueran <mayuerantom@outlook.com>
2020-08-27 20:32:04 +0800

    Add javadoc to FindCommand.java

--- src/duke/command/FindCommand.java
+++ src/duke/command/FindCommand.java
@@ -1,17 +1,29 @@
 package duke.command;
 
 import duke.Duke;
-import duke.data.DukeTaskList;
 import duke.task.Task;
 
 import java.util.ArrayList;
 
+/**
+ * FindCommand finds all tasks that contain the keyword entered by user,
+ * then report to user.
+ */
 public class FindCommand extends Command {
 
+    /**
+     * Constructs a FindCommand.
+     */
     public FindCommand() {
         names = new String[] { "find" };
     }
 
+    /**
+     * Finds all tasks containing the keyword using DukeTaskList,
+     * then use Ui to report to the user.
+     * @param str the keyword
+     * @param duke the current Duke
+     */
     @Override
     public void execute(String str, Duke duke) {
         ArrayList<Task> tasksFound = duke.taskList.findTasks(str);
