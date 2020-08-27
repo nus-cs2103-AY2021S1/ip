c793a5db0cc65a00cc3058dcefd644e0f623663f
Theodore Leebrant <theodoreleebrant@gmail.com>
2020-08-27 23:33:56 +0800

    Add more javadocs

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -4,7 +4,6 @@ public class Task {
     boolean done;
     public String description;
 
-    // TODO: 17/8/20 make a toString 
     public Task(String description) {
         this.done = false;
         this.description = description;
