be8f8a125878b262985785cabbbab748980fa0f1
Ma-Yueran <mayuerantom@outlook.com>
2020-08-27 11:39:22 +0800

    Use UiStub & DukeTaskListStub in DukeStub

--- src/test/java/duke/DukeStub.java
+++ src/test/java/duke/DukeStub.java
@@ -1,8 +1,20 @@
 package duke;
 
+import duke.command.CommandParser;
+import duke.data.DukeCommandSet;
+import duke.data.DukeState;
+import duke.data.DukeTaskListStub;
+import duke.storage.DukeStorage;
+import duke.ui.UiStub;
+
 public class DukeStub extends Duke {
 
     public DukeStub() {
-        super();
+        commandSet = new DukeCommandSet();
+        state = new DukeState();
+        ui = new UiStub(this);
+        commandParser = new CommandParser();
+        taskList = new DukeTaskListStub();
+        storage = new DukeStorage(this);
     }
 }
