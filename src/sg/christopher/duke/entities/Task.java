24e5a25aa7029e415bee712061aaf93b81d2c168
Christopher Goh <chrisgzf@gmail.com>
2020-08-27 21:42:38 +0800

    docs: Add javadocs for Task

--- src/sg/christopher/duke/entities/Task.java
+++ src/sg/christopher/duke/entities/Task.java
@@ -1,3 +1,6 @@
+/**
+ * Wrapper class for tasks.
+ */
 package sg.christopher.duke.entities;
 
 import java.io.Serializable;
@@ -6,19 +9,37 @@ public class Task implements Serializable {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Creates a new Task.
+     *
+     * @param description description of the task
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Gets a tick or cross representation of the done status of the task.
+     *
+     * @return tick or cross icon representation of the done status of the task
+     */
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
+    /**
+     * Gets the task's description.
+     *
+     * @return the task's description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Marks a task as done.
+     */
     public void markAsDone() {
         isDone = true;
     }
