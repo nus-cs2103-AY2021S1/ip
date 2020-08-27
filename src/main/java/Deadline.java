519147a4fd3dbe5951d54633dab9b79ff13577b7
Jerry Ho <jerryho@u.nus.edu>
2020-08-28 00:28:21 +0800

    Add new constructor method to Deadline, Task, Event and ToDo to support boolean isDone

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -7,6 +7,11 @@ public class Deadline extends Task {
         this.by = by;
     }
 
+    public Deadline(String description, String by, boolean isDone) {
+        super(description, isDone);
+        this.by = by;
+    }
+
     @Override
     public String toText() {
         String str = super.toText("D");
