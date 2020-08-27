e763c934411783d28dce06fa6a512edb57d1e981
Christopher Goh <chrisgzf@gmail.com>
2020-08-27 21:08:47 +0800

    test: Add Task unit tests

---
+++ src/test/sg/christopher/duke/entities/TaskTest.java
@@ -0,0 +1,27 @@
+package christopher.duke.entities;
+
+import org.junit.jupiter.api.Test;
+import sg.christopher.duke.entities.Task;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void task_shouldBeCreateable() {
+        Task task = new Task("mock desc");
+        return;
+    }
+
+    @Test
+    public void task_whenNotDone_shouldDisplayCross() {
+        Task task = new Task("mock desc");
+        assertEquals(task.getStatusIcon(), "\u2718");
+    }
+
+    @Test
+    public void task_whenDone_shouldDisplayTick() {
+        Task task = new Task("mock desc");
+        task.markAsDone();
+        assertEquals(task.getStatusIcon(), "\u2713");
+    }
+}
