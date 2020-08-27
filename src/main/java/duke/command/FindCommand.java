c840b7e92abfd704013f6d4dfd273af52544bbc3
Joe <jteddysanjaya@gmail.com>
2020-08-28 00:26:32 +0800

    Add JavaDoc to FindCommand

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -11,13 +11,26 @@ import duke.task.Task;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Represent a find command.
+ */
 public class FindCommand implements Command {
+
+    /**
+     * Parses the input to extract the detail: searchWord.
+     * Shows the list of filtered tasks that contains the searchWord.
+     *
+     * @param taskList
+     * @param ui
+     * @param input
+     * @throws EmptyDescriptionException
+     */
     @Override
-    public void execute(TaskList taskList, Ui ui, String input) throws InvalidTaskIdException, EmptyDescriptionException, InvalidFormatException {
-        String find = Parser.parseFind(input);
+    public void execute(TaskList taskList, Ui ui, String input) throws EmptyDescriptionException {
+        String searchWord = Parser.parseFind(input);
         List<Task> tasks = taskList.getTasks();
         List<Task> filtered = tasks.stream().
-                filter(task -> task.getTitle().contains(find)).collect(Collectors.toList());
+                filter(task -> task.getTitle().contains(searchWord)).collect(Collectors.toList());
         ui.filteredList(filtered);
     }
 }
