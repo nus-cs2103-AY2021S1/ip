0b7a2430048b27c3fb7a9f65163ec2287c81d742
yuanxing-y <zyx19991208@gmail.com>
2020-08-27 15:22:01 +0800

    Level 8 added, now deadline accept dates in yyyy-mm-dd format and print in MMM dd yyyy format.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     protected String by;
+    private LocalDate ddl = null;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        try {
+            this.ddl = LocalDate.parse(by);
+            this.by = ddl.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (Exception exception) {
+            System.out.println("Sorry I can't understand the time format you just inputted. \n" +
+                    "Try it in this format: yyyy-mm-dd (e.g. 2020-01-01)" +
+                    " so that I can remind you at the time");
+        }
     }
 
     @Override
