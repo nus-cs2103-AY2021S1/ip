8423ecbdb8360923640cf4f7001f340fdaf9eac6
Bangyi Wu <bangyi98@gmail.com>
2020-08-27 11:54:09 +0800

    A-CodingStandard

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -3,8 +3,7 @@ package duke;
 /**
  * The DukeException is a custom exception class for Duke.
  */
-public class DukeException
-        extends Exception {
+public class DukeException extends Exception {
     public DukeException(String errorMessage) {
         super(errorMessage);
     }
