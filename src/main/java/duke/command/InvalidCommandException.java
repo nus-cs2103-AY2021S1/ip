6e2da5da5085ab460c0334274d39d41da8079bd2
lll-jy <E0376997@u.nus.edu>
2020-08-27 19:27:22 +0800

    Add javadoc for exception InvalidCommandException

--- src/main/java/duke/command/InvalidCommandException.java
+++ src/main/java/duke/command/InvalidCommandException.java
@@ -1,5 +1,8 @@
 package duke.command;
 
+/**
+ * Throws when the user input of command cannot be parsed correctly.
+ */
 public class InvalidCommandException extends Exception {
     public InvalidCommandException() {
         super("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
