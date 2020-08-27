c03c607a9f71b9578c7f95dc0bc0ebb312aa4f05
yanbingtao <yanbingtao963@Gmail.com>
2020-08-27 08:02:38 +0800

    write javadoc for TaskList class

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -3,31 +3,34 @@ package duke;
 import java.util.ArrayList;
 
 /**
- *
+ * Contains the overall user's task list, and it provide operations to modify task in the task list.
  */
 public class TaskList {
     public ArrayList<Task> tasks;
 
     /**
+     * TaskList Constructor.
      *
-     * @param tasks
+     * @param tasks The overall user's task list.
      */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
     /**
+     * Returns the ArrayList<Task> contains all user's tasks in the task list.
      *
-     * @return
+     * @return The ArrayList<Task> contains all user's tasks in the task list.
      */
     public ArrayList<Task> getTaskList() {
         return this.tasks;
     }
 
     /**
+     * Adds new to-do type task to the task list and calls the Ui class to display a message.
      *
-     * @param taskTitle
-     * @param tasks
+     * @param taskTitle A string of Event task name.
+     * @param tasks The overall user's task list.
      */
     public static void addNewTodoTask(String taskTitle, ArrayList<Task> tasks) {
         ToDos newTodoTask = new ToDos(taskTitle, false);
@@ -36,10 +39,11 @@ public class TaskList {
     }
 
     /**
+     * Adds new deadline type task to the task list and calls the Ui class to display a message.
      *
-     * @param taskTitle
-     * @param deadlineTime
-     * @param tasks
+     * @param taskTitle A string of Event task name.
+     * @param deadlineTime A string of deadline time
+     * @param tasks The overall user's task list.
      */
     public static void addNewDeadlineTask(String taskTitle, String deadlineTime, ArrayList<Task> tasks) {
         Deadlines newDeadline = new Deadlines(taskTitle, deadlineTime, false);
@@ -48,10 +52,11 @@ public class TaskList {
     }
 
     /**
+     * Adds new event type task to the task list and calls the Ui class to display a message.
      *
-     * @param taskTitle
-     * @param eventTime
-     * @param tasks
+     * @param taskTitle A string of Event task name.
+     * @param eventTime A string of Event time.
+     * @param tasks The overall user's task list.
      */
     public static void addNewEventTask(String taskTitle, String eventTime, ArrayList<Task> tasks) {
         Events newEvent = new Events(taskTitle, eventTime, false);
@@ -60,9 +65,10 @@ public class TaskList {
     }
 
     /**
+     * Deletes a task from the task list and calls the Ui class to display a message.
      *
-     * @param index
-     * @param tasks
+     * @param index The int of the task series number in the task list.
+     * @param tasks The overall user's task list.
      */
     public static void deleteTask(int index, ArrayList<Task> tasks) {
         Task taskToDelete = tasks.get(index - 1);
@@ -72,9 +78,10 @@ public class TaskList {
     }
 
     /**
-     *
-     * @param index
-     * @param tasks
+     * Marks a task as done in the task list and calls the Ui class to display a message.
+     * 
+     * @param index The int of the task series number in the task list.
+     * @param tasks The overall user's task list.
      */
     public static void doneTask(int index, ArrayList<Task> tasks) {
         tasks.get(index - 1).markAsDone();
