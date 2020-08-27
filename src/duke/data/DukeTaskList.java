83b001276b8d4548cc9904538b57e750703f0d21
Ma-Yueran <mayuerantom@outlook.com>
2020-08-27 11:54:11 +0800

    Add getTask, addTask, deleteTask, getSize, setTasks, getTasks in DukeTaskList.java

--- src/duke/data/DukeTaskList.java
+++ src/duke/data/DukeTaskList.java
@@ -5,9 +5,36 @@ import duke.task.Task;
 
 public class DukeTaskList {
 
-    public ArrayList<Task> tasks;
+    private ArrayList<Task> tasks;
 
     public DukeTaskList() {
         tasks = new ArrayList<>();
+    }
+
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task deleteTask(int index) {
+        Task taskDelete = tasks.get(index);
+        tasks.remove(index);
+
+        return taskDelete;
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
     }
 }
