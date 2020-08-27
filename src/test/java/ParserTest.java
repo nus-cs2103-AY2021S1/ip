84d672b3455c98586bb3741d9b9bd399a1bc08c9
WeiJie96 <fongweijie96@u.nus.edu>
2020-08-28 05:49:46 +0800

    Add unit tests for Parser and TaskList

---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ParserTest {
+
+    @Test
+    public void parse_noDescription_exceptionThrown() {
+        try {
+            Parser.parse("event");
+            fail();
+        } catch (DukeException e) {
+            assertEquals("     OOPS!!! The description of a event cannot be empty.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void parse_wrongCommand_exceptionThrown() {
+        try {
+            Parser.parse("random");
+            fail();
+        } catch (DukeException e) {
+            assertEquals("     OOPS!!! I'm sorry, but I don't know what that means :-(", e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/TaskListTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TaskListTest {
+
+    @Test
+    public void handleDeadline_wrongDateFormat_exceptionThrown() {
+        try {
+            TaskList tasks = new TaskList();
+            CommandName deadline = CommandName.DEADLINE;
+            tasks.addTask(deadline, "desc", "2020/10/10");
+            fail();
+        } catch (DukeException e) {
+            assertEquals("     OOPS!!! Pass in a date in yyyy-mm-dd :-(", e.getMessage());
+        }
+    }
+}
