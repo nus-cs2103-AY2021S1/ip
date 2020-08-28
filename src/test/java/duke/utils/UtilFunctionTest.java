500f6cdb23ad7368d2586701aabe8b26cdf8200e
Eddy <eddy.chi.chu@gmail.com>
2020-08-28 11:57:14 +0800

    A-Jar
    Build Jar file

--- src/test/java/duke/utils/UtilFunctionTest.java
+++ src/test/java/duke/utils/UtilFunctionTest.java
@@ -4,8 +4,9 @@ package duke.utils;
 import duke.exceptions.DateFormatException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 public class UtilFunctionTest {
     @Test
