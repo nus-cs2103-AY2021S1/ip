18d0dbae0fbe569871e468d14474db7b74f88b93
lll-jy <E0376997@u.nus.edu>
2020-08-28 16:59:54 +0800

    Add custom behavior to DialogBix - iteration 3

--- src/main/java/duke/app/App.java
+++ src/main/java/duke/app/App.java
@@ -110,8 +110,8 @@ public class App extends Application {
         Label userText = new Label(userInput.getText());
         Label dukeText = new Label(getResponse(userInput.getText()));
         dialogContainer.getChildren().addAll(
-                new DialogBox(userText, new ImageView(user)),
-                new DialogBox(dukeText, new ImageView(duke))
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
         );
         userInput.clear();
     }
