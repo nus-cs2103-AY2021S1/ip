7377d8f6360e6d0ad0fc78c0ba0481f9676ff4ff
FooJingYi <foojingyi@comp.nus.edu.sg>
2020-08-28 18:26:02 +0800

    Update Duke.java with more OOP structure using the new classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,50 +6,40 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        List<Task> list = new ArrayList<>();
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
 
-        //  Load the data from the hard disk when Duke starts up.
-        File taskFile = new File("data/duke.txt");
-        if (!taskFile.exists()) {
-            File dataFolder = new File("data");
-            if (!dataFolder.exists()) {
-                dataFolder.mkdirs();
-            }
-            try {
-                taskFile.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
+    public Duke(String pathFile) {
+        this.ui = new Ui();
+        this.storage = new Storage(pathFile);
         try {
-            FileToListReader fileReader = new FileToListReader(taskFile);
-            list = fileReader.getList();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (WrongDeadlineException e) {
-            e.printStackTrace();
-        }
-
-        Message start = new Message("start", list, taskFile);
-        try {
-            start.reply();
+            this.tasks = new TaskList(storage.load());
         } catch (DukeException e) {
-            System.out.println(e.getMessage());
+            // todo
+            this.tasks = new TaskList();
         }
+    }
 
-        while (sc.hasNext()) {
-            Message msg = new Message(sc.nextLine(), list, taskFile);
+    public void run() {
+        ui.showWelcomeMsg();
+        boolean isExit = false;
+        while (!isExit) {
             try {
-                msg.reply();
+                String fullCommand = this.ui.readCommand();
+                this.ui.showLine();
+                Command command = Parser.parse(fullCommand);
+                command.execute(tasks, storage);
+                isExit = command.isExit();
             } catch (DukeException e) {
-                System.out.println(e.getMessage());
-            }
-            if (msg.getCmd() == Command.BYE) {
-                break;
+                ui.showError(e.getMessage());
+            } finally {
+                this.ui.showLine();
             }
         }
+    }
+
+    public static void main(String[] args) {
+        new Duke("data/tasks.txt").run();
     }
 }
