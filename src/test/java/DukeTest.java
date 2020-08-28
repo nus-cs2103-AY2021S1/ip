2c8e189b640a3152128c813818f67bc3ebf05b66
Gabriel Sim <gabrielsimbingyang@gmail.com>
2020-08-28 22:50:35 +0800

    Finish parser test cases for getDetails() and extractDetails()

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class DukeTest {
     @Test
     public void dummyTest(){
         assertEquals(2, 2);
     }
+
 }
