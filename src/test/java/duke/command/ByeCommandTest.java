2985dbbc08ac2adf1314ac38bdfb1b35f7ad4bb9
lll-jy <E0376997@u.nus.edu>
2020-08-28 10:35:19 +0800

    Change __ to _ in test method name

--- src/test/java/duke/command/ByeCommandTest.java
+++ src/test/java/duke/command/ByeCommandTest.java
@@ -4,6 +4,7 @@ import duke.component.Storage;
 import duke.component.StorageStub;
 import duke.component.TaskList;
 import duke.component.Ui;
+
 import org.testng.annotations.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,13 +12,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ByeCommandTest {
     @Test
-    public void isExit__alwaysTrue() {
+    public void isExit_alwaysTrue() {
         ByeCommand b = new ByeCommand("bye");
         assertTrue(b.isExit());
     }
 
     @Test
-    public void execute__nothingWrong() {
+    public void execute_nothingWrong() {
         Ui ui = new Ui();
         Storage storage = new StorageStub();
         TaskList list = storage.getList();
