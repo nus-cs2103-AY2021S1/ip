4b7fb0027702e53d3754292af96625204547fc4c
lucastai98 <e0406424@u.nus.edu>
2020-08-28 15:38:35 +0800

    Level-9

--- src/main/java/duke/components/Parser.java
+++ src/main/java/duke/components/Parser.java
@@ -4,12 +4,13 @@ import java.time.LocalDate;
 
 public class Parser {
 
-    boolean isList,isTask,isDelete,isBye,isDone,isValid;
+    boolean isList,isTask,isDelete,isBye,isDone,isValid,isFind;
     private int doneTaskNum, deleteTaskNum;
-    private String description, time;
+    private String description, time, findTask;
     private LocalDate date;
 
     public Parser(){
+        isFind=false;
         isList=false;
         isTask=false;
         isDelete=false;
@@ -41,6 +42,10 @@ public class Parser {
         return deleteTaskNum;
     }
 
+    public String getFindTask() {
+        return findTask;
+    }
+
     public void parse(String input){
         if (input.equals("list")) {
 
@@ -50,6 +55,10 @@ public class Parser {
 
             isDone = true;
             doneTaskNum = Character.getNumericValue(input.charAt(5))-1;
+        }else if(input.startsWith("find")){
+
+            isFind = true;
+            findTask = input.substring(5);
 
         }else if(input.startsWith("todo")){
 
