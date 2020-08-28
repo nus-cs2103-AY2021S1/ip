810b766674a3d081aa4bf1d13c912a90ac131ab9
davidcaiqifan <e0406982@u.nus.edu>
2020-08-28 13:14:00 +0800

    Add junit tests

---
+++ src/test/java/DogeTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class DogeTest {
+
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void uiTest() {
+        Ui ui = new Ui();
+        String testString = "--------------------------------------" + "\n"
+                + "Added to list : " + "printable message" + "\n"
+                + "--------------------------------------";
+    }
+
+    @Test
+    public void taskTest() {
+        Task task = new Task("Cycling", TaskType.E);
+        task.markAsDone();
+        assertEquals("\u2713", task.getTaskStatusIcon());
+    }
+
+    @Test
+    public void taskListTest() {
+        Task task = new Task("Cycling", TaskType.E);
+        TaskList.addToList(task);
+        assertEquals(task, TaskList.toDoList.get(0));
+    }
+
+    @Test
+    public void  parserTest() {
+        boolean isAddCommand = false;
+        try {
+            Command addCommand = Parser.manage("todo feed doge");
+            isAddCommand = addCommand instanceof AddCommand;
+        }
+        catch (DukeException e) {
+        }
+        assertEquals(true, isAddCommand);
+    }
+}
