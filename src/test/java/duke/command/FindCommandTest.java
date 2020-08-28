cb67d427b9595df86eda7923848326e0df73fb44
lll-jy <E0376997@u.nus.edu>
2020-08-28 10:56:36 +0800

    Replace '__' with '_' in test method name

--- src/test/java/duke/command/FindCommandTest.java
+++ src/test/java/duke/command/FindCommandTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class FindCommandTest {
     @Test
-    public void isExit__alwaysFalse() {
+    public void isExit_alwaysFalse() {
         assertFalse(new FindCommand("find this").isExit());
         assertFalse(new FindCommand("find ").isExit());
     }
