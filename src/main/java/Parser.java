238868ed3c662fcf7846dde0dd1b76b124f3111b
gohyeeloon <goh.yeeloon@gmail.com>
2020-08-28 20:28:11 +0800

    change if else to switch in parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -7,27 +7,26 @@ public class Parser {
         String firstWord = input.split(" ")[0];
 
         try {
-            if (firstWord.equals("list")) {
-                return tasks.returnList();
-            } else if (firstWord.equals("done")) {
-                int i = Integer.valueOf(input.substring(5));
-                return (tasks.done(i));
-            } else if (firstWord.equals("delete")) {
-                int i = Integer.valueOf(input.substring(7));
-                return tasks.delete(i);
-            } else if (firstWord.equals("todo")) {
-                return tasks.add(new ToDo(input));
-            } else if (firstWord.equals("deadline")) {
-                return tasks.add(new Deadline(input));
-            } else if (firstWord.equals("event")) {
-                return tasks.add(new Event(input));
-            } else if (firstWord.equals("find")) {
-                return tasks.find(input.substring(5));
-            } else {
-                throw new DukeException("oops! im sorry, but i do not know what that means :-(");
+            switch(firstWord) {
+                case "list":
+                    return tasks.returnList();
+                case "done":
+                    int i = Integer.parseInt(input.substring(5));
+                    return (tasks.done(i));
+                case "delete":
+                    int j = Integer.parseInt(input.substring(7));
+                    return tasks.delete(j);
+                case "todo":
+                    return tasks.add(new ToDo(input));
+                case "deadline":
+                    return tasks.add(new Deadline(input));
+                case "event":
+                    return tasks.add(new Event(input));
+                case "find":
+                    return tasks.find(input.substring(5));
+                default:
+                    throw new DukeException("oops! im sorry, but i do not know what that means :-(");
             }
-        } catch (EmptyDescriptionException e) {
-            return e.getMessage();
         } catch (DukeException e) {
             return e.getMessage();
         }
