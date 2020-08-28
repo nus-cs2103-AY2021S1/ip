84b758d9b1e4334bb1aa38e0d1ee4e8b81d74b88
lll-jy <E0376997@u.nus.edu>
2020-08-28 11:07:26 +0800

    Replace grouped import at the top of ungrouped and replace .* import with single class import

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -1,12 +1,12 @@
 package duke.task;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class EventTest {
     @Test
