8469f87ee71a659fa05296ccdf94e5f099f0c35f
lll-jy <E0376997@u.nus.edu>
2020-08-28 20:38:55 +0800

    Write printList method for FxmlUi

---
+++ src/main/java/duke/component/FxmlUi.java
@@ -0,0 +1,32 @@
+package duke.component;
+
+import duke.task.Task;
+
+import java.util.function.Predicate;
+
+public class FxmlUi implements Ui {
+    @Override
+    public void print(String str) { }
+
+    @Override
+    public void output(String message) { }
+
+    @Override
+    public String readInput() {
+        return null;
+    }
+
+    @Override
+    public String greeting() {
+        return "Hello! I'm Duke\nWhat can I do for you?";
+    }
+
+    @Override
+    public void close() { }
+
+    @Override
+    public String printList(TaskList list, Predicate<Task> predicate, String note) {
+        String res = "Here are the tasks " + note + "in your list:\n\n";
+        return list.print(res, predicate);
+    }
+}
