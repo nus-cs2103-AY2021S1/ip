b5cc47b334af03fa9f6d0232b9736a12148c322e
Jerry Ho <jerryho@u.nus.edu>
2020-08-29 13:58:33 +0800

    Add LocalDate support to Deadline.java and Duke.java
    - Modify Deadline.java to store dates as a LocalDate object and time as a String.
    - Update Deadline handling in Duke.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate date;
+    protected String time;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDate by, String time) {
         super(description);
-        this.by = by;
+        this.date = by;
+        this.time = time;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        String dateOutput = date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        return "[D]" + super.toString() + " (by: " + dateOutput + " " + time + ")";
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,7 +8,7 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? " ✓" : "✘"); //return tick or X symbols
+        return (isDone ? "✓" : "✘"); //return tick or X symbols
     }
 
     public void markAsDone() {
