db34e738c9a81b5f0f382d6972dbaa4535362765
JinHao-L <limjinhao@gmail.com>
2020-08-29 14:31:12 +0800

    Build basic layout for GUI

--- src/main/java/alice/Alice.java
+++ src/main/java/alice/Alice.java
@@ -8,13 +8,28 @@ import alice.task.TaskList;
 import alice.ui.Ui;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 /**
  * Runs the ALICE application.
  */
 public class Alice extends Application {
+    // Properties for GUI
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    // Properties for Alice functionality
     private final TaskList tasks;
     private final StorageFile storageFile;
     private final Ui ui;
@@ -86,20 +101,57 @@ public class Alice extends Application {
         }
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        // Create new Label control
-        Label helloWorld = new Label("Hello World!");
-        // Setting the scene to be our Label
-        Scene primaryScene = new Scene(helloWorld);
-
-        // Setting the stage to show our screen
-        primaryStage.setScene(primaryScene);
-        // Render the stage
-        primaryStage.show();
-    }
-
     public static void main(String[] args) {
         new Alice("data/tasks.txt").run();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // Setting up of required components
+
+        // Container for content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        // Bottom bar of chat scroll container.
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        // Formatting the window to look as expected
+        stage.setTitle("Alice");
+        stage.setResizable(true);
+        stage.setMinWidth(400);
+        stage.setMinHeight(600);
+
+        mainLayout.setPrefSize(400, 600);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325);
+        sendButton.setPrefWidth(55);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 }
