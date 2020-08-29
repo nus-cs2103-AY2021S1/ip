a6350ee96b7d641524a8b095a282646c1d832d44
Bennett Clement <benclmnt.dev@gmail.com>
2020-08-29 09:51:07 +0800

    Set up JavaFX

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 import duke.Duke;
+import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        Duke chatbot = new Duke();
-        chatbot.start();
+//        Duke chatbot = new Duke();
+//        chatbot.start();
+        Application.launch(Duke.class, args);
     }
 }
--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,20 +5,37 @@ import exception.InvalidUsageException;
 import exception.StorageException;
 import exception.UnknownCommandException;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * Main class for the Duke application
  */
-public class Duke {
-    // current task list 
-    TaskList taskList;
+public class Duke extends Application {
+    // current task list
+    private TaskList taskList;
     // storage file
-    Storage storage;
+    private Storage storage;
     // text ui interface
-    Ui ui;
+    private Ui ui;
 
+    /**
+     * Constructor for Duke application
+     */
     public Duke() {
         ui = new Ui();
         storage = new Storage();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label helloWorld = new Label("Hello World!");
+        Scene scene = new Scene(helloWorld);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     /**
