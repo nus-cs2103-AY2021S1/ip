adcbea018e50d2a49356e4ac7d270c72a2b1a9ba
leeyorktat <leeyorktat@gmail.com>
2020-08-29 18:55:53 +0800

    add Java Docs for main classes and Task class and subclasses

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -1,26 +1,48 @@
 package duke;
 
+/**
+ * Represents a task given by the user, containing the name and the completion status of the task.
+ */
 public abstract class Task {
     protected String description;
     protected boolean isDone;
-
+    
+    /**
+     * Creates a new task with the specified description.
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Gets the string of the completion status of the task.
+     * @return A string of the status icon.
+     */
     public String getStatusIcon() {
         return (isDone ? "✓" : "✘");
     }
 
+    /**
+     * Changes the completion status of the task to be done.
+     */
     public void completeTask() {
         this.isDone = true;
     }
 
+    /**
+     * Returns a string representation of the Task.
+     * @return A String containing the description and completion status of the task.
+     */
     @Override
     public String toString() {
         return String.format("[%s] %s", getStatusIcon(), description);
     }
-    
+
+    /**
+     * Returns a string representation of the Task stored in the storage.
+     * @return A String representing the code of the task stored in the storage.
+     */
     public abstract String encode();
 }
--- src/main/java/duke/ToDo.java
+++ src/main/java/duke/ToDo.java
@@ -1,16 +1,31 @@
 package duke;
 
+/**
+ * The ToDo class contains the name and completion status of the task.
+ */
 public class ToDo extends Task {
 
+    /**
+     * Constructor of a ToDo object.
+     * @param description The description of the task.
+     */
     public ToDo(String description) {
         super(description);
     }
-    
+
+    /**
+     * Returns a string representation of a ToDo object to be stored in the storage.
+     * @return A String representing the code of the task stored in the storage.
+     */
     @Override
     public String encode() {
         return String.format("T | %s | %s", getStatusIcon(), description);
     }
 
+    /**
+     * Returns a string representation of a ToDo object.
+     * @return A String containing the description and completion status of the ToDo object.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
