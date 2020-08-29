b8325f30b94a2efa52cebd553a41746ecfe592d3
sebastiantoh <sebas.tsj.98@gmail.com>
2020-08-29 13:55:44 +0800

    Clip image into circle

--- src/main/java/duke/ui/DialogBox.java
+++ src/main/java/duke/ui/DialogBox.java
@@ -11,8 +11,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -27,7 +28,7 @@ public class DialogBox extends HBox {
     @FXML
     private Label dialog;
     @FXML
-    private ImageView displayPicture;
+    private Circle displayPicture;
 
     private DialogBox(String text, Image img) {
         try {
@@ -41,7 +42,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        displayPicture.setImage(img);
+        displayPicture.setFill(new ImagePattern(img));
     }
 
     private void setDialogColour(String colour) {
