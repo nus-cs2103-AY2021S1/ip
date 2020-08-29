9f821c07b06771a21e5353242dc573fe94fb7672
e0316059 <e0316059@u.nus.edu>
2020-08-29 14:52:24 +0800

    complete level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        Scanner scanner = new Scanner(System.in);
+
+        greet();
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (!command.equals("bye")) {
+                echo(command);
+            } else {
+                exit();
+            }
+        }
+    }
+
+    private static void greet() {
+        System.out.println("Hello! I'm Duke\n What can I do for you?");
+    }
+
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void echo(String command) {
+        System.out.println("" + command);
     }
 }
