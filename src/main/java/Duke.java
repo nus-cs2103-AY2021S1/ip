af779bb59b3f3c7f8a56f25543bd2d5c38cf5909
Marcon2509 <marcus.duigan@gmail.com>
2020-08-29 19:48:02 +0800

    Added JavaFX. Added launcher class and makde duke class extend applicaiton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,26 @@
 import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * Main class which cretaes the application Duke that creates a checklist for tasks to do
  */
-public class Duke {
+public class Duke extends Application{
 
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hi if you are reviewing my code uh this the lvl10 things so not sure how it runs with it try looking at the tag with A-Gradle"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+        runDuke();
+    }
 
     public static void main(String[] args) {
 
