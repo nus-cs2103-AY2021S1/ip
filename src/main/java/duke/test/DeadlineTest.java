38c07d18f0017961eccad63f1a8a8841099842c5
sudogene <andywubby@gmail.com>
2020-08-29 12:54:14 +0800

    add gradle

--- src/main/java/duke/test/DeadlineTest.java
+++ src/main/java/duke/test/DeadlineTest.java
@@ -1,16 +1,16 @@
 package duke.test;
 
-import duke.task.Event;
+import duke.task.Deadline;
 import duke.util.DukeException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EventTest {
+class DeadlineTest {
 
     @org.junit.jupiter.api.Test
     void constructor_missingTime_success() {
         try {
-            new Event("unit testing", "2020-01-01");
+            new Deadline("unit testing", "2020-01-01");
         } catch (DukeException e) {
             fail("Should create task successfully, time is Optional.");
         }
