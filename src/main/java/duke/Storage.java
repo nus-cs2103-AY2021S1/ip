771150b233fe3c379191df38b830ca72069e8a01
Tan Zhuo Yao <e0406711@u.nus.edu>
2020-08-29 13:15:29 +0800

    Modify Code style convention

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -1,6 +1,11 @@
 package duke;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
