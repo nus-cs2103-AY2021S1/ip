84c4104423dbeaf645e616b7b6e446afe1c7e49a
Tan Chia Qian <tanchiaqian@Tans-MacBook-Pro.local>
2020-08-29 22:12:38 +0800

    A-Gradlea

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -3,7 +3,7 @@
  */
 public abstract class Command {
     
-    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws DoneException, DeleteException, DeadlineException, EventException, TodoException;
+    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws DoneException, DeleteException, DeadlineException, EventException, TodoException, FindException;
     public abstract boolean isExit();
     
 }
--- src/main/java/FindException.java
+++ src/main/java/FindException.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
-public class FindException {
+/**
+ * A subclass of Exception.
+ * Show error for find command.
+ */
+public class FindException extends Exception {
+    
+    public FindException(String msg) {
+        super(msg);
+    }
+    
 }
