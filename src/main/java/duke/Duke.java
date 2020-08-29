b131b2e9fb5e3875b90f370cbe141c43773d0a29
Licheng-Wu <wlc.licheng@gmail.com>
2020-08-30 00:24:54 +0800

    Add javaFX as shown in tutorial
    Shows Hello World as the label in GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,9 +1,14 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * Encapsulates the greenfield project Duke that manages user tasks.
  */
-public class Duke {
+public class Duke extends Application {
     private Ui ui;
     private Parser parser;
     private TaskManager taskManager;
@@ -23,6 +28,15 @@ public class Duke {
     public void run() {
         this.ui.printWelcomeMessage();
         this.taskManager.manage();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     /**
