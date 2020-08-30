5854e5bea1ed1a6b43797866bcb06e2117b8a8b1
Junhua <wenjh1998@gmail.com>
2020-08-31 00:04:59 +0800

    Add Unit test for DoneCommands

---
+++ src/test/java/ultron/commands/DoneCommandTest.java
@@ -0,0 +1,40 @@
+package ultron.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import ultron.Storage;
+import ultron.TaskList;
+import ultron.UI;
+import ultron.exceptions.UltronException;
+
+public class DoneCommandTest {
+    /**
+     * Assert if Done Command isExit is false.
+     */
+    @Test
+    public void isExitTest() {
+        DoneCommand command = new DoneCommand("");
+        assertFalse(command.isExit());
+    }
+
+    /**
+     * Check if the command can be executed with invalid arguments.
+     */
+    @Test
+    public void invalidArgumentTest() {
+        DoneCommand command = new DoneCommand("not suppose to be here");
+        TaskList taskList = new TaskList(new ArrayList<>());
+        UI ui = new UI();
+        Storage storage = new Storage("test.txt");
+        try {
+            command.execute(taskList, ui, storage);
+            assert false;
+        } catch (UltronException e) {
+            assert true;
+        }
+    }
+}
