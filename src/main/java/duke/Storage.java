ee1c6c4755c14a00aaaa3e47f56c98189b5edadf
Kwek Min Yih <minyihkwek@gmail.com>
2020-08-31 01:14:30 +0800

    Add JavaDocs for Storage class

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -8,14 +8,25 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * Storage is a class that handles the interactions between the application and local storage.
+ */
 public class Storage {
     private final String filePath = "data/tasks.txt";
     private File storage;
 
+    /**
+     * Creates a Storage object.
+     */
     public Storage() {
         this.storage = new File(filePath);
     }
 
+    /**
+     * Reads the lines of text in local storage and compiles them into an ArrayList of strings.
+     * @return An ArrayList containing String representations of all the Tasks in local storage.
+     * @throws StorageException if the storage cannot be accessed or read.
+     */
     public ArrayList<String> readTaskStorage() throws StorageException {
         ArrayList<String> existingTasks = new ArrayList<>();
         try {
@@ -41,6 +52,11 @@ public class Storage {
         return existingTasks;
     }
 
+    /**
+     * Appends a string representation of a Task to local storage.
+     * @param taskString The String representation of a Task.
+     * @throws StorageException if the storage cannot be accessed or read.
+     */
     public void appendTaskStorage(String taskString) throws StorageException {
         try {
             FileWriter appender = new FileWriter(filePath, true);
@@ -51,6 +67,11 @@ public class Storage {
         }
     }
 
+    /**
+     * Writes the string representations of all Tasks to local storage.
+     * @param taskString The String representation of all tasks in the TaskList.
+     * @throws StorageException if the storage cannot be accessed or read.
+     */
     public void writeTaskStorage(String taskString) throws StorageException {
         try {
             FileWriter rewriter = new FileWriter(filePath);
