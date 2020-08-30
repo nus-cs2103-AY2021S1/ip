8cc05e03461245ab0eba40127e4870277df4f9fe
e0316059 <e0316059@u.nus.edu>
2020-08-31 03:28:47 +0800

    complete level 6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -50,6 +50,12 @@ public class Duke {
         System.out.println("Nice! I've marked this task as done:\n" + task.toString());
     }
 
+    private static void delete(int index) {
+        System.out.println("Noted. I've removed this task:\n" + tasks.get(index-1).toString());
+        tasks.remove(index-1);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+    }
+
     private static void printList() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
@@ -72,6 +78,8 @@ public class Duke {
         } else if (command.split(" ")[0].equals("done")) {
             int index = Integer.parseInt(command.split(" ")[1]);
             done(tasks.get(index - 1));
+        } else if (command.split(" ")[0].equals("delete")) {
+            delete(Integer.parseInt(command.split(" ")[1]));
         } else if (command.split(" ")[0].equals("todo")) {
             String taskcommand = command.replace("todo", "");
             if (!taskcommand.equals("")) {
