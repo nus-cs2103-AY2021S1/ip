1b516bf83c48689bfc65c1afb0cb4f3dd2a64557
Wincent <wincenttsjoi@gmail.com>
2020-08-30 16:21:54 +0800

    Remove extra indentation for import statements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
 
-        import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Scene;
-        import javafx.scene.layout.AnchorPane;
-        import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * A GUI for Duke using FXML.
