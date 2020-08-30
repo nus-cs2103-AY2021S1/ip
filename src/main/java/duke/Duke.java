41628c13794f95c7416fbe13b824f8bee0302464
Ethan So <ethan.so.wai.yein@gmail.com>
2020-08-30 15:06:06 +0800

    Done GUI intro

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,5 +1,12 @@
 package duke;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
@@ -7,11 +14,20 @@ import java.util.Scanner;
 /**
  * Represents a task manager bot named Duke.
  */
-public class Duke {
+public class Duke{
 
     private duke.Storage storage;
     private duke.TaskList tasks;
     private Ui ui;
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/ethan.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/anotherEthan.png"));
 
     public Duke(String filepath) throws IOException {
         ui = new Ui();
@@ -22,6 +38,10 @@ public class Duke {
             ui.showLoadingError();
             tasks = new duke.TaskList();
         }
+    }
+
+    public Duke() {
+
     }
 
     public static void main(String[] args) throws duke.DukeException, IOException {
@@ -40,5 +60,9 @@ public class Duke {
             input = sc.nextLine();
         }
         ui.printExit();
+    }
+
+    String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
