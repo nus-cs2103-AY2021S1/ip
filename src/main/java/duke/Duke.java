4166c7b4523c7e6cad797fd9fd5f92f41d831609
erinmayg <erinmaygunawan@gmail.com>
2020-08-30 22:57:44 +0800

    GUI Packaging

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -65,7 +65,7 @@ public class Duke {
      * You should have your own function to generate a response to user input.
      * Replace this stub with your completed method.
      */
-    String getResponse(String input) {
+    public String getResponse(String input) {
         try {
             Command c = Parser.parse(input);
             return c.execute(tasks, ui, storage);
