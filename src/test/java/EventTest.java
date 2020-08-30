8324c8e3945250e73c6230a06d1d0a81fc99cd5d
Lye Yi Xian <lyeyx999@hotmail.com>
2020-08-30 15:52:04 +0800

    Add import statement to test class

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,3 +1,4 @@
+import duke.task.Event;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
--- src/test/java/ToDoTest.java
+++ src/test/java/ToDoTest.java
@@ -1,3 +1,4 @@
+import duke.task.ToDo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
