6c14986e3a114f516191a4f1d4a51770284fda77
sogggy <songyu98@gmail.com>
2020-08-30 16:44:23 +0800

    Add GUI interface

---
+++ src/main/java/com/duke/Launcher.java
@@ -0,0 +1,14 @@
+package com.duke;
+
+import com.duke.gui.Main;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
