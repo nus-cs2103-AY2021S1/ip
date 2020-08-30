e43880da077cc9318ea73cdf22aa333bd6a8a7a3
ngzhenteng <ngzhenteng@gmail.com>
2020-08-30 17:59:11 +0800

    A-Gradle
    Add gradle to project

--- src/test/java/duke/TaskListTest.java
+++ src/test/java/duke/TaskListTest.java
@@ -2,11 +2,12 @@ package duke;
 
 import main.java.duke.Deadline;
 import main.java.duke.TaskList;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class TaskListTest {
     @Test
