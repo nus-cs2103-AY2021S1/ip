3ed5b5fcedd531c82bbfc450eebbe8b5961343e1
Lee Ming De <leemingde@gmail.com>
2020-08-30 18:07:48 +0800

    Create simple GUI using JavaFX
    Added code for JavaFX to run a GUI that displays hello world

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,15 +2,25 @@ package duke;
 
 import duke.command.Command;
 import duke.exception.DukeException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * Represents a Duke chat bot.
  */
-public class Duke {
+public class Duke extends Application {
 
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+
+    /**
+     * Creates a new instance of Duke object.
+     */
+    public Duke() {
+    }
 
     /**
      * Creates a new instance of Duke object.
@@ -55,5 +65,19 @@ public class Duke {
      */
     public static void main(String[] args) {
         new Duke("data/tasks.txt").run();
+    }
+
+    /**
+     * Renders the stage.
+     *
+     * @param stage Primary stage that JavaFX provides.
+     */
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
