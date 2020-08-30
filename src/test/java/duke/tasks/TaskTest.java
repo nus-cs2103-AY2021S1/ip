c3cc8f89db364dc73bba031fd825555ac87d8e02
Melanie <melanienps@gmail.com>
2020-08-30 20:01:37 +0800

    Add dependency main to junit tests

--- src/test/java/duke/tasks/TaskTest.java
+++ src/test/java/duke/tasks/TaskTest.java
@@ -1,6 +1,6 @@
 package duke.tasks;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskTest {
