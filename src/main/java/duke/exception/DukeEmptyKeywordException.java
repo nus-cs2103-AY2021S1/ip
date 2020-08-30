d94923bf585fc0dfd415ce8c20270fd0a0369869
MarcusTw <60913700+MarcusTw@users.noreply.github.com>
2020-08-30 11:10:51 +0800

    Coding Branch doc

--- src/main/java/duke/exception/DukeEmptyKeywordException.java
+++ src/main/java/duke/exception/DukeEmptyKeywordException.java
@@ -1,5 +1,9 @@
 package duke.exception;
 
+/**
+ * Represents an exception thrown when keyword for search is left empty.
+ * Keyword is required for searching tasks.
+ */
 public class DukeEmptyKeywordException extends DukeException {
     /**
      * Class constructor.
