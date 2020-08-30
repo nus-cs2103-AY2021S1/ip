44bedc43d55662f64f7b35ba648fc85314b9e4d8
peter-yeh <yehyuchun2@gmail.com>
2020-08-30 20:46:22 +0800

    Add feature to display output to window from ui class
    System.out.println methods used in Ui class is now replaced by
    window.showsToDialog method to be displayed to window.

--- src/main/java/duke/utils/Ui.java
+++ src/main/java/duke/utils/Ui.java
@@ -2,7 +2,9 @@ package duke.utils;
 
 import java.util.List;
 
+import duke.Main;
 import duke.task.Task;
+
 
 /**
  * Handles the printing of output to the console
@@ -20,70 +22,86 @@ public class Ui {
      * @param s string to output to console
      */
     public void displayThis(String s) {
-        displayLines();
+        //        displayLines();
         displayString(s);
-        displayLines();
+        //        displayLines();
     }
 
 
-    /**
-     * Displays the contents to console.
-     *
-     * @param toDoList the Tasks to display to console
-     */
-    public void displayList(List<Task> toDoList) {
-        displayLines();
-        displayString("Here are your current tasks:");
-        for (int i = 0; i < toDoList.size(); i++) {
-            displayString((i + 1) + ". " + toDoList.get(i));
-        }
-        displayLines();
-    }
+    //    /**
+    //     * Displays the contents to console.
+    //     *
+    //     * @param toDoList the Tasks to display to console
+    //     */
+    //    public void displayList(List<Task> toDoList) {
+    ////        displayLines();
+    //        String s =
+    //        displayString("Here are your current tasks:");
+    //        for (int i = 0; i < toDoList.size(); i++) {
+    //            displayString((i + 1) + ". " + toDoList.get(i));
+    //        }
+    ////        displayLines();
+    //    }
 
 
     /**
      * Welcomes the user with a welcome message
      */
     public void displayList(List<Task> toDoList, String s) {
-        displayLines();
-        displayString(s);
+        //        displayLines();
+
+        StringBuilder sBuilder = new StringBuilder(s).append('\n');
+
         for (int i = 0; i < toDoList.size(); i++) {
-            displayString((i + 1) + ". " + toDoList.get(i));
+            sBuilder.append(i + 1).append(". ").append(toDoList.get(i)).append('\n');
         }
-        displayLines();
+
+        displayString(sBuilder.toString());
+        //        displayLines();
     }
 
     public void welcome() {
-        String logo = "     ____        _        \n"
-                + "    |  _ \\ _   _| | _____ \n"
-                + "    | | | | | | | |/ / _ \\\n"
-                + "    | |_| | |_| |   <  __/\n"
-                + "    |____/ \\__,_|_|\\_\\___|";
+        String logo = "____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|";
 
-        System.out.println(logo);
+        //        System.out.println(`logo);
+        //        System.out.println(""
+        //                + "\n    Hello! I'm duke.Duke, your Windows duke.task.Task Manager"
+        //                + "\n    Valid command:"
+        //                + "\n    - list"
+        //                + "\n    - done <num>"
+        //                + "\n    - todo <task>"
+        //                + "\n    - deadline <task> /by <yyyy-mm-dd>"
+        //                + "\n    - event <task> /at <yyyy-mm-dd>"
+        //                + "\n    - find <keyword>\n");
 
-        System.out.println(""
-                + "\n    Hello! I'm duke.Duke, your Windows duke.task.Task Manager"
-                + "\n    Valid command:"
-                + "\n    - list"
-                + "\n    - done <num>"
-                + "\n    - todo <task>"
-                + "\n    - deadline <task> /by <yyyy-mm-dd>"
-                + "\n    - event <task> /at <yyyy-mm-dd>"
-                + "\n    - find <keyword>\n");
+        displayString(logo);
+        displayString(""
+                + "\nHello! I'm duke.Duke, your Windows duke.task.Task Manager"
+                + "\nValid command:"
+                + "\n- list"
+                + "\n- done <num>"
+                + "\n- todo <task>"
+                + "\n- deadline <task> /by <yyyy-mm-dd>"
+                + "\n- event <task> /at <yyyy-mm-dd>"
+                + "\n- find <keyword>\n");
 
 
     }
 
 
     private void displayString(String s) {
-        System.out.println("    " + s);
+        //        System.out.println("    " + s);
+        Main.window.showsToDialog(s);
     }
-
-
-    private void displayLines() {
-        System.out.println("    -------------------------------------------------");
-    }
+    //
+    //
+    //    private void displayLines() {
+    //        System.out.println("    -------------------------------------------------");
+    //    }
 
 
 }
