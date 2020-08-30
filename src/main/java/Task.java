7a01956aa4a1fa17f17cf51c25098f622f401845
Ringo1225 <caoqin@Caos-MacBook-Pro.local>
2020-08-30 15:36:01 +0800

    Added a Task class, but did not merge it with the main class Duke.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+}
