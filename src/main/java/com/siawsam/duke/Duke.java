15cc8da28d3429d94f3f8838ef8de267c89ed3cd
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-08-30 20:34:06 +0800

    Set up javafx

--- src/main/java/com/siawsam/duke/Duke.java
+++ src/main/java/com/siawsam/duke/Duke.java
@@ -2,18 +2,29 @@ package com.siawsam.duke;
 
 import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * A Duke program.
  */
-public class Duke {
+public class Duke extends Application {
     /**
      * The file path for saving tasks.
      */
     static final String FILE_PATH = "savedTasks.txt";
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+
+        primaryStage.show();
+    }
 
     //CHECKSTYLE:OFF
     public static void main(String[] args) {
         //CHECKSTYLE:ON
+        Application.launch(args);
         Ui.showWelcomeMessage();
 
         Storage storage = new Storage(FILE_PATH);
