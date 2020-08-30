1bd26d06742d420e0b59d13346024c7db372f7e6
xinyee20 <codelearner19@gmail.com>
2020-08-30 16:01:00 +0800

    Add Parser.java for OOP

---
+++ src/main/java/Parser.java
@@ -0,0 +1,88 @@
+package main.java;
+
+public class Parser {
+    TaskList list;
+    private static String lastGreeting = "bye";
+
+    public Parser(TaskList list) {
+        this.list = list;
+    }
+
+    public void parse(String message) {
+        // check when to end the bot
+        if (message.equals(lastGreeting)) {
+            // prints out exit
+            System.out.println(new Greet(message));
+            return;
+        }
+        // take note of keyword "list" to display the lists
+        if (message.equals("list")) {
+            // reads list
+            list.readList();
+        }
+        // take note of keyword "done" to update task
+        else if (message.contains("done")) {
+            int taskNum = Integer.parseInt(message.substring(5));
+            list.setTaskDone(taskNum);
+        }
+        // take note of keyword "delete" to remove task from list
+        else if (message.contains("delete")) {
+            int taskNum = Integer.parseInt(message.substring(7));
+            list.removeTask(taskNum);
+        }
+        // take note of keyword "to-do" to add normal task
+        else if (message.contains("todo")) {
+            try {
+                String activity = message.substring(5);
+                ToDoTask newTask = new ToDoTask(activity, TaskSymbol.TODO);
+                list.addToList(newTask);
+            } catch (Exception e) {
+                WillyException error = new WillyException("Hmmm what would you like to do?");
+                System.out.println(error);
+            }
+
+        }
+
+        // deadline input format: dd/MM/yyyy HHmm, output format: dd MMM yyyy HH:mm a
+        // take note of keyword "deadline" to add task with deadline
+        else if (message.contains("deadline")) {
+            try {
+                int separatorIndex = message.indexOf("/");
+                String activity = message.substring(9, separatorIndex - 1);
+                String deadline = message.substring(separatorIndex + 4);
+                DeadlineTask newTask = new DeadlineTask(deadline, activity, TaskSymbol.DEADLINE);
+                list.addToList(newTask);
+
+            } catch (Exception e){
+                WillyException error = new WillyException("Hmmm are you missing description/deadline of the task? \n\tCheck and try again?");
+                System.out.println(error);
+            }
+        }
+
+        // deadline input format: dd/MM/yyyy HH:mm, output format: dd MMM yyyy HH:mm a
+        // take note of keyword "event" to add task with duration
+        else if (message.contains("event")) {
+            try {
+                int separatorIndex = message.indexOf("/");
+                String activity = message.substring(6, separatorIndex - 1);
+                String duration = message.substring(separatorIndex + 4);
+                EventsTask newTask = new EventsTask(duration, activity, TaskSymbol.EVENT);
+                list.addToList(newTask);
+            } catch (Exception e) {
+                WillyException error = new WillyException("Hmmm are you missing the description/timing of event? \n\tCheck and try again?");
+                System.out.println(error);
+            }
+
+        }
+        else if (message.contains("find")) {
+            String keyword = message.substring(5);
+            list.findTask(keyword);
+        }
+
+        // else is nonsense which will produce error
+        else {
+            WillyException error = new WillyException("Hmmm sorry I'm not sure what you are saying, try something else?:(");
+            System.out.println(error);
+        }
+    }
+}
--- src/main/java/Willy.java
+++ src/main/java/Willy.java
@@ -11,7 +11,6 @@ public class Willy {
 
     private static TaskStore storage;
     private String message;
-    private static String lastGreeting = "bye";
     static String style = "\t________________________________________________________________\n";
     static String logo = "__       ____       __\n"
             + "\\  \\    /    \\    /  /\n"
@@ -31,86 +30,11 @@ public class Willy {
         storage.createFile();
         ArrayList<Task> listOfTask  = storage.retrieveStorage();
         TaskList list = new TaskList(listOfTask, storage);
-
+        Parser parser = new Parser(list);
 
         while (input.hasNext()) {
             String message = input.nextLine();
-
-            // check when to end the bot
-            if (message.equals(lastGreeting)) {
-                // prints out exit
-                System.out.println(new Greet(message));
-                break;
-            }
-            // take note of keyword "list" to display the lists
-            if (message.equals("list")) {
-                // reads list
-                list.readList();
-            }
-            // take note of keyword "done" to update task
-            else if (message.contains("done")) {
-                int taskNum = Integer.parseInt(message.substring(5));
-                list.setTaskDone(taskNum);
-            }
-            // take note of keyword "delete" to remove task from list
-            else if (message.contains("delete")) {
-                int taskNum = Integer.parseInt(message.substring(7));
-                list.removeTask(taskNum);
-            }
-            // take note of keyword "to-do" to add normal task
-            else if (message.contains("todo")) {
-                try {
-                    String activity = message.substring(5);
-                    ToDoTask newTask = new ToDoTask(activity, TaskSymbol.TODO);
-                    list.addToList(newTask);
-                } catch (Exception e) {
-                    WillyException error = new WillyException("Hmmm what would you like to do?");
-                    System.out.println(error);
-                }
-
-            }
-
-            // deadline input format: dd/MM/yyyy HHmm, output format: dd MMM yyyy HH:mm a
-            // take note of keyword "deadline" to add task with deadline
-            else if (message.contains("deadline")) {
-                try {
-                    int separatorIndex = message.indexOf("/");
-                    String activity = message.substring(9, separatorIndex - 1);
-                    String deadline = message.substring(separatorIndex + 4);
-                    DeadlineTask newTask = new DeadlineTask(deadline, activity, TaskSymbol.DEADLINE);
-                    list.addToList(newTask);
-
-                } catch (Exception e){
-                    WillyException error = new WillyException("Hmmm are you missing description/deadline of the task? \n\tCheck and try again?");
-                    System.out.println(error);
-                }
-            }
-
-            // deadline input format: dd/MM/yyyy HH:mm, output format: dd MMM yyyy HH:mm a
-            // take note of keyword "event" to add task with duration
-            else if (message.contains("event")) {
-                try {
-                    int separatorIndex = message.indexOf("/");
-                    String activity = message.substring(6, separatorIndex - 1);
-                    String duration = message.substring(separatorIndex + 4);
-                    EventsTask newTask = new EventsTask(duration, activity, TaskSymbol.EVENT);
-                    list.addToList(newTask);
-                } catch (Exception e) {
-                    WillyException error = new WillyException("Hmmm are you missing the description/timing of event? \n\tCheck and try again?");
-                    System.out.println(error);
-                }
-
-            }
-            else if (message.contains("find")) {
-                    String keyword = message.substring(5);
-                    list.findTask(keyword);
-            }
-
-            // else is nonsense which will produce error
-            else {
-                WillyException error = new WillyException("Hmmm sorry I'm not sure what you are saying, try something else?:(");
-                System.out.println(error);
-            }
+            parser.parse(message);
         }
         input.close();
     }
