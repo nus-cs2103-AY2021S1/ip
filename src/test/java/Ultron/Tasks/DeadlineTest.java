388926e18cdf3c03116edcaacca882fe17015d84
Junhua <wenjh1998@gmail.com>
2020-08-31 00:59:18 +0800

    Update Unit test for Deadline

--- src/test/java/Ultron/Tasks/DeadlineTest.java
+++ src/test/java/Ultron/Tasks/DeadlineTest.java
@@ -30,5 +30,16 @@ public class DeadlineTest {
     public void getCommandTestDate() {
         assertEquals("hello /by 02-02-2002 1800", new Deadline("hello", "02-02-2002 1800").getCommand());
     }
+    /**
+     * Test Parsing functionality of deadline class.
+     */
+    @Test
+    public void parseCommandTest() {
+        Task deadline = Deadline.parseCommand("hello /by 02-02-2002 1800");
+        assertEquals("hello", deadline.getMessage());
+        assertEquals("DEADLINE", deadline.getType());
+        assertEquals("\u2718", deadline.getStatusIcon());
+        assertEquals("[D][\u2718] hello (by: 02-02-2002 1800)", deadline.toString());
+    }
 
 }
