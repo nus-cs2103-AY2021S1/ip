24c75d2aefa6bd5c1adfd9ce42061044445e046a
Zhengyang Lin <nauw1010@gmail.com>
2020-08-31 04:18:35 +0800

    Add a GUI to Duke. Use the JavaFX technology to implement the GUI.

---
+++ src/main/java/duke/Main.java
@@ -0,0 +1,37 @@
+package duke;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Duke duke = new Duke("data/duke.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            stage.setTitle("Duke");
+            stage.setResizable(false);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+            if (duke.isExit) {
+                Platform.exit();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
