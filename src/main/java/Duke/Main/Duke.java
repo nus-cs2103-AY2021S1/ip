83bbc222365b6eff7b2f92b0a2eaa9f07af45e1f
KendrewC <kendrewchanys@gmail.com>
2020-08-30 12:40:02 +0800

    gui test

--- src/main/java/Duke/Main/Duke.java
+++ src/main/java/Duke/Main/Duke.java
@@ -2,19 +2,35 @@ package duke.main;
 
 import java.util.Scanner;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+//import javafx.application.Application;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.ScrollPane;
+//import javafx.scene.control.TextField;
+//import javafx.scene.layout.AnchorPane;
+//import javafx.scene.layout.VBox;
+//import javafx.stage.Stage;
+//import javafx.scene.layout.Region;
+//import javafx.scene.control.Label;
+//import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
 
 /**
  * Duke is a chatbot that allows crud operations, and can add three different types of task,
  * namely todo, events, and deadlines
  */
-public class Duke extends Application {
+//public class Duke extends Application {
+public class Duke {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+//    private ScrollPane scrollPane;
+//    private VBox dialogContainer;
+//    private TextField userInput;
+//    private Button sendButton;
+//    private Scene scene;
+//    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+//    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     /**
      * Instantiates a new Duke.
@@ -33,7 +49,7 @@ public class Duke extends Application {
 
     public Duke() {
         this.ui = new Ui();
-        this.storage = new Storage("./data/tasks.txt");
+        this.storage = new Storage("data/tasks.txt");
         try {
             this.tasks = new TaskList(storage);
         } catch (Exception e) {
@@ -62,71 +78,11 @@ public class Duke extends Application {
         new Duke("./data/tasks.txt").run(sc);
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
-
-/**
- * The main class for running Duke, the ChatBot.
- */
-
-//public class Duke extends Application {
-
-//    private final Storage storage;
-//    private final TaskList tasks;
-//    private final Ui ui;
-//
-//    /**
-//     * Initialisation for Duke, the ChatBot.
-//     *
-//     * @param filepath Requires a filepath which will be relative to where Duke was ran, to save and load files from
-//     */
-//    public Duke(String filepath) {
-//        ui = new Ui();
-//        storage = new Storage(filepath);
-//        tasks = storage.loadData(ui);
-//    }
-//
-//    public Duke() {
-//        ui = new Ui();
-//        storage = new Storage("data/tasks.txt");
-//        tasks = storage.loadData(ui);
-//    }
-
-//    /**
-//     * Runs Duke, the ChatBot.
-//     */
-//    public void run() {
-//        ui.showWelcome();
-//        boolean isExit = false;
-//        while (!isExit) {
-//            try {
-//                String fullCommand = ui.readCommand();
-//                Command c = Parser.parse(fullCommand);
-//                c.execute(tasks, ui, storage);
-//                isExit = c.isExit();
-//            } catch (DukeError e) {
-//                ui.showError(e.getMessage());
-//            }
-//        }
-//    }
-
-//    public static void main(String[] args) {
-//        new Duke("data/tasks.txt").run();
-//    }
-//
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-//        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-//
-//        stage.setScene(scene); // Setting the stage to show our screen
-//        stage.show(); // Render the stage.
-//    }
-//}
