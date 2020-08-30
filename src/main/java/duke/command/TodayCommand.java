42d5212b8bd4cc4a5ea063be6a35ba3fc9f1a9b9
Kwek Min Yih <minyihkwek@gmail.com>
2020-08-31 00:23:01 +0800

    Add JavaDocs for TodayCommand class

--- src/main/java/duke/command/TodayCommand.java
+++ src/main/java/duke/command/TodayCommand.java
@@ -4,7 +4,15 @@ import duke.Storage;
 import duke.Ui;
 import duke.task.TaskList;
 
+/**
+ * Represents a Command given by the user to list all Tasks whose date is the current date (today).
+ */
 public class TodayCommand extends Command {
+    /**
+     * Prints all lists in the TaskList, whose date is the current date (today).
+     * @param list A TaskList containing the user's Tasks.
+     * @param storage A Storage object that handles the storage of tasks in local storage, allowing them to persist.
+     */
     @Override
     public void execute(TaskList list, Storage storage) {
         Ui.todayMessage(list.getTaskList());
