0c895b2d78d61be0b968de7be38ca6927e0ceaee
pongzers <you@example.com>
2020-08-31 14:23:45 +0800

    Add new test cases

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,11 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     Task toDo = new ToDo("Eat bread");
     Task event = new Event("Eat bread", "tomorrow");
     Task deadline = new Deadline("Eat bread", "tomorrow");
+    Parser parser = new Parser();
+    TaskList taskList = new TaskList(new ArrayList<Task>());
 
     @Test
     public void ToDoTest(){
@@ -21,4 +25,53 @@ public class DukeTest {
     public void DeadlineTest() {
         assertEquals("Eat bread", deadline.getTask());
     }
+
+    @Test
+    public void ParserDeadlineTest() {
+        assertEquals(Parser.Command.DEADLINE, parser.parse("deadline eat food /by tomorrow"));
+    }
+
+    @Test
+    public void ParserEventTest() {
+        assertEquals(Parser.Command.EVENT, parser.parse("event eat food /at tomorrow night"));
+    }
+
+    @Test
+    public void ParserTodoTest() {
+        assertEquals(Parser.Command.TODO, parser.parse("todo eat food"));
+    }
+
+    @Test
+    public void ParserEmptyTaskTest() {
+        assertEquals(Parser.Command.EMPTY_TASK_EVENT_DEADLINE, parser.parse("event /at tomorrow"));
+        assertEquals(Parser.Command.EMPTY_TASK_EVENT_DEADLINE, parser.parse("event /at "));
+    }
+
+    @Test
+    public void ParserEmptyDateTest() {
+        assertEquals(Parser.Command.EMPTY_DATE, parser.parse("event eat food"));
+        assertEquals(Parser.Command.EMPTY_DATE, parser.parse("event eat food /at"));
+    }
+
+    @Test
+    public void TaskListTest() {
+        taskList.add(event);
+        taskList.add(toDo);
+        taskList.add(deadline);
+        assertEquals(3, taskList.size());
+
+        taskList.set(0, event);
+        assertEquals(3, taskList.size());
+
+        taskList.remove(1);
+        assertEquals(2, taskList.size());
+
+        assertEquals(event, taskList.get(0));
+
+        assertEquals(2, taskList.find("bread").size());
+
+
+    }
+
+
 }
