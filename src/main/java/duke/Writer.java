469126008170dcdd0f3d930921a3cd3f6086680a
Raythx98 <65889977+raythx98@users.noreply.github.com>
2020-08-31 14:48:43 +0800

    update runtest.bat to handle missing directory.
    The classpath specified in the .bat file is incorrect, and program does
    not handle missing directory appropriately.
    
    Updated the classpath of .bat file and handled missing directories.
    
    Let's update code to handle missing directory, and we will delete these directories everytime we start the .bat file.
    
    Refer to this discussion on handling missing directory:
    nus-cs2103-AY2021S1/forum#47
    nus-cs2103-AY2021S1/forum#121

--- src/main/java/duke/Writer.java
+++ src/main/java/duke/Writer.java
@@ -9,11 +9,21 @@ public class Writer {
 
     public static void overwrite(String filepath, String input) {
         try {
+
             FileWriter writer = new FileWriter(new File(filepath), false);
             writer.write(input);
             writer.close();
         } catch (FileNotFoundException e) {
             System.out.println("No such file exists " + filepath);
+            File file = new File("data");
+            //Creating the directory
+            boolean bool = file.mkdir();
+            if (bool) {
+                System.out.println("Directory created successfully");
+            } else {
+                System.out.println("Sorry couldn’t create specified directory");
+            }
+            overwrite(filepath, input);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -26,6 +36,15 @@ public class Writer {
             writer.close();
         } catch (FileNotFoundException e) {
             System.out.println("No such file exists " + filepath);
+            File file = new File("data");
+            //Creating the directory
+            boolean bool = file.mkdir();
+            if (bool) {
+                System.out.println("Directory created successfully");
+            } else {
+                System.out.println("Sorry couldn’t create specified directory");
+            }
+            overwrite(filepath, input);
         } catch (IOException e) {
             e.printStackTrace();
         }
