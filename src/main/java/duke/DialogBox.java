096f637a90a52a5439660a1142d09cf59692aeeb
nicholasyeo <65457870+nicholasyeo@users.noreply.github.com>
2020-09-01 00:02:49 +0800

    Use FXML to clean up code

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -1,47 +1,65 @@
 package duke;
 
+import java.io.IOException;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
 
 public class DialogBox extends HBox {
 
+    @FXML
+    private Label dialog;
+    @FXML
+    private ImageView displayPicture;
+
     /**
      * Constructs a dialog box that stores the text and display picture.
-     * @param label The label that stores the text.
+     * @param text The dialog text to be stored.
      * @param image The image of the display picture.
      */
-    public DialogBox(Label label, Image image) {
+    public DialogBox(String text, Image image) {
         super(20);
-        label.setWrapText(true);
-        label.setPadding(new Insets(10));
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.setText(text);
+
+        // Add padding to text
+        dialog.setPadding(new Insets(8));
 
         // Add background color to text box
         Color color = Color.PINK;
         CornerRadii radii = new CornerRadii(10);
-        label.setBackground(new Background(new BackgroundFill(color, radii, Insets.EMPTY)));
+        dialog.setBackground(new Background(new BackgroundFill(color, radii, Insets.EMPTY)));
 
-        // Clips display picture into a circular view
-        Circle displayPicture = new Circle(40);
-        ImagePattern imagePattern = new ImagePattern(image);
-        displayPicture.setFill(imagePattern);
+        displayPicture.setImage(image);
 
-        this.setAlignment(Pos.TOP_RIGHT);
-
-        // Add padding between each ImageView and its label
-        this.setPadding(new Insets(10));
-        this.getChildren().addAll(label, displayPicture);
+        //        // Clips display picture into a circular view
+        //        Circle displayPicture = new Circle(40);
+        //        ImagePattern imagePattern = new ImagePattern(image);
+        //        displayPicture.setFill(imagePattern);
+        //
+        //        this.setAlignment(Pos.TOP_RIGHT);
     }
 
     /**
@@ -55,23 +73,23 @@ public class DialogBox extends HBox {
     }
 
     /**
-     * Returns a DialogBox object storing the label and the display picture.
-     * @param label The label that stores the text.
+     * Returns a DialogBox object storing the user's input and display picture.
+     * @param input The text representing the user input.
      * @param image The image of the display picture.
      * @return DialogBox object.
      */
-    public static DialogBox getUserDialog(Label label, Image image) {
-        return new DialogBox(label, image);
+    public static DialogBox getUserDialog(String input, Image image) {
+        return new DialogBox(input, image);
     }
 
     /**
-     * Returns a DialogBox object after switching the position of the label and display picture.
-     * @param label The label that stores the text.
+     * Returns a DialogBox object storing Duke's response and display picture.
+     * @param response The text representing Duke's response.
      * @param image The image of the display picture.
      * @return DialogBox object.
      */
-    public static DialogBox getDukeDialog(Label label, Image image) {
-        var db = new DialogBox(label, image);
+    public static DialogBox getDukeDialog(String response, Image image) {
+        var db = new DialogBox(response, image);
         db.flip();
         return db;
     }
