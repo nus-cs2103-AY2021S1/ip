f53294f798435392e2b6447a367bed7b4c024ddd
VaishakAnand <vaishak140498@gmail.com>
2020-08-31 16:57:48 +0800

    Minimise code repeating.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -12,64 +12,67 @@ public class Parser {
     /**
      * Interprets user input text, into understandable commands.
      *
-     * @param command Commands input by user.
+     * @param inputText Commands input by user.
      * @return Command understood by Chatbot.
      */
-    protected Command commandHandler(String command) {
+    protected Command commandHandler(String inputText) {
         try {
-            if (command.split(" ")[0].equals("find")) {
-                String keyword = command.split(" ")[1];
+            String[] splitArr = inputText.split(" ");
+            String command = splitArr[0];
+
+            if (command.equals("find")) {
+                String keyword = splitArr[1];
                 return new Command("find", keyword);
             } else if (command.equals("bye")) {
                 return new Command("bye");
             } else if (command.equals("list")) {
                 return new Command("list");
-            } else if (command.split(" ")[0].equals("done")) {
-                if (command.split(" ").length == 1) {
+            } else if (command.equals("done")) {
+                if (splitArr.length == 1) {
                     throw new UnknownTaskException("No task number entered");
                 }
-                int taskNo = Integer.parseInt(command.split(" ")[1]) - 1;
+                int taskNo = Integer.parseInt(splitArr[1]) - 1;
                 return new Command("done", taskNo);
-            } else if (command.split(" ")[0].equals("todo")) {
-                if (command.length() <= 5) {
+            } else if (command.equals("todo")) {
+                if (inputText.length() <= 5) {
                     throw new EmptyDescriptionException("No Description entered");
                 }
-                String description = command.substring(5);
+                String description = inputText.substring(5);
                 return new Command("todo", new Todo(description));
 
-            } else if (command.split(" ")[0].equals("deadline")) {
-                if (command.length() <= 9) {
+            } else if (command.equals("deadline")) {
+                if (inputText.length() <= 9) {
                     throw new EmptyDescriptionException("No Description entered");
                 }
-                String[] splitArr = command.split("/");
-                if (splitArr.length == 1) {
+                if (splitArr.length == 1 ) {
                     throw new UnknownTimeException("No by time added");
                 }
-                String description = splitArr[0].substring(9);
-                if (splitArr[1].length() <= 3) {
+                String[] newSplitArr = inputText.substring(9).split("/by ");
+                if (newSplitArr.length == 1) {
                     throw new EmptyTimeException("No time entered");
                 }
-                String by = splitArr[1].substring(3);
+                String description = newSplitArr[0];
+                String by = newSplitArr[1];
                 return new Command("deadline", new Deadline(description, by));
-            } else if (command.split(" ")[0].equals("event")) {
-                if (command.length() <= 6) {
+            } else if (command.equals("event")) {
+                if (inputText.length() <= 6) {
                     throw new EmptyDescriptionException("No Description entered");
                 }
-                String[] splitArr = command.split("/");
-                if (splitArr.length == 1) {
-                    throw new UnknownTimeException("No by time added");
+                if (splitArr.length == 1 ) {
+                    throw new UnknownTimeException("No at time added");
                 }
-                String description = splitArr[0].substring(6);
-                if (splitArr[1].length() <= 3) {
+                String[] newSplitArr = inputText.substring(6).split("/at ");
+                if (newSplitArr.length == 1) {
                     throw new EmptyTimeException("No time entered");
                 }
-                String at = splitArr[1].substring(3);
+                String description = newSplitArr[0];
+                String at = newSplitArr[1];
                 return new Command("event", new Event(description, at));
-            } else if (command.split(" ")[0].equals("delete")) {
-                if (command.split(" ").length == 1) {
+            } else if (command.equals("delete")) {
+                if (splitArr.length == 1) {
                     throw new UnknownTaskException("No task number entered");
                 }
-                int taskNo = Integer.parseInt(command.split(" ")[1]) - 1;
+                int taskNo = Integer.parseInt(splitArr[1]) - 1;
                 return new Command("delete", taskNo);
             } else {
                 throw new UnknownCommandException("Unknown command entered");
