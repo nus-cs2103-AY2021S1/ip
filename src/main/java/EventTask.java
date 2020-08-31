347e0b19b1e0d263e7089bde5a3e0af356896d05
Maxx <maxxyh@gmail.com>
2020-09-01 07:50:12 +0800

    Level-8: Dates and Times
    Deadlines and Event times now stored as DateTime objects

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,18 +1,36 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class EventTask extends Task {
 
-    private String eventTime;
+    private LocalDate eventDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
 
     public EventTask(String description) {
         super(description.split(" /")[0]);
         String[] output = description.split("/");
-        String pattern = ("(at?)(\\s)(.+)");
-        eventTime = output[1].replaceAll(pattern, "$3");
+        String pattern = ("(at?)(\\s)(\\S+)(\\s)(\\d{4})([-])(\\d{4})");
+        eventDate = LocalDate.parse(output[1].replaceAll(pattern, "$3"));
+        String startTemp = output[1].replaceAll(pattern, "$5");
+        String endTemp = output[1].replaceAll(pattern, "$7");
+        startTime = LocalTime.parse(startTemp.substring(0,2) + ":"
+            + startTemp.substring(2,4));
+        endTime = LocalTime.parse(endTemp.substring(0,2) + ":"
+                + endTemp.substring(2,4));
     }
 
     @Override
     public String toString()
     {
-        return String.format("[E][%s] %s (at: %s)", getStatusIcon(), description, eventTime);
+        return String.format("[E][%s] %s (at: %s)", getStatusIcon(), description,
+                String.format(
+                        eventDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                        + ", " + startTime.format(DateTimeFormatter.ofPattern("Hmm'hrs'"))
+                        + "-" + endTime.format(DateTimeFormatter.ofPattern("Hmm'hrs'"))
+                        ));
     }
 
 }
