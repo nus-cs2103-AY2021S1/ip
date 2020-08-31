fc09da715d0c813a11a1ee8852c9b26a4a6b917d
Maxx <maxxyh@gmail.com>
2020-09-01 06:44:34 +0800

    Level-7
    Save the tasks in the hard disk automatically whenever the task list changes. Load the data from the hard disk when Duke starts up. You may hard-code the file name and location

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,27 @@
+import jdk.jfr.Event;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Duke {
+
+    private static final String SAVE_PATH = "data/save.txt";
+
+    public static final String SEPARATOR = ",";
+
+    public static final Pattern TASK_DATA_ARGS_FORMAT =
+            Pattern.compile("(?<eventType>\\w+)" + SEPARATOR
+                    + "(?<isDone>[01])" + SEPARATOR
+                    + "(?<description>.+[^,]$?)" + SEPARATOR
+                    + "(?<date>.+)");
 
     public static void main(String[] args) {
         String logo = "   ___      _      _ __   _              \n" +
@@ -21,6 +39,13 @@ public class Duke {
         System.out.println(greeting);
 
         List<Task> taskList = new ArrayList<>();
+        try {
+            taskList = loadTasks();
+        } catch (StorageOperationException e) {
+            System.out.println("    Something went wrong!");
+            throw new RuntimeException(e);
+        }
+
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
@@ -36,6 +61,11 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
         System.out.println("    Bye. Try not to come again please... let me live.");
         System.out.println("    ____________________________________________________________");
+        try {
+            saveTasks(taskList);
+        } catch (StorageOperationException soe) {
+            System.out.println(soe.getMessage());
+        }
     }
 
     private static void respondToInput(String input, List<Task> taskList) throws DukeException {
@@ -118,6 +148,77 @@ public class Duke {
         for (int i = 0; i < taskList.size(); i++) {
             System.out.print("    " + (i+1) + ". ");
             System.out.println(taskList.get(i));
+        }
+    }
+
+    private static void saveTasks(List<Task> taskList) throws StorageOperationException {
+        List<String> dataLines = new ArrayList<>();
+        for (Task task : taskList) {
+            String line = String.join(",", task.getSaveData());
+            dataLines.add(line);
+        }
+        try {
+            Path path = Paths.get(SAVE_PATH);
+            Files.write(path, dataLines);
+        } catch (IOException ioe) {
+            throw new StorageOperationException("Error writing file: " + Paths.get(SAVE_PATH));
+        }
+    }
+
+    private static List<Task> loadTasks() throws StorageOperationException {
+
+        Path path = Paths.get(SAVE_PATH);
+        if (!Files.exists(path) || !Files.isRegularFile(path)) {
+            return new ArrayList<Task>();
+        }
+
+        try {
+            // read the file
+            List<String> dataLines = Files.readAllLines(path);
+            return DecodeTasksFromSave(dataLines);
+        // should be handled above so it's an assertion instead of an exception
+        } catch (FileNotFoundException fnfe) {
+            throw new AssertionError("A non-existent file scenario should already have been handled earlier.");
+        // io exception for example no permission to read a file, some process closed the stream
+        } catch (IOException ioe) {
+            throw new StorageOperationException("Error reading file: " + path);
+        // illegalValueException is when arguments cannot form a task
+        } catch (IllegalValueException ive) {
+            throw new StorageOperationException("File contains illegal data values; data type constraints not met");
+        }
+    }
+
+    private static List<Task> DecodeTasksFromSave(List<String> dataLines) throws IllegalValueException {
+        List<Task> output = new ArrayList<>();
+        for (String line : dataLines) {
+            Matcher matcher = TASK_DATA_ARGS_FORMAT.matcher(line);
+            if (!matcher.matches()) {
+                throw new IllegalValueException("Encoded task in invalid format. Unable to decode.");
+            }
+            String command = matcher.group("eventType");
+            boolean isDone = matcher.group("isDone").equals("0") ? false : true;
+            String description  = matcher.group("description");
+            if (command.equals("T")) {
+                output.add(new ToDoTask(description, isDone));
+            } else if (command.equals("E")) {
+                output.add(new EventTask(description, isDone, matcher.group("date")));
+            } else if (command.equals("D")) {
+                output.add(new DeadlineTask(description, isDone, matcher.group("date")));
+            } else {
+                throw new IllegalValueException("Encoded task in invalid format. Unable to decode.");
+            }
+        }
+
+        return output;
+    }
+
+    /**
+     * Signals that some error has occured while trying to convert and read/write data between the application
+     * and the storage file.
+     */
+    public static class StorageOperationException extends Exception {
+        public StorageOperationException(String message) {
+            super(message);
         }
     }
 }
---
+++ src/main/java/IllegalValueException.java
@@ -0,0 +1,8 @@
+public class IllegalValueException extends Exception {
+
+    public IllegalValueException(String message)
+    {
+        super(message);
+    }
+
+}
