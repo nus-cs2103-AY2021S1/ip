57bdc2532fddc75a124629097596b06e42dedd33
wltan <53135010+wltan@users.noreply.github.com>
2020-08-31 23:13:18 +0800

    Fix unit test
    Tests for this class may interfere with one another due to the internal TaskList state.
    
    Let us clear the state between tests so that each test is independent.

--- src/test/java/core/TaskListTest.java
+++ src/test/java/core/TaskListTest.java
@@ -1,12 +1,21 @@
 package core;
 
 import exceptions.DataException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tasks.Todo;
+
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskListTest {
+
+    @BeforeEach
+    public void reset() {
+        // clear all tasks in TaskList
+        IntStream.range(0, TaskList.size()).forEach(i -> TaskList.remove(0));
+    }
 
     @Test
     public void testAdd() throws DataException {
