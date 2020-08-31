e133d53336f426c8b97cc77d8259ef0c99bb40e2
Xingjian <xingjian.chen@yahoo.com.sg>
2020-09-01 01:17:44 +0800

    GUI Test setup

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -1,5 +1,16 @@
 package Duke;
 
+/**
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+ */
+
 import Duke.Command.*;
 import Duke.Exception.*;
 
@@ -8,7 +19,15 @@ import java.io.IOException;
 /**
  * Represents Duke
  */
-public class Duke {
+public class Duke { //extends Application{
+    /**
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+     */
+
     private Storage storage;
     private TaskList tasklist;
     private Ui ui;
@@ -70,5 +89,29 @@ public class Duke {
             System.out.println("File not recognised\n");
         }
     }
+
+    /**
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        // more code to be added here later
+    } */
 
 }
