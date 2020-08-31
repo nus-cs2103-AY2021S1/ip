9ee3bf7a68812f4f0d4d6075c360634c26727374
WayneTanKinLun <waynetan2000@gmail.com>
2020-08-31 17:55:32 +0800

    Add JavaFX by following the guide

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,26 +2,47 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 /**
  * Duke, a Personal Assistant Chatbot that helps a person to keep track of various things.
  *
  * @author Wayne Tan
  * @since Aug 2020
  */
-public class Duke {
+public class Duke  {
 
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
     /**
      * Duke construction specifying a filePath to initialise the Storage, Ui and TaskList.
      *
-     * @param filePath
      */
-    public Duke(String filePath) {
+    public Duke() {
         ui = new Ui();
-        storage = new Storage(filePath);
+        storage = new Storage("data/duke.txt");
         try {
             tasks = new TaskList(storage.load());
         } catch (FileNotFoundException e) {
@@ -58,7 +79,30 @@ public class Duke {
         }
     }
 
-    public static void main(String[] args) {
-        new Duke("data/duke.txt").run();
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    String getResponse(String input) {
+        return "Duke heard: " + input;
     }
+
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        // You will need to import `javafx.scene.control.Label`.
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+    public static void main(String[] args) {
+        new Duke().run();
+    }
+
 }
