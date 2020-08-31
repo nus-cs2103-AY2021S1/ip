46e0382f27a8cc180a12f6ee2995b9998a45b220
chshen1998 <chewhoashen@gmail.com>
2020-09-01 00:40:50 +0800

    Add more testcases

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class DukeTest {
     @Test
     public void testCommands() throws DukeException {
-        assertTrue(Parser.parse("Wrong Command") instanceof InvalidCommand);
         assertTrue(Parser.parse("list") instanceof ListCommand);
         assertTrue(Parser.parse("done 1") instanceof DoneCommand);
         assertTrue(Parser.parse("delete 1") instanceof DeleteCommand);
@@ -28,6 +27,11 @@ public class DukeTest {
         assertTrue(Parser.parse("todo t1") instanceof TodoCommand);
         assertTrue(Parser.parse("deadline d1 /by 20/02/2020 1200") instanceof DeadlineCommand);
         assertTrue(Parser.parse("event e1 /at 20/02/2020 1200") instanceof EventCommand);
+    }
+
+    @Test
+    public void testInvalidCommand() throws DukeException {
+        assertTrue(Parser.parse("Wrong Command") instanceof InvalidCommand);
     }
 
     @Test
@@ -41,5 +45,50 @@ public class DukeTest {
         assertEquals(new DoneCommand("test").isExit(), false);
         assertEquals(new FilterCommand("test").isExit(), false);
         assertEquals(new DeleteCommand("test").isExit(), false);
+    }
+
+    @Test
+    public void testIncorrectDone() {
+        try {
+            Parser.parse("done").execute(new TaskList(), new Ui(), new Storage("demo"));
+        } catch (Exception e) {
+            assertTrue(e instanceof DukeException);
+        }
+    }
+
+    @Test
+    public void testIncorrectDelete() {
+        try {
+            Parser.parse("delete").execute(new TaskList(), new Ui(), new Storage("demo"));
+        } catch (Exception e) {
+            assertTrue(e instanceof DukeException);
+        }
+    }
+
+    @Test
+    public void testIncorrectTodo() {
+        try {
+            Parser.parse("todo").execute(new TaskList(), new Ui(), new Storage("demo"));
+        } catch (Exception e) {
+            assertTrue(e instanceof DukeException);
+        }
+    }
+
+    @Test
+    public void testIncorrectDeadline() {
+        try {
+            Parser.parse("deadline d1 ").execute(new TaskList(), new Ui(), new Storage("demo"));
+        } catch (Exception e) {
+            assertTrue(e instanceof DukeException);
+        }
+    }
+
+    @Test
+    public void testIncorrectEvent() {
+        try {
+            Parser.parse("event e1").execute(new TaskList(), new Ui(), new Storage("demo"));
+        } catch (Exception e) {
+            assertTrue(e instanceof DukeException);
+        }
     }
 }
