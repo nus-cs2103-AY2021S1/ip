93aa5cc0a7f853635b4322115886b489f4e333ce
luo-git <lluoyi.lu@gmail.com>
2020-08-31 15:34:11 +0800

    Add GUI for duke

--- src/main/java/luoyi/duke/Main.java
+++ src/main/java/luoyi/duke/Main.java
@@ -1,33 +1,12 @@
 package luoyi.duke;
 
-import luoyi.duke.data.Duke;
-import luoyi.duke.data.IDuke;
-
-import java.util.Scanner;
+import javafx.application.Application;
 
 /**
  * Main driver class.
  */
 public class Main {
     public static void main(String[] args) {
-        IDuke bot = Duke.getDuke("data/duke.txt");
-        Scanner sc = new Scanner(System.in);
-
-        // Greet user
-        bot.greet();
-
-        // Handle commands until user types bye
-        while (sc.hasNext()) {
-            String s = sc.nextLine();
-            if (s.equals("bye")) {
-                break;
-            } else if (!s.matches("\\s*")) {
-                // Ignore white spaces or empty lines
-                bot = bot.handleCommand(s);
-            }
-        }
-
-        // Exit bot
-        bot.bye();
+        Application.launch(MainApp.class, args);
     }
 }
---
+++ src/main/java/luoyi/duke/MainApp.java
@@ -0,0 +1,33 @@
+package luoyi.duke;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import luoyi.duke.data.Duke;
+import luoyi.duke.data.IDuke;
+
+import java.io.IOException;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class MainApp extends Application {
+
+    private final IDuke duke = Duke.getDuke("data/duke.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
