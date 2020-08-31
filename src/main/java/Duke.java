ba2045f3bd4dd858cd46abde9a8aaf25d54aef15
SherryWu178 <sherry.wu178@gmail.com>
2020-08-31 20:36:20 +0800

    Add javafx

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
 package main.java;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 import main.java.commands.Command;
 import main.java.exceptions.DukeException;
 import main.java.tasklist.TaskList;
@@ -7,10 +12,14 @@ import main.java.parser.Parser;
 import main.java.storage.Storage;
 import main.java.ui.Ui;
 
-public class Duke {
+public class Duke extends Application{
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+
+    public Duke() {
+
+    }
 
     public Duke(String filePath) {
         ui = new Ui();
@@ -21,6 +30,19 @@ public class Duke {
             ui.displayErrorMessage(e);
             tasks = new TaskList();
         }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Label jianhan = new Label("Jianhan Cute???"); // Creating a new Label control
+
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        Scene scene2 = new Scene(jianhan); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.setScene(scene2);
+        stage.show(); // Render the stage.
     }
 
     /**
