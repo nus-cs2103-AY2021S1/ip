4b3a7e3041c90ac9d02f5fb7a1a66a0679cfb7bd
xxzz-tt <tanxuanzhi@gmail.com>
2020-09-01 22:46:40 +0800

    Add GUI to Duke

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,37 @@
-import java.io.File;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.io.IOException;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws IncorrectInputException, IOException {
-        Scanner sc = new Scanner(System.in);
+public class Main extends Application {
 
-        Chatbot bot = new Chatbot();
-        bot.greeting();
+    private Duke duke = new Duke();
 
-
-        while (sc.hasNext() && bot.chat(sc.nextLine())) {
-//            String next = sc.next();
-//            bot.chat(next);
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        sc.close();
     }
 }
