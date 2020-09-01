0949e8e73a4514eaac134b4eeaa1bd6d3ae4cb83
VaishakAnand <vaishak140498@gmail.com>
2020-09-01 19:37:08 +0800

    Implement functional basic Duke Gui

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,41 +1,19 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import gui.DialogBox;
-
 /**
  * Represents the Duke Chatbot.
  * Allows users to store a list of Tasks, consisting of three types,
  * namely Todos, Events and Deadlines. Users input, change and delete
  * such items via text commands to the bot.
  */
-public class Duke extends Application {
+public class Duke {
 
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
 
-    private Image man = new Image(this.getClass().getResourceAsStream("/images/Man.jpg"));
-    private Image woman = new Image(this.getClass().getResourceAsStream("/images/Woman.jpg"));
     /**
      * Builds an instance of the Duke Chatbot.
      */
@@ -49,132 +27,39 @@ public class Duke extends Application {
      * Runs the chatbot instance.
      * Chatbot will begin listening for commands with this method.
      */
-    public void run() {
+    private String run(String inputText) {
         try {
-            ui.intro();
-            Scanner input = new Scanner(System.in);
+            Command command = ui.parseCommand(inputText);
+            String output = "";
 
-            // Uses user input text and acts accordingly.
-            loop:
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                Command command = ui.parseCommand(line);
-                ui.lineBreak();
-
-                switch (command.name) {
-                case "find":
-                    tasks.findTasks(command.message);
-                    break;
-                case "bye":
-                    System.out.println("That's it? That's a shame. Well, see you later then.");
-                    ui.lineBreak();
-                    storage.saveTasks();
-                    break loop;
-                case "list":
-                    tasks.printList();
-                    break;
-                case "done":
-                    tasks.taskDone(command.index);
-                    break;
-                case "delete":
-                    tasks.removeFromList(command.index);
-                    break;
-                case "error":
-                    System.out.println(command.message);
-                    break;
-                default:
-                    tasks.addToList(command.task);
-                }
-                ui.lineBreak();
+            switch (command.name) {
+            case "find":
+                return tasks.findTasks(command.message);
+            case "bye":
+                storage.saveTasks();
+                return "That's it? That's a shame. Well, see you later then.";
+            case "list":
+                return tasks.printList();
+            case "done":
+                return tasks.taskDone(command.index);
+            case "delete":
+                return tasks.removeFromList(command.index);
+            case "error":
+                return command.message;
+            default:
+                return tasks.addToList(command.task);
             }
-
-            input.close();
         } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            return ex.getMessage();
         }
     }
 
-    public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.run();
-    }
-
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Step 3. Add functionality to handle user input.
-        //Part 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
     /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
      */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(userInput.getText());
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(man)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(woman))
-        );
-        userInput.clear();
+    protected String getResponse(String input) {
+        return this.run(input);
     }
 
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -14,55 +14,81 @@ public class TaskList {
     /**
      * Prints a list of current tasks.
      */
-    void printList() {
+    protected String printList() {
+        StringBuilder output = new StringBuilder();
         for (int i = 1; i <= this.list.size(); i++) {
-            System.out.println(i + ". " + this.list.get(i - 1));
+            output.append(i).append(". ").append(this.list.get(i - 1)).append("\n");
         }
+        return output.toString();
     }
 
     /**
      * Removes a task from the list of current tasks.
+     *
      * @param taskNo Index of task to remove.
      */
-    void removeFromList(int taskNo) {
+    protected String removeFromList(int taskNo) {
+        StringBuilder output = new StringBuilder();
         Task removedTask = this.list.remove(taskNo);
-        System.out.println("Well, if you insist. I've removed:");
-        System.out.println(removedTask);
+        output.append("Well, if you insist. I've removed:\n")
+                .append(removedTask);
+        return output.toString();
     }
 
     /**
      * Adds a task to the list of current tasks.
+     *
      * @param task Index of task to add.
      */
-    void addToList(Task task) {
-        System.out.println("Alright matey, I've added this task:");
+    protected String addToList(Task task) {
+        StringBuilder output = new StringBuilder();
+        output.append("Alright matey, I've added this task:\n");
         this.list.add(task);
-        System.out.println(task);
-        System.out.println("Looks like you have " + this.list.size() + " tasks in total.");
+        output.append(task)
+                .append("\n")
+                .append("Looks like you have ")
+                .append(this.list.size())
+                .append(" tasks in total.");
+        return output.toString();
     }
 
     /**
      * Marks a task in list of current tasks as completed.
+     *
      * @param taskNo Index of task to mark.
      */
-    void taskDone(int taskNo) {
+    protected String taskDone(int taskNo) {
+        StringBuilder output = new StringBuilder();
         Task toBeDone = this.list.get(taskNo);
         toBeDone.markAsDone();
         this.list.set(taskNo, toBeDone);
-        System.out.println("Good Job, this task is now done:");
-        System.out.println(toBeDone);
+        output.append("Good Job, this task is now done:\n").append(toBeDone);
+        return output.toString();
     }
 
-    void findTasks(String keyword) {
-        System.out.println("Here are the tasks you're trying to find.");
-        int count = 1;
+    protected String findTasks(String keyword) {
+        StringBuilder output = new StringBuilder();
+        int count = 0;
         for (int i = 0; i < this.list.size(); i++) {
             Task task = this.list.get(i);
             String description = task.description;
-            if (description.matches(".*\\b" + keyword + "\\b.*")) {
-                System.out.println(count + ". " + task);
+            boolean thereIsMatch = description.matches(".*\\b" + keyword + "\\b.*");
+            if (thereIsMatch) {
+                if (count == 0) {
+                    output.append("Here are the tasks you're trying to find.\n");
+                }
                 count++;
+                output.append(count)
+                        .append(". ")
+                        .append(task)
+                        .append("\n");
             }
         }
+
+        if (count == 0) {
+            output.append("No match found :(");
+        }
+
+        return output.toString();
     }
 }
