199d01bae556ed5d5a227d1429876e364a7d85db
ZoroarkDarkrai <fidellawi@gmail.com>
2020-09-02 02:40:13 +0800

    Add FXML

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -6,7 +6,12 @@ import javafx.application.Application;
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
+    /**
+     * Launcher entry's point.
+     *
+     * @param args an array of command-line arguments for the application.
+     */
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
