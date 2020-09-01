0e86e1de53fc6f1a4e94dee056d094a553037fa5
xinyee20 <codelearner19@gmail.com>
2020-09-01 15:04:07 +0800

    Add Launcher.java and modify Willy.java to use JavaFX technology to implement GUI

---
+++ src/main/java/willy/ui/Launcher.java
@@ -0,0 +1,12 @@
+package willy.ui;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Willy.class, args);
+    }
+}
--- src/main/java/willy/ui/Willy.java
+++ src/main/java/willy/ui/Willy.java
@@ -2,6 +2,11 @@ package willy.ui;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import willy.store.TaskStore;
 import willy.task.TaskList;
 import willy.exceptions.WillyException;
@@ -11,7 +16,7 @@ import willy.command.Parser;
 /**
  * A bot that records tasks for people and keeps track of it for them.
  */
-public class Willy {
+public class Willy extends Application {
 
     private static TaskStore storage;
     public static String style = "\t________________________________________________________________\n";
@@ -28,6 +33,14 @@ public class Willy {
         System.out.println(startDuke);
         storage = new TaskStore();
         storage.createFile();
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
 
