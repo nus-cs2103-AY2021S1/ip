23768aaf0ae18c103ab8309c9bb8437d0d332023
kimberly ong <e0424807@u.nus.edu>
2020-09-01 21:50:18 +0800

    Remove unused imports

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,15 +2,6 @@ package duke;
 
 import duke.command.Command;
 import duke.exception.DukeException;
-import duke.exception.InvalidCommandException;
-import duke.exception.InvalidInputException;
-import duke.exception.InvalidTaskTypeException;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
 
 /**
  * Entry point of Duke chatbot.
