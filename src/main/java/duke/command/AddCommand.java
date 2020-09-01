28e3c6de134fb399578d49efba2ba47c55b81bbc
jeminsieow <jemin.sieow@prepr.org>
2020-09-01 15:32:25 +0800

    Use CheckStyle. Update code to fit checkstyle.

--- src/main/java/duke/command/AddCommand.java
+++ src/main/java/duke/command/AddCommand.java
@@ -1,11 +1,19 @@
 package duke.command;
 
-import duke.*;
+import duke.DukeException;
+import duke.Storage;
+import duke.TaskList;
+import duke.Ui;
 import duke.task.Task;
 
 public class AddCommand extends Command {
     private Task task;
 
+    /**
+     * Initializes AddCommand object.
+     *
+     * @param task Task to be added to the list.
+     */
     public AddCommand(Task task) {
         super();
         this.task = task;
--- src/main/java/duke/command/DoneCommand.java
+++ src/main/java/duke/command/DoneCommand.java
@@ -1,13 +1,18 @@
 package duke.command;
 
 import duke.Storage;
-import duke.task.Task;
 import duke.TaskList;
 import duke.Ui;
+import duke.task.Task;
 
 public class DoneCommand extends Command {
     private int index;
 
+    /**
+     * Initializes DoneCommand object.
+     *
+     * @param index Index of the task to be marked as done.
+     */
     public DoneCommand(int index) {
         super();
         this.index = index;
