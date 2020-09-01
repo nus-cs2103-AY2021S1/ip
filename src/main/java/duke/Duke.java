1f6f1144138452ca92784434b1692b269d89718d
Ian Yong <ianyongyc@gmail.com>
2020-09-01 18:20:57 +0800

    Add basic GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,16 +5,26 @@ import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
+import javafx.application.Application;
+
 /**
  * Entrypoint for the Duke chatbot.
  */
 public class Duke {
     /**
-     * Starts the REPL environment.
+     * Starts the Duke chatbot.
      *
      * @param args The command line parameters (unused)
      */
     public static void main(String[] args) {
+        launchGui();
+        // TODO: Add option to launch Duke in CLI mode.
+    }
+
+    /**
+     * Launches the command line interface version of the Duke chatbot.
+     */
+    private static void launchCli() {
         try {
             // Set the encoding of `System.out` to UTF-8.
             System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, "UTF-8"));
@@ -22,5 +32,12 @@ public class Duke {
             System.err.println(e.getMessage());
         }
         Repl.run();
+    }
+
+    /**
+     * Launches the graphical user interface version of the Duke chatbot.
+     */
+    private static void launchGui() {
+        Application.launch(Gui.class);
     }
 }
---
+++ src/main/java/duke/Gui.java
@@ -0,0 +1,32 @@
+package duke;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A graphical user interface for the Duke chatbot.
+ */
+public class Gui extends Application {
+    /**
+     * Starts the graphical user interface.
+     *
+     * @param stage the top-level JavaFX container
+     */
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource("/views/MainWindow.fxml"));
+            AnchorPane anchorPane = fxmlLoader.load();
+            Scene scene = new Scene(anchorPane);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
