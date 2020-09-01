fa3deb0c19f6ca5443fb2de54a601f6267411607
Ritesh Kumar <ritesh@emerald.pink>
2020-09-01 15:12:38 +0800

    Complete JavaFX tutorial level 3

---
+++ src/main/java/duke/ui/Launcher.java
@@ -0,0 +1,9 @@
+package duke.ui;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(GraphicalUi.class, args);
+    }
+}
