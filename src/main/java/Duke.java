0f413279d44dbd7e5cdeea2772287cc1e59dbd64
the-reefshark <rishabh92k@gmail.com>
2020-09-01 14:09:13 +0800

    Add Level-10

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,36 @@
+import java.io.File;
+
+/**
+ * Class responsible for the execution of the Duke chatbot.
+ */
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        Chatbot chatBot = new Chatbot();
-        chatBot.start();
+    private final String DATA_PATH_NAME = "./data";
+    private final String DATA_FILE_PATH = "./data/dukedata.txt";
+
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
+
+    /**
+     * Initialises a new Duke object using the specified DATA_PATH_NAME and DATA_FILE_PATH values.
+     */
+    public Duke() {
+        ui = new Ui();
+        storage = new Storage(DATA_PATH_NAME, DATA_FILE_PATH);
+        File file = storage.getSavedData(ui);
+
+        if (file != null) {
+            tasks = new TaskList(file, ui);
+        } else {
+            tasks = new TaskList();
+        }
+    }
+
+    public String getResponse(String input) {
+        if (Parser.isDone()) {
+            System.exit(0);
+        }
+        String result = Parser.parse(input, storage, tasks, ui);
+        return result;
     }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -2,57 +2,65 @@
  * Represents the Ui that interacts with the user.
  */
 public class Ui {
-    private final String DIVIDER = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    private final String DIVIDER = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
     private final String INDENT = "        ";
 
     public void showWelcomeMessage() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
         System.out.println(INDENT + "What can I do for you delightful human?\n" + DIVIDER);
     }
 
-    public void showGoodbyeMessage() {
-        System.out.println(INDENT + "Guess its time for us to part ways\n"
-                + INDENT + "Thanks for the memories\n" + INDENT + ":`(");
+    public String showGoodbyeMessage() {
+        return INDENT + "Guess its time for us to part ways\n"
+                + INDENT + "Thanks for the memories\n" + INDENT + ":`(";
     }
 
-    public void showAllTasks(TaskList tasks) {
+    public String showAllTasks(TaskList tasks) {
         if (tasks.getSize() == 0) {
-            System.out.println(INDENT + "I have no tasks to show you sir");
-        } else {
-            System.out.println(INDENT + "Here are your tasks for today:");
-            for (int i = 0; i < tasks.getSize(); i++) {
-                Task curr = tasks.getTask(i);
-                System.out.println(INDENT + (i + 1) + "." + curr);
-            }
+            return INDENT + "I have no tasks to show you sir";
         }
+
+        String output = INDENT + "Here are your tasks for today:\n";
+        for (int i = 0; i < tasks.getSize(); i++) {
+            Task curr = tasks.getTask(i);
+            output += INDENT + (i + 1) + "." + curr + "\n";
+        }
+
+        return output;
     }
 
-    public void showDoneTask(Task task) {
-        System.out.println(INDENT + "Good job! I've marked this task as done:");
-        System.out.println(INDENT + "  " + task);
+    public String showDoneTask(Task task) {
+        return INDENT + "Good job! I've marked this task as done:\n" +
+            INDENT + "  " + task;
     }
 
-    public void showDeleteTask(Task task, TaskList tasks) {
-        System.out.println(INDENT + "Of course sir. I have removed this task:\n" +
-                INDENT + "  " + task);
-        System.out.println(INDENT + "Now you have " + tasks.getSize() + " tasks in the list.");
+    public String showDeleteTask(Task task, TaskList tasks) {
+        return INDENT + "Of course sir. I have removed this task:\n" +
+                INDENT + "  " + task + "\n" +
+                INDENT + "Now you have " + tasks.getSize() + " tasks in the list.";
     }
 
-    public void showAddTask(Task task, TaskList tasks) {
-        System.out.println(INDENT + "Affirmative. I've added this task:\n  " +
-                INDENT + task);
-        System.out.println(INDENT + "Now you have " + tasks.getSize() + " tasks in the list.");
+    public String showAddTask(Task task, TaskList tasks) {
+        return INDENT + "Affirmative. I've added this task:\n" +
+                INDENT + task + "\n" +
+                INDENT + "Now you have " + tasks.getSize() + " tasks in the list.";
     }
 
-    public void showDivider() {
-        System.out.println(DIVIDER);
+    public String showDivider() {
+        return "";
+        // return DIVIDER + "\n";
     }
 
-    public void showError(String message) {
-        System.out.println(INDENT + message);
-        showDivider();
+    public String showError(String message) {
+        return INDENT + message + "\n" + showDivider();
     }
 
-    public void showInvalidCommandMessage() {
-        System.out.println(INDENT + "Sorry sir but I don't know what you mean :(");
+    public String showInvalidCommandMessage() {
+        return INDENT + "Sorry sir but I don't know what you mean :(";
     }
 }
