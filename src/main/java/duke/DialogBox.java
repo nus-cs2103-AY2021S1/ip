afa4e28663635cf6061e417f3a530f22ad428511
JoshuaChewJianXiang <joshuachewabc@gmail.com>
2020-09-01 15:22:43 +0800

    Add background colours for dialog boxes

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -2,14 +2,26 @@ package duke;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
 
 public class DialogBox extends HBox {
+
+    private final double TEXT_IMAGE_SPACING = 5.0;
+
+    private static final Background DUKE_BACKGROUND =
+            new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY));
+    private static final Background USER_BACKGROUND =
+            new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY));
 
     private Label text;
     private ImageView displayPicture;
@@ -24,6 +36,7 @@ public class DialogBox extends HBox {
 
         this.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(text, displayPicture);
+        this.setSpacing(TEXT_IMAGE_SPACING);
     }
 
     /**
@@ -37,11 +50,14 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(Label l, ImageView iv) {
-        return new DialogBox(l, iv);
+        var db = new DialogBox(l, iv);
+        db.setBackground(USER_BACKGROUND);
+        return db;
     }
 
     public static DialogBox getDukeDialog(Label l, ImageView iv) {
         var db = new DialogBox(l, iv);
+        db.setBackground(DUKE_BACKGROUND);
         db.flip();
         return db;
     }
