4033091f0c46c062f352e04db6e2b0c2601aa890
MatthiasLHK <matthiasli1998@hotmail.com>
2020-09-02 01:00:19 +0800

    Add GUI in testing.java and launcher.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,12 +6,24 @@ import Parser.InputManager;
 import Tasks.TaskManager;
 import UI.UserInterface;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * Represents the main method of the MattBot program. The bot is able to track added todo tasks into the list and it
  * is stored locally in a save txt file called DukeTodoSave.txt located in the root of the project directory.
  * If this file is not present, the program will automatically create this text file in the root.
  */
-public class Duke {
+public class Duke extends Application{
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
     /**
      * Represents the main method of the program. The start of the mattbot program.
      * @param args args.
