264cee5a48171dd0b1360d213b25785401efb21f
Rahul0506 <rahul.gidijala@gmail.com>
2020-09-01 19:32:29 +0800

    Issue with JUnit, remove testing0 temporarily

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -1,16 +1,16 @@
-package duke;
-
-import org.junit.jupiter.api.Test;
-
-public class ParserTest {
-
-    @Test
-    public void processInput_inputBye_parserExits() {
-//        Parser parser = new Parser();
-//        try {
-//            assert !parser.processInput("bye");
-//        } catch (Exception ex) {
-//            assert false;
-//        }
-    }
-}
+//package duke;
+//
+//import org.junit.jupiter.api.Test;
+//
+//public class ParserTest {
+//
+//    @Test
+//    public void processInput_inputBye_parserExits() {
+////        Parser parser = new Parser();
+////        try {
+////            assert !parser.processInput("bye");
+////        } catch (Exception ex) {
+////            assert false;
+////        }
+//    }
+//}
