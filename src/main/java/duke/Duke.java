d8b02529feefd617615d65d688edef3fa474a584
chan-j-d <chan.j.d.98@gmail.com>
2020-09-02 00:37:55 +0800

    Create DukeGui to handle interaction with the user
    DukeGui handles direct interaction with the user and uses Duke as the
    engine. The Launcher class has been added to facilitate the creation of
    an instance of the DukeGui Application.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -20,7 +20,7 @@ import javafx.stage.Stage;
 /**
  * Duke is a Personal Assistant Chatbot that helps a user keep track of various things.
  */
-public class Duke extends Application {
+public class Duke {
 
     /** list of tasks to keep track of */
     private TaskList taskList;
@@ -29,14 +29,7 @@ public class Duke extends Application {
     /** ui object in charge of direct interaction with the user */
     private Ui ui;
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
 
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     /**
      * Standard constructor initialising the Duke bot.
@@ -48,6 +41,12 @@ public class Duke extends Application {
         storage = Storage.init();
         taskList = storage.readStoredData();
         ui = new UiDefault();
+    }
+
+    protected Duke(Ui ui) {
+        storage = storage.init();
+        taskList = storage.readStoredData();
+        this.ui = ui;
     }
 
     /**
@@ -69,9 +68,11 @@ public class Duke extends Application {
         boolean isExit = false;
         while (!isExit) {
             try {
-                Command command = CommandParser.parse(ui.getResponse());
+                String userInput = ui.getInput();
+                Command command = CommandParser.parse(userInput);
                 command.execute(taskList, ui, storage);
                 isExit = command.isExit();
+                ui.exit();
             } catch (DukeException e) {
                 ui.outputMessage(e.getMessage());
             }
@@ -79,98 +80,7 @@ public class Duke extends Application {
 
     }
 
-    @Override
-    public void start(Stage stage) {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
 
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-    /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    private String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
 
 }
 
