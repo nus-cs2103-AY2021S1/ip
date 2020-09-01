88047c38683e34041ec936009772daa3ec77b7fa
Tan Zhuo Yao <e0406711@u.nus.edu>
2020-09-01 13:43:20 +0800

    Modify error handling by adding InvalidCommand class

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -7,68 +7,56 @@ import duke.command.*;
  */
 public class Parser {
     /**
-     * Genereating a Command object based on user input
+     * Generating a Command object based on user input
      * @param command User input as String
      * @param list Current list of tasks
      * @param storage Access to duke.ser
      * @return The appropriate Command object to execute
      */
     public static Command parse(String command, TaskList list, Storage storage) {
-        String horizontalLine = "____________________________________\n";
-        Command c = new Command();
-        try{
-            if (command.equals("bye")) {
-                c = new ByeCommand(command, list, storage);
-            } else if (command.equals("list")) {
-                c = new ListCommand(command, list, storage);
-            } else if (command.startsWith("delete")) {
-                if (command.equals("delete")) {
-                    throw new ResponseException(horizontalLine + "☹ AIYO!!! Please state which task to delete la... \n"
-                            + horizontalLine);
-                } else {
-                    c = new DeleteCommand(command, list, storage);
-                }
-            } else if (command.startsWith("done")) {
-                if (command.equals("done")) {
-                    throw new ResponseException(horizontalLine + "☹ AIYO!!! Please state which task is done la... \n"
-                            + horizontalLine);
-                } else {
-                    c = new DoneCommand(command, list, storage);
-                }
-            } else if (command.startsWith("todo")) {
-                if (command.equals("todo")) {
-                    throw new ResponseException(horizontalLine
-                            + "☹ AIYOYO!!! The description of a todo cannot be empty la... \n" + horizontalLine);
-                } else {
-                    c = new TodoCommand(command, list, storage);
-                }
-            } else if (command.startsWith("deadline")) {
-                if (command.equals("deadline")) {
-                    throw new ResponseException(horizontalLine
-                            + "☹ AIYO!!! The description of a deadline cannot be empty la... \n" + horizontalLine);
-                } else {
-                    c = new DeadlineCommand(command, list, storage);
-                }
-            } else if (command.startsWith("event")) {
-                if (command.equals("event")) {
-                    throw new ResponseException(horizontalLine
-                            + "☹ AIYOYO!!! The description of a event cannot be empty la... \n" + horizontalLine);
-                } else {
-                    c = new EventCommand(command, list, storage);
-                }
-            } else if (command.startsWith("find")) {
-                if (command.equals("find")) {
-                    throw new ResponseException(horizontalLine
-                            + "☹ AIYOYO!!! What do you want me to find? Cannot be empty... \n" + horizontalLine);
-                } else {
-                    c = new FindCommand(command, list, storage);
-                }
+        Command c;
+        if (command.equals("bye")) {
+            c = new ByeCommand(command, list, storage);
+        } else if (command.equals("list")) {
+            c = new ListCommand(command, list, storage);
+        } else if (command.startsWith("delete")) {
+            if (command.equals("delete")) {
+                c = new InvalidCommand(command, list, storage);
             } else {
-                throw new ResponseException(horizontalLine + "☹ AIYO!!! What do you mean sia? :-( \n"
-                        + horizontalLine);
+                c = new DeleteCommand(command, list, storage);
             }
-        } catch (ResponseException ex) {
-            System.out.println(ex.getMessage());
+        } else if (command.startsWith("done")) {
+            if (command.equals("done")) {
+                c = new InvalidCommand(command, list, storage);
+            } else {
+                c = new DoneCommand(command, list, storage);
+            }
+        } else if (command.startsWith("todo")) {
+            if (command.equals("todo")) {
+                c = new InvalidCommand(command, list, storage);
+            } else {
+                c = new TodoCommand(command, list, storage);
+            }
+        } else if (command.startsWith("deadline")) {
+            if (command.equals("deadline")) {
+                c = new InvalidCommand(command, list, storage);
+            } else {
+                c = new DeadlineCommand(command, list, storage);
+            }
+        } else if (command.startsWith("event")) {
+            if (command.equals("event")) {
+                c = new InvalidCommand(command, list, storage);
+            } else {
+                c = new EventCommand(command, list, storage);
+            }
+        } else if (command.startsWith("find")) {
+            if (command.equals("find")) {
+                c = new InvalidCommand(command, list, storage);
+            } else {
+                c = new FindCommand(command, list, storage);
+            }
+        } else {
+            c = new InvalidCommand(command, list, storage);
         }
         return c;
     }
---
+++ src/main/java/duke/command/InvalidCommand.java
@@ -0,0 +1,26 @@
+package duke.command;
+
+import duke.Storage;
+import duke.TaskList;
+
+/**
+ * InvalidCommand class handles user inputs something invalid
+ */
+
+public class InvalidCommand extends Command {
+    public InvalidCommand(String command, TaskList list, Storage storage) {
+        super(command, list, storage);
+    }
+
+    /**
+     * Informs user that his command was invalid
+     * @param command User input
+     * @param list TaskList object containing all tasks
+     * @param storage Storage object that reads and writes to duke.ser
+     */
+    @Override
+    public String execute(String command, TaskList list, Storage storage) {
+        String horizontalLine = "____________________________________\n";
+        return horizontalLine + "AIYO!!! I don't understand what you want leh! Can try again?\n" + horizontalLine;
+    }
+}
