3468c4dc2529877237955f79fee6448aa047896f
ruixuantan <ruixuantan@yahoo.com.sg>
2020-09-01 17:06:16 +0800

    Add javadocs to exception package

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,10 +1,22 @@
 package duke.exception;
 
+/** Represents the exceptions in Duke. */
 public class DukeException extends Exception {
+
+    /**
+     * Constructor method
+     *
+     * @param msg the error message <code>String</code>.
+     */
     public DukeException(String msg) {
         super("Apologies. " + msg);
     }
 
+    /**
+     * Converts the exception into a <code>String</code>.
+     *
+     * @return the error message <code>String</code>.
+     */
     @Override
     public String toString() {
         return getMessage();
--- src/main/java/duke/exception/DukeParseException.java
+++ src/main/java/duke/exception/DukeParseException.java
@@ -1,6 +1,16 @@
 package duke.exception;
 
+/** Represents exceptions when Duke is not able to parse commands.
+ * These parse errors can be from the user input or when reading in the saved
+ * task storage text file.
+ */
 public class DukeParseException extends DukeException {
+
+    /**
+     * Constructor method.
+     *
+     * @param message the error message <code>String</code>.
+     */
     public DukeParseException(String message) {
         super(message);
     }
