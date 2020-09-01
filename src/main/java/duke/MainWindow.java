83016d76bdb10f9b8d71247ddebd3919727fa463
jonasngs <jonasngs30@gmail.com>
2020-09-01 12:37:23 +0800

    Update GUI for Duke to display welcome message upon launch

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -28,6 +28,9 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(Ui.showWelcome(), dukeImage)
+        );
     }
 
     public void setDuke(Duke d) {
