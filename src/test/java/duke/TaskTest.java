53cbaa60eefb0e84a8b0f6f043864d09c1b3a9d5
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-09-01 21:41:30 +0800

    Add checkstyle module

--- src/test/java/duke/TaskTest.java
+++ src/test/java/duke/TaskTest.java
@@ -1,9 +1,9 @@
 package duke;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class TaskTest {
 
