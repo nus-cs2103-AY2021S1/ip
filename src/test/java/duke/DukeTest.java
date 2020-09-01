399879e0bf175f43ffd109b0f9f720d36a787d5c
yu-ming-chen <chen.yuming.2015@gmail.com>
2020-09-01 15:46:45 +0800

    Complete A-Gradle, repackage Duke

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -11,7 +11,7 @@ import duke.task.TaskList;
 import duke.task.Todo;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class DukeTest {
