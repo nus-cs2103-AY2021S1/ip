c8ba74c5db2cebdc5b545aef4fac68992d4abb31
theyifan <zhang.yifan@u.nus.edu>
2020-09-01 16:38:13 +0800

    Set up basic structure for GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,10 +1,19 @@
 package duke;
 
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Duke extends Application {
 
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+
+    public Duke() {
+
+    }
 
     /**
      * Creates the Duke object.
@@ -48,4 +57,12 @@ public class Duke {
     }
 
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
 }
