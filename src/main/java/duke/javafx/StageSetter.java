a74c5fcee07ecff8dff36e7b5e99290e753c2783
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-09-01 15:57:07 +0800

    Rename label

--- src/main/java/duke/javafx/StageSetter.java
+++ src/main/java/duke/javafx/StageSetter.java
@@ -9,8 +9,8 @@ public class StageSetter extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Fuck You!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        Label label = new Label("Fuck You!"); // Creating a new Label control
+        Scene scene = new Scene(label); // Setting the scene to be our Label
 
         stage.setScene(scene); // Setting the stage to show our screen
         stage.show(); // Render the stage.
