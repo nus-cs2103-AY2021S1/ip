1ba2336352890f4c5982cdfb69614f7605d5c7f9
Elgoh <elgoh3@gmail.com>
2020-09-02 03:23:10 +0800

    Add gui

--- src/main/java/Gel.java
+++ src/main/java/Gel.java
@@ -6,30 +6,18 @@ import gel.Parser;
 import gel.Storage;
 import gel.TaskList;
 import gel.Ui;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+
 
 /**
  * Gel is an task planner. A <code>Gel</code> object is a bot which
  * enables user to plan their tasks.
  */
-public class Gel extends Application {
+public class Gel {
 
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
+
 
     /**
      * Constructor for class Gel.
@@ -45,10 +33,6 @@ public class Gel extends Application {
             ui.showLoadingError();
             taskList = new TaskList(ui);
         }
-    }
-
-    public Gel () {
-
     }
 
     /**
@@ -70,56 +54,5 @@ public class Gel extends Application {
 
     public static void main(String[] args) {
         new Gel("data/tasks.txt").run();
-    }
-
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Gel");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 }
--- src/main/java/gui/Launcher.java
+++ src/main/java/gui/Launcher.java
@@ -1,3 +1,5 @@
+package gui;
+
 import javafx.application.Application;
 
 /**
@@ -5,6 +7,6 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Gel.class, args);
+        Application.launch(Main.class, args);
     }
 }
