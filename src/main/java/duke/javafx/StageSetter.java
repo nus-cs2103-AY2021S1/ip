221a984ce11a4b570637179884c0ea214fe892e3
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-09-01 15:41:28 +0800

    Set up JavaFx stage

---
+++ src/main/java/duke/javafx/StageSetter.java
@@ -0,0 +1,18 @@
+package duke.javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class StageSetter extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Fuck You!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+}
