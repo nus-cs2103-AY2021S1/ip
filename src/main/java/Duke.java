bafd61a7e6e1286dc922c4273a67d924436b653b
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2020-09-02 11:10:58 +0800

    Create JavaFx Part 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import main.java.manager.Parser;
 
 /**
  * Represents the Duke chat bot.
  */
-public class Duke {
+public class Duke extends Application {
 
     private void printGreeting() {
         System.out.println("Hello! I'm Duke");
@@ -23,6 +27,15 @@ public class Duke {
         printGreeting();
         new Parser().handleUserInput();
         printGoodbye();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     /**
