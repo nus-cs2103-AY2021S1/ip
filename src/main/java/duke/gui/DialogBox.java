68b8e0427a64ee20e62f95d2c70ef1c25d0671e3
Terence Ho <terencehwy@gmail.com>
2020-09-02 23:36:57 +0800

    Add last level changes of GUI tutorial

--- src/main/java/duke/gui/DialogBox.java
+++ src/main/java/duke/gui/DialogBox.java
@@ -1,66 +1,61 @@
 package duke.gui;
+import java.io.IOException;
+import java.util.Collections;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+
+
 
 /**
  * Represents a DialogBox to be used by JavaFX GUI Application
  */
 public class DialogBox extends HBox {
 
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    public DialogBox(Label l, ImageView iv) {
-        text = l;
-        displayPicture = iv;
 
-        text.setWrapText(true);
-        Circle circle = new Circle(displayPicture.getLayoutX() + 50,
-                displayPicture.getLayoutY() + 50, 50);
-        displayPicture.setClip(circle);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-
-        this.setAlignment(Pos.BOTTOM_RIGHT);
-        this.getChildren().addAll(text, displayPicture);
-        this.setSpacing(15.0);
-        this.setPadding(new Insets(10, 10, 10, 10));
-        this.setBorder(new Border(new BorderStroke(Color.TRANSPARENT, Color.TRANSPARENT, Color.BLACK, Color.TRANSPARENT,
-                BorderStrokeStyle.NONE, BorderStrokeStyle.NONE, BorderStrokeStyle.DOTTED, BorderStrokeStyle.NONE,
-                CornerRadii.EMPTY, new BorderWidths(1), new Insets(0, 10, 0, 10))));
+        dialog.setText(text);
+        displayPicture.setImage(img);
     }
 
     /**
      * Flips the dialog box such that the ImageView is on the left and text on the right.
      */
     private void flip() {
-        this.setAlignment(Pos.BOTTOM_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
-    public static DialogBox getUserDialog(Label l, ImageView iv) {
-        return new DialogBox(l, iv);
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
     }
 
-    public static DialogBox getDukeDialog(Label l, ImageView iv) {
-        var db = new DialogBox(l, iv);
+    public static DialogBox getDukeDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
         db.flip();
         return db;
     }
--- src/main/java/duke/gui/Launcher.java
+++ src/main/java/duke/gui/Launcher.java
@@ -1,6 +1,5 @@
 package duke.gui;
 
-import duke.Duke;
 import javafx.application.Application;
 
 /**
@@ -8,6 +7,6 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
