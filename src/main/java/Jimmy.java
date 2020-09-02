93db30763f8877e707499b0c47a9e8eade821f96
chewypiano <gimikamil@gmail.com>
2020-09-02 11:00:45 +0800

    Add JavaDoc comments for Jimmy class and public methods

--- src/main/java/Jimmy.java
+++ src/main/java/Jimmy.java
@@ -1,5 +1,10 @@
-import javafx.application.Platform;
-
+/**
+ * The {@code Jimmy} class represents the core of the Jimmy/Duke
+ * program. The class allows the program to start/end, and it
+ * facilitates the behaviour of the program by making use of the
+ * {@code Ui}, {@code Storage}, {@code Parser} and {@code TaskList}
+ * classes.
+ */
 public class Jimmy {
 
     private final Ui ui;
@@ -7,6 +12,9 @@ public class Jimmy {
     private final TaskList planner;
     private boolean isLoaded;
 
+    /**
+     * Initialises an instance of Jimmy.
+     */
     public Jimmy() {
         this.ui = new Ui();
         this.storage = new Storage();
@@ -14,15 +22,28 @@ public class Jimmy {
         this.isLoaded = false;
     }
 
+    /**
+     * Loads any pre-existing data stored from previous use of the
+     * program. Method does nothing if no prior data is found.
+     */
     public void loadList() {
         storage.loadList(planner);
         this.isLoaded = true;
     }
 
+    /**
+     * Saves current {@code TaskList} into {@code Storage} for future
+     * reference. Method is usually called when the program terminates.
+     */
     public void saveList() {
         storage.updateList(planner);
     }
 
+    /**
+     * Starts the program on the CLI (console terminal) and allows current
+     * user to feed input and interact with the program. Upon inputting the
+     * "bye" command, the method allows the program to terminate.
+     */
     public void run() {
         ui.print(ui.greet());
         loadList();
@@ -43,8 +64,11 @@ public class Jimmy {
     }
 
     /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
+     * Generates {@code String }response from user input. This method is
+     * used as a helper function for the GUI part of the program.
+     *
+     * @param input The text that is typed by the user into the GUI text box.
+     * @return Appropriate response to user input.
      */
     public String getResponse(String input) {
         if (!isLoaded) {
