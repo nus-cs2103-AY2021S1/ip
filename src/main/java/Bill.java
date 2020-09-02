a64660af0b06bb86d59162b108625e687f5c69fa
BILL <xie.yaoren@u.nus.edu>
2020-09-02 23:48:03 +0800

    Done level 10

--- src/main/java/Bill.java
+++ src/main/java/Bill.java
@@ -247,26 +247,6 @@
 
 import java.io.IOException;
 
-import javafx.application.Application;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.Region;
-import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 
 
 public class Bill {
