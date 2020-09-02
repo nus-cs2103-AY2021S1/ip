2cd2e983f537d1d81a4c1e21a0dd6221421ba7fa
Elgoh <elgoh3@gmail.com>
2020-09-02 10:53:04 +0800

    Add JavaFX without fxml

---
+++ src/main/java/gui/MainWindow.java
@@ -0,0 +1,51 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+/**
+ * Controller for MainWindow. Provides the layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Gel gel;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/jake.jpg"));
+    private Image gelImage = new Image(this.getClass().getResourceAsStream("/images/finn.jpg"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setDuke(Gel g) {
+        gel = g;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = gel.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getGelDialog(response, gelImage)
+        );
+        userInput.clear();
+    }
+}
