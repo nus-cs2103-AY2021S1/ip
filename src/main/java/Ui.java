efb490b580906ca10184dbdf01ea17d88f4f8c58
foongsiqi <edu.foongs@gmail.com>
2020-09-02 14:14:21 +0800

    Add a GUI to Duke. Use the JavaFX technology to implement the GUI.

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -55,53 +55,70 @@ public class Ui {
     /**
      * This method prints all the tasks in a task list.
      * @param list The task list to be printed.
+     * @return Returns string containing what was printed.
      */
-    public void printTasks(final TaskList list) {
+    public String printTasks(final TaskList list) {
+        String result = "Here are the tasks in your list:\n";
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < list.size(); i++) {
             System.out.println(i + 1 + ". " + list.get(i).display());
+            result += i + 1 + ". " + list.get(i).display() + "\n";
         }
+        return result;
     }
 
     /**
      * Prints out all tasks in the list.
      * @param list  Task list containing tasks to be printed.
+     * @return String of message to be printed.
      */
-    public void printMatchingTasks(final TaskList list) {
+    public String printMatchingTasks(final TaskList list) {
+        String result = "Here are the matching tasks in your list:\n";
         System.out.println("Here are the matching tasks in your list:");
         for (int i = 0; i < list.size(); i++) {
             System.out.println(i + 1 + ". " + list.get(i).display());
+            result += i + 1 + ". " + list.get(i).display() + "\n";
         }
+        return result;
     }
 
     /**
      * This method prints a message confirming that a task has been added.
      * @param task  The task that has been added.
      * @param list  The list to which the task has been added.
+     * @return      String to be printed.
      */
-    public static void printTaskAdded(final Task task,
+    public static String printTaskAdded(final Task task,
                                       final ArrayList<Task> list) {
         System.out.println("Added task: " + task.display()
                 + "\nYou have " + list.size() + " task(s) left in your list.");
+        return "Added task: " + task.display()
+                + "\nYou have " + list.size() + " task(s) left in your list.";
     }
 
     /**
      * This method prints a message confirming that a task has been done.
      * @param newTask The task that has been marked as done.
+     * @return String of message to be printed.
      */
-    public static void printMarkAsDone(final Task newTask) {
+    public static String printMarkAsDone(final Task newTask) {
         System.out.println("Marked task as done:\n" + newTask.display());
+        return "Marked task as done:\n" + newTask.display();
     }
 
     /**
      * This method prints a message confirming that a task has been deleted.
      * @param removed   The task that has been removed.
      * @param taskList  The list from which the task has been removed.
+     * @return          String of message to be printed.
      */
-    public static void printRemoveTask(final Task removed,
+    public static String printRemoveTask(final Task removed,
                                        final ArrayList<Task> taskList) {
         System.out.println("Removed task:\n" + removed.display()
                 + "\nYou have " + taskList.size()
                 + " task(s) left in your list.");
+        return "Removed task:\n" + removed.display()
+                + "\nYou have " + taskList.size()
+                + " task(s) left in your list.";
     }
 }
