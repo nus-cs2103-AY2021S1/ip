e656e2a42e7cd3e5af94cb5708ad23e77c4c165a
Joe <jteddysanjaya@gmail.com>
2020-09-02 10:50:45 +0800

    Create a DialogBox for Duke GUI

---
+++ src/main/java/duke/DialogBox.java
@@ -0,0 +1,57 @@
+package duke;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class DialogBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialogBox(Label l, ImageView iv) {
+        text = l;
+        displayPicture = iv;
+
+        text.setWrapText(true);
+        BackgroundFill myBF = new BackgroundFill(Color.LIGHTCYAN, new CornerRadii(1),
+                new Insets(0,0,0,0));// or null for the padding
+        text.setBackground(new Background(myBF));
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        final Circle clip = new Circle(50, 50, 50);
+        displayPicture.setClip(clip);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+        this.setSpacing(10);
+        this.setPadding(new Insets(10,10,10,10));
+    }
+
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        return new DialogBox(l, iv);
+    }
+
+    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+        var db = new DialogBox(l, iv);
+        db.flip();
+        return db;
+    }
+}
