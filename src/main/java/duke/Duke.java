a38fe55a50acb9139f79439993f2480d3c65d006
Ma-Yueran <mayuerantom@outlook.com>
2020-09-02 22:34:53 +0800

    Remove getCommandParser in Duke.java

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -72,10 +72,6 @@ public class Duke {
         return ui;
     }
 
-    public CommandParser getCommandParser() {
-        return commandParser;
-    }
-
     public DukeTaskList getTaskList() {
         return taskList;
     }
