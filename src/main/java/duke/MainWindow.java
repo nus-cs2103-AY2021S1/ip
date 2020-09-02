945c0acd493ddf6c139670d7ad17d602e7740d82
peter-yeh <yehyuchun2@gmail.com>
2020-09-02 15:58:04 +0800

    Change user image

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -23,7 +23,7 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.jpg"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Duke.png"));
 
     @FXML
