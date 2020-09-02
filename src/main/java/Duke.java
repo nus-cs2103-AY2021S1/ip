e203b65a3c338b8bb12efd1dcd43624938f95952
Perpetual09 <jackgao0309@gmail.com>
2020-09-02 12:18:01 +0800

    branch-Level-10

---
+++ src/main/java/Duke.java
@@ -0,0 +1,60 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Duke {
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
+    public Duke(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(storage.readFile());
+        } catch (IOException e) {
+            ui.showLoadingError();
+            tasks = new TaskList();
+        }
+    }
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        String res;
+        ui.welcome();
+        while (true) {
+            try {
+                res = sc.nextLine();
+                if (res.equalsIgnoreCase(Operations.BYE.name())) {
+                    break;
+                } else if (res.equalsIgnoreCase(Operations.LIST.name())) {
+                    ui.printList(tasks.getData());
+                } else if (res.startsWith(Operations.DONE.name().toLowerCase())) {
+                    tasks.done(res, ui, storage);
+                } else if (res.startsWith(Operations.DELETE.name().toLowerCase())) {
+                    tasks.delete(res, ui, storage);
+                } else if (res.startsWith(Operations.TODO.name().toLowerCase())) {
+                    tasks.todo(res, ui, storage);
+                } else if (res.startsWith(Operations.DEADLINE.name().toLowerCase())) {
+                    tasks.deadline(res, ui, storage);
+                } else if (res.startsWith(Operations.EVENT.name().toLowerCase())) {
+                    tasks.event(res, ui, storage);
+                } else {
+                    // Exception: eg. ???
+                    throw new DukeException("      OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (NumberFormatException e) {
+                // Exception: eg. done some words
+                // Exception: eg. delete some words
+                ui.showNumberFormatError();
+            } catch (DukeException e) {
+                ui.showDukeError(e);
+            } catch (FileNotFoundException e) {
+                ui.showFileNotFoundError();
+            }
+        }
+        ui.bye();
+    }
+    public static void main(String[] args) {
+        new Duke("./log.txt").run();
+    }
+}
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "✓" : "✘"); //return tick or X symbols
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void done() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
