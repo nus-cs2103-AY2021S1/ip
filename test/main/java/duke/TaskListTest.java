53bcbd43101b44a9544f470b3d44e4db4d10e6bd
gan-jw <e0425717@u.nus.edu>
2020-09-02 18:00:17 +0800

    Add JUnit tests to test the behavior of the code

---
+++ test/main/java/duke/TaskListTest.java
@@ -0,0 +1,54 @@
+package main.java.duke;
+
+import main.java.duke.tasks.Todo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskListTest {
+
+    @Test
+    void addThreeTasks() {
+        TaskList taskList = new TaskList();
+        taskList.addTask(new Todo("taskOne"));
+        taskList.addTask(new Todo("taskTwo"));
+        taskList.addTask(new Todo("taskThree"));
+        assertEquals(3, taskList.getTaskCount());
+    }
+
+    @Test
+    void removeTask_FromNonEmptyList() throws DukeException {
+        TaskList taskList = new TaskList();
+        taskList.addTask(new Todo("taskOne"));
+        taskList.addTask(new Todo("taskTwo"));
+        taskList.addTask(new Todo("taskThree"));
+        taskList.removeTask(1);
+        assertEquals(2, taskList.getTaskCount());
+        taskList.removeTask(1);
+        assertEquals(1, taskList.getTaskCount());
+    }
+
+    @Test
+    void removeOneTask_FromEmptyList() {
+        TaskList taskList = new TaskList();
+        try {
+            taskList.removeTask(1);
+            fail();
+        } catch (DukeException e) {
+            assertEquals("☹ OOPSIE!! What are you deleting? Gotta specify a valid task number!", e.toString());
+        }
+    }
+
+    @Test
+    void removeInvalidTask_FromList() {
+        TaskList taskList = new TaskList();
+        taskList.addTask(new Todo("taskOne"));
+        try {
+            taskList.removeTask(2);
+            fail();
+        } catch (DukeException e) {
+            assertEquals("☹ OOPSIE!! What are you deleting? Gotta specify a valid task number!", e.toString());
+        }
+    }
+
+}
---
+++ test/main/java/duke/UiTest.java
@@ -0,0 +1,33 @@
+package main.java.duke;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UiTest {
+
+    @Test
+    void readCommand() {
+        String input = "return one\nreturn two\nreturn three";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        Ui ui = new Ui();
+        assertEquals("return one", ui.readCommand());
+        assertEquals("return two", ui.readCommand());
+        assertEquals("return three", ui.readCommand());
+    }
+
+    @Test
+    void printMessage() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        Ui ui = new Ui();
+        ui.printMessage("test print");
+        assertEquals("test print\n", outContent.toString());
+    }
+}
