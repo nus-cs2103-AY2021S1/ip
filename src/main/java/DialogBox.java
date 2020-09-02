309ead99579fddd96145364060b3de1ce5446c8f
Constance <constancensq@gmail.com>
2020-09-02 23:37:23 +0800

    Use FXML

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,9 +1,15 @@
+import java.io.IOException;
+import java.util.Collections;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -11,14 +17,25 @@ import javafx.scene.shape.Circle;
 
 public class DialogBox extends HBox {
 
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    public DialogBox(Label l, ImageView iv) {
-        text = l;
-        displayPicture = iv;
+    public DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        text.setWrapText(true);
+        dialog.setText(text);
+        displayPicture.setImage(img);
+
+        dialog.setWrapText(true);
 
         displayPicture.setFitWidth(100.0);
         displayPicture.setFitHeight(100.0);
@@ -26,32 +43,31 @@ public class DialogBox extends HBox {
                 displayPicture.getLayoutY() + 50, 50);
         displayPicture.setClip(circle);
 
-        this.setAlignment(Pos.CENTER_RIGHT);
-        this.getChildren().addAll(text, displayPicture);
-        this.setPadding(new Insets(0, 5, 5, 5));
-        this.setSpacing(10);
-        this.setBorder(new Border(new BorderStroke(Color.TRANSPARENT, Color.TRANSPARENT, Color.BLACK, Color.TRANSPARENT,
-                BorderStrokeStyle.NONE, BorderStrokeStyle.NONE, BorderStrokeStyle.DASHED, BorderStrokeStyle.NONE,
-                CornerRadii.EMPTY, new BorderWidths(1), new Insets(0, 10, 0, 10))));
-        this.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+//        this.getChildren().addAll(dialog, displayPicture);
+//        this.setPadding(new Insets(0, 5, 5, 5));
+//        this.setSpacing(10);
+//        this.setBorder(new Border(new BorderStroke(Color.TRANSPARENT, Color.TRANSPARENT, Color.BLACK, Color.TRANSPARENT,
+//                BorderStrokeStyle.NONE, BorderStrokeStyle.NONE, BorderStrokeStyle.DASHED, BorderStrokeStyle.NONE,
+//                CornerRadii.EMPTY, new BorderWidths(1), new Insets(0, 10, 0, 10))));
+//        this.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
     }
 
     /**
-     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     * Flips the dialog box such that the ImageView is on the left and dialog on the right.
      */
     private void flip() {
         this.setAlignment(Pos.CENTER_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
+        Collections.reverse(tmp);
         this.getChildren().setAll(tmp);
     }
 
-    public static DialogBox getUserDialog(Label l, ImageView iv) {
-        return new DialogBox(l, iv);
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
     }
 
-    public static DialogBox getDukeDialog(Label l, ImageView iv) {
-        var db = new DialogBox(l, iv);
+    public static DialogBox getDukeDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
         db.flip();
         return db;
     }
