275935faea4530204f7a82475cc360ea37585916
Gabriel Sim <gabrielsimbingyang@gmail.com>
2020-09-03 22:08:58 +0800

    Implement GUI level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,8 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * A personalized chat bot that is designed to help users manage their schedule. There are three
  * different types of listings - ToDos, Deadlines and Events.
@@ -7,13 +12,23 @@
  * @since 2020-09-02
  */
 
-public class Duke {
+public class Duke extends Application {
 
-  public static void main(String[] args) {
+  @Override
+  public void start(Stage stage) {
+    Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+    Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
-    Bot bot = new Bot();
-    bot.serve();
-
+    stage.setScene(scene); // Setting the stage to show our screen
+    stage.show(); // Render the stage.
   }
+
+//  public static void main(String[] args) {
+//
+//    Bot bot = new Bot();
+//    bot.serve();
+//
+//  }
+
 }
 
