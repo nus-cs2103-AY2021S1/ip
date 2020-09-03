105b0a6d560feb85d7745f344573ca5b15bb4d8b
xinyee20 <codelearner19@gmail.com>
2020-09-03 16:15:45 +0800

    Add constructor to Launcher.java

--- src/main/java/willy/ui/Launcher.java
+++ src/main/java/willy/ui/Launcher.java
@@ -6,6 +6,9 @@ import javafx.application.Application;
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
+
+    public Launcher() {}
+    
     public static void main(String[] args) {
         Application.launch(Willy.class, args);
     }
