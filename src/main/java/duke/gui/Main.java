8dbbd62e18fb716aa20258ffa263b9e52be81d76
Terence Ho <terencehwy@gmail.com>
2020-09-03 21:01:34 +0800

    Add window resizing functionality to GUI

--- src/main/java/duke/gui/Main.java
+++ src/main/java/duke/gui/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setResizable(true);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             fxmlLoader.<MainWindow>getController().introDuke();
             stage.show();
