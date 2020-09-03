3d3851340a35dc577ef613e45c9999ecded998ac
Gabriel Sim <gabrielsimbingyang@gmail.com>
2020-09-04 00:20:10 +0800

    Remove Main.Java

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -4,8 +4,9 @@ import javafx.application.Application;
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
+
   public static void main(String[] args) {
-    Application.launch(Main.class, args);
+    Application.launch(Duke.class, args);
   }
 
 }
