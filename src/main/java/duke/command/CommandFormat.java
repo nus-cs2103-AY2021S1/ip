d28a25f66f2390f676cd388023fd092b99e7f407
Ma-Yueran <mayuerantom@outlook.com>
2020-09-04 01:02:49 +0800

    Add CommandFormat.java

---
+++ src/main/java/duke/command/CommandFormat.java
@@ -0,0 +1,28 @@
+package duke.command;
+
+public class CommandFormat {
+
+    public static final String EXIT_CMD_FORMAT =
+            "bye";
+
+    public static final String LIST_CMD_FORMAT =
+            "list";
+
+    public static final String DEADLINE_CMD_FORMAT =
+            "deadline <task description> /by <time>";
+
+    public static final String EVENT_CMD_FORMAT =
+            "event <task description> /at <time>";
+
+    public static final String TODO_CMD_FORMAT =
+            "todo <task description>";
+
+    public static final String DONE_CMD_FORMAT =
+            "done <task index>";
+
+    public static final String DELETE_CMD_FORMAT =
+            "delete <task index>";
+
+    public static final String FIND_CMD_FORMAT =
+            "find <keyword>";
+}
