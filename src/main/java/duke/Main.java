b32b8becd5e0655f2ba5a7d4f67600585c8eb609
ZoroarkDarkrai <fidellawi@gmail.com>
2020-09-03 23:44:05 +0800

    Fix dialog box

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -1,12 +1,12 @@
 package duke;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * A GUI for Duke using FXML.
@@ -25,7 +25,6 @@ public class Main extends Application {
             stage.setTitle("Zoroark");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
-            
         } catch (IOException e) {
             e.printStackTrace();
         }
