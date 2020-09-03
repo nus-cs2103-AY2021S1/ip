814edd5654777b5a6e9ef6bc8186d9c5a1f7320d
Jiang <20010421jsy@gmail.com>
2020-09-03 15:43:06 +0800

    Level-10 - Final

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -21,24 +21,33 @@ public class Ui {
         System.out.println("    How may I help you today?");
         System.out.println("    -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-");
     }
-
+    
     /**
-     * Prints out "noproblem" messages to the user.
+     * Prints out "no problem" messages to the user.
      */
     public void noProblem() {
         System.out.println("     No problem! I've added this task to the list:");
     }
 
+    public String noProblemMessage() {
+        return "     No problem! I've added this task to the list:\n";
+    }
+    
     /**
-     * Prints out "markasdone" messages to the user.
+     * Prints out "mark as done" messages to the user.
      */
     public void markAsDone(Task thisTask) {
         System.out.println("     Nice! I've marked this task as done:");
         System.out.println("       " + thisTask.toString());
     }
 
+    public String markAsDoneMessage(Task thisTask) {
+        return "     Nice! I've marked this task as done:\n"
+                + "       " + thisTask.toString() + "\n";
+    }
+
     /**
-     * Prints out "deletedtask" messages to the user.
+     * Prints out "deleted task" messages to the user.
      */
     public void deletedTask(Task thisTask, ArrayList<Task> list) {
         System.out.println("     Sure! I've removed this task for you:");
@@ -46,14 +55,25 @@ public class Ui {
         System.out.println("     Now you have " + list.size() + " tasks in the list.");
     }
 
+    public String deletedTaskMessage(Task thisTask, ArrayList<Task> list) {
+        return "     Sure! I've removed this task for you:\n"
+                + "       " + thisTask.toString() + "\n"
+                + "     Now you have " + list.size() + " tasks in the list.\n";
+    }
+
     /**
-     * Prints out "updatedtask" messages to the user.
+     * Prints out "updated task" messages to the user.
      * 
      * @param list The list of tasks.
      */
     public void updatedTask(ArrayList<Task> list) {
         System.out.println("       " + list.get(list.size() - 1));
         System.out.println("     Now you have " + list.size() + " tasks in the list.");
+    }
+
+    public String updatedTaskMessage(ArrayList<Task> list) {
+        return "       " + list.get(list.size() - 1) + "\n"
+                + "     Now you have " + list.size() + " tasks in the list.\n";
     }
 
     /**
@@ -66,6 +86,16 @@ public class Ui {
             thisTask = list.get(i - 1);
             System.out.println("     " + i + "." + thisTask.toString());
         }
+    }
+
+    public String printListMessage(ArrayList<Task> list) {
+        Task thisTask;
+        String output = "     Here are the tasks in your list:\n";
+        for (int i = 1; i <= list.size(); i++) {
+            thisTask = list.get(i - 1);
+            output = output + "     " + i + "." + thisTask.toString() + "\n";
+        }
+        return output;
     }
 
     /**
@@ -86,6 +116,19 @@ public class Ui {
         }
     }
 
+    public String printFoundTaskMessage(String keyword, ArrayList<Task> list) {
+        Task thisTask;
+        String output = "     Here are the matching tasks in your list:\n";
+        for (int i = 1, j = 1; i <= list.size(); i++) {
+            thisTask = list.get(i - 1);
+            if (thisTask.taskname.contains(keyword)) {
+                output = output + "     " + j + "." + thisTask.toString() + "\n";
+                j++;
+            }
+        }
+        return output;
+    }
+
     /**
      * Prints out the a line to the user.
      */
@@ -100,12 +143,20 @@ public class Ui {
         System.out.println(ex.getMessage());
     }
 
+    public String printErrorMessage(DukeException ex) {
+        return ex.getMessage() + "\n";
+    }
+    
     /**
-     * Prints out byebye messages to the user.
+     * Prints out bye bye messages to the user.
      */
     public void bye() {
         System.out.println("    -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-");
         System.out.println("    Bye bye~ See ya!");
         System.out.println("    -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-");
+    }
+
+    public String byeMessage() {
+        return "    Bye bye~ See ya!\n";
     }
 }
