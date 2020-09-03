9115f5885338e729bf958ac29eb867650bc9ae27
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-09-03 13:34:34 +0800

    Add JavaFx tutorial part 4 features

--- src/main/java/gui/Launcher.java
+++ src/main/java/gui/Launcher.java
@@ -1,4 +1,4 @@
-package duke.javafx;
+package gui;
 
 import javafx.application.Application;
 
@@ -7,6 +7,6 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(StageSetter.class, args);
+        Application.launch(Main.class, args);
     }
 }
