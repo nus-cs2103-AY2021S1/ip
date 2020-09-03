23d5fd18f5e177bba9eeb93aeb25610d22f1f27a
rolandyuwy <rolandyuwenyang@hotmail.com>
2020-09-03 23:24:45 +0800

    Update test files in test-ui-test and ToDoTest.java

--- src/test/java/ToDoTest.java
+++ src/test/java/ToDoTest.java
@@ -1,10 +1,11 @@
+import duke.ToDo;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ToDoTest {
     @Test
     public void testGetStatusIcon() {
-        assertEquals("X", new ToDo("sleep").getStatusIcon());
+        assertEquals("not done", new ToDo("sleep").getStatus());
     }
 
     @Test
--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
-dir /s /B ..\src\main\java\*.java > sources.txt
+dir /s /B ..\src\main\java\duke\*.java > sources.txt
 javac  -cp ..\src -Xlint:none -d ..\bin @sources.txt
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
