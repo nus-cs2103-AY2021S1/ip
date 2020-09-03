9bc7fbdacf75fa236cbb4211671617d28a64b990
Jerry Ho <jerryho@u.nus.edu>
2020-09-03 18:13:42 +0800

    Create Command.java

---
+++ src/main/java/Command.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Command {
+    public static final int EXIT = 0;
+    public static final int LIST = 1;
+    public static final int DONE = 2;
+    public static final int DELETE = 3;
+    public static final int INVALID = 99;
+    public static final int CREATE_TODO = 11;
+    public static final int CREATE_DEADLINE = 12;
+    public static final int CREATE_EVENT = 13;
+
+    private int commandType;
+    private AdditionalInfo info;
+
+    public Command(int commandType, AdditionalInfo additionalInfo) {
+        this.commandType = commandType;
+        this.info = additionalInfo;
+    }
+
+    public Command(int commandType) {
+        this.commandType = commandType;
+    }
+
+    public int getCommandType() {
+        return this.commandType;
+    }
+
+    public AdditionalInfo getAdditionalInfo() {
+        return this.info;
+    }
+}
