373e0ed69ed894393b070c54799e7de9cf03c694
JoeTan <tjwjoe1998@gmail.com>
2020-09-04 14:10:12 +0800

    Merge branch 'master' into branch-A-CodeQuality

--- src/main/java/commands/AddCommand.java
+++ src/main/java/commands/AddCommand.java
@@ -41,6 +41,8 @@ public class AddCommand extends Command {
     public void exec(TaskList tasks, Ui ui, Storage storage) throws InvalidTaskException, UnknownCmdException,
             InvalidTimeException, BadDtFormatException {
         String[] info = extractStringInfo(text);
+        assert ui != null : "Null UI";
+        assert tasks != null : "Null TaskList";
         if (info[0].equals("todo")) {
             tasks.addItem(new Todo(info[1], false));
             ui.setMessageNewTask(info[1], tasks.size());
@@ -61,13 +63,13 @@ public class AddCommand extends Command {
      * @throws UnknownCmdException If an unknown command is entered.
      * @throws InvalidTimeException If an invalid time is entered for a Deadline or Event task.
      */
-
     public String[] extractStringInfo(String str) throws InvalidTaskException, UnknownCmdException,
             InvalidTimeException {
+        assert str != null : "Input is null!";
         String[] store = new String[3];
         // Handling the classification of event type
         if (str.startsWith("todo")) {
-            if (str.equals("todo") || str.strip().equals("todo")) {
+            if (str.strip().equals("todo")) {
                 throw new InvalidTaskException("Your task cannot be empty!");
             } else if (!str.startsWith("todo ")) {
                 throw new UnknownCmdException("Unknown command entered!");
@@ -75,7 +77,7 @@ public class AddCommand extends Command {
                 store[0] = "todo";
             }
         } else if (str.startsWith("deadline")) {
-            if (str.equals("deadline") || str.strip().equals("deadline")) {
+            if (str.strip().equals("deadline")) {
                 throw new InvalidTaskException("Your task cannot be empty!");
             } else if (!str.startsWith("deadline ")) {
                 throw new UnknownCmdException("Unknown command entered!");
@@ -83,7 +85,7 @@ public class AddCommand extends Command {
                 store[0] = "deadline";
             }
         } else if (str.startsWith("event")) {
-            if (str.equals("event") || str.strip().equals("event")) {
+            if (str.strip().equals("event")) {
                 throw new InvalidTaskException("Your task cannot be empty!");
             } else if (!str.startsWith("event ")) {
                 throw new UnknownCmdException("Unknown command entered!");
@@ -93,8 +95,7 @@ public class AddCommand extends Command {
         } else {
             throw new UnknownCmdException("Unknown command entered!");
         }
-
-        // handling the content of the event
+        // Handling the content of the event
         int splitPrefix = str.indexOf(" ");
         String content = str.substring(splitPrefix).strip();
         if (content.length() <= 0) {
