1ed60c95b80d166c3750ffb69d664172b8141748
Rayson Koh <rayson.rk.koh@gmail.com>
2020-09-04 23:50:05 +0800

    Add storage test for windows OS due to filepath error

--- src/test/java/duke/StorageTest.java
+++ src/test/java/duke/StorageTest.java
@@ -2,6 +2,9 @@ package duke;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.condition.OS.LINUX;
+import static org.junit.jupiter.api.condition.OS.MAC;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,19 +15,33 @@ import java.util.List;
 import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 
 import duke.exception.DukeException;
 
 public class StorageTest {
 
     @Test
-    public void load_noFile_throwDukeException() {
+    @EnabledOnOs({LINUX, MAC})
+    public void load_noFileNix_throwDukeException() {
         try {
             Storage storage = new Storage("data/storageTest.txt");
             storage.load();
             fail();
         } catch (DukeException e) {
             assertEquals("PROJ_ROOT/data/storageTest.txt not found!", e.getMessage());
+        }
+    }
+
+    @Test
+    @EnabledOnOs({WINDOWS})
+    public void load_noFileWindows_throwDukeException() {
+        try {
+            Storage storage = new Storage("data/storageTest.txt");
+            storage.load();
+            fail();
+        } catch (DukeException e) {
+            assertEquals("PROJ_ROOT/data\\storageTest.txt not found!", e.getMessage());
         }
     }
 
