4a70b37e8b44d3e5b4f099ffb7ceabd73a910de0
Tan Zhuo Yao <e0406711@u.nus.edu>
2020-09-04 14:55:01 +0800

    Add assert statements

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -2,7 +2,14 @@ package duke;
 
 import javafx.application.Application;
 
+/**
+ * Launches the application and opens up GUI.
+ */
 public class Launcher {
+    /**
+     * Main method to start Duke chat bot.
+     * @param args Args
+     */
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
