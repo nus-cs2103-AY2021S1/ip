409faa74e6cc78fcd03187f444cb7e8b69d82040
maguireong <maguire2622@yahoo.com.sg>
2020-09-04 13:15:37 +0800

    Add shadowjar

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -38,13 +38,13 @@ public class Duke extends Application {
     private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
-    /**
-     * Calls method run().
-     * @param args expecting the array of objects.
-     */
-    public static void main(String[] args) throws IOException {
-        new Duke();
-    }
+//    /**
+//     * Calls method run().
+//     * @param args expecting the array of objects.
+//     */
+//    public static void main(String[] args) throws IOException {
+//        new Duke();
+//    }
 
     /**
      * Creates a Duke.
