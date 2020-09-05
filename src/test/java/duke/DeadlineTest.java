33e9fe4423a4ec1539768717581078137ac9e9fb
iamjamestan <jamestanyuli@gmail.com>
2020-09-05 20:38:04 +0800

    Add javadocs for tests

--- src/test/java/duke/DeadlineTest.java
+++ src/test/java/duke/DeadlineTest.java
@@ -6,7 +6,13 @@ import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Class that encapsulates tests for the Deadline class.
+ */
 public class DeadlineTest {
+    /**
+     * Tests the toString() method for Deadline.
+     */
     @Test
     public void toString_test_formattedString() {
         Task t = new Deadline("test", true, LocalDate.parse("2020-03-19"));
--- src/test/java/duke/TaskTest.java
+++ src/test/java/duke/TaskTest.java
@@ -4,7 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Class that encapsulates tests for the Task class.
+ */
 public class TaskTest {
+    /**
+     * Tests the toString() method for Task.
+     */
     @Test
     public void toString_normal_formattedString() {
         Task t = new Task("test");
