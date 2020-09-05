fcf67c1d18e6023c31e1b53d978f436fd2b955d3
Lee Ming De <leemingde@gmail.com>
2020-09-05 15:36:16 +0800

    Fix bug in MainWindow
    Changed the path from /image/DaUser to /image/daUser

--- src/main/java/duke/gui/MainWindow.java
+++ src/main/java/duke/gui/MainWindow.java
@@ -29,8 +29,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/daUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/daDuke.png"));
 
     /**
      * Initializes the dialog container and display greeting message.
