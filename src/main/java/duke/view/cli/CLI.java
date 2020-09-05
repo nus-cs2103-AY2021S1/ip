79efca05cf23745e796256d081d5699050462010
geoboom <leegeorgie52@gmail.com>
2020-09-05 12:22:01 +0800

    Add CLI Observer to separate rendering of text from program logic

---
+++ src/main/java/duke/view/cli/CLI.java
@@ -0,0 +1,12 @@
+package duke.view.cli;
+
+public class CLI {
+
+  public CLI() {
+    System.out.println("Hi. Duke here.\nHow can I help you?");
+  }
+
+  public void update(String s) {
+    System.out.println(s);
+  }
+}
