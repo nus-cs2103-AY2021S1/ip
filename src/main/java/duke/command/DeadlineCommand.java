649cb22d588e77f56a50e3fa53e5171322284d52
geoboom <leegeorgie52@gmail.com>
2020-09-05 12:22:17 +0800

    Add Commands as abstraction

---
+++ src/main/java/duke/command/DeadlineCommand.java
@@ -0,0 +1,24 @@
+package duke.command;
+
+import duke.duke.Duke;
+
+public class DeadlineCommand extends Command {
+
+  private final String desc;
+  private final String by;
+
+  public DeadlineCommand(String desc, String by) {
+    this.desc = desc;
+    this.by = by;
+  }
+
+  @Override
+  public void execute(Duke duke) {
+    duke.addDeadlineTask(desc, by);
+  }
+
+  @Override
+  public void undo(Duke duke) {
+    // stub
+  }
+}
