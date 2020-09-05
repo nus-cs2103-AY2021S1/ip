f1364dc8a6da03ae2d33461fc68534eb1e3aa44c
Bobby Law <zeranium97@gmail.com>
2020-09-05 23:22:27 +0800

    Improve UI

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -38,6 +39,7 @@ public class DialogBox extends HBox {
         }
         dialog.setText(text);
         displayPicture.setImage(img);
+        displayPicture.setClip(new Circle(50,50,45));
     }
 
     /**
