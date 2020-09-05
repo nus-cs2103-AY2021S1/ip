cd854f22cf30601991cdea6703f1a4a458a538c5
IsaacTin <tin.isaac@yahoo.com.sg>
2020-09-06 02:36:29 +0800

    Fix CI

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,6 +12,11 @@ then
     rm ACTUAL.TXT
 fi
 
+if [ -e "./data/duke.txt" ]
+then
+    rm ./data/duke.txt
+fi
+
 # compile the code into the bin folder, terminates if error occurred
 if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/duke/*.java
 then
@@ -20,7 +25,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin duke.Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
