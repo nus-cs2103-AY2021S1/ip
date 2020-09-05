4f80016deeb77bdad567ac53d91c8dd239752433
peter-yeh <yehyuchun2@gmail.com>
2020-09-05 13:51:35 +0800

    Use SLAP to abstract Tasks class
    Abstracted methods which were commonly used and moved it to the Task
    class, to reduce duplicity.

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,38 +1,37 @@
 package duke.task;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
     protected LocalDate at;
 
     /**
-     * Creates a type of task with timing
+     * Creates a type of task with timing.
      *
-     * @param description detail of the task
-     * @param at          time in yyyy-mm-dd
+     * @param description detail of the task.
+     * @param at          time in yyyy-mm-dd.
      */
     public Event(String description, String at) {
         super(description);
-        this.at = LocalDate.parse(at.trim());
+        this.at = convertToLocalDate(at);
     }
 
 
     public Event(String description, boolean isDone, String at) {
         super(description, isDone);
-        this.at = LocalDate.parse(at.trim());
+        this.at = convertToLocalDate(at);
     }
 
 
     /**
-     * Overrides the toString method
+     * Overrides the toString method.
      *
-     * @return a custom event description
+     * @return a custom event description.
      */
     @Override
     public String toString() {
         return "[E][" + super.getStatusIcon() + "] " + super.description + " (at: "
-                + this.at.format(DateTimeFormatter.ofPattern("MMM dd yyy")) + ")";
+                + convertToString(this.at) + ")";
     }
 
 
