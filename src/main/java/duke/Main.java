89edad71b23fdc6e92ebbf4fb40575f1abac1571
ktaekwon000 <ktaekwon000@gmail.com>
2020-09-06 00:16:01 +0800

    minor UI changes

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
