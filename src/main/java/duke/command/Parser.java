ae1e8673a59acee09180f0671edf7c16839e745b
“pangpuncake” <pangpuncake@gmail.comssh-keygen -t rsa -C pangpuncake@gmail.comSHA256:Kf5gd25novZ4tCYTSJtNXvQDJ3p2yAQnn7XU7Dioj1Aexitgit config --global user.email pangpuncake@gmail.com>
2020-09-05 16:53:45 +0800

    Update if statements to else if statements in Parser.

--- src/main/java/duke/command/Parser.java
+++ src/main/java/duke/command/Parser.java
@@ -51,19 +51,18 @@ public class Parser {
     public String processCommand(String command) throws DukeException {
         Pattern pattern = Pattern.compile("^(.*?)\\s(.*?)(?:\\s/..\\s?(.*))?$");
         Matcher matcher = pattern.matcher(command);
-        if (command.equals("list")) {
+        if (command.equals("")) {
+            return "Please provide an input!";
+        } else if (command.equals("list")) {
             return this.list.toString();
-        }
-        if (command.equals("hello")) {
+        } else if (command.equals("hello")) {
             return "Hi! I'm Duke! Pleasure to meet you :)";
-        }
-        if (command.equals("bye")) {
+        } else if (command.equals("bye")) {
             this.isExit = true;
             Storage storage = new Storage();
             storage.updateFile(this.list);
             return "Bye! Hope to see you again soon!";
-        }
-        if (command.equals("help")) {
+        } else if (command.equals("help")) {
             return "Accepted commands:\n"
                     + "hello - hello!\n"
                     + "list - show current list\n"
@@ -76,8 +75,7 @@ public class Parser {
                     + "done <index> - mark the specified task as done\n"
                     + "undo <index> - mark the specified task as not done\n"
                     + "delete <index> - deletes the specified task from the list";
-        }
-        if (matcher.find()) {
+        } else if (matcher.find()) {
             String com = matcher.group(1);
             String task = matcher.group(2);
             String date = matcher.group(3);
@@ -96,11 +94,11 @@ public class Parser {
                 return this.processTask(com, task, date);
             default:
                 throw new DukeException("Sorry, I did not understand: " + command
-                        + ".\nUse \"help\" to look at available commands.");
+                        + "\nUse \"help\" to look at available commands.");
             }
         }
         throw new DukeException("Sorry, I did not understand: " + command
-                + ".\nUse \"help\" to look at available commands.");
+                + "\nUse \"help\" to look at available commands.");
     }
 
     /**
