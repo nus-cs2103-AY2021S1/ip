3b1028b745e252235eb6f609d8f206a4718de921
sebastiantoh <sebas.tsj.98@gmail.com>
2020-09-05 11:57:13 +0800

    Update example for inputting date

--- src/main/java/duke/exception/InvalidDateInputException.java
+++ src/main/java/duke/exception/InvalidDateInputException.java
@@ -12,6 +12,6 @@ public class InvalidDateInputException extends DukeException {
      */
     public InvalidDateInputException(String date) {
         super(String.format("%s is not a recognised date format. Please key in dates in the format "
-                + "yyyy-MM-dd. For example, 2007-12-03.", date));
+                + "yyyy-MM-dd. For example, 2007-03-31.", date));
     }
 }
