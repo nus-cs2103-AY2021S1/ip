119a612f1ced1c07fec311130fffb7f00f218328
siddarth2824 <siddarth2824@gmail.com>
2020-09-06 01:15:05 +0800

    Add helper methods in CommandLineInterfaceParser

--- src/main/java/duke/parser/CommandLineInterfaceParser.java
+++ src/main/java/duke/parser/CommandLineInterfaceParser.java
@@ -23,16 +23,11 @@ public class CommandLineInterfaceParser {
             CommandTypes commandType = CommandTypes.valueOf(words[0].toUpperCase());
             commandType.checkInput(userInput.toLowerCase());
             String content;
-            String dateTime;
-            String[] userInputArgs;
-            DukeDateTime dukeDateTime;
             switch (commandType) {
             case LIST:
-                ListTasksCommand listTasks = new ListTasksCommand();
-                return listTasks;
+                return newListTasksCommand();
             case BYE:
-                ExitCommand exitDuke = new ExitCommand();
-                return exitDuke;
+                return newExitCommand();
             case TODO:
                 content = userInput.replaceFirst("^(?i)todo", "");
                 String trimmedContent = content.trim();
@@ -40,58 +35,104 @@ public class CommandLineInterfaceParser {
                 return addTask;
             case DEADLINE:
                 userInput = userInput.replaceFirst("^(?i)deadline", "");
-                userInputArgs = userInput.split("\\s*/by\\s*");
-                content = userInputArgs[0].trim();
-                dateTime = userInputArgs[1];
-                dukeDateTime = DateTimeParser.parseDateTime(dateTime);
-                AddCommand addDeadline = new AddCommand(new Deadline(content, dukeDateTime));
-                return addDeadline;
+                return newAddTaskCommand(CommandTypes.DEADLINE, userInput);
             case EVENT:
                 userInput = userInput.replaceFirst("^(?i)event", "");
-                userInputArgs = userInput.split("\\s*/at\\s*");
-                content = userInputArgs[0].trim();
-                dateTime = userInputArgs[1];
-                dukeDateTime = DateTimeParser.parseDateTime(dateTime);
-                AddCommand addEvent = new AddCommand(new Event(content, dukeDateTime));
-                return addEvent;
+                return newAddTaskCommand(CommandTypes.EVENT, userInput);
             case DONE:
-                try {
-                    int completedTaskIndex = Integer.parseInt(words[1]);
-                    DoneCommand completedTask = new DoneCommand(completedTaskIndex);
-                    return completedTask;
-                } catch (IndexOutOfBoundsException e) {
-                    throw new DukeException(Messages.INVALID_TASK_INDEX_ERROR_MESSAGE);
-                }
+                int completedTaskIndex = Integer.parseInt(words[1]);
+                return newDoneCommand(completedTaskIndex);
             case DELETE:
-                try {
-                    int taskIndex = Integer.parseInt(words[1]);
-                    DeleteCommand deleteTask = new DeleteCommand(taskIndex);
-                    return deleteTask;
-                } catch (IndexOutOfBoundsException e) {
-                    throw new DukeException(Messages.INVALID_TASK_INDEX_ERROR_MESSAGE);
-                }
+                int deletedTaskIndex = Integer.parseInt(words[1]);
+                return newDeleteCommand(deletedTaskIndex);
             case FIND:
                 content = userInput.replaceFirst("^(?i)find", "");
-                trimmedContent = content.trim();
-                FindCommand findTaskCommand = new FindCommand(trimmedContent);
-                return findTaskCommand;
+                return newFindCommand(content);
             case TODAY:
-                TodayTasksCommand tasksForToday = new TodayTasksCommand();
-                return tasksForToday;
+                return newTodayTasksCommand();
             case UNCOMPLETED:
-                ListAllUncompletedTasksCommand uncompletedTasksCommand = new ListAllUncompletedTasksCommand();
-                return uncompletedTasksCommand;
+               return newListAllUncompletedTasksCommand();
             case COMPLETED:
-                ListAllCompletedTasksCommand completedTasksCommand = new ListAllCompletedTasksCommand();
-                return completedTasksCommand;
+                return newListAllCompletedTasksCommand();
             case OVERDUE:
-                OverdueCommand overdueTasksCommand = new OverdueCommand();
-                return overdueTasksCommand;
+                return newOverdueCommand();
             default:
                 throw new DukeException(Messages.INVALID_COMMAND_INPUT_MESSAGE);
             }
         } catch (IllegalArgumentException e) {
             throw new DukeException(Messages.INVALID_COMMAND_INPUT_MESSAGE);
         }
+    }
+
+    private static ListTasksCommand newListTasksCommand() {
+        return new ListTasksCommand();
+    }
+
+    private static ExitCommand newExitCommand() {
+        return new ExitCommand();
+    }
+
+    private static DoneCommand newDoneCommand(int completedTaskIndex) throws DukeException {
+        try {
+            DoneCommand doneCommand = new DoneCommand(completedTaskIndex);
+            return doneCommand;
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException(Messages.INVALID_TASK_INDEX_ERROR_MESSAGE);
+        }
+    }
+
+    private static DeleteCommand newDeleteCommand(int deletedTaskIndex) throws DukeException {
+        try {
+            DeleteCommand deleteCommand = new DeleteCommand(deletedTaskIndex);
+            return deleteCommand;
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException(Messages.INVALID_TASK_INDEX_ERROR_MESSAGE);
+        }
+    }
+
+    private static AddCommand newAddTaskCommand(CommandTypes taskType, String userInputContent) throws DukeException {
+        switch (taskType) {
+        case TODO:
+            String trimmedContent = userInputContent.trim();
+            return new AddCommand(new Todo(trimmedContent));
+        case EVENT:
+            String[] userInputEventArgs = userInputContent.split("\\s*/at\\s*");
+            String content = userInputEventArgs[0].trim();
+            String eventDateTime = userInputEventArgs[1];
+            DukeDateTime eventDukeDateTime = DateTimeParser.parseDateTime(eventDateTime);
+            AddCommand addEvent = new AddCommand(new Event(content, eventDukeDateTime));
+            return addEvent;
+        case DEADLINE:
+            String[] userInputDeadlineArgs = userInputContent.split("\\s*/by\\s*");
+            content = userInputDeadlineArgs[0].trim();
+            String deadlineDateTime = userInputDeadlineArgs[1];
+            DukeDateTime deadlineDukeDateTime = DateTimeParser.parseDateTime(deadlineDateTime);
+            AddCommand addDeadline = new AddCommand(new Deadline(content, deadlineDukeDateTime));
+            return addDeadline;
+        default:
+            throw new DukeException(Messages.INVALID_COMMAND_INPUT_MESSAGE);
+        }
+    }
+
+    private static FindCommand newFindCommand(String keyword) {
+        String trimmedKeyword = keyword.trim();
+        FindCommand newFindCommand = new FindCommand(trimmedKeyword);
+        return newFindCommand;
+    }
+
+    private static TodayTasksCommand newTodayTasksCommand() {
+        return new TodayTasksCommand();
+    }
+
+    private static ListAllUncompletedTasksCommand newListAllUncompletedTasksCommand() {
+        return new ListAllUncompletedTasksCommand();
+    }
+
+    private static ListAllCompletedTasksCommand newListAllCompletedTasksCommand() {
+        return new ListAllCompletedTasksCommand();
+    }
+
+    private static OverdueCommand newOverdueCommand() {
+        return new OverdueCommand();
     }
 }
