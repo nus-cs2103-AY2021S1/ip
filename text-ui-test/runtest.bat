86082448bbf503b5b442d0b10fd7bbb63d4274c3
IsaacTin <tin.isaac@yahoo.com.sg>
2020-09-06 02:43:22 +0800

    Fix CI

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,13 +1,17 @@
 @ECHO OFF
 
+set JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
+
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 del ACTUAL.TXT
 
+del .\data\duke.txt
+
 REM compile the code into the bin folder
-javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\duke.Duke.java
+javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/duke/*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
