3848a204c315feaa0173e1cc9ec40db917c4b0bf
jiaweiteo <jiawei.teo@hotmail.com>
2020-09-05 18:47:31 +0800

    Refactor Test class

--- src/test/java/seedu/duke/UiTest.java
+++ src/test/java/seedu/duke/UiTest.java
@@ -1,10 +1,14 @@
 package seedu.duke;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
 
 public class UiTest {
     @Test
