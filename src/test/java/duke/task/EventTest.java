46035f536a26b37fd32dfb417c20de62edd47055
Ben-Hanan <benhanan98@gmail.com>
2020-09-05 18:01:11 +0800

    fix imports according to checkstyle

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -1,9 +1,11 @@
 package duke.task;
 
-import duke.exception.DukeException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import duke.exception.DukeException;
 
 class EventTest {
 
