11d6d1e9bbe139721e9268e9a1215130bb71b8b0
Liu-2001 <1034368684@qq.com>
2020-09-07 00:35:10 +0800

    add methods to Parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -28,46 +28,29 @@ public class Parser {
             return ui.list();
         } else if(input.equals("bye")) {
             isFinished = true;
-            return ui.showEnd();
-        } else if(input.equals("todo")) {
-            throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
-        } else if(input.equals("deadline")) {
-            throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
-        } else if(input.equals("event")) {
-            throw new DukeException("OOPS!!! The description of an event cannot be empty.");
+            return Ui.showEnd();
+        } else if(input.equals("todo") || input.equals("deadline") || input.equals("event")) {
+            throw new DukeException("OOPS!!! The description cannot be empty.");
+
         } else if(input.startsWith("todo ")) {
-            description = input.substring(5);
-            tasks.addTask(new Todo(description));
-            return "Got it. I've added this task:\n"
-                    + "  " + tasks.get(tasks.count() - 1).toString()
-                    + "\nNow you have " + (tasks.count()) + " tasks in the list.";
+            return handleAddCommand("todo ", input);
+
         } else if(input.startsWith("deadline ")) {
-            description = input.substring(9);
-            location = description.indexOf("/");
-            date = description.substring(location + 4);
-            time = LocalDate.parse(date);
-            description = description.substring(0, location - 1);
-            tasks.addTask(new Deadline(description, time));
-            return "Got it. I've added this task:\n"
-                    + "  " + tasks.get(tasks.count() - 1).toString()
-                    + "\nNow you have " + (tasks.count()) + " tasks in the list.";
+            return handleAddCommand("deadline ", input);
+
         } else if(input.startsWith("event ")) {
-            description = input.substring(6);
-            location = description.indexOf("/");
-            date = description.substring(location + 4);
-            time = LocalDate.parse(date);
-            description = description.substring(0, location - 1);
-            tasks.addTask(new Event(description, time));
-            return "Got it. I've added this task:\n"
-                    + "  " + tasks.get(tasks.count() - 1).toString()
-                    + "\nNow you have " + (tasks.count()) + " tasks in the list.";
+            return handleAddCommand("event ", input);
+
         } else if(input.startsWith("done ")) {
+
             description = input.substring(5);
             int selected = Integer.parseInt(description);
             tasks.get(selected - 1).markAsDone();
             return "Nice! I've marked this task as done:\n" + "  "
                     + tasks.get(selected - 1).toString();
+
         } else if(input.startsWith("delete ")) {
+
             description = input.substring(7);
             int selected = Integer.parseInt(description);
             Task task = tasks.get(selected - 1);
@@ -75,13 +58,39 @@ public class Parser {
             return "Noted. I've removed this task:\n"
                     + "  " + task.toString()
                     + "\nNow you have " + (tasks.count()) + " tasks in the list.";
+
         } else if(input.startsWith("find ")) {
             description = input.substring(5);
             TaskList matchingTasks = tasks.findTasks(description);
             return ui.find(matchingTasks);
+
         } else {
             throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
+    }
+
+    private String handleAddCommand(String type, String input) {
+        description = input.substring(type.length());
+        if(type.equals("todo ")) {
+            tasks.addTask(new Todo(description));
+
+        } else if(type.equals("deadline ")) {
+            location = description.indexOf("/");
+            date = description.substring(location + 4);
+            time = LocalDate.parse(date);
+            description = description.substring(0, location - 1);
+            tasks.addTask(new Deadline(description, time));
+
+        } else {
+            location = description.indexOf("/");
+            date = description.substring(location + 4);
+            time = LocalDate.parse(date);
+            description = description.substring(0, location - 1);
+            tasks.addTask(new Event(description, time));
+        }
+        return "Got it. I've added this task:\n"
+                + "  " + tasks.get(tasks.count() - 1).toString()
+                + "\nNow you have " + (tasks.count()) + " tasks in the list.";
     }
 
     public boolean isFinished() {
