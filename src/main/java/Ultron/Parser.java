0d8689ab88c71b42a8438ab4ca49e1c6863d7e5c
Junhua <wenjh1998@gmail.com>
2020-09-06 12:18:40 +0800

    Remove redundant comments

--- src/main/java/Ultron/Parser.java
+++ src/main/java/Ultron/Parser.java
@@ -35,33 +35,19 @@ public final class Parser {
     private static Command checkInput(final String command,
                                       final String arguments)
         throws UltronException {
-
-        //Switch case to process the commands
         switch (command) {
-        //If the user keys in bye
         case "bye":
             return new ByeCommand(arguments);
-
         case "find":
             return new FindCommand(arguments);
-
-        //If the user keys in list
         case "list":
             return new ListCommand(arguments);
-
-        //Check if the user is asking for help
         case "help":
             return new HelpCommand(arguments);
-
-        //Check if the user is deleting some of the items
         case "delete":
             return new DeleteCommand(arguments);
-
-        //Check if the user is done with any task
         case "done":
             return new DoneCommand(arguments);
-
-        //Otherwise it will be a task to be added
         default:
             return new TaskAllocator(command, arguments);
         }
@@ -76,15 +62,10 @@ public final class Parser {
      * @throws UltronException if there is an error parsing the integer.
      */
     public static int parseInteger(final String args) throws UltronException {
-        //Catch any errors in the number
         try {
-
-            //Get the index of the items
             return Integer.parseInt(args) - 1;
 
         } catch (NumberFormatException e) {
-
-            //Throw a new Ultron exception
             throw new UltronException(args, ExceptionType.INVALID_NUMBER);
         }
     }
@@ -98,22 +79,12 @@ public final class Parser {
      */
     public static Command parseCommand(final String input)
         throws UltronException {
-        //Use regex to get the grp
         Matcher inputs = PATTERN.matcher(input);
-
-        //Find the items in the group
         if (!inputs.find()) {
-
-            //Throw an invalid command error if it is unable to find any matches
             throw new UltronException(input, ExceptionType.INVALID_COMMAND);
         }
-
-        //Get the command
         String inputCommand = inputs.group(1);
-
-        //Get the other arguments
         String arguments = inputs.group(2);
-
         return checkInput(inputCommand, arguments);
     }
 
