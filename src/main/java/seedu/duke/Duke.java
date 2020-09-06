6e01b778def42f5e48e50a26fd70e1c14c50e05f
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2020-09-06 18:23:43 +0800

    week 5 able to generate working jar file

---
+++ src/main/java/seedu/duke/Duke.java
@@ -0,0 +1,9 @@
+package seedu.duke;
+
+import javafx.application.Application;
+
+public class Duke {
+    public static void main(String[] args) {
+        Application.launch(duke.Duke.class, args);
+    }
+}
