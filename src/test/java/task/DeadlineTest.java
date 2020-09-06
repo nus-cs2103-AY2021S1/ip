63253bb729e71c14103a4d2e29e173587e62c94e
Tan Pinxi <h1110104@nushigh.edu.sg>
2020-09-06 15:26:40 +0800

    Fix test case

--- src/test/java/task/DeadlineTest.java
+++ src/test/java/task/DeadlineTest.java
@@ -9,8 +9,8 @@ public class DeadlineTest {
     @Test
     public void constructionTest() {
         Deadline deadline1 = new Deadline("deadline1", "2020-08-23", "");
-        assertEquals(deadline1.toString(), "[D][✗] deadline1 (by: Aug 23 2020)");
+        assertEquals(deadline1.toString(), "[D][\u2717] deadline1 (by: Aug 23 2020)");
         Deadline deadline2 = new Deadline("deadline2", "2020-09-24", "18:00");
-        assertEquals(deadline2.toString(), "[D][✗] deadline2 (by: Sep 24 2020 18:00)");
+        assertEquals(deadline2.toString(), "[D][\u2717] deadline2 (by: Sep 24 2020 18:00)");
     }
 }
