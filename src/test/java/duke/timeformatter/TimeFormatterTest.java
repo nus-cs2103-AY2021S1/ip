0a5264f74b535970d90d2239b1122b386bf63ba2
moyj01 <melvinlearnprogramming@gmail.com>
2020-09-06 14:55:49 +0800

    Update shadow plugin version and reformat code to adhere to coding standards

--- src/test/java/duke/timeformatter/TimeFormatterTest.java
+++ src/test/java/duke/timeformatter/TimeFormatterTest.java
@@ -1,16 +1,16 @@
 package duke.timeformatter;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class TimeFormatterTest {
 
     @Test
     public void convertDate() {
-        String by ="2019/10/10";
+        String by = "2019/10/10";
         String date = by.trim().replaceAll("/", "-");
         LocalDate localDate = LocalDate.parse(date);
         assertTrue(localDate.equals(LocalDate.parse("2019-10-10")));
@@ -18,9 +18,9 @@ class TimeFormatterTest {
 
     @Test
     public void convertToPrettyDate() {
-        LocalDate localDate =LocalDate.parse("2019-10-10");
+        LocalDate localDate = LocalDate.parse("2019-10-10");
         String testString = TimeFormatter.prettyDate(localDate);
-        String expectedOutput="THURSDAY,OCT 10 2019";
+        String expectedOutput = "THURSDAY,OCT 10 2019";
         assertTrue(testString.equals(expectedOutput));
     }
 }
