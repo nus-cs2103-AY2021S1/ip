b528f811b7a256daf1a084787c356496db5d9e29
duynguyen24501 <duynguyen24501@gmail.com>
2020-09-06 10:05:05 +0800

    Improve GUI display

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -26,8 +26,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser1.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke1.png"));
 
     @FXML
     public void initialize() {
