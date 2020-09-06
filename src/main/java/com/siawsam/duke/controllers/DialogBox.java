f4a3d0994470032ca9d17b35a5a46ed9702ac5d7
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-09-06 12:12:24 +0800

    Add assertion to DialogBox controller

--- src/main/java/com/siawsam/duke/controllers/DialogBox.java
+++ src/main/java/com/siawsam/duke/controllers/DialogBox.java
@@ -25,6 +25,8 @@ public class DialogBox extends HBox {
     private ImageView displayPicture;
     
     private DialogBox(String text, Image img) {
+        assert text.length() > 0 : "Dialogbox cannot have empty text";
+        
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(DialogBox.class.getResource("/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
