5a68a3d1932634e345512dcec7c33675461bad51
Kacalamaka <heinrich2001.ang@gmail.com>
2020-09-07 00:03:12 +0800

    Refactor code to improve readability

--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -4,6 +4,8 @@ import storage.Storage;
 import task.TaskList;
 import ui.Ui;
 
+import java.util.stream.Stream;
+
 /**
  * ListCommand would execute the program when user specify
  * "list" as the command. This would show all the existing
@@ -30,7 +32,7 @@ public class ListCommand extends Command {
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) {
         StringBuilder sb = new StringBuilder();
-
+        
         for (int i = 0; i < tasks.size(); i++) {
             boolean isLastTask = i != tasks.size() - 1;
             if (isLastTask) {
