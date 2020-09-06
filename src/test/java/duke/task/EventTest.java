635b32a76fb7bd6f2fa3319de9f2322e96d4f916
ypinhsuan <yenpinhsuan@gmail.com>
2020-09-06 11:36:14 +0800

    Edit JUnit test

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -10,20 +10,41 @@ public class EventTest {
     @Test
     void testStringConversion() {
         assertEquals(
-                "[E][\u2717] borrow book (at: Aug 25 2020 02:30 PM)",
-                new Event("borrow book", LocalDateTime.parse("2020-08-25T14:30")).toString());
+                "[E][\u2717] borrow book (at: Aug 25 2020 02:30 PM to Aug 25 2020 04:30 PM)",
+                new Event(
+                        "borrow book",
+                        LocalDateTime.parse("2020-08-25T14:30"),
+                        LocalDateTime.parse("2020-08-25T16:30"))
+                        .toString()
+        );
         assertEquals(
-                "[E][\u2713] return book (at: Aug 31 2020 08:00 PM)",
-                new Event("return book", true, LocalDateTime.parse("2020-08-31T20:00")).toString());
+                "[E][\u2713] return book (at: Aug 31 2020 08:00 PM to Aug 31 2020 09:00 PM)",
+                new Event(
+                        "return book",
+                        true,
+                        LocalDateTime.parse("2020-08-31T20:00"),
+                        LocalDateTime.parse("2020-08-31T21:00"))
+                        .toString()
+        );
     }
 
     @Test
     void storeTest() {
         assertEquals(
-                "E F borrow book /at 2020-08-25T14:30",
-                new Event("borrow book", LocalDateTime.parse("2020-08-25T14:30")).store());
+                "E F borrow book /at 2020-08-25T14:30 to 2020-08-25T18:00",
+                new Event("borrow book",
+                        LocalDateTime.parse("2020-08-25T14:30"),
+                        LocalDateTime.parse("2020-08-25T18:00"))
+                        .store()
+        );
         assertEquals(
-                "E T return book /at 2020-08-31T20:00",
-                new Event("return book", true, LocalDateTime.parse("2020-08-31T20:00")).store());
+                "E T return book /at 2020-08-31T20:00 to 2020-08-31T22:35",
+                new Event(
+                        "return book",
+                        true,
+                        LocalDateTime.parse("2020-08-31T20:00"),
+                        LocalDateTime.parse("2020-08-31T22:35"))
+                        .store()
+        );
     }
 }
