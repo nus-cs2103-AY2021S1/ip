8e5d23a7998a56d2ad6ea94713343937d5c93233
Lee Ming De <leemingde@gmail.com>
2020-09-06 22:22:06 +0800

    Remove unused imports

--- src/main/java/duke/command/ByeCommand.java
+++ src/main/java/duke/command/ByeCommand.java
@@ -3,8 +3,6 @@ package duke.command;
 import duke.Storage;
 import duke.TaskList;
 import duke.Ui;
-import duke.task.RecurringTask;
-import duke.task.Task;
 
 /**
  * Represents a command which ends the operation.
