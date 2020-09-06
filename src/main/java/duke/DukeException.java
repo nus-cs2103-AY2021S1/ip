49b612131fca57ad6db33b050c39b4c606927acc
joshtyf <tan.yin.feng@hotmail.com>
2020-09-06 22:35:01 +0800

    Make use of super constructor for error msg

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -4,23 +4,11 @@ package duke;
  * An exception class that handles user input exceptions.
  */
 public class DukeException extends Exception {
-    private String error;
-
     /**
      * Constructor
      * @param error the error message to show
      */
     public DukeException(String error) {
-        // TODO: use super(error) constructor instead
-        this.error = error;
-    }
-
-    /**
-     * <code>String</code> represenation of this exception.
-     * @return the string representation of this exception.
-     */
-    @Override
-    public String toString() {
-        return String.format("OOPS! %s", error);
+        super(error);
     }
 }
