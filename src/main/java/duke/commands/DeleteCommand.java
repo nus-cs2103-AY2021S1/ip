b6c4e1ffe903567990cf0718744b960f34e18cba
joshtyf <tan.yin.feng@hotmail.com>
2020-09-06 23:02:04 +0800

    Document DeleteCommand.java

--- src/main/java/duke/commands/DeleteCommand.java
+++ src/main/java/duke/commands/DeleteCommand.java
@@ -6,19 +6,13 @@ import duke.tasks.Task;
 import duke.tasks.TaskManager;
 
 /**
- * <code>duke.commands.DeleteCommand</code> inherits from the base class <code>duke.commands.Command</code>
- * and will handle the job of deleting duke.tasks.
+ * <code>DeleteCommand</code> inherits from the base class <code>Command</code>
+ * and will handle the job of deleting tasks.
  */
 public class DeleteCommand extends Command {
     /**
      * Deletes a task.
-     * Using the <code>duke.Ui</code> object in the parent class, it prints out
-     * the user interface to ask for the number of the task to be deleted.
-     * It uses the <code>Scanner</code> object in the parent class to receive the number.
-     * It uses the <code>TaskManager</code> object in the parent class and calls
-     * its <code>deleteTask</code> method with the task number passed as an argument to delete
-     * the task.
-     * @return <code>true</code>
+     * @return <code>true</code>.
      * @throws DukeException if an invalid task number was given by the user.
      */
     @Override
@@ -30,6 +24,13 @@ public class DeleteCommand extends Command {
         return true;
     }
 
+    /**
+     * Sets the utility tools <code>tm</code> and <code>ui</code>.
+     * In addition, it sets the initial response to ask for the task number to be marked
+     * as completed.
+     * @param tm the task manager.
+     * @param ui the ui.
+     */
     @Override
     public void init(TaskManager tm, Ui ui) {
         setUtility(tm, ui);
