407cb9db90df23686b6d574bbfb29a1649da88a6
Junhua <wenjh1998@gmail.com>
2020-09-06 12:17:37 +0800

    Removed Redundant methods

--- src/main/java/Ultron/Exceptions/ExceptionType.java
+++ src/main/java/Ultron/Exceptions/ExceptionType.java
@@ -5,32 +5,26 @@ public enum ExceptionType {
      * ExceptionType when too much arguments are supplied.
      */
     TOO_MUCH_ARGUMENTS("Clearly there is too much arguments for %s"),
-
     /**
      * ExceptionType when there are no arguments supplied.
      */
     NO_ARGUMENTS_SUPPLIED("%s has arguments you know"),
-
     /**
      * ExceptionType when arguments are invalid.
      */
     INVALID_ARGUMENT("Invalid value of %s with '%s'"),
-
     /**
      * ExceptionType when the input is not a valid number.
      */
     INVALID_NUMBER("'%s' is not a valid number. Do you even know what is a number?"),
-
     /**
      * ExceptionType when there is no such command.
      */
     INVALID_COMMAND("What are you even saying? There is no '%s'"),
-
     /**
      * ExceptionType when there are IO errors.
      */
     IO_EXCEPTION("Blasphemy! How do I not have permission for %s"),
-
     /**
      * ExceptionType when Directory cannot be created.
      */
@@ -39,23 +33,18 @@ public enum ExceptionType {
      * ExceptionType already done.
      */
     ALREADY_DONE("%s is already completed.");
-
     /**
      * Stores the error message for the enum.
      */
     private final String errorMessage;
-
     /**
      * Different types of Exceptions.
      *
      * @param errorMessage Error Message to be shown.
      */
     ExceptionType(final String errorMessage) {
-
-        //Store the error message
         this.errorMessage = errorMessage;
     }
-
     /**
      * Get the message of the ExceptionType.
      *
@@ -65,11 +54,8 @@ public enum ExceptionType {
      */
     public String getMessage(final String inputString,
                              final String inputArguments) {
-
-        //Return the string formatted error message
         return String.format(this.errorMessage, inputString, inputArguments);
     }
-
     /**
      * Get the message of the ExceptionType.
      *
@@ -77,16 +63,6 @@ public enum ExceptionType {
      * @return The message to be shown.
      */
     public String getMessage(final String inputString) {
-
-        //Return the formatted error message
         return String.format(this.errorMessage, inputString);
-
-    }
-
-    public String getMessage() {
-
-        //Return the formatted error message
-        return String.format(this.errorMessage);
-
     }
 }
