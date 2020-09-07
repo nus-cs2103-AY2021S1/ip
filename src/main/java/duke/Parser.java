561f2359ef9e76dbd3cef6b5d262ec3fc88fdbae
kimberly ong <e0424807@u.nus.edu>
2020-09-07 08:58:27 +0800

    Add assertions at TaskList and Parser

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -26,6 +26,7 @@ public class Parser {
     static Command parse (String fullCommand)
         throws InvalidInputException, InvalidCommandException, InvalidTaskTypeException {
         String[] commandArr = fullCommand.trim().split(" ", 2);
+        assert !fullCommand.isEmpty(): "Command cannot be empty";
         switch(commandArr[0]) {
         case "bye":
             return new ExitCommand();
