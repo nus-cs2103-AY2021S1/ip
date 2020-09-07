e9a070e9152471e8b52ae14d8a3cb83065b98f42
xxzz-tt <tanxuanzhi@gmail.com>
2020-09-07 23:11:53 +0800

    Improve code quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -52,64 +52,64 @@ public class Duke {
                 firstWord = s;
             }
             switch (firstWord) {
-                case "bye":
-                    return "bye";
-                case "done":
-                    char x = s.charAt(s.length() - 1);
-                    int i = Character.getNumericValue(x);
-                    Task t = taskList.getList().get(i - 1);
-                    t.markAsDone();
-                    taskList.getList().set(i - 1, t);
-                    return "Nice! I've marked this task as done:\n" + t.printTask();
-                case "delete":
-                    char y = s.charAt(s.length() - 1);
-                    int k = Character.getNumericValue(y);
-                    taskList.delete(k);
-                    return "Now you have " + taskList.getList().size() +
-                            (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list";
-                case "deadline":
-                    if (s.length() != "deadline".length()) {
-                        String[] value = s.split(" /by ");
-                        String date = value[1];
-                        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
-                        DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
-                        LocalDateTime dateTime = LocalDateTime.parse(date, inputFormat);
-                        dateTime.format(outputFormat);
-                        Deadline deadline = new Deadline(value[0].replace("deadline ", ""), dateTime);
+            case "bye":
+                return "bye";
+            case "done":
+                char x = s.charAt(s.length() - 1);
+                int i = Character.getNumericValue(x);
+                Task t = taskList.getList().get(i - 1);
+                t.markAsDone();
+                taskList.getList().set(i - 1, t);
+                return "Nice! I've marked this task as done:\n" + t.printTask();
+            case "delete":
+                char y = s.charAt(s.length() - 1);
+                int k = Character.getNumericValue(y);
+                taskList.delete(k);
+                return "Now you have " + taskList.getList().size() +
+                    (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list";
+            case "deadline":
+                if (s.length() != "deadline".length()) {
+                    String[] value = s.split(" /by ");
+                    String date = value[1];
+                    DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+                    DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
+                    LocalDateTime dateTime = LocalDateTime.parse(date, inputFormat);
+                    dateTime.format(outputFormat);
+                    Deadline deadline = new Deadline(value[0].replace("deadline ", ""), dateTime);
 
-                        taskList.addTask(deadline);
-                        storage.saveTask(deadline);
-                        return "Got it. I've added this task:\n" + deadline.printTask() + "\n" +
-                                "Now you have " + taskList.getList().size() +
+                    taskList.addTask(deadline);
+                    storage.saveTask(deadline);
+                    return "Got it. I've added this task:\n" + deadline.printTask() + "\n" +
+                            "Now you have " + taskList.getList().size() +
                                 (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list";
-                    } else {
-                        throw new IncorrectInputException("☹ OOPS!!! The description of an deadline cannot be empty.");
-                    }
-                case "todo":
-                    if (s.length() != "todo".length()) {
-                        ToDo toDo = new ToDo(s.replace("todo ", ""));
-                        taskList.addTask(toDo);
-                        storage.saveTask(toDo);
-                        return "Got it. I've added this task:\n" + toDo.printTask() + "\n" +
-                                "Now you have " + taskList.getList().size() +
-                                (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list";
-                    } else {
-                        throw new IncorrectInputException("☹ OOPS!!! The description of a todo cannot be empty.");
-                    }
-                case "event":
-                    if (s.length() != "event".length()) {
-                        String[] value = s.split(" /at ");
-                        Event event = new Event(value[0].replace("event ", ""), value[1]);
-                        taskList.addTask(event);
-                        storage.saveTask(event);
-                        return "Got it. I've added this task:\n" + event.printTask() + "\n" +
-                                "Now you have " + taskList.getList().size() +
-                                (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list";
-                    } else {
-                        throw new IncorrectInputException("☹ OOPS!!! The description of an event cannot be empty.");
-                    }
-                default:
-                    throw new IncorrectInputException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                } else {
+                    throw new IncorrectInputException("☹ OOPS!!! The description of an deadline cannot be empty.");
+                }
+            case "todo":
+                if (s.length() != "todo".length()) {
+                    ToDo toDo = new ToDo(s.replace("todo ", ""));
+                    taskList.addTask(toDo);
+                    storage.saveTask(toDo);
+                    return "Got it. I've added this task:\n" + toDo.printTask() + "\n" +
+                        "Now you have " + taskList.getList().size() +
+                            (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list";
+                } else {
+                    throw new IncorrectInputException("☹ OOPS!!! The description of a todo cannot be empty.");
+                }
+            case "event":
+                if (s.length() != "event".length()) {
+                    String[] value = s.split(" /at ");
+                    Event event = new Event(value[0].replace("event ", ""), value[1]);
+                    taskList.addTask(event);
+                    storage.saveTask(event);
+                    return "Got it. I've added this task:\n" + event.printTask() + "\n" +
+                        "Now you have " + taskList.getList().size() +
+                            (taskList.getList().size() > 1 ? " tasks" : " task") + " on the list";
+                } else {
+                    throw new IncorrectInputException("☹ OOPS!!! The description of an event cannot be empty.");
+                }
+            default:
+                throw new IncorrectInputException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
