7528152cf87653f2a60d102c9bd4fc939d025507
WaiLok <e0323018@u.nus.edu>
2020-09-07 21:26:53 +0800

    coding standard

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -9,74 +9,75 @@ public class Parser {
 
             try {
                 String[] keyword = textMessage.split(" ");
+                switch (keyword[0]) {
 
-                if (textMessage.equals("bye")) {
-                    response += "Bye. Hope to see you again soon!";
-                }
-
-                else if (keyword[0].equals("find")) {
-
-                    response += tasks.findTask(keyword[1]);
-                }
-
-                else if (keyword[0].equals("delete")) {
-                    int index = Integer.parseInt(keyword[1]) - 1;
-                    response += tasks.deleteTask(index);
-                }
-
-                else if (keyword[0].equals("todo")) {
-
-                    if(keyword.length == 1) {
-                        throw new InvalidTodoException();
-                    }
-                    Todo newTodo = new Todo(textMessage.substring(5), false);
-                    response += tasks.addTask(newTodo);
-                }
-
-
-                else if (keyword[0].equals("deadline")) {
-
-                    if(keyword.length == 1) {
-                        throw new InvalidDeadlineException();
-                    }
-                    try {
-                        String[] tempString = textMessage.substring(9).split(" /by ");
-                        Deadline newDeadline = new Deadline(tempString[0], false, LocalDate.parse(tempString[1]));
-                        response += tasks.addTask(newDeadline);
-                    } catch (Exception e){
-                        throw new InvalidFormatException();
+                    case "bye": {
+                        response += "Bye. Hope to see you again soon!";
                     }
 
-                }
+                    case "find": {
 
-                else if (keyword[0].equals("event")) {
-
-                    if(keyword.length == 1) {
-                        throw new InvalidEventException();
-                    }
-                    try {
-                        String[] tempString = textMessage.substring(7).split(" /at ");
-                        Event newEvent = new Event(tempString[0], false, LocalDate.parse(tempString[1]));
-                        response += tasks.addTask(newEvent);
-                    } catch (Exception e){
-                        throw new InvalidFormatException();
+                        response += tasks.findTask(keyword[1]);
                     }
 
-                }
+                    case "delete": {
+                        int index = Integer.parseInt(keyword[1]) - 1;
+                        response += tasks.deleteTask(index);
+                    }
 
-                else if (textMessage.equals("list")) {
-                    response += tasks.showList();
-                }
+                    case "todo": {
 
-                else if (keyword[0].equals("done")) {
-                    int index = Integer.parseInt(keyword[1]) - 1;
-                    tasks.getTasks().get(index).markDone();
-                    response += "Nice I've marked this tasks as done";
-                    response += tasks.getTasks().get(index);
-                }
+                        if (keyword.length == 1) {
+                            throw new InvalidTodoException();
+                        }
+                        Todo newTodo = new Todo(textMessage.substring(5), false);
+                        response += tasks.addTask(newTodo);
+                    }
 
-                else {
-                    throw new InvalidInputException();
+
+                    case "deadline": {
+
+                        if (keyword.length == 1) {
+                            throw new InvalidDeadlineException();
+                        }
+                        try {
+                            String[] tempString = textMessage.substring(9).split(" /by ");
+                            Deadline newDeadline = new Deadline(tempString[0], false, LocalDate.parse(tempString[1]));
+                            response += tasks.addTask(newDeadline);
+                        } catch (Exception e) {
+                            throw new InvalidFormatException();
+                        }
+
+                    }
+
+                    case "event": {
+
+                        if (keyword.length == 1) {
+                            throw new InvalidEventException();
+                        }
+                        try {
+                            String[] tempString = textMessage.substring(7).split(" /at ");
+                            Event newEvent = new Event(tempString[0], false, LocalDate.parse(tempString[1]));
+                            response += tasks.addTask(newEvent);
+                        } catch (Exception e) {
+                            throw new InvalidFormatException();
+                        }
+
+                    }
+
+                    case "list": {
+                        response += tasks.showList();
+                    }
+
+                    case "done": {
+                        int index = Integer.parseInt(keyword[1]) - 1;
+                        tasks.getTasks().get(index).markDone();
+                        response += "Nice I've marked this tasks as done";
+                        response += tasks.getTasks().get(index);
+                    }
+
+                    default:
+                        throw new InvalidInputException();
                 }
 
             }
