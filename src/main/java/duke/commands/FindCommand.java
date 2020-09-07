37e1f668f5e5f9a924938e3f0449271d982a0ab3
RuiFengg <ruifeng.chang98@hotmail.com>
2020-09-07 10:04:40 +0800

    Use streams

--- src/main/java/duke/commands/FindCommand.java
+++ src/main/java/duke/commands/FindCommand.java
@@ -1,8 +1,12 @@
 package duke.commands;
 
+import java.util.stream.IntStream;
+
 import duke.exceptions.DukeException;
 import duke.storage.Storage;
 import duke.task.TaskList;
+
+
 
 /**
  * Finds all tasks that contains the keyword the user specified.
@@ -14,19 +18,21 @@ public class FindCommand extends Command {
 
     @Override
     public String execute(TaskList tasks, Storage storage) {
-        String response = "Here are the matching tasks in your list: \n";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Here are the matching tasks in your list: \n");
 
-        for (int i = 0; i < tasks.size(); i++) {
+        IntStream.range(0, tasks.size()).forEach(index -> {
             try {
                 // Check if user input matches any task description in list
-                if (tasks.getTask(i + 1).getDescription().contains(super.command)) {
-                    String message = (i + 1) + "." + tasks.getTask(i + 1) + "\n";
-                    response += message;
+                if (tasks.getTask(index + 1).getDescription().contains(super.command)) {
+                    String message = (index + 1) + "." + tasks.getTask(index + 1) + "\n";
+                    stringBuilder.append(message);
                 }
             } catch (DukeException e) {
-                return e.getMessage();
+                System.out.println(e.getMessage());
             }
-        }
+        });
+        String response = stringBuilder.toString();
         assert response != "" : "Response is an empty string";
         return response;
     }
