1a0f370c8e53242f3b0030722f9e4bc53ff4f72f
jialin7878 <cjl@u.nus.edu>
2020-09-07 19:15:47 +0800

    A-CodeQuality

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -4,6 +4,11 @@ public class Deadline extends Task {
 
     protected static char sym = '\u270F';
 
+    /**
+     * Constructor for Deadline task object
+     * @param desc task description
+     * @param time deadline
+     */
     public Deadline(String desc, LocalDateTime time) {
         super(desc);
         symbol = sym;
--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -2,7 +2,12 @@ import javafx.application.Application;
 
 public class Launcher {
 
+    /**
+     * launches application
+     * @param args
+     */
     public static void main(String[] args) {
+        assert(false) : "shld be false";
         Application.launch(Main.class, args);
     }
 }
