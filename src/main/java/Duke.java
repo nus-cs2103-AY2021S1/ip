d24c2f1c0a2ace4c966b0605344d3924c60af18c
zhengweii <limzhengwei_99@hotmail.com>
2020-09-07 20:10:16 +0800

    Add FXML

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,14 @@
 import java.nio.file.Paths;
-import java.util.Scanner;
-import javafx.application.Application;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.scene.Scene;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
 
-public class Duke extends Application {
+public class Duke {
     private static final String DATABASE_DIRECTORY_PATH = Paths.get(System.getProperty("user.dir"),
             "../../../", "data").toString();
     private Parser parser;
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
     private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
@@ -49,90 +32,33 @@ public class Duke extends Application {
             this.tasks = new TaskList();
         }
     }
-
-    /**
-     * Starts the bot.
-     */
-    public void run() {
-        this.ui.greetUser();
-
-        boolean keepApplicationRunning = true;
-        Scanner sc = new Scanner(System.in);
-
-        while (keepApplicationRunning) {
-            try {
-                String userInput = this.ui.getUserInput(sc);
-                if (userInput.equals("bye")) {
-                    keepApplicationRunning = false;
-                }
-                this.parser.parseCommands(this.tasks, userInput, sc);
-                // Saves the state after each command instead of only saving to the database upon exit
-                this.storage.save(this.tasks.getDatabase());
-            } catch (DukeException e) {
-                this.ui.showErrorMessage(e.getMessage());
-            }
-        }
-
-        this.ui.showExitMessage();
-        System.exit(0);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-    }
+//
+//    /**
+//     * Starts the bot.
+//     */
+//    public void run() {
+//        this.ui.greetUser();
+//
+//        boolean keepApplicationRunning = true;
+//        Scanner sc = new Scanner(System.in);
+//
+//        while (keepApplicationRunning) {
+//            try {
+//                String userInput = this.ui.getUserInput(sc);
+//                if (userInput.equals("bye")) {
+//                    keepApplicationRunning = false;
+//                }
+//                this.parser.parseCommands(this.tasks, userInput, sc);
+//                // Saves the state after each command instead of only saving to the database upon exit
+//                this.storage.save(this.tasks.getDatabase());
+//            } catch (DukeException e) {
+//                this.ui.showErrorMessage(e.getMessage());
+//            }
+//        }
+//
+//        this.ui.showExitMessage();
+//        System.exit(0);
+//    }
 
     /**
      * Creates a label with the specified text and adds it to the dialog container.
@@ -147,32 +73,7 @@ public class Duke extends Application {
         return textToAdd;
     }
 
-    /**
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        ImageView userImage = new ImageView(user);
-        ImageView dukeImage = new ImageView(duke);
-        Circle userClip = new Circle(50, 50, 50);
-        Circle dukeClip = new Circle(50, 50, 50);
-        userImage.setClip(userClip);
-        dukeImage.setClip(dukeClip);
-
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, userImage),
-                DialogBox.getDukeDialog(dukeText, dukeImage)
-        );
-        userInput.clear();
-    }
-
-    private String getResponse(String input) {
+    public String getResponse(String input) {
         return "Duke heard: " + input;
-    }
-
-    public static void main(String[] args) {
-        new Duke(Duke.DATABASE_DIRECTORY_PATH).run();
     }
 }
