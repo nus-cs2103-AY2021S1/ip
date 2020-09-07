cfaf9b8ef60f36b998dfa46c86c88c97c635473b
chan-j-d <chan.j.d.98@gmail.com>
2020-09-08 02:15:47 +0800

    Fix Javadocs formatting in duke.util

--- src/main/java/duke/util/Pair.java
+++ src/main/java/duke/util/Pair.java
@@ -1,23 +1,48 @@
 package duke.util;
 
+/**
+ * Utility class representing a pair of objects, possibly of different types.
+ * @param <T> Type of the first object.
+ * @param <U> Type of the second object.
+ */
 public class Pair<T, U> {
 
     private T first;
     private U second;
 
+    /** Constructs the pair object from the objects provided */
     private Pair(T first, U second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * Returns the first object.
+     *
+     * @return the first object in the pair.
+     */
     public T getFirst() {
         return this.first;
     }
 
+    /**
+     * Returns the second object.
+     *
+     * @return the second object in the pair.
+     */
     public U getSecond() {
         return this.second;
     }
 
+    /**
+     * Returns a pair object containing the two objects provided as arguments.
+     *
+     * @param first first object in the pair.
+     * @param second second object in the pair.
+     * @param <T> type of the first object.
+     * @param <U> type of the second object.
+     * @return a pair object containing both objects.
+     */
     public static <T, U> Pair<T, U> of(T first, U second) {
         return new Pair<>(first, second);
     }
--- src/main/java/duke/util/TaskList.java
+++ src/main/java/duke/util/TaskList.java
@@ -6,30 +6,67 @@ import java.util.List;
 
 import duke.task.Task;
 
+/**
+ * An aggregation of tasks.
+ * Functions largely similar to java.util.List.
+ */
 public class TaskList implements Iterable<Task> {
 
+    /** List of tasks stored */
     protected List<Task> listOfTasks;
 
+    /**
+     * Constructs a new task list.
+     */
     public TaskList() {
         listOfTasks = new ArrayList<>();
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task task to be added.
+     */
     public void add(Task task) {
         listOfTasks.add(task);
     }
 
+    /**
+     * Returns the task at the provided index.
+     * Note that index starts at 1.
+     *
+     * @param index index of the task to be returned from the list.
+     * @return task at the index provided.
+     */
     public Task get(int index) {
         return listOfTasks.get(index - 1);
     }
 
+    /**
+     * Removes and returns the task at the provided index.
+     * Note that index starts at 1.
+     *
+     * @param index index of the task to be removed from the list.
+     * @return task removed.
+     */
     public Task remove(int index) {
         return listOfTasks.remove(index - 1);
     }
 
+    /**
+     * Returns an iterator for the tasks inside the list.
+     *
+     * @return iterator object for the tasks in the list.
+     */
     public Iterator<Task> iterator() {
         return listOfTasks.iterator();
     }
 
+    /**
+     * Returns the size of the task list.
+     *
+     * @return size of task list.
+     */
     public int size() {
         return listOfTasks.size();
     }
@@ -47,7 +84,11 @@ public class TaskList implements Iterable<Task> {
         return builder.toString();
     }
 
-
+    /**
+     * Returns a customised string message for the number of tasks in the list.
+     *
+     * @return string informing the user about the number of tasks in the list.
+     */
     public String createTaskNumberCountMessage() {
         return "Now you have " + listOfTasks.size() + " tasks in the list.";
     }
