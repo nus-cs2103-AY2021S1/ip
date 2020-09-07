9cb3fbdaad3a515719df0495b7ea8516ce8d0e29
Ngo Duc Binh <admin@Binzzzs-MacBook.local>
2020-09-08 00:36:25 +0800

    Fix code according to Code Quality

--- src/main/java/duke/main/Duke.java
+++ src/main/java/duke/main/Duke.java
@@ -2,6 +2,7 @@ package duke.main;
 
 import java.io.IOException;
 
+import duke.exception.InvalidCommandException;
 import duke.task.*;
 import duke.exception.DukeException;
 
@@ -12,7 +13,7 @@ import duke.exception.DukeException;
 public class Duke {
     private Ui ui;
     private Storage storage;
-    private TaskList taskList;
+    private TaskList tasks;
 
     /**
      * Creates a Duke object with an Ui object, storage object to the saved task list file in the
@@ -25,7 +26,7 @@ public class Duke {
         try {
             this.ui = new Ui();
             this.storage = new Storage(fileName);
-            this.taskList = storage.formTaskList();
+            this.tasks = storage.formTaskList();
         } catch (IOException error) {
             System.out.println(error.toString());
         }
@@ -35,10 +36,11 @@ public class Duke {
      * Runs the Duke program.
      */
     public void run() {
-        ui.printToScreen(ui.getGreetingMessage());
+        String greetingMessage = ui.getGreetingMessage();
+        ui.printToScreen(greetingMessage);
         while (true) {
             try {
-                Processor.process(taskList, storage, ui);
+                Processor.process(tasks, storage, ui);
                 break;
             } catch (DukeException dukeException) {
                 ui.printError(dukeException);
@@ -47,63 +49,37 @@ public class Duke {
     }
 
     public String getResponse(String userInput) {
-        String response = "";
+        StringBuilder response = new StringBuilder();
         try {
             String command = Parser.getCommand(userInput);
-            if (Keyword.isValid(command)) {
-                if (command.equals("bye")) {
-                    response = "Bye Master. Hope to see you again soon!";
-                } else if (command.equals("list")) {
-                    response = ui.getFullList(taskList);
-                } else if (command.equals("done")) {
-                    int index = Parser.getIndexTask(userInput);
-                    Task task = taskList.getTask(index - 1);
-                    task.markAsDone();
-                    response = ui.getMarkTaskAsDoneMessage(task);
-                } else if (command.equals("delete")) {
-                    int index = Parser.getIndexTask(userInput);
-                    Task deletedTask = taskList.getTask(index - 1);
-                    taskList.deleteTask(index - 1);
-                    response = ui.getDeleteTaskMessage(deletedTask, taskList);
-                } else if (command.equals("todo")) {
-                    String taskDescription = Parser.findDescription(Parser.getArgs(userInput));
-                    Task task = new ToDo(taskDescription);
-                    taskList.addTask(task);
-                    response = ui.getAddTaskMessage(task, taskList);
-                } else if (command.equals("deadline")) {
-                    String taskDescription = Parser.findDescription(Parser.getArgs(userInput));
-                    String deadlineTime = Parser.findTime(Parser.getArgs(userInput), "by");
-                    // check whether the date time format is correct
-                    Parser.isValidDate(deadlineTime);
-                    // check whether time is included
-                    boolean hasTime = Parser.hasTime(deadlineTime);
-                    Task deadine = new DeadLine(taskDescription, deadlineTime, hasTime, false);
-                    taskList.addTask(deadine);
-                    response = ui.getAddTaskMessage(deadine, taskList);
-                } else if (command.equals("event")) {
-                    String taskDescription = Parser.findDescription(Parser.getArgs(userInput));
-                    String eventTime = Parser.findTime(Parser.getArgs(userInput), "at");
-                    // check whether the date time format is correct
-                    Parser.isValidDate(eventTime);
-                    // check whether time is included
-                    boolean hasTime = Parser.hasTime(eventTime);
-                    Task event = new Event(taskDescription, eventTime, hasTime, false);
-                    taskList.addTask(event);
-                    response = ui.getAddTaskMessage(event, taskList);
-                } else {
-                    String keyword = Parser.getArgs(userInput);
-                    TaskList correspondTaskList = taskList.findTaskWithKeyword(keyword);
-                    response = ui.getMatchingList(correspondTaskList);
-                }
-            storage.writeTasks(taskList);
-
+            if (command.equals("bye")) {
+                Processor.handleByeCommand(response, ui);
+            } else if (command.equals("list")) {
+                Processor.handleListCommand(response, ui, tasks);
+            } else if (command.equals("done")) {
+                Processor.handleDoneCommand(userInput, response, ui, tasks);
+            } else if (command.equals("delete")) {
+                Processor.handleDeleteCommand(userInput, response, ui, tasks);
+            } else if (command.equals("todo")) {
+                Processor.handleTodoCommand(userInput, response, ui, tasks);
+            } else if (command.equals("deadline")) {
+                Processor.handleDeadlineCommand(userInput, response, ui, tasks);
+            } else if (command.equals("event")) {
+                Processor.handleEventCommand(userInput, response, ui, tasks);
+            } else if (command.equals("find")) {
+                Processor.handleFindCommand(userInput, response, ui, tasks);
+            } else {
+                throw new InvalidCommandException();
             }
+            storage.writeTasks(tasks);
         } catch (DukeException error) {
-            response = error.toString();
+            String errorMessage = error.toString();
+            response.append(errorMessage);
         } finally {
-            return response;
+            return response.toString();
         }
     }
+
     public static void main(String[] args) {
         new Duke("duke.txt").run();
     }
--- src/main/java/duke/main/Processor.java
+++ src/main/java/duke/main/Processor.java
@@ -2,6 +2,7 @@ package duke.main;
 
 import duke.exception.DukeException;
 
+import duke.exception.InvalidCommandException;
 import duke.task.DeadLine;
 import duke.task.Event;
 import duke.task.Task;
@@ -14,68 +15,175 @@ import duke.task.TaskList;
 public class Processor {
 
     /**
+     * Handles the case when user input bye command.
+     * @param ui The UI that handles interaction between program and user.
+     */
+    public static void handleByeCommand(StringBuilder response, Ui ui) {
+        String goodbyeMessage = ui.getGoodByeMessage();
+        response.append(goodbyeMessage);
+        ui.printToScreen(goodbyeMessage);
+    }
+
+    /**
+     * Handles the case when user input list command.
+     * @param response The program's response to user's input.
+     * @param ui The UI that handles interaction between program and user.
+     * @param tasks User's task list.
+     */
+    public static void handleListCommand(StringBuilder response, Ui ui, TaskList tasks) {
+        String taskListInString = ui.getFullList(tasks);
+        response.append(taskListInString);
+    }
+
+    /**
+     * Handles the case when user input done command.
+     * @param userInput The user's input.
+     * @param response The program's response to user's input.
+     * @param ui The UI that handles interaction between program and user.
+     * @param tasks User's task list.
+     * @throws DukeException when the index is invalid.
+     */
+    public static void handleDoneCommand(String userInput, StringBuilder response, Ui ui, TaskList tasks) throws DukeException {
+        int taskNumber = Parser.getIndexTask(userInput);
+        int taskIndex = taskNumber - 1;
+        Task task = tasks.getTask(taskIndex);
+        task.markAsDone();
+        String message = ui.getMarkTaskAsDoneMessage(task);
+        response.append(message);
+    }
+
+    /**
+     * Handles the case when user input delete command.
+     * @param userInput The user's input.
+     * @param response The program's response to user's input.
+     * @param ui The UI that handles interaction between program and user.
+     * @param tasks User's task list.
+     * @throws DukeException when the index is invalid.
+     */
+    public static void handleDeleteCommand(String userInput, StringBuilder response, Ui ui, TaskList tasks) throws DukeException {
+        int taskNumber = Parser.getIndexTask(userInput);
+        int taskIndex = taskNumber - 1;
+        Task deletedTask = tasks.getTask(taskIndex);
+        tasks.deleteTask(taskIndex);
+        String message = ui.getDeleteTaskMessage(deletedTask, tasks);
+        response.append(message);
+    }
+
+    /**
+     * Handles the case when user input todo command.
+     * @param userInput The user's input.
+     * @param response The program's response to user's input.
+     * @param ui The UI that handles interaction between program and user.
+     * @param tasks User's task list.
+     * @throws DukeException can be thrown when task description is empty.
+     */
+    public static void handleTodoCommand(String userInput, StringBuilder response, Ui ui, TaskList tasks) throws DukeException {
+        String argument = Parser.getArgs(userInput);
+        String taskDescription = Parser.findDescription(argument);
+        Task task = new ToDo(taskDescription);
+        tasks.addTask(task);
+        String message = ui.getAddTaskMessage(task, tasks);
+        response.append(message);
+    }
+
+    /**
+     * Handles the case when user input deadline command.
+     * @param userInput The user's input.
+     * @param response The program's response to user's input.
+     * @param ui The UI that handles interaction between program and user.
+     * @param tasks User's task list.
+     * @throws DukeException can be thrown when the task description or the time is empty, or
+     *                       date format is incorrect.
+     */
+    public static void handleDeadlineCommand(String userInput, StringBuilder response, Ui ui, TaskList tasks) throws DukeException {
+        String argument = Parser.getArgs(userInput);
+        String taskDescription = Parser.findDescription(argument);
+        String deadlineTime = Parser.findTime(argument, "by");
+        // check whether the date time format is correct
+        Parser.isValidDate(deadlineTime);
+        // check whether time is included
+        boolean hasTime = Parser.hasTime(deadlineTime);
+        Task deadLine = new DeadLine(taskDescription, deadlineTime, hasTime, false);
+        tasks.addTask(deadLine);
+        String message = ui.getAddTaskMessage(deadLine, tasks);
+        response.append(message);
+    }
+
+    /**
+     * Handles the case when user input event command.
+     * @param userInput The user's input.
+     * @param response The program's response to user's input.
+     * @param ui The UI that handles interaction between program and user.
+     * @param tasks User's task list.
+     * @throws DukeException can be thrown when the task description or the time is empty, or
+     *                       date format is incorrect.
+     */
+    public static void handleEventCommand(String userInput, StringBuilder response, Ui ui, TaskList tasks) throws DukeException {
+        String argument = Parser.getArgs(userInput);
+        String taskDescription = Parser.findDescription(argument);
+        String eventTime = Parser.findTime(argument, "at");
+        // check whether the date time format is correct
+        Parser.isValidDate(eventTime);
+        // check whether time is included
+        boolean hasTime = Parser.hasTime(eventTime);
+        Task event = new Event(taskDescription, eventTime, hasTime, false);
+        tasks.addTask(event);
+        String message = ui.getAddTaskMessage(event, tasks);
+        response.append(message);
+    }
+
+    /**
+     * Handles the case when user input find command
+     * @param userInput The user's input.
+     * @param response The program's response to user's input.
+     * @param ui The UI that handles interaction between program and user.
+     * @param tasks User's task list.
+     * @throws DukeException can be thrown when the task description or the time is empty, or
+     *                       date format is incorrect.
+     */
+    public static void handleFindCommand(String userInput, StringBuilder response, Ui ui, TaskList tasks) throws DukeException {
+        String keyword = Parser.getArgs(userInput);
+        TaskList correspondTaskList = tasks.findTaskWithKeyword(keyword);
+        String message = ui.getMatchingList(correspondTaskList);
+        response.append(message);
+    }
+
+    /**
      * Processes the run of the program.
-     * @param taskList The user's task list.
+     * @param tasks The user's task list.
      * @param storage The storage storing the user's saved task list.
      * @param ui UI that handles interaction with user.
      * @throws DukeException Exception can be thrown due to multiple reasons, such as
      *                       invalid command, empty task description, empty date or
      *                       wrong date format.
      */
-    public static void process(TaskList taskList, Storage storage, Ui ui) throws DukeException {
-        while(Parser.hasNextLine()) {
+    public static void process(TaskList tasks, Storage storage, Ui ui) throws DukeException {
+        while (Parser.hasNextLine()) {
             String userInput = Parser.readNextLine();
             String command = Parser.getCommand(userInput);
-            String response;
-            if (Keyword.isValid(command)) {
-                if (command.equals("bye")) {
-                    ui.printToScreen(ui.getGoodByeMessage());
-                    break;
-                } else if (command.equals("list")) {
-                    response = ui.getFullList(taskList);
-                } else if (command.equals("done")) {
-                    int index = Parser.getIndexTask(userInput);
-                    Task task = taskList.getTask(index - 1);
-                    task.markAsDone();
-                    response = ui.getMarkTaskAsDoneMessage(task);
-                } else if (command.equals("delete")) {
-                    int index = Parser.getIndexTask(userInput);
-                    Task deletedTask = taskList.getTask(index - 1);
-                    taskList.deleteTask(index - 1);
-                    response = ui.getDeleteTaskMessage(deletedTask, taskList);
-                } else if (command.equals("todo")) {
-                    String taskDescription = Parser.findDescription(Parser.getArgs(userInput));
-                    Task task = new ToDo(taskDescription);
-                    taskList.addTask(task);
-                    response = ui.getAddTaskMessage(task, taskList);
-                } else if (command.equals("deadline")) {
-                    String taskDescription = Parser.findDescription(Parser.getArgs(userInput));
-                    String deadlineTime = Parser.findTime(Parser.getArgs(userInput), "by");
-                    // check whether the date time format is correct
-                    Parser.isValidDate(deadlineTime);
-                    // check whether time is included
-                    boolean hasTime = Parser.hasTime(deadlineTime);
-                    Task deadine = new DeadLine(taskDescription, deadlineTime, hasTime, false);
-                    taskList.addTask(deadine);
-                    response = ui.getAddTaskMessage(deadine, taskList);
-                } else if (command.equals("event")) {
-                    String taskDescription = Parser.findDescription(Parser.getArgs(userInput));
-                    String eventTime = Parser.findTime(Parser.getArgs(userInput), "at");
-                    // check whether the date time format is correct
-                    Parser.isValidDate(eventTime);
-                    // check whether time is included
-                    boolean hasTime = Parser.hasTime(eventTime);
-                    Task event = new Event(taskDescription, eventTime, hasTime, false);
-                    taskList.addTask(event);
-                    response = ui.getAddTaskMessage(event, taskList);
-                } else {
-                    String keyword = Parser.getArgs(userInput);
-                    TaskList correspondTaskList = taskList.findTaskWithKeyword(keyword);
-                    response = ui.getMatchingList(correspondTaskList);
-                }
-                storage.writeTasks(taskList);
-                ui.printToScreen(response);
+            StringBuilder response = new StringBuilder();
+            if (command.equals("bye")) {
+                handleByeCommand(response, ui);
+                break;
+            } else if (command.equals("list")) {
+                handleListCommand(response, ui, tasks);
+            } else if (command.equals("done")) {
+                handleDoneCommand(userInput, response, ui, tasks);
+            } else if (command.equals("delete")) {
+                handleDeleteCommand(userInput, response, ui, tasks);
+            } else if (command.equals("todo")) {
+                handleTodoCommand(userInput, response, ui, tasks);
+            } else if (command.equals("deadline")) {
+                handleDeadlineCommand(userInput, response, ui, tasks);
+            } else if (command.equals("event")) {
+                handleEventCommand(userInput, response, ui, tasks);
+            } else if (command.equals("find")) {
+                handleFindCommand(userInput, response, ui, tasks);
+            } else {
+                throw new InvalidCommandException();
             }
+            storage.writeTasks(tasks);
+            ui.printToScreen(response.toString());
         }
     }
 }
