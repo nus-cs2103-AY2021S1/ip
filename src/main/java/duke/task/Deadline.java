34db1a70f8a42c5f74c8704f0b84e27bbb9e5370
pockii <yeokexin2@gmail.com>
2020-09-08 03:26:01 +0800

    Improve code quality through refactoring

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -7,14 +7,15 @@ import duke.exception.MissingDeadlineException;
  */
 public class Deadline extends Task {
     protected String deadline;
+    public static String SPLIT_IDENTIFIER = " /by ";
 
     public Deadline(String desc) throws MissingDeadlineException {
-        super("D", desc.split(" /by ", 2)[0]);
-        String[] temp = desc.split(" /by ", 2);
+        super("D", desc.split(SPLIT_IDENTIFIER, 2)[0]);
+        String[] temp = desc.split(SPLIT_IDENTIFIER, 2);
         if (temp.length == 1) {
             throw new MissingDeadlineException("deadline");
         } else {
-            String date = desc.split(" /by ", 2)[1];
+            String date = desc.split(SPLIT_IDENTIFIER, 2)[1];
             this.deadline = containsTime(date)
                                 ? formatDateTime(date)
                                 : formatDate(date);
