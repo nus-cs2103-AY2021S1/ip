098245449eb411c64a75b46778468857b1888f66
hakiem526 <e0325825@u.nus.edu>
2020-09-07 21:57:41 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String HORIZONTAL_LINE =
+            "_________________________________________________________________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +12,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(Duke.HORIZONTAL_LINE);
+        System.out.println("Hello I'm Duke\nWhat can I do for you?");
+        System.out.println(Duke.HORIZONTAL_LINE);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.next();
+            System.out.println(Duke.HORIZONTAL_LINE);
+            if (input.toLowerCase().equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+            System.out.println(Duke.HORIZONTAL_LINE);
+        }
     }
 }
