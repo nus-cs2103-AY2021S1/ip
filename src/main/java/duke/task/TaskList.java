39d567b90d33dfcda42193347fec4a17443037d4
Low Ming Lim <48002577+Avalionnet@users.noreply.github.com>
2020-09-07 19:08:23 +0800

    Refactor the parse method in Parser class using the extract method taught in the textbook

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -2,7 +2,6 @@ package duke.task;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 
