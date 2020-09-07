7bc8b6d9a41037a106a1b80a4de62122e425fec9
rgabelarde <rgabelarde@gmail.com>
2020-09-08 05:44:57 +0800

    Add a GUI to Duke/Moco Chat bot using JavaFX

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,6 +5,8 @@ import duke.logic.Storage;
 import duke.logic.TaskList;
 import duke.ui.Ui;
 
+import java.util.Scanner;
+
 public class Duke {
     private Storage storage;
     private TaskList tasks;
@@ -15,12 +17,26 @@ public class Duke {
      * If expected error is met, MocoException is thrown and
      * user is prompted.
      *
-     * @param filepath file path of saved tasklist.
+     * @param /filepath file path of saved tasklist.
      */
     public Duke(String filePath) throws MocoException {
-        ui = new Ui();
         storage = new Storage(filePath);
         tasks = new TaskList(storage.load());
+        ui = new Ui(tasks);
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        String parsed;
+        try {
+            parsed = Parser.parse(input, tasks, ui, storage);
+        } catch (MocoException e) {
+            parsed = e.getMessage();
+        }
+        return parsed;
     }
 
     /**
@@ -28,18 +44,17 @@ public class Duke {
      * If the user inputs "bye", Moco closes, saving task list
      */
     public void run() {
-        ui.startBot();
-        boolean isCommand = true;
-        while (isCommand) {
+        Scanner sc = new Scanner(System.in);
+        String command = "";
+        while (!command.equals("bye")) {
             try {
-                String input = ui.getInput();
-                isCommand = Parser.parse(input, tasks, ui, storage);
+                String input = sc.nextLine();
+                command = Parser.parse(input, tasks, ui, storage);
             } catch (MocoException e) {
                 System.out.println(e.getMessage());
-                ui.printBorder();
             }
         }
-        ui.stopBot();
+        sc.close();
     }
 
     public static void main(String[] args) throws MocoException {
