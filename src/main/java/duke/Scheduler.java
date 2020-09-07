9887fc2b0ca415b18d1259617e0a546c19da1b7a
AlexCQY <alex_chua@u.nus.edu>
2020-09-07 11:27:42 +0800

    Add new command schedule & new class scheduler

---
+++ src/main/java/duke/Scheduler.java
@@ -0,0 +1,12 @@
+package duke;
+
+public class Scheduler {
+
+    private TaskList tasksList;
+
+    Scheduler(TaskList tasksList) {
+        this.tasksList = tasksList;
+    }
+
+//    public TaskList getTaskByDate()
+}
