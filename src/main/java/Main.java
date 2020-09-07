d01dcd4aa75039521136f292e8e6f8546623c8b9
xxzz-tt <tanxuanzhi@gmail.com>
2020-09-07 23:02:45 +0800

    Add assertion statements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            assert fxmlLoader != null;
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
