3d188bd66b9f5467292d5eb87dbfd5beece01401
rachel170 <e0418127@u.nus.edu>
2020-09-08 17:28:13 +0800

    Add Update Task Description function
    iProject requirements for C-Update.
    
    Give Duke the ability to change the description of any
    task in its TaskList.

--- src/main/java/util/Command.java
+++ src/main/java/util/Command.java
@@ -9,11 +9,13 @@ package util;
  * LIST - 'list' which lists Duke's recorded tasks.
  * DONE - Helps mark task as done.
  * DELETE - Indicates task to be removed.
+ * FIND - Identifies tasks containing given keyword.
+ * UPDATE - Updates task description with given string.
  * Types of task that can be made:
  * DEADLINE, EVENT, TO DO
  * */
 public enum Command {
     INVALID, EXIT, LIST,
-    DONE, DELETE, FIND,
+    DONE, DELETE, FIND, UPDATE,
     TODO, DEADLINE, EVENT
 }
