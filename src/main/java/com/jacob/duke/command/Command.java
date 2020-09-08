d74a60597aa71ad13ffdb6ca62fc19c17323ddac
chuyouchia <58682208+chuyouchia@users.noreply.github.com>
2020-09-08 09:09:21 +0800

    Fix import statements

--- src/main/java/com/jacob/duke/command/Command.java
+++ src/main/java/com/jacob/duke/command/Command.java
@@ -2,8 +2,8 @@ package main.java.com.jacob.duke.command;
 
 import main.java.com.jacob.duke.DukeException;
 import main.java.com.jacob.duke.DukeList;
-import main.java.com.jacob.duke.io.Storage;
 import main.java.com.jacob.duke.Ui;
+import main.java.com.jacob.duke.io.Storage;
 
 
 public interface Command {
