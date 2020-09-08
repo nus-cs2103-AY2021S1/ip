8151d3aa0e99eaba69eb91bea62b9bc71a8e6ec5
chuyouchia <58682208+chuyouchia@users.noreply.github.com>
2020-09-08 08:13:51 +0800

    Update checkstyle imports errors

--- src/main/java/com/jacob/duke/command/Command.java
+++ src/main/java/com/jacob/duke/command/Command.java
@@ -1,8 +1,8 @@
 package main.java.com.jacob.duke.command;
 
 import main.java.com.jacob.duke.DukeException;
-import main.java.com.jacob.duke.Storage;
 import main.java.com.jacob.duke.DukeList;
+import main.java.com.jacob.duke.Storage;
 import main.java.com.jacob.duke.Ui;
 
 
--- src/main/java/com/jacob/duke/command/NoteCommand.java
+++ src/main/java/com/jacob/duke/command/NoteCommand.java
@@ -1,6 +1,5 @@
 package main.java.com.jacob.duke.command;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import main.java.com.jacob.duke.DukeException;
