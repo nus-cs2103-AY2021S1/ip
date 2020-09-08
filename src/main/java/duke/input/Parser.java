9f3dc2cb3b7c987c064e47147a0ac035fd478769
geoboom <leegeorgie52@gmail.com>
2020-09-08 13:25:13 +0800

    Add parsing of delete task and implement enums

--- src/main/java/duke/input/Parser.java
+++ src/main/java/duke/input/Parser.java
@@ -1,8 +1,10 @@
 package duke.input;
 
 import duke.command.Command;
+import duke.command.CommandEnum;
 import duke.command.CompleteTaskCommand;
 import duke.command.DeadlineCommand;
+import duke.command.DeleteTaskCommand;
 import duke.command.EventCommand;
 import duke.command.ListTasksCommand;
 import duke.command.TodoCommand;
@@ -13,20 +15,12 @@ import duke.exception.ParserException;
  * what command to execute and execute it.
  */
 public class Parser {
-
-  public static final String CMD_TERMINATE = "bye";
-  public static final String CMD_TASK_LIST = "list";
-  public static final String CMD_TASK_SET_DONE = "done";
-  public static final String CMD_TASK_ADD_TODO = "todo";
-  public static final String CMD_TASK_ADD_EVENT = "event";
-  public static final String CMD_TASK_ADD_DEADLINE = "deadline";
-
   /**
    * @param s command supplied
    * @return true if it's an exit command, ignoring case
    */
-  public static boolean isExitCommand(String s) {
-    return s.toLowerCase().equals(CMD_TERMINATE);
+  public static boolean isTerminate(String s) {
+    return CommandEnum.valueOf(s.toUpperCase()).equals(CommandEnum.TERMINATE);
   }
 
   /**
@@ -35,25 +29,39 @@ public class Parser {
    */
   public static Command parseCommand(String s) throws ParserException {
     String[] args = s.split("\\s+", 2);
+    CommandEnum cmdEnum;
+    try {
+      cmdEnum = CommandEnum.valueOf(args[0].toUpperCase());
+    } catch (IllegalArgumentException e) {
+      throw new ParserException("Invalid command.");
+    }
 
-    switch (args[0].toLowerCase()) {
-      case CMD_TASK_LIST:
+    switch (cmdEnum) {
+      case TASK_LIST:
         return new ListTasksCommand();
-      case CMD_TASK_ADD_TODO: {
+      case TASK_ADD_TODO: {
         String todo = parseTodo(args[1]);
         return new TodoCommand(todo);
       }
-      case CMD_TASK_ADD_EVENT: {
+      case TASK_ADD_EVENT: {
         String[] inputArr = parseEvent(args[1]);
         return new EventCommand(inputArr[0], inputArr[1]);
       }
-      case CMD_TASK_ADD_DEADLINE: {
+      case TASK_ADD_DEADLINE: {
         String[] inputArr = parseDeadline(args[1]);
         return new DeadlineCommand(inputArr[0], inputArr[1]);
       }
-      case CMD_TASK_SET_DONE: {
-        int[] taskIdList = parseMarkDone(args[1]);
+      case TASK_SET_DONE: {
+        int[] taskIdList = parseTaskIds(args[1]);
         return new CompleteTaskCommand(taskIdList[0]);
+//        for (int taskId : taskIdList) {
+//          Command command = new CompleteTaskCommand(taskId);
+//          command.execute(Duke.getInstance());
+//        }
+      }
+      case TASK_DELETE: {
+        int[] taskIdList = parseTaskIds(args[1]);
+        return new DeleteTaskCommand(taskIdList[0]);
 //        for (int taskId : taskIdList) {
 //          Command command = new CompleteTaskCommand(taskId);
 //          command.execute(Duke.getInstance());
@@ -87,10 +95,10 @@ public class Parser {
     return arr;
   }
 
-  public static int[] parseMarkDone(String s) throws ParserException {
+  public static int[] parseTaskIds(String s) throws ParserException {
     String[] arr = s.split(" ");
     if (arr.length <= 0) {
-      throw new ParserException("Mark tasks done format error.");
+      throw new ParserException("Task numbers format error.");
     }
     int[] taskIdList = new int[arr.length];
     for (int i = 0; i < arr.length; ++i) {
