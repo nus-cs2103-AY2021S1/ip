22e38a12711ed967f2e089776092831c299d2c6d
fanminj <fanmin98@gmail.com>
2020-09-08 17:35:51 +0800

    Modify DialogBox to fix Duke's reply being cut off

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -7,10 +7,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 /**
  * An example of a custom control using FXML.
@@ -19,7 +19,7 @@ import javafx.scene.layout.HBox;
  */
 public class DialogBox extends HBox {
     @FXML
-    private Label dialog;
+    private Text dialog;
     @FXML
     private ImageView displayPicture;
 
