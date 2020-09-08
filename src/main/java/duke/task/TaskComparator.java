ec3634565afa5329114253d726eee7fc3d3d8c68
Penn Han <leepennhan98@gmail.com>
2020-09-08 13:15:38 +0800

    Edit for Code Quality in TaskComparator class

--- src/main/java/duke/task/TaskComparator.java
+++ src/main/java/duke/task/TaskComparator.java
@@ -4,6 +4,16 @@ import java.util.Comparator;
 
 public class TaskComparator implements Comparator<Task> {
 
+    /**
+     * Overrides the Compare method of the Comparator Class to compare two tasks
+     * on 3 conditions
+     * 1. If a task is Done
+     * 2. If a task is marked as Important
+     * 3. If a task date is before or after the other.
+     * @param task1 Task object 1
+     * @param task2 Task object 2
+     * @return Int
+     */
     @Override
     public int compare(Task task1, Task task2) {
         if (!task1.isDone() && task2.isDone()) {
