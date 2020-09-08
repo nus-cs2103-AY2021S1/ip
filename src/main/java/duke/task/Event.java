6c49a539c9d84921865c78a8ebb945399290ae3b
samlsm <samlsm2803@gmail.com>
2020-09-08 21:34:48 +0800

    Checkstyle.

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -79,7 +79,7 @@ public class Event extends Task {
                     String formatDate = inputDate[0] + "-" + inputDate[1] + "-" + inputDate[2];
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-                    LocalDate date = LocalDate.parse(formatDate,formatter);
+                    LocalDate date = LocalDate.parse(formatDate, formatter);
                     String[] times = splitEventTime[1].trim().split("-");
                     LocalTime startTime = LocalTime.parse(times[0]);
                     LocalTime endTime = LocalTime.parse(times[1]);
