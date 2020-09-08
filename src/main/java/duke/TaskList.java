e5757c608b53d37e07800a57fc63c95b26130929
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-09-08 19:05:07 +0800

    Add tagging support

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -2,6 +2,7 @@ package duke;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -73,13 +74,17 @@ public class TaskList implements Iterable<Task> {
      * @throws DukeException if failed to add task.
      */
     public Task addTask(String type, String input, boolean done) throws DukeException {
+        if (input.isEmpty()) {
+            throw new DukeException("OOPS!!! I'm sorry, the description cannot be empty :<");
+        }
         Task task;
+        String[] splitTags = input.split("#");
         switch (type) {
         case "todo":
-            task = new Todo(input);
+            task = new Todo(splitTags[0]);
             break;
         case "deadline":
-            String[] arr = input.split("/by");
+            String[] arr = splitTags[0].split("/by");
             try {
                 task = new Deadline(arr[0].trim(), arr[1].trim());
             } catch (IndexOutOfBoundsException e) {
@@ -89,7 +94,7 @@ public class TaskList implements Iterable<Task> {
             }
             break;
         case "event":
-            arr = input.split("/at");
+            arr = splitTags[0].split("/at");
             try {
                 task = new Event(arr[0].trim(), arr[1].trim());
             } catch (IndexOutOfBoundsException e) {
@@ -101,10 +106,14 @@ public class TaskList implements Iterable<Task> {
         default:
             throw new DukeException("OOPS!!! I'm sorry, I don't know what that means :<");
         }
-        if (input.isEmpty()) {
-            throw new DukeException("OOPS!!! I'm sorry, the description cannot be empty :<");
-        }
         task = done ? task.setDone() : task;
+        if (splitTags.length > 1) {
+            for (String i: splitTags) {
+                if (i != splitTags[0]) {
+                    task.addTag(i.trim());
+                }
+            }
+        }
         this.list.add(task);
         return task;
     }
@@ -169,6 +178,22 @@ public class TaskList implements Iterable<Task> {
     }
 
     /**
+     * Tags the i-th task with input tag.
+     *
+     * @param i index of the tasks to be deleted.
+     * @param tag String tag for the ith item.
+     * @return Task if task is in this TaskList object.
+     * @throws DukeException if index out of bounds.
+     */
+    public Task tagTask(int i, String tag) throws DukeException {
+        try {
+            return this.list.get(i).addTag(tag);
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("OOPS!!! I'm sorry, the task number is out of range :<");
+        }
+    }
+
+    /**
      * Returns a string representation of object.
      *
      * @return String object.
@@ -199,6 +224,39 @@ public class TaskList implements Iterable<Task> {
             throw new DukeException("OOPS!!! I'm sorry, no such keyword :<");
         }
         return new TaskList(keywordTasks);
+    }
+
+    /**
+     * Returns tasks containing the specific tag.
+     * @param tag tag of tasks we want to retrieve
+     * @return list of tasks containing tag
+     * @throws DukeException if no such task
+     */
+    public TaskList findTag(String tag) throws DukeException {
+        ArrayList<Task> taggedTasks = new ArrayList<>(this.list);
+        taggedTasks.removeIf(i -> !i.hasTag(tag));
+        if (taggedTasks.isEmpty()) {
+            throw new DukeException("OOPS!!! I'm sorry, no such tag :<");
+        }
+        return new TaskList(taggedTasks);
+    }
+
+    public Task untagTask(int i, String tag) throws DukeException {
+        try {
+            return this.list.get(i).untag(tag);
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("OOPS!!! I'm sorry, the task number is out of range :<");
+        }
+    }
+
+    public TaskList untagAllTasks(String tag) throws DukeException {
+        ArrayList<Task> taggedTasks = new ArrayList<>(this.list);
+        taggedTasks.removeIf(i -> !i.hasTag(tag));
+        if (taggedTasks.isEmpty()) {
+            throw new DukeException("OOPS!!! I'm sorry, no such tag :<");
+        }
+        Object[] arr = taggedTasks.stream().map((Task i) -> i.untag(tag)).toArray();
+        return new TaskList(new ArrayList(Arrays.asList(arr)));
     }
 
 }
