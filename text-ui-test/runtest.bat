46a4a9594efbc4492945c9d5ec580d26b028e95f
yuming7144 <yuming7144@gmail.com>
2020-09-09 23:27:16 +0800

    Modify runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,4 +1,7 @@
+
 @ECHO OFF
+
+set JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
@@ -6,18 +9,19 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 del ACTUAL.TXT
 
-del %systemdrive%%homepath%\nekochan\localData\data.neko
-
 REM compile the code into the bin folder
-javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\*.java
+dir /s /B ..\src\*.java > sources.txt
+javac  -cp ..\src -Xlint:none -d ..\bin @sources.txt
+
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
+
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ..\bin duke.Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
-FC ACTUAL.TXT EXPECTED.TXTXT
+FC ACTUAL.TXT EXPECTED.TXT
