4c12fe04d502a3126b27baa487d385b617aa1d43
Nauman-S <58757898+Nauman-S@users.noreply.github.com>
2020-09-09 14:53:43 +0800

    GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,23 +1,10 @@
 package duke;
 
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class Duke extends Application{
-
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
-    }
+public class Duke{
+    
     
     private TaskList taskList;
     private UI userInterface = new UI();
