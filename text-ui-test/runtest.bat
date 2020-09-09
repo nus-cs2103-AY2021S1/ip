ce5d8682a0fcb1f3437f1cd985d3739b02105180
akgrenSoar <44816346+akgrenSoar@users.noreply.github.com>
2020-09-10 06:05:02 +0800

    Clean runtest

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,18 +1,7 @@
 @ECHO OFF
 
-REM create bin directory if it doesn't exist
-rem if not exist ..\bin mkdir ..\bin
-
 REM delete display from previous run
 del ACTUAL.TXT
-
-REM compile the code into the bin folder
-rem javac -cp .\lib\*.jar -Xlint:none -d ..\bin ..\src\main\java\duke\Main.java ..\src\main\java\duke\cmd\*.java ..\src\main\java\duke\executable\*.java ..\src\main\java\duke\task\*.java ..\src\main\java\duke\storage\*.java ..\src\main\java\duke\util\*.java ..\src\main\java\duke\parser\*.java ..\src\main\java\duke\gui\*.java
-rem IF ERRORLEVEL 1 (
-rem     echo ********** BUILD FAILURE **********
-rem     exit /b 1
-rem )
-REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the display to the ACTUAL.TXT
 java -jar ..\build\libs\duke.jar cmd < input.txt > ACTUAL.TXT 2>&1
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/env bash
 
-# create bin directory if it doesn't exist
-#if [ ! -d "../bin" ]
-#then
-#    mkdir ../bin
-#fi
-
 # delete output from previous run
 if [ -e "./ACTUAL.TXT" ]
 then
     rm ACTUAL.TXT
 fi
-
-# compile the code into the bin folder, terminates if error occurred
-#if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/duke/Main.java ../src/main/java/duke/**/*.java
-#then
-#    echo "********** BUILD FAILURE **********"
-#    exit 1
-#fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -jar ../build/libs/duke.jar cmd < input.txt &> ACTUAL.TXT
