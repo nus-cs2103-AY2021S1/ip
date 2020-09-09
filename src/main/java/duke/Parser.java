002bb407f02ab012c7c9ad82a235dfefca2a5d7a
Tan Su Yin <e0425292@u.nus.edu>
2020-09-09 19:48:11 +0800

    Add Java assertions

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -12,37 +12,15 @@ public class Parser {
      */
     public Parser() {
     }
-
-    /**
-     * Returns String with trimmed leading white spaces.
-     * @param input String to be trimmed of leading white spaces.
-     * @return String that has been trimmed of leading white spaces.
-     */
-
-    public String sanitiseInput(String input) {
-        return input.stripLeading();
-    }
-
+    
     /**
      * Processes user input. 
      * @param userInput String to be processed.
      */
     public void parse(String userInput) {
         this.splitUserInput = userInput.split(" ", 2);
-        this.sanitisedUserInput = userInput;
-        if (this.splitUserInput[0].isBlank()) {
-            this.sanitisedUserInput = sanitiseInput(userInput);
-            this.splitUserInput = this.sanitisedUserInput.split(" ", 2);
-        }
     }
-
-    /**
-     * Returns user input String that has been trimmed of white spaces, if any.
-     * @return Formatted String.
-     */
-    public String getSanitisedUserInput() {
-        return this.sanitisedUserInput;
-    }
+    
 
     /**
      * Returns user input String array that has been split.
--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -9,6 +9,7 @@ import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.io.IOException;
 
 /**
  * Represents a Storage that stores tasks.
@@ -51,16 +52,19 @@ public class Storage {
                         ToDo todo = new ToDo(task[3]);
                         if (Integer.parseInt(task[2]) == 1) todo.markAsDone();
                         storedTasks.add(todo);
+                        globalIndex++;
                         break;
                     case "D":
                         Deadline deadline = new Deadline(task[3]);
                         if (Integer.parseInt(task[2]) == 1) deadline.markAsDone();
                         storedTasks.add(deadline);
+                        globalIndex++;
                         break;
                     case "E":
                         Event event = new Event(task[3]);
                         if (Integer.parseInt(task[2]) == 1) event.markAsDone();
                         storedTasks.add(event);
+                        globalIndex++;
                         break;
                     }
                 }
@@ -74,12 +78,12 @@ public class Storage {
     /**
      * Writes a new task to file containing stored tasks.
      */
-    public void writeToFile(Task t, String input) {
-        String splitTask[] = input.split(" ", 2);
-        if (t != null) {
-            String type = "";
-            int done = 0;
-            try {
+    public static void writeToFile(Task t, String[] input) {
+        assert (t != null) : "Task is null";
+        String type = "";
+        int done = 0;
+        try {
+            if (t != null) {
                 if (t instanceof ToDo) {
                     type = "T";
                 } else if (t instanceof Deadline) {
@@ -87,17 +91,17 @@ public class Storage {
                 } else if (t instanceof Event) {
                     type = "E";
                 }
-                
+
                 if (t.isDone()) {
                     done = 1;
                 }
                 FileWriter fw = new FileWriter("storage" + File.separator + "data.txt", true);
-                fw.write(globalIndex + "|" + type + "|" + done + "|" + splitTask[1] + "\n");
+                fw.write(globalIndex + "|" + type + "|" + done + "|" + input[1] + "\n");
                 globalIndex++;
                 fw.close();
-            } catch (Exception e) {
-                System.out.println(e);
             }
+        } catch (IOException e) {
+            System.out.println(e);
         }
     }
 
@@ -106,33 +110,33 @@ public class Storage {
      * @param taskIndex Number corresponding to the specific task to be removed.
      */
     public void removeFromFile(int taskIndex) {
+        assert !(taskIndex <= 0) : "Task index is invalid";
         try {
-            if (taskIndex != -1) {
-                int index = 1;
-                String tempFilePath = "storage" + File.separator + "temp.txt";
-                String oldFilePath = "storage" + File.separator + "data.txt";
-                File tempFile = new File(tempFilePath);
-                FileReader fr = new FileReader(oldFilePath);
-                BufferedReader br = new BufferedReader(fr);
-                String currentLine;
-                String[] task;
-                FileWriter fw = new FileWriter(tempFile, true);
+            int index = 1;
+            String tempFilePath = "storage" + File.separator + "temp.txt";
+            String oldFilePath = "storage" + File.separator + "data.txt";
+            File tempFile = new File(tempFilePath);
+            FileReader fr = new FileReader(oldFilePath);
+            BufferedReader br = new BufferedReader(fr);
+            String currentLine;
+            String[] task;
+            FileWriter fw = new FileWriter(tempFile, true);
 
-                while ((currentLine = br.readLine()) != null) {
-                    task = currentLine.split("\\|");
-                    if (Integer.parseInt(task[0]) != taskIndex) {
-                        fw.write(index + "|" + task[1] + "|" + task[2] + "|" + task[3] + "\n");
-                        index++;
-                    }
+            while ((currentLine = br.readLine()) != null) {
+                task = currentLine.split("\\|");
+                if (Integer.parseInt(task[0]) != taskIndex) {
+                    fw.write(index + "|" + task[1] + "|" + task[2] + "|" + task[3] + "\n");
+                    index++;
                 }
-                globalIndex = index;
-                fw.close();
-                fr.close();
-                br.close();
-                Files.copy(Paths.get(tempFilePath), Paths.get(oldFilePath), StandardCopyOption.REPLACE_EXISTING);
-                Files.delete(Paths.get(tempFilePath));
             }
-        } catch (Exception err) {
+            globalIndex = index;
+            fw.close();
+            fr.close();
+            br.close();
+            Files.copy(Paths.get(tempFilePath), Paths.get(oldFilePath), StandardCopyOption.REPLACE_EXISTING);
+            Files.delete(Paths.get(tempFilePath));
+            
+        } catch (IOException err) {
             System.out.println(err);
         }
     }
@@ -142,32 +146,32 @@ public class Storage {
      * @param taskIndex Number corresponding to the specific task to be modified.
      */
     public void overwriteInFile(int taskIndex) {
+        assert !(taskIndex <= 0) : "Task index is invalid";
         try {
-            if (taskIndex != -1) {
-                String tempFilePath = "storage" + File.separator + "temp.txt";
-                String oldFilePath = "storage" + File.separator + "data.txt";
-                File tempFile = new File(tempFilePath);
-                FileReader fr = new FileReader(oldFilePath);
-                BufferedReader br = new BufferedReader(fr);
-                String currentLine;
-                String[] task;
-                FileWriter fw = new FileWriter(tempFile, true);
+            String tempFilePath = "storage" + File.separator + "temp.txt";
+            String oldFilePath = "storage" + File.separator + "data.txt";
+            File tempFile = new File(tempFilePath);
+            FileReader fr = new FileReader(oldFilePath);
+            BufferedReader br = new BufferedReader(fr);
+            String currentLine;
+            String[] task;
+            FileWriter fw = new FileWriter(tempFile, true);
 
-                while ((currentLine = br.readLine()) != null) {
-                    task = currentLine.split("\\|");
-                    if (Integer.parseInt(task[0]) != taskIndex) {
-                        fw.write(currentLine + "\n");
-                    } else {
-                        fw.write(taskIndex + "|" + task[1] + "|" + 1 + "|" + task[3] + "\n");
-                    }
+            while ((currentLine = br.readLine()) != null) {
+                task = currentLine.split("\\|");
+                if (Integer.parseInt(task[0]) != taskIndex) {
+                    fw.write(currentLine + "\n");
+                } else {
+                    fw.write(taskIndex + "|" + task[1] + "|" + 1 + "|" + task[3] + "\n");
                 }
-                fw.close();
-                fr.close();
-                br.close();
-                Files.copy(Paths.get(tempFilePath), Paths.get(oldFilePath), StandardCopyOption.REPLACE_EXISTING);
-                Files.delete(Paths.get(tempFilePath));
             }
-        } catch (Exception err) {
+            fw.close();
+            fr.close();
+            br.close();
+            Files.copy(Paths.get(tempFilePath), Paths.get(oldFilePath), StandardCopyOption.REPLACE_EXISTING);
+            Files.delete(Paths.get(tempFilePath));
+            
+        } catch (IOException err) {
             System.out.println(err);
         }
     }
