e929bd3879832e8669f569de054bf5c89ebad0b5
Ma-Yueran <mayuerantom@outlook.com>
2020-09-09 11:02:55 +0800

    Set title of GUI window to be "Duke"

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setUp(duke, stage);
             fxmlLoader.<MainWindow>getController().onStart();
+            stage.setTitle("Duke");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
