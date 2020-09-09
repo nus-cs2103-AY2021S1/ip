ef4bebd4bfe0406f412ad614f93b1bf75cb55564
Diwu-Yi <59159857+Diwu-Yi@users.noreply.github.com>
2020-09-09 14:07:05 +0800

    Generate fatjar with Gradle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,10 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
- * A GUI for Duke using FXML.
+ * A launcher class to workaround classpath issues.
  */
-public class Main extends Application {
-
-    private Duke duke = new Duke();
-
-    @Override
-    public void start(Stage stage) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-            AnchorPane ap = fxmlLoader.load();
-            Scene scene = new Scene(ap);
-            stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setDuke(duke);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(Window.class, args);
     }
-
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -24,155 +24,152 @@ public class Ui {
 
         Storage.read();
 
-        while (true) {
-            //String input = this.scanner.nextLine();
-            if (!parser.isBye(input)) {
+        if (!parser.isBye(input)) {
 
-                if (!parser.isList(input) && !parser.isFind(input)) {
+            if (!parser.isList(input) && !parser.isFind(input)) {
 
-                    if (!parser.isDone(input) && !parser.isDelete(input)) {
+                if (!parser.isDone(input) && !parser.isDelete(input)) {
 
-                        if (parser.isTodo(input) || parser.isEvent(input)
-                                || parser.isDeadline(input)) {
+                    if (parser.isTodo(input) || parser.isEvent(input)
+                            || parser.isDeadline(input)) {
 
-                            String description;
-                            String correctedInput;
-                            DateAndTime byOrAt;
+                        String description;
+                        String correctedInput;
+                        DateAndTime byOrAt;
 
-                            if (parser.isTodo(input)) {
+                        if (parser.isTodo(input)) {
 
-                                if (parser.isValidTodo(input)) {
+                            if (parser.isValidTodo(input)) {
 
-                                    description = input.substring(Parser.TODO_VALIDATION.length(),
+                                description = input.substring(Parser.TODO_VALIDATION.length(),
                                             Parser.TODO_VALIDATION.length() + 1);
+
+                                if (!parser.isEmptyDescription(description)) {
+
+                                    correctedInput = input.substring(Parser.TODO_VALIDATION.length());
+                                    byOrAt = new DateAndTime();
+                                    TaskList.write(correctedInput, Parser.TODO, byOrAt);
+                                    Storage.write(TaskList.taskStorage);
+
+                                    return TaskList.write(correctedInput, Parser.TODO, byOrAt);
+
+                                } else {
+                                    return ("OOPS!!! The description of a todo cannot be empty.");
+                                }
+
+                            } else {
+                                return ("The description of a todo cannot be empty lah.");
+                            }
+
+                        } else {
+
+                            String dateAndTime = input.substring(input.indexOf("/")
+                                        + Parser.BYORAT.length());
+
+                            if (parser.isEvent(input)) {
+
+                                if (parser.isValidEvent(input)) {
+
+                                    description = input.substring(Parser.EVT_VALIDATION.length(),
+                                                Parser.EVT_VALIDATION.length() + 1);
 
                                     if (!parser.isEmptyDescription(description)) {
 
-                                        correctedInput = input.substring(Parser.TODO_VALIDATION.length());
+                                        byOrAt = new DateAndTime(dateAndTime);
 
-                                        byOrAt = new DateAndTime();
-                                        TaskList.write(correctedInput, Parser.TODO, byOrAt);
+                                        correctedInput = input.substring(Parser.EVT_VALIDATION.length());
+                                        TaskList.write(correctedInput, Parser.EVT, byOrAt);
                                         Storage.write(TaskList.taskStorage);
-                                        return TaskList.write(correctedInput, Parser.TODO, byOrAt);
-                                    } else {
-                                        return ("OOPS!!! The description of a todo cannot be empty.");
-                                    }
 
-                                } else {
-                                    return ("The description of a todo cannot be empty lah.");
-                                }
-
-                            } else {
-
-                                String dateAndTime = input.substring(input.indexOf("/")
-                                        + Parser.BYORAT.length());
-
-                                if (parser.isEvent(input)) {
-
-                                    if (parser.isValidEvent(input)) {
-
-                                        description = input.substring(Parser.EVT_VALIDATION.length(),
-                                                Parser.EVT_VALIDATION.length() + 1);
-
-                                        if (!parser.isEmptyDescription(description)) {
-
-                                            byOrAt = new DateAndTime(dateAndTime);
-
-                                            correctedInput = input.substring(Parser.EVT_VALIDATION.length());
-                                            TaskList.write(correctedInput, Parser.EVT, byOrAt);
-                                            Storage.write(TaskList.taskStorage);
-
-                                            return TaskList.write(correctedInput, Parser.EVT, byOrAt);
-                                        } else {
-                                            return ("The description of an event cannot be empty lah.");
-                                        }
+                                        return TaskList.write(correctedInput, Parser.EVT, byOrAt);
                                     } else {
                                         return ("The description of an event cannot be empty lah.");
                                     }
-
                                 } else {
-
-                                    if (parser.isValidDeadline(input)) {
-
-                                        description = input.substring(Parser.DDL_VALIDATION.length(),
-                                                Parser.DDL_VALIDATION.length() + 1);
-
-                                        if (!parser.isEmptyDescription(description)) {
-
-                                            byOrAt = new DateAndTime(dateAndTime);
-
-                                            correctedInput = input.substring(Parser.DDL_VALIDATION.length());
-                                            TaskList.write(correctedInput, Parser.DDL, byOrAt);
-                                            Storage.write(TaskList.taskStorage);
-
-                                            return TaskList.write(correctedInput, Parser.DDL, byOrAt);
-                                        } else {
-                                            return ("The description of a deadline cannot be empty lah.");
-                                        }
-                                    } else {
-                                        return ("The description of a deadline cannot be empty lah.");
-                                    }
-
+                                    return ("The description of an event cannot be empty lah.");
                                 }
-                            }
-
-                        } else {
-                            return ("OOPS!!! I'm sorry, but I don't know what that means :-(");
-                        }
-
-                    } else {
-
-                        if (parser.isDone(input)) {
-
-                            int ref = Integer.parseInt(Character.toString(
-                                    input.charAt(Parser.DONE_VALID.length() - 1))) - 1;
-
-                            if (TaskList.taskStorage.get(ref) != null) {
-
-                                Task done = TaskList.taskStorage.get(ref).markAsDone();
-                                TaskList.taskStorage.remove(TaskList.taskStorage.get(ref));
-
-                                TaskList.taskStorage.add(ref, done);
-
-                                Storage.write(TaskList.taskStorage);
-
-                                return ("Nice! I've marked this task as done:\n"
-                                        + TaskList.taskStorage.get(ref));
 
                             } else {
 
-                                return ("I am afraid that it is not possible" +
-                                        "to do an unknown task.");
+                                if (parser.isValidDeadline(input)) {
+
+                                    description = input.substring(Parser.DDL_VALIDATION.length(),
+                                                Parser.DDL_VALIDATION.length() + 1);
+
+                                    if (!parser.isEmptyDescription(description)) {
+
+                                        byOrAt = new DateAndTime(dateAndTime);
+
+                                        correctedInput = input.substring(Parser.DDL_VALIDATION.length());
+                                        TaskList.write(correctedInput, Parser.DDL, byOrAt);
+                                        Storage.write(TaskList.taskStorage);
+
+                                        return TaskList.write(correctedInput, Parser.DDL, byOrAt);
+                                    } else {
+                                        return ("The description of a deadline cannot be empty lah.");
+                                    }
+                                } else {
+                                    return ("The description of a deadline cannot be empty lah.");
+                                }
 
                             }
+                        }
+
+                    } else {
+                        return ("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
+
+                } else {
+
+                    if (parser.isDone(input)) {
+
+                        int ref = Integer.parseInt(Character.toString(
+                                    input.charAt(Parser.DONE_VALID.length() - 1))) - 1;
+
+                        if (TaskList.taskStorage.get(ref) != null) {
+
+                            Task done = TaskList.taskStorage.get(ref).markAsDone();
+                            TaskList.taskStorage.remove(TaskList.taskStorage.get(ref));
+
+                            TaskList.taskStorage.add(ref, done);
+
+                            Storage.write(TaskList.taskStorage);
+
+                            return ("Nice! I've marked this task as done:\n"
+                                        + TaskList.taskStorage.get(ref));
 
                         } else {
 
-                            int ref = Integer.parseInt(Character.toString(
-                                    input.charAt(Parser.DEL_VALID.length() - 1))) - 1;
-
-                            TaskList.delete(ref);
-
-                            Storage.write(TaskList.taskStorage);
-                            return TaskList.delete(ref);
+                            return ("I am afraid that it is not possible" +
+                                        "to do an unknown task.");
 
                         }
 
-                    }
-                } else {
-
-                    if (!parser.isFind(input)) {
-                        return TaskList.read();
                     } else {
-                        return TaskList.find(input.substring(Parser.FIND.length() + 1));
-                    }
-                }
 
+                        int ref = Integer.parseInt(Character.toString(
+                                    input.charAt(Parser.DEL_VALID.length() - 1))) - 1;
+
+                        TaskList.delete(ref);
+
+                        Storage.write(TaskList.taskStorage);
+                        return TaskList.delete(ref);
+
+                    }
+
+                }
             } else {
-                return " Bye. Hope to see you again soon!";
-                //this.scanner.close();
-                //System.exit(0);
+
+                if (!parser.isFind(input)) {
+                    return TaskList.read();
+                } else {
+                    return TaskList.find(input.substring(Parser.FIND.length() + 1));
+                }
             }
+
+        } else {
+            return " Bye. Hope to see you again soon!";
+
         }
 
     }
