b9a46fdf4fbd8f0642aacb0f4df70cb8849d93b3
Wong-ZZ <wongzhengzhi@gmail.com>
2020-09-09 21:49:04 +0800

    Fix bug in parsing deadline/event parsing

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -4,12 +4,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
 import duke.exception.InvalidDeadlineException;
-import duke.parser.DateTimeParsing;
+import duke.util.DukeDateTime;
 
 public class Deadline extends Task {
     private static final String DEADLINE_SYMBOL = "[D]";
     private static final String DEADLINE_NAME = "deadline ";
-    private static final String DEADLINE_BY = "/by ";
+    private static final String DEADLINE_KEYWORD = "/by ";
 
     private final String time12h;
     private final LocalDate date;
@@ -28,17 +28,24 @@ public class Deadline extends Task {
      * @throws InvalidDeadlineException If the format of the details is invalid.
      */
     protected static Deadline createDeadline(String details) throws InvalidDeadlineException {
-        String[] info = details.split("/");
-        if (info.length == 1) {
+        boolean hasKeyword = details.contains(DEADLINE_KEYWORD);
+
+        if (!hasKeyword) {
             throw new InvalidDeadlineException();
         }
+        String[] info = details.split(DEADLINE_KEYWORD);
         String desc = info[0];
-        String[] dateTime = info[1].replaceFirst("by ", "").split(" ");
+        String[] dateTime = info[1].trim().split(" ");
+        if (dateTime.length < 2) {
+            throw new InvalidDeadlineException();
+        }
         try {
-            LocalDate date = DateTimeParsing.parseDate(dateTime[0]);
-            String time12h = DateTimeParsing.to12HTimeFormat(dateTime[1]);
+            LocalDate date = DukeDateTime.parseDate(dateTime[0]);
+            String time12h = DukeDateTime.to12HTimeFormat(dateTime[1]);
             return new Deadline(desc, time12h, date);
         } catch (DateTimeParseException | NumberFormatException e) {
+            System.err.println(dateTime[0]);
+            System.err.println(dateTime[1]);
             throw new InvalidDeadlineException();
         }
     }
@@ -50,14 +57,14 @@ public class Deadline extends Task {
 
     @Override
     public String toSaveString() {
-        String date = DateTimeParsing.localDateToString(this.date);
-        String time = DateTimeParsing.to24HTimeFormat(time12h);
-        return (isDone ? 1 : 0) + DEADLINE_NAME + description + DEADLINE_BY + date + " " + time;
+        String date = DukeDateTime.localDateToString(this.date);
+        String time = DukeDateTime.to24HTimeFormat(time12h);
+        return (isDone ? 1 : 0) + DEADLINE_NAME + description + DEADLINE_KEYWORD + date + " " + time;
     }
 
     @Override
     public String toString() {
-        String formattedDate = DateTimeParsing.localDateToFormattedString(date);
+        String formattedDate = DukeDateTime.localDateToFormattedString(date);
         return DEADLINE_SYMBOL + super.toString() + "(by: " + formattedDate + " " + time12h + ")";
     }
 }
