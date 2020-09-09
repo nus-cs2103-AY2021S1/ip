11358e229f5b2830fa50a7b0129b4b18c818a759
akgrenSoar <44816346+akgrenSoar@users.noreply.github.com>
2020-09-09 20:58:37 +0800

    Update runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # create bin directory if it doesn't exist
-if [ ! -d "../bin" ]
-then
-    mkdir ../bin
-fi
+#if [ ! -d "../bin" ]
+#then
+#    mkdir ../bin
+#fi
 
 # delete output from previous run
 if [ -e "./ACTUAL.TXT" ]
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/duke/Main.java ../src/main/java/duke/**/*.java
-then
-    echo "********** BUILD FAILURE **********"
-    exit 1
-fi
+#if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/duke/Main.java ../src/main/java/duke/**/*.java
+#then
+#    echo "********** BUILD FAILURE **********"
+#    exit 1
+#fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin duke/Main < input.txt > ACTUAL.TXT
+java -jar ../build/libs/duke.jar cmd < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
