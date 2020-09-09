e2a4c4d07bfb9450a1f21bb009e266837e541889
yuming7144 <yuming7144@gmail.com>
2020-09-09 23:36:04 +0800

    Modify runtest bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -10,7 +10,7 @@ REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
-dir /s /B ..\src\*.java > sources.txt
+find /s /B ..\src\main\java -name "*.java" > sources.txt
 javac  -cp ..\src -Xlint:none -d ..\bin @sources.txt
 
 IF ERRORLEVEL 1 (
