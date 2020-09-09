feaa72ee4f6372c3ba323f2e96587a5aa593a227
Soon Xiang, Yan <65216986+y4ns00n@users.noreply.github.com>
2020-09-10 03:31:26 +0800

    add assertions

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -16,21 +16,15 @@ public class Parser {
     public String commandParser(String command, TaskList list, Storage store)
             throws DukeException, FileNotFoundException {
         ArrayList<Task> storage = list.getTasks();
-
-        //Scanner scan1 = new Scanner(System.in);
-
-        //String command = scan1.nextLine();
         try {
-            if (command.equals("")) {
-                throw new DukeException(command);
-            } else if (command.equals("list")) {
+            assert !command.equals("") : "Please type in your command.";
+            if (command.equals("list")) {
                 if (list.isEmpty()) {
                     throw new DukeException(command);
                 }
                 String tasks = "";
                 for (Task item : storage) {
                     int position = storage.indexOf(item) + 1;
-                    //System.out.println(position + "." + item);
                     tasks = tasks + position + "." + item + "\n";
                 }
                 return tasks;
@@ -40,8 +34,6 @@ public class Parser {
                     Task current = storage.get(number - 1);
                     current.setDone();
                     store.save(storage);
-                    //System.out.println("Nice! I've marked this task as done:");
-                    //System.out.println(current.getStatusIcon() + " " + current.getDescription());
                     return "Nice! I've marked this task as done: " + "\n"
                             + current.getStatusIcon() + " " + current.getDescription();
                 } catch (IndexOutOfBoundsException e) {
@@ -50,59 +42,49 @@ public class Parser {
                     throw new DukeException(command);
                 }
             } else if (command.startsWith("todo")) {
-                String desc = command.split("do ")[1];
-                if (desc.equals("")) {
+                if (command.equals("todo")) {
                     throw new DukeException(command);
                 }
+                String[] string = command.split("do ");
+                if (string.length < 2) {
+                    throw new DukeException("deadline");
+                }
+                String desc = string[1];
                 Todo todo = new Todo(desc);
 
                 store.save(storage, todo);
                 int size = storage.size();
-                //System.out.println("Got it. I've added this task:");
-                //System.out.println("  " + todo);
-                //System.out.println("Now you have " + size + " tasks in the list.");
                 return "Got it. I've added this task: " + "\n" + todo + "\n"
                         + "Now you have " + size + " tasks in the list.";
             } else if (command.startsWith("deadline")) {
-                String desc = command.split("/by ")[1];
-                if (desc.equals("")) {
-                    throw new DukeException(command);
+                if (!command.startsWith("deadline /by")) {
+                    throw new DukeException("deadline");
                 }
                 String[] string = command.split("/by ");
                 if (string.length < 2) {
-                    throw new DukeException(command);
+                    throw new DukeException("deadline");
                 }
                 Deadline deadline = new Deadline(string[0], string[1]);
                 store.save(storage, deadline);
                 int size = storage.size();
-                //System.out.println("Got it. I've added this task:");
-                //System.out.println("  " + deadline);
-                //System.out.println("Now you have " + size + " tasks in the list.");
                 return "Got it. I've added this task: " + "\n" + deadline + "\n"
                         + "Now you have " + size + " tasks in the list.";
             } else if (command.startsWith("event")) {
-                String desc = command.split("/at ")[1];
-                if (desc.equals("")) {
-                    throw new DukeException(command);
+                if (!command.startsWith("event /at")) {
+                    throw new DukeException("event");
                 }
                 String[] string = command.split("/at ");
                 if (string.length < 2) {
-                    throw new DukeException(command);
+                    throw new DukeException("event");
                 }
                 Events event = new Events(string[0], string[1]);
                 store.save(storage, event);
                 int size = storage.size();
-                //System.out.println("Got it. I've added this task:");
-                //System.out.println("  " + event);
-                //System.out.println("Now you have " + size + " tasks in the list.");
                 return "Got it. I've added this task: " + "\n" + event + "\n"
                         + "Now you have " + size + " tasks in the list.";
             } else if (command.equals("bye")) {
                 String bye = "Bye. Hope to see you again soon!";
                 return bye;
-                //System.out.println(bye);
-                //scan1.close();
-                //break;
             } else if (command.startsWith("delete")) {
                 try {
                     int number = Integer.parseInt(command.split(" ")[1]);
@@ -110,9 +92,6 @@ public class Parser {
                     storage.remove(number - 1);
                     store.save(storage);
                     int size = storage.size();
-                    //System.out.println("Noted. I've removed this task:");
-                    //System.out.println("  " + task);
-                    //System.out.println("Now you have " + size + " tasks in the list.");
                     return "Noted, I've removed this task: " + "\n" + task + "\n"
                             + "Now you have " + size + " tasks in the list.";
                 } catch (IndexOutOfBoundsException e) {
@@ -131,7 +110,6 @@ public class Parser {
                 String tasks = "";
                 for (Task item : storage) {
                     if (item.getDescription().contains(keyword)) {
-                        //System.out.println(count + "." + item);
                         count++;
                         tasks = tasks + count + "." + item + "\n";
                     }
