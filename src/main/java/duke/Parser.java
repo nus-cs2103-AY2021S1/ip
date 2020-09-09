d4ea1d0894686bb3c3d8a2b426c58b77d4dfe8a8
yuming7144 <yuming7144@gmail.com>
2020-09-09 12:31:28 +0800

    Modify Parser class

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -34,85 +34,171 @@ public class Parser {
      * @throws DukeException DukeException if command is not in legal form.
      */
     public static String parseCommand(String command) throws DukeException {
-        if (command.equals("hi")) {
-            return Ui.greet();
-        } else if (command.equals("bye")) {
-            Storage.writeToFile(taskList);
-            return Ui.getExitMessage();
-        } else if (command.equals("list")) {
-            return taskList.taskListToString();
+        if (command.equals("hi") || command.equals("bye") || command.equals("list")) {
+            return parseOneWordCommand(command);
         } else {
             String[] strArr = command.split(" ", 2);
             String taskType = strArr[0];
+            String userInput = "";
+            if (strArr.length != 1) {
+                userInput = strArr[1];
+            }
             switch (taskType) {
-            case "done": {
-                int taskNumber = Integer.parseInt(strArr[1]);
-                return taskList.markTaskAsDone(taskNumber);
-            }
-            case "tag": {
-                String[] data = strArr[1].split(" ", 2);
-                int taskNumber = Integer.parseInt(data[0]);
-                String tagName = data[1];
-                return taskList.tagTaskInList(taskNumber, tagName);
-            }
-            case "delete": {
-                int taskNumber = Integer.parseInt(strArr[1]);
-                if (taskNumber <= taskList.getNumOfTask()) {
-                    return taskList.deleteTask(taskNumber);
-                } else {
-                    throw new DukeException("You only have " + taskList.getNumOfTask() + " tasks in your task list.");
-                }
-            }
+            case "done":
+                return parseDoneCommand(userInput);
+            case "tag":
+                return parseTagCommand(userInput);
+            case "delete":
+                return parseDeleteCommand(userInput);
             case "find":
-                String keyword = strArr[1];
-                return taskList.findTaskByKeyword(keyword);
+                return parseFindCommand(userInput);
             case "todo":
-                if (strArr.length == 1) {
-                    throw new DukeException("The description of a todo cannot be empty.");
-                } else {
-                    Todo todo = new Todo(strArr[1]);
-                    taskList.addTask(todo);
-                    return taskList.addedTaskToString(todo);
-                }
+                return parseToDoCommand(userInput);
             case "deadline":
-                if (strArr.length == 1) {
-                    throw new DukeException("The description of a deadline cannot be empty.");
-                } else {
-                    String[] deadlineArr = strArr[1].split(" /by ", 2);
-                    if (deadlineArr.length != 2) {
-                        throw new DukeException("Wrong format when describing a deadline.");
-                    } else {
-                        if (TimeParser.isValidTime(deadlineArr[1])) {
-                            System.out.println(deadlineArr[1]);
-                            String deadlineTime = TimeParser.parseTime(deadlineArr[1]);
-                            Deadline deadline = new Deadline(deadlineArr[0], deadlineTime);
-                            taskList.addTask(deadline);
-                            return taskList.addedTaskToString(deadline);
-                        } else {
-                            throw new DukeException("Wrong format when describing a date.");
-                        }
-                    }
-                }
+                return parseDeadlineCommand(userInput);
             case "event":
-                if (strArr.length == 1) {
-                    throw new DukeException("The description of an event cannot be empty.");
-                } else {
-                    String[] eventArr = strArr[1].split(" /at ", 2);
-                    if (eventArr.length != 2) {
-                        throw new DukeException("Wrong format when describing an event.");
-                    } else {
-                        if (TimeParser.isValidTime(eventArr[1])) {
-                            String eventTime = TimeParser.parseTime(eventArr[1]);
-                            Event event = new Event(eventArr[0], eventTime);
-                            taskList.addTask(event);
-                            return taskList.addedTaskToString(event);
-                        } else {
-                            throw new DukeException("Wrong format when describing a date.");
-                        }
-                    }
-                }
+                return parseEventCommand(userInput);
             default:
                 throw new DukeException("I'm sorry, but I don't know what that means :-(");
+            }
+        }
+    }
+
+    /**
+     * Processes hi, bye, list command.
+     *
+     * @param command A hi, bye, or list command.
+     * @return The response string.
+     * @throws DukeException DukeException.
+     */
+    public static String parseOneWordCommand(String command) throws DukeException {
+        switch (command) {
+        case "hi":
+            return Ui.greet();
+        case "bye":
+            Storage.writeToFile(taskList);
+            return Ui.getExitMessage();
+        case "list":
+            return taskList.taskListToString();
+        default:
+            throw new DukeException("I'm sorry, but I don't know what that means :-(");
+        }
+    }
+
+    /**
+     * Processes done command.
+     *
+     * @param input Details of done command.
+     * @return The response string.
+     */
+    public static String parseDoneCommand(String input) {
+        int taskNumber = Integer.parseInt(input);
+        return taskList.markTaskAsDone(taskNumber);
+    }
+
+    /**
+     * Processes tag command.
+     *
+     * @param userInput Details of tag command.
+     * @return The response string.
+     */
+    public static String parseTagCommand(String userInput) {
+        String[] data = userInput.split(" ", 2);
+        int taskNumber = Integer.parseInt(data[0]);
+        String tagName = data[1];
+        return taskList.tagTaskInList(taskNumber, tagName);
+
+    }
+
+    /**
+     * Processes delete command.
+     *
+     * @param userInput Details of delete command.
+     * @return The response string.
+     */
+    public static String parseDeleteCommand(String userInput) throws DukeException {
+        int taskNumber = Integer.parseInt(userInput);
+        if (taskNumber <= taskList.getNumOfTask()) {
+            return taskList.deleteTask(taskNumber);
+        } else {
+            throw new DukeException("You only have " + taskList.getNumOfTask() + " tasks in your task list.");
+        }
+    }
+
+    /**
+     * Processes find command.
+     *
+     * @param userInput Details of find command.
+     * @return The response string.
+     */
+    public static String parseFindCommand(String userInput) {
+        return taskList.findTaskByKeyword(userInput);
+    }
+
+    /**
+     * Processes to do command.
+     *
+     * @param userInput Details of to do command.
+     * @return The response string.
+     * @throws DukeException DukeException if description of a to do is empty.
+     */
+    public static String parseToDoCommand(String userInput) throws DukeException {
+        if (userInput.equals("")) {
+            throw new DukeException("The description of a todo cannot be empty.");
+        } else {
+            Todo todo = new Todo(userInput);
+            taskList.addTask(todo);
+            return taskList.addedTaskToString(todo);
+        }
+    }
+
+    /**
+     * Processes deadline command.
+     *
+     * @param userInput Details of deadline command.
+     * @return The response string.
+     * @throws DukeException DukeException if description of a deadline is in wrong format.
+     */
+    public static String parseDeadlineCommand(String userInput) throws DukeException {
+        if (userInput.equals("")) {
+            throw new DukeException("The description of a deadline cannot be empty.");
+        } else {
+            String[] deadlineArr = userInput.split(" /by ", 2);
+            if (deadlineArr.length != 2) {
+                throw new DukeException("Wrong format when describing a deadline.");
+            } else if (TimeParser.isValidTime(deadlineArr[1])) {
+                System.out.println(deadlineArr[1]);
+                String deadlineTime = TimeParser.parseTime(deadlineArr[1]);
+                Deadline deadline = new Deadline(deadlineArr[0], deadlineTime);
+                taskList.addTask(deadline);
+                return taskList.addedTaskToString(deadline);
+            } else {
+                throw new DukeException("Wrong format when describing a date.");
+            }
+        }
+    }
+
+    /**
+     * Processes event command.
+     *
+     * @param userInput Details of event command.
+     * @return The response string.
+     * @throws DukeException DukeException if description of an event is empty.
+     */
+    public static String parseEventCommand(String userInput) throws DukeException {
+        if (userInput.equals("")) {
+            throw new DukeException("The description of an event cannot be empty.");
+        } else {
+            String[] eventArr = userInput.split(" /at ", 2);
+            if (eventArr.length != 2) {
+                throw new DukeException("Wrong format when describing an event.");
+            } else if (TimeParser.isValidTime(eventArr[1])) {
+                String eventTime = TimeParser.parseTime(eventArr[1]);
+                Event event = new Event(eventArr[0], eventTime);
+                taskList.addTask(event);
+                return taskList.addedTaskToString(event);
+            } else {
+                throw new DukeException("Wrong format when describing a date.");
             }
         }
     }
