40833386c2130b365c5b3ec4e1b8a92e6053f6f1
yuming7144 <yuming7144@gmail.com>
2020-09-09 22:56:08 +0800

    Update runtest.bat again

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,3 +1,4 @@
+
 @ECHO OFF
 
 set JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
@@ -10,7 +11,7 @@ del ACTUAL.TXT
 
 REM compile the code into the bin folder
 dir /s /B ..\src\*.java > sources.txt
-javac  -cp ..\src -Xlint:none -d ../bin @sources.txt
+javac  -cp ..\src -Xlint:none -d ..\bin ..\src\Duke.java
 
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
@@ -20,7 +21,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin duke.Duke < input.txt > ACTUAL.TXT
+java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
