01e15867788c4ad241d33d0d2b51bbeabecaaeee
WayneTanKinLun <waynetan2000@gmail.com>
2020-09-10 23:11:00 +0800

    Improve Code Quality
    Let's,
    - Implement SLAP
    - reduce long method

--- src/main/java/StorageParser.java
+++ src/main/java/StorageParser.java
@@ -3,11 +3,29 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StorageParser {
-
     public static boolean isDone(String symbol) {
         return symbol.equals("\u2713");
     }
 
+    private static Deadline createDeadline(boolean isDone, String[] splitTask) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+
+        String organisedDescription = splitTask[1].replaceAll(" \\(by: ", "|")
+                .replaceAll("\\)", "");
+        String[] splitDescription = organisedDescription.split("\\|");
+        LocalDate localDate = LocalDate.parse(splitDescription[1], formatter);
+        return new Deadline(isDone, splitDescription[0], localDate);
+    }
+
+    private static Event createEvent(boolean isDone, String[] splitTask) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mma");
+
+        String organisedDescription = splitTask[1].replaceAll(" \\(at: ", "|")
+                .replaceAll("\\)", "");
+        String[] splitDescription = organisedDescription.split("\\|");
+        LocalDateTime localDateAndTime = LocalDateTime.parse(splitDescription[1], formatter);
+        return new Event(isDone, splitDescription[0], localDateAndTime);
+    }
     /**
      * Parse a line of String into Task.
      * @param input String
@@ -16,29 +34,18 @@ public class StorageParser {
     public static Task parse(String input) {
         Task task = null;
         String[] splitTask = input.split(" ", 2);
+
         String typeAndIsDone = splitTask[0].replaceFirst("\\]", "|")
                 .replaceAll("\\[", "").replaceAll("\\]", "");
         String[] typeAndIsDoneArr = typeAndIsDone.split("\\|");
+        boolean isDone = isDone(typeAndIsDoneArr[1]);
 
         if (typeAndIsDoneArr[0].equals("T")) {
-            boolean isDone = isDone(typeAndIsDoneArr[1]);
             task = new Todo(isDone, splitTask[1]);
         } else if (typeAndIsDoneArr[0].equals("D")) {
-            boolean isDone = isDone(typeAndIsDoneArr[1]);
-            String organisedDescription = splitTask[1].replaceAll(" \\(by: ", "|")
-                    .replaceAll("\\)", "");
-            String[] splitDescription = organisedDescription.split("\\|");
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
-            LocalDate localDate = LocalDate.parse(splitDescription[1], formatter);
-            task = new Deadline(isDone, splitDescription[0], localDate);
+            task = createDeadline(isDone, splitTask);
         } else if (typeAndIsDoneArr[0].equals("E")) {
-            boolean isDone = isDone(typeAndIsDoneArr[1]);
-            String organisedDescription = splitTask[1].replaceAll(" \\(at: ", "|")
-                    .replaceAll("\\)", "");
-            String[] splitDescription = organisedDescription.split("\\|");
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mma");
-            LocalDateTime localDateAndTime = LocalDateTime.parse(splitDescription[1], formatter);
-            task = new Event(isDone, splitDescription[0], localDateAndTime);
+            task = createEvent(isDone, splitTask);
         } else {
             System.out.println("error in reading input");
         }
