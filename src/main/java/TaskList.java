8ed4cfe7b4b11969b8fe71ac234d6450bf5305f3
plosslaw <spacequartz@gmail.com>
2020-09-10 19:40:09 +0800

    Add instance methods to TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskList {
+    private final List<Task> taskList = new ArrayList<>();
+
+    public void addTask() {
+
+    }
+
+    public void deleteTask() {
+
+    }
+
+    public void deleteAllTasks() {
+
+    }
+
+    public void displayTasks() {
+
+    }
+
+    public void setTaskDone() {
+        
+    }
 }
