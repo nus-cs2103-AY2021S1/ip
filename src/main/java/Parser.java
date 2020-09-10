474c6bef36fd580dac25cebc968288f78e110bde
xnmng <puaxuanmingpxm18@gmail.com>
2020-09-10 21:29:07 +0800

    Implement C-FriendlierSyntax

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -5,20 +5,27 @@ public class Parser {
         String[] commandArgs = userCommand.split(" ");
 
         switch (commandArgs[0]) {
+        case ("b"):
         case ("bye") :
             return new ByeCommand();
+        case ("l"):
         case ("list") :
             return new ListCommand();
         case ("done") :
             return new DoneCommand(userCommand);
+        case ("del"):
         case ("delete") :
             return new DeleteCommand(userCommand);
+        case ("e"):
         case ("event") :
             return new EventCommand(userCommand);
+        case("d"):
         case ("deadline") :
             return new DeadlineCommand(userCommand);
+        case ("t"):
         case ("todo") :
             return new ToDoCommand(userCommand);
+        case ("f"):
         case ("find") :
             return new FindCommand(userCommand);
         default :
