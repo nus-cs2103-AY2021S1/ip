89707ee44df88b636dfac53c1283a8f78317811d
zhengweii <limzhengwei_99@hotmail.com>
2020-09-11 17:49:18 +0800

    Move parsing logic to Parser class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,9 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Parser {
     public static final DateTimeFormatter DATE_INPUT_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -13,31 +17,31 @@ public class Parser {
      */
     public String parseCommands(TaskList tasks, Ui ui, String command) throws DukeException {
         String output;
-        String[] split = command.split(" ");
+        String[] splitCommand = command.split(" ");
 
-        switch (split[0]) {
+        switch (splitCommand[0]) {
         case "bye":
             output = ui.getExitMessage();
             break;
 
         case "deadline":
-            output = tasks.addNewDeadline(Arrays.copyOfRange(split, 1, split.length));
+            output = this.parseDeadline(splitCommand, tasks);
             break;
 
         case "delete":
-            output = tasks.deleteTask(split[1]);
+            output = tasks.deleteTask(this.parseIndexFromString(splitCommand));
             break;
 
         case "done":
-            output = tasks.markTaskAsDone(split[1]);
+            output = tasks.markTaskAsDone(this.parseIndexFromString(splitCommand));
             break;
 
         case "event":
-            output = tasks.addNewEvent(Arrays.copyOfRange(split, 1, split.length));
+            output = this.parseEvent(splitCommand, tasks);
             break;
 
         case "find":
-            output = tasks.findTasks(split[1]);
+            output = tasks.findTasks(splitCommand[1]);
             break;
 
         case "list":
@@ -45,7 +49,7 @@ public class Parser {
             break;
 
         case "todo":
-            output = tasks.addNewToDo(split[1]);
+            output = this.parseToDo(splitCommand, tasks);
             break;
 
         default:
@@ -55,5 +59,96 @@ public class Parser {
         assert output != null;
         assert !output.isEmpty();
         return output;
+    }
+
+    private String parseDeadline(String[] splitCommand, TaskList tasks) throws DukeException {
+        if (splitCommand.length < 2) {
+            throw new DukeException("Description of deadline cannot be empty");
+        }
+
+        int byIndex = IntStream.range(0, splitCommand.length)
+                .filter(i -> splitCommand[i].equals("by"))
+                .findFirst()
+                .orElse(-1);
+
+        if (byIndex == -1 || (byIndex + 3 > splitCommand.length)) {
+            throw new DukeException("Date and time of deadline cannot be empty");
+        }
+
+        String deadlineDescription = String.join(" ",
+                Arrays.copyOfRange(splitCommand, 1, splitCommand.length - 3));
+        String date = splitCommand[splitCommand.length - 2];
+        String time = splitCommand[splitCommand.length - 1];
+
+        try {
+            LocalDate parsedDate = LocalDate.parse(date, Parser.DATE_INPUT_FORMAT);
+            LocalTime parsedTime = LocalTime.parse(time.substring(0, 2) + ":" + time.substring(2));
+            String formattedDate = parsedDate.format(Parser.DATE_OUTPUT_FORMAT);
+            String formattedTime = parsedTime.format(Parser.TIME_OUTPUT_FORMAT);
+            return tasks.addNewDeadline(deadlineDescription, formattedDate, formattedTime);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Please input the date and time in a valid format e.g. by 20/02/2020 1600");
+        }
+    }
+
+    private String parseEvent(String[] splitCommand, TaskList tasks) throws DukeException {
+        if (splitCommand.length < 2) {
+            throw new DukeException("Description of event cannot be empty");
+        }
+
+        int atIndex = IntStream.range(0, splitCommand.length)
+                .filter(i -> splitCommand[i].equals("at"))
+                .findFirst()
+                .orElse(-1);
+
+        if (atIndex == -1 || (atIndex + 3 > splitCommand.length)) {
+            throw new DukeException("Date, start and end time of event cannot be empty");
+        }
+
+        String eventDescription = String.join(" ", Arrays.copyOfRange(splitCommand, 1, splitCommand.length - 3));
+        String date = splitCommand[splitCommand.length - 2];
+        String time = splitCommand[splitCommand.length - 1];
+
+        try {
+            LocalDate parsedDate = LocalDate.parse(date, Parser.DATE_INPUT_FORMAT);
+            String[] startEndTime = time.split("-");
+            if (startEndTime.length < 2) {
+                throw new DukeException("Please enter the end timing of the event e.g. at 20/02/2020 1600-1800");
+            }
+            LocalTime startTime = LocalTime.parse(startEndTime[0].substring(0, 2) + ":" + startEndTime[0].substring(2));
+            LocalTime endTime = LocalTime.parse(startEndTime[1].substring(0, 2) + ":" + startEndTime[1].substring(2));
+
+            String formattedDate = parsedDate.format(Parser.DATE_OUTPUT_FORMAT);
+            String formattedStartTime = startTime.format(Parser.TIME_OUTPUT_FORMAT);
+            String formattedEndTime = endTime.format(Parser.TIME_OUTPUT_FORMAT);
+            return tasks.addNewEvent(eventDescription, formattedDate, formattedStartTime, formattedEndTime);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Please input the date, start and end timing in a valid format "
+                    + "e.g. at 20/02/2020 1600-1800");
+        }
+    }
+
+    private int parseIndexFromString(String[] splitCommand) throws DukeException {
+        if (splitCommand.length < 2) {
+            throw new DukeException("Index of the task cannot be empty");
+        }
+
+        try {
+            int index = Integer.parseInt(splitCommand[1]);
+            return index;
+        } catch (NumberFormatException e) {
+            throw new DukeException("Index must be a number");
+        }
+    }
+
+    private String parseToDo(String[] splitCommand, TaskList tasks) throws DukeException {
+        String description = String.join(" ",
+                Arrays.copyOfRange(splitCommand, 1, splitCommand.length));
+
+        if (description.isEmpty()) {
+            throw new DukeException("Description of ToDo cannot be empty");
+        }
+
+        return tasks.addNewToDo(description);
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,5 +1,3 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class TaskList {
@@ -28,104 +26,52 @@ public class TaskList {
      * Adds a new Deadline to the database.
      *
      * @return the details of the deadline added to the database.
-     * @throws DukeException if an invalid date time format is passed in.
      */
-    public String addNewDeadline(String[] taskInformation) throws DukeException {
-        if (taskInformation.length == 0) {
-            throw new DukeException("Description of deadline cannot be empty");
-        } else if (taskInformation.length >= 1 && taskInformation.length < 4) {
-            throw new DukeException("Please input the deadline in a valid format e.g. by 20/02/2020 1600");
-        }
-
-        LocalDate date = LocalDate.parse(taskInformation[2], Parser.DATE_INPUT_FORMAT);
-        LocalTime time = LocalTime.parse(taskInformation[3].substring(0, 2) + ":" + taskInformation[3].substring(2));
-
-        String parsedDate = date.format(Parser.DATE_OUTPUT_FORMAT);
-        String parsedTime = time.format(Parser.TIME_OUTPUT_FORMAT);
-        Deadline deadline = new Deadline(taskInformation[0], parsedDate + " " + parsedTime);
-        return this.addTask(deadline);
+    public String addNewDeadline(String deadlineDescription, String date, String time) throws DukeException {
+        return this.addTask(new Deadline(deadlineDescription, date + " " + time));
     }
 
     /**
      * Adds a new Event to the database.
      *
      * @return details of the event added to the database.
-     * @throws DukeException if an invalid date time format is passed in.
      */
-    public String addNewEvent(String[] taskInformation) throws DukeException {
-        if (taskInformation.length == 0) {
-            throw new DukeException("Description of the event cannot be empty");
-        } else if (taskInformation.length >= 1 && taskInformation.length < 4) {
-            throw new DukeException("Please input the start and end timing in a valid format"
-                    + "e.g. at 20/02/2020 1600-1800");
-        }
-
-        LocalDate date = LocalDate.parse(taskInformation[2], Parser.DATE_INPUT_FORMAT);
-        String[] startEndTime = taskInformation[3].split("-");
-        LocalTime startTime = LocalTime.parse(startEndTime[0].substring(0, 2) + ":"
-                + startEndTime[0].substring(2));
-        LocalTime endTime = LocalTime.parse(startEndTime[1].substring(0, 2) + ":"
-                + startEndTime[1].substring(2));
-
-        String parsedDate = date.format(Parser.DATE_OUTPUT_FORMAT);
-        String parsedStartTime = startTime.format(Parser.TIME_OUTPUT_FORMAT);
-        String parsedEndTime = endTime.format(Parser.TIME_OUTPUT_FORMAT);
-        Event event = new Event(taskInformation[0], parsedDate + " " + parsedStartTime + " to " + parsedEndTime);
-        return this.addTask(event);
+    public String addNewEvent(String eventDescription, String date, String startTime, String endTime)
+            throws DukeException {
+        return this.addTask(new Event(eventDescription, date + " " + startTime + " to " + endTime));
     }
 
     /**
      * Adds a new ToDo to the database.
      *
      * @return details of the ToDo added to the database.
-     * @throws DukeException if description is empty.
      */
     public String addNewToDo(String description) throws DukeException {
-        String output;
-
-        if (description.equals("")) {
-            throw new DukeException("To-do description cannot be empty");
-        }
-
-        ToDo toDo = new ToDo(description.trim());
-        output = this.addTask(toDo);
-        return output;
+        return this.addTask(new ToDo(description));
     }
 
     /**
      * Deletes a specific task from the database.
      *
      * @return a success string if the task was successfully deleted.
-     * @throws DukeException if database is empty, if the index passed in is out of bounds
-     * or if index passed in is not a number.
+     * @throws DukeException if database is empty, if the index passed in is out of bounds.
      */
-    public String deleteTask(String indexStr) throws DukeException {
-        String output;
-
+    public String deleteTask(int index) throws DukeException {
         if (this.database.size() == 0) {
             throw new DukeException("There are no tasks in the database to delete");
-        } else {
-            try {
-                int index = Integer.parseInt(indexStr);
-                if (index > this.database.size()) {
-                    throw new DukeException("Index cannot be greater than size of database");
-                }
-
-                if (index <= 0) {
-                    throw new DukeException("Index cannot be smaller than 1");
-                }
-
-                Task removedTask = this.database.remove(index - 1);
-                output = "Noted! I have removed this task:\n"
-                        + removedTask
-                        + "\n"
-                        + "Now you have " + this.database.size() + " tasks in your list";
-            } catch (NumberFormatException e) {
-                throw new DukeException("Index must be a number");
-            }
+        }
+        if (index > this.database.size()) {
+            throw new DukeException("Index cannot be greater than size of database");
+        }
+        if (index <= 0) {
+            throw new DukeException("Index cannot be smaller than 1");
         }
 
-        return output;
+        Task removedTask = this.database.remove(index - 1);
+        return "Noted! I have removed this task:\n"
+                + removedTask
+                + "\n"
+                + "Now you have " + this.database.size() + " tasks in your list";
     }
 
     /**
@@ -189,50 +135,39 @@ public class TaskList {
      * @throws DukeException if the database is empty.
      */
     public String listAllTasks() throws DukeException {
-        String output;
         if (this.database.size() == 0) {
             throw new DukeException("There are no tasks in the database");
-        } else {
-            output = "Here are the tasks in your list:\n";
-            for (int i = 0; i < this.database.size(); i++) {
-                output += ((i + 1) + ". " + this.database.get(i));
-                output += "\n";
-            }
-
-            return output;
         }
+
+        String output = "Here are the tasks in your list:\n";
+        for (int i = 0; i < this.database.size(); i++) {
+            output += ((i + 1) + ". " + this.database.get(i));
+            output += "\n";
+        }
+
+        return output;
     }
 
     /**
      * Marks a specific task as done.
      *
      * @return a success string if the task was successfully deleted.
-     * @throws DukeException if the database is empty, or if the index is out of bounds
-     * or if index passed in is not a number.
+     * @throws DukeException if the database is empty, or if the index is out of bounds.
      */
-    public String markTaskAsDone(String indexStr) throws DukeException {
-        String output;
-
-        if (this.database.size() != 0) {
-            try {
-                int index = Integer.parseInt(indexStr);
-                if (index > this.database.size()) {
-                    throw new DukeException("Index cannot be greater than size of database");
-                }
-
-                if (index <= 0) {
-                    throw new DukeException("Index cannot be lesser than 1");
-                }
-
-                this.database.get(index - 1).markAsDone();
-                output = "Nice! I have marked this task as done!";
-            } catch (NumberFormatException e) {
-                throw new DukeException("Index must be a number");
-            }
-        } else {
+    public String markTaskAsDone(int index) throws DukeException {
+        if (this.database.size() == 0) {
             throw new DukeException("There are no tasks in the database to mark as done");
         }
 
-        return output;
+        if (index > this.database.size()) {
+            throw new DukeException("Index cannot be greater than size of database");
+        }
+
+        if (index <= 0) {
+            throw new DukeException("Index cannot be lesser than 1");
+        }
+
+        this.database.get(index - 1).markAsDone();
+        return "Nice! I have marked this task as done!";
     }
 }
