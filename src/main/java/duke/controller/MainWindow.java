ae126ceb1b65aa3ae00308c767b6eb6c4602f759
hcy <2320032466@qq.com>
2020-09-12 00:33:08 +0800

    Implement A-Release

--- src/main/java/duke/controller/MainWindow.java
+++ src/main/java/duke/controller/MainWindow.java
@@ -38,8 +38,6 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
-
-
     }
 
     /**
