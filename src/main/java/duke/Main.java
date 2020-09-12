b6c9a83c946f3e8149cbb1f028d8903973c536ea
aizatazhar <aizat.azharr@gmail.com>
2020-09-12 10:11:24 +0800

    Refator code

---
+++ src/main/java/duke/Main.java
@@ -0,0 +1,38 @@
+package duke;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Duke duke = new Duke();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            scene.getStylesheets().add("/view/styles.css");
+
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.setTitle("Duke-it");
+            stage.getIcons().add(new Image("/images/duke.png"));
+
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
