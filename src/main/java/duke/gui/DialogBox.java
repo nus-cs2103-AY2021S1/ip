797e211759b0933e531a39b5d7b97257343b3922
Rayson Koh <rayson.rk.koh@gmail.com>
2020-09-12 08:54:26 +0800

    Add rounded avatar

--- src/main/java/duke/gui/DialogBox.java
+++ src/main/java/duke/gui/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
@@ -37,6 +38,8 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+        Circle clip = new Circle(50, 50, 50);
+        displayPicture.setClip(clip);
     }
 
     /**
