d4547818682eb9d022cde2b6381a4e024f85a238
kswk <kswk@live.com>
2020-09-12 21:00:11 +0800

    Change display pictures of user and Duke

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -24,8 +24,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/batman.png"));
+    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/oldman.png"));
 
     /**
      * Initialises the the chat bot.
@@ -60,7 +60,7 @@ public class MainWindow extends AnchorPane {
      */
     private void printIntro() {
         DialogBox intro = DialogBox.getDukeDialog(Ui.GREET_MESSAGE,
-                new Image(this.getClass().getResourceAsStream("/images/DaDuke.png")));
+                new Image(this.getClass().getResourceAsStream("/images/oldman.png")));
         dialogContainer.getChildren().addAll(intro);
     }
 }
