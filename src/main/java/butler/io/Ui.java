268918db6c0188e06fc81208b294de5ecaf8f2b9
HoPinXian <hopinxian@gmail.com>
2020-09-12 18:37:28 +0800

    Modify Ui.java to return string responses
    Ui.java has been modified to return string responses
    instead of printing them out. Scanner is removed from Ui.java.

--- src/main/java/butler/io/Ui.java
+++ src/main/java/butler/io/Ui.java
@@ -1,105 +1,102 @@
 package butler.io;
 
-import java.util.Scanner;
-
 import butler.task.Task;
 import butler.task.TaskList;
 
 /**
  * Represents a user interface that interacts with the user.
- * <code>Ui</code> prints statements for the user and scans user input.
+ * <code>Ui</code> returns string statements for the user.
  */
 public class Ui {
-    private Scanner sc;
-
     /**
      * Constructs a user interface.
      */
     public Ui() {
-        sc = new Scanner(System.in);
     }
 
     /**
-     * Prints out a welcome message.
+     * Returns a welcome message.
+     *
+     * @return Welcome message.
      */
-    public void showWelcome() {
+    public String showWelcome() {
         String greetings = "Welcome! I am your personal manager, Butler.\n"
                 + "How may I help you today?\n";
-        System.out.println(greetings);
+        return greetings;
     }
 
     /**
-     * Reads the user input.
-     * Only one line of user input is read.
+     * Returns a dotted line separator.
      *
-     * @return User input
+     * @return Dotted line separator.
      */
-    public String readCommand() {
-        return sc.nextLine();
+    public String showLine() {
+        return "\n------------------------------\n";
     }
 
     /**
-     * Prints a dotted line separator.
-     */
-    public void showLine() {
-        System.out.println("\n------------------------------\n");
-    }
-
-    /**
-     * Prints the <code>errorMessage</code> given.
+     * Returns the formatted <code>errorMessage</code> given.
      *
      * @param errorMessage Error message to be printed.
+     * @return Formatted error message.
      */
-    public void showError(String errorMessage) {
-        System.out.println("ERRORS: " + errorMessage);
+    public String showError(String errorMessage) {
+        return "ERRORS: " + errorMessage;
     }
 
     /**
-     * Prints the farewell message.
+     * Returns the farewell message.
+     *
+     * @return Farewell message.
      */
-    public void showExit() {
+    public String showExit() {
         String farewells = "It is my honor to have served you.\n"
                 + "Please come back again.";
-        System.out.println(farewells);
+        return farewells;
     }
 
     /**
-     * Prints the loading error message.
+     * Returns the loading error message.
+     *
+     * @return Loading error message.
      */
-    public void showLoadingError() {
+    public String showLoadingError() {
         String loadingErrorMessage = "tasks.txt file was not detected.\n"
                 + "A new task list will be created.";
-        System.out.println(loadingErrorMessage);
+        return loadingErrorMessage;
     }
 
     /**
-     * Prints a response message that a task is marked as completed.
+     * Returns a response message that a task is marked as completed.
      * The task is specified by the <code>taskIndex</code> given.
      *
      * @param taskIndex Index of task marked as completed.
+     * @return Response message that a task is marked as completed.
      */
-    public void showTaskCompleted(int taskIndex) {
+    public String showTaskCompleted(int taskIndex) {
         String msg = "Task " + taskIndex + " has been marked as complete.";
-        System.out.println(msg);
+        return msg;
     }
 
     /**
-     * Prints a response message that a task is added.
+     * Returns a response message that a task is added.
      * Task that was added is specified by <code>task</code>.
      *
      * @param task Task that was added.
+     * @return Response message that a task is added.
      */
-    public void showTaskAdded(Task task) {
+    public String showTaskAdded(Task task) {
         String msg = "The following task has been added to task list.\n" + task;
-        System.out.println(msg);
+        return msg;
     }
 
     /**
-     * Prints out the list of tasks in the given <code>taskList</code>.
+     * Returns a string of the list of tasks in the given <code>taskList</code>.
      *
      * @param taskList List of tasks to be printed out.
+     * @return A string of the list of tasks.
      */
-    public void printTaskList(TaskList taskList) {
+    public String printTaskList(TaskList taskList) {
         String msg = "Here are your list of tasks.\n"
                 + "You have " + taskList.getSize()
                 + (taskList.getSize() > 1 ? " tasks" : " task")
@@ -107,26 +104,28 @@ public class Ui {
         for (int i = 0; i < taskList.getSize(); i++) {
             msg += "\n" + (i + 1) + ": " + taskList.getTask(i);
         }
-        System.out.println(msg);
+        return msg;
     }
 
     /**
-     * Prints out a response message that a task was deleted.
+     * Returns a response message that a task was deleted.
      * Deleted task is specified by <code>taskIndex</code>.
      *
      * @param taskIndex Index of task that was deleted.
+     * @return Response message that a task was deleted.
      */
-    public void showTaskDeleted(int taskIndex) {
+    public String showTaskDeleted(int taskIndex) {
         String msg = "Task " + taskIndex + " has been deleted.";
-        System.out.println(msg);
+        return msg;
     }
 
     /**
-     * Prints out the given filtered list of tasks.
+     * Returns a string of the given filtered list of tasks.
      *
      * @param taskList List of tasks to be printed out.
+     * @return A string of the list of filtered tasks.
      */
-    public void printFilteredTaskList(TaskList taskList) {
+    public String printFilteredTaskList(TaskList taskList) {
         String msg = "Here are the matching tasks in your list.\n"
                 + "You have " + taskList.getSize()
                 + (taskList.getSize() > 1 ? " tasks" : " task")
@@ -134,6 +133,6 @@ public class Ui {
         for (int i = 0; i < taskList.getSize(); i++) {
             msg += "\n" + (i + 1) + ": " + taskList.getTask(i);
         }
-        System.out.println(msg);
+        return msg;
     }
 }
