10b52dca7b08f21dc51bbd1957241925f71868c1
w-yeehong <e0418195@comp.nus.edu.sg>
2020-09-13 17:17:48 +0800

    Update Shadow to v6.0.0

--- src/main/java/rogue/Main.java
+++ src/main/java/rogue/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         Rogue rogue = new Rogue(DEFAULT_SAVE_FILE_PATH);
+        stage.setTitle("Rogue"); // Sets title for the window
         Ui.init(stage, rogue);
     }
 }
