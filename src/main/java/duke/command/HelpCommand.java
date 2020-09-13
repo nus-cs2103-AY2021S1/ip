895f2a60ecc17c6fcc0d3d0e25086cbb506dfd6e
hansebastian <sebastian_hans@hotmail.com>
2020-09-13 17:49:38 +0800

    Fix checkstyle errors

--- src/main/java/duke/command/HelpCommand.java
+++ src/main/java/duke/command/HelpCommand.java
@@ -8,7 +8,7 @@ import duke.task.TaskList;
 /**
  * Command that displays all the commands that Duke is able to understand to the user.
  */
-public class HelpCommand extends Command{
+public class HelpCommand extends Command {
     /**
      * Displays all the commands that Duke supports to the user.
      *
@@ -19,16 +19,16 @@ public class HelpCommand extends Command{
      */
     @Override
     public String execute(TaskList taskList, Storage storage, History history) throws DukeException {
-        return "Here are all the commands that you can give me:\n" +
-                "- 'list' to see all the tasks\n" +
-                "- 'todo <description>' to add a new todo task\n" +
-                "- 'event <description> /at <YYYY/MM/DD> <HH:MM>' to add a new todo task\n" +
-                "- 'todo <description> /by <YYYY/MM/DD> <HH:MM>' to add a new todo task\n" +
-                "- 'delete <taskId>' to delete the task with the given ID\n" +
-                "- 'done <taskId>' to mark the task with the given ID as completed\n" +
-                "- 'find <keyword>' to find all tasks with the descriptions matching the given keyword\n" +
-                "- 'undo' to undo the previous command given to Duke\n" +
-                "- 'help' to view this list again\n" +
-                "- 'bye' to exit the program\n";
+        return "Here are all the commands that you can give me:\n"
+                + "- 'list' to see all the tasks\n"
+                + "- 'todo <description>' to add a new todo task\n"
+                + "- 'event <description> /at <YYYY/MM/DD> <HH:MM>' to add a new todo task\n"
+                + "- 'todo <description> /by <YYYY/MM/DD> <HH:MM>' to add a new todo task\n"
+                + "- 'delete <taskId>' to delete the task with the given ID\n"
+                + "- 'done <taskId>' to mark the task with the given ID as completed\n"
+                + "- 'find <keyword>' to find all tasks with the descriptions matching the given keyword\n"
+                + "- 'undo' to undo the previous command given to Duke\n"
+                + "- 'help' to view this list again\n"
+                + "- 'bye' to exit the program\n";
     }
 }
