a760fe54da24406dc0ff2f034da89548e434c81d
TianYong-Goh <goh_ty@users.noreply.github.com>
2020-09-13 15:31:26 +0800

    Update JUnit test

--- src/test/java/mug/tasks/TaskListTest.java
+++ src/test/java/mug/tasks/TaskListTest.java
@@ -16,18 +16,18 @@ public class TaskListTest {
     @Test
     public void addTask() {
         String actResult = this.taskList.addTask(Command.TODO, "borrow book");
-        String expResult = "Got it. mug.Mug has added this task:\n"
+        String expResult = "Got it. Mug has added this task:\n"
                 + "[T][\u2718] borrow book"
                 + "\nNow you have "
                 + 3
-                + " mug.tasks in the list.";
+                + " tasks in the list.";
         assertEquals(expResult, actResult);
     }
 
     @Test
     public void taskDone() {
         String actResult = this.taskList.taskDone(2);
-        String expResult = "Congratz! mug.Mug has marked this task as done:\n"
+        String expResult = "Congratz! Mug has marked this task as done:\n"
                 + "[T][\u2713] return book";
         assertEquals(expResult, actResult);
     }
@@ -35,11 +35,11 @@ public class TaskListTest {
     @Test
     public void deleteTask() {
         String actResult = this.taskList.deleteTask(2);
-        String expResult = "Noted. mug.Mug has removed this task:\n"
+        String expResult = "Noted. Mug has removed this task:\n"
             + "[T][\u2718] return book"
             + "\nNow you have "
             + 1
-            + " mug.tasks in the list.";
+            + " tasks in the list.";
 
         assertEquals(expResult, actResult);
     }
@@ -47,7 +47,7 @@ public class TaskListTest {
     @Test
     public void readList() {
         String actResult = this.taskList.readList();
-        String expResult = "Here is your mug.tasks:\n"
+        String expResult = "Here is your tasks:\n"
                 + "1. [T][\u2718] read book\n"
                 + "2. [T][\u2718] return book\n";
         assertEquals(expResult, actResult);
