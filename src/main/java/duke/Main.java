abdec8bb4eae52c03530fd9b674cd09e07cda71a
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-09-14 20:17:13 +0800

    Do level 1 javafx tutorial with 2 stages

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -1,13 +1,10 @@
 package duke;
 
-import duke.gui.MainWindow;
-
 import javafx.application.Application;
-
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+//import javafx.scene.layout.AnchorPane;
 
 /**
  * This class is the main entry point into the Duke application.
@@ -24,11 +21,26 @@ public class Main extends Application {
      *
      * @param stage Stage shown on the UI.
      */
+//    @Override
+//    public void start(Stage stage) {
+//        AnchorPane ap = new MainWindow();
+//        Scene scene = new Scene(ap);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
     @Override
     public void start(Stage stage) {
-        AnchorPane ap = new MainWindow();
-        Scene scene = new Scene(ap);
-        stage.setScene(scene);
-        stage.show();
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        Label helloWorld2 = new Label("Hello World 2.0!"); // Creating a new Label control
+        Scene scene2 = new Scene(helloWorld2); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+
+        Stage stage2 = new Stage();
+        stage2.setScene(scene2); // Setting the stage to show our screen
+        stage2.show(); // Render the stage.
     }
 }
