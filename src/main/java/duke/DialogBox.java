539ce43c83ba52acd1641f04e0ed208a29f50db3
Bobby Law <zeranium97@gmail.com>
2020-09-14 12:31:00 +0800

    Remove unused import

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -14,7 +14,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
