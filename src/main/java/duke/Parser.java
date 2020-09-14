d7998d325cb2723b788ab2617f507bdd3ed3e497
rolandyuwy <rolandyuwenyang@hotmail.com>
2020-09-15 04:13:55 +0800

    Refactor code

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -25,7 +25,7 @@ public class Parser {
      * @param indexTo   Index of array to end appending.
      * @return Appended string.
      */
-    public static String getStringFromArray(String[] inputArr, int indexFrom, int indexTo) {
+    public static String generateStringFromArray(String[] inputArr, int indexFrom, int indexTo) {
         String output = "";
         for (int i = indexFrom; i < indexTo; i++) {
             output += inputArr[i] + " ";
@@ -47,66 +47,131 @@ public class Parser {
         } else if (inputInformation[0].equals(CommandType.LIST.getInput())) {
             return new ListCommand();
         } else if (inputInformation[0].equals(CommandType.DONE.getInput())) {
-            if (inputInformation.length > 1 && isNumber(inputInformation[1])) {
-                int taskNumber = Integer.parseInt(inputInformation[1]);
-                return new DoneCommand(taskNumber);
-            } else {
-                throw new DukeException("You need to include your task number to mark done...");
-            }
+            return generateDoneCommand(inputInformation);
         } else if (inputInformation[0].equals(CommandType.DELETE.getInput())) {
-            if (inputInformation.length > 1 && isNumber(inputInformation[1])) {
-                int taskNumber = Integer.parseInt(inputInformation[1]);
-                return new DeleteCommand(taskNumber);
-            } else {
-                throw new DukeException("You need to include your task number to delete...");
-            }
+            return generateDeleteCommand(inputInformation);
         } else if (inputInformation[0].equals(CommandType.TODO.getInput())) {
-            if (inputInformation.length > 1) {
-                String description = getStringFromArray(inputInformation, 1, inputInformation.length);
-                return new AddCommand(CommandType.TODO, description);
-            } else {
-                throw new DukeException("Your todo description can't be empty...");
-            }
+            return generateToDoCommand(inputInformation);
         } else if (inputInformation[0].equals(CommandType.DEADLINE.getInput())) {
-            if (inputInformation.length > 3) {
-                int indexOfBy = Arrays.asList(inputInformation).indexOf("/by");
-                if (indexOfBy == -1) {
-                    throw new DukeException("Did you include /by?");
-                } else if (indexOfBy == 1) {
-                    throw new DukeException("Did you include a description?");
-                } else {
-                    String description = getStringFromArray(inputInformation, 1, indexOfBy);
-                    String by = getStringFromArray(inputInformation, indexOfBy + 1, inputInformation.length);
-                    return new AddCommand(CommandType.DEADLINE, description, by);
-                }
-            } else {
-                throw new DukeException("Your deadline description or deadline can't be empty...");
-            }
+            return generateDeadlineCommand(inputInformation);
         } else if (inputInformation[0].equals(CommandType.EVENT.getInput())) {
-            if (inputInformation.length > 3) {
-                int indexOfAt = Arrays.asList(inputInformation).indexOf("/at");
-                if (indexOfAt == -1) {
-                    throw new DukeException("Did you include /at?");
-                } else if (indexOfAt == 1) {
-                    throw new DukeException("Did you include a description?");
-                } else {
-                    String description = getStringFromArray(inputInformation, 1, indexOfAt);
-                    String at = getStringFromArray(inputInformation, indexOfAt + 1, inputInformation.length);
-                    return new AddCommand(CommandType.EVENT, description, at);
-                }
-            } else {
-                throw new DukeException("Your event description or event period can't be empty...");
-            }
+            return generateEventCommand(inputInformation);
         } else if (inputInformation[0].equals(CommandType.FIND.getInput())) {
-            if (inputInformation.length > 1) {
-                String[] searchKeywords = Arrays.copyOfRange(inputInformation, 1, inputInformation.length);
-                return new FindCommand(searchKeywords);
-            } else {
-                throw new DukeException("You need to include your keyword...");
-            }
+            return generateFindCommand(inputInformation);
         } else {
             throw new DukeException("My duck instincts tell me your input makes no sense...");
         }
     }
 
+    /**
+     * Generates a Done Command.
+     *
+     * @param inputInformation String array containing input information.
+     * @return Done Command to be executed.
+     * @throws DukeException When command input is wrongly formatted.
+     */
+    public static DoneCommand generateDoneCommand(String[] inputInformation) throws DukeException {
+        if (inputInformation.length > 1 && isNumber(inputInformation[1])) {
+            int taskNumber = Integer.parseInt(inputInformation[1]);
+            return new DoneCommand(taskNumber);
+        } else {
+            throw new DukeException("You need to include your task number to mark done...");
+        }
+    }
+
+    /**
+     * Generates a Delete Command.
+     *
+     * @param inputInformation String array containing input information.
+     * @return Delete Command to be executed.
+     * @throws DukeException When command input is wrongly formatted.
+     */
+    public static DeleteCommand generateDeleteCommand(String[] inputInformation) throws DukeException {
+        if (inputInformation.length > 1 && isNumber(inputInformation[1])) {
+            int taskNumber = Integer.parseInt(inputInformation[1]);
+            return new DeleteCommand(taskNumber);
+        } else {
+            throw new DukeException("You need to include your task number to delete...");
+        }
+    }
+
+    /**
+     * Generates an Add Command for ToDo task.
+     *
+     * @param inputInformation String array containing input information.
+     * @return Add Command to be executed.
+     * @throws DukeException When command input is wrongly formatted.
+     */
+    public static AddCommand generateToDoCommand(String[] inputInformation) throws DukeException {
+        if (inputInformation.length > 1) {
+            String description = generateStringFromArray(inputInformation, 1, inputInformation.length);
+            return new AddCommand(CommandType.TODO, description);
+        } else {
+            throw new DukeException("Your todo description can't be empty...");
+        }
+    }
+
+    /**
+     * Generates an Add Command for Deadline task.
+     *
+     * @param inputInformation String array containing input information.
+     * @return Add Command to be executed.
+     * @throws DukeException When command input is wrongly formatted.
+     */
+    public static AddCommand generateDeadlineCommand(String[] inputInformation) throws DukeException {
+        if (inputInformation.length > 3) {
+            int indexOfBy = Arrays.asList(inputInformation).indexOf("/by");
+            if (indexOfBy == -1) {
+                throw new DukeException("Did you include /by?");
+            } else if (indexOfBy == 1) {
+                throw new DukeException("Did you include a description?");
+            } else {
+                String description = generateStringFromArray(inputInformation, 1, indexOfBy);
+                String by = generateStringFromArray(inputInformation, indexOfBy + 1, inputInformation.length);
+                return new AddCommand(CommandType.DEADLINE, description, by);
+            }
+        } else {
+            throw new DukeException("Your deadline description or deadline can't be empty...");
+        }
+    }
+
+    /**
+     * Generates an Add Command for Event task.
+     *
+     * @param inputInformation String array containing input information.
+     * @return Add Command to be executed.
+     * @throws DukeException When command input is wrongly formatted.
+     */
+    public static AddCommand generateEventCommand(String[] inputInformation) throws DukeException {
+        if (inputInformation.length > 3) {
+            int indexOfAt = Arrays.asList(inputInformation).indexOf("/at");
+            if (indexOfAt == -1) {
+                throw new DukeException("Did you include /at?");
+            } else if (indexOfAt == 1) {
+                throw new DukeException("Did you include a description?");
+            } else {
+                String description = generateStringFromArray(inputInformation, 1, indexOfAt);
+                String at = generateStringFromArray(inputInformation, indexOfAt + 1, inputInformation.length);
+                return new AddCommand(CommandType.EVENT, description, at);
+            }
+        } else {
+            throw new DukeException("Your event description or event period can't be empty...");
+        }
+    }
+
+    /**
+     * Generates a Find Command.
+     *
+     * @param inputInformation String array containing input information.
+     * @return Find Command to be executed.
+     * @throws DukeException When command input is wrongly formatted.
+     */
+    public static FindCommand generateFindCommand(String[] inputInformation) throws DukeException {
+        if (inputInformation.length > 1) {
+            String[] searchKeywords = Arrays.copyOfRange(inputInformation, 1, inputInformation.length);
+            return new FindCommand(searchKeywords);
+        } else {
+            throw new DukeException("You need to include your keyword...");
+        }
+    }
 }
