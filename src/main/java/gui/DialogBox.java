946ef674cbf366dcc7ade773f909228ba8a21b1e
zhaohuanqdcn <61936274+zhaohuanqdcn@users.noreply.github.com>
2020-09-15 00:13:18 +0800

    Update to Better GUI
    Update to better GUI
    
    Newly add clipped avatars, bubbled speeches and a consistent icon for GUI panel

--- src/main/java/gui/DialogBox.java
+++ src/main/java/gui/DialogBox.java
@@ -7,12 +7,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+
 
 /**
  * An FXML control representing a dialog box consisting of
@@ -25,7 +28,13 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
-    private DialogBox(String text, Image img) {
+    private final Circle circle = new Circle();
+
+    public enum Direction {
+        USER, DUKE
+    }
+
+    private DialogBox(String text, Image img, Direction dir) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(
                     MainWindow.class.getResource("/view/DialogBox.fxml"));
@@ -36,8 +45,23 @@ public class DialogBox extends HBox {
             e.printStackTrace();
         }
 
-        dialog.setText(text);
+        circle.setCenterX(38);
+        circle.setCenterY(38);
+        circle.setRadius(38);
+        displayPicture.setClip(circle);
         displayPicture.setImage(img);
+
+        dialog.setText(text);
+        dialog.setPadding(new Insets(10, 15, 10, 15));
+        if (dir == Direction.USER) {
+            dialog.setStyle("-fx-background-color: #64FF64; " +
+                    "-fx-background-radius: 45;");
+        } else {
+            dialog.setStyle("-fx-background-color: #FFFFFF; " +
+                    "-fx-background-radius: 45;");
+            this.flip();
+        }
+
     }
 
     /**
@@ -52,12 +76,10 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        return new DialogBox(text, img, Direction.USER);
     }
 
     public static DialogBox getDukeDialog(String text, Image img) {
-        var db = new DialogBox(text, img);
-        db.flip();
-        return db;
+        return new DialogBox(text, img, Direction.DUKE);
     }
 }
