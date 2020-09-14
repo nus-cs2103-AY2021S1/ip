a33c27fbf9410696bd037df069e60da1b90c0ce7
Ma-Yueran <mayuerantom@outlook.com>
2020-09-14 14:17:04 +0800

    Replace MainWindow with DukeGuiWindow (new gui) in Main.java

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -2,11 +2,12 @@ package duke;
 
 import java.io.IOException;
 
+import duke.gui.DukeGuiWindow;
 import duke.gui.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -20,12 +21,12 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             duke.onStart(true);
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-            AnchorPane ap = fxmlLoader.load();
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/DukeGui.fxml"));
+            BorderPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setUp(duke, stage);
-            fxmlLoader.<MainWindow>getController().onStart();
+            fxmlLoader.<DukeGuiWindow>getController().setUp(duke, stage);
+            fxmlLoader.<DukeGuiWindow>getController().onStart();
             stage.setTitle("Duke");
             stage.show();
         } catch (IOException e) {
