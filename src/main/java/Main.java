f68e6731b5fdf7165cdc4a9544279000fc0aa63d
Jonathan Foo <jonathanfoocy@gmail.com>
2020-09-14 16:41:14 +0800

    Add title to GUI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Sparrow");
             fxmlLoader.<MainWindow>getController().setSparrow(sparrow);
             fxmlLoader.<MainWindow>getController().welcome();
             stage.show();
