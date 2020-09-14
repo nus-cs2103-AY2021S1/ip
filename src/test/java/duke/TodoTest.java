7f6c561750131565bc0a5557b06d4502b346dac9
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-09-15 00:16:05 +0800

    Update user guide

--- src/test/java/duke/TodoTest.java
+++ src/test/java/duke/TodoTest.java
@@ -1,15 +1,15 @@
 package duke;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class TodoTest {
     @Test
     public void testSerialize() {
-        assertEquals(Arrays.toString(new String[]{"1", "Todo", "kappa"}), 
+        assertEquals(Arrays.toString(new String[]{"1", "Todo", "kappa"}),
                 Arrays.toString(new Todo("kappa", true).serialize()));
     }
 }
