5bcd595f3914823a79b82c2118079bd6879de15e
HoPinXian <hopinxian@gmail.com>
2020-09-14 17:42:18 +0800

    Add continuous integration

--- src/test/java/butler/io/UiTest.java
+++ src/test/java/butler/io/UiTest.java
@@ -2,21 +2,13 @@ package butler.io;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import org.junit.jupiter.api.Test;
 
 public class UiTest {
     @Test
-    public void showTaskDeletedTest() {
+    public void showTaskIsDeletedTest() {
         Ui ui = new Ui();
-
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        String expectedOutput = "Task 1 has been deleted." + System.lineSeparator();
-        ui.showTaskIsDeleted(1);
-        assertEquals(expectedOutput, outContent.toString());
+        String expectedOutput = "Task 1 has been deleted.";
+        assertEquals(expectedOutput, ui.showTaskIsDeleted(1));
     }
 }
