7dc99696a28721d279f7fbf76e13cf29e2f1c05f
duynguyen24501 <duynguyen24501@gmail.com>
2020-09-15 01:41:59 +0800

    Fix import bugs

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -1,6 +1,10 @@
 package duke.storage;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
