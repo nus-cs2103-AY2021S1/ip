d064d671fe61d0994c65ed31468a43111bc736e6
sagarsureka <sagar150102@gmail.com>
2020-09-15 18:06:10 +0530

    Improve GUI and exit platform on bye command

--- src/main/java/dobby/gui/Main.java
+++ src/main/java/dobby/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("DOBBY- Task Manager");
             fxmlLoader.<MainWindow>getController().setDuke(dobby);
             stage.show();
         } catch (IOException e) {
