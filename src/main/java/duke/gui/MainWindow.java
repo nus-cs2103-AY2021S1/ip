9974f9299e7415f13be24541abb1ab9b1c96407b
chshen1998 <chewhoashen@gmail.com>
2020-09-15 13:04:05 +0800

    Add background colour

--- src/main/java/duke/gui/MainWindow.java
+++ src/main/java/duke/gui/MainWindow.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
 
 import java.util.Timer;
