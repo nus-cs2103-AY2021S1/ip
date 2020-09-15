f381213eaa45523fe9c9334fa91e6a333a64e58c
rolandyuwy <rolandyuwenyang@hotmail.com>
2020-09-15 20:46:49 +0800

    Refactor code

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -28,19 +28,7 @@ public class Storage {
             if (file.exists()) {
                 Scanner sc = new Scanner(file);
                 while (sc.hasNextLine()) {
-                    String[] taskInformation = sc.nextLine().split("\\" + SPLITTER);
-                    if (taskInformation[0].equals(TaskType.TODO.getInitial())) {
-                        assert taskInformation.length == 3; //change to erORORRO
-                        taskList.add(new ToDo(taskInformation[2], isDone(taskInformation[1])));
-                    } else if (taskInformation[0].equals(TaskType.DEADLINE.getInitial())) {
-                        assert taskInformation.length == 4;
-                        taskList.add(new Deadline(taskInformation[2], isDone(taskInformation[1]), taskInformation[3]));
-                    } else if (taskInformation[0].equals(TaskType.EVENT.getInitial())) {
-                        assert taskInformation.length == 4;
-                        taskList.add(new Event(taskInformation[2], isDone(taskInformation[1]), taskInformation[3]));
-                    } else {
-                        throw new DukeException("File formatting is wrong...");
-                    }
+                    processLine(sc.nextLine(), taskList);
                 }
                 sc.close();
             } else {
@@ -51,6 +39,71 @@ public class Storage {
             e.printStackTrace();
         }
         return taskList;
+    }
+
+    /**
+     * Processes the line by splitting relevant information into a String array.
+     *
+     * @param lineToProcess Line to be processed.
+     * @param taskList Task list to store tasks after the line is processed.
+     * @throws DukeException When file formatting is wrong.
+     */
+    private void processLine(String lineToProcess, List<Task> taskList) throws DukeException {
+        String[] taskInformation = lineToProcess.split("\\" + SPLITTER);
+        if (taskInformation[0].equals(TaskType.TODO.getInitial())) {
+            processToDoTask(taskInformation, taskList);
+        } else if (taskInformation[0].equals(TaskType.DEADLINE.getInitial())) {
+            processDeadlineTask(taskInformation, taskList);
+        } else if (taskInformation[0].equals(TaskType.EVENT.getInitial())) {
+            processEventTask(taskInformation, taskList);
+        } else {
+            throw new DukeException("File formatting is wrong...");
+        }
+    }
+
+    /**
+     * Processes the task information by accessing the array elements, then adding the task into the task list.
+     *
+     * @param taskInformation Contains information of the task to be added.
+     * @param taskList Task list to store tasks after processing the task information.
+     * @throws DukeException When To Do task information formatting is wrong.
+     */
+    private void processToDoTask(String[] taskInformation, List<Task> taskList) throws DukeException {
+        try {
+            taskList.add(new ToDo(taskInformation[2], isDone(taskInformation[1])));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("File formatting is wrong...");
+        }
+    }
+
+    /**
+     * Processes the task information by accessing the array elements, then adding the task into the task list.
+     *
+     * @param taskInformation Contains information of the task to be added.
+     * @param taskList Task list to store tasks after processing the task information.
+     * @throws DukeException When Deadline task information formatting is wrong.
+     */
+    private void processDeadlineTask(String[] taskInformation, List<Task> taskList) throws DukeException {
+        try {
+            taskList.add(new Deadline(taskInformation[2], isDone(taskInformation[1]), taskInformation[3]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("File formatting is wrong...");
+        }
+    }
+
+    /**
+     * Processes the task information by accessing the array elements, then adding the task into the task list.
+     *
+     * @param taskInformation Contains information of the task to be added.
+     * @param taskList Task list to store tasks after processing the task information.
+     * @throws DukeException When Event task information formatting is wrong.
+     */
+    private void processEventTask(String[] taskInformation, List<Task> taskList) throws DukeException {
+        try {
+            taskList.add(new Event(taskInformation[2], isDone(taskInformation[1]), taskInformation[3]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("File formatting is wrong...");
+        }
     }
 
     public String getDoneString(Task task) {
