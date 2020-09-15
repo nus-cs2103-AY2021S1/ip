8ffe6f58057456a5adb94caad5c83cc637fb88a5
UncleGrandpa925 <vuonglongtoan@gmail.com>
2020-09-15 13:31:22 +0800

    Add a full test to parser.Run

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -1,7 +1,13 @@
 package duke;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 class ParserTest {
     private static Parser parser;
@@ -36,5 +42,20 @@ class ParserTest {
         parser.run("todo return book");
     }
 
+    static String readFile(String filename) throws IOException {
+        ClassLoader classLoader = ParserTest.class.getClassLoader();
+        return Files.readString(Path.of(classLoader.getResource("file/" + filename).getFile()));
+    }
 
+    @Test
+    void testRunAll1() throws IOException {
+        String input = ParserTest.readFile("input1.txt");
+        String[] lineInput = input.split("\n");
+        String expectedOutput = ParserTest.readFile("output1.txt");
+        StringBuilder output = new StringBuilder();
+        for (String line : lineInput) {
+            output.append(parser.run(line));
+        }
+        Assertions.assertEquals(expectedOutput, output.toString());
+    }
 }
