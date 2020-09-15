b1990e666bd937ce473e84d0e8cd7b7f3fe5b87c
ChengJiyuqing <chengjiyuqing@gmail.com>
2020-09-16 01:08:47 +0800

    added 2 Junit class

---
+++ src/test/java/duke/ParserTest.java
@@ -0,0 +1,34 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class ParserTest {
+    //set up a Storage instance for testing
+    Storage defaultStorage;
+    File file;
+
+    public ParserTest() throws IOException {
+        this.defaultStorage = new Storage("parserTest.txt");
+        FileWriter clearFile = new FileWriter("parserTest.txt");
+        clearFile.write("");
+        clearFile.close();
+        file = new File("parserTest.txt");
+    }
+
+    @Test
+     public void parse_input_to_file_test() throws IOException, DukeException {
+        String msg = "todo sleep";
+        Parser.parseInput(msg,defaultStorage);
+        Scanner fileSc = new Scanner(file);
+        String taskString = fileSc.nextLine();
+        assertEquals("[T] [✘] sleep", taskString, "wrong file recording");
+    }
+}
---
+++ src/test/java/duke/StorageTest.java
@@ -0,0 +1,22 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StorageTest {
+    Storage testStorage;
+    File file;
+
+    public StorageTest() throws IOException {
+        testStorage = new Storage("storageTest.txt");
+        file = new File("storageTest.txt");
+    }
+
+    @Test
+    public void is_file_created_test() {
+        assertEquals(true,file.isFile(), "failed to create a file");
+    }
+}
