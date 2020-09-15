b092d2d38f1d475cf60edf47248456d13d4d4f1b
kimberly ong <e0424807@u.nus.edu>
2020-09-15 12:28:39 +0800

    Improve the GUI

--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -24,8 +24,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/teddy-bear.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/bear.png"));
 
     /** Initialises the main window with a Welcome Message */
     @FXML
