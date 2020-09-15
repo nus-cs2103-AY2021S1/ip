79d3c788ec14b8c7aa68b645b018ba0e44107ab0
Wong-ZZ <wongzhengzhi@gmail.com>
2020-09-15 20:00:34 +0800

    Update GUI to be more pleasing

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -1,6 +1,10 @@
 package duke;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -15,6 +19,10 @@ import javafx.util.Duration;
  */
 public class MainWindow extends AnchorPane {
     private static final String START_MSG = "Duke:\nHello! I'm Duke.\nWhat can I do for you?";
+    private static final double DELAY_CLOSE = 1.0;
+    private static final double DELAY_RESPONSE = 0.3;
+    private static final double DELAY_TRANSITION = 0.2;
+
     @FXML
     private ScrollPane scrollPane;
     @FXML
@@ -37,7 +45,7 @@ public class MainWindow extends AnchorPane {
     }
 
     private void generateStartMsg() {
-        dialogContainer.getChildren().addAll(DialogBox.getDukeDialog(START_MSG, dukeImage));
+        addDukeResponse(DialogBox.getDukeDialog(START_MSG, dukeImage));
     }
 
     public void setDuke(Duke d) {
@@ -61,18 +69,44 @@ public class MainWindow extends AnchorPane {
             return;
         }
 
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(input, userImage),
-                DialogBox.getDukeDialog(response, dukeImage)
-        );
-
         userInput.clear();
+
+        DialogBox userDialog = DialogBox.getUserDialog(input, userImage);
+        addUserDialog(userDialog);
+
+        DialogBox dukeResponse = DialogBox.getDukeDialog(response, dukeImage);
+        addDukeResponse(dukeResponse);
 
         if (duke.shouldExit()) {
             userInput.setDisable(true);
-            PauseTransition delay = new PauseTransition(Duration.seconds(1));
+            PauseTransition delay = new PauseTransition(Duration.seconds(DELAY_CLOSE));
             delay.setOnFinished(event -> exitFunction.run());
             delay.play();
         }
+    }
+
+    private void addUserDialog(DialogBox userDialog) {
+        userDialog.translateXProperty().set(-1 * dialogContainer.getWidth());
+        dialogContainer.getChildren().addAll(userDialog);
+
+        KeyValue keyValue = new KeyValue(userDialog.translateXProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(DELAY_TRANSITION), keyValue);
+        Timeline timeline = new Timeline(keyFrame);
+        timeline.play();
+    }
+
+    private void addDukeResponse(DialogBox dukeResponse) {
+        PauseTransition delayResponse = new PauseTransition(Duration.seconds(DELAY_RESPONSE));
+        delayResponse.setOnFinished(event -> {
+            dukeResponse.translateXProperty().set(1 * dialogContainer.getWidth());
+            dialogContainer.getChildren().addAll(dukeResponse);
+
+            KeyValue keyValue = new KeyValue(dukeResponse.translateXProperty(), 0, Interpolator.EASE_IN);
+            KeyFrame keyFrame = new KeyFrame(Duration.seconds(DELAY_TRANSITION), keyValue);
+            Timeline timeline = new Timeline(keyFrame);
+            timeline.play();
+        });
+        delayResponse.play();
+
     }
 }
