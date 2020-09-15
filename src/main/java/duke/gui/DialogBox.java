60a996aa6f836a92480d4e33a92376c4c44380d0
rishi <rishi5154@gmail.com>
2020-09-15 11:26:35 +0800

    A-BetterGui: Better GUI

--- src/main/java/duke/gui/DialogBox.java
+++ src/main/java/duke/gui/DialogBox.java
@@ -7,12 +7,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 /**
@@ -52,12 +57,29 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        // @@author Rishi5154-reused
+        // Background colour and curve code idea reused from @@jiayushe
+        var db = new DialogBox(text, img);
+        BackgroundFill fill = new BackgroundFill(Color.PALEGOLDENROD, new CornerRadii(15),
+                new Insets(5, 5, 5, 5)
+        );
+        Background background = new Background(fill);
+        db.setBackground(background);
+        return db;
+        //@@author
     }
 
     public static DialogBox getDukeDialog(String text, Image img) {
+        // @@author Rishi5154-reused
+        // Background colour and curve code idea reused from @@jiayushe
         var db = new DialogBox(text, img);
         db.flip();
+        BackgroundFill fill = new BackgroundFill(Color.SKYBLUE, new CornerRadii(15),
+                new Insets(5, 5, 5, 5)
+        );
+        Background background = new Background(fill);
+        db.setBackground(background);
         return db;
+        //@@author
     }
 }
