9ec83ac94ef5d6faaa09239878dca26c9cbb47eb
chshen1998 <chewhoashen@gmail.com>
2020-09-15 14:35:38 +0800

    Update duke and user image

--- src/main/java/duke/gui/MainWindow.java
+++ src/main/java/duke/gui/MainWindow.java
@@ -32,8 +32,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/MortyUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/ShrekDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/ricardo.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/dukechatbot.png"));
 
     @FXML
     public void initialize() {
