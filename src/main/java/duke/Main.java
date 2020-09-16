ad29ff2ce96b1f7c47dc961ec9f26d1ee04c1250
royleochan <royleochan@gmail.com>
2020-09-16 23:22:29 +0800

    Add product name in title bar of window

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         MainWindow mw = new MainWindow();
         Scene scene = new Scene(mw);
+        stage.setTitle("Serina");
         stage.setScene(scene);
         mw.setDuke(duke);
         mw.initialize();
