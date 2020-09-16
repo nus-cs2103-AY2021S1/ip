32f5c8af2dfd94a7be1e79cb13ecf3f9baca6a55
rolandyuwy <rolandyuwenyang@hotmail.com>
2020-09-17 02:17:03 +0800

    Set title to GUI

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duck");
             duke = new Duke("data/duke.txt", new GraphicalUi());
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             fxmlLoader.<MainWindow>getController().greet();
