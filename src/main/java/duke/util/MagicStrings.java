9001088b5bd68a1c4f2f49631e86206dd55178fe
Clara <claraadora@gmail.com>
2020-09-16 14:26:24 +0800

    Revamp UI

--- src/main/java/duke/util/MagicStrings.java
+++ src/main/java/duke/util/MagicStrings.java
@@ -3,21 +3,23 @@ package duke.util;
 public class MagicStrings {
     // Error messages
     public static final String ERROR_TODO_DESCRIPTION_EMPTY =
-            "\tSorry! The description of a todo cannot be empty :')";
+            "Sorry. The description of a todo cannot be empty :')";
     public static final String ERROR_DEADLINE_FORMAT_INCORRECT =
-            "\tPaise! :') Please use the format: deadline <task> /by <time>"
-            + "\n\t\t*time format: <yyyy-mm-dd> or  <yyyy-mm-dd HH:mm>";
+            "Sorry. Please use the format: deadline <task> /by <time>"
+            + "\n\t*time format: <yyyy-mm-dd> or  <yyyy-mm-dd HH:mm>";
     public static final String ERROR_EVENT_FORMAT_INCORRECT =
-            "\tPaise! :') Please use the correct format: event <task> /at <time> +"
-                    + "\n\t\t*time format: <yyyy-mm-dd> or  <yyyy-mm-dd HH:mm>";
+            "Sorry. Please use the correct format: event <task> /at <time> +"
+                    + "\n\t*time format: <yyyy-mm-dd> or  <yyyy-mm-dd HH:mm>";
     public static final String ERROR_INDEX_OUT_OF_BOUNDS =
-            "Sorry! The index is out of bounds! :')";
+            "Sorry. The index is out of bounds! :')";
     public static final String ERROR_DONE_FORMAT_INCORRECT =
-            "\tPaise! :') Please use the correct format: done <order of task in the list>";
+            "Sorry. Please use the correct format: done <order of task in the list>";
     public static final String ERROR_DELETE_FORMAT_INCORRECT =
-            "\tPaise! :') Please use the correct format: delete <order of task in the list>";
+            "Sorry. Please use the correct format: delete <order of task in the list>";
     public static final String ERROR_COMMAND_FORMAT_INCORRECT =
-            "\tApologies! I do not understand what that means :')";
+            "Sorry. I do not understand what that means :')";
+    public static final String ERROR_TIME_FORMAT_INCORRECT =
+            "Sorry. Please use the correct time format: y-M-d HH:mm or y-M-d";
 
     // UI response messages and headers
     public static final String MESSAGE_WELCOME = "Hello! I'm Clara! :D How may I help you? :)\n";
@@ -25,8 +27,8 @@ public class MagicStrings {
     public static final String HEADER_TASK_FIND = "These are the tasks that you are looking for.:)\n";
     public static final String HEADER_TASK_ALL = "These are the tasks in your list. Jiayous! :)\n";
     public static final String HEADER_TASK_NONE = "You have no task in your list. :D\n";
-    public static final String HEADER_TASK_ADD = "Okay! I've added this task:";
-    public static final String HEADER_TASK_DONE = "Nice! I've marked this task as done:\n\t\t";
-    public static final String HEADER_TASK_DELETE = "Okay! I've removed this task:\n\t\t";
-    public static final String HEADER_TASK_EDIT = "Okay! I've edited this task:\n\t\t";
+    public static final String HEADER_TASK_ADD = "Okay! I've added this task:\n";
+    public static final String HEADER_TASK_DONE = "Nice! I've marked this task as done:\n\t";
+    public static final String HEADER_TASK_DELETE = "Okay! I've removed this task:\n\t";
+    public static final String HEADER_TASK_EDIT = "Okay! I've edited this task:\n\t";
 }
