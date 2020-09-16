2547454d733bb643c21046c0329d2e9b96d36a97
Daniel Adi <daniel@adip.me>
2020-09-16 21:13:50 +0800

    Remove unused imports

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -9,8 +9,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
-import duke.ui.Ui;
-
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
