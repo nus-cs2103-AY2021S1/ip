7e7f2a477b5027d9ce490cc335ed88c31d1ac2d6
pr4aveen <ajjuu98@gmail.com>
2020-09-16 13:52:30 +0800

    Update GUI
    Current GUI lacks customization.
    
    This makes the app harder to differentiate from others.
    
    Let's add a background and some more colours to the GUI.

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.setTitle("Duke");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
