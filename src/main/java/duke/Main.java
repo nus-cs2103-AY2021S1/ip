28af88f7c9d36ac4a411029346d451660942c873
Zhu Yuxuan <e0424709@u.nus.edu>
2020-09-16 21:10:14 +0800

    Add Ui.png

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            stage.setTitle("Duke");
             FXMLLoader fxmlLoader = new FXMLLoader(
                     Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
