ff399f07ea53a33453e6850c2c27ce038077e5b0
pr4aveen <ajjuu98@gmail.com>
2020-09-16 12:32:47 +0800

    Update help command
    Help command displays a list of all commands.
    
    This means that every time the user guide is updated, the information
    displayed by the help command needs to be updated as well. The
    information displayed on the app is also limited as compared to the
    user guide.
    
    Let's redirect the user to the user guide when the help command is used.

--- src/main/java/duke/command/HelpCommand.java
+++ src/main/java/duke/command/HelpCommand.java
@@ -1,5 +1,10 @@
 package duke.command;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import duke.Storage;
 import duke.UI;
 import duke.task.TaskList;
@@ -29,15 +34,17 @@ public class HelpCommand extends Command {
         assert storage != null : "Storage cannot be null";
         assert taskList != null : "Task list cannot be null";
 
-        ui.printToConsole("Hi there! The following commands are available\n"
-                + "todo {task name} - Create a new ToDo task\n"
-                + "event {event name} /at {event date time} - Create a new Event task\n"
-                + "deadline {deadline name} /by {deadline date time} - Create a new Deadline task\n"
-                + "done {task ID} - Mark a task as done\n"
-                + "delete {task ID} - Delete a task\n"
-                + "find {keyword/task type/task status} - Filter tasks by a keyword\n"
-                + "date {date} - Filter tasks that are due on a certain date\n"
-                + "help - View help\n"
-                + "bye - Leave app\n");
+        String helpUrl = "http://pr4aveen.github.io/ip";
+
+        ui.printToConsole(String.format("Please visit %s for more information"
+                + " on the commands available.", helpUrl));
+
+        try {
+            Desktop desktop = java.awt.Desktop.getDesktop();
+            URI userGuideUrl = new URI(helpUrl);
+            desktop.browse(userGuideUrl);
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
