c9bbf2072eb087ae2156a4939768d177f94a9a94
murtubak <murtubakchickenss@gmail.com>
2020-09-16 17:44:35 +0800

    Update for checkstyle

--- src/main/java/controller/DialogBox.java
+++ src/main/java/controller/DialogBox.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
 
 /**
  * An example of a custom control using FXML.
--- src/main/java/controller/MainWindow.java
+++ src/main/java/controller/MainWindow.java
@@ -3,13 +3,15 @@ package controller;
 import duke.Duke;
 import duke.DukeException;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
@@ -54,7 +56,6 @@ public class MainWindow extends AnchorPane {
                     DialogBox.getDukeDialog(response, dukeImage)
             );
             userInput.clear();
-            System.out.println(response);
         } catch (DukeException e) {
             showError(e);
         }
