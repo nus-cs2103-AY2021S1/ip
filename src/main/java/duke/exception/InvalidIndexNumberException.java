be44d923ca6ca05be7599a3f31bf757e045cd03c
ChooJiaXin <e0426255@u.nus.edu>
2020-09-17 03:14:05 +0800

    Minor changes to display message

--- src/main/java/duke/exception/InvalidIndexNumberException.java
+++ src/main/java/duke/exception/InvalidIndexNumberException.java
@@ -8,6 +8,6 @@ public class InvalidIndexNumberException extends NumberFormatException {
      * Constructs a new InvalidIndexNumberException with no specified detail message.
      */
     public InvalidIndexNumberException() {
-        super("OOPS! Please enter a numerical number to mark tasks as done :)");
+        super("OOPS! Please enter a numerical number :)");
     }
 }
