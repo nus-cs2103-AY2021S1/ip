c5720f5cc9a27d1a0921b1cf2eb2990ca521ee0f
Aaron S <aaronsgseah@gmail.comm>
2020-09-16 10:31:01 +0800

    Add title to fxml window

--- src/main/java/dukechatbot/duke/Main.java
+++ src/main/java/dukechatbot/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke Chatbot");
             stage.setResizable(false);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
