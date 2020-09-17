7b12b1722bd57b81d6a75a43ce6ed2e3b2af0fc1
Jonathan Foo <jonathanfoocy@gmail.com>
2020-09-18 00:37:35 +0800

    Add user guide link to HelpCommand

--- src/main/java/sparrow/commands/HelpCommand.java
+++ src/main/java/sparrow/commands/HelpCommand.java
@@ -9,9 +9,10 @@ public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
 
-    // TODO: implement proper help documentation
+    public static final String USER_GUIDE_URL = "https://jonfoocy.github.io/ip/";
+
     @Override
     public String execute(TaskList tasks, VocabList vocabList, Ui ui, Storage storage) {
-        return "You asked for help?";
+        return "This command ain't recognised. Go t' https://jonfoocy.github.io/ip/ fer a list o' supported commands";
     }
 }
---
+++ src/test/java/sparrow/parser/ParserTest.java
@@ -0,0 +1,23 @@
+package sparrow.parser;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import sparrow.data.exceptions.IncorrectCommandException;
+
+public class ParserTest {
+    Parser parser = new Parser();
+
+    @Test
+    public void parseCommand_noInput_throwsAssertionError() {
+        assertThrows(IncorrectCommandException.class, () -> {
+            parser.parseCommand("");
+        });
+
+        assertThrows(IncorrectCommandException.class, () -> {
+            parser.parseCommand("   ");
+        });
+    }
+
+}
