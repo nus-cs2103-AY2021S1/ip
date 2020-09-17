c8b454b4e0f127fa595506f6b90e9a2d00f48ffe
zeying99 <758504129@qq.com>
2020-09-17 22:33:20 +0800

    Edit README.md in doc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            stage.setTitle("ChatboxDuke");
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
