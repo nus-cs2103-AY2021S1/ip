f90e2e2371d87c7386347df81eece8a9109133f0
xinyue <xinyue98.zhang@gmail.com>
2020-09-17 20:10:50 +0800

    Improve ui

--- src/main/java/AddCommand.java
+++ src/main/java/AddCommand.java
@@ -1,11 +1,26 @@
+/**
+ * Encapsulates an AddCommand object, contains information about the task being added.
+ */
 public class AddCommand extends Command {
-    
+
+    /**
+     * Represents the task being added to the task list.
+     */
     private final Task task;
     
     AddCommand(Task task) {
         this.task = task;
     }
-    
+
+    /**
+     * Adds a task to the task list and updates the storage.
+     * 
+     * @param taskList taskList which the task is being added to.  
+     * @param ui ui used to show the added task.
+     * @param storage storage used to store the taskList.
+     * @return a string representation of the added task.
+     * @throws DukeException if updateTask fails.
+     */
     @Override
     String execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
         taskList.addNewTask(task);
