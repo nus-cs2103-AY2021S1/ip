eb582737fcd87785d40387c903ec7bf519c71157
zhaolingshan <e0425857@u.nus.edu>
2020-09-17 20:13:57 +0800

    add javadocs

--- src/main/java/AddCommand.java
+++ src/main/java/AddCommand.java
@@ -1,16 +1,33 @@
+/**
+ * Encapsulates an add command with a Task object.
+ */
 public class AddCommand extends Command {
+    /**
+     * Represents the task which will be added into the task list.
+     */
     public Task task;
-    
+
+    /**
+     * Instantiates an AddCommand object.
+     * @param task the task to be added into the task list.
+     */
     public AddCommand(Task task) {
         this.task = task;
     }
 
+    /**
+     * Adds a task into the task list 
+     * and updates the storage with the new task.
+     * @param tasks the task list which the task is to be added into.
+     * @param ui the ui used to display the added task.
+     * @param storage the storage used to store the new task.
+     * @return a string representation of the task to be added.
+     */
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) {
         tasks.addTask(task);
         storage.save(tasks);
         return ui.showAddedTask(task, tasks.taskListLength());
-        
     }
 
     @Override
