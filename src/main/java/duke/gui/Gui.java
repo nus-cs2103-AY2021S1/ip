ae1e474d43d03298948eeebe79da31aefec32c5c
FooJingYi <foojingyi@comp.nus.edu.sg>
2020-09-18 17:21:17 +0800

    Change file on disk to save to.

--- src/main/java/duke/gui/Gui.java
+++ src/main/java/duke/gui/Gui.java
@@ -10,11 +10,9 @@ import javafx.stage.Stage;
  * A GUI for Duke using FXML.
  */
 public class Gui extends Application {
-
-    private Duke duke = new Duke("data/gui-test.txt");
-
     @Override
     public void start(Stage stage) {
+        Duke duke = new Duke("data/tasks.txt");
         AnchorPane ap = new MainWindow(duke);
         Scene scene = new Scene(ap);
         stage.setTitle("Meimei Bot");
