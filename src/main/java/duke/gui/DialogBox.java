13da57de41276e5b9435b921c2481f23289c87d2
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-09-18 11:25:24 +0800

    Fix Gui labels

--- src/main/java/duke/gui/DialogBox.java
+++ src/main/java/duke/gui/DialogBox.java
@@ -1,21 +1,25 @@
 package duke.gui;
 
 import java.io.IOException;
+
 import java.util.Collections;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+
 import javafx.geometry.Pos;
+
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
- * An example of a custom control using FXML.
  * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
  * containing text from the speaker.
  */
@@ -25,6 +29,15 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
+    /**
+     * Instantiates a dialog box object.
+     * Solution below adapted from https://github.com/gloon99/ip
+     * and https://github.com/yejiadong/ip
+     *
+     * @author gloon99, yejiadong
+     * @param text text to be shown.
+     * @param img display picture.
+     */
     private DialogBox(String text, Image img) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
@@ -36,7 +49,16 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
+        dialog.setStyle("-fx-background-color: rgb(0, 98, 255);"
+                + "-fx-background-radius: 15;"
+                + "-fx-padding: 7.5;"
+                + "-fx-text-fill: rgb(255, 255, 255);"
+                + "-fx-border-color: rgb(255, 204, 229);"
+                + "-fx-border-radius: 15;");
+
         displayPicture.setImage(img);
+        final Circle clip = new Circle(49, 48, 48);
+        displayPicture.setClip(clip);
     }
 
     /**
@@ -49,10 +71,24 @@ public class DialogBox extends HBox {
         setAlignment(Pos.TOP_LEFT);
     }
 
+    /**
+     * Returns a dialog box for the user.
+     *
+     * @param text user text.
+     * @param img user display picture.
+     * @return DialogBox object.
+     */
     public static DialogBox getUserDialog(String text, Image img) {
         return new DialogBox(text, img);
     }
 
+    /**
+     * Returns a dialog box for duke.
+     *
+     * @param text duke response text.
+     * @param img duke display picture.
+     * @return DialogBox object.
+     */
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
--- src/main/java/duke/gui/MainWindow.java
+++ src/main/java/duke/gui/MainWindow.java
@@ -2,6 +2,14 @@ package duke.gui;
 
 import duke.Duke;
 import duke.DukeException;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+
+import javafx.util.Duration;
+
+import javafx.application.Platform;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -9,6 +17,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
@@ -27,11 +36,24 @@ public class MainWindow extends AnchorPane {
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/LukeDaKing.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/LukeDaDuke.png"));
 
+    /**
+     * Initializes the main window of the gui.
+     */
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        dialogContainer.getChildren().add(DialogBox
+                .getDukeDialog(new Duke()
+                        .getUi()
+                        .welcomeMessage(), dukeImage
+                ));
     }
 
+    /**
+     * Sets duke object to duke variable.
+     *
+     * @param d Duke object.
+     */
     public void setDuke(Duke d) {
         duke = d;
     }
@@ -39,20 +61,32 @@ public class MainWindow extends AnchorPane {
     /**
      * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
      * the dialog container. Clears the user input after processing.
+     * Solution below adapted from https://github.com/pennhanlee/ip
+     *
+     * @author pennhanlee
      */
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
+
         String response = "";
         try {
             response += duke.getResponse(input);
-        } catch (DukeException ex){
+        } catch (DukeException ex) {
             response += ex;
         }
+
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
                 DialogBox.getDukeDialog(response, dukeImage)
         );
         userInput.clear();
+
+        if (input.equals("bye")) {
+            Timeline timeline = new Timeline(new KeyFrame(
+                    Duration.millis(1500), x -> Platform.exit()
+            ));
+            timeline.play();
+        }
     }
 }
