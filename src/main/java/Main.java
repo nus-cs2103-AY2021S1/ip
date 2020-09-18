12062f75489e46d3d20344145f3439a7114fc8db
luciatirta <luciatirtag@gmail.com>
2020-09-18 23:04:01 +0800

    Add README.md

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import duke.Duke;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -24,6 +25,8 @@ public class Main extends Application {
             scene.getStylesheets().add("/view/main.css");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/icon.png")));
+            stage.setTitle("Task Manager Duke");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
