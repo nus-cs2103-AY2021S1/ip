bd1b7aaf782f6e6e18eaa305c557f86078465de2
Rahul0506 <rahul.gidijala@gmail.com>
2020-09-18 18:11:52 +0800

    Add MassOpCommand

---
+++ src/main/java/duke/commands/MassOpCommand.java
@@ -0,0 +1,63 @@
+package duke.commands;
+
+import duke.Duke;
+import duke.DukeException;
+import duke.tasks.Task;
+import duke.util.Storage;
+import duke.util.TaskList;
+import duke.util.Ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Command sub-type to define finding Tasks.
+ */
+public class MassOpCommand extends Command {
+
+    public static final String DONE_OP = "marked done";
+    public static final String DELETE_OP = "deleted";
+
+    /**
+     * Creates FindCommand object.
+     *
+     * @param attributes input attributes from user
+     */
+    public MassOpCommand(String attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public boolean runCLI(TaskList taskList, Storage storage, Ui ui) throws DukeException {
+        runGUI(taskList, storage, ui);
+        return true;
+    }
+
+    @Override
+    public String runGUI(TaskList taskList, Storage storage, Ui ui) throws DukeException{
+        String[] attrSplit = attributes.split("/find");
+        if (attrSplit.length != 2) {
+            throw new DukeException("Invalid mass operation format");
+        }
+        attrSplit[0] = attrSplit[0].strip();
+        attrSplit[1] = attrSplit[1].strip();
+
+        String op;
+        if (attrSplit[0].equals("done")) {
+            op = DONE_OP;
+        } else if (attrSplit[0].equals("delete")) {
+            op = DELETE_OP;
+        } else {
+            System.out.println(attrSplit[0]);
+            throw new DukeException("Invalid operation specified");
+        }
+
+        List<Task> foundTasks = taskList.findTasks(attrSplit[1]);
+        if (op.equals(DONE_OP)) {
+            taskList.massMarkDone(foundTasks);
+        } else {
+            taskList.massDelete(foundTasks);
+        }
+        return ui.writeMassOp(foundTasks, op);
+    }
+}
