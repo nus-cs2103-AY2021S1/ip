42282855f81e57744359f82cda465bb6ee81b8bb
Hendey Fan <hendeyfan@u.nus.edu>
2020-09-18 12:58:40 +0800

    removed comment

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,7 +3,6 @@ package duke;
 import javafx.application.Application;
 
 public class Duke {
-    // comment cos i did it, but not in a branch so this comment is part of making the branch :/
 
     public static void main(String[] args) {
         Application.launch(WindowDisplay.class);
