81fb75028635e2c192afec297da66a3f4f098134
KohHanMing <kohhanming1997@gmail.com>
2020-09-18 20:11:24 +0800

    Edit test

--- src/test/java/DukeTaskListTest.java
+++ src/test/java/DukeTaskListTest.java
@@ -3,8 +3,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import duke.TaskList;
-import duke.task.Deadline;
-import duke.task.Event;
 import duke.task.TaskType;
 import duke.task.Todo;
 
