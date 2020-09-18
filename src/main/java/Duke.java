9975db126be4ba6315efe67c7b4852dd1ad8ef6d
e0316059 <e0316059@u.nus.edu>
2020-09-18 22:27:45 +0800

    add basic gui

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,15 @@
 import java.io.FileNotFoundException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 
 /**
  * Represents a Duke robot that deals with multiple tasks.
@@ -20,6 +31,7 @@ public class Duke {
      * The <code>Parser</code> used in Duke.
      */
     private Parser parser;
+
 
     /**
      * Creates a new <code>Duke</code> with the given <code>filePath</code>.
@@ -49,4 +61,8 @@ public class Duke {
     public static void main(String[] args) {
         new Duke("C:\\Users\\e0316059\\Desktop\\Duke\\src\\main\\java\\data\\Duke.txt").run();
     }
+//}
+
+
+
 }
