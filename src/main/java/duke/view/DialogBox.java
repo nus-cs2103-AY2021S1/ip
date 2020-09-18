4194341cedfd40259807ed29398bd0bf7a57a543
Rahul0506 <rahul.gidijala@gmail.com>
2020-09-18 23:37:21 +0800

    Improve Ui

--- src/main/java/duke/view/DialogBox.java
+++ src/main/java/duke/view/DialogBox.java
@@ -56,6 +56,7 @@ public class DialogBox extends HBox {
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
+        db.autosize();
         return db;
     }
 }
