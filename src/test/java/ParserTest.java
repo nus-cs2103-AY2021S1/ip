67a29bfa2b7f290c84ec23485c77ef24d6845b06
Ng Wei Hong <justweihong@gmail.com>
2020-09-18 16:21:48 +0800

    refactor test classes for gradle support, modify build gradle to build jar file
    test classes have the wrong imports and cause gradle to produce
    exceptions during build

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,8 +1,9 @@
 import duke.util.DukeException;
 import duke.util.Parser;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.Assert.assertEquals;
 
 /**
  * Testing class for Parser object.
@@ -12,7 +13,7 @@ public class ParserTest {
     @Test
     public void ParseRemainingStringTest() throws DukeException {
         String remainingString = " CS2103T assignment /by 2020-08-22 2359";
-        String[] actual = Parser.parseRemainingString("deadline", remainingString);
+        String[] actual = Parser.parseAddCommandString("deadline", remainingString);
         String[] expected = new String[] {"deadline", "CS2103T assignment", "2020-08-22 2359"};
         assertEquals(expected[0], actual[0]);
         assertEquals(expected[1], actual[1]);
