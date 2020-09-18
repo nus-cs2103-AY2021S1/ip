4d84525927eba5b10c23473e98c06cb2f4be40ec
EthanTheGoondu <ethannoahrozario@gmail.com>
2020-09-18 16:34:55 +0800

    Aesthetic changes to UI, and addition of title, final touches.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("IrohBot");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             duke.initialise();
             stage.show();
