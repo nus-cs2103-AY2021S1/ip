99089245d7e36582f32a1f84ca98c816fd0428e7
Rahul0506 <rahul.gidijala@gmail.com>
2020-09-18 23:43:26 +0800

    Add basic tests

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -1,16 +1,18 @@
-//package duke;
-//
-//import org.junit.jupiter.api.Test;
-//
-//public class ParserTest {
-//
-//    @Test
-//    public void processInput_inputBye_parserExits() {
-////        Parser parser = new Parser();
-////        try {
-////            assert !parser.processInput("bye");
-////        } catch (Exception ex) {
-////            assert false;
-////        }
-//    }
-//}
+package duke;
+
+import duke.commands.ExitCommand;
+import duke.util.Parser;
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+
+    @Test
+    public void processInput_inputBye_parserExits() {
+        Parser parser = new Parser();
+        try {
+            assert (parser.processInput("bye") instanceof ExitCommand);
+        } catch (Exception ex) {
+            assert false;
+        }
+    }
+}
--- src/test/java/duke/TaskListTest.java
+++ src/test/java/duke/TaskListTest.java
@@ -1,35 +1,36 @@
-//package duke;
-//
-//import duke.tasks.Task;
-//import org.junit.jupiter.api.Test;
-//
-//public class TaskListTest {
-//
-//    public TaskList setup() {
-//        TaskList taskList = new TaskList();
-//        taskList.addTodo("Task 1");
-//        taskList.addTodo("Task 2");
-//        taskList.addTodo("Task 3");
-//        return taskList;
-//    }
-//
-//    @Test
-//    public void addTask_addTodo_todoAdded() {
-//        TaskList taskList = setup();
-//
-//        Task added = taskList.addTodo("New Todo");
-//        assert taskList.getList().contains(added);
-//    }
-//
-//    @Test
-//    public void deleteTask_deleteTaskInList_taskDeleted() {
-//        TaskList taskList = setup(); // 3 Tasks in list
-//
-//        Task temp = taskList.addTodo("Temp Task");
-//        boolean added = taskList.getList().contains(temp);
-//        try {
-//            taskList.deleteTask(4);
-//        } catch (Exception ex) {}
-//        assert added && !taskList.getList().contains(temp);
-//    }
-//}
+package duke;
+
+import duke.tasks.Task;
+import duke.util.TaskList;
+import org.junit.jupiter.api.Test;
+
+public class TaskListTest {
+
+    public TaskList setup() {
+        TaskList taskList = new TaskList();
+        taskList.addTodo("Task 1");
+        taskList.addTodo("Task 2");
+        taskList.addTodo("Task 3");
+        return taskList;
+    }
+
+    @Test
+    public void addTask_addTodo_todoAdded() {
+        TaskList taskList = setup();
+
+        Task added = taskList.addTodo("New Todo");
+        assert taskList.getList().contains(added);
+    }
+
+    @Test
+    public void deleteTask_deleteTaskInList_taskDeleted() {
+        TaskList taskList = setup(); // 3 Tasks in list
+
+        Task temp = taskList.addTodo("Temp Task");
+        boolean added = taskList.getList().contains(temp);
+        try {
+            taskList.deleteTask(4);
+        } catch (Exception ex) {}
+        assert added && !taskList.getList().contains(temp);
+    }
+}
