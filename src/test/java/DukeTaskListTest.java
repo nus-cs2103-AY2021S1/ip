06f636532df7213a408402880724bf34b69fbadf
KohHanMing <kohhanming1997@gmail.com>
2020-09-18 20:08:50 +0800

    Edit test

--- src/test/java/DukeTaskListTest.java
+++ src/test/java/DukeTaskListTest.java
@@ -19,15 +19,9 @@ public class DukeTaskListTest {
     @Test
     public void addTasksTest() {
         taskList.addTask(new Todo("Finish Drawing", TaskType.TODO));
-        taskList.addTask(new Deadline("Essay", " 11/10/2020 1800", TaskType.DEADLINE));
-        taskList.addTask(new Event("Meeting", " 20/06/2020 1400-1600", TaskType.EVENT));
         String todo = "[Todo][\u2718] Finish Drawing - Priority: UNDEFINED";
-        String deadline = "[Deadline][\u2718] Essay (by: 11 Oct 2020, 18:00) - Priority: UNDEFINED";
-        String event = "[Event][\u2718] Meeting (at: 20 Jun 2020, 14:00-16:00) - Priority: UNDEFINED";
 
         assertEquals(todo, taskList.getTask(0).toString());
-        assertEquals(deadline, taskList.getTask(1).toString());
-        assertEquals(event, taskList.getTask(2).toString());
     }
 
 }
