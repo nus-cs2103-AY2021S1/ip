50308ef5ef8d9d820b89541e365a3b249b6fc567
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-09-20 01:20:44 +0800

    Add JUnit tests for Parser and ToDo classes

---
+++ src/test/java/duke/task/ToDoTest.java
@@ -0,0 +1,21 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+    @Test
+    public void dataStorage_displaysCorrectStorageName_displayZeroForUndone() {
+        ToDo testToDoUndone = new ToDo("Test for not done");
+        assertEquals("T | 0 | Test for not done", testToDoUndone.saveToDo());
+    }
+
+    @Test
+    public void dataStorage_displaysCorrectStorageNameForDone_displayOneForDone() {
+        ToDo testToDoUndone = new ToDo("Test for done");
+        testToDoUndone.markAsDone();
+        assertEquals("T | 1 | Test for done", testToDoUndone.saveToDo());
+    }
+
+}
