d840ba052ec6e46f44d4104020ae3fd6360bce4f
Royxuzeng <e0376917@u.nus.edu>
2020-09-22 13:34:20 +0800

    Update DoneCommand

--- src/main/java/DoneCommand.java
+++ src/main/java/DoneCommand.java
@@ -12,7 +12,7 @@ public class DoneCommand extends Command {
         assert order >= 1 : "order should be at least one";
         Task task = taskList.get(order - 1);
         task.markAsDone();
-        message = message + "  Nice! I've marked this task as done:" + "    ["
+        message = message + "  Nice! I've marked this task as done:\n" + "    ["
                 + task.getStatusIcon() + "] "
                 + task.getDescription();
 
