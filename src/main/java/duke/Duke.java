0ce5803bc6a1991a993f3c8d1221477a882da15d
Shulong <e0424740@u.nus.edu>
2020-09-30 14:06:21 +0800

    Fixed import spacing issues

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,9 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-
 import javafx.scene.image.Image;
-
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
