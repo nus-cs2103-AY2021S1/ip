55fbbd482b49814c4196b29e6603808e1f0e9a81
limhaishan <haishanlim@gmail.com>
2020-10-18 19:35:02 +0800

    Update tests

--- src/test/java/emily/task/EventTest.java
+++ src/test/java/emily/task/EventTest.java
@@ -1,19 +1,19 @@
 package emily.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import main.java.emily.task.Event;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class EventTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testToString() {
         LocalDateTime date = LocalDateTime.parse("2020-01-10 1430",
                 DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-        assertEquals("[E][" + "\u2718" + "] book reading (at: 2020-01-10 14:30)",
+        assertEquals("[E][" + "\u2718" + "] book reading  (at: 10-Jan-2020 14:30)",
                 new Event("book reading ", "2020-01-10 1430").toString());
     }
 }
