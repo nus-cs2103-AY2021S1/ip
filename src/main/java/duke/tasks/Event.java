1622e7edadbe5cf19d9ad78de975e652b024f1f0
Eryn <yeryeon@gmail.com>
2020-10-19 11:42:34 +0900

    Add EventTest

--- src/main/java/duke/tasks/Event.java
+++ src/main/java/duke/tasks/Event.java
@@ -14,7 +14,20 @@ public class Event extends Task implements Serializable{
         super(s);
         time = t;
     }
-    
+
+    public Event(String s, Boolean b, LocalDate t) {
+        super(s, b);
+        time = t;
+    }
+
+    public LocalDate getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDate time) {
+        this.time = time;
+    }
+
     @Override
     public String toString() {
         List<Tag> tags = super.getTags();
@@ -28,4 +41,25 @@ public class Event extends Task implements Serializable{
         return "[E]" + "[" + (super.isDone() ? "O" : "X") + "] " + super.getName() + " (at: "
                 + time.format(DateTimeFormatter.ofPattern("MMM dd, yyyy")) + ")" + s;
     }
+
+
+    /**
+     * Returns true if both events have the same name and boolean and localdate.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Task)) {
+            return false;
+        }
+
+        Event otherConsultation = (Event) other;
+        return otherConsultation.getName().equals(getName())
+                && otherConsultation.isDone().equals(isDone())
+                && otherConsultation.getTime().equals(getTime());
+    }
+
 }
