ea837e0142a154afb2d761fe389d83f7e0ddf52c
geraldneo567 <e0543413@u.nus.edu>
2022-01-14 23:13:50 +0800

    Added Level-1 Functionality to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.*;
+
 public class Duke {
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scan = new Scanner(System.in);
+        printLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printLine();
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            printLine();
+            System.out.println(input);
+            printLine();
+        }
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+
     }
 }
