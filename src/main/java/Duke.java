debd796f4ca11629c2579c7a4667fcee1e8566b0
jgyj123 <jonathangohyj@gmail.com>
2022-08-17 20:44:58 +0800

    Added command echoing (level -1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner nextCommand = new Scanner(System.in);
+        while (true) {
+            String command = nextCommand.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(command);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
