d26111e294ee0ffe55cc653aa9e0aac018c2fb5c
JONATHAN\Aaron Liew <jonathangohyj@gmail.com>
2022-08-18 12:56:53 +0800

    Add Exception Handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,37 +8,77 @@ public class Duke {
 
         while (true) {
             String command = nextCommand.nextLine();
-            if (command.equals("bye")) {
-                break;
-            }
-            if (command.equals("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    Task nextTask = tasks.get(i);
-                    System.out.println((i + 1) + ". " + nextTask.toString());
+            boolean taskAdded = false;
+            try {
+                if (command.equals("bye")) {
+                    break;
                 }
-            } else if (command.length() == 6 && command.substring(0,4).equals("done")){
-                int completedIndex = Character.getNumericValue(command.charAt(5));
-                Task currentTask = tasks.get(completedIndex - 1);
-                currentTask.setComplete(true);
-                System.out.println("Nice! I've marked this task as done: [✓] " + currentTask.getTaskName());
-            } else {
-                if (command.length() >= 4 && command.substring(0,4).equals("todo")) {
-                    Task newTask = new Todo(command);
-                    tasks.add(newTask);
+                //if command is list
+                if (command.equals("list")) {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        Task nextTask = tasks.get(i);
+                        System.out.println((i + 1) + ". " + nextTask.toString());
+                    }
+                    //else if command is done
+                } else if (command.length() == 6 && command.substring(0,4).equals("done")){
+                    int completedIndex = Character.getNumericValue(command.charAt(5));
+                    Task currentTask = tasks.get(completedIndex - 1);
+                    currentTask.setComplete(true);
+                    System.out.println("Nice! I've marked this task as done: [✓] " + currentTask.getTaskName());
+                } else {
+                    //Todo
+                    if (command.length() >= 4 && command.substring(0,4).equals("todo")) {
+                        try {
+                            if (command.length() == 4) {
+                                throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                            }
+                            Task newTask = new Todo(command);
+                            tasks.add(newTask);
+                            taskAdded = true;
+                        } catch (DukeException e) {
+                            System.out.println(e.getMessage());
+                        }
 
-                } else if (command.length() >= 5 && command.substring(0,5).equals("event")) {
-                    Task newTask = new Event(command);
-                    tasks.add(newTask);
+                        //event
+                    } else if (command.length() >= 5 && command.substring(0,5).equals("event")) {
+                        try {
+                            if (command.length() == 5) {
+                                throw new DukeException("OOPS!!! The description of an event cannot be empty.");
+                            }
+                            Task newTask = new Event(command);
+                            tasks.add(newTask);
+                            taskAdded = true;
+                        } catch (DukeException e) {
+                            System.out.println(e.getMessage());
+                        }
 
-                } else if (command.length() >= 8 && command.substring(0,8).equals("deadline")) {
-                    Task newTask = new Deadline(command);
-                    tasks.add(newTask);
-                }
-                Task addedTask = tasks.get(tasks.size() - 1);
-                System.out.println("Got it. I've added this task: \n" +
-                        addedTask.toString() +
-                        "\nNow you have " +  tasks.size() +  " tasks in the list.");
+                        //deadline
+                    } else if (command.length() >= 8 && command.substring(0,8).equals("deadline")) {
+                        try {
+                            if (command.length() == 8) {
+                                throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                            }
+                            Task newTask = new Deadline(command);
+                            tasks.add(newTask);
+                            taskAdded = true;
+                        } catch (DukeException e) {
+                            System.out.println(e.getMessage());
+                        }
+
+                    } else {
+                        throw new DukeException("Sorry, I don't know what that means");
+                    }
             }
+               if(taskAdded) {
+                   Task addedTask = tasks.get(tasks.size() - 1);
+                   System.out.println("Got it. I've added this task: \n" + addedTask.toString() +
+                           "\nNow you have " +  tasks.size() +  " tasks in the list.");
+               }
+
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            }
+
 
 
         }
