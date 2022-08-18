a0e62e74e9714610584610b32dfc24834aaf77f1
Coco <chuaziyifaith@gmail.com>
2022-08-18 14:20:59 +0800

    Level 2
    @Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     private static String line = "___________________________________";
+    private static List<String> words = new ArrayList<>();
 
     public static void greet() {
         System.out.println(line);
@@ -23,6 +27,25 @@ public class Duke {
         }
     }
 
+    public static void addList(String word) {
+        if (word.equals("bye") || word.equals("Bye")) {
+            System.out.println(line);
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(line);
+            System.exit(0);
+        } else if (word.equals("list") || word.equals("List")) {
+            for (int i = 0; i < words.size(); i++) {
+                int counter = i + 1;
+                System.out.println(counter + ". " + words.get(i));
+            }
+        } else {
+            System.out.println(line);
+            words.add(word);
+            System.out.println("added: " + word);
+            System.out.println(line);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -33,9 +56,11 @@ public class Duke {
         greet();
 
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String nextWord = sc.next();
-            echo((nextWord));
+        while (sc.hasNextLine()) {
+            String nextWord = sc.nextLine();
+            if (!nextWord.equals("")) {
+                addList((nextWord));
+            }
         }
     }
 }
