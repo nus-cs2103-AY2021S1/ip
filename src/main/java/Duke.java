f48d7172d5bee5f7f2e8b8c3c60d1a01ac350aa4
DarrenCsAcc <darrentng@yahoo.com.sg>
2022-08-21 23:52:38 +0800

    v1.0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.*;
+/*Level 1*/
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args){
+        System.out.println("Hello! I'm Duke What can I do for you?");
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            String message = input.nextLine();
+            String ToExit = "bye";
+            if (message.equals(ToExit)){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(message);
+        }
     }
 }
