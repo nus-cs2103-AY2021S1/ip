f4e74248ce38e084c3fea976325ebf40a5ef0f76
jgyj123 <jonathangohyj@gmail.com>
2022-08-24 18:38:31 +0800

    Add date parsing functionality

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Deadline extends Task {
-    private String date;
+    private LocalDate date;
     public Deadline (String s) {
         super(s.split("/by")[0].substring(8).strip());
-        date = s.split("/by")[1].strip();
+        date = LocalDate.parse(s.split("/by")[1].strip());
     }
     @Override
     public String toString() {
         String completion = this.isComplete() ? "[✓]" : "[✗]";
-        return "[D]" + completion + " " +this.getTaskName() + " (by: " + date + ")";
+        return "[D]" + completion + " " + this.getTaskName() + " (by: " +
+                date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String date;
+    private LocalDate date;
     public Event (String s) {
         super(s.split("/at")[0].substring(5).strip());
-        date = s.split("/at")[1].strip();
+        date = LocalDate.parse(s.split("/at")[1].strip());
     }
     @Override
     public String toString() {
         String completion = this.isComplete() ? "[✓]" : "[✗]";
-        return "[E]" + completion + " " + this.getTaskName() + " (at: " + date + ")";
+        return "[E]" + completion + " " + this.getTaskName() + " (at: "
+            + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
