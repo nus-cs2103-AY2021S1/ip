92d373274bcfaa1b409618a54332fd3c3af73933
jgyj123 <jonathangohyj@gmail.com>
2022-08-24 18:50:10 +0800

    Add parsing for dates

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,16 +1,24 @@
 public class Deadline extends Task {
     private String date;
+<<<<<<< HEAD
     public Deadline (String s , boolean isCompleted) {
         super(s.split("/by")[0].substring(8).strip(), isCompleted);
+=======
+    public Deadline (String s) {
+        super(s.split("/by")[0].substring(8).strip());
+>>>>>>> parent of f4e7424 (Add date parsing functionality)
         date = s.split("/by")[1].strip();
     }
     @Override
     public String toString() {
         String completion = this.isComplete() ? "[✓]" : "[✗]";
         return "[D]" + completion + " " +this.getTaskName() + " (by: " + date + ")";
+<<<<<<< HEAD
     }
     public String toFormattedString() {
         String completion = this.isComplete() ? "1" : "0";
         return "D | " + completion + " | " + this.getTaskName() + " | " + date;
+=======
+>>>>>>> parent of f4e7424 (Add date parsing functionality)
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,24 @@
 public class Event extends Task {
     private String date;
+<<<<<<< HEAD
     public Event (String s, boolean isCompleted) {
         super(s.split("/at")[0].substring(5).strip(),isCompleted);
+=======
+    public Event (String s) {
+        super(s.split("/at")[0].substring(5).strip());
+>>>>>>> parent of f4e7424 (Add date parsing functionality)
         date = s.split("/at")[1].strip();
     }
     @Override
     public String toString() {
         String completion = this.isComplete() ? "[✓]" : "[✗]";
         return "[E]" + completion + " " + this.getTaskName() + " (at: " + date + ")";
+<<<<<<< HEAD
     }
     public String toFormattedString() {
         String completion = this.isComplete() ? "1" : "0";
         return "E | " + completion + " | " + this.getTaskName() + " | " + date;
+=======
+>>>>>>> parent of f4e7424 (Add date parsing functionality)
     }
 }
