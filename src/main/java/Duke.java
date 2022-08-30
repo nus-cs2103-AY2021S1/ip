48b8aaf1493bf9ce8886eca23d37ee431dab5376
DarrenCsAcc <darrentng@yahoo.com.sg>
2022-08-31 00:20:56 +0800

    Finished the duke exception leve 1-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import jdk.jfr.Event;
 
 import java.util.*;
+import java.util.stream.Stream;
+
 /*Level 1*/
 /*Need Fixing */
 public class Duke {
@@ -50,177 +52,188 @@ public class Duke {
         return NumberOfItems;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws DukeException {
+
         Openingmessage(); /*Opening Message*/
         Scanner input = new Scanner(System.in);
         int tasktobedone;
 
+            while (true) { //Main start
 
-        while (true) {
+                String message = input.next();
 
-            String message = input.next();
+                //Make sure message is valid
+                if(!(message.equals("list")) &&!(message.equals("todo"))&& !(message.equals("event"))&&!(message.equals("deadline"))){
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-();");
+                }
 
-            String ToDoItem = "todo";
-            if (message.equals(ToDoItem)) {
-                String remainingmessage = input.nextLine();
-                ToDos t = new ToDos(remainingmessage);
-                ListofMessages.add(t);
-                String GotIt = "Got it. I've added this task: ";
-                String DisplayItem = t.getItem();
-                String getStatus = t.getStatusIcon();
-                String DisplayItemWithTask = "  "  +  DisplayItem +"[" + getStatus + "]" + remainingmessage;
-                System.out.println(GotIt);
-                System.out.println(DisplayItemWithTask);
-                System.out.println(NumberOfItemsInList());
+                String ToDoItem = "todo";
+                if (message.equals(ToDoItem)) {
 
-            }
+                    String remainingmessage = input.nextLine();
+                    ToDos t = new ToDos(remainingmessage);
 
-            //Deadline item
-            String DeadLineItem = "deadline";
-            if (message.equals(DeadLineItem)) {
-                String remainingmessage = input.nextLine();
-
-                int i = 0;
-                int lengthofremainingmessage = remainingmessage.length();
-                //Locate the / icon
-                while (true) {
-                    if(remainingmessage.charAt(i) == '/'){
-                        break;
+                    //Fix this to try and catch blocks?
+                    if (remainingmessage.isEmpty()) { //Message is only todo
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
                     }
-                    i = i + 1;
+
+                    ListofMessages.add(t);
+                    String GotIt = "Got it. I've added this task: ";
+                    String DisplayItem = t.getItem();
+                    String getStatus = t.getStatusIcon();
+                    String DisplayItemWithTask = "  " + DisplayItem + "[" + getStatus + "]" + remainingmessage;
+                    System.out.println(GotIt);
+                    System.out.println(DisplayItemWithTask);
+                    System.out.println(NumberOfItemsInList());
+
                 }
 
-                String firsthalf = remainingmessage.substring(1,i);
-                String secondhalf = remainingmessage.substring (i + 4,lengthofremainingmessage);
+                //Deadline item
+                String DeadLineItem = "deadline";
 
-                Deadlines t = new Deadlines(firsthalf,secondhalf);
-                ListofMessages.add(t);
-                String GotIt = "Got it. I've added this task: ";
-                System.out.println(GotIt);
-                //Display item with task
-                String DisplayItem = "  " + t.getItem();
-                String getStatus = t.getStatusIcon();
-                getStatus = "[" + getStatus + "] ";
-                String DisplayItemWithTask = DisplayItem + getStatus + firsthalf + "(" + "by: " + secondhalf + ")" ;
-                System.out.println(DisplayItemWithTask);
-                System.out.println(NumberOfItemsInList());
-            }
+                if (message.equals(DeadLineItem)) {
+                    try {
+                        String remainingmessage = input.nextLine();
 
-            //For events
-            String Eventitem = "event";
-            if (message.equals(Eventitem)) {
-                String remainingmessage = input.nextLine();
+                        int i = 0;
+                        int lengthofremainingmessage = remainingmessage.length();
+                        //Locate the / icon
+                        while (true) {
+                            if (remainingmessage.charAt(i) == '/') {
+                                break;
+                            }
+                            i = i + 1;
+                        }
 
-                int i = 0;
-                int lengthofremainingmessage = remainingmessage.length();
-                //Locate the / icon
-                while (true) {
-                    if(remainingmessage.charAt(i) == '/'){
-                        break;
+                        String firsthalf = remainingmessage.substring(1, i);
+                        String secondhalf = remainingmessage.substring(i + 4, lengthofremainingmessage);
+
+                        Deadlines t = new Deadlines(firsthalf, secondhalf);
+                        ListofMessages.add(t);
+                        String GotIt = "Got it. I've added this task: ";
+                        System.out.println(GotIt);
+                        //Display item with task
+                        String DisplayItem = "  " + t.getItem();
+                        String getStatus = t.getStatusIcon();
+                        getStatus = "[" + getStatus + "] ";
+                        String DisplayItemWithTask = DisplayItem + getStatus + firsthalf + "(" + "by: " + secondhalf + ")";
+                        System.out.println(DisplayItemWithTask);
+                        System.out.println(NumberOfItemsInList());
+                    } catch (StringIndexOutOfBoundsException e) {
+                        throw new DukeException(" ☹ OOPS!!! The description of a deadline cannot be empty.");
                     }
-                    i = i + 1;
+
                 }
 
-                String firsthalf = remainingmessage.substring(1,i);
-                String secondhalf = remainingmessage.substring (i + 4,lengthofremainingmessage);
+                //For events
+                String Eventitem = "event";
+                if (message.equals(Eventitem)) {
 
-                Events t = new Events(firsthalf,secondhalf);
-                ListofMessages.add(t);
-                String GotIt = "Got it. I've added this task: ";
-                System.out.println(GotIt);
-                //Display item with task
-                String DisplayItem = "  " + t.getItem();
-                String getStatus = t.getStatusIcon();
-                getStatus = "[" + getStatus + "] ";
-                String DisplayItemWithTask = DisplayItem + getStatus + firsthalf + "(" + "at: " + secondhalf + ")" ;
-                System.out.println(DisplayItemWithTask);
-                System.out.println(NumberOfItemsInList());
+                    String remainingmessage = input.nextLine();
+
+                    int i = 0;
+                    int lengthofremainingmessage = remainingmessage.length();
+                    //Locate the / icon
+                    while (true) {
+                        if (remainingmessage.charAt(i) == '/') {
+                            break;
+                        }
+                        i = i + 1;
+                    }
+
+                    String firsthalf = remainingmessage.substring(1, i);
+                    String secondhalf = remainingmessage.substring(i + 4, lengthofremainingmessage);
+
+                    Events t = new Events(firsthalf, secondhalf);
+                    ListofMessages.add(t);
+                    String GotIt = "Got it. I've added this task: ";
+                    System.out.println(GotIt);
+                    //Display item with task
+                    String DisplayItem = "  " + t.getItem();
+                    String getStatus = t.getStatusIcon();
+                    getStatus = "[" + getStatus + "] ";
+                    String DisplayItemWithTask = DisplayItem + getStatus + firsthalf + "(" + "at: " + secondhalf + ")";
+                    System.out.println(DisplayItemWithTask);
+                    System.out.println(NumberOfItemsInList());
 
 
-            }
-
-            //String remainingmessage = input.nextLine();
-            //message = message + remainingmessage;
-            //This is for the earlier task for now we will remove it
-
-            //Command is list
-            String ToShowMessagesInArrayList = "list";
-            if (message.equals(ToShowMessagesInArrayList)) {
-                DisplayListOfMessages();
-            }
-
-            //Check if the message is done
-            String CheckIfTaskIsDone = "done";
-            if(message.equals(CheckIfTaskIsDone)){
-                tasktobedone = (input.nextInt()) - 1;
-
-                Task TheTask = (ListofMessages.get(tasktobedone));
-                //Task ToReplace = new Task(TheTask,tasktobedone);
-                //ListofMessages.set(tasktobedone,ToReplace);
-          
-                if(TheTask instanceof ToDos){
-                    TheTask.setStatus();
-                    ListofMessages.set(tasktobedone,TheTask);
-                    System.out.println("Nice! I've marked this task as done");
-                    String ToBeprinted = TheTask.getStatusIcon();
-                    ToBeprinted = "[" + ToBeprinted + "]" + TheTask.getTask();
-                    System.out.println(ToBeprinted);
-                }else if(TheTask instanceof Deadlines){
-                    TheTask.setStatus();
-                    ListofMessages.set(tasktobedone,TheTask);
-                    System.out.println("Nice! I've marked this task as done");
-                    String ToBeprinted = TheTask.getStatusIcon();
-                 //   String ToAdd = ((Deadlines) ListofMessages.get(tasktobedone)).getDeadLineTask();
-                    ToBeprinted = "[" + ToBeprinted + "]"  + TheTask.getTask();
-                    System.out.println(ToBeprinted);
-                }else{
-                    TheTask.setStatus();
-                    ListofMessages.set(tasktobedone,TheTask);
-                    System.out.println("Nice! I've marked this task as done");
-                    String ToBeprinted = TheTask.getStatusIcon();
-                    String ToAdd = ((Events) ListofMessages.get(tasktobedone)).getEventsDescription();
-                //    System.out.println("hello world");
-                   // System.out.println(ToAdd);
-                    ToBeprinted = "[" + ToBeprinted + "]" + TheTask.getTask();
-                    System.out.println(ToBeprinted);
                 }
+
+                //String remainingmessage = input.nextLine();
+                //message = message + remainingmessage;
+                //This is for the earlier task for now we will remove it
+
+                //Command is list
+                String ToShowMessagesInArrayList = "list";
+                if (message.equals(ToShowMessagesInArrayList)) {
+                    DisplayListOfMessages();
+                }
+
+                //Check if the message is done
+                String CheckIfTaskIsDone = "done";
+                if (message.equals(CheckIfTaskIsDone)) {
+                    tasktobedone = (input.nextInt()) - 1;
+
+                    Task TheTask = (ListofMessages.get(tasktobedone));
+                    //Task ToReplace = new Task(TheTask,tasktobedone);
+                    //ListofMessages.set(tasktobedone,ToReplace);
+
+                    if (TheTask instanceof ToDos) {
+                        TheTask.setStatus();
+                        ListofMessages.set(tasktobedone, TheTask);
+                        System.out.println("Nice! I've marked this task as done");
+                        String ToBeprinted = TheTask.getStatusIcon();
+                        ToBeprinted = "[" + ToBeprinted + "]" + TheTask.getTask();
+                        System.out.println(ToBeprinted);
+                    } else if (TheTask instanceof Deadlines) {
+                        TheTask.setStatus();
+                        ListofMessages.set(tasktobedone, TheTask);
+                        System.out.println("Nice! I've marked this task as done");
+                        String ToBeprinted = TheTask.getStatusIcon();
+                        //   String ToAdd = ((Deadlines) ListofMessages.get(tasktobedone)).getDeadLineTask();
+                        ToBeprinted = "[" + ToBeprinted + "]" + TheTask.getTask();
+                        System.out.println(ToBeprinted);
+                    } else {
+                        TheTask.setStatus();
+                        ListofMessages.set(tasktobedone, TheTask);
+                        System.out.println("Nice! I've marked this task as done");
+                        String ToBeprinted = TheTask.getStatusIcon();
+                        String ToAdd = ((Events) ListofMessages.get(tasktobedone)).getEventsDescription();
+                        //    System.out.println("hello world");
+                        // System.out.println(ToAdd);
+                        ToBeprinted = "[" + ToBeprinted + "]" + TheTask.getTask();
+                        System.out.println(ToBeprinted);
+                    }
 
 //                System.out.println("Nice! I've marked this task as done");
 //                String ToBeprinted = ToReplace.getStatusIcon();
 //                ToBeprinted = "[" + ToBeprinted + "]" + ToReplace.getTask();
 //                System.out.println(ToBeprinted);
-            }
+                }
+
+                //Command is Exit
+                String ToExit = "bye";
+                if (message.equals(ToExit)) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                }
+
+
+
+            }//Main End
+
+    }
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-            //Command is Exit
-            String ToExit = "bye";
-            if (message.equals(ToExit)){
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-
-            //Not needed for now used in earlier task
-            //Make sure the message is not the same as any content
+//Not needed for now used in earlier task
+//Make sure the message is not the same as any content
 //            if (!(message.equals("list")) && (!(message.equals(CheckIfTaskIsDone))) && (!(message.equals(ToDoItem)))) {
 //                Task t = new Task(message);
 //                ListofMessages.add(t);/*Stores message in ArrayList if it is not a command*/
 //                String ItemAdded = "added: " + message;
 //                System.out.println(ItemAdded);
 //            }
-
-
-        }
-    }
-}
